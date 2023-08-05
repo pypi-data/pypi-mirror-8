fe15f77b8d22fa4314bbd8e5e012960c437da131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:50 +0000

    Add package prompt-toolkit/0.15/prompt_toolkit-0.15.tar.gz

---
+++ packages/prompt-toolkit/prompt_toolkit-0.15.tar.gz/prompt_toolkit-0.15/prompt_toolkit/contrib/shell/completers.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+import os
+
+from prompt_toolkit.completion import Completion
+
+
+class Path(object):
+    """
+    Complete for Path variables.
+    """
+    _include_files = True
+
+    def complete(self, text):
+        try:
+            directory = os.path.dirname(text) or '.'
+            prefix = os.path.basename(text)
+
+            for filename in os.listdir(directory):
+                if filename.startswith(prefix):
+                    completion = filename[len(prefix):]
+
+                    if os.path.isdir(os.path.join(directory, filename)):
+                        completion += '/'
+                    else:
+                        if not self._include_files:
+                            continue
+
+                    yield Completion(completion, 0, display=filename)
+        except OSError:
+            pass
+
+
+class Directory(Path):
+    _include_files = False
+
---
+++ packages/prompt-toolkit/prompt_toolkit-0.15.tar.gz/prompt_toolkit-0.15/prompt_toolkit/enums.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+
+
+class IncrementalSearchDirection:
+    FORWARD = 'forward'
+    BACKWARD = 'backward'
+
+
+class InputMode(object):
+    INSERT = 'vi-insert'
+
+    VI_NAVIGATION = 'vi-navigation'
+    VI_REPLACE = 'vi-replace'
+
+    # Selection mode. The type of selection (characters/lines/block is saved in
+    # the line object.)
+    SELECTION = 'selection'
+
+    #: Ctrl-R/Ctrl-S incremental search.
+    INCREMENTAL_SEARCH = 'incremental-search'
+
+    #: Vi-style forward search. Usually with a '/' or '?' prompt.
+    VI_SEARCH = 'vi-forward-search'
