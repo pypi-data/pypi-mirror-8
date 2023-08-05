510e890d11d4972c1fe549e1b67b67d6eae4bf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:23 +0000

    Add package ipython-mongo/0.1.2/ipython-mongo-0.1.2.tar.gz

---
+++ packages/ipython-mongo/ipython-mongo-0.1.2.tar.gz/ipython-mongo-0.1.2/setup.py
@@ -0,0 +1,38 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+version = '0.1.2'
+
+install_requires = [
+    'ipython>=1.0',
+]
+
+setup(name='ipython-mongo',
+    version=version,
+    description="MONGODB access via IPython",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: IPython',
+        'Topic :: Database',
+        'Topic :: Database :: Front-Ends',
+        'Programming Language :: Python :: 2',
+    ],
+    keywords='database ipython mongodb shell',
+    author='Yeaji Shin',
+    author_email='yeahjishin@gmail.com',
+    url='https://github.com/Bloodevil/ipython_mongo',
+    download_url='https://github.com/Bloodevil/ipython_mongo/tarball/0.1.2',
+    license='MIT',
+    py_modules=['imongo'],
+    zip_safe=False,
+    install_requires=install_requires,
+)
