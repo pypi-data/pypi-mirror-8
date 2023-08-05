6310be2a68a796ed7583a5af83555d2f47313387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:38 +0000

    Add package biloba/1.1.3/biloba-1.1.3.tar.gz

---
+++ packages/biloba/biloba-1.1.3.tar.gz/biloba-1.1.3/biloba/__init__.py
@@ -0,0 +1,17 @@
+from .service import Service, ConfigurableService
+from .config import parse_address
+from .util import waitany
+
+from . import _meta
+
+
+__version__ = _meta.version
+__version_info__ = _meta.version_info
+
+
+__all__ = [
+    'ConfigurableService',
+    'Service',
+    'parse_address',
+    'waitany',
+]
