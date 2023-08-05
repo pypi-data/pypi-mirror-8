a75e57bde76c1ae61e5c25d97e2aedea33fe545e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:28 +0000

    Add package discomll/0.1.3/discomll-0.1.3.tar.gz

---
+++ packages/discomll/discomll-0.1.3.tar.gz/discomll-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='discomll',
+    version='0.1.3',
+    author='Roman Orac',
+    author_email='orac.roman@gmail.com',
+    url="https://github.com/romanorac/discomll",
+    packages = find_packages(), # adding packages
+    package_data = {"": ["*.txt", "*.sh", "*.csv"]}, #add files from all packages
+    test_suite = "tests",
+    license = 'Apache License, Version 2.0',
+    description = "Disco Machine Learning Library."
+)
