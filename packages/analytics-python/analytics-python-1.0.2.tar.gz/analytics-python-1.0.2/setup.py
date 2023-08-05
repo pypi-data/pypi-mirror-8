1498077d9b3aa9e2a73ba39b94c41a9939f9f2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package analytics-python/1.0.2/analytics-python-1.0.2.tar.gz

---
+++ packages/analytics-python/analytics-python-1.0.2.tar.gz/analytics-python-1.0.2/setup.py
@@ -0,0 +1,42 @@
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Don't import analytics-python module here, since deps may not be installed
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'analytics'))
+from version import VERSION
+
+long_description = '''
+Segment.io is the simplest way to integrate analytics into your application.
+One API allows you to turn on any other analytics service. No more learning
+new APIs, repeated code, and wasted development time.
+
+This is the official python client that wraps the Segment.io REST API (https://segment.io).
+
+Documentation and more details at https://github.com/segmentio/analytics-python
+'''
+
+setup(
+    name='analytics-python',
+    version=VERSION,
+    url='https://github.com/segmentio/analytics-python',
+    author='Segment.io',
+    author_email='friends@segment.io',
+    maintainer='Segment.io',
+    maintainer_email='friends@segment.io',
+    test_suite='analytics.test.all',
+    packages=['analytics', 'analytics.test'],
+    license='MIT License',
+    install_requires=[
+        'python-dateutil',
+        'requests',
+        'six'
+    ],
+    description='The hassle-free way to integrate analytics into any python application.',
+    long_description=long_description
+)
