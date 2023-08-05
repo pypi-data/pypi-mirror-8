b26ed8a002930f256193d962194fab6222ebb1d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package asyncoro/3.0/asyncoro-3.0.tar.gz

---
+++ packages/asyncoro/asyncoro-3.0.tar.gz/asyncoro-3.0/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(name='asyncoro',
+      version='3.0',
+      description='This module provides framework for concurrent, asynchronous network programming with coroutines, asynchronous completions and message passing.',
+      long_description=open('README').read(),
+      url='http://asyncoro.sourceforge.net',
+      author='Giridhar Pemmasani',
+      author_email='pgiri@yahoo.com',
+      license='The MIT License: http://www.opensource.org/licenses/mit-license.php',
+      platforms='any',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.1',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Software Development', ]
+      )
