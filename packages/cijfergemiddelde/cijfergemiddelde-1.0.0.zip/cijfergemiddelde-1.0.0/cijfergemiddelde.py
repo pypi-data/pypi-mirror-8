e7eda64811a550fa1f0635fca87cd66b1f2e5279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:39 +0000

    Add package cijfergemiddelde/1.0.0/cijfergemiddelde-1.0.0.zip

---
+++ packages/cijfergemiddelde/cijfergemiddelde-1.0.0.zip/cijfergemiddelde-1.0.0/cijfergemiddelde.py
@@ -0,0 +1,17 @@
+'''Dit is een cijfergemiddelde programma zoals berekend wordt
+op het Calandlyceum in Amsterdam.'''
+
+def print_cijfergemiddelde(cijfers, gewichten):
+    '''Deze functie heeft twee lijsten als argument.
+    Een lijst met behaalde cijfers en een lijst met
+    bijbehorende gewichten.'''
+
+    som = 0 # Hierin opgeslagen alle cijfers vermenigvuldigt met bijbehorende gewichten
+    totaal = 0 # Hierin opgeslagen de opgetelde gewichten
+    for c, g in zip(cijfers, gewichten):
+      som = som + float(c) * int(g)
+      totaal = totaal + int(g)
+    gemiddelde = format(som / totaal, '.1f') #uitrekenen gemiddelde, afgerond op 1 decimaal
+    print(gemiddelde)
+
+
