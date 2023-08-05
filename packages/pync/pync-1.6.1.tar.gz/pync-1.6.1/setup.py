f81b018be3ca49772b2bde6acbb04932622be227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package pync/1.6.1/pync-1.6.1.tar.gz

---
+++ packages/pync/pync-1.6.1.tar.gz/pync-1.6.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+file_contents = []
+for file_name in ('README.md',):
+    path = os.path.join(os.path.dirname(__file__), file_name)
+    file_contents.append(open(path).read())
+long_description = '\n\n'.join(file_contents)
+
+terminal_notifier_files = []
+for root, dirs, files in os.walk('pync/vendor/'):
+    root = '/'.join(root.split('/')[1:])
+    for f in files:
+        terminal_notifier_files.append(os.path.join(root, f))
+
+setup(name = 'pync',
+    version = "1.6.1",
+    description = 'Python Wrapper for Mac OS 10.8 Notification Center',
+    long_description = long_description,
+    author = 'Vladislav Syabruk',
+    author_email = 'sjabrik@gmail.com',
+    url = 'https://github.com/setem/pync',
+    download_url = 'https://github.com/SeTeM/pync/archive/v1.6.1.zip',
+    license = "MIT",
+    platforms = "MacOS X",
+    keywords = "mac notification center wrapper",
+    zip_safe = True,
+    include_package_data = True,
+    install_requires = [
+        'python-dateutil>=2.0'
+    ],
+    packages = find_packages(),
+    classifiers = [
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
