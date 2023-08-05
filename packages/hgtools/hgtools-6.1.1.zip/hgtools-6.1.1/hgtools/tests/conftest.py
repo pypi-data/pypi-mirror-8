be875f43bb145305337ea6759939ab8e4d574033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:02 +0000

    Add package hgtools/6.1.1/hgtools-6.1.1.zip

---
+++ packages/hgtools/hgtools-6.1.1.zip/hgtools-6.1.1/hgtools/tests/conftest.py
@@ -0,0 +1,41 @@
+import os
+
+import pytest
+
+from hgtools import managers
+
+@pytest.fixture
+def hg_repo(tmpdir):
+	tmpdir.chdir()
+	mgr = managers.MercurialManager()
+	mgr._invoke('init', '.')
+	os.makedirs('bar')
+	touch('bar/baz')
+	mgr._invoke('addremove')
+	mgr._invoke('ci', '-m', 'committed')
+	with open('bar/baz', 'w') as baz:
+		baz.write('content')
+	mgr._invoke('ci', '-m', 'added content')
+	return tmpdir
+
+
+@pytest.fixture
+def git_repo(tmpdir):
+	tmpdir.chdir()
+	mgr = managers.GitManager()
+	mgr._invoke('init')
+	mgr._invoke('config', 'user.email', 'hgtools@example.com')
+	mgr._invoke('config', 'user.name', 'HGTools')
+	os.makedirs('bar')
+	touch('bar/baz')
+	mgr._invoke('add', '.')
+	mgr._invoke('commit', '-m', 'committed')
+	with open('bar/baz', 'w') as baz:
+		baz.write('content')
+	mgr._invoke('commit', '-am', 'added content')
+	return tmpdir
+
+
+def touch(filename):
+	with open(filename, 'a'):
+		pass
