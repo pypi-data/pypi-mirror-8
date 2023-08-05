bb914142de06fe5e2d45aeca4629efd2780d3aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:07 +0000

    Add package INSDCalculator/1.0.0/INSDCalculator-1.0.0.zip

---
+++ packages/INSDCalculator/INSDCalculator-1.0.0.zip/INSDCalculator-1.0.0/INSDCalculator.py
@@ -0,0 +1,11 @@
+def Addition(x, y):
+	return x + y;
+
+def Subtraction(x, y):
+	return x - y;
+
+def Multiplication(x, y):
+	return x * y;
+
+def Division(x, y):
+	return x / y;
---
+++ packages/INSDCalculator/INSDCalculator-1.0.0.zip/INSDCalculator-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup;
+
+setup(
+	name = "INSDCalculator",
+	version = "1.0.0",
+	py_modules = ["INSDCalculator"],
+	author = "imnsilvadev",
+	author_email = "imnsilvadev@gmail.com",
+	url = "",
+	description = "Simple Calculator - Addition, Subtraction, Multiplication, Division"
+);
