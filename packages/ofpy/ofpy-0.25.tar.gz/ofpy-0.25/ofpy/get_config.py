86a8daa1be69441998499a4703b6ab61323248ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package ofpy/0.25/ofpy-0.25.tar.gz

---
+++ packages/ofpy/ofpy-0.25.tar.gz/ofpy-0.25/ofpy/get_config.py
@@ -0,0 +1,89 @@
+"""get_config.py
+Imports the config for ofpy, or tries to create one and exits if it doesn't
+exist yet.
+"""
+
+
+import os.path
+import logging
+import sys
+
+try:
+    from configparser import SafeConfigParser
+except ImportError:
+    from ConfigParser import SafeConfigParser
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+
+logger_name = str(__file__) + " :: " + str(__name__)
+logger = logging.getLogger(logger_name)
+
+# Defaults to ~/.ofpy_config
+custom_config_dir = None
+
+
+def make_config(config_path):
+    default_config = '''# Config file for ofpy
+# https://github.com/n8henrie/ofpy
+
+[MAILDROP]
+# Your OmniFocus Maildrop address
+maildrop_address = example@sync.omnigroup.com
+
+[EMAIL]
+# Your email server info, defaults for Gmail
+# Escape % in your password with another %
+username = example@gmail.com
+password = my_awesome_password
+port = 587
+server_address = smtp.gmail.com
+
+[DROPBOX]
+# The Dropbox folder that Hazel monitors
+task_folder = ~/Dropbox
+
+[EDITOR]
+# Your preferred editor. If blank, will use your $EDITOR, or default to vim.
+editor = vim
+'''
+
+    with open(config_path, 'w') as w:
+        w.write(default_config)
+
+
+def get_config():
+    config = SafeConfigParser()
+
+    try:
+        custom_config_dir = os.path.expanduser(custom_config_dir)
+        config_path = os.path.join(custom_config_dir, '.ofpy_config')
+    except UnboundLocalError:
+        home_dir = os.path.expanduser('~')
+        config_path = os.path.join(home_dir, '.ofpy_config')
+
+    if not os.path.exists(config_path):
+        try:
+            make_config(config_path)
+
+        except Exception as e:
+            logger.exception("It doesn't look like you have a config file, but"
+                             " I wasn't able to create the template for you.")
+        else:
+            msg = ('Config file did not exist, so I created a template'
+                   ' at ~/.ofpy_config. Please edit with your config.')
+            logger.warning(msg)
+            sys.exit(0)
+
+    logger.debug("Parsing config file {}".format(config_path))
+    with open(config_path) as r:
+        try:
+            config.read_file(r)
+        except AttributeError:
+            config.readfp(r)
+
+    return config
---
+++ packages/ofpy/ofpy-0.25.tar.gz/ofpy-0.25/ofpy/ofpy.py
@@ -0,0 +1,98 @@
+"""ofpy.py
+A small module to add tasks to OmniFocus from the command line
+Designed with the goal of making it easy to add OF tasks from Linux when I'm 
+away from my Mac.
+
+Config file: ~/.ofpy_config (will create on first run).
+
+Use `ofpy` without arguments to open a task in your editor.
+Use `ofpy "My task"` to add a task directly with OmniGroup's MailDrop.
+"""
+
+import sys
+import datetime
+import os.path
+import subprocess
+import logging
+
+try:
+    from ofpy.get_config import get_config
+    from ofpy.internet_on import internet_on
+    from ofpy.maildrop import maildrop
+except ImportError as e:
+    from get_config import get_config
+    from internet_on import internet_on
+    from maildrop import maildrop
+    
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+logger_name = str(__file__) + " :: " + str(__name__)
+logger = logging.getLogger(logger_name)
+
+logger.debug(sys.version)
+
+def internet_is_on():
+    try:
+        internet_on()
+        return True
+    except:
+        return False
+
+
+def set_task_path(config):
+    now = datetime.datetime.now()
+    ts = now.strftime('%Y%m%dT%H%M%S')
+
+    task_name = '{}.txt'.format(ts)
+    task_folder = os.path.expanduser(config.get('DROPBOX', 'task_folder'))
+    task_path = os.path.join(task_folder, task_name)
+    return task_path
+
+
+def main():
+
+    try:
+        logger.debug("Getting config.")
+        config = get_config()
+    except Exception as e:
+        logger.exception('Config file problem. If this is your first run,'
+                         ' you may need to edit ~/.ofpy_config.')
+
+    logger.debug("sys.argv length: {}".format(len(sys.argv)))
+
+    if len(sys.argv) == 1:
+        # No arguments given with the script, so make a new task in editor.
+
+        task_path = set_task_path(config)
+        editor = config.get('EDITOR', 'editor')
+
+        logger.debug("Calling editor {} to path {}".format(editor, task_path))
+
+        subprocess.call([editor, task_path])
+
+    elif len(sys.argv) > 1:
+        # Arguments given with script, so assume a one-liner. Email using
+        # maildrop given an internet connection, otherwise write to a file as
+        # per above.
+
+        task_list = sys.argv[1:]
+        task = ' '.join(task_list)
+
+        if internet_is_on():
+            logger.debug("Internet is on. Passing to maildrop:\ntask: "
+                         "{}\nconfig:{}".format(task, config))
+            maildrop(task, config)
+        else:
+            logger.debug("Internet is off. Writing to file.")
+
+            task_path = set_task_path(config)
+            with open(task_path, 'w') as f:
+                f.write(task)
+
+if __name__ == '__main__':
+    main()
