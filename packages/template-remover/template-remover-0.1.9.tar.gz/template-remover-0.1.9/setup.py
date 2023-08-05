c08507e51b70884925387a603cda8355e4d7a19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:01 +0000

    Add package template-remover/0.1.9/template-remover-0.1.9.tar.gz

---
+++ packages/template-remover/template-remover-0.1.9.tar.gz/template-remover-0.1.9/setup.py
@@ -0,0 +1,44 @@
+# Copyright 2014 Deezer (http://www.deezer.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='template-remover',
+    version='0.1.9',
+    description='Remove the template markup from html files',
+    long_description=open('README.rst').read(),
+    author='Sebastian Kreft - Deezer',
+    author_email='skreft@deezer.com',
+    url='http://github.com/deezer/template-remover',
+    py_modules=['template_remover'],
+    install_requires=['docopt>=0.6.1'],
+    tests_require=['nose>=1.3'],
+    scripts=['scripts/remove_template.py'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+    ],
+)
