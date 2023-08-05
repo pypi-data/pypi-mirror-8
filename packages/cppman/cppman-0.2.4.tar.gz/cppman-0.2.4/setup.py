ccc133447dbf9054636c174aa49c131adc27f887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:13 +0000

    Add package cppman/0.2.4/cppman-0.2.4.tar.gz

---
+++ packages/cppman/cppman-0.2.4.tar.gz/cppman-0.2.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+_data_files = [
+	('lib/cppman', ['lib/index.db', 'lib/pager_vim.sh', 'lib/pager_less.sh',
+                        'lib/cppman.vim']),
+        ('share/doc/cppman',  ['README.md', 'AUTHORS', 'COPYING', 'ChangeLog']),
+	('share/man/man1', ['misc/cppman.1'])
+	]
+
+setup(
+	name = 'cppman',
+	version = '0.2.4',
+	description = 'C++ man pages generater that generates C++ man pages'
+                      'from cplusplus.com',
+	author = 'Wei-Ning Huang (AZ)',
+	author_email = 'aitjcize@gmail.com',
+        url = 'http://github.com/Aitjcize/cppman',
+	license = 'GPL',
+    	packages = ['cppman'],
+	scripts = ['bin/cppman'],
+	data_files = _data_files
+)
