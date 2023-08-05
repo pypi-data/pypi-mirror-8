b32352e176cb2262ed1888a1d1b542d2e5724ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:06 +0000

    Add package Beeswarm/0.5.0/Beeswarm-0.5.0.tar.gz

---
+++ packages/Beeswarm/Beeswarm-0.5.0.tar.gz/Beeswarm-0.5.0/beeswarm/server/db/session_persister.py
@@ -0,0 +1,140 @@
+# Copyright (C) 2014 Johnny Vestergaard <jkv@unixcluster.dk>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import logging
+from datetime import datetime
+
+import zmq.green as zmq
+import gevent
+from gevent import Greenlet
+
+import beeswarm
+import beeswarm.shared
+from beeswarm.server.db import database_setup
+from beeswarm.server.db.entities import Client, BaitSession, Session, Honeypot, Authentication, Classification, \
+    Transcript
+from beeswarm.shared.helpers import send_zmq_request_socket
+from beeswarm.shared.message_enum import Messages
+
+
+logger = logging.getLogger(__name__)
+
+
+class SessionPersister(gevent.Greenlet):
+    def __init__(self, clear_sessions=False):
+        Greenlet.__init__(self)
+        db_session = database_setup.get_session()
+        # clear all pending sessions on startup, pending sessions on startup
+        pending_classification = db_session.query(Classification).filter(Classification.type == 'pending').one()
+        pending_deleted = db_session.query(Session).filter(Session.classification == pending_classification).delete()
+        db_session.commit()
+        logging.info('Cleaned {0} pending sessions on startup'.format(pending_deleted))
+        if clear_sessions:
+            count = db_session.query(Session).delete()
+            logging.info('Deleting {0} sessions on startup.'.format(count))
+            db_session.commit()
+        context = beeswarm.shared.zmq_context
+        self.subscriber_sessions = context.socket(zmq.SUB)
+        self.subscriber_sessions.connect('inproc://sessionPublisher')
+        self.subscriber_sessions.setsockopt(zmq.SUBSCRIBE, '')
+
+        self.config_actor_socket = context.socket(zmq.REQ)
+        self.config_actor_socket.connect('inproc://configCommands')
+
+    def _run(self):
+        poller = zmq.Poller()
+        poller.register(self.subscriber_sessions, zmq.POLLIN)
+        while True:
+            # .recv() gives no context switch - why not? using poller with timeout instead
+            socks = dict(poller.poll(100))
+            gevent.sleep()
+
+            if self.subscriber_sessions in socks and socks[self.subscriber_sessions] == zmq.POLLIN:
+                topic, session_json = self.subscriber_sessions.recv().split(' ', 1)
+                logger.debug('Received message from publisher')
+                self.persist_session(session_json, topic)
+
+    def persist_session(self, session_json, session_type):
+        try:
+            data = json.loads(session_json)
+        except UnicodeDecodeError:
+            data = json.loads(unicode(session_json, "ISO-8859-1"))
+        logger.debug('Persisting {0} session: {1}'.format(session_type, data))
+        db_session = database_setup.get_session()
+        classification = db_session.query(Classification).filter(Classification.type == 'pending').one()
+        if data['honeypot_id'] is not None:
+            _honeypot = db_session.query(Honeypot).filter(Honeypot.id == data['honeypot_id']).one()
+        else:
+            _honeypot = None
+
+        if session_type == Messages.SESSION_HONEYPOT:
+            session = Session()
+            for entry in data['transcript']:
+                transcript_timestamp = datetime.strptime(entry['timestamp'], '%Y-%m-%dT%H:%M:%S.%f')
+                transcript = Transcript(timestamp=transcript_timestamp, direction=entry['direction'],
+                                        data=entry['data'])
+                session.transcript.append(transcript)
+
+            for auth in data['login_attempts']:
+                authentication = self.extract_auth_entity(auth)
+                session.authentication.append(authentication)
+
+        elif session_type == Messages.SESSION_CLIENT:
+            ignore_failed_bait_sessions = self.send_config_request('{0} {1}'.format(Messages.GET_CONFIG_ITEM,
+                                                                                    'ignore_failed_bait_session'))
+            if not data['did_complete'] and ignore_failed_bait_sessions:
+                return
+            session = BaitSession()
+            client = db_session.query(Client).filter(Client.id == data['client_id']).one()
+            client.last_activity = datetime.now()
+            session.did_connect = data['did_connect']
+            session.did_login = data['did_login']
+            session.did_complete = data['did_complete']
+            session.client = client
+            for auth in data['login_attempts']:
+                authentication = self.extract_auth_entity(auth)
+                session.authentication.append(authentication)
+        else:
+            logger.warn('Unknown message type: {0}'.format(session_type))
+            return
+
+        session.id = data['id']
+        session.classification = classification
+        session.timestamp = datetime.strptime(data['timestamp'], '%Y-%m-%dT%H:%M:%S.%f')
+        session.received = datetime.utcnow()
+        session.protocol = data['protocol']
+        session.destination_ip = data['destination_ip']
+        session.destination_port = data['destination_port']
+        session.source_ip = data['source_ip']
+        session.source_port = data['source_port']
+        session.honeypot = _honeypot
+
+        db_session.add(session)
+        db_session.commit()
+
+    def extract_auth_entity(self, auth_data):
+        username = auth_data.get('username', '')
+        password = auth_data.get('password', '')
+        authentication = Authentication(id=auth_data['id'], username=username, password=password,
+                                        successful=auth_data['successful'],
+                                        timestamp=datetime.strptime(auth_data['timestamp'], '%Y-%m-%dT%H:%M:%S.%f'))
+        return authentication
+
+    def send_config_request(self, request):
+            return send_zmq_request_socket(self.config_actor_socket, request)
+
+
+
---
+++ packages/Beeswarm/Beeswarm-0.5.0.tar.gz/Beeswarm-0.5.0/beeswarm/server/misc/classifier.py
@@ -0,0 +1,149 @@
+# Copyright (C) 2013 Johnny Vestergaard <jkv@unixcluster.dk>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import datetime
+
+from sqlalchemy.orm import joinedload
+
+from beeswarm.server.db import database_setup
+from beeswarm.server.db.entities import Classification, BaitSession, Session, Authentication
+
+
+logger = logging.getLogger(__name__)
+
+
+class Classifier(object):
+    def __init(self):
+        self.enabled = True
+
+    # match bait session with session
+    def get_matching_session(self, bait_session, timediff=5, db_session=None):
+        """
+        Provided a bait_session object a matching session is returned. If no matching
+        session is found None is returned.
+
+        :param bait_session: bait_session object which will be used as base for query.
+        :param timediff: +/- allowed time difference between a bait_session and a potential matching session.
+        """
+        min_datetime = bait_session.timestamp - datetime.timedelta(seconds=timediff)
+        max_datetime = bait_session.timestamp + datetime.timedelta(seconds=timediff)
+
+        if not db_session:
+            db_session = database_setup.get_session()
+
+        # default return value
+        match = None
+
+        # get all sessions that matches basic properties.
+        sessions = db_session.query(Session).options(joinedload(Session.authentication)) \
+            .filter(Session.protocol == bait_session.protocol) \
+            .filter(Session.honeypot == bait_session.honeypot) \
+            .filter(Session.discriminator == None) \
+            .filter(Session.timestamp >= min_datetime) \
+            .filter(Session.timestamp <= max_datetime)
+
+        # identify the correct session by comparing authentication.
+        # this could properly also be done using some fancy ORM/SQL construct.
+        for session in sessions:
+            for honey_auth in bait_session.authentication:
+                for session_auth in session.authentication:
+                    if session_auth.username == honey_auth.username and \
+                                    session_auth.password == honey_auth.password and \
+                                    session_auth.successful == honey_auth.successful:
+                        match = session
+                        break
+
+        return match
+
+    def classify_bait_session(self, delay_seconds=10, db_session=None):
+        """
+        Will classify all unclassified bait_sessions as either legit or malicious activity. A bait session can e.g. be classified
+        as involved in malicious activity if the bait session is subject to a MiTM attack.
+
+        :param delay_seconds: no bait_sessions newer than (now - delay_seconds) will be processed.
+        """
+        min_datetime = datetime.datetime.utcnow() - datetime.timedelta(seconds=delay_seconds)
+
+        if not db_session:
+            db_session = database_setup.get_session()
+
+        bait_sessions = db_session.query(BaitSession).options(joinedload(BaitSession.authentication)) \
+            .filter(BaitSession.classification_id == 'pending') \
+            .filter(BaitSession.did_complete == True) \
+            .filter(BaitSession.timestamp < min_datetime).all()
+
+        for bait_session in bait_sessions:
+            session_match = self.get_matching_session(bait_session, db_session=db_session)
+            # if we have a match this is legit bait session
+            if session_match:
+                logger.debug('Classifying bait session with id {0} as legit bait session and deleting '
+                             'matching session with id {1}'.format(bait_session.id, session_match.id))
+                bait_session.classification = db_session.query(Classification).filter(
+                    Classification.type == 'bait_session').one()
+                bait_session.transcript = session_match.transcript
+                bait_session.session_data = session_match.session_data
+                db_session.add(bait_session)
+                db_session.delete(session_match)
+            # else we classify it as a MiTM attack
+            else:
+                logger.debug('Classifying bait session with id {0} as MITM'.format(bait_session.id))
+                bait_session.classification = db_session.query(Classification).filter(
+                    Classification.type == 'mitm').one()
+
+        db_session.commit()
+
+    def classify_sessions(self, delay_seconds=30, db_session=None):
+        """
+        Will classify all sessions (which are not bait session) as malicious activity. Note: The classify_bait_session method
+        should be called before this method.
+
+        :param delay_seconds: no sessions newer than (now - delay_seconds) will be processed.
+        """
+        min_datetime = datetime.datetime.utcnow() - datetime.timedelta(seconds=delay_seconds)
+
+        if not db_session:
+            db_session = database_setup.get_session()
+
+        sessions = db_session.query(Session).filter(Session.discriminator == None) \
+            .filter(Session.timestamp <= min_datetime) \
+            .filter(Session.classification_id == 'pending') \
+            .all()
+
+        for session in sessions:
+            bait_match = None
+            for a in session.authentication:
+                bait_match = db_session.query(BaitSession)\
+                    .filter(BaitSession.authentication.any(username=a.username, password=a.password)).first()
+                if bait_match:
+                    break
+
+            if bait_match:
+                logger.debug('Classifying session with id {0} as attack which involved the reused '
+                             'of previously transmitted credentials.'.format(session.id))
+                session.classification = db_session.query(Classification).filter(
+                    Classification.type == 'credentials_reuse').one()
+            elif len(session.authentication) == 0:
+                logger.debug('Classifying session with id {0} as probe.'.format(session.id))
+                session.classification = db_session.query(Classification).filter(Classification.type == 'probe').one()
+            else:
+                # we have never transmitted this username/password combo
+                logger.debug('Classifying session with id {0} as bruteforce attempt.'.format(session.id))
+                session.classification = db_session.query(Classification).filter(
+                    Classification.type == 'bruteforce').one()
+        db_session.commit()
+
+    def stop(self):
+        self.enabled = False
