abf4e35a0dd5e70fe4822977d2d9cb3517c9d7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:24 +0000

    Add package udplogviewer/0.0.1/udplogviewer-0.0.1.tar.gz

---
+++ packages/udplogviewer/udplogviewer-0.0.1.tar.gz/udplogviewer-0.0.1/setup.py
@@ -0,0 +1,9 @@
+
+from distutils.core import setup
+
+setup(name='udplogviewer',
+        version='0.0.1',
+        description='Udp log message receiver and viewer',
+        author='Windel Bouwman',
+        scripts=['udplogreceiver.py']
+     )
