c2634e0aed524e50e59f839db650b0c46d5ea4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:26 +0000

    Add package FlexGet/1.2.207/FlexGet-1.2.207.tar.gz

---
+++ packages/FlexGet/FlexGet-1.2.207.tar.gz/FlexGet-1.2.207/tests/test_tmdb.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals, division, absolute_import
+from tests import FlexGetBase
+from nose.plugins.attrib import attr
+
+
+class TestTmdbLookup(FlexGetBase):
+
+    __yaml__ = """
+        tasks:
+          test:
+            mock:
+              - {title: '[Group] Taken 720p', imdb_url: 'http://www.imdb.com/title/tt0936501/'}
+              - {title: 'The Matrix'}
+            tmdb_lookup: yes
+            # Access a field to cause lazy loading to occur
+            set:
+              afield: "{{ tmdb_id }}"
+    """
+
+    @attr(online=True)
+    def test_tmdb_lookup(self):
+        self.execute_task('test')
+        # check that these were created
+        assert self.task.find_entry(tmdb_name='Taken', tmdb_year=2008), 'Didn\'t populate tmdb info for Taken'
+        assert self.task.find_entry(tmdb_name='The Matrix', tmdb_year=1999), \
+                'Didn\'t populate tmdb info for The Matrix'
