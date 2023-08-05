ed55d833e38e7efdccb1c63ebac0a1ea884fa249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:23 +0000

    Add package pyqode.core/2.3.0/pyqode.core-2.3.0.tar.gz

---
+++ packages/pyqode.core/pyqode.core-2.3.0.tar.gz/pyqode.core-2.3.0/conftest.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+This scripts configures the test suite. We do two things:
+
+    - setup the logging module
+    - create ONE SINGLE INSTANCE of QApplication:
+      this implies that you must use **QApplication.instance** in your
+      test scripts.
+"""
+import logging
+import os
+import sys
+try:
+    import faulthandler
+    faulthandler.enable()
+except ImportError:
+    pass
+import pytest
+from pyqode.core.api.code_edit import CodeEdit
+
+
+# -------------------
+# Setup runtest
+# -------------------
+def pytest_runtest_setup(item):
+    """
+    Display test method name in active window title bar
+    """
+    global _widget
+    module, line, method = item.location
+    module = module.replace('.py', '.')
+    title = module + method
+    widgets = QApplication.instance().topLevelWidgets()
+    for w in widgets:
+        w.setWindowTitle(title)
+    logging.info("------------------- %s -------------------", title)
+
+
+# -------------------
+# Setup logging
+# -------------------
+logging.basicConfig(level=logging.DEBUG,
+                    filename='pytest.log',
+                    filemode='w')
+
+# -------------------
+# Setup QApplication
+# -------------------
+# 2. create qt application
+from pyqode.qt.QtWidgets import QApplication
+_app = QApplication(sys.argv)
+_widget = None
+
+
+# -------------------
+# Session fixtures
+# -------------------
+@pytest.fixture(scope="session")
+def app(request):
+    global _app
+    return app
+
+
+@pytest.fixture(scope="session")
+def editor(request):
+    global _app, _widget
+    from test import helpers
+
+    logging.info('setup session editor')
+
+    _widget = CodeEdit()
+    _widget.backend.start(helpers.server_path())
+    helpers.wait_for_connected(_widget)
+    helpers.setup_editor(_widget)
+    _widget.show()
+    _widget.resize(800, 600)
+    _app.setActiveWindow(_widget)
+    _widget.save_on_focus_out = False
+
+    def fin():
+        global _widget
+        logging.info('teardown session editor')
+        _widget.backend.stop()
+        del _widget
+
+    request.addfinalizer(fin)
+
+    return _widget
---
+++ packages/pyqode.core/pyqode.core-2.3.0.tar.gz/pyqode.core-2.3.0/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Setup script for pyqode.core
+"""
+from setuptools import setup, find_packages
+
+#
+# add ``build_ui command`` (optional, for development only)
+# this command requires the following packages:
+#   - pyqt_distutils
+#   - pyqode-uic
+#
+try:
+    from pyqt_distutils.build_ui import build_ui
+    cmdclass = {'build_ui': build_ui}
+except ImportError:
+    cmdclass = {}
+
+
+def read_version():
+    with open("pyqode/core/__init__.py") as f:
+        lines = f.read().splitlines()
+        for l in lines:
+            if "__version__" in l:
+                return l.split("=")[1].strip().replace("'", '')
+
+
+def readme():
+    return str(open('README.rst').read())
+
+
+setup(
+    name='pyqode.core',
+    namespace_packages=['pyqode'],
+    version=read_version(),
+    packages=[p for p in find_packages() if 'test' not in p],
+    keywords=["CodeEdit PyQt source code editor widget qt"],
+    url='https://github.com/pyQode/pyqode.core',
+    license='MIT',
+    author='Colin Duquesnoy',
+    author_email='colin.duquesnoy@gmail.com',
+    description='Python/Qt Code Editor widget',
+    long_description=readme(),
+    install_requires=['pygments', 'pyqode.qt', 'future'],
+    entry_points={
+        'console_scripts': [
+            'pyqode-console = pyqode.core.tools.console:main'
+        ],
+        'pyqode_plugins':
+            ['code_edit = pyqode.core._designer_plugins'],
+        'pygments.styles':
+            ['qt = pyqode.core.styles.qt:QtStyle',
+             'darcula = pyqode.core.styles.darcula:DarculaStyle']
+    },
+    zip_safe=False,
+    cmdclass=cmdclass,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: X11 Applications :: Qt',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Widget Sets',
+        'Topic :: Text Editors :: Integrated Development Environments (IDE)'])
