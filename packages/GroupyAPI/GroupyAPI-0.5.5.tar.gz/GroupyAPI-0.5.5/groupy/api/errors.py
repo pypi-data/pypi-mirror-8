bcb5f861143a43ad84e1c81467bf81733f7baeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:20 +0000

    Add package GroupyAPI/0.5.5/GroupyAPI-0.5.5.tar.gz

---
+++ packages/GroupyAPI/GroupyAPI-0.5.5.tar.gz/GroupyAPI-0.5.5/groupy/api/errors.py
@@ -0,0 +1,28 @@
+"""
+.. module:: errors
+   :platform: Unix, Windows
+   :synopsis: Module containing all GroupMe related error classes.
+
+.. moduleauthor:: Robert Grant <rhgrant10@gmail.com>
+
+The ``error`` module contains all of the exceptions thrown by the
+GroupMe API.
+
+"""
+
+class GroupMeError(Exception):
+    """A general GroupMe error.
+    """
+    pass
+
+
+class InvalidResponseError(GroupMeError):
+    """Error representing an unparsable response from the API.
+    """
+    pass
+
+
+class InvalidOperatorError(NotImplementedError):
+    """Error thrown when an unsupported filter is used.
+    """
+    pass
---
+++ packages/GroupyAPI/GroupyAPI-0.5.5.tar.gz/GroupyAPI-0.5.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name = 'GroupyAPI',
+    packages = ['groupy', 'groupy.object', 'groupy.api'],
+    version = '0.5.5',
+    install_requires = [
+        'requests==2.3.0',
+        'Pillow==2.5.3'
+    ],
+    description = 'The simple yet powerful wrapper for the GroupMe API',
+    author = 'Robert Grant',
+    author_email = 'rhgrant10@gmail.com',
+    url = 'https://github.com/rhgrant10/Groupy', # use the URL to the github repo
+    keywords = ['api', 'GroupMe'], # arbitrary keywords
+    classifiers = ['Programming Language :: Python :: 3'],
+    long_description=open('README.rst', 'r').read()
+)
