9b2ab762a1e0ae3fcd4aa5fb82ebf55e3e2e051c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:45 +0000

    Add package xlwings/0.2.3/xlwings-0.2.3.tar.gz

---
+++ packages/xlwings/xlwings-0.2.3.tar.gz/xlwings-0.2.3/xlwings/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+import sys
+
+__version__ = '0.2.3'
+
+# Python 2 vs 3
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    string_types = str
+else:
+    string_types = basestring
+
+# Platform specifics
+if sys.platform.startswith('win'):
+    from . import _xlwindows as xlplatform
+else:
+    from . import _xlmac as xlplatform
+
+time_types = xlplatform.time_types
+
+# API
+from .main import Workbook, Range, Chart, Sheet
+from .constants import *
