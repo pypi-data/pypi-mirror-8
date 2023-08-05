bb2498a62fb55ee9af74e8b4fd436fd95ac4c3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package feincms-banners/1.2.0/feincms_banners-1.2.0-py2-none-any.whl

---
+++ packages/feincms-banners/feincms_banners-1.2.0-py2-none-any.whl/feincms_banners-1.2.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,8 @@
+===========================================================================
+feincms-banners -- a simple banner system with views and clicks for FeinCMS
+===========================================================================
+
+.. image:: https://travis-ci.org/feincms/feincms-banners.png?branch=master
+   :target: https://travis-ci.org/feincms/feincms-banners
+
+
---
+++ packages/feincms-banners/feincms_banners-1.2.0-py2-none-any.whl/feincms_banners/views.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+
+from feincms_banners.models import Banner
+
+
+def click(request, code):
+    banner = get_object_or_404(Banner.objects.active(), code=code)
+    banner.click(request)
+    return HttpResponseRedirect(banner.url)
+
+
+def impression(request, code):
+    if (request.is_ajax() and Banner.objects.filter(code=code).update(
+            impressions=F('impressions') + 1
+            )):
+        return HttpResponse('+1')
+    return HttpResponse('?')
