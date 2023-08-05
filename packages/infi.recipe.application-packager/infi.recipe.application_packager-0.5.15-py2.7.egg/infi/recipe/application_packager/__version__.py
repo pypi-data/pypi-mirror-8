c17a4f103af496d4cd5d6c1e8ded1a63c44c78e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package infi.recipe.application-packager/0.5.15/infi.recipe.application_packager-0.5.15-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.15-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.15"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.5.15)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '94379c01d635e277934a22b212e59e0ffcc19ff4'
+__git_head_subject__ = 'TRIVIAL fixing setup.in'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
