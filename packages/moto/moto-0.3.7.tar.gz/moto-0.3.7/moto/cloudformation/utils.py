5c1f094da202013e58bb3506a49bfe369b58d8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:07 +0000

    Add package moto/0.3.7/moto-0.3.7.tar.gz

---
+++ packages/moto/moto-0.3.7.tar.gz/moto-0.3.7/moto/cloudformation/utils.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+import uuid
+
+
+def generate_stack_id(stack_name):
+    random_id = uuid.uuid4()
+    return "arn:aws:cloudformation:us-east-1:123456789:stack/{0}/{1}".format(stack_name, random_id)
