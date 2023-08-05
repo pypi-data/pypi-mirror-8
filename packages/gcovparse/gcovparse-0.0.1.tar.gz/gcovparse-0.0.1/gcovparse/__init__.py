c1d52f78864d34608b6c92db77c7b0de4e83bb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:39 +0000

    Add package gcovparse/0.0.1/gcovparse-0.0.1.tar.gz

---
+++ packages/gcovparse/gcovparse-0.0.1.tar.gz/gcovparse-0.0.1/gcovparse/__init__.py
@@ -0,0 +1,29 @@
+version = VERSION = __version__ = "0.0.1"
+
+def gcovparse(combined):
+    # clean and strip lines
+    assert ':Source:' in combined, 'gcov file is missing ":Source:" line(s)'
+    files = filter(lambda f: f!='', combined.strip().split("0:Source:"))
+    reports = map(_part, files[1:])
+    return reports
+
+def _part(chunk):
+    report = {
+        "file": chunk.split('\n',1)[0],
+        "stats": {},
+        "lines": []
+    }
+    map(lambda l: _line(l, report), chunk.strip().split('\n')[1:])
+
+    return report
+
+def _line(l, report):
+    line = l.split(':', 2)
+    if len(line)==3:
+        hit, line, data = tuple(line)
+        if data.startswith("Runs"):
+            report["stats"]["runs"] = data[5:]
+        elif data.startswith("Programs"):
+            report["stats"]["programs"] = data[9:]
+        elif int(line) > 0:
+            report['lines'].append(dict(line=line.strip(), hit=None if '-' in hit else 0 if '#' in hit else hit.strip()))
---
+++ packages/gcovparse/gcovparse-0.0.1.tar.gz/gcovparse-0.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+version = '0.0.1'
+classifiers = ["Development Status :: 4 - Beta",
+               "Environment :: Plugins",
+               "Intended Audience :: Developers",
+               "License :: OSI Approved :: Apache Software License",
+               "Topic :: Software Development :: Testing"]
+
+setup(name='gcovparse',
+      version=version,
+      description="gcov to json",
+      long_description=None,
+      classifiers=classifiers,
+      keywords='coverage',
+      author='@codecov',
+      author_email='hello@codecov.io',
+      url='http://github.com/codecov/gcov-parse',
+      license='http://www.apache.org/licenses/LICENSE-2.0',
+      packages=['gcovparse'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[],
+      entry_points=None)
