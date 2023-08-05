8546017389acd58504284a314458d74144de6de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:00 +0000

    Add package watch-fs/1.1.0/watch-fs-1.1.0.tar.gz

---
+++ packages/watch-fs/watch-fs-1.1.0.tar.gz/watch-fs-1.1.0/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+setuptools.setup(
+    name='watch-fs',
+    version='1.1.0',
+
+    author="Sam Clements",
+    author_email="sam@borntyping.co.uk",
+
+    url="https://github.com/borntyping/watch-fs",
+    description="A command line tool to run commands when files change",
+    long_description=open('README.rst').read(),
+
+    py_modules=[
+        'watch_fs'
+    ],
+
+    install_requires=[
+        'click>=3.3',
+        'pyinotify>=0.9.4'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'watch-fs = watch_fs:main',
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities'
+    ],
+)
