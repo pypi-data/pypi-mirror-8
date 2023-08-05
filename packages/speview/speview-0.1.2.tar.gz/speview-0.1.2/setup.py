2105f84a79eb410b64af2b42b15e459053d66a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:44 +0000

    Add package speview/0.1.2/speview-0.1.2.tar.gz

---
+++ packages/speview/speview-0.1.2.tar.gz/speview-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+setup(
+    name='speview',
+    version='0.1.2',
+    author='Roman Kiselev',
+    author_email='roman.kiselew@gmail.com',
+    scripts=['speview.py'],
+    url='https://github.com/ximeg/speview',
+    license='LICENSE.txt',
+    description='Program to display binary SPE files with Raman spectra',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "argparse>=1.2.1",
+        "mock>=1.0.1",
+        "nose>=1.3.4",
+        "pyparsing>=2.0.2",
+        "python-dateutil>=2.2",
+        "six>=1.8.0",
+        "wsgiref>=0.1.2",
+        "PySide>=1.2.2",
+        "numpy>=1.9.0",
+        "matplotlib>=1.4.0",
+        "scipy>=0.14.0",
+        "xcal-raman>=0.1.5"
+        ]
+)
+
