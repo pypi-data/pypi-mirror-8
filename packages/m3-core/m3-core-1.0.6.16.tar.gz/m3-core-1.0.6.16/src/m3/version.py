64497f0238d86a48b66797df10796862431ea0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:50 +0000

    Add package m3-core/1.0.6.16/m3-core-1.0.6.16.tar.gz

---
+++ packages/m3-core/m3-core-1.0.6.16.tar.gz/m3-core-1.0.6.16/src/m3/version.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+
+__version__ = '1.0'
+__appname__ = u'Платформа М3'
+
+__require__ = {
+    'django': '1.3',
+}
---
+++ packages/m3-core/m3-core-1.0.6.16.tar.gz/m3-core-1.0.6.16/src/m3/workflow/exceptions.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+'''
+Created on 11.03.2010
+
+@author: akvarats
+'''
+
+class ImproperlyConfigured(Exception):
+    '''
+    Исключение рабочего процесса говорящее о неверной конфигурации класса
+    '''
+    pass
