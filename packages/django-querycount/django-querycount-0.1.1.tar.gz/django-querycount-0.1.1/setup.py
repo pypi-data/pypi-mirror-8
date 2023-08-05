7964ad740e227fb8b4e684312ab7a40466734e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:58 +0000

    Add package django-querycount/0.1.1/django-querycount-0.1.1.tar.gz

---
+++ packages/django-querycount/django-querycount-0.1.1.tar.gz/django-querycount-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from querycount import __version__
+
+setup(
+    name="django-querycount",
+    version=__version__,
+    author="Brad Montgomery",
+    author_email="brad@bradmontgomery.net",
+    description=("Middleware that Prints the number of DB queries to the runserver console."),
+    install_requires=[],
+    license="MIT",
+    keywords="django querycount database performance",
+    url="https://github.com/bradmontgomery/django-querycount/tarball/0.0.0",
+    packages=[
+        "querycount",
+    ],
+    long_description="this project gives you a middleware that"
+    "prints DB query counts in Django's runserver console output.",
+    classifiers=[
+        "Topic :: Utilities",
+    ],
+)
