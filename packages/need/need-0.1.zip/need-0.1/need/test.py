cca0b1fab1900d0976c18cbef0e070f24c2fc972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:28 +0000

    Add package need/0.1/need-0.1.zip

---
+++ packages/need/need-0.1.zip/need-0.1/need/test.py
@@ -0,0 +1,2 @@
+def general():
+    return('i m in need to learn.....   !!!''...intelligence without ambition is like a bird without wings!!!...')
---
+++ packages/need/need-0.1.zip/need-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='need',
+      version='0.1',
+      description='NEED TO LEARN',
+      url='http://github.com/jasmine/need',
+      author='jasneed',
+      author_email='jasneed@example.com',
+      license='MIT',
+      packages=['need'],
+      zip_safe=False)
