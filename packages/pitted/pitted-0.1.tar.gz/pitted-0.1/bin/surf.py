e6174f238e25f490c20d650ce0358d7069018dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package pitted/0.1/pitted-0.1.tar.gz

---
+++ packages/pitted/pitted-0.1.tar.gz/pitted-0.1/bin/surf.py
@@ -0,0 +1,7 @@
+import pitted, os, glob
+
+print 'drop in {d}'.format(d=pitted.drop_in())
+for f in glob.glob(os.path.join(os.path.dirname(os.path.abspath(pitted.__file__)), 'gnar/*')):
+    with open(f) as b:
+        for l in b.readlines():
+            print l
