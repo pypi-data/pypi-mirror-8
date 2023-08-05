7db14dbb9135ec657a6e026817fc5ca20c70804b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:23 +0000

    Add package threebot-hook/0.1.0/threebot-hook-0.1.0.tar.gz

---
+++ packages/threebot-hook/threebot-hook-0.1.0.tar.gz/threebot-hook-0.1.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+
+
+from setuptools import setup
+from os.path import join, dirname
+
+def long_description():
+    try:
+        return open(join(dirname(__file__), 'README.md')).read()
+    except IOError:
+        return "LONG_DESCRIPTION Error"
+        
+    
+setup(
+    name='threebot-hook',
+    version='0.1.0',
+    description='GitHub (& Bitbucket) webhooks for 3bot',
+    long_description=long_description(),
+    author='arteria GmbH',
+    author_email='admin@arteria.ch',
+    packages = ['threebot_hook'],
+    install_requires=['Django', 'djangorestframework', 'threebot'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 4 - Beta',
+    ] 
+)
