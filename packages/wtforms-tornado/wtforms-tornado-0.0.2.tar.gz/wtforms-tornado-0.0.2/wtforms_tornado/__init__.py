0e991ddbe2d8c145a869d199610775ed0ca6d4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:49 +0000

    Add package wtforms-tornado/0.0.2/wtforms-tornado-0.0.2.tar.gz

---
+++ packages/wtforms-tornado/wtforms-tornado-0.0.2.tar.gz/wtforms-tornado-0.0.2/wtforms_tornado/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""WTForms extensions for Tornado."""
+from wtforms import compat
+
+
+version_tuple = (0, 0, 2)
+
+
+def get_version_string():
+    if isinstance(version_tuple[-1], compat.string_types):
+        return '.'.join(map(str, version_tuple[:-1])) + version_tuple[-1]
+    return '.'.join(map(str, version_tuple))
+
+version = get_version_string()
+"""Current version of wtforms-tornado."""
+
+__author__ = 'Jorge Puente Sarrín <puentesarrin@gmail.com>'
+__since__ = '2013-09-25'
+__version__ = version
+
+
+from wtforms_tornado.form import Form
