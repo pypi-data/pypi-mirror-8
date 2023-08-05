f79818aa3c11810697e1b1d9cedcc2f624bcd76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package tornwrap/0.1.0/tornwrap-0.1.0.tar.gz

---
+++ packages/tornwrap/tornwrap-0.1.0.tar.gz/tornwrap-0.1.0/tornwrap/__init__.py
@@ -0,0 +1,7 @@
+from .validated import validated
+from .ratelimited import ratelimited
+from .authenticated import authenticated
+from .error import ErrorHandler
+from . import logger
+
+version = VERSION = __version__ = "0.1.0"
