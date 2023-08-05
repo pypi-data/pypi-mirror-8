8daf1a6e681688fd4754be2bdbb3481ba898d177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package shellvars-py/0.1.1/shellvars-py-0.1.1.tar.gz

---
+++ packages/shellvars-py/shellvars-py-0.1.1.tar.gz/shellvars-py-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def read(f):
+    with open(f) as file:
+        try:
+            return file.read()
+        except:
+            return ""
+
+setup(name='shellvars-py',
+      version='0.1.1',
+      description='Read environment variables defined in a shell script into Python.',
+      author_email='aneil.mallavar@gmail.com',
+      license='Apache2',
+      py_modules=['shellvars'],
+      long_description = read('README.md'),
+      url="http://github.com/aneilbaboo/shellvars-py",
+      author="Aneil Mallavarapu",
+      include_package_data = True,
+      classifiers = [
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS :: MacOS X',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+)
