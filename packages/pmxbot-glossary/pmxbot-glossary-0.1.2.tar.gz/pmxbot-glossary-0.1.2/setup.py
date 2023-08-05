a349fd7cf01740aacbb40530b07f16e620b2ff31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:06 +0000

    Add package pmxbot-glossary/0.1.2/pmxbot-glossary-0.1.2.tar.gz

---
+++ packages/pmxbot-glossary/pmxbot-glossary-0.1.2.tar.gz/pmxbot-glossary-0.1.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import glossary
+
+setup(
+    name='pmxbot-glossary',
+    version=glossary.__version__,
+    description='A pmxbot glossary extension.',
+    author='Harvey Rogers',
+    author_email='harveyr@gmail.com ',
+    url='https://github.com/harveyr/pmxbot-glossary/',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'pmxbot',
+        'python-dateutil',
+    ],
+    entry_points=dict(
+        pmxbot_handlers=[
+            'Glossary = glossary.glossary:Glossary.initialize',
+        ]
+    ),
+)
