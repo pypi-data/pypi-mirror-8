4727984eed3d15203784a19b38a539b889fed98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:09 +0000

    Add package infi.parted/0.2.6/infi.parted-0.2.6.tar.gz

---
+++ packages/infi.parted/infi.parted-0.2.6.tar.gz/infi.parted-0.2.6/src/infi/parted/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.6"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '922873e1c27ef7fff080bf1cc9f3aa7ff26a57ff'
+__git_head_subject__ = 'Finished release v0.2.6.'
+__git_head_message__ = '* release/v0.2.6:\n  HIP-1298 fixing log formatting\n  HIP-1298 fixing NameError'
+__git_dirty_diff__ = ''
