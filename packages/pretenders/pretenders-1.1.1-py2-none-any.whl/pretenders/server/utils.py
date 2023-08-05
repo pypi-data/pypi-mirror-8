ed518acd110c69253862dbe4eff5c4df049d5b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package pretenders/1.1.1/pretenders-1.1.1-py2-none-any.whl

---
+++ packages/pretenders/pretenders-1.1.1-py2-none-any.whl/pretenders/server/utils.py
@@ -0,0 +1,21 @@
+import bottle
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    #2.6 compatibility
+    from pretenders.common.compat.ordered_dict import OrderedDict
+
+from pretenders.server.log import get_logger
+from pretenders.common.constants import REQUEST_ONLY_HEADERS
+
+LOGGER = get_logger('pretenders.server.utils')
+
+
+def acceptable_response_header(header):
+    "Use to filter which HTTP headers in the request should be removed"
+    return header not in REQUEST_ONLY_HEADERS
+
+
+def get_header(header, default=None):
+    return bottle.request.headers.get(header, default)
