691b9e08df898cc433847e5d012ce3edf4d6c668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:50 +0000

    Add package pysimpleinjector/1.1/pysimpleinjector-1.1.tar.gz

---
+++ packages/pysimpleinjector/pysimpleinjector-1.1.tar.gz/pysimpleinjector-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='pysimpleinjector',
+    version='1.1',
+    author='Hugo Osvaldo Barrera',
+    author_email='hugo@barrera.io',
+    packages=['simpleinjector'],
+    url='https://github.com/hobarrera/simpleinjector',
+    license='ISC',
+    description="An extremely simple framework-independent dependency " +
+        "injector."
+)
