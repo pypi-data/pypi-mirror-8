0ce06bd37c672cd271efa404487081f31e3defb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:33 +0000

    Add package sirdjango/0.1.0/sirdjango-0.1.0.tar.gz

---
+++ packages/sirdjango/sirdjango-0.1.0.tar.gz/sirdjango-0.1.0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+Install sirdjango using setuptools
+"""
+
+from sirdjango import __version__
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='sirdjango',
+    version=__version__,
+    description='Sir Trevor integration with Django',
+    long_description=readme,
+    author='Tim Heap',
+    author_email='tim@takeflight.com.au',
+    url='https://bitbucket.org/takeflight/sirdjango',
+
+    install_requires=[
+        'django>=1.6',
+        'django-jsonfield',
+        'markdown',
+    ],
+    zip_safe=False,
+    license='BSD License',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+    package_data={ },
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
