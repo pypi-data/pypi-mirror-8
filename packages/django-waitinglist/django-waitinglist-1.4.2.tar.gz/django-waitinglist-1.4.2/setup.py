45fb5a90c80ab4d601e1902377ef9a6b1a9c09e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:47 +0000

    Add package django-waitinglist/1.4.2/django-waitinglist-1.4.2.tar.gz

---
+++ packages/django-waitinglist/django-waitinglist-1.4.2.tar.gz/django-waitinglist-1.4.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "django-waitinglist",
+    version = "1.4.2",
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    description = "a Django waiting list app for running a private beta with cohorts support",
+    long_description = open("README.rst").read(),
+    license = "MIT",
+    url = "http://github.com/pinax/django-waitinglist",
+    packages = find_packages(),
+    package_data = {"waitinglist": ["templates/*/*/*"]},
+    install_requires = [
+        "django-appconf==0.5",
+    ],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
