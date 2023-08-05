8e6e0dd07b17dc0a673f78d933234c05fabab58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:15 +0000

    Add package ko-po-check/0.96.0/ko-po-check-0.96.0.tar.gz

---
+++ packages/ko-po-check/ko-po-check-0.96.0.tar.gz/ko-po-check-0.96.0/KPC/checks/language/punctuation.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import re
+from KPC.classes import Error, BaseCheck
+
+# 괄호 띄어쓰기
+check_list = [
+    [
+        re.compile('[\uac00-\ud7a3] \((?!%a)', re.UNICODE),
+        '맞춤법 규정에 따라 괄호 앞에 띄어 쓰지 않습니다'
+    ]
+]
+
+
+class PunctuationCheck(BaseCheck):
+    def check(self, entry, context):
+        msgstr = entry.msgstr
+        errors = []
+        for (r, emsg) in check_list:
+            mo = r.search(msgstr)
+            if mo:
+                errors.append(Error(emsg))
+        return errors
+
+name = 'language/punctuation'
+description = '잘못된 한국어 구문 규칙을 찾아냅니다'
+checker = PunctuationCheck()
