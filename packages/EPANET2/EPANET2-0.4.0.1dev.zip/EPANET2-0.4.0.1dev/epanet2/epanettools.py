86986f7c82e3fc2b5bbff8e0035a017cacf8eee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package EPANET2/0.4.0.1dev/EPANET2-0.4.0.1dev.zip

---
+++ packages/EPANET2/EPANET2-0.4.0.1dev.zip/EPANET2-0.4.0.1dev/epanet2/epanettools.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+EPANETTools class
+(C) Assela Pathirana 2014.. under GPL V3
+"""
+from . import epanet2
+from collections import OrderedDict
+from tempfile import mkstemp
+from os.path import basename, dirname
+from  os import close, remove
+
+class EPANET2Simulation(object):
+    pass
