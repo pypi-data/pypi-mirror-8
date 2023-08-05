40015b38cbb6a2766909748b829f2fd13d499927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:58 +0000

    Add package pycse/1.48/pycse-1.48.tar.gz

---
+++ packages/pycse/pycse-1.48.tar.gz/pycse-1.48/pycse/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.48'
+'''
+9/16/2013 made odelay multivariable and return arrays
+10/19/2013 changed bvp to work for multiple first order equations.
+9/4/2014 minor changes to odelay documentation and bug fix on endpoint
+9/29/2014 added sandbox.py
+'''
+from PYCSE import *
+
+# quantities math functions
+from qmath import * 
