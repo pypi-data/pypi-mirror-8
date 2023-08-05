27e12408244a55c6c35ffc2168585fd0aa72c5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:11 +0000

    Add package twitterxauth/0.0.3dev/twitterxauth-0.0.3dev.tar.gz

---
+++ packages/twitterxauth/twitterxauth-0.0.3dev.tar.gz/twitterxauth-0.0.3dev/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+version = '0.0.3'
+
+setup(
+    name='twitterxauth',
+    version=version,
+    description="thin wrapper module for xAuth twitter.com",
+    long_description="""\
+""",
+    classifiers=[
+    ],
+    keywords='twitter xauth',
+    author='yuitest',
+    author_email='yuitest+git@cjhat.net',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'httplib2>=0.9',
+        'oauth2>=1.5',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+""",
+)
