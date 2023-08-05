37588fddc1e5bae7873d85c97df6882e0ccb6e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:26 +0000

    Add package bobsleigh_seddonym/0.1.1/bobsleigh_seddonym-0.1.1.tar.gz

---
+++ packages/bobsleigh_seddonym/bobsleigh_seddonym-0.1.1.tar.gz/bobsleigh_seddonym-0.1.1/bobsleigh_seddonym/handlers/local.py
@@ -0,0 +1,71 @@
+from bobsleigh.conf.handlers import InstallationHandler
+
+
+class LocalHandler(InstallationHandler):
+    "For my local development setup."
+
+    def get_required_kwargs(self):
+        return ('sitename',)
+
+    def get_optional_kwargs(self):
+        optional_kwargs = super(LocalHandler, self).get_optional_kwargs()
+        optional_kwargs.update({
+            'host': 'lanky',
+            'debug': True,
+            'monitor': True,
+            'email_host': 'smtp.gmail.com',
+            'email_use_tls': True,
+            'email_port': 587,
+            'email_host_user': 'davidseddonis@gmail.com',
+        })
+        return optional_kwargs
+
+    def get_config_patterns(self):
+        patterns = super(LocalHandler, self).get_config_patterns()
+        patterns += (
+            ('domain', '%(sitename)s.localhost'),
+            ('db_name', '%(sitename)s'),
+            ('db_user', '%(sitename)s'),
+            ('log_path', '/var/log/django/%(sitename)s'),
+            ('static_path', '/home/david/var/www/%(sitename)s/static'),
+            ('media_path', '/home/david/var/www/%(sitename)s/uploads'),
+            ('virtualenv_path', '/home/david/.virtualenvs/%(sitename)s'),
+            ('project_path', '/home/david/www/%(sitename)s'),
+            ('server_email', 'contact@%(domain)s'),
+        )
+        return patterns
+
+    def adjust(self):
+        super(LocalHandler, self).adjust()
+        self._settings['EMAIL_USE_TLS'] = self.config.email_use_tls
+        self._settings['EMAIL_PORT'] = self.config.email_port
+
+
+class VagrantHandler(InstallationHandler):
+    "Geared up towards Vagrant machines."
+
+    def get_required_kwargs(self):
+        required_kwargs = super(VagrantHandler, self).get_required_kwargs()
+        required_kwargs += 'sitename'
+        return required_kwargs
+
+    def get_optional_kwargs(self):
+        optional_kwargs = super(LocalHandler, self).get_optional_kwargs()
+        optional_kwargs.update({
+            'host': 'precise32',
+        })
+        return optional_kwargs
+
+    def get_config_patterns(self):
+        patterns = super(LocalHandler, self).get_config_patterns()
+        patterns += (
+            ('domain', '%(sitename)s.vagrant'),
+            ('db_name', '%(sitename)s'),
+            ('db_user', '%(sitename)s'),
+            ('log_path', '/var/log/django'),
+            ('static_path', '/opt/site/static'),
+            ('media_path', '/opt/site/uploads'),
+            ('virtualenv_path', '/opt/site/.virtualenvs/site'),
+            ('project_path', '/opt/site/django'),
+        )
+        return patterns
