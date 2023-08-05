a158ee088507305179d9fa657e1f6438e3d99163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:32 +0000

    Add package ckanext-deadoralive/0.1.3/ckanext-deadoralive-0.1.3.tar.gz

---
+++ packages/ckanext-deadoralive/ckanext-deadoralive-0.1.3.tar.gz/ckanext-deadoralive-0.1.3/ckanext/deadoralive/logic/action/update.py
@@ -0,0 +1,33 @@
+import ckan.plugins.toolkit as toolkit
+import ckanext.deadoralive.model.results as results
+
+
+def upsert(context, data_dict, last_checked=None):
+    """Save a link check result for a resource.
+
+    :param resource_id: the id of the resource that was checked
+    :type resource_id: string
+
+    :param alive: whether or not the link was found to be alive
+    :type alive: bool
+
+    :param status: the HTTP status code when the resource was checked,
+        e.g. 200, 404 or 500
+    :type status: int
+
+    :param reason: the reason for the failed or successful resource check,
+        e.g. "OK", "Not Found", "Internal Server Error"
+    :type reason: string
+
+    """
+    toolkit.check_access("ckanext_deadoralive_upsert", context, data_dict)
+
+    # TODO: Validation.
+
+    resource_id = data_dict["resource_id"]
+    alive = data_dict["alive"]
+    status = data_dict.get("status")
+    reason = data_dict.get("reason")
+
+    results.upsert(resource_id, alive, status=status, reason=reason,
+                   last_checked=last_checked)
