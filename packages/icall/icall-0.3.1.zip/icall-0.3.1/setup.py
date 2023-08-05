87540039a4bfcd97e3cbb21941965f315a179491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package icall/0.3.1/icall-0.3.1.zip

---
+++ packages/icall/icall-0.3.1.zip/icall-0.3.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(
+    name = 'icall',
+    version = '0.3.1',
+    py_modules = ['icall'],
+    description = 'Parameters call function, :-)',
+    long_description = open('README').read(),
+    author = 'huyx',
+    author_email = 'ycyuxin@gmail.com',
+    url = 'https://github.com/huyx/icall',
+    keywords = ['functools', 'function', 'call'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+    )
