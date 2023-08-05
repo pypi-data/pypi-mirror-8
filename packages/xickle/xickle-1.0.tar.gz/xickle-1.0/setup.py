d89f68bcfb50556ebc82eca0e0e55072815a73c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package xickle/1.0/xickle-1.0.tar.gz

---
+++ packages/xickle/xickle-1.0.tar.gz/xickle-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='xickle',
+      version='1.0',
+      py_modules=['xickle'],
+      description='XML-persistence, in the most python-ic way',
+      author='Hasan Diwan',
+      author_email='hasan.diwan@gmail.com',
+      install_requires=['xmltodict==0.9.0'],
+      license='PSF'
+)
