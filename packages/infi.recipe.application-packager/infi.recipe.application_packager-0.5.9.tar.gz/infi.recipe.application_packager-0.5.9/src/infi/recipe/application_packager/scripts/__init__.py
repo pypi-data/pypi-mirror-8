78fad1a18351c31eb6abc832c5482753ee95cbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:41 +0000

    Add package infi.recipe.application-packager/0.5.9/infi.recipe.application_packager-0.5.9.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.9.tar.gz/infi.recipe.application_packager-0.5.9/src/infi/recipe/application_packager/scripts/__init__.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+__import__("pkg_resources").declare_namespace(__name__)
+
+from sys import argv
+import time
+import os
+
+INSTALLDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), #scripts
+                                          os.path.pardir, #application_packager
+                                          os.path.pardir, #recipe
+                                          os.path.pardir, #infi
+                                          os.path.pardir, #src
+                                          os.path.pardir))
+
+def hello(argv=argv):
+    print('hello world')
+
+def _write_file(name):
+    with open(os.path.join(INSTALLDIR, name), 'w') as fd:
+        fd.write(' ')
+
+def post_install(argv=argv):
+    _write_file('post_install')
+
+def pre_uninstall(argv=argv):
+    _write_file(os.path.join(INSTALLDIR, os.path.pardir, 'pre_uninstall'))
+
+def sample(argv=argv):
+    raw_input('sample, hit enter to exit')
+
+def sleep(argv=argv):
+    t0 = time.time()
+    timeout = int(argv[-1])
+    try:
+        time.sleep(timeout)
+    except KeyboardInterrupt:  # windows installer does this
+        timeout = abs(timeout - (time.time() - t0))
+        time.sleep(timeout)
