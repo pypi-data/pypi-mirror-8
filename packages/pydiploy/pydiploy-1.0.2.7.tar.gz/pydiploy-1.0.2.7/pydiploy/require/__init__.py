90fc60b6148fb9fea2f2bdacaaa25b834f35ba6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:25 +0000

    Add package pydiploy/1.0.2.7/pydiploy-1.0.2.7.tar.gz

---
+++ packages/pydiploy/pydiploy-1.0.2.7.tar.gz/pydiploy-1.0.2.7/pydiploy/require/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from pydiploy.require import database
+from pydiploy.require import django
+from pydiploy.require import nginx
+from pydiploy.require import circus
+from pydiploy.require import python
+from pydiploy.require import system
+from pydiploy.require import git
+from pydiploy.require import releases_manager
