f7761cddfacf9c6ecaaf02e82fa445ec526cb16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:19 +0000

    Add package mrep/0.1.1/mrep-0.1.1-py2.7.egg

---
+++ packages/mrep/mrep-0.1.1-py2.7.egg/mrep/morph.py
@@ -0,0 +1,27 @@
+import MeCab
+
+
+class MeCabParser(object):
+    def __init__(self, arg=''):
+        model = MeCab.Model_create(arg)
+        if model is None:
+            raise Exception('Cannot initialize mecab')
+        self.model = model
+
+    def parse(self, s):
+        tagger = self.model.createTagger()
+        lattice = self.model.createLattice()
+        lattice.set_sentence(s)
+        tagger.parse(lattice)
+        node = lattice.bos_node()
+        morphs = []
+        while node:
+            if node.surface != '':
+                feature = node.feature
+                morphs.append({
+                    'surface': node.surface,
+                    'pos': feature[0:feature.find(',')],
+                    'feature': feature,
+                })
+            node = node.next
+        return morphs
