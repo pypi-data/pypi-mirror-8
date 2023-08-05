daefa554548ba4ff33b3ef6707c9c52fe376f16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:20 +0000

    Add package sqlnet/0.1/sqlnet-0.1.zip

---
+++ packages/sqlnet/sqlnet-0.1.zip/sqlnet-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sqlnet',
+    version='0.1',
+    description='A DB API v2.0 compatible wrapper to .NET SqlClient',
+    url='https://github.com/beckettsimmons/sqlnet',
+    author='Beckett Simmons',
+    author_email='beckettsimmons@hotmail.com',
+    license='MIT',
+    packages=['sqlnet'],
+    zip_safe=False
+)
