38ff5318b02c92462a926b72880ad7bd92ed13af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:36 +0000

    Add package infi.recipe.application-packager/0.5.18/infi.recipe.application_packager-0.5.18-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.18-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.18"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.5.18)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '13793c8a20667b45ee9f2d6bf9c3ee83dddcc221'
+__git_head_subject__ = 'TRIVIAL fixing _apply_project_specific_on_top_of_platform_defaults'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
