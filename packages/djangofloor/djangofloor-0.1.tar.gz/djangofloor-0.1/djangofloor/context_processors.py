af2ba148d799764ab6b52884b7abb0f1ccd48fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:35 +0000

    Add package djangofloor/0.1/djangofloor-0.1.tar.gz

---
+++ packages/djangofloor/djangofloor-0.1.tar.gz/djangofloor-0.1/djangofloor/context_processors.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+from django.conf import settings
+
+
+__author__ = 'flanker'
+
+
+def context_base(request):
+
+    return {'df_remote_authenticated': request.df_remote_authenticated,
+            'df_project_name': settings.FLOOR_PROJECT_NAME,
+            'df_user': request.user,
+            }
