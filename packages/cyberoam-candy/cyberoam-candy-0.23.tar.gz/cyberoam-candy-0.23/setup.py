a114201ea8b6ecb81838918841a0946f80d0fda9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:39 +0000

    Add package cyberoam-candy/0.23/cyberoam-candy-0.23.tar.gz

---
+++ packages/cyberoam-candy/cyberoam-candy-0.23.tar.gz/cyberoam-candy-0.23/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cyberoam-candy',
+		version='0.23',
+		description='Basic Cyberoam login client. Command line interface.',
+		url='http://github.com/bhavul/cyberoam-candy',
+		author='Bhavul Gauri',
+		author_email='bhavul93@gmail.com',
+		license='MIT',
+		packages=['cyberoam_candy'],
+		zip_safe=False)
