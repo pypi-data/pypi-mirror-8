433ac6d6661bf3f6b5ff93438a8d7016f08cb38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:00 +0000

    Add package pelican-git/0.1.1/pelican-git-0.1.1.tar.gz

---
+++ packages/pelican-git/pelican-git-0.1.1.tar.gz/pelican-git-0.1.1/pelican_git/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+__title__ = 'pelican-git'
+__version__ = '0.1.1'
+__author__ = 'Ha Minh'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014'
+
+from pelican_git.plugin import register
