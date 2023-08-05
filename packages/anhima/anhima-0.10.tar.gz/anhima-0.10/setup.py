f94330982703b703480a9de842d8722bd5b45b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:26 +0000

    Add package anhima/0.10/anhima-0.10.tar.gz

---
+++ packages/anhima/anhima-0.10.tar.gz/anhima-0.10/setup.py
@@ -0,0 +1,31 @@
+from ast import literal_eval
+from distutils.core import setup
+from distutils.extension import Extension
+
+
+def get_version(source='src/anhima/__init__.py'):
+    with open(source) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return literal_eval(line.split('=')[-1].lstrip())
+    raise ValueError("__version__ not found")
+
+
+setup(
+    name='anhima',
+    version=get_version(),
+    author='Alistair Miles',
+    author_email='alimanfoo@googlemail.com',
+    package_dir={'': 'src'},
+    packages=['anhima', 'anhima.opt'],
+    ext_modules=[Extension('anhima.opt.ld', ['src/anhima/opt/ld.c'])],
+    url='https://github.com/alimanfoo/anhima',
+    license='MIT License',
+    description='Exploration and Analysis of genetic variation data.',
+    long_description=open('README.md').read(),
+    classifiers=['Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development :: Libraries :: Python Modules'
+                 ]
+)
---
+++ packages/anhima/anhima-0.10.tar.gz/anhima-0.10/src/anhima/__init__.py
@@ -0,0 +1,32 @@
+"""
+The ``anhima`` package.
+
+"""
+
+
+# flake8: noqa
+
+
+from __future__ import division, print_function, unicode_literals, \
+    absolute_import
+
+
+__version__ = '0.10'
+
+
+# make sub-packages available without explicit import
+from . import af
+from . import dist
+from . import f2
+from . import gt
+from . import h5
+from . import io
+from . import ld
+from . import loc
+from . import mds
+from . import pca
+from . import ped
+from . import sf
+from . import sim
+from . import tree
+from . import util
