72e9db66ad93bcbad5768b8e0db78f2f968cc25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:23 +0000

    Add package Fiona/1.4.2/Fiona-1.4.2.tar.gz

---
+++ packages/Fiona/Fiona-1.4.2.tar.gz/Fiona-1.4.2/setup.py
@@ -0,0 +1,148 @@
+import logging
+import os
+import subprocess
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+# Have to do this after importing setuptools, which monkey patches distutils.
+from distutils.extension import Extension
+
+# Use Cython if available.
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+logging.basicConfig()
+log = logging.getLogger()
+
+# Parse the version from the fiona module.
+with open('fiona/__init__.py', 'r') as f:
+    for line in f:
+        if line.find("__version__") >= 0:
+            version = line.split("=")[1].strip()
+            version = version.strip('"')
+            version = version.strip("'")
+            continue
+
+# Fiona's auxiliary files are UTF-8 encoded and we'll specify this when
+# reading with Python 3+
+open_kwds = {}
+if sys.version_info > (3,):
+    open_kwds['encoding'] = 'utf-8'
+
+with open('VERSION.txt', 'w', **open_kwds) as f:
+    f.write(version)
+
+with open('README.rst', **open_kwds) as f:
+    readme = f.read()
+
+with open('CREDITS.txt', **open_kwds) as f:
+    credits = f.read()
+
+with open('CHANGES.txt', **open_kwds) as f:
+    changes = f.read()
+
+# By default we'll try to get options via gdal-config. On systems without,
+# options will need to be set in setup.cfg or on the setup command line.
+include_dirs = []
+library_dirs = []
+libraries = []
+extra_link_args = []
+
+try:
+    gdal_config = "gdal-config"
+    with open("gdal-config.txt", "w") as gcfg:
+        subprocess.call([gdal_config, "--cflags"], stdout=gcfg)
+        subprocess.call([gdal_config, "--libs"], stdout=gcfg)
+    with open("gdal-config.txt", "r") as gcfg:
+        cflags = gcfg.readline().strip()
+        libs = gcfg.readline().strip()
+    for item in cflags.split():
+        if item.startswith("-I"):
+            include_dirs.extend(item[2:].split(":"))
+    for item in libs.split():
+        if item.startswith("-L"):
+            library_dirs.extend(item[2:].split(":"))
+        elif item.startswith("-l"):
+            libraries.append(item[2:])
+        else:
+            # e.g. -framework GDAL
+            extra_link_args.append(item)
+
+except Exception as e:
+    log.warning("Failed to get options via gdal-config: %s", str(e))
+
+ext_options = dict(
+    include_dirs=include_dirs,
+    library_dirs=library_dirs,
+    libraries=libraries,
+    extra_link_args=extra_link_args)
+
+# When building from a repo, Cython is required.
+if os.path.exists("MANIFEST.in"):
+    log.info("MANIFEST.in found, presume a repo, cythonizing...")
+    if not cythonize:
+        log.critical(
+            "Cython.Build.cythonize not found. "
+            "Cython is required to build from a repo.")
+        sys.exit(1)
+    ext_modules = cythonize([
+        Extension('fiona._geometry', ['fiona/_geometry.pyx'], **ext_options),
+        Extension('fiona._transform', ['fiona/_transform.pyx'], **ext_options),
+        Extension('fiona._drivers', ['fiona/_drivers.pyx'], **ext_options),
+        Extension('fiona._err', ['fiona/_err.pyx'], **ext_options),
+        Extension('fiona.ogrext', ['fiona/ogrext.pyx'], **ext_options)])
+# If there's no manifest template, as in an sdist, we just specify .c files.
+else:
+    ext_modules = [
+        Extension('fiona._transform', ['fiona/_transform.cpp'], **ext_options),
+        Extension('fiona._geometry', ['fiona/_geometry.c'], **ext_options),
+        Extension('fiona._drivers', ['fiona/_drivers.c'], **ext_options),
+        Extension('fiona._err', ['fiona/_err.c'], **ext_options),
+        Extension('fiona.ogrext', ['fiona/ogrext.c'], **ext_options)]
+
+requirements = ['click', 'six']
+if sys.version_info < (2, 7):
+    requirements.append('argparse')
+    requirements.append('ordereddict')
+
+setup(
+    metadata_version='1.2',
+    name='Fiona',
+    version=version,
+    requires_python = '>=2.6',
+    requires_external = 'GDAL (>=1.8)',
+    description="Fiona reads and writes spatial data files",
+    license='BSD',
+    keywords='gis vector feature data',
+    author='Sean Gillies',
+    author_email='sean.gillies@gmail.com',
+    maintainer='Sean Gillies',
+    maintainer_email='sean.gillies@gmail.com',
+    url='http://github.com/Toblerity/Fiona',
+    long_description=readme + "\n" + changes + "\n" + credits,
+    package_dir={'': '.'},
+    packages=['fiona', 'fiona.fio'],
+    entry_points='''
+        [console_scripts]
+        fio=fiona.fio.fio:cli
+        ''',
+    install_requires=requirements,
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    ext_modules=ext_modules,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
+)
