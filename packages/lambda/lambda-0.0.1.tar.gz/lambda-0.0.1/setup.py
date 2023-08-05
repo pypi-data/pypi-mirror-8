f0a7a17727405e6dcf0f4299799ecd28566b3dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:08 +0000

    Add package lambda/0.0.1/lambda-0.0.1.tar.gz

---
+++ packages/lambda/lambda-0.0.1.tar.gz/lambda-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+version = '0.0.1'
+
+setup(
+    name='lambda',
+    author='Trent Hauck',
+    author_email='trent@trenthauck.com',
+    version=version,
+    url='http://github.com/tshauck/lol',
+    packages=['lambda'],
+    description="Trent Hauck's personal package for evil.",
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+)
