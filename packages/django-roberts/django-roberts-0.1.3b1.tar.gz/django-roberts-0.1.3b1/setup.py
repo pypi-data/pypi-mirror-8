65ade74fa8a93fb8992076ab2e22b59a6db02fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:44 +0000

    Add package django-roberts/0.1.3b1/django-roberts-0.1.3b1.tar.gz

---
+++ packages/django-roberts/django-roberts-0.1.3b1.tar.gz/django-roberts-0.1.3b1/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+from robots import __version__
+
+setup(
+    name = "django-roberts",
+    version = __version__,
+    author = "Mike Hurt",
+    author_email = "mike@mhtechnical.net",
+    description = "A simple Django app to provide access to a robots.txt file",
+    license = "MIT",
+    keywords = "django robots.txt",
+    url = "https://bitbucket.org/mhurt/django-roberts",
+    packages=['robots'],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
