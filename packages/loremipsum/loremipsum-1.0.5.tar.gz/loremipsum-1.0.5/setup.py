443c0857f2aafe87d9209d1166f4313483f9d343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package loremipsum/1.0.5/loremipsum-1.0.5.tar.gz

---
+++ packages/loremipsum/loremipsum-1.0.5.tar.gz/loremipsum-1.0.5/setup.py
@@ -0,0 +1,38 @@
+"""
+python setup.py bdist_egg
+"""
+import sys
+from setuptools import setup
+import os
+
+WD = os.path.dirname(os.path.abspath(__file__))
+os.chdir(WD)
+sys.path.insert(1, WD)
+
+NAME = 'loremipsum'
+PACKAGE = __import__(NAME)
+AUTHOR, EMAIL = PACKAGE.__author__.rsplit(' ', 1)
+
+with open('README.rst', 'r') as README:
+    DESCRIPTION = README.readline().strip()
+    LONG_DESCRIPTION = ''.join((DESCRIPTION, README.read()))
+
+URL = 'http://projects.monkeython.com/%s' % NAME
+
+EGG = {
+    'name': NAME,
+    'version': PACKAGE.__version__,
+    'author': AUTHOR,
+    'author_email': EMAIL.strip('<>'),
+    'url': URL,
+    'description': DESCRIPTION,
+    'long_description': LONG_DESCRIPTION,
+    'classifiers': PACKAGE.__classifiers__,
+    'keywords': PACKAGE.__keywords__,
+    'packages': [NAME],
+    'include_package_data': True,
+    'test_suite': 'tests.suite'
+}
+
+if __name__ == '__main__':
+    setup(**EGG)
