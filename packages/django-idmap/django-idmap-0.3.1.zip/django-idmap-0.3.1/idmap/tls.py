50040bfe8afbf98b3ede773c3c5640517ab83a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:43 +0000

    Add package django-idmap/0.3.1/django-idmap-0.3.1.zip

---
+++ packages/django-idmap/django-idmap-0.3.1.zip/django-idmap-0.3.1/idmap/tls.py
@@ -0,0 +1,44 @@
+"""
+Thread local storage for instances cache
+"""
+
+import threading
+from weakref import WeakValueDictionary
+
+
+_tls = threading.local()
+
+
+def init_idmap():
+    _tls.idmap_cache = {}
+
+
+def create_cache(cls, reset=False):
+    idm_cache = getattr(_tls, 'idmap_cache', None)
+    if not idm_cache:
+        # create idmap cache
+        init_idmap()
+    elif not reset and cls in idm_cache:
+        # the class exists in the cache, nothing to do
+        return idm_cache[cls]
+    new_cache = {} if cls.use_strong_refs else WeakValueDictionary()
+    _tls.idmap_cache[cls] = new_cache
+    return new_cache
+
+
+def cache_instance(cls, instance, pk):
+    cache = create_cache(cls)
+    cache[pk] = instance
+
+
+def get_cached_instance(cls, pk):
+    if not hasattr(_tls, 'idmap_cache'):
+        return None
+    cache = _tls.idmap_cache.get(cls, None)
+    if not cache:
+        return None
+    return cache.get(pk, None)
+
+
+def flush_cache_key(cls, pk):
+    del _tls.idmap_cache[cls][pk]
