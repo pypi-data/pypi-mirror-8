2a7870826a0f1dd803a15cf1801099feb4680ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:47 +0000

    Add package Products.mediaPersons/0.63/Products.mediaPersons-0.63.tar.gz

---
+++ packages/Products.mediaPersons/Products.mediaPersons-0.63.tar.gz/Products.mediaPersons-0.63/Products/mediaPerson/interfaces/mediaperson.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+# -*- Additional Imports Here -*-
+
+
+class IMediaPerson(Interface):
+    """Person with media such as video and images"""
+
+    # -*- schema definition goes here -*-
---
+++ packages/Products.mediaPersons/Products.mediaPersons-0.63.tar.gz/Products.mediaPersons-0.63/setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of Products.mediaPerson
+"""
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.63'
+
+long_description = (
+    read('docs/README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('docs/CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('Products', 'mediaPerson', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('docs/CONTRIBUTORS.txt')
+    + '\n' +
+    'Download\n'
+    '********\n')
+
+tests_require = ['zope.testing']
+
+setup(name='Products.mediaPersons',
+      version=version,
+      description="Person type with folderish behaviour to store related media.",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='type folderish person',
+      author='David Jonas, Andre Goncalves',
+      author_email='david@intk.com, andre@intk.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite='Products.mediaPerson.tests.test_docs.test_suite',
+      entry_points="""
+      # -*- entry_points -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
