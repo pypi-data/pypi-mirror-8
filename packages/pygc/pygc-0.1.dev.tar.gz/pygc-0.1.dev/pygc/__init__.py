4bc9758f75d1689945381f01c335d33db6c0eee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:33 +0000

    Add package pygc/0.1.dev/pygc-0.1.dev.tar.gz

---
+++ packages/pygc/pygc-0.1.dev.tar.gz/pygc-0.1.dev/pygc/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.dev'
+
+from pygc.gc import great_circle
+from pygc.gc import great_distance
---
+++ packages/pygc/pygc-0.1.dev.tar.gz/pygc-0.1.dev/setup.py
@@ -0,0 +1,33 @@
+from __future__ import with_statement
+from setuptools import setup, find_packages
+from pkg_resources import parse_version
+from pygc import __version__
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+reqs = [line.strip() for line in open('requirements.txt')]
+
+setup(namespace_packages = [],
+    name                 = "pygc",
+    version              = __version__,
+    description          = "Great Circle calculations in Python using Vincenty's formulae",
+    long_description     = readme(),
+    license              = 'MIT',
+    author               = "Kyle Wilcox",
+    author_email         = "wilcox.kyle@gmail.com",
+    url                  = "https://github.com/kwilcox/pygc",
+    packages             = find_packages(),
+    install_requires     = reqs,
+    classifiers          = [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering',
+        ],
+)
