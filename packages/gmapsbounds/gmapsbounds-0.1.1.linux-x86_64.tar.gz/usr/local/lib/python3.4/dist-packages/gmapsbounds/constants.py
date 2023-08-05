60094cdc0fa3bcfdc849e9bdfed314639a8b5788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:51 +0000

    Add package gmapsbounds/0.1.1/gmapsbounds-0.1.1.linux-x86_64.tar.gz

---
+++ packages/gmapsbounds/gmapsbounds-0.1.1.linux-x86_64.tar.gz/usr/local/lib/python3.4/dist-packages/gmapsbounds/constants.py
@@ -0,0 +1,16 @@
+ZOOM_OUT = {
+	'40.7590615, -73.969231': 'Manhattan, NY',
+}
+
+MAX_NODE_DIFFERENCE = 75
+MAX_NODE_BACKTRACK = 50
+
+MENU_BORDERS = [[15, 123], [30, 429]]
+
+WATER_RED = list(range(148, 185))
+WATER_GREEN = list(range(193, 215))
+WATER_BLUE = list(range(253, 256))
+
+ALLOWABLE_RED = list(range(218, 241))
+ALLOWABLE_GREEN = list(range(0, 191))
+ALLOWABLE_BLUE = list(range(65, 181))
