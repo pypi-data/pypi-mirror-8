300d01ec7cfd54fca38eccff836d8b5031fd272e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:43 +0000

    Add package print_it/1.0.1/print_it-1.0.1.tar.gz

---
+++ packages/print_it/print_it-1.0.1.tar.gz/print_it-1.0.1/print_it.py
@@ -0,0 +1,12 @@
+                        
+"""this is a eaxmple of print out every element in list"""
+def print_it(the_list,level): 
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_it(each_item,level+1)
+                else:
+                   for tab in range(level):
+                                print("\t",end='')
+                   print(each_item)
+cast=["zhao","qian","sun","li",["zhou jia","wu yi",["zheng bing wu"]]]
+print_it(cast,0)
