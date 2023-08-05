2260b0ff5f85dd8852ab9d40353b9fe4a8cf12ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package cmsplugin-flickr-player/0.2.8/cmsplugin-flickr-player-0.2.8.tar.gz

---
+++ packages/cmsplugin-flickr-player/cmsplugin-flickr-player-0.2.8.tar.gz/cmsplugin-flickr-player-0.2.8/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Communications',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+]
+
+ 
+setup(
+    name='cmsplugin-flickr-player',
+    version='0.2.8',
+    description='Flickr iframe with photo set for django-cms',
+    author='ELCODO',
+    author_email='info@elcodo.pl',
+    url='https://github.com/elcodo/cmsplugin-flickr-player',
+    packages=['cmsplugin_flickr_player', 'cmsplugin_flickr_player.templates', 'cmsplugin_flickr_player.locale', 'cmsplugin_flickr_player.migrations'],
+    license=open('LICENSE').read(),
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    zip_safe=False,
+)
