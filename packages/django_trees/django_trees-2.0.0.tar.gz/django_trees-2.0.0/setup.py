e3564a596a83afcf26c94488133e4a4f6e85c31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:19 +0000

    Add package django_trees/2.0.0/django_trees-2.0.0.tar.gz

---
+++ packages/django_trees/django_trees-2.0.0.tar.gz/django_trees-2.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django_trees",
+    version="2.0.0",
+    author='imtapps',
+    author_email='serveradmin@imtapps.com',
+    description='Simple way to create, persist and manipulate reliable tree structures using Django models.',
+    long_description=file('README.rst').read(),
+    url='https://github.com/imtapps/django_trees',
+    install_requires=file('requirements.txt').read(),
+    packages=find_packages(exclude=('project', )),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
