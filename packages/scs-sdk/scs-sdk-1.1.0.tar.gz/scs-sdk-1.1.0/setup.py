5816e602af750c3992065aa42ecec027c6df1205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:24 +0000

    Add package scs-sdk/1.1.0/scs-sdk-1.1.0.tar.gz

---
+++ packages/scs-sdk/scs-sdk-1.1.0.tar.gz/scs-sdk-1.1.0/setup.py
@@ -0,0 +1,35 @@
+#-*- coding:UTF-8 -*-
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+PACKAGE = "sinastorage"
+NAME = "scs-sdk"
+DESCRIPTION = u"Python SDK For 新浪云存储"
+AUTHOR = "sina cloud storage"
+AUTHOR_EMAIL = "hanchao3@staff.sina.com.cn"
+URL = "http://open.sinastorage.com/"
+VERSION = '1.1.0'       #__import__(PACKAGE).__version__
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="BSD",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    url=URL,
+    download_url = 'https://github.com/SinaCloudStorage/SinaStorage-SDK-Python',
+    keywords = ['sina', 'scs', 'sinacloudstorage'],
+    packages=find_packages(exclude=["sample*", "sample"]),
+    install_requires=['filechunkio'],
+    
+)
