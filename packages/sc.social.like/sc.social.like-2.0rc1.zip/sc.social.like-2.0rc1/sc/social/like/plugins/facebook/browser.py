1042bd914eebfdaab0dcf629f27682ceee0faf67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:43 +0000

    Add package sc.social.like/2.0rc1/sc.social.like-2.0rc1.zip

---
+++ packages/sc.social.like/sc.social.like-2.0rc1.zip/sc.social.like-2.0rc1/sc/social/like/plugins/facebook/browser.py
@@ -0,0 +1,89 @@
+# -*- coding:utf-8 -*-
+from Products.CMFCore.utils import getToolByName
+from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from sc.social.like.plugins.facebook.utils import facebook_language
+from sc.social.like.utils import get_content_image
+from sc.social.like.utils import get_language
+from zope.component import getMultiAdapter
+
+BASE_URL = 'https://www.facebook.com/plugins/like.php?'
+PARAMS = 'locale=%s&href=%s&send=false&layout=%s&show_faces=true&action=%s'
+
+
+class PluginView(BrowserView):
+
+    enabled_portal_types = []
+    typebutton = ''
+    fb_enabled = False
+    fbaction = ''
+    fbadmins = ''
+    language = 'en_US'
+
+    metadata = ViewPageTemplateFile("templates/metadata.pt")
+    plugin = ViewPageTemplateFile("templates/plugin.pt")
+
+    def __init__(self, context, request):
+        super(PluginView, self).__init__(context, request)
+        pp = getToolByName(context, 'portal_properties')
+
+        self.context = context
+        self.title = context.title
+        self.description = context.Description()
+        self.request = request
+        self.portal_state = getMultiAdapter((self.context, self.request),
+                                            name=u'plone_portal_state')
+        self.portal = self.portal_state.portal()
+        self.site_url = self.portal_state.portal_url()
+        self.portal_title = self.portal_state.portal_title()
+        self.url = context.absolute_url()
+        self.language = facebook_language(get_language(context), self.language)
+        self.sheet = getattr(pp, 'sc_social_likes_properties', None)
+        self.image = get_content_image(context, width=1200, height=630)
+        if self.sheet:
+            self.fbaction = self.sheet.getProperty("fbaction", "")
+            self.fbapp_id = self.sheet.getProperty("fbapp_id", "")
+            self.fbadmins = self.sheet.getProperty("fbadmins", "")
+            self.button = self.typebutton
+
+    def image_height(self):
+        """ Return height to image
+        """
+        img = self.image
+        if img:
+            return img.height
+
+    def image_type(self):
+        """ Return content type to image
+        """
+        img = self.image
+        if img:
+            return getattr(img, 'content_type',
+                           getattr(img, 'mimetype', 'image/jpeg'))
+
+    def image_width(self):
+        """ Return width to image
+        """
+        img = self.image
+        if img:
+            return img.width
+
+    def image_url(self):
+        """ Return url to image
+        """
+        img = self.image
+        if img:
+            return img.url
+        else:
+            return '%s/logo.png' % self.site_url
+
+    @property
+    def typebutton(self):
+        typebutton = self.sheet.getProperty("typebutton", "")
+        if typebutton == 'horizontal':
+            typebutton = 'button_count'
+            self.width = '90px'
+        else:
+            typebutton = 'box_count'
+            self.width = '55px'
+        return typebutton
---
+++ packages/sc.social.like/sc.social.like-2.0rc1.zip/sc.social.like-2.0rc1/sc/social/like/testing.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from PIL import Image
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from StringIO import StringIO
+
+import random
+
+
+def generate_image(width, height):
+    # Mandelbrot fractal
+    # FB - 201003254
+    # drawing area
+    xa = -2.0
+    xb = 1.0
+    ya = -1.5
+    yb = 1.5
+    maxIt = 25  # max iterations allowed
+    # image size
+    image = Image.new("RGB", (width, height))
+    c = complex(random.random() * 2.0 - 1.0, random.random() - 0.5)
+
+    for y in range(height):
+        zy = y * (yb - ya) / (height - 1) + ya
+        for x in range(width):
+            zx = x * (xb - xa) / (width - 1) + xa
+            z = complex(zx, zy)
+            for i in range(maxIt):
+                if abs(z) > 2.0:
+                    break
+                z = z * z + c
+            r = i % 4 * 64
+            g = i % 8 * 32
+            b = i % 16 * 16
+            image.putpixel((x, y), b * 65536 + g * 256 + r)
+
+    output = StringIO()
+    image.save(output, format="PNG")
+    return output.getvalue()
+
+
+class Fixture(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import sc.social.like
+        self.loadZCML(package=sc.social.like)
+
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        self.applyProfile(portal, 'sc.social.like:default')
+
+FIXTURE = Fixture()
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,),
+    name='sc.social.like:Integration',
+)
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FIXTURE,),
+    name='sc.social.like:Functional',
+)
