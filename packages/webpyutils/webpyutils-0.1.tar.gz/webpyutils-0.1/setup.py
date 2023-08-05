e81c454c368eb1eeca4adc318b9956bd475a7c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package webpyutils/0.1/webpyutils-0.1.tar.gz

---
+++ packages/webpyutils/webpyutils-0.1.tar.gz/webpyutils-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='webpyutils',
+    version='0.1',
+    description='web.py utilities to build quick RESTful API Services',
+    author='Jesse V. Yen',
+    author_email='jesse@jads.com',
+    url='https://github.com/jvy1106/webpyutils',
+    download_url='https://github.com/jvy1106/webpyutils/tarball/0.1',
+    keywords = ['api', 'restful', 'server'],
+    install_requires=[
+        'DBUtils',
+        'web.py',
+        'flup',
+    ],  
+    packages=[
+        'webpyutils',
+    ],  
+)
