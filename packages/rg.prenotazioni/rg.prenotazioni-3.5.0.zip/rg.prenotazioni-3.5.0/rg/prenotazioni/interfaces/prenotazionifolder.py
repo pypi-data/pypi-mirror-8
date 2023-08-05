0975c31b82339f886bbc034cb298360c25f7a35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package rg.prenotazioni/3.5.0/rg.prenotazioni-3.5.0.zip

---
+++ packages/rg.prenotazioni/rg.prenotazioni-3.5.0.zip/rg.prenotazioni-3.5.0/rg/prenotazioni/interfaces/prenotazionifolder.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from plone.contentrules.engine.interfaces import IRuleAssignable
+
+
+class IPrenotazioniFolder(IRuleAssignable):
+
+    """Description of the Example Type"""
