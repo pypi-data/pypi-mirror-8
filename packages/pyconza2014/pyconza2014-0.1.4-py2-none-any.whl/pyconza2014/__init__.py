0214143acc58abb1a032c975536e4a0035c13136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:46 +0000

    Add package pyconza2014/0.1.4/pyconza2014-0.1.4-py2-none-any.whl

---
+++ packages/pyconza2014/pyconza2014-0.1.4-py2-none-any.whl/pyconza2014/__init__.py
@@ -0,0 +1,10 @@
+"""PyConZA 2014 Demo"""
+
+__author__ = 'Michael Joseph'
+__email__ = 'michaeljoseph@gmail.com'
+__url__ = 'https://github.com/michaeljoseph/pyconza2014'
+__version__ = '0.1.4'
+
+
+def pyconza2014():
+    return 'Hello PyConZa 2014!'
