5e82064fc4dd7279d158d3fadc8026969c730756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:05 +0000

    Add package mewa/0.18/mewa-0.18.tar.gz

---
+++ packages/mewa/mewa-0.18.tar.gz/mewa-0.18/mewa/Protocol.py
@@ -0,0 +1,24 @@
+'''
+Created on 2014-07-30
+
+@author: Krzysztof Langner
+'''
+import json
+
+def connect(channel, device, password, prefixes):
+    msg = {"type": "connect", "channel":channel, "device":device, "password":password, "subscribe": prefixes}
+    return json.dumps(msg)
+
+def disconnect():
+    return '{"type": "disconnect"}'
+
+def getDevices():
+    return '{"type": "get-devices"}'
+
+def sendEvent(eventId, params, ack=False):
+    msg = {"type": "send-event", "id": eventId, "ack": ack, "params": json.dumps(params)}
+    return json.dumps(msg)
+
+def sendMessage(device, msgId, params):
+    msg = {"type": "send-message", "device": device, "id": msgId, "params": json.dumps(params)}
+    return json.dumps(msg)
