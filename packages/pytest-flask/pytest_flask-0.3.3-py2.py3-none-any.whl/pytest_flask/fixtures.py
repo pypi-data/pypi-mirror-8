cc96321944eac2d0f40e825e4c6eb008c162dcf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:12 +0000

    Add package pytest-flask/0.3.3/pytest_flask-0.3.3-py2.py3-none-any.whl

---
+++ packages/pytest-flask/pytest_flask-0.3.3-py2.py3-none-any.whl/pytest_flask/fixtures.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.yield_fixture
+def client(app):
+    """A Flask test client. An instance of :class:`flask.testing.TestClient`
+    by default.
+    """
+    with app.test_client() as client:
+        yield client
+
+
+@pytest.fixture
+def config(app):
+    """An application config."""
+    return app.config
+
+
+@pytest.fixture(params=['application/json', 'text/html'])
+def mimetype(request):
+    return request.param
+
+
+@pytest.fixture
+def accept_mimetype(mimetype):
+    return [('Accept', mimetype)]
+
+
+@pytest.fixture
+def accept_json(request):
+    return accept_mimetype('application/json')
+
+
+@pytest.fixture
+def accept_jsonp():
+    return accept_mimetype('application/json-p')
+
+
+@pytest.fixture(params=['*', '*/*'])
+def accept_any(request):
+    return accept_mimetype(request.param)
