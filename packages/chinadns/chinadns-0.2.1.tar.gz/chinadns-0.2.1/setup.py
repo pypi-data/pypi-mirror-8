9bab3164be9271ef97643611359e31e680483f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:27 +0000

    Add package chinadns/0.2.1/chinadns-0.2.1.tar.gz

---
+++ packages/chinadns/chinadns-0.2.1.tar.gz/chinadns-0.2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name="chinadns",
+    version="0.2.1",
+    license='MIT',
+    description="A DNS forwarder that ignore incorrect responses",
+    author='clowwindy',
+    author_email='clowwindy42@gmail.com',
+    url='https://github.com/clowwindy/ChinaDNS',
+    packages=['chinadns'],
+    package_data={
+        'chinadns': ['README.rst', 'LICENSE', 'config.json']
+    },
+    install_requires=[
+        'shadowsocks==2.0.11'
+    ],
+    entry_points="""
+    [console_scripts]
+    chinadns = chinadns.dnsrelay:main
+    """,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: Proxy Servers',
+    ],
+    long_description=long_description,
+)
