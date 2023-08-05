957b251398d42e575826627f61b2abe40a3a4e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:05 +0000

    Add package django-subdomain-instances/0.7/django-subdomain-instances-0.7.tar.gz

---
+++ packages/django-subdomain-instances/django-subdomain-instances-0.7.tar.gz/django-subdomain-instances-0.7/instances/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
+
+from .fields import DNSLabelField
+
+class InstanceManager(models.Manager):
+    def for_instance(self, instance):
+        return self.get_query_set().filter(instance=instance)
+
+@python_2_unicode_compatible
+class Instance(models.Model):
+    label = DNSLabelField( db_index=True, unique=True )
+    title = models.CharField( max_length=100 )
+    description = models.TextField( blank=True )
+    users = models.ManyToManyField(User, related_name='instances', blank=True)
+    created_by = models.ForeignKey(User, related_name='created_instances', null=True, blank=True)
+
+    def __str__(self):
+        return u'Instance %s' % self.label
+
+    def get_absolute_url(self):
+        url = 'http://%s.%s' % (self.label, getattr(settings, 'BASE_HOST', '127.0.0.1.xip.io'))
+        if getattr(settings, 'BASE_PORT', None):
+            url += ':%s' % settings.BASE_PORT
+        return url
+
+class InstanceMixin(models.Model):
+    instance = models.ForeignKey(Instance)
+
+    objects = InstanceManager()
+
+    class Meta:
+        abstract = True
---
+++ packages/django-subdomain-instances/django-subdomain-instances-0.7.tar.gz/django-subdomain-instances-0.7/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+file_dir = os.path.abspath(os.path.dirname(__file__))
+
+def read_file(filename):
+    filepath = os.path.join(file_dir, filename)
+    return open(filepath).read()
+
+setup(
+    name="django-subdomain-instances",
+    version='0.7',
+    description='A way of allowing subdomains to be served by the same project, and associating objects with particular subdomains.',
+    long_description=read_file('README.rst'),
+    author='mySociety',
+    author_email='matthew@mysociety.org',
+    url='https://github.com/mysociety/django-subdomain-instances',
+    packages=find_packages(),
+    install_requires=[
+        'Django >= 1.4, < 1.8',
+    ],
+    classifiers=[
+        'Framework :: Django',
+    ],
+)
