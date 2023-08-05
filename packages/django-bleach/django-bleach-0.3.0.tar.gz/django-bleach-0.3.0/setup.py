1f0b7138c5e66ddaabcb4c9af85caf5f83be8048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:11 +0000

    Add package django-bleach/0.3.0/django-bleach-0.3.0.tar.gz

---
+++ packages/django-bleach/django-bleach-0.3.0.tar.gz/django-bleach-0.3.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='django-bleach',
+    version="0.3.0",
+    description='Easily use bleach with Django models and templates',
+    author='Tim Heap',
+    author_email='tim@timheap.me',
+    url='https://bitbucket.org/tim_heap/django-bleach',
+    packages=find_packages(),
+    install_requires=[
+        'bleach',
+        'Django>=1.3',
+    ],
+    package_data={},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
