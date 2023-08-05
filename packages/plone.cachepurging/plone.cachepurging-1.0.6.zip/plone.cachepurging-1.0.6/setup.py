5ca167c52aa89556aa199f425d0ac8a4d84b3977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:08 +0000

    Add package plone.cachepurging/1.0.6/plone.cachepurging-1.0.6.zip

---
+++ packages/plone.cachepurging/plone.cachepurging-1.0.6.zip/plone.cachepurging-1.0.6/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+version = '1.0.6'
+
+setup(name='plone.cachepurging',
+      version=version,
+      description="Cache purging support for Zope 2 applications",
+      long_description=open('README.txt').read() + '\n' +
+                       open('CHANGES.txt').read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone cache purge',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.cachepurging',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'five.globalrequest',
+          'plone.registry',
+          'z3c.caching'
+          'zope.annotation',
+          'zope.component',
+          'zope.event',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.lifecycleevent',
+          'zope.schema'
+          'zope.testing'  # on purger.py not on tests
+          'Zope2',
+      ],
+      extras_require={
+        'test': ['plone.app.testing'],
+      },
+      )
