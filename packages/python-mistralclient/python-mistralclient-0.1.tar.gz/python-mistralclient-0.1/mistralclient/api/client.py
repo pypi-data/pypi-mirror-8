d8d3c53f25595abf126322a9ea82ba1ecb7485a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:56 +0000

    Add package python-mistralclient/0.1/python-mistralclient-0.1.tar.gz

---
+++ packages/python-mistralclient/python-mistralclient-0.1.tar.gz/python-mistralclient-0.1/mistralclient/api/client.py
@@ -0,0 +1,53 @@
+# Copyright 2013 - Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import six
+
+from mistralclient.api.v1 import client as client_v1
+from mistralclient.api.v2 import client as client_v2
+
+
+def client(mistral_url=None, username=None, api_key=None,
+           project_name=None, auth_url=None, project_id=None,
+           endpoint_type='publicURL', service_type='workflow',
+           auth_token=None, user_id=None):
+
+        if mistral_url and not isinstance(mistral_url, six.string_types):
+            raise RuntimeError('Mistral url should be string')
+
+        if not mistral_url:
+            mistral_url = "http://localhost:8989/v2"
+
+        version = determine_client_version(mistral_url)
+
+        if version == 1:
+            client_cls = client_v1.Client
+        else:
+            client_cls = client_v2.Client
+
+        return client_cls(mistral_url=mistral_url, username=username,
+                          api_key=api_key, project_name=project_name,
+                          auth_url=auth_url, project_id=project_id,
+                          endpoint_type=endpoint_type,
+                          service_type=service_type, auth_token=auth_token,
+                          user_id=user_id)
+
+
+def determine_client_version(mistral_url):
+    if mistral_url.find("v2") != -1:
+        return 2
+    elif mistral_url.find("v1") != -1:
+        return 1
+
+    raise RuntimeError("Can not determine mistral API version")
---
+++ packages/python-mistralclient/python-mistralclient-0.1.tar.gz/python-mistralclient-0.1/mistralclient/api/v2/actions.py
@@ -0,0 +1,65 @@
+# Copyright 2014 - Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import json
+from mistralclient.api import base
+
+
+class Action(base.Resource):
+    resource_name = 'Action'
+
+
+class ActionManager(base.ResourceManager):
+    resource_class = Action
+
+    def create(self, definition):
+        self._ensure_not_empty(definition=definition)
+
+        resp = self.client.http_client.post(
+            '/actions',
+            json.dumps({'definition': definition})
+        )
+
+        if resp.status_code != 201:
+            self._raise_api_exception(resp)
+
+        return [self.resource_class(self, resource_data)
+                for resource_data in base.extract_json(resp, 'actions')]
+
+    def update(self, definition):
+        self._ensure_not_empty(definition=definition)
+
+        resp = self.client.http_client.put(
+            '/actions',
+            json.dumps({'definition': definition})
+        )
+
+        if resp.status_code != 200:
+            self._raise_api_exception(resp)
+
+        return [self.resource_class(self, resource_data)
+                for resource_data in base.extract_json(resp, 'actions')]
+
+    def list(self):
+        return self._list('/actions', response_key='actions')
+
+    def get(self, name):
+        self._ensure_not_empty(name=name)
+
+        return self._get('/actions/%s' % name)
+
+    def delete(self, name):
+        self._ensure_not_empty(name=name)
+
+        self._delete('/actions/%s' % name)
