ee9004dc40dd66fe37db62f97036041797b35537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:36 +0000

    Add package pypelogs/0.3.5/pypelogs-0.3.5.zip

---
+++ packages/pypelogs/pypelogs-0.3.5.zip/pypelogs-0.3.5/pypeout/__init__.py
@@ -0,0 +1,34 @@
+from .json_out import JSONOut
+from .mongodb import MongoDB
+from .csv_out import CSVOut
+from .sql_out import SQLOut
+from .text_out import TextOut
+from .wget import Wget
+
+CLASSES = {
+    'csv': CSVOut,
+    'json': JSONOut,
+    'mongodb': MongoDB,
+    'sql': SQLOut,
+    'text': TextOut,
+    'wget': Wget
+}
+
+
+def output_for(s):
+    spec_args = s.split(':', 1)
+    clz = CLASSES.get(spec_args[0])
+    if not clz:
+        raise NoSuchOutputException(spec_args[0])
+    return clz() if len(spec_args) == 1 else clz(spec_args[1])
+
+def register(s, clz):
+    CLASSES[s] = clz
+
+
+class NoSuchOutputException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
