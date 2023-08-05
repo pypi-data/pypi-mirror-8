a6b5455efac1d478b96d3080461e8f6f187d4d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:59 +0000

    Add package bika.health/3.1.5/bika.health-3.1.5.tar.gz

---
+++ packages/bika.health/bika.health-3.1.5.tar.gz/bika.health-3.1.5/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os
+
+version = '3.1.5'
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='bika.health',
+      version=version,
+      description="Bika Health Open Source LIMS",
+      long_description=open("README.md").read() + "\n\n" +
+                       open("CHANGELOG.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        ],
+      keywords=['lims', 'health', 'bika', 'opensource'],
+      author='Bika Laboratory Systems',
+      author_email='support@bikalabs.com',
+      maintainer='Naralabs',
+      maintainer_email='info@naralabs.com',
+      url='http://github.com/bikalabs/bika.health',
+      license='AGPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['bika'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'bika.lims>=3.1.5',
+          'archetypes.schemaextender',
+          'collective.wtf',
+      ],
+      extras_require={
+          'test': [
+                  'plone.app.testing',
+                  'robotsuite',
+                  'robotframework-selenium2library',
+                  'plone.app.robotframework',
+                  'robotframework-debuglibrary',
+              ]
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
