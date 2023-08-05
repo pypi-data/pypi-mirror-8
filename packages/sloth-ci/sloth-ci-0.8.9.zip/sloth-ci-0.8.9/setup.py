1aed8fcbd408da41c4e5994363ec53a84b2543b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:09 +0000

    Add package sloth-ci/0.8.9/sloth-ci-0.8.9.zip

---
+++ packages/sloth-ci/sloth-ci-0.8.9.zip/sloth-ci-0.8.9/setup.py
@@ -0,0 +1,46 @@
+﻿from distutils.core import setup
+
+from os.path import join
+
+import sloth_ci
+from sloth_ci.utils import get_default_configs_path
+
+
+try:
+    readme = open('README.rst').read()
+except:
+    readme = sloth_ci.__long_description__
+
+
+setup(
+    name=sloth_ci.__title__,
+    version=sloth_ci.__version__,
+    author=sloth_ci.__author__,
+    description=sloth_ci.__description__,
+    long_description=readme,
+    author_email=sloth_ci.__author_email__,
+    url='https://bitbucket.org/moigagoo/sloth-ci',
+    packages=[
+        'sloth_ci'
+    ],
+    install_requires = [
+        'CherryPy',
+        'awesome-slugify',
+        'configs>=3.0.2'
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'],
+    entry_points={
+        'console_scripts':
+            ['sloth-ci = sloth_ci.api:main']
+    },
+    data_files=[
+        (get_default_configs_path(), ['server.conf']),
+        (join(get_default_configs_path(), 'apps'), [])
+    ]
+)
---
+++ packages/sloth-ci/sloth-ci-0.8.9.zip/sloth-ci-0.8.9/sloth_ci/api.py
@@ -0,0 +1,88 @@
+﻿from sys import exit
+from importlib import import_module
+
+from os.path import abspath, join, exists
+from os import makedirs
+
+from argparse import ArgumentParser
+
+import cherrypy
+
+from configs import load
+
+from .utils import ConfigChecker, get_default_configs_path, get_default_logs_path
+from .bed import Bed
+
+def run(host, port, log_dir, daemon, config_locations):
+    '''Runs CherryPy loop to listen for payload.
+
+    :param host: host
+    :param port: port
+    :param log_dir: directory to store logs (absolute or relative)
+    :param config_files: Sloth app config files
+    :param config_dirs: directories to look for Sloth app config files in
+    '''
+
+    cherrypy.config.update(
+        {
+            'environment': 'production',
+            'server.socket_host': host,
+            'server.socket_port': port,
+            'log.access_file': abspath(join(log_dir, '_access.log')),
+            'log.error_file': abspath(join(log_dir, '_error.log'))
+        }
+    )
+
+    if daemon:
+        cherrypy.process.plugins.Daemonizer(cherrypy.engine).subscribe()
+
+    ConfigChecker(cherrypy.engine, config_locations).subscribe()
+
+    bed = Bed(cherrypy.engine)
+
+    cherrypy.engine.subscribe('sloth-add', bed.add_sloth)
+    cherrypy.engine.subscribe('sloth-update', bed.update_sloth)
+    cherrypy.engine.subscribe('sloth-remove', bed.remove_sloth)
+    
+    cherrypy.engine.subscribe('stop', bed.remove_all_sloths)
+
+    cherrypy.quickstart(bed.make_listener())
+
+
+def main():
+    '''Main API function.'''
+
+    parser = ArgumentParser()
+    parser.add_argument('-s', '--sconfig', help='Server config.')
+    parser.add_argument('-H', '--host', help='Host for the Sloth server (overrides value in sconfig).')
+    parser.add_argument('-p', '--port', type=int, help='Port for the Sloth server (overrides value in sconfig).')
+    parser.add_argument('-l', '--log_dir', help='Where the log files should be stored (overrides value in sconfig).')
+    parser.add_argument('-d', '--daemon', action='store_true', help='Run as daemon.')
+    parser.add_argument('config', nargs='*', help='Sloth app config files or dirs.')
+
+    parsed_args = parser.parse_args()
+
+    sconfig_file = parsed_args.sconfig
+
+    if sconfig_file:
+        sconfig = load(sconfig_file)
+    else:
+        try:
+            sconfig = load(join(get_default_configs_path(), 'server.conf'))
+        except:
+            sconfig = {}
+
+    host = parsed_args.host or sconfig.get('host')
+    port = parsed_args.port or sconfig.get('port')
+    log_dir = parsed_args.log_dir or sconfig.get('log_dir') or get_default_logs_path()
+    daemon = parsed_args.daemon or sconfig.get('daemon')
+
+    if not (host and port and log_dir):
+        exit('Missing server param(s).')
+
+    config_locations = parsed_args.config or join(get_default_configs_path(), 'apps')
+
+    if not exists(abspath(log_dir)):
+        makedirs(abspath(log_dir))
+
+    run(host, port, log_dir, daemon, config_locations)
