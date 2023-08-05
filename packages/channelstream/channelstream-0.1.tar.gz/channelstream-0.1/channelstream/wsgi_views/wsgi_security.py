94da59a6fa4cbc58bdaa6e13501c8006a3342835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:12 +0000

    Add package channelstream/0.1/channelstream-0.1.tar.gz

---
+++ packages/channelstream/channelstream-0.1.tar.gz/channelstream-0.1/channelstream/wsgi_views/wsgi_security.py
@@ -0,0 +1,28 @@
+from pyramid.security import Allow, Everyone, ALL_PERMISSIONS, authenticated_userid
+from channelstream.util import hmac_validate
+
+class RequestBasicChannenge(Exception):
+    pass
+
+class APIFactory(object):
+    def __init__(self, request):
+        self.__acl__ = []
+        config = request.registry.settings
+        req_url_secret = request.params.get('secret')
+        req_secret = request.headers.get('x-channelstream-secret',
+                                         req_url_secret)
+        if request.environ['REMOTE_ADDR'] not in config['allow_posting_from']:
+            return
+        if req_secret:
+            hmac_validate(config['secret'], request.path, req_secret)
+        else:
+            return
+        self.__acl__ = [(Allow, Everyone, ALL_PERMISSIONS)]
+
+class BasicAuthFactory(object):
+    def __init__(self, request):
+        self.__acl__ = []
+        user = authenticated_userid(request)
+        if user != 'admin':
+            raise RequestBasicChannenge()
+        self.__acl__ = [(Allow, Everyone, ALL_PERMISSIONS)]
