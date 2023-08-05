983ba68ae2f1ab5b2eb6adf3f34265c74ad702e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package piprot/0.8.0/piprot-0.8.0.tar.gz

---
+++ packages/piprot/piprot-0.8.0.tar.gz/piprot-0.8.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+"""
+Use pandoc to convert README.md to README.rst before uploading
+   pandoc README.md -o README.rst
+"""
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('HISTORY.rst') as f:
+    history = f.read()
+
+
+setup(
+    name='piprot',
+    version='0.8.0',
+    author='Brenton Cleeland',
+    author_email='brenton@brntn.me',
+    packages=['piprot',],
+    url='http://github.com/sesh/piprot',
+    license='MIT License',
+    description='How rotten are your requirements?',
+    long_description=readme + '\n\n' + history,
+    entry_points={
+        'console_scripts': [
+            'piprot = piprot.piprot:piprot',
+        ]
+    },
+    install_requires=[
+        'requests',
+        'requests-futures',
+        'six'
+    ],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    )
+)
