fdcef830f6bf515d3c2c0797d6c0edfc1136e245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:18 +0000

    Add package ped_parser/0.8/ped_parser-0.8.tar.gz

---
+++ packages/ped_parser/ped_parser-0.8.tar.gz/ped_parser-0.8/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# with open('README.txt') as file:
+long_description = 'A pedigree parser.'
+
+setup(name="ped_parser",
+	version="0.8",
+	author="Mans Magnusson",
+	author_email="mans.magnusson@scilifelab.se",
+    license='MIT License',
+    install_requires=['pytest'],
+	description=("A ped file parser."),
+	long_description = long_description,
+    packages={'ped_parser'},
+    url='https://github.com/moonso/ped_parser',
+    # scripts=[''],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Intended Audience :: Science/Research",
+    ],
+)
