42eff2aa8a7b179e832317987a8732bc8cc8618c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:36 +0000

    Add package zmqdecorators/0.7.0/zmqdecorators-0.7.0.tar.gz

---
+++ packages/zmqdecorators/zmqdecorators-0.7.0.tar.gz/zmqdecorators-0.7.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+import subprocess
+
+git_version = str(subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD'])).strip()
+
+setup(
+    name='zmqdecorators',
+#    version='0.7.1dev-%s' % git_version,
+    version='0.7.0',
+    author='Eero "rambo" af Heurlin',
+    author_email='rambo@iki.fi',
+    packages=['zmqdecorators',],
+    license='GNU LGPL',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'tornado>=2.0',
+        'pyzmq>=2.0',
+        'pybonjour>=1.1',
+    ],
+    url='https://github.com/rambo/python-zmqdecorators',
+)
+
