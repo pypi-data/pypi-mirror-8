4ce625f040ef4b0213e7a1a0d2ba68267966cecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:42 +0000

    Add package django-variant/0.0.2/django_variant-0.0.2-py2.7.egg

---
+++ packages/django-variant/django_variant-0.0.2-py2.7.egg/variant/middleware.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+import six
+
+from .utils import get_experiment_cookie_name
+
+
+class VariantMiddleware(object):
+    def process_request(self, request):
+        request.variant_experiments = {}
+
+    def process_response(self, request, response):
+        for name, variant in six.iteritems(request.variant_experiments):
+            if variant:
+                cookie_name = get_experiment_cookie_name(name)
+                response.set_cookie(
+                    cookie_name, variant,
+                    max_age=getattr(
+                        settings, 'VARIANT_MAX_COOKIE_AGE', 2592000),
+                    secure=getattr(settings, 'VARIANT_SECURE_COOKIE', False))
+
+        return response
