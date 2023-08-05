acb1fc3800453c18196656308e65b37f414e20b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:51 +0000

    Add package django-permission/0.8.3/django-permission-0.8.3.tar.gz

---
+++ packages/django-permission/django-permission-0.8.3.tar.gz/django-permission-0.8.3/src/permission/conf.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+"""
+django-permission application configure
+"""
+__author__ = 'Alisue <lambdalisue@hashnote.net>'
+__all__ = ('settings',)
+from django.conf import settings
+from appconf import AppConf
+from permission.handlers import LogicalPermissionHandler
+
+class PermissionConf(AppConf):
+    DEFAULT_PERMISSION_HANDLER = LogicalPermissionHandler
+    """Default permission handler class"""
+
+    CHECK_PERMISSION_PRESENCE = settings.DEBUG
+    """Check if the specified string permission exists"""
+
+    REPLACE_BUILTIN_IF = True
+    """Whether replace builtin if templatetag"""
+
+    DEFAULT_APL_FIELD_NAME = 'author'
+    DEFAULT_APL_ANY_PERMISSION = False
+    DEFAULT_APL_CHANGE_PERMISSION = True
+    DEFAULT_APL_DELETE_PERMISSION = True
+
+    DEFAULT_CPL_FIELD_NAME = 'collaborators'
+    DEFAULT_CPL_ANY_PERMISSION = False
+    DEFAULT_CPL_CHANGE_PERMISSION = True
+    DEFAULT_CPL_DELETE_PERMISSION = False
+
+    DEFAULT_GIPL_ANY_PERMISSION = False
+    DEFAULT_GIPL_ADD_PERMISSION = True
+    DEFAULT_GIPL_CHANGE_PERMISSION = True
+    DEFAULT_GIPL_DELETE_PERMISSION = False
+
+    DEFAULT_OSPL_ANY_PERMISSION = False
+    DEFAULT_OSPL_CHANGE_PERMISSION = True
+    DEFAULT_OSPL_DELETE_PERMISSION = True
+
+    DEFAULT_SPL_ANY_PERMISSION = False
+    DEFAULT_SPL_ADD_PERMISSION = True
+    DEFAULT_SPL_CHANGE_PERMISSION = True
+    DEFAULT_SPL_DELETE_PERMISSION = False
+
+    AUTODISCOVER_MODULE_NAME = 'perms'
+    AUTODISCOVER_VARIABLE_NAME = 'PERMISSION_LOGICS'
