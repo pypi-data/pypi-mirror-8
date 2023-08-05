ad33314b6fa34835b333cefab685d302cf55d71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:57 +0000

    Add package guoid/0.2/guoid-0.2.tar.gz

---
+++ packages/guoid/guoid-0.2.tar.gz/guoid-0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='guoid',
+      version='0.2',
+      description='Python SnowFlake Clone - Global Unique Object ID',
+      author='DaeMyung Kang',
+      author_email='charsyam@gmail.com',
+      url='https://github.com/charsyam/python-guoid',
+      platforms='any',
+      install_requires=[],
+      packages=['guoid'],
+      scripts=['guoid/__init__.py']
+      )
