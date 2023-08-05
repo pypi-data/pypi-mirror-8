8d975fc33f0450cf8474cb9b76daadb7429d1d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:57 +0000

    Add package okcupyd/0.1.0/okcupyd-0.1.0.tar.gz

---
+++ packages/okcupyd/okcupyd-0.1.0.tar.gz/okcupyd-0.1.0/examples/start.py
@@ -0,0 +1,6 @@
+from okcupyd import *
+from okcupyd.util import enable_logger
+
+
+af = AttractivenessFinder()
+u = User()
---
+++ packages/okcupyd/okcupyd-0.1.0.tar.gz/okcupyd-0.1.0/okcupyd/__init__.py
@@ -0,0 +1,5 @@
+from .search import search
+from .user import User
+from .attractiveness_finder import AttractivenessFinder
+from .statistics import Statistics
+from .util import save_file
