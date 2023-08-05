d89ccd5c0801b3db6b394c4069941ebc4b029d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:38 +0000

    Add package faster-closeio/0.1.16/faster_closeio-0.1.16.tar.gz

---
+++ packages/faster-closeio/faster_closeio-0.1.16.tar.gz/faster_closeio-0.1.16/closeio/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Denis Cornehl'
+__email__ = 'denis.cornehl@gmail.com'
+__version__ = '0.1.16'
+
+
+from closeio import CloseIO, CloseIOError  # noqa
