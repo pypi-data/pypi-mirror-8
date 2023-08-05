18bf5963d6060e7811b59423cde37d4099c9e3ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:18 +0000

    Add package senpy/0.2.1/senpy-0.2.1.tar.gz

---
+++ packages/senpy/senpy-0.2.1.tar.gz/senpy-0.2.1/senpy/blueprints.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#    Copyright 2014 J. Fernando Sánchez Rada - Grupo de Sistemas Inteligentes
+#                                                       DIT, UPM
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+'''
+Simple Sentiment Analysis server
+'''
+from flask import Blueprint, render_template, request, jsonify, current_app
+import json
+
+nif_blueprint = Blueprint("NIF Sentiment Analysis Server", __name__)
+
+PARAMS = {"input": {"aliases": ["i", "input"],
+                    "required": True,
+                    "help": "Input text"
+                    },
+          "informat": {"aliases": ["f", "informat"],
+                       "required": False,
+                       "default": "text",
+                       "options": ["turtle", "text"],
+                       },
+          "intype": {"aliases": ["intype", "t"],
+                     "required": False,
+                     "default": "direct",
+                     "options": ["direct", "url", "file"],
+                     },
+          "outformat": {"aliases": ["outformat", "o"],
+                        "default": "json-ld",
+                        "required": False,
+                        "options": ["json-ld"],
+                        },
+          "algorithm": {"aliases": ["algorithm", "a", "algo"],
+                        "required": False,
+                        },
+          "language": {"aliases": ["language", "l"],
+                       "required": False,
+                       "options": ["es", "en"],
+                       },
+          "urischeme": {"aliases": ["urischeme", "u"],
+                        "required": False,
+                        "default": "RFC5147String",
+                        "options": "RFC5147String"
+                        },
+          }
+
+
+def get_params(req):
+    indict = None
+    if req.method == 'POST':
+        indict = req.form
+    elif req.method == 'GET':
+        indict = req.args
+    else:
+        raise ValueError("Invalid data")
+
+    outdict = {}
+    wrongParams = {}
+    for param, options in PARAMS.iteritems():
+        for alias in options["aliases"]:
+            if alias in indict:
+                outdict[param] = indict[alias]
+        if param not in outdict:
+            if options.get("required", False):
+                wrongParams[param] = PARAMS[param]
+            else:
+                if "default" in options:
+                    outdict[param] = options["default"]
+        else:
+            if "options" in PARAMS[param] and \
+                outdict[param] not in PARAMS[param]["options"]:
+                wrongParams[param] = PARAMS[param]
+    if wrongParams:
+        message = {"status": "failed", "message": "Missing or invalid parameters"}
+        message["parameters"] = outdict
+        message["errors"] = {param:error for param, error in wrongParams.iteritems()}
+        raise ValueError(json.dumps(message))
+    return outdict
+
+def basic_analysis(params):
+    response = {"@context": ["http://demos.gsi.dit.upm.es/eurosentiment/static/context.jsonld",
+                             {
+                                 "@base": "{}#".format(request.url.encode('utf-8'))
+                             }
+                             ],
+                "analysis": [{
+                    "@type": "marl:SentimentAnalysis"
+                }],
+                "entries": []
+                }
+    if "language" in params:
+        response["language"] = params["language"]
+    for idx, sentence in enumerate(params["input"].split(".")):
+        response["entries"].append({
+            "@id": "Sentence{}".format(idx),
+            "nif:isString": sentence
+        })
+    return response
+
+@nif_blueprint.route('/', methods=['POST', 'GET'])
+def home(entries=None):
+    try:
+        params = get_params(request)
+    except ValueError as ex:
+        return ex.message
+    response = current_app.senpy.analyse(**params)
+    return jsonify(response)
+
+@nif_blueprint.route('/plugins/', methods=['POST', 'GET'])
+@nif_blueprint.route('/plugins/<plugin>', methods=['POST', 'GET'])
+@nif_blueprint.route('/plugins/<plugin>/<action>', methods=['POST', 'GET'])
+def plugins(plugin=None, action="list"):
+    print current_app.senpy.plugins.keys()
+    if plugin:
+        plugs = {plugin:current_app.senpy.plugins[plugin]}
+    else:
+        plugs = current_app.senpy.plugins
+    if action == "list":
+        dic = {plug:plugs[plug].enabled for plug in plugs}
+        return jsonify(dic)
+    elif action == "disable":
+        plugs[plugin].enabled = False
+        return "Ok"
+    elif action == "enable":
+        plugs[plugin].enabled = True
+        return "Ok"
+    else:
+        return "action '{}' not allowed".format(action), 404
+
+if __name__ == '__main__':
+    import config
+    from flask import Flask
+    app = Flask(__name__)
+    app.register_blueprint(nif_blueprint)
+    app.debug = config.DEBUG
+    app.run()
---
+++ packages/senpy/senpy-0.2.1.tar.gz/senpy-0.2.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+  name = 'senpy',
+  packages = ['senpy'], # this must be the same as the name above
+  version = '0.2.1',
+  description = '''
+    A sentiment analysis server implementation. Designed to be \
+extendable, so new algorithms and sources can be used.
+    ''',
+  author = 'J. Fernando Sanchez',
+  author_email = 'balkian@gmail.com',
+  url = 'https://github.com/balkian/senpy', # use the URL to the github repo
+  download_url = 'https://github.com/balkian/senpy/archive/0.2.1.tar.gz',
+  keywords = ['eurosentiment', 'sentiment', 'emotions', 'nif'], # arbitrary keywords
+  classifiers = [],
+)
