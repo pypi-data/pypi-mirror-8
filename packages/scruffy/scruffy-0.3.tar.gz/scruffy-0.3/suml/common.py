2488066fade95d9570badc0d888f6a9ced523b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:43 +0000

    Add package scruffy/0.3/scruffy-0.3.tar.gz

---
+++ packages/scruffy/scruffy-0.3.tar.gz/scruffy-0.3/suml/common.py
@@ -0,0 +1,93 @@
+# Copyright (C) 2011 by Aivars Kalvans <aivars.kalvans@gmail.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import subprocess
+from PIL import Image, ImageChops
+from operator import attrgetter
+
+def hasFont(font_name):
+    """ Checks if font is installed (using fc-list; are there other possibilities?) """
+    stdout = subprocess.Popen(['fc-list', font_name], shell=True, stdout=subprocess.PIPE).stdout
+    return len(stdout.readlines()) > 0
+
+def defaultScruffyFont():
+    """ Returns installed font with scruffy look """
+    for font_name in ('Purisa',):
+        if hasFont(font_name):
+            return font_name
+    return None
+
+class Box:
+    n = 0
+    def __init__(self, name, spec):
+        self.name = name
+        self.spec = spec
+        self.uid = 'A%03d' % Box.n
+        Box.n += 1
+
+    def update(self, spec):
+        if len(self.spec) < len(spec):
+            self.spec = spec
+        return self
+
+class Boxes:
+    def __init__(self):
+        self.boxes = {}
+
+    def addBox(self, spec):
+        name = spec.split('|')[0].strip()
+        if name not in self.boxes:
+            self.boxes[name] = Box(name, spec)
+        return self.boxes[name].update(spec)
+
+    def getBoxes(self):
+        res = self.boxes.values()
+        res.sort(key=attrgetter('uid'))
+        return res 
+
+def splitYUML(spec):
+    word = ''
+    shapeDepth = 0
+    for c in spec:
+        if c == '[':
+            shapeDepth += 1
+        elif c == ']':
+            shapeDepth -= 1
+
+        if shapeDepth == 1 and c == '[':
+            yield word.strip()
+            word = c
+            continue
+
+        word += c
+        if shapeDepth == 0 and c == ']':
+            yield word.strip()
+            word = ''
+    if word:
+        yield word.strip()
+
+def crop(fin, fout):
+    img = Image.open(fin)
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    bg = Image.new('RGB', img.size, (255, 255, 255))
+    diff = ImageChops.difference(img, bg)
+    area = img.crop(diff.getbbox())
+    area.save(fout, 'png')
---
+++ packages/scruffy/scruffy-0.3.tar.gz/scruffy-0.3/suml/suml2pic.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# Copyright (C) 2011 by Aivars Kalvans <aivars.kalvans@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+'''
+[class]
+[class]message>[class]
+[class]<message[class]
+'''
+
+import os
+import common
+
+sequence_pic = os.path.join(os.path.dirname(__file__), 'sequence.pic')
+
+def sumlExpr(spec):
+    expr = []
+    for part in common.splitYUML(spec):
+        if not part: continue
+        if part == ',':
+            if expr: yield expr
+            expr = []
+
+        # [something like this]
+        elif part[0] == '[' and part[-1] == ']':
+            part = part[1:-1]
+            expr.append(('record', part.strip()))
+        # <message
+        # >message
+        elif part[0] in '<>':
+            expr.append((part[0], part[1:].strip()))
+        # message>
+        # message<
+        elif part[-1] in '<>':
+            expr.append((part[-1], part[:-1].strip()))
+
+    if expr: yield expr
+
+def getFontWidth():
+    return 0.13
+
+def suml2pic(spec, options):
+    boxes = common.Boxes()
+    exprs = list(sumlExpr(spec))
+
+    pic = []
+    pic.append('.PS')
+    pic.append('copy "%s";' % (sequence_pic))
+    pic.append('underline=0;')
+
+    messages = []
+    for expr in exprs:
+        assert len(expr) in (1, 3)
+        if len(expr) == 1:
+            assert expr[0][0] == 'record'
+            boxes.addBox(expr[0][1])
+
+        elif len(expr) == 3:
+            assert expr[0][0] == 'record'
+            assert expr[2][0] == 'record'
+
+            box1 = boxes.addBox(expr[0][1])
+            box2 = boxes.addBox(expr[2][1])
+
+            msgType = expr[1][0]
+            if msgType == '>':
+                messages.append('message(%s,%s,"%s");' % (box1.uid, box2.uid, expr[1][1]))
+            elif msgType == '<':
+                messages.append('message(%s,%s,"%s");' % (box2.uid, box1.uid, expr[1][1]))
+
+    all_boxes = boxes.getBoxes()
+
+    for box in all_boxes:
+        #pic.append('object(%s,"%s");' % (box.uid, box.spec))
+        pic.append('object3(%s,"%s",%f);' % (box.uid, box.spec, getFontWidth() * len(box.spec)))
+    pic.append('step();')
+    for box in all_boxes:
+        pic.append('active(%s);' % (box.uid))
+
+    pic.extend(messages)
+
+    pic.append('step();')
+    for box in all_boxes:
+        pic.append('complete(%s);' % (box.uid))
+
+    pic.append('.PE')
+    return '\n'.join(pic) + '\n'
+
+def transform(expr, fout, options):
+    pic = suml2pic(expr, options)
+
+    if options.png or options.svg:
+        import subprocess
+        import StringIO
+
+        if options.scruffy:
+            import scruffy
+
+            svg = subprocess.Popen(['pic2plot', '-Tsvg'], stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate(input=pic)[0]
+            if options.png:
+                tocrop = StringIO.StringIO()
+                scruffy.transform(StringIO.StringIO(svg), tocrop, options)
+                common.crop(StringIO.StringIO(tocrop.getvalue()), fout)
+            else:
+                scruffy.transform(StringIO.StringIO(svg), fout, options)
+        elif options.png:
+            png = subprocess.Popen(['pic2plot', '-Tpng'], stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate(input=pic)[0]
+            common.crop(StringIO.StringIO(png), fout)
+        elif options.svg:
+            subprocess.Popen(['pic2plot', '-Tsvg'], stdin=subprocess.PIPE, stdout=fout).communicate(input=pic)
+    else:
+        fout.write(pic)
