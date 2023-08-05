c66d10d384fd09c632c5cd8336583fb7298cfc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    Add package python-ldap-test/0.0.9/python-ldap-test-0.0.9.tar.gz

---
+++ packages/python-ldap-test/python-ldap-test-0.0.9.tar.gz/python-ldap-test-0.0.9/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+from setuptools.command.install import install
+
+
+try:
+    description = open('README.txt').read()
+except:
+    description = open('README.md').read()
+
+
+setup(
+    name='python-ldap-test',
+    version='0.0.9',
+    author='Adrian Gruntkowski',
+    author_email='adrian.gruntkowski@gmail.com',
+    packages=['ldap_test', 'ldap_test.test'],
+    url='https://github.com/zoldar/python-ldap-test/',
+    license='LICENSE.txt',
+    description=('Tool for testing code speaking with LDAP server. Allows to easily'
+                 ' configure and run an embedded, in-memory LDAP server. Uses'
+                 ' UnboundID LDAP SDK through Py4J.'),
+    keywords = ['testing', 'tests', 'test', 'ldap'],
+    long_description=description,
+    install_requires=[
+        "py4j >= 0.8",
+    ],
+    package_data={
+        '': ['*.txt'],
+        'ldap_test': ['*.jar'],
+    }
+)
