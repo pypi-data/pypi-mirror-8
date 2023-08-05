fd0f5d1855d86e80dbc909737e5b0f8cd1c56a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package rst2html5-tools/0.2/rst2html5-tools-0.2.tar.gz

---
+++ packages/rst2html5-tools/rst2html5-tools-0.2.tar.gz/rst2html5-tools-0.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup
+
+setup(
+    name='rst2html5-tools',
+    version='0.2',
+    author='Mariano Guerra',
+    author_email='luismarianoguerra@gmail.com',
+    url='https://github.com/marianoguerra/rst2html5',
+    long_description=open('README.rst').read(),
+    packages=['html5css3'],
+    package_data={'html5css3': ['thirdparty/*/*.*']},
+    include_package_data=True,
+    install_requires=['docutils'],
+    entry_points={
+        'console_scripts': [
+            'rst2html5 = html5css3.main:main',
+        ]},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+    ],
+)
