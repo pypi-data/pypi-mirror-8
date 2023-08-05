31bab752e3556378900c8efb57b24a5735253a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package django-passreset/0.3/django-passreset-0.3.tar.gz

---
+++ packages/django-passreset/django-passreset-0.3.tar.gz/django-passreset-0.3/setup.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-passreset',
+    version='0.3',
+    author='Ben Davis',
+    author_email='bendavis78@gmail.com',
+    url='http://github.com/bendavis78',
+    description='Easily add password reset workflows to your django project.',
+    keywords='django admin',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=find_packages(),
+    include_package_data=True
+)
