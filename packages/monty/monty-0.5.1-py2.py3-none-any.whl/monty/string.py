a000eec61a8711c1bb1f10ded81da7f03f5953b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package monty/0.5.1/monty-0.5.1-py2.py3-none-any.whl

---
+++ packages/monty/monty-0.5.1-py2.py3-none-any.whl/monty/string.py
@@ -0,0 +1,57 @@
+"""
+Useful additional string functions.
+"""
+
+from __future__ import absolute_import, division
+
+__author__ = "Shyue Ping Ong"
+__copyright__ = "Copyright 2012, The Materials Project"
+__version__ = "0.1"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "ongsp@ucsd.edu"
+__date__ = "2/28/14"
+
+
+def remove_non_ascii(s):
+    """
+    Remove non-ascii characters in a file. Needed when support for non-ASCII
+    is not available.
+
+    Args:
+        s (str): Input string
+
+    Returns:
+        String with all non-ascii characters removed.
+    """
+    return "".join(i for i in s if ord(i) < 128)
+
+
+def marquee(text="", width=78, mark='*'):
+    """
+    Return the input string centered in a 'marquee'.
+
+    Args:
+        text (str): Input string
+        width (int): Width of final output string.
+        mark (str): Character used to fill string.
+
+    :Examples:
+
+    >>> marquee('A test', width=40)
+    '**************** A test ****************'
+
+    >>> marquee('A test', width=40, mark='-')
+    '---------------- A test ----------------'
+
+    marquee('A test',40, ' ')
+    '                 A test                 '
+    """
+    if not text:
+        return (mark*width)[:width]
+
+    nmark = (width-len(text)-2)//len(mark)//2
+    if nmark < 0: 
+        nmark = 0
+
+    marks = mark * nmark
+    return '%s %s %s' % (marks, text, marks)
