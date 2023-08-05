9670d7f7d42bb15b443c7d1db518059a863ea729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:22 +0000

    Add package plone.app.workflowmanager-overhaul/1.1a/plone.app.workflowmanager-overhaul-1.1a.tar.gz

---
+++ packages/plone.app.workflowmanager-overhaul/plone.app.workflowmanager-overhaul-1.1a.tar.gz/plone.app.workflowmanager-overhaul-1.1a/src/plone/app/workflowmanager/tests/test_utils.py
@@ -0,0 +1,31 @@
+import unittest2 as unittest
+
+from plone.app.workflowmanager.testing import INTEGRATION_MANAGER_TESTING
+from plone.app.workflowmanager.testing import BaseTest
+
+from plone.app.workflowmanager.utils import generate_id
+
+
+class TestUtils(BaseTest):
+
+    layer = INTEGRATION_MANAGER_TESTING
+
+    def test_generate_id(self):
+        title = "1"
+        ids = []
+        id = generate_id(title, ids)
+        self.assertEquals(title, id)
+
+    def test_generate_id_with_ids(self):
+        title = '1'
+        ids = ['1', '2', '3']
+        id = generate_id(title, ids)
+        self.assertEquals(title + '-1', id)
+
+        ids.append(id)
+        id = generate_id(title, ids)
+        self.assertEquals(title + '-2', id)
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
