96b51e43df04cbd6cbb5ef137de140c8d4efca7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:51 +0000

    Add package django-tablib/3.1.1/django-tablib-3.1.1.tar.gz

---
+++ packages/django-tablib/django-tablib-3.1.1.tar.gz/django-tablib-3.1.1/django_tablib/admin/actions.py
@@ -0,0 +1,42 @@
+from django import get_version
+from django.http import HttpResponse
+from django.utils.encoding import smart_str
+from django.utils.translation import ugettext as _
+
+from django_tablib.datasets import SimpleDataset
+from django_tablib.base import mimetype_map
+
+
+def tablib_export_action(modeladmin, request, queryset, file_type="xls"):
+    """
+    Allow the user to download the current filtered list of items
+
+    :param format:
+        One of the formats supported by tablib (e.g. "xls", "csv", "html",
+        etc.)
+    """
+
+    dataset = SimpleDataset(queryset, headers=None)
+    filename = '%s.%s' % (
+        smart_str(modeladmin.model._meta.verbose_name_plural), file_type)
+
+    response_kwargs = {}
+    key = 'content_type' if get_version().split('.')[1] > 6 else 'mimetype'
+    response_kwargs[key] = mimetype_map.get(
+        file_type, 'application/octet-stream')
+
+    response = HttpResponse(getattr(dataset, file_type), **response_kwargs)
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    return response
+
+
+def xls_export_action(*args, **kwargs):
+    return tablib_export_action(format="xls", *args, **kwargs)
+xls_export_action.__doc__ = tablib_export_action.__doc__
+xls_export_action.short_description = _("Export to Excel")
+
+
+def csv_export_action(*args, **kwargs):
+    return tablib_export_action(format="csv", *args, **kwargs)
+csv_export_action.__doc__ = tablib_export_action.__doc__
+csv_export_action.short_description = _("Export to CSV")
---
+++ packages/django-tablib/django-tablib-3.1.1.tar.gz/django-tablib-3.1.1/django_tablib/base.py
@@ -0,0 +1,77 @@
+import datetime
+import tablib
+
+from django.template.defaultfilters import date
+from django.utils.encoding import smart_unicode
+from django.utils.translation import ugettext_lazy as _
+
+mimetype_map = {
+    'xls': 'application/vnd.ms-excel',
+    'csv': 'text/csv',
+    'html': 'text/html',
+    'yaml': 'text/yaml',
+    'json': 'application/json',
+}
+
+
+class BaseDataset(tablib.Dataset):
+
+    encoding = 'utf-8'
+
+    def __init__(self):
+        data = map(self._getattrs, self.queryset)
+        super(BaseDataset, self).__init__(headers=self.header_list, *data)
+
+    def _cleanval(self, value, attr):
+        if callable(value):
+            value = value()
+        elif value is None or unicode(value) == u"None":
+            value = ""
+
+        t = type(value)
+        if t is str:
+            return value
+        elif t is bool:
+            value = _("Y") if value else _("N")
+            return smart_unicode(value).encode(self.encoding)
+        elif t in [datetime.date, datetime.datetime]:
+            return date(value, 'SHORT_DATE_FORMAT').encode(self.encoding)
+
+        return smart_unicode(value).encode(self.encoding)
+
+    def _getattrs(self, obj):
+        attrs = []
+        for attr in self.attr_list:
+            if callable(attr):
+                attr = self._cleanval(attr(obj), attr)
+            else:
+                if hasattr(obj, 'get_%s_display' % attr):
+                    value = getattr(obj, 'get_%s_display' % attr)()
+                else:
+                    value = getattr(obj, attr)
+                attr = self._cleanval(value, attr)
+            attrs.append(attr)
+        return attrs
+
+    def append(self, *args, **kwargs):
+        # Thanks to my previous decision to simply not support columns, this
+        # dumb conditional is necessary to preserve backwards compatibility.
+        if len(args) == 1:
+            # if using old syntax, just set django_object to args[0] and
+            # col to None
+            django_object = args[0]
+            col = None
+        else:
+            # otherwise assume both row and col may have been passed and
+            # handle appropriately
+            django_object = kwargs.get('row', None)
+            col = kwargs.get('col', None)
+
+        # make sure that both row and col are in a format that can be passed
+        # straight to tablib
+        if django_object is not None:
+            row = self._getattrs(django_object)
+        else:
+            row = django_object
+
+        super(BaseDataset, self).append(row=row, col=col)
