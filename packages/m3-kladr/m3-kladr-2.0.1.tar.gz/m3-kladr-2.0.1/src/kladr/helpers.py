3d7619a3170fcd0227e983883ea1df2c4e6e2080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package m3-kladr/2.0.1/m3-kladr-2.0.1.tar.gz

---
+++ packages/m3-kladr/m3-kladr-2.0.1.tar.gz/m3-kladr-2.0.1/src/kladr/helpers.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+
+import models
+
+def geo_rows_query(parent = None, filter = None):
+    '''
+    Возвращает QuerySet для отбора записей из подсправочника KladrGeo
+    '''
+    if parent == '-1':
+        parent = None
+    rows = models.KladrGeo.objects.filter(parent=parent)
+    return rows
+
+def geo_streets_query(parent, filter):
+    '''
+    Возвращает QuerySet для отбора записей подсправочника KladrStreet
+    '''
+    streets = models.KladrGeo.objects.get(pk=parent)
+    return streets.kladrstreet_set.all()
+
---
+++ packages/m3-kladr/m3-kladr-2.0.1.tar.gz/m3-kladr-2.0.1/src/tests/core/tests.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+'''
+Created on 06.09.2011
+
+@author: akvarats
+'''
+
+import json
+
+from django.test import TestCase
+
+from kladr import (actions,
+                   addrfield, 
+                   appmeta,
+                   fill_kladr,
+                   helpers, 
+                   models,
+                   ui,)
+
+
+class SomeTests(TestCase):
+    '''
+    Тесты для источников данных
+    '''
+    def test1(self):   
+        pass
