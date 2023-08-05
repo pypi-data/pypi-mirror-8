2220189d64c95a07f050c24383e1c8cfed7ac1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:45 +0000

    Add package TinySpider/1.0.0/TinySpider-1.0.0.zip

---
+++ packages/TinySpider/TinySpider-1.0.0.zip/TinySpider-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+        name  = 'TinySpider',
+        version  = '1.0.0',
+        py_modules = ['TinySpider'],
+        author = 'chaif',
+        author_email = 'admin@cfishacker.com',
+        url = 'https://github.com/cforth/TinySpider',
+        defcription = 'A simple web spider of Python 3',
+    )
