56162b5272e189b5e302ea135bcebcb70de715fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:35 +0000

    Add package callwith/0.0.3/callwith-0.0.3.zip

---
+++ packages/callwith/callwith-0.0.3.zip/callwith-0.0.3/callwith.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+
+class CallWith:
+    __slot__ = ('args', 'kwargs')
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, func):
+        return func(*self.args, **self.kwargs)
+
+    def __str__(self):
+        items = []
+        items.extend(map(repr, self.args))
+        items.extend(map(lambda kv: '{}={!r}'.format(*kv), self.kwargs.items()))
+        return ', '.join(items)
+
+    def __repr__(self):
+        return 'CallWith({})'.format(self)
---
+++ packages/callwith/callwith-0.0.3.zip/callwith-0.0.3/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(
+    name = 'callwith',
+    version = '0.0.3',
+    py_modules = ['callwith'],
+    description = 'Call function with parameters',
+    long_description = open('README').read(),
+    author = 'huyx',
+    author_email = 'ycyuxin@gmail.com',
+    url = 'https://pypi.python.org/pypi/callwith',
+    keywords = ['call', 'function'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+    )
