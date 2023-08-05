97d194f5314c708314e24b3390d4402a542c4492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:29 +0000

    Add package django-render-json/0.2.6/django-render-json-0.2.6.tar.gz

---
+++ packages/django-render-json/django-render-json-0.2.6.tar.gz/django-render-json-0.2.6/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='django-render-json',
+    version='0.2.6',
+    description='utilities to render json as http response for django views',
+    author='jarvys',
+    author_email='yuhan534@126.com',
+	url='https://github.com/limijiaoyin/django-render-json',
+    py_modules=['django_render_json'],
+    install_requires=[
+        'django>=1.6'
+    ],
+	keywords=['django', 'json', 'response', 'http']
+)
