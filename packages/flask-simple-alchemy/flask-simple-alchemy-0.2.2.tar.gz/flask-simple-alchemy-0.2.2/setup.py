8f1318d2827b4cc2328e4da795171134ad2ec660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:10 +0000

    Add package flask-simple-alchemy/0.2.2/flask-simple-alchemy-0.2.2.tar.gz

---
+++ packages/flask-simple-alchemy/flask-simple-alchemy-0.2.2.tar.gz/flask-simple-alchemy-0.2.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+project = "flask-simple-alchemy"
+
+setup(
+    name=project,
+    version='0.2.2',  # Alpha Release
+    url='https://github.com/jlgoldb2/flask-simple-alchemy',
+    description='A Simplification of SQLAlchemy\'s declarative using'
+                ' Flask-SQLAlchemy',
+    author='Jason Goldberger',
+    license='MIT',
+    author_email='jlgoldb2@asu.edu',
+    packages=["flask_simple_alchemy"],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy',
+        'pytest',
+        'testfixtures'
+    ],
+    test_suite='tests'
+)
