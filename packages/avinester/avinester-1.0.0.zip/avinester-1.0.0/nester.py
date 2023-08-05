10770f0ed532e1109ee62733977b039016bf65ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:39 +0000

    Add package avinester/1.0.0/avinester-1.0.0.zip

---
+++ packages/avinester/avinester-1.0.0.zip/avinester-1.0.0/nester.py
@@ -0,0 +1,7 @@
+"""First Python program to process lists"""
+def print_list(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_list(each_item)
+        else:
+            print(each_item)
---
+++ packages/avinester/avinester-1.0.0.zip/avinester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'avinester',
+        version = '1.0.0',
+        py_modules = ['nester'],
+        author = 'Avishek.Banerjee',
+        author_email = 'avishekbanerjee.ranchi@gmail.com',
+        url = 'https://www.facebook.com/avishekbanerjee.ranchi',
+        decsription = 'A nested list printer'
+    )
