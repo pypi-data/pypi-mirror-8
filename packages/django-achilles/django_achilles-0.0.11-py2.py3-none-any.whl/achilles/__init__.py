f2161c96fc1449797da484e85942e2b0143af246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:39 +0000

    Add package django-achilles/0.0.11/django_achilles-0.0.11-py2.py3-none-any.whl

---
+++ packages/django-achilles/django_achilles-0.0.11-py2.py3-none-any.whl/achilles/__init__.py
@@ -0,0 +1,14 @@
+"""
+Achilles provides a framework to create Django applications using AJAX. Trough
+the use of some primitives it allows you to build asynchronous without the need
+of writting javascript to manage client side.
+"""
+
+__version__ = (0, 0, 11)
+
+
+def get_version():
+    """
+    Get current version as a string (ie: '1.0.0')
+    """
+    return '.'.join([str(x) for x in __version__])
---
+++ packages/django-achilles/django_achilles-0.0.11-py2.py3-none-any.whl/achilles/tables/__init__.py
@@ -0,0 +1,8 @@
+from . common import Table
+from . columns import Column, MergeColumn, ButtonColumn, ActionColumn
+
+__all__ = ["Table",
+           "Column",
+           "MergeColumn",
+           "ButtonColumn",
+           "ActionColumn"]
