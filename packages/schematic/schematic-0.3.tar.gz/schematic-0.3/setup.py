6745d6ad34574c90d9924e86b1020c634a479789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:13 +0000

    Add package schematic/0.3/schematic-0.3.tar.gz

---
+++ packages/schematic/schematic-0.3.tar.gz/schematic-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='schematic',
+    version='0.3',
+    url='https://github.com/mozilla/schematic',
+    author_email='dev-webdev@lists.mozilla.org',
+    scripts=['schematic'],
+)
