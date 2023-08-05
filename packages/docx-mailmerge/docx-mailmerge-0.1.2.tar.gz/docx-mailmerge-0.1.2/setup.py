a719a5b39936fb71f3f7fb307d5e846b075228f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:11 +0000

    Add package docx-mailmerge/0.1.2/docx-mailmerge-0.1.2.tar.gz

---
+++ packages/docx-mailmerge/docx-mailmerge-0.1.2.tar.gz/docx-mailmerge-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version = '0.1.2'
+
+setup(name='docx-mailmerge',
+      version=version,
+      description='Performs a Mail Merge on docx (Microsoft Office Word) files',
+      long_description=open('README.rst').read(),
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Text Processing',
+      ],
+      author='Bouke Haarsma',
+      author_email='bouke@webatoom.nl',
+      url='http://github.com/Bouke/docx-mailmerge',
+      license='MIT',
+      py_modules=['mailmerge'],
+      zip_safe=False,
+)
