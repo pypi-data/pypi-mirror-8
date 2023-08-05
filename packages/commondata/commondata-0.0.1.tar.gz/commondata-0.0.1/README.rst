4a2634760864442306c4d276c3cce0ba5591e969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:28 +0000

    Add package commondata/0.0.1/commondata-0.0.1.tar.gz

---
+++ packages/commondata/commondata-0.0.1.tar.gz/commondata-0.0.1/README.rst
@@ -0,0 +1,45 @@
+====================================
+The ``commondata`` namespace package
+====================================
+
+Note: we are discussing whether this package is meaningful.  See
+http://lino-framework.org/tickets/109.html
+
+.. module:: commondata
+
+This package is the heart of "common data", a sustainable way of
+maintaining and sharing structured common knowledge.  The Python
+package itself contains just some utilities and defines the
+``commondata`` namespace. It is the base for packages like
+
+- `commondata.be <https://github.com/lsaffre/commondata-be>`_ : 
+  Common knowledge about Belgium
+- `commondata.ee <https://github.com/lsaffre/commondata-ee>`_: 
+  Common knowledge about Estonia
+
+
+Maintained in Python
+--------------------
+
+The Python programming language brings together two qualities 
+
+- a syntax which makes it easy (or at least possible) to be used by
+  non-programmers
+- a powerful programming language working behind the scenes
+
+
+
+
+Freely available under the GPL
+------------------------------
+
+Free software should not depend on non-free material.
+
+Designed to be imported
+-----------------------
+
+The library does **not** provide much querying functionality.  Just
+the basic minimum, used to write test cases.  This is a design
+choice. This data is meant to be imported into existing systems which
+offer their own querying facilities.
+
---
+++ packages/commondata/commondata-0.0.1.tar.gz/commondata-0.0.1/commondata/utils.py
@@ -0,0 +1,97 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2014 Luc Saffre
+# This file is part of the commondata library.
+# The commondata library is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3 of
+# the License, or (at your option) any later version.
+# The commondata library is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with the commondata library; if not, see
+# <http://www.gnu.org/licenses/>.
+
+"""
+
+Note that Town is a subclass of Municipality. E.g. Võru vald and Võru
+linn are the same object while Võru maakond is a separat object.
+
+"""
+
+class Place(object):
+    value = None
+
+    def __init__(self, pg, parent, *args, **kwargs):
+        self.children = []
+        self.parent = parent
+        if parent is not None:
+            parent.children.append(self)
+        for i, v in enumerate(args):
+            k = pg.args[i]
+            if k in kwargs:
+                raise Exception("Multiple values for %s" % k)
+            kwargs[k] = v
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    @classmethod
+    def factory(self, pg):
+        def create(*args, **kwargs):
+            prev = pg.current
+            if prev is None:
+                parent = None
+            elif prev.value < self.value:
+                parent = prev
+            else:
+                parent = prev.parent
+                while parent and not self.can_be_child(parent):
+                    parent = parent.parent
+            i = self(pg, parent, *args, **kwargs)
+            pg.current = i
+            return i
+        return create
+
+    @classmethod
+    def can_be_child(self, p):
+        if p.value < self.value:
+            return True
+        return False
+
+    def match(self, **kwargs):
+        for k, v in kwargs.items():
+            if getattr(self, k) != v:
+                return False
+        return True
+
+    def find(self, **kwargs):
+        def f(i):
+            return i.match(**kwargs)
+        return filter(f, self.children)
+
+    def get(self, **kwargs):
+        q = self.find(**kwargs)
+        if len(q) != 1:
+            raise Exception("Found %d items!" % len(q))
+        return q[0]
+
+
+class PlaceGenerator(object):
+    def __init__(self):
+        self.current = None
+
+    def install(self, *classes):
+        for c in classes:
+            k = c.__name__.lower()
+            if hasattr(self, k):
+                raise Exception("Attempt to redefine name %r" % k)
+            setattr(self, k, c.factory(self))
+
+    def set_args(self, args):
+        self.args = tuple(args.split())
+
+
+
+
