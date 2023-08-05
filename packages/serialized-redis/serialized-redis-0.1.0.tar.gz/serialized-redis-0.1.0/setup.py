98ae6d06b64bdc24b64d3c9cbb80980a81ec2d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package serialized-redis/0.1.0/serialized-redis-0.1.0.tar.gz

---
+++ packages/serialized-redis/serialized-redis-0.1.0.tar.gz/serialized-redis-0.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# SerializedRedis
+# author: Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
+# https://github.com/evandrolg
+# License: MIT
+
+from setuptools import setup
+
+
+setup(
+    name='serialized-redis',
+    version='0.1.0',
+    description='A solution to have lists and dictionaries serialized using redis-py.',
+    author='EvandroLG',
+    author_email='evandrolgoncalves@gmail.com',
+    keywords=['Redis', 'redis-py', 'serialized datas'],
+    url='http://github.com/EvandroLG/serialized-redis',
+    license='MIT',
+    tests_require='pytest>=2.5.0',
+    install_requires=[
+        'redis',
+    ],
+    packages=['serialized_redis']
+)
