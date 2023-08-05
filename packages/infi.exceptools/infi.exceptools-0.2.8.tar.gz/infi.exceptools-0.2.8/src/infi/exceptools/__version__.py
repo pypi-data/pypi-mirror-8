6856b8ae0c57e6d7e9d25736e8d95f48e43910a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:40 +0000

    Add package infi.exceptools/0.2.8/infi.exceptools-0.2.8.tar.gz

---
+++ packages/infi.exceptools/infi.exceptools-0.2.8.tar.gz/infi.exceptools-0.2.8/src/infi/exceptools/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.8"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '6e5991f8103158b4869d0bc5cc9ce3b3bd5f5bde'
+__git_head_subject__ = 'HIP-1278'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
