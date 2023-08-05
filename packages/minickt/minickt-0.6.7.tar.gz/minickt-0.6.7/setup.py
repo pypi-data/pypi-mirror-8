38628a739844c5d94abeef88e86d391e16540117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:16 +0000

    Add package minickt/0.6.7/minickt-0.6.7.tar.gz

---
+++ packages/minickt/minickt-0.6.7.tar.gz/minickt-0.6.7/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+	name = 'minickt',
+	packages = ['minickt', 'cktParser', 'cktWriter'],
+	scripts = ['minickt.py'],
+	version = '0.6.7',
+	description = 'Analyzer and simulator of logic circuit',
+	author = 'dokelung',
+	author_email = 'dokelung@gmail.com',
+	url = 'https://github.com/dokelung/minickt',
+	download_url = 'https://github.com/dokelung/minickt/tarball/v0.6.7',
+	keywords = ['circuit','eda','verification','logic','gate'],
+	classifiers = [],
+)
