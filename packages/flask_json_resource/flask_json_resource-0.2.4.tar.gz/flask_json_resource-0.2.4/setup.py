6e7a0a289402fa7780fbbb6a73ab24a247935efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:44 +0000

    Add package flask_json_resource/0.2.4/flask_json_resource-0.2.4.tar.gz

---
+++ packages/flask_json_resource/flask_json_resource-0.2.4.tar.gz/flask_json_resource-0.2.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from setuptools_behave import behave_test
+
+
+__VERSION__ = '0.2.4'
+
+setup(
+    name='flask_json_resource',
+    version=__VERSION__,
+    packages=['flask_json_resource', 'flask_json_resource.features',
+              'flask_json_resource.features.steps'],
+    package_data={
+        'flask_json_resource.features.steps': ['features/steps/schemas/*.json']
+    },
+    long_description=open('./README.rst').read(),
+    author='Ernst Odolphi',
+    author_email='ernst.odolphi@gmail.com',
+    install_requires=['flask', 'json_resource', 'flask-pymongo'],
+    tests_require=['behave', 'ensure'],
+    cmdclass={"behave_test": behave_test}
+)
+
+
+
+
