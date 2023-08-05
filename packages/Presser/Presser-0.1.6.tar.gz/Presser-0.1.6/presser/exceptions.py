2e07acd7620b6adef5ffc816783bdde9a127da2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:45 +0000

    Add package Presser/0.1.6/Presser-0.1.6.tar.gz

---
+++ packages/Presser/Presser-0.1.6.tar.gz/Presser-0.1.6/presser/exceptions.py
@@ -0,0 +1,15 @@
+
+class PresserJavaScriptParseError(Exception):
+    pass
+
+class PresserInvalidVineIdError(Exception):
+    pass
+
+class PresserURLError(Exception):
+    pass
+
+class Presser404Error(Exception):
+    pass
+
+class PresserRequestError(Exception):
+    pass
