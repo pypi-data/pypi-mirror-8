cba2fb3e323f7b1360a1d8eb3a7d294a2f5afa36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package fzsl/0.1/fzsl-0.1.tar.gz

---
+++ packages/fzsl/fzsl-0.1.tar.gz/fzsl-0.1/fzsl/__init__.py
@@ -0,0 +1,19 @@
+from . core import (
+    FuzzyMatch,
+)
+
+from . ui import (
+    ncurses,
+    SimplePager,
+)
+
+from . scanner import (
+    ConfigError,
+    NoTypeError,
+    Scanner,
+    scanner_from_configparser,
+    SimpleScanner,
+    SubprocessError,
+    UnknownTypeError,
+)
+
