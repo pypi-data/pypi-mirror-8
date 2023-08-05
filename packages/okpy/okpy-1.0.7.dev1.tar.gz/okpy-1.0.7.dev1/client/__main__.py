91f801f8c08b937f375b90843346b0932091096a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:53 +0000

    Add package okpy/1.0.7.dev1/okpy-1.0.7.dev1.tar.gz

---
+++ packages/okpy/okpy-1.0.7.dev1.tar.gz/okpy-1.0.7.dev1/client/__main__.py
@@ -0,0 +1,13 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.getcwd())
+
+import ok
+
+def main():
+    ok.ok_main(ok.parse_input())
+
+if __name__ == '__main__':
+    main()
+
