c554a8fa4017537f384841fad75934865c9b77bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:52 +0000

    Add package tabkit/0.9/tabkit-0.9.tar.gz

---
+++ packages/tabkit/tabkit-0.9.tar.gz/tabkit-0.9/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="tabkit",
+    version="0.9",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            'tcat = tabkit.scripts:cat',
+            'tcut = tabkit.scripts:cut',
+            'tsrt = tabkit.scripts:sort',
+            'tjoin = tabkit.scripts:join',
+            'tmap_awk = tabkit.scripts:map',
+            'tpretty = tabkit.scripts:pretty'
+        ]
+    },
+    author="Andrei Fyodorov",
+    author_email="sour-times@yandex.ru",
+    description="Coreutils-like kit for headed tab-separated files processing",
+    license="PSF",
+    url="https://github.com/andreifyodorov/tabkit"
+)
