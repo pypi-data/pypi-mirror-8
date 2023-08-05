f5823d3ab8e6d74711ac8d0e82e592064d5bb7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:43 +0000

    Add package torchrs/0.2/torchrs-0.2.tar.gz

---
+++ packages/torchrs/torchrs-0.2.tar.gz/torchrs-0.2/torchrs/module/postprocess.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = "Binh Vu <binh@toan2.com>"
+
+"""
+
+	Execute some of after initialization module
+
+"""
+class PostProcess(object):
+
+	def __init__(self, module):
+		self.module = module
+
+	def execute(self, config):
+		if "postexecute" in config:
+			module = {
+				"location": self.module.location,
+				"name": config["name"]
+			}
+
+			exec(config["postexecute"])
