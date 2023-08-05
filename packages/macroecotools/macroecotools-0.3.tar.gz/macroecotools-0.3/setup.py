fe38fa6ddebf5d87d81e689ca3315f6fda7ee04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package macroecotools/0.3/macroecotools-0.3.tar.gz

---
+++ packages/macroecotools/macroecotools-0.3.tar.gz/macroecotools-0.3/setup.py
@@ -0,0 +1,16 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name = 'macroecotools',
+      version= '0.3',
+      description = 'Tools for conducting macroecological analyses',
+      author = "Xiao Xiao, Ethan White and Katherine Thibault",
+      url = 'https://github.com/weecology/macroecotools',
+      packages = ['macroecotools', 'macroeco_distributions'],
+      license = 'MIT',
+      install_requires = ['numpy>=1.6', 'scipy>=0.12', 'pandas>=0.13']
+)
