3d493d77a5d6238879ee34c40c92e0c690d60ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:05 +0000

    Add package wheezy.caching/0.1.112/wheezy.caching-0.1.112.tar.gz

---
+++ packages/wheezy.caching/wheezy.caching-0.1.112.tar.gz/wheezy.caching-0.1.112/src/wheezy/caching/__init__.py
@@ -0,0 +1,12 @@
+
+"""
+"""
+
+__version__ = '0.1.112'
+
+# flake8: noqa
+
+from wheezy.caching.client import CacheClient
+from wheezy.caching.dependency import CacheDependency
+from wheezy.caching.memory import MemoryCache
+from wheezy.caching.null import NullCache
