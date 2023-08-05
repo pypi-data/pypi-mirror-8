29e3ee7fb3e557bd91b1e7474c87043c2dc50e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:04 +0000

    Add package django-swstags/0.1.287/django-swstags-0.1.287.tar.gz

---
+++ packages/django-swstags/django-swstags-0.1.287.tar.gz/django-swstags-0.1.287/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-swstags',
+    version='0.1.287',
+    packages=find_packages(),
+    license='',
+    long_description=open('README.txt').read(),
+    url='http://www.stoneworksolutions.net',
+    author='Miguel Angel Sanchez',
+    author_email='miguel.sanchez@stoneworksolutions.net',
+    package_data={'': ['*.html']},
+    include_package_data=True,
+    install_requires=[],
+    )
+
