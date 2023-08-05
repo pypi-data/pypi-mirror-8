f7eab6b6cb8d0d7bc967f830991e43c663b401be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:09 +0000

    Add package PyUPS/0.6.0/PyUPS-0.6.0.tar.gz

---
+++ packages/PyUPS/PyUPS-0.6.0.tar.gz/PyUPS-0.6.0/ups/tests/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Test Suite for UPS Shipping
+
+    :copyright: (c) 2011 by Openlabs Technologies & Consulting (P) Limited
+    :license: AGPL, see LICENSE for more details.
+"""
