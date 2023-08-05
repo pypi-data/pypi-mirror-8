5181db15f26b1083e0ff0340e022380283ac796a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:16 +0000

    Add package pypdf2xml/0.3/pypdf2xml-0.3.tar.gz

---
+++ packages/pypdf2xml/pypdf2xml-0.3.tar.gz/pypdf2xml-0.3/setup.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+from distutils.core import setup
+
+setup(name='pypdf2xml',
+      version='0.3',
+      description='A reimplementation of pdftoxml in Python, using pdfMiner. Handles unicode characters better.',
+      author=u'Gašper Žejn'.encode('utf-8'),
+      author_email='zejn@owca.info',
+      url='https://github.com/zejn/pypdf2xml',
+      scripts=['pdf2xml', 'pdfxml2csv', 'headerfilter'],
+      packages=['pypdf2xml'],
+      install_requires=['pdfminer==20140328', 'lxml'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          ]
+     )
+
+
