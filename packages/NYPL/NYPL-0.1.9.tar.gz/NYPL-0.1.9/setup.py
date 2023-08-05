2fd09e869866eb9414e435088a49713a3d608ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package NYPL/0.1.9/NYPL-0.1.9.tar.gz

---
+++ packages/NYPL/NYPL-0.1.9.tar.gz/NYPL-0.1.9/setup.py
@@ -0,0 +1,20 @@
+
+
+from distutils.core import setup
+from setuptools import setup
+try:
+  with open('README1.txt') as file:
+    long_description = file.read()
+except IOError:
+  long_description=[]
+
+setup(name='NYPL',
+      version='0.1.9',
+      description='An unoffical API for the New York Public Library',
+      author='Susan Steinman',
+      author_email="steinman.tutoring@gmail.com",
+      url='https://github.com/susinmotion/NYPL',
+      packages=['NYPL'],
+      long_description=long_description,
+      install_requires=['requests>=1.2.0', 'beautifulsoup4']
+     )
