0578d8f541562de1808f46cbc835e249397817bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package ore.yui/0.5.3/ore.yui-0.5.4-py2.7.egg

---
+++ packages/ore.yui/ore.yui-0.5.4-py2.7.egg/ore/yui/css.py
@@ -0,0 +1,25 @@
+##############################################################################
+#
+# Copyright (c) 2008 Kapil Thangavelu
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+$Id: $
+"""
+
+from zope.viewlet.viewlet import CSSViewlet
+
+YahooDataTable = CSSViewlet('yui/build/datatable/assets/datatable.css')
+YahooDataTableSkin = CSSViewlet('yui/build/assets/skins/sam/datatable.css')
+YahooTreeView = CSSViewlet('yui/build/assets/skins/sam/treeview.css')
+YahooLogger = CSSViewlet('yui/build/logger/assets/skins/sam/logger.css')
+YahooGridCSS = CSSViewlet('yui/build/reset-fonts-grids/reset-fonts-grids.css')
+
---
+++ packages/ore.yui/ore.yui-0.5.4-py2.7.egg/ore/yui/js.py
@@ -0,0 +1,27 @@
+##############################################################################
+#
+# Copyright (c) 2008 Kapil Thangavelu
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from zope.viewlet.viewlet import JavaScriptViewlet
+
+YahooBase = JavaScriptViewlet('yui/build/yahoo/yahoo.js')
+YahooEvent = JavaScriptViewlet('yui/build/event/event.js')
+YahooConnection = JavaScriptViewlet('yui/build/connection/connection.js')
+YahooDom = JavaScriptViewlet('yui/build/dom/dom.js')
+YahooCore = JavaScriptViewlet('yui/build/yahoo-dom-event/yahoo-dom-event.js')
+YahooLogger = JavaScriptViewlet('yui/build/logger/logger-min.js')
+YahooElement = JavaScriptViewlet('yui/build/element/element.js')
+YahooDataSource = JavaScriptViewlet('yui/build/datasource/datasource.js')
+YahooDataTable = JavaScriptViewlet('yui/build/datatable/datatable.js')
+YahooTree = JavaScriptViewlet('yui/build/treeview/treeview.js')
+YahooSelector = JavaScriptViewlet('yui/build/selector/selector.js')
+YahooTabView = JavaScriptViewlet('yui/build/tabview/tabview.js')
