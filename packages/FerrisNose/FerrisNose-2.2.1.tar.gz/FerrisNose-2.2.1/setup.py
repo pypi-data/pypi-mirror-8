09bdc3704e4a3a19f655f84b5b85c7460afd7c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:49 +0000

    Add package FerrisNose/2.2.1/FerrisNose-2.2.1.tar.gz

---
+++ packages/FerrisNose/FerrisNose-2.2.1.tar.gz/FerrisNose-2.2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+VERSION = '2.2.1'
+
+setup(
+    name="FerrisNose",
+    version=VERSION,
+    author='Jon Parrott',
+    author_email='jon.wayne.parrott@gmail.com',
+    maintainer='Jon Parrott',
+    maintainer_email='jon.wayne.parrott@gmail.com',
+    description='Nose plugin for testing Google App Engine application. Designed for the Ferris Framework but should work for any App Engine app.',
+    url='https://github.com/jonparrott/FerrisNose',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        'nose.plugins.0.10': [
+            'ferris = ferrisnose.plugin:FerrisNose'
+        ]
+    },
+    packages=['ferrisnose'],
+    install_requires=[
+        'nose>=1.3.0',
+        'google-api-python-client>=1.2.0',
+        'oauth2client>=1.2.0',
+        'webtest',
+        'protorpc>=0.9.0',
+    ],
+)
