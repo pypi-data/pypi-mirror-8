e548f243822419cd9da9a79250c59a2fc6b80d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:08 +0000

    Add package fatek-fbs-lib/0.1.0/fatek-fbs-lib-0.1.0.tar.gz

---
+++ packages/fatek-fbs-lib/fatek-fbs-lib-0.1.0.tar.gz/fatek-fbs-lib-0.1.0/fatek/fatek.py
@@ -0,0 +1,25 @@
+from pymodbus.client.sync import ModbusTcpClient
+import logging
+
+from target import FatekTarget
+
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+class Fatek(object):
+    def __init__(self, address='192.168.13.250'):
+        self.address = address
+        self.client = ModbusTcpClient(address)
+
+    def read(self, symbol):
+        t = FatekTarget(self.client, symbol)
+        return t.read()
+
+    def write(self, symbol, value=True):
+        t = FatekTarget(self.client, symbol)
+        return t.write(value)
+
+    def read_all(self, symbol, count):
+        t = FatekTarget(self.client, symbol)
+        return t.read_all(count)
