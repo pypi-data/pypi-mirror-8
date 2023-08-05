93e3a32e08cc55a098b9ec46f4286337c38e8103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:34 +0000

    Add package program1/1.2.0/program1-1.2.0.zip

---
+++ packages/program1/program1-1.2.0.zip/program1-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'program1',
+    version = '1.2.0',
+    py_modules = ['program1'],
+    author = 'Gagan',
+    author_email = 'g.deepsingh1@gmail.com',
+    description = 'a simple priter',
+    )
