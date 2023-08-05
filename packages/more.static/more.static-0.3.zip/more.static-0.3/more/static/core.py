fb1f60c2f281a4a4f5e0b23b8634fdba24d8fc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:59 +0000

    Add package more.static/0.3/more.static-0.3.zip

---
+++ packages/more.static/more.static-0.3.zip/more.static-0.3/more/static/core.py
@@ -0,0 +1,38 @@
+from morepath.request import Request
+from morepath.reify import reify
+from morepath.app import App
+from reg import generic
+from morepath import Directive
+
+
+@generic
+def get_static_components():
+    raise NotImplementedError()
+
+
+class StaticApp(App):
+    def request(self, environ):
+        request = IncludeRequest(environ)
+        request.lookup = self.lookup
+        return request
+
+
+@StaticApp.directive('static_components')
+class StaticComponentsDirective(Directive):
+    def identifier(self, app):
+        # only one static components per app
+        return ()
+
+    def perform(self, registry, obj):
+        registry.register(get_static_components, (), obj)
+
+
+class IncludeRequest(Request):
+
+    @reify
+    def static_components(self):
+        return get_static_components(lookup=self.lookup)
+
+    def include(self, path_or_resource):
+        include = self.static_components.includer(self.environ)
+        include(path_or_resource)
