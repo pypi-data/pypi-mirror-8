a4452c4822fd9d815ad2dd515610e5234ac2f5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package Pyro4/4.28/Pyro4-4.28.tar.gz

---
+++ packages/Pyro4/Pyro4-4.28.tar.gz/Pyro4-4.28/examples/attributes/Readme.txt
@@ -0,0 +1,9 @@
+This is an example that shows the DOTTEDNAMES support and implications.
+
+You can start the server with or without DOTTEDNAMES enabled.
+Try both. See what the client does with both settings.
+
+The client also tries to perform a security exploit in the server, which
+will fail if DOTTEDNAMES is not enabled (the default).
+
+Lastly, direct remote attribute access.
---
+++ packages/Pyro4/Pyro4-4.28.tar.gz/Pyro4-4.28/examples/attributes/server.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+import sys
+import Pyro4
+
+
+if sys.version_info < (3, 0):
+    input = raw_input
+
+print("Note that DOTTEDNAMES is a deprecated feature that will be removed in the next version.")
+dotted = input("enter value for DOTTEDNAMES config item: ").strip()
+Pyro4.config.DOTTEDNAMES = dotted in ("1", "true", "on", "yes")
+
+something = "Something"
+
+
+class SubThingy(object):
+    def __init__(self):
+        self.value = 42
+        self._value = 123
+        self.__value = 999
+
+    def getValue(self):
+        return self.value
+
+    def addToValue(self, amount):
+        self.value += amount
+
+
+class Thingy(object):
+    def __init__(self):
+        self.sub = SubThingy()
+        self.value = 42
+        self._value = 123
+        self.__value = 999
+
+    @Pyro4.expose
+    def __dunder__(self):
+        return "yep"
+
+    @Pyro4.expose
+    def __len__(self):
+        return 200
+
+    def getSubValue(self):
+        return self.sub.getValue()
+
+    def addSubValue(self, amount):
+        self.sub.addToValue(amount)
+
+    def dottedNames(self):
+        return Pyro4.config.DOTTEDNAMES
+
+    def printSomething(self):
+        print("something:", something)
+        return something
+
+    def getValue(self):
+        return self.value
+
+    @Pyro4.expose
+    @property
+    def prop_value(self):
+        return self.value
+
+    @Pyro4.expose
+    @prop_value.setter
+    def prop_value(self, value):
+        self.value = value
+
+    @Pyro4.expose
+    @property
+    def prop_sub(self):
+        return self.sub
+
+
+d = Pyro4.Daemon()
+uri = d.register(Thingy(), "example.attributes")
+print("server object uri:", uri)
+print("DOTTEDNAMES=", Pyro4.config.DOTTEDNAMES)
+print("attributes server running.")
+d.requestLoop()
