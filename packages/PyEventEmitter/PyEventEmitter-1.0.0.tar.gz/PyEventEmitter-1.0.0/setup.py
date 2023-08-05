96acf41a350418c41ab4ca13cf7a930903fd9564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package PyEventEmitter/1.0.0/PyEventEmitter-1.0.0.tar.gz

---
+++ packages/PyEventEmitter/PyEventEmitter-1.0.0.tar.gz/PyEventEmitter-1.0.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+classifiers = [
+    "Programming Language :: Python",
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved",
+    "Natural Language :: French",
+    "Operating System :: OS Independent",
+    "Topic :: Utilities",
+]
+
+setup(name='PyEventEmitter',
+      version='1.0.0',
+      description='Simple python events library',
+      long_description=long_description,
+      author='Etienne Tissieres',
+      author_email='etienne.tissieres@gmail.com',
+      url='https://github.com/etissieres/PyEventEmitter',
+      packages=['event_emitter'],
+      license='MIT',
+      classifiers=classifiers)
