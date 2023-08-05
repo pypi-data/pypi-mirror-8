4ade951e6c5ebe4d0e8b67868ff79b6acaf55833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package django-mediasite/0.1/django-mediasite-0.1.tar.gz

---
+++ packages/django-mediasite/django-mediasite-0.1.tar.gz/django-mediasite-0.1/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-mediasite',
+    version='0.1',
+    packages=['mediasite'],
+    include_package_data=True,
+    license='GNU License',  # example license
+    description='A simple Django app to list Web-based Articles.',
+    long_description='README.md',
+    url='https://github.com/lkshakya/mediasite',
+    author='Lalit Shakya',
+    author_email='opposite800@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
+
