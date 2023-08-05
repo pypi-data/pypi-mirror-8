57b05030651d1914cd4f9e1eac7be198dee5a9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:36 +0000

    Add package alchy/1.3.1/alchy-1.3.1-py2.py3-none-any.whl

---
+++ packages/alchy/alchy-1.3.1-py2.py3-none-any.whl/alchy/__meta__.py
@@ -0,0 +1,21 @@
+"""Project metadata.
+"""
+
+__all__ = [
+    '__title__',
+    '__summary__',
+    '__url__',
+    '__version__',
+    '__author__',
+    '__email__',
+    '__license__',
+]
+
+__title__ = 'alchy'
+__summary__ = ('A SQLAlchemy extension for its declarative ORM that provides '
+               'enhancements for model classes, queries, and sessions.')
+__url__ = 'https://github.com/dgilland/alchy'
+__version__ = '1.3.1'
+__author__ = 'Derrick Gilland'
+__email__ = 'dgilland@gmail.com'
+__license__ = 'MIT License'
