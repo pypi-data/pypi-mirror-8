57f10e90111d7eb155e276b97469c7a9442aad3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:42 +0000

    Add package lmdb/0.84/lmdb-0.84-cp34-none-win32.whl

---
+++ packages/lmdb/lmdb-0.84-cp34-none-win32.whl/lmdb/_config.py
@@ -0,0 +1,2 @@
+CONFIG = {'extra_library_dirs': [], 'extra_sources': ['lib/mdb.c', 'lib/midl.c'], 'libraries': ['Advapi32'], 'extra_compile_args': ['-UNDEBUG', '-w', '/FIPython.h'], 'extra_include_dirs': ['lib', 'lib\\win32']}
+
