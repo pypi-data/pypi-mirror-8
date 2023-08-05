e31d9dfc301d43f47b122523902e6cd190f9f25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:10 +0000

    Add package cache-stdout/0.6.0/cache-stdout-0.6.0.tar.gz

---
+++ packages/cache-stdout/cache-stdout-0.6.0.tar.gz/cache-stdout-0.6.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='cache-stdout',
+    version='0.6.0',
+    description='Command-line utility to store output and reuse it later',
+    author='Sergey Valiev',
+    author_email='svaliev@gmail.com',
+    url='https://github.com/Valiev/cache',
+    scripts=['cache']
+)
