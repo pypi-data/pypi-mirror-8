72ac329e5fc6dfe8f7f456b25de412a8a9e1f96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:49 +0000

    Add package django-nopassword/1.0.0/django_nopassword-1.0.0-py2.py3-none-any.whl

---
+++ packages/django-nopassword/django_nopassword-1.0.0-py2.py3-none-any.whl/nopassword/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='LoginCode',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID',
+                 auto_created=True)),
+                ('code', models.CharField(editable=False, verbose_name='code', max_length=20)),
+                ('timestamp', models.DateTimeField(editable=False)),
+                ('next', models.TextField(blank=True, editable=False)),
+                ('user', models.ForeignKey(related_name='login_codes', verbose_name='user',
+                 to=settings.AUTH_USER_MODEL, editable=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
