31d1eb3c151f712df79cdc8b99b2e40d1940b608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:04 +0000

    Add package booby/0.6.0/booby-0.6.0.tar.gz

---
+++ packages/booby/booby-0.6.0.tar.gz/booby-0.6.0/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_description = open('README.rst').read()
+
+setup(
+    name='booby',
+    version='0.6.0',
+    description='Data modeling and validation Python library',
+    long_description=long_description,
+    url='https://github.com/jaimegildesagredo/booby',
+    author='Jaime Gil de Sagredo Luna',
+    author_email='jaimegildesagredo@gmail.com',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    use_2to3=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    )
