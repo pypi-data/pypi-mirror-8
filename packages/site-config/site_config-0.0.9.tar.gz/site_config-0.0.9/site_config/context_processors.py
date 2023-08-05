166dd3a170de14bb462cb2283ba15fc8d2be058b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:06 +0000

    Add package site-config/0.0.9/site_config-0.0.9.tar.gz

---
+++ packages/site-config/site_config-0.0.9.tar.gz/site_config-0.0.9/site_config/context_processors.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from site_config.utils import website_override_template
+from django.conf import settings
+
+
+def decide_base_template(request):
+    base_name = getattr(
+        settings, 'SITECONFIG_BASE_TEMPLATE', 'base_site.html'
+    )
+    try:
+        website = request.resolver_match.kwargs.get('website', None)
+    except:
+        website = None
+    if website:
+        base_name = website_override_template(base_name, website).name
+    return {
+        'base_template': base_name
+    }
