7712dc2f085de62d8ebc1b8273ab88a830261d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:18 +0000

    Add package mad2/0.1.18/mad2-0.1.18.tar.gz

---
+++ packages/mad2/mad2-0.1.18.tar.gz/mad2-0.1.18/mad2/plugin/print.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import logging
+
+import leip
+from mad2.util import get_all_mad_files
+
+lg = logging.getLogger(__name__)
+
+
+@leip.arg('file', nargs='*')
+@leip.arg('key', help='key to filter on')
+@leip.commandName('print')
+def mad_print(app, args):
+    """
+    print a single value from a single file
+    """
+    for madfile in get_all_mad_files(app, args):
+        print(madfile.render(madfile[args.key], [madfile, app.conf]))
+
