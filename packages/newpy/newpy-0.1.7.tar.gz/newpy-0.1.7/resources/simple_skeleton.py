7c4da40e7f4c43702dcca24cc270fb64cb603f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package newpy/0.1.7/newpy-0.1.7.tar.gz

---
+++ packages/newpy/newpy-0.1.7.tar.gz/newpy-0.1.7/resources/simple_skeleton.py
@@ -0,0 +1,19 @@
+""" sample docstring
+
+$DESCRIPTION
+
+$AUTHOR
+
+"""
+
+# sample import
+import argparse
+
+
+def main():
+    """ A sample main method """
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="$DESCRIPTION")
+    exit(main())
