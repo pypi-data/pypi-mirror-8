d45875c049e1f8ded6cd20dcec26b7ad5f5f6f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:08 +0000

    Add package releaseme/0.2.8/releaseme-0.2.8.zip

---
+++ packages/releaseme/releaseme-0.2.8.zip/releaseme-0.2.8/releaseme/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.2.8"
+
+
+class APP(object):
+    version = __version__
+    description = (
+        'Library and command line app'
+        ' to manage project releases or versions'
+        )
