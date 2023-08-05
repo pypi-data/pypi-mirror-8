d275507811de6eac45f871edcaa60d0537108bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:36 +0000

    Add package RomanAlphabet/1.3/RomanAlphabet-1.3.tar.gz

---
+++ packages/RomanAlphabet/RomanAlphabet-1.3.tar.gz/RomanAlphabet-1.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    long_description = readme.read()
+
+setup(
+  name = 'RomanAlphabet',
+  packages = ['RomanAlphabet'],
+  version = '1.3',
+  description = 'Numeral systems, binary, roman, integer',
+  author = u'Barbaros YILDIRIM',
+  author_email = 'barbarosaliyildirim@gmail.com',
+  url = 'https://github.com/RedXBeard/RomanNumbers',
+  download_url = 'https://github.com/RedXBeard/RomanNumbers/tarball/1.3',
+  keywords = ['roman', 'romanalphabet', 'integer converter', 'integer to roman',
+              'roman to integer', 'binary', 'binary convertion'],
+  classifiers = [
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.7',
+    'Natural Language :: English',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+  ],
+  long_description = long_description
+)
