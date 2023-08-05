260a383cb7fa3d83919f81a87d7cf678aa257040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:16 +0000

    Add package gtlaunch/0.1.0/gtlaunch-0.1.0.tar.gz

---
+++ packages/gtlaunch/gtlaunch-0.1.0.tar.gz/gtlaunch-0.1.0/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='gtlaunch',
+    version='0.1.0',
+    description='Gnome Terminal launcher',
+    long_description=read('README.rst'),
+    author='Zbigniew Siciarz',
+    author_email='zbigniew@siciarz.net',
+    url='http://github.com/zsiciarz/gtlaunch',
+    download_url='http://pypi.python.org/pypi/gtlaunch',
+    license='MIT',
+    packages=find_packages(),
+    platforms='any',
+    test_suite='tests',
+    entry_points={
+        'console_scripts': [
+            'gtlaunch = gtlaunch:run',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Utilities'
+    ],
+)
