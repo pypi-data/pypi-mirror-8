0e5cb4347b63ee16afc0f95e5e327a39228ce646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package concoct/0.4.0/concoct-0.4.0.tar.gz

---
+++ packages/concoct/concoct-0.4.0.tar.gz/concoct-0.4.0/README.md
@@ -0,0 +1,19 @@
+#CONCOCT 0.3.3 [![Build Status](https://travis-ci.org/BinPro/CONCOCT.png?branch=master)](https://travis-ci.org/BinPro/CONCOCT)#
+
+A program for unsupervised binning of metagenomic contigs by using nucleotide composition, 
+coverage data in multiple samples and linkage data from paired end reads.
+
+Warning! This software is to be considered under development. Functionality and the user interface may still change significantly from one version to another.
+If you want to use this software, please stay up to date with the list of known issues:
+https://github.com/BinPro/CONCOCT/issues
+
+## Documentation ##
+A comprehensive documentation for concoct is hosted on [readthedocs](https://concoct.readthedocs.org).
+
+##Support##
+If you are having issues, please let us know. We have a mailing list located at: concoct-support@lists.sourcefourge.net which you can also subscribe to [here](https://lists.sourceforge.net/lists/listinfo/concoct-support).
+
+## Contribute ##
+
+ - Issue Tracker: [github](https://github.com/binpro/CONCOCT/issues)
+ - Source Code: [github](https://github.com/binpro/CONCOCT)
---
+++ packages/concoct/concoct-0.4.0.tar.gz/concoct-0.4.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys, os
+from distutils.core import Extension
+
+version = '0.4.0'
+
+module1 = Extension('vbgmm',
+        libraries =['gsl',  'gslcblas'],
+        include_dirs = ['c-concoct'],
+        sources = ['c-concoct/vbgmmmodule.c'])
+
+setup(name='concoct',
+      version=version,
+      description="Clustering cONtigs with COverage and ComposiTion",
+      long_description="""Concoct is a program that combines three types of 
+      information - sequence composition, coverage across multiple sample, 
+      and read-pair linkage - to automatically bin metagenomic contigs 
+      into genomes. """,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='Python Scilifelab Metagenomics Binning Clustering Contig',
+      author='Brynjar Smari Bjarnason, Johannes Alneberg, Christopher Quince, Anders Andersson, Ino de Bruijn',
+      author_email='binni@binnisb.com',
+      maintainer='Johannes Alneberg',
+      maintainer_email='johannes.alneberg@scilifelab.se',
+      url='https://github.com/BinPro/CONCOCT',
+      license='FreeBSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      scripts=["bin/concoct"],
+      include_package_data=True,
+      zip_safe=False,
+      ext_modules = [module1],
+      install_requires=['cython>=0.19.1',
+                        'numpy>=1.7.1',
+                        'scipy>=0.12.0',
+                        'pandas>=0.11.0',
+                        'biopython>=1.62b',
+                        'scikit-learn>=0.13.1',
+                        'nose==1.3.0',
+                        'sphinx-rtd-theme>=0.1.6',
+                        'Sphinx>=1.2.2'],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
+
