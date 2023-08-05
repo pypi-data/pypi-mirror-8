979453379233a140a1fe2ace94636c632ec53b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package deltas/0.0.3/deltas-0.0.3.tar.gz

---
+++ packages/deltas/deltas-0.0.3.tar.gz/deltas-0.0.3/deltas/tokenizers/tokenizer.py
@@ -0,0 +1,9 @@
+class Tokenizer:
+    """
+    An abstract class representing the tokenizer interfact.
+    """
+    def tokenize(self, text):
+        """
+        Extend this class for a custom tokenizer.
+        """
+        raise NotImplementedError()
