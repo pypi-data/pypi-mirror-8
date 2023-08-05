c5b60902a622394cb53dc9d34ac8b2fbf09114f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:08 +0000

    Add package arvados_fuse/0.1.1410188012.d43fb92/arvados_fuse-0.1.1410188012.d43fb92.tar.gz

---
+++ packages/arvados_fuse/arvados_fuse-0.1.1410188012.d43fb92.tar.gz/arvados_fuse-0.1.1410188012.d43fb92/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
+
+setup(name='arvados_fuse',
+      version='0.1',
+      description='Arvados FUSE driver',
+      long_description=open(README).read(),
+      author='Arvados',
+      author_email='info@arvados.org',
+      url="https://arvados.org",
+      download_url="https://github.com/curoverse/arvados.git",
+      license='GNU Affero General Public License, version 3.0',
+      packages=['arvados_fuse'],
+      scripts=[
+        'bin/arv-mount'
+        ],
+      install_requires=[
+        'arvados-python-client',
+        'llfuse',
+        'python-daemon'
+        ],
+      test_suite='tests',
+      tests_require=['PyYAML'],
+      zip_safe=False)
