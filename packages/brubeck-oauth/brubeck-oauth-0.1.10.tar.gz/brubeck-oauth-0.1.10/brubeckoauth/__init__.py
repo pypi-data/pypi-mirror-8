d774b3fd017936043abaa23904bed9dc8204b1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:58 +0000

    Add package brubeck-oauth/0.1.10/brubeck-oauth-0.1.10.tar.gz

---
+++ packages/brubeck-oauth/brubeck-oauth-0.1.10.tar.gz/brubeck-oauth-0.1.10/brubeckoauth/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""The Brubeck oAuth module."""
+
+version = "0.1.10"
+version_info = (0, 1, 10)
+__all__ = [ 'base',
+            'handlers',
+            'queries',
+            'settings']
