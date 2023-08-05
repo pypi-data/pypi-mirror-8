d04ea805dcaf0bf46a0d5fc2eb507c2e5fd405cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:34 +0000

    Add package Animapy/1.5/Animapy-1.5.tar.gz

---
+++ packages/Animapy/Animapy-1.5.tar.gz/Animapy-1.5/animapy/__init__.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+from animapy.sources.anitube import anitube
+from animapy.sources.nwanime import nwanime
+import thread
+import urllib2
+import time
+
+class anime(object):
+
+    @classmethod
+    def searchAnimes(cls, anime, quant, order = None, lang='pt'):
+        anime = anime.replace(" ", "+")
+        return cls().__getData(anime, quant, order, lang)
+    
+    
+    def __returner(self, data):
+        self.count = self.count + 1
+        
+    
+    def setResult(self, episodes, position):
+        self.data[position] = episodes
+
+    def __getData(self, anime, quant, order, lang):
+        self.count = 0
+        self.data = [''] * quant
+        if lang == 'pt':
+            animes = anitube()
+            for i in range(quant):
+                thread.start_new_thread( animes.getAnimes, (anime, 1, i, order, self, i,) )
+
+            while self.count != quant:
+                pass
+            return self.data
+            
+        else: #if any other language, returns the EN version
+            animes = nwanime()
+            for i in range(quant):
+                thread.start_new_thread( animes.getAnimes, (anime, 1, i, order, self, i,) )
+
+            while self.count != quant:
+                pass
+            return self.data
---
+++ packages/Animapy/Animapy-1.5.tar.gz/Animapy-1.5/animapy/sources/anitube.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import urllib2
+from animapy.helpers.common import functions
+
+class anitube(functions):
+    
+    def getAnimes(self, anime, quant, offset, order, parent, position):
+        # gets the correct URL
+        if order == 'date':
+            url = 'http://www.anitube.se/search/basic/1/?sort=addate&search_type=&search_id=' + anime
+        else:
+            url = 'http://www.anitube.se/search/?search_id=' + anime
+            
+        content = self.calUrl(url)
+        soup = BeautifulSoup(content)
+        links = soup.findAll('li', { "class" : 'mainList' })
+
+        episodes = None
+        # in case the result is lower than the desired quantity
+        # quant resets to result count
+        if len(links) > offset:
+            
+            aTag = links[offset].find('div', { "class" : 'videoTitle' }).a
+
+            title = aTag.contents[0].encode('ascii','ignore')
+            image = links[offset].find('img').get('src').encode('ascii','ignore')
+            hd = ''
+            normal = ''
+
+            # calls to get the movie url
+            content = self.calUrl(aTag.get('href'))
+            newSoup = BeautifulSoup(content)
+            data = newSoup.find(id="videoPlayer").findAll('script')[2].get('src')
+
+            response = urllib2.urlopen(data)
+
+            # loops throught the javascript lines to get the movie links
+            for line in response:
+                if 'cdn.anitu.be' in line:
+                    if '_hd' in line:
+                        hd = line.rstrip()[9:-2]
+                    else:
+                        normal = line.rstrip()[9:-2]
+
+            episodes = self.createObject(title, image, normal, hd)
+        if episodes != None:
+            parent.setResult(episodes, position)
+        parent.count = parent.count + 1
