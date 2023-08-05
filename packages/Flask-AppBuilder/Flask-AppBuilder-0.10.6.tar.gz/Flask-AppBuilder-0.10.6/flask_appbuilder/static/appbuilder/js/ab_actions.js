74888da61103f4ea3f30af3a6accdc36e771769d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:20 +0000

    Add package Flask-AppBuilder/0.10.6/Flask-AppBuilder-0.10.6.tar.gz

---
+++ packages/Flask-AppBuilder/Flask-AppBuilder-0.10.6.tar.gz/Flask-AppBuilder-0.10.6/flask_appbuilder/static/appbuilder/js/ab_actions.js
@@ -0,0 +1,55 @@
+// This js work is based on:
+// Copyright (c) 2014, Serge S. Koval and contributors. See AUTHORS
+// for more details.
+//
+
+var AdminActions = function() {
+
+    var chkAllFlag = true;
+
+    this.execute_multiple = function(name, confirmation) {
+        var selected = $('input.action_check:checked').size();
+
+        if (selected == 0) {
+            alert('No row selected');
+            return false;
+        }
+
+        if (!!confirmation) {
+            if (!confirm(confirmation)) {
+                return false;
+            }
+        }
+
+        // Update hidden form and submit it
+        var form = $('#action_form');
+        $('#action', form).val(name);
+
+        $('input.action_check', form).remove();
+        $('input.action_check:checked').each(function() {
+            form.append($(this).clone());
+        });
+
+        form.submit();
+
+        return false;
+    };
+
+    this.execute_single = function(url, confirmation) {
+
+        if (!!confirmation) {
+            if (!confirm(confirmation)) {
+                return false;
+            }
+        }
+
+        window.location.href = url;
+    };
+
+    $('.action_check_all').click(function() {
+        $('.action_check').prop('checked', chkAllFlag);
+        chkAllFlag = !chkAllFlag;
+    });
+
+};
+
