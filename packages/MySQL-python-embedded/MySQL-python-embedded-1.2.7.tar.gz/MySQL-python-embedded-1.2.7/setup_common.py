6051032adb2463f67dbbaeda293bcbefb18ca5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package MySQL-python-embedded/1.2.7/MySQL-python-embedded-1.2.7.tar.gz

---
+++ packages/MySQL-python-embedded/MySQL-python-embedded-1.2.7.tar.gz/MySQL-python-embedded-1.2.7/setup_common.py
@@ -0,0 +1,40 @@
+try:
+    # Python 2.x
+    from ConfigParser import SafeConfigParser
+except ImportError:
+    # Python 3.x
+    from configparser import ConfigParser as SafeConfigParser
+
+def get_metadata_and_options():
+    config = SafeConfigParser()
+    config.read(['metadata.cfg', 'site.cfg'])
+
+    metadata = dict(config.items('metadata'))
+    options = dict(config.items('options'))
+
+    metadata['py_modules'] = list(filter(None, metadata['py_modules'].split('\n')))
+    metadata['classifiers'] = list(filter(None, metadata['classifiers'].split('\n')))
+
+    return metadata, options
+
+def enabled(options, option):
+    value = options[option]
+    s = value.lower()
+    if s in ('yes','true','1','y'):
+        return True
+    elif s in ('no', 'false', '0', 'n'):
+        return False
+    else:
+        raise ValueError("Unknown value %s for option %s" % (value, option))
+
+def create_release_file(metadata):
+    import os
+    if not os.path.exists("MySQLdb_embedded/release.py"):
+        return
+    rel = open("MySQLdb_embedded/release.py",'w')
+    rel.write("""
+__author__ = "%(author)s <%(author_email)s>"
+version_info = %(version_info)s
+__version__ = "%(version)s"
+""" % metadata)
+    rel.close()
