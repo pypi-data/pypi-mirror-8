39adf62840d8c02008c5cb646f1927022c8507c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:47 +0000

    Add package jin2cli/0.2/jin2cli-0.2.tar.gz

---
+++ packages/jin2cli/jin2cli-0.2.tar.gz/jin2cli-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jin2cli',
+    version = '0.2',
+    description='json + jinja2 template == file',
+    url='https://github.com/tommyvn/jin2cli',
+    author='Tom van Neerijnen',
+    author_email='tom@tomvn.com',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['jinja2'],
+    keywords='cli jinja2 template',
+    license='Apache',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    entry_points={
+        'console_scripts': [
+            'jin2cli=jin2cli.cli:main',
+        ],
+    },
+)
