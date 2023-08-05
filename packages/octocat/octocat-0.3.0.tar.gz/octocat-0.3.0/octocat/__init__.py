12c2a4f021702c43c93d104ecff4a85d171a5b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:46 +0000

    Add package octocat/0.3.0/octocat-0.3.0.tar.gz

---
+++ packages/octocat/octocat-0.3.0.tar.gz/octocat-0.3.0/octocat/__init__.py
@@ -0,0 +1,11 @@
+""" octocat description. """
+
+# Package information
+# ===================
+
+__version__ = "0.3.0"
+__project__ = "octocat"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "BSD"
+
+from .octocat import OctocatClient, OctocatException
