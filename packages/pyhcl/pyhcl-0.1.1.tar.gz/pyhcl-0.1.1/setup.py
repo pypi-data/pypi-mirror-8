8a3719998898005e9dcaab5ea38c5c77fdd32138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:31 +0000

    Add package pyhcl/0.1.1/pyhcl-0.1.1.tar.gz

---
+++ packages/pyhcl/pyhcl-0.1.1.tar.gz/pyhcl-0.1.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from os.path import dirname, join, exists
+from distutils.core import setup
+
+try:
+    from setuptools.command.install import install as _install
+except ImportError:
+    from distutils.command.install import install as _install
+
+import sys
+import subprocess
+
+setup_dir = dirname(__file__)
+
+def _post_install():
+    '''Initialize the parse table at install time'''
+    import hcl
+    from hcl.parser import HclParser
+    parser = HclParser()
+
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        self.execute(_post_install, (), msg="Generating parse table...")
+
+def get_version():
+    d = {}
+    with open(join(setup_dir, 'src', 'hcl', 'version.py')) as fp:
+        exec(compile(fp.read(), 'version.py', 'exec'), {}, d)
+    return d['__version__']
+
+with open(join(dirname(__file__), 'README.md'), 'r') as readme_file:
+    long_description = readme_file.read()
+
+install_requires=open(join(setup_dir, 'requirements.txt')).readlines()
+
+setup(name='pyhcl',
+      version=get_version(),
+      description='HCL configuration parser for python',
+      long_description=long_description,
+      author='Dustin Spicuzza',
+      author_email='dustin@virtualroadside.com',
+      package_dir={'': 'src'},
+      packages=['hcl'],
+      scripts=["scripts/hcltool"],
+      setup_requires=['ply'],
+      install_requires=install_requires,
+      cmdclass={'install': install})
+
+
