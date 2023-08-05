0ddf77f15970dbf3c25e562d0030ca304f3913ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package django-rest-framework-proxy/1.2.0/django-rest-framework-proxy-1.2.0.tar.gz

---
+++ packages/django-rest-framework-proxy/django-rest-framework-proxy-1.2.0.tar.gz/django-rest-framework-proxy-1.2.0/rest_framework_proxy/settings.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+
+from rest_framework.settings import APISettings
+
+
+USER_SETTINGS = getattr(settings, 'REST_PROXY', None)
+
+DEFAULTS = {
+    'HOST': None,
+    'AUTH': {
+        'user': None,
+        'password': None,
+    },
+    'TIMEOUT': None,
+    'DEFAULT_HTTP_ACCEPT': 'application/json',
+    'DEFAULT_HTTP_ACCEPT_LANGUAGE': 'en-US,en;q=0.8',
+    'DEFAULT_CONTENT_TYPE': 'text/plain',
+
+    # Return response as-is if enabled
+    'RETURN_RAW': False,
+
+    # Used to translate Accept HTTP field
+    'ACCEPT_MAPS': {
+        'text/html': 'application/json',
+    },
+
+    # Do not pass following parameters
+    'DISALLOWED_PARAMS': ('format',)
+}
+
+api_proxy_settings = APISettings(USER_SETTINGS, DEFAULTS)
