38d086bd9aa6739b120e62882de37525de064ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package pysolve/0.1.7/pysolve-0.1.7.tar.gz

---
+++ packages/pysolve/pysolve-0.1.7.tar.gz/pysolve-0.1.7/setup.py
@@ -0,0 +1,30 @@
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='pysolve',
+    version='0.1.7',
+    packages=find_packages(),
+    install_requires=['sympy', 'numpy'],
+    license='MIT',
+    author='Kenn Takara',
+    author_email='kenn.takara@outlook.com',
+    classifiers={
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    },
+    url='https://github.com/kennt/pylinsolve',
+    long_description=read('README.rst'),
+    description='A simple interface for solving systems of linear equations',
+)
