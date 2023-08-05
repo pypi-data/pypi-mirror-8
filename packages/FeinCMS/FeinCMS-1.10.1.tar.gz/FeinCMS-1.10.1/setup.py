f7c330823894b31a32483e81cacca9eeebb3b651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package FeinCMS/1.10.1/FeinCMS-1.10.1.tar.gz

---
+++ packages/FeinCMS/FeinCMS-1.10.1.tar.gz/FeinCMS-1.10.1/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='FeinCMS',
+    version=__import__('feincms').__version__,
+    description='Django-based Page CMS and CMS building toolkit.',
+    long_description=read('README.rst'),
+    author='Matthias Kestenholz',
+    author_email='mk@feinheit.ch',
+    url='http://github.com/feincms/feincms/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=find_packages(
+        exclude=['tests', 'example'],
+    ),
+    package_data={
+        '': ['*.html', '*.txt'],
+        'feincms': [
+            'locale/*/*/*.*',
+            'static/feincms/*.*',
+            'static/feincms/*/*.*',
+            'templates/*.*',
+            'templates/*/*.*',
+            'templates/*/*/*.*',
+            'templates/*/*/*/*.*',
+            'templates/*/*/*/*/*.*',
+        ],
+    },
+    install_requires=[
+        'Django>=1.4.2',
+        'django-mptt>=0.6.0',
+        'Pillow>=2.0.0',
+        'feedparser>=5.0.0',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    zip_safe=False,
+)
