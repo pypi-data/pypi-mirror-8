e39e6097e26e0a73068f6f436cb9092218b0af8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package pympi-ling/0.99b/pympi-ling-0.99b.tar.gz

---
+++ packages/pympi-ling/pympi-ling-0.99b.tar.gz/pympi-ling-0.99b/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pympi-ling',
+      version='0.99b',
+      description='Python Linguistic Modules',
+      author='Mart Lubbers',
+      author_email='mart@martlubbers.net',
+      url='https://github.com/dopefishh/pympi',
+      packages=['pympi']
+      )
