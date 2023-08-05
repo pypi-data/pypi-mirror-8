13ba0eee0595fba13b3bd791d9ccdd2c5a9ed050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:55 +0000

    Add package leagueoflegends/1.4b/leagueoflegends-1.4b.tar.gz

---
+++ packages/leagueoflegends/leagueoflegends-1.4b.tar.gz/leagueoflegends-1.4b/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+long_description = open('README.md').read()
+
+setup(name="leagueoflegends",
+      version="1.4b",
+      py_modules=["leagueoflegends.leagueoflegends"],
+      description="Unofficial libraries for interacting with the official League of Legends API",
+      author="Jennie Lees <jennielees@gmail.com>",
+      author_email="jennielees@gmail.com",
+      license="WTFPL",
+      url="http://github.com/jennielees/leagueoflegends-python",
+      long_description=long_description,
+      platforms=["any"],
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Software Development" +
+                   " :: Libraries :: Python Modules",
+                   ]
+      )
