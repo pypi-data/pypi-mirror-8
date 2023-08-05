6234e2de01d061e8011fcaa4e9edf2125b220a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:13 +0000

    Add package nsone/0.9/nsone-0.9-py2.py3-none-any.whl

---
+++ packages/nsone/nsone-0.9-py2.py3-none-any.whl/nsone/rest/transport/twisted.py
@@ -0,0 +1,106 @@
+#
+# Copyright (c) 2014 NSONE, Inc.
+#
+# License under The MIT License (MIT). See LICENSE in project root.
+from __future__ import absolute_import
+
+from nsone.rest.transport.base import TransportBase
+from nsone.rest.errors import ResourceException, RateLimitException, \
+    AuthException
+import json
+
+try:
+    from twisted.internet import reactor
+    from twisted.web.client import Agent, readBody
+    from twisted.web.http_headers import Headers
+    from twisted.internet.defer import succeed
+    have_twisted = True
+except Exception as e:
+    have_twisted = False
+
+
+class StringProducer(object):
+
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
+
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return succeed(None)
+
+    def pauseProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
+
+
+class TwistedTransport(TransportBase):
+
+    def __init__(self, config):
+        if not have_twisted:
+            raise ImportError('Twisted required for TwistedTransport')
+        TransportBase.__init__(self, config, self.__module__)
+        self.agent = Agent(reactor)
+
+    def _callback(self, response, user_callback, data, headers):
+        d = readBody(response)
+        d.addCallback(self._onBody, response, user_callback, data, headers)
+        return d
+
+    def _onBody(self, body, response, user_callback, data, headers):
+        self._logHeaders(headers)
+        self._log.debug("%s %s %s %s" % (response.request.method,
+                                         response.request.absoluteURI,
+                                         response.code,
+                                         data))
+        if response.code != 200:
+            if response.code == 429:
+                raise RateLimitException('rate limit exceeded',
+                                         response,
+                                         body)
+            elif response.code == 401:
+                raise AuthException('unauthorized',
+                                    response,
+                                    body)
+            else:
+                raise ResourceException('server error', response, body)
+        try:
+            jsonOut = json.loads(body)
+        except:
+            raise ResourceException('invalid json in response',
+                                    response,
+                                    body)
+        if user_callback:
+            # set these in case callback throws, so we have them for errback
+            self.response = response
+            self.body = body
+            return user_callback(jsonOut, body, response)
+        else:
+            return jsonOut
+
+    def _errback(self, failure, user_errback):
+        # print "failure: %s" % failure.printTraceback()
+        if user_errback:
+            return user_errback(failure)
+        else:
+            if failure.check(ResourceException, RateLimitException):
+                raise failure.value
+            raise ResourceException(failure.getErrorMessage())
+
+    def send(self, method, url, headers=None, data=None,
+             callback=None, errback=None):
+        theaders = None
+        if headers:
+            theaders = Headers({str(k): [str(v)]
+                                for (k, v) in headers.iteritems()})
+        bProducer = None
+        if data:
+            bProducer = StringProducer(data)
+        d = self.agent.request(method, str(url), theaders, bProducer)
+        d.addCallback(self._callback, callback, data, headers)
+        d.addErrback(self._errback, errback)
+        return d
+
+TransportBase.REGISTRY['twisted'] = TwistedTransport
---
+++ packages/nsone/nsone-0.9-py2.py3-none-any.whl/nsone/rest/zones.py
@@ -0,0 +1,52 @@
+#
+# Copyright (c) 2014 NSONE, Inc.
+#
+# License under The MIT License (MIT). See LICENSE in project root.
+#
+
+from . import resource
+
+
+class Zones(resource.BaseResource):
+
+    ROOT = 'zones'
+
+    INT_FIELDS = ['retry', 'refresh', 'expiry', 'nx_ttl']
+    PASSTHRU_FIELDS = ['secondary', 'hostmaster', 'meta', 'networks']
+
+    def _buildBody(self, zone, **kwargs):
+        body = {}
+        body['zone'] = zone
+        self._buildStdBody(body, kwargs)
+        return body
+
+    def create(self, zone, callback=None, errback=None, **kwargs):
+        body = self._buildBody(zone, **kwargs)
+        return self._make_request('PUT',
+                                  '%s/%s' % (self.ROOT, zone),
+                                  body=body,
+                                  callback=callback,
+                                  errback=errback)
+
+    def update(self, zone, callback=None, errback=None, **kwargs):
+        body = self._buildBody(zone, **kwargs)
+        return self._make_request('POST',
+                                  '%s/%s' % (self.ROOT, zone),
+                                  body=body,
+                                  callback=callback,
+                                  errback=errback)
+
+    def delete(self, zone, callback=None, errback=None):
+        return self._make_request('DELETE', '%s/%s' % (self.ROOT, zone),
+                                  callback=callback,
+                                  errback=errback)
+
+    def list(self, callback=None, errback=None):
+        return self._make_request('GET', '%s' % self.ROOT,
+                                  callback=callback,
+                                  errback=errback)
+
+    def retrieve(self, zone, callback=None, errback=None):
+        return self._make_request('GET', '%s/%s' % (self.ROOT, zone),
+                                  callback=callback,
+                                  errback=errback)
