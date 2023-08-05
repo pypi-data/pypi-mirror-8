fcdceb789b35c22480b7599b5003408edf8e8630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package curco/0.1/curco-0.1.tar.gz

---
+++ packages/curco/curco-0.1.tar.gz/curco-0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='curco',
+      version='0.1',
+      description='A dummy currency converter',
+      author='Nicola Lamacchia',
+      author_email='nicola.lamacchia@gmail.com',
+      scripts=['curco'],
+      url='https://github.com/nicolalamacchia/curco',
+      license='Unlicense',
+      keywords='fortune cowsay terminal',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Topic :: Office/Business',
+          'Topic :: Office/Business :: Financial',
+          'Operating System :: OS Independent',
+          'Intended Audience :: End Users/Desktop',
+          'License :: Public Domain',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.0',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4'
+      ])
