8123635376806186a26de346b4dc477123a4e86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:31 +0000

    Add package layers/0.1.3/layers-0.1.3.tar.gz

---
+++ packages/layers/layers-0.1.3.tar.gz/layers-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+import os
+import sys
+import imp
+
+from setuptools import setup, find_packages
+
+setup(
+    name='layers',
+    version='0.1.3',
+    author='Alex Rudakov',
+    author_email='ribozz+layers@gmail.com',
+    maintainer='Alex Rudakov',
+    maintainer_email='ribozz+layers@gmail.com',
+    url='https://github.com/ribozz/layers',
+    description='Layered source layouts for software development projects',
+    long_description=open('README.rst').read(),
+
+    packages=find_packages(),
+    install_requires=[
+        'PyYaml',
+        'bashutils'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'layers = layers:main'
+        ],
+    }
+)
