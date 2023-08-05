41c93053fb70f3c8cd3ac0a478dae2b135f5b75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package commonmark/0.5.3/CommonMark-0.5.3.tar.gz

---
+++ packages/commonmark/CommonMark-0.5.3.tar.gz/CommonMark-0.5.3/setup.py
@@ -0,0 +1,30 @@
+# CommonMark.py - setup.py
+from distutils.core import setup
+
+setup(
+	name = "CommonMark",
+	packages = ['CommonMark'],
+	scripts=['bin/cmark.py'],
+	version = "0.5.3",
+	license = "BSD License",
+	description = "Python parser for the CommonMark Markdown spec",
+	author = "Bibek Kafle <bkafle662@gmail.com>, Roland Shoemaker <rolandshoemaker@gmail.com>",
+	author_email = "rolandshoemaker@gmail.com",
+	url = "https://github.com/rolandshoemaker/CommonMark-py",
+	download_url = "https://github.com/rolandshoemaker/CommonMark-py/tarball/v0.5.3",
+	keywords = ["markup", "markdown", "commonmark"],
+	classifiers = ["Programming Language :: Python",
+	"Programming Language :: Python :: 2",
+	"Programming Language :: Python :: 3",
+	"Development Status :: 4 - Beta",
+	"Environment :: Other Environment",
+	"Environment :: Console",
+	"Intended Audience :: Developers",
+	"License :: OSI Approved :: BSD License",
+	"Operating System :: OS Independent",
+	"Topic :: Documentation",
+	"Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+	"Topic :: Software Development :: Documentation",
+	"Topic :: Software Development :: Libraries :: Python Modules",
+	"Topic :: Text Processing :: Markup",
+	"Topic :: Utilities"])
