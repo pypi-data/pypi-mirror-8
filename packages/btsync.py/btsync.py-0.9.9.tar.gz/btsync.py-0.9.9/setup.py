18cc8f590ad1ceb01add327f362a0918ce18f322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:37 +0000

    Add package btsync.py/0.9.9/btsync.py-0.9.9.tar.gz

---
+++ packages/btsync.py/btsync.py-0.9.9.tar.gz/btsync.py-0.9.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+version = '0.9.9'
+
+setup(name='btsync.py',
+      version=version,
+      description="A Python API client for BitTorrent Sync",
+      long_description=open('README.rst').read(),
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Environment :: Console',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+      ],
+      keywords='api',
+      author='Kevin Jing Qiu',
+      author_email='kevin.jing.qiu@gmail.com',
+      url='https://github.com/kevinjqiu/btsync.py',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      data_files=[('.', ['requirements.txt', 'README.rst', 'LICENSE'])],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=open('requirements.txt').readlines(),
+      entry_points={},
+      )
