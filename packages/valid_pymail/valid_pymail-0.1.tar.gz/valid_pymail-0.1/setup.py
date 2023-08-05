2774eda04a1a6bf31c11d948fa295c4994a350eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:43 +0000

    Add package valid_pymail/0.1/valid_pymail-0.1.tar.gz

---
+++ packages/valid_pymail/valid_pymail-0.1.tar.gz/valid_pymail-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = "valid_pymail",
+    version = "0.1",
+    author = "Michael Malocha",
+    author_email = "mmalocha13@gmail.com",
+    description = ("A wrapper for the Mailgun email validator API"),
+    license = "BSD",
+    keywords = "mailgun email validator API wrapper",
+    url = "https://github.com/mjm159/valid_pymail",
+    download_url = "https://github.com/mjm159/valid_pymail/tarball/0.1",
+    packages=['valid_pymail'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License"
+    ]
+)
