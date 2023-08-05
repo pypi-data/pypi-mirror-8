b761eff0ea8e885d112b7b2e2da7bfc421f48c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package django-proxy-server/0.1.8.4/django-proxy-server-0.1.8.4.tar.gz

---
+++ packages/django-proxy-server/django-proxy-server-0.1.8.4.tar.gz/django-proxy-server-0.1.8.4/proxy_server/__init__.py
@@ -0,0 +1,23 @@
+RESTFUL_HEADER = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+}
+
+# Header names from request
+HTTP_API_KEY = 'HTTP_API_KEY'
+HTTP_USER_TOKEN = 'HTTP_USER_TOKEN'
+HTTP_FROM = 'HTTP_X_FORWARDED_FOR'
+CSRF_COOKIE_USED = 'CSRF_COOKIE_USED'
+
+# Header names from response
+USER_TOKEN = 'user-token'
+API_KEY = 'api-key'
+CLIENT_IP = 'client-ip'
+CSRF_TOKEN = 'X-CSRFToken'
+
+# Response content
+MESSAGE = 'message'
+SUCCESS = 'success'
+ERROR = 'error'
+RESPONSE = 'response'
+EXPIRATION_DATE = 'expiration-date'
---
+++ packages/django-proxy-server/django-proxy-server-0.1.8.4.tar.gz/django-proxy-server-0.1.8.4/tests/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url, patterns
+
+urlpatterns = patterns('views',
+    url(r'^test/$', 'test'),
+    url(r'^test_error/$', 'test_error'),
+    url(r'^test_token_validation/$', 'test_token_validation'),
+)
