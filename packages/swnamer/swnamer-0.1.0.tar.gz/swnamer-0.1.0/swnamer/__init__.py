d10ac379b0db6b914dc1bc766394af6382f51086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package swnamer/0.1.0/swnamer-0.1.0.tar.gz

---
+++ packages/swnamer/swnamer-0.1.0.tar.gz/swnamer-0.1.0/swnamer/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of swnamer.
+# https://github.com/heynemann/swnamer
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2014 Bernardo Heynemann heynemann@gmail.com
+
+from swnamer.version import __version__
+from swnamer.generator import NameGenerator
