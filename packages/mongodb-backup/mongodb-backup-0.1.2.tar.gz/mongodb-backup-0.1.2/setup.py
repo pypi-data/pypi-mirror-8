a766d32bd2d2a84b24a4013bfe95c42f27e475d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:33 +0000

    Add package mongodb-backup/0.1.2/mongodb-backup-0.1.2.tar.gz

---
+++ packages/mongodb-backup/mongodb-backup-0.1.2.tar.gz/mongodb-backup-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+        name='mongodb-backup',
+        version='0.1.2',
+        py_modules=['backup'],
+        entry_points={
+            'console_scripts': [
+                'mongodbbackup=backup:main',
+                ]
+            },
+        url='http://github.com/zhangliyong/mongodb-backup',
+        license='BSD',
+        author='Lyon Zhang',
+        author_email='lyzhang87@gmail.com',
+        install_requires=['click >= 3.0', 'pymongo >= 2.3'],
+        description='A tool to backup mongodb',
+        long_description=open('README.rst').read(),
+)
