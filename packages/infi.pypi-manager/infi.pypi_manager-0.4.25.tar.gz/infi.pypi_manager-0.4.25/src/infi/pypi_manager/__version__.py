8c57d31e8c71402ee9537c4c68bdc4ac127a7f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package infi.pypi-manager/0.4.25/infi.pypi_manager-0.4.25.tar.gz

---
+++ packages/infi.pypi-manager/infi.pypi_manager-0.4.25.tar.gz/infi.pypi_manager-0.4.25/src/infi/pypi_manager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.4.25"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.4.25)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd0715288638ad7985574ff7b55a930c10709d70f'
+__git_head_subject__ = 'fix PyPI.get_all_packages'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
