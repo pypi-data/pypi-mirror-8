c39549a3951eeeb881621b7b91cd1f4684d1e9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:56 +0000

    Add package hanja/0.10.2/hanja-0.10.2.tar.gz

---
+++ packages/hanja/hanja-0.10.2.tar.gz/hanja-0.10.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import hanja
+
+
+def readme():
+    try:
+        with open('README.rst') as f:
+            return f.read()
+    except:
+        return '(Could not read from README.rst)'
+
+
+setup(name='hanja',
+      py_modules=['hanja', 'hanja.pairs', 'hanja.hanja', 'hanja.hangul'],
+      version=hanja.__version__,
+      description='Hangul & Hanja library',
+      long_description=readme(),
+      author=hanja.__author__,
+      author_email=hanja.__email__,
+      url='http://github.com/suminb/hanja',
+      packages=[],
+)
