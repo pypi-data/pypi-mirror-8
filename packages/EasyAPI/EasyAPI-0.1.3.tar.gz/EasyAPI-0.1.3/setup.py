7996a1c9323588ec2cfac3dfb4af4fbffe0c953c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:06 +0000

    Add package EasyAPI/0.1.3/EasyAPI-0.1.3.tar.gz

---
+++ packages/EasyAPI/EasyAPI-0.1.3.tar.gz/EasyAPI-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='EasyAPI',
+    version='0.1.3',
+    author='Adrian Czebiniak',
+    author_email='adrian@artivest.co',
+    packages=['easy_api', 'easy_api.test'],
+    url='http://pypi.python.org/pypi/EasyAPI/',
+    license='LICENSE.txt',
+    description='A generic RESTful API interface for Python',
+    long_description=open('README.rst').read(),
+    install_requires=[
+    ],
+)
