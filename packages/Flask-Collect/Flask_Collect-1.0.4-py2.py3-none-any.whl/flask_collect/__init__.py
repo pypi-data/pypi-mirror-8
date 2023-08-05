205d636ac4b33bd0e696754d61d865b6ca6bc414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:12 +0000

    Add package Flask-Collect/1.0.4/Flask_Collect-1.0.4-py2.py3-none-any.whl

---
+++ packages/Flask-Collect/Flask_Collect-1.0.4-py2.py3-none-any.whl/flask_collect/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+    Flask-Collect
+    =============
+
+    Flack-Collect is simply application for collect static files in Flask_
+    project.
+    Serve static files with Flask_ -- bad idea for production, with this you
+    will can collect them in one command.
+
+    This extension checks application blueprints for static files and copy it
+    to specific folder (saves related paths).
+
+"""
+
+from .collect import Collect # noqa
+
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "BSD"
+__project__ = __name__
+__version__ = "1.0.4"
