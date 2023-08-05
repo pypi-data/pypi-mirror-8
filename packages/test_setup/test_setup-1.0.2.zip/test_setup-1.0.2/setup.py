47a218fb70633a97f80018a42dbdd3accb94ac3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package test_setup/1.0.2/test_setup-1.0.2.zip

---
+++ packages/test_setup/test_setup-1.0.2.zip/test_setup-1.0.2/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+
+from distutils.core import setup
+
+setup(
+    name = 'test_setup',
+    version = '1.0.2',
+    py_modules = ['test_setup'],
+    author = 'caimaoy',
+    author_email = 'caimaoy@gmail.com',
+    url = 'caimaoy.github.com',
+    description = 'first test of setup'
+)
---
+++ packages/test_setup/test_setup-1.0.2.zip/test_setup-1.0.2/test_setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*- 
+
+'''
+just a test of setup
+
+We wish we could say more
+'''
+
+def foo():
+    print 'test of setup'
+
+def foo2():
+    print 'test of setup2'
+
+
