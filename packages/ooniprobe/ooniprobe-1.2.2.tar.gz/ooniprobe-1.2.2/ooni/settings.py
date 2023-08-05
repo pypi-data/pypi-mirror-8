da5a2250efbf904439c2e23cc960aab135111b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:47 +0000

    Add package ooniprobe/1.2.2/ooniprobe-1.2.2.tar.gz

---
+++ packages/ooniprobe/ooniprobe-1.2.2.tar.gz/ooniprobe-1.2.2/ooni/settings.py
@@ -0,0 +1,180 @@
+import os
+import sys
+import yaml
+import getpass
+
+from twisted.internet import defer, reactor
+from twisted.internet.endpoints import TCP4ClientEndpoint
+
+from os.path import abspath, expanduser
+
+from ooni.utils.net import ConnectAndCloseProtocol, connectProtocol
+from ooni import geoip
+from ooni.utils import Storage, log
+from ooni import errors
+
+
+class OConfig(object):
+    _custom_home = None
+
+    def __init__(self):
+        self.current_user = getpass.getuser()
+        self.global_options = {}
+        self.reports = Storage()
+        self.scapyFactory = None
+        self.tor_state = None
+        # This is used to store the probes IP address obtained via Tor
+        self.probe_ip = geoip.ProbeIP()
+        self.logging = True
+        self.basic = Storage()
+        self.advanced = Storage()
+        self.tor = Storage()
+        self.privacy = Storage()
+        self.set_paths()
+
+    @property
+    def data_directory(self):
+        data_directory = abspath(os.path.join(__file__, '..', '..', 'data'))
+
+        if os.getenv("OONI_DATA_DIR"):
+            data_directory = os.getenv("OONI_DATA_DIR")
+        elif self.global_options.get('datadir'):
+            data_directory = abspath(expanduser(self.global_options['datadir']))
+        elif self.advanced.get('data_dir'):
+            data_directory = self.advanced['data_dir']
+        elif hasattr(sys, 'real_prefix'):
+            data_directory = os.path.abspath(os.path.join(sys.prefix, 'share', 'ooni'))
+        elif not os.path.exists(data_directory):
+            data_directory = '/usr/share/ooni/'
+
+        return data_directory
+
+    def set_paths(self, ooni_home=None):
+        if ooni_home:
+            self._custom_home = ooni_home
+
+        self.nettest_directory = abspath(os.path.join(__file__, '..', 'nettests'))
+
+        self.ooni_home = os.path.join(expanduser('~'+self.current_user), '.ooni')
+        if self._custom_home:
+            self.ooni_home = self._custom_home
+        self.inputs_directory = os.path.join(self.ooni_home, 'inputs')
+        self.decks_directory = os.path.join(self.ooni_home, 'decks')
+        self.reports_directory = os.path.join(self.ooni_home, 'reports')
+        self.report_log_file = os.path.join(self.ooni_home, 'reporting.yml')
+        self.resources_directory = os.path.join(self.data_directory, "resources")
+
+        if self.global_options.get('configfile'):
+            config_file = self.global_options['configfile']
+            self.config_file = expanduser(config_file)
+        else:
+            self.config_file = os.path.join(self.ooni_home, 'ooniprobe.conf')
+
+        if 'logfile' in self.basic:
+            self.basic.logfile = expanduser(self.basic.logfile.replace('~','~'+self.current_user))
+
+        if not os.path.exists(self.data_directory):
+            log.err("Data directory %s does not exists" % self.data_directory)
+            log.err("Edit data_dir inside of %s" % self.config_file)
+
+
+    def initialize_ooni_home(self, ooni_home=None):
+        if ooni_home:
+            self.set_paths(ooni_home)
+
+        if not os.path.isdir(self.ooni_home):
+            print "Ooni home directory does not exist."
+            print "Creating it in '%s'." % self.ooni_home
+            os.mkdir(self.ooni_home)
+            os.mkdir(self.inputs_directory)
+            os.mkdir(self.decks_directory)
+        if not os.path.isdir(self.reports_directory):
+            os.mkdir(self.reports_directory)
+
+    def _create_config_file(self):
+        target_config_file = self.config_file
+        print "Creating it for you in '%s'." % target_config_file
+        usr_share_path = '/usr/share'
+        if hasattr(sys, 'real_prefix'):
+            usr_share_path = os.path.abspath(os.path.join(sys.prefix, 'share'))
+        sample_config_file = os.path.join(self.data_directory,
+                                          'ooniprobe.conf.sample')
+
+        with open(sample_config_file) as f:
+            with open(target_config_file, 'w+') as w:
+                for line in f:
+                    if line.startswith('    data_dir: '):
+                        w.write('    data_dir: %s\n' % self.data_directory)
+                    elif line.startswith('    geoip_data_dir: '):
+                        w.write('    geoip_data_dir: %s\n' % os.path.join(usr_share_path, 'GeoIP'))
+                    else:
+                        w.write(line)
+
+    def read_config_file(self, check_incoherences=False):
+        if not os.path.isfile(self.config_file):
+            print "Configuration file does not exist."
+            self._create_config_file()
+            self.read_config_file()
+
+        with open(self.config_file) as f:
+            config_file_contents = '\n'.join(f.readlines())
+            configuration = yaml.safe_load(config_file_contents)
+
+        for setting in configuration.keys():
+            if setting in dir(self) and configuration[setting] is not None:
+                for k, v in configuration[setting].items():
+                    getattr(self, setting)[k] = v
+
+        self.set_paths()
+        if check_incoherences:
+            self.check_incoherences(configuration)
+
+    def check_incoherences(self, configuration):
+        incoherent = []
+
+        if configuration['advanced']['interface'] != 'auto':
+            from scapy.all import get_if_list
+            if configuration['advanced']['interface'] not in get_if_list():
+                incoherent.append('advanced:interface')
+
+        self.log_incoherences(incoherent)
+
+    def log_incoherences(self, incoherences):
+        if len(incoherences) > 0:
+            if len(incoherences) > 1:
+                incoherent_pretty = ", ".join(incoherences[:-1]) + ' and ' + incoherences[-1]
+            else:
+                incoherent_pretty = incoherences[0]
+            log.err("You must set properly %s in %s." % (incoherent_pretty, self.config_file))
+            raise errors.ConfigFileIncoherent
+
+    @defer.inlineCallbacks
+    def check_tor(self):
+        """
+        Called only when we must start tor by director.start
+        """
+        incoherent = []
+        if not self.advanced.start_tor:
+            if self.tor.socks_port is None:
+                incoherent.append('tor:socks_port')
+            else:
+                socks_port_ep = TCP4ClientEndpoint(reactor,
+                                                   "localhost",
+                                                   self.tor.socks_port)
+                try:
+                    yield connectProtocol(socks_port_ep, ConnectAndCloseProtocol())
+                except Exception:
+                    incoherent.append('tor:socks_port')
+
+            if self.tor.control_port is not None:
+                control_port_ep = TCP4ClientEndpoint(reactor,
+                                                     "localhost",
+                                                     self.tor.control_port)
+                try:
+                    yield connectProtocol(control_port_ep, ConnectAndCloseProtocol())
+                except Exception:
+                    incoherent.append('tor:control_port')
+
+            self.log_incoherences(incoherent)
+
+config = OConfig()
---
+++ packages/ooniprobe/ooniprobe-1.2.2.tar.gz/ooniprobe-1.2.2/setup.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from ooni import __version__, __author__
+import urllib2
+import os
+import gzip
+from os.path import join as pj
+import sys
+from setuptools import setup
+
+def download_geoip_files(dst):
+    urls = [
+        'http://www.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz',
+        'http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz',
+        'http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz'
+    ]
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    for url in urls:
+        target_gz_file = pj(dst, os.path.basename(url))
+        target_file = target_gz_file.replace('.gz', '')
+
+        if os.path.isfile(target_file):
+            print "%s already exists. Skipping." % target_file
+            continue
+
+        print "Downloading %s" % url
+        response = urllib2.urlopen(url)
+
+        CHUNK = 4 * 1024
+        with open(target_gz_file, 'w+') as f:
+            while True:
+                chunk = response.read(CHUNK)
+                if not chunk: break
+                f.write(chunk)
+
+        with open(target_file, 'w+') as f:
+            gf = gzip.open(target_gz_file, 'rb')
+            while True:
+                chunk = gf.read(CHUNK)
+                if not chunk: break
+                f.write(chunk)
+            gf.close()
+
+        os.unlink(target_gz_file)
+
+usr_share_path = '/usr/share'
+# If this is true then it means we are in a virtualenv
+# therefore we should not place our data files inside /usr/share/ooni, but
+# place them inside the virtual env system prefix.
+if hasattr(sys, 'real_prefix'):
+    usr_share_path = os.path.abspath(pj(sys.prefix, 'share'))
+
+download_geoip_files(pj(usr_share_path, 'GeoIP'))
+
+install_requires = []
+dependency_links = []
+data_files = []
+
+for root, dirs, file_names in os.walk('data/'):
+    files = []
+    for file_name in file_names:
+        if file_name.endswith('.pyc'):
+            continue
+        elif file_name.endswith('.dat') and \
+                file_name.startswith('Geo'):
+            continue
+        files.append(pj(root, file_name))
+    data_files.append([pj(usr_share_path, 'ooni', root.replace('data/', '')), files])
+
+
+packages=[
+    'ooni',
+    'ooni.api',
+    'ooni.deckgen',
+    'ooni.deckgen.processors',
+    'ooni.kit',
+    'ooni.nettests',
+    'ooni.nettests.manipulation',
+    'ooni.nettests.experimental',
+    'ooni.nettests.scanning',
+    'ooni.nettests.blocking',
+    'ooni.nettests.third_party',
+    'ooni.report',
+    'ooni.resources',
+    'ooni.templates',
+    'ooni.tests',
+    'ooni.utils'
+]
+
+with open('requirements.txt') as f:
+    for line in f:
+        if line.startswith("#"):
+            continue
+        if line.startswith('https'):
+            dependency_links.append(line)
+            continue
+        install_requires.append(line)
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('ChangeLog.rst') as f:
+    changelog = f.read()
+
+setup(
+    name="ooniprobe",
+    version=__version__,
+    author=__author__,
+    author_email="ooni-dev@torproject.org",
+    description="Network Interference detection tool.",
+    long_description=readme + '\n\n' + changelog,
+    license='BSD 2 clause',
+    url="https://ooni.torproject.org/",
+    package_dir={'ooni': 'ooni'},
+    data_files=data_files,
+    packages=packages,
+    scripts=["bin/oonideckgen", "bin/ooniprobe",
+             "bin/oonireport", "bin/ooniresources"],
+    dependency_links=dependency_links,
+    install_requires=install_requires,
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Framework :: Twisted",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: BSD License"
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: BSD :: BSD/OS",
+        "Operating System :: POSIX :: BSD :: FreeBSD",
+        "Operating System :: POSIX :: BSD :: NetBSD",
+        "Operating System :: POSIX :: BSD :: OpenBSD",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Testing :: Traffic Generation",
+        "Topic :: System :: Networking :: Monitoring",
+    )
+)
