423785a319216ed8ea470723cd47e4023803900e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:07 +0000

    Add package django-variant/0.1.0/django-variant-0.1.0.tar.gz

---
+++ packages/django-variant/django-variant-0.1.0.tar.gz/django-variant-0.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='django-variant',
+    version='0.1.0',
+    description='Django variant testing framework',
+    author='Jeremy Sattefield',
+    author_email='jsatt@jsatt.com',
+    url='https://github.com/jsatt/django-variant',
+    packages=[
+        'variant', 'variant.migrations'],
+    install_requires=[
+        'Django>=1.5', 'six'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Site Management'],
+)
