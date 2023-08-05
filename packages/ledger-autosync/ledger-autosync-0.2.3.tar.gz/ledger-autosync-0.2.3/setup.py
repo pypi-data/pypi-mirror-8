ffe6b245187d694d8c67a4f045fcda1d749dc529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:44 +0000

    Add package ledger-autosync/0.2.3/ledger-autosync-0.2.3.tar.gz

---
+++ packages/ledger-autosync/ledger-autosync-0.2.3.tar.gz/ledger-autosync-0.2.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='ledger-autosync',
+      version="0.2.3",
+      description="Automatically sync your bank's data with ledger",
+      long_description=long_description,
+      classifiers=[
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+      ],
+      author='Erik Hetzner',
+      author_email='egh@e6h.org',
+      url='https://bitbucket.org/egh/ledger-autosync',
+      packages=find_packages(exclude=[
+          'tests']),
+      entry_points={
+          'console_scripts': [
+              'ledger-autosync = ledgerautosync.cli:run',
+              'hledger-autosync = ledgerautosync.cli:run'
+          ]
+      },
+      install_requires=[
+          "ofxclient",
+          "ofxparse>=0.14"
+      ],
+      setup_requires=['nose>=1.0',
+                      'mock'],
+      test_suite = 'nose.collector'
+      )
---
+++ packages/ledger-autosync/ledger-autosync-0.2.3.tar.gz/ledger-autosync-0.2.3/tests/test_cli.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2013, 2014 Erik Hetzner
+# 
+# This file is part of ledger-autosync
+#
+# ledger-autosync is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# ledger-autosync is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ledger-autosync. If not, see
+# <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+from ledgerautosync.cli import sync
+from ledgerautosync.ledgerwrap import Ledger, LedgerPython, HLedger
+from ofxclient.config import OfxConfig
+import os.path
+
+from unittest import TestCase
+from mock import Mock
+
+class TestCli(TestCase):
+    def test_run(self):
+        for lgr in [LedgerPython(os.path.join('fixtures', 'empty.lgr')),
+                    HLedger(os.path.join('fixtures', 'empty.lgr')),
+                    Ledger(os.path.join('fixtures', 'empty.lgr'), no_pipe=True)]:
+            config = OfxConfig(os.path.join('fixtures', 'ofxclient.ini'))
+            acct = config.accounts()[0]
+            acct.download = Mock(return_value=file(os.path.join('fixtures', 'checking.ofx')))
+            config.accounts = Mock(return_value=[acct])
+            sync(lgr, config, 7)
+
+    def test_empty_run(self):
+        for lgr in [LedgerPython(os.path.join('fixtures', 'checking.lgr')),
+                    HLedger(os.path.join('fixtures', 'checking.lgr')),
+                    Ledger(os.path.join('fixtures', 'checking.lgr'), no_pipe=True)]:
+            config = OfxConfig(os.path.join('fixtures', 'ofxclient.ini'))
+            acct = config.accounts()[0]
+            acct.download = Mock(return_value=file(os.path.join('fixtures', 'checking.ofx')))
+            config.accounts = Mock(return_value=[acct])
+            sync(lgr, config, 7)
+
