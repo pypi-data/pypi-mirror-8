32bb50c848ea770c04aae0017493172ccf692a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:11 +0000

    Add package collective.wfeffectiverange/1.0/collective.wfeffectiverange-1.0.zip

---
+++ packages/collective.wfeffectiverange/collective.wfeffectiverange-1.0.zip/collective.wfeffectiverange-1.0/CHANGES.rst
@@ -0,0 +1,9 @@
+
+Changes
+=======
+
+1.0 (2014-09-22)
+----------------
+
+- Initial implementation.
+  [jensens, 2014-09-22]
---
+++ packages/collective.wfeffectiverange/collective.wfeffectiverange-1.0.zip/collective.wfeffectiverange-1.0/src/collective/wfeffectiverange/browser/resources/wfeffectiverange-reload-vocab.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function () {
+
+    function reload_vocab(current, contenttype) {
+//        this function reloads the possible values for expires_transition, each time
+//        effective_transition has changed
+
+        // get current options
+        var options = $("#form-widgets-IWFEffectiveRange-expires_transition option");
+        //get expires selector
+        var selector =  $("#form-widgets-IWFEffectiveRange-expires_transition");
+            // remove old options
+            selector.empty();
+
+        $.getJSON("@@wfeffectiverange_vocab?current=" + current + "&contenttype=" + contenttype, function (result) {
+            //the first option is always 'no-value'
+            selector.append(options[0]);
+
+            //set new options
+            $.each(result, function (idx, term) {
+                var new_option = $("<option></option>")
+                new_option.attr("value", term.token).text(term.token);
+                selector.append(new_option);
+            });
+        });
+    }
+
+    $("#form-widgets-IWFEffectiveRange-effective_transition").change(function () {
+        //extra geht the portaltype from the url
+        var path = window.location.pathname;
+        var contenttype = path.replace(/.*\+{2}add\+{2}/, "");
+        reload_vocab($(this).val(), contenttype);
+    });
+});
+
