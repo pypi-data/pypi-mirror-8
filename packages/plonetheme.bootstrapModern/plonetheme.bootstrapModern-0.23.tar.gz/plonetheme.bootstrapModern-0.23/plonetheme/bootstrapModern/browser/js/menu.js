e617e0213c7bca167184cef63e74e18a1fe32ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:59 +0000

    Add package plonetheme.bootstrapModern/0.23/plonetheme.bootstrapModern-0.23.tar.gz

---
+++ packages/plonetheme.bootstrapModern/plonetheme.bootstrapModern-0.23.tar.gz/plonetheme.bootstrapModern-0.23/plonetheme/bootstrapModern/browser/js/menu.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+	$("#portal-languageselector li:first-child").append(' / ');
+	var $items = $( '.menu ul li' );
+	var totalItems = $items.length;
+	var maxHeight = 100/totalItems;
+	maxHeight = maxHeight + "%";
+	$( '.menu ul li' ).css('height', maxHeight);
+
+	var triggerBttn = document.getElementById( 'trigger-overlay' );
+	var overlay = document.querySelector( '.menu' );
+	if (overlay != undefined) {
+		var closeBttn = overlay.querySelector( 'button.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+	}
+
+	function toggleOverlay() {
+		if (overlay != undefined) {
+			if( classie.has( overlay, 'open' ) ) {
+				classie.remove( overlay, 'open' );
+				classie.add( overlay, 'close' );
+
+				var onEndTransitionFn = function( ev ) {
+					if( support.transitions ) {
+						if( ev.propertyName !== 'visibility' ) return;
+						this.removeEventListener( transEndEventName, onEndTransitionFn );
+					}
+					$(triggerBttn).show();
+					$("#portal-languageselector").hide();
+					classie.remove( overlay, 'close' );
+				};
+				if( support.transitions ) {
+					overlay.addEventListener( transEndEventName, onEndTransitionFn );
+				}
+				else {
+					onEndTransitionFn();
+				}
+			}
+			else if( !classie.has( overlay, 'close' ) ) {
+				$(triggerBttn).hide();
+				$("#portal-languageselector").show();
+				classie.add( overlay, 'open' );
+			}
+		}
+	}
+	if (triggerBttn != null) {
+		triggerBttn.addEventListener( 'click', toggleOverlay );
+		closeBttn.addEventListener( 'click', toggleOverlay );
+	}
+});
