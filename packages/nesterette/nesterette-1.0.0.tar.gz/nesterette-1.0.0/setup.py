dd0f3eedd7bba0e79133fa463560b4d565d0082b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:16 +0000

    Add package nesterette/1.0.0/nesterette-1.0.0.tar.gz

---
+++ packages/nesterette/nesterette-1.0.0.tar.gz/nesterette-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+	
+	name = 'nesterette',
+	version = '1.0.0',
+	pymodules = ['nesterette'],
+	author = 'kamudhar',
+	author_email = 'kamudhar@gmail.com',
+	url = 'http://kamudhar.wordpress.com',
+	description = 'prints nested lists',
+	)
