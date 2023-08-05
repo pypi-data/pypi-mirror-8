2d5cbedf1a585053c6ec2184a4ed42e4bea0f55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:15 +0000

    Add package mendeley/0.2.1/mendeley-0.2.1.tar.gz

---
+++ packages/mendeley/mendeley-0.2.1.tar.gz/mendeley-0.2.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+__version__ = None
+with open('mendeley/version.py') as f:
+    exec(f.read())
+
+setup(
+    name='mendeley',
+    version=__version__,
+    packages=['mendeley', 'mendeley.models', 'mendeley.resources'],
+    url='http://dev.mendeley.com',
+    license='Apache',
+    author='Mendeley',
+    author_email='api@mendeley.com',
+    description='Python SDK for the Mendeley API',
+
+    install_requires=[
+        'arrow',
+        'future',
+        'memoized-property',
+        'requests',
+        'requests-oauthlib',
+    ],
+
+    tests_require=[
+        'pytest',
+        'vcrpy'
+    ],
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
