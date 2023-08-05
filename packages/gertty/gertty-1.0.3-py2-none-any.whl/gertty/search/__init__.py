8cacb4ef6d19012acbe8e1ef4faf1d90293c2c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package gertty/1.0.3/gertty-1.0.3-py2-none-any.whl

---
+++ packages/gertty/gertty-1.0.3-py2-none-any.whl/gertty/search/__init__.py
@@ -0,0 +1,33 @@
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import sqlalchemy.sql.expression
+
+from gertty.search import tokenizer, parser
+import gertty.db
+
+
+class SearchSyntaxError(Exception):
+    pass
+
+
+class SearchCompiler(object):
+    def __init__(self, app):
+        self.app = app
+        self.lexer = tokenizer.SearchTokenizer()
+        self.parser = parser.SearchParser()
+
+    def parse(self, data):
+        self.parser.username = self.app.config.username
+        return self.parser.parse(data, lexer=self.lexer)
