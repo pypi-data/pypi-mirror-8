42ad9e46b32d5ec58e1ac8c9851a5fa3ff631f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package wingcommander/0.1.4/wingcommander-0.1.4.tar.gz

---
+++ packages/wingcommander/wingcommander-0.1.4.tar.gz/wingcommander-0.1.4/src/wingcommander/__init__.py
@@ -0,0 +1,5 @@
+from wingcommander.commander import WingCommander
+from wingcommander.util import run, watch, tablize
+
+__version__ = "0.1.4"
+__all__ = ["WingCommander", "run", "watch", "tablize"]
