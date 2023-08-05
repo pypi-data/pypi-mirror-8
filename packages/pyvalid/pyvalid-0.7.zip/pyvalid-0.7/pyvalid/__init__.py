d64b14aec2411782a17f3fef56f89666e0d23288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:36 +0000

    Add package pyvalid/0.7/pyvalid-0.7.zip

---
+++ packages/pyvalid/pyvalid-0.7.zip/pyvalid-0.7/pyvalid/__init__.py
@@ -0,0 +1,19 @@
+from pyvalid.__accepts import Accepts
+from pyvalid.__returns import Returns
+from pyvalid.__exceptions import InvalidArgumentNumberError, \
+    InvalidReturnType, ArgumentValidationError
+
+
+accepts = Accepts
+returns = Returns
+version = '0.7'
+
+
+__all__ = [
+    'version',
+    'accepts',
+    'returns',
+    'InvalidArgumentNumberError',
+    'InvalidReturnType',
+    'ArgumentValidationError'
+]
