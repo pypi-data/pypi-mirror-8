945988143fe61a51980ac59e4c2f35fc7f5da4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package xiaoli_nester/1.2.0/xiaoli_nester-1.2.0.zip

---
+++ packages/xiaoli_nester/xiaoli_nester-1.2.0.zip/xiaoli_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name = 'xiaoli_nester',
+       version = '1.2.0',
+       py_modules = ['nester'],
+       author = 'xiaoli',
+       author_email = 'xiaoli20140109@gmail.com',
+       url = 'http://www.lucy.com',
+       description = '首次上传多多指教',
+       )
