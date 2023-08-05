1d156d287099b872eb3e627b48e628295ef93632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:39 +0000

    Add package linkmanager/0.4.0.4/linkmanager-0.4.0.4.tar.gz

---
+++ packages/linkmanager/linkmanager-0.4.0.4.tar.gz/linkmanager-0.4.0.4/linkmanager/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__appname__ = 'linkmanager'
+__author__ = "Jérémie Ferry <jerem.ferry@gmail.com>"
+__licence__ = "BSD"
+__website__ = "https://github.com/mothsART/linkmanager"
+__version__ = '0.4.0.4'
+VERSION = tuple(map(int, __version__.split('.')))
+
+
+def interface(test=False):
+    from .tty import TTYInterface
+    return TTYInterface(test=test)
