ef463165554e13fee599a0de17f50eae48807287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package pive/0.2.1/pive-0.2.1.tar.gz

---
+++ packages/pive/pive-0.2.1.tar.gz/pive-0.2.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+print (find_packages())
+
+setup(
+	name = 'pive',
+	packages = ['pive', 'pive/visualization'],
+	#package_data = {'': ['*.html', '*.css', '*.js', '*.json', '.jinja']},
+	#package_dir={'': 'visualization'},
+	use_2to3 = True,
+	include_package_data=True,
+	version = '0.2.1',
+	url = 'python-ive.org',
+	license = 'BSD',
+	description = 'Interactive visualization tool',
+	long_description=open('README.txt').read(),
+	author = 'David Bothe',
+	author_email = 'davbothe@googlemail.com',
+	classifiers = [
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'Environment :: Web Environment',		
+		'Topic :: Scientific/Engineering :: Visualization',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+	],
+	install_requires = ['jinja2']
+
+)
