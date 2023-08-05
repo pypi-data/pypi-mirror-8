0c19f05fc5703cafed5dcf41de12f2c40905293e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:50 +0000

    Add package simpledist/0.1.5-3/simpledist-0.1.5-3.tar.gz

---
+++ packages/simpledist/simpledist-0.1.5-3.tar.gz/simpledist-0.1.5-3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name = "simpledist",
+    version = "0.1.5-3",
+    description = "Defines objects useful for describing simple probability distributions.",
+    long_description = readme(),
+    author = "Timothy D. Morton",
+    author_email = "tim.morton@gmail.com",
+    url = "https://github.com/timothydmorton/simpledist",
+    packages = ['simpledist'],
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Science/Research',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Scientific/Engineering'
+      ],
+    install_requires=['plotutils','pandas>=0.13'],
+    zip_safe=False
+) 
