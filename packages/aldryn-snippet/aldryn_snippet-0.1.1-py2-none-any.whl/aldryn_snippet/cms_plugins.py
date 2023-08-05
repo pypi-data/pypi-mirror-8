38afb19a732ffa0fd0feb79b54c67e9c3f75c4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:00 +0000

    Add package aldryn-snippet/0.1.1/aldryn_snippet-0.1.1-py2-none-any.whl

---
+++ packages/aldryn-snippet/aldryn_snippet-0.1.1-py2-none-any.whl/aldryn_snippet/cms_plugins.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from .models import SnippetPlugin
+from .forms import SnippetForm
+
+
+class Snippet(CMSPluginBase):
+    name = _('Snippet')
+    model = SnippetPlugin
+    form = SnippetForm
+    change_form_template = 'aldryn_snippet/admin/snippet_change_form.html'
+    render_template = 'aldryn_snippet/snippet.html'
+
+
+plugin_pool.register_plugin(Snippet)
