a53791eca5fec0a831c5dcf2e1db007eeaa335fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:00 +0000

    Add package ztilde/0.1/ztilde-0.1.tar.gz

---
+++ packages/ztilde/ztilde-0.1.tar.gz/ztilde-0.1/setup.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+from distutils.sysconfig import get_python_lib
+
+setup(
+    name='ztilde',
+    version='0.1',
+    url='http://www.ztilde.com/',
+    author='Thiago F Pappacena',
+    author_email='pappacena@ztilde.com',
+    description=('Python client lib for ztilde.com machine learning  services'),
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
