4d7d5087cc0e3a2cbba6b693ea64a950fbed7b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package sufartest/0.0.1/sufartest-0.0.1.tar.gz

---
+++ packages/sufartest/sufartest-0.0.1.tar.gz/sufartest-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup,find_packages
+setup(name='sufartest',
+     version='0.0.1',
+     keywords=('bee','egg'),
+     description='a simple egg',
+     license='MIT License',
+     url='http://liluo.org',
+     author='Sufar',
+     author_email='1160189043@qq.com',
+     packages=find_packages(),
+     include_package_data=True,
+     platforms='any',
+     install_requires =[])
