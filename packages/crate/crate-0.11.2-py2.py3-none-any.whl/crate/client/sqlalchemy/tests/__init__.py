5c6c0a8958186b471e7b012115868f342ebf189f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:54 +0000

    Add package crate/0.11.2/crate-0.11.2-py2.py3-none-any.whl

---
+++ packages/crate/crate-0.11.2-py2.py3-none-any.whl/crate/client/sqlalchemy/tests/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from unittest import TestSuite, makeSuite
+from .connection_test import SqlAlchemyConnectionTest
+from .dict_test import SqlAlchemyDictTypeTest
+from .datetime_test import SqlAlchemyDateAndDateTimeTest
+from .compiler_test import SqlAlchemyCompilerTest
+from .update_test import SqlAlchemyUpdateTest
+
+
+def test_suite():
+    tests = TestSuite()
+    tests.addTest(makeSuite(SqlAlchemyConnectionTest))
+    tests.addTest(makeSuite(SqlAlchemyDictTypeTest))
+    tests.addTest(makeSuite(SqlAlchemyDateAndDateTimeTest))
+    tests.addTest(makeSuite(SqlAlchemyCompilerTest))
+    tests.addTest(makeSuite(SqlAlchemyUpdateTest))
+    return tests
