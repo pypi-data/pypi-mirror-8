7cc9c0b219801d981bee202b6d9beeabfe2c43a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package boxcalendar/0.0.11/boxcalendar-0.0.11.tar.gz

---
+++ packages/boxcalendar/boxcalendar-0.0.11.tar.gz/boxcalendar-0.0.11/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'boxcalendar',
+    version = '0.0.11',
+    keywords = ('calendar', 'chinese'),
+    description = 'just a simple chinese calendar',
+    license = 'MIT License',
+    author = 'CcdjhMarx',
+    author_email = 'ccdjh.marx@all.com',
+    packages = ['boxcalendar'],
+)
