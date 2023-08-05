aba842fe5be4b831235f7e909c6f2a1b1542b116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:49 +0000

    Add package zopen.frs/1.2/zopen.frs-1.2.tar.gz

---
+++ packages/zopen.frs/zopen.frs-1.2.tar.gz/zopen.frs-1.2/zopen/frs/archive.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+""" asset archive management
+"""
+import time
+from xml.sax import parseString, ContentHandler
+from xml.sax.saxutils import escape
+
+from utils import timetag
+from config import FRS_ARCHIVED_FOLDER_NAME
+
+
+class ArchiveSupportMixin:
+
+    def archivedpath(self, path, *args):
+        return self.frspath(path, FRS_ARCHIVED_FOLDER_NAME, *args)
+
+    def archive(self, path, id):
+        archivePath = self.archivedpath(path)
+        if not self.exists(archivePath):
+            self.makedirs(archivePath)
+
+        dstpath = self.joinpath(archivePath, id)
+        self.copyAsset(path, dstpath, copycache=0)
+
+    def listArchives(self, path):
+        archivePath = self.archivedpath(path)
+        if self.exists(archivePath):
+            archives = self.listdir(archivePath)
+        else:
+            archives = []
+        return archives
+
+    def copyArchive(self, path, archiveName, dstPath):
+        assetpath = self.archivedpath(path, archiveName)
+        return self.copyAsset(assetpath, dstPath, copycache=0)
+
+    def removeArchive(self, path, archiveName):
+        self.removeAsset( self.archivedpath(path, archiveName) )
+
---
+++ packages/zopen.frs/zopen.frs-1.2.tar.gz/zopen.frs-1.2/zopen/frs/config.py
@@ -0,0 +1,8 @@
+# for adapters
+FRS_REMOVED_FOLDER_NAME = '.frsremoved'
+FRS_ARCHIVED_FOLDER_NAME = 'archived'
+FRS_CACHE_FOLDER_PREFIX = '.frs.'
+
+import sys
+# FS_CHARSET = 'gbk'
+FS_CHARSET = sys.getfilesystemencoding()
