ddbabf46616432f7cec0bab336bbfd4ced84e8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:52 +0000

    Add package PGeoMODIS/0.1.2/PGeoMODIS-0.1.2.tar.gz

---
+++ packages/PGeoMODIS/PGeoMODIS-0.1.2.tar.gz/PGeoMODIS-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='PGeoMODIS',
+    version='0.1.2',
+    author='Simone Murzilli; Guido Barbaglia',
+    author_email='geobrickspy@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pgeo',
+        'flask'
+    ],
+    url='http://pypi.python.org/pypi/PGeoMODIS/',
+    license='LICENSE.txt',
+    description='MODIS plug-in for PGeo.',
+    long_description=open('README.md').read()
+)
