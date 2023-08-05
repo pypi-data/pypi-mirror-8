3f70350a45eed595476bcb9336b3b3bd7fb4466f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package inbox/0.1.1/inbox-0.1.1.tar.gz

---
+++ packages/inbox/inbox-0.1.1.tar.gz/inbox-0.1.1/setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+setup(
+    name="inbox",
+    version="0.1.1",
+    packages=find_packages(),
+
+    install_requires=[
+        "requests>=2.3.0",
+        "six>=1.4.1",
+    ],
+    dependency_links=[],
+
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        # '': ['*.txt', '*.rst'],
+        # And include any *.msg files found in the 'hello' package, too:
+        # 'hello': ['*.msg'],
+    },
+
+    author="Inbox Team",
+    author_email="admin@inboxapp.com",
+    description='Python bindings for Inbox, the next-generation email platform.',
+    license="MIT",
+    keywords="inbox app appserver email",
+    url='https://github.com/inboxapp/inbox-python'
+)
