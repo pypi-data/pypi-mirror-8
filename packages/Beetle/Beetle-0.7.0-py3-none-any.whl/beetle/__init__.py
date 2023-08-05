b0398f64a8bb42587c55fc2643265e7fe6452662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:37 +0000

    Add package Beetle/0.7.0/Beetle-0.7.0-py3-none-any.whl

---
+++ packages/Beetle/Beetle-0.7.0-py3-none-any.whl/beetle/__init__.py
@@ -0,0 +1,11 @@
+name = 'Beetle'
+version = '0.7.0'
+project_url = 'https://github.com/cknv/beetle'
+
+class BeetleError(Exception):
+    def __init__(self, message, page=None):
+        self.message = message
+        self.page = page
+
+    def __str__(self):
+        return self.message
