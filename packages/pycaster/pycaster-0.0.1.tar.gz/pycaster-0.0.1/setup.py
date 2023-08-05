de36e507284dfd790715f28de5b94ebc3354126e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:33 +0000

    Add package pycaster/0.0.1/pycaster-0.0.1.tar.gz

---
+++ packages/pycaster/pycaster-0.0.1.tar.gz/pycaster-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='pycaster',
+    version='0.0.1',
+    author='Adamos Kyriakou',
+    author_email='somada141@gmail.com',
+    packages=['pycaster',
+              'pycaster.test'],
+    package_data={'pycaster.test': ['data/*.*']},
+    url='http://pypi.python.org/pypi/pycaster/',
+    license='LICENSE.txt',
+    description='A Python package which uses VTK to perform ray-casting',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy >= 1.8.0",
+        "vtk >= 5.10.1",
+        "fieldpy >= 0.1.5",
+        "nose >= 1.3.3",
+    ],
+)
