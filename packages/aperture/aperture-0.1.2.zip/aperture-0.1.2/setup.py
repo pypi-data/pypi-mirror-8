b60f355a1d5851ca8ef3b016674aebedb250a604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:46 +0000

    Add package aperture/0.1.2/aperture-0.1.2.zip

---
+++ packages/aperture/aperture-0.1.2.zip/aperture-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='aperture',
+      version='0.1.2',
+      description='Wrappers, settings, and extensions for Python/matplotlib',
+      url='https://github.com/kevinsprong/aperture',
+      author='Kevin Sprong',
+      author_email='kevin@kevinsprong.com',
+      license='MIT',
+      packages=['aperture'],
+      install_requires=[
+          'matplotlib',
+      ],
+      zip_safe=False)
