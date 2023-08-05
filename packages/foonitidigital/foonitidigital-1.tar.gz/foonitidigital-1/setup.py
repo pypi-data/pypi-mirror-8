e8352aa1e4e1d51481f5f941a0b60ab77c773bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:37 +0000

    Add package foonitidigital/1/foonitidigital-1.tar.gz

---
+++ packages/foonitidigital/foonitidigital-1.tar.gz/foonitidigital-1/setup.py
@@ -0,0 +1,43 @@
+
+from distutils.core import setup
+
+#This is a list of files to install, and where
+#(relative to the 'root' dir, where setup.py is)
+#You could be more specific.
+files = ["things/*"]
+
+setup(name = "foonitidigital",
+    version = "1",
+    description = "Testing the app",
+    author = "Raj",
+    author_email = "raj.pawar@netcore.co.in",
+    maintainer = "raj",
+    maintainer_email = "raj.pawar@netcore.co.in",
+    url = "whatever",
+    #Name the folder where your packages live:
+    #(If you have other packages (dirs) or modules (py files) then
+    #put them into the package directory - they will be found 
+    #recursively.)
+    packages = ['package'],
+    #'package' package must contain files (see list above)
+    #I called the package 'package' thus cleverly confusing the whole issue...
+    #This dict maps the package name =to=> directories
+    #It says, package *needs* these files.
+    package_data = {'package' : files },
+    #'runner' is in the root.
+    scripts = ["runner"],
+    long_description = """Really long text here.""" ,
+    #
+    #This next part it for the Cheese Shop, look a little down the page.
+    classifiers=[      
+      'Development Status :: 4 - Beta',
+      'Environment :: X11 Applications :: GTK',
+      'Intended Audience :: End Users/Desktop',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python',
+      'Topic :: Desktop Environment',
+      'Topic :: Text Processing :: Fonts'
+   ],
+)
