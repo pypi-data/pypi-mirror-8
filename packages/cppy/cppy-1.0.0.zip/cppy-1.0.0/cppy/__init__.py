8001557ed34dcaec56cfb7d6c924bf8cd1c0dcb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:22 +0000

    Add package cppy/1.0.0/cppy-1.0.0.zip

---
+++ packages/cppy/cppy-1.0.0.zip/cppy-1.0.0/cppy/__init__.py
@@ -0,0 +1,21 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2014, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+__major_version__ = 1
+__minor_version__ = 0
+__patch_version__ = 0
+
+
+__version_info__ = (__major_version__, __minor_version__, __patch_version__)
+
+
+__version__ = '%s.%s.%s' % __version_info__
+
+
+def get_include():
+    import os
+    return os.path.join(os.path.dirname(__file__), 'include')
---
+++ packages/cppy/cppy-1.0.0.zip/cppy-1.0.0/cppy/include/cppy/defines.h
@@ -0,0 +1,20 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014, Nucleic Development Team.
+|
+| Distributed under the terms of the Modified BSD License.
+|
+| The full license is in the file COPYING.txt, distributed with this software.
+|----------------------------------------------------------------------------*/
+#pragma once
+
+#include <Python.h>
+
+#define CPPY_MAJOR_VERSION 1
+#define CPPY_MINOR_VERSION 0
+#define CPPY_PATCH_VERSION 0
+
+#define CPPY_VERSION "1.0.0"
+
+#if PY_MAJOR_VERSION >= 3
+#define IS_PY3K
+#endif
