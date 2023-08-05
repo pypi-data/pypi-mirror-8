0f7a29fea3d09a8d72b5365467816317ba5db83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:12 +0000

    Add package unicore-cms/0.2.3/unicore-cms-0.2.3.tar.gz

---
+++ packages/unicore-cms/unicore-cms-0.2.3.tar.gz/unicore-cms-0.2.3/cms/api/utils.py
@@ -0,0 +1,24 @@
+import os
+import pygit2
+
+from unicore_gitmodels import models
+from cms import utils
+
+
+class ApiBase(object):
+
+    def __init__(self, request):
+        self.request = request
+
+    def get_registered_ws(self):
+        repo_path = os.path.join(
+            self.request.registry.settings['git.path'], '.git')
+        repo = pygit2.Repository(repo_path)
+        ws = utils.get_workspace(repo)
+        ws.register_model(models.GitPageModel)
+        ws.register_model(models.GitCategoryModel)
+        return ws
+
+    def get_repo_models(self):
+        ws = self.get_registered_ws()
+        return ws.import_models(models)
