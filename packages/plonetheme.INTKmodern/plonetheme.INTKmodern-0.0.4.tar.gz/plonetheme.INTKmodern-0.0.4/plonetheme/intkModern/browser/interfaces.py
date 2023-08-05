cecd3a5971460d0e58f04454d700da1082d3b912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:00 +0000

    Add package plonetheme.INTKmodern/0.0.4/plonetheme.INTKmodern-0.0.4.tar.gz

---
+++ packages/plonetheme.INTKmodern/plonetheme.INTKmodern-0.0.4.tar.gz/plonetheme.INTKmodern-0.0.4/plonetheme/intkModern/browser/interfaces.py
@@ -0,0 +1,14 @@
+from zope.interface import Interface
+from plone.theme.interfaces import IDefaultPloneLayer
+from zope.viewlet.interfaces import IViewletManager
+from zope import schema
+
+class IMediaTypes(Interface):
+    """
+    Marks all Media types from Products.media*
+    """
+
+class IThemeSpecific(Interface):
+    """Marker interface that defines a Zope 3 browser layer.
+    """
+    
