7b3bd6e70ed0a34a227e846554c3047b53398ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:18 +0000

    Add package ncdr/0.0.3/ncdr-0.0.3.tar.gz

---
+++ packages/ncdr/ncdr-0.0.3.tar.gz/ncdr-0.0.3/setup.py
@@ -0,0 +1,16 @@
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='ncdr',
+    version='0.0.3',
+    packages=find_packages(),
+    license='',
+    long_description=open('README.txt').read(),
+    url='http://www.stoneworksolutions.net',
+    author='StoneworkSolutions',
+    author_email='desarrollo@stoneworksolutions.net',
+    package_data={'': ['*.html']},
+    include_package_data=True,
+    install_requires=[],
+    )
