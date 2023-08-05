c7461f6133781dbbeaeba48c8023daa5c935ef8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:27 +0000

    Add package django-cachalot/0.5.0/django-cachalot-0.5.0.tar.gz

---
+++ packages/django-cachalot/django-cachalot-0.5.0.tar.gz/django-cachalot-0.5.0/README.rst
@@ -0,0 +1,169 @@
+Django-cachalot
+===============
+
+Caches your Django ORM queries and automatically invalidates them.
+
+.. image:: https://raw.github.com/BertrandBordage/django-cachalot/master/django-cachalot.jpg
+
+Project status:
+
+**Currently in beta, do not use in production**
+
+.. image:: https://travis-ci.org/BertrandBordage/django-cachalot.png
+   :target: https://travis-ci.org/BertrandBordage/django-cachalot
+
+.. image:: https://coveralls.io/repos/BertrandBordage/django-cachalot/badge.png?branch=master
+   :target: https://coveralls.io/r/BertrandBordage/django-cachalot?branch=master
+
+
+Quick start
+-----------
+
+Requirements
+............
+
+- Django 1.6 or 1.7
+- Python 2.6, 2.7, 3.2, 3.3, or 3.4
+- `locmem <https://docs.djangoproject.com/en/1.7/topics/cache/#local-memory-caching>`_
+  or `django-redis <https://github.com/niwibe/django-redis>`_
+  (memcached coming soon)
+- SQLite, PostgreSQL or MySQL (it should work with Oracle,
+  but I don’t have 17.5k$ to test)
+
+Usage
+.....
+
+#. ``pip install django-cachalot``
+#. Add ``'cachalot',`` to your ``INSTALLED_APPS``
+#. Enjoy!
+
+
+Settings
+........
+
+==================== ============= ============================================
+Setting              Default value Description
+==================== ============= ============================================
+``CACHALOT_ENABLED`` ``True``      If set to ``False``, disables SQL caching
+                                   but keeps invalidating to avoid stale cache
+``CACHALOT_CACHE``   ``'default'`` Name of the cache from |CACHES|_ used by
+                                   django-cachalot
+==================== ============= ============================================
+
+.. |CACHES| replace:: ``CACHES``
+.. _CACHES: https://docs.djangoproject.com/en/1.7/ref/settings/#std:setting-CACHES
+
+These settings can be changed whenever you want.
+You have to use ``cachalot_settings`` as a context manager, a decorator,
+or simply by changing its attributes:
+
+.. code:: python
+
+    from cachalot.settings import cachalot_settings
+
+    with cachalot_settings(CACHALOT_ENABLED=False):
+        # SQL queries are not cached in this block
+
+    @cachalot_settings(CACHALOT_CACHE='another_cache')
+    def your_function():
+        # What’s in this function uses another cache
+
+    # Globally disables SQL caching until you set it back to True
+    cachalot_settings.CACHALOT_ENABLED = False
+
+In tests, you can use
+`Django’s testing tools <https://docs.djangoproject.com/en/1.7/topics/testing/tools/#overriding-settings>`_
+as well as ``cachalot_settings``.  They will both work the same.
+
+
+Limits
+------
+
+Django-cachalot doesn’t cache queries it can’t reliably invalidate.
+If a SQL query or a part of it is written in pure SQL, it won’t be cached.
+
+That’s why ``QuerySet.extra`` with ``select`` or ``where`` arguments,
+``Model.objects.raw(…)``, & ``cursor.execute(…)`` queries are not cached.
+
+
+Bug reports, questions, discussion, new features
+------------------------------------------------
+
+- If you spotted **a bug**, please file a precise bug report
+  `on GitHub <https://github.com/BertrandBordage/django-cachalot/issues>`_
+- If you have **a question** on how django-cachalot works or to **simply
+  discuss**, `go to our Google group
+  <https://groups.google.com/forum/#!forum/django-cachalot>`_.
+- If you want **to add a feature**:
+
+  - if you have an idea on how to implement it, you can fork the project
+    and send a pull request, but **please open an issue first**, because
+    someone else could already be working on it
+  - if you’re sure that it’s a must-have feature, open an issue
+  - if it’s just a vague idea, please ask on google groups before
+
+
+How django-cachalot works
+-------------------------
+
+**(If you don’t care/understand, just pretend it’s magic)**
+
+Reverse engineering
+...................
+
+It’s a lot of Django reverse engineering combined with a strong test suite.
+Such a test suite is crucial for a reverse engineering project.
+If some important part of Django changes and breaks the expected behaviour,
+you can be sure that the test suite will fail.
+
+Monkey patching
+...............
+
+django-cachalot modifies Django in place during execution to add a caching tool
+just before SQL queries are executed.
+We detect which cache keys must be removed when some data
+is created/changed/deleted on the database.
+
+
+What still needs to be done
+---------------------------
+
+For version 1.0
+...............
+
+- Find out if it’s thread-safe and test it
+- Write tests for `multi-table inheritance <https://docs.djangoproject.com/en/1.7/topics/db/models/#multi-table-inheritance>`_
+- Add memcached support
+- Handle multiple databases
+- Add invalidation on migrations in Django 1.7 (& South?)
+
+In a more distant future
+........................
+
+- Add a setting to choose if we cache ``QuerySet.order_by('?')``
+- Cache ``QuerySet.extra`` if none of
+  ``set(connection.introspection.table_names())
+  - set(connection.introspection.django_table_names())``
+  is found in the extra ``select`` and ``where`` queries
+- Add a setting to disable caching on ``QuerySet.extra`` when it has ``select``
+  or ``where`` rules because we can’t reliably detect other databases (and
+  meta databases like ``information_schema``) on every database backend
+- Maybe parse ``QuerySet.extra`` with ``select`` or ``where`` arguments
+  in order to find which tables are implied, and therefore be able
+  to cache them
+
+
+Legacy
+------
+
+This work is highly inspired of
+`johnny-cache <https://github.com/jmoiron/johnny-cache>`_, another easy-to-use
+ORM caching tool!  It’s working with Django <= 1.5.
+I used it in production during 3 years, it’s an excellent module!
+
+Unfortunately, we failed to make it migrate to Django 1.6 (I was involved).
+It was mostly because of the transaction system that was entirely refactored.
+
+I also noticed a few advanced invalidation issues when using ``QuerySet.extra``
+and some complex cases implying multi-table inheritance
+and related ``ManyToManyField``.
---
+++ packages/django-cachalot/django-cachalot-0.5.0.tar.gz/django-cachalot-0.5.0/cachalot/monkey_patch.py
@@ -0,0 +1,274 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+from collections import defaultdict, Iterable
+import re
+
+from django.conf import settings
+# TODO: Replace with caches[CACHALOT_CACHE] when we drop Django 1.6 support.
+from django.core.cache import get_cache as get_django_cache
+from django.db import connection
+from django.db.models.query import EmptyResultSet
+from django.db.models.sql.compiler import (
+    SQLCompiler, SQLAggregateCompiler, SQLDateCompiler, SQLDateTimeCompiler,
+    SQLInsertCompiler, SQLUpdateCompiler, SQLDeleteCompiler)
+from django.db.models.sql.where import ExtraWhere
+from django.db.transaction import Atomic
+
+from .settings import cachalot_settings
+
+
+COMPILERS = (SQLCompiler,
+             SQLAggregateCompiler, SQLDateCompiler, SQLDateTimeCompiler,
+             SQLInsertCompiler, SQLUpdateCompiler, SQLDeleteCompiler)
+WRITE_COMPILERS = (SQLInsertCompiler, SQLUpdateCompiler, SQLDeleteCompiler)
+READ_COMPILERS = [c for c in COMPILERS if c not in WRITE_COMPILERS]
+
+
+PATCHED = False
+MISS_VALUE = '[[Missing cache key]]'
+
+
+def _get_tables(query):
+    """
+    Returns a ``set`` of all database table names used by ``query``.
+    """
+    tables = set(query.tables)
+    tables.add(query.model._meta.db_table)
+    return tables
+
+
+def _get_table_cache_key(table):
+    return '%s_queries' % table
+
+
+def _get_tables_cache_keys(query):
+    return [_get_table_cache_key(t) for t in _get_tables(query)]
+
+
+def _update_tables_queries(cache, query, cache_key):
+    tables_cache_keys = _get_tables_cache_keys(query)
+    tables_queries = cache.get_many(tables_cache_keys)
+    for k in tables_cache_keys:
+        queries = tables_queries.get(k, [])
+        queries.append(cache_key)
+        tables_queries[k] = queries
+    cache.set_many(tables_queries)
+
+
+def _invalidate_tables_cache_keys(cache, tables_cache_keys):
+    tables_queries = cache.get_many(tables_cache_keys)
+    queries = [q for k in tables_cache_keys for q in tables_queries.get(k, [])]
+    cache.delete_many(queries + tables_cache_keys)
+
+
+def _invalidate_tables(cache, query):
+    tables_cache_keys = _get_tables_cache_keys(query)
+    _invalidate_tables_cache_keys(cache, tables_cache_keys)
+
+
+def clear_cache(cache):
+    tables = connection.introspection.table_names()
+    tables_cache_keys = [_get_table_cache_key(t) for t in  tables]
+    _invalidate_tables_cache_keys(cache, tables_cache_keys)
+
+
+def clear_all_caches():
+    for cache in settings.CACHES:
+        clear_cache(get_django_cache(cache))
+
+
+COLUMN_RE = re.compile(r'^"(?P<table>[\w_]+)"\."(?P<column>[\w_]+)"$')
+
+
+def _has_extra_select_or_where(query):
+    """
+    Returns True if ``query`` contains a ``QuerySet.extra`` with ``select``
+    or ``where`` arguments.
+
+    We also have to check for ``prefetch_related``, as it internally uses a
+    ``QuerySet.extra(select={'_prefetch_related_val_…': '"table"."column"'})``.
+
+    For more details on how prefetch_related uses ``QuerySet.extra``, see:
+    https://github.com/django/django/blob/1.6.7/django/db/models/fields/related.py#L553-L577
+    """
+
+    # Checks if there’s an extra where
+    if any(isinstance(child, ExtraWhere) for child in query.where.children):
+        return True
+
+    # Checks if there’s an extra select
+    if query.extra_select and query.extra_select_mask is None:
+        tables = _get_tables(query)
+        # Checks if extra subqueries are something else than one or several
+        # ``prefetch_related``.
+        for subquery, params in query.extra_select.values():
+            match = COLUMN_RE.match(subquery)
+            return match is None or match.group('table') not in tables
+    return False
+
+
+NESTED_CACHES = defaultdict(list)
+
+
+class AtomicCache(dict):
+    def __init__(self):
+        super(AtomicCache, self).__init__()
+        self.parent_cache = get_cache()
+        self.to_be_deleted = set()
+
+    def get(self, k, default=None):
+        if k in self.to_be_deleted:
+            return default
+        if k in self:
+            return self[k]
+        return self.parent_cache.get(k, default)
+
+    def set(self, k, v):
+        if k in self.to_be_deleted:
+            self.to_be_deleted.remove(k)
+        self[k] = v
+
+    def delete(self, k):
+        self.to_be_deleted.add(k)
+
+    def get_many(self, keys):
+        data = {}
+        for k in keys:
+            v = self.get(k, MISS_VALUE)
+            if v != MISS_VALUE:
+                data[k] = v
+        return data
+
+    def set_many(self, data):
+        self.to_be_deleted.difference_update(set(data))
+        self.update(data)
+
+    def delete_many(self, keys):
+        self.to_be_deleted.update(keys)
+
+    def commit(self):
+        self.parent_cache.set_many(self)
+        self.parent_cache.delete_many(self.to_be_deleted)
+
+
+def get_cache():
+    cache_name = cachalot_settings.CACHALOT_CACHE
+    nested_caches = NESTED_CACHES[cache_name]
+    if nested_caches:
+        return nested_caches[-1]
+    return get_django_cache(cache_name)
+
+
+def _patch_orm_read():
+    def patch_execute_sql(original):
+        def inner(compiler, *args, **kwargs):
+            if not cachalot_settings.CACHALOT_ENABLED \
+                    or isinstance(compiler, WRITE_COMPILERS):
+                return original(compiler, *args, **kwargs)
+
+            query = compiler.query
+
+            if _has_extra_select_or_where(query):
+                return original(compiler, *args, **kwargs)
+
+            try:
+                cache_key = compiler.as_sql()
+            except EmptyResultSet:
+                return original(compiler, *args, **kwargs)
+
+            cache = get_cache()
+            result = cache.get(cache_key, MISS_VALUE)
+
+            if result == MISS_VALUE:
+                result = original(compiler, *args, **kwargs)
+                if isinstance(result, Iterable) \
+                        and not isinstance(result, (tuple, list)):
+                    result = list(result)
+
+                _update_tables_queries(cache, query, cache_key)
+
+                cache.set(cache_key, result)
+
+            return result
+
+        inner.original = original
+        return inner
+
+    for compiler in READ_COMPILERS:
+        compiler.execute_sql = patch_execute_sql(compiler.execute_sql)
+
+
+def _patch_orm_write():
+    def patch_execute_sql(original):
+        def inner(compiler, *args, **kwargs):
+            _invalidate_tables(get_cache(), compiler.query)
+            return original(compiler, *args, **kwargs)
+
+        inner.original = original
+        return inner
+
+    for compiler in WRITE_COMPILERS:
+        compiler.execute_sql = patch_execute_sql(compiler.execute_sql)
+
+
+def _patch_atomic():
+    def patch_enter(original):
+        def inner(self):
+            nested_caches = NESTED_CACHES[cachalot_settings.CACHALOT_CACHE]
+            nested_caches.append(AtomicCache())
+            original(self)
+
+        inner.original = original
+        return inner
+
+    def patch_exit(original):
+        def inner(self, exc_type, exc_value, traceback):
+            nested_caches = NESTED_CACHES[cachalot_settings.CACHALOT_CACHE]
+            atomic_cache = nested_caches.pop()
+            if exc_type is None and not connection.needs_rollback:
+                atomic_cache.commit()
+
+            original(self, exc_type, exc_value, traceback)
+
+        inner.original = original
+        return inner
+
+    Atomic.__enter__ = patch_enter(Atomic.__enter__)
+    Atomic.__exit__ = patch_exit(Atomic.__exit__)
+
+
+def _patch_test_db():
+    def patch_creation(original):
+        def inner(*args, **kwargs):
+            out = original(*args, **kwargs)
+            clear_all_caches()
+            return out
+
+        inner.original = original
+        return inner
+
+    def patch_destruction(original):
+        def inner(*args, **kwargs):
+            clear_all_caches()
+            return original(*args, **kwargs)
+
+        inner.original = original
+        return inner
+
+    creation = connection.creation
+    creation.create_test_db = patch_creation(creation.create_test_db)
+    creation.destroy_test_db = patch_destruction(creation.destroy_test_db)
+
+
+def patch():
+    global PATCHED
+    _patch_test_db()
+    _patch_orm_write()
+    _patch_orm_read()
+    _patch_atomic()
+    PATCHED = True
+
+
+def is_patched():
+    return PATCHED
