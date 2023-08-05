4e4b9d9d9c4fa4381d247ae27a35a3f3630b5067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:00 +0000

    Add package NSLDS/1.4/NSLDS-1.4.tar.gz

---
+++ packages/NSLDS/NSLDS-1.4.tar.gz/NSLDS-1.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='NSLDS',
+        version='1.4',
+        description='Python wrapper to National Student Loan Data System',
+        author='Derek Kaknes',
+        author_email='derek.kaknes@gmail.com',
+        url='TBD',
+        py_modules=['nslds', 'json_parser', 'null_parser'],
+        install_requires=['requests', 'lxml'],
+        )
