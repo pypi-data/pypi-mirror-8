cd86bfa06fbdfdb385ee1ab7ef12bb845338ced2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:22 +0000

    Add package django-cms-named-menus/0.1.4/django-cms-named-menus-0.1.4.tar.gz

---
+++ packages/django-cms-named-menus/django-cms-named-menus-0.1.4.tar.gz/django-cms-named-menus-0.1.4/cms_named_menus/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from autoslug.fields import AutoSlugField
+from jsonfield import JSONField
+import collections
+
+
+class CMSNamedMenu(models.Model):
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(always_update=True, populate_from='name')
+    pages = JSONField(blank=True, null=True,
+                      load_kwargs={
+                          'object_pairs_hook': collections.OrderedDict
+                      },
+                      default=[])
+
+    def __unicode__(self):
+        return self.name
