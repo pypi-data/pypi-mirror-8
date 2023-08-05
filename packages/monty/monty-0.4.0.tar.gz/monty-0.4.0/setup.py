fafa9b8ebeadff3b390d0fbf04ed6156c0f278b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:44 +0000

    Add package monty/0.4.0/monty-0.4.0.tar.gz

---
+++ packages/monty/monty-0.4.0.tar.gz/monty-0.4.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_desc = f.read()
+
+setup(
+    name="monty",
+    packages=find_packages(),
+    version="0.4.0",
+    install_requires=[],
+    extras_require={"yaml": ["pyyaml>=3.1"],},
+    package_data={},
+    author="Shyue Ping Ong",
+    author_email="ongsp@ucsd.edu",
+    maintainer="Shyue Ping Ong",
+    url="https://github.com/materialsvirtuallab/monty",
+    license="MIT",
+    description="Monty is the missing complement to Python.",
+    long_description=long_desc,
+    keywords=["monty"],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
