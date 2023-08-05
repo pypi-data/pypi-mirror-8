dcda09e3e547d21b5dee0044afcafc3dd6ce4ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:45 +0000

    Add package ArangoPy/0.4.1/ArangoPy-0.4.1.tar.gz

---
+++ packages/ArangoPy/ArangoPy-0.4.1.tar.gz/ArangoPy-0.4.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ArangoPy',
+    version= '0.4.1',
+    packages=find_packages(),
+    requires=[
+        'slumber',
+    ],
+    url='https://github.com/saeschdivara/ArangoPy',
+    license='MIT',
+    author='saskyrardisaskyr',
+    author_email='saeschdivara@gmail.com',
+    description='Driver for ArangoDB'
+)
