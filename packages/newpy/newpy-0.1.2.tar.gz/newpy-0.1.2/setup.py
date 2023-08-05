3f128e7b3fd231c86fd78f23a9bd4625696e3c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:39 +0000

    Add package newpy/0.1.2/newpy-0.1.2.tar.gz

---
+++ packages/newpy/newpy-0.1.2.tar.gz/newpy-0.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(
+        name="newpy",
+        version="0.1.2",
+        description="Quickly and easily create a new python project",
+        url="https://bitbucket.org/edwardonsoftware/newpy",
+        author="Edward",
+        author_email="edwardprentice@gmail.com",
+        keywords="tutorial new example skeleton",
+        license="MIT",
+        packages=["newpy"],
+        zip_safe=False,
+        include_package_data=True,
+        data_files=[("resources", ["resources/Logger.py", "resources/simple_skeleton.py", "skeleton_with_logger.py"])],
+        entry_points={
+            'console_scripts': [
+                'newpy=newpy:main',
+            ],
+        },
+        classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+
+            'License :: OSI Approved :: MIT License',
+
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+        ],
+)
