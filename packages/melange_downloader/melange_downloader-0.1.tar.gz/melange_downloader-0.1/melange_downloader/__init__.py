d59106c507df3bf1107e27be2595bf9ecdc155ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package melange_downloader/0.1/melange_downloader-0.1.tar.gz

---
+++ packages/melange_downloader/melange_downloader-0.1.tar.gz/melange_downloader-0.1/melange_downloader/__init__.py
@@ -0,0 +1,1 @@
+from .melange import download_testimonials
---
+++ packages/melange_downloader/melange_downloader-0.1.tar.gz/melange_downloader-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='melange_downloader',
+		version='0.1',
+		description='Download testimonials in a sorted way from BITS Melange website.',
+		url='http://github.com/bhavul/testimonials-downloader',
+		author='Bhavul Gauri',
+		author_email='bhavul93@gmail.com',
+		license='MIT',
+		packages=['melange_downloader'],
+		zip_safe=False)
