e7c3147f730f0f9b178c5189891a6e57fb6c69fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package sender/0.2/sender-0.2.tar.gz

---
+++ packages/sender/sender-0.2.tar.gz/sender-0.2/setup.py
@@ -0,0 +1,55 @@
+"""
+Sender
+------
+
+Sender is easy
+``````````````
+
+.. code:: python
+    
+    from sender import Mail
+
+    mail = Mail()
+
+    mail.send_message("Hello", fromaddr="from@example.com",
+                      to="to@example.com", body="Hello world!")
+
+Install
+```````
+
+.. code:: bash
+
+    $ pip install sender
+
+Links
+`````
+
+* `documentation <http://sender.readthedocs.org/>`_
+* `github <https://github.com/fengsp/sender>`_
+* `development version
+  <http://github.com/fengsp/sender/zipball/master#egg=sender-dev>`_
+
+"""
+from setuptools import setup
+
+
+setup(
+    name='sender',
+    version='0.2',
+    url='https://github.com/fengsp/sender',
+    license='BSD',
+    author='Shipeng Feng',
+    author_email='fsp261@gmail.com',
+    description='Python SMTP Client for Humans',
+    long_description=__doc__,
+    py_modules= ['sender',],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
