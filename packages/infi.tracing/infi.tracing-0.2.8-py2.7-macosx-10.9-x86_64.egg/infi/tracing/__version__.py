a06f6df16a283de48a39027098a408e6a42f5e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:41 +0000

    Add package infi.tracing/0.2.8/infi.tracing-0.2.8-py2.7-macosx-10.9-x86_64.egg

---
+++ packages/infi.tracing/infi.tracing-0.2.8-py2.7-macosx-10.9-x86_64.egg/infi/tracing/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.8"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'edf2d363f1f15abdce8dc9a8012f379ad8915df9'
+__git_head_subject__ = 'HOSTDEV-1732'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
