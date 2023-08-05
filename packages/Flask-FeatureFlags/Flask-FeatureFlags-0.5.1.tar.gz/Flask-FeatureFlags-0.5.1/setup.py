a728920d210d3e5d0a7901947fa0941e64dd5f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:27 +0000

    Add package Flask-FeatureFlags/0.5.1/Flask-FeatureFlags-0.5.1.tar.gz

---
+++ packages/Flask-FeatureFlags/Flask-FeatureFlags-0.5.1.tar.gz/Flask-FeatureFlags-0.5.1/setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import setup
+from sys import argv
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+  README = open(os.path.join(here, 'README.md')).read()
+  CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+except:
+  README = ''
+  CHANGES = ''
+
+install_requires = ["Flask"]
+
+if "develop" in argv:
+  install_requires.append('Sphinx')
+  install_requires.append('Sphinx-PyPI-upload')
+
+setup(
+  name='Flask-FeatureFlags',
+  version='0.5.1',
+  url='https://github.com/trustrachel/Flask-FeatureFlags',
+  license='Apache',
+  author='Rachel Sanders',
+  author_email='rachel@trustrachel.com',
+  maintainer='Rachel Sanders',
+  maintainer_email='rachel@trustrachel.com',
+  description='Enable or disable features in Flask apps based on configuration',
+  long_description=README + '\n\n' + CHANGES,
+  zip_safe=False,
+  test_suite="tests",
+  platforms='any',
+  include_package_data=True,
+  packages=['flask_featureflags'],
+  install_requires=[
+    'Flask',
+    ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+  ]
+)
