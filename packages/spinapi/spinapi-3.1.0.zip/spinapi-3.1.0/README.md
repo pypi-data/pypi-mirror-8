143729db8c339e4348a5027f32bf4d224e903c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package spinapi/3.1.0/spinapi-3.1.0.zip

---
+++ packages/spinapi/spinapi-3.1.0.zip/spinapi-3.1.0/README.md
@@ -0,0 +1,12 @@
+# spinapi
+
+Python wrapper around the Spincore PulseBlaster API using ctypes.
+( 
+[view on pypi](https://pypi.python.org/pypi/spinapi/);
+[view on Bitbucket](https://bitbucket.org/cbillington/spinapi)
+)
+
+   * Install `python setup.py install`.
+   * If using Windows, this package assumes that the spincore API is installed on your system.
+   * If using unix, the spincore API shared object is bundled.
+   * For Windows, you should install whichever spincore API has the same bit count - 32 or 64 - for your operating system, not your python installation. If you use 32 bit Python but 64 bit windows, you should still install the 64 bit spincore API. It provides 32 bit dlls as well, which this wrapper will use in that case. Installing the 64 bit spincore API ensures you have the correct USB device drivers Windows will require to talk to the PulseBlaster.
---
+++ packages/spinapi/spinapi-3.1.0.zip/spinapi-3.1.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# To upload a version to PyPI, run:
+#
+#    python setup.py sdist upload
+#
+# If the package is not registered with PyPI yet, do so with:
+#
+# python setup.py register
+
+from distutils.core import setup
+import os
+import platform
+
+VERSION = '3.1.0'
+
+# Auto generate a __version__ package for the package to import
+with open(os.path.join('spinapi', '__version__.py'), 'w') as f:
+    f.write("__version__ = '%s'\n"%VERSION)
+
+arch = platform.architecture()
+
+if arch == ('32bit', 'ELF'):
+    bundled_shared_objects = ['libspinapi.so']
+elif arch == ('64bit', 'ELF'):
+    bundled_shared_objects = ['libspinapi64.so']
+else:
+    bundled_shared_objects = []
+    
+setup(name='spinapi',
+      version=VERSION,
+      description='Python wrapper around the Spincore PulseBlaster API using ctypes.',
+      author='Chris Billington',
+      author_email='chrisjbillington@gmail.com',
+      url='https://bitbucket.org/cbillington/spinapi/',
+      license="BSD",
+      packages=['spinapi'],
+      package_data={'spinapi': bundled_shared_objects}
+     )
