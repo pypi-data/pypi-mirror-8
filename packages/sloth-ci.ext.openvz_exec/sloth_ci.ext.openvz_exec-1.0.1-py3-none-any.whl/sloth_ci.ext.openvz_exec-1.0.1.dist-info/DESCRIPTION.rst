ad3af041870929fb4a78d989ceb3b69b58632b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:55 +0000

    Add package sloth-ci.ext.openvz_exec/1.0.1/sloth_ci.ext.openvz_exec-1.0.1-py3-none-any.whl

---
+++ packages/sloth-ci.ext.openvz_exec/sloth_ci.ext.openvz_exec-1.0.1-py3-none-any.whl/sloth_ci.ext.openvz_exec-1.0.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,13 @@
+OpenVZ executor for Sloth CI app extension that replaces the default executor and runs actions inside a given OpenVZ container.
+
+Config params::
+
+    [openvz_exec]
+    ;Container name.
+    container_name = foo
+    ;Container ID.
+    container_id = 123
+
+If name is provided, ID is ignored. If name is not provided, ID is mandatory.
+
+
---
+++ packages/sloth-ci.ext.openvz_exec/sloth_ci.ext.openvz_exec-1.0.1-py3-none-any.whl/sloth_ci/ext/openvz_exec.py
@@ -0,0 +1,56 @@
+﻿'''OpenVZ executor for Sloth CI app extension that replaces the default executor and runs actions inside a given OpenVZ container.
+
+Config params::
+
+    [openvz_exec]
+    ;Container name.
+    container_name = foo
+    ;Container ID.
+    container_id = 123
+
+If name is provided, ID is ignored. If name is not provided, ID is mandatory.
+'''
+
+
+__title__ = 'sloth-ci.ext.openvz_exec'
+__description__ = 'OpenVZ executor app extension for Sloth CI'
+__version__ = '1.0.1'
+__author__ = 'Konstantin Molchanov'
+__author_email__ = 'moigagoo@live.com'
+__license__ = 'MIT'
+
+
+def extend(cls):
+    class Sloth(cls):
+        def __init__(self, config):
+            super().__init__(config)
+
+            self._openvz_config = self.config['openvz_exec']
+
+        def execute(self, action):
+            '''Execute an action inside an OpenVZ container. The container must exist and be running.
+
+            :param action: action to be executed
+
+            :returns: True if successful, Exception otherwise
+            '''
+
+            try:
+                container_name = self._openvz_config.get('container_name')
+
+                if container_name:
+                    openvz_exec_command = 'vzctl exec %s' % container_name
+
+                else:
+                    container_id = self._openvz_config['container_id']
+                    
+                    openvz_exec_command = 'vzctl exec %d' % container_id
+
+                super().execute(openvz_exec_command + ' ' + action)
+
+                return True
+
+            except Exception:
+                raise
+
+    return Sloth
