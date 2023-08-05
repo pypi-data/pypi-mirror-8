294edc859f2af91b649482870d6a9d8219e2a22b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:59 +0000

    Add package mewa/0.17/mewa-0.17.tar.gz

---
+++ packages/mewa/mewa-0.17.tar.gz/mewa-0.17/setup.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# Authors: Krzysztof Langner klangner@gmail.com
+# LICENSE: BSD3
+
+from distutils.core import setup
+
+setup(
+    name="mewa",
+    version="0.17",
+    description="Client for Mewa server",
+    maintainer="Krzysztof Langner",
+    maintainer_email="klangner@gmail.com",
+    license="BSD3",
+    url='http://github.com/AnthillTech/python-mewa-client',
+    packages=[
+        'mewa',
+    ],
+)
