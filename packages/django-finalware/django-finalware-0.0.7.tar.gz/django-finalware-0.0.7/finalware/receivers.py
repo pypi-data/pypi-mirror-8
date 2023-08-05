e3ca735b22b2dc0ace715f9edde3175449e4dc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:05 +0000

    Add package django-finalware/0.0.7/django-finalware-0.0.7.tar.gz

---
+++ packages/django-finalware/django-finalware-0.0.7.tar.gz/django-finalware-0.0.7/finalware/receivers.py
@@ -0,0 +1,51 @@
+import logging
+
+from django.conf import settings
+from django.db.models import signals
+from django.apps import apps
+
+from .utils import load_site_objects
+from .utils import load_template_tags
+from .utils import create_superuser
+
+
+log = logging.getLogger(__name__)
+
+__all__ = ['pre_migrate_receiver', 'post_migrate_receiver', ]
+
+
+class PreMigrateReceiver(object):
+    """
+    Disable the superuser creation prompt.
+    """
+    def __init__(self):
+        self.call_counter = 0
+
+    def __call__(self, signal, sender, **kwargs):
+        self.call_counter += 1
+        from django.contrib.auth import management
+        if self.call_counter == 1 and hasattr(management, 'create_superuser'):
+            signals.post_migrate.disconnect(
+                management.create_superuser,
+                sender=apps.get_app_config('auth'),
+                dispatch_uid="django.contrib.auth.management.create_superuser"
+            )
+
+pre_migrate_receiver = PreMigrateReceiver()
+
+
+class PostMigrateReceiver(object):
+    """
+    Finalize the website loading.
+    """
+    def __init__(self):
+        self.call_counter = 0
+
+    def __call__(self, signal, sender, **kwargs):
+            self.call_counter += 1
+            if self.call_counter == 1:
+                load_site_objects()
+                if getattr(settings, 'SITE_SUPERUSER_ID', False):
+                    create_superuser()
+
+post_migrate_receiver = PostMigrateReceiver()
---
+++ packages/django-finalware/django-finalware-0.0.7.tar.gz/django-finalware-0.0.7/finalware/utils.py
@@ -0,0 +1,60 @@
+import logging
+from django import template
+from django.conf import settings
+from django.apps import apps
+from django.contrib.sites.models import Site
+from django.utils.translation import ugettext as _
+
+from . import defaults
+
+log = logging.getLogger(__name__)
+
+
+# Setup the available sites for this project
+def load_site_objects():
+    """
+    Load the available Sites for this project.
+    `SITE_ID` in the settings file will decide the `current` site object.
+    """
+    if not apps.is_installed('django.contrib.sites'):
+        return
+    site_info = getattr(defaults, 'SITE_OBJECTS_INFO_DICT')
+    if site_info:
+        for pk in sorted(site_info.keys()):
+            site, created = Site.objects.get_or_create(pk=pk)
+            if site:
+                site.name = site_info[pk]['name']
+                site.domain = site_info[pk]['domain']
+                site.save()
+
+
+def load_template_tags():
+    """
+    Loads template tags found in SITE_TEMPLATE_TAGS_AUTO_LOAD_LIST on startup
+    """
+    for t in defaults.SITE_TEMPLATE_TAGS_AUTO_LOAD_LIST:
+        template.add_to_builtins(t)
+
+
+def create_superuser():
+    """
+    Create or update a superuser.
+    """
+    user_id = getattr(settings, 'SITE_SUPERUSER_ID', '')
+    username = getattr(settings, 'SITE_SUPERUSER_USERNAME', '')
+    email = getattr(settings, 'SITE_SUPERUSER_EMAIL', '')
+    password = getattr(settings, 'SITE_SUPERUSER_PASSWORD', '')
+    if user_id and username and email and password:
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        user, created = User.objects.get_or_create(pk=user_id)
+        if user:
+            user.set_password(password)
+            setattr(user, user.USERNAME_FIELD, username)
+            if hasattr(user, 'email'):
+                user.email = email
+            user.is_staff = True
+            user.is_active = True
+            user.is_superuser = True
+            user.save()
+            log.info(_('Superuser created/updated'))
