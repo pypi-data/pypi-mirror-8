4db0e6329324fa4f5b31093993d8d20a2e187278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:03 +0000

    Add package terminal_velocity/0.1.8/terminal_velocity-0.1.8.tar.gz

---
+++ packages/terminal_velocity/terminal_velocity-0.1.8.tar.gz/terminal_velocity-0.1.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="terminal_velocity",
+    version="0.1.8",
+    author="Sean Hammond",
+    packages=["terminal_velocity"],
+    scripts=["bin/terminal_velocity"],
+    url="http://seanh.github.com/terminal_velocity/",
+    license="GNU General Public License, Version 3",
+    description="A fast note-taking app for the UNIX terminal",
+    long_description=open("README.rst").read(),
+    install_requires=[
+        "urwid==1.1.1",
+        "chardet==2.1.1",
+        ],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        ],
+)
