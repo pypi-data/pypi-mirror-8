a0e57138bd32cd753977ce182f00284a65556df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:15 +0000

    Add package vitalstyles/1.1.4/vitalstyles-1.1.4.tar.gz

---
+++ packages/vitalstyles/vitalstyles-1.1.4.tar.gz/vitalstyles-1.1.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'vitalstyles',
+    description = 'Generate CSS/SASS/LESS documentation with previews using Markdown in comments.',
+    version = '1.1.4',
+    license = 'BSD',
+    author = 'Espen Angell Kristiansen',
+    author_email = 'post@espenak.net',
+    url = 'https://github.com/appressoas/vitalstyles',
+    packages=find_packages(exclude=['manage']),
+    install_requires = [
+        'setuptools',
+        'Markdown',
+        'Jinja2',
+        'pygments',
+    ],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+          'vitalstyles-cli = vitalstyles.cli:cli',
+        ],
+    },
+)
