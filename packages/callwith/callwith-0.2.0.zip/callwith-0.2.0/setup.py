0e7ee689d915cb2b7c57816fa96ac85802245a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package callwith/0.2.0/callwith-0.2.0.zip

---
+++ packages/callwith/callwith-0.2.0.zip/callwith-0.2.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+setup(
+    name = 'callwith',
+    version = '0.2.0',
+    py_modules = ['callwith'],
+    description = 'Call function with parameters',
+    long_description = open('README').read(),
+    author = 'huyx',
+    author_email = 'ycyuxin@gmail.com',
+    url = 'https://github.com/huyx/callwith',
+    keywords = ['functools', 'function', 'call'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+    )
