4fdfb2721e92c05a49109536730432a9c9e49ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:42 +0000

    Add package Beeswarm/0.5.3/Beeswarm-0.5.3.tar.gz

---
+++ packages/Beeswarm/Beeswarm-0.5.3.tar.gz/Beeswarm-0.5.3/beeswarm/shared/message_enum.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class Messages(Enum):
+    STOP = 'STOP'
+    START = 'START'
+    CONFIG = 'CONFIG'
+    # dump of all configuration elements known to the sender
+    CONFIG_FULL = 'CONFIG_FULL'
+    # mapping between clients, honeypots, capabilities and bait users
+    CONFIG_ARCHITECTURE = 'CONFIG_ARCHITECTURE'
+    BROADCAST = 'BROADCAST'
+
+    OK = 'OK'
+    FAIL = 'FAIL'
+    # KEY DRONE_ID DRONE_PRIVATE_KEY
+    KEY = 'KEY'
+    # CERT DRONE_ID DRONE_CERT
+    CERT = 'CERT'
+    SESSION_HONEYPOT = 'SESSION_HONEYPOT'
+    SESSION_CLIENT = 'SESSION_CLIENT'
+    SET_CONFIG_ITEM = 'SET'
+    GET_CONFIG_ITEM = 'GET'
+    GEN_ZMQ_KEYS = 'GEN_ZMQ_KEYS'
+    PING = 'PING'
+    PONG = 'PING'
+    IP = 'IP'
+    DRONE_CONFIG = 'DRONE_CONFIG'
+    DRONE_CONFIG_CHANGED = 'DRONE_CONFIG_CHANGED'
+    # ID USERNAME PASSWORD
+    BAIT_USER_ADD = 'BAIT_USER_ADD'
+    BAIT_USER_DELETE = 'BAIT_USER_DELETE'
+    DRONE_DELETE = 'DRONE_DELETE'
