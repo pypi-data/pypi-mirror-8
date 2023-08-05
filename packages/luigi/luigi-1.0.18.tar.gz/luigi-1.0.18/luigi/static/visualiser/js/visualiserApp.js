9d0c189decbbaa2404eb293ca3d0580d56aad3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:40 +0000

    Add package luigi/1.0.18/luigi-1.0.18.tar.gz

---
+++ packages/luigi/luigi-1.0.18.tar.gz/luigi-1.0.18/luigi/static/visualiser/js/visualiserApp.js
@@ -0,0 +1,202 @@
+function visualiserApp(luigi) {
+    var templates = {};
+    var invertDependencies = false;
+
+    function loadTemplates() {
+        $("script[type='text/template']").each(function(i, element) {
+            var name = $(element).attr("name");
+            var content = $(element).text();
+            templates[name] = content;
+        });
+    }
+
+    function renderTemplate(templateName, dataObject) {
+        return $("<div>").html(Mustache.render(templates[templateName], dataObject));
+    }
+
+
+    function formatTime(dateObject) {
+        return dateObject.getHours() + ":" + dateObject.getMinutes() + ":" + dateObject.getSeconds();
+    }
+
+    function taskToDisplayTask(task) {
+        var taskIdParts = /([A-Za-z0-9_]*)\((.*)\)/.exec(task.taskId);
+        var taskName = taskIdParts[1];
+        var taskParams = taskIdParts[2];
+        var displayTime = new Date(Math.floor(task.start_time*1000)).toLocaleString();
+        if (task.status == "RUNNING" && "time_running" in task) {
+            var current_time = new Date().getTime()
+            var minutes_running = Math.round((current_time - task.time_running * 1000) / 1000 / 60)
+            displayTime += " | " + minutes_running + " minutes"
+        }
+        return {
+            taskId: task.taskId,
+            taskName: taskName,
+            taskParams: taskParams,
+            displayTime: displayTime,
+            displayTimestamp : task.start_time,
+            trackingUrl: task.trackingUrl,
+            status: task.status,
+            graph: (task.status == "PENDING" || task.status == "RUNNING" || task.status == "DONE"),
+            error: task.status == "FAILED"
+        };
+    }
+
+    function indexByProperty(tasks, fieldName) {
+        indexedTasks = {};
+        $.each(tasks, function(i, task) {
+            if (!indexedTasks.hasOwnProperty(task[fieldName])) {
+                indexedTasks[task[fieldName]] = [];
+            }
+            indexedTasks[task[fieldName]].push(task);
+        });
+        return indexedTasks;
+    }
+
+    function entryList(object) {
+        var list = [];
+        $.each(object, function(key, value) {
+            list.push({key: key, value:value});
+        });
+        return list;
+    }
+
+    function renderTasks(tasks) {
+        var displayTasks = tasks.map(taskToDisplayTask);
+        displayTasks.sort(function(a,b) { return b.displayTimestamp - a.displayTimestamp; });
+        var tasksByFamily = entryList(indexByProperty(displayTasks, "taskName"));
+        tasksByFamily.sort(function(a,b) { return a.key.localeCompare(b.key); });
+        return renderTemplate("rowTemplate", {tasks: tasksByFamily});
+    }
+
+    function switchTab(tabId) {
+        $(".tabButton").parent().removeClass("active");
+        $(".tab-pane").removeClass("active");
+        $("#"+tabId).addClass("active");
+        $(".tabButton[data-tab="+tabId+"]").parent().addClass("active");
+    }
+
+    function showErrorTrace(error) {
+        $("#errorModal").empty().append(renderTemplate("errorTemplate", error));
+        $("#errorModal").modal({});
+    }
+
+    function processHashChange() {
+        var hash = location.hash;
+        if (hash) {
+            var taskId = hash.substr(1);
+            $("#graphContainer").hide();
+            $("#graphPlaceholder svg").empty();
+            $("#searchError").empty();
+            $("#searchError").removeClass();
+            if (taskId != "g") {
+                depGraphCallback = function(dependencyGraph) {
+                    $("#graphPlaceholder svg").empty();
+                    $("#searchError").empty();
+                    $("#searchError").removeClass();
+                    if(dependencyGraph.length > 0) {
+                      $("#dependencyTitle").text(taskId);
+                      $("#graphPlaceholder").get(0).graph.updateData(dependencyGraph);
+                      $("#graphContainer").show();
+                      bindGraphEvents();
+                    } else {
+                      $("#searchError").addClass("alert alert-error")
+                      $("#searchError").append("Couldn't find task " + taskId)
+                    }
+                }
+                if (invertDependencies) {
+                    luigi.getInverseDependencyGraph(taskId, depGraphCallback);
+                } else {
+                    luigi.getDependencyGraph(taskId, depGraphCallback);
+                }
+            }
+            switchTab("dependencyGraph");
+        } else {
+            switchTab("taskList");
+        }
+    }
+
+    function bindGraphEvents() {
+        $(".graph-node-a").click(function(event) {
+            var taskId = $(this).attr("data-task-id");
+            var status = $(this).attr("data-task-status");
+            if (status=="FAILED") {
+                event.preventDefault();
+                luigi.getErrorTrace(taskId, function(error) {
+                   showErrorTrace(error);
+                });
+            }
+        });
+    }
+
+    function bindListEvents() {
+        $(window).on('hashchange', processHashChange);
+        $("#invertCheckbox").click(function() {
+            invertDependencies = this.checked;
+            processHashChange();
+        });
+        $("a[href=#list]").click(function() { location.hash=""; });
+        $("#loadTaskForm").submit(function(event) {
+            event.preventDefault();
+            location.hash = $(this).find("input").val();
+        });
+    }
+
+    function bindTaskEvents(id, expand) {
+        $(id + " [data-action=expandTaskRows]").click(function(event) {
+            event.preventDefault();
+            var icon = $(this).find("span");
+            if (icon.hasClass("icon-plus")) {
+                icon.removeClass("icon-plus");
+                icon.addClass("icon-minus");
+            } else {
+                icon.removeClass("icon-minus");
+                icon.addClass("icon-plus");
+            }
+            var taskRows = $(this).closest(".taskFamily").find(".taskRows").slideToggle("fast");
+        });
+        if (expand) {
+            $(id + " [data-action=expandTaskRows]").click();
+        }
+        $(id + " .error-trace-button").click(function() {
+            luigi.getErrorTrace($(this).attr("data-task-id"), function(error) {
+               showErrorTrace(error);
+            });
+        });
+    }
+
+    $(document).ready(function() {
+        loadTemplates();
+
+        luigi.getRunningTaskList(function(runningTasks) {
+            $("#runningTasks").append(renderTasks(runningTasks));
+            bindTaskEvents("#runningTasks", true);
+        });
+
+        luigi.getFailedTaskList(function(failedTasks) {
+            $("#failedTasks").append(renderTasks(failedTasks));
+            bindTaskEvents("#failedTasks");
+        });
+
+        luigi.getUpstreamFailedTaskList(function(upstreamFailedTasks) {
+            $("#upstreamFailedTasks").append(renderTasks(upstreamFailedTasks));
+            bindTaskEvents("#upstreamFailedTasks");
+        });
+
+        luigi.getPendingTaskList(function(pendingTasks) {
+            $("#pendingTasks").append(renderTasks(pendingTasks));
+            bindTaskEvents("#pendingTasks");
+        });
+
+        luigi.getDoneTaskList(function(doneTasks) {
+            $("#doneTasks").append(renderTasks(doneTasks));
+            bindTaskEvents("#doneTasks");
+        });
+
+        bindListEvents();
+
+        var graph = new Graph.DependencyGraph($("#graphPlaceholder")[0]);
+        $("#graphPlaceholder")[0].graph = graph;
+        processHashChange();
+    });
+}
---
+++ packages/luigi/luigi-1.0.18.tar.gz/luigi-1.0.18/test/central_planner_test.py
@@ -0,0 +1,194 @@
+# Copyright (c) 2012 Spotify AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import time
+from luigi.scheduler import CentralPlannerScheduler, DONE, FAILED
+import unittest
+import luigi.notifications
+luigi.notifications.DEBUG = True
+WORKER = 'myworker'
+
+
+class CentralPlannerTest(unittest.TestCase):
+    def setUp(self):
+        self.sch = CentralPlannerScheduler(retry_delay=100, remove_delay=1000, worker_disconnect_delay=10)
+        self.time = time.time
+
+    def tearDown(self):
+        if time.time != self.time:
+            time.time = self.time
+
+    def setTime(self, t):
+        time.time = lambda: t
+
+    def test_dep(self):
+        self.sch.add_task(WORKER, 'B', deps=('A',))
+        self.sch.add_task(WORKER, 'A')
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], 'A')
+        self.sch.add_task(WORKER, 'A', status=DONE)
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], 'B')
+        self.sch.add_task(WORKER, 'B', status=DONE)
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], None)
+
+    def test_failed_dep(self):
+        self.sch.add_task(WORKER, 'B', deps=('A',))
+        self.sch.add_task(WORKER, 'A')
+
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], 'A')
+        self.sch.add_task(WORKER, 'A', status=FAILED)
+
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], None)  # can still wait and retry: TODO: do we want this?
+        self.sch.add_task(WORKER, 'A', DONE)
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], 'B')
+        self.sch.add_task(WORKER, 'B', DONE)
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], None)
+
+    def test_broken_dep(self):
+        self.sch.add_task(WORKER, 'B', deps=('A',))
+        self.sch.add_task(WORKER, 'A', runnable=False)
+
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], None)  # can still wait and retry: TODO: do we want this?
+        self.sch.add_task(WORKER, 'A', DONE)
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], 'B')
+        self.sch.add_task(WORKER, 'B', DONE)
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], None)
+
+    def test_two_workers(self):
+        # Worker X wants to build A -> B
+        # Worker Y wants to build A -> C
+        self.sch.add_task(worker='X', task_id='A')
+        self.sch.add_task(worker='Y', task_id='A')
+        self.sch.add_task(task_id='B', deps=('A',), worker='X')
+        self.sch.add_task(task_id='C', deps=('A',), worker='Y')
+
+        self.assertEqual(self.sch.get_work(worker='X')['task_id'], 'A')
+        self.assertEqual(self.sch.get_work(worker='Y')['task_id'], None)  # Worker Y is pending on A to be done
+        self.sch.add_task(worker='X', task_id='A', status=DONE)
+        self.assertEqual(self.sch.get_work(worker='Y')['task_id'], 'C')
+        self.assertEqual(self.sch.get_work(worker='X')['task_id'], 'B')
+
+    def test_retry(self):
+        # Try to build A but fails, will retry after 100s
+        self.setTime(0)
+        self.sch.add_task(WORKER, 'A')
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], 'A')
+        self.sch.add_task(WORKER, 'A', FAILED)
+        for t in xrange(100):
+            self.setTime(t)
+            self.assertEqual(self.sch.get_work(WORKER)['task_id'], None)
+            self.sch.ping(WORKER)
+            if t % 10 == 0:
+                self.sch.prune()
+
+        self.setTime(101)
+        self.sch.prune()
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], 'A')
+
+    def test_disconnect_running(self):
+        # X and Y wants to run A.
+        # X starts but does not report back. Y does.
+        # After some timeout, Y will build it instead
+        self.setTime(0)
+        self.sch.add_task(task_id='A', worker='X')
+        self.sch.add_task(task_id='A', worker='Y')
+        self.assertEqual(self.sch.get_work(worker='X')['task_id'], 'A')
+        for t in xrange(200):
+            self.setTime(t)
+            self.sch.ping(worker='Y')
+            if t % 10 == 0:
+                self.sch.prune()
+
+        self.assertEqual(self.sch.get_work(worker='Y')['task_id'], 'A')
+
+    def test_remove_dep(self):
+        # X schedules A -> B, A is broken
+        # Y schedules C -> B: this should remove A as a dep of B
+        self.sch.add_task(task_id='A', worker='X', runnable=False)
+        self.sch.add_task(task_id='B', deps=('A',), worker='X')
+
+        # X can't build anything
+        self.assertEqual(self.sch.get_work(worker='X')['task_id'], None)
+
+        self.sch.add_task(task_id='B', deps=('C',), worker='Y')  # should reset dependencies for A
+        self.sch.add_task(task_id='C', worker='Y', status=DONE)
+
+        self.assertEqual(self.sch.get_work(worker='Y')['task_id'], 'B')
+
+    def test_timeout(self):
+        # A bug that was earlier present when restarting the same flow
+        self.setTime(0)
+        self.sch.add_task(task_id='A', worker='X')
+        self.assertEqual(self.sch.get_work(worker='X')['task_id'], 'A')
+        self.setTime(10000)
+        self.sch.add_task(task_id='A', worker='Y')  # Will timeout X but not schedule A for removal
+        for i in xrange(2000):
+            self.setTime(10000 + i)
+            self.sch.ping(worker='Y')
+        self.sch.add_task(task_id='A', status=DONE, worker='Y')  # This used to raise an exception since A was removed
+
+    def test_disallowed_state_changes(self):
+        # Test that we can not schedule an already running task
+        t = 'A'
+        self.sch.add_task(task_id=t, worker='X')
+        self.assertEqual(self.sch.get_work(worker='X')['task_id'], t)
+        self.sch.add_task(task_id=t, worker='Y')
+        self.assertEqual(self.sch.get_work(worker='Y')['task_id'], None)
+
+    def test_two_worker_info(self):
+        # Make sure the scheduler returns info that some other worker is running task A
+        self.sch.add_task(worker='X', task_id='A')
+        self.sch.add_task(worker='Y', task_id='A')
+
+        self.assertEqual(self.sch.get_work(worker='X')['task_id'], 'A')
+        r = self.sch.get_work(worker='Y')
+        self.assertEqual(r['task_id'], None)  # Worker Y is pending on A to be done
+        s = r['running_tasks'][0]
+        self.assertEqual(s['task_id'], 'A')
+        self.assertEqual(s['worker'], 'X')
+
+    def test_priorities(self):
+        self.sch.add_task(WORKER, 'A', priority=10)
+        self.sch.add_task(WORKER, 'B', priority=5)
+        self.sch.add_task(WORKER, 'C', priority=15)
+        self.sch.add_task(WORKER, 'D', priority=9)
+        for expected_id in ['C', 'A', 'D', 'B']:
+            self.assertEqual(self.sch.get_work(WORKER)['task_id'], expected_id)
+            self.sch.add_task(WORKER, expected_id, status=DONE)
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], None)
+
+    def test_priorities_default_and_negative(self):
+        self.sch.add_task(WORKER, 'A', priority=10)
+        self.sch.add_task(WORKER, 'B')
+        self.sch.add_task(WORKER, 'C', priority=15)
+        self.sch.add_task(WORKER, 'D', priority=-20)
+        self.sch.add_task(WORKER, 'E', priority=1)
+        for expected_id in ['C', 'A', 'E', 'B', 'D']:
+            self.assertEqual(self.sch.get_work(WORKER)['task_id'], expected_id)
+            self.sch.add_task(WORKER, expected_id, status=DONE)
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], None)
+
+    def test_priorities_and_dependencies(self):
+        self.sch.add_task(WORKER, 'A', deps=['Z'], priority=10)
+        self.sch.add_task(WORKER, 'B', priority=5)
+        self.sch.add_task(WORKER, 'C', deps=['Z'], priority=3)
+        self.sch.add_task(WORKER, 'D', priority=2)
+        self.sch.add_task(WORKER, 'Z', priority=1)
+        for expected_id in ['B', 'D', 'Z', 'A', 'C']:
+            self.assertEqual(self.sch.get_work(WORKER)['task_id'], expected_id)
+            self.sch.add_task(WORKER, expected_id, status=DONE)
+        self.assertEqual(self.sch.get_work(WORKER)['task_id'], None)
+
+
+if __name__ == '__main__':
+    unittest.main()
