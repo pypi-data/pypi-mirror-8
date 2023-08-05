d014beca9b0e983b305512f7e8f9676c97897892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package thumbor-web/0.1.14/thumbor-web-0.1.14.tar.gz

---
+++ packages/thumbor-web/thumbor-web-0.1.14.tar.gz/thumbor-web-0.1.14/thumbor_web/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of thumbor-web.
+# https://github.com/thumbor/thumbor-web
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2014 Bernardo Heynemann heynemann@gmail.com
+
+from thumbor_web.version import __version__
