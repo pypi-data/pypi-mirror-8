c0e229f1e35816cf59268842bb21824c8be5c74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package TracDjangoAuth/0.3.1/TracDjangoAuth-0.3.1.tar.gz

---
+++ packages/TracDjangoAuth/TracDjangoAuth-0.3.1.tar.gz/TracDjangoAuth-0.3.1/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""TracDjangoAuth, setuptools configuration file"""
+
+from setuptools import setup
+
+setup(
+    name="TracDjangoAuth",
+    version="0.3.1",
+    author="Ville Korhonen",
+    author_email="ville@xd.fi",
+    url="https://github.com/ypcs/TracDjangoAuth",
+    download_url="https://github.com/ypcs/TracDjangoAuth/tarball/master",
+    description="Trac Authentication against Django's userdb",
+    keywords=[
+        'AccountManager',
+        'acct_mgr',
+        'authentication',
+        'Django',
+        'Trac',
+    ],
+    packages=[
+        'tracdjangoauth',
+    ],
+    install_requires=[
+        'Trac>=0.11',
+        'TracAccountManager>=0.2',
+        'Django>=1.0',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Framework :: Django",
+        "Framework :: Trac",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: System :: Systems Administration :: Authentication/Directory",
+    ],
+    entry_points={
+        'trac.plugins': [
+            'tracdjangoauth.auth_django = tracdjangoauth.auth_django',
+        ]
+    },
+)
