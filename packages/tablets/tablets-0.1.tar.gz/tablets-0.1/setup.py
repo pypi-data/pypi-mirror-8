79c824d01a7d02409f07c49f60abea77c8aa1784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:28 +0000

    Add package tablets/0.1/tablets-0.1.tar.gz

---
+++ packages/tablets/tablets-0.1.tar.gz/tablets-0.1/setup.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+from distutils.core import setup
+from setuptools import find_packages
+
+from tablets import __project__, __version__
+
+README = open("README.md").read()
+CHANGES = open("CHANGES.md").read()
+
+
+CLASSIFIERS = [
+    "Framework :: Django",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python",
+]
+
+setup(
+    name=__project__.lower(),
+    packages=[__project__.lower()],
+    version=__version__,
+    description="The ultimate database-driven Django template experience",
+    long_description = (README + "\n" + CHANGES),
+    url="https://github.com/craiglabenz/tablets",
+    download_url="https://github.com/craiglabenz/tablets/tarball/0.1",
+    keywords=["django", "templates", "database-templates"],
+    classifiers=CLASSIFIERS,
+    install_requires=open("requirements.txt").readlines(),
+)
