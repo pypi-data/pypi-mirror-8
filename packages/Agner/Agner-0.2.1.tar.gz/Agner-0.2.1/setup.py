13d7f899c1bdfb2377c5fd0d6d77df7585bd3da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package Agner/0.2.1/Agner-0.2.1.tar.gz

---
+++ packages/Agner/Agner-0.2.1.tar.gz/Agner-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup, Command
+
+class PyTest(Command):
+  user_options = []
+  def initialize_options(self):
+    pass
+  def finalize_options(self):
+    pass
+  def run(self):
+    import sys, subprocess
+    errno = subprocess.call([sys.executable, 'runtests.py'])
+    raise SystemExit(errno)
+
+setup(
+    name="Agner",
+    description="simple data-only queueing system backed by Redis",
+    author="Trey Stout",
+    author_email="treystout@gmail.com",
+    url="https://github.com/treystout/agner",
+    version="0.2.1",
+    packages=['agner'],
+    cmdclass={'test':PyTest},
+)
+
