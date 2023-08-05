b70512d6319f8d3f1f2af381ff3bf4986ea1bac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:14 +0000

    Add package django-dslforms/1.0/django-dslforms-1.0.tar.gz

---
+++ packages/django-dslforms/django-dslforms-1.0.tar.gz/django-dslforms-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = """
+Create simple Django forms with a mini-language in your templates.
+
+See:
+
+https://github.com/prestontimmons/django-dslforms
+"""
+
+
+setup(
+    name="django-dslforms",
+    version="1.0",
+    author="Preston Timmons",
+    author_email="prestontimmons@gmail.com",
+    url="https://github.com/prestontimmons/django-dslforms",
+    description="Create simple Django forms with a mini-language in your templates.",
+    long_description=DESCRIPTION,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "pyparsing==1.5.7",
+    ],
+)
