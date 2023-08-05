64d27d5e2ec4487bd3ca1b390c608d2ab83ae15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:40 +0000

    Add package jwt.py/0.1.0/jwt.py-0.1.0.tar.gz

---
+++ packages/jwt.py/jwt.py-0.1.0.tar.gz/jwt.py-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='jwt.py',
+    version='0.1.0',
+    author='iPlant Collaborative',
+    author_email='atmodevs@gmail.com',
+    url='https://github.com/iPlantCollaborativeOpenSource/jwt.py',
+    py_modules=['jwt'],
+    install_requires=['pycrypto>=2.6']
+)
