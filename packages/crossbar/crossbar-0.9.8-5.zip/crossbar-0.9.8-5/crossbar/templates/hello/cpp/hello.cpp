1b12580a6705508d379be741ff8a101542207a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:53 +0000

    Add package crossbar/0.9.8-5/crossbar-0.9.8-5.zip

---
+++ packages/crossbar/crossbar-0.9.8-5.zip/crossbar-0.9.8-5/crossbar/templates/hello/cpp/hello.cpp
@@ -0,0 +1,149 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2014, Tavendo GmbH and/or collaborators. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <string>
+#include <iostream>
+
+#include "autobahn.hpp"
+
+#include <boost/asio.hpp>
+#include <boost/version.hpp>
+
+using namespace std;
+using namespace boost;
+using namespace autobahn;
+
+using boost::asio::ip::tcp;
+
+
+any add2(const anyvec& args, const anymap& kwargs) {
+
+   cerr << "Someone is calling add2() .." << endl;
+
+   uint64_t x = any_cast<uint64_t> (args[0]);
+   uint64_t y = any_cast<uint64_t> (args[1]);
+   return x + y;
+}
+
+
+
+int main () {
+
+   cerr << "Running on " << BOOST_VERSION << endl;
+
+   try {
+      // ASIO service object
+      //
+      asio::io_service io;
+
+      // the TCP socket we connect
+      //
+      tcp::socket socket(io);
+
+      // setting this option minimizes latency at some cost
+      //
+      socket.set_option(tcp::no_delay(true));
+
+      // connect to this server/port
+      //
+      tcp::resolver resolver(io);
+      auto endpoint_iterator = resolver.resolve({"127.0.0.1", "8090"});
+
+      // create a WAMP session that talks over TCP
+      //
+      bool debug = false;
+      autobahn::session<tcp::socket,
+                        tcp::socket> session(io, socket, socket, debug);
+
+      // make sure the future returned from the session joining a realm (see below)
+      // does not run out of scope (being destructed prematurely ..)
+      //
+      future<void> session_future;
+
+      // now do an asynchronous connect ..
+      //
+      boost::asio::async_connect(socket, endpoint_iterator,
+
+         // we either connected or an error happened during connect ..
+         //
+         [&](boost::system::error_code ec, tcp::resolver::iterator) {
+
+            if (!ec) {
+               cerr << "Connected to server" << endl;
+
+               // start the WAMP session on the transport that has been connected
+               //
+               session.start();
+
+               // join a realm with the WAMP session
+               //
+               session_future = session.join("realm1").then([&](future<uint64_t> s) {
+
+                  cerr << "Session joined to realm with session ID " << s.get() << endl;
+
+                  // register a free standing function for remoting
+                  //
+                  auto r1 = session.provide("com.myapp.add2", &add2);
+
+                  r1.then([](future<registration> reg) {
+                     cerr << "Registered with registration ID " << reg.get().id << endl;
+                  }).wait();
+
+
+                  // register a lambda for remoting
+                  //
+                  session.provide("com.myapp.square",
+
+                     [](const anyvec& args, const anymap& kwargs) {
+
+                        cerr << "Someone is calling my lambda function .." << endl;
+
+                        uint64_t x = any_cast<uint64_t> (args[0]);
+                        return x * x;
+                     }
+                  ).wait();
+               });
+
+            } else {
+               cerr << "Could not connect to server: " << ec.message() << endl;
+            }
+         }
+      );
+
+      cerr << "Starting ASIO I/O loop .." << endl;
+
+      io.run();
+
+      cerr << "ASIO I/O loop ended" << endl;
+   }
+   catch (std::exception& e) {
+      cerr << e.what() << endl;
+      return 1;
+   }
+   return 0;
+}
---
+++ packages/crossbar/crossbar-0.9.8-5.zip/crossbar-0.9.8-5/setup.py
@@ -0,0 +1,131 @@
+###############################################################################
+##
+##  Copyright (C) 2011-2014 Tavendo GmbH
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU Affero General Public License, version 3,
+##  as published by the Free Software Foundation.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+##  GNU Affero General Public License for more details.
+##
+##  You should have received a copy of the GNU Affero General Public License
+##  along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
+###############################################################################
+
+from __future__ import absolute_import
+
+import sys
+from setuptools import setup, find_packages
+
+
+## Get package version and docstring from crossbar/__init__.py
+##
+import re
+PACKAGE_FILE = "crossbar/__init__.py"
+initfile = open(PACKAGE_FILE, "rt").read()
+
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, initfile, re.M)
+if mo:
+   verstr = mo.group(1)
+else:
+   raise RuntimeError("Unable to find version string in {}.".format(PACKAGE_FILE))
+
+DSRE = r"__doc__ = \"\"\"(.*)\"\"\""
+mo = re.search(DSRE, initfile, re.DOTALL)
+if mo:
+   docstr = mo.group(1)
+else:
+   raise RuntimeError("Unable to find doc string in {}.".format(PACKAGE_FILE))
+
+
+extra_require_system = [
+   'psutil>=2.1.1',        # BSD license
+   'setproctitle>=1.1.8'   # BSD license
+]
+if sys.platform.startswith('linux'):
+   extra_require_system.append('pyinotify>=0.9.4') ## MIT license
+elif sys.platform.startswith('win'):
+   ## PyWin32 - Python Software Foundation License
+   pass
+else:
+   pass
+
+
+setup (
+   name = 'crossbar',
+   version = verstr,
+   description = 'Crossbar.io - The Unified Application Router',
+   long_description = docstr,
+   author = 'Tavendo GmbH',
+   author_email = 'autobahnws@googlegroups.com',
+   url = 'http://crossbar.io/',
+   platforms = ('Any'),
+   install_requires = [
+      'setuptools>=2.2',            # Python Software Foundation license
+      'zope.interface>=3.6.0',      # Zope Public license
+      'twisted>=twisted-13.2',      # MIT license
+      'autobahn[twisted]>=0.9.2',   # Apache license
+      'netaddr>=0.7.11',            # BSD license
+      'pytrie>=0.2',                # BSD license
+      'jinja2>=2.7.2',              # BSD license
+      'mistune>=0.3.0',             # BSD license
+      'pygments>=1.6',              # BSD license
+      'pyyaml>=3.11',               # MIT license
+   ],
+   extras_require = {
+      'tls': [
+         'cryptography>=0.4',    # Apache license
+         'pyOpenSSL>=0.14',      # Apache license
+         'pyasn1',               # BSD license
+         'pyasn1-modules',       # BSD license
+         'service_identity',     # MIT license
+      ],
+      'oracle': [
+         'cx_Oracle>=5.1.2'      # Python Software Foundation license
+      ],
+      'postgres': [
+         'psycopg2>=2.5.1'       # LGPL license
+      ],
+      'manhole': [
+         'pyasn1>=0.1.7',        # BSD license
+         'pycrypto>=2.6.1'       # Public Domain license
+      ],
+      'msgpack': [
+         'msgpack-python>=0.4.2' # Apache license
+      ],
+      'system': extra_require_system
+   },
+   entry_points = {
+      'console_scripts': [
+         'crossbar = crossbar.controller.cli:run'
+      ]},
+   packages = find_packages(),
+   include_package_data = True,
+   data_files = [('.', ['LICENSE'])],
+   zip_safe = False,
+   ## http://pypi.python.org/pypi?%3Aaction=list_classifiers
+   ##
+   classifiers = ["License :: OSI Approved :: GNU Affero General Public License v3",
+                  "Development Status :: 3 - Alpha",
+                  "Environment :: Console",
+                  "Framework :: Twisted",
+                  "Intended Audience :: Developers",
+                  "Operating System :: OS Independent",
+                  "Programming Language :: Python",
+                  "Topic :: Internet",
+                  "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+                  "Topic :: Communications",
+                  "Topic :: Database",
+                  "Topic :: Home Automation",
+                  "Topic :: Software Development :: Libraries",
+                  "Topic :: Software Development :: Libraries :: Application Frameworks",
+                  "Topic :: Software Development :: Embedded Systems",
+                  "Topic :: System :: Distributed Computing",
+                  "Topic :: System :: Networking"],
+   keywords = 'crossbar router autobahn autobahn.ws websocket realtime rfc6455 wamp rpc pubsub oracle postgres postgresql'
+)
