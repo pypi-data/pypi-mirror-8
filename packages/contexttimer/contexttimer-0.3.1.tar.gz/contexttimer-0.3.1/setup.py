a02048e6c253cc92db092d5c0a3cecc65cba506d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:40 +0000

    Add package contexttimer/0.3.1/contexttimer-0.3.1.tar.gz

---
+++ packages/contexttimer/contexttimer-0.3.1.tar.gz/contexttimer-0.3.1/setup.py
@@ -0,0 +1,24 @@
+from os.path import join, dirname
+
+from setuptools import setup
+
+setup(
+    name="contexttimer",
+    version='0.3.1',
+    license="GPLv3",
+    description='A timer context manager measuring the clock wall time of the code block it contains.',
+    author="Balthazar Rouberol",
+    author_email='brouberol@imap.cc',
+    url='https://github.com/brouberol/contexttimer',
+    packages=['contexttimer'],
+    keywords=['time', 'timer'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+)
