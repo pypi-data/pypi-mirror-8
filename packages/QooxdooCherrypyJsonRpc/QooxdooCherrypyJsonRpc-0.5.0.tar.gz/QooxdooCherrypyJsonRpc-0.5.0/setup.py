3ea2c31e7b0dabbeff5a2e3f45e995b8c257059f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:52 +0000

    Add package QooxdooCherrypyJsonRpc/0.5.0/QooxdooCherrypyJsonRpc-0.5.0.tar.gz

---
+++ packages/QooxdooCherrypyJsonRpc/QooxdooCherrypyJsonRpc-0.5.0.tar.gz/QooxdooCherrypyJsonRpc-0.5.0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+'''
+@author: saaj
+'''
+
+
+import sys
+
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+  
+# See ``qxcpjsonrpc.test`` docstring for the purpose 
+sys.warnoptions.append('always')  
+
+setup(
+  name             = 'QooxdooCherrypyJsonRpc',
+  version          = '0.5.0',
+  author           = 'saaj',
+  author_email     = 'mail@saaj.me',
+  packages         = ['qxcpjsonrpc', 'qxcpjsonrpc.test'],
+  package_data     = {'qxcpjsonrpc.test' : ['fixture/*']},    
+  test_suite       = 'qxcpjsonrpc.test.suite',
+  url              = 'http://code.google.com/p/qooxdoo-cherrypy-json-rpc/',
+  license          = 'LGPL-2.1+',
+  description      = 'Qooxdoo-specific CherryPy-based JSON RPC-server',
+  long_description = open('README.txt').read(),
+  install_requires = ['CherryPy >= 3.2'],
+  platforms        = ['Any'],
+  classifiers      = [
+    'Topic :: Communications',
+    'Topic :: Software Development :: Libraries',
+    'Framework :: CherryPy',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Intended Audience :: Developers'
+  ]
+)
+
