ec17e4228c8be7f7fc824558c2d7d851e2df680c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:22 +0000

    Add package secure-smtpd/2.1.0/secure-smtpd-2.1.0.tar.gz

---
+++ packages/secure-smtpd/secure-smtpd-2.1.0.tar.gz/secure-smtpd-2.1.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name="secure-smtpd",
+    version="2.1.0",
+    description="Adds support for SSL, AUTH, and other goodies, to Petri Lehtinen's SMTPD library.",
+    author="Benjamin Coe",
+    author_email="bencoe@gmail.com",
+    url="https://github.com/bcoe/secure-smtpd",
+    packages = find_packages(),
+    install_requires = [
+        'argparse'
+    ],
+    tests_require=[
+        'nose'
+    ]
+)
