53e1a87dfa976e521f901e3416a9de0ae3f81a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package xcal_raman/0.1.0/xcal_raman-0.1.0.tar.gz

---
+++ packages/xcal_raman/xcal_raman-0.1.0.tar.gz/xcal_raman-0.1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name="xcal_raman",
+      version="0.1.0",
+      description="Module for x-axis calibration of Raman spectrometers",
+      author="Roman Kiselev",
+      author_email="roman.kiselew@gmail.com",
+      py_modules=["xcal_raman"],
+      scripts=[],
+      license="GNU GPL v3",
+      url="https://pypi.python.org/pypi/xcal_raman",
+      platforms=["Linux", "Windows"],
+      requires=[
+              "numpy",
+              "pylab",
+              "scipy",
+          ],
+      classifiers=[
+              "Development Status :: 2 - Pre-Alpha",
+              "Topic :: Scientific/Engineering :: Physics",
+              "Topic :: Scientific/Engineering :: Chemistry",
+          ],
+     )
