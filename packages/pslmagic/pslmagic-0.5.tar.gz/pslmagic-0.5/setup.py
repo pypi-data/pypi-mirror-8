baafbff8d10cdf4c6edded910878847b2a06798e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package pslmagic/0.5/pslmagic-0.5.tar.gz

---
+++ packages/pslmagic/pslmagic-0.5.tar.gz/pslmagic-0.5/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+with open('README') as f:
+    long_description = f.read()
+
+
+setup(
+    name = 'pslmagic',
+    packages = ['pslmagic'], # this must be the same as the name above
+    version = '0.5',
+    description = 'IPython magic for ParselTongue',
+    long_description = long_description,
+    author = 'Todd Iverson',
+    author_email = 'tiverson@smumn.edu',
+    url = 'https://github.com/yardsale8/pslmagic',   # use the URL to the github repo
+    download_url = 'https://github.com/yardsale8/pslmagic/tarball/0.5', # I'll explain this in a second
+    keywords = ['ParselTongue', 'IPython extension', 'IPython magic'], # arbitrary keywords
+    classifiers = ["Development Status :: 3 - Alpha"],
+)
