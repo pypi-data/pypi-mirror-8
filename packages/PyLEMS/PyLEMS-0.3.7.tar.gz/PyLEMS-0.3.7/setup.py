a0912f3ef45070e5f05c1d9128333cd65b5ef392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package PyLEMS/0.3.7/PyLEMS-0.3.7.tar.gz

---
+++ packages/PyLEMS/PyLEMS-0.3.7.tar.gz/PyLEMS-0.3.7/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+long_description = open("README.md").read()
+
+import lems
+version = lems.__version__
+
+setup(
+    name = "PyLEMS",
+    version = version,
+    packages = ['lems','lems.base','lems.model','lems.parser','lems.sim', 'lems.dlems'],
+    scripts=['pylems'],
+    author = "PyLEMS authors and contributors",
+    author_email = "gautham@lisphacker.org, p.gleeson@gmail.com",
+    description = "A Python library for working with the Low Entropy Model Specification language (LEMS)",
+    long_description = long_description,
+    license = "BSD",
+    url="https://github.com/LEMS/pylems",
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Scientific/Engineering']
+)
+
+
+
