8275593d3d655fcc66a3d35d9bfcf54c14af6569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:05 +0000

    Add package amqpy/0.4.8/amqpy-0.4.8.tar.gz

---
+++ packages/amqpy/amqpy-0.4.8.tar.gz/amqpy-0.4.8/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+from setuptools import setup, find_packages
+
+import amqpy
+
+name = 'amqpy'
+description = 'an AMQP 0.9.1 client library for Python >= 3.2.0'
+
+if sys.version_info < (3, 2):
+    raise Exception('amqpy requires Python 3.2 or higher')
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+    'Intended Audience :: Developers',
+]
+
+package_data = {
+    '': ['*.md', '*.ini', 'AUTHORS', 'LICENSE'],
+}
+
+keywords = ['amqp', 'rabbitmq', 'qpid']
+
+is_pypy = hasattr(sys, 'pypy_version_info')
+
+long_description = description
+if os.path.exists('README.md'):
+    with open('README.md') as f:
+        long_description = f.read()
+
+setup(
+    name=name,
+    description=description,
+    long_description=long_description,
+    version=amqpy.__version__,
+    author=amqpy.__author__,
+    author_email=amqpy.__contact__,
+    maintainer=amqpy.__maintainer__,
+    url=amqpy.__homepage__,
+    platforms=['any'],
+    license='LGPL',
+    packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
+    package_data=package_data,
+    tests_require=['pytest>=2.6'],
+    classifiers=classifiers,
+    keywords=keywords
+)
