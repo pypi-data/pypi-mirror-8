2b7dc5894a6d0a6c84fade32a143da7a84443d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package rayle/1.0.2/rayle-1.0.2.tar.gz

---
+++ packages/rayle/rayle-1.0.2.tar.gz/rayle-1.0.2/rayle.py
@@ -0,0 +1,14 @@
+def test(movie, level=0):
+    for i in movie:
+        if isinstance(i, list):
+            level += 1
+            test(i, level)
+        else:
+
+            print '\t' * level,
+            print i
+
+movie = ['i', 'love', ['you', 'are', 'a', 'dream',['love', 'is', 'blind']]]
+test(movie, 0)
+
+print 'dream is a dream'
