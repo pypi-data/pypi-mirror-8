b5abd3ff3a98e1d55d115cadd1c4ca17067dcb11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:09 +0000

    Add package feincms-extensions/0.3.5/feincms_extensions-0.3.5-py2.py3-none-any.whl

---
+++ packages/feincms-extensions/feincms_extensions-0.3.5-py2.py3-none-any.whl/feincms_extensions/tests/content.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class TestContent(models.Model):
+    """A stub feincms content type"""
+    class Meta:
+        abstract = True
+
+    def render(self, **kwargs):
+        return ''
