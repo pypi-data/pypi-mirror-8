9028d057315e95e03f06e9572995428d2e91cc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:48 +0000

    Add package zdeskcfg/1.0.2/zdeskcfg-1.0.2.tar.gz

---
+++ packages/zdeskcfg/zdeskcfg-1.0.2.tar.gz/zdeskcfg-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import sys
+
+should_2to3 = sys.version >= '3'
+
+setup(name="zdeskcfg",
+      version="1.0.2",
+      description="Easy configuration of zendesk/zdesk scripts.",
+      long_description=open("README.txt").read(),
+      py_modules=["zdeskcfg"],
+      classifiers=["Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Libraries",
+      ],
+      use_2to3=should_2to3,
+      keywords="zendesk zdesk config",
+      author="Brent Woodruff",
+      author_email="brent@fprimex.com",
+      url="http://github.com/fprimex/zdeskcfg",
+      license="BSD",
+      zip_safe=False,
+      install_requires=["plac_ini"]
+)
