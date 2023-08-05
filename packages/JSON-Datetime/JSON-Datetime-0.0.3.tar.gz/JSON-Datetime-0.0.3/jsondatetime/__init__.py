d7099a376ac76a0df631ad13c571eccd74b71025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:52 +0000

    Add package JSON-Datetime/0.0.3/JSON-Datetime-0.0.3.tar.gz

---
+++ packages/JSON-Datetime/JSON-Datetime-0.0.3.tar.gz/JSON-Datetime-0.0.3/jsondatetime/__init__.py
@@ -0,0 +1,1 @@
+from jsondatetime import loads
---
+++ packages/JSON-Datetime/JSON-Datetime-0.0.3.tar.gz/JSON-Datetime-0.0.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+#from jsondatetime import __version__
+
+DESCRIPTION = ("Allows for proper decoding of datetime values contained in "
+               "JSON streams")
+LONG_DESCRIPTION = open('README.md').read()
+
+setup(
+      name='JSON-Datetime',
+      version='0.0.3',
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author='Nicola Iarocci',
+      author_email='nicola@nicolaiarocci.com',
+      url='http://github.com/nicolaiarocci/json-datetime',
+      license=open('LICENSE').read(),
+      platforms=["any"],
+      packages=find_packages(),
+      test_suite="jsondatetime.tests",
+      requires=['simplejson'],
+      install_requires=['simplejson'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: ISC License (ISCL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+      ],
+     )
