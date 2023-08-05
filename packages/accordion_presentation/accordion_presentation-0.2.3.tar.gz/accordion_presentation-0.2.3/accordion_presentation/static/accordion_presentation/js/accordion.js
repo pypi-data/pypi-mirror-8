2e2207872618ea6a77b339203bb0704c11070ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package accordion_presentation/0.2.3/accordion_presentation-0.2.3.tar.gz

---
+++ packages/accordion_presentation/accordion_presentation-0.2.3.tar.gz/accordion_presentation-0.2.3/accordion_presentation/static/accordion_presentation/js/accordion.js
@@ -0,0 +1,58 @@
+switch_acordion =true;
+
+function get_proportion(real_size, proportion){
+	return parseInt(real_size*proportion/100);
+}
+    
+
+function resize_image_max_width(element, height, width, maxCol){
+	$(element).removeClass('ap_height');
+	$(element).addClass('ap_width');
+	switch_acordion = true;
+	var colS = (width - 10) / (maxCol+4);
+	
+	var c = get_proportion(height, 60);
+	$(element).find('.ap_slide').css('width', (colS*4)+'px');
+	$(element).find('.ap_img').each(function(e,i){
+		$(i).css('width', (colS*4)+'px');
+		$(i).css('height', c+'px');
+	});
+	$(element).find('.ap_plug').each(function(e,i){
+		$(i).css('width', colS+'px');
+		$(i).css('height', c+'px');
+	});	
+
+}
+
+function resize_image_max_height(element, height, width, maxCol){
+	$(element).removeClass('ap_width');
+	$(element).addClass('ap_height');
+	$('.cycle-slideshow').insertAfter($('.ap_col_first')); 
+	switch_acordion = false;
+	
+	var colS = (width - 10) / (maxCol);
+	var p = get_proportion(height, 60);
+	$(element).find('.ap_slide').css('height', ((p/3)*2)+'px');
+	$(element).find('.ap_img').each(function(e,i){
+		$(i).css('height', ((p/4)*2)+'px');
+		$(i).css('width', width+'px');
+	});
+	$(element).find('.ap_plug').each(function(e,i){
+		$(i).css('width', colS+'px');
+		$(i).css('height', (p/3)+'px');
+	});	
+
+}
+
+function resize_image(element){
+	var height = $(window).height();
+	var width = $(window).width();
+	var maxCol = $(element).data( "maxcol");
+
+	if(height > width){
+		resize_image_max_height(element, height, width, maxCol);
+	}else{
+		resize_image_max_width(element, height, width, maxCol);
+	}
+
+}
