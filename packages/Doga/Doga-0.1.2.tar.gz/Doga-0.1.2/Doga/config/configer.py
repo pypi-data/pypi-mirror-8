097ab904b00c86d1012f8fdd4ede5bb5769c1d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package Doga/0.1.2/Doga-0.1.2.tar.gz

---
+++ packages/Doga/Doga-0.1.2.tar.gz/Doga-0.1.2/Doga/config/configer.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+Doga.config.configer
+
+This module gives other modules access to use configuration values
+"""
+
+import os
+from ConfigParser import SafeConfigParser
+
+# configuration file path
+config_file_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+config_file = os.path.abspath(config_file_path)
+
+# read config data using ConfigParser module
+parser = SafeConfigParser()
+parser.read(config_file)
+
+
+def value(key):
+    """ return config value for key
+
+    param: key(str) : config data key
+    return: config value for the requested key(str)
+    """
+
+    return parser.get('Doga', key)
