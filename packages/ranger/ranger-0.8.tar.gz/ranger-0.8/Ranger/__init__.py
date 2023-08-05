aab44909c17a1fb33f1a41143498ede5c07b831a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:44 +0000

    Add package ranger/0.8/ranger-0.8.tar.gz

---
+++ packages/ranger/ranger-0.8.tar.gz/ranger-0.8/Ranger/__init__.py
@@ -0,0 +1,8 @@
+__author__ = 'Eli Rodgers-Melnick'
+__license__ = 'BSD'
+
+# Make imports
+from Ranger.src.Range.Range import Range
+from Ranger.src.Collections.RangeSet import RangeSet
+from Ranger.src.Collections.RangeMap import RangeMap
+from Ranger.src.Collections.RangeBucketMap import RangeBucketMap
