a76900e204c5a6bc38f77952fa8af455790b00d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package smop/0.25.4/smop-0.25.4.tar.gz

---
+++ packages/smop/smop-0.25.4.tar.gz/smop-0.25.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    author = 'Victor Leikehman',
+    author_email = 'victorlei@gmail.com',
+    description = 'Matlab to Python converter',
+    license = 'MIT',
+    keywords = 'convert translate matlab octave python',
+    url = 'https://github.com/victorlei/smop',
+    download_url = 'https://github.com/victorlei/smop/archive/master.zip',
+    name = 'smop',
+    version = '0.25.4',
+    entry_points = { 'console_scripts': [ 'smop = smop.main:main', ], },
+    packages = ['smop'],
+    #package_dir = {'':'src'},
+    #test_suite = "smop.testsuite.test_lexer",
+    #include_package_data = True,
+    install_requires = ['numpy', 'scipy', 'networkx'],
+)
