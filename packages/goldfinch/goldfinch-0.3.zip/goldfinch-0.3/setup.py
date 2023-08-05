2c457b1cab9fd1e744a829731662c96bf6101854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:47 +0000

    Add package goldfinch/0.3/goldfinch-0.3.zip

---
+++ packages/goldfinch/goldfinch-0.3.zip/goldfinch-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='goldfinch',
+      version='0.3',
+      description='Use to make sure that filenames will always be valid',
+      url='https://github.com/PerAkeMattias/goldfinch',
+      author='Per Ake Mattias',
+      author_email='PerAkeMattias@gmail.com',
+      license='MIT',
+      packages=['goldfinch'],
+      zip_safe=True)
