106a0aa20771c73b8a6cc331655497093b608d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:53 +0000

    Add package collective.folderishtypes/2.0b1/collective.folderishtypes-2.0b1.tar.gz

---
+++ packages/collective.folderishtypes/collective.folderishtypes-2.0b1.tar.gz/collective.folderishtypes-2.0b1/src/collective/folderishtypes/interfaces.py
@@ -0,0 +1,21 @@
+from zope.interface import Interface
+
+
+class IFolderishType(Interface):
+    """Marker interface
+    """
+
+
+class IFolderishDocument(IFolderishType):
+    """Marker interface
+    """
+
+
+class IFolderishEvent(IFolderishType):
+    """Marker interface
+    """
+
+
+class IFolderishNewsItem(IFolderishType):
+    """Marker interface
+    """
