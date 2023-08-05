0e33e29db145e9812dd552b4619ab71f43358e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package firebase-token-generator/2.0.0/firebase-token-generator-2.0.0.tar.gz

---
+++ packages/firebase-token-generator/firebase-token-generator-2.0.0.tar.gz/firebase-token-generator-2.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='firebase-token-generator',
+    version='2.0.0',
+    author='Greg Soltis',
+    author_email='support@firebase.com',
+    zip_safe=False,
+    py_modules=['firebase_token_generator'],
+    license='LICENSE',
+    url='https://github.com/firebase/firebase-token-generator-python',
+    description='A utility to generate signed Firebase Authentication Tokens',
+    long_description=read('README')
+)
