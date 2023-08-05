ee102e06fa59a988aa152ff8ee949e75da92045a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package jenkins-view-builder/0.4.1/jenkins-view-builder-0.4.1.tar.gz

---
+++ packages/jenkins-view-builder/jenkins-view-builder-0.4.1.tar.gz/jenkins-view-builder-0.4.1/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env/python
+
+PROJECT = 'jenkins-view-builder'
+
+VERSION = '0.4.1'
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'jenkins-view-builder',
+    'version': '0.4.1',
+
+    'description': 'Build jenkins views in YAML',
+
+    'author': 'Piyush Srivastava',
+    'author_email': 'piyush.0101@gmail.com',
+
+    'url': 'https://github.com/piyush0101/jenkins-view-builder',
+    'download_url': 'https://github.com/piyush0101/jenkins-view-builder/archive/0.4.1.tar.gz',
+
+    'classifiers': [],
+
+    'platforms': ['Any'],
+
+    'scripts': [],
+
+    'provides': [],
+
+    'install_requires': ['cliff', 'PyYAML', 'argparse', 'cmd2',
+        'gnureadline', 'prettytable', 'pyparsing', 'six',
+        'stevedore', 'wsgiref', 'requests'],
+
+    'packages': find_packages(),
+    'include_package_data': True,
+    'package_data': {'builder.converter.template': ['*.xml']},
+
+    'entry_points' : {
+            'console_scripts' : [
+                    'jenkins-view-builder = builder.main:main',
+                    ],
+            'builder.commands' : [
+                        'simple = builder.commands.simple:Simple',
+                        'update = builder.commands.update:Update',
+                        'test = builder.commands.test:Test',
+                    ],
+        },
+
+
+    'zip_safe': False,
+}
+
+setup(**config)
