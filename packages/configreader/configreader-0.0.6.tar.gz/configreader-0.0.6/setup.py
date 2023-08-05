698395a6e846c14b47e0ffa2315c7b14d3029723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:24 +0000

    Add package configreader/0.0.6/configreader-0.0.6.tar.gz

---
+++ packages/configreader/configreader-0.0.6.tar.gz/configreader-0.0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(name="configreader",
+    description="An easy-to-use, powerful configuration module for Python",
+    long_description = open('README.rst','r').read(),
+    license="MIT",
+    version = "0.0.6",
+    author = "Martin Ortbauer",
+    author_email = "mortbauer@gmail.com",
+    url = "https://github.com/mortbauer/configreader",
+    py_modules = ["configreader"],
+    classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Topic :: Utilities",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4 ",
+    ],
+    )
