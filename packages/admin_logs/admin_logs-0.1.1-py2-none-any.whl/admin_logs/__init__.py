cac807d0d82fdf2f339e298b639f7fd2fd62ebb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:20 +0000

    Add package admin_logs/0.1.1/admin_logs-0.1.1-py2-none-any.whl

---
+++ packages/admin_logs/admin_logs-0.1.1-py2-none-any.whl/admin_logs/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+from admin_logs.log import AdminLogHandler
+
+
+__version__ = (0, 1, '1')
+
+
+def setup_level(level):
+    handler = AdminLogHandler()
+    handler.setLevel(level)
+    logging.root.addHandler(handler)
