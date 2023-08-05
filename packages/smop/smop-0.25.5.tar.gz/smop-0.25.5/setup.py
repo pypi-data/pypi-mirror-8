76b1956baedfe6d3e66edfd257135f144c6361a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package smop/0.25.5/smop-0.25.5.tar.gz

---
+++ packages/smop/smop-0.25.5.tar.gz/smop-0.25.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    author = 'Victor Leikehman',
+    author_email = 'victorlei@gmail.com',
+    description = 'Matlab to Python converter',
+    license = 'MIT',
+    keywords = 'convert translate matlab octave python',
+    url = 'https://github.com/victorlei/smop',
+    download_url = 'https://github.com/victorlei/smop/archive/master.zip',
+    name = 'smop',
+    version = '0.25.5',
+    entry_points = { 'console_scripts': [ 'smop = smop.main:main', ], },
+    packages = ['smop'],
+    #package_dir = {'':'src'},
+    #test_suite = "smop.testsuite.test_lexer",
+    include_package_data = True,
+    package_data = { '': ['README.rst', '*.m', 'Makefile'], },
+    install_requires = ['numpy', 'scipy', 'networkx'],
+)
---
+++ packages/smop/smop-0.25.5.tar.gz/smop-0.25.5/smop/__init__.py
@@ -0,0 +1,5 @@
+# SMOP compiler -- Simple Matlab/Octave to Python compiler
+# Copyright 2011-2014 Victor Leikehman
+
+import parse,resolve,backend,main
+__version__ = main.__version__
