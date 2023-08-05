0e285b3b89a3f3f7ab39dfdca065c9e5a037ecd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package zipgun/0.3.1/zipgun-0.3.1.tar.gz

---
+++ packages/zipgun/zipgun-0.3.1.tar.gz/zipgun-0.3.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+try:
+    import setuptools
+except ImportError:
+    import distutils.core
+    setup = distutils.core.setup
+else:
+    setup = setuptools.setup
+
+tests_require = [
+    'nose >= 1.1.2',
+]
+
+install_requires = [
+    'sqlitedict',
+]
+
+setup(
+    name='zipgun',
+    version='0.3.1',
+    description='Postal code library for Python',
+    author='Marc Sherry',
+    author_email='msherry@gmail.com',
+    packages=['zipgun', 'zipgun.scripts'],
+    url='http://github.com/msherry/zipgun',
+    tests_require=tests_require,
+    install_requires=install_requires,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
