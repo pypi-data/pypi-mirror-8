20d403a05851c06bea638c94dfde94a7323852f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package TimeDiff/0.9.4/TimeDiff-0.9.4.tar.gz

---
+++ packages/TimeDiff/TimeDiff-0.9.4.tar.gz/TimeDiff-0.9.4/timediff/conf_reader.py
@@ -0,0 +1,44 @@
+import json
+import sys
+from pkg_resources import Requirement, resource_filename
+
+class ConfReader():
+	"""
+
+Class used to parse command-line input. Currently holds presets for formatting options
+
+	"""
+	DEFAULT_PRESET = ""
+	"""
+Default formatting if no options are given.
+	"""
+	PRESETS = {}
+	"""
+
+List of all possible formatting presets and their values.
+
+	"""
+	VERSION = "0.9.4"
+	"""
+
+Current version of program.
+
+	"""
+	OUTPUT_FORMAT = ".png"
+	def __init__(self):
+		"""
+
+Reads configuration file and 
+
+		"""
+		config = {"version":"0.9.4", "default-preset":"custom1", "presets":{"custom1":"%Y%m%d_%H%M%S", "linux1":"%b %d %H:%M:%S"}}
+		try:
+			with open("/etc/timediff/timediff.json", "r") as f:
+				config = json.load(f)
+		except IOError:
+			sys.stderr.write("Error loading configuration file timediff.json.\n")
+			sys.exit(1)
+		self.DEFAULT_PRESET = config["default-preset"]
+		self.PRESETS = config["presets"]
+		if config["version"] == "0.9.4":
+			self.OUTPUT_FORMAT = config["image-out"]
