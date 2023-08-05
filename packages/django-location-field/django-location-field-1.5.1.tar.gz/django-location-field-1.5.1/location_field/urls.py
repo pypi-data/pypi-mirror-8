d9bac1eb3f5a481d78c7d2bf10f2b415fa1bcb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:29 +0000

    Add package django-location-field/1.5.1/django-location-field-1.5.1.tar.gz

---
+++ packages/django-location-field/django-location-field-1.5.1.tar.gz/django-location-field-1.5.1/location_field/urls.py
@@ -0,0 +1,14 @@
+try:
+    from django.conf.urls import patterns
+except ImportError:
+    from django.conf.urls.defaults import patterns
+
+import os
+
+app_dir = os.path.dirname(__file__)
+
+urlpatterns = patterns(
+    '',
+    (r'^media/(.*)$', 'django.views.static.serve', {
+        'document_root': '%s/media' % app_dir}),
+)
