9e5a2d2776598b79e4dcb183e74a1ca8c4cd41b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:23 +0000

    Add package pystan/2.4.0.3/pystan-2.4.0.3-cp27-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.whl

---
+++ packages/pystan/pystan-2.4.0.3-cp27-none-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.whl/pystan/stan/src/stan/math/matrix/Eigen.hpp
@@ -0,0 +1,8 @@
+#ifndef __STAN__MATH__MATRIX__EIGEN_HPP__
+#define __STAN__MATH__MATRIX__EIGEN_HPP__
+
+#define EIGEN_DENSEBASE_PLUGIN "stan/math/matrix/EigenDenseBaseAddons.h"
+#include <Eigen/Dense>
+#include <Eigen/QR>
+
+#endif
