4970df2941faa192c0d6d110307f0c0cc4dafde1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package enum34-custom/0.6.5/enum34-custom-0.6.5.tar.gz

---
+++ packages/enum34-custom/enum34-custom-0.6.5.tar.gz/enum34-custom-0.6.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+setup(
+    name = 'enum34-custom',
+    version = '0.6.5',
+    description = 'Custom Enum classes for Python 3.4',
+    long_description = open('README.rst').read(),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords = ['enum'],
+    author = 'Kiss György',
+    author_email = 'kissgyorgy@me.com',
+    url = 'https://github.com/Walkman/enum34-custom',
+    license = 'MIT',
+    py_modules = ['enum34_custom'],
+    tests_require = ['pytest', 'pytest-raisesregexp'],
+)
