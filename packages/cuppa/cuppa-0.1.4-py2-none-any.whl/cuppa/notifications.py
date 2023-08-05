c75048c0b9340c658cc1c61400097a6a54a5e847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package cuppa/0.1.4/cuppa-0.1.4-py2-none-any.whl

---
+++ packages/cuppa/cuppa-0.1.4-py2-none-any.whl/cuppa/notifications.py
@@ -0,0 +1,42 @@
+#
+##          Copyright Jamie Allsop 2014-2014
+## Distributed under the Boost Software License, Version 1.0.
+##    (See accompanying file LICENSE_1_0.txt or copy at
+##          http://www.boost.org/LICENSE_1_0.txt)
+#
+##-------------------------------------------------------------------------------
+##   PrintLog
+##-------------------------------------------------------------------------------
+#
+#
+#
+#def print_log():
+#
+#
+#
+#class PrintLog(object):
+#
+#    def __init__( self, colouriser ):
+#        self._colouriser = colouriser
+#
+#
+#    def
+#
+#
+#
+#
+#
+#def emphasised( self, text ):
+#        return self._colouriser.emphasise( text )
+#
+#
+#def as_error( self, text ):
+#    return self._colouriser.colour( 'error', text )
+#
+#
+#def as_warning( self, text ):
+#    return self._colouriser.colour( 'warning', text )
+#
+#
+#def as_notice( self, text ):
+#    return self._colourise.colour( 'notice', text )
