658e9e0bd237e9eb516860382b9baf521b46900f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:18 +0000

    Add package imprime_listas/1.1.0/imprime_listas-1.1.0.zip

---
+++ packages/imprime_listas/imprime_listas-1.1.0.zip/imprime_listas-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core  import setup
+
+setup(
+    name        = 'imprime_listas',
+    version     = '1.1.0',
+    py_modules  = ['imprime_listas'],
+    author      = 'fonzar',
+    author_email= 'andrefonzar@gmail.com',
+    url         = 'http://',
+    description = 'Uma simples aplicacao para impressao de listas e sub-listas',
+    )
