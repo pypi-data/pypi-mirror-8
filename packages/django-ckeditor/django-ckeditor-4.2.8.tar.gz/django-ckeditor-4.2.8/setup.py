696773af1a4311e54ec2aef24d65889022105e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:23 +0000

    Add package django-ckeditor/4.2.8/django-ckeditor-4.2.8.tar.gz

---
+++ packages/django-ckeditor/django-ckeditor-4.2.8.tar.gz/django-ckeditor-4.2.8/setup.py
@@ -0,0 +1,34 @@
+import os.path
+from setuptools import setup, find_packages
+
+
+def get_source_files():
+    for dirname, _, files in os.walk('ckeditor/static/ckeditor/ckeditor/_source'):
+        for filename in files:
+            yield os.path.join('/'.join(dirname.split('/')[1:]), filename)
+
+setup(
+    name='django-ckeditor',
+    version='4.2.8',
+    description='Django admin CKEditor integration.',
+    long_description=open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Piotr Malinski',
+    author_email='riklaunim@gmail.com',
+    url='https://github.com/riklaunim/django-ckeditor',
+    packages=find_packages(exclude=["*.demo"]),
+    install_requires=[
+        'Django',
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
