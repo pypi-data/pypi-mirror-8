9a74e0de06f5c65182403da6c57c2a6a1cbe40ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:57 +0000

    Add package shodan/1.0.7/shodan-1.0.7.tar.gz

---
+++ packages/shodan/shodan-1.0.7.tar.gz/shodan-1.0.7/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = 'shodan',
+    version = '1.0.7',
+    description = 'Python library for Shodan (https://developer.shodan.io)',
+    author = 'John Matherly',
+    author_email = 'jmath@shodan.io',
+    url = 'http://github.com/achillean/shodan-python/tree/master',
+    packages = ['shodan'],
+    install_requires=["simplejson", "requests"],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
