e2822055f99ef6e62e9523d00055d9cf08aed0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:00 +0000

    Add package rundeckrun/0.1.10/rundeckrun-0.1.10.zip

---
+++ packages/rundeckrun/rundeckrun-0.1.10.zip/rundeckrun-0.1.10/rundeck/defaults.py
@@ -0,0 +1,58 @@
+"""
+:summary: Default values
+
+:license: Creative Commons Attribution-ShareAlike 3.0 Unported
+:author: Mark LaPerriere
+:contact: rundeckrun@mindmind.com
+:copyright: Mark LaPerriere 2013
+"""
+__docformat__ = "restructuredtext en"
+
+
+def enum(name, *sequential, **named):
+    values = dict(zip(sequential, range(len(sequential))), **named)
+    values['values'] = list(values.values())
+    values['keys'] = list(values.keys())
+    return type(name, (), values)
+
+Status = enum(
+    'Status',
+    RUNNING='running',
+    SUCCEEDED='succeeded',
+    FAILED='failed',
+    ABORTED='aborted',
+    SKIPPED='skipped',
+    PENDING='pending'
+    )
+
+DupeOption = enum(
+    'DupeOption',
+    SKIP='skip',
+    CREATE='create',
+    UPDATE='update'
+    )
+
+UuidOption = enum(
+    'UuidOption',
+    PRESERVE='preserve',
+    REMOVE='remove'
+    )
+
+JobDefFormat = enum(
+    'JobDefFormat',
+    XML='xml',
+    YAML='yaml'
+    )
+
+ExecutionOutputFormat = enum(
+    'ExecutionOutputFormat',
+    TEXT='text',
+    **dict(zip(JobDefFormat.keys, JobDefFormat.values))
+    )
+
+RUNDECK_API_VERSION = 11
+GET = 'get'
+POST = 'post'
+DELETE = 'delete'
+JOB_RUN_TIMEOUT = 60
+JOB_RUN_INTERVAL = 3
---
+++ packages/rundeckrun/rundeckrun-0.1.10.zip/rundeckrun-0.1.10/setup.py
@@ -0,0 +1,45 @@
+__docformat__ = "restructuredtext en"
+"""
+:summary: Setup script for rundeckrun
+
+:license: Creative Commons Attribution-ShareAlike 3.0 Unported
+:author: Mark LaPerriere
+:contact: rundeckrun@mindmind.com
+:copyright: Mark LaPerriere 2013
+"""
+import os
+from setuptools import setup, find_packages
+from rundeck import VERSION
+
+project = 'rundeckrun'
+install_requires = ['requests>=1.2.0']
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'README.rst'), 'r') as fp:
+    long_description = fp.read()
+
+setup(
+    name=project,
+    license='http://creativecommons.org/licenses/by-sa/3.0/',
+    version=VERSION,
+    packages=find_packages(),
+    description='Rundeck API Python client',
+    long_description=long_description,
+    url='https://github.com/marklap/{0}'.format(project),
+    author='{0}@mindmind.com'.format(project),
+    author_email='{0}@mindmind.com'.format(project),
+    maintainer='{0}@mindmind.com'.format(project),
+    maintainer_email='{0}@mindmind.com'.format(project),
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: System :: Software Distribution',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+)
