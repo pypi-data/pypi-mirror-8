204c44c11bf82b61a28e68a8ddda8690a6e4ac3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package junos-eznc/1.0.2/junos-eznc-1.0.2.tar.gz

---
+++ packages/junos-eznc/junos-eznc-1.0.2.tar.gz/junos-eznc-1.0.2/lib/jnpr/junos/__init__.py
@@ -0,0 +1,11 @@
+from jnpr.junos.device import Device
+from . import jxml
+from . import jxml as JXML
+from . import version
+from . import exception
+
+__version__ = version.VERSION
+__date__ = version.DATE
+
+#import time
+#__date__ = time.strftime("%Y-%b-%d")
