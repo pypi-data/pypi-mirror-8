162d7262e2a23618aaa675d3295eeab9e4895247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:49 +0000

    Add package protopigeon/2.0.0/protopigeon-2.0.0.tar.gz

---
+++ packages/protopigeon/protopigeon-2.0.0.tar.gz/protopigeon-2.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+VERSION = '2.0.0'
+
+setup(
+    name="protopigeon",
+    version=VERSION,
+    author='Jon Parrott',
+    author_email='jjramone13@gmail.com',
+    maintainer='Jon Parrott / Cloud Sherpas',
+    maintainer_email='jon.wayne.parrott@gmail.com',
+    description="A helper library for working with Google's protorpc and App Engine's datastore",
+    url='https://github.com/jonparrott/Protopigeon',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    packages=['protopigeon'],
+    install_requires=[
+    ],
+)
