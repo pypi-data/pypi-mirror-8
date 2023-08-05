c1999f0d22a94a1a6cf1d4574ee1285b1130a690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:40 +0000

    Add package infi.jira_cli/0.2.2/infi.jira_cli-0.2.2-py2.7.egg

---
+++ packages/infi.jira_cli/infi.jira_cli-0.2.2-py2.7.egg/infi/jira_cli/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8e84a5ddb266f607a311ebf73426bb80a3d2522a'
+__git_head_subject__ = 'summary'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
