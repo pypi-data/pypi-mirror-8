288af4db1554450436eaffa2a574c2cbf125e293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:56 +0000

    Add package django-vies/2.0.0/django-vies-2.0.0.tar.gz

---
+++ packages/django-vies/django-vies-2.0.0.tar.gz/django-vies-2.0.0/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from setuptools import setup, Command
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        import subprocess
+
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
+setup(
+    name='django-vies',
+    version='2.0.0',
+    description='European VIES VAT field for Django',
+    author='codingjoe',
+    url='https://github.com/codingjoe/django-vies',
+    author_email='info@johanneshoppe.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Topic :: Office/Business :: Financial :: Accounting',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+    packages=['vies'],
+    include_package_data=True,
+    install_requires=['django>=1.4', 'suds-jurko>=0.6', 'retrying>=1.1.0'],
+    cmdclass={'test': PyTest},
+)
