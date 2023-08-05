381660b1031e05f7ea9c14a56ddc14a495401391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:07 +0000

    Add package django-newsletter-subscription/0.2.0/django-newsletter-subscription-0.2.0.tar.gz

---
+++ packages/django-newsletter-subscription/django-newsletter-subscription-0.2.0.tar.gz/django-newsletter-subscription-0.2.0/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name='django-newsletter-subscription',
+    version=__import__('newsletter_subscription').__version__,
+    description='Another newsletter subscription app.',
+    long_description=read('README.rst'),
+    author='Matthias Kestenholz',
+    author_email='mk@406.ch',
+    url='http://github.com/matthiask/django-newsletter-subscription/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    package_data={
+        '': ['*.html', '*.txt'],
+        'newsletter_subscription': [
+            'locale/*/*/*.*',
+            # 'static/newsletter_subscription/*.*',
+            # 'static/newsletter_subscription/*/*.*',
+            'templates/*.*',
+            'templates/*/*.*',
+            'templates/*/*/*.*',
+            'templates/*/*/*/*.*',
+        ],
+    },
+    install_requires=[
+        'Django>=1.4.2',
+        # Yes, newsletter_subscription can be used without towel. towel is
+        # only a requirement if you want to use the bundled templates.
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    zip_safe=False,
+)
