1b688b948850ad4e319a0f265024bd652f125b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:42 +0000

    Add package cloudwatch-to-graphite/0.2.0/cloudwatch-to-graphite-0.2.0.tar.gz

---
+++ packages/cloudwatch-to-graphite/cloudwatch-to-graphite-0.2.0.tar.gz/cloudwatch-to-graphite-0.2.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+setup(
+    name='cloudwatch-to-graphite',
+    description='Helper for pushing AWS CloudWatch metrics to Graphite',
+    version='0.2.0',
+    author='Chris Chang',
+    author_email='c@crccheck.com',
+    url='https://github.com/crccheck/cloudwatch-to-graphite',
+    py_modules=['leadbutt'],
+    entry_points={
+        'console_scripts': [
+            'leadbutt = leadbutt:main',
+        ],
+    },
+    install_requires=[
+        'boto',
+        'PyYAML',
+        'docopt',
+    ],
+    license='Apache License, Version 2.0',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
