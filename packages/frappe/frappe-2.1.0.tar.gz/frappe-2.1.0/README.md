eb87c387f43f77f4966c51fe2f6a14de038d01fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:11 +0000

    Add package frappe/2.1.0/frappe-2.1.0.tar.gz

---
+++ packages/frappe/frappe-2.1.0.tar.gz/frappe-2.1.0/README.md
@@ -0,0 +1,24 @@
+[![Build Status](https://travis-ci.org/grafos-ml/frappe.svg?branch=master)](https://travis-ci.org/grafos-ml/frappe)
+
+Installation
+============
+You can check the official **documentation** [here](http://grafos-ml.github.io/frappe/).
+
+$ pip install https://github.com/grafos-ml/test.fm/archive/v1.0.4.tar.gz
+
+$ pip install frappe
+
+Nosetests
+=========
+In the package src folder
+
+$ echo yes | ./manage.py test
+
+Build Documentation
+===================
+In the package doc folder
+
+$ make html
+
+
+© 2014 Linas Baltrunas, Joao Baptista, Telefonica Research
---
+++ packages/frappe/frappe-2.1.0.tar.gz/frappe-2.1.0/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#! -*- encoding: utf-8 -*-
+
+__author__ = "joaonrb"
+VERSION = "2.1.0"
+
+try:
+    import testfm
+except ImportError:
+    raise LookupError("You must have test.fm >= 1.0.4 installed before (https://github.com/grafos-ml/frappe)")
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="frappe",
+    version=VERSION,
+    description="Frappe recommendation system backend.",
+    author="Linas Baltrunas",
+    author_email="linas@tid.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    package_data={
+        "": ["*.txt"],
+    },
+    scripts = ["scripts/sb"],
+    license="copyright.txt",
+    include_package_data=True,
+    install_requires=[
+        "django==1.6.7",
+        "pymysql",
+        "django-cors-headers",
+        "djangorestframework",
+        "python-memcached",
+        #"django-uwsgi-cache",
+        "uwsgi",
+        "docopt",
+        "click"
+    ],
+    long_description=README,
+    url = "https://github.com/grafos-ml/frappe",
+    download_url = "https://github.com/grafos-ml/frappe/archive/v%s.tar.gz" % VERSION,
+    keywords = ["recommendation"],
+)
