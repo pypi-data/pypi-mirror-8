17cdd49dd92244374f32cfbf9b3e79b01cad3b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:06 +0000

    Add package crew/0.6.1/crew-0.6.1.tar.gz

---
+++ packages/crew/crew-0.6.1.tar.gz/crew-0.6.1/crew/worker/handler.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+from .context import context
+
+
+class HandlerClass(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    @classmethod
+    def as_handler(cls, data):
+        obj = cls(data)
+        return obj.process()
+
+    def process(self):
+        return None
+
+    @classmethod
+    def bind(cls, task_id):
+        context.handlers[task_id] = cls.as_handler
