9638ff314a6cd5492c2e249fcf2c5b93a86025b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:28 +0000

    Add package corenlp-python/3.3.0-2/corenlp-python-3.3.0-2.tar.gz

---
+++ packages/corenlp-python/corenlp-python-3.3.0-2.tar.gz/corenlp-python-3.3.0-2/corenlp/client.py
@@ -0,0 +1,25 @@
+import json
+# from jsonrpc import ServerProxy, JsonRpc20, TransportTcpIp
+import jsonrpclib
+from corenlp import batch_parse
+from pprint import pprint
+
+
+# results = batch_parse("sample_raw_text")
+# for i in results:
+#     print i
+
+class StanfordNLP:
+    def __init__(self, port_number=8080):
+        self.server = jsonrpclib.Server("http://localhost:%d" % port_number)
+
+    def parse(self, text):
+        return json.loads(self.server.parse(text))
+
+nlp = StanfordNLP()
+result = nlp.parse("Hello world!  It is so beautiful.")
+pprint(result)
+
+from nltk.tree import Tree
+tree = Tree.parse(result['sentences'][0]['parsetree'])
+pprint(tree)
