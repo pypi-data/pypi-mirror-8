30e567e9b08d68b5589549fc882fddc879bd7bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:12 +0000

    Add package flufl.lock/2.3/flufl.lock-2.3.tar.gz

---
+++ packages/flufl.lock/flufl.lock-2.3.tar.gz/flufl.lock-2.3/setup.py
@@ -0,0 +1,55 @@
+# Copyright (C) 2004-2014 by Barry A. Warsaw
+#
+# This file is part of flufl.lock.
+#
+# flufl.lock is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# flufl.lock is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with flufl.lock.  If not, see <http://www.gnu.org/licenses/>.
+
+from setup_helpers import (
+    description, get_version, long_description, require_python)
+from setuptools import setup, find_packages
+
+
+require_python(0x20600f0)
+__version__ = get_version('flufl/lock/__init__.py')
+
+
+setup(
+    name='flufl.lock',
+    version=__version__,
+    namespace_packages=['flufl'],
+    packages=find_packages(),
+    include_package_data=True,
+    maintainer='Barry Warsaw',
+    maintainer_email='barry@python.org',
+    description=description('README.rst'),
+    long_description=long_description('README.rst', 'flufl/lock/NEWS.rst'),
+    license='LGPLv3',
+    url='http://launchpad.net/flufl.lock',
+    download_url='https://pypi.python.org/pypi/flufl.lock',
+    test_suite='flufl.lock.tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: '
+            'GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    )
