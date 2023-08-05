9e312ca1181974a8db4df4c6116f62f14ea7816f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:16 +0000

    Add package usethis-django-bootstrap/3.2.3/usethis-django-bootstrap-3.2.3.tar.gz

---
+++ packages/usethis-django-bootstrap/usethis-django-bootstrap-3.2.3.tar.gz/usethis-django-bootstrap-3.2.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+BSVERSION = '3.2.0'
+
+setup(name='usethis-django-bootstrap',
+      version='3.2.3',
+      description="Bootstrap 3.2.0 for Django with themes and theme chooser.",
+      author="Jeff Buttars",
+      author_email="jeffbuttars@gmail.com",
+      packages=['usethis_bootstrap'],
+      url='https://github.com/jeffbuttars/usethis-django-bootstrap',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Internet',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Internet :: WWW/HTTP :: Browsers',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+      ],
+      package_dir={'usethis_bootstrap': 'usethis_bootstrap'},
+      package_data={'usethis_bootstrap':
+                    ['static/bootstrap-%s/css/*' % BSVERSION,
+                     'static/bootstrap-%s/fonts/*' % BSVERSION,
+                     'static/bootstrap-%s/js/*.js' % BSVERSION,
+                     'static/bootstrap-%s/*_css/*' % BSVERSION,
+                     'templates/*.html',
+                     ]}
+      )
