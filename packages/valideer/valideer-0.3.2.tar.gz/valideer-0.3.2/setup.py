f06e40837a1a750ba827111b3d49be08bee99045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package valideer/0.3.2/valideer-0.3.2.tar.gz

---
+++ packages/valideer/valideer-0.3.2.tar.gz/valideer-0.3.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="valideer",
+    version="0.3.2",
+    description="Lightweight data validation and adaptation library for Python",
+    long_description=open("README.rst").read(),
+    url="https://github.com/podio/valideer",
+    author="George Sakkis",
+    author_email="george.sakkis@gmail.com",
+    packages=find_packages(),
+    install_requires=["decorator"],
+    tests_require=["nose", "coverage"],
+    platforms=["any"],
+    keywords="validation adaptation typechecking jsonschema",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
