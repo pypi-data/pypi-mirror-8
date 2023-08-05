837abac967fe4ff2cd6be2886b4579b032941c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:25 +0000

    Add package p7edt2ics/0.1.1-alpha3/p7edt2ics-0.1.1-alpha3.tar.gz

---
+++ packages/p7edt2ics/p7edt2ics-0.1.1-alpha3.tar.gz/p7edt2ics-0.1.1-alpha3/edt2ics/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from ical import iCalSchedule
+from scraper import ScheduleScraper
+
+__version__ = '0.1.1-alpha3'
+
+# silent pyflakes
+iCalSchedule, ScheduleScraper
