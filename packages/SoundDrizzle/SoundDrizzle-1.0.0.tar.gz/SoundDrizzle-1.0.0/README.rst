35014c551730b8d2db84c5b51afef7cdc552eb18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:50 +0000

    Add package SoundDrizzle/1.0.0/SoundDrizzle-1.0.0.tar.gz

---
+++ packages/SoundDrizzle/SoundDrizzle-1.0.0.tar.gz/SoundDrizzle-1.0.0/README.rst
@@ -0,0 +1,9 @@
+SoundDrizzle
+============
+
+This is nothing more than a simple downloader for SoundCloud so that you can listen to your favorite tracks offline.
+
+Be a nice person and don't abuse the program for mass downloading.
+
+Usage (please consult drizzle --help for more info)::
+    drizzle <url> [<output>]
---
+++ packages/SoundDrizzle/SoundDrizzle-1.0.0.tar.gz/SoundDrizzle-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='SoundDrizzle',
+    version='1.0.0',
+    description='An easy client to add SoundCloud tracks to your offline collection',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    license='LICENSE',
+    author='Patrick Stegmann (aka. @wonderb0lt)',
+    author_email='code@patrick-stegmann.de',
+    url='https://github.com/wonderb0lt/sounddrizzle',
+    install_requires=[
+        'mutagen',
+        'Click',
+        'requests',
+        'termcolor',
+    ],
+    py_modules=['sounddrizzle'],
+    entry_points='''
+        [console_scripts]
+        drizzle=sounddrizzle:pour
+    ''',
+)
