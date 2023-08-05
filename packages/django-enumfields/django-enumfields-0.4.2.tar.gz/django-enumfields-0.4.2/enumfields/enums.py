ae7fa45bf2f77c528e8aa4a91c14626ae7374e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:49 +0000

    Add package django-enumfields/0.4.2/django-enumfields-0.4.2.tar.gz

---
+++ packages/django-enumfields/django-enumfields-0.4.2.tar.gz/django-enumfields-0.4.2/enumfields/enums.py
@@ -0,0 +1,40 @@
+import inspect
+from django.utils.encoding import python_2_unicode_compatible
+from enum import Enum as BaseEnum, EnumMeta as BaseEnumMeta
+import six
+
+
+class EnumMeta(BaseEnumMeta):
+    def __new__(cls, name, bases, attrs):
+        Labels = attrs.get('Labels')
+
+        if Labels is not None and inspect.isclass(Labels):
+            del attrs['Labels']
+            if hasattr(attrs, '_member_names'):
+                attrs._member_names.remove('Labels')
+
+        obj = BaseEnumMeta.__new__(cls, name, bases, attrs)
+        for m in obj:
+            try:
+                m.label = getattr(Labels, m.name)
+            except AttributeError:
+                m.label = m.name.replace('_', ' ').title()
+
+        return obj
+
+
+@python_2_unicode_compatible
+class Enum(six.with_metaclass(EnumMeta, BaseEnum)):
+    @classmethod
+    def choices(cls):
+        """
+        Returns a list formatted for use as field choices.
+        (See https://docs.djangoproject.com/en/dev/ref/models/fields/#choices)
+        """
+        return tuple((m.value, m.label) for m in cls)
+
+    def __str__(self):
+        """
+        Show our label when Django uses the Enum for displaying in a view
+        """
+        return self.label
