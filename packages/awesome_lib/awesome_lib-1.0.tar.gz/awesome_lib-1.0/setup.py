146bfbe633de4615f47e41e207b6fc025dd25aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:51 +0000

    Add package awesome_lib/1.0/awesome_lib-1.0.tar.gz

---
+++ packages/awesome_lib/awesome_lib-1.0.tar.gz/awesome_lib-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='awesome_lib',
+      description='EBB group retreat package presentation',
+      author='Tristan Bitard Feildel',
+      author_email='t.bitard.feildel@uni-muenster.de',
+      version='1.0',
+      py_modules=['my_awesome_lib'],
+      )
+
+#setup(name='awesome_lib',
+#      version='1.0',
+#      py_modules=['my_awesome_lib'],
+#      )
