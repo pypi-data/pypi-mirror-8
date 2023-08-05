8763837f7458106431064f08ca8eebcf73ed0134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:36 +0000

    Add package porticus/0.9.2/porticus-0.9.2.tar.gz

---
+++ packages/porticus/porticus-0.9.2.tar.gz/porticus-0.9.2/porticus/admin.py
@@ -0,0 +1,111 @@
+"""
+Admin for porticus
+"""
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from mptt.admin import MPTTModelAdmin
+
+from easy_thumbnails.files import get_thumbnailer
+
+from porticus.models import Ressource, Gallery, Album
+
+def admin_image(obj):
+    if obj.image:
+        #try:
+        url = get_thumbnailer(obj.image).get_thumbnail({'size': (75, 75), 'crop': True}).url
+        #except:
+            #return _('Invalid image for %s') % unicode(obj)
+        return '<img src="%s" alt="%s" />' % (url, unicode(obj))
+    else:
+        return _('No image for %s') % unicode(obj)
+admin_image.short_description = _('image')
+admin_image.allow_tags = True
+
+
+class GalleryAdmin(admin.ModelAdmin):
+    ordering = ('-creation_date',)
+    search_fields = ('name', 'description', 'slug')
+    list_filter = ('creation_date', 'publish')
+    list_editable = ('priority', 'publish', 'template_name')
+    list_display = (admin_image, 'name', 'publish', 'priority', 'template_name')
+    list_display_links = (admin_image, 'name')
+    prepopulated_fields = {'slug': ('name', )}
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'image')
+        }),
+        (None, {
+            'fields': ('publish', 'template_name', 'priority', 'slug')
+        }),
+        (None, {
+            'fields': ('description',),
+        }),
+    )
+
+
+class RessourceInline(admin.StackedInline):
+    model = Ressource
+    ordering = ('priority', 'name')
+
+
+class AlbumAdmin(MPTTModelAdmin):
+    ordering = ('-creation_date',)
+    search_fields = ('name', 'description', 'slug')
+    list_filter = ('creation_date', 'gallery', 'publish')
+    list_editable = ('priority', 'publish', 'template_name')
+    list_display = ('name', 'slug', 'publish', 'priority', 'template_name', 'ressources_count')
+    fieldsets = (
+        (None, {
+            'fields': ('gallery','parent',)
+        }),
+        (None, {
+            'fields': ('name', 'image',)
+        }),
+        (None, {
+            'fields': ('publish', 'template_name', 'priority', 'slug')
+        }),
+        (None, {
+            'fields': ('description',),
+        }),
+    )
+    prepopulated_fields = {'slug': ('name', )}
+    inlines = [
+        RessourceInline,
+    ]
+    mptt_level_indent = 25
+
+    def ressources_count(self, album):
+        return album.ressource_set.all().count()
+    ressources_count.short_description = _('Ressources')
+
+
+class RessourceAdmin(admin.ModelAdmin):
+    ordering = ('-creation_date',)
+    search_fields = ('name', 'description')
+    list_filter = ('file_type', 'creation_date', 'album', 'publish')
+    list_editable = ('priority', 'publish')
+    list_display = (admin_image, 'album', 'name', 'publish', 'priority', 'file_type', 'file_weight')
+    fieldsets = (
+        (None, {
+            'fields': ('album',),
+        }),
+        (None, {
+            'fields': ('publish', 'priority'),
+        }),
+        (None, {
+            'fields': ('name', 'image', 'tags'),
+        }),
+        (_('File'), {
+            'fields': ('file_type', 'file', 'file_url', 'file_weight'), 
+            'description': _('You must fill one of these fields'),
+        }),
+        (None, {
+            'fields': ('description',),
+        }),
+    )
+
+
+admin.site.register(Gallery, GalleryAdmin)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Ressource, RessourceAdmin)
---
+++ packages/porticus/porticus-0.9.2.tar.gz/porticus-0.9.2/porticus/models.py
@@ -0,0 +1,172 @@
+"""
+Models for porticus
+"""
+import os
+
+from django.conf import settings
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+
+from porticus.managers import RessourcePublishedManager, GalleryPublishedManager, AlbumPublishedManager
+
+from mptt.models import MPTTModel, TreeForeignKey, TreeManager
+
+from filer.fields.image import FilerImageField
+from filer.fields.file import FilerFileField
+
+from tagging.fields import TagField
+from tagging.models import Tag
+
+PUBLISHED_CHOICES = (
+    (True, _('Published')),
+    (False, _('Unpublished')),
+)
+
+class Gallery(models.Model):
+    """Model representing a gallery"""
+
+    name = models.CharField(_('name'), max_length=250)
+
+    description = models.TextField(_('description'), blank=True)
+
+    image = FilerImageField(verbose_name=_('image'), related_name="gallery_image", null=True, blank=True, default=None)
+
+    template_name = models.CharField(_('template'), max_length=255, help_text=_('Template used to render the gallery'), choices=settings.PORTICUS_GALLERY_TEMPLATE_CHOICES, default=settings.PORTICUS_GALLERY_TEMPLATE_DEFAULT)
+
+    publish = models.BooleanField(_('published'), choices=PUBLISHED_CHOICES, default=True)
+
+    priority = models.IntegerField(_('display priority'), default=100)
+
+    creation_date = models.DateTimeField(_('creation date'), auto_now_add=True)
+
+    slug = models.SlugField(_('slug'), unique=True, max_length=100)
+
+    objects = models.Manager()
+    published = GalleryPublishedManager()
+
+    #@models.permalink
+    #def get_absolute_url(self):
+        #return ('porticus:gallery-detail', (self.slug,))
+
+    def get_tags(self):
+        return Tag.objects.get_for_object(self)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-priority', 'name')
+        verbose_name = _('gallery')
+        verbose_name_plural = _('galleries')
+
+
+class Album(MPTTModel):
+    """Model representing an album"""
+    gallery = models.ForeignKey(Gallery)
+
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+
+    name = models.CharField(_('name'), max_length=250)
+
+    description = models.TextField(_('description'), blank=True)
+
+    image = FilerImageField(verbose_name=_('image'), related_name="album_image", null=True, blank=True, default=None)
+
+    template_name = models.CharField(_('template'), max_length=255, help_text=_('Template used to render the album'), choices=settings.PORTICUS_ALBUM_TEMPLATE_CHOICES, default=settings.PORTICUS_ALBUM_TEMPLATE_DEFAULT)
+
+    publish = models.BooleanField(_('published'), choices=PUBLISHED_CHOICES, default=True)
+    
+    priority = models.IntegerField(_('display priority'), default=100)
+
+    creation_date = models.DateTimeField(_('creation date'), auto_now_add=True)
+
+    slug = models.SlugField(_('slug'), unique=True, max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+    objects = TreeManager()
+    published = AlbumPublishedManager()
+
+    #@models.permalink
+    #def get_absolute_url(self):
+        #return ('porticus:album-detail', (self.gallery.slug, self.slug,))
+
+    def get_tags(self):
+        return Tag.objects.get_for_object(self)
+
+    def get_published_children(self):
+        """
+        Return all ressources for the album and all its children
+        """
+        return self.get_children().filter(publish=True)
+    
+    def get_published_descendants(self):
+        """
+        Return all ressources for the album and its direct descendants
+        """
+        return self.get_descendants().filter(publish=True)
+    
+    def get_published_ressources(self):
+        """
+        Return all ressources for the album
+        """
+        return self.ressource_set.filter(publish=True).order_by('priority', 'name')
+
+    class Meta:
+        verbose_name = _('album')
+        verbose_name_plural = _('albums')
+
+    class MPTTMeta:
+        order_insertion_by = ['gallery', 'priority', 'name']
+
+
+class Ressource(models.Model):
+    """Model for representing a ressource"""
+    album = models.ForeignKey(Album)
+
+    name = models.CharField(_('name'), max_length=250)
+
+    description = models.TextField(_('description'), blank=True)
+
+    image = FilerImageField(verbose_name=_('image'), related_name="ressource_image", null=True, blank=True, default=None)
+
+    file_type = models.IntegerField(_('file type'), choices=settings.PORTICUS_RESSOURCE_FILETYPE_CHOICES, default=settings.PORTICUS_RESSOURCE_FILETYPE_DEFAULT)
+    file = FilerFileField(verbose_name=_('file'), related_name="ressource_file", null=True, blank=True, default=None)
+    file_url = models.URLField(_('file url'), blank=True)
+    file_weight = models.CharField(_('file weight'), blank=True, max_length=15)
+
+    publish = models.BooleanField(_('published'), choices=PUBLISHED_CHOICES, default=True)
+
+    priority = models.IntegerField(_('display priority'), default=100)
+
+    creation_date = models.DateTimeField(_('creation date'), auto_now_add=True)
+
+    tags = TagField(_('tags'))
+
+    objects = models.Manager()
+    published = RessourcePublishedManager()
+
+    def get_tags(self):
+        return Tag.objects.get_for_object(self)
+
+    @property
+    def get_file(self):
+        try:
+            return self.file_url or self.file.url
+        except ValueError:
+            return None
+
+
+    def clean(self):
+        if not self.get_file:
+            raise ValidationError(_('Please upload a file or give a file url'))
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-priority', 'name')
+        verbose_name = _('ressource')
+        verbose_name_plural = _('ressources')
