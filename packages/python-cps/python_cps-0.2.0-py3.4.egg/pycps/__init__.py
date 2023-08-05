1a3c50f752c89744df57290e2c71e16b5e8b846e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:13 +0000

    Add package python-cps/0.2.0/python_cps-0.2.0-py3.4.egg

---
+++ packages/python-cps/python_cps-0.2.0-py3.4.egg/pycps/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__version__ = '0.2.0'
+
+from pycps import api
+from pycps import compat
+from pycps import downloaders
+from pycps import parsers
---
+++ packages/python-cps/python_cps-0.2.0-py3.4.egg/pycps/logtest.py
@@ -0,0 +1,9 @@
+import logging
+
+from pycps import api
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    logger.info("Testing INFO")
+    logger.warn("Testing WARNING")
