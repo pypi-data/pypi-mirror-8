f5a452de1fba81ee9ae85f6ae5aa088453d4cc8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package okcupyd/0.4.1/okcupyd-0.4.1.tar.gz

---
+++ packages/okcupyd/okcupyd-0.4.1.tar.gz/okcupyd-0.4.1/tests/xpath_test.py
@@ -0,0 +1,28 @@
+import pytest
+from lxml import etree
+
+from okcupyd import xpath
+
+
+def test_selected_attribute():
+    node = xpath.XPathNode(element='element', selected_attribute='value')
+    assert node.xpath == '//element/@value'
+
+    tree = etree.XML("<top><container><element value='1'>"
+                 "</element><element value='2'></element></container></top>")
+    builder = xpath.xpb.container.element.select_attribute_('value')
+
+    assert builder.xpath == './/container//element/@value'
+    assert builder.apply_(tree) == ['1', '2']
+
+    assert xpath.xpb.element.select_attribute_('value', elem=tree) == ['1', '2']
+
+
+def test_text_for_many():
+    tree = etree.XML("<top><container>"
+                     "<element value='1'>one</element>"
+                     "<element value='2'>two</element>"
+                     "</container></top>")
+
+    result = xpath.xpb.container.element.text_.apply_(tree)
+    assert set(result) == set(['one', 'two'])
