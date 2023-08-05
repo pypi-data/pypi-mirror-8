2a3bec50890ecaa766de5e0da36c7039e3e64fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:34 +0000

    Add package path.py/7.0/path.py-7.0.zip

---
+++ packages/path.py/path.py-7.0.zip/path.py-7.0/docs/conf.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+project = 'path.py'
+copyright = '2013, Mikhail Gusarov, Jason R. Coombs'
+
+import path
+import re
+
+release = path.__version__
+version = re.match('[^.]+[.][^.]+', release).group(0)
+
+pygments_style = 'sphinx'
+html_theme = 'default'
+html_static_path = ['_static']
+htmlhelp_basename = 'pathpydoc'
+templates_path = ['_templates']
+exclude_patterns = ['_build']
+source_suffix = '.rst'
+master_doc = 'index'
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+
+intersphinx_mapping = {'python': ('http://docs.python.org/', None)}
