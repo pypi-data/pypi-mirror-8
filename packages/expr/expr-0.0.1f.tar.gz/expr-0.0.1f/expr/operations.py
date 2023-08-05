8b46476921e92020388ceba156fa8929e909a9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:56 +0000

    Add package expr/0.0.1f/expr-0.0.1f.tar.gz

---
+++ packages/expr/expr-0.0.1f.tar.gz/expr-0.0.1f/expr/operations.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import operator
+
+
+OPERATIONS = {
+    '*': operator.mul,
+    '+': operator.add,
+    '/': operator.div,
+    '-': operator.sub,
+    '%': operator.mod,
+}
+
+OP_ALIAS = {'*': '×', '/': '÷'}
