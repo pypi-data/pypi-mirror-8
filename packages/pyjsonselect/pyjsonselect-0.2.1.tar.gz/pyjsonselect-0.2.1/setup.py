1d188da70c57ce796fdbf17f756d5dc9309879f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package pyjsonselect/0.2.1/pyjsonselect-0.2.1.tar.gz

---
+++ packages/pyjsonselect/pyjsonselect-0.2.1.tar.gz/pyjsonselect-0.2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='pyjsonselect',
+      version='0.2.1',
+      description='Fully-compliant implementation of JSONSelect',
+      author='Dan Vanderkam',
+      author_email='danvdk@gmail.com',
+      url='https://github.com/danvk/pyjsonselect/',
+      packages=['jsonselect'],
+      install_requires=[],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Topic :: Utilities',
+          'Topic :: Text Processing'
+      ],
+      keywords=[
+          'css',
+          'json',
+          'jsonselect'
+      ]
+)
