d3c910542da07f9da11abe2f953b1ffe6de7fd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:32 +0000

    Add package birdfeeder/0.1.6/birdfeeder-0.1.6.tar.gz

---
+++ packages/birdfeeder/birdfeeder-0.1.6.tar.gz/birdfeeder-0.1.6/birdfeeder/connection.py
@@ -0,0 +1,25 @@
+from twython import Twython 
+import credentials
+
+def connect(**kw):
+  """
+  Given environmental variables / kw, connect to twitter's api
+  """
+  
+  # load credentials
+  app_key = kw.get('app_key', credentials.TWT_API_KEY)
+  app_secret = kw.get('app_secret', credentials.TWT_API_SECRET)
+  oauth_token = kw.get('oauth_token', credentials.TWT_ACCESS_TOKEN)
+  oauth_token_secret = kw.get('oauth_token_secret', credentials.TWT_ACCESS_SECRET)
+  access_token = kw.get('access_token', None)
+
+  conn = Twython(
+    app_key = app_key,
+    app_secret = app_secret,
+    oauth_token = oauth_token,
+    oauth_token_secret = oauth_token_secret,
+    access_token = access_token
+    )
+
+  # authenticate
+  return conn
---
+++ packages/birdfeeder/birdfeeder-0.1.6.tar.gz/birdfeeder-0.1.6/birdfeeder/credentials.py
@@ -0,0 +1,8 @@
+import os
+
+TWT_API_KEY = os.getenv('TWT_API_KEY')
+TWT_API_SECRET = os.getenv('TWT_API_SECRET')
+TWT_ACCESS_TOKEN = os.getenv('TWT_ACCESS_TOKEN')
+TWT_ACCESS_SECRET = os.getenv('TWT_ACCESS_SECRET')
+
+
