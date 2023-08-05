554ddefc8b2833d0bdffdde2a73811b474a4bcf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:30 +0000

    Add package mozsvc/0.8/mozsvc-0.8.tar.gz

---
+++ packages/mozsvc/mozsvc-0.8.tar.gz/mozsvc-0.8/mozsvc/tests/test_secrets.py
@@ -0,0 +1,94 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+import unittest2
+import tempfile
+import os
+import time
+import itertools
+
+from mozsvc.secrets import Secrets, FixedSecrets, DerivedSecrets
+
+
+class TestSecrets(unittest2.TestCase):
+
+    def setUp(self):
+        self._files = []
+
+    def tearDown(self):
+        for file in self._files:
+            if os.path.exists(file):
+                os.remove(file)
+
+    def tempfile(self):
+        fd, path = tempfile.mkstemp()
+        os.close(fd)
+        self._files.append(path)
+        return path
+
+    def test_read_write(self):
+        secrets = Secrets()
+
+        # We can only add one secret per second to the file, since
+        # they are timestamped to 1s resolution.  Fake it.
+        real_time = time.time
+        time.time = itertools.count(int(real_time())).next
+        try:
+            secrets.add('phx23456')
+            secrets.add('phx23456')
+            secrets.add('phx23')
+        finally:
+            time.time = real_time
+
+        phx23456_secrets = secrets.get('phx23456')
+        self.assertEqual(len(secrets.get('phx23456')), 2)
+        self.assertEqual(len(secrets.get('phx23')), 1)
+
+        path = self.tempfile()
+
+        secrets.save(path)
+
+        secrets2 = Secrets(path)
+        self.assertEqual(len(secrets2.get('phx23456')), 2)
+        self.assertEqual(len(secrets2.get('phx23')), 1)
+        self.assertEquals(secrets2.get('phx23456'), phx23456_secrets)
+
+    def test_multiple_files(self):
+        # creating two distinct files
+        secrets = Secrets()
+        secrets.add('phx23456')
+        one = self.tempfile()
+        secrets.save(one)
+
+        secrets = Secrets()
+        secrets.add('phx123')
+        two = self.tempfile()
+        secrets.save(two)
+
+        # loading the two files
+        files = one, two
+        secrets = Secrets(files)
+        keys = secrets.keys()
+        keys.sort()
+        self.assertEqual(keys, ['phx123', 'phx23456'])
+
+    def test_fixed_secrets(self):
+        secrets = FixedSecrets(['one', 'two'])
+        self.assertEquals(secrets.get('phx123'), ['one', 'two'])
+        self.assertEquals(secrets.get('phx234'), ['one', 'two'])
+
+    def test_derived_secrets(self):
+        master_secrets = ['abcdef', '1234567890']
+        secrets = DerivedSecrets(master_secrets)
+        derived1 = secrets.get('phx123')
+        derived2 = secrets.get('phx987')
+        # Secrets for the same node should derived consistently.
+        self.assertEquals(derived1, secrets.get('phx123'))
+        self.assertEquals(derived2, secrets.get('phx987'))
+        # Secrets for different nodes should be different.
+        self.assertEquals(set(derived1).intersection(derived2), set())
+        # Length of derived secret mathches length of master secret.
+        for derived in (derived1, derived2):
+            self.assertEquals(len(derived), len(master_secrets))
+            for d, m in zip(derived, master_secrets):
+                self.assertEquals(len(d), len(m))
---
+++ packages/mozsvc/mozsvc-0.8.tar.gz/mozsvc-0.8/setup.py
@@ -0,0 +1,48 @@
+import os
+from setuptools import setup, find_packages
+
+# Importing this up front prevents a pointless error traceback
+# from being printed after running `python setup.py test.`
+import multiprocessing
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = ['pyramid>=1.5', 'simplejson', 'konfig']
+
+tests_require = requires + [
+            'pyramid_hawkauth', 'tokenlib', 'hawkauthlib>=0.1.1',
+            'cornice>=0.10', 'wsgiproxy', 'unittest2', 'webtest',
+            'gunicorn', 'gevent', 'testfixtures']
+
+extras_require = {
+    'memcache': ['umemcache>=1.3'],
+}
+
+
+setup(name='mozsvc',
+      version='0.8',
+      description='Various utilities for Mozilla apps',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Mozilla Services',
+      author_email='services-dev@mozilla.org',
+      url='https://github.com/mozilla-services/mozservices',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      extras_require=extras_require,
+      tests_require=tests_require,
+      test_suite="mozsvc.tests")
