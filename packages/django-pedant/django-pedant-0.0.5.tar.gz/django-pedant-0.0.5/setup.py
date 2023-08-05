380f9109baf87ed6f3bce017bd2dec2cf50e695a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:36 +0000

    Add package django-pedant/0.0.5/django-pedant-0.0.5.tar.gz

---
+++ packages/django-pedant/django-pedant-0.0.5.tar.gz/django-pedant-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+from pip.req import parse_requirements
+
+from pedant import __version__
+
+
+setup(
+    name='django-pedant',
+    version=__version__,
+    author='Lucas Wiman',
+    author_email='lucaswiman@counsyl.com',
+    maintainer='Counsyl',
+    maintainer_email='root@counsyl.com',
+    description='Make django templates fail fast on errors',
+    url='https://github.com/counsyl/django-pedant',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        str(ir.req) for ir in parse_requirements('./requirements.txt')],
+    zip_safe=False,
+)
