72f320ad2c7c4cc9c1e715c8252e918aace3be81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package pyul/0.3.11/pyul-0.3.11.tar.gz

---
+++ packages/pyul/pyul-0.3.11.tar.gz/pyul-0.3.11/jenkins/release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+venv_name=.virtualenv_release
+virtualenv_activate=./${venv_name}/bin/activate
+
+# Validate the virtualenv and activate it
+if [[ ! -e $virtualenv_activate ]]
+then
+  virtualenv $venv_name
+fi
+. ${virtualenv_activate}
+
+pip install wheel
+
+if [[ "${RELEASE_TYPE}" == "" ]]
+then 
+  RELEASE_TYPE=patch
+fi
+if [[ "${PYPI_NAME}" == "" ]]
+then 
+  PYPI_NAME=pypi
+fi
+
+if [[ -e "./setup.py" ]]
+then
+  python setup.py tag --${RELEASE_TYPE} register -r ${PYPI_NAME} sdist bdist_egg bdist_wheel upload -r ${PYPI_NAME}
+fi
