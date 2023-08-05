27181bc60f7d2f82ff9e248e9e7b7afb6058c678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package stomper/0.3.0/stomper-0.3.0.tar.gz

---
+++ packages/stomper/stomper-0.3.0.tar.gz/stomper-0.3.0/lib/stomper/__init__.py
@@ -0,0 +1,23 @@
+from stomp_11 import (
+    Engine,
+    Frame,
+    FrameError,
+
+    abort,
+    ack,
+    nack,
+    begin,
+    commit,
+    connect,
+    disconnect,
+    send,
+    subscribe,
+    unpack_frame,
+    unsubscribe,
+
+    VALID_COMMANDS,
+
+    NO_RESPONSE_NEEDED,
+    NO_REPONSE_NEEDED,
+    NULL,
+)
