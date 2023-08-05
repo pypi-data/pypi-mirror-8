1e250d72dde8e8402be80a7a77c21c1e3d1f3d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:46 +0000

    Add package okcupyd/0.3.0/okcupyd-0.3.0.tar.gz

---
+++ packages/okcupyd/okcupyd-0.3.0.tar.gz/okcupyd-0.3.0/okcupyd/settings.py
@@ -0,0 +1,10 @@
+import os
+
+
+USERNAME = os.environ.get('OKC_USERNAME')
+PASSWORD = os.environ.get('OKC_PASSWORD')
+
+AF_USERNAME = os.environ.get('AF_USERNAME', USERNAME)
+AF_PASSWORD = os.environ.get('AF_PASSWORD', PASSWORD)
+
+DELAY = 0
