45dd92233848abecbeb49bc8308b05a847226bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:03 +0000

    Add package google-distance-matrix/0.1.8/google-distance-matrix-0.1.8.tar.gz

---
+++ packages/google-distance-matrix/google-distance-matrix-0.1.8.tar.gz/google-distance-matrix-0.1.8/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='google-distance-matrix',
+    version='0.1.8',
+    packages=['google_distance_matrix'],
+    author='Manuel Miranda de Cid',
+    author_email='manu.mirandad@gmail.com',
+    url='https://github.com/argaen/python-google-distance-matrix',
+    description='Code to calculate distances between different points using google distance matrix',
+    keywords=['geolocation', 'google', 'distance', 'matrix', 'python', 'coordinates']
+)
