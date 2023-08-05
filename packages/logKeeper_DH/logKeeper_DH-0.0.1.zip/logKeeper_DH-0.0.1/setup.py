d3e8b7af7e01f2897e0545b70ed40c68c01f1118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:06 +0000

    Add package logKeeper_DH/0.0.1/logKeeper_DH-0.0.1.zip

---
+++ packages/logKeeper_DH/logKeeper_DH-0.0.1.zip/logKeeper_DH-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'logKeeper_DH', 
+    version =	'0.0.1', 
+    py_modules = ['logKeeper'], 
+    author =	'Daine Huang', 
+    author_email = 'daine199@gmail.com', 
+    url = 'http://hi.baidu.com/huangyif', 
+    description = 'keeping logfile, it can open a file and add log to the end.',
+    )
