9abdc9bd5a407a090e4164e14a7a7b9b1b372b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:08 +0000

    Add package collective.inviting/0.2/collective.inviting-0.2.zip

---
+++ packages/collective.inviting/collective.inviting-0.2.zip/collective.inviting-0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='collective.inviting',
+      version=version,
+      description="Plone add-on for event invitations, confirmation.",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("doc", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        ],
+      keywords='',
+      author='Sean Upton',
+      author_email='sean.upton@hsc.utah.edu',
+      url='https://github.com/collective/collective.inviting',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.subscribe',
+          'Products.CMFPlone',
+          'icalendar', # http://codespeak.net/icalendar/
+          # -*- Extra requirements: -*-
+      ],
+      extras_require = {
+          'test': [ 'plone.app.testing', ],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
+
