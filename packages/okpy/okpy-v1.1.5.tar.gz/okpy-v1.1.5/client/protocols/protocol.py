db105a7f66411092f17f0e761267e73af922f5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:06 +0000

    Add package okpy/v1.1.5/okpy-v1.1.5.tar.gz

---
+++ packages/okpy/okpy-v1.1.5.tar.gz/okpy-v1.1.5/client/protocols/protocol.py
@@ -0,0 +1,40 @@
+from client import exceptions
+
+class Protocol(object):
+    """A Protocol encapsulates a single aspect of ok.py functionality."""
+    name = None # Override in sub-class.
+
+    def __init__(self, cmd_line_args, assignment, logger):
+        """Constructor.
+
+        PARAMETERS:
+        cmd_line_args -- Namespace; parsed command line arguments.
+                         command line, as parsed by argparse.
+        assignment    -- dict; general information about the assignment.
+        logger        -- OutputLogger; used to control output
+                         destination, as well as capturing output from
+                         an autograder session.
+        """
+        self.args = cmd_line_args
+        self.assignment = assignment
+        self.logger = logger
+        self.analytics = {}
+
+    def on_start(self):
+        """Called when ok.py starts. Returns an object to be sent to server."""
+
+    def on_interact(self):
+        """Called to execute an interactive or output-intensive session."""
+
+def get_protocols(names):
+    mapping = {}
+    subclasses = Protocol.__subclasses__()
+    while subclasses:
+        protocol = subclasses.pop()
+        if protocol.name != Protocol.name:
+            mapping[protocol.name] = protocol
+        subclasses.extend(protocol.__subclasses__())
+    try:
+        return [mapping[name] for name in names]
+    except KeyError as e:
+        raise exceptions.OkException(str(e) + ' is not a protocol')
