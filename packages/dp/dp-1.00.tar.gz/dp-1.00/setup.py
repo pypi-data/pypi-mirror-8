902edf50997896c443bc608f024495420c0f46a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:46 +0000

    Add package dp/1.00/dp-1.00.tar.gz

---
+++ packages/dp/dp-1.00.tar.gz/dp-1.00/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dp',
+  packages = ['dp'],
+  version = '1.00',
+  description = 'Automatically transform recursive functions to dynamic programming',
+  author = 'Yuxuan Dong',
+  author_email = 'm@dyx.name',
+  url = 'https://github.com/dongyx/PyDP',
+  download_url = 'https://github.com/dongyx/PyDP/tarball/1.00',
+  keywords = ['Dynamic Programming', 'DP'],
+  classifiers = ['Operating System :: OS Independent'],
+)
