4278edad9c31aeba31195d1469472ea5072e91c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package docker-harpoon/0.3.26/docker-harpoon-0.3.26.tar.gz

---
+++ packages/docker-harpoon/docker-harpoon-0.3.26.tar.gz/docker-harpoon-0.3.26/harpoon/overview.py
@@ -0,0 +1,207 @@
+from harpoon.errors import BadConfiguration, BadTask, BadYaml
+from harpoon.formatter import MergedOptionStringFormatter
+from harpoon.option_spec.harpoon_specs import HarpoonSpec
+from harpoon.processes import command_output
+from harpoon.tasks import available_tasks
+from harpoon.option_spec.objs import Task
+from harpoon.imager import Imager
+
+from option_merge import MergedOptions
+from input_algorithms.meta import Meta
+import logging
+import yaml
+import os
+
+log = logging.getLogger("harpoon.executor")
+
+class Harpoon(object):
+    def __init__(self, configuration_file, docker_context, silent_build=False, interactive=True, logging_handler=None):
+        self.interactive = interactive
+        self.docker_context = docker_context
+        self.logging_handler = logging_handler
+
+        self.configuration = self.collect_configuration(configuration_file)
+        self.configuration_folder = os.path.dirname(os.path.abspath(configuration_file))
+        self.imager = Imager(self.configuration, docker_context, interactive=self.interactive, silent_build=silent_build)
+        self.setup_logging_theme()
+
+    def start(self, task, extra=None, keep_replaced=False, no_intervention=False, env=None, ports=None, **kwargs):
+        """Do the harpooning"""
+        if not self.configuration.get("images"):
+            raise BadConfiguration("Didn't find any images in the configuration")
+
+        if extra is None:
+            extra = ""
+        self.configuration["$@"] = extra
+        self.configuration["config_root"] = self.configuration_folder
+
+        if "harpoon" not in self.configuration:
+            self.configuration["harpoon"] = {}
+
+        for (name, val) in [
+            ("env", env), ("ports", ports), ("keep_replaced", keep_replaced), ("no_intervention", no_intervention)
+        ]:
+            self.configuration["harpoon"][name] = val or self.configuration["harpoon"].get(name)
+
+        tasks = self.find_tasks()
+        if task not in tasks:
+            raise BadTask("Unknown task", task=task, available=tasks.keys())
+
+        tasks[task].run(self, kwargs)
+
+    ########################
+    ###   THEME
+    ########################
+
+    def setup_logging_theme(self):
+        """Setup a logging theme"""
+        if "term_colors" not in self.configuration:
+            return
+
+        if not getattr(self, "logging_handler", None):
+            log.warning("Told to set term_colors but don't have a logging_handler to change")
+            return
+
+        colors = self.configuration.get("term_colors")
+        if not colors:
+            return
+
+        if colors not in ("light", "dark"):
+            log.warning("Told to set colors to a theme we don't have\tgot=%s\thave=[light, dark]", colors)
+            return
+
+        # Haven't put much effort into actually working out more than just the message colour
+        if colors == "light":
+            self.logging_handler._column_color['%(message)s'][logging.INFO] = ('cyan', None, False)
+        else:
+            self.logging_handler._column_color['%(message)s'][logging.INFO] = ('blue', None, False)
+
+    ########################
+    ###   CONFIG
+    ########################
+
+    def home_dir_configuration(self):
+        """Return a dictionary from ~/.harpoon.yml"""
+        location = os.path.expanduser("~/.harpoon.yml")
+        if not os.path.exists(location):
+            return {}
+
+        result = self.read_yaml(location)
+        if not result:
+            result = {}
+        if not isinstance(result, dict):
+            raise BadYaml("Expected yaml file to declare a dictionary", location=location, got=type(result))
+
+        result["__mtime__"] = self.get_committime_or_mtime(location)
+        return result
+
+    def read_yaml(self, filepath):
+        """Read in a yaml file"""
+        try:
+            if os.stat(filepath).st_size == 0:
+                return {}
+            return yaml.load(open(filepath))
+        except yaml.parser.ParserError as error:
+            raise BadYaml("Failed to read yaml", location=filepath, error_type=error.__class__.__name__, error=error.problem)
+
+    def get_committime_or_mtime(self, location):
+        """Get the commit time of some file or the modified time of of it if can't get from git"""
+        date, status = command_output("git show -s --format=%at -n1 -- {0}".format(os.path.basename(location)), cwd=os.path.dirname(location))
+        if status == 0 and date:
+            return int(date[0])
+        else:
+            return os.path.getmtime(location)
+
+    def collect_configuration(self, configuration_file):
+        """Return us a MergedOptions with this configuration and any collected configurations"""
+        errors = []
+        collected = []
+        result = self.read_yaml(configuration_file)
+        result["__mtime__"] = self.get_committime_or_mtime(configuration_file)
+
+        configuration = MergedOptions.using(result)
+        configuration_dir = os.path.dirname(os.path.abspath(configuration_file))
+        if "images.__images_from__" in configuration:
+            images_from = MergedOptionStringFormatter(configuration, "images.__images_from__").format()
+            del configuration["images.__images_from__"]
+
+            if not images_from.startswith("/"):
+                images_from = os.path.join(configuration_dir, images_from)
+
+            if not os.path.exists(images_from) or not os.path.isdir(images_from):
+                raise BadConfiguration("Specified folder for other configuration files points to a folder that doesn't exist", path="images.__images_from__", value=images_from)
+
+            for root, dirs, files in os.walk(images_from):
+                for fle in files:
+                    if fle.endswith(".yml") or fle.endswith(".yaml"):
+                        location = os.path.join(root, fle)
+                        try:
+                            name = os.path.splitext(fle)[0]
+                            result = self.read_yaml(location)
+                            result["__mtime__"] = self.get_committime_or_mtime(location)
+                            collected.append({"images": {name: result}})
+                        except BadYaml as error:
+                            errors.append(error)
+
+        return MergedOptions.using(self.home_dir_configuration(), configuration, *collected)
+
+    ########################
+    ###   TASKS
+    ########################
+
+    def default_tasks(self):
+        """Return default tasks"""
+        def t(action, description, **options):
+            return (action, Task(action, description=description, options=options, label="Harpoon"))
+        return dict([
+              t("ssh", "Run bash in one of the containers", command="/bin/bash")
+            , t("run", "Run a command in one of the containers")
+
+            , t("make", "Make one of the images")
+            , t("make_all", "Make all of the images")
+            , t("make_pushable", "Make only the pushable images and their dependencies")
+
+            , t("pull", "Pull one of the images")
+            , t("pull_all", "Pull all of the images")
+
+            , t("push", "Push one of the images")
+            , t("push_all", "Push all of the images")
+
+            , t("show", "Show the available images")
+            , t("show_pushable", "Show the layers for only the pushable images")
+
+            , t("list_tasks", "List the available tasks")
+            , t("delete_untagged", "Delete untagged images")
+            ])
+
+    def interpret_tasks(self, configuration, path):
+        """Find the tasks in the specified key"""
+        if path not in configuration:
+            return {}
+
+        found = configuration.get(path)
+        tasks = HarpoonSpec().tasks_spec(available_tasks).normalise(Meta(configuration, path), found)
+
+        for key, task in tasks.items():
+            if task.options:
+                task_path = "{0}.{1}".format(path, key)
+                formatter = lambda s: MergedOptionStringFormatter(self.configuration, task_path, value=s).format()
+                task.options = dict((k, formatter(v)) for k, v in task.options.items())
+
+        return tasks
+
+    def find_tasks(self, configuration=None):
+        """Find some tasks"""
+        if configuration is None:
+            configuration = self.configuration
+
+        tasks = self.default_tasks()
+        tasks.update(self.interpret_tasks(configuration, "tasks"))
+        for image in configuration["images"]:
+            nxt = self.interpret_tasks(configuration, "images.{0}.tasks".format(image))
+            for task in nxt.values():
+                task.add_option_defaults(image=image)
+            tasks.update(nxt)
+
+        return tasks
+
---
+++ packages/docker-harpoon/docker-harpoon-0.3.26.tar.gz/docker-harpoon-0.3.26/harpoon/tasks.py
@@ -0,0 +1,166 @@
+from harpoon.errors import BadOption
+
+from docker.errors import APIError as DockerAPIError
+import itertools
+import logging
+import os
+
+log = logging.getLogger("harpoon.tasks")
+
+available_tasks = {}
+def a_task(func):
+    available_tasks[func.__name__] = func
+    return func
+
+def determine_image(harpoon, image):
+    """Complain if we don't have an image"""
+    available = None
+    try:
+        available = harpoon.imager.images.keys()
+    except:
+        pass
+
+    if image is None:
+        info = {}
+        if available:
+            info["available"] = available
+        raise BadOption("Please use --image to specify an image to run /bin/bash in", **info)
+
+    images = harpoon.imager.images
+    if image not in images:
+        raise BadOption("No such image", wanted=image, available=images.keys())
+
+    return images, image
+
+def find_missing_env(env):
+    """Find any missing environment variables"""
+    missing = []
+    if isinstance(env, list):
+        for thing in env:
+            if '=' not in thing and ":" not in thing:
+                if thing not in os.environ:
+                    missing.append(thing)
+
+    if missing:
+        raise BadOption("Some environment variables aren't in the current environment", missing=missing)
+
+@a_task
+def push(harpoon, image=None, **kwargs):
+    """Push an image"""
+    images, image = determine_image(harpoon, image)
+    pushable = dict((image, instance) for image, instance in images.items() if instance.heira_formatted("image_index", default=None))
+    if image not in pushable:
+        raise BadOption("The chosen image does not have a image_index configuration", wanted=image, available=pushable.keys())
+    harpoon.imager.make_image(image)
+    pushable[image].push()
+
+@a_task
+def push_all(harpoon, **kwargs):
+    """Push all the images"""
+    make_all(harpoon, push=True)
+
+@a_task
+def pull(harpoon, image=None, ignore_missing=False, **kwargs):
+    """Pull an image"""
+    images, image = determine_image(harpoon, image)
+    pullable = dict((image, instance) for image, instance in images.items() if instance.heira_formatted("image_index", default=None))
+    if image not in pullable:
+        raise BadOption("The chosen image does not have a image_index configuration", wanted=image, available=pullable.keys())
+    pullable[image].pull(ignore_missing=ignore_missing)
+
+@a_task
+def pull_all(harpoon, ignore_missing=False, **kwargs):
+    """Pull all the images"""
+    for layer in harpoon.imager.layered(only_pushable=True):
+        for image_name, image in layer:
+            log.info("Pulling %s", image_name)
+            image.pull(ignore_missing=ignore_missing)
+
+@a_task
+def make(harpoon, image=None, **kwargs):
+    """Just create an image"""
+    images, image = determine_image(harpoon, image)
+    harpoon.imager.make_image(image)
+    print("Created image {0}".format(images[image].image_name))
+
+@a_task
+def make_all(harpoon, push=False, only_pushable=False, **kwargs):
+    """Creates all the images in layered order"""
+    if push:
+        only_pushable = True
+
+    for layer in harpoon.imager.layered(only_pushable=only_pushable):
+        for image_name, image in layer:
+            harpoon.imager.make_image(image_name, ignore_deps=True)
+            print("Created image {0}".format(image.image_name))
+            if push and image.heira_formatted("image_index", default=None):
+                image.push()
+
+@a_task
+def make_pushable(harpoon, **kwargs):
+    """Make only the pushable images and their dependencies"""
+    make_all(harpoon, push=False, only_pushable=True)
+
+@a_task
+def run(harpoon, image=None, command=None, bash=None, env=None, volumes=None, ports=None, **kwargs):
+    """Run specified task in this image"""
+    if bash:
+        command = "/bin/bash -c '{0}'".format(bash)
+    _, image = determine_image(harpoon, image)
+    find_missing_env(env)
+    harpoon.imager.run(image, command=command, env=env, volumes=volumes, ports=ports)
+
+@a_task
+def list_tasks(harpoon, **kwargs):
+    """List the available_tasks"""
+    print("Available tasks to choose from are:")
+    print("Use the --task option to choose one")
+    print("")
+    keygetter = lambda item: item[1].label
+    tasks = sorted(harpoon.find_tasks().items(), key=keygetter)
+    for label, items in itertools.groupby(tasks, keygetter):
+        print("--- {0}".format(label))
+        print("----{0}".format("-" * len(label)))
+        sorted_tasks = sorted(list(items), key=lambda item: len(item[0]))
+        max_length = max(len(name) for name, _ in sorted_tasks)
+        for key, task in sorted_tasks:
+            print("\t{0}{1} :-: {2}".format(" " * (max_length-len(key)), key, task.description or ""))
+        print("")
+
+@a_task
+def delete_untagged(harpoon, **kwargs):
+    """Find the untagged images and remove them"""
+    images = harpoon.docker_context.images()
+    found = False
+    for image in images:
+        if image["RepoTags"] == ["<none>:<none>"]:
+            found = True
+            image_id = image["Id"]
+            log.info("Deleting untagged image\thash=%s", image_id)
+            try:
+                harpoon.docker_context.remove_image(image["Id"])
+            except DockerAPIError as error:
+                log.error("Failed to delete image\thash=%s\terror=%s", image_id, error)
+
+    if not found:
+        log.info("Didn't find any untagged images to delete!")
+
+@a_task
+def show(harpoon, only_pushable=False, flat=False, **kwargs):
+    """Show what images we have"""
+    for index, layer in enumerate(harpoon.imager.layered(only_pushable=only_pushable)):
+        if flat:
+            for _, image in layer:
+                print(image.image_name)
+        else:
+            print("Layer {0}".format(index))
+            for _, image in layer:
+                print("    {0}".format(image.display_line()))
+            print("")
+
+@a_task
+def show_pushable(harpoon, **kwargs):
+    """Show what images we have"""
+    kwargs['only_pushable'] = True
+    show(harpoon, **kwargs)
+
