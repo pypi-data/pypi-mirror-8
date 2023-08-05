3c171af63f3fb8809e26ff9e79e4b40dedbdcc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package snp-pipeline/0.2.0/snp-pipeline-0.2.0.tar.gz

---
+++ packages/snp-pipeline/snp-pipeline-0.2.0.tar.gz/snp-pipeline-0.2.0/scripts/alignSampleToReference.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+#
+#Directives for Portable Batch System (PBS) if HPC with Torque or equivalent is installed.
+#PBS -N job.alignSamples
+#PBS -m be
+#PBS -j oe
+#PBS -M user.name@fda.hhs.gov    #TODO Set this to be your email address
+#
+#Author: Hugh A. Rand (har)
+#        Steven C. Davis (scd)
+#Purpose: Aligns sample sequence data to reference for snppipline code.
+#Input:
+#    numBowtieThreads
+#    referenceDir/referenceName
+#    samplePath to fastq file
+#    [optional samplePath to mate fastq file if paired]
+#Output:
+#    SAM file
+#    written into the same directories containing the input sample fastq files
+#Use example:
+#   On workstation with one sample, unpaired
+#       alignSampleToReference.sh Users/NC_011149.fasta Users/ERR178926.fastq
+#   On workstation with one sample, paired
+#       alignSampleToReference.sh Users/NC_011149.fasta Users/ERR178926_1.fastq Users/ERR178926_2.fastq
+#   On workstation with multiple samples
+#       numCores=$(grep -c ^processor /proc/cpuinfo)
+#       ls -d samples/* | xargs ls -L -s -m | grep -E "(samples|total)" | sed 'N;s/\n//;s/:total//' | sort -k 2 -n -r | cut -f 1 -d " " > sampleDirectories.txt
+#       TMPFILE1=$(mktemp tmp.fastqs.XXXXXXXX)
+#       cat sampleDirectories.txt | while read dir; do echo $dir/*.fastq* >> $TMPFILE1; echo $dir/*.fq* >> $TMPFILE1; done
+#       grep -v '*.fq*' $TMPFILE1 | grep -v '*.fastq*' > sampleFullPathNames.txt
+#       rm $TMPFILE1
+#       cat sampleFullPathNames.txt | xargs --max-args=2 --max-lines=1 alignSampleToReference.sh -p $NUMCORES Users/NC_011149.fasta
+#   On a workstation with gnu parallel:
+#       cat sampleFullPathNames.txt | parallel alignSampleToReference.sh -p 1 reference/NC_011149.fasta
+#   With PBS
+#       qsub -d $PWD temp.sh ERR178926 NC_011149
+#       qsub -d $PWD temp1.sh
+#History:
+#   20140715-scd: Started - based on previous code copied from prepSamples.sh
+#   20140721-scd: Print the bowtie version and command line to facilitate troubleshooting
+#   20140903-scd: Use getopts to parse the command arguments.  Changed the calling convention.
+#   20140905-scd: Expects the full file name of the reference on the command line.
+#   20140910-scd: Outputs are not rebuilt when already fresh, unless the -f (force) option is specified.
+#Notes:
+#
+#Bugs:
+#
+
+
+usage()
+{
+    echo usage: $0 [-h] [-f] [-p INT] referenceFile sampleFastqFile1 [sampleFastqFile2]
+    echo
+    echo 'Align the sequence reads for a specified sample to a specified reference genome.'
+    echo 'The output is written to the file "reads.sam" in the sample directory.'
+    echo
+    echo 'Positional arguments:'
+    echo '  referenceFile    : Relative or absolute path to the reference fasta file'
+    echo '  sampleFastqFile1 : Relative or absolute path to the fastq file'
+    echo '  sampleFastqFile2 : Optional relative or absolute path to the mate fastq file, if paired'
+    echo
+    echo 'Options:'
+    echo '  -h               : Show this help message and exit'
+    echo '  -p INT           : Number of CPU cores to use concurrently during bowtie alignment (default: all)'
+    echo '  -f               : Force processing even when result files already exist and are newer than inputs'
+    echo
+}
+
+# --------------------------------------------------------
+# getopts command line option handler: 
+
+# For each valid option, 
+#   If it is given, create a var dynamically to
+#   indicate it is set: $opt_name_set = 1
+
+#   If var gets an arg, create another var to
+#   hold its value: $opt_name_arg = some value
+
+# For invalid options given, 
+#   Invoke Usage routine
+
+# precede option list with a colon
+# option list is a list of allowed option characters
+# options that require an arg are followed by a colon
+
+# example: ":abc:d"
+# -abc 14 -d
+
+while getopts ":p:hf" option; do
+  if [ "$option" = "h" ]; then
+    usage
+    exit 0
+  elif [ "$option" = "?" ]; then
+    echo
+    echo "Invalid option -- '$OPTARG'"
+    usage
+    exit 1
+  elif [ "$option" = ":" ]; then
+    echo
+    echo "Missing argument for option -- '$OPTARG'"
+    usage
+    exit 2
+  else
+    declare opt_"$option"_set="1"
+    if [ "$OPTARG" != "" ]; then
+      declare opt_"$option"_arg="$OPTARG"
+    fi
+  fi
+done
+
+# --------------------------------------------------------
+# get the arguments
+
+shift $((OPTIND-1))
+referenceFilePath="$1"
+if [ "$referenceFilePath" = "" ]; then
+  echo "Missing reference file"
+  echo
+  usage
+  exit 3
+fi
+referenceBasePath=${referenceFilePath%.fasta} # strip the file extension
+
+sampleFilePath1="$2"
+if [ "$sampleFilePath1" = "" ]; then
+  echo "Missing sample fastq file 1"
+  echo
+  usage
+  exit 4
+fi
+
+sampleFilePath2="$3"
+
+sampleDir=${sampleFilePath1%/*}
+sampleId=${sampleFilePath1##*/} # strip the directory
+sampleId=${sampleId%.gz*} # strip the .gz file extension if any
+sampleId=${sampleId%_1.fastq} # strip the _1.fastq file extension if any
+sampleId=${sampleId%.fastq} # strip the .fastq file extension regardless of leading _1
+sampleId=${sampleId%_1.fq} # strip the _1.fq file extension if any
+sampleId=${sampleId%.fq} # strip the .fq file extension regardless of leading _1
+referenceId=${referenceBasePath##*/} # strip the directory
+
+if [ "$opt_p_set" = "1" ]; then
+    numCores="$opt_p_arg"
+else
+    numCores=$(grep -c ^processor /proc/cpuinfo)
+fi
+
+# echo
+# echo numCores=$numCores
+# echo referenceFilePath=$referenceFilePath
+# echo sampleFilePath1=$sampleFilePath1
+# echo sampleFilePath2=$sampleFilePath2
+# if [ $sampleFilePath2 ]; then
+#     echo sampleFilePath2 exists
+# fi
+# echo sampleDir=$sampleDir
+# echo sampleId=$sampleId
+# echo referenceId=$referenceId
+# echo
+# exit 0 
+
+#Check if alignment to reference has been done; if not align sequences to reference
+if [ $sampleFilePath2 ]; then
+    if [[ "$opt_f_set" != "1" && "$sampleDir/reads.sam" -nt "$referenceBasePath.rev.1.bt2" && "$sampleDir/reads.sam" -nt "$sampleFilePath1" && "$sampleDir/reads.sam" -nt "$sampleFilePath2" ]]; then
+        echo "# $sampleId has already been aligned to $referenceId"
+    else
+        echo "# Align sequence $sampleId to reference $referenceId"
+        echo "# "$(date +"%Y-%m-%d %T") bowtie2 -p $numCores --reorder -q -x "$referenceBasePath" -1 "$sampleFilePath1" -2 "$sampleFilePath2"
+        echo "# "$(bowtie2 --version | grep -i -E "bowtie.*version")
+        bowtie2 -p $numCores --reorder -q -x $referenceBasePath -1 $sampleFilePath1 -2 $sampleFilePath2 > $sampleDir/'reads.sam'
+    fi
+else
+    if [[ "$opt_f_set" != "1" && "$sampleDir/reads.sam" -nt "$referenceBasePath.rev.1.bt2" && "$sampleDir/reads.sam" -nt "$sampleFilePath1" ]]; then
+        echo "# $sampleId has already been aligned to $referenceId"
+    else
+        echo "# Align sequence $sampleId to reference $referenceId"
+        echo "# "$(date +"%Y-%m-%d %T") bowtie2 -p $numCores --reorder -q -x "$referenceBasePath" "$sampleFilePath1"
+        echo "# "$(bowtie2 --version | grep -i -E "bowtie.*version")
+        bowtie2 -p $numCores --reorder -q -x "$referenceBasePath" "$sampleFilePath1" > "$sampleDir/reads.sam"
+    fi
+fi
+echo $(date +"# %Y-%m-%d %T") alignSampleToReference.sh finished
---
+++ packages/snp-pipeline/snp-pipeline-0.2.0.tar.gz/snp-pipeline-0.2.0/scripts/prepSamples.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+#
+#Directives for Portable Batch System (PBS) if HPC with Torque or equivalent is installed.
+#PBS -N job.prepSamples
+#PBS -m be
+#PBS -j oe
+#PBS -M user.name@fda.hhs.gov    #TODO Set this to be your email address
+#
+#Author: Hugh A. Rand (har)
+#        Steven C. Davis (scd)
+#Purpose: Preps sample sequence data for snppipline code.
+#Input:
+#    referenceDir/referenceName
+#    sampleDir
+#Output:
+#    various files too tedious to explain
+#    written into the same directories containing the input sample fastq files
+#Use example:
+#   On workstation with one sample, unpaired
+#       prepSamples.sh Users/NC_011149.fasta Users/ERR178926
+#   On workstation with multiple samples
+#       ls -d --color=never samples/* > sampleDirectoryNames.txt
+#       cat sampleDirectoryNames.txt | xargs -n 1 prepSamples.sh reference/NC_011149.fasta
+#   On a workstation with gnu parallel:
+#       cat sampleDirectoryNames.txt | parallel prepSamples.sh reference/NC_011149.fasta
+#   With PBS
+#       qsub -d $PWD temp.sh ERR178926 NC_011149
+#       qsub -d $PWD temp1.sh
+#History:
+#   20140512-har: Started.
+#   20140520-har: Download of sequence moved to different script.
+#   20140623-scd: Changes for varscan.
+#   20140715-scd: Moved the bowtie align to the alignSampleToReference.sh script.
+#   20140728-scd: Log the executed commands with all options to stdout.
+#   20140905-scd: Use getopts to parse the command arguments.  Improved online help.
+#   20140905-scd: Expects the full file name of the reference on the command line.
+#   20140910-scd: Outputs are not rebuilt when already fresh, unless the -f (force) option is specified.
+#Notes:
+#
+#Bugs:
+#   1. Should add prints to stdout to show progress to user
+#
+
+usage()
+{
+    echo usage: $0 [-h] [-f] referenceFile sampleDir
+    echo
+    echo 'Find variants in a specified sample.'
+    echo 'The output files are written to the sample directory.'
+    echo
+    echo 'Positional arguments:'
+    echo '  referenceFile    : Relative or absolute path to the reference fasta file'
+    echo '  sampleDir        : Relative or absolute directory of the sample'
+    echo
+    echo 'Options:'
+    echo '  -h               : Show this help message and exit'
+    echo '  -f               : Force processing even when result files already exist and are newer than inputs'
+    echo
+}
+
+# --------------------------------------------------------
+# getopts command line option handler: 
+
+# For each valid option, 
+#   If it is given, create a var dynamically to
+#   indicate it is set: $opt_name_set = 1
+
+#   If var gets an arg, create another var to
+#   hold its value: $opt_name_arg = some value
+
+# For invalid options given, 
+#   Invoke Usage routine
+
+# precede option list with a colon
+# option list is a list of allowed option characters
+# options that require an arg are followed by a colon
+
+# example: ":abc:d"
+# -abc 14 -d
+
+while getopts ":hf" option; do
+  if [ "$option" = "h" ]; then
+    usage
+    exit 0
+  elif [ "$option" = "?" ]; then
+    echo
+    echo "Invalid option -- '$OPTARG'"
+    usage
+    exit 1
+  elif [ "$option" = ":" ]; then
+    echo
+    echo "Missing argument for option -- '$OPTARG'"
+    usage
+    exit 2
+  else
+    declare opt_"$option"_set="1"
+    if [ "$OPTARG" != "" ]; then
+      declare opt_"$option"_arg="$OPTARG"
+    fi
+  fi
+done
+
+# --------------------------------------------------------
+# get the arguments
+
+shift $((OPTIND-1))
+referenceFilePath="$1"
+if [ "$referenceFilePath" = "" ]; then
+  echo "Missing reference file"
+  echo
+  usage
+  exit 3
+fi
+
+sampleDir="$2"
+if [ "$sampleDir" = "" ]; then
+  echo "Missing sample directory"
+  echo
+  usage
+  exit 4
+fi
+
+sampleId=${sampleDir##*/} # strip the parent directories
+
+# Check for fresh bam file; if not, convert to bam file with only mapped positions
+if [[ "$opt_f_set" != "1" && "$sampleDir/reads.unsorted.bam" -nt "$sampleDir/reads.sam" ]]; then
+    echo "# Unsorted bam file is already freshly created for $sampleId"
+else
+    echo "# Convert sam file to bam file with only mapped positions."
+    echo "# "$(date +"%Y-%m-%d %T") samtools view -bS -F 4 -o "$sampleDir/reads.unsorted.bam" "$sampleDir/reads.sam"
+    echo "# SAMtools "$(samtools 2>&1 > /dev/null | grep Version)
+    samtools view -bS -F 4 -o "$sampleDir/reads.unsorted.bam" "$sampleDir/reads.sam"
+fi
+
+
+# Check for fresh sorted bam file; if not, sort it
+if [[ "$opt_f_set" != "1" && "$sampleDir/reads.sorted.bam" -nt "$sampleDir/reads.unsorted.bam" ]]; then
+    echo "# Sorted bam file is already freshly created for $sampleId"
+else
+    #Convert to a sorted bam
+    echo "# Convert bam to sorted bam file."
+    echo "# "$(date +"%Y-%m-%d %T") samtools sort "$sampleDir/reads.unsorted.bam" "$sampleDir/reads.sorted"
+    echo "# SAMtools "$(samtools 2>&1 > /dev/null | grep Version)
+    samtools sort "$sampleDir/reads.unsorted.bam" "$sampleDir/reads.sorted"
+fi
+
+#Check for fresh pileup; if not, create it 
+if [[ "$opt_f_set" != "1" && "$sampleDir/reads.all.pileup" -nt "$sampleDir/reads.sorted.bam" && "$sampleDir/reads.all.pileup" -nt "$referenceFilePath" ]]; then
+    echo "# Pileup file is already freshly created for $sampleId"
+else
+    echo "# Create pileup from bam file."
+    echo "# "$(date +"%Y-%m-%d %T") samtools mpileup -f "$referenceFilePath" "$sampleDir/reads.sorted.bam"
+    echo "# SAMtools "$(samtools 2>&1 > /dev/null | grep Version)
+    samtools mpileup -f "$referenceFilePath" "$sampleDir/reads.sorted.bam" > "$sampleDir/reads.all.pileup"
+fi
+
+#Check for fresh unfiltered vcf; if not, create it
+if [[ "$opt_f_set" != "1" && "$sampleDir/var.flt.vcf" -nt "$sampleDir/reads.all.pileup" ]]; then
+    echo "# Vcf file is already freshly created for $sampleId"
+else
+    echo "# Create vcf file"
+    if [ ! -z "$CLASSPATH" ]; then
+        echo "# "$(date +"%Y-%m-%d %T") java net.sf.varscan.VarScan mpileup2snp "$sampleDir/reads.all.pileup" --min-var-freq 0.90 --output-vcf 1
+        echo "# "$(java net.sf.varscan.VarScan 2>&1 > /dev/null | head -n 1)
+        java net.sf.varscan.VarScan mpileup2snp "$sampleDir/reads.all.pileup" --min-var-freq 0.90 --output-vcf 1 > "$sampleDir/var.flt.vcf"
+    else
+        echo "*** Error: cannot execute VarScan. Define the path to VarScan in the CLASSPATH environment variable."
+        exit 2
+    fi
+fi
+
+echo "# "$(date +"%Y-%m-%d %T") prepSamples.sh finished
