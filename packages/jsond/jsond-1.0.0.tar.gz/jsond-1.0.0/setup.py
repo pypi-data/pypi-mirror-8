8a169270d6711457391c8291e76c529bebdb9418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package jsond/1.0.0/jsond-1.0.0.tar.gz

---
+++ packages/jsond/jsond-1.0.0.tar.gz/jsond-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+REQUIREMENTS = [
+    "nose==1.3.0",
+    "python-dateutil==1.5",
+]
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="jsond",
+        version="1.0.0",
+        author="EDITD",
+        author_email="engineering@editd.com",
+        packages=setuptools.find_packages(),
+        scripts=[],
+        url="https://github.com/EDITD/jsond",
+        license="LICENSE.txt",
+        description="JSON (with dates)",
+        long_description="View the github page (https://github.com/EDITD/jsond) for more details.",
+        install_requires=REQUIREMENTS
+    )
