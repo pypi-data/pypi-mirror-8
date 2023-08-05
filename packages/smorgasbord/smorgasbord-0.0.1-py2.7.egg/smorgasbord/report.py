e15f2b2c0706d79f29209c22b6e8b4cf78cf75c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package smorgasbord/0.0.1/smorgasbord-0.0.1-py2.7.egg

---
+++ packages/smorgasbord/smorgasbord-0.0.1-py2.7.egg/smorgasbord/report.py
@@ -0,0 +1,42 @@
+from __future__ import division
+import os
+from .language import Language
+from .coverage import Coverage
+from unicodeset import FrozenUnicodeSet
+
+
+class Report(object):
+
+    def __init__(self, characters, language_filepath):
+        self.language_filepath = language_filepath
+        self.characters = self._maybe_make_set(characters)
+        self.language = Language.parse(language_filepath)
+
+    @property
+    def coverage(self):
+        return Coverage(len(self.covered) / len(self.language.characters))
+
+    @property
+    def code(self):
+        return self.language.code
+
+    @property
+    def covered(self):
+        return self.language.characters & self.characters
+
+    @property
+    def uncovered(self):
+        return self.language.characters - self.characters
+
+    @property
+    def complete(self):
+        return not self.uncovered
+
+    @property
+    def incomplete(self):
+        return not self.complete
+
+    def _maybe_make_set(self, iterable):
+        if not isinstance(iterable, set) or isinstance(iterable, frozenset):
+            iterable = FrozenUnicodeSet(iterable)
+        return iterable
