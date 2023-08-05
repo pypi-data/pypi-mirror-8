b745613749f06d1cc0dbd359b870b396edb51ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:25 +0000

    Add package wispr/0.1/wispr-0.1.tar.gz

---
+++ packages/wispr/wispr-0.1.tar.gz/wispr-0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages
+from setuptools import setup
+
+version = '0.1'
+
+setup(
+        version=version,
+        name='wispr',
+        description='WISPr command line client',
+        long_description=open('README.rst').read(),
+        keywords='wispr wifi hotspot',
+        classifiers=[
+            'Environment :: Console',
+            'License :: DFSG approved',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Intended Audience :: End Users/Desktop',
+        ],
+        author='Wichert Akkerman',
+        author_email='wichert@wiggy.net',
+        url='https://github.com/wichert/wispr',
+        license='BSD',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        install_requires=[
+            'requests',
+        ],
+        entry_points='''
+        [console_scripts]
+        wispr = wispr:main
+        ''')
