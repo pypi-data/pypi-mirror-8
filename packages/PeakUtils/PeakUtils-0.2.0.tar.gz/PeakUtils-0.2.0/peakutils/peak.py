8f709a8e828ce3c427f4aff2c9582b1c8882b561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:18 +0000

    Add package PeakUtils/0.2.0/PeakUtils-0.2.0.tar.gz

---
+++ packages/PeakUtils/PeakUtils-0.2.0.tar.gz/PeakUtils-0.2.0/peakutils/peak.py
@@ -0,0 +1,148 @@
+'''Peak detection algorithms.'''
+
+import numpy as np
+from scipy import optimize
+
+def indexes(y, thres=0.3, min_dist=1):
+    '''Peak detection routine.
+
+    Finds the peaks in *y* by taking its first order difference. By using
+    *thres* and *min_dist* parameters, it is possible to reduce the number of
+    detected peaks.
+
+    Parameters
+    ----------
+    y : ndarray
+        1D amplitude data to search for peaks.
+    thres : float between [0., 1.]
+        Normalized threshold. Only the peaks with amplitude higher than the
+        threshold will be detected.
+    min_dist : int
+        Minimum distance between each detected peak. The peak with the highest
+        amplitude is preferred to satisfy this constraint.
+
+    Returns
+    -------
+    ndarray
+        Array containing the indexes of the peaks that were detected
+    '''
+    thres *= np.max(y) - np.min(y)
+
+    # find the peaks by using the first order difference
+    dy = np.diff(y)
+    peaks = np.where((np.hstack([dy, 0.]) < 0.)
+                     & (np.hstack([0., dy]) > 0.)
+                     & (y > thres))[0]
+
+    if peaks.size > 1 and min_dist > 1:
+        new_peaks = []
+        top = peaks[0]
+        padding = y.size+min_dist+1
+
+        for p in np.append(peaks, padding):
+            if (p - top) <= min_dist: # still in the same group?
+                if y[p] > y[top]:
+                    top = p
+            else: # end, start another 'group'
+                new_peaks.append(top)
+                top = p
+
+        peaks = np.array(new_peaks)
+
+    return peaks
+
+def centroid(x, y):
+    '''Computes the centroid for the specified data.
+
+    Parameters
+    ----------
+    x : ndarray
+        Data on the x axis.
+    y : ndarray
+        Data on the y axis.
+
+    Returns
+    -------
+    float
+        Centroid of the data.
+    '''
+    return np.sum(x*y)/np.sum(y)
+
+def gaussian(x, ampl, center, dev):
+    '''Computes the Gaussian function.
+
+    Parameters
+    ----------
+    x : float
+        Point to evaluate the Gaussian for.
+    a : float
+        Amplitude.
+    b : float
+        Center.
+    c : float
+        Width.
+
+    Returns
+    -------
+    float
+        Value of the specified Gaussian at *x*
+    '''
+    return ampl * np.exp(-(x-center)**2 / (2*dev**2))
+
+def gaussian_fit(x, y):
+    '''Performs a Gaussian fitting of the specified data.
+
+    Parameters
+    ----------
+    x : ndarray
+        Data on the x axis.
+    y : ndarray
+        Data on the y axis.
+
+    Returns
+    -------
+    ndarray
+        Parameters of the Gaussian that fits the specified data
+    '''
+    initial = [np.max(y), x[0], (x[1]-x[0])*5]
+    params, *_ = optimize.curve_fit(gaussian, x, y, initial)
+    return params[1]
+
+def interpolate(x, y, ind=None, width=10, func=gaussian_fit):
+    '''Tries to enhance the resolution of the peak detection by using
+    Gaussian fitting, centroid computation or an arbitrary function on the
+    neighborhood of each previously detected peak index.
+
+    Parameters
+    ----------
+    x : ndarray
+        Data on the x dimension.
+    y : ndarray
+        Data on the y dimension.
+    ind : ndarray
+        Indexes of the previously detected peaks. If None, indexes() will be
+        called with the default parameters.
+    width : int
+        Number of points (before and after) each peak index to pass to *func*
+        in order to encrease the resolution in *x*.
+    func : function(x,y)
+        Function that will be called to detect an unique peak in the x,y data.
+
+    Returns
+    -------
+    ndarray :
+        Array with the adjusted peak positions (in *x*)
+    '''
+
+    if ind is None:
+        ind = indexes(y)
+
+    out = []
+    for slice_ in (slice(i-width, i+width) for i in ind):
+        try:
+            fit = func(x[slice_], y[slice_])
+            out.append(fit)
+        except:
+            pass
+
+    return np.array(out)
---
+++ packages/PeakUtils/PeakUtils-0.2.0.tar.gz/PeakUtils-0.2.0/tests/unit.py
@@ -0,0 +1,97 @@
+import unittest
+import peakutils
+import os
+import numpy
+from numpy.testing import assert_array_almost_equal
+import scipy.signal
+
+def load(name):
+    p = os.path.join(os.path.dirname(__file__), name)
+    return numpy.loadtxt(p)
+
+class ExperimentalData(unittest.TestCase):
+    '''Tests with experimental data'''
+    def setUp(self):
+        self.data = []
+        self.data.append( (load('noise'), 8) )
+        self.data.append( (load('baseline'), 2) )
+
+    def test_peaks(self):
+        for data in self.data:
+            y, n_peaks = data[0][:,1], data[1]
+            filtered = scipy.signal.savgol_filter(y, 21, 1)
+            idx = peakutils.indexes(filtered, thres=0.08, min_dist=50)
+
+            for p in range(idx.size, 1):
+                self.assertGreater(idx[p], 0)
+                self.assertLess(idx[p], idx.size-1)
+                self.assertGreater(idx[p], idx[p-1])
+
+            self.assertEqual(idx.size, n_peaks)
+
+class SimulatedData(unittest.TestCase):
+    '''Tests with simulated data (3 peaks + baseline + nois)'''
+    def setUp(self):
+        self.x = numpy.linspace(0, 100, 1000)
+        self.centers = (20, 40, 70)
+        self.y = peakutils.gaussian(self.x, 1, self.centers[0], 3) + \
+                 peakutils.gaussian(self.x, 2, self.centers[1], 5) + \
+                 peakutils.gaussian(self.x, 3, self.centers[2], 1) + \
+                 numpy.random.random(self.x.size) * 0.2
+
+        self.y_base = self.y + numpy.polyval([2., -3., 5.], self.x)
+
+    def test_peaks(self):
+        filtered = scipy.signal.savgol_filter(self.y, 51, 3)
+        idx = peakutils.indexes(filtered, thres=0.3, min_dist=100)
+        peaks = peakutils.interpolate(self.x, self.y, idx, width=30)
+        self.assertEqual(idx.size, len(self.centers))
+        self.assertEqual(peaks.size, len(self.centers))
+
+        # interpolation should work!
+        for i in range(peaks.size):
+            self.assertAlmostEqual(peaks[i], self.centers[i], delta=0.5)
+
+    def test_baseline(self):
+        y = self.y_base - peakutils.baseline(self.y_base, 2)
+        self.assertGreater(numpy.linalg.norm(self.y_base)*0.1,
+                           numpy.linalg.norm(y))
+
+        filtered = scipy.signal.savgol_filter(y, 51, 3)
+        idx = peakutils.indexes(filtered, thres=0.25, min_dist=100)
+        self.assertEqual(idx.size, 3)
+
+        for i, ind in enumerate(idx):
+            self.assertAlmostEqual(self.x[ind], self.centers[i], delta=2.)
+
+class Baseline(unittest.TestCase):
+    '''Tests the conditioning of the lsqreg in the implementation'''
+    def setUp(self):
+        self.data = load('exp')
+
+    def test_conditioning(self):
+        _, y = self.data[:,0], self.data[:,1]
+        mult = 1e-6
+
+        while mult < 100001:
+            ny = y * mult
+            base = peakutils.baseline(ny, 9) / mult
+            self.assertTrue(0.8 < base.max() < 1.0)
+            self.assertTrue(-0.1 <= base.min() < 0.1)
+            mult *= 10
+
+class Prepare(unittest.TestCase):
+    '''Tests the prepare module'''
+    def setUp(self):
+        self.orig = numpy.array([-2, -1, 0.5, 1, 3])
+
+    def test_scale(self):
+        x1, range_old = peakutils.scale(self.orig, (-10, 8))
+        x2, range_new = peakutils.scale(x1, range_old)
+
+        assert_array_almost_equal(self.orig, x2)
+        self.assertTupleEqual(range_new, (-10, 8))
+
+if __name__ == '__main__':
+    numpy.random.seed(1997)
+    unittest.main()
