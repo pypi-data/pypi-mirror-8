ff8bf29102649f570836617a868ad2f9f7158c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:48 +0000

    Add package Flask-Dissect/1.0.3/Flask-Dissect-1.0.3.tar.gz

---
+++ packages/Flask-Dissect/Flask-Dissect-1.0.3.tar.gz/Flask-Dissect-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='Flask-Dissect',
+    version='1.0.3',
+    url='http://bit.epix.com.br/flask-dissect/',
+    license='BSD',
+    author='Leandro Martelli',
+    author_email='martelli@epix.com.br',
+    description='Dissect Distributed Session Control',
+    long_description=__doc__,
+    packages=['flask_dissect'],
+    platforms='any',
+    install_requires=[
+        'Flask', 'requests'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
