42c001a5aa6f22b18790acaf06a7d088959239f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package otree-core/0.2.173/otree-core-0.2.173.zip

---
+++ packages/otree-core/otree-core-0.2.173.zip/otree-core-0.2.173/otree/views/__init__.py
@@ -0,0 +1,64 @@
+"""public api"""
+
+from otree.views.abstract import (
+    InitializePlayer,
+    InitializeExperimenter,
+)
+
+from importlib import import_module
+abstract = import_module('otree.views.abstract')
+
+class MatchWaitPage(abstract.MatchCheckpoint):
+
+    def body_text(self):
+        return super(MatchWaitPage, self).body_text()
+
+    def title_text(self):
+        return super(MatchWaitPage, self).title_text()
+
+    def after_all_players_arrive(self):
+        return super(MatchWaitPage, self).after_all_players_arrive()
+
+class SubsessionWaitPage(abstract.SubsessionCheckpoint):
+
+    def body_text(self):
+        return super(SubsessionWaitPage, self).body_text()
+
+    def title_text(self):
+        return super(SubsessionWaitPage, self).title_text()
+
+    def after_all_players_arrive(self):
+        return super(SubsessionWaitPage, self).after_all_players_arrive()
+
+class Page(abstract.PlayerUpdateView):
+    # R: This methods do nothing, could be just deleted
+
+    def variables_for_template(self):
+        return super(Page, self).variables_for_template()
+
+    def after_valid_form_submission(self):
+        return super(Page, self).after_valid_form_submission()
+
+    def get_form_class(self):
+        return super(Page, self).get_form_class()
+
+    def participate_condition(self):
+        return super(Page, self).participate_condition()
+
+    template_name = None
+
+class ExperimenterPage(abstract.ExperimenterUpdateView):
+
+    def variables_for_template(self):
+        return super(ExperimenterPage, self).variables_for_template()
+
+    def after_valid_form_submission(self):
+        return super(ExperimenterPage, self).after_valid_form_submission()
+
+    def get_form_class(self):
+        return super(ExperimenterPage, self).get_form_class()
+
+    def participate_condition(self):
+        return super(ExperimenterPage, self).participate_condition()
+
+    template_name = None
