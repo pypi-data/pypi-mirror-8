79ae373eb09ce7ef8659310a4687b1a5d191a266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:54 +0000

    Add package dbaas-credentials/0.9.8/dbaas_credentials-0.9.8.tar.gz

---
+++ packages/dbaas-credentials/dbaas_credentials-0.9.8.tar.gz/dbaas_credentials-0.9.8/dbaas_credentials/admin/integration_credential.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from django.contrib import admin
+
+
+class CredentialAdmin(admin.ModelAdmin):
+    search_fields = ("endpoint",)
+    list_display = ("integration_type", "environment", "endpoint", "user", )
+    filter_horizontal = ("environments",)
+    save_on_top = True
