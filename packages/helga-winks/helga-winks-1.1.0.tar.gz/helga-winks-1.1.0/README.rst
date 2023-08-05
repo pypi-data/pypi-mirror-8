eea52effc2020ef6becfd9686f34e1f085b2c419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package helga-winks/1.1.0/helga-winks-1.1.0.tar.gz

---
+++ packages/helga-winks/helga-winks-1.1.0.tar.gz/helga-winks-1.1.0/README.rst
@@ -0,0 +1,10 @@
+helga-winks
+===========
+~*~*~* winks @ u *~*~*~
+
+License
+-------
+
+Copyright (c) 2014 Michael Orr
+
+GPLv3 Licensed
---
+++ packages/helga-winks/helga-winks-1.1.0.tar.gz/helga-winks-1.1.0/helga_winks.py
@@ -0,0 +1,14 @@
+from helga.plugins import command
+
+
+@command('wink', aliases=['winks'], help='winks @ u')
+def winks(client, channel, nick, message, cmd, args):
+    """
+    ~*~*~* winks @ u *~*~*~
+    """
+    if len(args) == 0:
+        return u'~*~*~* {0} winks @ u *~*~*~'.format(nick)
+    elif len(args) == 1:
+        return u'~*~*~* {0} winks @ {1} *~*~*~'.format(nick, args[0])
+    else:
+        return u'~*~*~* {0} {1} *~*~*~'.format(nick, ' '.join(args))
