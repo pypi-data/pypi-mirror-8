c46e6d83a7ea1a65986ce80c183e53bd9990d371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:03 +0000

    Add package warouter/0.0.3/warouter-0.0.3.tar.gz

---
+++ packages/warouter/warouter-0.0.3.tar.gz/warouter-0.0.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+
+setup(
+    name='warouter',
+    version='0.0.3',
+    author='Remco Haszing',
+    author_email='remcohaszing@gmail.com',
+    url='https://github.com/remcohaszing/python-warouter',
+    description='Warouter is a simple routing wrapper around webapp2.',
+    long_description=readme,
+    license='MIT',
+    py_modules=['warouter'],
+    install_requires=[
+        'webapp2',
+        'webob'
+    ],
+    tests_require=[
+        'mock'
+    ],
+    test_suite='tests',
+    zip_safe=True)
