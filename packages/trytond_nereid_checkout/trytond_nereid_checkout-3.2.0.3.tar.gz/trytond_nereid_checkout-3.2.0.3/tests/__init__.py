75d7478a2293b4339454d98a199f89f7a4ebf386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:55 +0000

    Add package trytond_nereid_checkout/3.2.0.3/trytond_nereid_checkout-3.2.0.3.tar.gz

---
+++ packages/trytond_nereid_checkout/trytond_nereid_checkout-3.2.0.3.tar.gz/trytond_nereid_checkout-3.2.0.3/tests/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    Add the test suite here so that setup.py test loader picks it up
+
+    :copyright: © 2013 by Openlabs Technologies & Consulting (P) Limited
+    :license: GPLv3, see LICENSE for more details.
+"""
+# Flake8: noqa
+from .test_checkout import suite
