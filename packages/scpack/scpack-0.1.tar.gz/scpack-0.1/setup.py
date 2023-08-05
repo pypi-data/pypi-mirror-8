cec3a90c4444dec9c38a688af15d3775ad08e0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:56 +0000

    Add package scpack/0.1/scpack-0.1.tar.gz

---
+++ packages/scpack/scpack-0.1.tar.gz/scpack-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'scpack',
+  packages = ['scpack'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Scott Ivey',
+  author_email = 'scott.ivey@gmail.com',
+  url = 'https://github.com/scivey/scpack', # use the URL to the github repo
+  # download_url = 'https://github.com/scivey/scpack/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
