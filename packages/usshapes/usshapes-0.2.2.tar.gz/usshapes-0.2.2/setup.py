1e0494171f7f0f0d6d5e80c2c62bf69b3bdc7775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:48 +0000

    Add package usshapes/0.2.2/usshapes-0.2.2.tar.gz

---
+++ packages/usshapes/usshapes-0.2.2.tar.gz/usshapes-0.2.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='usshapes',
+      version='0.2.2',
+      description='U.S. Shapefile Indexer for Elasticsearch',
+      author='Daniel Sarfati',
+      author_email='daniel@knockrentals.com',
+      url='https://github.com/fatisar/us-shapes',
+      packages=['usshapes'],
+      install_requires=['pyes', 'beautifulsoup4']
+)
