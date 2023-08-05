40ae14d7e5ae39a03ba33eb26df11af9916eb00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:23 +0000

    Add package strict-rfc3339/0.5/strict-rfc3339-0.5.tar.gz

---
+++ packages/strict-rfc3339/strict-rfc3339-0.5.tar.gz/strict-rfc3339-0.5/setup.py
@@ -0,0 +1,26 @@
+import os.path
+from distutils.core import setup
+
+readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+readme = open(readme_file).read()
+
+setup(
+    name='strict-rfc3339',
+    version='0.5',
+    author='Daniel Richman, Adam Greig',
+    author_email='main@danielrichman.co.uk',
+    url='http://www.danielrichman.co.uk/projects/strict-rfc3339.html',
+    py_modules=['strict_rfc3339'],
+    description="Strict, simple, lightweight RFC3339 functions",
+    long_description=readme,
+    license="GNU General Public License Version 3",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
+)
