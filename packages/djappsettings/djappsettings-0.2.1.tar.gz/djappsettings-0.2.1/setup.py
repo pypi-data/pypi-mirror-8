375bf68ad4635f5b3666b6f501291ba357f5752f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package djappsettings/0.2.1/djappsettings-0.2.1.tar.gz

---
+++ packages/djappsettings/djappsettings-0.2.1.tar.gz/djappsettings-0.2.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# vim: et ts=4 sw=4
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="djappsettings",
+    version="0.2.1",
+    license="BSD",
+
+    author="Adam Mckaig",
+    author_email="adam.mckaig@gmail.com",
+
+    description="Per-app default settings for Django",
+    url="http://github.com/adammck/djappsettings",
+    packages=find_packages())
