1e1c0d0f55f317aaadd8ac81ae31042382aca999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package django-pods/1.0/django-pods-1.0.tar.gz

---
+++ packages/django-pods/django-pods-1.0.tar.gz/django-pods-1.0/tests/mocks/app_test_settings.py
@@ -0,0 +1,14 @@
+DEFAULT = 10
+
+DICT_STYLE_OVERRIDE = False
+
+PREFIX_STYLE_OVERRIDE = False
+
+APP_MODULE = "tests.mocks.app_test_imports.TestClass"
+
+MULTIPLE_APP_MODULES = (
+    "tests.mocks.app_test_imports.TestClass",
+    "tests.mocks.app_test_imports.OtherTestClass"
+)
+
+NON_EXISTENT_APP_MODULE = "tests.mocks.app_test_imports.DoesNotExist"
