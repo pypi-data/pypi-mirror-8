9b83228fe54145aceaae88075a7e5dc584e155fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package Flask-GCM/0.1.0/Flask-GCM-0.1.0.tar.gz

---
+++ packages/Flask-GCM/Flask-GCM-0.1.0.tar.gz/Flask-GCM-0.1.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+Setup script for Flask-GCM.
+"""
+
+import setuptools
+
+import os
+if os.path.exists('README.rst'):
+    README = open('README.rst').read()
+else:
+    README = ""  # a placeholder, readme is generated on release
+CHANGES = open('CHANGES.md').read()
+
+
+setuptools.setup(
+    name='Flask-GCM',
+    version='0.1.0',
+
+    description="Flask-GCM is a simple wrapper for the python-gcm library to be used with Flask applications.",
+    url='https://github.com/michiganlabs/flask-gcm',
+    author='Josh Friend',
+    author_email='jfriend@michiganlabs.com',
+
+    packages=setuptools.find_packages(),
+
+    entry_points={'console_scripts': []},
+
+    long_description=(README + '\n' + CHANGES),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    install_requires=open('requirements.txt').readlines(),
+)
