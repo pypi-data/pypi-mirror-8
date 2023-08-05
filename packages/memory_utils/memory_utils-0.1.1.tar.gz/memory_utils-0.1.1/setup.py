3aff74c736eab37f8bffe44f9e979485cc61f326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package memory_utils/0.1.1/memory_utils-0.1.1.tar.gz

---
+++ packages/memory_utils/memory_utils-0.1.1.tar.gz/memory_utils-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+packages = ['memory_utils']
+required_modules = [
+    "colorama==0.3.2",
+    "psutil==2.1.1",
+    "six==1.8.0"
+]
+
+
+setup(name='memory_utils',
+      version='0.1.1',
+      description='Tools to help with memory leaks',
+      url='http://github.com/jtushman/memory_utils',
+      author='Jonathan Tushman',
+      author_email='jonathan@zefr.com',
+      install_requires=required_modules,
+      license='MIT',
+      packages=packages,
+      zip_safe=False,
+      tests_require=['nose'],
+      test_suite='nose.collector',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+      ]
+)
