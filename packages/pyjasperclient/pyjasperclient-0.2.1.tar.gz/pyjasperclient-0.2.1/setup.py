176c12fe4b92e62f80066c4c01846c903e3e163f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:11 +0000

    Add package pyjasperclient/0.2.1/pyjasperclient-0.2.1.tar.gz

---
+++ packages/pyjasperclient/pyjasperclient-0.2.1.tar.gz/pyjasperclient-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+name = "pyjasperclient"
+version = "0.2.1"
+
+setup(
+    name=name,
+    version=version,
+    description='JasperServer SOAP client for Python',
+    packages=['pyjasperclient',],
+    install_requires = ['suds','elementtree'],
+    url='https://github.com/agaoglu/pyjasperclient',
+    license='Apache',
+    author='Erdem Agaoglu',
+    author_email='erdem[dot]agaoglu[at]gmail.com'
+)
