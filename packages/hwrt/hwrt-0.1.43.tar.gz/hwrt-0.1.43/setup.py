61f7c7c2a64ccb52f77b69761c3116deda2c40d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package hwrt/0.1.43/hwrt-0.1.43.tar.gz

---
+++ packages/hwrt/hwrt-0.1.43.tar.gz/hwrt-0.1.43/setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'hwrt',
+    'version': '0.1.43',
+    'author': 'Martin Thoma',
+    'author_email': 'info@martin-thoma.de',
+    'packages': ['hwrt'],
+    'scripts': ['bin/backup.py', 'bin/get_top_n_error.py', 'bin/view.py',
+                'bin/download.py', 'bin/test.py', 'bin/train.py',
+                'bin/analyze_data.py', 'bin/hwrt'],
+    'package_data': {'hwrt': ['templates/*']},
+    'url': 'https://github.com/MartinThoma/hwrt',
+    'license': 'MIT',
+    'description': 'Handwriting Recognition Tools',
+    'long_description': """A tookit for handwriting recognition. It was
+    developed as part of the bachelors thesis of Martin Thoma.""",
+    'install_requires': [
+        "argparse",
+        "theano",
+        "nose",
+    ],
+    'keywords': ['HWRT', 'recognition', 'handwriting', 'on-line'],
+    'download_url': 'https://github.com/MartinThoma/hwrt',
+}
+
+setup(**config)
