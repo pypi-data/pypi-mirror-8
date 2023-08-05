fa08b74378c66ff6c3ee479573eeb9662348f805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package notebooktools/0.0.7/notebooktools-0.0.7.tar.gz

---
+++ packages/notebooktools/notebooktools-0.0.7.tar.gz/notebooktools-0.0.7/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+version = '0.0.7'
+
+setup(name='notebooktools',
+      version=version,
+      description='Tools for IPython Notebook used with Tellurium',
+      author='Stanley Gu',
+      author_email='stanleygu@gmail.com',
+      url='https://github.com/stanleygu/ipython-notebook-modules',
+      packages=['notebooktools', 'diffevolution']
+      )
