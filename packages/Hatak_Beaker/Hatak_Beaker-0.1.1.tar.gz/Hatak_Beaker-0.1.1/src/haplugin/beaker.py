d30f86a72a86b7397d8f52ea24610364490097c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:52 +0000

    Add package Hatak_Beaker/0.1.1/Hatak_Beaker-0.1.1.tar.gz

---
+++ packages/Hatak_Beaker/Hatak_Beaker-0.1.1.tar.gz/Hatak_Beaker-0.1.1/src/haplugin/beaker.py
@@ -0,0 +1,10 @@
+from hatak.plugin import Plugin
+
+
+class BeakerPlugin(Plugin):
+
+    def get_include_name(self):
+        return 'pyramid_beaker'
+
+    def add_unpackers(self, unpacker):
+        unpacker.add('session', lambda req: req.session)
