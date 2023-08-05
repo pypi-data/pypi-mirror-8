571853c7dca1e93717fe6e08330a9eba2b5fc084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:38 +0000

    Add package linkmanager/0.4.0.11/linkmanager-0.4.0.11.tar.gz

---
+++ packages/linkmanager/linkmanager-0.4.0.11.tar.gz/linkmanager-0.4.0.11/README.rst
@@ -0,0 +1,88 @@
+.. image:: https://badge.fury.io/py/linkmanager.png
+    :target: http://badge.fury.io/py/linkmanager
+
+.. image:: https://pypip.in/d/linkmanager/badge.png
+    :target: https://pypi.python.org/pypi/linkmanager
+
+.. image:: https://travis-ci.org/mothsART/linkmanager.png?branch=master
+   :target: https://travis-ci.org/mothsART/linkmanager
+
+.. image:: https://coveralls.io/repos/mothsART/linkmanager/badge.png?branch=master
+    :target: https://coveralls.io/r/mothsART/linkmanager?branch=master
+
+::
+
+    _*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
+    *_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*
+
+           _
+          |_|
+      _    _ __    _ _   __   __      ___ ___  __    __ ___   ______  ______ ______
+     | |  | |  \  | | | / /  |  \    /  |/ _ \|  \  | |/ _ \ /  ____|| _____|  __  \
+     | |  | |   \ | | |/ / _ |   \  /   | |_| |   \ | | |_| |  /  ___|  |___| |__| |
+     | |  | | |\ \| |   \ |_|| |\ \/ /| |  _  | |\ \| |  _  | |  |_ _|  ___||     _/
+     | |__| | | \   | |\ \   | | \  / | | | | | | \   | | | |  \__| ||  |___| |\  \
+     |____|_|_|  \__|_| \_\  |_|  \/  |_|_| |_|_|  \__|_| |_|\______/|______|_| \__\
+
+
+                                LinkManager 0.4.0.11
+
+    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    *_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+
+**LinkManager** manage your link on terminal.
+
+Replace bookmark tool present on browser because :
+    * is often heavy
+    * dependent of the browser in question
+    * has a lot of frills
+    * DataBase usage depend on browser
+    * find a local link should not require several hundred MB of Ram and eat your CPU
+    * one software for one thing (Unix Philosophy)
+    * KISS for import/export
+    * many other good reasons
+
+
+Requirements
+------------
+
+Linkmanager depends on **redis** Database and GIT (personnal "clint" version).
+You must install it like this (on debian/ubuntu) :
+
+.. code-block:: bash
+
+    $ sudo apt-get install redis-server git
+
+To enjoy "completion", you normaly should do nothing.
+A "sudo pip install" should add it automatically.
+Otherwise, just put that line in your ~/.bashrc (or ~/.zshrc) :
+
+.. code-block:: bash
+
+    $ eval "$(register-python-argcomplete linkm)"
+
+
+Examples
+--------
+
+.. code-block:: bash
+
+    $ linkm add http://stackoverflow.com # add a link on Database
+    $ linkm update http://stackoverflow.com # update properties on a existent link
+    $ linkm remove http://stackoverflow.com # remove a link on DataBase
+    $ linkm search python linux # search a link a link on DataBase with tags
+    $ linkm dump >| backup.json # serialize a entire Database on a JSON file
+    $ linkm load backup.json # load a list of links on DataBase
+    $ linkm flush # erase all DataBase
+
+when you add/edit links, the "shell" will ask you for each one to edit tags, priority value and a description.
+
+.. code-block:: bash
+
+    $ linkm add http://djangoproject.com
+        "http://djangoproject.com" properties :
+            tags (separate with ",") : django, python, framework
+            priority value (integer value between 1 and 10) : 5
+            give a description : Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design.
---
+++ packages/linkmanager/linkmanager-0.4.0.11.tar.gz/linkmanager-0.4.0.11/linkmserver/__init__.py
@@ -0,0 +1,166 @@
+import os
+import subprocess
+import json
+
+import arrow
+
+from flask import (
+    Flask, render_template, abort,
+    request, jsonify
+)
+from flask.ext.assets import Environment
+# from werkzeug.debug import get_current_traceback
+
+from linkmanager import settings
+from linkmanager.db import DataBase
+
+app = Flask(__name__)
+assets = Environment(app)
+if settings.SERVER:
+    assets.directory = '/var/cache/linkmanager/static'
+    os.makedirs('/var/cache/linkmanager/static', mode=0o755)
+
+db = DataBase()
+
+# Decorator : get an Unauthorize 403 when read only's settings is True
+def read_only(func):
+    def wrapper():
+        if settings.READ_ONLY:
+            return abort(403)
+        return func()
+    return wrapper
+
+
+def launch_browser(BROWSER=False):
+    subprocess.call(
+        'sleep 0.5;nohup %s http://127.0.0.1:%s/ &' % (
+            BROWSER,
+            settings.HTTP_PORT
+        ),
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        shell=True
+    )
+
+
+@app.route("/")
+def index():
+    return render_template(
+        'index.html',
+        DEBUG=settings.DEBUG,
+        read_only=settings.READ_ONLY,
+        nb_links=len(db)
+    )
+    # try:
+    #     error
+    # except Exception:
+    #     track = get_current_traceback(
+    #         skip=1, show_hidden_frames=True,
+    #         ignore_system_exceptions=False
+    #     )
+    #     track.log()
+    #     abort(500)
+
+
+@app.route("/editmode", methods=['GET', 'POST'])
+@read_only
+def editmode():
+    if request.method == 'GET':
+        return jsonify({'editmode': True})
+    editmode = not json.loads(request.form['editmode'])
+    return jsonify({'editmode': editmode})
+
+
+@read_only
+@app.route("/add", methods=['POST'])
+def add():
+    fixture = {}
+    link = request.form['link']
+    fixture[link] = {
+        "tags": request.form['tags'].split(),
+        "priority": request.form['priority'],
+        "description": request.form['description'],
+        "title": request.form['title'],
+        "init date": str(arrow.now())
+    }
+    result = db.add_link(json.dumps(fixture))
+    return jsonify({'is_add': result})
+
+
+@read_only
+@app.route("/update", methods=['POST'])
+def update():
+    fixture = {}
+    link = request.form['link']
+    if request.form['link'] != request.form['newlink']:
+        result = db.delete_link(request.form['link'])
+        if not result:
+            return jsonify({'is_update': False})
+        link = request.form['newlink']
+    old_link = db.get_link_properties(link)
+    fixture[link] = {
+        "tags": request.form['tags'].split(),
+        "priority": request.form['priority'],
+        "description": request.form['description'],
+        "title": request.form['title'],
+        "init date": old_link['init date'],
+        "update date": str(arrow.now())
+    }
+    if request.form['link'] != request.form['newlink']:
+        fixture[link]["init date"] = str(arrow.now())
+        fixture[link]["update date"] = old_link['update date']
+
+    result = db.add_link(json.dumps(fixture))
+    return jsonify({'is_update': result})
+
+
+@read_only
+@app.route("/delete", methods=['POST'])
+def delete():
+    result = db.delete_link(request.form['link'])
+    return jsonify({'is_delete': result})
+
+
+@app.route("/search")
+def search():
+    results = {}
+    try:
+        tags = next(request.args.items())[0].split()
+        links = db.sorted_links(*tags)
+    except:
+        links = db.sorted_links()
+
+    results = {}
+    for l in links:
+        properties = db.get_link_properties(l)
+        results[l] = properties
+    return jsonify(**results)
+
+
+@app.route("/suggest")
+def suggest():
+    tags = request.args.get('tags')
+    if not tags:
+        return jsonify({})
+    keywords = tags.split()
+    last_keyword = keywords[len(keywords) - 1]
+    str_suggestion = ' '.join(keywords[:-1])
+
+    suggestions = {}
+    for s in db.complete_tags(last_keyword):
+        if s not in keywords:
+            suggestions[str_suggestion + ' ' + s] = 10
+    return jsonify(**suggestions)
+
+
+def run(browser=None):
+    BROWSER = settings.BROWSER
+    if browser:
+        BROWSER = browser
+    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
+        launch_browser(BROWSER)
+    app.debug = settings.DEBUG
+    app.run(host=settings.HTTP_HOST, port=settings.HTTP_PORT)
+
+if __name__ == '__main__':
+    app.debug = settings.DEBUG
+    app.run(host=settings.HTTP_HOST, port=settings.HTTP_PORT)
