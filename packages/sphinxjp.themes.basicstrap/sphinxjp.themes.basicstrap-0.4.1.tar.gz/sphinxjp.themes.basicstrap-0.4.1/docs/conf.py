b4fa2b04f1b8c940324074637a26890782c37895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:49 +0000

    Add package sphinxjp.themes.basicstrap/0.4.1/sphinxjp.themes.basicstrap-0.4.1.tar.gz

---
+++ packages/sphinxjp.themes.basicstrap/sphinxjp.themes.basicstrap-0.4.1.tar.gz/sphinxjp.themes.basicstrap-0.4.1/docs/conf.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# -- General configuration -----------------------------------------------------
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = u'sphinx theme for basicstrap style'
+copyright = u'2014, tell-k'
+
+version = '0.4.1'
+
+# -- Options for HTML output ---------------------------------------------------
+
+extensions = ['sphinxjp.themes.basicstrap']
+html_theme = 'basicstrap'
+
+# -- HTML theme options for `basicstrap` style -------------------------------------
+
+html_theme_options = {
+    'lang': 'en',
+    'nosidebar': False,
+    'rightsidebar': False,
+    'sidebar_span': 3,
+    'nav_fixed_top': True,
+
+    'nav_fixed': False,
+    'nav_width': '900px',
+
+    'content_fixed': False,
+    'content_width': '900px',
+
+    'row_fixed': False,
+    'noresponsive': False,
+    'noflatdesign': False,
+
+    'googlewebfont': False,
+    'googlewebfont_url': 'http://fonts.googleapis.com/css?family=Lily+Script+One',
+    'googlewebfont_style': u"font-family: 'Lily Script One' cursive;",
+
+    'header_inverse': False,
+    'relbar_inverse': False,
+
+    'inner_theme': True,
+    'inner_theme_name': 'bootswatch-flatly',
+
+    'bootstrap_version': '3',
+    'quick_preview': False,
+
+    # 'h1_size': '3.0em',
+    # 'h2_size': '2.6em',
+    # 'h3_size': '2.2em',
+    # 'h4_size': '1.8em',
+    # 'h5_size': '1.4em',
+    # 'h6_size': '1.1em',
+}
---
+++ packages/sphinxjp.themes.basicstrap/sphinxjp.themes.basicstrap-0.4.1.tar.gz/sphinxjp.themes.basicstrap-0.4.1/src/sphinxjp/themes/basicstrap/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+    sphinxjp.themes.basicstrap
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :author: tell-k <ffk2005@gmail.com>
+    :copyright: tell-k. All Rights Reserved.
+"""
+
+from os import path
+from sphinxjp.themes.basicstrap import directives
+
+package_dir = path.abspath(path.dirname(__file__))
+template_path = path.join(package_dir, 'templates')
+
+
+def get_path():
+    """entry-point for theme."""
+    return template_path
+
+
+def setup(app):
+    """entry-point for sphinx directive."""
+    directives.setup(app)
