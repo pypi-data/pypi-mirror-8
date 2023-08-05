77590ee50d72bbd60db1845aead9781d07407ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package os-client-config/0.2.0/os-client-config-0.2.0.tar.gz

---
+++ packages/os-client-config/os-client-config-0.2.0.tar.gz/os-client-config-0.2.0/os_client_config/config.py
@@ -0,0 +1,154 @@
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+import os
+
+import yaml
+
+from os_client_config import cloud_config
+from os_client_config import defaults_dict
+from os_client_config import exceptions
+from os_client_config import vendors
+
+CONFIG_HOME = os.path.join(os.path.expanduser(
+    os.environ.get('XDG_CONFIG_HOME', os.path.join('~', '.config'))),
+    'openstack')
+CONFIG_SEARCH_PATH = [os.getcwd(), CONFIG_HOME, '/etc/openstack']
+CONFIG_FILES = [
+    os.path.join(d, 'clouds.yaml') for d in CONFIG_SEARCH_PATH]
+BOOL_KEYS = ('insecure', 'cache')
+REQUIRED_VALUES = ('auth_url', 'username', 'password', 'project_id')
+
+
+def get_boolean(value):
+    if value.lower() == 'true':
+        return True
+    return False
+
+
+class OpenStackConfig(object):
+
+    def __init__(self, config_files=None):
+        self._config_files = config_files or CONFIG_FILES
+
+        defaults = defaults_dict.DefaultsDict()
+        defaults.add('username')
+        defaults.add('user_domain_name')
+        defaults.add('password')
+        defaults.add('project_id', defaults['username'], also='tenant_name')
+        defaults.add('project_domain_name')
+        defaults.add('auth_url')
+        defaults.add('region_name')
+        defaults.add('cache')
+        defaults.add('auth_token')
+        defaults.add('insecure')
+        defaults.add('endpoint_type')
+        defaults.add('cacert')
+        defaults.add('auth_type')
+
+        self.defaults = defaults
+
+        # use a config file if it exists where expected
+        self.cloud_config = self._load_config_file()
+        if not self.cloud_config:
+            self.cloud_config = dict(
+                clouds=dict(openstack=dict(self.defaults)))
+
+    @classmethod
+    def get_services(klass):
+        return SERVICES
+
+    def _load_config_file(self):
+        for path in self._config_files:
+            if os.path.exists(path):
+                return yaml.load(open(path, 'r'))
+
+    def _get_regions(self, cloud):
+        try:
+            return self.cloud_config['clouds'][cloud]['region_name']
+        except KeyError:
+            # No region configured
+            return ''
+
+    def _get_region(self, cloud):
+        return self._get_regions(cloud).split(',')[0]
+
+    def _get_cloud_sections(self):
+        return self.cloud_config['clouds'].keys()
+
+    def _get_base_cloud_config(self, name):
+        cloud = dict()
+        if name in self.cloud_config['clouds']:
+            our_cloud = self.cloud_config['clouds'][name]
+        else:
+            our_cloud = dict()
+
+        # Get the defaults (including env vars) first
+        cloud.update(self.defaults)
+
+        # yes, I know the next line looks silly
+        if 'cloud' in our_cloud:
+            try:
+                cloud.update(vendors.CLOUD_DEFAULTS[our_cloud['cloud']])
+            except KeyError:
+                # Can't find the requested vendor config, go about business
+                pass
+
+        cloud.update(our_cloud)
+        if 'cloud' in cloud:
+            del cloud['cloud']
+        return cloud
+
+    def get_all_clouds(self):
+
+        clouds = []
+
+        for cloud in self._get_cloud_sections():
+            for region in self._get_regions(cloud).split(','):
+                clouds.append(self.get_one_cloud(cloud, region))
+        return clouds
+
+    def get_one_cloud(self, name='openstack', region=None):
+
+        if not region:
+            region = self._get_region(name)
+
+        config = self._get_base_cloud_config(name)
+        config['region_name'] = region
+
+        for key in BOOL_KEYS:
+            if key in config:
+                config[key] = get_boolean(config[key])
+
+        for key in REQUIRED_VALUES:
+            if key not in config or not config[key]:
+                raise exceptions.OpenStackConfigException(
+                    'Unable to find full auth information for cloud {name} in'
+                    ' config files {files}'
+                    ' or environment variables.'.format(
+                        name=name, files=','.join(self._config_files)))
+
+        # If any of the defaults reference other values, we need to expand
+        for (key, value) in config.items():
+            if hasattr(value, 'format'):
+                config[key] = value.format(**config)
+
+        return cloud_config.CloudConfig(
+            name=name, region=region, config=config)
+
+if __name__ == '__main__':
+    config = OpenStackConfig().get_all_clouds()
+    for cloud in config:
+        print(cloud.name, cloud.region, cloud.config)
