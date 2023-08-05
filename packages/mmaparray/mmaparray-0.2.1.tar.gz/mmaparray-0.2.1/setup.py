a2561808f004c32ab8803fa83eba24623d75d4af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:46 +0000

    Add package mmaparray/0.2.1/mmaparray-0.2.1.tar.gz

---
+++ packages/mmaparray/mmaparray-0.2.1.tar.gz/mmaparray-0.2.1/setup.py
@@ -0,0 +1,46 @@
+'''
+(c) 2014 Farsight Security Inc.
+(c) 2010 Victor Ng
+
+Released under the MIT license.  See license.txt.
+'''
+
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Distutils import build_ext
+from os.path import join
+
+import os
+
+ext_modules=[ 
+        Extension("mmaparray", 
+            extra_compile_args=['-std=gnu99', '-O2', '-D_LARGEFILE64_SOURCE'],
+            sources = [
+                "src/mmaparray.pyx",
+                'src/mmap_writer.c',],
+            include_dirs = ['src'],
+            ),
+]
+
+setup(
+  name = 'mmaparray',
+  author='Henry Stern',
+  author_email = 'stern@fsi.io',
+  description = 'MMap File-Backed Arrays for Python',
+  long_description = open('README.rst').read(),
+  version='0.2.1',
+  url='https://github.com/farsightsec/mmaparray',
+  license = 'MIT License',
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules,
+  test_suite = 'tests',
+  requires = [ 'six', 'Cython (>=0.13)' ],
+  classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Cython',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries',
+  ],
+)
