ecffba544a088cdf74d4485c64bb8b2b7f9eeb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package pynamodb/1.3.4/pynamodb-1.3.4.tar.gz

---
+++ packages/pynamodb/pynamodb-1.3.4.tar.gz/pynamodb-1.3.4/setup.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    print("Now tag me :)")
+    print("  git tag -a {0} -m 'version {0}'".format(__import__('pynamodb').__version__))
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name='pynamodb',
+    version=__import__('pynamodb').__version__,
+    packages=find_packages(),
+    url='http://jlafon.io/pynamodb.html',
+    author='Jharrod LaFon',
+    author_email='jlafon@eyesopen.com',
+    description='A Pythonic Interface to DynamoDB',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    license='MIT',
+    keywords='python dynamodb amazon',
+    install_requires=[
+        'Delorean',
+        'six',
+        'ordereddict',
+        'botocore>=0.42.0',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
