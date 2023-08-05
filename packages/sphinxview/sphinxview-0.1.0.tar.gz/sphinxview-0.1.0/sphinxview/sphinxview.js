0d4e42e36e9ce70b958d051981485a1374f4901d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:08 +0000

    Add package sphinxview/0.1.0/sphinxview-0.1.0.tar.gz

---
+++ packages/sphinxview/sphinxview-0.1.0.tar.gz/sphinxview-0.1.0/sphinxview/sphinxview.js
@@ -0,0 +1,22 @@
+// Copyright 2014 Troels Brødsgaard
+// License: 2-clause BSD, see LICENSE for details
+
+$(document).ready(function () {
+  var xmlHttp = null;
+  setTimeout(function () {
+    if (window.XMLHttpRequest) {
+      xmlHttp = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+      xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == '200') {
+        window.location.reload(true);
+      }
+    }
+    var elements = document.getElementsByClassName("footer");
+    var last_updated = elements[0].innerHTML;
+    xmlHttp.open('HEAD', '/polling?build_file=' + location.pathname + '&last_updated=' + last_updated, true);
+    xmlHttp.send(null);
+  }, 0);
+});
