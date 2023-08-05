2f8a2d41471a6fedb80cf3516e849dbcc7d40302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:46 +0000

    Add package jinjatools/0.1/jinjatools-0.1.tar.gz

---
+++ packages/jinjatools/jinjatools-0.1.tar.gz/jinjatools-0.1/jinjatools/coffee/__init__.py
@@ -0,0 +1,32 @@
+# jinja-tools
+#
+# Various tools for Jinja2,
+# including new filters and tests based on python-moretools,
+# a JinjaLoader class for Django,
+# and a simple JinjaBuilder class for SCons.
+#
+# Copyright (C) 2011-2013 Stefan Zimmermann <zimmermann.code@gmail.com>
+#
+# jinja-tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# jinja-tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with jinja-tools.  If not, see <http://www.gnu.org/licenses/>.
+
+"""jinjatools.coffee
+
+Defines the :class:`CoffeeExtension`, which provides a {% coffee %} tag
+for translating CoffeeScript to JavaScript.
+
+.. moduleauthor:: Stefan Zimmermann <zimmermann.code@gmail.com>
+"""
+
+class CoffeeExtension(Extension):
+    tags = set(['coffee'])
