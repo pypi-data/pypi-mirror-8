bb460538e3873c44ce03e090e25c18c20f3392c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:56 +0000

    Add package yapybot/0.7.3.3/yapybot-0.7.3.3.tar.gz

---
+++ packages/yapybot/yapybot-0.7.3.3.tar.gz/yapybot-0.7.3.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='yapybot', version='0.7.3.3', description='yet another python irc bot', 
+      author='hlmtre',
+      author_email='hellmitre@gmail.com',
+      url='http://bits.zero9f9.com/pybot',
+      packages=['pybot'],
+      scripts=['pybot/bot.py'],
+      classifiers=['Development Status :: 5 - Production/Stable']
+      )
+
