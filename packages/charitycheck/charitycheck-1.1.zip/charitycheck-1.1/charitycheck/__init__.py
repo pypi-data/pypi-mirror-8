7e05973fb254f432b48080c6489e9730b5399187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:16 +0000

    Add package charitycheck/1.1/charitycheck-1.1.zip

---
+++ packages/charitycheck/charitycheck-1.1.zip/charitycheck-1.1/charitycheck/__init__.py
@@ -0,0 +1,5 @@
+from .charitycheck import (
+    make_dbm,
+    get_nonprofit_data,
+    verify_nonprofit,
+    get_deductability_code)
---
+++ packages/charitycheck/charitycheck-1.1.zip/charitycheck-1.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='charitycheck',
+    version='1.1',
+    author='Nicholas A. Lourie',
+    author_email='developer.nick@kozbox.com',
+    packages=['charitycheck',],
+    license='MIT License',
+    description=('a small module to verify information'
+                 'about nonprofits using their EINs'),
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    keywords="nonprofit nonprofits EIN IRS verify",
+    url="https://github.com/nalourie/charitycheck",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
+    
