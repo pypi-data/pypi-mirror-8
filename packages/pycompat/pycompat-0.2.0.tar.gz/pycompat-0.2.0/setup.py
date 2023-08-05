5678a2be6b9e1ff0f9e7f394ce5720224864ce5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:23 +0000

    Add package pycompat/0.2.0/pycompat-0.2.0.tar.gz

---
+++ packages/pycompat/pycompat-0.2.0.tar.gz/pycompat-0.2.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='pycompat',
+    version='0.2.0',
+    author='Alexandre Vicenzi',
+    author_email='vicenzi.alexandre@gmail.com',
+    packages=['pycompat'],
+    url='https://github.com/alexandrevicenzi/pycompat',
+    license='MIT',
+    description='Library to check Python and System version in a easy way',
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Topic :: System',
+          'Topic :: Utilities',
+          ],
+)
