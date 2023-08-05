0d785bbc3d9c9b3bcf80a47052fbe1b5ab34dbe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:46 +0000

    Add package aperture/0.1.1/aperture-0.1.1.zip

---
+++ packages/aperture/aperture-0.1.1.zip/aperture-0.1.1/aperture/__init__.py
@@ -0,0 +1,10 @@
+"""
+package for some plotting wrappers and formatting
+"""
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from aperture import util
+from aperture.set_plot_params import *
+from aperture.heatmaps import *
+
