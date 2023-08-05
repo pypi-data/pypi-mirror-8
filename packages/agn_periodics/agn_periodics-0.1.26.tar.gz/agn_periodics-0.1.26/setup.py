c8d202048feaac4b7698321410394fccd6043185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:31 +0000

    Add package agn_periodics/0.1.26/agn_periodics-0.1.26.tar.gz

---
+++ packages/agn_periodics/agn_periodics-0.1.26.tar.gz/agn_periodics-0.1.26/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='agn_periodics',
+    version='0.1.26',
+    packages=['agn_periodics'],
+    package_data={'agn_periodics': ['config.json']},
+    data_files=[('agn_periodics', ['agn_periodics/config.json'])],
+    url='',
+    license='GPL',
+    author='yarnaid',
+    author_email='yarnaid@gmail.com',
+    description='Some statistical functions for searchin periodics in time rows'
+)
