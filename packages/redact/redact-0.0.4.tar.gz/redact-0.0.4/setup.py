a2c683dd63bb1d57ea55ed652bb779750c2c14dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:48 +0000

    Add package redact/0.0.4/redact-0.0.4.tar.gz

---
+++ packages/redact/redact-0.0.4.tar.gz/redact-0.0.4/setup.py
@@ -0,0 +1,16 @@
+# from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='redact',
+    url='https://github.com/zweifisch/redact',
+    version='0.0.4',
+    description='rendering config files from templates',
+    author='Feng Zhou',
+    author_email='zf.pascal@gmail.com',
+    packages=['redact'],
+    install_requires=['PyYAML', 'opster'],
+    entry_points={
+        'console_scripts': ['redact=redact:redact'],
+    },
+)
