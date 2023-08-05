2fa9fe917ac6e37da738c35ff11f05d9f761d73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:52 +0000

    Add package solvebio/1.5.2/solvebio-1.5.2.tar.gz

---
+++ packages/solvebio/solvebio-1.5.2.tar.gz/solvebio-1.5.2/README.md
@@ -0,0 +1,60 @@
+[![Build
+Status](https://travis-ci.org/solvebio/solvebio-python.svg)](https://travis-ci.org/solvebio/solvebio-python)
+
+SolveBio Python Client
+======================
+
+The solve Python package and command-line interface (CLI) are used to work in our bioinformatics environment.
+
+For more information about SolveBio see http://www.solvebio.com
+
+
+Automatic Installer
+-------------------
+
+To use our guided installer, open up your terminal paste this:
+
+    curl -kL install.solvebio.com | bash
+
+
+
+Manual Installation
+-------------------
+
+You may want to first install `readline` for a better experience:
+
+    easy_install readline
+
+
+Now just install `solvebio` from PyPI:
+
+    pip install solvebio
+
+
+To log in, type:
+
+    solvebio login
+
+Enter your credentials and you should be good to go!
+Just type `solvebio` to enter the SolveBio Python shell.
+
+
+Installing from Git
+-------------------
+
+    pip install -e git+https://github.com/solvebio/solvebio-python.git#egg=solve
+
+
+
+Development
+-----------
+
+    git clone https://github.com/solvebio/solvebio-python.git
+    cd solve-python/
+    python setup.py develop
+
+
+To tag new versions:
+
+    git tag `cat solvebio/version.py | cut -d "'" -f 2`
+    git push --tags origin master
---
+++ packages/solvebio/solvebio-1.5.2.tar.gz/solvebio-1.5.2/solvebio/cli/auth.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import getpass
+
+import solvebio
+from solvebio.client import client, SolveError
+from solvebio.credentials import (get_credentials, delete_credentials,
+                                  save_credentials)
+
+
+def _ask_for_credentials():
+    while True:
+        email = raw_input('Email address: ')
+        password = getpass.getpass('Password (typing will be hidden): ')
+        if email and password:
+            return (email, password)
+        else:
+            print 'Email and password are both required.'
+
+
+def _send_install_report():
+    import platform
+    data = {
+        'hostname': platform.node(),
+        'python_version': platform.python_version(),
+        'python_implementation': platform.python_implementation(),
+        'platform': platform.platform(),
+        'architecture': platform.machine(),
+        'processor': platform.processor(),
+        'pyexe_build': platform.architecture()[0]
+    }
+    try:
+        client.request('post', '/v1/reports/install', data)
+    except:
+        pass
+
+
+def login(args):
+    """
+    Prompt user for login information (email/password).
+    Email and password are used to get the user's auth_token key.
+    """
+    delete_credentials()
+
+    email, password = _ask_for_credentials()
+    data = {
+        'email': email,
+        'password': password
+    }
+    try:
+        response = client.request('post', '/v1/auth/token', data)
+    except SolveError as e:
+        print 'Login failed: %s' % e.message
+    else:
+        save_credentials(email.lower(), response['token'])
+        # reset the default client's auth token
+        solvebio.api_key = response['token']
+        _send_install_report()
+        print 'You are now logged-in.'
+
+
+def logout(args):
+    if get_credentials():
+        delete_credentials()
+        client.auth = None
+        print 'You have been logged out.'
+    else:
+        print 'You are not logged-in.'
+
+
+def whoami(args):
+    creds = get_credentials()
+    if creds:
+        print creds[0]
+    else:
+        print 'You are not logged-in.'
