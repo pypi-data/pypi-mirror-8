550e3beb77c637ea105b20fa5848b4dacbb27c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package voluptuous/0.8.6/voluptuous-0.8.6.tar.gz

---
+++ packages/voluptuous/voluptuous-0.8.6.tar.gz/voluptuous-0.8.6/setup.py
@@ -0,0 +1,35 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+sys.path.insert(0, '.')
+version = __import__('voluptuous').__version__
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    print('WARNING: Could not locate pandoc, using Markdown long_description.')
+    long_description = open('README.md').read()
+
+description = long_description.splitlines()[0].strip()
+
+
+setup(
+    name='voluptuous',
+    url='http://github.com/alecthomas/voluptuous',
+    download_url='http://pypi.python.org/pypi/voluptuous',
+    version=version,
+    description=description,
+    long_description=long_description,
+    license='BSD',
+    platforms=['any'],
+    py_modules=['voluptuous'],
+    author='Alec Thomas',
+    author_email='alec@swapoff.org',
+    install_requires=[
+        'setuptools >= 0.6b1',
+    ],
+)
