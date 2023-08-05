fbb6405a7d37a7d332e3d388b8c1a07d988bc0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:00 +0000

    Add package Pycolor/1.2.0/Pycolor-1.2.0.tar.gz

---
+++ packages/Pycolor/Pycolor-1.2.0.tar.gz/Pycolor-1.2.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# setup.py for Pycolor
+
+from distutils.core import setup
+
+setup(name='Pycolor',
+      version='1.2.0',
+      description='Ansi color for python',
+      author='Will Drach',
+      license='Beerware',
+      author_email='will.drach@live.com',
+      url='http://www.drach.co/pycolor',
+      py_modules=['pycolor'],
+      classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 3',
+      'License :: Public Domain',
+      'Natural Language :: English',
+      'Operating System :: Unix',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      keywords='ansi color terminal colors',
+      install_requires=['random', 'sys']
+      )
