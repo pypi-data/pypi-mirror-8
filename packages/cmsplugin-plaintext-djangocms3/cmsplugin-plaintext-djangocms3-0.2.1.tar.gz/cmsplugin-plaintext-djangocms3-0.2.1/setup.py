e15df0df9d979aaf1827e2509523a10c0ea8fd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:26 +0000

    Add package cmsplugin-plaintext-djangocms3/0.2.1/cmsplugin-plaintext-djangocms3-0.2.1.tar.gz

---
+++ packages/cmsplugin-plaintext-djangocms3/cmsplugin-plaintext-djangocms3-0.2.1.tar.gz/cmsplugin-plaintext-djangocms3-0.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+version = '0.2.1'
+
+setup(
+    name='cmsplugin-plaintext-djangocms3',
+    version=version,
+    description='Adds a plaintext plugin for django-cms. Forked from https://bitbucket.org/xenofox/cmsplugin-plaintext to add django-cms3 support',
+    author='Changer',
+    author_email='support@changer.nl',
+    url='http://bitbucket.org/changer/cmsplugin-plaintext/',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
