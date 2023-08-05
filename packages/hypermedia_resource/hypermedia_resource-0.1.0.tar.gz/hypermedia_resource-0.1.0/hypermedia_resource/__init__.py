6f48a296c3405c5ff0c9ea5a5e533866780b37dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:27 +0000

    Add package hypermedia_resource/0.1.0/hypermedia_resource-0.1.0.tar.gz

---
+++ packages/hypermedia_resource/hypermedia_resource-0.1.0.tar.gz/hypermedia_resource-0.1.0/hypermedia_resource/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'hypermedia_resource'
+__version__ = '0.1.0'
+__author__ = 'Stephen Mizell'
+__license__ = 'MIT'
+
+from base import HypermediaResource
+from adapters.hal_json import HalJSONAdapter
+from adapters.maze_xml import MazeXMLAdapter
+
+HypermediaResource.adapters.add(HalJSONAdapter)
+HypermediaResource.adapters.add(MazeXMLAdapter)
