063503a234e325d54e9693492a4c905c51b711ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:03 +0000

    Add package coursera_offline/0.1.3/coursera_offline-0.1.3.tar.gz

---
+++ packages/coursera_offline/coursera_offline-0.1.3.tar.gz/coursera_offline-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+	name = "coursera_offline",
+
+	version = "0.1.3",
+
+	author="Sanketh Mopuru",
+	author_email="sanketh.mopuru@gmail.com",
+
+	url='https://github.com/sanketh95/coursera-offline',
+	license='GPLv3',
+
+	description = 'Download Coursera videos for offline viewing',
+
+	install_requires=[
+		"pyquery"
+	],
+
+
+	scripts=['coursera_offline']
+	)
