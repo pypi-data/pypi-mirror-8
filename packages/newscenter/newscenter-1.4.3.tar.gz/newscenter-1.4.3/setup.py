6e77ee612f60e938d06f18d6d97231e9a77e27dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package newscenter/1.4.3/newscenter-1.4.3.tar.gz

---
+++ packages/newscenter/newscenter-1.4.3.tar.gz/newscenter-1.4.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newscenter',
+    version='1.4.3',
+    description='A News Release Application for Django',
+    long_description=open('README.rst').read(),
+
+    author='Imaginary Landscape',
+    author_email='dbertrand@imagescape.com',
+
+    install_requires=(
+        'pillow',
+        'easy-thumbnails',
+        'feedparser',
+    ),
+
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(),
+)
