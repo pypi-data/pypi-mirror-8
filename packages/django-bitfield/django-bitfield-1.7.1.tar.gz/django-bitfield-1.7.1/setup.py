851b6c7d79fd6866c31b00af94734759ed67eae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package django-bitfield/1.7.1/django-bitfield-1.7.1.tar.gz

---
+++ packages/django-bitfield/django-bitfield-1.7.1.tar.gz/django-bitfield-1.7.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-bitfield',
+    version='1.7.1',
+    author='DISQUS',
+    author_email='opensource@disqus.com',
+    url='https://github.com/disqus/django-bitfield',
+    description='BitField in Django',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'Django>=1.2',
+        'six',
+    ],
+    setup_requires=[
+        'nose>=1.0',
+    ],
+    tests_require=[
+        'django-nose>=0.1.3',
+        'psycopg2>=2.3',
+    ],
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ],
+)
