22953155c14af4c25fc71e07156bab5c45fe8221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:55 +0000

    Add package tinycms/0.2/tinycms-0.2.tar.gz

---
+++ packages/tinycms/tinycms-0.2.tar.gz/tinycms-0.2/tinycms/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.conf import settings
+
+from mptt.admin import MPTTModelAdmin
+
+from models import *
+
+
+class ContentInline(admin.StackedInline):
+    model = Content
+    extra = 0
+
+class PageAdmin(MPTTModelAdmin):
+    inlines = [ContentInline]
+
+#admin.site.register(Page,MPTTModelAdmin)
+admin.site.register(Page,PageAdmin)
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display  = ['page','value_name']
+admin.site.register(Content,ContentAdmin)
+
+
+
+
