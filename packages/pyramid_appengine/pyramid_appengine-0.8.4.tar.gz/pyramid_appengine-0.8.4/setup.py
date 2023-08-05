3c194b4f9d628e3481be68035d9cd5bc553549dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:14 +0000

    Add package pyramid_appengine/0.8.4/pyramid_appengine-0.8.4.tar.gz

---
+++ packages/pyramid_appengine/pyramid_appengine-0.8.4.tar.gz/pyramid_appengine-0.8.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'readme.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+version = "0.8.4"
+requires = ["pyramid"]
+setup(name='pyramid_appengine',
+      version=version,
+      description="Scaffold + Tools for creating/developing pyramid applications on Google App Engine",
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+          ],
+      keywords='web pyramid pylons google-app-engine',
+      author='Tom Willis',
+      author_email='me@twillis.me',
+      url='https://github.com/twillis/pyramid_appengine',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""
+      # -*- Entry points: -*-
+      [pyramid.scaffold]
+      appengine_starter=pyramid_appengine.scaffolds:PyramidAppEngineStarterTemplate
+      """,
+      )
