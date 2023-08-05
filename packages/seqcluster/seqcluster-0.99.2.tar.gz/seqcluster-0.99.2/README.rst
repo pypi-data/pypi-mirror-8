207c93434d4bc7c2088098b70031644bb5f10a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package seqcluster/0.99.2/seqcluster-0.99.2.tar.gz

---
+++ packages/seqcluster/seqcluster-0.99.2.tar.gz/seqcluster-0.99.2/README.rst
@@ -0,0 +1,13 @@
+seqcluster
+---------
+
+small RNA analysis from NGS data
+
+.. image:: https://travis-ci.org/lpantano/seqcluster.png?branch=master
+    :target: https://travis-ci.org/lpantano/seqcluster.png?branch=master
+    
+.. image:: https://badge.fury.io/py/seqcluster.svg
+    :target: http://badge.fury.io/py/seqcluster
+
+.. image:: https://pypip.in/d/seqcluster/badge.png
+    :target: https://pypi.python.org/pypi/seqcluster
---
+++ packages/seqcluster/seqcluster-0.99.2.tar.gz/seqcluster-0.99.2/seqcluster/command_line.py
@@ -0,0 +1,25 @@
+from libs.logger import initialize_logger
+import sys
+from libs.parse import parse_cl
+from preparedata import prepare
+from makecluster import cluster
+from explore_cluster import explore
+import libs.logger as mylog
+import time
+
+
+def main(**kwargs):
+    kwargs = parse_cl(sys.argv[1:])
+    initialize_logger(kwargs['args'].out)
+    logger = mylog.getLogger(__name__)
+    start = time.time()
+    if "prepare" in kwargs:
+        logger.info("Run prepare")
+        prepare(kwargs["args"])
+    elif "cluster" in kwargs:
+        logger.info("Run cluster")
+        cluster(kwargs["args"])
+    elif "explore" in kwargs:
+        logger.info("Run explore")
+        explore(kwargs["args"])
+    logger.info('It took %.3f minutes' % ((time.time()-start)/60))
