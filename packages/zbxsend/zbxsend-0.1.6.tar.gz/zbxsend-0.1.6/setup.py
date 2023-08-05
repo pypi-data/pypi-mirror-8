7e5400157a331a350983b62701cd59ff1bf2d484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package zbxsend/0.1.6/zbxsend-0.1.6.tar.gz

---
+++ packages/zbxsend/zbxsend-0.1.6.tar.gz/zbxsend-0.1.6/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "zbxsend",
+    version = "0.1.6",
+    author = "Sergey Kirillov",
+    author_email = "sergey.kirillov@gmail.com",
+    description = ("Module used to send metrics to Zabbix."),
+    url='https://github.com/pistolero/zbxsend',
+    license = "BSD",
+    py_modules=['zbxsend'],
+    long_description=read('README.txt'),
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+    ],
+)
