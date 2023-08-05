6887d9ba792ff11a2827179174cf87386dbc8c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:55 +0000

    Add package ofpy/0.11/ofpy-0.11.tar.gz

---
+++ packages/ofpy/ofpy-0.11.tar.gz/ofpy-0.11/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ofpy',
+  packages = ['ofpy'],
+  version = '0.11',
+  description = 'Add tasks to OmniFocus from Linux',
+  author = 'Nathan Henrie',
+  author_email = 'nate@n8henrie.com',
+  url = 'http://n8henrie.com/2014/09/ofpy',
+  download_url = 'https://github.com/n8henrie/ofpy/tarball/0.11',
+  keywords = ['omnifocus', 'productivity', 'tasklist'],
+  classifiers = [],
+)
