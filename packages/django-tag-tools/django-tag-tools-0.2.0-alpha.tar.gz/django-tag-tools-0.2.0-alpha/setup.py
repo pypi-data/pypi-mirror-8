a848bd25e3e44ea9fe351f42ca793a431baa7bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package django-tag-tools/0.2.0-alpha/django-tag-tools-0.2.0-alpha.tar.gz

---
+++ packages/django-tag-tools/django-tag-tools-0.2.0-alpha.tar.gz/django-tag-tools-0.2.0-alpha/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+README_FILE = open('README.txt')
+try:
+    long_description = README_FILE.read()
+finally:
+    README_FILE.close()
+
+packages = (
+        'tagtools',
+        'tagtools.backends',
+        'tagtools.backends.taggit',
+        'tagtools.backends.tagging',
+)
+
+setup(name='django-tag-tools',
+        version='0.2.0-alpha',
+        packages=('tagtools',),
+        include_package_data=True,
+        zip_safe=False,
+        platforms=['any'],
+        description='Tag cloud for django tagging and django-taggit.',
+        author_email='kaleissin@gmail.com',
+        author='kaleissin',
+        long_description=long_description,
+        url='https://github.com/kaleissin/django-tag-tools',
+        classifiers=[
+                'Development Status :: 4 - Beta',
+                'Environment :: Web Environment',
+                'Framework :: Django',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: MIT License',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 3',
+                'Topic :: Software Development :: Libraries :: Application Frameworks',
+                'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+)
---
+++ packages/django-tag-tools/django-tag-tools-0.2.0-alpha.tar.gz/django-tag-tools-0.2.0-alpha/tagtools/__init__.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import, unicode_literals
+
+from tagtools.tagcloud import *
+from tagtools.settings import *
+
+def get_tagcloud_for_model(model, steps=CLOUD_STEPS, min_count=CLOUD_MIN_COUNT):
+    raise NotImplementedError('No tag-app found')
+
+def set_tags_for_model(tags, model):
+    raise NotImplementedError('No tag-app found')
+
+if PREFERRED_APP_ORDER and not TAG_APP:
+    taggit = None
+    tagging = None
+    _tag_app_map = {}
+    if 'taggit' in PREFERRED_APP_ORDER:
+        from tagtools.backends import taggit
+        _tag_app_map['taggit'] = taggit.get_tagcloud_for_model
+    if 'tagging' in PREFERRED_APP_ORDER:
+        from tagtools.backends import tagging
+        _tag_app_map['tagging'] = tagging.get_tagcloud_for_model
+
+    def _generic_get_tagcloud_for_model(model, steps=CLOUD_STEPS, min_count=CLOUD_MIN_COUNT):
+        for app in PREFERRED_APP_ORDER:
+            return _tag_app_map[app](model, steps, min_count)
+        return ()
+
+    def _generic_set_tags_for_model(tags, model):
+        if getattr(model.tags, '__module__', False):
+            return taggit.set_tags_for_model(tags, model)
+        return model, None
+
+if TAG_APP == 'taggit':
+    from tagtools.backends.taggit import *
+elif TAG_APP == 'tagging':
+    from tagtools.backends.tagging import *
+elif not TAG_APP:
+    if PREFERRED_APP_ORDER:
+        get_tagcloud_for_model = _generic_get_tagcloud_for_model
+        set_tags_for_model = _generic_set_tags_for_model
