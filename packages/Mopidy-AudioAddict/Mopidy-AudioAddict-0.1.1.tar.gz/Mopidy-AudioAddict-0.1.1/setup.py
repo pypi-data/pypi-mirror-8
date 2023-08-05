6e97ef1eec26faae7eca466d3ee58de0685bcbcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:35 +0000

    Add package Mopidy-AudioAddict/0.1.1/Mopidy-AudioAddict-0.1.1.tar.gz

---
+++ packages/Mopidy-AudioAddict/Mopidy-AudioAddict-0.1.1.tar.gz/Mopidy-AudioAddict-0.1.1/setup.py
@@ -0,0 +1,49 @@
+from __future__ import unicode_literals
+
+import re
+from setuptools import setup, find_packages
+
+
+def get_version(filename):
+    content = open(filename).read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", content))
+    return metadata['version']
+
+
+setup(
+    name='Mopidy-AudioAddict',
+    version=get_version('mopidy_audioaddict/__init__.py'),
+    url='https://github.com/nilicule/mopidy-audioaddict',
+    license='Apache License, Version 2.0',
+    author='Remco Brink',
+    author_email='remco@rc6.org',
+    description='Mopidy extension for the AudioAddict network of radiostations',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'setuptools',
+        'Mopidy >= 0.18',
+        'Pykka >= 1.1',
+        'pycountry',
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'mock >= 1.0',
+    ],
+    entry_points={
+        'mopidy.ext': [
+            'audioaddict = mopidy_audioaddict:Extension',
+        ],
+    },
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+)
