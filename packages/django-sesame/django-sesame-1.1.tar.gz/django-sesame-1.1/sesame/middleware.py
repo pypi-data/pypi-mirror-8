434fa75da9aec8cac9e259f88dc64e72bd79fa0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package django-sesame/1.1/django-sesame-1.1.tar.gz

---
+++ packages/django-sesame/django-sesame-1.1.tar.gz/django-sesame-1.1/sesame/middleware.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import AnonymousUser
+
+
+TOKEN_FIELD_NAME = 'url_auth_token'
+
+
+class AuthenticationMiddleware(object):
+
+    def process_request(self, request):
+        """Log user in if if `request` contains a valid login token."""
+        token = request.GET.get(TOKEN_FIELD_NAME)
+        if token is None:
+            return
+        user = authenticate(url_auth_token=token)
+        if user is None:
+            return
+
+        # If the sessions framework is enabled and the token is valid,
+        # persist the login in session.
+        if hasattr(request, 'session') and user is not None:
+            login(request, user)
+
+        # If the authentication middleware isn't enabled, set request.user.
+        # (This attribute is overwritten by the authentication middleware
+        # if it runs after this one.)
+        if not hasattr(request, 'user'):
+            request.user = user if user is not None else AnonymousUser()
---
+++ packages/django-sesame/django-sesame-1.1.tar.gz/django-sesame-1.1/setup.py
@@ -0,0 +1,48 @@
+import distutils.core
+import os
+
+# Avoid polluting the .tar.gz with ._* files under Mac OS X
+os.putenv('COPYFILE_DISABLE', 'true')
+
+# Prevent distutils from complaining that a standard file wasn't found
+README = os.path.join(os.path.dirname(__file__), 'README')
+if not os.path.exists(README):
+    os.symlink(README + '.rst', README)
+
+description = ('URL-based authentication, an application that provides '
+               'one-click login via specially crafted URLs')
+
+with open(README) as f:
+    long_description = '\n\n'.join(f.read().split('\n\n')[2:8])
+
+distutils.core.setup(
+    name='django-sesame',
+    version='1.1',
+    author='Aymeric Augustin',
+    author_email='aymeric.augustin@m4x.org',
+    url='https://github.com/aaugustin/django-sesame',
+    description=description,
+    long_description=long_description,
+    download_url='http://pypi.python.org/pypi/django-sesame',
+    packages=[
+        'sesame',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ],
+    platforms='all',
+    license='BSD'
+)
