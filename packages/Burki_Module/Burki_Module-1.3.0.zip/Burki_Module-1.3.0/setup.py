44d091f63f29eef261eadcda5ff35ca8617fa95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:34 +0000

    Add package Burki_Module/1.3.0/Burki_Module-1.3.0.zip

---
+++ packages/Burki_Module/Burki_Module-1.3.0.zip/Burki_Module-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+    name = "Burki_Module",
+    version = "1.3.0",
+    py_modules = ["Burki_Module"],
+    author = "Haseeb",
+    author_email = "hzburki@ymail.com",
+    url = "",
+    description = "Im sick of this shit. :P"
+    )
