c1bd1461ae30688c939f0ec6a0f94803879eddd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package sortroms/0.0.2/sortroms-0.0.2.tar.gz

---
+++ packages/sortroms/sortroms-0.0.2.tar.gz/sortroms-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+	name='sortroms',
+	version='0.0.2',
+	description='Python package for sorting emulator ROM files',
+	url='http://github.com/natemara/sortroms',
+	author='Nate Mara',
+	author_email='natemara@gmail.com',
+	license='MIT',
+	packages=['sortrom'],
+	package_data={'': ['sortrom/*.json']},
+	zip_safe=False
+)
