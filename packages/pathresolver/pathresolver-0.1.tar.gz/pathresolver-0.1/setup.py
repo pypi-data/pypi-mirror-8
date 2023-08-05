1482a9fbdc5e7710ab9dfd63d57f71f183da6856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:01 +0000

    Add package pathresolver/0.1/pathresolver-0.1.tar.gz

---
+++ packages/pathresolver/pathresolver-0.1.tar.gz/pathresolver-0.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+
+# Get whatever in README.md and assign it to the `long_description` var.
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    long_description = f.read()
+
+
+setup(
+    name='pathresolver',
+    version='0.1',
+    description='Resolve nested object paths',
+    author='Andrew Crosio',
+    author_email='andrew@andrewcrosio.com',
+    url='https://github.com/Andrew-Crosio/pathresolver',
+    long_description=long_description,
+    packages=['pathresolver'],
+    include_package_data=True,
+    test_module=['tests'],
+    setup_requires=[],
+    install_requires=[],
+    test_suite='tests',
+    tests_require=[]
+)
