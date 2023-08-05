58ec893f9f7275a17a7212c315b125912e823b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:40 +0000

    Add package infi.wioctl/0.1.9/infi.wioctl-0.1.9-py2.7.egg

---
+++ packages/infi.wioctl/infi.wioctl-0.1.9-py2.7.egg/infi/wioctl/__init__.py
@@ -0,0 +1,42 @@
+
+from contextlib import contextmanager
+from . import errors, constants, structures, api
+import ctypes
+
+@contextmanager
+def open_handle(device_path, open_generic=False, open_shared=True):
+    from constants import GENERIC_READ, GENERIC_WRITE, OPEN_EXISTING
+    from constants import FILE_SHARE_READ, FILE_SHARE_WRITE, OPEN_EXISTING
+    handle = None
+    try:
+        handle = api.CreateFileW(ctypes.create_unicode_buffer(device_path),
+                             GENERIC_READ | GENERIC_WRITE if open_generic else 0,
+                             FILE_SHARE_READ | FILE_SHARE_WRITE if open_shared else 0,
+                             0, OPEN_EXISTING, 0, 0)
+        yield handle
+    finally:
+        if handle is not None:
+            api.CloseHandle(handle)
+
+def ioctl(handle, control_code, in_buffer, in_buffer_size, out_buffer, out_buffer_size):
+    bytes_returned = ctypes.c_ulong()
+    api.DeviceIoControl(handle, control_code, in_buffer, in_buffer_size, out_buffer, out_buffer_size,
+                        ctypes.byref(bytes_returned), None)
+    return bytes_returned.value
+
+class DeviceIoControl(object):
+    def __init__(self, device_path, open_generic=False, open_shared=True):
+        super(DeviceIoControl, self).__init__()
+        self.device_path = device_path
+        self._open_generic = open_generic
+        self._open_shared = open_shared
+
+    @contextmanager
+    def open_handle(self):
+        with open_handle(self.device_path, self._open_generic, self._open_shared) as handle:
+            yield handle
+
+    def ioctl(self, control_code, in_buffer, in_buffer_size, out_buffer, out_buffer_size):
+        with self.open_handle() as handle:
+            return ioctl(handle, control_code, in_buffer, in_buffer_size, out_buffer, out_buffer_size)
+
---
+++ packages/infi.wioctl/infi.wioctl-0.1.9-py2.7.egg/infi/wioctl/structures.py
@@ -0,0 +1,23 @@
+from infi.instruct import SLInt64, ULInt64, ULInt32, ULInt16, ULInt8, Field, FixedSizeArray, FixedSizeString
+from infi.instruct import Struct
+
+def is_64bit():
+    import sys
+    return sys.maxsize > 2 ** 32
+
+POINTER = ULInt64 if is_64bit() else ULInt32
+WORD = ULInt16
+DWORD = ULInt32
+BYTE = ULInt8
+ULONG = DWORD
+UCHAR = BYTE
+BOOLEAN = BYTE
+ULONG64 = ULInt64
+WCHAR = ULInt16
+
+class LARGE_INTEGER(Struct):
+    _fields_ = [SLInt64("QuadPart"), ] if is_64bit() else [DWORD("LowPart"), DWORD("HighPart")]
+
+class GUID(Struct):
+    _fields_ = [DWORD("Data1"), WORD("Data2"), WORD("Data3"), FixedSizeArray("Data4", 8, BYTE)]
+
