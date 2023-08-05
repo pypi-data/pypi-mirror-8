c6aca3a7320b6045ad9998f619b40e6afc5f73f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package trytond_health/2.6.3/trytond_health-2.6.3.tar.gz

---
+++ packages/trytond_health/trytond_health-2.6.3.tar.gz/trytond_health-2.6.3/__init__.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    GNU Health: The Free Health and Hospital Information System
+#    Copyright (C) 2008-2014 Luis Falcon <lfalcon@gnusolidario.org>
+#    Copyright (C) 2011-2014 GNU Solidario <health@gnusolidario.org>
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from trytond.pool import Pool
+from .health import *
+from wizard import *
+
+def register():
+    Pool.register(
+        OperationalArea,
+        OperationalSector,
+        DomiciliaryUnit,
+        Occupation,
+        Ethnicity,
+        PartyPatient,
+        PartyAddress,
+        DrugDoseUnits,
+        MedicationFrequency,
+        DrugForm,
+        DrugRoute,
+        MedicalSpecialty,
+        HealthInstitution,
+        HealthInstitutionSpecialties,
+        HealthInstitutionOperationalSector,
+        HealthInstitutionO2M,
+        HospitalBuilding,
+        HospitalUnit,
+        HospitalOR,
+        HospitalWard,
+        HospitalBed,
+        HealthProfessional,
+        HealthProfessionalSpecialties,        
+        PhysicianSP,
+        Family,
+        FamilyMember,
+        MedicamentCategory,
+        Medicament,
+        PathologyCategory,
+        PathologyGroup,
+        Pathology,
+        DiseaseMembers,
+        ProcedureCode,
+        InsurancePlan,
+        Insurance,
+        AlternativePersonID,
+        ProductCategory,
+        ProductTemplate,
+        Product,
+        GnuHealthSequences,
+        PatientData,
+        PatientDiseaseInfo,
+        Appointment,
+        AppointmentReport,
+        OpenAppointmentReportStart,
+        PatientMedication,
+        PatientVaccination,
+        PatientPrescriptionOrder,
+        PrescriptionLine,
+        PatientEvaluation,
+        Directions,
+        SecondaryCondition,
+        DiagnosticHypothesis,
+        SignsAndSymptoms,
+        module='health', type_='model')
+    Pool.register(
+        OpenAppointmentReport,
+        CreateAppointmentEvaluation,
+        module='health', type_='wizard')
