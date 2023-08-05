53d7120445e0d376b2dee9769d4e4f12e354352b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:45 +0000

    Add package monty/0.5.7/monty-0.5.7-py2.py3-none-any.whl

---
+++ packages/monty/monty-0.5.7-py2.py3-none-any.whl/monty/collections.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import
+
+__author__ = 'Shyue Ping Ong'
+__copyright__ = "Copyright 2014, The Materials Virtual Lab"
+__version__ = '0.1'
+__maintainer__ = 'Shyue Ping Ong'
+__email__ = 'ongsp@ucsd.edu'
+__date__ = '1/24/14'
+
+
+class frozendict(dict):
+    """
+    A dictionary that does not permit changes. The naming
+    violates PEP8 to be consistent with standard Python's "frozenset" naming.
+    """
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+
+    def __setitem__(self, key, val):
+        raise KeyError("Cannot overwrite existing key: %s" % str(key))
+
+
+class Namespace(dict):
+    """A dictionary that does not permit to redefine its keys."""
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+
+    def __setitem__(self, key, val):
+        if key in self:
+            raise KeyError("Cannot overwrite existent key: %s" % str(key))
+
+        dict.__setitem__(self, key, val)
+
+    def update(self, *args, **kwargs):
+        for k, v in dict(*args, **kwargs).items():
+            self[k] = v
+
+#MG TODO To be removed 
+NotOverwritableDict = Namespace
+
+
+class AttrDict(dict):
+    """
+    Allows to access dict keys as obj.foo in addition
+    to the traditional way obj['foo']"
+
+    Example:
+        >> d = AttrDict(foo=1, bar=2)
+        >> d["foo"] == d.foo
+        True
+        >> d.bar = "hello"
+        >> d.bar
+        'hello'
+    """
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+    def copy(self):
+        newd = super(AttrDict, self).copy()
+        return self.__class__(**newd)
---
+++ packages/monty/monty-0.5.7-py2.py3-none-any.whl/tests/test_collections.py
@@ -0,0 +1,38 @@
+__author__ = 'Shyue Ping Ong'
+__copyright__ = 'Copyright 2014, The Materials Virtual Lab'
+__version__ = '0.1'
+__maintainer__ = 'Shyue Ping Ong'
+__email__ = 'ongsp@ucsd.edu'
+__date__ = '1/24/14'
+
+
+import unittest
+import os
+
+from monty.collections import frozendict, Namespace, AttrDict
+
+test_dir = os.path.join(os.path.dirname(__file__), 'test_files')
+
+class FrozenDictTest(unittest.TestCase):
+
+    def test_frozen_dict(self):
+        d = frozendict({"hello": "world"})
+        self.assertRaises(KeyError, d.__setitem__, "k", "v")
+        self.assertEqual(d["hello"], "world")
+
+    def test_namespace_dict(self):
+        d = Namespace(foo="bar")
+        d["hello"] = "world"
+        self.assertEqual(d["foo"], "bar")
+        self.assertRaises(KeyError, d.__setitem__, "foo", "spam")
+
+    def test_attr_dict(self):
+        d = AttrDict(foo=1, bar=2)
+        self.assertEqual(d.bar, 2)
+        self.assertEqual(d["foo"], d.foo)
+        d.bar = "hello"
+        self.assertEqual(d["bar"], "hello")
+
+
+if __name__ == "__main__":
+    unittest.main()
