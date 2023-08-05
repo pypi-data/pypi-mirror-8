40e92d6ea9e1ff74665adab2ac4b22aa35ecf1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:59 +0000

    Add package reddcoin-electrum-server/0.9.0/reddcoin-electrum-server-0.9.0.tar.gz

---
+++ packages/reddcoin-electrum-server/reddcoin-electrum-server-0.9.0.tar.gz/reddcoin-electrum-server-0.9.0/src/networks.py
@@ -0,0 +1,24 @@
+# Main network and testnet3 definitions
+
+params = {
+    'bitcoin_main': {
+        'pubkey_address': 0,
+        'script_address': 5,
+        'genesis_hash': '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+    },
+    'bitcoin_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
+        'genesis_hash': '000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943'
+    },
+    'reddcoin_main': {
+        'pubkey_address': 61,
+        'script_address': 5,
+        'genesis_hash': 'b868e0d95a3c3c0e0dadc67ee587aaf9dc8acbf99e3b4b3110fad4eb74c1decc'
+    },
+    'reddcoin_test': {
+        'pubkey_address': 111,
+        'script_address': 196,
+        'genesis_hash': '46db8d519c64586e3f0bf16153f61fdcf1d30b71ab159bf4a5b46dbcbc79d814'
+    }
+}
