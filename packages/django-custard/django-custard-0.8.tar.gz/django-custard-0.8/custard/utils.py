e013e02ab8564bef5b7149710f0bb675605a0e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:59 +0000

    Add package django-custard/0.8/django-custard-0.8.tar.gz

---
+++ packages/django-custard/django-custard-0.8.tar.gz/django-custard-0.8/custard/utils.py
@@ -0,0 +1,10 @@
+from django.utils import importlib
+
+
+#==============================================================================
+def import_class(name):
+    components = name.split('.')
+    mod = importlib.import_module(components[0])
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
