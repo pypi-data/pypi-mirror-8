5b8cf2aef22262f8e6e962ffdbaa688fcbe82963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:47 +0000

    Add package babelchart/0.1.1/babelchart-0.1.1.tar.gz

---
+++ packages/babelchart/babelchart-0.1.1.tar.gz/babelchart-0.1.1/babelchart/__init__.py
@@ -0,0 +1,4 @@
+from .sources import CloudWatchSource, GraphiteSource
+from .sinks import GoogleChartsSink
+
+__version__ = '0.1.1'
---
+++ packages/babelchart/babelchart-0.1.1.tar.gz/babelchart-0.1.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+
+import babelchart
+
+setup(name = 'babelchart',
+      version = babelchart.__version__,
+      description = 'Pluggable timeseries data converter',
+      author = 'Mike Babineau',
+      author_email = 'michael.babineau@gmail.com',
+      # install_requires = [],
+      url = 'https://github.com/mbabineau/babelchart',
+      packages = ['babelchart', 'babelchart.sinks', 'babelchart.sources', 'babelchart.dependencies'],
+      license = 'Apache v2.0',
+      platforms = 'Posix; MacOS X; Windows',
+      classifiers = [ 'Development Status :: 3 - Alpha',
+                      'Intended Audience :: Developers',
+                      'Intended Audience :: System Administrators',
+                      'License :: OSI Approved :: Apache Software License',
+                      'Operating System :: OS Independent',
+                      'Topic :: System :: Logging',
+                      ]
+      )
