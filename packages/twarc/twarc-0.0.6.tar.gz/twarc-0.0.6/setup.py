c14fa64bfd576d3eecb40e64026eebc98e824309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:53 +0000

    Add package twarc/0.0.6/twarc-0.0.6.tar.gz

---
+++ packages/twarc/twarc-0.0.6.tar.gz/twarc-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, Command
+
+class PyTest(Command):
+    """
+    A command to convince setuptools to run pytests.
+    """
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import pytest
+        pytest.main("test.py")
+
+setup(
+    name = 'twarc',
+    version = '0.0.6',
+    url = 'http://github.com/edsu/twarc',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    py_modules = ['twarc',],
+    scripts = ['twarc.py'],
+    description = 'command line utility to archive Twitter search results as line-oriented-json', 
+    cmdclass = {'test': PyTest},
+    install_requires = ['oauth2', 'python-dateutil'],
+    tests_require=['pytest']
+)
