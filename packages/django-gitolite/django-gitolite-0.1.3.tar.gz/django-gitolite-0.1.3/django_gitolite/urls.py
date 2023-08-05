534b0422b5823eecbe2dcc10170941671defbdf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:59 +0000

    Add package django-gitolite/0.1.3/django-gitolite-0.1.3.tar.gz

---
+++ packages/django-gitolite/django-gitolite-0.1.3.tar.gz/django-gitolite-0.1.3/django_gitolite/urls.py
@@ -0,0 +1,13 @@
+# Copyright 2014 Jon Eyolfson
+#
+# This file is distributed under the GPLv3 license
+
+from django.conf.urls import url
+
+from django_gitolite import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<path>([A-Za-z0-9-+]+/)*[A-Za-z0-9-+]+)/$',
+        views.repo, name='repo'),
+]
