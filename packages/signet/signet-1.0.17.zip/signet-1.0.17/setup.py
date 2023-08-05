a2fa930bad4bd3bafd64eaccd349f1b6e6543664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:26 +0000

    Add package signet/1.0.17/signet-1.0.17.zip

---
+++ packages/signet/signet-1.0.17.zip/signet-1.0.17/setup.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python2.7
+## \file setup.py
+# \brief Signet distutils setup.py
+# \date July 8th, 2014
+# \copyright Copyright(c), 2014, Carroll-Net, Inc.
+# \copyright All Rights Reserved.
+r"""Signet distutils setup.py
+
+Copyright(c), 2014, Carroll-Net, Inc.
+All Rights Reserved"""
+
+# ----------------------------------------------------------------------------
+# Standard library imports
+# ----------------------------------------------------------------------------
+from setuptools import setup
+
+try:
+    # http://bugs.python.org/issue15881#msg170215
+    # pylint: disable=W0611
+    import multiprocessing
+except ImportError:
+    pass
+
+# ----------------------------------------------------------------------------
+# Project imports
+# ----------------------------------------------------------------------------
+import signet
+
+# ----------------------------------------------------------------------------
+# Module level initializations
+# ----------------------------------------------------------------------------
+__pychecker__  = 'unusednames=__maintainer__,__status__'
+__version__    = '2.4.1'
+__author__     = 'Jim Carroll'
+__maintainer__ = 'Jim Carroll'
+__email__      = 'jim@carroll.com'
+__status__     = 'Distribution'
+__copyright__  = 'Copyright(c) 2014, Carroll-Net, Inc., All Rights Reserved'
+
+long_description = []
+
+try:
+    with open('docs/index.rst') as fin:
+        for line in fin:
+            if line.startswith('Project Background'):
+                break
+            long_description.append(line)
+except IOError:
+    pass
+
+setup(
+    # project meta-data
+
+    name = 'signet',
+    version = signet.__version__,
+    description = ('Signet provides support for building and '
+                        'delivering tamper resistant python to your '
+                        'users and customers.'),
+    long_description = ''.join(long_description),
+    author = 'Jim Carroll',
+    author_email = 'jim@carroll.com',
+    url = 'http://jamercee.github.io/signet',
+    download_url = 'http://github.com/jamercee/signet',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Security',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Software Distribution',
+        ],
+    license = 'Signet is licensed under the 3-clause BSD License',
+
+    # project details
+
+    packages = ['signet', 'signet.command'],
+    package_data = {'signet': ['command/templates/*', 
+                               'command/lib/*',
+                               'command/static/*',
+                               ]},
+
+    # testing (assumes you have nose installed)
+
+    test_suite      = 'nose.collector',
+    setup_requires  = ['nose>=1.0'],
+
+    )
+     
---
+++ packages/signet/signet-1.0.17.zip/signet-1.0.17/signet/command/templates/README.txt
@@ -0,0 +1,8 @@
+This directory contains the signet secure loader template. It will be
+customized by signet.commands.build_signet during the build phase.
+
+If you provide you're own loader template, you should consider including
+"loader.h" near the top of your code. This will include the three
+signet declarations (see loader.h for further explanation).
+
+
