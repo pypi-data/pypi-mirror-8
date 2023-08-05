b7524af67b3e66f71f4b8e8a1a4c5c3746cdae3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:13 +0000

    Add package sedge/1.0.0/sedge-1.0.0.tar.gz

---
+++ packages/sedge/sedge-1.0.0.tar.gz/sedge-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+dev_requires = ['flake8', 'nose']
+install_requires = ['requests']
+
+setup(
+    name = "sedge",
+    version = "1.0.0",
+    packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    extras_require = {
+        'dev': dev_requires
+    },
+    install_requires = install_requires,
+    entry_points = {
+        'console_scripts': [
+            'sedge = sedge.cli:main',
+        ],
+    }
+)
