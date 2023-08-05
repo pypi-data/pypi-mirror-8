d428f895b1548396f5800a0e65e287e8534893dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:54 +0000

    Add package sqoot/20140923/sqoot-20140923.tar.gz

---
+++ packages/sqoot/sqoot-20140923.tar.gz/sqoot-20140923/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# (c) 2014 Rajat Agarwal
+
+from setuptools import setup, find_packages
+
+import sqoot
+
+# Work around mbcs bug in distutils.
+# http://bugs.python.org/issue10945
+import codecs
+try:
+    codecs.lookup('mbcs')
+except LookupError:
+    ascii = codecs.lookup('ascii')
+    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
+    codecs.register(func)
+
+setup(
+    name='sqoot',
+    version='20140923',
+    author='Rajat Agarwal',
+    author_email='rajatagarwal@alumni.purdue.edu',
+    url='https://github.com/ragarwal6397',
+    description='Sqoot wrapper library',
+    long_description=open('./README.txt', 'r').read(),
+    download_url='https://github.com/ragarwal6397/sqoot/tarball/master',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'License :: OSI Approved :: MIT License',
+        ],
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.1',
+    ],
+    license='MIT License',
+    keywords='sqoot api',
+    include_package_data=True,
+    zip_safe=True,
+)
