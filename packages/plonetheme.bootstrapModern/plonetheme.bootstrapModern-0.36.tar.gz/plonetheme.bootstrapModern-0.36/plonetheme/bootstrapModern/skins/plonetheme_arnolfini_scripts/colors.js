37504169091ab461d8c161135ae38ea4d9b61302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package plonetheme.bootstrapModern/0.36/plonetheme.bootstrapModern-0.36.tar.gz

---
+++ packages/plonetheme.bootstrapModern/plonetheme.bootstrapModern-0.36.tar.gz/plonetheme.bootstrapModern-0.36/plonetheme/bootstrapModern/skins/plonetheme_arnolfini_scripts/colors.js
@@ -0,0 +1,8 @@
+Arnolfini.colorMapping = [
+     {'tag': 'Exhibition', 'class': 'red'},
+     {'tag': 'Family', 'class': 'green'},
+     {'tag': 'Music', 'class': 'orange'},
+     {'tag': 'Live', 'class': 'orange'},
+     {'tag': 'Performance', 'class': 'purple'},
+];
+
