fb9004c11bc1346f3b47dd14c6cec563346cfa1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:58 +0000

    Add package ChatterBot/0.0.0/ChatterBot-0.0.0.tar.gz

---
+++ packages/ChatterBot/ChatterBot-0.0.0.tar.gz/ChatterBot-0.0.0/tests/tests.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from ChatterBot.engram import Engram
+
+
+class Tests(TestCase):
+
+    def test_engram(self):
+        """
+        Make sure that text is returned from an engram.
+        """
+        engram = Engram()
+        response = engram.engram("hello")
+
+        self.assertTrue(len(response) > 0)
+
+    def test_twitter_api(self):
+        """
+        Make sure that results from the twitter api can be used.
+        """
+        pass
