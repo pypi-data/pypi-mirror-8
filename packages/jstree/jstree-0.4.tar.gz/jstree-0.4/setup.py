b9a05e0dd2cfd00e4d6f0d45b865ab0f17c4f9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package jstree/0.4/jstree-0.4.tar.gz

---
+++ packages/jstree/jstree-0.4.tar.gz/jstree-0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+
+def read(filename):
+  fin = None
+  data = None
+  try:
+    fin = open(filename)
+    data = fin.read()
+  finally:
+    if fin is not None:
+      fin.close()
+  return data
+
+setup(
+  name='jstree',
+  version='0.4',
+  author='William Grim',
+  author_email='william@grimapps.com',
+  url='https://github.com/grimwm/py-jstree',
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+  description='A package that helps generate JSON data for jQuery jsTree.',
+  long_description=read('README.txt') if os.path.exists('README.txt') else '',
+  install_requires=['dictobj'],
+  py_modules=['jstree'],
+  test_suite='jstree_test',
+  )
