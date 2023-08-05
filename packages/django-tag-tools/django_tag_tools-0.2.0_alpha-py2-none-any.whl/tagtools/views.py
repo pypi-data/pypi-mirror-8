ac01d5e58ccc4e42453b605d81dcd2e7d28f3d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package django-tag-tools/0.2.0-alpha/django_tag_tools-0.2.0_alpha-py2-none-any.whl

---
+++ packages/django-tag-tools/django_tag_tools-0.2.0_alpha-py2-none-any.whl/tagtools/views.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+
+from tagtools.settings import *
+
+if TAG_APP == 'taggit':
+    from tagtools.backends.taggit.views import tagged_object_list
+elif TAG_APP == 'tagging':
+    from tagtools.backends.tagging.views import tagged_object_list
+else:
+    def tagged_object_list(*args, **kwargs):
+        raise NotImplementedError('No tag-app found, set to: %s' % TAG_APP)
