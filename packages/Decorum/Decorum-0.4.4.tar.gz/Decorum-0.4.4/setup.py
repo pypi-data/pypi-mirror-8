399e326b9e3244a969b0c7e3d154fc97e06226ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package Decorum/0.4.4/Decorum-0.4.4.tar.gz

---
+++ packages/Decorum/Decorum-0.4.4.tar.gz/Decorum-0.4.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Decorum',
+    version='0.4.4',
+    author='Zach Kelling',
+    author_email='zk@monoid.io',
+    packages=['decorum',],
+    license='LICENSE',
+    description='Tool for writing simple decorators',
+    long_description=open('README.rst').read(),
+)
