060b1369d96b94a637c976f5d56eb3534ec513ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:17 +0000

    Add package wpparser/1.0.2/wpparser-1.0.2.tar.gz

---
+++ packages/wpparser/wpparser-1.0.2.tar.gz/wpparser-1.0.2/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests"""
+
+import os
+import unittest
+from wpparser import parse
+
+
+class ParseTestCase(unittest.TestCase):
+    def test_parse(self):
+        result = parse("./blog.wordpress.2014-09-26.xml")
+
+        assert len(result["posts"]) is 3
+        assert result["blog"]["title"] == "Blog"
+        assert len(result["categories"]) is 1
+        assert len(result["tags"]) is 1
+
+    def test_attachment_metadata(self):
+        result = parse("./blog.wordpress.2014-09-26.xml")
+
+        post = result["posts"][2]
+
+        assert "postmeta" in post
+        assert "attached_file" in post["postmeta"]
+        assert "attachment_metadata" in post["postmeta"]
+
+
+if __name__ == "__main__":
+    unittest.main()
