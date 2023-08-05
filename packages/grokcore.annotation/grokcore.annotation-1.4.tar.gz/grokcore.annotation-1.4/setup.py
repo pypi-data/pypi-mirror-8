239133a10dd1c9b636a9cf658c3a8ab5bdac1238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:17 +0000

    Add package grokcore.annotation/1.4/grokcore.annotation-1.4.tar.gz

---
+++ packages/grokcore.annotation/grokcore.annotation-1.4.tar.gz/grokcore.annotation-1.4/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    )
+
+tests_require = [
+    'zope.configuration',
+    'zope.testing',
+    ]
+
+
+setup(
+    name='grokcore.annotation',
+    version='1.4',
+    author='Grok Team',
+    author_email='grok-dev@zope.org',
+    url='http://grok.zope.org',
+    download_url='http://pypi.python.org/pypi/grokcore.annotation',
+    description='Grok-like configuration for Zope annotations',
+    long_description=long_description,
+    license='ZPL',
+    classifiers=['Environment :: Web Environment',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Zope Public License',
+                 'Programming Language :: Python',
+                 'Framework :: Zope3',
+                 ],
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['grokcore'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'ZODB3',
+        'grokcore.component >= 2.5dev',
+        'martian',
+        'setuptools',
+        'zope.annotation',
+        'zope.component',
+        'zope.container',
+        'zope.interface',
+        ],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    )
