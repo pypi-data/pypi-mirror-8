5c14f06540d93ebdae7ce8b8649aa0b9ba2d06cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:04 +0000

    Add package hashtag/0.1/hashtag-0.1.zip

---
+++ packages/hashtag/hashtag-0.1.zip/hashtag-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hashtag',
+      version='0.1',
+      description='hashtagging for immortals.',
+      url='http://github.com/adivandhya/hashTag',
+      author='Adivandhya | Rahul | Sameer',
+      author_email='adivandhya@yahoo.co.in',
+      license='MIT',
+      packages=['hashTag'],
+      zip_safe=False)
