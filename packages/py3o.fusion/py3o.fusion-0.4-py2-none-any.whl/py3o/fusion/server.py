04b06bc384fe91f2bb18216c29e4b7322b65ab2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:40 +0000

    Add package py3o.fusion/0.4/py3o.fusion-0.4-py2-none-any.whl

---
+++ packages/py3o.fusion/py3o.fusion-0.4-py2-none-any.whl/py3o/fusion/server.py
@@ -0,0 +1,68 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+
+import optparse
+
+from twisted.web.server import Site
+from twisted.web.static import File
+from twisted.web.resource import Resource
+from twisted.internet import reactor
+
+import pkg_resources
+
+from py3o.fusion.formpage import FormPage
+from py3o.fusion.indexpage import RootPage
+from py3o.fusion.featurespage import FeaturesPage
+
+
+def cmd_line_server():
+    logging.info("py3o.fusion server starting")
+    optparser = optparse.OptionParser()
+
+    optparser.add_option(
+        "-p", "--listenport",
+        dest="listen_port",
+        help="specify the PORT on which our service will listen",
+        metavar="PORT",
+        default=8765)
+
+    optparser.add_option(
+        "-s", "--renderserver",
+        dest="render_server",
+        help="specify the hostname/ip of the render server",
+        metavar="RENDERSERVER",
+        default=None)
+
+    optparser.add_option(
+        "-r", "--renderport",
+        dest="render_port",
+        help="specify the PORT on which the renderserver is available",
+        metavar="RPORT",
+        default=8994)
+
+    (options, args) = optparser.parse_args()
+
+    logging.info("listing on port: %s" % options.listen_port)
+    start_server(options)
+
+
+def start_server(options):
+
+    reactor.suggestThreadPoolSize(30)
+
+    root = Resource()
+    root.putChild(
+        "static",
+        File(
+            pkg_resources.resource_filename("py3o.fusion", "static")
+        )
+    )
+    root.putChild("", RootPage())
+
+    formpage = FormPage(options.render_server, options.render_port)
+
+    root.putChild("form", formpage)
+    root.putChild("features", FeaturesPage())
+    factory = Site(root)
+    reactor.listenTCP(int(options.listen_port), factory)
+    reactor.run(installSignalHandlers=1)
