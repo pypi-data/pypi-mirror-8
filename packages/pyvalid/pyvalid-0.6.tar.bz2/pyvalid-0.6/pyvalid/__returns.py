a1aa5c089c2ce92a91d745589993b6634b75869e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:58 +0000

    Add package pyvalid/0.6/pyvalid-0.6.tar.bz2

---
+++ packages/pyvalid/pyvalid-0.6.tar.bz2/pyvalid-0.6/pyvalid/__returns.py
@@ -0,0 +1,31 @@
+from collections import Callable
+from pyvalid.__exceptions import InvalidReturnType
+
+
+class Returns(Callable):
+    """A decorator to validate the returns value of a given function.
+    """
+
+    def __init__(self, *accepted_returns_values):
+        self.accepted_returns_values = accepted_returns_values
+
+    def __call__(self, func):
+        def decorator_wrapper(*func_args, **func_kwargs):
+            returns_val = func(*func_args, **func_kwargs)
+            if self.accepted_returns_values:
+                is_valid = False
+                for accepted_returns_val in self.accepted_returns_values:
+                    if isinstance(accepted_returns_val, type):
+                        is_valid = isinstance(
+                            returns_val, accepted_returns_val
+                        )
+                    elif isinstance(accepted_returns_val, Callable):
+                        is_valid = accepted_returns_val(returns_val)
+                    else:
+                        is_valid = returns_val == accepted_returns_val
+                    if is_valid:
+                        break
+                if not is_valid:
+                    raise InvalidReturnType(type(returns_val), func.__name__)
+            return returns_val
+        return decorator_wrapper
