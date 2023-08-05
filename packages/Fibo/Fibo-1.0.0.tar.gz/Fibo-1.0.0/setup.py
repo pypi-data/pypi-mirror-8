5f9fdd9343be27f1089169cfb2484985e5990721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:36 +0000

    Add package Fibo/1.0.0/Fibo-1.0.0.tar.gz

---
+++ packages/Fibo/Fibo-1.0.0.tar.gz/Fibo-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'Fibo',
+	version = '1.0.0',
+	py_modules = ['Fibo'],
+	author = 'xingning',
+	author_email = 'xingningjing0507@163.com',
+	url = 'http://www.baidu.com',
+	description = 'Some functions about Fibo',
+)
