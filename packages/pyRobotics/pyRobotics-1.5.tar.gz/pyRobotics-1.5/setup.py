e9a6cf5b41d52fa175954855f2e4e008ecabbe72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:50 +0000

    Add package pyRobotics/1.5/pyRobotics-1.5.tar.gz

---
+++ packages/pyRobotics/pyRobotics-1.5.tar.gz/pyRobotics-1.5/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='pyRobotics',
+    version='1.5',
+    author='Adrián Revuelta Cuauhtli',
+    author_email='adrianrc.89@gmail.com',
+    url='http://bioroboticsunam.github.io/pyRobotics',
+    license='LICENSE.txt',
+    data_files=[('', ['README.md', 'LICENSE.txt'])],
+    packages=['pyrobotics'])
