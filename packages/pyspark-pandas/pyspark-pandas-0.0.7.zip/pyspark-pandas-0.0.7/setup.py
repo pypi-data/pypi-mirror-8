07d82b23c9999ac6b29005b91b308d7be8274b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:36 +0000

    Add package pyspark-pandas/0.0.7/pyspark-pandas-0.0.7.zip

---
+++ packages/pyspark-pandas/pyspark-pandas-0.0.7.zip/pyspark-pandas-0.0.7/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+try:
+    from setuptools import find_packages
+except ImportError:
+    print ("Please install Distutils and setuptools"
+           " before installing this package")
+    raise
+
+setup(
+    name='pyspark-pandas',
+    version='0.0.7',
+    description=(
+        'Tools and algorithms for pandas Dataframes distributed on pyspark.'
+        ' Please consider the SparklingPandas project before this one'),
+    long_description="Check the project homepage for details",
+    keywords=['spark pyspark pandas dataframe series', ],
+
+    author='Alex Gaudio',
+    author_email='adgaudio@gmail.com',
+    url='https://github.com/adgaudio/pyspark_pandas',
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires = ['pandas'],
+    tests_require=['nose'],
+    test_suite="nose.main",
+)
