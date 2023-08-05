4ff043fc28444f82f6caf936d347ee49ef47321b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:57 +0000

    Add package requery/0.3.1/requery-0.3.1.zip

---
+++ packages/requery/requery-0.3.1.zip/requery-0.3.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup
+import setuplib
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
+
+packages, package_data = setuplib.find_packages('requery')
+
+setup(
+    name='requery',
+    version='0.3.1',
+    url='http://github.com/ebertti/requery/',
+    author='Ezequiel Bertti',
+    author_email='ebertti@gmail.com',
+    install_requires=reqs,
+    packages=['requery'],
+    license='MIT License',
+    platforms=['OS Independent'],
+    description="Simple way to store and use querys in database for use of DBA's for Django Admin",
+    long_description=(open('README.rst').read()),
+    keywords = ['query', 'dba', 'repository', 'report']
+)
