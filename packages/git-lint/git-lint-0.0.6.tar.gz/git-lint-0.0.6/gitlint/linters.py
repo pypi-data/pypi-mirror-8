4f3545a24d824377443ea452bde183a1095a2978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:11 +0000

    Add package git-lint/0.0.6/git-lint-0.0.6.tar.gz

---
+++ packages/git-lint/git-lint-0.0.6.tar.gz/git-lint-0.0.6/gitlint/linters.py
@@ -0,0 +1,192 @@
+# Copyright 2013-2014 Sebastian Kreft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Functions for invoking a lint command."""
+
+import collections
+import functools
+import os
+import os.path
+import subprocess
+
+import gitlint.utils as utils
+
+
+class Partial(functools.partial):
+    """Wrapper around functools partial to support equality comparisons."""
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and self.args == other.args
+                and self.keywords == other.keywords)
+
+    def __repr__(self):
+        # This method should never be executed, only in failing tests.
+        return ('Partial: func: %s, args: %s, kwargs: %s' %
+            (self.func.__name__, self.args, self.keywords))  # pragma: no cover
+
+
+def missing_requirements_command(unused_name, missing_programs,
+                                 installation_string, filename, unused_lines):
+    """Pseudo-command to be used when requirements are missing."""
+    verb = 'is'
+    if len(missing_programs) > 1:
+        verb = 'are'
+    return {
+        filename: {
+            'skipped': [
+                '%s %s not installed. %s' % (', '.join(missing_programs),
+                                             verb,
+                                             installation_string)
+            ]
+        }
+    }
+
+
+# TODO(skreft): add test case for result already in cache.
+def lint_command(name, program, arguments, filter_regex, filename, lines):
+    """Executes a lint program and filter the output.
+
+    Executes the lint tool 'program' with arguments 'arguments' over the file
+    'filename' returning only those lines matching the regular expression
+    'filter_regex'.
+
+    Args:
+      name: string: the name of the linter.
+      program: string: lint program.
+      arguments: list[string]: extra arguments for the program.
+      filter_regex: string: regular expression to filter lines.
+      filename: string: filename to lint.
+      lines: list[int]|None: list of lines that we want to capture. If None,
+        then all lines will be captured.
+
+    Returns: dict: a dict with the extracted info from the message.
+    """
+    output = utils.get_output_from_cache(name, filename)
+
+    if output is None:
+        call_arguments = [program] + arguments + [filename]
+        try:
+            output = subprocess.check_output(call_arguments,
+                                             stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as error:
+            output = error.output
+        except OSError:
+            return {
+                filename: {
+                    'error': [('Could not execute "%s".%sMake sure all ' +
+                               'required programs are installed') %
+                              (' '.join(call_arguments), os.linesep)]
+                }
+            }
+        output = output.decode('utf-8')
+        utils.save_output_in_cache(name, filename, output)
+
+    output_lines = output.split(os.linesep)
+
+    if lines is None:
+        lines_regex = r'\d+'
+    else:
+        lines_regex = '|'.join(map(str, lines))
+    lines_regex = '(%s)' % lines_regex
+
+    groups = ('line', 'column', 'message', 'severity', 'message_id')
+    filtered_lines = utils.filter_lines(output_lines,
+                                        filter_regex % {'lines': lines_regex,
+                                                        'filename': filename},
+                                        groups=groups)
+
+    result = []
+    for data in filtered_lines:
+        comment = dict(p for p in zip(groups, data) if p[1] is not None)
+        if 'line' in comment:
+            comment['line'] = int(comment['line'])
+        if 'column' in comment:
+            comment['column'] = int(comment['column'])
+        if 'severity' in comment:
+            comment['severity'] = comment['severity'].title()
+        result.append(comment)
+
+    return {
+        filename: {
+            'comments': result
+        }
+    }
+
+
+# TODO(skreft): validate data['filter'], ie check that only has valid fields.
+def parse_yaml_config(yaml_config, repo_home):
+    """Converts a dictionary (parsed Yaml) to the internal representation."""
+    config = collections.defaultdict(list)
+
+    variables = {
+        'DEFAULT_CONFIGS': os.path.join(os.path.dirname(__file__),
+                                        'configs'),
+        'REPO_HOME': repo_home,
+    }
+
+    for name, data in yaml_config.items():
+        command = data['command'] % variables
+        requirements = [req % variables for req in data.get('requirements', [])]
+        arguments = [arg % variables for arg in data.get('arguments', [])]
+
+        not_found_programs = utils.programs_not_in_path(
+            [command] + requirements)
+        if not_found_programs:
+            linter_command = Partial(missing_requirements_command,
+                                     name,
+                                     not_found_programs,
+                                     data['installation'])
+        else:
+            linter_command = Partial(lint_command,
+                                     name,
+                                     command,
+                                     arguments,
+                                     data['filter'])
+        for extension in data['extensions']:
+            config[extension].append(linter_command)
+
+    return config
+
+
+def lint(filename, lines, config):
+    """Lints a file.
+
+    Args:
+        filename: string: filename to lint.
+        lines: list[int]|None: list of lines that we want to capture. If None,
+          then all lines will be captured.
+        config: dict[string: linter]: mapping from extension to a linter
+          function.
+
+    Returns: dict: if there were errors running the command then the field
+      'error' will have the reasons in a list. if the lint process was skipped,
+      then a field 'skipped' will be set with the reasons. Otherwise, the field
+      'comments' will have the messages.
+    """
+    _, ext = os.path.splitext(filename)
+    if ext in config:
+        output = collections.defaultdict(list)
+        for linter in config[ext]:
+            linter_output = linter(filename, lines)
+            for category, values in linter_output[filename].items():
+                output[category].extend(values)
+
+        return {
+            filename: dict(output)
+        }
+    else:
+        return {
+            filename: {
+                'skipped': ['SKIPPED: no linter is defined or enabled for files'
+                            ' with extension "%s"' % ext]
+            }
+        }
---
+++ packages/git-lint/git-lint-0.0.6.tar.gz/git-lint-0.0.6/test/unittest/test_linters.py
@@ -0,0 +1,464 @@
+# Copyright 2013-2014 Sebastian Kreft
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import functools
+import os
+import subprocess
+import unittest
+
+import mock
+
+import gitlint
+import gitlint.utils
+import gitlint.linters as linters
+
+# pylint: disable=too-many-public-methods,protected-access
+
+
+class LintersTest(unittest.TestCase):
+    def test_lint_command_success(self):
+        with mock.patch('subprocess.check_output') as check_output, \
+                mock.patch('os.path.getmtime', side_effect=[1, 0, 1, 0]):
+            check_output.return_value = os.linesep.join([
+                'Line 1:1: 1',
+                'Line 5:2: 5',
+                'Line 7:3: 7',
+                'Line 9:4: 9']).encode('utf-8')
+            command = functools.partial(
+                linters.lint_command,
+                'l',
+                'linter',
+                ['-f', '--compact'],
+                r'^Line (?P<line>%(lines)s):(?P<column>\d+): (?P<message>.*)$')
+            filename = 'foo.txt'
+            self.assertEqual(
+                {
+                    filename: {
+                        'comments': [
+                            {
+                                'line': 5,
+                                'column': 2,
+                                'message': '5'
+                            },
+                            {
+                                'line': 7,
+                                'column': 3,
+                                'message': '7'
+                            },
+                        ],
+                    },
+                },
+                command(filename, lines=[3, 5, 7]))
+            self.assertEqual(
+                {
+                    filename: {
+                        'comments': [
+                            {
+                                'line': 1,
+                                'column': 1,
+                                'message': '1'
+                            },
+                            {
+                                'line': 5,
+                                'column': 2,
+                                'message': '5'
+                            },
+                            {
+                                'line': 7,
+                                'column': 3,
+                                'message': '7'
+                            },
+                            {
+                                'line': 9,
+                                'column': 4,
+                                'message': '9'
+                            },
+                        ],
+                    },
+                },
+                command(filename, lines=None))
+            expected_calls = [
+                mock.call(['linter', '-f', '--compact', 'foo.txt'],
+                          stderr=subprocess.STDOUT),
+                mock.call(['linter', '-f', '--compact', 'foo.txt'],
+                          stderr=subprocess.STDOUT)]
+            self.assertEqual(expected_calls, check_output.call_args_list)
+
+    def test_lint_command_all_fields(self):
+        with mock.patch('subprocess.check_output') as check_output, \
+                mock.patch('os.path.getmtime', side_effect=[1, 0, 1, 0]):
+            check_output.return_value = os.linesep.join(
+                ['ERROR: line 1, col 1: (W32) missing foo']).encode('utf-8')
+            command = functools.partial(
+                linters.lint_command,
+                'l',
+                'linter',
+                ['-f', '--compact'],
+                r'^(?P<severity>.*): line (?P<line>%(lines)s)(, col ' +
+                r'(?P<column>\d+)): \((?P<message_id>.*)\) (?P<message>.*)$')
+            filename = 'foo.txt'
+            self.assertEqual(
+                {
+                    filename: {
+                        'comments': [
+                            {
+                                'line': 1,
+                                'column': 1,
+                                'message': 'missing foo',
+                                'message_id': 'W32',
+                                'severity': 'Error',
+                            },
+                        ],
+                    },
+                },
+                command(filename, lines=None))
+
+    def test_lint_command_error(self):
+        output = os.linesep.join([
+            'Line 1: 1',
+            'Line 5: 5',
+            'Line 7: 7',
+            'Line 9: 9']).encode('utf-8')
+        with mock.patch('subprocess.check_output',
+                        side_effect=subprocess.CalledProcessError(
+                            1, 'linter', output)) as check_output, \
+                mock.patch('os.path.getmtime', side_effect=[1, 0, 1, 0]):
+            command = functools.partial(
+                linters.lint_command,
+                'l',
+                'linter',
+                ['-f', '--compact'],
+                '^Line (?P<line>%(lines)s): (?P<message>.*)$')
+            filename = 'foo.txt'
+            self.assertEqual(
+                {
+                    filename: {
+                        'comments': [
+                            {
+                                'line': 5,
+                                'message': '5'
+                            },
+                            {
+                                'line': 7,
+                                'message': '7'
+                            },
+                        ],
+                    },
+                },
+                command(filename, lines=[3, 5, 7]))
+            self.assertEqual(
+                {
+                    filename: {
+                        'comments': [
+                            {
+                                'line': 1,
+                                'message': '1'
+                            },
+                            {
+                                'line': 5,
+                                'message': '5'
+                            },
+                            {
+                                'line': 7,
+                                'message': '7'
+                            },
+                            {
+                                'line': 9,
+                                'message': '9'
+                            },
+                        ],
+                    },
+                },
+                command(filename, lines=None))
+            expected_calls = [
+                mock.call(['linter', '-f', '--compact', 'foo.txt'],
+                          stderr=subprocess.STDOUT),
+                mock.call(['linter', '-f', '--compact', 'foo.txt'],
+                          stderr=subprocess.STDOUT)]
+            self.assertEqual(expected_calls, check_output.call_args_list)
+
+    def test_lint_command_not_found(self):
+        with mock.patch('subprocess.check_output',
+                        side_effect=OSError('Not found')) as check_output, \
+                mock.patch('os.path.getmtime', side_effect=[1, 0]):
+            command = functools.partial(linters.lint_command,
+                                        'l',
+                                        'linter',
+                                        ['-f', '--compact'],
+                                        '^Line (%(lines)s):')
+            filename = 'foo.txt'
+            output = command(filename, lines=[3, 5, 7])
+            self.assertEqual(1, len(output[filename]['error']))
+            output[filename]['error'] = []
+            self.assertTrue(
+                {
+                    filename: {
+                        'error': ''
+                    }
+                },
+                output)
+            expected_calls = [
+                mock.call(['linter', '-f', '--compact', 'foo.txt'],
+                          stderr=subprocess.STDOUT)]
+            self.assertEqual(expected_calls, check_output.call_args_list)
+
+    def test_lint(self):
+        linter1 = functools.partial(
+            linters.lint_command,
+            'l1',
+            'linter1', ['-f'],
+            '^Line (?P<line>%(lines)s): (?P<message>.*)$')
+        linter2 = functools.partial(
+            linters.lint_command,
+            'l2',
+            'linter2', [],
+            '^ line (?P<line>%(lines)s): (?P<message>.*)$')
+        config = {
+            '.txt': [linter1, linter2]
+        }
+        outputs = [os.linesep.join(['Line 1: 1', 'Line 5: 5']).encode('utf-8'),
+                   os.linesep.join([' line 4: 4']).encode('utf-8')]
+        with mock.patch('subprocess.check_output',
+                        side_effect=outputs) as check_output, \
+                mock.patch('os.path.getmtime', side_effect=[1, 0, 1, 0]):
+            filename = 'foo.txt'
+            self.assertEqual(
+                {
+                    filename: {
+                        'comments': [
+                            {
+                                'line': 5,
+                                'message': '5'
+                            },
+                            {
+                                'line': 4,
+                                'message': '4'
+                            },
+                        ],
+                    },
+                },
+                linters.lint(filename, lines=[4, 5], config=config))
+            expected_calls = [
+                mock.call(['linter1', '-f', 'foo.txt'],
+                          stderr=subprocess.STDOUT),
+                mock.call(['linter2', 'foo.txt'], stderr=subprocess.STDOUT)]
+            self.assertEqual(expected_calls, check_output.call_args_list)
+
+    def test_lint_one_empty_lint(self):
+        linter1 = functools.partial(
+            linters.lint_command,
+            'l1',
+            'linter1',
+            ['-f'],
+            '^Line (?P<line>%(lines)s): (?P<message>.*)$')
+        linter2 = functools.partial(
+            linters.lint_command,
+            'l2',
+            'linter2',
+            [],
+            '^ line (?P<line>%(lines)s): (?P<message>.*)$')
+        config = {
+            '.txt': [linter1, linter2]
+        }
+        outputs = [b'',
+                   os.linesep.join([' line 4: 4']).encode('utf-8')]
+        with mock.patch('subprocess.check_output',
+                        side_effect=outputs) as check_output, \
+                mock.patch('os.path.getmtime', side_effect=[1, 0, 1, 0]):
+            filename = 'foo.txt'
+            self.assertEqual(
+                {
+                    filename: {
+                        'comments': [
+                            {
+                                'line': 4,
+                                'message': '4'
+                            },
+                        ],
+                    },
+                },
+                linters.lint(filename, lines=[4, 5], config=config))
+            expected_calls = [
+                mock.call(['linter1', '-f', 'foo.txt'],
+                          stderr=subprocess.STDOUT),
+                mock.call(['linter2', 'foo.txt'], stderr=subprocess.STDOUT)]
+            self.assertEqual(expected_calls, check_output.call_args_list)
+
+    def test_lint_all_empty_lint(self):
+        linter1 = functools.partial(
+            linters.lint_command, 'l1', 'linter1', ['-f'], '^Line %(lines)s:')
+        linter2 = functools.partial(
+            linters.lint_command, 'l2', 'linter2', [], '^ line %(lines)s:')
+        config = {
+            '.txt': [linter1, linter2]
+        }
+        outputs = [b'', b'']
+        with mock.patch('subprocess.check_output',
+                        side_effect=outputs) as check_output, \
+                mock.patch('os.path.getmtime', side_effect=[1, 0, 1, 0]):
+            filename = 'foo.txt'
+            self.assertEqual(
+                {
+                    filename: {
+                        'comments': []
+                    }
+                },
+                linters.lint(filename, lines=[4, 5], config=config))
+            expected_calls = [
+                mock.call(['linter1', '-f', 'foo.txt'],
+                          stderr=subprocess.STDOUT),
+                mock.call(['linter2', 'foo.txt'], stderr=subprocess.STDOUT)]
+            self.assertEqual(expected_calls, check_output.call_args_list)
+
+    def test_lint_extension_not_defined(self):
+        config = {}
+        output = linters.lint('foo.txt', lines=[4, 5], config=config)
+        self.assertEqual(1, len(output['foo.txt']['skipped']))
+        output['foo.txt']['skipped'] = []
+        self.assertEqual(
+            {
+                'foo.txt': {
+                    'skipped': []
+                }
+            },
+            output)
+
+    def test_lint_missing_programs(self):
+        linter1 = functools.partial(
+            linters.missing_requirements_command,
+            'l1', ['p1', 'p2'], 'Install p1 and p2')
+        config = {
+            '.txt': [linter1]
+        }
+        output = linters.lint('foo.txt', lines=[4, 5], config=config)
+        self.assertEqual(1, len(output['foo.txt']['skipped']))
+        output['foo.txt']['skipped'] = []
+        self.assertEqual(
+            {
+                'foo.txt': {
+                    'skipped': []
+                }
+            },
+            output)
+
+    def test_lint_two_missing_programs(self):
+        linter1 = functools.partial(
+            linters.missing_requirements_command,
+            'l1', ['p1', 'p2'], 'Install p1 and p2')
+        config = {
+            '.txt': [linter1, linter1]
+        }
+        output = linters.lint('foo.txt', lines=[4, 5], config=config)
+        self.assertEqual(2, len(output['foo.txt']['skipped']))
+        output['foo.txt']['skipped'] = []
+        self.assertEqual(
+            {
+                'foo.txt': {
+                    'skipped': []
+                }
+            },
+            output)
+
+    def test_parse_yaml_config_command_not_in_path(self):
+        yaml_config = {
+            'linter': {
+                'arguments': [],
+                'command': 'some_unexistent_program_name',
+                'extensions': ['.foo'],
+                'filter': '.*',
+                'installation': ('Go to some_unexistent_program_name.com to ' +
+                                 'install it.'),
+            }
+        }
+        config = linters.parse_yaml_config(yaml_config, '')
+        self.assertEqual(
+            {
+                'filename': {
+                    'skipped': ['some_unexistent_program_name is not ' +
+                                'installed. Go to some_unexistent_program_' +
+                                'name.com to install it.']
+                }
+            },
+            config['.foo'][0]('filename', []))
+
+    def test_parse_yaml_config_requirements_not_in_path(self):
+        yaml_config = {
+            'linter': {
+                'arguments': [],
+                'command': 'ls',
+                'requirements': [
+                    'some_unexistent_command_one',
+                    'ls',
+                    'some_unexistent_command_two',
+                ],
+                'extensions': ['.foo'],
+                'filter': '.*',
+                'installation': 'Run apt-get install command_one command_two',
+            }
+        }
+        config = linters.parse_yaml_config(yaml_config, '')
+        self.assertEqual(
+            {
+                'filename': {
+                    'skipped': ['some_unexistent_command_one, ' +
+                                'some_unexistent_command_two are not '
+                                'installed. Run apt-get install command_one ' +
+                                'command_two']
+                }
+            },
+            config['.foo'][0]('filename', []))
+
+    def test_parse_yaml_config_with_variables(self):
+        yaml_config_with_vars = {
+            'linter': {
+                'arguments': [
+                    '--config',
+                    '%(DEFAULT_CONFIGS)s/foo.config'
+                ],
+                'command': '%(REPO_HOME)s/bin/linter',
+                'requirements': [
+                    '%(REPO_HOME)s/bin/dep1',
+                ],
+                'extensions': ['.foo'],
+                'filter': '.*',
+                'installation': 'install',
+            }
+        }
+        variables = {
+            'DEFAULT_CONFIGS': os.path.join(os.path.dirname(gitlint.__file__),
+                                            'configs'),
+            'REPO_HOME': '/usr/home/repo',
+        }
+        yaml_config_no_vars = {
+            'linter': {
+                'arguments': [
+                    '--config',
+                    '%(DEFAULT_CONFIGS)s/foo.config' % variables
+                ],
+                'command': '%(REPO_HOME)s/bin/linter' % variables,
+                'requirements': [
+                    '%(REPO_HOME)s/bin/dep1' % variables,
+                ],
+                'extensions': ['.foo'],
+                'filter': '.*',
+                'installation': 'install',
+            }
+        }
+
+        with mock.patch('gitlint.utils.which', return_value=['lint']):
+            config_with_vars = linters.parse_yaml_config(
+                yaml_config_with_vars, variables['REPO_HOME'])
+            config_no_vars = linters.parse_yaml_config(
+                yaml_config_no_vars, variables['REPO_HOME'])
+
+            self.assertEqual(config_with_vars, config_no_vars)
