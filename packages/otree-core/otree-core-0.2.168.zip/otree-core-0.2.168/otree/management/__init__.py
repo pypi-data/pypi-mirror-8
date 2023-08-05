5256cdffe221b2ac71512e602bc0f2ccaa242b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:04 +0000

    Add package otree-core/0.2.168/otree-core-0.2.168.zip

---
+++ packages/otree-core/otree-core-0.2.168.zip/otree-core-0.2.168/otree/management/__init__.py
@@ -0,0 +1,53 @@
+from django.conf import settings
+from django.contrib.auth import models as auth_models
+from django.contrib.auth.management import create_superuser
+from django.db.models import signals
+import otree.constants
+from otree.sessionlib.models import StubModel, GlobalData, Session
+
+
+def create_default_superuser(app, created_models, verbosity, **kwargs):
+    """
+    Creates our default superuser.
+    """
+    username = settings.ADMIN_USERNAME
+    password = settings.ADMIN_PASSWORD
+    email = getattr(settings, 'ADMIN_EMAIL', '')
+    try:
+        auth_models.User.objects.get(username=username)
+    except auth_models.User.DoesNotExist:
+        s = '\n'.join(['Creating default superuser.',
+             'Username: {}'.format(username),
+             'Email: {}'.format(email)])
+        assert auth_models.User.objects.create_superuser(username, email, password)
+    else:
+        print 'Default superuser already exists.'
+
+if getattr(settings, 'CREATE_DEFAULT_SUPERUSER', False):
+    # From http://stackoverflow.com/questions/1466827/:
+    # Prevent interactive question about wanting a superuser created.
+    # (This code has to go in this otherwise empty "models" module
+    # so that it gets processed by the "syncdb" command during
+    # database creation.)
+    signals.post_syncdb.disconnect(
+        create_superuser,
+        sender=auth_models,
+        dispatch_uid='django.contrib.auth.management.create_superuser'
+    )
+
+    # Trigger default superuser creation.
+    signals.post_syncdb.connect(
+        create_default_superuser,
+        sender=auth_models,
+        dispatch_uid='common.models.create_testuser'
+    )
+
+def create_singleton_objects(sender, **kwargs):
+    if sender.__name__ == 'django.contrib.auth.models':
+        for ModelClass in [StubModel, GlobalData]:
+            # if it doesn't already exist, create one.
+            if not ModelClass.objects.all().exists():
+                ModelClass().save()
+
+signals.post_syncdb.connect(create_singleton_objects)
+
