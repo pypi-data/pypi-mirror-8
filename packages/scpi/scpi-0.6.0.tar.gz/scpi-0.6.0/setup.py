5f841edd223e925624b72c8557183e8791e1a855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:37 +0000

    Add package scpi/0.6.0/scpi-0.6.0.tar.gz

---
+++ packages/scpi/scpi-0.6.0.tar.gz/scpi-0.6.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+import subprocess
+
+git_version = str(subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD'])).strip()
+
+setup(
+    name='scpi',
+#    version='0.6.0dev-%s' % git_version,
+    version='0.6.0',
+    author='Eero "rambo" af Heurlin',
+    author_email='rambo@iki.fi',
+    packages=['scpi',],
+    license='GNU LGPL',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'pyserial>=2.7',
+    ],
+    url='https://github.com/rambo/python-scpi',
+)
+
