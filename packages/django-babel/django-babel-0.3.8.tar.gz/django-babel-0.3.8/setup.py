585504277c28277a3870b1b7ca1bb868b3fdc777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:29 +0000

    Add package django-babel/0.3.8/django-babel-0.3.8.tar.gz

---
+++ packages/django-babel/django-babel-0.3.8.tar.gz/django-babel-0.3.8/setup.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import codecs
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+
+test_requires = [
+    'coverage',
+    'pytest',
+    'pytest-cov>=1.4',
+    'pytest-flakes',
+    'pytest-pep8',
+    'python-coveralls',
+]
+
+
+install_requires = [
+    'Django>=1.4,<1.8',
+    'Babel>=1.3',
+]
+
+
+dev_requires = [
+    'flake8>=2.0',
+    'invoke',
+    'twine'
+]
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='django-babel',
+    description='Utilities for using Babel in Django',
+    long_description=read('README.rst') + u'\n\n' + read('CHANGELOG.rst'),
+    version='0.3.8',
+    license='BSD',
+    author='Christopher Grebs',
+    author_email='cg@webshox.org',
+    url='http://github.com/graingert/django-babel/',
+    packages=find_packages(exclude=('tests',)),
+    tests_require=test_requires,
+    install_requires=install_requires,
+    cmdclass={'test': PyTest},
+    extras_require={
+        'docs': ['sphinx'],
+        'tox': ['tox'],
+        'tests': test_requires,
+        'dev': dev_requires,
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    entry_points={
+        'babel.extractors': [
+            'django = django_babel.extract:extract_django',
+        ]
+    }
+)
