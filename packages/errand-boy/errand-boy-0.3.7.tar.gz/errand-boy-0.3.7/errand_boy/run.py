4a43c47a49982c0b85b223896d07c362fb12a000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:35 +0000

    Add package errand-boy/0.3.7/errand-boy-0.3.7.tar.gz

---
+++ packages/errand-boy/errand-boy-0.3.7.tar.gz/errand-boy-0.3.7/errand_boy/run.py
@@ -0,0 +1,97 @@
+import argparse
+import importlib
+import logging
+import logging.config
+import sys
+
+from . import __version__
+
+
+parser = argparse.ArgumentParser(description='Start errand-boy.')
+parser.add_argument('-t', '--transport', dest='transport', nargs='?',
+           default='errand_boy.transports.unixsocket.UNIXSocketTransport',
+           help='Python path to the transport to use.')
+parser.add_argument('--pool-size', dest='pool_size', nargs='?', type=int,
+           default=10,
+           help='Number of worker processes to use.')
+parser.add_argument('--max-accepts', dest='max_accepts', nargs='?', type=int,
+           default=5000,
+           help='Max number of connections the server will accept.')
+parser.add_argument('--max-child-tasks', dest='max_child_tasks', nargs='?', type=int,
+           default=100,
+           help='Max number of tasks each child process will handle before being replaced.')
+parser.add_argument('command', nargs=argparse.REMAINDER)
+parser.add_argument('--version', action='version', version=__version__)
+
+class MaxLevelFilter(logging.Filter):
+    def __init__(self, level):
+        self._level = level
+
+    def filter(self, rec):
+        return rec.levelno <= self._level
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+        'stdout': {
+            '()': MaxLevelFilter,
+            'level': 'WARNING',
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(process)d %(thread)d %(name)s:%(lineno)s %(funcName)s() %(message)s'
+        },
+    },
+    'handlers': {
+        'stdout': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+            'formatter': 'verbose',
+        },
+        'stderr': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stderr,
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'errand_boy': {
+            'handlers': ['stderr', 'stdout'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
+def main(argv):
+    parsed_args = parser.parse_args(argv[1:])
+    
+    mod, klass = parsed_args.transport.rsplit('.', 1)
+    
+    transport = getattr(importlib.import_module(mod), klass)()
+    
+    command = parsed_args.command
+    
+    if not command:
+        logging.config.dictConfig(LOGGING)
+        
+        transport.run_server(
+            pool_size=parsed_args.pool_size,
+            max_accepts=parsed_args.max_accepts,
+            max_child_tasks=parsed_args.max_child_tasks
+        )
+    else:
+        stdout, stderr, returncode = transport.run_cmd(' '.join(command))
+        
+        sys.stdout.write(stdout)
+        sys.stderr.write(stderr)
+        
+        sys.exit(returncode)
+
+if __name__ == '__main__':
+    main(sys.argv)
+
---
+++ packages/errand-boy/errand-boy-0.3.7.tar.gz/errand-boy-0.3.7/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import errand_boy
+
+package_name = 'errand_boy'
+test_package_name = 'tests'
+
+setup(
+    name='errand-boy',
+    version=errand_boy.__version__,
+    description="Establish a connection to the errand-boy deamon to execute commands without the memory overhead incurred by os.fork().",
+    author='Seán Hayes',
+    author_email='sean@seanhayes.name',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    keywords='os fork process memory celery',
+    url='https://github.com/SeanHayes/errand-boy',
+    download_url='https://github.com/SeanHayes/errand-boy',
+    license='BSD',
+    packages=find_packages(),
+    install_requires=['eventlet', 'six'],
+    tests_require=[
+        'mock'
+    ],
+    include_package_data=True,
+    test_suite = test_package_name,
+)
+
