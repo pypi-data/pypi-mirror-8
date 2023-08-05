bcf02acd1bd16964bb36b36f8c8324d2341c4260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package pelican-git/0.1.9/pelican_git-0.1.9-py2.py3-none-any.whl

---
+++ packages/pelican-git/pelican_git-0.1.9-py2.py3-none-any.whl/pelican_git/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+__title__ = 'pelican-git'
+__version__ = '0.1.9'
+__author__ = 'Ha.Minh'
+__email__ = 'minhhh@minhhuyha.info'
+__url__ = 'https://github.com/minhhh/pelican_git'
+__desc__ = 'Easily embed Github files in your Pelican articles.'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014'
+
+from pelican_git.plugin import register
