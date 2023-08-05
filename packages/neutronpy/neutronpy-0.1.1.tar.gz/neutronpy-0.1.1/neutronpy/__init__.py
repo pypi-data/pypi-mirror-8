c70f688db642873cffc44fec0a8ce90bd5ab799f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package neutronpy/0.1.1/neutronpy-0.1.1.tar.gz

---
+++ packages/neutronpy/neutronpy-0.1.1.tar.gz/neutronpy-0.1.1/neutronpy/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+__version__ = '0.1.1'
+
+from . import constants
+from . import tools
+from . import resolution
+from . import form_facs
+from . import functions
+from . import models
+
+try:
+    from .kmpfit import Fitter
+except ImportError:
+    print(ImportError(u'Fitter module is not loaded.'))
