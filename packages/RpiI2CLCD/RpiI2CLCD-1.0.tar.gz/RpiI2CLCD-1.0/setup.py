c69f20ec5f2490a81437b6f49a00c178738f5e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:20 +0000

    Add package RpiI2CLCD/1.0/RpiI2CLCD-1.0.tar.gz

---
+++ packages/RpiI2CLCD/RpiI2CLCD-1.0.tar.gz/RpiI2CLCD-1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='RpiI2CLCD',
+      version='1.0',
+      description='Python I2C LCD for Raspberry Pi',
+      author='David Smith',
+      author_email='dave.g.smith@gmail.com',
+      py_modules=['rpii2clcd'],
+     )
