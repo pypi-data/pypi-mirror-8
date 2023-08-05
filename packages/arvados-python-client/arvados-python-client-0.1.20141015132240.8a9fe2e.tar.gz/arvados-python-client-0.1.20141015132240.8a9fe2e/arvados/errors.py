0a47561ea1f787f3546d5c4699f3b45372bc07c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:49 +0000

    Add package arvados-python-client/0.1.20141015132240.8a9fe2e/arvados-python-client-0.1.20141015132240.8a9fe2e.tar.gz

---
+++ packages/arvados-python-client/arvados-python-client-0.1.20141015132240.8a9fe2e.tar.gz/arvados-python-client-0.1.20141015132240.8a9fe2e/arvados/errors.py
@@ -0,0 +1,33 @@
+# errors.py - Arvados-specific exceptions.
+
+import apiclient.errors
+import json
+
+class ApiError(apiclient.errors.HttpError):
+    def _get_reason(self):
+        try:
+            return '; '.join(json.loads(self.content)['errors'])
+        except (KeyError, TypeError, ValueError):
+            return super(ApiError, self)._get_reason()
+
+
+class ArgumentError(Exception):
+    pass
+class SyntaxError(Exception):
+    pass
+class AssertionError(Exception):
+    pass
+class CommandFailedError(Exception):
+    pass
+class KeepReadError(Exception):
+    pass
+class KeepWriteError(Exception):
+    pass
+class NotFoundError(KeepReadError):
+    pass
+class NotImplementedError(Exception):
+    pass
+class NoKeepServersError(Exception):
+    pass
+class StaleWriterStateError(Exception):
+    pass
