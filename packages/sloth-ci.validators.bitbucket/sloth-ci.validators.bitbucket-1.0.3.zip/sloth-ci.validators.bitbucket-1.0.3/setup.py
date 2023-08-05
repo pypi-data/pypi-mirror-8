51934fe01d2fc3c9b2dd560d6e28b5125a5be7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:00 +0000

    Add package sloth-ci.validators.bitbucket/1.0.3/sloth-ci.validators.bitbucket-1.0.3.zip

---
+++ packages/sloth-ci.validators.bitbucket/sloth-ci.validators.bitbucket-1.0.3.zip/sloth-ci.validators.bitbucket-1.0.3/setup.py
@@ -0,0 +1,28 @@
+﻿from setuptools import setup
+
+import bitbucket as validator
+
+
+package = 'sloth_ci.validators'
+
+setup(
+    name=validator.__title__,
+    version=validator.__version__,
+    author=validator.__author__,
+    description=validator.__description__,
+    long_description=validator.__doc__,
+    author_email=validator.__author_email__,
+    url='https://bitbucket.org/moigagoo/sloth-ci-validators',
+    py_modules=['%s.%s' % (package, validator.__name__)],
+    package_dir={package: '.'},
+    install_requires = [
+        'sloth_ci>=0.6.3'
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3']
+    )
