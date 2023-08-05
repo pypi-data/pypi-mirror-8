52cc8a28f9d542ee68765479c347178891d22394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package agn_periodics/0.1.23/agn_periodics-0.1.23.tar.gz

---
+++ packages/agn_periodics/agn_periodics-0.1.23.tar.gz/agn_periodics-0.1.23/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+
+setup(
+    name='agn_periodics',
+    version='0.1.23',
+    packages=['agn_periodics'],
+    package_data={'package': 'agn_periodcs/*'},
+    data_files=[('', ['agn_periodcs/config.json'])],
+    url='',
+    license='GPL',
+    author='yarnaid',
+    author_email='yarnaid@gmail.com',
+    description='Some statistical functions for searchin periodics in time rows'
+)
