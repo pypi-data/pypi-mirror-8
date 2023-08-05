596faac1aae5d27766bacd65a8e619767718b1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:10 +0000

    Add package unicodedata2/7.0.0/unicodedata2-7.0.0.tar.gz

---
+++ packages/unicodedata2/unicodedata2-7.0.0.tar.gz/unicodedata2-7.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Extension
+
+module1 = Extension('unicodedata2',
+                    sources = ['./unicodedata2/unicodedata.c'],
+                    include_dirs = ['./unicodedata2'],
+)
+
+setup (name = "unicodedata2",
+       version = "7.0.0",
+       description = "Unicodedata backport for python 2 updated to the latest unicode version.",
+       ext_modules = [module1],
+       author="Mike Kaplinskiy",
+       author_email="mike.kaplinskiy@gmail.com",
+       download_url="http://github.com/mikekap/unicodedata2",
+       license="Apache License 2.0",
+       platforms=['any'],
+       url="http://github.com/mikekap/unicodedata2",
+)
