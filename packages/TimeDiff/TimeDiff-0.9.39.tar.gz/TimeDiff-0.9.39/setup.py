c362eecdaa8f8f9f4e532a14bcd880180995c568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package TimeDiff/0.9.39/TimeDiff-0.9.39.tar.gz

---
+++ packages/TimeDiff/TimeDiff-0.9.39.tar.gz/TimeDiff-0.9.39/setup.py
@@ -0,0 +1,42 @@
+# -*- coding=utf-8 -*-
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="TimeDiff",
+
+    # Version number (initial):
+    version="0.9.39", 
+
+    # Packages
+    packages=["timediff"],
+
+    data_files=[('/etc/timediff', ['timediff/timediff.json'])],
+
+    # Details
+    url="https://github.com/nomelif/time-diff",
+
+    # Application author details:
+    author="Théo Friberg",
+    author_email="theo.friberg@gmail.com",
+
+    # Dependent packages (distributions)
+    requires=[
+        "matplotlib", "numpy", "scipy"
+    ],
+
+    provides=["timediff",],
+
+    # License
+    license=open("LICENSE.txt").read(),
+
+    # Description
+    description="Program for login and plotting time-differences in log-files.",
+
+    # Long description
+    long_description=open("README.md").read(),
+
+    # Scripts
+    scripts=["bin/timedifftext", "bin/timediffplot"]
+
+)
