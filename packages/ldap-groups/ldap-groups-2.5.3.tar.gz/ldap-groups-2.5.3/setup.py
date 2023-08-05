0dc404fe2be94ce0e711f9c30ff609ef655608b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:35 +0000

    Add package ldap-groups/2.5.3/ldap-groups-2.5.3.tar.gz

---
+++ packages/ldap-groups/ldap-groups-2.5.3.tar.gz/ldap-groups-2.5.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='ldap-groups',
+    version='2.5.3',
+    author='Alex Kavanaugh',
+    author_email='kavanaugh.development@outlook.com',
+    packages=['ldap_groups'],
+    url='https://bitbucket.org/kavanaugh_development/ldap-groups/',
+    license='GNU LGPL (http://www.gnu.org/licenses/lgpl.html)',
+    description="A python/django Active Directory group management abstraction that uses python-ldap as a backend for cross-platform compatibility.",
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "python-ldap>=2.4.13",
+    ],
+)
