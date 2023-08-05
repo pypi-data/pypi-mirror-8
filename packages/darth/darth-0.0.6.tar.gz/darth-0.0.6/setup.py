bdbb8b000450f7b95315f8e2b802572a5a676a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package darth/0.0.6/darth-0.0.6.tar.gz

---
+++ packages/darth/darth-0.0.6.tar.gz/darth-0.0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+VERSION = '0.0.6'
+
+setup(
+    name="darth",
+    version=VERSION,
+    author='Jon Wayne Parrott, [proppy], Michael R. Bernstein',
+    author_email='jon.wayne.parrott@gmail.com, [proppy], michael@fandomhome.com',
+    maintainer='Jon Wayne Parrott',
+    maintainer_email='jon.wayne.parrott@gmail.com',
+    description='Third-party vendoring helper for Google App Engine and other sandboxed python environments.',
+    url='https://github.com/jonparrott/Darth-Vendor',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    py_modules=['darth', 'darth_bootstrap'],
+    entry_points={
+        'console_scripts': ['darth-bootstrap=darth_bootstrap:bootstrap']
+    },
+    test_suite='nose.collector',
+    install_requires=[
+    ],
+)
