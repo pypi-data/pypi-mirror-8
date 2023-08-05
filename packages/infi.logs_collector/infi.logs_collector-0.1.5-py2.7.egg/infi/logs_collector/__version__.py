0daf45c2c1c3db5c06a8f75b13da9f6fdb2697a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:40 +0000

    Add package infi.logs_collector/0.1.5/infi.logs_collector-0.1.5-py2.7.egg

---
+++ packages/infi.logs_collector/infi.logs_collector-0.1.5-py2.7.egg/infi/logs_collector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.5"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '49b84309b88b7be6025889a1ed841919ff0ed6c1'
+__git_head_subject__ = 'Finished release v0.1.5.'
+__git_head_message__ = '* release/v0.1.5:\n  Do not collect /var/crash files since they are very large. Collect only listing of such files.\n  added Script collectable item\n  HIP-1247 Infinihost log collection should also save /var/crash (if exists)'
+__git_dirty_diff__ = ''
