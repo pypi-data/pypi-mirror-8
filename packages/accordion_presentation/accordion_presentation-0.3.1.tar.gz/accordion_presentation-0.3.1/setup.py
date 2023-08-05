029608e2be263c07201a5dc41038fca4ef8fdbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:50 +0000

    Add package accordion_presentation/0.3.1/accordion_presentation-0.3.1.tar.gz

---
+++ packages/accordion_presentation/accordion_presentation-0.3.1.tar.gz/accordion_presentation-0.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+setup(
+    name='accordion_presentation',
+    version='0.3.1',
+    description='It is a simple horizontal accordion for django cms',
+    long_description=open('README.rst').read(),
+    author='Luisza',
+    license = "GPLv2",
+    author_email='luisza14@gmail.com',
+    url='http://github.com/luisza/accordion_presentation',
+    packages=find_packages(),
+    keywords='tab django cms django-cms plugin',
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=open('requirements.txt').read().split('\n'),
+)
