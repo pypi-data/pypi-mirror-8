58a01c00c23c35dfdccce09f41d2d5421c2543f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package sticky/0.3/sticky-0.3.tar.gz

---
+++ packages/sticky/sticky-0.3.tar.gz/sticky-0.3/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sticky',
+    version='0.3',
+    description='Sticky on Terminal',
+    license='MIT License',
+    install_requires=[],
+
+    author='keroro520',
+    author_email='keroro520@gmail.com',
+
+    url='https://github.com/keroro520/Sticky',
+    packages=find_packages(),
+    platforms='linux',
+)
