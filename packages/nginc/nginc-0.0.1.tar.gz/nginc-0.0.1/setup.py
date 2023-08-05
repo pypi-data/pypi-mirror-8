bb7aea38faad4d0a553514d4493cd023b7e8e5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:47 +0000

    Add package nginc/0.0.1/nginc-0.0.1.tar.gz

---
+++ packages/nginc/nginc-0.0.1.tar.gz/nginc-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name='nginc',
+    version='0.0.1',
+    desciption='run nginx from the commandline',
+    author='Florian Ludwig',
+    author_email='f.ludwig@greyrook.com',
+    install_requires=['setuptools'],
+    packages=['nginc'],
+    package_data={'nginc': ['*.conf']},
+    entry_points={
+        'console_scripts': [
+            'nginc = nginc:main',
+        ],
+    },
+)
