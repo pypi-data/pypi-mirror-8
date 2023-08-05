c0d2701162cc9caa09019668c1bdb54a3a17a0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:23 +0000

    Add package serialized-redis/0.1.5/serialized-redis-0.1.5.tar.gz

---
+++ packages/serialized-redis/serialized-redis-0.1.5.tar.gz/serialized-redis-0.1.5/serialized_redis/__init__.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# SerializedRedis
+# author: Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
+# https://github.com/evandrolg
+# License: MIT
+
+from redis import Redis
+import pickle
+
+
+class SerializedRedis(Redis):
+    def _execute_command(self, key, value, method):
+        is_list_or_dict = isinstance(value, list) or isinstance(value, dict)
+
+        if is_list_or_dict:
+            return method(key, pickle.dumps(value))
+
+        return method(key, value)
+
+    def set(self, key, value):
+        return self._execute_command(key, value, super(Redis, self).set)
+
+    def get(self, key):
+        value = super(Redis, self).get(key)
+
+        try:
+            return pickle.loads(value)
+        except:
+            pass
+
+        return value
+
+    def rpushx(self, key, value):
+        return self._execute_command(key, value, super(Redis, self).rpushx)
+
+    def rpush(self, key, value):
+        return self._execute_command(key, value, super(Redis, self).rpush)
+
+    def lrange(self, key, start, end):
+        value_list = super(Redis, self).lrange(key, start, end)
+        is_empty = len(value_list) == 0
+
+        if is_empty:
+            return value_list
+
+        def make_result(value):
+            try:
+                return pickle.loads(value)
+            except:
+                pass
+
+            return value
+
+        result = [make_result(value) for value in value_list]
+
+        return result
---
+++ packages/serialized-redis/serialized-redis-0.1.5.tar.gz/serialized-redis-0.1.5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# SerializedRedis
+# author: Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
+# https://github.com/evandrolg
+# License: MIT
+
+from setuptools import setup
+import os
+
+
+setup(
+    name='serialized-redis',
+    version='0.1.5',
+    description='A solution to have lists and dictionaries serialized using redis-py.',
+    author='EvandroLG',
+    author_email='evandrolgoncalves@gmail.com',
+    keywords=['Redis', 'redis-py', 'serialized datas'],
+    url='http://github.com/EvandroLG/serialized-redis',
+    license='MIT',
+    tests_require='pytest>=2.5.0',
+    install_requires=[
+        'redis',
+    ],
+    packages=['serialized_redis']
+)
