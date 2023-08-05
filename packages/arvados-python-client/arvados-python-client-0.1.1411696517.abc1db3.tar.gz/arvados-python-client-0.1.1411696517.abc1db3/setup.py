df63384b729a98fe7a40893940bf16d61f2caea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package arvados-python-client/0.1.1411696517.abc1db3/arvados-python-client-0.1.1411696517.abc1db3.tar.gz

---
+++ packages/arvados-python-client/arvados-python-client-0.1.1411696517.abc1db3.tar.gz/arvados-python-client-0.1.1411696517.abc1db3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup, find_packages
+
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
+
+setup(name='arvados-python-client',
+      version='0.1',
+      description='Arvados client library',
+      long_description=open(README).read(),
+      author='Arvados',
+      author_email='info@arvados.org',
+      url="https://arvados.org",
+      download_url="https://github.com/curoverse/arvados.git",
+      license='Apache 2.0',
+      packages=find_packages(),
+      scripts=[
+        'bin/arv-get',
+        'bin/arv-keepdocker',
+        'bin/arv-ls',
+        'bin/arv-normalize',
+        'bin/arv-put',
+        'bin/arv-ws',
+        ],
+      install_requires=[
+        'python-gflags',
+        'google-api-python-client',
+        'httplib2',
+        'urllib3',
+        'ws4py'
+        ],
+      test_suite='tests',
+      tests_require=['mock>=1.0', 'PyYAML'],
+      zip_safe=False)
