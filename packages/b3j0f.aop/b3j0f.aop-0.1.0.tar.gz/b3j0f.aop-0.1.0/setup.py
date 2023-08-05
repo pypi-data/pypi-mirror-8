eac7bf698021e3eed1d5a48a553d4e42a3eafad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:50 +0000

    Add package b3j0f.aop/0.1.0/b3j0f.aop-0.1.0.tar.gz

---
+++ packages/b3j0f.aop/b3j0f.aop-0.1.0.tar.gz/b3j0f.aop-0.1.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+from os.path import abspath, dirname, join
+
+from sys import path
+
+# get setup directory abspath
+_path = dirname(abspath(__file__))
+
+# import aspects
+path.append(_path)
+import b3j0f.aop as package
+
+# get long description
+with open(join(_path, 'README')) as f:
+    desc = f.read()
+
+dependencies = ['b3j0f.utils==0.1.0']
+
+setup(
+    name=package.__name__,
+    version=package.__version__,
+    install_requires=dependencies,
+    packages=find_packages(where=_path, exclude=['*.test']),
+    package_dir={'': _path},
+    author="b3j0f",
+    author_email="mrb3j0f@gmail.com",
+    description="Python Aspect Oriented Programming",
+    long_description=desc,
+    url='https://github.com/mrbozzo/aop/',
+    license='MIT License',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Communications",
+    ],
+    test_suite='b3j0f'
+)
