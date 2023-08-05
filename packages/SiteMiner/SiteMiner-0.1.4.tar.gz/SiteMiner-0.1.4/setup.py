9948e00c8c2156ef763f43311756737e95114ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:06 +0000

    Add package SiteMiner/0.1.4/SiteMiner-0.1.4.tar.gz

---
+++ packages/SiteMiner/SiteMiner-0.1.4.tar.gz/SiteMiner-0.1.4/setup.py
@@ -0,0 +1,20 @@
+__author__ = 'massimo'
+
+from setuptools import setup
+
+setup(
+    name = "SiteMiner",
+    version = "0.1.4",
+    author = "massimo",
+    author_email = "yangsibai@gmail.com",
+    description = "crawl a site and find out all link http status code",
+    license = "MIT",
+    keywords = "http status site error",
+    url = "https://github.com/yangsibai/SiteMiner",
+    packages = ['SiteMiner'],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Internet :: WWW/HTTP",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
