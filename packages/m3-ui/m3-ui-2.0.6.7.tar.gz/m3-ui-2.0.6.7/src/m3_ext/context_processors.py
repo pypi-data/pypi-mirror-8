1ee242b9964060df8ebd3690bebaad566247f20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package m3-ui/2.0.6.7/m3-ui-2.0.6.7.tar.gz

---
+++ packages/m3-ui/m3-ui-2.0.6.7.tar.gz/m3-ui-2.0.6.7/src/m3_ext/context_processors.py
@@ -0,0 +1,63 @@
+#coding: utf-8
+import json
+
+from m3_ext.ui.app_ui import (
+    DesktopModel, DesktopLoader, BaseDesktopElement, MenuSeparator
+)
+from m3.actions import ControllerCache
+
+
+def _dump_element(obj):
+    """
+    Возвращает UI-элемент в сериализованном виде
+    :param obj: элемент рабочего стола
+    :type obj: MenuSeparator / BaseDesktopElement
+    """
+    if isinstance(obj, MenuSeparator):
+        result = '-'
+    elif isinstance(obj, BaseDesktopElement):
+        result = {
+            'text': obj.name,
+            'icon': obj.icon,
+        }
+        if obj.has_subitems:
+            result['items'] = list(obj.subitems)
+        else:
+            result['url'] = obj.url
+            context = getattr(obj, 'context', {})
+            if context:
+                result['context'] = context
+            else:
+                result['context'] = {}
+    else:
+        raise TypeError("%r is not JSON-able!" % obj)
+    return result
+
+
+def desktop_processor(request):
+    """
+    Добавляет в контекст элементы Рабочего Стола
+    :param request: request
+    :type request: Request
+    """
+    desktop_model = DesktopModel(request)
+    ControllerCache.populate()
+    DesktopLoader._success = False
+    DesktopLoader.populate_desktop(desktop=desktop_model)
+    desktop_items = list(desktop_model.desktop.subitems)
+    return {
+        'desktop': json.dumps(
+            {
+                'menuItems': list(desktop_model.start_menu.subitems),
+                'topToolbarItems': list(desktop_model.toptoolbar.subitems),
+                'toolboxItems': list(desktop_model.toolbox.subitems),
+                'desktopItems': desktop_items,
+            },
+            indent=2, default=_dump_element, ensure_ascii=False
+        ),
+        # TODO: переделать на JS!
+        'desktop_icons': [
+            (idx, i.name, i.icon)
+            for (idx, i) in enumerate(desktop_items, 1)
+        ]
+    }
