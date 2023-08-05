9bbe6a9ade66fa0903f751b570f9b16a4423b1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:02 +0000

    Add package ajenti/1.2.22.12/ajenti-1.2.22.12.tar.gz

---
+++ packages/ajenti/ajenti-1.2.22.12.tar.gz/ajenti-1.2.22.12/ajenti/plugins/custom_branding_hostt/content/js/branding.m.js
@@ -0,0 +1,4 @@
+$(function () {
+    $('.page.auth img.logo').attr('src', '/ajenti:static/custom_branding_hostt/logo.png');
+    $('.page.auth img.logo').addClass('visible');
+});
---
+++ packages/ajenti/ajenti-1.2.22.12.tar.gz/ajenti-1.2.22.12/ajenti/plugins/custom_branding_kaboa/__init__.py
@@ -0,0 +1,9 @@
+from ajenti.api import *
+from ajenti.plugins import *
+
+info = PluginInfo(
+    title='Kaboa Branding',
+    icon='asterisk',
+)
+
+raise Exception()
