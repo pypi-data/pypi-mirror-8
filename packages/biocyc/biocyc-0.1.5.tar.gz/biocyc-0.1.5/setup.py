5ce386039386eb37589cdc7b2dc1845bbfe71afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:35 +0000

    Add package biocyc/0.1.5/biocyc-0.1.5.tar.gz

---
+++ packages/biocyc/biocyc-0.1.5.tar.gz/biocyc-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='biocyc',
+    version='0.1.5',
+    author='Martin Fitzpatrick',
+    author_email='martin.fitzpatrick@gmail.com',
+    packages=['biocyc'],
+    url='https://github.com/mfitzp/biocyc',
+    #license='LICENSE.txt',
+    description='Python interface to BioCyc REST API with caching mechanism',
+    #long_description=open('README.txt').read()
+    )
