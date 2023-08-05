2d4d07521c127c22683860f1dd536d8ce4fdb7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:58 +0000

    Add package otree-core/0.2.199/otree-core-0.2.199.zip

---
+++ packages/otree-core/otree-core-0.2.199.zip/otree-core-0.2.199/tests/settings.py
@@ -0,0 +1,57 @@
+import os
+
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__)))
+
+
+DEBUG = True
+
+
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'otree'
+SECRET_KEY = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+
+STATIC_URL = '/static/'
+
+
+INSTALLED_APPS = (
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'otree',
+    'otree.sessionlib',
+
+    # floppyforms need to come after 'otree' in order to load the correct
+    # form templates.
+    'floppyforms',
+
+    'raven.contrib.django.raven_compat',
+    'tests',
+    'tests.demo',
+)
+
+
+CURRENCY_CODE = 'USD'
+CURRENCY_LOCALE = 'en_US'
+LANGUAGE_CODE = 'en-us'
+
+
+ROOT_URLCONF = 'tests.demo.urls'
+
+
+INSTALLED_OTREE_APPS = [
+    'trust',
+    'lab_results',
+]
