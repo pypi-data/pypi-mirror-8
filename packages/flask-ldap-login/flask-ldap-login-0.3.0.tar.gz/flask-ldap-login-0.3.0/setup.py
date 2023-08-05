0d91a36e90de9b66d08ed0180af459cbfb56f8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:36 +0000

    Add package flask-ldap-login/0.3.0/flask-ldap-login-0.3.0.tar.gz

---
+++ packages/flask-ldap-login/flask-ldap-login-0.3.0.tar.gz/flask-ldap-login-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='flask-ldap-login',
+    version='0.3.0',
+    author='Continuum Analytics',
+    author_email='dev@continuum.io',
+    url='https://github.com/ContinuumIO/flask-ldap-login',
+    packages=find_packages(),
+
+    include_package_data=True,
+    zip_safe=False,
+
+    entry_points={
+        'console_scripts': [
+            'flask-ldap-login-check = flask_ldap_login.check:main',
+        ],
+    },
+
+)
