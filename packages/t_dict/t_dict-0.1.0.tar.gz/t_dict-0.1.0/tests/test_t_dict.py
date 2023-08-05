a217ad7dafaedb7941948bcdb90b175163922ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:53 +0000

    Add package t_dict/0.1.0/t_dict-0.1.0.tar.gz

---
+++ packages/t_dict/t_dict-0.1.0.tar.gz/t_dict-0.1.0/tests/test_t_dict.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_t_dict
+----------------------------------
+
+Tests for `t_dict` module.
+"""
+
+import unittest
+
+from t_dict.t_dict import TDict
+
+class TDictFromTDict(unittest.TestCase):
+    def setUp(self):
+        self.td = TDict({
+            'nested': {
+                'dict': {
+                    'key': 'Hello!!'
+                }
+            }
+        })
+
+    def test_passing_tdict_to_init(self):
+        td = TDict(self.td)
+        self.assertEqual(self.td.find('/nested/dict/key'), 'Hello!!')
+        self.assertEqual(self.td.find('/nested/dict/notfound', 'mydefault'), 'mydefault')
+        self.td.setin('/nested/dict/key', 'Horadric cube!')
+        self.assertEqual(self.td['nested']['dict']['key'], 'Horadric cube!')
+
+
+class TestJsonPointerTraversal(unittest.TestCase):
+
+    def setUp(self):
+        self.td = TDict({
+            'nested': {
+                'dict': {
+                    'key': 'Hello!!'
+                }
+            }
+        })
+
+    def test_find(self):
+        self.assertEqual(self.td.find('/nested/dict/key'), 'Hello!!')
+        self.assertEqual(self.td.find('/nested/dict/notfound', 'mydefault'), 'mydefault')
+
+    def test_setin(self):
+        self.td.setin('/nested/dict/key', 'Horadric cube!')
+        self.assertEqual(self.td['nested']['dict']['key'], 'Horadric cube!')
+
+
+if __name__ == '__main__':
+    unittest.main()
