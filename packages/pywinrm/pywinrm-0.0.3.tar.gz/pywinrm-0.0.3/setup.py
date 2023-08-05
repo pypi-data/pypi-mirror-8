ff3c875298e22519ae26854af94be70854c6f22b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:28 +0000

    Add package pywinrm/0.0.3/pywinrm-0.0.3.tar.gz

---
+++ packages/pywinrm/pywinrm-0.0.3.tar.gz/pywinrm-0.0.3/setup.py
@@ -0,0 +1,48 @@
+from distutils.core import setup
+
+__version__ = '0.0.3'
+project_name = 'pywinrm'
+
+# PyPi supports only reStructuredText, so pandoc should be installed
+# before uploading package
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = ''
+
+
+setup(
+    name=project_name,
+    version=__version__,
+    description='Python library for Windows Remote Management',
+    long_description=long_description,
+    keywords='winrm ws-man devops ws-management'.split(' '),
+    author='Alexey Diyan',
+    author_email='alexey.diyan@gmail.com',
+    url='http://github.com/diyan/pywinrm/',
+    license='MIT license',
+    packages=('winrm', 'winrm.tests'),
+    package_data={'winrm.tests': ['*.ps1']},
+    install_requires=['xmltodict', 'isodate'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Systems Administration'
+    ],
+)
