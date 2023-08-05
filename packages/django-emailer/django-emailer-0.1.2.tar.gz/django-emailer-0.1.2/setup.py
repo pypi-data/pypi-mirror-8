e6a5fbba0e0849623cd393aa78a9a173365a027f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:55 +0000

    Add package django-emailer/0.1.2/django-emailer-0.1.2.tar.gz

---
+++ packages/django-emailer/django-emailer-0.1.2.tar.gz/django-emailer-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='django-emailer',
+    version='0.1.2',
+    author='Evandro Myller',
+    author_email='emyller@7ws.co',
+    description='Template based email sending with SMTP connection management',
+    url='https://github.com/7ws/django-emailer',
+    install_requires=[
+        'django >= 1.5',
+    ],
+    packages=['emailer'],
+    keywords=['django', 'email', 'smtp'],
+    classifiers=[
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+)
