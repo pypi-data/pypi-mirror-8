7670a7726a4858973bb278dd2555850135a99dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:27 +0000

    Add package motor/0.3.3/motor-0.3.3.tar.gz

---
+++ packages/motor/motor-0.3.3.tar.gz/motor-0.3.3/test/__init__.py
@@ -0,0 +1,297 @@
+# Copyright 2012-2014 MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test Motor, an asynchronous driver for MongoDB and Tornado."""
+
+from __future__ import unicode_literals
+
+import contextlib
+import datetime
+import functools
+import os
+import time
+
+try:
+    from unittest import SkipTest
+except ImportError:
+    # Python 2.6.
+    from unittest2 import SkipTest
+
+import pymongo
+import pymongo.errors
+from pymongo.mongo_client import _partition_node
+from tornado import gen, testing
+
+import motor
+
+HAVE_SSL = True
+try:
+    import ssl
+except ImportError:
+    HAVE_SSL = False
+    ssl = None
+
+
+host = os.environ.get("DB_IP", "localhost")
+port = int(os.environ.get("DB_PORT", 27017))
+
+CERT_PATH = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), 'certificates')
+CLIENT_PEM = os.path.join(CERT_PATH, 'client.pem')
+CA_PEM = os.path.join(CERT_PATH, 'ca.pem')
+
+setup_package_has_run = False
+mongod_started_with_ssl = False
+mongod_validates_client_cert = False
+sync_cx = None
+sync_db = None
+sync_collection = None
+is_replica_set = False
+rs_name = None
+w = None
+hosts = None
+arbiters = None
+primary = None
+secondaries = None
+
+
+def setup_package():
+    """Called the first time any setUpModule() is run by unittest."""
+    global setup_package_has_run
+    if setup_package_has_run:
+        return
+
+    setup_package_has_run = True
+
+    global mongod_started_with_ssl
+    global mongod_validates_client_cert
+    global sync_cx
+    global sync_db
+    global sync_collection
+    global is_replica_set
+    global rs_name
+    global w
+    global hosts
+    global arbiters
+    global primary
+    global secondaries
+
+    connectTimeoutMS = socketTimeoutMS = 30 * 1000
+
+    # Store a regular synchronous pymongo MongoClient for convenience while
+    # testing. Try over SSL first.
+    try:
+        sync_cx = pymongo.MongoClient(
+            host, port,
+            connectTimeoutMS=connectTimeoutMS,
+            socketTimeoutMS=socketTimeoutMS,
+            ssl=True)
+
+        mongod_started_with_ssl = True
+    except pymongo.errors.ConnectionFailure:
+        try:
+            sync_cx = pymongo.MongoClient(
+                host, port,
+                connectTimeoutMS=connectTimeoutMS,
+                socketTimeoutMS=socketTimeoutMS,
+                ssl_certfile=CLIENT_PEM)
+
+            mongod_started_with_ssl = True
+            mongod_validates_client_cert = True
+        except pymongo.errors.ConnectionFailure:
+            sync_cx = pymongo.MongoClient(
+                host, port,
+                connectTimeoutMS=connectTimeoutMS,
+                socketTimeoutMS=socketTimeoutMS,
+                ssl=False)
+
+    sync_db = sync_cx.motor_test
+    sync_collection = sync_db.test_collection
+
+    is_replica_set = False
+    response = sync_cx.admin.command('ismaster')
+    if 'setName' in response:
+        is_replica_set = True
+        rs_name = str(response['setName'])
+        w = len(response['hosts'])
+        hosts = set([_partition_node(h) for h in response["hosts"]])
+        arbiters = set([
+            _partition_node(h) for h in response.get("arbiters", [])])
+
+        repl_set_status = sync_cx.admin.command('replSetGetStatus')
+        primary_info = [
+            m for m in repl_set_status['members']
+            if m['stateStr'] == 'PRIMARY'][0]
+
+        primary = _partition_node(primary_info['name'])
+        secondaries = [
+            _partition_node(m['name']) for m in repl_set_status['members']
+            if m['stateStr'] == 'SECONDARY']
+        
+
+def setUpModule():
+    """Import this into your test file to run setup_package exactly once."""
+    setup_package()
+
+
+@contextlib.contextmanager
+def assert_raises(exc_class):
+    """Roughly a backport of Python 2.7's TestCase.assertRaises"""
+    try:
+        yield
+    except exc_class:
+        pass
+    else:
+        assert False, "%s not raised" % exc_class
+
+
+class PauseMixin(object):
+    @gen.coroutine
+    def pause(self, seconds):
+        yield gen.Task(
+            self.io_loop.add_timeout, datetime.timedelta(seconds=seconds))
+
+
+class MotorTest(PauseMixin, testing.AsyncTestCase):
+    longMessage = True  # Used by unittest.TestCase
+    ssl = False  # If True, connect with SSL, skip if mongod isn't SSL
+
+    def setUp(self):
+        super(MotorTest, self).setUp()
+
+        if self.ssl and not mongod_started_with_ssl:
+            raise SkipTest("mongod doesn't support SSL, or is down")
+
+        self.cx = self.motor_client(ssl=self.ssl)
+        self.db = self.cx.motor_test
+        self.collection = self.db.test_collection
+
+    @gen.coroutine
+    def make_test_data(self):
+        yield self.collection.remove()
+        yield self.collection.insert([{'_id': i} for i in range(200)])
+
+    make_test_data.__test__ = False
+
+    @gen.coroutine
+    def wait_for_cursor(self, collection, cursor_id, retrieved):
+        """Ensure a cursor opened during the test is closed on the
+        server, e.g. after dereferencing an open cursor on the client:
+
+            collection = self.cx.motor_test.test_collection
+            cursor = collection.find()
+
+            # Open it server-side
+            yield cursor.fetch_next
+            cursor_id = cursor.cursor_id
+            retrieved = cursor.delegate._Cursor__retrieved
+
+            # Clear cursor reference from this scope and from Runner
+            del cursor
+            yield gen.Task(self.io_loop.add_callback)
+
+            # Wait for cursor to be closed server-side
+            yield self.wait_for_cursor(collection, cursor_id, retrieved)
+
+        `yield cursor.close()` is usually simpler.
+        """
+        patience_seconds = 20
+        start = time.time()
+        collection_name = collection.name
+        db_name = collection.database.name
+        sync_collection = sync_cx[db_name][collection_name]
+        while True:
+            sync_cursor = sync_collection.find()
+            sync_cursor._Cursor__id = cursor_id
+            sync_cursor._Cursor__retrieved = retrieved
+
+            try:
+                next(sync_cursor)
+            except pymongo.errors.CursorNotFound:
+                # Success!
+                return
+            finally:
+                # Avoid spurious errors trying to close this cursor.
+                sync_cursor._Cursor__id = None
+
+            now = time.time()
+            if now - start > patience_seconds:
+                self.fail("Cursor not closed")
+            else:
+                # Let the loop run, might be working on closing the cursor
+                yield self.pause(0.1)
+
+    def motor_client(self, host=host, port=port, *args, **kwargs):
+        """Get a MotorClient.
+
+        Ignores self.ssl, you must pass 'ssl' argument. You'll probably need to
+        close the client to avoid file-descriptor problems after AsyncTestCase
+        calls self.io_loop.close(all_fds=True).
+        """
+        return motor.MotorClient(
+            host, port, *args, io_loop=self.io_loop, **kwargs)
+
+    @gen.coroutine
+    def check_optional_callback(self, fn, *args, **kwargs):
+        """Take a function and verify that it accepts a 'callback' parameter
+        and properly type-checks it. If 'required', check that fn requires
+        a callback.
+
+        NOTE: This method can call fn several times, so it should be relatively
+        free of side-effects. Otherwise you should test fn without this method.
+
+        :Parameters:
+          - `fn`: A function that accepts a callback
+          - `required`: Whether `fn` should require a callback or not
+          - `callback`: To be called with ``(None, error)`` when done
+        """
+        partial_fn = functools.partial(fn, *args, **kwargs)
+        self.assertRaises(TypeError, partial_fn, callback='foo')
+        self.assertRaises(TypeError, partial_fn, callback=1)
+
+        # Should not raise
+        yield partial_fn(callback=None)
+
+        # Should not raise
+        (result, error), _ = yield gen.Task(partial_fn)
+        if error:
+            raise error
+
+    def tearDown(self):
+        sync_collection.remove()
+        self.cx.close()
+        super(MotorTest, self).tearDown()
+
+
+class MotorReplicaSetTestBase(MotorTest):
+    def setUp(self):
+        super(MotorReplicaSetTestBase, self).setUp()
+        if not is_replica_set:
+            raise SkipTest("Not connected to a replica set")
+
+        self.rsc = self.motor_rsc()
+
+    def motor_rsc(self, h=host, p=port, *args, **kwargs):
+        """Get an open MotorReplicaSetClient. Ignores self.ssl, you must pass
+        'ssl' argument. You'll probably need to close the client to avoid
+        file-descriptor problems after AsyncTestCase calls
+        self.io_loop.close(all_fds=True).
+        """
+        return motor.MotorReplicaSetClient(
+            '%s:%s' % (h, p), *args, io_loop=self.io_loop,
+            replicaSet=rs_name, **kwargs)
+
+    def tearDown(self):
+        self.rsc.close()
+        super(MotorReplicaSetTestBase, self).tearDown()
---
+++ packages/motor/motor-0.3.3.tar.gz/motor-0.3.3/test/motor_client_test_generic.py
@@ -0,0 +1,241 @@
+# Copyright 2012-2014 MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import unicode_literals
+
+"""Generic tests for MotorClient and MotorReplicaSetClient."""
+
+import gc
+import time
+
+import pymongo.errors
+import pymongo.mongo_replica_set_client
+from tornado import gen
+from tornado.testing import gen_test
+
+from test import assert_raises
+from test.utils import server_is_master_with_slave, remove_all_users
+from test.utils import skip_if_mongos
+
+
+class MotorClientTestMixin(object):
+    def get_client(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def test_requests(self):
+        cx = self.get_client()
+        for method in 'start_request', 'in_request', 'end_request':
+            self.assertRaises(TypeError, getattr(cx, method))
+
+        cx.close()
+        gc.collect()
+
+    @gen_test
+    def test_copy_db_argument_checking(self):
+        cx = self.get_client()
+        with assert_raises(TypeError):
+            yield cx.copy_database(4, 'foo')
+
+        with assert_raises(TypeError):
+            yield cx.copy_database('foo', 4)
+
+        with assert_raises(pymongo.errors.InvalidName):
+            yield cx.copy_database('foo', '$foo')
+
+        cx.close()
+        gc.collect()
+
+    @gen_test
+    def test_copy_db_callback(self):
+        cx = self.get_client()
+        yield cx.drop_database('target')
+        name = cx.motor_test.name
+        (result, error), _ = yield gen.Task(
+            cx.copy_database, name, 'target')
+
+        self.assertTrue(isinstance(result, dict))
+        self.assertEqual(error, None)
+
+        yield cx.drop_database('target')
+        cx.close()
+
+        client = self.get_client('doesntexist', connectTimeoutMS=10)
+        (result, error), _ = yield gen.Task(
+            client.copy_database, name, 'target')
+
+        self.assertEqual(result, None)
+        self.assertTrue(isinstance(error, Exception))
+        client.close()
+        gc.collect()
+
+    @gen.coroutine
+    def drop_databases(self, cx, database_names):
+        for test_db_name in database_names:
+            yield cx.drop_database(test_db_name)
+
+        # Due to SERVER-2329, databases may not disappear from a master
+        # in a master-slave pair.
+        if not (yield server_is_master_with_slave(cx)):
+            start = time.time()
+
+            # There may be a race condition in the server's dropDatabase. Wait
+            # for it to update its namespaces.
+            db_names = yield cx.database_names()
+            while time.time() - start < 30:
+                remaining_test_dbs = (
+                    set(database_names).intersection(db_names))
+
+                if not remaining_test_dbs:
+                    # All test DBs are removed.
+                    break
+
+                yield self.pause(0.1)
+                db_names = yield cx.database_names()
+
+            for test_db_name in database_names:
+                self.assertFalse(
+                    test_db_name in db_names,
+                    "%s not dropped" % test_db_name)
+
+    @gen.coroutine
+    def check_copydb_results(self, cx, doc, test_db_names):
+        for test_db_name in test_db_names:
+            self.assertEqual(
+                doc,
+                (yield cx[test_db_name].test_collection.find_one()))
+
+    @gen_test
+    def test_copy_db(self):
+        # This will catch any socket leaks.
+        cx = self.get_client(max_pool_size=1, waitQueueTimeoutMS=1)
+        target_db_name = 'motor_test_2'
+
+        yield cx.drop_database(target_db_name)
+        yield self.collection.insert({'_id': 1})
+        result = yield cx.copy_database("motor_test", target_db_name)
+        self.assertTrue(isinstance(result, dict))
+        self.assertEqual(
+            {'_id': 1},
+            (yield cx[target_db_name].test_collection.find_one()))
+
+        yield cx.drop_database(target_db_name)
+        cx.close()
+        gc.collect()
+
+    @gen_test(timeout=300)
+    def test_copy_db_concurrent(self):
+        n_copies = 2
+        target_db_names = ['motor_test_%s' % i for i in range(n_copies)]
+
+        # 1. Drop old test DBs
+        cx = self.get_client()
+        yield cx.drop_database('motor_test')
+        yield self.drop_databases(cx, target_db_names)
+
+        # 2. Copy a test DB N times at once
+        collection = cx.motor_test.test_collection
+        yield collection.insert({'_id': 1})
+        results = yield [
+            cx.copy_database('motor_test', test_db_name)
+            for test_db_name in target_db_names]
+
+        self.assertTrue(all(isinstance(i, dict) for i in results))
+        yield self.check_copydb_results(cx, {'_id': 1}, target_db_names)
+        yield self.drop_databases(cx, target_db_names)
+        cx.close()
+        gc.collect()
+
+    @gen_test
+    def test_copy_db_auth(self):
+        # This will catch any socket leaks.
+        cx = self.get_client(max_pool_size=1, waitQueueTimeoutMS=1)
+
+        # SERVER-6427, can't copy database via mongos with auth.
+        yield skip_if_mongos(cx)
+
+        target_db_name = 'motor_test_2'
+
+        collection = cx.motor_test.test_collection
+        yield collection.remove()
+        yield collection.insert({'_id': 1})
+
+        yield cx.admin.add_user('admin', 'password')
+        yield cx.admin.authenticate('admin', 'password')
+
+        try:
+            yield cx.motor_test.add_user('mike', 'password')
+
+            with assert_raises(pymongo.errors.OperationFailure):
+                yield cx.copy_database(
+                    'motor_test', target_db_name,
+                    username='foo', password='bar')
+
+            with assert_raises(pymongo.errors.OperationFailure):
+                yield cx.copy_database(
+                    'motor_test', target_db_name,
+                    username='mike', password='bar')
+
+            # Copy a database using name and password.
+            yield cx.copy_database(
+                'motor_test', target_db_name,
+                username='mike', password='password')
+
+            self.assertEqual(
+                {'_id': 1},
+                (yield cx[target_db_name].test_collection.find_one()))
+
+            yield cx.drop_database(target_db_name)
+        finally:
+            yield remove_all_users(cx.motor_test)
+            yield cx.admin.remove_user('admin')
+            cx.close()
+            gc.collect()
+
+    @gen_test(timeout=30)
+    def test_copy_db_auth_concurrent(self):
+        cx = self.get_client()
+        yield skip_if_mongos(cx)
+
+        n_copies = 2
+        test_db_names = ['motor_test_%s' % i for i in range(n_copies)]
+
+        # 1. Drop old test DBs
+        yield cx.drop_database('motor_test')
+        yield self.drop_databases(cx, test_db_names)
+
+        # 2. Copy a test DB N times at once
+        collection = cx.motor_test.test_collection
+        yield collection.remove()
+        yield collection.insert({'_id': 1})
+
+        yield cx.admin.add_user('admin', 'password', )
+        yield cx.admin.authenticate('admin', 'password')
+
+        try:
+            yield cx.motor_test.add_user('mike', 'password')
+
+            results = yield [
+                cx.copy_database(
+                    'motor_test', test_db_name,
+                    username='mike', password='password')
+                for test_db_name in test_db_names]
+
+            self.assertTrue(all(isinstance(i, dict) for i in results))
+            yield self.check_copydb_results(cx, {'_id': 1}, test_db_names)
+        finally:
+            yield remove_all_users(cx.motor_test)
+            yield self.drop_databases(cx, test_db_names)
+            yield cx.admin.remove_user('admin')
+            cx.close()
+            gc.collect()
