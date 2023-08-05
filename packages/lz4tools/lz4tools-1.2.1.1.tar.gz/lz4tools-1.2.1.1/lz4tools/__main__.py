02667217af1361b3e74cd54bbb7a60b53d4a5f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:47 +0000

    Add package lz4tools/1.2.1.1/lz4tools-1.2.1.1.tar.gz

---
+++ packages/lz4tools/lz4tools-1.2.1.1.tar.gz/lz4tools-1.2.1.1/lz4tools/__main__.py
@@ -0,0 +1,4 @@
+import __init__ as init
+import sys
+
+init.compressFileDefault(sys.argv[1])
---
+++ packages/lz4tools/lz4tools-1.2.1.1.tar.gz/lz4tools-1.2.1.1/lz4tools/lz4tar.py
@@ -0,0 +1,27 @@
+import sys
+import tarfile
+if sys.version_info.major >= 3:
+    import builtins as __builtin__
+    from .lz4file import Lz4File
+else:
+    import __builtin__
+    from lz4file import Lz4File
+
+class Lz4Tar(tarfile.TarFile):
+    @classmethod
+    def lz4open(cls, name=None, mode='r', fileobj=None):
+        if name and not fileobj:
+            fileobj=__builtin__.open(name, 'rb')
+        elif not name and not fileobj:
+            print('Unable to open without a name or fileobj')
+            return
+        if not name and hasattr(fileobj.name):
+            name = fileobj.name
+        lz4FileOut = Lz4File.open(fileObj=fileobj)
+        try:
+            t = cls(None, mode, lz4FileOut)
+        except:
+            lz4FileOut.close()
+        t._extfileobj = False
+        return t
+    tarfile.TarFile.OPEN_METH.update({'lz4': 'lz4open'})
