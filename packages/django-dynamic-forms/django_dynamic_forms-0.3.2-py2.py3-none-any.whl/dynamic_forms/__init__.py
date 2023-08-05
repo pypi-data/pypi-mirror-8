7ab20a2308c476c6e4b346c52ceeb9d7f7ff939e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:08 +0000

    Add package django-dynamic-forms/0.3.2/django_dynamic_forms-0.3.2-py2.py3-none-any.whl

---
+++ packages/django-dynamic-forms/django_dynamic_forms-0.3.2-py2.py3-none-any.whl/dynamic_forms/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+__author__ = 'Markus Holtermann'
+__email__ = 'info@sinnwerkstatt.com'
+__version__ = '0.3.2'
+
+default_app_config = 'dynamic_forms.apps.DynamicFormsConfig'
