c20a87435b567bf9b4ab3bc47de48e92e02ac075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:05 +0000

    Add package pyalveo/0.3/pyalveo-0.3.tar.gz

---
+++ packages/pyalveo/pyalveo-0.3.tar.gz/pyalveo-0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'readme.rst'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name='pyalveo',
+	version='0.3',
+	description="A Python library for interfacing with the Alveo/HCSvLab API",
+	long_description=long_description,
+
+	url='https://github.com/Alveo/pyalveo',
+
+	maintainer='Steve Cassidy',
+	maintainer_email='Steve.Cassidy@mq.edu.au',
+	license = 'BSD',
+
+	keywords='alveo hcsvlab python library',
+
+	packages = find_packages(exclude=['contrib', 'docs', 'tests*']),
+	
+    install_requires=[
+        "python-dateutil",
+    ],
+    
+    test_suite='tests'
+	)
