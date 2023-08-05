47a7faeeddc35b86ab29c73aadc276e0d4113758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package Collectfast/0.2.1/Collectfast-0.2.1.tar.gz

---
+++ packages/Collectfast/Collectfast-0.2.1.tar.gz/Collectfast-0.2.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='Collectfast',
+    description='Collectstatic on Steroids',
+    version='0.2.1',
+    long_description=open('README.md').read(),
+    author='Anton Agestam',
+    author_email='msn@antonagestam.se',
+    packages=find_packages(),
+    url='https://github.com/FundedByMe/collectfast/',
+    license='Creative Commons Attribution-ShareAlike 3.0 Unported License',
+    include_package_data=True,
+    install_requires=['Django>=1.4', 'python-dateutil>=2.1', 'pytz>=2014.2', ],
+    classifiers=['Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3']
+)
