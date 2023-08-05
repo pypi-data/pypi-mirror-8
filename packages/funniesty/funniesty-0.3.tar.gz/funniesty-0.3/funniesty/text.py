4ffb4151cd1b74806b41174fb9d0cfd2ebb38e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package funniesty/0.3/funniesty-0.3.tar.gz

---
+++ packages/funniesty/funniesty-0.3.tar.gz/funniesty-0.3/funniesty/text.py
@@ -0,0 +1,2 @@
+def joke():
+	return "got a handjob from a blind girl last night. She said 'You have the biggest dick I've put my hands on'. I said 'Nah. You're just pulling my leg'"
---
+++ packages/funniesty/funniesty-0.3.tar.gz/funniesty-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = 'funniesty',
+	  version = '0.3',
+	  description = 'The funniest joke in the world. NOT SO MUCH',
+	  url = 'http://www.stangggtestsite/wp',
+	  author = 'Flying Circus',
+	  author_email = 'flyingcircus@example.com',
+	  license = 'MIT',
+	  packages = ['funniesty'],
+	  install_requires = ['markdown'],
+	  zip_safe = False)
