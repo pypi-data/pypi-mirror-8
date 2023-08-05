9553e46a4a986e61947179932f9200d364d0a7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:45 +0000

    Add package blackbelt/0.9.2/blackbelt-0.9.2-py2.py3-none-any.whl

---
+++ packages/blackbelt/blackbelt-0.9.2-py2.py3-none-any.whl/blackbelt/commands/t.py
@@ -0,0 +1,38 @@
+import click
+
+from blackbelt.handle_trello import (
+    migrate_label as ml,
+    schedule_list as sl,
+    next_card as n
+)
+
+
+@click.group(help='Handle Trello-related actions and integrations.')
+def cli():
+    pass
+
+
+@cli.command(name='migrate-label')
+@click.option('--label', default='', help='Label to migrate away')
+@click.option('--board', help='Board to migrate from')
+@click.option('--board-to', help='Board to migrate to')
+@click.option('--column', help='Column to migrate from')
+@click.option('--column-to', help='Column to migrate to')
+def migrate_label(*args, **kwargs):
+    ml(**kwargs)
+
+
+@cli.command(name='schedule-list')
+@click.option('--story-list', help='Name of the list that should be converted to cards')
+@click.option('--owner', help='Whom to assign a created work card')
+@click.option('--label', help='Assign a label to the newly-created card. For now, label must be a color name.')
+@click.argument('story_card')
+def schedule_list(**kwargs):
+    """ Takes a TODO checklist on a given Story Card. Converts the items into the Work Overview Cards. """
+    sl(**kwargs)
+
+
+@cli.command()
+def next(**kwargs):
+    """ Takes the top card from To Do queue, moves it to doing and creates proper branch """
+    n(**kwargs)
