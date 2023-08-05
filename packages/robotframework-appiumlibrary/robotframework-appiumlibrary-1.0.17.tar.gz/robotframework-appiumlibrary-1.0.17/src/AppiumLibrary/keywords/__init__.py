911b7d67a4e3b1caf46111d1408268668a69fb3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package robotframework-appiumlibrary/1.0.17/robotframework-appiumlibrary-1.0.17.tar.gz

---
+++ packages/robotframework-appiumlibrary/robotframework-appiumlibrary-1.0.17.tar.gz/robotframework-appiumlibrary-1.0.17/src/AppiumLibrary/keywords/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from _logging import _LoggingKeywords
+from _runonfailure import _RunOnFailureKeywords
+from _element import _ElementKeywords
+from _screenshot import _ScreenshotKeywords
+from _applicationmanagement import _ApplicationManagementKeywords
+from _waiting import _WaitingKeywords
+from _touch import _TouchKeywords
+
+__all__ = ["_LoggingKeywords",
+           "_RunOnFailureKeywords",
+           "_ElementKeywords",
+           "_ScreenshotKeywords",
+           "_ApplicationManagementKeywords",
+           "_WaitingKeywords",
+           "_TouchKeywords",
+          ]
