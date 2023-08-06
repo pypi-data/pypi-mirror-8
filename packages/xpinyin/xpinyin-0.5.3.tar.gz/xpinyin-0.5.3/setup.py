5802ba6393e7d13de6984e0f2df5b0e51d8c7f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:26 +0000

    Add package xpinyin/0.5.3/xpinyin-0.5.3.tar.gz

---
+++ packages/xpinyin/xpinyin-0.5.3.tar.gz/xpinyin-0.5.3/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+
+setup(name="xpinyin",
+      version='0.5.3',
+      description="translate chinese hanzi to pinyin by python",
+      long_description=README + '\n\n' + CHANGES,
+      author="Eric Lo",
+      author_email="lxneng@gmail.com",
+      url="https://github.com/lxneng/xpinyin",
+      packages=find_packages('src'),
+      test_suite='xpinyin.tests',
+      package_dir={'': 'src'},
+      include_package_data=True,
+      license="MIT License")
