9d2a4a0dcc008a7e6776aa18bc03f11ff63f4221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:48 +0000

    Add package themis-core/0.1/themis-core-0.1.tar.gz

---
+++ packages/themis-core/themis-core-0.1.tar.gz/themis-core-0.1/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+
+from setuptools import setup
+
+packages = { 
+'themis_core_package' : {
+      'name' : 'themis-core',
+      'version' : '0.1',
+      'author' : 'Sandro Mello',
+      'author_email' : 'sandromll@gmail.com',
+      'url' : 'https://github.com/sandromello/themis-py',
+      'description' : 'Core themis library',
+      'long_description' : 'Themis Core Tools is responsible for providing helper functions for themis',
+      'packages' : ['themis'],
+      'install_requires' : [
+        'netaddr', 'pyyaml', 'redis', 'numpy'
+      ],
+      'package_dir' : {'themis' : 'src/themis'},
+      'scripts' : [ 'src/sbin/tmscli' ],
+
+  },
+'themis_package' : {
+    'name' : 'themis',
+    'version' : '0.1',
+    'author' : 'Sandro Mello',
+    'author_email' : 'sandromll@gmail.com',
+    'url' : 'https://github.com/sandromello/themis-py',
+    'description' : 'Postfix milter behavior rate limiter',
+    'long_description' : 'Themis is a policy daemon to predict and control the rate of sending mails in Postfix. \
+      Is designed for large scale mail hosting environments, build on top of the python-milter API. \
+      The features was built not only for rate limiting but also to provide useful information about your mail environment.',
+    'install_requires' : [
+        'pymilter', 'pyspf'
+     ],
+    'data_files' : [
+        ('/etc/themis', ['src/config/config.yaml']),
+        ('/etc/init.d', ['src/init.d/themisd']),
+        ('sbin', ['src/themismilter.py']),
+        ('/var/log/themis', [])
+      ]
+  }
+}
+
+core = 'themis_core_package'
+tms = 'themis_package'
+setup(**packages[core])
