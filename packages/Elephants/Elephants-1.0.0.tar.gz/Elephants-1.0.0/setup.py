41a9f96fb59fb55c0def5692e98578defe179ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:30 +0000

    Add package Elephants/1.0.0/Elephants-1.0.0.tar.gz

---
+++ packages/Elephants/Elephants-1.0.0.tar.gz/Elephants-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+with open('README.rst', 'r') as fh:  txt = fh.read()
+with open('LICENSE.txt', 'r') as fh:  lic = fh.read()
+script = "s = '''\n" + txt + '\n' + lic + "\n'''\n\ndef readme(): return s"
+with open('elephants/readme.py', 'w') as fh: fh.write(script)
+
+setup(
+  name='Elephants',
+  version='1.0.0',
+  author='Marc Schwarzschild',
+  author_email='ms@TheBrookhavenGroup.com',
+  url='http://github.com/Schwarzschild/Elephants',
+  license='MIT',
+  description='Memoization utilities with fading memory.',
+  keywords=['memo', 'memoize', 'cache'],
+  packages=['elephants',],
+)
