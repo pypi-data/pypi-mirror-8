564c1c1b7527f8acfb76e2374254fc40d6b0069c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:12 +0000

    Add package pyvat/1.3.0/pyvat-1.3.0.tar.gz

---
+++ packages/pyvat/pyvat-1.3.0.tar.gz/pyvat-1.3.0/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+packages = [
+    'pyvat',
+]
+
+requires = [
+    'requests>=1.0.0,<3.0',
+    'pycountry',
+]
+
+tests_require = [
+    'nose',
+    'rednose',
+    'flake8',
+    'unittest2',
+]
+
+setup(
+    name='pyvat',
+    version='1.3.0',
+    description='VAT validation for Python',
+    author='Nick Bruun',
+    author_email='nick@bruun.co',
+    url='http://bruun.co/',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'pyvat': 'pyvat'},
+    include_package_data=True,
+    tests_require=tests_require,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    zip_safe=True,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ),
+)
