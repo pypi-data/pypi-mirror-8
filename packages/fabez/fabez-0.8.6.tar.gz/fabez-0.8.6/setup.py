30dd76477b076d4244352058fea7c82bd2ee8721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:01 +0000

    Add package fabez/0.8.6/fabez-0.8.6.tar.gz

---
+++ packages/fabez/fabez-0.8.6.tar.gz/fabez-0.8.6/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import fabez
+
+setup(
+    name='fabez',
+    version=fabez.version,
+    packages=['fabez'],
+    url='https://github.com/kbonez/fabez',
+    download_url='https://github.com/kbonez/fabez/tarball/master',
+    license='http://opensource.org/licenses/MIT',
+    install_requires=[
+        'fabric',
+        'tornadoez',
+        'requests',
+        'github3.py',
+    ],
+    author='Breeze.Kay',
+    author_email='wangwenpei@kbonez.com',
+    description='fabric helper tools',
+    keywords='fabric,fabez,ez',
+    include_package_data=True,
+    package_data={
+        'fabez': ['tpl/*.*']
+    },
+    entry_points={
+        'console_scripts': [
+            'fabez = fabez.main:main'
+        ]
+    },
+
+)
+
