c5ef4136ebcd17bfddc71c9f352515e86c446e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:19 +0000

    Add package liebiao/1.1.0/liebiao-1.1.0.zip

---
+++ packages/liebiao/liebiao-1.1.0.zip/liebiao-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='liebiao',
+	version='1.1.0',
+	py_modules=['liebiao'],
+	author='longyg',
+	author_email='1020231698@qq.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nester lists'
+)
