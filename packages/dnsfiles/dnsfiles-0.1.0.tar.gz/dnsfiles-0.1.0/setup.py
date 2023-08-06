86ad89ac9762b0d08774403e0ffbc3cf295d9679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:45 +0000

    Add package dnsfiles/0.1.0/dnsfiles-0.1.0.tar.gz

---
+++ packages/dnsfiles/dnsfiles-0.1.0.tar.gz/dnsfiles-0.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dnsfiles',
+      version='0.1.0',
+      description='Managing GANDI DNS from the file system.',
+      author='Jared Bunting',
+      author_email='jared.bunting@peachjean.com',
+      url='https://github.com/peachjean/dnsfiles',
+      packages=['dnsfiles'],
+      scripts=['scripts/dnsfiles-init', 'scripts/dnsfiles-sync'],
+      install_requires=['pyyaml>=3.11'],
+     )
