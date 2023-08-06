2df4a5913d99e76a536998e627fbddecd7df1ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:27 +0000

    Add package infi.recipe.python/0.6.16/infi.recipe.python-0.6.16.tar.gz

---
+++ packages/infi.recipe.python/infi.recipe.python-0.6.16.tar.gz/infi.recipe.python-0.6.16/setup.py
@@ -0,0 +1,49 @@
+
+SETUP_INFO = dict(
+    name = 'infi.recipe.python',
+    version = '0.6.16',
+    author = 'Arnon Yaari',
+    author_email = 'arnony@infinidat.com',
+
+    url = 'https://github.com/Infinidat/infi.recipe.python',
+    license = 'PSF',
+    description = """a recipe for downloading and packing portable builds of Python""",
+    long_description = """when building large applications in Python, you want to embed the interpreter within your app. This recipe lets you do it""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = [
+'infi.os_info',
+'infi.recipe.template.version>=0.3.3',
+'setuptools'
+],
+    namespace_packages = ['infi', 'infi.recipe'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = {
+        'zc.buildout' : ['download = infi.recipe.python.download:Recipe',
+                         'pack = infi.recipe.python.pack:Recipe',
+                         'default = infi.recipe.python.download:Recipe']
+    })
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
---
+++ packages/infi.recipe.python/infi.recipe.python-0.6.16.tar.gz/infi.recipe.python-0.6.16/src/infi/recipe/python/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.16"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.6.16)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd08aa0485ba253859653f4a04338fbf460d7cff7'
+__git_head_subject__ = 'Finished release v0.6.16.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
