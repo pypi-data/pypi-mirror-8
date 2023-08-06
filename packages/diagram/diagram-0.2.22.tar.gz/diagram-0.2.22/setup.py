c971b371f5fa909c8c8ed0972bbe2fa93d05dc41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:28 +0000

    Add package diagram/0.2.22/diagram-0.2.22.tar.gz

---
+++ packages/diagram/diagram-0.2.22.tar.gz/diagram-0.2.22/setup.py
@@ -0,0 +1,19 @@
+#from distutils.core import setup
+from setuptools import setup
+import os
+
+def read(filename):
+    return file(filename).read()
+
+setup(
+    name='diagram',
+    version='0.2.22',
+    description='Text mode diagrams using UTF-8 characters and fancy colors',
+    long_description=read('README.md'),
+    url='https://github.com/tehmaze/diagram',
+    author='Wijnand Modderman-Lenstra',
+    author_email='maze@pyth0n.org',
+    keywords=['diagram', 'graph', 'ascii', 'ansi', 'text'],
+    py_modules=['diagram'],
+    scripts=[os.path.join('bin', 'diagram')],
+)
