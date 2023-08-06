01475c04c589d49f6e02b7ca4ca48315f888ba18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:18 +0000

    Add package tablets/0.3.1/tablets-0.3.1.tar.gz

---
+++ packages/tablets/tablets-0.3.1.tar.gz/tablets-0.3.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from distutils.core import setup
+
+from tablets import __version__
+
+
+CLASSIFIERS = [
+    "Framework :: Django",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python",
+]
+
+INSTALL_REQUIRES = [
+    "django-annoying",
+    "django-ace",
+]
+
+setup(
+    name="tablets",
+    packages=["tablets"],
+    version=__version__,
+    description="The ultimate database-driven Django template experience",
+    url="https://github.com/craiglabenz/tablets",
+    download_url="https://github.com/craiglabenz/tablets/tarball/{0}".format(__version__),
+    keywords=["django", "templates", "database-templates"],
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIRES,
+    author="Craig Labenz",
+    author_email="craig.labenz@gmail.com",
+    license="MIT"
+)
