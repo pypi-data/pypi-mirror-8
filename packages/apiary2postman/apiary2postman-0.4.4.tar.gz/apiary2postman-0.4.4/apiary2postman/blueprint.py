34c862f282ae2bf921e0749c6d1217c279087d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:06 +0000

    Add package apiary2postman/0.4.4/apiary2postman-0.4.4.tar.gz

---
+++ packages/apiary2postman/apiary2postman-0.4.4.tar.gz/apiary2postman-0.4.4/apiary2postman/blueprint.py
@@ -0,0 +1,17 @@
+from urllib2 import Request, urlopen
+from subprocess import Popen, PIPE
+from tempfile import NamedTemporaryFile
+import json
+
+def fetch_blueprint(name, key):	
+	request = Request("https://api.apiary.io/blueprint/get/" + name)
+	request.add_header('authentication', 'Token ' + key)
+	request.add_header('Accept', 'application/json')
+	response_body = json.loads(urlopen(request).read())
+	return response_body['code']
+
+def blueprint2json(blueprint):
+	p = Popen(['snowcrash', '--format', 'json'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+	out, err = p.communicate(blueprint)
+	return out
+	
