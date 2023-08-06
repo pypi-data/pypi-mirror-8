8aaeb124b71ca9de951ea1230466e1687a79c7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:18 +0000

    Add package mintr/0.0.6/mintr-0.0.6.tar.gz

---
+++ packages/mintr/mintr-0.0.6.tar.gz/mintr-0.0.6/README.rst
@@ -0,0 +1,19 @@
+Mint.com API Client
+===================
+
+Usage
+=====
+
+Decide how you will manage your Mint.com credentials.
+
+::
+
+    pip install mintr
+
+::
+
+    import mintr
+    mintr.login(MINT_USERNAME, MINT_PASSWORD)
+    account_summaries = mintr.get_account_summaries()
+    # etc...
+
---
+++ packages/mintr/mintr-0.0.6.tar.gz/mintr-0.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+MAJOR=0
+MINOR=0
+BUILD=6
+
+setup(
+  author = 'Ben Shank',
+  author_email = 'ben@shankware.com',
+  name = 'mintr',
+  packages = ['mintr'],
+  version = '{0}.{1}.{2}'.format(MAJOR, MINOR, BUILD),
+  license = 'MIT',
+  description = 'An API client for Mint.com',
+  url = 'https://github.com/shanksauce/mintr',
+  keywords = ['client', 'api', 'mint', 'json'],
+  zip_safe = False,
+  install_requires = ['requests']
+)
