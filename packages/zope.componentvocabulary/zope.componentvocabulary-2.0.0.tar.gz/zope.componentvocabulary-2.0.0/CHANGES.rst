22f1bd1033c35aaba01cd636048c11723b411222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:08 +0000

    Add package zope.componentvocabulary/2.0.0/zope.componentvocabulary-2.0.0.tar.gz

---
+++ packages/zope.componentvocabulary/zope.componentvocabulary-2.0.0.tar.gz/zope.componentvocabulary-2.0.0/CHANGES.rst
@@ -0,0 +1,45 @@
+=======
+CHANGES
+=======
+
+2.0.0 (2014-12-24)
+------------------
+
+- Added support for PyPy.  (PyPy3 is pending release of a fix for:
+  https://bitbucket.org/pypy/pypy/issue/1946)
+
+- Add support for Python 3.4.
+
+- Add support for testing on Travis.
+
+
+2.0.0a1 (2013-02-25)
+--------------------
+
+- Add support for Python 3.3.
+
+- Replace deprecated ``zope.interface.classProvides`` usage with equivalent
+  ``zope.interface.provider`` decorator.
+
+- Replace deprecated ``zope.interface.implements`` usage with equivalent
+  ``zope.interface.implementer`` decorator.
+
+- Drop support for Python 2.4 and 2.5.
+
+- When loading this package's ZCML configuration, make sure to configure
+  ``zope.component`` first since we require part of its configuration.
+
+
+1.0.1 (2010-09-25)
+------------------
+
+- Add undeclared but needed dependency on ``zope.component``.
+
+- Add test extra to declare test dependency on ``zope.component[test]``.
+
+
+1.0 (2009-05-19)
+----------------
+
+* Initial public release, derived from zope.app.component and
+  zope.app.interface to replace them.
---
+++ packages/zope.componentvocabulary/zope.componentvocabulary-2.0.0.tar.gz/zope.componentvocabulary-2.0.0/setup.py
@@ -0,0 +1,94 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.catalog package
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+def alltests():
+    import os
+    import sys
+    import unittest
+    # use the zope.testrunner machinery to find all the
+    # test suites we've put under ourselves
+    import zope.testrunner.find
+    import zope.testrunner.options
+    here = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+    args = sys.argv[:]
+    defaults = ["--test-path", here]
+    options = zope.testrunner.options.get_options(args, defaults)
+    suites = list(zope.testrunner.find.find_suites(options))
+    return unittest.TestSuite(suites)
+
+setup(name='zope.componentvocabulary',
+      version='2.0.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Component vocabularies',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')
+          ),
+      keywords="zope component architecture vocabulary",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.componentvocabulary',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope'],
+      install_requires=[
+          'setuptools',
+          'six',
+          'zope.component',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema',
+          'zope.security',
+          ],
+      extras_require=dict(
+          test=[
+              'zope.component',
+              'zope.configuration',
+              'zope.testing',
+              ]),
+      tests_require = [
+          'zope.configuration',
+          'zope.testing',
+          'zope.testrunner',
+          ],
+      test_suite = '__main__.alltests',
+      include_package_data=True,
+      zip_safe=False,
+      )
