bff92585ba4c547682f65ab8d2f52f31b1dcc7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:00 +0000

    Add package meuhdb/0.0.2/meuhdb-0.0.2.tar.gz

---
+++ packages/meuhdb/meuhdb-0.0.2.tar.gz/meuhdb-0.0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+NAME = 'meuhdb'
+DESCRIPTION = 'A database that says "Meuh".'
+REQUIREMENTS = [
+]
+__VERSION__ = '0.0.2'
+
+params = dict(
+    name=NAME,
+    description=DESCRIPTION,
+    packages=['meuhdb'],
+    version=__VERSION__,
+    author='Bruno Bord',
+    author_email='bruno@jehaisleprintemps.net',
+    url="https://github.com/brunobord/meuhdb/",
+    license='MIT License',
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    zip_safe=False,
+    classifiers=[
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**params)
