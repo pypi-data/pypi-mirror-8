1d0d61bb8482ab002df1d19ab21d27609a969eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:19 +0000

    Add package scikit-bio/0.2.2/scikit-bio-0.2.2.tar.gz

---
+++ packages/scikit-bio/scikit-bio-0.2.2.tar.gz/scikit-bio-0.2.2/skbio/draw/__init__.py
@@ -0,0 +1,36 @@
+"""
+Visualizations (:mod:`skbio.draw`)
+==================================
+
+.. currentmodule:: skbio.draw
+
+This module provides functionality for visualization of data.
+
+Distribution visualizations
+---------------------------
+
+Functions
+^^^^^^^^^
+
+.. autosummary::
+   :toctree: generated/
+
+   boxplots
+   grouped_distributions
+
+"""
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2013--, scikit-bio development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from ._distributions import boxplots, grouped_distributions
+
+__all__ = ['boxplots', 'grouped_distributions']
+
+from numpy.testing import Tester
+test = Tester().test
