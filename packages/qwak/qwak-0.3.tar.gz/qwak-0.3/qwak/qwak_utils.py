91c878ffb3442a0d2ef65436e7b40b322d0ab0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:27 +0000

    Add package qwak/0.3/qwak-0.3.tar.gz

---
+++ packages/qwak/qwak-0.3.tar.gz/qwak-0.3/qwak/qwak_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import requests
+
+config_file = '~/.qwak_config.py'
+sys.path.append(os.path.dirname(os.path.expanduser(config_file)))
+
+from qwak_config import config
+
+__title__   = 'qwak'
+__version__ = '0.3'
+__author__  = '@c0ding'
+__repo__    = 'https://github.com/c0ding/qwak'
+__license__ = 'Apache v2.0 License'
+
+
+def make_request(api_action):
+	payload = {
+		'page': 'api',
+		'action': api_action,
+		'api_key': config['api_key'],
+	}
+	d = requests.get(config['pool_url'], params=payload)
+	return d.text
