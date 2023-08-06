3f7e3d060fe60cd2c3903805083ae224da2a4e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:23 +0000

    Add package Rtree/0.8.1/Rtree-0.8.1.tar.gz

---
+++ packages/Rtree/Rtree-0.8.1.tar.gz/Rtree-0.8.1/setup.py
@@ -0,0 +1,47 @@
+from glob import glob
+from setuptools import setup
+
+import rtree
+
+# Get text from README.txt
+readme_text = open('docs/source/README.txt', 'r').read()
+
+import os
+
+if os.name == 'nt':
+    data_files=[('Lib/site-packages/rtree',
+                 [r'D:\libspatialindex\bin\spatialindex.dll',
+                  r'D:\libspatialindex\bin\spatialindex_c.dll',]),]
+else:
+    data_files = None
+    
+setup(name          = 'Rtree',
+      version       = rtree.__version__,
+      description   = 'R-Tree spatial index for Python GIS',
+      license       = 'LGPL',
+      keywords      = 'gis spatial index r-tree',
+      author        = 'Sean Gillies',
+      author_email  = 'sean.gillies@gmail.com',
+      maintainer        = 'Howard Butler',
+      maintainer_email  = 'hobu@hobu.net',
+      url   = 'http://toblerity.github.com/rtree/',
+      long_description = readme_text,
+      packages      = ['rtree'],
+      install_requires = ['setuptools'],
+      test_suite = 'tests.test_suite',
+      data_files = data_files,
+      zip_safe = False,
+      classifiers   = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: C',
+        'Programming Language :: C++',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Database',
+        ],
+)
+
