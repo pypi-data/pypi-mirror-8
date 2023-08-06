ffb5d07d7e4acee364c199718fd72e5ad9fa9311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:16 +0000

    Add package plone.app.search/1.2.2/plone.app.search-1.2.2.zip

---
+++ packages/plone.app.search/plone.app.search-1.2.2.zip/plone.app.search-1.2.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2.2'
+
+tests_require = ['plone.app.testing',
+                 'selenium>=2.0a5']
+
+setup(name='plone.app.search',
+      version=version,
+      description="Search user interface for Plone CMS.",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("CHANGES.rst")).read(),
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Denys Mishunov',
+      author_email='denys.mishunov@gmail.com',
+      url='http://github.com/plone/plone.app.search',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.contentlisting',
+          # -*- Extra requirements: -*-
+      ],
+      tests_require=tests_require,
+      extras_require={'test': tests_require},
+      entry_points = '''
+          [z3c.autoinclude.plugin]
+          target = plone
+      ''',
+      )
