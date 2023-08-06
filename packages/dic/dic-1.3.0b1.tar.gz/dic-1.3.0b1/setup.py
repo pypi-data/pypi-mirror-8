0174dbd873fe6693ba9810b659b9679c273a2acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:53 +0000

    Add package dic/1.3.0b1/dic-1.3.0b1.tar.gz

---
+++ packages/dic/dic-1.3.0b1.tar.gz/dic-1.3.0b1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    long_description = readme.read()
+
+from dic import __version__
+
+setup(
+    name='dic',
+    version=__version__,
+    author='Zachary Sims',
+    author_email='zsims@users.noreply.github.com',
+    packages=['dic', 'dic.test'],
+    scripts=[],
+    url='https://github.com/zsims/dic',
+    license='LICENSE.txt',
+    description='Dependency Injection Container Micro-framework for implementing IoC',
+    long_description=long_description,
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='development design ioc di',
+)
