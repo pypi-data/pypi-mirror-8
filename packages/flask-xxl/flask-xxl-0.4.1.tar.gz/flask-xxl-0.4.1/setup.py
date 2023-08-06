b21a49e7763454be9997967d82e3763e277a2f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:20 +0000

    Add package flask-xxl/0.4.1/flask-xxl-0.4.1.tar.gz

---
+++ packages/flask-xxl/flask-xxl-0.4.1.tar.gz/flask-xxl-0.4.1/setup.py
@@ -0,0 +1,35 @@
+VERSION = '0,4,1'
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+    def find_packages():
+        return ['flask-xxl']
+
+def get_description():
+    return open('README.md','r').read()
+
+def get_version():
+    l,m,s = VERSION.split(',')
+    return '{}.{}.{}'.format(l,m,s).strip()
+
+config = dict(
+        name='flask-xxl',
+        version=get_version(),#'0.0.9',
+        include_package_data=True,
+        package_data = {
+            '':['*.md','*.txt']
+        },
+        author='Kyle Roux',
+        author_email='kyle@level2designs.com',
+        description='quick way to design large flask projects',
+        long_description=get_description(),
+        packages=find_packages(),
+        package_dir={'':'.'},
+        install_requires=['LoginUtils'],
+        entry_points=dict(
+            console_scripts='flaskxxl-manage.py=flask_xxl.cli:main'
+            )
+)
+
+setup(**config)
