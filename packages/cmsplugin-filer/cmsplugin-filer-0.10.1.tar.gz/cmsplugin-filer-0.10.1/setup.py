d81e58e21adf28d073f4d718ba91afc8480b94fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:46 +0000

    Add package cmsplugin-filer/0.10.1/cmsplugin-filer-0.10.1.tar.gz

---
+++ packages/cmsplugin-filer/cmsplugin-filer-0.10.1.tar.gz/cmsplugin-filer-0.10.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="cmsplugin-filer",
+    version="0.10.1",
+    url='http://github.com/stefanfoulis/cmsplugin-filer',
+    license='BSD',
+    description="django-cms plugins for django-filer",
+    long_description=open('README.rst').read(),
+    author='Stefan Foulis',
+    author_email='stefan.foulis@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires=[
+        "Django >= 1.4",
+        "django-cms >= 3.0",
+        "django-sekizai >= 0.4.2",
+        "easy_thumbnails >= 1.0",
+        "django-filer >= 0.9",
+        "django-appconf",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
