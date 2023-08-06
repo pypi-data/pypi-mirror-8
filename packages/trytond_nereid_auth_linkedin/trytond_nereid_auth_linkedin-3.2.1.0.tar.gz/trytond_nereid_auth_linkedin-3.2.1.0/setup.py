d429517599456509e27574c2b60b80b6d7fe7b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:09 +0000

    Add package trytond_nereid_auth_linkedin/3.2.1.0/trytond_nereid_auth_linkedin-3.2.1.0.tar.gz

---
+++ packages/trytond_nereid_auth_linkedin/trytond_nereid_auth_linkedin-3.2.1.0.tar.gz/trytond_nereid_auth_linkedin-3.2.1.0/setup.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+import sys
+import unittest
+import os
+import re
+import ConfigParser
+from setuptools import setup, Command
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+class SQLiteTest(Command):
+    """
+    Run the tests on SQLite
+    """
+    description = "Run tests on SQLite"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+
+        if self.distribution.tests_require:
+            self.distribution.fetch_build_eggs(self.distribution.tests_require)
+
+        from trytond.config import CONFIG
+        CONFIG['db_type'] = 'sqlite'
+        os.environ['DB_NAME'] = ':memory:'
+
+        from tests import suite
+        test_result = unittest.TextTestRunner(verbosity=3).run(suite())
+
+        if test_result.wasSuccessful():
+            sys.exit(0)
+        sys.exit(-1)
+
+config = ConfigParser.ConfigParser()
+config.readfp(open('tryton.cfg'))
+info = dict(config.items('tryton'))
+for key in ('depends', 'extras_depend', 'xml'):
+    if key in info:
+        info[key] = info[key].strip().splitlines()
+major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
+major_version = int(major_version)
+minor_version = int(minor_version)
+
+requires = [
+    'flask-oauth',
+]
+MODULE2PREFIX = {}
+
+for dep in info.get('depends', []):
+    if not re.match(r'(ir|res|webdav)(\W|$)', dep):
+        requires.append(
+            '%s_%s >= %s.%s, < %s.%s' % (
+                MODULE2PREFIX.get(dep, 'trytond'), dep, major_version,
+                minor_version, major_version, minor_version + 1
+            )
+        )
+requires.append(
+    'trytond >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)
+)
+
+MODULE = "nereid_auth_linkedin"
+PREFIX = "trytond"
+
+setup(
+    name='%s_%s' % (PREFIX, MODULE),
+    version=info.get('version', '0.0.1'),
+    description='Nereid User Authentication using LinkedIn',
+    author='Openlabs Technologies and Consulting P Ltd.',
+    url='http://openlabs.co.in/',
+    download_url="https://github.com/openlabs/nereid-auth-linkedin",
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
+    packages=[
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
+    ],
+    package_data={
+        'trytond.modules.%s' % MODULE:
+            info.get('xml', []) + ['tryton.cfg', 'view/*.xml'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Framework :: Tryton',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Legal Industry',
+        'Intended Audience :: Manufacturing',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business',
+    ],
+    license='GPL-3',
+    install_requires=requires,
+    zip_safe=False,
+    entry_points="""
+    [trytond.modules]
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
+    test_suite='tests',
+    test_loader='trytond.test_loader:Loader',
+    cmdclass={
+        'test': SQLiteTest,
+    },
+)
---
+++ packages/trytond_nereid_auth_linkedin/trytond_nereid_auth_linkedin-3.2.1.0.tar.gz/trytond_nereid_auth_linkedin-3.2.1.0/user.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+"""
+    user
+
+    Facebook based user authentication code
+
+    :copyright: (c) 2012-2014 by Openlabs Technologies & Consulting (P) LTD
+    :license: GPLv3, see LICENSE for more details.
+"""
+from nereid import url_for, flash, redirect, current_app, route
+from nereid.globals import session, request
+from nereid.signals import login, failed_login
+from flask_oauth import OAuth
+from trytond.model import fields
+from trytond.pool import PoolMeta, Pool
+from trytond.transaction import Transaction
+
+from .i18n import _
+
+
+__all__ = ['Website', 'NereidUser']
+__metaclass__ = PoolMeta
+
+
+class Website:
+    """Add Linkedin settings"""
+    __name__ = "nereid.website"
+
+    linkedin_api_key = fields.Char("LinkedIn API Key")
+    linkedin_api_secret = fields.Char("LinkedIn Secret Key")
+
+    def get_linkedin_oauth_client(
+        self, scope='r_basicprofile,r_emailaddress',
+        token='linkedin_oauth_token'
+    ):
+        """Returns a instance of WebCollect
+
+        :param scope: Scope of information to be fetched from linkedin
+        :param token: Token for authentication
+        """
+        if not all([self.linkedin_api_key, self.linkedin_api_secret]):
+            current_app.logger.error("LinkedIn api settings are missing")
+            flash(_("LinkedIn login is not available at the moment"))
+            return None
+
+        oauth = OAuth()
+        linkedin = oauth.remote_app(
+            'linkedin',
+            base_url='https://api.linkedin.com',
+            request_token_url='/uas/oauth/requestToken',
+            access_token_url='/uas/oauth/accessToken',
+            authorize_url='/uas/oauth/authenticate',
+            consumer_key=self.linkedin_api_key,
+            consumer_secret=self.linkedin_api_secret,
+            request_token_params={'scope': scope}
+        )
+        linkedin.tokengetter_func = lambda *a: session.get(token)
+        return linkedin
+
+
+class NereidUser:
+    "Nereid User"
+    __name__ = "nereid.user"
+
+    linkedin_auth = fields.Boolean('LinkedIn Auth')
+
+    @classmethod
+    @route("/auth/linkedin", methods=["GET"])
+    def linkedin_login(cls):
+        """The URL to which a new request to authenticate to linedin begins
+        Usually issues a redirect.
+        """
+        linkedin = request.nereid_website.get_linkedin_oauth_client()
+        if linkedin is None:
+            return redirect(
+                request.referrer or url_for('nereid.website.login')
+            )
+        return linkedin.authorize(
+            callback=url_for(
+                'nereid.user.linkedin_authorized_login',
+                next=request.args.get('next') or request.referrer or None,
+                _external=True
+            )
+        )
+
+    @classmethod
+    @route("/auth/linkedin_authorized_login", methods=["GET"])
+    def linkedin_authorized_login(cls):
+        """Authorized handler to which linkedin will redirect the user to
+        after the login attempt is made.
+        """
+        Party = Pool().get('party.party')
+
+        linkedin = request.nereid_website.get_linkedin_oauth_client()
+        if linkedin is None:
+            return redirect(
+                request.referrer or url_for('nereid.website.login')
+            )
+
+        try:
+            if 'oauth_verifier' in request.args:
+                data = linkedin.handle_oauth1_response()
+            elif 'code' in request.args:
+                data = linkedin.handle_oauth2_response()
+            else:
+                data = linkedin.handle_unknown_response()
+            linkedin.free_request_token()
+        except Exception, exc:
+            current_app.logger.error("LinkedIn login failed %s" % exc)
+            flash(_(
+                "We cannot talk to linkedin at this time. Please try again"
+            ))
+            return redirect(
+                request.referrer or url_for('nereid.website.login')
+            )
+
+        if data is None:
+            flash(_(
+                "Access was denied to linkedin: %(reason)s",
+                reason=request.args['error_reason']
+            ))
+            failed_login.send(form=data)
+            return redirect(url_for('nereid.website.login'))
+
+        # Write the oauth token to the session
+        session['linkedin_oauth_token'] = (
+            data['oauth_token'], data['oauth_token_secret']
+        )
+
+        # Find the information from facebook
+        me = linkedin.get(
+            'http://api.linkedin.com/v1/people/~?format=json'
+        )
+        email = linkedin.get(
+            'http://api.linkedin.com/v1/people/~/email-address?format=json'
+        )
+        session.pop('linkedin_oauth_token')
+
+        # Find the user
+        with Transaction().set_context(active_test=False):
+            users = cls.search([
+                ('email', '=', email.data),
+                ('company', '=', request.nereid_website.company.id),
+            ])
+        if not users:
+            current_app.logger.debug(
+                "No LinkedIn user with email %s" % email.data
+            )
+            name = u'%s %s' % (me.data['firstName'], me.data['lastName'])
+            current_app.logger.debug("Registering new user %s" % name)
+            user, = cls.create([{
+                'party': Party.create([{'name': name}])[0].id,
+                'display_name': name,
+                'email': email.data,
+                'linkedin_auth': True,
+                'active': True,
+            }])
+            flash(
+                _('Thanks for registering with us using linkedin')
+            )
+        else:
+            user, = users
+
+        # Add the user to session and trigger signals
+        session['user'] = user.id
+        if not user.linkedin_auth:
+            cls.write([user], {'linkedin_auth': True})
+        flash(_(
+            "You are now logged in. Welcome %(name)s", name=user.rec_name
+        ))
+        login.send()
+        if request.is_xhr:
+            return 'OK'
+        return redirect(
+            request.values.get(
+                'next', url_for('nereid.website.home')
+            )
+        )
