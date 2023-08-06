4610c422879414f20525fb46e1ef90cfd45fd88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:45 +0000

    Add package python-kyototycoon-ng/0.7.2/python-kyototycoon-ng-0.7.2.zip

---
+++ packages/python-kyototycoon-ng/python-kyototycoon-ng-0.7.2.zip/python-kyototycoon-ng-0.7.2/kyototycoon/kt_common.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2011, Toru Maesaka
+#
+# Redistribution and use of this source code is licensed under
+# the BSD license. See COPYING file for license description.
+
+KT_PACKER_CUSTOM = 0
+KT_PACKER_PICKLE = 1
+KT_PACKER_JSON   = 2
+KT_PACKER_STRING = 3
+KT_PACKER_BYTES  = 4
+
+# EOF - kt_common.py
---
+++ packages/python-kyototycoon-ng/python-kyototycoon-ng-0.7.2.zip/python-kyototycoon-ng-0.7.2/kyototycoon/kt_error.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014, Carlos Rodrigues
+#
+# Redistribution and use of this source code is licensed under
+# the BSD license. See COPYING file for license description.
+
+class KyotoTycoonException(Exception):
+    pass
+
+# EOF - kt_error.py
