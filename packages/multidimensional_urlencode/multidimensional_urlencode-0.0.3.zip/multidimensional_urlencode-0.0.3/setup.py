952541ea9e78adfc5473395a114bb1a9cce38bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:40 +0000

    Add package multidimensional_urlencode/0.0.3/multidimensional_urlencode-0.0.3.zip

---
+++ packages/multidimensional_urlencode/multidimensional_urlencode-0.0.3.zip/multidimensional_urlencode-0.0.3/setup.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+import sys
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+def read_long_description(filename="README.rst"):
+    with open(filename) as f:
+        return f.read().strip()
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+setup(
+    name='multidimensional_urlencode',
+    version='0.0.3',
+    url='https://github.com/uber/multidimensional_urlencode',
+    license='MIT',
+    author='Sam Marcellus, Charles-Axel Dein',
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    author_email='charles@uber.com',
+    description='Urlencode a multidimensional dict ',
+    long_description=read_long_description(),
+    packages=['multidimensional_urlencode'],
+    include_package_data=True,
+    platforms='any',
+    test_suite='multidimensional_urlencode.tests.test_urlencode',
+    zip_safe=False,
+    keywords=["urlencode", "params", "multidimensional"],
+    classifiers=[
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP',
+        ],
+)
