d71d47a651ec5638c483558eb5f7e6c7f31ad405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:38 +0000

    Add package django-bmemcached/0.2.2/django-bmemcached-0.2.2.tar.gz

---
+++ packages/django-bmemcached/django-bmemcached-0.2.2.tar.gz/django-bmemcached-0.2.2/django_bmemcached/memcached.py
@@ -0,0 +1,51 @@
+import os
+from django.core.cache.backends import memcached
+
+
+class BMemcached(memcached.BaseMemcachedCache):
+    """
+    An implementation of a cache binding using python-binary-memcached
+    A.K.A BMemcached.
+    """
+    def __init__(self, server, params):
+        import bmemcached
+        if not params.get('OPTIONS', None):
+            params['OPTIONS'] = {}
+
+        username = params['OPTIONS'].get('username',
+            params.get('USERNAME',
+            os.environ.get('MEMCACHE_USERNAME')))
+
+        if username:
+            params['OPTIONS']['username'] = username
+
+        password = params['OPTIONS'].get('password',
+            params.get('PASSWORD',
+            os.environ.get('MEMCACHE_PASSWORD')))
+
+        if password:
+            params['OPTIONS']['password'] = password
+
+        if not server:
+            server = tuple(os.environ.get('MEMCACHE_SERVERS', '').split(','))
+
+        super(BMemcached, self).__init__(server, params,
+             library=bmemcached,
+             value_not_found_exception=ValueError)
+
+    @property
+    def _cache(self):
+        client = getattr(self, '_client', None)
+        if client:
+            return client
+
+        if self._options:
+            client = self._lib.Client(self._servers,
+                self._options.get('username', None),
+                self._options.get('password', None))
+        else:
+            client = self._lib.Client(self._servers,)
+
+        self._client = client
+
+        return client
