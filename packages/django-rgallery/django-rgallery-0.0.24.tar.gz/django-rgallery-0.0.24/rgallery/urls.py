acff2abf28ba2d62944fe328de85a4e6a7f60b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:41 +0000

    Add package django-rgallery/0.0.24/django-rgallery-0.0.24.tar.gz

---
+++ packages/django-rgallery/django-rgallery-0.0.24.tar.gz/django-rgallery-0.0.24/rgallery/urls.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls.defaults import *
+from rgallery.views import (Photos,
+                            Videos,
+                            PhotosFolder,
+                            PhotoDelete,
+                            PhotoAdd,
+                            PhotosTag,
+                            PhotoAddTag)
+
+urlpatterns = patterns('',
+    url(r'^$', Photos.as_view(), name='app_gallery-gallery'),
+    url(r'^page/(?P<page>\d+)/$', Photos.as_view(), name='app_gallery-gallery-page'),
+    url(r'^photo/del/$', PhotoDelete.as_view(), name='app_gallery-photo-del'),
+    url(r'^photo/add/$', PhotoAdd.as_view(), name='app_gallery-photo-add'),
+    url(r'^photos/tag/(?P<slug>[-\w]+)/$', PhotosTag.as_view(), name='app_gallery-photos-tag'),
+    url(r'^photo/add/tag/$', PhotoAddTag.as_view(), name='app_gallery-photo-add-tag'),
+    url(r'^videos/$', Videos.as_view(), name='app_gallery-videos'),
+    url(r'^videos/page/(?P<page>\d+)/$', Videos.as_view(), name='app_gallery-videos-page'),
+    url(r'^(?P<folder>[-_A-Za-z0-9]+)/$', PhotosFolder.as_view(), name='app_gallery-folder'),
+)
