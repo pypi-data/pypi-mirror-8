f0122db81d16204bf5c1dd17cd136d4380e08cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:54 +0000

    Add package requests-aliyun/0.1.2/requests-aliyun-0.1.2.tar.gz

---
+++ packages/requests-aliyun/requests-aliyun-0.1.2.tar.gz/requests-aliyun-0.1.2/aliyunauth/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__author__ = "SkyLothar"
+__email__ = "allothar@gmail.com"
+__url__ = "http://github.com/skylothar/requests-aliyun"
+
+from .oss import OssAuth
+from .ecs import EcsAuth
+
+
+__all__ = ["EcsAuth", "OssAuth"]
