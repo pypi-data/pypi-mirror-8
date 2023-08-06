76888979bb06e63391863f97cf48ffb4b2269020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:37 +0000

    Add package pu/0.6.3/pu-0.6.3.zip

---
+++ packages/pu/pu-0.6.3.zip/pu-0.6.3/pu/util.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import re
+
+
+def shorten(s, width=80):
+    '''
+    >>> shorten('a very very very very long sentence', 20)
+    'a very very ..(23)..'
+    '''
+    if not isinstance(s, str):
+        s = str(s)
+
+    length = len(s)
+    if length < width:
+        return s
+
+    cut_length = length - width + 6
+    x = len(str(cut_length))
+    cut_length += x
+
+    # 长度调整
+    if x != len(str(cut_length)):
+        cut_length += 1
+
+    end_pos = length - cut_length
+    return s[:end_pos] + '..(%d)..' % cut_length
+
+
+def bytes_fromhex(s):
+    return bytes.fromhex(re.sub('\s', '', s))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
