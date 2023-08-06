32c0db0320e697e26eabe17018cf0e0c6614aff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:08 +0000

    Add package reprozip/0.5.1/reprozip-0.5.1.tar.gz

---
+++ packages/reprozip/reprozip-0.5.1.tar.gz/reprozip-0.5.1/reprozip/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2014 New York University
+# This file is part of ReproZip which is released under the Revised BSD License
+# See file LICENSE for full license details.
+
+__version__ = '0.5.1'
