cc30f86c944e1b054c2a27321d95e2fd79772e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:48 +0000

    Add package hiplogging/0.2/hiplogging-0.2.tar.gz

---
+++ packages/hiplogging/hiplogging-0.2.tar.gz/hiplogging-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    description="HipChat support for the Python logging module",
+    name='hiplogging',
+    url='https://github.com/invernizzi/hiplogging',
+    version='0.2',
+    packages=['hiplogging'],
+    author='Luca Invernizzi',
+    author_email='invernizzi.l@gmail.com',
+    license='MIT',
+    keywords=['hipchat', 'log', 'logging'],
+    download_url = 'https://github.com/invernizzi/hiplogging/tarball/0.2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
