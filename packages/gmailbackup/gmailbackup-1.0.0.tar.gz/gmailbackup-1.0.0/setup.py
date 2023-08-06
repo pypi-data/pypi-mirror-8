28b9da685fa2781edaaa7859cef6b6f8a2468bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package gmailbackup/1.0.0/gmailbackup-1.0.0.tar.gz

---
+++ packages/gmailbackup/gmailbackup-1.0.0.tar.gz/gmailbackup-1.0.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Allow trove classifiers in previous python versions
+from sys import version
+if version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+
+from gmailbackup import __version__ as version
+
+setup(
+    name='gmailbackup',
+    version=version,
+
+    author='Chris Lyon',
+    author_email='flushot@gmail.com',
+    url='https://github.com/Flushot/gmailbackup',
+
+    description='Gmail Backup',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators'
+    ],
+    
+    entry_points={
+        'console_scripts': [
+            'gmailbackup=gmailbackup:main'
+        ]
+    },
+
+    install_requires=[
+        'argparse'
+    ],
+
+    test_suite='test'
+)
