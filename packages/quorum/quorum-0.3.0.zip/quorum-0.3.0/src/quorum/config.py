310040ee29e7a471071485261827cc013c07a56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:13 +0000

    Add package quorum/0.3.0/quorum-0.3.0.zip

---
+++ packages/quorum/quorum-0.3.0.zip/quorum-0.3.0/src/quorum/config.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Flask Quorum
+# Copyright (C) 2008-2014 Hive Solutions Lda.
+#
+# This file is part of Hive Flask Quorum.
+#
+# Hive Flask Quorum is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Flask Quorum is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Flask Quorum. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+CASTS = {
+    bool : lambda v: v if type(v) == bool else v == "1",
+    list : lambda v: v if type(v) == list else v.split(";"),
+    tuple : lambda v: v if type(v) == tuple else tuple(v.split(";"))
+}
+""" The map containing the various cast method
+operation associated with the various data types,
+they provide a different type of casting strategy """
+
+config_g = {}
+""" The map containing the various global wide
+configuration for the current application """
+
+def conf(name, default = None, cast = None):
+    cast = CASTS.get(cast, cast)
+    value = config_g.get(name, default)
+    value = cast(value) if cast else value
+    return value
+
+def conf_prefix(prefix):
+    config = dict()
+    for name, value in config_g.items():
+        if not name.startswith(prefix): continue
+        config[name] = value
+    return config
+
+def confs(name, value):
+    config_g[name] = value
