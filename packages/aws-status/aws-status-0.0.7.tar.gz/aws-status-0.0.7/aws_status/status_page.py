90b1a6f8c7247314c82593c99a455eb4ff0867a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:12 +0000

    Add package aws-status/0.0.7/aws-status-0.0.7.tar.gz

---
+++ packages/aws-status/aws-status-0.0.7.tar.gz/aws-status-0.0.7/aws_status/status_page.py
@@ -0,0 +1,38 @@
+import re
+import urllib
+from .feed import Feed
+
+class StatusPage(object):
+    """
+    This class wraps access to Amazon status page and can be used to retrieve
+    all available RSS feeds, and through these feeds monitored regions and
+    services.
+    It may be used to automatically discover new available feeds for instance,
+    or for checking regions/services list.
+    """
+    BASE_URL = 'http://status.aws.amazon.com/'
+    RSS_RE = r'href="(?P<feed_url>rss/[^"]+\.rss)"'
+
+    def __init__(self):
+        self.page_source = None
+        self.rss_urls = set()
+        self.services = set()
+        self.regions = set()
+        self.parse_page()
+
+    def page(self):
+        if not self.page_source:
+            self.page_source = urllib.urlopen(self.BASE_URL)
+        return self.page_source
+
+    def parse_page(self):
+        page_content = self.page().read()
+        for match in re.finditer(self.RSS_RE, page_content):
+            absolute_url = self.BASE_URL + match.group('feed_url')
+            feed = Feed(absolute_url)
+            self.rss_urls.add(feed.url)
+            if feed.service:
+                self.services.add(feed.service)
+            if feed.region:
+                self.regions.add(feed.region)
+
