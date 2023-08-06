588627fedc6a5b489774d70eef8dd84868996fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:07 +0000

    Add package django-bootstrap3-datepicker/0.3/django-bootstrap3-datepicker-0.3.tar.gz

---
+++ packages/django-bootstrap3-datepicker/django-bootstrap3-datepicker-0.3.tar.gz/django-bootstrap3-datepicker-0.3/bootstrap3_datepicker/fields.py
@@ -0,0 +1,18 @@
+from django.forms import DateField
+
+from widgets import DatePickerInput
+
+
+class DatePickerField(DateField):
+    widget = DatePickerInput
+
+    def __init__(self, widget=None, input_formats=None, widget_attrs=None, widget_options=None, *args, **kwargs):
+        widget = widget or self.widget
+
+        # create widget instance with format and options applied
+        if isinstance(widget, type) and input_formats:
+            assert isinstance(input_formats, list)
+
+            widget = widget(format=input_formats[0], attrs=widget_attrs, options=widget_options)
+
+        super(DatePickerField, self).__init__(widget=widget, input_formats=input_formats, *args, **kwargs)
