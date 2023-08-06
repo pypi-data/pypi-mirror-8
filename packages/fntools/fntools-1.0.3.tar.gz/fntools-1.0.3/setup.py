a99b0a3b3e6beec2a1fbc5900cf86e5953fef073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:05 +0000

    Add package fntools/1.0.3/fntools-1.0.3.tar.gz

---
+++ packages/fntools/fntools-1.0.3.tar.gz/fntools-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='fntools',
+      description='Functional programming tools for data processing',
+      author=u'Taurus Olson',
+      author_email=u'taurusolson@gmail.com',
+      url='https://github.com/TaurusOlson/fntools',
+      version='1.0.3',
+      packages=['fntools'],
+      zip_safe=False
+      )
