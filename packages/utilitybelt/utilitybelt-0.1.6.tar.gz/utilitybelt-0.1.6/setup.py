8dcccf7122d162e2d7fdc458d836d88a02b2de5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:51 +0000

    Add package utilitybelt/0.1.6/utilitybelt-0.1.6.tar.gz

---
+++ packages/utilitybelt/utilitybelt-0.1.6.tar.gz/utilitybelt-0.1.6/setup.py
@@ -0,0 +1,28 @@
+"""
+Useful Utils
+==============
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='utilitybelt',
+    version='0.1.6',
+    author='Halfmoon Labs',
+    author_email='hello@halfmoonlabs.com',
+    description='Generally useful tools.',
+    keywords='dict dictionary scrub to_dict todict json characters charset hex',
+    url='https://github.com/onenameio/utilitybelt',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    zip_safe=False, 
+)
