1aceb4bc9ef563df90581bf7f9c1db1de6aa50b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:38 +0000

    Add package ppp_core/0.4.1/ppp_core-0.4.1.tar.gz

---
+++ packages/ppp_core/ppp_core-0.4.1.tar.gz/ppp_core-0.4.1/ppp_core/router.py
@@ -0,0 +1,80 @@
+"""Router of the PPP core."""
+
+import json
+import logging
+import requests
+import operator
+import itertools
+import functools
+from ppp_datamodel.communication import Request, Response
+from .config import Config
+from .exceptions import ClientError, BadGateway
+
+s = lambda x:x if isinstance(x, str) else x.decode()
+
+class Router:
+    def __init__(self, request):
+        self.id = request.id
+        self.language = request.language
+        self.tree = request.tree
+        self.config = Config()
+
+    def answer(self):
+        # First make all requests so modules can prepare their answer
+        # while we send requests to other modules
+        streams = self._get_streams()
+        answers = map(self._stream_reader, streams)
+        answers = map(self._process_answers, answers)
+        answers = itertools.chain(*list(answers)) # Flatten answers lists
+        answers = filter(bool, answers) # Eliminate None values
+        # TODO: should sort according to accuracy too
+        return sorted(answers, key=lambda x:x.measures['relevance'],
+                      reverse=True)
+
+    def _get_streams(self):
+        headers = {'Content-type': 'application/json',
+                   'Accept': 'application/json'}
+        payload = Request(self.id, self.language, self.tree).as_json()
+        getter = functools.partial(requests.post, stream=True,
+                                   headers=headers, data=payload)
+        streams = []
+        for module in self.config.modules:
+            try:
+                streams.append((module, getter(module.url)))
+            except requests.exceptions.ConnectionError as exc: # pragma: no cover
+                logging.warning('Module %s could not be queried: %s' %
+                                (module, exc.args[0]))
+                pass
+        return streams
+
+    def _stream_reader(self, stream):
+        (module, stream) = stream
+        if stream.status_code != 200:
+            logging.warning('Module %s returned %d: %s' %
+                            (module, stream.status_code, stream.content))
+            return None
+        else:
+            return (module, json.loads(s(stream.content)))
+
+    def _process_answers(self, t):
+        if t:
+            (module, answers) = t
+            answers = map(Response.from_dict, answers)
+            return list(map(functools.partial(self._process_answer, module), answers))
+        else:
+            return []
+
+    def _process_answer(self, module, answer):
+        missing = {'accuracy', 'relevance'} - set(answer.measures)
+        if missing:
+            logging.warning('Missing mandatory measures from module %s: %r' %
+                             (missing, module))
+        accuracy = answer.measures.get('accuracy', 0)
+        relevance = answer.measures.get('relevance', 0)
+        if accuracy < 0 or accuracy > 1:
+            logging.warning('Module %s answered with invalid accuracy: %r' %
+                    (module, accuracy))
+            return None
+        answer.measures['accuracy'] = accuracy
+        answer.measures['relevance'] = relevance * module.coefficient
+        return answer
