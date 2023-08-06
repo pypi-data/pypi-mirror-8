210a13a6748ed4fc730a63e126743e189415603b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:55 +0000

    Add package moesearch/0.1.5/moesearch-0.1.5.tar.gz

---
+++ packages/moesearch/moesearch-0.1.5.tar.gz/moesearch-0.1.5/moesearch/api.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import requests
+from .parser import *
+from .exceptions import is_error, ExceptionFactory
+from pprint import pprint
+
+#disabling warnings about unverified SSL certificates
+#as urllib3 is embedded, we don't have to worry about
+#urllib3 being missing.
+requests.packages.urllib3.disable_warnings()
+def __closeRequests(req_obj):
+  """internal. closes requests' objects.
+     will be removed when Requests is fixed"""
+  for queue_conn in req_obj.raw._pool.pool.queue:
+    if queue_conn:
+      queue_conn.close()
+
+def index(board, page=1):
+  req = requests.get("https://api.archive.moe/index",
+          stream=False, verify=False,
+          params = {"board": board, "page": int(page)})
+  res = req.json()
+#  __closeRequests(req)
+  if is_error(res):
+    raise ExceptionFactory.generateException(res)
+  for thread_num in res:
+    res[thread_num] = IndexResult(res[thread_num])
+  return res 
+
+def search(**kwargs):
+  url = "https://api.archive.moe/search"
+  #print("[api.search] url", url)
+  #kwargs["board"]=board
+  req = requests.get(url, stream=False, verify=False, params=kwargs)
+  res = req.json()
+#  __closeRequests(req)
+  if is_error(res):
+    raise ExceptionFactory.generateException(res)
+  res = res[0]
+  return [Post(post_obj) for post_obj in res["posts"]]
+
+def thread(board, thread_num, latest_doc_id=-1, last_limit=-1):
+  url = "https://api.archive.moe/thread"
+  payload = {"board": board, "num": thread_num}
+  if(latest_doc_id != -1):
+    payload["latest_doc_id"] = (int(latest_doc_id))
+  if(last_limit != -1):
+    payload["last_limit"] = (int(last_limit))
+  req = requests.get(url, stream=False, verify=False, params=payload)
+  res = req.json()
+#  __closeRequests(req)
+  if is_error(res):
+    raise ExceptionFactory.generateException(res)
+  return Thread(res)
+
+def post(board, post_num):
+  req = requests.get("https://api.archive.moe/post", verify=False,  stream=False,
+                  params={"board":board, "num":post_num})
+  res = req.json()
+#  __closeRequests(req)
+  if is_error(res):
+    raise ExceptionFactory.generateException(res)
+  return Post(res)
---
+++ packages/moesearch/moesearch-0.1.5.tar.gz/moesearch-0.1.5/setup.py
@@ -0,0 +1,25 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='moesearch',
+    version='0.1.5',
+    author='alfateam123',
+    author_email='alfateam123@gmail.com',
+    packages=['moesearch'],
+    scripts=['bin/index', 'bin/post_details', 'bin/thread_details', 'bin/search'],
+    url='http://github.com/alfateam123/moesearch',
+    #license='LICENSE.txt',
+    description='archive.moe Search REST API Library',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "requests >= 2.5.0"
+    ],
+    classifiers=[
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
