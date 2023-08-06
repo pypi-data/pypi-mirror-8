fd6b414677d04149fd016effe9cf164ab30d9843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:58 +0000

    Add package RPi_AS3935/0.0.4/RPi_AS3935-0.0.4.tar.gz

---
+++ packages/RPi_AS3935/RPi_AS3935-0.0.4.tar.gz/RPi_AS3935-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name = 'RPi_AS3935',
+      version = '0.0.4',
+      description = 'Raspberry Pi <-I2C-> AS3935 lightning sensor communication library',
+      author = 'Phil Fenstermacher',
+      author_email = 'phillip.fenstermacher@gmail.com',
+      url = 'https://github.com/pcfens/RaspberryPi-AS3935',
+      packages = ['RPi_AS3935'],
+      keywords = [ 'RaspberryPi', 'AS3935', 'lightning', 'weather' ],
+      license = 'GPL',
+)
