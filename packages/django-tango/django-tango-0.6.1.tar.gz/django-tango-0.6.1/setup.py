3cd1a2ecd1167928d5613e0eea1b4f6dbcd1af0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:33 +0000

    Add package django-tango/0.6.1/django-tango-0.6.1.tar.gz

---
+++ packages/django-tango/django-tango-0.6.1.tar.gz/django-tango-0.6.1/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('docs/requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='django-tango',
+    version='0.6.1',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    description='Faster, simpler Django content management.',
+    long_description=open('README.md').read(),
+    url='https://github.com/tBaxter/Tango',
+    license='LICENSE.txt',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=required,
+    zip_safe=False,
+)
