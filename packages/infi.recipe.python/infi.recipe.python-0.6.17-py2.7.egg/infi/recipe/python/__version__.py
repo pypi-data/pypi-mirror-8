95c6bf72bbb79bf6b04bd36211de9f4e3892097d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:01 +0000

    Add package infi.recipe.python/0.6.17/infi.recipe.python-0.6.17-py2.7.egg

---
+++ packages/infi.recipe.python/infi.recipe.python-0.6.17-py2.7.egg/infi/recipe/python/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.17"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.6.17)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2f86c651b3963f9c44e68e34733fdb4be3024011'
+__git_head_subject__ = 'Finished release v0.6.17.'
+__git_head_message__ = '* release/v0.6.17:\n  buildout.cfg: adding zc.buildout to requirements\n  buildout.cfg: remove infi.recipe.template.version>=0.3.3 from requirements'
+__git_dirty_diff__ = ''
