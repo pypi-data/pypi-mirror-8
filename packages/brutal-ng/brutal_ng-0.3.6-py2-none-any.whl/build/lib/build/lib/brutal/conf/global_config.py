97df161c691a891fad7da165766354fe0d7ccade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:57 +0000

    Add package brutal-ng/0.3.6/brutal_ng-0.3.6-py2-none-any.whl

---
+++ packages/brutal-ng/brutal_ng-0.3.6-py2-none-any.whl/build/lib/build/lib/brutal/conf/global_config.py
@@ -0,0 +1,15 @@
+import logging
+
+DEBUG = False
+LOG_LEVEL = logging.INFO
+LOG_FILE = 'bot.log'
+LOG_FORMAT = '%({})-21s %({})s %({})s (%({})-s) %({})d:%({})d - %({})s'
+LOG_FORMAT = LOG_FORMAT.format('asctime',
+                               'levelname',
+                               'name',
+                               'funcName',
+                               'process',
+                               'thread',
+                               'message')
+
+INSTALLED_PLUGINS = ()
