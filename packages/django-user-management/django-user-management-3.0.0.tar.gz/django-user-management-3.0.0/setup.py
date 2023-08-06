fbfefb3ceadd4ba01f15f7a8abbb0a31c7403742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:41 +0000

    Add package django-user-management/3.0.0/django-user-management-3.0.0.tar.gz

---
+++ packages/django-user-management/django-user-management-3.0.0.tar.gz/django-user-management-3.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+version = '3.0.0'
+
+
+install_requires = (
+    'djangorestframework>=2.4.3,<3',
+    'incuna_mail>=2.0.0,<3',
+)
+
+extras_require = {
+    'avatar': [
+        'django-imagekit>=3.2',
+    ],
+}
+
+setup(
+    name='django-user-management',
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    description='User management model mixins and api views.',
+    long_description='',
+    author='Incuna',
+    author_email='admin@incuna.com',
+    url='https://github.com/incuna/django-user-management/',
+    install_requires=install_requires,
+    extras_require=extras_require,
+    zip_safe=False,
+)
