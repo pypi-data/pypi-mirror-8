3ca109b77d0c3c6d86299d8bcd708536122c0d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:52 +0000

    Add package zope.index/4.1.0/zope.index-4.1.0.tar.gz

---
+++ packages/zope.index/zope.index-4.1.0.tar.gz/zope.index-4.1.0/README.rst
@@ -0,0 +1,16 @@
+``zope.index``
+==============
+
+.. image:: https://travis-ci.org/zopefoundation/zope.index.png?branch=master
+        :target: https://travis-ci.org/zopefoundation/zope.index
+
+The ``zope.index`` package provides several indices for the Zope
+catalog.  These include:
+
+* a field index (for indexing orderable values),
+
+* a keyword index,
+
+* a topic index,
+
+* a text index (with support for lexicon, splitter, normalizer, etc.)
---
+++ packages/zope.index/zope.index-4.1.0.tar.gz/zope.index-4.1.0/setup.py
@@ -0,0 +1,131 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.index package
+"""
+from __future__ import print_function
+import sys
+import os
+
+from setuptools import setup, find_packages, Extension
+from distutils.command.build_ext import build_ext
+from distutils.errors import CCompilerError
+from distutils.errors import DistutilsExecError
+from distutils.errors import DistutilsPlatformError
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+long_description = (open('README.rst').read() +
+                    '\n\n' +
+                    open('CHANGES.rst').read())
+
+class optional_build_ext(build_ext):
+    """This class subclasses build_ext and allows
+       the building of C extensions to fail.
+    """
+    def run(self):
+        try:
+            build_ext.run(self)
+
+        except DistutilsPlatformError as e:
+            self._unavailable(e)
+
+    def build_extension(self, ext):
+       try:
+           build_ext.build_extension(self, ext)
+
+       except (CCompilerError, DistutilsExecError) as e:
+           self._unavailable(e)
+
+    def _unavailable(self, e):
+        print('*' * 80, file=sys.stderr)
+        print("""WARNING:
+
+        An optional code optimization (C extension) could not be compiled.
+
+        Optimizations for this package will not be available!""",
+              file=sys.stderr)
+        print('', file=sys.stderr)
+        print(e, file=sys.stderr)
+        print('*' * 80, file=sys.stderr)
+
+def alltests():
+    import os
+    import sys
+    import unittest
+    # use the zope.testrunner machinery to find all the
+    # test suites we've put under ourselves
+    import zope.testrunner.find
+    import zope.testrunner.options
+    here = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+    args = sys.argv[:]
+    defaults = ["--test-path", here]
+    options = zope.testrunner.options.get_options(args, defaults)
+    suites = list(zope.testrunner.find.find_suites(options))
+    return unittest.TestSuite(suites)
+
+setup(name='zope.index',
+      version='4.1.0',
+      url='http://pypi.python.org/pypi/zope.index',
+      license='ZPL 2.1',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description="Indices for using with catalog like text, field, etc.",
+      long_description=long_description,
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        ],
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope',],
+      extras_require={
+        'test': [],
+        'tools': ['ZODB', 'transaction']},
+      install_requires=[
+        'persistent',
+        'BTrees',
+        'setuptools',
+        'six',
+        'zope.interface'],
+      tests_require = ['zope.testrunner'],
+      test_suite = '__main__.alltests',
+      ext_modules=[
+          Extension('zope.index.text.okascore',
+              [os.path.join('src', 'zope', 'index', 'text', 'okascore.c')]),
+      ],
+      cmdclass = {'build_ext':optional_build_ext},
+      include_package_data = True,
+      zip_safe=False,
+      )
