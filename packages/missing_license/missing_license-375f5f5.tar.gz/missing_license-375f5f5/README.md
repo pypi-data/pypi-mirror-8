5fcd1f3b6d11c0bd87d08fb7473c92e9e9b8dbab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:10 +0000

    Add package missing_license/375f5f5/missing_license-375f5f5.tar.gz

---
+++ packages/missing_license/missing_license-375f5f5.tar.gz/missing_license-375f5f5/README.md
@@ -0,0 +1,8 @@
+missing-license
+===============
+
+It looks like you're missing a license.
+
+This is a python script that creates issues on
+GitHub repositories that notify the repository
+owners that that they don't have a license.
---
+++ packages/missing_license/missing_license-375f5f5.tar.gz/missing_license-375f5f5/missing_license/__init__.py
@@ -0,0 +1,47 @@
+import sys
+
+from github import Github
+import click
+
+issue_body = """
+First, the standard disclaimer: I am not a lawyer, and this does not constitute legal or financial advice.
+
+Generally, IMHO, it is a good idea to use FSF or OSI Approved Licenses (which can be found here https://www.gnu.org/licenses/licenses.html and here http://opensource.org/licenses/category)
+
+The Free Software Foundation has a useful guide for choosing a license: https://www.gnu.org/licenses/license-recommendations.html
+
+I often reference the Software Freedom Law Center's Legal Primer for both practical and academic purposes (highly recommended): https://www.softwarefreedom.org/resources/2008/foss-primer.html#x1-60002.2
+
+https://tldrlegal.com/ is quite a useful resource for comparing the various FOSS licenses out there once you have some context
+
+To get ahold of actual lawyers/advisors who help FOSS projects, you can reach out to the FSF, SFLC, and OSI at:
+licensing@fsf.org
+help@softwarefreedom.org
+license-discuss@opensource.org
+
+Hope this helps, and happy hacking!
+"""
+
+@click.command()
+@click.option('--username',
+              help='GitHub Username',
+              prompt='Username: (testUser)')
+@click.option('--repo',
+              help='GitHub Repo That is Missing License',
+              prompt='Repo: (testUser/myRepo)')
+@click.option('--password',
+              prompt=True,
+              hide_input=True,
+              confirmation_prompt=True,
+              help='Your GitHub Password')
+def create_issue(username, repo, password):
+    g = Github(username, password)
+
+
+    gitRepo = g.get_repo(repo)
+    gitRepo.create_issue("Missing LICENSE?", issue_body)
+
+    print "Issue Created at repo: " + repo
+
+if __name__ == '__main__':
+    create_issue()
