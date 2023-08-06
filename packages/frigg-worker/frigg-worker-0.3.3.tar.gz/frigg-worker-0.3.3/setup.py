6b1aa0a095769ec5d535536ae778fde9ffad2be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:02 +0000

    Add package frigg-worker/0.3.3/frigg-worker-0.3.3.tar.gz

---
+++ packages/frigg-worker/frigg-worker-0.3.3.tar.gz/frigg-worker-0.3.3/setup.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='frigg-worker',
+    version='0.3.3',
+    description='A worker application that listens to the frigg broker '
+                'an pick up builds and build them.',
+    author='The frigg team',
+    author_email='hi@frigg.io',
+    license='MIT',
+    url='https://github.com/frigg/frigg-worker',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'fabric',
+        'redis',
+        'pyyaml',
+        'requests',
+        'raven'
+    ],
+    entry_points={
+        'console_scripts': ['frigg-worker = frigg.worker.cli:main']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+    ]
+)
