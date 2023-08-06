26bd0f64f8cbffb79df599d3ad8b00bd42f37c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:32 +0000

    Add package My1stProgram/1.0.1/My1stProgram-1.0.1.zip

---
+++ packages/My1stProgram/My1stProgram-1.0.1.zip/My1stProgram-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'My1stProgram',
+        version = '1.0.1',
+        py_modules = ['My1stProgram'],
+        author = 'clickjames',
+        author_email = 'clickjames@sina.com',
+        url = 'http://clickjames.com',
+        decription = 'Just a test.',
+    )                      
