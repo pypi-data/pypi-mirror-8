c4087463c28107b9b97aa24f80227ed2f63e9861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:42 +0000

    Add package testypie/0.2.0/testypie-0.2.0.tar.gz

---
+++ packages/testypie/testypie-0.2.0.tar.gz/testypie-0.2.0/testypie.py
@@ -0,0 +1,32 @@
+import shelve
+from flask import Flask, request, Response, stream_with_context
+import requests
+
+
+app = Flask(__name__)
+
+
+BASEDIR = 'fixtures'
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def proxy(path):
+    with shelve.open(BASEDIR) as cache:
+
+        if request.url not in cache:
+            client_headers = {
+                'Accept': request.headers['Accept']
+            }
+            upstream = requests.get(request.url, allow_redirects=False, headers=client_headers)
+
+            server_headers = {
+                'Content-Type': upstream.headers['content-type']
+            }
+            response = Response(response=upstream.content, status=upstream.status_code, headers=server_headers)
+            cache[request.url] = response
+
+        return cache[request.url]
+
+if __name__ == "__main__":
+    app.run(debug=True)
