7a3cbaeb4ffcee0e77c2a8dc218d0659b210d83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:52 +0000

    Add package infi.dtypes.hctl/0.0.8/infi.dtypes.hctl-0.0.8.tar.gz

---
+++ packages/infi.dtypes.hctl/infi.dtypes.hctl-0.0.8.tar.gz/infi.dtypes.hctl-0.0.8/src/infi/dtypes/hctl/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.8"
+__git_commiter_name__ = "Ayala Shachar"
+__git_commiter_email__ = "shachar.ayala@gmail.com"
+__git_branch__ = '(detached from v0.0.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '1f70ba9d91d41f0ef7da62a04bc5923e50c73ae4'
+__git_head_subject__ = 'Fix python3 compatability'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
