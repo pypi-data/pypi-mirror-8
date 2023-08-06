91f3e81fa661602a2970de2f2ea2636d546ee5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:34 +0000

    Add package ymci-ext-acl/1.0/ymci-ext-acl-1.0.tar.gz

---
+++ packages/ymci-ext-acl/ymci-ext-acl-1.0.tar.gz/ymci-ext-acl-1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ymci-ext-acl',
+    version='1.0',
+    description='ACL rights plugin for ymci',
+    author='Jean-Marc Martins, Kozea',
+    author_email='jmartins@kozea.fr',
+
+    platforms=['Any'],
+
+    scripts=[],
+
+    provides=['ymci_ext_acl'],
+    packages=find_packages(),
+    package_data={'ymci_ext_acl': ['templates/*', 'static/*']},
+
+    entry_points={
+        'ymci.ext.routes.Route': [
+            'acl_route = ymci_ext_acl.routes:RightsList'
+        ],
+        'ymci.ext': [
+            'ymci-ext-acl = ymci_ext_acl'
+        ],
+        'ymci.ext.db.Table': [
+            'acl_table = ymci_ext_acl.db:Acl'
+        ],
+        'ymci.ext.hooks.PrepareHook': [
+            'acl_hook = ymci_ext_acl.hooks:AclHook'
+        ]
+    }
+)
---
+++ packages/ymci-ext-acl/ymci-ext-acl-1.0.tar.gz/ymci-ext-acl-1.0/ymci_ext_acl/db.py
@@ -0,0 +1,74 @@
+from ymci.ext.db import Table
+from sqlalchemy import (
+    Column, Integer, ForeignKeyConstraint, String, UniqueConstraint)
+from sqlalchemy.orm import relationship, backref
+
+
+class Set(Table):
+    __tablename__ = 'ymci_ext_acl_set'
+    __table_args__ = (
+        ForeignKeyConstraint(['group_id'], ['ymci_ext_acl_group.group_id']),
+        ForeignKeyConstraint(['login'], ['ymci_user.login']))
+
+    group_id = Column(Integer, primary_key=True)
+    login = Column(String, primary_key=True)
+
+    group = relationship('Group', backref='sets')
+    user = relationship('User', backref='sets')
+
+
+class Group(Table):
+    __tablename__ = 'ymci_ext_acl_group'
+    __table_args__ = (UniqueConstraint('name'),)
+
+    group_id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    user = relationship(
+        "User", secondary='ymci_ext_acl_set',
+        backref=backref('groups', cascade='all'))
+
+
+class AclLevel(Table):
+    __tablename__ = 'ymci_ext_acl_level'
+    __table_args__ = (UniqueConstraint('level_id', 'name'),)
+
+    level_id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+
+
+class UserLevel(Table):
+    __tablename__ = 'ymci_ext_acl_user_level'
+    __table_args__ = (
+        ForeignKeyConstraint(['login'], ['ymci_user.login']),
+        ForeignKeyConstraint(['level_id'], ['ymci_ext_acl_level.level_id']))
+
+    login = Column(String, primary_key=True)
+    level_id = Column(Integer, primary_key=True)
+
+    user = relationship('User', backref=backref('level', uselist=False))
+    acl_level = relationship('AclLevel', backref='user_level')
+
+
+class Acl(Table):
+    __tablename__ = "ymci_ext_acl"
+    __table_args__ = (
+        ForeignKeyConstraint(['login'], ['ymci_user.login']),
+        ForeignKeyConstraint(['group_id'], ['ymci_ext_acl_group.group_id']),
+        ForeignKeyConstraint(['project_id'], ['project.project_id']),
+        ForeignKeyConstraint(['level_id'], ['ymci_ext_acl_level.level_id']),
+        UniqueConstraint('login', 'level_id', 'route'),
+        UniqueConstraint('group_id', 'route'))
+
+    acl_id = Column(Integer, primary_key=True)
+    login = Column(String)
+    group_id = Column(Integer)
+    level_id = Column(Integer, nullable=True)
+    project_id = Column(Integer, nullable=True)
+    route = Column(String, nullable=True)
+
+    project = relationship(
+        'Project', backref=backref('acls', cascade='all, delete-orphan'))
+    level = relationship('AclLevel', backref='acls', uselist=False)
+    user = relationship('User', backref='acls')
+    group = relationship('Group', backref='acls')
