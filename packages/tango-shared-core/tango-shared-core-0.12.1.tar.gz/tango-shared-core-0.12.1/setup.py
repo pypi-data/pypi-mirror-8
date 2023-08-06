9604668a1cbda3f8b649ae307d40c221156b8c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:31 +0000

    Add package tango-shared-core/0.12.1/tango-shared-core-0.12.1.tar.gz

---
+++ packages/tango-shared-core/tango-shared-core-0.12.1.tar.gz/tango-shared-core-0.12.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('docs/requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='tango-shared-core',
+    version='0.12.1',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    description='Tango shared/core functionality.',
+    long_description=open('README.md').read(),
+    url='https://github.com/tBaxter/tango-shared-core',
+    license='LICENSE',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=required,
+    include_package_data=True,
+    dependency_links = [
+        'http://github.com/tBaxter/django-voting/tarball/master#egg=tango-voting',
+    ]
+)
