96bed86145ad958f55e16d8d14182a701b07fa77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:57 +0000

    Add package iansface/1.0/iansface-1.0.tar.gz

---
+++ packages/iansface/iansface-1.0.tar.gz/iansface-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='iansface', 
+      version='1.0',
+      description='A module for printing Ian\'s face.',
+      author='gigawhitlocks',
+      author_email='giggles@alum.hackerschool.com',
+      url='http://github.com/gigawhitlocks/myface',
+      download_url='https://github.com/gigawhitlocks/myface/archive/master.zip',
+      py_modules=['iansface'])
