42979980f57b510257a8639989e9802cec4d4fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:12 +0000

    Add package braincore/2014.11.2/braincore-2014.11.2.tar.gz

---
+++ packages/braincore/braincore-2014.11.2.tar.gz/braincore-2014.11.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from datetime import datetime #for version string
+import os
+import sys
+sys.path.insert(0,os.path.join(os.path.dirname(__file__),'braincore'))
+import braincore
+
+now = datetime.now()
+
+
+#fuck it, good enough edition
+version="%s.%s.2" % (now.year, now.month) # PEP440 compliant
+
+setup(name="braincore",
+      version=version,
+      description="Core objects needed for most brainfuck-like languages",
+      url="https://github.com/jdukes/braincore",
+      author="Josh Dukes",
+      author_email="hex@neg9.org",
+      license="MIT",
+      entry_points = {
+          'console_scripts': [
+              'brainfuck=braincore.examples.brainfuck:main',
+              'brainloller=braincore.examples.brainloller:main',
+              'brainfuckshitstack=braincore.examples.brainfuckshitstack:main'],
+      },
+      keywords = "brainfuck, brainlol",
+      long_description=braincore.__doc__,
+      packages=["braincore"])
