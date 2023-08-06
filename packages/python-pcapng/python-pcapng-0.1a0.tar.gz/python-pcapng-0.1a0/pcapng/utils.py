9205f9cd3f29b707d4c8a163d6609a7ad849788e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:32 +0000

    Add package python-pcapng/0.1a0/python-pcapng-0.1a0.tar.gz

---
+++ packages/python-pcapng/python-pcapng-0.1a0.tar.gz/python-pcapng-0.1a0/pcapng/utils.py
@@ -0,0 +1,29 @@
+import io
+import socket
+
+
+def unpack_ipv4(data):
+    return socket.inet_ntoa(data)
+
+
+def _get_pairs(data):
+    stream = io.BytesIO(data)
+    while True:
+        b = stream.read(2)
+        if not b:
+            return
+        yield b
+
+
+def unpack_ipv6(data):
+    return ':'.join(
+        '{0:02x}{1:02x}'.format(ord(x), ord(y))
+        for (x, y) in _get_pairs(data))
+
+
+def unpack_macaddr(data):
+    return ':'.join(format(ord(x), '02x') for x in data)
+
+
+def unpack_euiaddr(data):
+    return unpack_macaddr(data)
