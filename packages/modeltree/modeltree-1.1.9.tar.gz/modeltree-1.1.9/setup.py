4587469af580b524230b31f9d514a8b2447218dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:21 +0000

    Add package modeltree/1.1.9/modeltree-1.1.9.tar.gz

---
+++ packages/modeltree/modeltree-1.1.9.tar.gz/modeltree-1.1.9/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import modeltree
+
+kwargs = {
+    # Packages
+    'packages': find_packages(exclude=['tests', '*.tests', '*.tests.*', 'tests.*']),
+    'include_package_data': True,
+
+    # Dependencies
+    'install_requires': ['django>=1.4,<1.8'],
+
+    'test_suite': 'test_suite',
+
+    # Metadata
+    'version': modeltree.__version__,
+    'name': 'modeltree',
+    'author': 'Byron Ruth',
+    'author_email': 'b@devel.io',
+    'description': 'Django ORM metaprogramming layer',
+    'license': 'BSD',
+    'keywords': 'Django meta ORM dynamic runtime',
+    'url': 'http://cbmi.github.com/modeltree/',
+    'classifiers': [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+    ],
+}
+
+setup(**kwargs)
