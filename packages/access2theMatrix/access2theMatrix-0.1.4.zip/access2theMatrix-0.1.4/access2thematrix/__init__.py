66b92195135ce64a20e52600863a6d454adc1e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:25 +0000

    Add package access2theMatrix/0.1.4/access2theMatrix-0.1.4.zip

---
+++ packages/access2theMatrix/access2theMatrix-0.1.4.zip/access2theMatrix-0.1.4/access2thematrix/__init__.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#
+#   Copyright © 2014 Stephan Zevenhuizen
+#   access2theMatrix, (20-12-2014).
+#
+
+'''
+    access2theMatrix is a Python library for accessing Omicron NanoTechnology's
+    MATRIX Control System result files. Only topography image data will be
+    accessed by this library.
+'''
+
+__version__ = '0.1.4'
+
+__all__ = ['Im', 'MtrxData']
+
+import access2thematrix
+
+class Im(access2thematrix.Im):
+    '''access2theMatrix_image_structure'''
+
+class MtrxData(access2thematrix.MtrxData):
+    '''
+        The methods to open result files and to select one out of the four
+        traces (forward/up, backward/up, forward/down and backward/down).
+
+    '''
+
+    def open(self, result_data_file):
+        '''
+        Opens a MATRIX Control System result file.
+
+        In attribute scan the images are stored. In attribute param the
+        metadata and parameters of the experiment are stored. See source code.
+
+        Parameters
+        ----------
+        result_data_file : str
+            The pathname of the MATRIX Control System result file.
+
+        Returns
+        -------
+        traces : dict
+            Dictionary of enumerated tracenames for the trace parameter in
+            the instancemethod select_image.
+        message : str
+            Error or succes message of the opening of the file.
+        '''
+        return access2thematrix.MtrxData.open(self, result_data_file)
+
+    def select_image(self, trace = access2thematrix.MtrxData.ALL_TRACES[0]):
+        '''
+        The selected image is returned.
+
+        Parameters
+        ----------
+        trace : str, optional
+            Use the traces dictionary, a return from the open method, to set
+            this parameter.
+
+        Returns
+        -------
+        im : access2theMatrix_image_structure
+            Returns a Im class (image structure) containing the selected
+            image and metadata.
+        message : str
+            Error or succes message of the image selection.
+        '''
+        return access2thematrix.MtrxData.select_image(self, trace)
---
+++ packages/access2theMatrix/access2theMatrix-0.1.4.zip/access2theMatrix-0.1.4/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from codecs import open
+from os import path
+import sys
+
+if len(sys.argv) == 1:
+    sys.argv.append('sdist')
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.txt'), encoding = 'utf-8') as f:
+    long_description = f.read().replace('\r\n', '\n').expandtabs(4)
+
+setup(name = 'access2theMatrix',
+      version = __import__('access2thematrix').__version__,
+      description = 'Omicron NanoTechnology\'s MATRIX Control System result'
+      ' file accessing tool',
+      long_description = long_description,
+      url = '',
+      author = 'Stephan Zevenhuizen',
+      author_email = 'S.J.M.Zevenhuizen@uu.nl',
+      license = 'The license file is in the package.',
+      keywords = 'SPM scanning probe microscopy image analysis',
+      classifiers = ['Development Status :: 5 - Production/Stable',
+                     'Intended Audience :: Science/Research',
+                     'License :: OSI Approved :: BSD License',
+                     'Natural Language :: English',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python :: 2.7',
+                     'Topic :: Scientific/Engineering :: Chemistry',
+                     'Topic :: Scientific/Engineering :: Information Analysis',
+                     'Topic :: Scientific/Engineering :: Physics',
+                     'Topic :: Software Development :: Libraries ::'
+                     ' Python Modules'],
+      packages = ['access2thematrix'],
+      package_data = {'access2thematrix': ['LICENSE.txt']})
