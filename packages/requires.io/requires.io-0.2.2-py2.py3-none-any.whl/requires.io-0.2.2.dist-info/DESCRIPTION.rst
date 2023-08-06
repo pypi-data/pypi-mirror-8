e9eed608e4d6092ceb00f1843d3a38769e1c955c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:45 +0000

    Add package requires.io/0.2.2/requires.io-0.2.2-py2.py3-none-any.whl

---
+++ packages/requires.io/requires.io-0.2.2-py2.py3-none-any.whl/requires.io-0.2.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,50 @@
+requires.io API
+---------------
+
+.. image:: https://requires.io/github/requires/api/requirements.svg?branch=master
+   :target: https://requires.io/github/requires/api/requirements/?branch=master
+   :alt: Requirements Status
+
+Install package:
+
+.. code-block:: text
+
+    pip install requires.io
+
+Create or update a repository:
+
+.. code-block:: text
+
+    requires.io update-repo -t MY_TOKEN -r MY_REPO (--public | --private)
+
+Create or update a branch:
+
+.. code-block:: text
+
+    requires.io update-branch -t MY_TOKEN -r MY_REPO -n MY_BRANCH /path/to/my/sources
+
+Create or update a tag:
+
+.. code-block:: text
+
+    requires.io update-tag -t MY_TOKEN -r MY_REPO -n MY_TAG /path/to/my/sources
+
+Monitor a site:
+
+* freeze the current environment with pip
+* hostname is the default site name
+
+.. code-block:: text
+
+    requires.io update-site -t MY_TOKEN -r MY_REPO
+
+Delete repositories, branches, tags and sites:
+
+.. code-block:: text
+
+    requires.io delete-repo -t MY_TOKEN -r MY_REPO
+    requires.io delete-branch -t MY_TOKEN -r MY_REPO -n MY_BRANCH
+    requires.io delete-tag -t MY_TOKEN -r MY_REPO -n MY_TAG
+    requires.io delete-site -t MY_TOKEN -r MY_REPO -n MY_SITE
+
+
---
+++ packages/requires.io/requires.io-0.2.2-py2.py3-none-any.whl/requires_io/tests.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import os
+import codecs
+import shutil
+import unittest
+import tempfile
+import contextlib
+
+from requests.exceptions import HTTPError
+from requests.status_codes import codes
+
+from requires_io.api import _to_urls
+from requires_io.commands import glob_type_re, GlobType, main
+
+
+class Repository(object):
+    def __init__(self, name):
+        self.name = name
+        self.root = None
+
+    @contextlib.contextmanager
+    def context(self):
+        tmp = tempfile.mkdtemp()
+        try:
+            self.root = os.path.join(tmp, self.name)
+            yield self
+        finally:
+            shutil.rmtree(tmp)
+            self.root = None
+
+    def write(self, filename, content, **kwargs):
+        path = os.path.normpath(os.path.join(self.root, filename))
+        fld = os.path.dirname(path)
+        if not os.path.exists(fld):
+            os.makedirs(fld)
+        with codecs.open(path, kwargs.pop('mode', 'w'), **kwargs) as fd:
+            fd.write(content)
+            if kwargs.get('clrf', True):
+                if not content.endswith('\n'):
+                    fd.write('\n')
+
+
+class TestCase(unittest.TestCase):
+    def assertIsNotNone(self, val):  # missing in 2.6
+        self.assertTrue(val is not None)
+
+    def assertRaiseForStatus(self, status_code, callable_obj, *args):
+        try:
+            callable_obj(*args)
+        except HTTPError as e:
+            self.assertEquals(status_code, e.response.status_code)
+        else:
+            self.fail('failed to raise status code %d' % status_code)
+
+    def test_re(self):
+        self.assertIsNotNone(glob_type_re.search('/foo/bar/setup.py'))
+        self.assertIsNotNone(glob_type_re.search('/foo/bar/tox.ini'))
+        self.assertIsNotNone(glob_type_re.search('/foo/bar/buildout.cfg'))
+        self.assertIsNotNone(glob_type_re.search('/foo/bar/versions.cfg'))
+        self.assertIsNotNone(glob_type_re.search('/foo/bar/requirements.txt'))
+        self.assertIsNotNone(glob_type_re.search('/foo/bar/requirements.pip'))
+        self.assertIsNotNone(glob_type_re.search('/foo/bar/requirements/prod.txt'))
+        self.assertIsNotNone(glob_type_re.search('/foo/bar/requirements/test.pip'))
+
+    def test_to_url(self):
+        self.assertEquals([], _to_urls([]))
+        self.assertEquals(['setup.py'], _to_urls(['/foo/bar/setup.py']))
+        self.assertEquals(
+            ['setup.py', 'requirements/prod.txt'],
+            _to_urls([os.path.normpath('/foo/bar/setup.py'), os.path.normpath('/foo/bar/requirements/prod.txt')]),
+        )
+
+    def assertPaths(self, paths, path):
+        self.assertEquals(paths, GlobType()(path))
+
+    def test_paths(self):
+        repository = Repository('foo')
+        with repository.context():
+            repository.write('setup.py', 'hello')
+            repository.write('foobar.txt', 'hello')
+            self.assertPaths(set([os.path.join(repository.root, 'setup.py')]), repository.root)
+            self.assertPaths(
+                set([os.path.join(repository.root, 'foobar.txt')]),
+                os.path.join(repository.root, '*.txt')
+            )
+
+    def test_update_site(self):
+        self.assertRaiseForStatus(codes.NOT_FOUND, main, ['requires.io', 'update-site', '-t', '1234', '-r', 'foo'])
+
+
+if __name__ == '__main__':
+    unittest.main()
