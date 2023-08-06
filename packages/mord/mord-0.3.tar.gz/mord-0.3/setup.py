616bf308b835744ab1b9524f6b7d9f00d1775000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:22 +0000

    Add package mord/0.3/mord-0.3.tar.gz

---
+++ packages/mord/mord-0.3.tar.gz/mord-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+import mord
+
+setup(name='mord',
+    version=mord.__version__,
+    description='Ordinal regression models',
+    long_description=open('README.rst').read(),
+    author='Fabian Pedregosa',
+    author_email='f@bianp.net',
+    url='https://pypi.python.org/pypi/mord',
+    packages=['mord'],
+    requires = ['numpy', 'scipy'],
+)
