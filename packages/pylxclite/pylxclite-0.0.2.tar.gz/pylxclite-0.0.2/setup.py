3a59f114d9a8b62b753c97228799e32641315e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:57 +0000

    Add package pylxclite/0.0.2/pylxclite-0.0.2.tar.gz

---
+++ packages/pylxclite/pylxclite-0.0.2.tar.gz/pylxclite-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+version = '0.0.2'
+
+setup(name='pylxclite',
+      description="A python wrapper for Linux Containers (LXC)",
+      long_description="A python wrapper for LXC commands",
+      version=version,
+      url='https://github.com/Unixlike/pylxclite',
+      author="Camel",
+      author_email="393789775@qq.com",
+      packages=find_packages(),
+      zip_safe=True,
+      license = "LGPLv3",
+      classifiers = ['Development Status :: 5 - Production/Stable',
+                     'Intended Audience :: System Administrators',
+                     'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+                     'Operating System :: POSIX :: Linux',
+                     'Programming Language :: Python :: 2.6',
+                     'Topic :: System :: Systems Administration'],
+      
+      )
