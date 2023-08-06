a37709479a16f8c4b536b949c0d8ae30ad923a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:47 +0000

    Add package flup6/0.2a/flup6-0.2a.tar.gz

---
+++ packages/flup6/flup6-0.2a.tar.gz/flup6-0.2a/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'flup6',
+    version = '0.2a',
+    packages = find_packages(),
+    zip_safe = True,
+    
+    entry_points = """
+    [paste.server_runner]
+    ajp = flup.server.paste_factory:run_ajp_thread
+    fcgi = flup.server.paste_factory:run_fcgi_thread
+    scgi = flup.server.paste_factory:run_scgi_thread
+    ajp_thread = flup.server.paste_factory:run_ajp_thread
+    fcgi_thread = flup.server.paste_factory:run_fcgi_thread
+    scgi_thread = flup.server.paste_factory:run_scgi_thread
+    ajp_fork = flup.server.paste_factory:run_ajp_fork
+    fcgi_fork = flup.server.paste_factory:run_fcgi_fork
+    scgi_fork = flup.server.paste_factory:run_scgi_fork
+    """,
+    
+    author = 'Mikhail Denisenko',
+    author_email = 'denisenkom@gmail.com',
+    description = 'Random assortment of WSGI servers',
+    license = 'BSD',
+    url='https://bitbucket.org/denisenkom/flup',
+    classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    )
