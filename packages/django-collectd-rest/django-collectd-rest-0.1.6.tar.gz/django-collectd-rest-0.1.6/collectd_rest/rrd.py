179ad9e074cedb43cebd1397098ab0d23506621a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:25 +0000

    Add package django-collectd-rest/0.1.6/django-collectd-rest-0.1.6.tar.gz

---
+++ packages/django-collectd-rest/django-collectd-rest-0.1.6.tar.gz/django-collectd-rest-0.1.6/collectd_rest/rrd.py
@@ -0,0 +1,14 @@
+import shlex, subprocess
+
+class RRDError(Exception):
+	def __init__(self, message):
+		self.message = message
+
+def render(command, format):
+	format = format.upper()
+	args = ['rrdtool','graph','-','--imgformat',format] + shlex.split(command)
+	p = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	code = p.wait()
+	if code != 0:
+		raise RRDError(p.stderr.read())
+	return p.stdout.read()
