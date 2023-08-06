8ccf559ae62720a6ae322524607ded1c72913f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:44 +0000

    Add package importd/0.3.0/importd-0.3.0.zip

---
+++ packages/importd/importd-0.3.0.zip/importd-0.3.0/importd/urlconf.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+try:
+    from django.conf.urls import patterns
+except ImportError:
+    from django.conf.urls.defaults import patterns
+finally:
+    urlpatterns = patterns("")
