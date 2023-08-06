d509c2a6be8dad0420846079f162214a7839e64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:43 +0000

    Add package vishap/0.1.2/vishap-0.1.2.tar.gz

---
+++ packages/vishap/vishap-0.1.2.tar.gz/vishap-0.1.2/src/vishap/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'vishap'
+__version__ = '0.1.2'
+__build__ = 0x000003
+__author__ = 'Artur Barseghyan'
+__all__ = ('render_video', 'get_registered_plugins', 'get_registered_plugin_uids', 'ensure_autodiscover')
+
+from vishap.utils import render_video, get_registered_plugins, get_registered_plugin_uids, ensure_autodiscover
