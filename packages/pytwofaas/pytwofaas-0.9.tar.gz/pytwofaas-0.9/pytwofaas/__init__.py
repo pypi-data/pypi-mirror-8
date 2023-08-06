34f854ddd23e76e3b783c4c08b138903197981d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:14 +0000

    Add package pytwofaas/0.9/pytwofaas-0.9.tar.gz

---
+++ packages/pytwofaas/pytwofaas-0.9.tar.gz/pytwofaas-0.9/pytwofaas/__init__.py
@@ -0,0 +1,23 @@
+import requests
+
+class PyTwoFaas():
+
+  url = "http://127.0.0.1:5000"
+
+  def __init__(self, companyToken):
+    self.cToken = companyToken
+    print self.cToken
+
+  def init(self, clientId, phoneNum):
+    print "It got here"
+    payload = { 'userID' : clientId, 'compTK': self.cToken, 'userNum': phoneNum }
+    print "Here"
+    import pdb; pdb.set_trace
+    r = requests.post(self.url + "/init", data=payload)
+    print "There"
+    print(r.text)
+
+  def validate(self, clientId, code):
+    payload = {'userID' :clientId,'compTK':self.cToken,'twoAuth':code}
+    r = requests.post(self.url + "/validate", data=payload)
+    print(r.text)
