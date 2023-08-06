66ac228f7a5db2ad59cd4dcb92b4d2349fac4013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:53 +0000

    Add package goenv/0.0.6/goenv-0.0.6.tar.gz

---
+++ packages/goenv/goenv-0.0.6.tar.gz/goenv-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+files = \
+[
+  t.replace('goenv/', '') for t in reduce(
+    lambda a, b: a+b, [
+      map(lambda y: x[0]+'/'+y, x[2]) \
+      for x in os.walk('goenv/data')
+    ]
+  )
+] + [
+  'goenv.sh'
+]
+
+
+setup(
+  name='goenv',
+  version='0.0.6',
+  description='Golang environment manager',
+  url='https://github.com/biinilya/goenv',
+  author='Ilya Biin',
+  author_email='me@ilyabiin.com',
+  packages=['goenv'],
+  scripts=['goenv/goenv'],
+  package_data={'goenv': files},
+)
