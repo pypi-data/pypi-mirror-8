a63e30e340f94d675f0f34f7e600870847d9249b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:50 +0000

    Add package snor/0.0.7/snor-0.0.7.tar.gz

---
+++ packages/snor/snor-0.0.7.tar.gz/snor-0.0.7/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='snor',
+    version='0.0.7',
+    author='Paul Tax',
+    author_email='paultax@gmail.com',
+    include_package_data=True,
+    install_requires = [
+        'requests>=0.14.0',
+        'transmissionrpc>=0.9',
+        'peewee>=2.0.8',
+        'Flask>=0.9',
+        'Jinja2>=2.6'
+    ],
+    packages=[
+        'snor',
+        'snor.search',
+        'snor.clients'
+    ],
+    entry_points={
+        'console_scripts': [
+            'snor = snor.server:main',
+        ],
+    },
+    url='https://github.com/tax/snor',
+    license='MIT licence, see LICENCE.txt',
+    description='A very lightweight "sickbeard for torrents" ',
+    long_description=open('README.md').read(),
+)
---
+++ packages/snor/snor-0.0.7.tar.gz/snor-0.0.7/snor/conf.py
@@ -0,0 +1,58 @@
+import os
+import json
+import datetime
+import clients
+import search
+from models import Setting
+
+
+class Conf():
+    setting_name = 'snor.conf'
+    _settings = {
+        'login_required': False,
+        'username': 'admin',
+        'password': 'admin',
+        'filters': '',
+        'secret_key': os.urandom(24).encode('base64'),
+        'api_key': os.urandom(24).encode('base64').replace('+', '-'),
+        'folder': os.path.expanduser('~'),
+        'client': clients.get_torrent_clients()[0],
+        'search_client': search.get_search_clients()[0],
+        'use_season_folders': True,
+        'download_new_only': False
+    }
+
+    def __getattr__(self, name):
+        settings = self.get_settings()
+        if name not in self._settings:
+            raise KeyError('Invalid setting: {0}'.format(name))
+        return settings.get(name, None)
+
+    def get_settings(self):
+        try:
+            setting = Setting.get(name=self.setting_name)
+            return json.loads(setting.value)
+        except:
+            Setting.create(
+                name=self.setting_name,
+                value=json.dumps(self._settings)
+            )
+        return self._settings
+
+    def set_settings(self, **kwargs):
+        # Only copy valid keys fallback to default settings
+        vk = self._settings.keys()
+        kwargs = {k: v for k, v in kwargs.items() if k in vk}
+        kwargs = dict(self._settings.items() + kwargs.items())
+        try:
+            s = Setting.get(name=self.setting_name)
+            s.value = json.dumps(kwargs)
+            s.save()
+        except:
+            Setting.create(
+                name=self.setting_name,
+                value=json.dumps(kwargs),
+                date_last_updated=datetime.datetime.now()
+            )
+
+settings = Conf()
