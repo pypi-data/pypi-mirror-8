cf8f5438c22c9c860d80cea96751e2d97c7b055f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:17 +0000

    Add package django-pico/0.0.4/django-pico-0.0.4.tar.gz

---
+++ packages/django-pico/django-pico-0.0.4.tar.gz/django-pico-0.0.4/djpico/views.py
@@ -0,0 +1,32 @@
+import pico.server
+from pico import PicoError
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
+pico.server.DEBUG = settings.DEBUG
+
+
+@csrf_exempt
+def index(request):
+    params = {}
+    params.update(request.GET.dict())
+    params.update(request.POST.dict())
+    path = '/' + request.path.split('/pico/')[-1]
+    try:
+        pico_response = pico.server.handle_api_v2(path, params, request)
+    except PicoError, e:
+        pico_response = e.response
+    code, reason = pico_response.status.split(' ', 1)
+    response = HttpResponse(pico_response.output,
+                            status=int(code),
+                            reason=reason)
+    for key, header in pico_response.headers:
+        response[key] = header
+    return response
+
+
+def picojs(request):
+    f = open(pico.server.pico_path + 'client.js')
+    return HttpResponse(f.read(), mimetype='application/javascript')
