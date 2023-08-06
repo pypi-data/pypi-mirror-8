2aa95725d1e2cbb367ff015ffeb9f3e501d12b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:02 +0000

    Add package infi.storagemodel/0.2.35/infi.storagemodel-0.2.35.tar.gz

---
+++ packages/infi.storagemodel/infi.storagemodel-0.2.35.tar.gz/infi.storagemodel-0.2.35/src/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.35"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.35)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8f1826bc3d628afb86779bb453f199b31876fdad'
+__git_head_subject__ = 'HIPVM-603 fix'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
