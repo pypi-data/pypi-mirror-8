3630793380a23f3215b8be0218b4929b183d7baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:53 +0000

    Add package basicsorts/1.0/basicsorts-1.0.tar.gz

---
+++ packages/basicsorts/basicsorts-1.0.tar.gz/basicsorts-1.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+	name='basicsorts',
+	packages=['basicsorts'],
+	version='1.0',
+	description='Basic sorting algorithmes for arrays',
+	author='Samy Melaine',
+	author_email='melaine.samy@gmail.com',
+	url='https://github.com/SamyMe/basicsorts/',
+	download_url='https://github.com/SamyMe/basicsorts/tarball/1.0',
+	keywords=['sort','array','basic','bubble','merge','quick','insertion','heap','semectop'],
+	classifiers=[ 
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python',
+	],
+)
