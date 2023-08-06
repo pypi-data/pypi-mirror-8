a67bd9869cbce4111eeffa08aef76682ce87e628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:06 +0000

    Add package QQhistorycut/0.0.2/QQhistorycut-0.0.2.tar.gz

---
+++ packages/QQhistorycut/QQhistorycut-0.0.2.tar.gz/QQhistorycut-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+PACKAGE='qqxx'
+NAME='QQhistorycut'
+DESCRIPTION='Cut QQ history'
+AUTHOR='Gryfalcon'
+AUTHOR_EMAIL='lh441027725@gamil.com'
+URL='https://github.com/haoli001/qqxx'
+
+setup(
+	name='QQhistorycut',
+	version='0.0.2',
+	py_modules=['qqxx'],
+	author='Gryfalcon',
+	author_email='lh441027725@gamil.com',
+	description='Cut QQ history'
+)
