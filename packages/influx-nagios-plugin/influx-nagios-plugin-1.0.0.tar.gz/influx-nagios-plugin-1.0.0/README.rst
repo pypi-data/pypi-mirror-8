4185eaff6e01196f1eaa39bbdd76f5c371fb7633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:50 +0000

    Add package influx-nagios-plugin/1.0.0/influx-nagios-plugin-1.0.0.tar.gz

---
+++ packages/influx-nagios-plugin/influx-nagios-plugin-1.0.0.tar.gz/influx-nagios-plugin-1.0.0/README.rst
@@ -0,0 +1,6 @@
+nagios-influx-plugin
+====================
+
+Nagios plugin for querying stats from InfluxDB. See usage `documentation`_.
+
+.. _documentation: https://github.com/shaharke/influx-nagios-plugin
---
+++ packages/influx-nagios-plugin/influx-nagios-plugin-1.0.0.tar.gz/influx-nagios-plugin-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+ 
+setup(
+    name='influx-nagios-plugin',
+    version="1.0.0",
+    packages=['src'],
+    install_requires=['influxdb', 'NagAconda'],
+    license='MIT',
+    url="https://github.com/shaharke/nagios-influx-plugin.git",
+    description='Nagios plugin for querying stats from InfluxDB',
+    author='Shahar Kedar',
+    author_email='shahar at gmail com',
+    scripts=['src/check_influx']
+)
