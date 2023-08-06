f7c029c62e114ddab0e7508f70b34495bf9bb4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:36 +0000

    Add package flumelogger/0.3.3/flumelogger-0.3.3.tar.gz

---
+++ packages/flumelogger/flumelogger-0.3.3.tar.gz/flumelogger-0.3.3/flumelogger/eventserver.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from thrift import Thrift
+from thrift.transport import TTransport
+from thrift.transport import TSocket
+from thrift.protocol import TCompactProtocol, TBinaryProtocol
+from flumelogger.flumeng import ThriftSourceProtocol
+from flumelogger.flumeog import ThriftFlumeEventServer
+
+class FlumeEventServer(object):
+    def __init__(self, host="localhost", port=9090, type='ng'):
+        self.host = host
+        self.port = port
+        self.type = type
+        self.transport = None
+        self.client = None
+
+    def connect(self):
+        socket = TSocket.TSocket(self.host, self.port)
+        socket._timeout = 1000
+        if self.type == 'ng':
+            self.transport = TTransport.TFramedTransport(socket)
+            protocol = TCompactProtocol.TCompactProtocol(self.transport)
+            self.client = ThriftSourceProtocol.Client(protocol)
+        elif self.type == 'og':
+            self.transport = TTransport.TBufferedTransport(socket)
+            protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
+            self.client = ThriftFlumeEventServer.Client(protocol)
+        self.transport.open()
+
+    def append(self, event):
+        try:
+            if self.client is None:
+                self.connect()
+
+            self.client.append(event)
+        except Exception, tx:
+            print 'Thrift: %s' % tx
+            self.client = None
+            self.transport.close()
---
+++ packages/flumelogger/flumelogger-0.3.3.tar.gz/flumelogger-0.3.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'flumelogger',
+    version = '0.3.3',
+    description = 'Flume logging handler for sending log events to Flume',
+    author = 'Lars Sjostrom',
+    author_email = 'lars@radicore.se',
+    url = 'http://github.com/lsjostro/flumelogger',
+    packages = find_packages(),
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'thrift >= 0.9.0',
+    ],
+)
