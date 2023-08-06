2e16531450fc29878cb8d0cbb06dd4e2c629eff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:56 +0000

    Add package cartodb_dashboard/0.2.11/cartodb_dashboard-0.2.11.tar.gz

---
+++ packages/cartodb_dashboard/cartodb_dashboard-0.2.11.tar.gz/cartodb_dashboard-0.2.11/setup.py
@@ -0,0 +1,19 @@
+#from distutils.core import setup
+from setuptools import setup
+
+VERSION  = '0.2.11'
+REQUIRES = ['simplejson', 'httplib2']
+URL      = 'https://github.com/realestate-com-au/cartodb-dashboard-python'
+
+setup(name             = 'cartodb_dashboard',
+      author           = 'Simon Hope',
+      author_email     = 'shope@geoplex.com.au',
+      description      = 'Provides access to the cartodb dashboard',
+      version          = VERSION,
+      url              = URL,
+      download_url     = URL + '/tarball/' + VERSION,
+      install_requires = REQUIRES,
+      packages         = ['cartodb_dashboard'],
+      requires         = REQUIRES,
+      test_suite       = 'test.client'
+)
