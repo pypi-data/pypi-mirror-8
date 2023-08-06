c469a2e453d9a6b6abdabe67c106e15e8644ee91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:06 +0000

    Add package titlecase/0.7.2/titlecase-0.7.2.tar.gz

---
+++ packages/titlecase/titlecase-0.7.2.tar.gz/titlecase-0.7.2/setup.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+from titlecase import __version__
+
+setup(name='titlecase',
+    version=__version__,
+    description="Python Port of John Gruber's titlecase.pl",
+    long_description=readme(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Text Processing :: Filters",
+    ],
+    keywords='string formatting',
+    author="Pat Pannuto, Stuart Colville, John Gruber",
+    author_email="pat.pannuto+titlecase@gmail.com",
+    url="https://github.com/ppannuto/python-titlecase",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    tests_require=['nose'],
+    setup_requires=['nose>=1.0'],
+    test_suite="titlecase.tests",
+    entry_points = """\
+    """
+)
+
