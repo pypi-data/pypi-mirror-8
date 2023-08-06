f1ce687844eb86be80cef0d1d96b9a046bdfa80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:18 +0000

    Add package UkPostcodeParser/1.0.3/UkPostcodeParser-1.0.3.tar.gz

---
+++ packages/UkPostcodeParser/UkPostcodeParser-1.0.3.tar.gz/UkPostcodeParser-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='UkPostcodeParser',
+    version='1.0.3',
+    author='Simon Brunning',
+    author_email='simon@brunningonline.net',
+    packages=['ukpostcodeparser', 'ukpostcodeparser.test'],
+    url='https://github.com/hamstah/ukpostcodeparser',
+    description='UK Postcode parser',
+    license='MIT',
+)
