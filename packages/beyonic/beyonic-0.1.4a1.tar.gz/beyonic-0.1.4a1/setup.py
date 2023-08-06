a62f9b96f330f56131677c3fd1f53d237217c3f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:03 +0000

    Add package beyonic/0.1.4a1/beyonic-0.1.4a1.tar.gz

---
+++ packages/beyonic/beyonic-0.1.4a1.tar.gz/beyonic-0.1.4a1/setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from setuptools import setup
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+from setuptools import setup
+import os.path
+
+if os.path.exists('README.md'):
+    import shutil
+    shutil.copyfile('README.md', 'README.txt')
+
+VERSION = "0.1.4a1"
+
+setup(
+    name="beyonic",
+    version=VERSION,
+    description="The official Python client for the Beyonic.com API",
+    author="Beyonic",
+    author_email="info@beyonic.com",
+    long_description=read('README.txt'),
+    packages=["beyonic", "beyonic.apis"],
+    install_requires=["requests"],
+    license="MIT",
+    keywords=["api", "mobile payments", "mobile money", "beyonic", "mpesa"],
+    url="http://beyonic.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
