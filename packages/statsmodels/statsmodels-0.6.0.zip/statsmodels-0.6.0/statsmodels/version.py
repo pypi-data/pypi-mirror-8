16767317b5cea3fdca3ba0adb49061ea9b3b357b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:00 +0000

    Add package statsmodels/0.6.0/statsmodels-0.6.0.zip

---
+++ packages/statsmodels/statsmodels-0.6.0.zip/statsmodels-0.6.0/statsmodels/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.6.0'
+version = '0.6.0'
+full_version = '0.6.0'
+git_revision = 'd946eb0fbfa37ff9bbb71c39ef411d80a08b3ca2'
+release = True
+
+if not release:
+    version = full_version
