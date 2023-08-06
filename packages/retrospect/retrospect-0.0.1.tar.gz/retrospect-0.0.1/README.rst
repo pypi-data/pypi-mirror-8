3077978bce0c458fd1d2e7623084536cd170a8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:58 +0000

    Add package retrospect/0.0.1/retrospect-0.0.1.tar.gz

---
+++ packages/retrospect/retrospect-0.0.1.tar.gz/retrospect-0.0.1/README.rst
@@ -0,0 +1,8 @@
+==========
+Retrospect
+==========
+
+**Dynamic and adaptive logging AFTER the fact**
+
+Classic logging expects perfect foresight, but everyone knows only hindsight
+is 20/20!
---
+++ packages/retrospect/retrospect-0.0.1.tar.gz/retrospect-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# encoding: utf8
+
+
+from setuptools import setup
+
+
+setup(
+    name='retrospect',
+    version='0.0.1',
+    description='Log with 20/20 vision',
+    long_description=open('README.rst').read(),
+    url='https://github.com/xtfxme/retrospect',
+    author='C Anthony Risinger',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        ],
+    package_data={},
+    packages=[],
+    install_requires=[],
+    entry_points={},
+    zip_safe=True,
+    )
