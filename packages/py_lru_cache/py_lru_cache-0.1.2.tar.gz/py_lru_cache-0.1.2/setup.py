7766e75b3fdcc6968b585ef98ebe92c3f3db5fbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:56 +0000

    Add package py_lru_cache/0.1.2/py_lru_cache-0.1.2.tar.gz

---
+++ packages/py_lru_cache/py_lru_cache-0.1.2.tar.gz/py_lru_cache-0.1.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(name='py_lru_cache',
+      version='0.1.2',
+      description="""LRU cache for python. Provides a dictionary-like object as well as a method decorator.""",
+      author='Chris Stucchio',
+      author_email='stucchio@gmail.com',
+      license='Dual: GPL v3 or BSD',
+      url='https://github.com/stucchio/Python-LRU-cache',
+      packages = ['lru'],
+     )
