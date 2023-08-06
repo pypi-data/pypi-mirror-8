28a1f2aca380f10935bb6319fa34422329ad03bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:42 +0000

    Add package ConsistentHashing/0.1.7/ConsistentHashing-0.1.7.tar.gz

---
+++ packages/ConsistentHashing/ConsistentHashing-0.1.7.tar.gz/ConsistentHashing-0.1.7/setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import setup
+
+fd = open('./README')
+long_desc = fd.read()
+fd.close()
+
+setup(name='ConsistentHashing',
+      version = '0.1.7',
+      author="Ziang Guo",
+      author_email="iziang@yeah.net",
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      py_modules=['consistenthashing'],
+      platforms=["Any"],
+      license="BSD",
+      keywords='hashing hash consistent distributed',
+      description="A simple implement of consistent hashing.",
+      long_description=long_desc
+)
