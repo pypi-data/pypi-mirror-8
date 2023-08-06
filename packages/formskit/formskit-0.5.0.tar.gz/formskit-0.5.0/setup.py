9bc6efee681afa76f2e34d931f56df86c69a9a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:51 +0000

    Add package formskit/0.5.0/formskit-0.5.0.tar.gz

---
+++ packages/formskit/formskit-0.5.0.tar.gz/formskit-0.5.0/setup.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+    'python-dateutil',
+]
+
+if __name__ == '__main__':
+    setup(name='formskit',
+          version='0.5.0',
+          author=['Dominik "Socek" Długajczy'],
+          author_email=['msocek@gmail.com', ],
+          packages=find_packages(),
+          test_suite='formskit.tests.get_all_test_suite',
+          )
