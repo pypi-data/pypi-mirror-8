00baadf8ff7e23fed4db889c3fbc5f5a973f8fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:29 +0000

    Add package SOLARNET/0.1/SOLARNET-0.1.tar.gz

---
+++ packages/SOLARNET/SOLARNET-0.1.tar.gz/SOLARNET-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'SOLARNET',
+	packages = ['SOLARNET'],
+	version = '0.1',
+	description = 'A python client to access the RESTfull interface of the SOLARNET Data Archive',
+	author = 'Benjamin Mampaey',
+	author_email = 'bmampaey@gmail.com',
+	url = 'https://github.com/bmampaey/SOLARNET-python-client',
+	download_url = 'https://github.com/bmampaey/SOLARNET-python-client/tarball/0.1',
+	keywords = ['sun', 'physics', 'SOLARNET', 'RESTfull'],
+	classifiers = [],
+)
