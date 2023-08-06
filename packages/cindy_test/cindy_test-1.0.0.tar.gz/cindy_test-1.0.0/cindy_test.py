ef4b2011037e7fbb3ba9d7813b7802af5c1f76f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:25 +0000

    Add package cindy_test/1.0.0/cindy_test-1.0.0.tar.gz

---
+++ packages/cindy_test/cindy_test-1.0.0.tar.gz/cindy_test-1.0.0/cindy_test.py
@@ -0,0 +1,10 @@
+"""这是nester.py模块，提供函数用来打印循环嵌套列表"""
+def print_lol(the_list,level):
+    """这个函数打印一个包含嵌套列表的列表，并tab缩进"""
+    for each in the_list:
+        if isinstance(each,list):
+            print_lol(each,level+1)
+        else:
+            for num in range(level):
+                print("\t",end='')
+            print(each)
