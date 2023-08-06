795482dd2c556410429d6405f51f4dc5427758af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package IndicoIo/0.4.10/IndicoIo-0.4.10.tar.gz

---
+++ packages/IndicoIo/IndicoIo-0.4.10.tar.gz/IndicoIo-0.4.10/indicoio/__init__.py
@@ -0,0 +1,24 @@
+from functools import partial
+import indicoio.config as config 
+
+JSON_HEADERS = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+Version, version, __version__, VERSION = ('0.4.5',) * 4
+
+from indicoio.text.sentiment import political, posneg
+from indicoio.text.sentiment import posneg as sentiment
+from indicoio.text.lang import language
+from indicoio.text.tagging import text_tags
+from indicoio.images.fer import fer
+from indicoio.images.features import facial_features
+from indicoio.images.features import image_features
+
+political = partial(political, config.api_root)
+posneg = partial(posneg, config.api_root)
+sentiment = partial(sentiment, config.api_root)
+posneg = partial(sentiment, config.api_root)
+language = partial(language, config.api_root)
+fer = partial(fer, config.api_root)
+facial_features = partial(facial_features, config.api_root)
+image_features = partial(image_features, config.api_root)
+text_tags = partial(text_tags, config.api_root)
---
+++ packages/IndicoIo/IndicoIo-0.4.10.tar.gz/IndicoIo-0.4.10/setup.py
@@ -0,0 +1,39 @@
+"""
+Setup for indico apis
+"""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="IndicoIo",
+    version='0.4.10',
+    packages=[
+        "indicoio",
+        "indicoio.text",
+        "indicoio.images",
+        "indicoio.utils",
+        "indicoio.local",
+        "tests",
+    ],
+    description="""
+        A Python Wrapper for indico.
+        Use pre-built state of the art machine learning algorithms with a single line of code.
+    """,
+    license="MIT License (See LICENSE)",
+    long_description=open("README").read(),
+    url="https://github.com/IndicoDataSolutions/indicoio-python",
+    author="Alec Radford, Slater Victoroff, Aidan McLaughlin",
+    author_email="""
+        Alec Radford <alec@indicodatasolutions.com>,
+        Slater Victoroff <slater@indicodatasolutions.com>,
+        Aidan McLaughlin <aidan@indicodatasolutions.com>
+    """,
+    install_requires=[
+        "requests >= 1.2.3",
+        "six >= 1.3.0",
+        "numpy >= 1.8.1",
+        "scikit-image >= 0.10.1",
+    ],
+)
