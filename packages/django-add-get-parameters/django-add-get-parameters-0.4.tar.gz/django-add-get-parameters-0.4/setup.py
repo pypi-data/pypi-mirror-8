dba40ac7d183d90eae41cf7c40b8be2ebe51926a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:17 +0000

    Add package django-add-get-parameters/0.4/django-add-get-parameters-0.4.tar.gz

---
+++ packages/django-add-get-parameters/django-add-get-parameters-0.4.tar.gz/django-add-get-parameters-0.4/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-add-get-parameters',
+    version='0.4',
+    description='Template tag to add get parameter to the current url.',
+    long_description='Template tag to add get parameter to the current url.',
+    author='Robin Ramael',
+    author_email='robin.ramael@gmail.com',
+    url='http://github.com/RobinRamael/django-add-get-parameters',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+)
