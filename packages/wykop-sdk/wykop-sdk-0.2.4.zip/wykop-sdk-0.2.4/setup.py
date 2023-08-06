35dd8637602f6f206da15610b60ea9e73981a56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:58 +0000

    Add package wykop-sdk/0.2.4/wykop-sdk-0.2.4.zip

---
+++ packages/wykop-sdk/wykop-sdk-0.2.4.zip/wykop-sdk-0.2.4/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+'''
+Created on 18-12-2012
+
+@author: maciag.artur
+'''
+from setuptools import setup, find_packages
+
+setup(
+    name='wykop-sdk',
+    version='0.2.4',
+    description='Client library for Wykop API',
+
+    long_description=open("README.rst").read(),
+    author='Artur Maciag',
+    author_email='maciag.artur@gmail.com',
+    url='https://github.com/p1c2u/wykop-sdk',
+    license='BSD',
+    py_modules=[
+        'wykop',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
