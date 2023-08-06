180b2c219f84054c3107b7f270dbdc46c6fd3b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:52 +0000

    Add package lisa-server/0.2.0.3/lisa-server-0.2.0.3.tar.gz

---
+++ packages/lisa-server/lisa-server-0.2.0.3.tar.gz/lisa-server-0.2.0.3/lisa/server/web/frontend/build/src/app/chat/chat.js
@@ -0,0 +1,195 @@
+/**
+ * Each section of the site has its own module. It probably also has
+ * submodules, though this boilerplate is too simple to demonstrate it. Within
+ * `src/app/home`, however, could exist several additional folders representing
+ * additional modules that would then be listed as dependencies of this one.
+ * For example, a `note` section could have the submodules `note.create`,
+ * `note.delete`, `note.edit`, etc.
+ *
+ * Regardless, so long as dependencies are managed correctly, the build process
+ * will automatically take take of the rest.
+ *
+ * The dependencies block here is also where component dependencies should be
+ * specified, as shown below.
+ */
+angular.module( 'lisa-frontend.chat', [
+  'ui.router',
+  'gettext',
+  'restangular',
+  'ConfigurationManager',
+  'luegg.directives',
+  'bd.sockjs'
+])
+
+/**
+ * Each section or module of the site can also have its own routes. AngularJS
+ * will handle ensuring they are all available at run-time, but splitting it
+ * this way makes each module more "self-contained".
+ */
+.config(function config( $stateProvider, gettext ) {
+})
+
+.directive('lisaChat', function() {
+    return {
+        restrict: 'EAC',
+        templateUrl: 'chat/chat.tpl.html'
+    };
+})
+
+.directive('microphone', function() {
+    return {
+        restrict: 'EAC',
+        templateUrl: 'chat/microphone.tpl.html'
+    };
+})
+
+.factory('$lisaSocket', function ($location, socketFactory) {
+    var $port = '';
+    if($location.port()){
+        $port = ':' + $location.port();
+    }
+
+    return socketFactory({
+      url: 'http://' + $location.host() + $port + '/websocket'
+    });
+})
+
+.controller( 'MicrophoneCtrl', function MicrophoneController( $rootScope, $scope, $Configuration, $lisaSocket ) {
+        if ($rootScope.mic === undefined) {
+            $rootScope.mic = new Wit.Microphone(document.getElementById("microphone"));
+        }
+
+        var debug = false;
+
+        if(debug) {
+            $rootScope.mic.onready = function () {
+                console.log("Microphone is ready to record");
+            };
+            $rootScope.mic.onaudiostart = function () {
+                console.log("Recording started");
+            };
+            $rootScope.mic.onaudioend = function () {
+                console.log("Recording stopped, processing started");
+            };
+            $rootScope.mic.onerror = function (err) {
+                console.log("Error: " + err);
+            };
+            $rootScope.mic.onconnecting = function () {
+                console.log("Microphone is connecting");
+            };
+            $rootScope.mic.ondisconnected = function () {
+                console.log("Microphone is not connected");
+            };
+        }
+
+        $rootScope.mic.onresult = function (intent, entities, response) {
+            $rootScope.chatMessages.push({'body': response.msg_body, 'class': 'message-me'});
+            $lisaSocket.send(angular.toJson({'body': response.msg_body, 'outcome': response.outcome, 'type': 'chat', 'from': 'Lisa-Web', 'zone': 'WebSocket'}));
+
+            var r = kv("intent", intent);
+
+            for (var k in entities) {
+                var e = entities[k];
+
+                if (!(e instanceof Array)) {
+                    r += kv(k, e.value);
+                } else {
+                    for (var i = 0; i < e.length; i++) {
+                        r += kv(k, e[i].value);
+                    }
+                }
+            }
+        };
+
+        $rootScope.mic.connect($Configuration.configuration.wit_client_token);
+
+        function kv(k, v) {
+            if (toString.call(v) !== "[object String]") {
+                v = JSON.stringify(v);
+            }
+            return k + "=" + v + "\n";
+        }
+    })
+
+
+/**
+ * And of course we define a controller for our route.
+ */
+.controller( 'ChatCtrl', function ChatController( $rootScope, $location, $scope, Restangular, $Configuration, $lisaSocket ) {
+    var source;
+
+    try {
+        // Fix up for prefixing
+        window.AudioContext = window.AudioContext||window.webkitAudioContext;
+        $rootScope.audioContext = new AudioContext();
+    } catch(e) {
+        alert('Web Audio API is not supported in this browser');
+    }
+
+    var analyser = $rootScope.audioContext.createAnalyser();
+    var canvas=document.getElementById('lisa-canvas');
+    var canvasContext=canvas.getContext('2d');
+    var isFinished = false;
+
+    var playSound = function(buffer) {
+        // TODO Implement a queue to avoid playing multiple sound in the same time
+        source = $rootScope.audioContext.createBufferSource();
+        source.buffer = buffer;
+        source.connect(analyser);
+        analyser.connect($rootScope.audioContext.destination);
+        source.onended = function() {
+            isFinished = true;
+        };
+        source.start(0);
+    };
+
+    var genSound = function(message) {
+        var data = new FormData();
+        data.append('message', message);
+        data.append('lang', $Configuration.configuration.lang);
+
+        Restangular
+            .all('lisa/tts-google/')
+            .withHttpConfig({responseType: 'arraybuffer'})
+            .post(angular.toJson({message: message, lang: $Configuration.configuration.lang}))
+            .then(function(response){
+                $rootScope.audioContext.decodeAudioData(response, function(buffer) {
+                    playSound(buffer);
+                });
+            }, function(response){
+                console.log('Problem with sound generation');
+            }
+        );
+    };
+
+    if ($rootScope.chatMessages === undefined) {
+        $rootScope.chatMessages = [];
+    }
+
+    $scope.messages = $rootScope.chatMessages;
+    $scope.sendMessage = function() {
+        if ($scope.messageText) {
+            //var text = '{"body": "' + $scope.messageText + '", "type": "chat", "from": "Lisa-Web", "zone": "WebSocket"}';
+            var text = $scope.messageText;
+            $lisaSocket.send(text);
+            $rootScope.chatMessages.push({'body':$scope.messageText, 'class': 'message-me'});
+            $scope.messageText = "";
+        }
+    };
+
+    $lisaSocket.setHandler('message', function(e) {
+        $scope.isopen = true;
+        var oResponse = angular.fromJson(e.data);
+        if ($scope.sound) {
+            genSound(oResponse.body);
+        }
+        $rootScope.chatMessages.push({'body':oResponse.body, 'class': 'message-lisa'});
+        $scope.$apply();
+    });
+
+    $scope.isopen = false;
+    $scope.sound = true;
+
+
+})
+;
---
+++ packages/lisa-server/lisa-server-0.2.0.3.tar.gz/lisa-server-0.2.0.3/setup.py
@@ -0,0 +1,77 @@
+from setuptools import setup
+import os
+
+VERSION = '0.2.0.3'
+
+# When pip installs anything from packages, py_modules, or ext_modules that
+# includes a twistd plugin (which are installed to twisted/plugins/),
+# setuptools/distribute writes a Package.egg-info/top_level.txt that includes
+# "twisted".  If you later uninstall Package with `pip uninstall Package`,
+# pip <1.2 removes all of twisted/ instead of just Package's twistd plugins.
+# See https://github.com/pypa/pip/issues/355 (now fixed)
+#
+# To work around this problem, we monkeypatch
+# setuptools.command.egg_info.write_toplevel_names to not write the line
+# "twisted".  This fixes the behavior of `pip uninstall Package`.  Note that
+# even with this workaround, `pip uninstall Package` still correctly uninstalls
+# Package's twistd plugins from twisted/plugins/, since pip also uses
+# Package.egg-info/installed-files.txt to determine what to uninstall,
+# and the paths to the plugin files are indeed listed in installed-files.txt.
+try:
+    from setuptools.command import egg_info
+    egg_info.write_toplevel_names
+except (ImportError, AttributeError):
+    pass
+else:
+    def _top_level_package(name):
+        return name.split('.', 1)[0]
+
+    def _hacked_write_toplevel_names(cmd, basename, filename):
+        pkgs = dict.fromkeys(
+            [_top_level_package(k)
+                for k in cmd.distribution.iter_distribution_names()
+                if _top_level_package(k) != "twisted"
+            ]
+        )
+        cmd.write_file("top-level names", filename, '\n'.join(pkgs) + '\n')
+
+    egg_info.write_toplevel_names = _hacked_write_toplevel_names
+
+def listify(filename):
+    return [i for i in open(filename, 'r').read().strip('\n').split('\n') if 'git://' not in i]
+
+if __name__ == '__main__':
+    setup(
+        version=VERSION,
+        name='lisa-server',
+        packages=["lisa", "twisted.plugins"],
+        url='http://www.lisa-project.net',
+        license='MIT',
+        author='Julien Syx',
+        author_email='julien.syx@gmail.com',
+        description='LISA home automation system - Server',
+        include_package_data=True,
+        namespace_packages=['lisa'],
+        scripts = ['lisa/server/lisa-cli'],
+        install_requires=listify('requirements.txt'),
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Internet :: WWW/HTTP',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    )
+
+    from twisted.plugin import IPlugin, getPlugins
+    list(getPlugins(IPlugin))
+
+    try:
+        from django.core.management import call_command
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lisa.server.web.weblisa.settings")
+        call_command('collectstatic', interactive=False)
+    except:
+        pass
