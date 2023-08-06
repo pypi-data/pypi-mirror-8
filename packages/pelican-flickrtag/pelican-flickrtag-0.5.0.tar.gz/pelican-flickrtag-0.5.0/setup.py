6a785ea13c3a3a42dc407dbdc5f4b079132ee407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:30 +0000

    Add package pelican-flickrtag/0.5.0/pelican-flickrtag-0.5.0.tar.gz

---
+++ packages/pelican-flickrtag/pelican-flickrtag-0.5.0.tar.gz/pelican-flickrtag-0.5.0/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import pelican_flickrtag
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
+
+packages = [
+    'pelican_flickrtag',
+]
+
+requires = [
+    'jinja2',
+    'pelican',
+]
+
+setup(
+    name='pelican-flickrtag',
+    version=pelican_flickrtag.__version__,
+    description='Display Flickr images easily in your Pelican articles.',
+    long_description=readme,
+    author='Chris Streeter',
+    author_email='chris@chrisstreeter.com',
+    url='https://github.com/streeter/pelican-flickrtag',
+    packages=packages,
+    package_data={'': ['LICENSE', ]},
+    package_dir={'pelican_flickrtag': 'pelican_flickrtag'},
+    include_package_data=True,
+    install_requires=requires,
+    license='MIT',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+    ),
+)
