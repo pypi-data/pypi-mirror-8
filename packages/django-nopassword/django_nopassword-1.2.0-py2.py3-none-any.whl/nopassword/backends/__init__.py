3e21f3522a11729f8951fba8ad68646d0a06fb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:55 +0000

    Add package django-nopassword/1.2.0/django_nopassword-1.2.0-py2.py3-none-any.whl

---
+++ packages/django-nopassword/django_nopassword-1.2.0-py2.py3-none-any.whl/nopassword/backends/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from .email import EmailBackend
+from .sms import TwilioBackend
---
+++ packages/django-nopassword/django_nopassword-1.2.0-py2.py3-none-any.whl/tests/tests.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import django
+if django.VERSION < (1, 6):
+    from .test_backends import *
+    from .test_models import *
+    from .test_views import *
