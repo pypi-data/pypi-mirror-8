656e43bc53fced3a44c5679c387de51997b42701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package taxcalc/0.1/taxcalc-0.1.tar.gz

---
+++ packages/taxcalc/taxcalc-0.1.tar.gz/taxcalc-0.1/taxcalc/__init__.py
@@ -0,0 +1,8 @@
+from .calculate import *
+from .parameters import *
+from .puf import *
+from .utils import *
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
