8e1b8341bb4d06b8850868ef4dabced3a3f2dfb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:19 +0000

    Add package zope.publisher/4.0.0/zope.publisher-4.0.0.tar.gz

---
+++ packages/zope.publisher/zope.publisher-4.0.0.tar.gz/zope.publisher-4.0.0/setup.py
@@ -0,0 +1,94 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+import os
+from setuptools import setup, find_packages
+
+def alltests():
+    import sys
+    import unittest
+    # use the zope.testrunner machinery to find all the
+    # test suites we've put under ourselves
+    import zope.testrunner.find
+    import zope.testrunner.options
+    here = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+    args = sys.argv[:]
+    defaults = ["--test-path", here]
+    options = zope.testrunner.options.get_options(args, defaults)
+    suites = list(zope.testrunner.find.find_suites(options))
+    return unittest.TestSuite(suites)
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+entry_points = '''
+[paste.app_factory]
+main = zope.publisher.paste:Application
+
+[zope.publisher.publication_factory]
+sample = zope.publisher.tests.test_paste:SamplePublication
+'''
+
+setup(name='zope.publisher',
+      version='4.0.0',
+      url='http://pypi.python.org/pypi/zope.publisher',
+      license='ZPL 2.1',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='The Zope publisher publishes Python objects on the web.',
+      long_description=read('README.rst') + '\n\n' + read('CHANGES.rst'),
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        ],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope',],
+      install_requires=['setuptools',
+                        'six',
+                        'zope.browser',
+                        'zope.component',
+                        'zope.configuration',
+                        'zope.contenttype>=4.0.0',
+                        'zope.event',
+                        'zope.exceptions',
+                        'zope.i18n>=4.0.0',
+                        'zope.interface>=4.0.0',
+                        'zope.location',
+                        'zope.proxy',
+                        'zope.security>=4.0.0',
+                       ],
+      extras_require={'test': ['zope.testing']},
+      tests_require = ['zope.testing', 'zope.testrunner'],
+      test_suite = '__main__.alltests',
+      entry_points=entry_points,
+      include_package_data=True,
+      zip_safe=False,
+      )
