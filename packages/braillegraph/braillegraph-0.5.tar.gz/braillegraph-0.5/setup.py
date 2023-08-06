7062eeb2a9eff65b4076d6be630d1cf16ca7011a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:22 +0000

    Add package braillegraph/0.5/braillegraph-0.5.tar.gz

---
+++ packages/braillegraph/braillegraph-0.5.tar.gz/braillegraph-0.5/setup.py
@@ -0,0 +1,23 @@
+'''Install the braillegraph package.'''
+
+from setuptools import setup
+
+setup(name='braillegraph',
+      version='0.5',
+      description='A library for creating graphs using Unicode braille characters',
+      url='http://github.com/chrisbouchard/braillegraph',
+      author='Chris Bouchard',
+      author_email='chris@upliftinglemma.net',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Utilities'
+      ],
+      packages=['braillegraph'],
+      zip_safe=False)
+
