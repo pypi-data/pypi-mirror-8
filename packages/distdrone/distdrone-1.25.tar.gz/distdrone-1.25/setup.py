e61024e59413f580866fdf6baacb58efb74e6921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:18 +0000

    Add package distdrone/1.25/distdrone-1.25.tar.gz

---
+++ packages/distdrone/distdrone-1.25.tar.gz/distdrone-1.25/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+	with open("README.txt") as f:
+		return f.read()
+
+setup(name="distdrone",
+	version="1.25",
+	description="package to drive parallel drone swarm",
+	url="https://github.com/isaacrob/distdrone",
+	author="Isaac Robinson",
+	author_email="isaacrob@me.com",
+	license="MIT",
+	packages=["distdrone","distdrone.motion","distdrone.imgproc"],
+	scripts=["bin/trigger","bin/testpredictor","bin/centersearch","bin/installdronedeps"],
+	keywords=['drone','OpenCV','IPython','parallel'],
+	zip_safe=False)
