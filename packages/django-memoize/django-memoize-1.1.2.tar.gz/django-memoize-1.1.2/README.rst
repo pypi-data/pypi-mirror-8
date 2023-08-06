14fe3ffeed4360728bbeb323536cd1e797d5de1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:10 +0000

    Add package django-memoize/1.1.2/django-memoize-1.1.2.tar.gz

---
+++ packages/django-memoize/django-memoize-1.1.2.tar.gz/django-memoize-1.1.2/README.rst
@@ -0,0 +1,22 @@
+django-memoize
+==============
+
+.. image:: https://travis-ci.org/tvavrys/django-memoize.svg?branch=master
+    :target: https://travis-ci.org/tvavrys/django-memoize
+.. image:: https://pypip.in/d/django-memoize/badge.svg
+    :target: https://pypi.python.org/pypi/django-memoize
+|
+**django-memoize** is an implementation
+of `memoization <http://en.wikipedia.org/wiki/Memoization>`_ technique
+for Django. You can think of it as a cache for function or method results.
+
+
+Documentation
+-------------
+
+Documentation is available at http://pythonhosted.org/django-memoize/.
+
+Requirements
+------------
+
+Django >= 1.4 is required.
---
+++ packages/django-memoize/django-memoize-1.1.2.tar.gz/django-memoize-1.1.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+django-memoize
+--------------
+
+**django-memoize** is an implementation
+of `memoization <http://en.wikipedia.org/wiki/Memoization>`_ technique
+for Django. You can think of it as a cache for function or method results.
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-memoize',
+    version='1.1.2',
+    url='https://github.com/tvavrys/django-memoize',
+    license='BSD',
+    author='Thomas Vavrys',
+    author_email='tvavrys@sleio.com',
+    description='An implementation of memoization technique for Django.',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'django >= 1.4'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    tests_require=(
+        'django-setuptest',
+        'argparse',  # Required by django-setuptools on Python 2.6
+    ),
+)
