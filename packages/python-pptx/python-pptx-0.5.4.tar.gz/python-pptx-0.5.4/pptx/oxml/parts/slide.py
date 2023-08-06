8b55bd00b3189d3d1798ca8e00927ee8a6001250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:16 +0000

    Add package python-pptx/0.5.4/python-pptx-0.5.4.tar.gz

---
+++ packages/python-pptx/python-pptx-0.5.4.tar.gz/python-pptx-0.5.4/pptx/oxml/parts/slide.py
@@ -0,0 +1,59 @@
+# encoding: utf-8
+
+"""
+lxml custom element classes for slide master-related XML elements.
+"""
+
+from __future__ import absolute_import
+
+from .. import parse_xml
+from ..ns import nsdecls
+from ..simpletypes import XsdString
+from ..xmlchemy import (
+    BaseOxmlElement, OneAndOnlyOne, OptionalAttribute, ZeroOrOne
+)
+
+
+class CT_CommonSlideData(BaseOxmlElement):
+    """
+    ``<p:cSld>`` element.
+    """
+    spTree = OneAndOnlyOne('p:spTree')
+    name = OptionalAttribute('name', XsdString, default='')
+
+
+class CT_Slide(BaseOxmlElement):
+    """
+    ``<p:sld>`` element, root of a slide part
+    """
+    cSld = OneAndOnlyOne('p:cSld')
+    clrMapOvr = ZeroOrOne('p:clrMapOvr', successors=(
+        'p:transition', 'p:timing', 'p:extLst'
+    ))
+
+    @classmethod
+    def new(cls):
+        """
+        Return a new ``<p:sld>`` element configured as a base slide shape.
+        """
+        return parse_xml(cls._sld_xml())
+
+    @staticmethod
+    def _sld_xml():
+        return (
+            '<p:sld %s>\n'
+            '  <p:cSld>\n'
+            '    <p:spTree>\n'
+            '      <p:nvGrpSpPr>\n'
+            '        <p:cNvPr id="1" name=""/>\n'
+            '        <p:cNvGrpSpPr/>\n'
+            '        <p:nvPr/>\n'
+            '      </p:nvGrpSpPr>\n'
+            '      <p:grpSpPr/>\n'
+            '    </p:spTree>\n'
+            '  </p:cSld>\n'
+            '  <p:clrMapOvr>\n'
+            '    <a:masterClrMapping/>\n'
+            '  </p:clrMapOvr>\n'
+            '</p:sld>' % nsdecls('a', 'p', 'r')
+        )
