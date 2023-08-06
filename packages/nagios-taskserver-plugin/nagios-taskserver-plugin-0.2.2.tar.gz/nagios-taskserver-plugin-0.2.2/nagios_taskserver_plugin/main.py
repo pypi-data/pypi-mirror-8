e799bf1f11cea09e2317a734ae4870deee583aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:33 +0000

    Add package nagios-taskserver-plugin/0.2.2/nagios-taskserver-plugin-0.2.2.tar.gz

---
+++ packages/nagios-taskserver-plugin/nagios-taskserver-plugin-0.2.2.tar.gz/nagios-taskserver-plugin-0.2.2/nagios_taskserver_plugin/main.py
@@ -0,0 +1,21 @@
+import subprocess
+import time
+
+
+def main(*args):
+    config_path = args[0]
+
+    started = time.time()
+    subprocess.call(
+        [
+            'task',
+            'rc:%s' % config_path,
+            'sync',
+        ],
+        shell=True,
+    )
+    finished = time.time()
+
+    total_duration = finished - started
+
+    return "Sync Successful", "%s" % total_duration
