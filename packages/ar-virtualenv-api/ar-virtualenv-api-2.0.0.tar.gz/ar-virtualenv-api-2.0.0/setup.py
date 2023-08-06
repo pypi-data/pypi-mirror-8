98ce10c1ef3b78061c8925a62791337efa3eaf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:50 +0000

    Add package ar-virtualenv-api/2.0.0/ar-virtualenv-api-2.0.0.tar.gz

---
+++ packages/ar-virtualenv-api/ar-virtualenv-api-2.0.0.tar.gz/ar-virtualenv-api-2.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+import virtualenvapi
+
+version = virtualenvapi.__version__
+
+setup(
+    name='ar-virtualenv-api',
+    version=version,
+    license='BSD',
+    author='arteria GmbH',
+    author_email='admin@arteria.ch',
+    description='An API for virtualenv/pip',
+    url='https://github.com/arteria/ar-virtualenv-api',
+    packages=find_packages(),
+)
---
+++ packages/ar-virtualenv-api/ar-virtualenv-api-2.0.0.tar.gz/ar-virtualenv-api-2.0.0/virtualenvapi/exceptions.py
@@ -0,0 +1,10 @@
+class VirtualenvCreationException(EnvironmentError):
+    pass
+
+
+class PackageInstallationException(EnvironmentError):
+    pass
+
+
+class PackageRemovalException(EnvironmentError):
+    pass
