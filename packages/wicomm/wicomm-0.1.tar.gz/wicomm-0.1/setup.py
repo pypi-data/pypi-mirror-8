8e1758d93272be9e5980fe7a50b5cd58aa2b3816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package wicomm/0.1/wicomm-0.1.tar.gz

---
+++ packages/wicomm/wicomm-0.1.tar.gz/wicomm-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wicomm',
+      version='0.1',
+      description='Python package containing a wireless communication API.',
+      url='http://hardworkin-man.com:8080/scm/git/wicomm',
+      author='Mike Moore',
+      author_email='mickety.mike@gmail.com',
+      license='MIT',
+      packages=['wicomm'],
+      zip_safe=False)
