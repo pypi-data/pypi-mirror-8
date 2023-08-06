7c923c2b5e3b86ed2ac04dd7812861cca233f922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:25 +0000

    Add package pyramid_jwtauth/0.1.0/pyramid_jwtauth-0.1.0.tar.gz

---
+++ packages/pyramid_jwtauth/pyramid_jwtauth-0.1.0.tar.gz/pyramid_jwtauth-0.1.0/setup.py
@@ -0,0 +1,43 @@
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+# TODO: fix the imports.
+requires = ['pyramid >= 1.3', 'webtest', 'cryptography', 'PyJWT  == 0.4.0']
+
+setup(name='pyramid_jwtauth',
+      version='0.1.0',
+      description='pyramid_jwtauth',
+      long_description=README + '\n\n' + CHANGES,
+      license='MPLv2.0',
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Pyramid",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        ],
+      author='Alex Kavanagh (Websand)',
+      author_email='alex@ajkavanagh.co.uk',
+      url='https://github.com/ajkavanagh/pyramid_jwtauth',
+      keywords='authentication token JWT JSON',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="pyramid_jwtauth")
