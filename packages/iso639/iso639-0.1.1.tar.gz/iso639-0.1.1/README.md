97b620d5c52fbbf1e4e794edfcb0b2f983b01c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:50 +0000

    Add package iso639/0.1.1/iso639-0.1.1.tar.gz

---
+++ packages/iso639/iso639-0.1.1.tar.gz/iso639-0.1.1/README.md
@@ -0,0 +1,10 @@
+iso639
+======
+A simple library for working with ISO639-2 language codes.
+Should work in both Python 2 & 3.
+
+Includes data from Congress library: http://www.loc.gov/standards/iso639-2/php/code_list.php
+
+Alternatives
+------------
+* **pycountry**: https://bitbucket.org/flyingcircus/pycountry 
---
+++ packages/iso639/iso639-0.1.1.tar.gz/iso639-0.1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import itertools
+
+options = dict(
+    name='iso639',
+    version='0.1.1',
+    packages=find_packages(),
+    license='MIT',
+    description='ISO639-2 support for Python.',
+    long_description=open('README.md').read(),
+    package_data={'': ['iso639/ISO-639-2_utf-8.txt']},
+    include_package_data=True,
+    author='Jan Pipek',
+    author_email='jan.pipek@gmail.com',
+    url='https://github.com/janpipek/iso639-python',
+    install_requires = [],
+    extras_require = {}
+)
+
+extras = options['extras_require']
+extras['full'] = list(set(itertools.chain.from_iterable(extras.values())))
+setup(**options)
