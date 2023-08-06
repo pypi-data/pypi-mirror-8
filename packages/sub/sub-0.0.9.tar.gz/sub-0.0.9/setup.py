c8424ba0d66d061e811bf6205b231fc440d41da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:51 +0000

    Add package sub/0.0.9/sub-0.0.9.tar.gz

---
+++ packages/sub/sub-0.0.9.tar.gz/sub-0.0.9/setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def readFile(fName):
+    with open(fName) as f:
+        lines = f.read()
+    return lines
+
+setup(
+    name = 'sub',
+    version = '0.0.9',
+    author = 'Sartaj Singh',
+    author_email = 'singhsartaj94@gmail.com',
+    description = ('Simple Tool to download Subtitles.'),
+    long_description = readFile('README.md'),
+    license = 'MIT',
+    keywords = 'subtitles download movies tv shows',
+    url = 'http://github.com/leosartaj/sub',
+    packages=['sub'],
+    scripts=['bin/sub'],
+    install_requires = ['requests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
