6aedf22408f49de7c802dd7f911f32b3f6d6c557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:59 +0000

    Add package django-versionfield2/0.3.3/django-versionfield2-0.3.3.tar.gz

---
+++ packages/django-versionfield2/django-versionfield2-0.3.3.tar.gz/django-versionfield2-0.3.3/versionfield/forms.py
@@ -0,0 +1,31 @@
+"""Custom form fields."""
+
+from django import forms
+
+from .version import Version
+from .constants import DEFAULT_NUMBER_BITS
+from .utils import convert_version_int_to_string
+
+
+class VersionField(forms.IntegerField):
+
+    """A form field dedicated to version numbers."""
+
+    def __init__(self, number_bits=DEFAULT_NUMBER_BITS, **kwargs):
+        self.number_bits = number_bits
+        return super(VersionField, self).__init__(**kwargs)
+
+    def to_python(self, value):
+        """
+        Verifies that value can be converted to a Version object
+        """
+        if not value:
+            return None
+
+        if isinstance(value, basestring):
+            return Version(value, self.number_bits)
+
+        return Version(
+            convert_version_int_to_string(value, self.number_bits),
+            self.number_bits
+        )
