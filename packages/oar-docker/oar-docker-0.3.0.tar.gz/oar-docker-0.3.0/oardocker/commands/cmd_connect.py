3a58d2d71ff3717b1b7ce95f55c241078a204b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:10 +0000

    Add package oar-docker/0.3.0/oar-docker-0.3.0.tar.gz

---
+++ packages/oar-docker/oar-docker-0.3.0.tar.gz/oar-docker-0.3.0/oardocker/commands/cmd_connect.py
@@ -0,0 +1,15 @@
+import click
+from oardocker.cli import pass_context, pass_state
+from oardocker.actions import execute
+
+
+@click.command('connect')
+@click.option('-l', '--user', default="docker")
+@click.option('-s', '--shell', default="bash")
+@click.argument('hostname', required=False, default="frontend")
+@pass_state
+@pass_context
+def cli(ctx, state, user, shell, hostname):
+    """Connect to a node."""
+    cmd = ["cat /etc/motd", "&&", shell]
+    execute(ctx, state, user, hostname, cmd)
