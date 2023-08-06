47279d129f729a580742ff4a64e8e9f72b29f2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:22 +0000

    Add package pyexperian/0.9/pyexperian-0.9-py2-none-any.whl

---
+++ packages/pyexperian/pyexperian-0.9-py2-none-any.whl/pyexperian/exceptions.py
@@ -0,0 +1,31 @@
+
+
+class InvalidNetConnectUrlException(Exception):
+    def __init__(self):
+        super(InvalidNetConnectUrlException, self).__init__("Net Connect URL is invalid.")
+
+
+class EcalsLookupException(Exception):
+    def __init__(self):
+        super(EcalsLookupException, self).__init__("Failed to retrieve Net Connect URL via ECALS lookup.")
+
+
+class MaxAuthAttemptsException(Exception):
+    def __init__(self):
+        super(MaxAuthAttemptsException, self).__init__("You've maxed out your authentication attempts.")
+
+
+class FailedAuthException(Exception):
+    def __init__(self):
+        super(FailedAuthException, self).__init__("Bad credentials.  Did you remember to reset your password?")
+
+
+class BadRequestException(Exception):
+    def __init__(self):
+        super(BadRequestException, self).__init__("Check your config and query data for bad or missing values.")
+
+
+class TermsException(Exception):
+    def __init__(self):
+        super(TermsException, self).__init__("Re-submit your request if you have consent to request the customer's personal credit.")
+
