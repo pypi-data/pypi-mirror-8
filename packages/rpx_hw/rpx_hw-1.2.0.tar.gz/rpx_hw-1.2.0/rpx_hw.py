8a05915c21bbedc9690394c41b36b6d046d37e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:02 +0000

    Add package rpx_hw/1.2.0/rpx_hw-1.2.0.tar.gz

---
+++ packages/rpx_hw/rpx_hw-1.2.0.tar.gz/rpx_hw-1.2.0/rpx_hw.py
@@ -0,0 +1,8 @@
+def print_list(the_list,level=0):
+		for each_item in the_list:
+				if isinstance(each_item,list):
+						print_list(each_item,level+1)
+				else:
+					for tab_stop in range(level):
+						print"\t",
+					print(each_item)
