80b50b30d41b78e25c1247209a2e67c16f237fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:43 +0000

    Add package django-slim/0.7.5/django-slim-0.7.5.tar.gz

---
+++ packages/django-slim/django-slim-0.7.5.tar.gz/django-slim-0.7.5/src/slim/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'slim'
+__version__ = '0.7.5'
+__build__ = 0x000014
+__author__ = 'Artur Barseghyan'
+__copyright__ = 'Copyright (c) 2013-2014 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+
+from slim.helpers import *
+from slim.models import *
+from slim.models.fields import *
