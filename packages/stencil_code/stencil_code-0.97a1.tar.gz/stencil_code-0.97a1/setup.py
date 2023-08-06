2edc725e256338f45b37b2fe76584b56db1ae88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:25 +0000

    Add package stencil_code/0.97a1/stencil_code-0.97a1.tar.gz

---
+++ packages/stencil_code/stencil_code-0.97a1.tar.gz/stencil_code-0.97a1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='stencil_code',
+    description='A specializer built on the ASP SEJITS framework',
+    url='https://github.com/ucb-sejits/stencil_code/',
+    version='0.97a1',
+
+    packages=[
+        'stencil_code',
+        'stencil_code.backend',
+    ],
+
+    package_data={
+        'stencil_code': ['defaults.cfg'],
+    },
+
+    install_requires=[
+        'ctree',
+        'numpy',
+        'hindemith',
+        'pycl'
+    ]
+)
