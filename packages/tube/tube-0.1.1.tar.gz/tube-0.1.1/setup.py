ea90628f1f97680fa6976e99e91f519f785e3d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:33 +0000

    Add package tube/0.1.1/tube-0.1.1.tar.gz

---
+++ packages/tube/tube-0.1.1.tar.gz/tube-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='tube',
+    version='0.1.1',
+    author='Adam Gilman',
+    author_email='me@adamgilman.com',
+    packages=['tube', 'tube'],
+    scripts=[''],
+    url='http://pypi.python.org/pypi/tube/',
+    license='LICENSE.txt',
+    description='Python object wrapper for TfL (Transport for London) TrackerNet information service',
+    install_requires=[
+        "requests==2.5.0",
+        "xmltodict==0.9.0",
+    ],
+)
