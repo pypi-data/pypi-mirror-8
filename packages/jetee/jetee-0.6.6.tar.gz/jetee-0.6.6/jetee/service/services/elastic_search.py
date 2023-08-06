b79929d5e7b42d2dfbd95acd17c45e29120daa16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:15 +0000

    Add package jetee/0.6.6/jetee-0.6.6.tar.gz

---
+++ packages/jetee/jetee-0.6.6.tar.gz/jetee-0.6.6/jetee/service/services/elastic_search.py
@@ -0,0 +1,19 @@
+from jetee.base.service import DockerServiceAbstract, PortsMapping
+from jetee.common.config_factories.service.docker import AnsibleDockerContainerTaskConfigFactory
+
+
+class ElasticSearchService(DockerServiceAbstract):
+    """
+    ElasticSearch service
+    """
+    _config_factories_list = (
+        AnsibleDockerContainerTaskConfigFactory,
+    )
+
+    image = u'dockerfile/elasticsearch'
+    ports_mappings = [
+        PortsMapping(
+            interface=u'172.17.42.1',
+            internal_port=9200
+        )
+    ]
