41f45490363d1e8ceb8a8264934b867e3eaa9a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:42 +0000

    Add package rmoq/0.2.1/rmoq-0.2.1.tar.gz

---
+++ packages/rmoq/rmoq-0.2.1.tar.gz/rmoq-0.2.1/setup.py
@@ -0,0 +1,26 @@
+import codecs
+from os import path
+from setuptools import setup
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+setup(
+    name='rmoq',
+    version='0.2.1',
+    url='http://github.com/relekang/rmoq',
+    author='Rolf Erik Lekang',
+    author_email='me@rolflekang.com',
+    description='A simple request-mocker that will download',
+    long_description=read('README.rst'),
+    py_modules=['rmoq'],
+    license='MIT',
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
