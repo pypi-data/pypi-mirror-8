87833a7c6403480bfcb63302fdda6cd29584b4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:24 +0000

    Add package getconf/1.2.1/getconf-1.2.1.tar.gz

---
+++ packages/getconf/getconf-1.2.1.tar.gz/getconf-1.2.1/docs/index.rst
@@ -0,0 +1,23 @@
+.. include:: ../README.rst
+
+
+Contents:
+---------
+
+.. toctree::
+   :maxdepth: 2
+
+   self
+   reference
+   advanced
+   changelog
+
+
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ packages/getconf/getconf-1.2.1.tar.gz/getconf-1.2.1/getconf/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011-2014 Polyconseil SAS.
+# This code is distributed under the two-clause BSD License.
+
+
+__author__ = "Polyconseil <opensource+getconf@polyconseil.fr>"
+__version__ = '1.2.1'
+
+
+from .base import ConfigGetter
