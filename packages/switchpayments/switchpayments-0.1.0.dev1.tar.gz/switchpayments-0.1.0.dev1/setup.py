4e14099997fa9457709de4957c6501e082a2ebd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:53 +0000

    Add package switchpayments/0.1.0.dev1/switchpayments-0.1.0.dev1.tar.gz

---
+++ packages/switchpayments/switchpayments-0.1.0.dev1.tar.gz/switchpayments-0.1.0.dev1/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='switchpayments',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
+    version='0.1.0.dev1',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+
+    description='SwitchPayments',
+    long_description=long_description,
+
+    # Author details
+    author='The Switch. Team',
+    author_email='info@switchpayments.com',
+
+    # Choose your license
+    license='MIT',
+    url='https://pypi.python.org/pypi/test_pypi',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    # What does your project relate to?
+    keywords='payment payments',
+
+
+)
