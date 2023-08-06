aee60540eac9e405c83f6199bbedc95712f7b702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:15 +0000

    Add package prorab/0.1/prorab-0.1.tar.gz

---
+++ packages/prorab/prorab-0.1.tar.gz/prorab-0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='prorab',
+      version='0.1',
+      description='no description',
+      author='Vasiliy Horbachenko',
+      author_email='shadow.prince@ya.ru',
+      url='',
+      scripts=['bin/prorab', 'bin/prorabd', ],
+      packages=['prorab', ],
+     )
