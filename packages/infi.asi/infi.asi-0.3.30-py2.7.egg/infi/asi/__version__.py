753829d1d186e6184a51f18edd630680ecfc796e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:50 +0000

    Add package infi.asi/0.3.30/infi.asi-0.3.30-py2.7.egg

---
+++ packages/infi.asi/infi.asi-0.3.30-py2.7.egg/infi/asi/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.30"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.3.30)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ee3922ce2955e206dbca112246a76eae30b50c70'
+__git_head_subject__ = 'Finished release v0.3.30.'
+__git_head_message__ = '* release/v0.3.30:\n  HOSTDEV-1812 implement asi for solaris\n  buildout.cfg: changed isolated python version to v2.7.6.21'
+__git_dirty_diff__ = ''
