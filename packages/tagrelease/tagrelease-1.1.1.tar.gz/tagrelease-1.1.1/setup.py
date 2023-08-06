90d546dbb4b6d21b299500a4eca030fb7a00ebe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:15 +0000

    Add package tagrelease/1.1.1/tagrelease-1.1.1.tar.gz

---
+++ packages/tagrelease/tagrelease-1.1.1.tar.gz/tagrelease-1.1.1/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+README = open(os.path.join(here, 'README.org')).read()
+
+setup(name='tagrelease',
+      version='1.1.1',
+      long_description=README,
+      classifiers=[
+	  "Programming Language :: Python",
+      ],
+      install_requires = ['GitPython', 'pyyaml', 'argdeclare'],
+      author='Parnell Springmeyer',
+      author_email='parnell@plumlife.com',
+      keywords='git release tagging script plum',
+      scripts=['tagrelease'],
+      url="https://github.com/plumlife/tagrelease",
+      zip_safe=False
+      )
