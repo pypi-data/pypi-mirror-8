e6b72f6869279fb658dcac8c168e7f6f761b032e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:29 +0000

    Add package djangobars/0.1.5/djangobars-0.1.5.tar.gz

---
+++ packages/djangobars/djangobars-0.1.5.tar.gz/djangobars-0.1.5/djangobars/template/base.py
@@ -0,0 +1,41 @@
+import pybars
+from .helpers import _djangobars_
+
+try:
+    strtype = unicode
+except NameError:
+    strtype = str
+
+
+class HandlebarsTemplate (object):
+    PARTIALS = {}
+
+    def __init__(self, template_string, origin=None,
+                 name='<Handlebars Template>',
+                 partials=PARTIALS, is_partial=True):
+        c = pybars.Compiler()
+        self.fn = c.compile(template_string)
+        self.name = name
+        self.helpers = _djangobars_['helpers'].copy()
+        self.partials = partials
+
+        if is_partial:
+            self.partials[name] = self.fn
+
+    def render(self, context):
+        if hasattr(context, 'render_context'):
+            context.render_context.push()
+
+        try:
+            s = self.fn(
+                context, helpers=self.helpers, partials=self.partials)
+            return strtype(s)
+        except KeyError as e:
+            from djangobars.template.loader import get_template
+            partial_name = strtype(e).strip("'")
+            template = get_template(partial_name)
+            self.partials[partial_name] = template.fn
+            return self.render(context)
+        finally:
+            if hasattr(context, 'render_context'):
+                context.render_context.pop()
