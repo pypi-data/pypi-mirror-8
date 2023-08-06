bc49c6f43edeb90274df84b08c7a164ffe329157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:04 +0000

    Add package django-fsm-log/1.0.1/django-fsm-log-1.0.1.tar.gz

---
+++ packages/django-fsm-log/django-fsm-log-1.0.1.tar.gz/django-fsm-log-1.0.1/django_fsm_log/models.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals
+
+from django_fsm_log.conf import settings
+from django.contrib.contenttypes.generic import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.utils.timezone import now
+
+from django_fsm.signals import pre_transition, post_transition
+
+from .managers import StateLogManager
+from django.utils.module_loading import import_by_path
+
+
+class StateLog(models.Model):
+    timestamp = models.DateTimeField(default=now)
+    by = models.ForeignKey(getattr(settings, 'AUTH_USER_MODEL', 'auth.User'), blank=True, null=True)
+    state = models.CharField(max_length=255, db_index=True)
+    transition = models.CharField(max_length=255)
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField(db_index=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    objects = StateLogManager()
+
+    def __unicode__(self):
+        return '{} - {} - {}'.format(
+            self.timestamp,
+            self.content_object,
+            self.transition
+        )
+
+try:
+    import django.apps
+except: # django < 1.7
+    backend = import_by_path(settings.DJANGO_FSM_LOG_STORAGE_METHOD)
+    backend.setup_model(StateLog)
+
+    pre_transition.connect(backend.pre_transition_callback)
+    post_transition.connect(backend.post_transition_callback)
