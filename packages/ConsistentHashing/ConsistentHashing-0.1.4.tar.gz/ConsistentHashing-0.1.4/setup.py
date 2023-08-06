898ea94f4d1f3b8ca280312faa39ca32eb573591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:35 +0000

    Add package ConsistentHashing/0.1.4/ConsistentHashing-0.1.4.tar.gz

---
+++ packages/ConsistentHashing/ConsistentHashing-0.1.4.tar.gz/ConsistentHashing-0.1.4/setup.py
@@ -0,0 +1,23 @@
+import os
+
+from setuptools import setup
+
+setup(name='ConsistentHashing',
+      version = '0.1.4',
+      author="Ziang Guo",
+      author_email="iziang@yeah.net",
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      py_modules=['consistenthashing'],
+      platforms=["Any"],
+      license="BSD",
+      keywords='hashing hash consistent distributed',
+      description="A simple implement of consistent hashing.",
+      long_description="A simple implement of consistent hashing."
+)
