a6ac45d61dd71fd88e0d97ecba6213b2d9e552ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:58 +0000

    Add package feedme/1.4.0/feedme-1.4.0.tar.gz

---
+++ packages/feedme/feedme-1.4.0.tar.gz/feedme-1.4.0/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='feedme',
+    version='1.4.0',
+    author='dotkom',
+    author_email='dotkom@online.ntnu.no',
+    packages=['feedme'],
+    license='BSD License',  # example license
+    description='Food ordering management for django',
+    long_description=README,
+    url='http://online.ntnu.no/feedme',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    include_package_data=True,
+)
