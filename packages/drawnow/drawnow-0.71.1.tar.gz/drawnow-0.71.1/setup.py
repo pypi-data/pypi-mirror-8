2136e37e7241512f12fe85c1d919538f20ce3fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:25 +0000

    Add package drawnow/0.71.1/drawnow-0.71.1.tar.gz

---
+++ packages/drawnow/drawnow-0.71.1.tar.gz/drawnow-0.71.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+  name         = 'drawnow',
+  packages     = ['drawnow'],
+  version      = '0.71.1',
+  description  = 'MATLAB-like drawnow',
+  author       = 'Scott Sievert',
+  author_email = 'sieve121@umn.edu',
+  url          = 'https://github.com/scottsievert/python-drawnow',
+  download_url = 'https://github.com/scottsievert/python-drawnow/archive/master.zip',
+  keywords     = ['figure', 'plotting', 'visualization', 'matlab'],
+  use_2to3     = True,
+  requires     = ['matplotlib'],
+  classifiers  = [
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Scientific/Engineering",
+          "Topic :: Scientific/Engineering :: Visualization"
+      ],
+)
