24b2026e74c87222f8660d067de2aa3c5be17780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:12 +0000

    Add package swapi/0.1.1/swapi-0.1.1.tar.gz

---
+++ packages/swapi/swapi-0.1.1.tar.gz/swapi-0.1.1/swapi/settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''
+Settings and stuff.
+'''
+
+
+BASE_URL = 'http://swapi.co/api'
+
+PEOPLE = 'people'
+PLANETS = 'planets'
+STARSHIPS = 'starships'
+VEHICLES = 'vehicles'
+FILMS = 'films'
+SPECIES = 'species'
