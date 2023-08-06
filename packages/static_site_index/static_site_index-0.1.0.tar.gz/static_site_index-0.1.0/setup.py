a80fa0191c395d029bc1f6c6377243b2c422c47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:46 +0000

    Add package static_site_index/0.1.0/static_site_index-0.1.0.tar.gz

---
+++ packages/static_site_index/static_site_index-0.1.0.tar.gz/static_site_index-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='static_site_index',
+      description='builds directory index pages for static websites',
+      version='0.1.0',
+      packages=['static_site_index',
+                ],
+      url='https://github.com/astraw/static_site_index',
+      author='Andrew Straw',
+      author_email='strawman@astraw.com',
+      package_data={ 'static_site_index': ['*.js','*.html'],
+                     },
+      )
