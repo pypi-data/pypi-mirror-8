4fb3ad9cb0ea759944fa98faefdb1016d4a18eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:09 +0000

    Add package kafka-quixey/0.9.0-q3/kafka-quixey-0.9.0-q3.tar.gz

---
+++ packages/kafka-quixey/kafka-quixey-0.9.0-q3.tar.gz/kafka-quixey-0.9.0-q3/setup.py
@@ -0,0 +1,43 @@
+import sys
+
+from setuptools import setup, Command
+
+
+class Tox(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import tox
+        sys.exit(tox.cmdline([]))
+
+
+setup(
+    name="kafka-quixey",
+    version="0.9.0-q3",
+
+    tests_require=["tox", "mock"],
+    cmdclass={"test": Tox},
+
+    packages=["kafka"],
+
+    author="David Arthur, Jim Lim",
+    author_email="mumrah@gmail.com, jim@quixey.com",
+    url="https://github.com/quixey/kafka-python",
+    license="Copyright 2012, David Arthur under Apache License, v2.0",
+    description="Pure Python client for Apache Kafka",
+    long_description="""
+I (Jim Lim) am releasing this to pypi under quixey for personal convenience.
+
+This module provides low-level protocol support for Apache Kafka as well as
+high-level consumer and producer classes. Request batching is supported by the
+protocol as well as broker-aware request routing. Gzip and Snappy compression
+is also supported for message sets.
+"""
+)
