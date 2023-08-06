0a51497b93845c1eaf3a5c6aaea666cc5857c270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:06 +0000

    Add package jetee/0.6.5/jetee-0.6.5.tar.gz

---
+++ packages/jetee/jetee-0.6.5.tar.gz/jetee-0.6.5/jetee/processes/__init__.py
@@ -0,0 +1,28 @@
+from jetee.base.process import ProcessAbstract
+
+
+class CustomProcess(ProcessAbstract):
+    """
+    Custom python process
+
+    Be sure to override :attr:`command` and :attr:`process_name` attributes
+    """
+    command = None
+    name = None
+
+    def __init__(self, command, name, env_variables=None):
+        self.command = command
+        self.name = name
+        self.env_variables = env_variables or None
+
+    def get_command(self):
+        return self.command
+
+    def get_name(self):
+        return self.name
+
+
+from jetee.processes.celery import *
+from jetee.processes.cron import *
+from jetee.processes.django import *
+from jetee.processes.uwsgi import *
---
+++ packages/jetee/jetee-0.6.5.tar.gz/jetee-0.6.5/jetee/service/services/postgresql.py
@@ -0,0 +1,23 @@
+from jetee.base.service import DockerServiceAbstract, PortsMapping
+from jetee.common.config_factories.service.docker import AnsibleDockerContainerTaskConfigFactory
+
+
+class PostgreSQLService(DockerServiceAbstract):
+    """
+    Postgresql service
+    | Database name: docker
+    | Username: docker
+    | Password: docker
+    """
+    _config_factories_list = (
+        AnsibleDockerContainerTaskConfigFactory,
+    )
+
+    image = u'zumbrunnen/postgresql'
+    command = u'/usr/bin/supervisord'
+    ports_mappings = [
+        PortsMapping(
+            interface=u'172.17.42.1',
+            internal_port=5432
+        )
+    ]
