ef525e64a84e325cebb5ed05d8f4572ba79c534c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:00 +0000

    Add package ritzfizz/0.8/ritzfizz-0.8.zip

---
+++ packages/ritzfizz/ritzfizz-0.8.zip/ritzfizz-0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'ritzfizz',
+  packages = ['ritzfizz'],
+  version = '0.8',
+  description = 'Ritzfizz Sphinx Theme',
+  author = 'Adam Temple',
+  author_email = 'adam@bixly.com',
+  url = 'http://xepler.com',
+  keywords = ['sphinx', 'theme', 'readthedocs', 'ritzfizz', 'xepler'],
+  classifiers = [],
+  include_package_data=True,
+)
