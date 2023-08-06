ce07086944c6b696b2e0d09fc551e1494d91568e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:46 +0000

    Add package nozdormu/0.2/nozdormu-0.2.tar.gz

---
+++ packages/nozdormu/nozdormu-0.2.tar.gz/nozdormu-0.2/nozdormu/loader.py
@@ -0,0 +1,26 @@
+from nozdormu.batch import BenchBatch
+from nozdormu.suite import BenchSuite
+
+class BenchLoader:
+    """Load a batch of benchmarks and return them"""
+
+    def __init__(self, benchSuite=BenchSuite):
+        self.suiteClass = benchSuite
+
+    def loadFromModule(self, module):
+        """Get all batches from a module and all benchs from a batch"""
+        benchs = []
+        for name in dir(module):
+            obj = getattr(module, name)
+            if isinstance(obj, type) and issubclass(obj, BenchBatch):
+                benchs.append(self.loadFromBatch(obj))
+        return self.suiteClass(benchs)
+
+    def loadFromBatch(self, batch):
+        """Get all benchs from a batch and return a loaded suite"""
+        isbench = lambda name: name.startswith('bench')
+        benchNames = list(filter(isbench, dir(batch)))
+        suite = self.suiteClass(map(batch, benchNames))
+        suite.batch=batch
+        return suite
+
