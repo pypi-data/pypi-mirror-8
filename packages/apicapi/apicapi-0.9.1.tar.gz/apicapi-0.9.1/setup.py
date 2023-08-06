dffaf7f4724de1bb13a7cf3e6d557151723c9ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:58 +0000

    Add package apicapi/0.9.1/apicapi-0.9.1.tar.gz

---
+++ packages/apicapi/apicapi-0.9.1.tar.gz/apicapi-0.9.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name = "apicapi",
+    version = "0.9.1",
+    packages = ["apicapi"],
+    author = "Cisco Systems, Inc.",
+    author_email = "apicapi@noironetworks.com",
+    url = "http://github.com/noironetworks/apicapi/",
+    license = "http://www.apache.org/licenses/LICENSE-2.0",
+    description = "This library provides an interface to the APIC REST api.",
+)
