508adbf962ca3843b9c9781ce9e49737ce79fd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:12 +0000

    Add package Loslassa/0.3.9/Loslassa-0.3.9.tar.gz

---
+++ packages/Loslassa/Loslassa-0.3.9.tar.gz/Loslassa-0.3.9/setup.py
@@ -0,0 +1,50 @@
+"""
+
+.pypirc should look like this::
+
+    [server-login]
+    username:oliver.bestwalter
+    password:<wont tell ya>
+
+Release process:
+ * update __version__
+ * python ./setup.py sdist upload
+"""
+import os
+
+from setuptools import setup
+
+from loslassa import __version__
+
+HERE = os.path.dirname(__file__)
+with open(os.path.join(HERE, "requirements.txt")) as f:
+    requirements = f.readlines()
+
+setup(
+    name='Loslassa',
+    description='Just another toy static website generator',
+    version=__version__,
+    license='BSD',
+    url='http://github.com/obestwalter/loslassa/',
+    author='Oliver Bestwalter',
+    author_email='oliver@bestwalter.de',
+    long_description='',
+    packages=['loslassa'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='Unix',
+    install_requires=requirements,
+    entry_points=dict(console_scripts=['loslassa=loslassa._loslassa:main']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
