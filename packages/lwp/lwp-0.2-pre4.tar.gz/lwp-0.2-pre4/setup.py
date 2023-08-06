f74142032f990292c164e20848d68f674024216f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:42 +0000

    Add package lwp/0.2-pre4/lwp-0.2-pre4.tar.gz

---
+++ packages/lwp/lwp-0.2-pre4.tar.gz/lwp-0.2-pre4/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from __future__ import absolute_import, print_function
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+
+__version__ = '0.2-pre4'
+__author__ = 'Élie Deloumeau, Antoine Tanzilli'
+
+
+supports = {
+    'install_requires': [
+        'flask==0.9',
+        'arconfig',
+        'object_cacher',
+    ]
+}
+
+data_files = []
+if not os.path.exists('/var/lib/lxc/lwp.db'):
+    data_files.append(('/var/lib/lxc/', ['resources/lwp.db']))
+
+setup(
+    name='lwp',
+    version=__version__,
+    author=__author__,
+    license="MIT",
+    description="LXC Web Interface",
+    platforms="linux",
+    classifiers=[
+        'Environment :: Console',
+        'Programming Language :: Python',
+    ],
+    scripts=['bin/lwp'],
+    include_package_data=True,
+    zip_safe=False,
+    data_files=data_files,
+    packages=find_packages(),
+    **supports
+)
