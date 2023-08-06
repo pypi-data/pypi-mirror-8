4d2aced7a6c1fac8201e0281e179a643f06865b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:22 +0000

    Add package LogToPG/0.0.1/LogToPG-0.0.1.tar.gz

---
+++ packages/LogToPG/LogToPG-0.0.1.tar.gz/LogToPG-0.0.1/setup.py
@@ -0,0 +1,13 @@
+# vim: set expandtab ts=4 sw=4 filetype=python fileencoding=utf8:
+
+from setuptools import setup
+
+setup(
+    name="LogToPG",
+    version="0.0.1",
+    packages=["logtopg"],
+    author="216 Software, LLC",
+    author_email="info@216software.com",
+    license="BSD License",
+    include_package_data=True,
+)
