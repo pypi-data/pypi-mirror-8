a67dff9afcaa716314258914063725da75585bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:38 +0000

    Add package pypi_indra_test/1.1.0/pypi_indra_test-1.1.0.zip

---
+++ packages/pypi_indra_test/pypi_indra_test-1.1.0.zip/pypi_indra_test-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'pypi_indra_test',
+    version = '1.1.0',
+    py_modules = ['pypi_indra_test'],
+    author = 'indrachavan',
+    author_email= 'dontcare@dunno.com',
+    url = 'http://www.soddy.com',
+    description = 'Simple print shit',
+)
