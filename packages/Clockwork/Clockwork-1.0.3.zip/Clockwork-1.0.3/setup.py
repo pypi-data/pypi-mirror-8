1efe266a3f20bfa242f502f8412d42c055626eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:11 +0000

    Add package Clockwork/1.0.3/Clockwork-1.0.3.zip

---
+++ packages/Clockwork/Clockwork-1.0.3.zip/Clockwork-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Clockwork',
+    version='1.0.3',
+    packages=['clockwork'],
+    install_requires=['lxml'],
+    license='MIT',
+    author='Mediaburst',
+    author_email='hello@clockworksms.com',
+    long_description=open('README.md').read(),
+    description='Python wrapper for the clockwork SMS Api',
+    url='https://github.com/mediaburst/clockwork-python'
+)
