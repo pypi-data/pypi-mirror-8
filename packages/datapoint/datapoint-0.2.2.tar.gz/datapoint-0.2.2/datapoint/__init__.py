9506e6654020d615f9c016017dee628b025ee10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:36 +0000

    Add package datapoint/0.2.2/datapoint-0.2.2.tar.gz

---
+++ packages/datapoint/datapoint-0.2.2.tar.gz/datapoint-0.2.2/datapoint/__init__.py
@@ -0,0 +1,23 @@
+"""Datapoint API to retrieve Met Office data"""
+
+__version__ = "0.2.2"
+__author__ = "Jacob Tomlinson"
+__author_email__ = "jacob.tomlinson@metoffice.gov.uk"
+
+import os.path
+
+from .Manager import Manager
+import datapoint.profile
+
+
+def connection(profile_name='default', api_key=None):
+    """Connect to DataPoint with the given API key profile name."""
+    if api_key is None:
+        profile_fname = datapoint.profile.API_profile_fname(profile_name)
+        if not os.path.exists(profile_fname):
+            raise ValueError('Profile not found in {}. Please install your API \n'
+                             'key with datapoint.profile.install_API_key('
+                             '"<YOUR-KEY>")'.format(profile_fname))
+        with open(profile_fname) as fh:
+            api_key = fh.readlines()
+    return Manager(api_key=api_key)
