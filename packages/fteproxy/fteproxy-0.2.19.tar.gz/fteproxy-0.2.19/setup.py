d7ece11a470cd36aedf9b5c4c6a3a07aad18c6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:38 +0000

    Add package fteproxy/0.2.19/fteproxy-0.2.19.tar.gz

---
+++ packages/fteproxy/fteproxy-0.2.19.tar.gz/fteproxy-0.2.19/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import Extension
+
+import glob
+import sys
+import os
+if os.name == 'nt':
+    import py2exe
+
+with open('fteproxy/VERSION') as fh:
+    FTEPROXY_RELEASE = fh.read().strip()
+
+package_data_files = []
+package_data_files += ['VERSION']
+for filename in glob.glob('fteproxy/defs/*.json'):
+    jsonFile = filename.split('/')[-1]
+    package_data_files += ['defs/'+jsonFile]
+package_data = {'fteproxy': package_data_files}
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name='fteproxy',
+      long_description=long_description,
+      console=['./bin/fteproxy'],
+      test_suite='fteproxy.tests.suite',
+      zipfile="fteproxy.zip",
+      package_data=package_data,
+      options={"py2exe": {
+          "bundle_files": 2,
+          "compressed": True,
+          "dll_excludes": ["w9xpopen.exe"],
+      }
+      },
+      version=FTEPROXY_RELEASE,
+      description='fteproxy',
+      author='Kevin P. Dyer',
+      author_email='kpdyer@gmail.com',
+      url='https://fteproxy.org/',
+      packages=['fteproxy', 'fteproxy.defs', 'fteproxy.tests'],
+      install_requires=['fte','twisted','pyptlib','obfsproxy'],
+      entry_points = {
+        'console_scripts': [
+            'fteproxy = fteproxy.cli:main'
+            ]
+        },
+      )
