f2d26b006a301c819aad8e57a1ce94a935a2b7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:05 +0000

    Add package infi.os-info/0.1/infi.os_info-0.1-py2.7.egg

---
+++ packages/infi.os-info/infi.os_info-0.1-py2.7.egg/infi/os_info/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd834eca6ffd7c3ea7c7c657958ec9ae8cdfd9cdf'
+__git_head_subject__ = 'Finished release v0.1.'
+__git_head_message__ = '* release/v0.1:\n  HOSTDEV-1928 +g\n  HOSTDEV-1928 adjust the code to support pkg_resources.parse_version from setuptools 8\n  HOSTDEV-1928 adjust the code to support pkg_resources.parse_version from setuptools 8'
+__git_dirty_diff__ = ''
