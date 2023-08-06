d4f34208f2a9ec0d7df524f07cb4d8570ad08ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:42 +0000

    Add package bade/0.0.5/bade-0.0.5.tar.gz

---
+++ packages/bade/bade-0.0.5.tar.gz/bade-0.0.5/setup.py
@@ -0,0 +1,52 @@
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    'Hook into py.test to run the test suite'
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+def readme():
+    'Dump out the readme'
+    with open('README.rst') as readme_:
+        return readme_.read()
+
+setup(
+    name='bade',
+    version='0.0.5',
+    description='Micro-blogging with rST',
+    packages=find_packages(),
+    long_description=readme(),
+    url='http://bmcorser.github.com/bade',
+    author='bmcorser',
+    author_email='bmcorser@gmail.com',
+    install_requires=[
+        'click',
+        'docutils',
+        'mako',
+        'sass-cli',
+        'pyyaml',
+    ],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+    entry_points='''
+        [console_scripts]
+        bade=bade.cli:main
+    '''
+)
