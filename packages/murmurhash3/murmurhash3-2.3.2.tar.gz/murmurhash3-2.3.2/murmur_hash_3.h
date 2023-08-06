d688ebb1250ee184b613c49dead275b8a8f3a08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:48 +0000

    Add package murmurhash3/2.3.2/murmurhash3-2.3.2.tar.gz

---
+++ packages/murmurhash3/murmurhash3-2.3.2.tar.gz/murmurhash3-2.3.2/murmur_hash_3.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#if defined(_MSC_VER)
+typedef unsigned char uint8_t;
+typedef unsigned long uint32_t;
+typedef unsigned __int64 uint64_t;
+#else
+#include <stdint.h>
+#endif
+
+void MurmurHash3_x86_32(const void *key, int len, uint32_t seed, void *out);
+void MurmurHash3_x86_128(const void *key, int len, uint32_t seed, void *out);
+void MurmurHash3_x64_128(const void *key, int len, uint32_t seed, void *out);
