12d2ed2c876aa2488833a5702981e2f01af6f1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:49 +0000

    Add package pybsorts/0.1/pybsorts-0.1.tar.gz

---
+++ packages/pybsorts/pybsorts-0.1.tar.gz/pybsorts-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+	name='pybsorts',
+	version='0.1',
+	description='Basic sorting algorithmes for arrays',
+	author='Samy Melaine',
+	author_email='melaine.samy@gmail.com',
+	url='https://github.com/SamyMe/pybsorts/',
+	download_url='https://github.com/SamyMe/pybsorts/tarball/0.1',
+	keywords=['sort','array','basic','bubble','merge','quick','insertion','heap','semectop'],
+	classifiers=[ 
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python',
+	],
+)
