1b2872bf868e1e779c9e85ad1515134cab4a0afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:39 +0000

    Add package portier/0.1.0/portier-0.1.0.zip

---
+++ packages/portier/portier-0.1.0.zip/portier-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# :coding: utf-8
+
+import setuptools
+
+setuptools.setup(
+    name="portier",
+    version="0.1.0",
+    description=(
+        "A tool to check a host address "
+        "for open ports using multiple threads."
+    ),
+    long_description=open("README.rst").read(),
+    keywords="host, localhost, web, port, scanner, thread",
+    url="https://cb109@bitbucket.org/cb109/portier.git",
+    author="Christoph Buelter",
+    author_email="c.buelter@arcor.de",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "portier=portier:main",
+        ],
+    }
+)
+
