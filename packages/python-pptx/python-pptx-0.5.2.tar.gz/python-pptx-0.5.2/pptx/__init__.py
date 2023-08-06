3ac06a774f03252e0217631b6d5da4e1d99fd9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:41 +0000

    Add package python-pptx/0.5.2/python-pptx-0.5.2.tar.gz

---
+++ packages/python-pptx/python-pptx-0.5.2.tar.gz/python-pptx-0.5.2/pptx/__init__.py
@@ -0,0 +1,51 @@
+# encoding: utf-8
+
+"""
+Initialization module for python-pptx
+"""
+
+__version__ = '0.5.2'
+
+
+import pptx.exc as exceptions
+import sys
+sys.modules['pptx.exceptions'] = exceptions
+del sys
+
+from pptx.api import Presentation  # noqa
+
+from pptx.opc.constants import CONTENT_TYPE as CT
+from pptx.opc.package import PartFactory
+from pptx.parts.chart import ChartPart
+from pptx.parts.coreprops import CoreProperties
+from pptx.parts.image import Image
+from pptx.parts.presentation import PresentationPart
+from pptx.parts.slide import Slide
+from pptx.parts.slidelayout import SlideLayout
+from pptx.parts.slidemaster import SlideMaster
+
+content_type_to_part_class_map = {
+    CT.PML_PRESENTATION_MAIN: PresentationPart,
+    CT.PML_TEMPLATE_MAIN:     PresentationPart,
+    CT.PML_SLIDESHOW_MAIN:    PresentationPart,
+    CT.OPC_CORE_PROPERTIES:   CoreProperties,
+    CT.PML_SLIDE:             Slide,
+    CT.PML_SLIDE_LAYOUT:      SlideLayout,
+    CT.PML_SLIDE_MASTER:      SlideMaster,
+    CT.DML_CHART:             ChartPart,
+    CT.BMP:                   Image,
+    CT.GIF:                   Image,
+    CT.JPEG:                  Image,
+    CT.MS_PHOTO:              Image,
+    CT.PNG:                   Image,
+    CT.TIFF:                  Image,
+    CT.X_EMF:                 Image,
+    CT.X_WMF:                 Image,
+}
+
+PartFactory.part_type_for.update(content_type_to_part_class_map)
+
+del (
+    ChartPart, CoreProperties, Image, Slide, SlideLayout, SlideMaster,
+    PresentationPart, CT, PartFactory
+)
---
+++ packages/python-pptx/python-pptx-0.5.2.tar.gz/python-pptx-0.5.2/tests/test_package.py
@@ -0,0 +1,69 @@
+# encoding: utf-8
+
+"""
+Test suite for pptx.package module
+"""
+
+from __future__ import absolute_import, print_function
+
+import pytest
+
+from pptx.package import Package
+from pptx.parts.coreprops import CoreProperties
+from pptx.parts.presentation import PresentationPart
+
+
+from .unitutil.file import absjoin, test_file_dir
+
+
+images_pptx_path = absjoin(test_file_dir, 'with_images.pptx')
+
+
+class DescribePackage(object):
+
+    def it_loads_default_template_when_opened_with_no_path(self):
+        prs = Package.open().presentation
+        assert prs is not None
+        slide_masters = prs.slide_masters
+        assert slide_masters is not None
+        assert len(slide_masters) == 1
+        slide_layouts = slide_masters[0].slide_layouts
+        assert slide_layouts is not None
+        assert len(slide_layouts) == 11
+
+    def it_gathers_package_image_parts_on_open(self):
+        pkg = Package.open(images_pptx_path)
+        assert len(pkg._images) == 7
+
+    def it_provides_ref_to_package_presentation_part(self):
+        pkg = Package.open()
+        assert isinstance(pkg.presentation, PresentationPart)
+
+    def it_provides_ref_to_package_core_properties_part(self):
+        pkg = Package.open()
+        assert isinstance(pkg.core_properties, CoreProperties)
+
+    def it_can_save_itself_to_a_pptx_file(self, temp_pptx_path):
+        """
+        Package.save produces a .pptx with plausible contents
+        """
+        # setup ------------------------
+        pkg = Package.open()
+        # exercise ---------------------
+        pkg.save(temp_pptx_path)
+        # verify -----------------------
+        pkg = Package.open(temp_pptx_path)
+        prs = pkg.presentation
+        assert prs is not None
+        slide_masters = prs.slide_masters
+        assert slide_masters is not None
+        assert len(slide_masters) == 1
+        slide_layouts = slide_masters[0].slide_layouts
+        assert slide_layouts is not None
+        assert len(slide_layouts) == 11
+
+    # fixtures ---------------------------------------------
+
+    @pytest.fixture
+    def temp_pptx_path(self, tmpdir):
+        return absjoin(str(tmpdir), 'test-pptx.pptx')
