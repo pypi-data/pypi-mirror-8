206a2a428ab88a2fed0d91e51b802c92c15834a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:18 +0000

    Add package youtube_dl/2014.11.09/youtube_dl-2014.11.09.tar.gz

---
+++ packages/youtube_dl/youtube_dl-2014.11.09.tar.gz/youtube_dl-2014.11.09/youtube_dl/postprocessor/__init__.py
@@ -0,0 +1,26 @@
+
+from .atomicparsley import AtomicParsleyPP
+from .ffmpeg import (
+    FFmpegPostProcessor,
+    FFmpegAudioFixPP,
+    FFmpegEmbedSubtitlePP,
+    FFmpegExtractAudioPP,
+    FFmpegMergerPP,
+    FFmpegMetadataPP,
+    FFmpegVideoConvertor,
+)
+from .xattrpp import XAttrMetadataPP
+from .execafterdownload import ExecAfterDownloadPP
+
+__all__ = [
+    'AtomicParsleyPP',
+    'ExecAfterDownloadPP',
+    'FFmpegAudioFixPP',
+    'FFmpegEmbedSubtitlePP',
+    'FFmpegExtractAudioPP',
+    'FFmpegMergerPP',
+    'FFmpegMetadataPP',
+    'FFmpegPostProcessor',
+    'FFmpegVideoConvertor',
+    'XAttrMetadataPP',
+]
