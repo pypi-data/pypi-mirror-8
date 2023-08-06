71673a796c6a2dffcc68c662a0a1522c26b0184f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:17 +0000

    Add package setuptools/10.0.1/setuptools-10.0.1-py2.py3-none-any.whl

---
+++ packages/setuptools/setuptools-10.0.1-py2.py3-none-any.whl/setuptools/tests/py26compat.py
@@ -0,0 +1,27 @@
+import sys
+import unittest
+import tarfile
+
+try:
+	# provide skipIf for Python 2.4-2.6
+	skipIf = unittest.skipIf
+except AttributeError:
+	def skipIf(condition, reason):
+		def skipper(func):
+			def skip(*args, **kwargs):
+				return
+			if condition:
+				return skip
+			return func
+		return skipper
+
+def _tarfile_open_ex(*args, **kwargs):
+	"""
+	Extend result as a context manager.
+	"""
+	res = tarfile.open(*args, **kwargs)
+	res.__exit__ = lambda exc_type, exc_value, traceback: res.close()
+	res.__enter__ = lambda: res
+	return res
+
+tarfile_open = _tarfile_open_ex if sys.version_info < (2,7) else tarfile.open
