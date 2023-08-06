fd56c06f6ce91ae93171fd68d7d5a4f1118fb9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:43 +0000

    Add package python-shorturl/0.0.3/python-shorturl-0.0.3.tar.gz

---
+++ packages/python-shorturl/python-shorturl-0.0.3.tar.gz/python-shorturl-0.0.3/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+VERSION = "0.0.3"
+
+setup(
+    name="python-shorturl",
+    version=VERSION,
+    description="Python url shortner library for tinyurl.com, is.gd and bit.ly",
+    author="Abdul Hamid",
+    author_email="ap@appscluster.com",
+    url="https://github.com/appscluster/python-shorturl",
+    download_url="https://github.com/appscluster/python-shorturl/archive/master.zip",
+    packages=["shorturlpy"],
+    platforms=['OS Independent'],
+    keywords = ['tinyurl', 'shorturl', 'utility', 'bitly', 'is.gd'],
+    license='MIT',
+    long_description=open("README.md").read(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7"
+    ]
+)
---
+++ packages/python-shorturl/python-shorturl-0.0.3.tar.gz/python-shorturl-0.0.3/shorturlpy/__init__.py
@@ -0,0 +1,1 @@
+from .shorturlpy import ShortUrlPy
