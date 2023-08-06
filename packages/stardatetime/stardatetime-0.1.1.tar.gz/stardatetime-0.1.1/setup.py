971f0247b5c296241371eebde43f5c81cd320576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:01 +0000

    Add package stardatetime/0.1.1/stardatetime-0.1.1.tar.gz

---
+++ packages/stardatetime/stardatetime-0.1.1.tar.gz/stardatetime-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stardatetime',
+      version='0.1.1',
+      description='datetime for the next generation',
+      url='https://github.com/amygdalama/stardatetime',
+      author='Amy Hanlon',
+      license='MIT',
+      packages=['stardatetime'],
+      zip_safe=False)
+
