1b58ae336b3b05fbec33487752ade13875547b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:15 +0000

    Add package docxstache/0.0.2/docxstache-0.0.2.tar.gz

---
+++ packages/docxstache/docxstache-0.0.2.tar.gz/docxstache-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='docxstache',
+    version='0.0.2',
+    author='V. David Zvenyach',
+    author_email='dave@esq.io',
+    packages=['docxstache'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/docxstache/',
+    download_url="https://github.com/vzvenyach/docxstache2/tarball/0.0.1",
+    license='LICENSE.txt',
+    description='Mustache for docx',
+    long_description=open('readme.md').read(),
+    install_requires=[
+        "pystache>=0.5.4",
+    ],
+)
