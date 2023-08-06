a2f6f0136d22d4f2c085eb9f0eb8bece63d435b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:51 +0000

    Add package Localization/0.1.4/Localization-0.1.4.tar.gz

---
+++ packages/Localization/Localization-0.1.4.tar.gz/Localization-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Localization',
+    version='0.1.4',
+    author='Kamal Shadi',
+    author_email='kamal.shadi85@gmail.com',
+    packages=['localization', 'localization.test'],
+    scripts=['bin/sample.py'],
+    url='https://github.com/kamalshadi/Localization',
+    license='LICENSE.txt',
+    description='Multilateration and triangulation.',
+    long_description=open('README.txt').read(),
+)
+
+install_requires=['shapely','scipy','numpy']
