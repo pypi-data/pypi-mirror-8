2096f29708de2e0962626a1082b8eabd3182cefb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:19 +0000

    Add package detektor/1.1.0-beta.011/detektor-1.1.0-beta.011.tar.gz

---
+++ packages/detektor/detektor-1.1.0-beta.011.tar.gz/detektor-1.1.0-beta.011/detektor/functionextractor/clike.py
@@ -0,0 +1,43 @@
+from pprint import pprint
+import re
+from detektor.functionextractor.base import BaseFunctionExtractor
+from detektor.functionextractor.base import ExtractedFunction
+
+
+class ClikeFunctionExtractor(BaseFunctionExtractor):
+    functionstartpattern = re.compile(
+        r'(?P<returntype>\w+[^ ]*)?\s+(?P<name>\w+)\w*\((?P<arguments>.*?)\)\s*\{')
+
+    def __init__(self, keywords):
+        self.keywords = keywords
+
+    def _find_function_end(self, sourcecode, startindex):
+        blocklevel = 1
+        for index in xrange(startindex, len(sourcecode)):
+            character = sourcecode[index]
+            if character == '{':
+                blocklevel += 1
+            elif character == '}':
+                blocklevel -= 1
+            if blocklevel == 0:
+                return index
+        return len(sourcecode) - 1
+
+    def extract(self, sourcecode):
+        functions = []
+        for match in self.functionstartpattern.finditer(sourcecode):
+            groupdict = match.groupdict()
+            if groupdict['name'] not in self.keywords:
+                function_startindex = match.end()
+                function_endindex = self._find_function_end(sourcecode, function_startindex)
+                function_sourcecode = sourcecode[function_startindex:function_endindex]
+                matches = match.groupdict()
+                returntype = unicode(matches['returntype'], 'utf-8', 'replace')
+                name = unicode(matches['name'], 'utf-8', 'replace')
+                arguments = unicode(matches['arguments'], 'utf-8', 'replace')
+                functions.append(ExtractedFunction(
+                    name=u'{returntype} {name}({arguments})'.format(
+                        returntype=returntype, name=name, arguments=arguments),
+                    sourcecode=function_sourcecode
+                ))
+        return functions
---
+++ packages/detektor/detektor-1.1.0-beta.011.tar.gz/detektor-1.1.0-beta.011/detektor/tests/functionextractor/test_clike.py
@@ -0,0 +1,44 @@
+import unittest
+from detektor.functionextractor.clike import ClikeFunctionExtractor
+
+
+class TestClikeFunctionExtractor(unittest.TestCase):
+
+    def test_extract_function(self):
+        keywords = {
+            'if', 'while'
+        }
+        functions = ClikeFunctionExtractor(keywords).extract("""
+            void main(int i) {
+                // Some code here
+            }
+
+            int[] test() {
+            }
+
+            Array<String> anotherTest() {
+                if(i == 10) {
+                    while(true) {
+                        print("Something");
+                    }
+                }
+            }
+        """)
+        self.assertEquals(len(functions), 3)
+        self.assertEquals(functions[0].name, 'void main(int i)')
+        self.assertEquals(functions[1].name, 'int[] test()')
+        self.assertEquals(functions[2].name, 'Array<String> anotherTest()')
+        self.assertEquals(functions[0].sourcecode.strip(), '// Some code here')
+
+    def test_comment_before_ignored(self):
+        keywords = {
+            'if', 'while'
+        }
+        functions = ClikeFunctionExtractor(keywords).extract("""
+            // Mycomment
+            main(int i) {
+                // Some code here
+            }
+        """)
+        self.assertEquals(len(functions), 1)
+        self.assertEquals(functions[0].name, 'void main(int i)')
