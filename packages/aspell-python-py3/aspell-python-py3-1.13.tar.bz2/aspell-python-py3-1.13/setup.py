0cd164916d8d7c88554e18c9e383fa7c9b838dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:14 +0000

    Add package aspell-python-py3/1.13/aspell-python-py3-1.13.tar.bz2

---
+++ packages/aspell-python-py3/aspell-python-py3-1.13.tar.bz2/aspell-python-py3-1.13/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup, Extension
+
+module = Extension('aspell',
+    libraries = ['aspell'],
+    library_dirs = ['/usr/local/lib/'],
+    sources = ['aspell.c']
+)
+
+setup (name = 'aspell-python-py3',
+    version = '1.13',
+    ext_modules = [module],
+
+    description      = "Wrapper around GNU Aspell for python 3",
+    author           = "Wojciech Muła",
+    author_email     = "wojciech_mula@poczta.onet.pl",
+    maintainer       = "Wojciech Muła",
+    maintainer_email = "wojciech_mula@poczta.onet.pl",
+    url              = "http://github.com/WojciechMula/aspell-python",
+    platforms        = ["Linux", "Windows"],
+    license          = "BSD (3 clauses)",
+    long_description = "aspell-python - C extension for Python 3",
+    classifiers      = [
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: C",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Text Editors :: Text Processing",
+    ],
+)
