b936c3de3bc147bc882b1a4e40f7c0b9b7a3fe93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:33 +0000

    Add package nose-parameterized/0.3.5/nose-parameterized-0.3.5.tar.gz

---
+++ packages/nose-parameterized/nose-parameterized-0.3.5.tar.gz/nose-parameterized-0.3.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+os.chdir(os.path.dirname(sys.argv[0]) or ".")
+
+setup(
+    name="nose-parameterized",
+    version="0.3.5",
+    url="https://github.com/wolever/nose-parameterized",
+    author="David Wolever",
+    author_email="david@wolever.net",
+    description="Decorator for parameterized testing with Nose",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+    ],
+    packages=find_packages(),
+)
