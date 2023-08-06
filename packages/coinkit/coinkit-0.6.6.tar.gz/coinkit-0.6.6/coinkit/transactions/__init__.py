0ff2e57fb72d1b2caf52613d48a96a0f7e422319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:19 +0000

    Add package coinkit/0.6.6/coinkit-0.6.6.tar.gz

---
+++ packages/coinkit/coinkit-0.6.6.tar.gz/coinkit-0.6.6/coinkit/transactions/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    Coinkit
+    ~~~~~
+
+    :copyright: (c) 2014 by Halfmoon Labs
+    :license: MIT, see LICENSE for more details.
+"""
+
+import opcodes
+
+from .network import broadcast_transaction, send_to_address, get_unspents
+from .scripts import make_pay_to_address_script
+from .serialize import make_pay_to_address_transaction, serialize_input, \
+    serialize_output, serialize_transaction
+from .utils import flip_endian, variable_length_int, STANDARD_FEE
---
+++ packages/coinkit/coinkit-0.6.6.tar.gz/coinkit-0.6.6/setup.py
@@ -0,0 +1,40 @@
+"""
+Coinkit
+==============
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='coinkit',
+    version='0.6.6',
+    url='https://github.com/onenameio/coinkit',
+    license='MIT',
+    author='Onename (Halfmoon Labs)',
+    author_email='hello@halfmoon.io',
+    description='Tools for Bitcoin & other cryptocurrencies (incl. Litecoin, Namecoin, Peercoin, Primecoin, Dogecoin, Worldcoin, Megacoin, Anoncoin, Feathercoin, Terracoin, and Novacoin).',
+    keywords='bitcoin btc litecoin namecoin dogecoin cryptocurrency',
+    packages=[
+        'coinkit',
+        'coinkit.services',
+        'coinkit.transactions',
+        'coinkit.passphrases'
+    ],
+    zip_safe=False,
+    install_requires=[
+        'ecdsa==0.11',
+        'utilitybelt==0.1.7',
+        'requests==2.4.3',
+        'pybitcointools==1.1.15'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
