03a50fa7c9f1e76e224631422d2f2b86e1632e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:40 +0000

    Add package pyhcl/0.1.8/pyhcl-0.1.8.tar.gz

---
+++ packages/pyhcl/pyhcl-0.1.8.tar.gz/pyhcl-0.1.8/CHANGELOG.md
@@ -0,0 +1,30 @@
+0.1.7 (2014-11-14)
+------------------
+* Parse floats properly
+* Properly decode \\n
+* Complex keys can be set using strings
+
+0.1.6 (2014-10-23)
+------------------
+* Fix bug that prevented pyhcl from working in a pyinstaller environment
+
+0.1.5 (2014-10-15)
+------------------
+* Support escaped strings
+* Support trailing commas
+* Remove support for alternative boolean values
+
+0.1.3 (2014-09-12)
+------------------
+* Update documentation
+* Fix py3 support regarding bytes/strings
+* Add tests for bytes and strings
+* Support various boolean values
+
+0.1.2 (2014-09-10)
+------------------
+* Add travis-ci tests, fix minor bugs in setup.py
+
+0.1.0 (2014-09-05)
+------------------
+* Initial release
---
+++ packages/pyhcl/pyhcl-0.1.8.tar.gz/pyhcl-0.1.8/tests/test_lexer.py
@@ -0,0 +1,89 @@
+#
+# These tests are taken from hcl/lex_test.go
+#
+
+from __future__ import print_function
+
+from os.path import join, dirname
+import hcl.lexer
+
+import pytest
+
+class Error:
+    pass
+
+LEX_FIXTURE_DIR = join(dirname(__file__), 'lex-fixtures')
+LEX_FIXTURES = [
+    (
+        "comment.hcl",
+        ["IDENTIFIER", "EQUAL", "STRING", None]
+    ),
+    (
+        "multiple.hcl",
+        [
+            "IDENTIFIER", "EQUAL", "STRING",
+            "IDENTIFIER", "EQUAL", "NUMBER",
+            None,
+        ],
+    ),
+    (
+        "list.hcl",
+        [
+            "IDENTIFIER", "EQUAL", "LEFTBRACKET",
+            "NUMBER", "COMMA", "NUMBER", "COMMA", "STRING",
+            "RIGHTBRACKET", None,
+        ],
+    ),
+    (
+        "old.hcl",
+        [
+            "IDENTIFIER", "EQUAL", "LEFTBRACE", "STRING", Error
+        ],
+    ),
+    (
+        "structure_basic.hcl",
+        [
+            "IDENTIFIER", "LEFTBRACE",
+            "IDENTIFIER", "EQUAL", "NUMBER",
+            "STRING", "EQUAL", "NUMBER",
+            "STRING", "EQUAL", "NUMBER",
+            "RIGHTBRACE", None,
+        ],
+    ),
+    (
+        "structure.hcl",
+        [
+            "IDENTIFIER", "IDENTIFIER", "STRING", "LEFTBRACE",
+            "IDENTIFIER", "EQUAL", "NUMBER",
+            "IDENTIFIER", "EQUAL", "STRING",
+            "RIGHTBRACE", None,
+        ],
+    ),
+]
+
+@pytest.mark.parametrize("hcl_fname,tokens", LEX_FIXTURES)
+def test_lexer(hcl_fname, tokens):
+    
+    with open(join(LEX_FIXTURE_DIR, hcl_fname), 'r') as fp:
+        input = fp.read()
+    
+    print(input)
+    
+    lexer = hcl.lexer.Lexer()
+    lexer.input(input)
+
+    for tok in tokens:
+        try:
+            lex_tok = lexer.token()
+        except ValueError:
+            assert tok is Error
+            return
+            
+        if lex_tok is None:
+            assert tok is None
+        else:
+            assert tok == lex_tok.type 
+        print(lex_tok)
+            
+    assert lexer.token() is None
+
