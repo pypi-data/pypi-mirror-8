aa61a5d21f2a4e394b90137e048adfa471da7b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:12 +0000

    Add package Loslassa/0.3.8/Loslassa-0.3.8.tar.gz

---
+++ packages/Loslassa/Loslassa-0.3.8.tar.gz/Loslassa-0.3.8/loslassa/__scratchpad.py
@@ -0,0 +1,9 @@
+print(
+    " ".join(
+        ['/usr/local/bin/sphinx-build', '-b', 'dirhtml', '-d',
+         '/home/obestwalter/projects/linda/bilderwerkstatt_ravensburg.de/'
+         '__build/doctrees',
+         '/home/obestwalter/projects/linda/bilderwerkstatt_ravensburg.de',
+         '/home/obestwalter/projects/linda/bilderwerkstatt_ravensburg.de/'
+         '__build/html']
+    ))
