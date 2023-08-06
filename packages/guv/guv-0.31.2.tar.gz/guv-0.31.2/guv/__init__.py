caa964f5f81b4e8b9bbaba11cc37cbc06d5d43b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:38 +0000

    Add package guv/0.31.2/guv-0.31.2.tar.gz

---
+++ packages/guv/guv-0.31.2.tar.gz/guv-0.31.2/guv/__init__.py
@@ -0,0 +1,23 @@
+version_info = (0, 31, 2)
+__version__ = '.'.join(map(str, version_info))
+
+try:
+    from . import greenpool
+    from . import queue
+    from .hubs.trampoline import gyield
+    from .greenthread import sleep, spawn, spawn_n, spawn_after, kill
+    from .greenpool import GreenPool, GreenPile
+    from .timeout import Timeout, with_timeout
+    from .patcher import import_patched, monkey_patch
+    from .server import serve, listen, connect, StopServe, wrap_ssl
+
+    import pyuv_cffi  # only to compile the shared library before monkey-patching
+
+except ImportError as e:
+    # This is to make Debian packaging easier, it ignores import errors of greenlet so that the
+    # packager can still at least access the version. Also this makes easy_install a little quieter
+    if 'greenlet' not in str(e):
+        # any other exception should be printed
+        import traceback
+
+        traceback.print_exc()
