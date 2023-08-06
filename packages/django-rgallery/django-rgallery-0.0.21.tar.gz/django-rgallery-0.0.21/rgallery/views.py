ba19d7f73150c83d57f06515311947ddddaf8cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:46 +0000

    Add package django-rgallery/0.0.21/django-rgallery-0.0.21.tar.gz

---
+++ packages/django-rgallery/django-rgallery-0.0.21.tar.gz/django-rgallery-0.0.21/rgallery/views.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import Image
+from StringIO import StringIO
+from datetime import datetime
+
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.sites.models import Site
+from django.contrib import messages
+import models as mymodels
+import forms as myforms
+
+from sorl.thumbnail import get_thumbnail
+from braces.views import LoginRequiredMixin, SuperuserRequiredMixin
+
+from django.conf import settings as conf
+
+from django.views.generic import DetailView, ListView, CreateView
+
+from management.commands.mediasync import get_exif, img_rotate
+from forms import PhotoForm
+
+class Photos(ListView):
+
+    template_name = "rgallery/photos.html"
+    context_object_name = "photos"
+    paginate_by = 51
+
+    def get_queryset(self):
+        return mymodels.Photo.objects.all().filter(status=1, folder__isnull=True).order_by('-capture_date')
+
+    def get_context_data(self, **kwargs):
+        context = super(Photos, self).get_context_data(**kwargs)
+        folders = mymodels.Folder.objects.filter(status=1)
+        form = PhotoForm
+
+        context.update({
+            'title': _(u'Photos'),
+            'form': form,
+            'folders': folders,
+        })
+        return context
+
+
+class PhotosFolder(ListView):
+
+    template_name = "rgallery/photos.html"
+    context_object_name = "photos"
+
+    def get_queryset(self):
+        folder = mymodels.Folder.objects.get(slug=self.kwargs['folder'])
+        return mymodels.Photo.objects.all().filter(status=1, folder=folder).order_by('-capture_date')
+
+    def get_context_data(self, **kwargs):
+        context = super(PhotosFolder, self).get_context_data(**kwargs)
+
+        context.update({
+            'title': _(u'Photos'),
+        })
+        return context
+
+
+class PhotoAdd(LoginRequiredMixin, SuperuserRequiredMixin, CreateView):
+
+    model = mymodels.Photo
+    form_class = PhotoForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        # Name (if exists in database)
+        try:
+            im = mymodels.Photo.objects.get(origen=os.path.basename(form.files['file'].name))
+        except:
+            nombre_imagen = str(os.path.basename(form.files['file'].name)).replace('/', '').replace(' ','_').replace(':','-')
+
+            # Extracting Exif data
+            data_image = get_exif(form.files['file'])
+            capture_data = datetime.strptime(str(data_image['DateTimeOriginal']), "%Y:%m:%d %H:%M:%S")
+
+            # Name (if exists in disk)
+            destdir = os.path.join(conf.PROJECT_DIR, 'media', 'uploads', 'photos')
+            if os.path.exists(os.path.join(destdir, nombre_imagen)) == True:
+                nombre_imagen = str(os.path.basename(form.files['file'].name)).replace('/', '').replace(' ','_').replace(':','-').replace('.jpg','') + '_1.jpg'
+            data = ""
+            for c in form.files['file'].chunks():
+                data += c
+            imagefile  = StringIO(data)
+            img = Image.open(imagefile)
+            img = img_rotate(img)
+
+            # Save
+            img.save(os.path.join(destdir, nombre_imagen), img.format)
+
+            # Add to database
+            im = mymodels.Photo(image="uploads/photos/" + nombre_imagen,
+                                origen=form.files['file'].name,
+                                insert_date=datetime.now(),
+                                capture_date=capture_data,
+                                status=True)
+            im.save()
+
+            # Thumbs
+            thumbs = [200, 750, 1000]
+            for thumb in thumbs:
+                im = get_thumbnail(im, "%sx%s" % (thumb, thumb))
+
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class PhotoDelete(LoginRequiredMixin, SuperuserRequiredMixin, ListView):
+
+    model = mymodels.Photo
+    template_name = "rgallery/photo_delete.html"
+
+    def get_context_data(self, **kwargs):
+        if self.request.is_ajax() and self.request.method == 'GET':
+            pk = json.loads(self.request.GET.get('pk', None))
+            photo = get_object_or_404(mymodels.Photo, pk=pk)
+            photo.delete()
+            return True
+
+
+class Videos(ListView):
+
+    template_name = "rgallery/videos.html"
+    context_object_name = "videos"
+    paginate_by = 6
+
+    def get_queryset(self):
+        vid = mymodels.Video.objects.all().filter(status=1, folder__isnull=True).order_by('-capture_date')
+        print vid
+        return vid
+
+    def get_context_data(self, **kwargs):
+        context = super(Videos, self).get_context_data(**kwargs)
+        context.update({
+            'title': _(u'Videos'),
+        })
+        return context
