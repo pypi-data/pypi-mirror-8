4c22f12077c6b3d52b86b180c4ea97d4d3cfc763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:16 +0000

    Add package collective.slickslideshow/0.0.1/collective.slickslideshow-0.0.1.tar.gz

---
+++ packages/collective.slickslideshow/collective.slickslideshow-0.0.1.tar.gz/collective.slickslideshow-0.0.1/README.txt
@@ -0,0 +1,4 @@
+Introduction
+============
+
+Slick Slideshow solution for Plone.
---
+++ packages/collective.slickslideshow/collective.slickslideshow-0.0.1.tar.gz/collective.slickslideshow-0.0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1'
+
+setup(name='collective.slickslideshow',
+      version=version,
+      description="Slick Slideshow solution for Plone.",
+      long_description=open("README.txt").read() + "\n" +
+                       open("docs/HISTORY.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='slideshow',
+      author='Andre Goncalves',
+      author_email='andre@intk.com',
+      url='https://github.com/intk/collective.slickslideshow',
+      download_url='https://github.com/intk/collective.slickslideshow/tarball/0.0.1',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.slideshow'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
