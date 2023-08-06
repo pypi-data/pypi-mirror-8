25bee3f757b3ec5f4f0e6c2225551ef92263e6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:18 +0000

    Add package arc_library/1.0.1/arc_library-1.0.1.tar.gz

---
+++ packages/arc_library/arc_library-1.0.1.tar.gz/arc_library-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+ 
+setup(
+    name = 'arc_library',
+    version = '1.0.1',
+    py_modules = ['arc_django_models', 'arc_functions', 'arc_jsonobject', 'arc_storage', 'arc_tag'],
+    author = 'Arcanelux',
+    author_email = 'Arcanelux@gmail.com',
+    url = 'iiii.so',
+    description = 'Arcaneluxl Python Library',
+)
