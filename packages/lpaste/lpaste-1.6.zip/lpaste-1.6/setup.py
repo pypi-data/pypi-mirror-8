5386d9f12339f795002a0f7f96187ee74d409b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:33 +0000

    Add package lpaste/1.6/lpaste-1.6.zip

---
+++ packages/lpaste/lpaste-1.6.zip/lpaste-1.6/setup.py
@@ -0,0 +1,52 @@
+import sys
+import platform
+from collections import defaultdict
+
+import setuptools
+
+py26reqs = ['importlib', 'argparse'] if sys.version_info < (2, 7) else []
+
+# add any platform-specific requirements
+clipboard_support = defaultdict(lambda: [], {
+	'Windows': ['jaraco.windows>=2.1'],
+	})[platform.system()]
+
+setup_params = dict(
+	name="lpaste",
+	use_hg_version=True,
+	packages=setuptools.find_packages(),
+	entry_points = {
+		'console_scripts': [
+			'lpaste = lpaste.lpaste:main',
+		],
+	},
+	install_requires = [
+		'requests',
+		'keyring>=0.6',
+		'six>=1.4',
+	] + py26reqs,
+	extras_require = dict(
+		clipboard = clipboard_support,
+	),
+	description="Library Paste command-line client",
+	license = 'MIT',
+	author="Chris Mulligan",
+	author_email="chmullig@gmail.com",
+	maintainer = 'Chris Mulligan',
+	maintainer_email = 'chmullig@gmail.com',
+	url = 'http://bitbucket.org/chmullig/librarypaste-tools/lpaste',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: POSIX',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+	],
+	setup_requires = [
+		'hgtools',
+	],
+)
+
+if __name__ == '__main__':
+	setuptools.setup(**setup_params)
