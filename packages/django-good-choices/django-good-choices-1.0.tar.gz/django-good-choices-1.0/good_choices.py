bbef371cd4ea85e48fe6c4b0aa078b4233cd9ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:15 +0000

    Add package django-good-choices/1.0/django-good-choices-1.0.tar.gz

---
+++ packages/django-good-choices/django-good-choices-1.0.tar.gz/django-good-choices-1.0/good_choices.py
@@ -0,0 +1,30 @@
+import inspect
+import six
+
+
+__all__ = ['Choices', 'VERSION']
+
+VERSION = (1, 0)
+
+
+class ChoicesMeta(type):
+    def __new__(cls, name, bases, attrs):
+        ch = list()
+        updated_attrs = dict()
+        for name, value in six.iteritems(attrs):
+            if not name.startswith('_'):
+                try:
+                    index, label = value
+                except ValueError:
+                    index = label = value
+                ch.append((index, label))
+                updated_attrs[name] = index
+        attrs.update(updated_attrs)
+        attrs['choices'] = sorted(ch, key=lambda c: c[0])
+        return super(ChoicesMeta, cls).__new__(cls, name, bases, attrs)
+
+
+@six.add_metaclass(ChoicesMeta)
+class Choices(object):
+    pass
+
