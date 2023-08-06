68b0a90de46a954dc9432438ed9ab39f2e125a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:06 +0000

    Add package plone.app.linkintegrity/1.5.5/plone.app.linkintegrity-1.5.5.zip

---
+++ packages/plone.app.linkintegrity/plone.app.linkintegrity-1.5.5.zip/plone.app.linkintegrity-1.5.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = '1.5.5'
+
+setup(name='plone.app.linkintegrity',
+      version=version,
+      description='Manage link integrity in Plone.',
+      long_description=open("README.rst").read() + '\n' +
+                       open('CHANGES.rst').read(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "Intended Audience :: Other Audience",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP :: Site Management :: Link Checking",
+        ],
+      keywords='link integrity plone',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.linkintegrity',
+      license='GPL version 2',
+      packages=find_packages(),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      install_requires=[
+        'setuptools',
+      ],
+      extras_require={'test': [
+        'collective.testcaselayer',
+      ]},
+      platforms='Any',
+      zip_safe=False,
+      entry_points='''
+        [z3c.autoinclude.plugin]
+        target = plone
+      ''',
+)
