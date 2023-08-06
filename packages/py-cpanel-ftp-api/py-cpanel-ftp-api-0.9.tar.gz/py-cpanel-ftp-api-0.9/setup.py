90b465469da7e37e1dfadaf73de71465d0a27c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:43 +0000

    Add package py-cpanel-ftp-api/0.9/py-cpanel-ftp-api-0.9.tar.gz

---
+++ packages/py-cpanel-ftp-api/py-cpanel-ftp-api-0.9.tar.gz/py-cpanel-ftp-api-0.9/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='py-cpanel-ftp-api',
+      description='Python client library for interacting with Cpanel\'s API2 FTP Module',
+      author='Benton Snyder',
+      author_email='benton@bensnyde.me',
+      url='https://github.com/bensnyde/py-cpanel-ftp-api/',
+      version='0.9',
+      py_modules=['cpanel_ftp_api'],
+      )
