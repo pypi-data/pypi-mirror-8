2ac4182fbdcba359f8327c143e0fcb46df08a6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:21 +0000

    Add package Bommel_looplist/1.2.1/Bommel_looplist-1.2.1.tar.gz

---
+++ packages/Bommel_looplist/Bommel_looplist-1.2.1.tar.gz/Bommel_looplist-1.2.1/looplist.py
@@ -0,0 +1,16 @@
+"""This is the “nester.py" module, and it provides one function called
+listloop() which prints lists that may or may not include nested lists."""
+
+def listloop(the_list, indent=False, level=0):
+    """This function takes a positional argument called “the_list", which is any 
+    Python list (of, possibly, nested lists). Each data item in the provided list
+    is (recursively) printed to the screen on its own line.
+    A second argument is used to insert tab stops when a nested list is encountered"""
+    for i in the_list:
+        if isinstance(i, list):
+            listloop(i, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(i)
---
+++ packages/Bommel_looplist/Bommel_looplist-1.2.1.tar.gz/Bommel_looplist-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'Bommel_looplist',
+	version = '1.2.1',
+	py_modules = ['looplist'],
+	author = 'Bommel',
+	author_email = 'wiesnert100@gmail.com',
+	url = 'https://pypi.python.org',
+	description = 'A simple print of nested lists',
+	)
