69d752a0faa09c5456038fc5d073066b2277ae8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:07 +0000

    Add package zope.catalog/4.0.0/zope.catalog-4.0.0.tar.gz

---
+++ packages/zope.catalog/zope.catalog-4.0.0.tar.gz/zope.catalog-4.0.0/CHANGES.rst
@@ -0,0 +1,35 @@
+=======
+CHANGES
+=======
+
+4.0.0 (unreleased)
+------------------
+
+- Add support for Python 3.4.
+
+4.0.0a1 (2013-02-25)
+--------------------
+
+- Add support for Python 3.3.
+
+- Replace deprecated ``zope.interface.implements`` usage with equivalent
+  ``zope.interface.implementer`` decorator.
+
+- Drop support for Python 2.4 and 2.5.
+
+3.8.2 (2011-11-29)
+------------------
+
+- Conform to repository policy.
+
+3.8.1 (2009-12-27)
+------------------
+
+- Remov ``zope.app.testing`` dependency.
+
+3.8.0 (2009-02-01)
+------------------
+
+- Move core functionality from ``zope.app.catalog`` to this package.
+  The ``zope.app.catalog`` package now only contains ZMI-related browser
+  views and backward-compatibility imports.
---
+++ packages/zope.catalog/zope.catalog-4.0.0.tar.gz/zope.catalog-4.0.0/setup.py
@@ -0,0 +1,89 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.catalog package
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name = 'zope.catalog',
+      version='4.0.0',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Cataloging and Indexing Framework for the Zope Toolkit',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          'Detailed Documentation\n'
+          '**********************\n'
+          + '\n\n' +
+          read('src', 'zope', 'catalog', 'README.txt')
+          + '\n\n' +
+          read('src', 'zope', 'catalog', 'event.txt')
+          + '\n\n' +
+          read('CHANGES.rst')
+          ),
+      keywords = "zope3 catalog index",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.catalog',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.testing',
+                'zope.site',
+                ]),
+      install_requires = [
+          'setuptools',
+          'persistent',
+          'BTrees',
+          'zope.annotation',
+          'zope.intid',
+          'zope.component',
+          'zope.container',
+          'zope.index>=3.5.0',
+          'zope.interface',
+          'zope.lifecycleevent',
+          'zope.location',
+          'zope.schema',
+          ],
+      tests_require = ['zope.testing', 'zope.site'],
+      test_suite = 'zope.catalog.tests.test_suite',
+      include_package_data = True,
+      zip_safe = False,
+      )
