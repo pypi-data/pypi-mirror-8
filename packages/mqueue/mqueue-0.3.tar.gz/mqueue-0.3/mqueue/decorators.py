934d4b62ba1a10e00063e90830fb547b51535008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:07 +0000

    Add package mqueue/0.3/mqueue-0.3.tar.gz

---
+++ packages/mqueue/mqueue-0.3.tar.gz/mqueue-0.3/mqueue/decorators.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from crontab import CronTab
+from datetime import datetime, timedelta
+from decorated import Function
+from decorated.base.context import Context, ctx, ContextError
+from loggingd import log_return
+from mqueue import util
+from mqueue.db import Task as TaskModel
+import json
+import mqueue
+
+crons = []
+
+class Task(Function):
+    @property
+    def name(self):
+        return util.obj_to_path(self._func)
+        
+    @log_return('Enqueued task {self.name}.')
+    def enqueue(self, *args, **kw):
+        args = self._resolve_args(*args, **kw)
+        args = json.dumps(args)
+        eta = datetime.now() + Delay.value()
+        model = TaskModel(
+            args=args,
+            eta=eta,
+            name=self.name,
+            queue=mqueue.QUEUE,
+            retries=0
+        )
+        ctx.session.add(model)
+            
+class Cron(Task):
+    def is_overdue(self, last):
+        delta = self._schedule.next(last)
+        delta = timedelta(seconds=delta)
+        overdue = last + delta < datetime.now()
+        if not overdue:
+            return False
+        if self._skip_if_scheduled:
+            cnt = ctx.session.query(TaskModel) \
+                    .filter(TaskModel.name == self.name) \
+                    .count()
+            if cnt > 0:
+                return False
+        return True
+    
+    def _decorate(self, func):
+        crons.append(self)
+        return super(Cron, self)._decorate(func)
+    
+    def _init(self, schedule, skip_if_scheduled=False):
+        super(Cron, self)._init()
+        self._schedule = CronTab(schedule)
+        self._skip_if_scheduled = skip_if_scheduled
+    
+class Delay(Context):
+    @staticmethod
+    def value():
+        zero_delay = timedelta(minutes=0)
+        try:
+            return ctx.dict().get('delay', zero_delay)
+        except ContextError:
+            return zero_delay
+    
+    def __init__(self, delay):
+        super(Delay, self).__init__(delay=delay)
+        
---
+++ packages/mqueue/mqueue-0.3.tar.gz/mqueue-0.3/mqueue/scheduler.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from loggingd import log_error
+from mqueue import db, decorators
+from mqueue.db import Cron as CronModel
+from mqueue.util import Timer
+import mqueue
+
+class SchedulerThread(Timer):
+    @property
+    def interval(self):
+        return 60 - datetime.now().second + 1
+    
+    @log_error('Scheduler failed.', exc_info=True)
+    def _run(self):
+        for cron in decorators.crons:
+            _check(cron)
+    
+@log_error('Failed to check cron {cron.name}.', exc_info=True)
+def _check(cron):
+    with db.dao.SessionContext() as ctx:  # @UndefinedVariable
+        model = ctx.session.get_or_create(CronModel, mqueue.QUEUE, cron.name)
+        if model.last is None or cron.is_overdue(model.last):
+            cron.enqueue()
+        model.last = datetime.now()
