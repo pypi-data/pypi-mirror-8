a987f6b5e60232aa2411dd4122f219b559f5f357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:38 +0000

    Add package django-classy-mail/0.1.6/django-classy-mail-0.1.6.tar.gz

---
+++ packages/django-classy-mail/django-classy-mail-0.1.6.tar.gz/django-classy-mail-0.1.6/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='django-classy-mail',
+    version='0.1.6',
+    author='Alex Lovell-Troy',
+    author_email='alex@lovelltroy.org',
+    description='Class-Based Email for Django built with Mixins',
+    packages=['classy_mail', 'classy_mail.mixins' ],
+    url='https://github.com/alexlovelltroy/django-classy-mail',
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "Django >= 1.5",
+        "markdown >= 2.4",
+        "BeautifulSoup >= 3.2",
+        "PyYAML >= 3.10",
+    ],
+)
