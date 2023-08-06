cbb45462bfaaf220b25c6f3eb3d7d37494398a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:52 +0000

    Add package BASC-py4chan/0.4.0/BASC-py4chan-0.4.0.tar.gz

---
+++ packages/BASC-py4chan/BASC-py4chan-0.4.0.tar.gz/BASC-py4chan-0.4.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+    py-4chan-setup
+    ~~~~~~~~~~~~~~
+
+    Python Wrappers to access 4chan's API.
+
+    This program is free software. It comes without any warranty, to
+    the extent permitted by applicable law. You can redistribute it
+    and/or modify it under the terms of the Do What The Fuck You Want
+    To Public License, Version 2, as published by Sam Hocevar. See
+    http://sam.zoy.org/wtfpl/COPYING for more details.
+
+"""
+
+from setuptools import setup
+from py4chan import __version__
+
+setup(name='BASC-py4chan',
+    version=__version__,
+    description='Python 4chan API Wrapper. Improved version of Edgeworth\'s original py-4chan wrapper.',
+    long_description = open('README.rst').read(), 
+    license=open('LICENSE').read(),
+    author='Antonizoon (Lawrence Wu)',
+    author_email='sagnessagiel@gmail.com',
+    url='http://github.com/bibanon/BASC-py4chan',
+    packages=['py4chan'],
+    install_requires = ['requests >= 1.0.0']
+)
