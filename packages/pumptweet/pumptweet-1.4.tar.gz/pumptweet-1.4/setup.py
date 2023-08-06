3def548f8d363aeb2c53f2fda4b43ed6b5e5ac98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:43 +0000

    Add package pumptweet/1.4/pumptweet-1.4.tar.gz

---
+++ packages/pumptweet/pumptweet-1.4.tar.gz/pumptweet-1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pumptweet',
+      version='1.4',
+      description='A Python script that cross posts from a Pump.io server to Twitter.',
+      url='http://github.com/dper/pumptweet',
+      author='Douglas Paul Perkins',
+      author_email='contact@dperkins.org',
+      license='MIT',
+      packages=['pumptweet'],
+      zip_safe=False)
