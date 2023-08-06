1e99d90caa8c0d6f17c93216a898969d6b1e686a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:22 +0000

    Add package outpost/0.2.3/outpost-0.2.3.zip

---
+++ packages/outpost/outpost-0.2.3.zip/outpost-0.2.3/outpost/filterinc.py
@@ -0,0 +1,54 @@
+
+import json
+
+# quick and dirty string replacements
+
+def appendhead(file, settings):
+    htmlfile = settings.get("filter.appendhead")
+    if not htmlfile:
+        return file
+    try:
+        with open(htmlfile) as f:
+            data = f.read()
+    except IOError:
+        return file
+    # process
+    file.body = file.body.replace("</head>", data+"</head>")
+    return file
+
+
+def appendbody(file, settings):
+    htmlfile = settings.get("filter.appendbody")
+    if not htmlfile:
+        return file
+    try:
+        with open(htmlfile) as f:
+            data = f.read()
+    except IOError:
+        return file
+    # process
+    file.body = file.body.replace("</body>", data+"</body>")
+    return file
+
+
+def replacestr(file, settings):
+    conf = settings.get("filter.replacestr")
+    if not conf:
+        return file
+    conf = json.loads(conf)
+    # process
+    if not isinstance(conf, (list,tuple)):
+        conf = (conf,)
+    for repl in conf:
+        codepage = repl.get("codepage", "utf-8") or "utf-8"
+        file.body = file.body.replace(repl["str"].encode(codepage), repl["new"].encode(codepage))
+    return file
+
+
+# available filter sections ------------------------------------------------------------------------
+FILTER = (
+    ("filter.appendhead", appendhead), 
+    ("filter.appendbody", appendbody), 
+    ("filter.replacestr", replacestr)
+)
+
