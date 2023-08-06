20cc299682e40690f896f610df9a5eadc03cee28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:13 +0000

    Add package better_od/0.2.1/better_od-0.2.1.tar.gz

---
+++ packages/better_od/better_od-0.2.1.tar.gz/better_od-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='better_od',
+    version='0.2.1',
+    description='A more flexible implementation of OrderedDict',
+    author='Nick Dumas',
+    author_email='drakonik@gmail.com',
+    url='https://github.com/therealfakemoot/BetterOrderedDict/',
+    packages=find_packages(),
+    zip_safe=False
+)
