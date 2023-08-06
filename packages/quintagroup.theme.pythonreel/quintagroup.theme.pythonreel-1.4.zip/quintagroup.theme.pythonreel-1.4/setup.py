b5a486f3cccaf260179a47961b870a9e046f20d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:23 +0000

    Add package quintagroup.theme.pythonreel/1.4/quintagroup.theme.pythonreel-1.4.zip

---
+++ packages/quintagroup.theme.pythonreel/quintagroup.theme.pythonreel-1.4.zip/quintagroup.theme.pythonreel-1.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.4'
+
+setup(name='quintagroup.theme.pythonreel',
+      version=version,
+      description="",
+      long_description=open(os.path.join("quintagroup", "theme", "pythonreel", "README.txt")).read() + "\n\n" +
+                       open(os.path.join("docs", "INSTALL.rst")).read() + "\n\n"+
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        ],
+      keywords='web zope plone theme diazo quintagroup',
+      author='Quintagroup',
+      author_email='skins@quintagroup.com',
+      url='http://themes.quintagroup.com/product/pythonreel',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['quintagroup', 'quintagroup.theme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.theming',
+          'plone.app.themingplugins',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
