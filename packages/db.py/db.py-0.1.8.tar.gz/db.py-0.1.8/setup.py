83157bcb24266122ee4540d774ca660ac3907b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:22 +0000

    Add package db.py/0.1.8/db.py-0.1.8.tar.gz

---
+++ packages/db.py/db.py-0.1.8.tar.gz/db.py-0.1.8/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+required = [
+    "prettytable==0.7.2",
+    "pandas==0.15.0"
+]
+
+setup(
+    name="db.py",
+    version="0.1.8",
+    author="Greg Lamp",
+    author_email="greg@yhathq.com",
+    url="https://github.com/yhat/db.py",
+    license="BSD",
+    packages=find_packages(),
+    package_dir={"db": "db"},
+    package_data={"db": []},
+    description="a db package that doesn't suck",
+    long_description=open("README.rst").read(),
+    install_requires=required,
+)
+
