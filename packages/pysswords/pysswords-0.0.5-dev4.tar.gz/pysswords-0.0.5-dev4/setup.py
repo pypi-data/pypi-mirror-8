40695902ff84a44b38e2e9fd08b3d0079023cc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:17 +0000

    Add package pysswords/0.0.5-dev4/pysswords-0.0.5-dev4.tar.gz

---
+++ packages/pysswords/pysswords-0.0.5-dev4.tar.gz/pysswords-0.0.5-dev4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+__version__ = "0.0.5-dev4"
+
+# requirements = [pkg.strip() for pkg in open('requirements.txt').readlines()]
+requirements = ["gnupg==1.4.0", "pyperclip==1.5.7"]
+
+with open("README.rst") as f:
+    long_description = f.read() + '\n'
+
+setup(
+    name='pysswords',
+    version=__version__,
+    license='License :: OSI Approved :: MIT License',
+    description="Manage your login credentials from the terminal painlessly.",
+    long_description=long_description,
+    author='Marc Webbie',
+    author_email='marcwebbie@gmail.com',
+    url='https://github.com/marcwebbie/pysswords',
+    scripts=["bin/pysswords"],
+    download_url='https://pypi.python.org/pypi/pysswords',
+    packages=['pysswords'],
+    install_requires=requirements,
+    test_suite='tests.test',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
