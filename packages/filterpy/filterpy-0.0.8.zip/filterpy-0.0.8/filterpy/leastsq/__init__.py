cfb7d8d3322ae256174b05f979040e2e203df850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:17 +0000

    Add package filterpy/0.0.8/filterpy-0.0.8.zip

---
+++ packages/filterpy/filterpy-0.0.8.zip/filterpy-0.0.8/filterpy/leastsq/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+__all__=["least_squares"]
+
+from .least_squares import LeastSquaresFilter
