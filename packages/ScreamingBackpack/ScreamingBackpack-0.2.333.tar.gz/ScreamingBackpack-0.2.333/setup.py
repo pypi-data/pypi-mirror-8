fe58509b834908558f0e143d9d22a0741684b053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:20 +0000

    Add package ScreamingBackpack/0.2.333/ScreamingBackpack-0.2.333.tar.gz

---
+++ packages/ScreamingBackpack/ScreamingBackpack-0.2.333.tar.gz/ScreamingBackpack-0.2.333/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='ScreamingBackpack',
+    version='0.2.333',
+    author='Michael Imelfort',
+    author_email='mike@mikeimelfort.com',
+    packages=['screamingbackpack'],
+    scripts=['bin/screamingBackpack'],
+    url='http://pypi.python.org/pypi/ScreamingBackpack/',
+    license='GPLv3',
+    description='ScreamingBackpack',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
+
