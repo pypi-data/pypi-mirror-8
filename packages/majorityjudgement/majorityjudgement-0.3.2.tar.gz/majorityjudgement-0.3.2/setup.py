2864bb623e44e285f9fabf1c2f1605a94364a943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:46 +0000

    Add package majorityjudgement/0.3.2/majorityjudgement-0.3.2.tar.gz

---
+++ packages/majorityjudgement/majorityjudgement-0.3.2.tar.gz/majorityjudgement-0.3.2/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='majorityjudgement',
+    version='0.3.2',
+    author='David R. MacIver',
+    author_email='david@drmaciver.com',
+    packages=['majorityjudgement'],
+    url='https://github.com/DRMacIver/majority-judgement-python',
+    license='LICENSE.txt',
+    description='Implementation of majority judgement voting procedure',
+    long_description=open('README.txt').read(),
+    tests_require=['pytest', 'hypothesis'],
+    cmdclass={'test': PyTest},
+)
