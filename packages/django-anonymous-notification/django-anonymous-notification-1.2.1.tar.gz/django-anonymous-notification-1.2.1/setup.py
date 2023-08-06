f1caa319aa676bb01489682be32664807888a34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:28 +0000

    Add package django-anonymous-notification/1.2.1/django-anonymous-notification-1.2.1.tar.gz

---
+++ packages/django-anonymous-notification/django-anonymous-notification-1.2.1.tar.gz/django-anonymous-notification-1.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-anonymous-notification",
+    version=__import__("notification").__version__,
+    description="User notification management for the Django web framework",
+    long_description=open("docs/usage.rst").read(),
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    url="https://github.com/pinax/django-notification",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Framework :: Django",
+    ],
+    include_package_data=True,
+    test_suite='runtests',
+    install_requires=[
+        'django>=1.4',
+    ],
+    zip_safe=False,
+)
