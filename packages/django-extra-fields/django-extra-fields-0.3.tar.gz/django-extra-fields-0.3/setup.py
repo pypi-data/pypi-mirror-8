50b89c57bc4bbfc80ae75138b1f040a0ac65d54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:08 +0000

    Add package django-extra-fields/0.3/django-extra-fields-0.3.tar.gz

---
+++ packages/django-extra-fields/django-extra-fields-0.3.tar.gz/django-extra-fields-0.3/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-extra-fields',
+    version='0.3',
+    packages=['drf_extra_fields'],
+    include_package_data=True,
+    license=' License',  # example license
+    description='Additional fields for Django Rest Framework.',
+    long_description=README,
+    author='hipo',
+    author_email='pypi@hipolabs.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
