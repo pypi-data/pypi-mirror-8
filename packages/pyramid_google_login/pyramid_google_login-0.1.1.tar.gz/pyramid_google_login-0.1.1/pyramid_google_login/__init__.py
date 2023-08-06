5941d77f89f42e16be3266d66dfbd1773ded1360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:02 +0000

    Add package pyramid_google_login/0.1.1/pyramid_google_login-0.1.1.tar.gz

---
+++ packages/pyramid_google_login/pyramid_google_login-0.1.1.tar.gz/pyramid_google_login-0.1.1/pyramid_google_login/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from pyramid.httpexceptions import HTTPFound
+
+log = logging.getLogger(__name__)
+
+
+SETTINGS_PREFIX = 'security.google_login.'
+
+
+class AuthFailed(Exception):
+    pass
+
+
+def includeme(config):
+    log.info("Add pyramid_google_login")
+
+    config.include('pyramid_mako')
+
+    config.add_route('auth_signin', '/auth/signin')
+    config.add_route('auth_signin_redirect', '/auth/signin_redirect')
+    config.add_route('auth_callback', '/auth/oauth2callback')
+    config.add_route('auth_logout', '/auth/logout')
+
+    config.add_static_view('static/pyramid_google_login',
+                           'pyramid_google_login:static',
+                           cache_max_age=300)
+
+    config.scan()
+
+
+def redirect_to_signin(request, message=None, url=None, headers=None):
+    """ Redirect to the sign in page with message and next url """
+    query = {}
+    if message is not None:
+        query['message'] = message
+    if url is not None:
+        query['url'] = url
+    url = request.route_url('auth_signin', _query=query)
+    raise HTTPFound(location=url, headers=headers)
