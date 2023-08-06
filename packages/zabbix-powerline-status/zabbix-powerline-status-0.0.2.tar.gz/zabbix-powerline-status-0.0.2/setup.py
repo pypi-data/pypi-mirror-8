74cd0f870ae553b5d6049cf25ea12ea70a51336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:18 +0000

    Add package zabbix-powerline-status/0.0.2/zabbix-powerline-status-0.0.2.tar.gz

---
+++ packages/zabbix-powerline-status/zabbix-powerline-status-0.0.2.tar.gz/zabbix-powerline-status-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zabbix-powerline-status',
+    author='Colin Wood',
+    author_email='cwood06@gmail.com',
+    url='http://bitbucket.org/colinbits/zabbix-powerline-status',
+    download_url='http://bitbucket.org/colinbits/zabbix-powerline-status',
+    long_description='A extension to powerline to show zabbix triggers',
+    version='0.0.2',
+    include_package_data=True,
+    install_requires=[
+        'zabbix-client==0.1.0'
+    ],
+    packages=find_packages(),
+    description='Zabbix powerline extension to show current triggers',
+    tests_require=[
+        'nose',
+    ],
+    keywords=[],
+)
