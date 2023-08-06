b2488c9f065aaefb0798f33f992826dcb3fc3024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:59 +0000

    Add package PySelection/0.1.0/PySelection-0.1.0.tar.gz

---
+++ packages/PySelection/PySelection-0.1.0.tar.gz/PySelection-0.1.0/pyselection/about.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
+name         = "PySelection"
+version      = "0.1.0"
+summary      = "A Python wrapper for Codeml selection analysis"
+author_name  = "Thomas Walsh"
+author_email = "walsht26@mail.dcu.ie"
+copyright    = "Copyright 2015 Dublin City University (DCU)"
+package_url  = "https://github.com/walsht26/PySelection"
+download_url = "https://github.com/walsht26/PySelection"
+packages     = [ "pyselection" ]
+scripts      = [ ]
+
+classifiers  = [
+    "Development Status :: 1 - Planning",
+    "Environment :: Console",   
+    "Intended Audience :: Science/Research",    
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",   
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python",    
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.1",
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4"
+]
+
+license      = "GNU GPL 3.0"
+keywords     = "bioinformatics evolution"
+data_files   = [ ]
+
