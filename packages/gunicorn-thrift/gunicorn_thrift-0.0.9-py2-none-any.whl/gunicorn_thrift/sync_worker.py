310fd4cc108c781ea600537e4fcb6263a89a7f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:34 +0000

    Add package gunicorn-thrift/0.0.9/gunicorn_thrift-0.0.9-py2-none-any.whl

---
+++ packages/gunicorn-thrift/gunicorn_thrift-0.0.9-py2-none-any.whl/gunicorn_thrift/sync_worker.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+try:
+    import thrift
+except ImportError:
+    raise RuntimeError("You need thrift installed to use this worker.")
+
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+
+from gunicorn.workers.sync import SyncWorker
+
+
+class SyncThriftWorker(SyncWorker):
+    def get_thrift_transports_and_protos(self, result):
+        itrans = self.app.tfactory.getTransport(result)
+        otrans = self.app.tfactory.getTransport(result)
+        iprot = self.app.pfactory.getProtocol(itrans)
+        oprot = self.app.pfactory.getProtocol(otrans)
+
+        return (itrans, otrans), (iprot, oprot)
+
+    def handle(self, listener, client, addr):
+        if self.app.cfg.thrift_client_timeout is not None:
+            client.settimeout(self.app.cfg.thrift_client_timeout)
+
+        result = TSocket.TSocket()
+        result.setHandle(client)
+
+        try:
+            (itrans, otrans), (iprot, oprot) = \
+                self.get_thrift_transports_and_protos(result)
+
+            try:
+                while True:
+                    self.app.thrift_app.process(iprot, oprot)
+                    self.notify()
+            except TTransport.TTransportException:
+                pass
+        except Exception as e:
+            logging.exception(e)
+        finally:
+            itrans.close()
+            otrans.close()
+
+    def handle_exit(self, sig, frame):
+        ret = super(SyncThriftWorker, self).handle_exit(sig, frame)
+        self.cfg.worker_term(self)
+        return ret
