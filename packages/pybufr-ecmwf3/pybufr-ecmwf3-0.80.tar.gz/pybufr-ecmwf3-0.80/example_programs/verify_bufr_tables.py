d42f5fc759962c4900eabb9f01b5db5ca503eb7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:12 +0000

    Add package pybufr-ecmwf3/0.80/pybufr-ecmwf3-0.80.tar.gz

---
+++ packages/pybufr-ecmwf3/pybufr-ecmwf3-0.80.tar.gz/pybufr-ecmwf3-0.80/example_programs/verify_bufr_tables.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+this small example program loads the BUFR B- and D-tables
+and reports any inconsistencies it finds in the table definitions
+"""
+
+import sys
+from pybufr_ecmwf.bufr_table import BufrTable
+
+if len(sys.argv) < 3:
+    print('please give 2 BUFR TABLE files as argument')
+    sys.exit(1)
+
+BTABLE_FILE = sys.argv[1]
+DTABLE_FILE = sys.argv[2]
+
+#BT = bufr.BufrTable(tables_dir="my_BUFR_TABLES")
+#BT = bufr.BufrTable(tables_dir="../ecmwf_bufrtables")
+BT = BufrTable()
+BT.load(BTABLE_FILE)
+BT.load(DTABLE_FILE)
---
+++ packages/pybufr-ecmwf3/pybufr-ecmwf3-0.80.tar.gz/pybufr-ecmwf3-0.80/pybufr_ecmwf/__init__.py
@@ -0,0 +1,27 @@
+"""A module to allow access (read/write/composing) to the
+BUFR format as defined by the WMO, by interfacing to the
+Fortran BUFR library made available by ECMWF.
+
+Author:  J. de Kloe, KNMI
+Created: 04-Feb-2010
+"""
+
+#__all__ = ['bufr_template',
+#           'raw_bufr_file',
+#           'bufr_interface_ecmwf',
+#           'bufr',
+#           'helpers']
+
+#try:
+#    import pybufr_ecmwf.ecmwfbufr
+#except ImportError as imperr:
+#    print "===="
+#    print "Sorry, this module needs to be build before you can use it !"
+#    print "===="
+#    raise imperr
+
+#from pybufr_ecmwf.bufr_template import *
+#from pybufr_ecmwf.raw_bufr_file import *
+#from pybufr_ecmwf.bufr_interface_ecmwf import *
+#from pybufr_ecmwf.bufr import *
+
