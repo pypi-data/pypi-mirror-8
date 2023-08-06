5b400a9ab9e6653855c98fe3a8b5396672d9c192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:49 +0000

    Add package vr.builder/1.0.5/vr.builder-1.0.5.tar.gz

---
+++ packages/vr.builder/vr.builder-1.0.5.tar.gz/vr.builder-1.0.5/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='vr.builder',
+    namespace_packages=['vr'],
+    version='1.0.5',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'vr.runners>=2.4,<3',
+        'PyYAML==3.10',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vbuild = vr.builder.main:main',
+        ]
+    },
+    description=('Command line tools to build apps in containers.'),
+)
