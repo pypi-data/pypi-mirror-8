50584a11653b8f2e422c7aa2a4f2c949f41910be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:17 +0000

    Add package django-viewgroups/0.1.8/django-viewgroups-0.1.8.tar.gz

---
+++ packages/django-viewgroups/django-viewgroups-0.1.8.tar.gz/django-viewgroups-0.1.8/viewsets/mixins/base.py
@@ -0,0 +1,44 @@
+from copy import deepcopy
+from django.core.urlresolvers import reverse
+
+
+class SessionDataMixin(object):
+    session_prefix = "data"
+
+    def get_session_prefix(self):
+        return "{}:{}".format(self.session_prefix, type(self).__name__)
+
+#     def get_used_parameters(self, *args):
+#         return args
+
+    def get_data(self):
+        prefix = self.get_session_prefix()
+
+        data = getattr(self, "___stored_data", None)
+        if data is None:
+            data = self.request.session.get(prefix, {})
+            items = self.request.GET.copy()
+            items.pop("page", 1)
+
+            # clear
+            cleared = False
+            if "_clear" in items:
+                data = {}
+                cleared = True
+                items.pop("_clear", None)
+
+            # remove specific keys
+            if "_remove" in items:
+                for remove in items.get("_remove", "").split(","):
+                    if remove in data:
+                        del data[remove]
+                items.pop("_clear", None)
+
+            if len(items.keys()) > 0 or cleared:
+                for n, v in items.items():
+                    data[n] = v
+                self.request.session[prefix] = data
+
+            self.___stored_data = data
+
+        return data.copy()
