7c046c581e9b3f6a9dd86e4c8f95617f9d82c839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:46 +0000

    Add package upyun/2.2.5/upyun-2.2.5.tar.gz

---
+++ packages/upyun/upyun-2.2.5.tar.gz/upyun-2.2.5/setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+import sys
+import upyun
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='upyun',
+    version=upyun.__version__,
+    description='UpYun Storage SDK for Python',
+    license='License :: OSI Approved :: MIT License',
+    platforms='Platform Independent',
+    author='Monkey Zhang (timebug)',
+    author_email='timebug.info@gmail.com',
+    url='https://github.com/upyun/python-sdk',
+    packages=['upyun'],
+    keywords=['upyun', 'python', 'sdk'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
---
+++ packages/upyun/upyun-2.2.5.tar.gz/upyun-2.2.5/upyun/__init__.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__ = 'upyun'
+__version__ = '2.2.5'
+__author__ = 'Monkey Zhang (timebug)'
+__license__ = 'MIT License: http://www.opensource.org/licenses/mit-license.php'
+__copyright__ = 'Copyright 2014 UPYUN'
+
+from .upyun import UpYun, UpYunServiceException, UpYunClientException
+from .upyun import ED_AUTO, ED_TELECOM, ED_CNC, ED_CTT
+
+__all__ = [
+    'UpYun', 'UpYunServiceException', 'UpYunClientException',
+    'ED_AUTO', 'ED_TELECOM', 'ED_CNC', 'ED_CTT'
+]
