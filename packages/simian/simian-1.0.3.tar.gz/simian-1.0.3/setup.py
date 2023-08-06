bea3ed3e62f4d23197e1d331c1ad30da16852d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:09 +0000

    Add package simian/1.0.3/simian-1.0.3.tar.gz

---
+++ packages/simian/simian-1.0.3.tar.gz/simian-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='simian',
+    version='1.0.3',
+    packages=('simian',),
+    url='https://github.com/themattrix/python-simian',
+    license='MIT',
+    author='Matthew Tardiff',
+    author_email='mattrix@gmail.com',
+    install_requires=('mock', 'contextlib2'),
+    tests_require=('nose', 'flake8'),
+    description=(
+        'A decorator for easily mocking out multiple dependencies by '
+        'monkey-patching.'),
+    classifiers=(
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'))
