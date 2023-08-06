5d6757113d2a1393cb1adcc7d47853dedb010b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:10 +0000

    Add package pyshould/0.6.2/pyshould-0.6.2.tar.gz

---
+++ packages/pyshould/pyshould-0.6.2.tar.gz/pyshould-0.6.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyshould',
+    description='Should style asserts based on pyhamcrest',
+    version='0.6.2',
+    url='https://github.com/drslump/pyshould',
+    author='Ivan -DrSlump- Montes',
+    author_email='drslump@pollinimini.net',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['pyhamcrest'],
+    tests_require=['nose','pyhamcrest'],
+    test_suite="nose.collector",
+    zip_safe=False,
+    )
