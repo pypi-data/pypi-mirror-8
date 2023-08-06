595879f16bbe603c1f4cd60eb99eb0bd72e05dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:57 +0000

    Add package neovim/0.0.23/neovim-0.0.23.tar.gz

---
+++ packages/neovim/neovim-0.0.23.tar.gz/neovim-0.0.23/setup.py
@@ -0,0 +1,29 @@
+import platform
+import sys
+
+from setuptools import setup
+
+install_requires = [
+    'msgpack-python',
+]
+
+if sys.version_info < (3, 4):
+    # trollius is just a backport of 3.4 asyncio module
+    install_requires.append('trollius')
+
+if not platform.python_implementation() == 'PyPy':
+    # pypy already includes an implementation of the greenlet module
+    install_requires.append('greenlet')
+
+setup(name='neovim',
+      version='0.0.23',
+      description='Python client to neovim',
+      url='http://github.com/neovim/python-client',
+      download_url='https://github.com/neovim/python-client/archive/0.0.23.tar.gz',
+      author='Thiago de Arruda',
+      author_email='tpadilha84@gmail.com',
+      license='MIT',
+      packages=['neovim', 'neovim.api', 'neovim.msgpack_rpc',
+                'neovim.msgpack_rpc.event_loop', 'neovim.plugins'],
+      install_requires=install_requires,
+      zip_safe=False)
