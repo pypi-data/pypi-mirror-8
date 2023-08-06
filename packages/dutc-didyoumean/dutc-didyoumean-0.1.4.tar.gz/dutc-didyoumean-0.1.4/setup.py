a1311773347137728385fd7f11eeddc2af9d1b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:00 +0000

    Add package dutc-didyoumean/0.1.4/dutc-didyoumean-0.1.4.tar.gz

---
+++ packages/dutc-didyoumean/dutc-didyoumean-0.1.4.tar.gz/dutc-didyoumean-0.1.4/setup.py
@@ -0,0 +1,22 @@
+# didyoumean - "Did You Mean?" Functionality on AttributeError 
+
+from setuptools import setup, find_packages, Extension
+from os.path import join, dirname
+
+didyoumean = Extension('didyoumean', 
+                       sources=['src/didyoumean.c', 
+                                'src/didyoumean-safe.c'],
+                       include_dirs=['src'], 
+                       depends=['src/didyoumean-safe.h'])
+
+setup(
+    name='dutc-didyoumean',
+    version='0.1.4',
+    description='"Did You Mean?" on AttributeError',
+    long_description=''.join(open(join(dirname(__file__),'README.md'))),
+    author='James Powell',
+    author_email='james@dontusethiscode.com',
+    url='https://github.com/dutc/didyoumean',
+    packages=find_packages(exclude=['*demos*']),
+    ext_modules=[didyoumean],
+)
