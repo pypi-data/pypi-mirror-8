5dc1e4d346f9181359c5f60d75cb1c084807f57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:34 +0000

    Add package korpokkur/0.0.8.1/korpokkur-0.0.8.1.tar.gz

---
+++ packages/korpokkur/korpokkur-0.0.8.1.tar.gz/korpokkur-0.0.8.1/korpokkur/scaffolds/package/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+from zope.interface import implementer
+from korpokkur.interfaces import IScaffoldTemplate
+import os.path
+
+
+# see: korpokkur.interfaces:IScaffoldTemplate
+@implementer(IScaffoldTemplate)
+class Package(object):
+    """sample python package scaffold extensions=[nose, pytest]"""
+    source_directory = os.path.join(os.path.abspath(os.path.dirname(__file__)), "+package+")
+    # varname -> description, default
+    expected_words = {
+        "package": ("package name", "sample"),
+        "description": ("package description", "-"),
+        "version": ("version number for project", "0.0")
+    }
+    __dro__ = ["korpokkur.scaffolds.pygitignore:Package"]  # todo:validation
+    template_engine = "mako"
+    support_extensions = ["unittest", "nose", "pytest"]  # default is unittest
