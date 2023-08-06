8a3056fd48b6e5ca19bcc29d5c1c88e9abd0d7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:02 +0000

    Add package autotag/0.1/autotag-0.1.tar.gz

---
+++ packages/autotag/autotag-0.1.tar.gz/autotag-0.1/setup.py
@@ -0,0 +1,16 @@
+'''
+Created on Dec 29, 2014
+
+@author: jordan
+'''
+from setuptools import setup
+
+setup(name='autotag',
+      version='0.1',
+      description='Tag suggestion according to text ',
+      url='https://github.com/jvalansi/autotag',
+      author='Jordan Valansi',
+      author_email='jvalansi_autotag@gmail.com',
+      license='MIT',
+      packages=['autotag'],
+      zip_safe=False)
