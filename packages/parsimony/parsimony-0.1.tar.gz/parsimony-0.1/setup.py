2627f0a2970a004428260eeaa8c42729ced3f420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:58 +0000

    Add package parsimony/0.1/parsimony-0.1.tar.gz

---
+++ packages/parsimony/parsimony-0.1.tar.gz/parsimony-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+import parsimony
+setup(
+    name="parsimony",
+    version=parsimony.__version__,
+    packages=find_packages(),
+    install_requires=['dill>=0.2.1']
+)
