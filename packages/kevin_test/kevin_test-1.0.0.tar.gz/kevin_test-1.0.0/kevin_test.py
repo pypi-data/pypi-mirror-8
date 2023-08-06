6a7b156f40303435d51c552971bfcaa5fec3ef0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:35 +0000

    Add package kevin_test/1.0.0/kevin_test-1.0.0.tar.gz

---
+++ packages/kevin_test/kevin_test-1.0.0.tar.gz/kevin_test-1.0.0/kevin_test.py
@@ -0,0 +1,4 @@
+def print_sequence():
+	for i in range(9):
+		print(i)
+
---
+++ packages/kevin_test/kevin_test-1.0.0.tar.gz/kevin_test-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'kevin_test',
+	version	= '1.0.0',
+	py_modules	=['kevin_test'],
+	author	= 'depeng',
+	author_email = 'wangdepeng5215@gmail.com',
+	url	= 'http://www.heafirstlabs.com',
+	description = 'A test',
+)
