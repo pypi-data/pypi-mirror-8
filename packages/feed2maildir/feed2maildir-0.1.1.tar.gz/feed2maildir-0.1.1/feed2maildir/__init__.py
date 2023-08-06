d3b7415026c3d24d7f0b88094a78f7dae05f18c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:22 +0000

    Add package feed2maildir/0.1.1/feed2maildir-0.1.1.tar.gz

---
+++ packages/feed2maildir/feed2maildir-0.1.1.tar.gz/feed2maildir-0.1.1/feed2maildir/__init__.py
@@ -0,0 +1,5 @@
+VERSION = '0.1.1'
+
+import feed2maildir.main
+main = feed2maildir.main.main
+
---
+++ packages/feed2maildir/feed2maildir-0.1.1.tar.gz/feed2maildir-0.1.1/feed2maildir/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from feed2maildir.loader import Loader
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    main()
+
