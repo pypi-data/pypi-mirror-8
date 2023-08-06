b70b089a0bf59ce134f8a8e48e738fd50fd7fb30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:03 +0000

    Add package django-earthdistance/0.6/django-earthdistance-0.6.tar.gz

---
+++ packages/django-earthdistance/django-earthdistance-0.6.tar.gz/django-earthdistance-0.6/setup.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-earthdistance',
+    version='0.6',
+    install_requires=[
+        'djorm-ext-core>=0.7',
+        'djorm-ext-expressions>=0.5'],
+    url='https://github.com/jneight/django-earthdistance',
+    description='Add support for PostgreSQL earthdistance extension to Django',
+    long_description=open("README.rst").read(),
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache 2.0',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    author='Javier Cordero Martinez',
+    author_email='jcorderomartinez@gmail.com'
+)
