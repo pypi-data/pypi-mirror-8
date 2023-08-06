3ad3a184fad8fbd4346fc12a0102cb9ec20b9a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:58 +0000

    Add package paymill-jsonobject/0.7.0beta/paymill-jsonobject-0.7.0beta.tar.gz

---
+++ packages/paymill-jsonobject/paymill-jsonobject-0.7.0beta.tar.gz/paymill-jsonobject-0.7.0beta/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import sys
+setup(
+    name='paymill-jsonobject',
+    version='0.7.0beta',
+    author='Aleksandar Yalnazov',
+    author_email='aleksandar.yalnazov@paymill.de',
+    description='This is a fork of jsonobject library and is only intended to be used temporary until jsonobject 0.7.0 release is out.',
+    long_description='This is a fork of jsonobject library and is only intended to be used temporary until jsonobject 0.7.0 release is out.',
+    url='https://github.com/dannyroberts/jsonobject',
+    packages=['jsonobject'],
+    install_requires=['six'],
+    tests_require=['unittest2'] if sys.version_info[0] == 2 else [],
+    test_suite='test',
+)
