bcd8cec327071cf7966a9c63f30d7f5039f954d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package certdata/0.1/certdata-0.1.tar.gz

---
+++ packages/certdata/certdata-0.1.tar.gz/certdata-0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os.path
+
+from setuptools import setup
+
+setup(
+    name='certdata',
+    version='0.1',
+    description='certdata.txt parser',
+    author='Alexey V Gorshkov',
+    author_email='animus@wayround.org',
+    url='https://github.com/AnimusPEXUS/certdata',
+    packages=[
+        'certdata'
+        ]
+    )
