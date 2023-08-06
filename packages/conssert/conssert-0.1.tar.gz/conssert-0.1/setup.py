588e078f5faf49b5a3ac3b67f4050abe3532a19a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:22 +0000

    Add package conssert/0.1/conssert-0.1.tar.gz

---
+++ packages/conssert/conssert-0.1.tar.gz/conssert-0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="conssert",
+    version="0.1",
+    description="Content assertion library for Python",
+    url="https://github.com/podio/conssert",
+    download_url="https://github.com/podio/conssert/tarball/0.1",
+    author="Juan Alvarez",
+    author_email="juan.afernandez@ymail.com",
+    platforms=["any"],
+    keywords=["validation", "test", "unit test", "content assertion"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
