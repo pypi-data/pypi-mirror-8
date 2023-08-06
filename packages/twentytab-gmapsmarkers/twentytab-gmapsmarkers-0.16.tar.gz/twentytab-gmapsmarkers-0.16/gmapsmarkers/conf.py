0e613f64ee170c6f529b089509fe9d85276ca5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:51 +0000

    Add package twentytab-gmapsmarkers/0.16/twentytab-gmapsmarkers-0.16.tar.gz

---
+++ packages/twentytab-gmapsmarkers/twentytab-gmapsmarkers-0.16.tar.gz/twentytab-gmapsmarkers-0.16/gmapsmarkers/conf.py
@@ -0,0 +1,60 @@
+from appconf import AppConf
+from django.conf import settings
+from django.utils.translation import get_language
+
+
+class GmapsConf(AppConf):
+    STATIC_URL = u'/static/'
+    JQUERY_LIB = u"{}{}".format(
+        getattr(settings, u'STATIC_URL', u'/static/'),
+        u"gmapsmarkers/js/jquery-2.1.0.min.js"
+    )
+    SELECT2_LIB = u"{}{}".format(
+        getattr(settings, u'STATIC_URL', u'/static/'),
+        u"gmapsmarkers/select2-3.4.8/select2.min.js"
+    )
+    SELECT2_CSS_LIB = u"{}{}".format(
+        getattr(settings, u'STATIC_URL', u'/static/'),
+        u"gmapsmarkers/select2-3.4.8/select2.css"
+    )
+    LANGUAGE_CODE = get_language()
+    GMAPS_LANGUAGE_CODE = u'en'
+
+    def configure_static_url(self, value):
+        if not getattr(settings, 'STATIC_URL', None):
+            self._meta.holder.STATIC_URL = value
+            return value
+        return getattr(settings, 'STATIC_URL')
+
+    def configure_jquery_lib(self, value):
+        if not getattr(settings, 'JQUERY_LIB', None):
+            self._meta.holder.JQUERY_LIB = value
+            return value
+        return getattr(settings, 'JQUERY_LIB')
+
+    def configure_select2_lib(self, value):
+        if not getattr(settings, 'SELECT2_LIB', None):
+            self._meta.holder.SELECT2_LIB = value
+            return value
+        return getattr(settings, 'SELECT2_LIB')
+
+    def configure_select2_css_lib(self, value):
+        if not getattr(settings, 'SELECT2_CSS_LIB', None):
+            self._meta.holder.SELECT2_CSS_LIB = value
+            return value
+        return getattr(settings, 'SELECT2_CSS_LIB')
+
+    def configure_language_code(self, value):
+        if not getattr(settings, 'LANGUAGE_CODE', None):
+            self._meta.holder.LANGUAGE_CODE = value
+            return value
+        return getattr(settings, 'LANGUAGE_CODE')
+
+    def configure_gmaps_language_code(self, value):
+        if not getattr(settings, 'GMAPS_LANGUAGE_CODE', None):
+            self._meta.holder.GMAPS_LANGUAGE_CODE = value
+            return value
+        return getattr(settings, 'GMAPS_LANGUAGE_CODE')
+
+    class Meta:
+        prefix = ""
---
+++ packages/twentytab-gmapsmarkers/twentytab-gmapsmarkers-0.16.tar.gz/twentytab-gmapsmarkers-0.16/gmapsmarkers/fields.py
@@ -0,0 +1,67 @@
+from . import conf
+from django.conf import settings
+from django.db import models
+from django import forms
+from gmapsmarkers.widgets import GmapsSelectAutocomplete, GeotypeSelect
+
+
+class GmapsField(models.CharField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = kwargs.pop("max_length", 250)
+        self.plugin_options = kwargs.pop("plugin_options", {})
+        self.select2_options = kwargs.pop("select2_options", {})
+        self.language_code = kwargs.pop(
+            "language_code", settings.GMAPS_LANGUAGE_CODE)
+        super(GmapsField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        self.plugin_options['gmaps_field_name'] = self.name
+        defaults = {
+            'form_class': GmapsFormField,
+            'plugin_options': self.plugin_options,
+            'select2_options': self.select2_options,
+            'language_code': self.language_code
+        }
+        defaults.update(kwargs)
+        return super(GmapsField, self).formfield(**defaults)
+
+
+class GmapsFormField(forms.CharField):
+
+    def __init__(self, plugin_options={}, select2_options={},
+                 language_code=settings.GMAPS_LANGUAGE_CODE, *args, **kwargs):
+        kwargs.update({'widget': GmapsSelectAutocomplete(
+            plugin_options=plugin_options, select2_options=select2_options,
+            language_code=language_code
+        )})
+        super(GmapsFormField, self).__init__(*args, **kwargs)
+
+
+class GeotypeField(models.CharField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = kwargs.pop("max_length", 250)
+        super(GeotypeField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': GeotypeFormField
+        }
+        defaults.update(kwargs)
+        return super(GeotypeField, self).formfield(**defaults)
+
+
+class GeotypeFormField(forms.CharField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs.update({'widget': GeotypeSelect})
+        super(GeotypeFormField, self).__init__(*args, **kwargs)
+
+# Fix field for South
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^gmapsmarkers\.fields\.GmapsField"])
+    add_introspection_rules([], ["^gmapsmarkers\.fields\.GeotypeField"])
+except:
+    pass
