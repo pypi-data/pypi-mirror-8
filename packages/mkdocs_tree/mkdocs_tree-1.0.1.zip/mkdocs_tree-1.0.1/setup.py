debd4ae5861dad3d88d4897c0909101c49322fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:25 +0000

    Add package mkdocs_tree/1.0.1/mkdocs_tree-1.0.1.zip

---
+++ packages/mkdocs_tree/mkdocs_tree-1.0.1.zip/mkdocs_tree-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mkdocs_tree',
+    version = '1.0.1',
+    author = 'Eric Hulser',
+    author_email = 'eric.hulser@projexsoftware.com',
+    maintainer = 'Eric Hulser',
+    maintainer_email = 'eric.hulser@projexsoftware.com',
+    description = 'Tree extension for the mkdocs documentation system.',
+    license = 'LGPL',
+    keywords = '',
+    url = 'https://github.com/erichulser/mkdocs_autodoc',
+    include_package_data=True,
+    packages = find_packages(),
+    install_requires = ['mkdocs'],
+    classifiers=[],
+)
