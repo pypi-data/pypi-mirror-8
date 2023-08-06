d658b6c2a856ec3457e6a6a71d7e024a152fb47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:40 +0000

    Add package EulerMods/0.1.1/EulerMods-0.1.1.zip

---
+++ packages/EulerMods/EulerMods-0.1.1.zip/EulerMods-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='EulerMods',
+    version='0.1.1',
+    author='Ryan Cox',
+    author_email='arconyx6@gmail.com',
+    url='https://pypi.python.org/pypi/EulerMods/',
+    packages=['mods'],
+    license='LICENSE.txt',
+    description='Modules to aid in Project Euler calculations though functions for common tasks.',
+    long_description=open('README.txt').read(),
+)
