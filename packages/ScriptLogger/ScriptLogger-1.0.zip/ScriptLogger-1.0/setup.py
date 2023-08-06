199e25d3b049c57755b771fc71900640a5c68da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:04 +0000

    Add package ScriptLogger/1.0/ScriptLogger-1.0.zip

---
+++ packages/ScriptLogger/ScriptLogger-1.0.zip/ScriptLogger-1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = "ScriptLogger",
+    version = "1.0",
+    author = "FourOhFour",
+    author_email = "404.fourohfour@gmail.com",
+    url = "https://github.com/fourohfour/ScriptLogger",
+    description = "A simple tool that helps with logging info during the process of running a script.",
+    py_modules = ['scriptlogger'],
+    classifiers = ["Development Status :: 4 - Beta",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Topic :: Software Development :: Debuggers",
+                   "Operating System :: OS Independent",
+                   "Intended Audience :: Developers"]
+    )
