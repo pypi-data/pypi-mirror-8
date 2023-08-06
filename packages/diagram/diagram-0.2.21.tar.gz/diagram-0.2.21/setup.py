553e6a22c45ea60b05f6527ac956489bd5728133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:52 +0000

    Add package diagram/0.2.21/diagram-0.2.21.tar.gz

---
+++ packages/diagram/diagram-0.2.21.tar.gz/diagram-0.2.21/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import os
+
+
+setup(
+    name='diagram',
+    version='0.2.21',
+    description='Text mode diagrams using UTF-8 characters and fancy colors',
+    author='Wijnand Modderman-Lenstra',
+    author_email='maze@pyth0n.org',
+    py_modules=['diagram'],
+    scripts=[os.path.join('bin', 'diagram')],
+)
