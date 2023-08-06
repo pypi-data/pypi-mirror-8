6976f592b883b9584329d65d4a1d1d5a996ae0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:59 +0000

    Add package malibu/0.1.0-5/malibu-0.1.0-5.tar.gz

---
+++ packages/malibu/malibu-0.1.0-5.tar.gz/malibu-0.1.0-5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+import malibu
+
+setup(
+    name = 'malibu',
+    version = malibu.__version__,
+    description = "maiome's library of utilities",
+
+    url = "http://phabricator.maio.me/tag/malibu",
+    author = "Sean Johnson",
+    author_email = "sean.johnson@maio.me",
+    
+    license = "Unlicense",
+    
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "License :: Public Domain",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    packages = ['malibu',
+                'malibu.config',
+                'malibu.connection',
+                'malibu.database',
+                'malibu.util'],
+    package_dir = {'malibu': 'malibu'},
+    zip_safe = True
+)
