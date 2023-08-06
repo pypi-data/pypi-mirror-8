93edc96559578e8e94884d185e6d7e2edd7a0997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:20 +0000

    Add package Yahoo-ticker-downloader/0.4.0/Yahoo-ticker-downloader-0.4.0.tar.gz

---
+++ packages/Yahoo-ticker-downloader/Yahoo-ticker-downloader-0.4.0.tar.gz/Yahoo-ticker-downloader-0.4.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Yahoo-ticker-downloader',
+    version = '0.4.0',
+    author = 'Benny',
+    author_email = 'Benny@GMX.it',
+    url='https://github.com/Benny-/Yahoo-ticker-symbol-downloader',
+    license='Public Domain',
+    keywords = "market finance yahoo ticker stock stocks etf future futures index mutualfund currency warrant bond bonds".split(),
+    description='A web scraper for ticker symbols from yahoo finance',
+    long_description = open('README.rst').read(),
+    packages = find_packages(),
+    scripts = ['YahooTickerDownloader.py'],
+    install_requires=[
+        "beautifulsoup4 >= 4.2.1",
+        "requests >= 2.2.1",
+        "tablib >= 0.9.11",
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Environment :: Console",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "License :: Public Domain",
+    ],
+)
+
