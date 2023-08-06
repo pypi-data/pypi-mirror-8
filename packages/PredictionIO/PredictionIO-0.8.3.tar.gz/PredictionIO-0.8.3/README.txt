c53b79c886b5550fb3b2d39fd90df148a756e373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:02 +0000

    Add package PredictionIO/0.8.3/PredictionIO-0.8.3.tar.gz

---
+++ packages/PredictionIO/PredictionIO-0.8.3.tar.gz/PredictionIO-0.8.3/README.txt
@@ -0,0 +1,63 @@
+PredictionIO Python SDK
+=======================
+
+
+Installation
+============
+
+To install the module from PyPI, you may
+
+    $ pip install predictionio
+
+or
+
+    $ easy_install predictionio
+
+If you have cloned the repository and want to install directly from there,
+do the following in the root directory of the repository:
+
+    $ python setup.py install
+
+This will install the "predictionio" module to your Python distribution.
+
+
+Usage
+=====
+
+To use predictionio in your Python script,
+
+    >>> import predictionio
+
+
+Documentation
+=============
+
+The latest documentation is located at http://pythonhosted.org/PredictionIO/
+
+If you want to build a local copy, please install Sphinx
+(http://sphinx-doc.org/install.html).
+
+To generate documentation,
+
+    % cd docs
+    % make html
+
+To view the generated documentation, go to docs/build/html/index.html.
+
+
+SUPPORT
+=======
+
+
+Forum
+-----
+
+https://groups.google.com/group/predictionio-user
+
+
+Issue Tracker
+-------------
+
+https://predictionio.atlassian.net
+
+If you are unsure whether a behavior is an issue, bringing it up in the forum is highly encouraged.
---
+++ packages/PredictionIO/PredictionIO-0.8.3.tar.gz/PredictionIO-0.8.3/setup.py
@@ -0,0 +1,54 @@
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+__author__ = "The PredictionIO Team"
+__email__ = "help@tappingstone.com"
+__copyright__ = "Copyright 2014, TappingStone, Inc."
+__license__ = "Apache License, Version 2.0"
+
+setup(
+    name='PredictionIO',
+    version="0.8.3",
+    author=__author__,
+    author_email=__email__,
+    packages=['predictionio'],
+    url='http://prediction.io',
+    license='LICENSE.txt',
+    description='PredictionIO Python SDK',
+    classifiers=[
+      'Programming Language :: Python',
+      'License :: OSI Approved :: Apache Software License',
+      'Operating System :: OS Independent',
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Science/Research',
+      'Environment :: Web Environment',
+      'Topic :: Internet :: WWW/HTTP',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Scientific/Engineering :: Information Analysis',
+      'Topic :: Software Development :: Libraries :: Python Modules'],
+    long_description="""PredictionIO Python SDK
+
+                       PredictionIO is a prediction server for building smart
+                       applications. While you search data through a database
+                       server, you can make prediction through PredictionIO.
+
+                       With PredictionIO, you can write apps
+
+                       - that predict user behaviors based on solid data
+                         science
+                       - using your choice of state-of-the-art machine
+                         learning algorithms
+                       - without worrying about scalability
+
+                       Detailed documentation is available on our
+                       `documentation site <http://docs.prediction.io>`_.
+
+                       This module provides convenient access of the
+                       PredictionIO API to Python programmers so that they
+                       can focus on their application logic.
+                       """,
+    install_requires=["pytz >= 2014.2",],
+    )
