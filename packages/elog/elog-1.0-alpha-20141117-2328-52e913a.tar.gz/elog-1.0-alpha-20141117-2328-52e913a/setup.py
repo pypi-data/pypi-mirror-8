ec4ee4b0152c0eb9b23c5d83457e4589236e250c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:05 +0000

    Add package elog/1.0-alpha-20141117-2328-52e913a/elog-1.0-alpha-20141117-2328-52e913a.tar.gz

---
+++ packages/elog/elog-1.0-alpha-20141117-2328-52e913a.tar.gz/elog-1.0-alpha-20141117-2328-52e913a/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="elog",
+        version="1.0",
+        url="https://github.com/yandex-sysmon/elog",
+        license="LGPLv3",
+        author="Devaev Maxim",
+        author_email="mdevaev@gmail.com",
+        description="ElasticSearch logging handler and tools",
+        platforms="any",
+
+        packages=[
+            "elog",
+        ],
+
+        classifiers=[  # http://pypi.python.org/pypi?:action=list_classifiers
+            "Development Status :: 2 - Pre-Alpha",
+            "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Topic :: System :: Networking :: Monitoring",
+            "Topic :: System :: Logging",
+        ],
+
+        install_requires=[
+            "elasticsearch",
+            "requests",
+        ],
+    )
