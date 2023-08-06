084661538dc273a17de890d1460125f9528e1d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:49 +0000

    Add package django-hmin/0.3/django-hmin-0.3.tar.gz

---
+++ packages/django-hmin/django-hmin-0.3.tar.gz/django-hmin-0.3/hmin/base.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+try:
+    import re2 as re
+except ImportError:
+    import re
+
+flags = re.S | re.I
+PLACEHOLDER = '<@!hmin_placeholder_%s_!@>'
+RE_REPLACED_TAG = re.compile(
+    ur'(<(script|textarea|style|pre).*?>.*?</(script|textarea|style|pre)>)',
+    flags
+)
+RE_COMMENTS = re.compile(ur'<!--(?!\[if.*?\]).*?-->', flags)
+RE_PLACEHOLDER = re.compile(
+    ur'%s' % PLACEHOLDER.replace('%s', '([0-9]+)')
+)
+
+
+def minify(content, remove_comments=True):
+    # helpers
+    def tag_replace(m):
+        safe_storage[ m.start()] = m.group(1)
+        return PLACEHOLDER %  m.start()
+
+    def tag_return(m):
+        return safe_storage[int(m.group(1))]
+
+    # decode unicode
+    try:
+        content = content.decode('utf8')
+    except (UnicodeDecodeError, UnicodeEncodeError):
+        pass
+
+    # replace dangerous tags with placeholders
+    safe_storage = dict()
+    content = RE_REPLACED_TAG.sub(tag_replace, content)
+
+    # fast version of "\s{2,} to \s"
+    content = ' '.join(content.split())
+    # "strip_space_between_tags"
+    content = content.replace('> <', '><')
+    if remove_comments:
+        content = RE_COMMENTS.sub('', content)
+
+    # and return dangerous tags back
+    return RE_PLACEHOLDER.sub(tag_return, content).strip()
