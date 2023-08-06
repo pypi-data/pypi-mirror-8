7d0c3b5bbb8fab035cead61d7dae4f7442866a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:06 +0000

    Add package stoqdrivers/0.9.22/stoqdrivers-0.9.22.tar.gz

---
+++ packages/stoqdrivers/stoqdrivers-0.9.22.tar.gz/stoqdrivers-0.9.22/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# Setup file for StoqDrivers
+# Code by Async Open Source <http://www.async.com.br>
+
+from kiwi.dist import setup, listpackages, listfiles
+
+from stoqdrivers import __version__
+
+
+with open('requirements.txt') as f:
+    install_requires = [l.strip() for l in f.readlines() if
+                        l.strip() and not l.startswith('#')]
+
+setup(
+    name="stoqdrivers",
+    version=".".join(map(str, __version__)),
+    author="Async Open Source",
+    author_email="stoq-devel@async.com.br",
+    description="Python fiscal printer (ECF) drivers",
+    long_description=("This package provices device drivers "
+                      "for fiscal printers, ECF (Emissor de Coupon Fiscal) "
+                      "written in Python. Supports printers from Bematech, "
+                      "Daruma, Dataregis, Perto, Sweda and the generic "
+                      "FiscNET protocol."),
+    url="http://www.stoq.com.br",
+    license="GNU LGPL 2.1 (see COPYING)",
+    packages=listpackages('stoqdrivers'),
+    data_files=[("$datadir/conf", listfiles("stoqdrivers/conf", "*.ini"))],
+    global_resources=dict(conf="$datadir/conf"),
+    resources=dict(locale="$prefix/share/locale"),
+    install_requires=install_requires,
+)
