d98b8ae29a51cebc31332b648875c1ebecc0b7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:54 +0000

    Add package malss/0.0.1/malss-0.0.1.zip

---
+++ packages/malss/malss-0.0.1.zip/malss-0.0.1/malss/malss.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import types
+from jinja2 import Environment, FileSystemLoader
+from sklearn.cross_validation import StratifiedKFold, KFold
+from sklearn.utils import shuffle as sk_shuffle
+from sklearn.grid_search import GridSearchCV
+from sklearn.learning_curve import learning_curve
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.svm import SVC, LinearSVC, SVR
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import f1_score
+from sklearn.datasets import fetch_mldata
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression, Ridge, SGDRegressor
+
+from algorithm import Algorithm
+from data import Data
+
+
+class MALSS(object):
+    def __init__(self, X, y, task, shuffle=True, n_jobs=1, random_state=0,
+                 verbose=True):
+        """
+        Set the given training data.
+
+        Parameters
+        ----------
+        X : {numpy.ndarray, pandas.DataFrame}, shape = [n_samples, n_features]
+            Training vector, where n_samples in the number of samples and
+            n_features is the number of features.
+        y : {numpy.ndarray, pandas.Series}, shape = [n_samples]
+            Target values (class labels in classification, real numbers in
+            regression)
+        task : string
+            Specifies the task of the analysis. It must be one of
+            'classification', 'regression'.
+        shuffle : boolean, optional (default=True)
+            Whether to shuffle the data.
+        n_jobs : integer, optional (default=1)
+            The number of jobs to run in parallel. If -1, then the number of
+            jobs is set to the number of cores.
+        random_state : int seed, RandomState instance, or None (default=0)
+            The seed of the pseudo random number generator
+        verbos : bool, default: True
+            Enable verbose output.
+        """
+
+        self.data = Data(X, y, shuffle, random_state)
+        self.task = task
+        self.n_jobs = n_jobs
+        self.random_state = random_state
+        self.verbose = verbose
+        self.algorithms = self.__choose_algorithm()
+        if task == 'classification':
+            self.scoring = 'f1'
+            self.cv = StratifiedKFold(self.data.y, n_folds=5, shuffle=True,
+                                      random_state=self.random_state)
+        elif task == 'regression':
+            self.scoring = 'r2'
+            self.cv = KFold(self.data.X.shape[0], n_folds=5, shuffle=True,
+                            random_state=self.random_state)
+        else:
+            raise ValueError('task:%s is not supported' % task)
+
+        env = Environment(loader=FileSystemLoader('template', encoding='utf8'))
+        self.tmpl = env.get_template('report.html.tmp')
+
+    def __choose_algorithm(self):
+        algorithms = []
+        if self.task == 'classification':
+            if self.data.X.shape[0] * self.data.X.shape[1] <= 1e+06:
+                if self.data.X.shape[0] ** 2 * self.data.X.shape[1] <= 1e+09:
+                    algorithms.append(
+                        Algorithm(
+                            SVC(random_state=self.random_state),
+                            [{'kernel': ['rbf'],
+                              'C': [10, 100, 1000, 10000],
+                              'gamma': [1e-4, 1e-3, 1e-2, 1e-1]}],
+                            'Support Vector Machine (RBF Kernel)'))
+                algorithms.append(
+                    Algorithm(
+                        LinearSVC(random_state=self.random_state),
+                        [{'C': [0.1, 1, 10, 100]}],
+                        'Support Vector Machine (Linear Kernel)'))
+                algorithms.append(
+                    Algorithm(
+                        LogisticRegression(random_state=self.random_state),
+                        [{'penalty': ['l2', 'l1'],
+                          'C': [0.1, 0.3, 1, 3, 10],
+                          'class_weight': [None, 'auto']}],
+                        'Logistic Regression'))
+                algorithms.append(
+                    Algorithm(
+                        DecisionTreeClassifier(random_state=self.random_state),
+                        [{'max_depth': [3, 5, 7, 9, 11]}],
+                        'Decision Tree'))
+                algorithms.append(
+                    Algorithm(
+                        KNeighborsClassifier(),
+                        [{'n_neighbors': [2, 6, 10, 14, 18]}],
+                        'k-Nearest Neighbors'))
+            else:
+                algorithms.append(
+                    Algorithm(
+                        SGDClassifier(
+                            random_state=self.random_state,
+                            n_jobs=self.n_jobs),
+                        [{'loss': ['hinge', 'log'],
+                          'penalty': ['l2', 'l1'],
+                          'alpha': [1e-05, 3e-05, 1e-04, 3e-04, 1e-03],
+                          'class_weight': [None, 'auto']}],
+                        'SGD Classifier'))
+        if self.task == 'regression':
+            if self.data.X.shape[0] * self.data.X.shape[1] <= 1e+06:
+                if self.data.X.shape[0] ** 2 * self.data.X.shape[1] <= 1e+09:
+                    algorithms.append(
+                        Algorithm(
+                            SVR(random_state=self.random_state),
+                            [{'kernel': ['rbf'],
+                              'C': [10, 100, 1000, 10000],
+                              'gamma': [1e-4, 1e-3, 1e-2, 1e-1]}],
+                            'Support Vector Machine (RBF Kernel)'))
+                algorithms.append(
+                    Algorithm(
+                        Ridge(),
+                        [{'alpha':
+                            [0.01, 0.1, 1, 10, 100]}],
+                        'Ridge Regression'))
+            else:
+                algorithms.append(
+                    Algorithm(
+                        SGDRegressor(
+                            random_state=self.random_state),
+                        [{'penalty': ['l2', 'l1'],
+                          'alpha': [1e-05, 3e-05, 1e-04, 3e-04, 1e-03]}],
+                        'SGD Regressor'))
+        return algorithms
+
+    def execute(self):
+        self.__tune_parameters()
+        if self.task == 'classification':
+            self.__report_classification_result()
+
+    def __search_best_algorithm(self):
+        best_score = float('-Inf')
+        best_index = -1
+        for i in xrange(len(self.algorithms)):
+            if self.algorithms[i].best_score > best_score:
+                best_score = self.algorithms[i].best_score
+                best_index = i
+        self.algorithms[best_index].is_best_algorithm = True
+
+    def __tune_parameters(self):
+        for i in xrange(len(self.algorithms)):
+            estimator = self.algorithms[i].estimator
+            parameters = self.algorithms[i].parameters
+            sc = f1score if self.scoring == 'f1' else self.scoring
+            clf = GridSearchCV(
+                estimator, parameters, cv=self.cv, scoring=sc,
+                n_jobs=self.n_jobs)
+            clf.fit(self.data.X, self.data.y)
+            self.algorithms[i].estimator = clf.best_estimator_
+            self.algorithms[i].best_score = clf.best_score_
+            self.algorithms[i].best_params = clf.best_params_
+            self.algorithms[i].grid_scores = clf.grid_scores_
+
+        self.__search_best_algorithm()
+
+    def __report_classification_result(self):
+        for i in xrange(len(self.algorithms)):
+            est = self.algorithms[i].estimator
+            self.algorithms[i].classification_report =\
+                classification_report(self.data.y, est.predict(self.data.X))
+
+    def __plot_learning_curve(self, dname=None):
+        for alg in self.algorithms:
+            estimator = alg.estimator
+            train_sizes, train_scores, test_scores = learning_curve(
+                estimator,
+                self.data.X,
+                self.data.y,
+                cv=self.cv,
+                n_jobs=self.n_jobs)
+            train_scores_mean = np.mean(train_scores, axis=1)
+            train_scores_std = np.std(train_scores, axis=1)
+            test_scores_mean = np.mean(test_scores, axis=1)
+            test_scores_std = np.std(test_scores, axis=1)
+
+            plt.figure()
+            plt.title(estimator.__class__.__name__)
+            plt.xlabel("Training examples")
+            plt.ylabel("Score")
+            plt.grid()
+
+            plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                             train_scores_mean + train_scores_std, alpha=0.1,
+                             color="r")
+            plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                             test_scores_mean + test_scores_std,
+                             alpha=0.1, color="g")
+            plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+                     label="Training score")
+            plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+                     label="Cross-validation score")
+
+            plt.legend(loc="lower right")
+            if dname is not None and not os.path.exists(dname):
+                os.mkdir(dname)
+            if dname is not None:
+                plt.savefig('%s/learning_curve_%s.png' %
+                            (dname, estimator.__class__.__name__),
+                            bbox_inches='tight', dpi=75)
+            else:
+                plt.savefig('learning_curve_%s.png' %
+                            estimator.__class__.__name__,
+                            bbox_inches='tight', dpi=75)
+            plt.close()
+
+    def make_report(self, dname='report'):
+        """
+        Make the report
+
+        Parameters
+        ----------
+        dname : string (default="report")
+            A string containing a path to a output directory.
+        """
+
+        if not os.path.exists(dname):
+            os.mkdir(dname)
+
+        self.__plot_learning_curve(dname)
+
+        html = self.tmpl.render(algorithms=self.algorithms,
+                                scoring=self.scoring,
+                                taks=self.task,
+                                data=self.data,
+                                verbose=self.verbose).encode('utf-8')
+        fo = open(dname + '/report.html', 'w')
+        fo.write(html)
+        fo.close()
+
+
+def f1score(estimator, X, y):
+    return f1_score(y, estimator.predict(X), average=None).mean()
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/malss/malss-0.0.1.zip/malss-0.0.1/malss/test.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+from sklearn.datasets.samples_generator import make_classification,\
+    make_regression
+from malss import MALSS
+import pandas as pd
+
+
+def test_classification_2classes_small():
+    X, y = make_classification(n_samples=1000,
+                               n_features=10,
+                               n_classes=2,
+                               n_informative=2,
+                               n_redundant=0,
+                               n_repeated=0,
+                               weights=[0.7, 0.3],
+                               random_state=0)
+    X = pd.DataFrame(X)
+    y = pd.Series(y)
+    cls = MALSS(X, y, 'classification', n_jobs=3)
+    cls.execute()
+    cls.make_report('test_classification_2classes_small')
+
+
+def test_classification_multiclass_small():
+    X, y = make_classification(n_samples=1000,
+                               n_features=20,
+                               n_classes=3,
+                               n_informative=10,
+                               weights=[0.6, 0.2, 0.2],
+                               random_state=0)
+    X = pd.DataFrame(X)
+    y = pd.Series(y)
+    cls = MALSS(X, y, 'classification', n_jobs=3)
+    cls.execute()
+    cls.make_report('test_classification_multiclass_small')
+
+
+def test_classification_2classes_medium():
+    X, y = make_classification(n_samples=100000,
+                               n_features=10,
+                               n_classes=2,
+                               n_informative=2,
+                               n_redundant=0,
+                               n_repeated=0,
+                               weights=[0.7, 0.3],
+                               random_state=0)
+    X = pd.DataFrame(X)
+    y = pd.Series(y)
+    cls = MALSS(X, y, 'classification', n_jobs=3)
+    cls.execute()
+    cls.make_report('test_classification_2classes_medium')
+
+
+def test_classification_2classes_big():
+    X, y = make_classification(n_samples=200000,
+                               n_features=20,
+                               n_classes=2,
+                               n_informative=3,
+                               weights=[0.7, 0.3],
+                               random_state=0)
+    X = pd.DataFrame(X)
+    y = pd.Series(y)
+    cls = MALSS(X, y, 'classification', n_jobs=3)
+    cls.execute()
+    cls.make_report('test_classification_2classes_big')
+
+
+def test_regression_small():
+    X, y = make_regression(n_samples=2000,
+                           n_features=10,
+                           n_informative=5,
+                           noise=30.0,
+                           random_state=0)
+    X = pd.DataFrame(X)
+    y = pd.Series(y)
+    cls = MALSS(X, y, 'regression', n_jobs=3)
+    cls.execute()
+    cls.make_report('test_regression_small')
+
+
+def test_regression_medium():
+    X, y = make_regression(n_samples=20000,
+                           n_features=10,
+                           n_informative=5,
+                           noise=30.0,
+                           random_state=0)
+    X = pd.DataFrame(X)
+    y = pd.Series(y)
+    cls = MALSS(X, y, 'regression', n_jobs=3)
+    cls.execute()
+    cls.make_report('test_regression_medium')
+
+
+def test_regression_big():
+    X, y = make_regression(n_samples=200000,
+                           n_features=10,
+                           n_informative=5,
+                           noise=30.0,
+                           random_state=0)
+    X = pd.DataFrame(X)
+    y = pd.Series(y)
+    cls = MALSS(X, y, 'regression', n_jobs=3)
+    cls.execute()
+    cls.make_report('test_regression_big')
+
+
+def test_classification_categorical():
+    import os
+
+    if not os.path.exists('data/heart.csv'):
+        if not os.path.exists('data'):
+            os.mkdir('data')
+
+        data = pd.read_csv('http://www-bcf.usc.edu/~gareth/ISL/Heart.csv',
+                           index_col=0, na_values=[''])
+        data.to_csv('data/heart.csv', na_rep='')
+    else:
+        data = pd.read_csv('data/heart.csv', na_values=[''])
+
+    y = data['AHD']
+    del data['AHD']
+
+    cls = MALSS(data, y, 'classification', n_jobs=3)
+    cls.execute()
+    cls.make_report('test_classification_categorical')
+
+
+if __name__ == "__main__":
+    # test_classification_categorical()
+    test_classification_2classes_small()
