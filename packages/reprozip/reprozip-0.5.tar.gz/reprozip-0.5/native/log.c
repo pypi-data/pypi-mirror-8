77912ca5fa19373791670c58e4157db7449a87a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:13 +0000

    Add package reprozip/0.5/reprozip-0.5.tar.gz

---
+++ packages/reprozip/reprozip-0.5.tar.gz/reprozip-0.5/native/log.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <time.h>
+
+#include "log.h"
+
+
+void log_real_(pid_t tid, const char *tag, const char *format, ...)
+{
+    va_list args;
+    char datestr[13]; /* HH:MM:SS.mmm */
+    va_start(args, format);
+    {
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        strftime(datestr, 13, "%H:%M:%S", localtime(&tv.tv_sec));
+        sprintf(datestr+8, ".%03u", (unsigned int)(tv.tv_usec / 1000));
+    }
+    fprintf(stderr, "[REPROZIP] %s %s: ", datestr, tag);
+    if(tid > 0)
+        fprintf(stderr, "[%d] ", tid);
+    vfprintf(stderr, format, args);
+    va_end(args);
+}
