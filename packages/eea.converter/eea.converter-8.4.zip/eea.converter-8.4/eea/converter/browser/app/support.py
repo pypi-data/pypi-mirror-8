cb30906065067654b1b3d3b7da5e106f8550bf85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:26 +0000

    Add package eea.converter/8.4/eea.converter-8.4.zip

---
+++ packages/eea.converter/eea.converter-8.4.zip/eea.converter-8.4/eea/converter/browser/app/support.py
@@ -0,0 +1,14 @@
+""" PDF Support
+"""
+from zope.interface import implementer
+from Products.Five.browser import BrowserView
+from eea.converter.interfaces import ISupport
+
+@implementer(ISupport)
+class Support(BrowserView):
+    """ PDF Support
+    """
+    def can_download(self):
+        """ Override this adapter in order to restrict access to PDF download
+        """
+        return True
