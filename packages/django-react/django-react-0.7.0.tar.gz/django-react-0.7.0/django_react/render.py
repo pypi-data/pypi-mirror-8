95e928dc075c510507ea524c285e0cdb72e3b608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package django-react/0.7.0/django-react-0.7.0.tar.gz

---
+++ packages/django-react/django-react-0.7.0.tar.gz/django-react-0.7.0/django_react/render.py
@@ -0,0 +1,50 @@
+import os
+import tempfile
+from django_node import npm, node
+from .settings import PATH_TO_RENDERER, NPM_VERSION_REQUIRED, NODE_VERSION_REQUIRED, DEBUG
+from .exceptions import RenderingError
+
+
+# Ensure that the external dependencies are met
+node.ensure_version_gte(NODE_VERSION_REQUIRED)
+npm.ensure_version_gte(NPM_VERSION_REQUIRED)
+
+# Ensure that the required packages have been installed
+npm.install(os.path.dirname(__file__))
+
+
+def render_component(path_to_source, serialized_props=None, to_static_markup=None):
+    if serialized_props is None:
+        serialized_props = '{}'
+
+    if to_static_markup is None:
+        render_to = 'string'
+    else:
+        render_to = 'static'
+
+    with tempfile.NamedTemporaryFile() as serialized_props_file:
+        serialized_props_file.write(serialized_props)
+        serialized_props_file.flush()
+
+        arguments = (
+            PATH_TO_RENDERER,
+            '--path-to-source', path_to_source,
+            '--render-to', render_to,
+            '--serialized-props-file', serialized_props_file.name,
+        )
+
+        # While rendering templates Django will silently ignore some types of exceptions,
+        # so we need to intercept them and raise our own class of exception
+        try:
+            stderr, stdout = node.run(*arguments, production=DEBUG)
+        except (TypeError, AttributeError) as e:
+            raise RenderingError(e.__class__.__name__, *e.args)
+
+        if stderr:
+            raise RenderingError(stderr)
+
+        # `console.log` appends a new line to the render output
+        if stdout.endswith('\n'):
+            stdout = stdout[:-1]
+
+        return stdout
---
+++ packages/django-react/django-react-0.7.0.tar.gz/django-react-0.7.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+VERSION = '0.7.0'
+
+setup(
+    name='django-react',
+    version=VERSION,
+    packages=['django_react'],
+    package_data={
+        'django_react': [
+            'render.js',
+            'package.json',
+            'templates/django_react/*.html',
+            'tests/*.py',
+            'tests/test_components/*.jsx',
+            'tests/test_components/*.js',
+        ]
+    },
+    install_requires=[
+        'django',
+        'django-node >= 2.1.0',
+        'django-webpack >= 2.0.0',
+    ],
+    description='Render and bundle React components from a Django application',
+    long_description='Documentation at https://github.com/markfinger/django-react',
+    author='Mark Finger',
+    author_email='markfinger@gmail.com',
+    url='https://github.com/markfinger/django-react',
+)
