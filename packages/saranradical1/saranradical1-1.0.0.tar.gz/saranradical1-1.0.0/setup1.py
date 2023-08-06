336e893db262160cc29205d3fc13af1962183634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:36 +0000

    Add package saranradical1/1.0.0/saranradical1-1.0.0.tar.gz

---
+++ packages/saranradical1/saranradical1-1.0.0.tar.gz/saranradical1-1.0.0/setup1.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name        = 'saranradical1',
+      version     = '1.0.0',
+      py_modules  = ['saranradical1'],
+      author      = 'sarankumar',
+      author_email= 'saranradical@gmail.com',
+      url         = 'http://www.headfirstlabs.com',
+      description = 'list printer',
+)
