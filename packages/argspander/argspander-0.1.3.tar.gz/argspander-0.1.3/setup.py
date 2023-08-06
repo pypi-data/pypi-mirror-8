51f0ddb8b9c7e4267f58685fdddbb1e5cefbb88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:32 +0000

    Add package argspander/0.1.3/argspander-0.1.3.tar.gz

---
+++ packages/argspander/argspander-0.1.3.tar.gz/argspander-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.rst", "r").read()
+
+setup(
+    name="argspander",
+    version="0.1.3",
+    license="MIT",
+    description="Argument expansion of objects",
+    long_description=long_description,
+    author="Richard Lancaster",
+    author_email="lancasterrich@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords="expansion splat argument unpacking"
+)
