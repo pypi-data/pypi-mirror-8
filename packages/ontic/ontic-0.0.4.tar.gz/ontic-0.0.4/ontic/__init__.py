1c4f5aa65deaf66c85fc67d190142ab103de3927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:10 +0000

    Add package ontic/0.0.4/ontic-0.0.4.tar.gz

---
+++ packages/ontic/ontic-0.0.4.tar.gz/ontic-0.0.4/ontic/__init__.py
@@ -0,0 +1,4 @@
+"""Package for creating objects and corresponding schema."""
+from ontic import meta_type, ontic_type, schema_type, validation_exception
+
+__all__ = ['meta_type', 'ontic_type', 'schema_type', 'validation_exception']
---
+++ packages/ontic/ontic-0.0.4.tar.gz/ontic-0.0.4/setup.py
@@ -0,0 +1,28 @@
+"""The setuptools setup file."""
+from setuptools import setup
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+setup(
+    name='ontic',
+    version='0.0.4',
+    author='Raul Gonzalez',
+    author_email='mindbender@gmail.com',
+    url='https://github.com/neoinsanity/ontic',
+    license='Apache License 2.0',
+    description='Qualities and Quantities are the stuff of Objects.',
+    long_description=long_description,
+    packages=['ontic',],
+    install_requires=[],
+    include_package_data = True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+    ]
+)
