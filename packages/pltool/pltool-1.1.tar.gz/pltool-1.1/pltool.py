34aa41e98da4e2e863bc2d76aad0de502d33c080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:32 +0000

    Add package pltool/1.1/pltool-1.1.tar.gz

---
+++ packages/pltool/pltool-1.1.tar.gz/pltool-1.1/pltool.py
@@ -0,0 +1,5 @@
+#test python
+def plist(lname):
+	for each_item in lname:
+		print("TAG::"+each_item)
+
---
+++ packages/pltool/pltool-1.1.tar.gz/pltool-1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='pltool',
+	version='1.1',
+	py_modules=['pltool'],
+	author='arthur',
+	author_email='arthur4it@gmail.com',
+	url='http://home.ustc.edu.cn/~arthury',
+	description='a simple test on python',
+	)
