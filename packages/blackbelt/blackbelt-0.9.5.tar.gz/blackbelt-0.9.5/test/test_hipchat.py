cc3cdd2182857edcf4a3fd856e1d40fe40817336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:40 +0000

    Add package blackbelt/0.9.5/blackbelt-0.9.5.tar.gz

---
+++ packages/blackbelt/blackbelt-0.9.5.tar.gz/blackbelt-0.9.5/test/test_hipchat.py
@@ -0,0 +1,26 @@
+from nose.tools import assert_equals, assert_raises
+from nose.plugins.skip import SkipTest
+
+import os
+
+from blackbelt.hipchat import Client
+
+"""
+Integration exploratory playground for CircleCI.
+Don't really treat it as a test, but it may still be useful for you.
+"""
+
+
+class TestBuildInfoRetrieval(object):
+
+    def setUp(self):
+        try:
+            self.client = Client()
+        except ValueError:
+            raise SkipTest()
+
+        if os.environ.get('FORCE_DISCOVERY') != '1':
+            raise SkipTest()
+
+    def test_posting_message(self):
+        self.client.post_message("Testing HipChat Messages in Black Belt Exploratory Test Suite")
