cc570892643e5cf8b0f4c3cd43872930cbba31ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:06 +0000

    Add package thinc/1.69/thinc-1.69.tar.gz

---
+++ packages/thinc/thinc-1.69.tar.gz/thinc-1.69/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import sys
+import os
+from os import path
+from glob import glob
+import subprocess
+
+
+from setuptools import setup
+from distutils.core import Extension
+import shutil
+
+
+pwd = os.path.dirname(__file__)
+
+includes = ['.', path.join(sys.prefix, 'include')]
+
+
+libs = []
+
+
+compile_args = []
+link_args = []
+
+
+exts = [
+    Extension('thinc.learner', ['thinc/learner.cpp'],
+              language="c++",
+              include_dirs=includes,
+              extra_compile_args=['-O3'] + compile_args,
+              extra_link_args=['-O3'] + link_args),
+    Extension('thinc.weights', ['thinc/weights.cpp'],
+              language="c++",
+              include_dirs=includes,
+              extra_compile_args=['-O3'] + compile_args,
+              extra_link_args=['-O3'] + link_args),
+    Extension("thinc.features", ["thinc/features.cpp"],
+              language="c++", include_dirs=includes,
+              extra_compile_args=compile_args,
+              extra_link_args=link_args),
+    Extension("thinc.search", ["thinc/search.cpp"], language="c++"),
+    Extension("thinc.cache", ["thinc/cache.cpp"], include_dirs=includes, language="c++"),
+    #Extension("tests.c_test_search", ["tests/c_test_search.cpp"], include_dirs=includes, language="c++")
+]
+
+
+if sys.argv[1] == 'clean':
+    print >> sys.stderr, "cleaning .c, .c++ and .so files matching sources"
+    map(clean, exts)
+
+
+setup(
+    name='thinc',
+    packages=['thinc'],
+    version='1.69',
+    description="Learn sparse linear models",
+    author='Matthew Honnibal',
+    author_email='honnibal@gmail.com',
+    url="http://github.com/syllog1sm/thinc",
+    package_data={"thinc": ["*.pyx", "*.pxd", "*.pxi"]},
+    ext_modules=exts,
+    install_requires=["murmurhash", "cymem", "preshed"],
+    setup_requires=["headers_workaround"]
+)
+
+
+import headers_workaround
+import sys
+
+
+headers_workaround.install_headers('murmurhash')
