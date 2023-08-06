27181cb6d4c9d56b3c03a940c48b4e7ebc42bdf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:07 +0000

    Add package redisjobs/0.4.1/redisjobs-0.4.1.tar.gz

---
+++ packages/redisjobs/redisjobs-0.4.1.tar.gz/redisjobs-0.4.1/redisjobs/base.py
@@ -0,0 +1,12 @@
+from functools import partial
+import redis
+
+
+class StrictRedis(redis.StrictRedis):
+    def __init__(self, *vargs, **kwargs):
+        super(StrictRedis, self).__init__(*vargs, **kwargs)
+        commands = self.hgetall('commands')
+
+        for command, sha in commands.items():
+            method = partial(self.evalsha, sha)
+            setattr(self, command, method)
