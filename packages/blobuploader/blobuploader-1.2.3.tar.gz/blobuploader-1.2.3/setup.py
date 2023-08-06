3457907662e91522b73233c1342902ad27e2f2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:19 +0000

    Add package blobuploader/1.2.3/blobuploader-1.2.3.tar.gz

---
+++ packages/blobuploader/blobuploader-1.2.3.tar.gz/blobuploader-1.2.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name="blobuploader",
+      description="Command-line client for uploading blobs to the Mozilla [blobber] server.",
+      long_description=readme,
+      version="1.2.3",
+      author="Mihai Tabara",
+      author_email="mtabara@mozilla.com",
+      url="https://github.com/catlee/blobber",
+      scripts=["blobberc.py"],
+      license="MPL",
+      install_requires=required,
+      packages=['blobuploader'],
+      package_dir={'blobuploader': 'blobuploader'},
+      package_data={'blobuploader': ['*.pem']},
+      include_package_data=True,
+      )
