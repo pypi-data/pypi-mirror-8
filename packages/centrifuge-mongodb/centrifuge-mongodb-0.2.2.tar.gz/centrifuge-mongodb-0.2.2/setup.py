8f0b59d3746b4f8bf17b92d1bff44466851f66f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:00 +0000

    Add package centrifuge-mongodb/0.2.2/centrifuge-mongodb-0.2.2.tar.gz

---
+++ packages/centrifuge-mongodb/centrifuge-mongodb-0.2.2.tar.gz/centrifuge-mongodb-0.2.2/setup.py
@@ -0,0 +1,51 @@
+import os
+import sys
+from setuptools import setup
+
+
+if sys.argv[-1] == 'test':
+    status = os.system('python tests/tests.py')
+    sys.exit(1 if status > 127 else status)
+
+
+requirements = [
+    'centrifuge',
+    'six==1.3.0',
+    'motor==0.1.2'
+]
+
+
+def long_description():
+    return "MongoDB structure backend for Centrifuge"
+
+
+setup(
+    name='centrifuge-mongodb',
+    version='0.2.2',
+    description="MongoDB structure backend for Centrifuge",
+    long_description=long_description(),
+    url='https://github.com/centrifugal/centrifuge-mongodb',
+    download_url='https://github.com/centrifugal/centrifuge-mongodb',
+    author="Alexandr Emelin",
+    author_email='frvzmb@gmail.com',
+    license='MIT',
+    packages=['centrifuge_mongodb'],
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        'Topic :: System :: Networking',
+        'Topic :: Terminals',
+        'Topic :: Text Processing',
+        'Topic :: Utilities'
+    ]
+)
