827474c4a049dfeea9f805636fd4248f10c1f535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:41 +0000

    Add package pycantonese/0.1/pycantonese-0.1.tar.gz

---
+++ packages/pycantonese/pycantonese-0.1.tar.gz/pycantonese-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pycantonese',
+    version='0.1',
+    description='Working with Cantonese corpus data using Python',
+    url='http://pycantonese.github.io/',
+    author='Jackson L. Lee',
+    author_email='jsllee.phon@gmail.com',
+    license='Apache',
+    packages=['pycantonese'],
+    install_requires=['nltk'],
+    zip_safe=False)
