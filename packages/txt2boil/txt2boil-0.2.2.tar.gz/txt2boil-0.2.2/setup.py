544f1112618a2e99c31151b2a356e0294daca25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:19 +0000

    Add package txt2boil/0.2.2/txt2boil-0.2.2.tar.gz

---
+++ packages/txt2boil/txt2boil-0.2.2.tar.gz/txt2boil-0.2.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+import os
+import glob
+from boil.version import version
+
+with open(glob.glob('README.*')[0]) as f:
+    long_description = f.read()
+
+setup(
+    name='txt2boil', license='MIT', version=version,
+
+    description='A configurable boilerplate generator.',
+    long_description=long_description,
+
+    author='Kieran Colford', author_email='colfordk@gmail.com',
+    maintainer='Kieran Colford', maintainer_email='colfordk@gmail.com',
+
+    url='https://github.com/kcolford/bp',
+    download_url='',            # TODO
+
+    packages=['boil', 'boil.core'],
+    scripts=['txt2boil'],
+
+    platforms=['Any'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Code Generators',
+    ],
+)
