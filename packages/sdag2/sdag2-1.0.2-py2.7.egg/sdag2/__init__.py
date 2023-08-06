5e7affaa541bfbbae8c7ad64ff996a674d5f50a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:03 +0000

    Add package sdag2/1.0.2/sdag2-1.0.2-py2.7.egg

---
+++ packages/sdag2/sdag2-1.0.2-py2.7.egg/sdag2/__init__.py
@@ -0,0 +1,16 @@
+'''
+sdag2 - Python Simple Directed Acyclic Graph whith Cicle Detector and
+TopoloGical sorter utilities
+
+Created on Feb 15, 2013
+
+@author: Moises P Sena
+'''
+
+__author__ = 'Moises P Sena <moisespsena@gmail.com>'
+__license__ = "BSD"
+__version__ = "1.0"
+__maintainer__ = "Moises P. Sena"
+__email__ = "moisespsena@gmail.com"
+
+from .core import *
