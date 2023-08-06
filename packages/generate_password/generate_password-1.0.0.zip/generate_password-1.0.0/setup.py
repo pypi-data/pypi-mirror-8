30886c2b1066cd01ffca567831980340acea9cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package generate_password/1.0.0/generate_password-1.0.0.zip

---
+++ packages/generate_password/generate_password-1.0.0.zip/generate_password-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'generate_password',
+        version = '1.0.0',
+        py_modules = ['generate_password'],
+        author = 'jack',
+        author_email = 'lilingzj@163.com',
+        url = 'lilingzj.qq.com',
+        description = 'generate a password'
+    )
