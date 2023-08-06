4e52834146cddf1d23b5687bba8c35958c41639e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:54 +0000

    Add package cisco_vpn/0.0.3/cisco_vpn-0.0.3.macosx-10.10-x86_64.tar.gz

---
+++ packages/cisco_vpn/cisco_vpn-0.0.3.macosx-10.10-x86_64.tar.gz/Users/fdhuang/test/bin/vpn.py
@@ -0,0 +1,18 @@
+#!/usr/local/opt/python/bin/python2.7
+# -*- coding: utf-8 -*-
+
+"""Usage:
+  cli.py connect
+  cli.py disconnect
+"""
+
+from docopt import docopt
+
+from lib import connect
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='0.1.1rc')
+    if arguments['connect']:
+        connect.main()
+    if arguments['disconnect']:
+        connect.disconnect()
