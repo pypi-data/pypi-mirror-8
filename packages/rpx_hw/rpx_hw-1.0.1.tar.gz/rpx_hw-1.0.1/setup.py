7532211cd543db07b208719e71038863bc8c6203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:19 +0000

    Add package rpx_hw/1.0.1/rpx_hw-1.0.1.tar.gz

---
+++ packages/rpx_hw/rpx_hw-1.0.1.tar.gz/rpx_hw-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+		name='rpx_hw',
+		version='1.0.1',
+		py_modules=['rpx_hw'],
+		author='zrp007008009000',
+		author_email='zrp007008009000@gmail.com',
+		url='http://wwww.headfirstlabs.com',
+		description='A simple printer of hello world lists',
+	  )
