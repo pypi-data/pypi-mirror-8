cc5232d9a90e49408a98bf10014b63098b6aca1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:51 +0000

    Add package ares-util/0.1.4/ares_util-0.1.4.zip

---
+++ packages/ares-util/ares_util-0.1.4.zip/ares_util-0.1.4/ares_util/exceptions.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python
+# coding=utf-8
+
+from __future__ import unicode_literals
+
+
+class InvalidCompanyIDError(Exception):
+    pass
+
+
+class AresNoResponseError(Exception):
+    pass
