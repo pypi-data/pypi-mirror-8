636517debf690ab21f16baaf2b91dafb0703f160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:01 +0000

    Add package infi.pyvmomi_wrapper/0.1.5/infi.pyvmomi_wrapper-0.1.5-py2.7.egg

---
+++ packages/infi.pyvmomi_wrapper/infi.pyvmomi_wrapper-0.1.5-py2.7.egg/infi/pyvmomi_wrapper/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.5"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '885c1fbbeaeed2476f95a7cfb0a1617fabb8b144'
+__git_head_subject__ = 'Finished release v0.1.5.'
+__git_head_message__ = '* release/v0.1.5:\n  TRIVIAL logging InvokeMethod\n  HIPVM-640 removing unecessary log calls'
+__git_dirty_diff__ = ''
