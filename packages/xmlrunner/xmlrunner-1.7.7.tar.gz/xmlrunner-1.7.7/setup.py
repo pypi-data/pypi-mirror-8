b9a22b9c93ce65057fe4083a888bb9b153e4cdc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:20 +0000

    Add package xmlrunner/1.7.7/xmlrunner-1.7.7.tar.gz

---
+++ packages/xmlrunner/xmlrunner-1.7.7.tar.gz/xmlrunner-1.7.7/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+from xmlrunner.version import __version__
+
+setup(
+    name = 'xmlrunner',
+    version = __version__,
+    author = 'Daniel Fernandes Martins',
+    author_email = 'daniel.tritone@gmail.com',
+    description = 'PyUnit-based test runner with JUnit like XML reporting.',
+    license = 'LGPL',
+    platforms = ['Any'],
+    keywords = ['pyunit', 'unittest', 'junit xml', 'report', 'testrunner'],
+    url = 'https://github.com/pycontribs/xmlrunner',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing'
+    ],
+    packages = ['xmlrunner'],
+    provides = ['xmlrunner'],
+    zip_safe = True,
+    include_package_data = True,
+    test_suite = 'xmlrunner.tests.test_xmlrunner'
+)
