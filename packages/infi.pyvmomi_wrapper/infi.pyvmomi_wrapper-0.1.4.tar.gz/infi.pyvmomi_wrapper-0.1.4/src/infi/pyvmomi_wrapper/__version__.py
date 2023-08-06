0f689010ec58cbe136e453e1e54c82b765c1f7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:27 +0000

    Add package infi.pyvmomi_wrapper/0.1.4/infi.pyvmomi_wrapper-0.1.4.tar.gz

---
+++ packages/infi.pyvmomi_wrapper/infi.pyvmomi_wrapper-0.1.4.tar.gz/infi.pyvmomi_wrapper-0.1.4/src/infi/pyvmomi_wrapper/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.4"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.4)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '2ec3c27cc33d4aa136ca22e818e9032cd061a8b6'
+__git_head_subject__ = 'Finished release v0.1.4.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
