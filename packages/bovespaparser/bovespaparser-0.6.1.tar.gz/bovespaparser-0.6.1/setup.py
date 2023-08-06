1cb635725a0da5cf5e8fa6058359f21a9a5e6126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:17 +0000

    Add package bovespaparser/0.6.1/bovespaparser-0.6.1.tar.gz

---
+++ packages/bovespaparser/bovespaparser-0.6.1.tar.gz/bovespaparser-0.6.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# Filename: setup.py
+
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+setup(name="bovespaparser",
+      version="0.6.1",
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      author="Roberto Haddock Lobo",
+      author_email="rhlobo+stockexperiments@gmail.com",
+      description="Bovespa's historical series files parser.",
+      long_description=read('README.md'),
+      license="MIT",
+      test_suite='bovespaparser.tests.bovespaparser_tests.TestBovespaParserFunctions',
+      url='http://pypi.python.org/pypi/bovespaparser',
+      keywords=["bovespa", "parser", "historical", "series", "cotahist", "stock"],
+      classifiers=["Programming Language :: Python",
+                   "Programming Language :: Python :: 2.7",
+                   "Development Status :: 4 - Beta",
+                   "Environment :: Other Environment",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: Financial and Insurance Industry",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Topic :: Office/Business :: Financial :: Investment"])
