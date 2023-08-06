720830207b89b394fd5629616e42c14ac8328301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:25 +0000

    Add package django-rstamper/0.0.2/django-rstamper-0.0.2.tar.gz

---
+++ packages/django-rstamper/django-rstamper-0.0.2.tar.gz/django-rstamper-0.0.2/rstamper/admin.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import os.path
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+import models as mymodels
+import forms as myforms
+
+
+class StampAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'start', 'end', 'customer', 'action')
+    search_fields = ('customer', 'action', )
+
+
+admin.site.register(mymodels.Stamp, StampAdmin)
+admin.site.register(mymodels.Customer)
