8823ef1808a4bd4210d4b3f8cde0a6a076ce75ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:25 +0000

    Add package infi.os-info/0.0.2/infi.os_info-0.0.2.tar.gz

---
+++ packages/infi.os-info/infi.os_info-0.0.2.tar.gz/infi.os_info-0.0.2/src/infi/os_info/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.2"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.0.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd3a798d54224e0e8fe557ebd8a702410528572b2'
+__git_head_subject__ = 'Finished release v0.0.2.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
