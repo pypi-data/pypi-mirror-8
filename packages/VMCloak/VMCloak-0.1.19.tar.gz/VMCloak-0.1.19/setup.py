bc64f55cd0fe544c3155a4a76324da3b85d07730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:12 +0000

    Add package VMCloak/0.1.19/VMCloak-0.1.19.tar.gz

---
+++ packages/VMCloak/VMCloak-0.1.19.tar.gz/VMCloak-0.1.19/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Copyright (C) 2014 Jurriaan Bremer.
+# This file is part of VMCloak - http://www.vmcloak.org/.
+# See the file 'docs/LICENSE.txt' for copying permission.
+
+from setuptools import setup
+
+
+setup(
+    name='VMCloak',
+    version='0.1.19',
+    author='Jurriaan Bremer',
+    author_email='jurriaanbremer@gmail.com',
+    packages=[
+        'vmcloak',
+        'vmcloak.data',
+        'vmcloak.data.bootstrap',
+        'vmcloak.data.hwconf',
+    ],
+    scripts=[
+        'bin/vmcloak',
+        'bin/vmcloak-clone',
+        'bin/vmcloak-deps',
+        'bin/vmcloak-gethwconf',
+        'bin/vmcloak-iptables',
+        'bin/vmcloak-killvbox',
+        'bin/vmcloak-removevms',
+        'bin/vmcloak-vboxnet0',
+    ],
+    url='http://vmcloak.org/',
+    license='GPLv3',
+    description='Automated Virtual Machine Generation and Cloaking '
+                'for Cuckoo Sandbox.',
+    include_package_data=True,
+    package_data={
+        'vmcloak.data': ['*.*'],
+        'vmcloak.data.bootstrap': ['*.*'],
+    },
+    install_requires=[
+        # Libraries to build the documentation.
+        'sphinx',
+
+        # Required on BSD systems for unknown reasons.
+        'jinja2',
+    ],
+)
