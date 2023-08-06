1900b82316d9dda19267def750b93e380fb7a5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:58 +0000

    Add package PbPython/0.1.9/PbPython-0.1.9.tar.gz

---
+++ packages/PbPython/PbPython-0.1.9.tar.gz/PbPython-0.1.9/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='PbPython',
+    version='0.1.9',
+    author='Michael Kunze',
+    author_email='michael.kunze@pandorabots.com',
+    packages=['pb_py'],
+    license='LICENSE.txt',
+    url="https://developer.pandorabots.com/docs",
+    description='Code for making API calls to Pandorabots server.',
+    install_requires=[
+        "requests >= 2.1.1",
+        ]
+)
