5440e56cb3ab51ddc71174ef6e7ec75bac1e90c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:18 +0000

    Add package feedzilla-api/0.1/feedzilla-api-0.1.tar.gz

---
+++ packages/feedzilla-api/feedzilla-api-0.1.tar.gz/feedzilla-api-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'feedzilla-api',
+    packages = ['pyfeed'], 
+    version = '0.1',
+    description = 'Unofficial Feedzilla Python API Wrapper',
+    author = 'Melvin Philips',
+    author_email = 'me@melvinphilips.com',
+    url = 'https://github.com/melvin0008/PyFeed',
+    download_url = 'https://github.com/melvin0008/PyFeed/tarball/0.1',
+    keywords = ['feedzilla-api', 'api', 'feedzilla','feedzilla-wrapper'], 
+    classifiers = [],  
+    )
