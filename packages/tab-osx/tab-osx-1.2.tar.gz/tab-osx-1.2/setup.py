c9e230385e738d6e1e2ff179eeb9cfa917e46281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:05 +0000

    Add package tab-osx/1.2/tab-osx-1.2.tar.gz

---
+++ packages/tab-osx/tab-osx-1.2.tar.gz/tab-osx-1.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+long_description = open('README').read()
+
+setup(
+    name='tab-osx',
+    version='1.2',
+    description='Opens a new OS X Terminal window in the current directory and '
+        'runs an optional command in it.',
+    long_description=long_description,
+    author='Martin Mahner',
+    author_email='martin@mahner.org',
+    url='https://github.com/bartTC/tab/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+    ],
+    packages=find_packages(),
+    scripts=['tab'],
+    include_package_data=True,
+)
