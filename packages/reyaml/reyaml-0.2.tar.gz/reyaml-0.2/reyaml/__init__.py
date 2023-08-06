8071646014f58f7b7bf68fc37635f6dae8436db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:10 +0000

    Add package reyaml/0.2/reyaml-0.2.tar.gz

---
+++ packages/reyaml/reyaml-0.2.tar.gz/reyaml-0.2/reyaml/__init__.py
@@ -0,0 +1,43 @@
+import yaml
+
+
+def load(raw):
+    '''Parses a YAML string, tolerating comments and inline comments, as well as
+    warning you about mixed tabs and spaces'''
+    lines = []
+    spaces = False
+    tabs = False
+
+    for line in raw.splitlines():
+        if line.lstrip().startswith('#'):
+            continue # a line that is a comment (doesn't begin with whitespace)
+        if 0 == len( line.strip() ):
+            continue # this is an empty line, skip it
+        if line.startswith('\t'):
+            tabs = True
+            line = line.replace('\t', '    ')
+        elif line.startswith('  '):
+            spaces = True
+        if spaces and tabs:
+            error = 'You mixed tabs and spaces. Use one or the other, but not both. Problematic line:\n'
+            error += line
+            raise SyntaxError(error)
+
+        #find lines that contain an unquoted URL with an anchor
+        if '#' in line:
+            for part in line.split():
+                if '://' in part and not part[0] in ['\'', '\"']:
+                    error = 'You have a line with a URL and a comment, the comment could be a URL anchor.\n'
+                    error += 'To avoid ambiguities, quote such URLs. Problematic line:\n'
+                    error += line
+                    raise SyntaxError(error)
+        lines.append(line)
+
+    return yaml.load('\n'.join(lines) )
+
+
+def load_from_file(path):
+    '''Load a YAML file and return its contents as a dictionary'''
+    with open(path, 'rb') as f:
+        raw = f.read()
+        return load(raw)
---
+++ packages/reyaml/reyaml-0.2.tar.gz/reyaml-0.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+LONG_DESC = '''Extends the syntax of YAML files to accomodate things that an administrator
+appreciates - comments inside a configuration file. Applies some additional sanity checks
+to the data and fails verbosely, providing a detailed error message.'''
+
+setup(
+    name = 'reyaml',
+    packages = ['reyaml'], # this must be the same as the name above
+    version = '0.2',
+    long_description=LONG_DESC,
+    description = 'Reader of humane YAML files',
+    author = 'Alex Railean',
+    author_email = 'ralienpp@gmail.com',
+    url = 'https://github.com/ralienpp/reyaml',
+    download_url = 'https://github.com/ralienpp/reyaml/tarball/0.2',
+    keywords = ['yaml', 'parser', 'configuration', 'config'],
+    license = 'BSD',
+    classifiers = [
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Natural Language :: English',
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License'
+      ],
+    install_requires=[
+        'PyYAML',
+      ]
+)
