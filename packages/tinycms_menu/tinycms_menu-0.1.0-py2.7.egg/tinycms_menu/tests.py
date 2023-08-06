a2cf736bd66e5e62b0eb035f89fba56621efd19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:09 +0000

    Add package tinycms_menu/0.1.0/tinycms_menu-0.1.0-py2.7.egg

---
+++ packages/tinycms_menu/tinycms_menu-0.1.0-py2.7.egg/tinycms_menu/tests.py
@@ -0,0 +1,32 @@
+﻿from django.test import TestCase
+from django.test.client import Client
+
+from models import *
+from views import *
+
+import datetime
+
+class ModellTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_model_normal(self):
+        pass
+
+
+class DummyRequest:
+    def __init__(self,user=None,GET={}):
+        self.user=user
+        self.GET=GET
+        self.POST={}
+        self.method="GET"
+
+class ViewTest(TestCase):
+    def setUp(self):
+        Dispatcher.clear()
+
+    def test_content(self):
+        pass
+
+
+
