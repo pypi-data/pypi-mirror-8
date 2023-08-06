c066629c067051af9bf5e95542ec878371482e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:00 +0000

    Add package fake2db/0.1.3/fake2db-0.1.3.tar.gz

---
+++ packages/fake2db/fake2db-0.1.3.tar.gz/fake2db-0.1.3/fake2db/fake2db.py
@@ -0,0 +1,104 @@
+import sys
+import argparse
+import socket
+import getpass
+import logging
+import subprocess
+import time
+
+from sqlite_handler import Fake2dbSqliteHandler
+from mysql_handler import Fake2dbMySqlHandler
+from postgresql_handler import Fake2dbPostgresqlHandler
+from mongodb_handler import Fake2dbMongodbHandler
+from helpers import fake2db_logger
+
+logger, extra_information = fake2db_logger()
+
+class InstanciateDBHandlerException(Exception):
+    '''An Exception at the instantiation of the handler '''
+
+
+def _postgresql_process_checkpoint():
+    '''this helper method checks if
+    postgresql server is available in the sys
+    if not fires up one
+    '''
+    try:
+        postgresql_check = subprocess.check_output("pgrep postgres", shell=True)
+    except:
+        logger.warning('Your postgresql server is offline, fake2db will try to launch it now!', extra=extra_information)
+        # close_fds = True argument is the flag that is responsible
+        # for Popen to launch the process completely independent
+        subprocess.Popen("postgres -D /usr/local/pgsql/data", close_fds=True, shell=True)
+        time.sleep(3)
+
+
+def _mysqld_process_checkpoint():
+    '''this helper method checks if 
+    mysql server is available in the sys
+    if not fires up one
+    '''
+    try:
+        mysqld_check = subprocess.check_output("pgrep mysqld", shell=True)
+    except:
+        logger.warning('Your mysql server is offline, fake2db will try to launch it now!', extra=extra_information)
+        # close_fds = True argument is the flag that is responsible
+        # for Popen to launch the process completely independent
+        subprocess.Popen("mysqld", close_fds=True, shell=True)
+        time.sleep(3)
+
+def _mongodb_process_checkpoint():
+    '''this helper method checks if 
+    mongodb server is available in the sys
+    if not fires up one
+    '''
+    try:
+        mongodb_check = subprocess.check_output("pgrep mongod", shell=True)
+    except:
+        logger.warning('Your mongodb server is offline, fake2db will try to launch it now!', extra=extra_information)
+        # close_fds = True argument is the flag that is responsible
+        # for Popen to launch the process completely independent
+        subprocess.Popen("mongod", close_fds=True, shell=True)
+        time.sleep(3)
+
+try:
+    fake_sqlite_handler = Fake2dbSqliteHandler()
+    fake_mysql_handler = Fake2dbMySqlHandler()
+    fake_postgresql_handler = Fake2dbPostgresqlHandler()
+    fake_mongodb_handler = Fake2dbMongodbHandler()
+except:
+    raise InstanciateDBHandlerException
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rows", help="Amount of rows desired per table")
+    parser.add_argument("--db", help="Db type for creation: sqlite, mysql, postgresql, mongodb, couchdb, to be expanded")
+    args = parser.parse_args()
+
+    if not args.rows or not args.db:
+        logger.error('Please use with --help argument for usage information!', extra=extra_information)
+
+    if args.rows:
+        if args.db:
+            logger.info('arguments found(rows and db), starting faking!!', extra=extra_information)
+            logger.warning('Rows argument : %s', args.rows, extra=extra_information)
+            logger.info('DB argument : %s', args.db, extra=extra_information)
+            
+            if args.db == 'sqlite':
+                fake_sqlite_handler.fake2db_sqlite_initiator(int(args.rows))
+            elif args.db == 'mysql':
+                _mysqld_process_checkpoint()
+                fake_mysql_handler.fake2db_mysql_initiator(int(args.rows))
+            elif args.db == 'postgresql':
+                _postgresql_process_checkpoint()
+                fake_postgresql_handler.fake2db_postgresql_initiator(int(args.rows))
+            elif args.db == 'mongodb':
+                _mongodb_process_checkpoint()
+                fake_mongodb_handler.fake2db_mongodb_initiator(int(args.rows))
+            else:
+                logger.error('Wrong arg for db parameter. Valid ones : sqlite - mysql - postgresql - mongodb', extra=extra_information)
+        else:
+            logger.error('Please use with --help argument for usage information!', extra=extra_information)
+            
+if __name__ == '__main__':
+    main()
---
+++ packages/fake2db/fake2db-0.1.3.tar.gz/fake2db-0.1.3/fake2db/postgresql_handler.py
@@ -0,0 +1,171 @@
+import sys
+import time
+import getpass
+import subprocess
+
+from helpers import fake2db_logger, str_generator
+
+logger, extra_information = fake2db_logger()
+d = extra_information
+
+try:
+    from faker import Factory
+except ImportError:
+    logger.error('faker package not found onto python packages, please run : \
+    pip install -r requirements.txt  \
+    on the root of the project')
+
+
+try:
+    import psycopg2
+except ImportError:
+    logger.error('psycopg2 package not found onto python packages, please run : \
+    pip install -r requirements.txt  \
+    on the root of the project')
+    sys.exit(0)
+
+
+class Fake2dbPostgresqlHandler():
+    faker = Factory.create()
+
+    def fake2db_postgresql_initiator(self, number_of_rows):
+        '''Main handler for the operation
+        '''
+        rows = number_of_rows
+        cursor, conn = self.database_caller_creator()
+
+        self.data_filler_simple_registration(rows, cursor, conn)
+        self.data_filler_detailed_registration(rows, cursor, conn)
+        self.data_filler_company(rows, cursor, conn)
+        self.data_filler_user_agent(rows, cursor, conn)
+        self.data_filler_customer(rows, cursor, conn)
+        cursor.close()
+        conn.close()
+
+    def database_caller_creator(self):
+        '''creates a postgresql db
+        returns the related connection object
+        which will be later used to spawn the cursor
+        '''
+        cursor = None
+        conn = None
+        username = getpass.getuser()
+        
+        try:
+            db = 'postgresql_' + str_generator(self)
+            subprocess.Popen("createdb --no-password --owner " + username + " " + db, shell=True)
+            time.sleep(1)
+            conn = psycopg2.connect("dbname=" + db + " user=" + username)
+            cursor = conn.cursor()
+            logger.warning('Database created and opened succesfully: %s' % db, extra=d)
+        except Exception as err:
+            logger.error(err, extra=d)
+
+        return cursor, conn
+
+    def data_filler_simple_registration(self, number_of_rows, cursor, conn):
+        '''creates and fills the table with simple regis. information
+        '''
+        
+        cursor.execute("CREATE TABLE simple_registration (id serial PRIMARY KEY, email varchar, password varchar);")
+        conn.commit()
+
+        try:
+            
+            for i in range(0, number_of_rows):
+                cursor.execute("INSERT INTO simple_registration "
+                               "(email, password) "
+                               "VALUES (%s, %s)", (self.faker.safe_email(), self.faker.md5(raw_output=False)))
+                conn.commit()
+                
+            logger.warning('simple_registration Commits are successful after write job!', extra=d)
+            
+        except Exception as e:
+            logger.error(e, extra=d)
+
+    def data_filler_detailed_registration(self, number_of_rows, cursor, conn):
+        '''creates and fills the table with detailed regis. information
+        '''
+        
+        cursor.execute(
+            "CREATE TABLE detailed_registration "
+            "(id serial PRIMARY KEY, email varchar, password varchar, "
+            "lastname varchar, name varchar, adress varchar, phone varchar);")
+        conn.commit()
+        try:
+            
+            for i in range(0, number_of_rows):
+                cursor.execute("INSERT INTO detailed_registration "
+                               "(email, password, lastname, name, adress, phone) "
+                               "VALUES (%s, %s, %s, %s, %s, %s)",
+                               (self.faker.safe_email(), self.faker.md5(raw_output=False), self.faker.last_name(), self.faker.name(), self.faker.address(), self.faker.phone_number()))
+                conn.commit()
+                
+            logger.warning('detailed_registration Commits are successful after write job!', extra=d)
+            
+        except Exception as e:
+            logger.error(e, extra=d)
+
+    def data_filler_user_agent(self, number_of_rows, cursor, conn):
+        '''creates and fills the table with user agent data
+        '''
+        
+        cursor.execute(
+            "CREATE TABLE user_agent (id serial PRIMARY KEY, ip varchar, countrycode varchar, useragent varchar);")
+        try:
+
+            for i in range(0, number_of_rows):
+                cursor.execute("INSERT INTO user_agent "
+                               "(ip, countrycode, useragent) "
+                               "VALUES (%s, %s, %s)",
+                               (self.faker.ipv4(), self.faker.country_code(),
+                                self.faker.user_agent()))
+                conn.commit()
+                
+            logger.warning('user_agent Commits are successful after write job!', extra=d)
+        except Exception as e:
+            logger.error(e, extra=d)
+
+    def data_filler_company(self, number_of_rows, cursor, conn):
+        '''creates and fills the table with company data
+        '''
+        
+        cursor.execute(
+            "CREATE TABLE company (id serial PRIMARY KEY, "
+            "name varchar, sdate date, email varchar, domain varchar, city varchar);")
+        conn.commit()
+        try:
+            for i in range(0, number_of_rows):
+                companies_payload = ("INSERT INTO companies "
+                                     "(name, sdate, email, domain, city) "
+                                     "VALUES (%s, %s, %s, %s, %s)",
+                                     (self.faker.name(), self.faker.date(pattern="%d-%m-%Y"),
+                                self.faker.company_email(), self.faker.safe_email(), self.faker.city()))
+                conn.commit()
+                
+            logger.warning('companies Commits are successful after write job!', extra=d)
+        except Exception as e:
+            logger.error(e, extra=d)
+
+        
+    def data_filler_customer(self, number_of_rows, cursor, conn):
+        '''creates and fills the table with customer data
+        '''
+        
+        cursor.execute(
+            "CREATE TABLE customer (id serial PRIMARY KEY, "
+            "name varchar, lastname varchar, address varchar, country varchar, city varchar, registry_date varchar, birthdate varchar, email varchar, phone_number varchar, locale varchar);")
+        conn.commit()
+        try:
+            for i in range(0, number_of_rows):
+                cursor.execute("INSERT INTO customer "
+                               "(name, lastname, address, country, city, registry_date, birthdate, email, phone_number, locale)"
+                               "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                               (self.faker.name(), self.faker.last_name(), self.faker.address(),
+                                self.faker.country(), self.faker.city(), self.faker.date(pattern="%d-%m-%Y"),
+                                self.faker.date(pattern="%d-%m-%Y"), self.faker.safe_email(), self.faker.phone_number(),
+                                self.faker.locale()))
+            conn.commit()
+            logger.warning('customer Commits are successful after write job!', extra=d)
+        except Exception as e:
+            logger.error(e, extra=d)
