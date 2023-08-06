7c2cb5c5c9df8eb377a6997961ebfee33663f09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:53 +0000

    Add package NoseGAE/0.3.0/NoseGAE-0.3.0.tar.gz

---
+++ packages/NoseGAE/NoseGAE-0.3.0.tar.gz/NoseGAE-0.3.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+DESCRIPTION = """\
+NoseGAE: nose plugin for Google App Engine testing\
+"""
+LONG_DESCRIPTION = """\
+Basic usage::
+    
+    $ cd your/app
+    $ nosetests --with-gae
+
+See http://farmdev.com/projects/nosegae/ for complete documentation
+or open docs/readme.html after downloading the package.
+
+"""
+VERSION = '0.3.0'
+
+setup(
+    name='NoseGAE',
+    version=VERSION,
+    author="Jason Pellerin",
+    author_email="jpellerin@gmail.com",
+    maintainer="Josh Johnston",
+    maintainer_email="johnston.joshua+nosegae@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Topic :: Software Development :: Testing"
+        ],
+    url='http://code.google.com/p/nose-gae/',
+    license='LGPL License',
+    entry_points={
+        'nose.plugins.0.10': ['nosegae = nosegae:NoseGAE']
+        },
+    py_modules=['nosegae'],
+    install_requires=['nose>=0.10.1'],
+    tests_require=['WebTest', 'trestle>=0.2a1', 'Mock']
+    )
