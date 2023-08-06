16c98dcd8c600cc0d4b296676110b184741e799c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:33 +0000

    Add package django-view-as/0.3.1/django-view-as-0.3.1.tar.gz

---
+++ packages/django-view-as/django-view-as-0.3.1.tar.gz/django-view-as-0.3.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+tests_require = [
+    'mock',
+    'pytest',
+    'pytest-django',
+]
+
+requires = [
+    'Django',
+]
+
+entry_points = {
+}
+
+
+setup(
+    name='django-view-as',
+    version='0.3.1',
+    description="A Django middleware which allows you to view the site on behalf of a user.",
+    author="David Cramer",
+    author_email="dcramer@gmail.com",
+    license='Apache License 2.0',
+    package_dir={'': 'src'},
+    packages=find_packages("src"),
+    include_package_data=True,
+    install_requires=requires,
+    extras_require={
+        'tests': tests_require,
+    },
+    entry_points=entry_points,
+    zip_safe=False,
+)
