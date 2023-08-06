62c6ef44c4e229f74f3cd3361126e2aa3fb58bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:38 +0000

    Add package flawless/0.2.0/flawless-0.2.0-py2.7.egg

---
+++ packages/flawless/flawless-0.2.0-py2.7.egg/flawless/lib/data_structures/stubs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2011-2013, Shopkick Inc.
+# All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# ---
+# Author: John Egan <john@shopkick.com>
+
+from flawless.lib.data_structures import ProxyContainerMethodsMetaClass
+from flawless.lib.storage import StorageInterface
+
+
+class StorageStub(object):
+
+    __metaclass__ = ProxyContainerMethodsMetaClass
+
+    def _proxyfunc_(attr, self, *args, **kwargs):
+        try:
+            return getattr(self.dict, attr)(*args, **kwargs)
+        except KeyError:
+            return None
+
+    def __init__(self, partition):
+        self.partition = partition
+        self.dict = dict()
+
+    def iteritems(self):
+        return self.dict.iteritems()
+
+StorageInterface.register(StorageStub)
