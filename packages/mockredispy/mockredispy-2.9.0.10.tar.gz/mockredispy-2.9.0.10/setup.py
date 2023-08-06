f234f247748a98122bf9d9e30834ec4778b7e693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:54 +0000

    Add package mockredispy/2.9.0.10/mockredispy-2.9.0.10.tar.gz

---
+++ packages/mockredispy/mockredispy-2.9.0.10.tar.gz/mockredispy-2.9.0.10/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# Match releases to redis-py versions
+__version__ = '2.9.0.10'
+
+# Jenkins will replace __build__ with a unique value.
+__build__ = ''
+
+setup(name='mockredispy',
+      version=__version__ + __build__,
+      description='Mock for redis-py',
+      url='http://www.github.com/locationlabs/mockredis',
+      license='Apache2',
+      packages=find_packages(exclude=['*.tests']),
+      setup_requires=[
+          'nose'
+      ],
+      extras_require={
+          'lua': ['lunatic-python-bugfix==1.1.1'],
+      },
+      tests_require=[
+          'redis>=2.9.0'
+      ],
+      test_suite='mockredis.tests',
+      entry_points={
+          'nose.plugins.0.10': [
+              'with_redis = mockredis.noseplugin:WithRedis'
+          ]
+      })
