80468427b48a2222e52218db1eb4e1d69c03958e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:31 +0000

    Add package uwsgiit-py/0.9.0/uwsgiit-py-0.9.0.tar.gz

---
+++ packages/uwsgiit-py/uwsgiit-py-0.9.0.tar.gz/uwsgiit-py-0.9.0/uwsgiit/api.py
@@ -0,0 +1,180 @@
+import requests
+import json
+
+class UwsgiItAlarmLevel:
+    SYSTEM    = 0
+    USER      = 1
+    EXCEPTION = 2
+    TRACEBACK = 3
+    LOG       = 4
+
+class UwsgiItClient(object):
+    def __init__(self, username, password, url):
+        self.username = username
+        self.password = password
+        self.url = url
+
+    def _parse_response(self, response):
+        try:
+            response.raise_for_status()
+            response.uerror = False
+            response.umessage = ""
+        except requests.exceptions.HTTPError as e:
+            response.umessage = str(e)
+            response.uerror = True
+
+        return response
+
+    def _path_join(self, path_list):
+        # there should be only integers and strings
+        path = []
+        for p in path_list:
+            if isinstance(p, int):
+                path.append("%d" % (p))
+            else:
+                path.append(p.strip('/'))
+        return '/'.join(path)
+
+    def _build_uri(self, path):
+        if isinstance(path, list):
+            url = [self.url] + path
+        else:
+            url = [self.url, path]
+        return self._path_join(url)
+
+    def _get(self, path, params=None):
+        uri = self._build_uri(path)
+        r = requests.get(uri, auth=(self.username, self.password), params=params)
+        return self._parse_response(r)
+
+    def _post(self, path, data, params):
+        uri = self._build_uri(path)
+        payload = json.dumps(data)
+        r = requests.post(uri, data=payload, params=params, auth=(self.username, self.password))
+        return self._parse_response(r)
+
+    def _delete(self, path, data=None):
+        uri = self._build_uri(path)
+        if data:
+            payload = json.dumps(data)
+        else:
+            payload = None
+        r = requests.delete(uri, data=payload, auth=(self.username, self.password))
+        return self._parse_response(r)
+
+    """
+    Public API
+    """
+    # base methods
+    def get(self, resource, params=None):
+        return self._get(resource, params)
+
+    def post(self, resource, data, params=None):
+        return self._post(resource, data, params)
+
+    def delete(self, resource, data=None):
+        return self._delete(resource, data)
+
+    # wrappers!
+    def news(self):
+        return self.get("news")
+
+    def me(self):
+        return self.get("me")
+
+    def containers(self, tags=None):
+        if tags:
+            params = {"tags": ",".join(tags)}
+        else:
+            params = None
+        return self.get("containers", params)
+
+    def container(self, id):
+        return self.get(["containers", id])
+
+    def distros(self):
+        return self.get("distros")
+
+    def domains(self, tags=None):
+        if tags:
+            params = {"tags": ",".join(tags)}
+        else:
+            params = None
+        return self.get("domains", params)
+
+    def domain(self, id):
+        return self.get(["domains", id])
+
+    def update_me(self, data):
+        return self.post("me", data)
+
+    def add_domain(self, domain):
+        return self.post("domains", {'name': domain})
+
+    def delete_domain(self, domain):
+        return self.delete("domains", {'name': domain})
+
+    def update_domain(self, domain, data):
+        return self.post(["domains", domain], data)
+
+    def update_container(self, container, data):
+        return self.post(["containers", container], data)
+
+    def container_set_distro(self, container, distro):
+        return self.update_container(container, {'distro': distro})
+
+    def container_set_keys(self, container, keys):
+        return self.update_container(container, {'ssh_keys': keys})
+
+    def reboot_container(self, container):
+        return self.post(["containers", container], {'reboot': 1})
+
+    def create_tag(self, name):
+        return self.post("tags", {"name": name})
+
+    def delete_tag(self, tag_id):
+        return self.delete(["tags", tag_id])
+
+    def list_tags(self):
+        return self.get("tags")
+
+    def container_metric(self, container, metric, params=None):
+        return self.get(['metrics', 'container.{}'.format(metric), container], params)
+
+    def domain_metric(self, domain, metric, params=None):
+        return self.get(['metrics', 'domain.{}'.format(metric), domain], params)
+
+    def loopboxes(self, container=None, tags=None):
+        params = {}
+        if tags:
+            params['tags'] = ','.join(tags)
+        if container:
+            params['container'] = container
+        return self.get('loopboxes', params)
+
+    def loopbox(self, id):
+        return self.get(['loopboxes', id])
+
+    def create_loopbox(self, container, filename, mountpoint, readonly=False):
+        return self.post('loopboxes', {'container': container, 'filename': filename, 'mountpoint': mountpoint, 'ro': readonly})
+
+    def update_loopbox(self, id, data):
+        return self.post(['loopboxes', id], data)
+
+    def delete_loopbox(self, id):
+        return self.delete(['loopboxes', id])
+
+    def alarms(self, params=None):
+        return self.get('alarms', params)
+
+    def alarm(self, id):
+        return self.get(['alarms', id])
+
+    def create_alarm(self, container, message, params=None):
+        return self.post(['alarms', 'raise', container], message, params)
+
+    def create_alarm_key(self, container):
+        return self.get(['alarm_key', container])
+
+    def delete_alarm(self, id):
+        return self.delete(['alarms', id])
---
+++ packages/uwsgiit-py/uwsgiit-py-0.9.0.tar.gz/uwsgiit-py-0.9.0/uwsgiit/tests.py
@@ -0,0 +1,238 @@
+import unittest
+import os
+import random
+import uuid
+
+from uwsgiit.api import UwsgiItClient as UClient
+
+
+class ClientTestCase(unittest.TestCase):
+    """
+    https://github.com/unbit/uwsgi.it/blob/master/CustomerQuickstart.md
+    """
+
+    def setUp(self):
+        username = os.environ['UWSGI_IT_USERNAME']
+        password = os.environ['UWSGI_IT_PASSWORD']
+        url = os.environ['UWSGI_IT_URL']
+        self.client = UClient(username, password, url)
+
+        self.container = os.environ['UWSGI_IT_CONTAINER']
+        self.loopbox = os.environ['UWSGI_IT_LOOPBOX']
+        self.alarm = os.environ['UWSGI_IT_ALARM']
+
+    """
+    News: curl https://kratos:deimos@foobar.com/api/news/
+    """
+    def test_news_get(self):
+        r = self.client.news()
+        news = r.json()
+        self.assertEqual(r.uerror, False)
+
+    def test_news_get_without_credentials(self):
+        client = UClient(None, None, os.environ['UWSGI_IT_URL'])
+        r = client.news()
+        news = r.json()
+        self.assertEqual(r.uerror, False)
+
+    """
+    Account Info: curl https://kratos:deimos@foobar.com/api/me/
+    """
+
+    def test_me_get(self):
+        r = self.client.me()
+        me = r.json()
+        self.assertEqual(r.uerror, False)
+
+    def test_me_post_company(self):
+        company = "DogeCom"
+        r = self.client.update_me({'company': company})
+        self.assertEqual(r.uerror, False)
+
+        r = self.client.me()
+        me = r.json()
+        self.assertEqual(me['company'], company)
+
+    """
+    Distributions: curl https://kratos:deimos17@foobar.com/api/distros/
+    """
+
+    def test_distros_get(self):
+        r = self.client.distros()
+        distros = r.json()
+        self.assertEqual(r.uerror, False)
+
+    """
+    Containers: curl https://kratos:deimos@foobar.com/api/me/containers/
+    """
+
+    def test_containers_get(self):
+        r = self.client.containers()
+        containers = r.json()
+        self.assertEqual(r.uerror, False)
+
+    def test_containers_get_single(self):
+        r = self.client.container(self.container)
+        container = r.json()
+        self.assertEqual(r.uerror, False)
+
+    def test_containers_set_distro(self):
+        distros = self.client.distros().json()
+        distro = random.choice([d['id'] for d in distros])
+        r = self.client.container_set_distro(self.container, distro)
+        self.assertEqual(r.uerror, False)
+
+        r = self.client.container(self.container)
+        container = r.json()
+        self.assertEqual(container['distro'], distro)
+
+    def test_containers_set_keys(self):
+        keys = ["ssh-rsa miao"]
+        r = self.client.container_set_keys(self.container, keys)
+        self.assertEqual(r.uerror, False)
+
+        r = self.client.container(self.container)
+        container = r.json()
+        self.assertEqual(container['ssh_keys'], keys)
+
+    """
+    Domains: curl https://kratos:deimos@foobar.com/api/domains/
+    """
+
+    def test_domains_get(self):
+        r = self.client.domains()
+        domains = r.json()
+        self.assertEqual(r.uerror, False)
+
+    def tests_domains_get_single(self):
+        r = self.client.domain(100)
+        self.assertEqual(r.status_code, 404)
+
+    def test_domains_add_delete(self):
+        domain = str(uuid.uuid4())
+        # add
+        r = self.client.add_domain(domain)
+        # lame but fails without settings the dns
+        self.assertEqual(r.uerror, True)
+        self.assertEqual("FORBIDDEN" in r.umessage, True)
+
+        # delete
+        # lame but delete fails if domain does not exist
+        self.client.delete_domain(domain)
+        self.assertEqual(r.uerror, True)
+        self.assertEqual("FORBIDDEN" in r.umessage, True)
+
+        r = self.client.domains()
+        domains = r.json()
+        found = [d for d in domains if d['name'] == domain]
+        self.assertEqual(found, [])
+
+    """
+    Tags: curl https://kratos:deimos@foobar.com/api/tags/
+    """
+
+    def test_tag_creation_delete(self):
+        r = self.client.create_tag("mytag")
+        tag_id = r.json()["id"]
+        r = self.client.delete_tag(tag_id)
+        r = self.client.list_tags()
+        tags = r.json()
+        found_tag = [t for t in tags if t['id'] == tag_id]
+        self.assertEqual(found_tag, [])
+
+    def test_tag_domain_filter(self):
+        r = self.client.domains(tags=["ciao"])
+        domains = r.json()
+        self.assertEqual(r.uerror, False)
+
+    def test_tag_container_filter(self):
+        r = self.client.containers(tags=["ciao"])
+        containers = r.json()
+        self.assertEqual(r.uerror, False)
+
+    """
+    Reboot: curl -X POST -d '{"reboot":1}' https://kratos:deimos17@foobar.com/api/containers/30009
+    """
+
+    def test_reboot(self):
+        r = self.client.reboot_container(self.container)
+        self.assertEqual(r.uerror, False)
+
+    """
+    Metrics: curl https://kratos:deimos@foobar.com/api/metrics/container.io.read/
+             curl https://kratos:deimos@foobar.com/api/metrics/domain.net.rx/
+    """
+
+    def test_container_metric(self):
+        r = self.client.container_metric(self.container, "io.read")
+        self.assertEqual(r.uerror, False)
+
+    def test_domain_metric(self):
+        r = self.client.domains()
+        domain = r.json()[0]['id']
+        r = self.client.domain_metric(domain, "net.rx")
+        self.assertEqual(r.uerror, False)
+
+    """
+    Loopboxes: curl https://kratos:deimos@foobar.com/api/loopboxes/
+    """
+    def test_loopboxes(self):
+        r = self.client.loopboxes()
+        loopboxes = r.json()
+        self.assertEqual(r.uerror, False)
+
+    def test_loopboxes_creation_delete(self):
+        r = self.client.create_loopbox(self.container, 'myloopbox', 'myloop')
+        created_message = r.json()['message']
+        self.assertEqual(created_message, 'Created')
+
+        r = self.client.loopboxes()
+        loopboxes = r.json()
+        loopbox_id = [x['id'] for x in loopboxes if x['container'] == int(self.container) and x['filename'] == 'myloopbox' and x['mountpoint'] == 'myloop'][0]
+
+        r = self.client.delete_loopbox(loopbox_id)
+        deleted_message = r.json()['message']
+        self.assertEqual(deleted_message, 'Ok')
+
+    def test_loopbox(self):
+        r = self.client.loopbox(self.loopbox)
+        loopbox = r.json()
+        self.assertEqual(r.uerror, False)
+
+    """
+    Alarms: curl https://kratos:deimos@foobar.com/api/alarms/
+    """
+    def test_alarms_get(self):
+        r = self.client.alarms()
+        alarms = r.json()
+        self.assertEqual(r.uerror, False)
+
+    def test_alarms_creation_delete(self):
+        params = {
+            'class': 'my-uwsgiit-py-testclass'
+        }
+        r = self.client.create_alarm(self.container, 'myalarm', params)
+        created_message = r.json()['message']
+        self.assertEqual(created_message, 'Created')
+
+        r = self.client.alarms(params)
+        alarms = r.json()
+        self.assertEqual(len(alarms), 1)
+        alarm_id = alarms[0]['id']
+
+        r = self.client.delete_alarm(alarm_id)
+        deleted_message = r.json()['message']
+        self.assertEqual(deleted_message, 'Ok')
+
+    def test_alarm(self):
+        r = self.client.alarm(self.alarm)
+        alarm = r.json()
+        self.assertEqual(r.uerror, False)
+
+    def test_create_alarm_key(self):
+        r = self.client.create_alarm_key(self.container)
+        key = r.json()
+        self.assertEqual(r.uerror, False)
+
+if __name__ == '__main__':
+    unittest.main()
