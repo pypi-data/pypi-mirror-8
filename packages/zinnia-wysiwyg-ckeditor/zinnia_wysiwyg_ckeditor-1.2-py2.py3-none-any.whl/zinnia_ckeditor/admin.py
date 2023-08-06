aff20cb76f0d39865b385f1b175a7bacf83774ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:18 +0000

    Add package zinnia-wysiwyg-ckeditor/1.2/zinnia_wysiwyg_ckeditor-1.2-py2.py3-none-any.whl

---
+++ packages/zinnia-wysiwyg-ckeditor/zinnia_wysiwyg_ckeditor-1.2-py2.py3-none-any.whl/zinnia_ckeditor/admin.py
@@ -0,0 +1,47 @@
+"""EntryAdmin for zinnia-ckeditor"""
+from django import forms
+from django.contrib import admin
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from ckeditor.widgets import CKEditorWidget
+
+from zinnia.models import Entry
+from zinnia.admin.entry import EntryAdmin
+from zinnia.admin.forms import EntryAdminForm
+from zinnia.settings import ENTRY_BASE_MODEL
+
+
+CONFIG_NAME = 'zinnia-content'
+CKEDITOR_CONFIG = getattr(settings, 'CKEDITOR_CONFIGS', {})
+if CONFIG_NAME not in CKEDITOR_CONFIG:
+    CONFIG_NAME = 'default'
+
+
+class EntryAdminCKEditorForm(EntryAdminForm):
+    """
+    Define the CKEditor widget for the content field.
+    """
+    content = forms.CharField(
+        label=_('Content'), required=False,
+        widget=CKEditorWidget(config_name=CONFIG_NAME))
+
+
+class EntryAdminCKEditorMixin(object):
+    """
+    Mixin adding CKEditor for editing Entry.content field.
+    """
+    form = EntryAdminCKEditorForm
+
+
+class EntryAdminCKEditor(EntryAdminCKEditorMixin,
+                         EntryAdmin):
+    """
+    Enrich the default EntryAdmin with CKEditor.
+    """
+    pass
+
+
+if ENTRY_BASE_MODEL == 'zinnia.models_bases.entry.AbstractEntry':
+    admin.site.unregister(Entry)
+    admin.site.register(Entry, EntryAdminCKEditor)
