26aa7a3816ad8803b5ea519dca00647f3efffd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:51 +0000

    Add package ripe.atlas.sagan/0.5/ripe.atlas.sagan-0.5.tar.gz

---
+++ packages/ripe.atlas.sagan/ripe.atlas.sagan-0.5.tar.gz/ripe.atlas.sagan-0.5/ripe/atlas/sagan/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+
+import os
+
+from .base import Result, ResultError, ResultParseError
+from .dns import DnsResult
+from .http import HttpResult
+from .ping import PingResult
+from .ssl import SslResult
+from .traceroute import TracerouteResult
+
+def get_version():
+    version_file = os.path.join(
+        os.path.dirname(__file__),
+        "version"
+    )
+    with open(version_file) as f:
+        return f.read().strip()
+
+
+version = get_version()
+
+__all__ = (
+    "Result",
+    "PingResult",
+    "TracerouteResult",
+    "DnsResult",
+    "SslResult",
+    "HttpResult",
+    "version",
+)
+
