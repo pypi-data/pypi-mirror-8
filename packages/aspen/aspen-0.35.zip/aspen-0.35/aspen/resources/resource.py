893b25a143c9a5a0930e3dd42abebf7f0ebec1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:55 +0000

    Add package aspen/0.35/aspen-0.35.zip

---
+++ packages/aspen/aspen-0.35.zip/aspen-0.35/aspen/resources/resource.py
@@ -0,0 +1,20 @@
+"""
+aspen.resources.resource
+~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+class Resource(object):
+    """This is a base class for both static and dynamic resources.
+    """
+
+    def __init__(self, website, fs, raw, media_type, mtime):
+        self.website = website
+        self.fs = fs
+        self.raw = raw
+        self.media_type = media_type
+        self.mtime = mtime
