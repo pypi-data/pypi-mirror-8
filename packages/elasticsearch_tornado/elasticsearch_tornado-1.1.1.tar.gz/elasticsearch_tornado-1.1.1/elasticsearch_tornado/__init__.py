9965943722f570ac3c4d54d5c5a2850301de0cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:00 +0000

    Add package elasticsearch_tornado/1.1.1/elasticsearch_tornado-1.1.1.tar.gz

---
+++ packages/elasticsearch_tornado/elasticsearch_tornado-1.1.1.tar.gz/elasticsearch_tornado-1.1.1/elasticsearch_tornado/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+VERSION = (1, 0, 0)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+from .client import BaseClient
+from .cat import MeowClient
+from .cluster import ClusterClient
+from .indices import IndicesClient
+from .nodes import NodesClient
+from .snapshot import SnapshotClient
+
+class EsClient(BaseClient):
+    def __init__(self, *args, **kwargs):
+        super(EsClient, self).__init__(*args, **kwargs)
+        self.cat      = MeowClient(*args, **kwargs)
+        self.cluster  = ClusterClient(*args, **kwargs)
+        self.indices  = IndicesClient(*args, **kwargs)
+        self.nodes    = NodesClient(*args, **kwargs)
+        self.snapshot = SnapshotClient(*args, **kwargs)
