de58a1724b83bb711bc92aff126c96540869e557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:59 +0000

    Add package gae_command/0.1/gae_command-0.1.tar.gz

---
+++ packages/gae_command/gae_command-0.1.tar.gz/gae_command-0.1/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+	name = "gae_command",
+	version = "0.1",
+	author = "Alex Bodnaru",
+	author_email = "alexbodn@gmail.com",
+	description = ("A tool to download and invoke gae commands."),
+	license = "BSD",
+	keywords = "gae",
+	url = "http://packages.python.org/gae_command",
+	packages=['gae_command', 'tests'],
+	long_description=read('README'),
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Topic :: Utilities",
+		"Environment :: Console",
+		"License :: OSI Approved :: BSD License",
+	],
+	install_requires=[
+		'PasteScript',
+	],
+	entry_points="""
+	[paste.global_paster_command]
+	gae = gae_command.command:GAEGlobalCommand
+	gae-template = gae_command.template:GAETemplateCommand
+	[paste.paster_create_template]
+	gae = gae_command.template:GAETemplate
+	""",
+	include_package_data=True,
+)
+
