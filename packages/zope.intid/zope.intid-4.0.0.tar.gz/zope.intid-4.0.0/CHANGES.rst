2c4a119209a64ec0179bb5a641c3c883f68324f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:45 +0000

    Add package zope.intid/4.0.0/zope.intid-4.0.0.tar.gz

---
+++ packages/zope.intid/zope.intid-4.0.0.tar.gz/zope.intid-4.0.0/CHANGES.rst
@@ -0,0 +1,49 @@
+=======
+CHANGES
+=======
+
+4.0.0 (2014-12-20)
+------------------
+
+- Added support for testing on Travis.
+
+
+4.0.0a1 (2013-02-22)
+--------------------
+
+- Added support for Python 3.3.
+
+- Replaced deprecated ``zope.interface.implements`` usage with equivalent
+  ``zope.interface.implementer`` decorator.
+
+- Dropped support for Python 2.4 and 2.5.
+
+- Bug fix: ensure that the IntId utility never generates ids greater
+  than the maxint of the BTree family being used.
+
+3.7.2 (2009-12-27)
+------------------
+
+- Use the zope.component API in favor of ztapi.
+
+- Removed ``zope.app.testing`` dependency.
+
+3.7.1 (2009-05-18)
+------------------
+
+- Remove dependencies on ``zope.container``.  Instead import
+  ``Object*Event`` classes from ``zope.lifecycleevent`` and import
+  ``IContained`` from ``zope.location``.  In order to be able to do
+  this, depend on ``zope.lifecycleevent``>=3.5.2 and
+  ``zope.location``>=3.5.4.
+
+- Remove a dependency on ``zope.container.contained.Contained``
+  (this is a dumb base class that defines __parent__ and __name__
+  as None and declares that the class implements IContained).
+
+3.7.0 (2009-02-01)
+------------------
+
+- Split out this package from ``zope.app.intid``. The latter one
+  now only contains browser views and compatibility imports while
+  whole IntId functionality is moved here.
---
+++ packages/zope.intid/zope.intid-4.0.0.tar.gz/zope.intid-4.0.0/setup.py
@@ -0,0 +1,87 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.intid package
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name = 'zope.intid',
+      version='4.0.0',
+      author='Albertas Agejevas and Gintautas Miliauskas',
+      maintainer='Zope developers',
+      maintainer_email='zope-dev@zope.org',
+      description='Integer Id Utility',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')
+          ),
+      keywords = "zope3 integer id utility",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.intid',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      extras_require = dict(
+        test=['zope.testing',
+              'zope.site',
+              'zope.traversing',
+              'zope.container',]),
+      install_requires = [
+        'persistent',
+        'BTrees',
+        'ZODB',
+        'setuptools',
+        'zope.lifecycleevent>=3.5.2',
+        'zope.component',
+        'zope.event',
+        'zope.interface',
+        'zope.keyreference',
+        'zope.location>=3.5.4',
+        'zope.security',
+        ],
+      tests_require = [
+        'zope.testing',
+        'zope.site',
+        'zope.traversing',
+        'zope.container',
+        ],
+      test_suite = 'zope.intid.tests.test_suite',
+      include_package_data = True,
+      zip_safe = False,
+      )
