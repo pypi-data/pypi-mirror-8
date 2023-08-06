ebd270f4ebca4343d9a48993fff539d56af88336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:07 +0000

    Add package django-highlightjs/0.2.2/django-highlightjs-0.2.2.tar.gz

---
+++ packages/django-highlightjs/django-highlightjs-0.2.2.tar.gz/django-highlightjs-0.2.2/highlightjs/renderer.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.utils.html import escape
+from django.forms.widgets import flatatt
+from django.utils.safestring import mark_safe
+
+try:
+	from django.utils.encoding import force_text
+except ImportError:
+	from django.utils.encoding import force_unicode as force_text
+
+
+def text_value(value):
+	"""
+	Force a value to text, render None as an empty string
+	"""
+	if value is None:
+		return ''
+	return force_text(value)
+
+
+def render_tag(attrs=None, content=None):
+    """
+    Render a HTML tag
+    """
+    builder = '<pre><code{attrs}>{content}</code></pre>'
+    return builder.format(
+        attrs=flatatt(attrs) if attrs else '',
+        content=escape(text_value(content)),
+    )
+
+
+def render_highlightjs(context_var, language=None):
+	attrs = {'class': language} if language else None
+	return mark_safe(render_tag(attrs=attrs, content=context_var))
---
+++ packages/django-highlightjs/django-highlightjs-0.2.2.tar.gz/django-highlightjs-0.2.2/highlightjs/settings.py
@@ -0,0 +1,53 @@
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf import settings
+
+
+HIGHLIGHTJS_DEFAULTS = {
+    'jquery_url': '//code.jquery.com/jquery.min.js',
+    'base_url': '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.3/highlight.min.js',
+    'css_url': '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.3/styles/{0}.min.css',
+    'include_jquery': False,
+    'style': 'monokai_sublime',
+}
+
+# Start with a copy of default settings
+HIGHLIGHTJS = HIGHLIGHTJS_DEFAULTS.copy()
+
+# Override with user settings from settings.py
+def update_settings():
+    """
+    Useful method in test to update settings when using @override_settings
+    """
+    HIGHLIGHTJS.update(getattr(settings, 'HIGHLIGHTJS', {}))
+
+update_settings()
+
+
+def get_highlightjs_setting(setting, default=None):
+    """
+    Read a setting
+    """
+    return HIGHLIGHTJS.get(setting, default)
+
+
+def highlightjs_url():
+    """
+    Prefix a relative url with the bootstrap base url
+    """
+    return get_highlightjs_setting('base_url')
+
+
+def highlightjs_jquery_url():
+    """
+    Return the full url to jQuery file to use
+    """
+    return get_highlightjs_setting('jquery_url')
+
+
+def css_url():
+    """
+    Return the full url to the highlightjs CSS file
+    """
+    return get_highlightjs_setting('css_url').format(get_highlightjs_setting('style'))
