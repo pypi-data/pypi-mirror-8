e5ff9f4306fac892acc79969822adf7ad522a4cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:21 +0000

    Add package maketools/0.1.4/maketools-0.1.4.tar.gz

---
+++ packages/maketools/maketools-0.1.4.tar.gz/maketools-0.1.4/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+readme_path = os.path.join(os.path.dirname(
+  os.path.abspath(__file__)),
+  'README.rst',
+)
+long_description = open(readme_path).read()
+version_path = os.path.join(os.path.dirname(
+  os.path.abspath(__file__)),
+  'VERSION',
+)
+version = open(version_path).read()
+
+setup(
+  name='maketools',
+  version=version,
+  packages=['maketools'],
+  author="Nick Whyte",
+  author_email='nick@nickwhyte.com',
+  description="Add build prerequisites to your python project.",
+  long_description=long_description,
+  url='https://github.com/nickw444/python-maketools',
+  include_package_data=True,
+  zip_safe=False,
+  install_requires=[
+  ],
+)
