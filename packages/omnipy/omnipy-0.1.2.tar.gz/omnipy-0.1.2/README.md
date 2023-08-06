fbfc56e69ad94d1cdd965e17ea5af1e4c57f5390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:16 +0000

    Add package omnipy/0.1.2/omnipy-0.1.2.tar.gz

---
+++ packages/omnipy/omnipy-0.1.2.tar.gz/omnipy-0.1.2/README.md
@@ -0,0 +1,24 @@
+omnipy
+==========
+
+Useful python modules in Omnilab traffic log processing.
+
+
+How To Install
+----------
+
+    $ pip install -U omnipy
+    
+or
+
+    $ easy_install -U omnipy
+    
+Maybe `sudo` privilege is required when the system does not permit installing.
+
+
+Contact
+-----------
+
+Xiaming Chen
+
+chenxm35@gmail.com
---
+++ packages/omnipy/omnipy-0.1.2.tar.gz/omnipy-0.1.2/setup.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+from omnipy import __version__
+
+setup(
+    name = "omnipy",
+    version = __version__,
+    url = 'https://github.com/caesar0301/omnipy',
+
+    author = 'X. Chen',
+    author_email = 'chenxm35@gmail.com',
+
+    packages = find_packages(),
+    install_requires = ['treelib', 'tldextract'],
+    package_data = {
+        '': ['*.csv']
+    },
+
+    description = 'My misc python library kit in daily data processing.',
+    license = "GPLv3",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: Freely Distributable',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+   ],
+)
