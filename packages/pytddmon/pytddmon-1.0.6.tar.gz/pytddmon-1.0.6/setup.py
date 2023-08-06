b08f36e226e4c65c259e7c9ed8c62f0a00fe4946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:54 +0000

    Add package pytddmon/1.0.6/pytddmon-1.0.6.tar.gz

---
+++ packages/pytddmon/pytddmon-1.0.6.tar.gz/pytddmon-1.0.6/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+authors = '''\
+Olof Bjarnason, Fredrik Wendt, Krunoslav Saho,
+Samuel Ytterbrink, Rafael Capucho, Ilian Iliev,
+Henrik Bohre, Wari Wahab, Maximilien Riehl,
+Javier J. Gutiérrez'''
+
+if __name__ == '__main__':
+    setup(
+        name='pytddmon',
+        version='1.0.6',
+        description='continuous unit testing in Python',
+        long_description='Read the pytddmon blog and more documentation at http://pytddmon.org',
+        author=authors,
+        author_email="olof.bjarnason@gmail.com",
+        license='MIT',
+        url='http://pytddmon.org',
+        scripts=['src/pytddmon.py'],
+        test_suite='nose.collector',
+        zip_safe=True
+    )
