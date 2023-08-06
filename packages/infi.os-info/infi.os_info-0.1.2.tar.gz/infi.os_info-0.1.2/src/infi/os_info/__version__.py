8561e4ceefa75a141e7aea67b68635029e5843c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:32 +0000

    Add package infi.os-info/0.1.2/infi.os_info-0.1.2.tar.gz

---
+++ packages/infi.os-info/infi.os_info-0.1.2.tar.gz/infi.os_info-0.1.2/src/infi/os_info/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ee74d73acc350e80a09b24c2b800c0ef804cbbcc'
+__git_head_subject__ = 'Finished release v0.1.2.'
+__git_head_message__ = '* release/v0.1.2:\n  HOSTDEV-1945 using platform.uname() to get the correct solaris version\n  HOSTDEV-1945 using platform.uname() to get the correct solaris version'
+__git_dirty_diff__ = ''
