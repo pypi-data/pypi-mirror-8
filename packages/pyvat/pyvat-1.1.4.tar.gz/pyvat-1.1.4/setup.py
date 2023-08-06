6626f9188a9e89ad0b300af1021f62b02537a233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:25 +0000

    Add package pyvat/1.1.4/pyvat-1.1.4.tar.gz

---
+++ packages/pyvat/pyvat-1.1.4.tar.gz/pyvat-1.1.4/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+packages = [
+    'pyvat',
+]
+
+requires = [
+    'requests>=1.0.0,<3.0',
+    'pycountry',
+]
+
+tests_require = [
+    'nose',
+    'rednose',
+    'pep8',
+    'pyflakes',
+]
+
+setup(
+    name='pyvat',
+    version='1.1.4',
+    description='VAT validation for Python',
+    author='Nick Bruun',
+    author_email='nick@bruun.co',
+    url='http://bruun.co/',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'pyvat': 'pyvat'},
+    include_package_data=True,
+    tests_require=tests_require,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    zip_safe=True,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
