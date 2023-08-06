e22aa456b61d78595ca955e1d7c5eb10f0d1be57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:20 +0000

    Add package SQLAlchemy-Wrapper/1.2.3/SQLAlchemy-Wrapper-1.2.3.tar.gz

---
+++ packages/SQLAlchemy-Wrapper/SQLAlchemy-Wrapper-1.2.3.tar.gz/SQLAlchemy-Wrapper-1.2.3/sqlalchemy_wrapper/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+    ==================
+    SQLAlchemy-Wrapper
+    ==================
+
+    A framework-independent wrapper for SQLAlchemy that makes it really easy
+    to use.
+
+    :copyright: © 2012 by `Juan Pablo Scaletti <http://jpscaletti.com>`_.
+    :license: MIT, see LICENSE for more details.
+
+"""
+from .main import SQLAlchemy  # noqa
+from .paginator import Paginator  # noqa
+
+
+__version__ = '1.2.3'
