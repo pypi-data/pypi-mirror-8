fd6028c1a7daed3514f2c095b917cbb9ffd6c4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package pitted/0.3.4/pitted-0.3.4.tar.gz

---
+++ packages/pitted/pitted-0.3.4.tar.gz/pitted-0.3.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import glob
+setup(
+    name = 'pitted',
+    version = '0.3.4',
+    packages = find_packages(),
+    package_data  = {
+        '' : ['*.md', '*.txt', 'LICENSE'],
+        'pitted' : ['gnar/*'],
+    },
+    scripts = glob.glob('bin/*py'),
+    zip_safe = False,
+
+    description = 'Get pitted, so pitted.',
+    author = 'kortox',
+    author_email = 'kortox@gmail.com',
+    license = 'MIT',
+    keywords = 'pitted drop in get so',
+    url = 'https://github.com/kortox/pitted',
+)
