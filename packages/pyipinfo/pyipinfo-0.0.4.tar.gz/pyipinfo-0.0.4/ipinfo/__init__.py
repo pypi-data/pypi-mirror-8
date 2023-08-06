724406c67b27da81afe94329abbc8bfc52385536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:28 +0000

    Add package pyipinfo/0.0.4/pyipinfo-0.0.4.tar.gz

---
+++ packages/pyipinfo/pyipinfo-0.0.4.tar.gz/pyipinfo-0.0.4/ipinfo/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests
+import sys
+from ipinfo.utils import ip_list_from_string
+
+
+def request_to_ipinfo(ip):
+    ''' return a json from the request '''
+    full_url = 'http://ipinfo.io/{}'.format(ip)
+    headers = {'User-Agent': 'curl/7.30.0'}
+    req = requests.get(full_url, headers=headers)
+    if req.status_code == 200:
+        return req.json()
+
+
+def main():
+    string = sys.stdin.read()
+    ip_list = ip_list_from_string(string)
+
+    if ip_list == []:
+        exit()
+
+    ip_set = set(ip_list)
+
+    ips_info = []
+    for ip in ip_set:
+        ips_info.append(request_to_ipinfo(ip))
+
+    all_keys = [
+        'ip', 'city', 'region', 'country',
+        'hostname', 'org', 'postal', 'loc'
+    ]
+
+    for info in ips_info:
+        for k in all_keys:
+            try:
+                print '{}\t'.format(info[k]),
+            except KeyError:
+                print '\t',
+        print
+
+
+if __name__ == '__main__':
+    main()
