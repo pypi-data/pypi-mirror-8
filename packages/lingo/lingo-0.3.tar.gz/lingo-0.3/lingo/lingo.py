aa534b0901bd2d09fd2bae3cc7a4b1aeea38f498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:12 +0000

    Add package lingo/0.3/lingo-0.3.tar.gz

---
+++ packages/lingo/lingo-0.3.tar.gz/lingo-0.3/lingo/lingo.py
@@ -0,0 +1,146 @@
+import functools
+import inspect
+
+import bson
+
+from errors import *
+import database
+
+class combomethod(object):
+    def __init__(self, method):
+        self.method = method
+
+    def __get__(self, obj=None, objtype=None):
+        @functools.wraps(self.method)
+        def _wrapper(*args, **kwargs):
+            if obj is not None:
+                return self.method(obj, *args, **kwargs)
+            else:
+                return self.method(objtype, *args, **kwargs)
+        return _wrapper
+
+class Field(object):
+	def __init__(self, ftype=None, fsubtype=None, validation=None, default=None, doc=None, cast=True):
+		self.ftype=ftype
+		self.fsubtype=fsubtype
+		self.validation=validation
+		self.default=default
+		self.doc=doc
+		self.cast=cast
+
+	def _validate(self, ftype, value):
+		if ftype is not None:
+			if not isinstance(value, ftype) and value is not None:
+				if self.cast:
+					try:
+						if issubclass(ftype, Model):
+							value=ftype(**value)
+						else:
+							value=ftype(value)
+					except TypeError as e:
+						raise ValidationError("%s(%s): %s"%(str(ftype), str(value), str(e)))
+				else:
+					raise ValidationError("Expected %s, got %s"%(ftype.__name__, value.__class__.__name__))
+		return value
+
+	def validate(self, value):
+		if issubclass(self.ftype, list):
+			self._validate(self.ftype, value)
+			value=[self._validate(self.fsubtype, v) for v in value]
+			if self.validation:
+				for v in value:
+					self.validation(v) #Should raise ValidationError on failure
+		else:
+			value=self._validate(self.ftype, value)
+			if self.validation:
+				self.validation(value)
+		return value
+
+class Model(object):
+	class __DefaultPrototype__:
+		__Strict__=False	#If true, assignments to attributes not defined in __Prototype__ are an error
+		__Embedded__=False	#If true, this type of model only exists within other models
+
+	@classmethod
+	def _clsattr(self, attrName):
+		if hasattr(self, "__Prototype__"):
+			if hasattr(self.__Prototype__, attrName):
+				return getattr(self.__Prototype__, attrName)
+			elif hasattr(self.__DefaultPrototype__, attrName):
+				return getattr(self.__DefaultPrototype__, attrName)
+			else:
+				return None
+		else:
+			raise ModelError("Model %s lacks a prototype"%(self.__name__,))
+
+	@combomethod
+	def database(self):
+		if self is not Model:
+			if hasattr(self, '_database'):
+				return database.DatabasePartial(self, self._database)
+		cls = self if inspect.isclass(self) and issubclass(self, Model) else self.__class__
+		db_string = cls._clsattr('__Database__')
+		if db_string is None:
+			raise ModelError("The prototype of class '%s' does not define __Database__" % (cls.__name__,))
+		return database.DatabasePartial(self, database.Database.get_instance(db_string))
+
+	@classmethod
+	def _fields(self):
+		if hasattr(self, "__Prototype__"):
+			return {k:v for k,v in self.__Prototype__.__dict__.items() if isinstance(v, Field)}
+		else:
+			raise ModelError("Model %s lacks a prototype"%(self.__name__,))
+
+	@classmethod
+	def get_type_name(self):
+		return self._clsattr("__Type__") or self._clsattr("__Collection__") or self.__name__
+
+	def __init__(self, **kwargs):
+		self.__data__={}
+		for k,v in self.__class__._fields().items():
+			if k in kwargs:
+				setattr(self, k, kwargs[k])
+			else:
+				setattr(self, k, v.default() if callable(v.default) else v.default)
+
+	def __setattr__(self, k, v):
+		if isinstance(self._clsattr(k), Field):
+			try:
+				v=self._clsattr(k).validate(v)
+			except ValidationError as e:
+				raise ValidationError("%s.%s: %s"%(self.__class__.__name__, k, str(e)))
+			self.__dict__['__data__'][k]=v
+		elif (k in self.__dict__) or (not self._clsattr("__Strict__")):
+			self.__dict__[k]=v
+		else:
+			raise ValidationError("%s: No such attribute %s"%(self.__class__.__name__, k))
+
+	def __getattr__(self, k):
+		f=self.__class__._clsattr(k)
+		if isinstance(f, Field):
+			out=self.__dict__['__data__'][k]
+			if issubclass(f.ftype, Model) and not isinstance(out, f.ftype) and out is not None:
+				#Requested field should be a model instance, but it is not - load it
+				if f.ftype._clsattr("__Embedded__"):
+					out=f.ftype(**out)
+				else:
+					#A load from the database is required.
+					pass #TODO: load from the database
+				setattr(self, k, out)
+			return out
+		else:
+			return self.__dict__[k]
+
+	def _asdict(self, skip=None, extra = {}):
+		out={}
+		for k,v in self.__dict__['__data__'].items():
+			if skip and k in skip:
+				continue
+			if isinstance(v, Model):
+				if v.__class__._clsattr("__Embedded__"):
+					v=v._asdict()
+				else:
+					v=str(v._id)
+			out[k]=v
+		out.update(extra)
+		return out
---
+++ packages/lingo/lingo-0.3.tar.gz/lingo-0.3/setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup
+
+# Using vbox, hard links do not work
+if os.environ.get('USER','') == 'vagrant':
+    del os.link
+
+setup(
+	name='lingo',
+	version='0.3',
+	description='A simple MongoDB/CouchDB ORM for Python',
+	long_description='',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'License :: Other/Proprietary License',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
+		'Topic :: Database',
+		'Topic :: Software Development :: Libraries'
+	],
+	keywords='mongo mongodb orm python couchdb',
+	url='https://github.com/BasementCat/lingo',
+	author='BasementCat',
+	author_email='basementcat@basementcat.net',
+	license='',
+	packages=['lingo'],
+	install_requires=['pymongo', 'couchdb'],
+	test_suite='nose.collector',
+	tests_require=['nose', 'pymongo', 'couchdb'],
+	zip_safe=False
+)
