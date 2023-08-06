60d9f727fcb16ec26c0547812aca3dd88dcbed56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:21 +0000

    Add package infi.recipe.application-packager/0.5.20/infi.recipe.application_packager-0.5.20-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.20-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.20"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.5.20)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '9116b71276d2537e3cb9422d3928ce02adcd57c1'
+__git_head_subject__ = 'Finished release v0.5.20.'
+__git_head_message__ = '* release/v0.5.20:\n  buildout.cfg: changed isolated python version to v2.7.6.20\n  buildout.cfg: changed isolated python version to v2.7.6.19\n  HOSTDEV-1805 solaris packaging tests are working'
+__git_dirty_diff__ = ''
