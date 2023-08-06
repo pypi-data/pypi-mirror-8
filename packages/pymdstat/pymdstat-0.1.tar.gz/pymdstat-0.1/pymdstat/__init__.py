ad14d7af7de3178e5f90e0e4ff84b8c9ea0ebd8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:28 +0000

    Add package pymdstat/0.1/pymdstat-0.1.tar.gz

---
+++ packages/pymdstat/pymdstat-0.1.tar.gz/pymdstat-0.1/pymdstat/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# PyMDstat
+# ...
+#
+# Copyright (C) 2013 Nicolargo <nicolas@nicolargo.com>
+
+__appname__ = "PyMDstat"
+__version__ = "0.1"
+__author__ = "Nicolas Hennion <nicolas@nicolargo.com>"
+__licence__ = "MIT"
+
+from pymdstat import MdStat
