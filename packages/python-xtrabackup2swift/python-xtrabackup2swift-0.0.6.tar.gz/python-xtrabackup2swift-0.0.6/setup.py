6760bb361e142d5313b4f64060677e4041b3c1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:41 +0000

    Add package python-xtrabackup2swift/0.0.6/python-xtrabackup2swift-0.0.6.tar.gz

---
+++ packages/python-xtrabackup2swift/python-xtrabackup2swift-0.0.6.tar.gz/python-xtrabackup2swift-0.0.6/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# Copyright 2012 Hewlett-Packard Development Company, L.P. All Rights Reserved.
+#
+# Author: Simon McCartney <simon.mccartney@hp.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-xtrabackup2swift',
+    version='0.0.6',
+    description='xtrabackup wrappers to store & retrieve from swift',
+    author='Simon McCartney',
+    author_email='simon.mccartney@hp.com',
+    url='https://github.com/moniker-dns/python-xtrabackup2swift',
+    packages=find_packages(exclude=['bin']),
+    install_requires=[
+        'pycrypto',
+        'python-swiftclient',
+        'python-keystoneclient',
+    ],
+    scripts=[
+        'bin/xtrabackup2swift',
+        'bin/swift2xtrabackup'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Environment :: Console'
+    ],
+)
