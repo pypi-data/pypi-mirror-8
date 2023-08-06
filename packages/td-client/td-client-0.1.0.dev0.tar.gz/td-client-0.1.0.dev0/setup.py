4905a5384cd55d7fcda8ca6feb7e01fde615c898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:42 +0000

    Add package td-client/0.1.0.dev0/td-client-0.1.0.dev0.tar.gz

---
+++ packages/td-client/td-client-0.1.0.dev0.tar.gz/td-client-0.1.0.dev0/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import re
+import sys
+
+version = None
+
+with open("tdclient/version.py") as fp:
+    m = re.search(r"^__version__ *= *\"([^\"]*)\" *$", fp.read(), re.MULTILINE)
+    if m is not None:
+        version = m.group(1)
+
+if version is None:
+    raise(RuntimeError("could not read tdclient/version.py"))
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+setup(
+    name="td-client",
+    version=version,
+    description="Treasure Data API library for Python",
+    author="Treasure Data, Inc.",
+    author_email="support@treasure-data.com",
+    url="http://treasuredata.com/",
+    install_requires=[
+        "msgpack-python>=0.4,<0.5",
+    ],
+    tests_require=[
+        "coveralls>=0.5,<0.6",
+        "pytest>=2.6,<2.7",
+        "pytest-cov>=1.8,<1.9",
+        "tox>=1.8,<1.9",
+    ],
+    packages=find_packages(),
+    cmdclass = {"test": PyTest},
+    license="Apache Software License",
+    platforms="Posix; MacOS X; Windows",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+    ],
+)
---
+++ packages/td-client/td-client-0.1.0.dev0.tar.gz/td-client-0.1.0.dev0/tdclient/test/database_api_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import with_statement
+
+import functools
+import os
+
+from tdclient import api
+from tdclient import version
+from tdclient.test.test_helper import *
+
+def setup_function(function):
+    try:
+        del os.environ["TD_API_SERVER"]
+    except KeyError:
+        pass
+    try:
+        del os.environ["HTTP_PROXY"]
+    except KeyError:
+        pass
+
+def test_list_databases():
+    client = api.API("apikey")
+    body = """
+        {
+            "databases":[
+                {"name":"sample_datasets","count":8812278,"created_at":"2014-10-04 01:13:11 UTC","updated_at":"2014-10-08 18:42:12 UTC","organization":null,"permission":"administrator"},
+                {"name":"jma_weather","count":29545,"created_at":"2014-10-12 06:33:01 UTC","updated_at":"2014-10-12 06:33:01 UTC","organization":null,"permission":"administrator"},
+                {"name":"test_db","count":15,"created_at":"2014-10-16 09:48:44 UTC","updated_at":"2014-10-16 09:48:44 UTC","organization":null,"permission":"administrator"}
+            ]
+        }
+    """
+    response = Response(200, body, {})
+    client.get = functools.partial(get, response)
+    databases = client.list_databases()
+    assert response.request_method == "GET"
+    assert response.request_path == "/v3/database/list"
+    assert len(databases) == 3
+    assert sorted(databases.keys()) == ["jma_weather", "sample_datasets", "test_db"]
+    assert sorted([ v[0] for v in databases.values() ]) == [15, 29545, 8812278]
