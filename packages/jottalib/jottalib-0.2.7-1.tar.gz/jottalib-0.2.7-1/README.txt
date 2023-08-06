7db8129591a8805ed5258a8d0ae715a7b93e7f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:41 +0000

    Add package jottalib/0.2.7-1/jottalib-0.2.7-1.tar.gz

---
+++ packages/jottalib/jottalib-0.2.7-1.tar.gz/jottalib-0.2.7-1/README.txt
@@ -0,0 +1,137 @@
+JOTTALIB
+========
+
+This is a bare-bones, pythonic interface to the Jottacloud backup/cloud
+storage service. The service itself exposes a nice and simple HTTP REST
+api, and this library wraps that interface in a python module, in the
+hope that it may be useful.
+
+This is a community project, not an official Jottacloud product. It is
+developed `according to the company founder's
+instructions <http://forum.jotta.no/jotta/topics/api_http>`__, with
+write support reverse engineered `with the company's
+blessing <http://forum.jotta.no/jotta/topics/jotta_api_for_remote_storage_fetch#reply_14928642>`__.
+
+All code is GPLv3 licensed, and the `documentation is
+online <https://pythonhosted.org/jottalib/>`__.
+
+There are also some general tools and a FUSE implementation in here,
+mostly to test the library, but it is fully working and ready for use as
+a file system.
+
+Caveats
+-------
+
+This code has **alpha status**, it's **not production ready** and
+**don't trust it with your data**! It might not eat your cat, but it
+might mangle your cat photos!
+
+Write support is reverse engineered and not based on official docs. Bugs
+patrol these waters! (When you find them, add to the `bug
+tracker! <https://github.com/havardgulldahl/jottalib/issues/>`__ )
+
+Installation
+------------
+
+Via pip
+~~~~~~~
+
+The easiest way: ``pip install jottalib``
+
+From a local git clone
+~~~~~~~~~~~~~~~~~~~~~~
+
+``pip install -r requirements.txt``
+
+Optional requirements
+~~~~~~~~~~~~~~~~~~~~~
+
+::
+
+    fusepy for Fuse client
+    python-qt4 for the Qt models
+
+How to get started
+------------------
+
+Export your Jottacloud username and password to the running environment.
+Running macosx or linux, it would normally go like this:
+
+::
+
+    export JOTTACLOUD_USERNAME="yourusername"
+    export JOTTACLOUD_PASSWORD="yourpassword"
+
+Fuse client
+-----------
+
+This will "mount" jottacloud as a folder on your system, allowing you to
+use your normal file system utilities to browse your account.
+
+0. Install ``fusepy``
+
+   pip install fusepy
+
+1. Create a folder where you want your Jottacloud file system:
+
+   mkdir $HOME/jottafs
+
+2. Run fuse **as a normal user**:
+
+   jottafuse.py $HOME/jottafs
+
+*Note* Being a remote mounted folder, it won't be anywhere as snappy as
+a locally synchronised folder. Since everything has to go over the
+network, the performance will probably make you sad.
+
+QT models
+---------
+
+Take a look at ``jottalib.qt``, where you'll find a
+JFSModel(QtGui.QStandardItemModel) class and various
+JFSNode(QtGui.QStandardItem) classes to match the jottacloud api. Using
+these classes you'll hopefully be able to focus on the UI/UX and leave
+the plumbing to jottalib.
+
+Remember to ``pip install python-qt4``
+
+QT Gui
+~~~~~~
+
+A simple try at a usable gui, using the qt models provided here, lives
+`in its own repository <https://gitorious.org/jottafs/jottagui>`__.
+
+Have a nice idea for a swooshingly fresh JottaCloud app? Feel free to
+include these models in your own, grand design! And submit pull requests
+or ask for a linkback.
+
+jottashare.py
+-------------
+
+A simple command line script to easily upload and share a file at a
+public, secret URI
+
+Usage:
+
+::
+
+    jottashare.py <some great file you need to share>
+
+Duplicity backend
+-----------------
+
+**WORK IN PROGRESS**
+
+The goal is to integrate JottaCloud with
+`duplicity <http://duplicity.nongnu.org/>`__. Then we can run automated
+backups and store them in the JottaCloud.
+
+Authors
+-------
+
+The library is written by havard@gulldahl.no, with crucial help from
+jkaberg.com in revealing the details of the protocol.
+
+If you have a suggestion or have some new functionality in place, `let
+us know and we'll include
+it <https://github.com/havardgulldahl/jottalib/issues/>`__
---
+++ packages/jottalib/jottalib-0.2.7-1.tar.gz/jottalib-0.2.7-1/src/jottalib/qt.py
@@ -0,0 +1,173 @@
+# -*- encoding: utf-8 -*-
+#
+# This file is part of jottafs.
+#
+# jottafs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# jottafs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with jottafs.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2011,2013,2014 Håvard Gulldahl <havard@gulldahl.no>
+
+# stdlib
+import os.path
+import logging, itertools, tempfile
+
+# Part of jottalib.
+import jottalib.JFS as JFS
+
+# This is only needed for Python v2 but is harmless for Python v3.
+import sip
+sip.setapi('QString', 2)
+
+from PyQt4 import QtCore, QtGui
+
+class JFSNode(QtGui.QStandardItem):
+    def __init__(self, obj, jfs, parent=None):
+        super(JFSNode, self).__init__(parent)
+        self.obj = obj
+        self.setText(obj.name)
+        self.jfs = jfs
+        self.childNodes = []
+
+    def flags(self):
+        return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled
+    def columnCount(self): return 1
+    def hasChildren(self): return len(self.childNodes) > 0
+    def rowCount(self): return len(self.childNodes)
+    def pullChildren(self): pass
+    def child(self, row, col=0): return self.childNodes[row]
+
+class JFSFileNode(JFSNode):
+    def __init__(self, obj, jfs, parent=None):
+        super(JFSFileNode, self).__init__(obj, jfs, parent)
+        self.isUploading = False # a flag that is True when the file is currently uploadign
+
+    def flags(self):
+        if self.isUploading:
+            return QtCore.Qt.NoItemFlags
+        elif self.obj.is_deleted():
+            return QtCore.Qt.NoItemFlags
+        return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsDragEnabled
+
+class JFSFolderNode(JFSNode):
+    def __init__(self, obj, jfs, parent=None):
+        super(JFSFolderNode, self).__init__(obj, jfs, parent)
+        self.childrenAlreadyPulled = False
+
+    def flags(self):
+        return QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsDropEnabled
+
+    def pullChildren(self, hideDeleted=True):
+        if self.childrenAlreadyPulled: return
+        for obj in self.obj.folders():
+            if hideDeleted and obj.is_deleted(): continue
+            self.appendRow(JFSFolderNode(obj, self.jfs, self))
+        for obj in self.obj.files():
+            if hideDeleted and obj.is_deleted(): continue
+            self.appendRow(JFSFileNode(obj, self.jfs, self))
+        self.childrenAlreadyPulled = True
+
+class JFSDeviceNode(JFSNode):
+    def __init__(self, obj, jfs, parent=None):
+        super(JFSDeviceNode, self).__init__(obj, jfs, parent)
+        self.childNodes = list([JFSFolderNode(item, self.jfs, self) for item in self.obj.mountPoints.values()])
+
+class JFSModel(QtGui.QStandardItemModel):
+    tempfileCreated = QtCore.pyqtSignal(unicode)
+    uploadFile = QtCore.pyqtSignal(unicode, unicode) # (local file path, remote folder path)
+
+    def __init__(self, jfs, rootPath, parent=None):
+        super(JFSModel, self).__init__(parent)
+        self.jfs = jfs # a jottalib.JFS.JFS instance
+        self.rootItem = self.invisibleRootItem() # top item
+        self.rootPath = rootPath
+        rawObj = self.jfs.getObject(self.rootPath)
+        if isinstance(rawObj, JFS.JFSDevice):
+            self.rootObject = JFSDeviceNode(rawObj, jfs)
+        elif isinstance(rawObj, (JFS.JFSMountPoint, JFS.JFSFolder)):
+            self.rootObject = JFSFolderNode(rawObj, jfs)
+        elif isinstance(rawObj, JFS.JFSFile):
+            self.rootObject = JFSFileNode(rawObj, jfs)
+        self.rootItem.appendRows(self.rootObject.childNodes)
+        self.uploadFile.connect(self.upload)
+
+    def upload(self, localpath, remotefolder):
+        logging.debug('upload %s -> %s', localpath, remotefolder)
+        self.jfs.up(remotefolder, open(localpath, 'rb'))
+
+    def populateChildNodes(self, idx):
+        logging.debug('populateChildNodes %s', idx)
+        if self.hasChildren(idx):
+            self.itemFromIndex(idx).pullChildren()
+
+    def hasChildren(self, idx):
+        item = self.itemFromIndex(idx)
+        # if item is not None:
+        #     logging.debug('hasChildren item: %s (%s)', item, unicode(item.text()))
+        if isinstance(item, JFSFileNode):
+            return False
+        return True
+
+    def flags(self, idx):
+        item = self.itemFromIndex(idx)
+        if item is not None: return item.flags()
+
+    def mimeData(self, idxes):
+        item = self.itemFromIndex(idxes.pop())
+        if item is None:
+            return
+        logging.debug('mimeData item: %s (%s)', item, unicode(item.text()))
+
+        md = jottaMimeData()
+        data = item.obj.read()
+        md.dataRequested.connect(lambda mt: self.createData(mt, data))
+        self.tempfileCreated.connect(lambda fn: md.setTempfile(fn))
+        md.setData(item.obj.mime, data)
+        if item.obj.is_image():
+            img = QtGui.QImage(item.obj.name)
+            img.loadFromData(data)
+            md.setImageData(img)
+        return md
+
+    def createData(self, mimeType, data):
+      #if not mimeType.split('/')[0] in ('image', ):
+      #    return
+      print "createData", mimeType
+      with tempfile.NamedTemporaryFile(delete=False) as f:
+          f.write(data)
+          print f.name
+          self.tempfileCreated.emit(f.name)
+
+    def supportedDropActions(self):
+        return QtCore.Qt.CopyAction
+
+    def dropMimeData(self, mimedata, action, row, column, parent):
+        logging.debug("dropMimeData %s (parent %s)", action, parent)
+        logging.debug("dropped: %s", mimedata.formats())
+        if action == QtCore.Qt.CopyAction:
+            # put this data on jottacloud
+            logging.debug("upload data")
+
+            return True
+        return False
+
+class jottaMimeData(QtCore.QMimeData):
+    dataRequested = QtCore.pyqtSignal(unicode)
+
+    def retrieveData(self, mimeType, _type):
+      print "retrieveData", mimeType, _type
+      self.dataRequested.emit(mimeType)
+      return super(jottaMimeData, self).retrieveData(mimeType, _type)
+
+    def setTempfile(self, fn):
+      print "setTempfile", fn
+      self.setUrls([QtCore.QUrl(fn), ])
