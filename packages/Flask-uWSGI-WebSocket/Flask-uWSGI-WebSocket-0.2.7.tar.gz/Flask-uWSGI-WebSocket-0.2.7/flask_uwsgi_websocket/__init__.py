f823179847de75b06e9995460cabb1216bc0a7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:12 +0000

    Add package Flask-uWSGI-WebSocket/0.2.7/Flask-uWSGI-WebSocket-0.2.7.tar.gz

---
+++ packages/Flask-uWSGI-WebSocket/Flask-uWSGI-WebSocket-0.2.7.tar.gz/Flask-uWSGI-WebSocket-0.2.7/flask_uwsgi_websocket/__init__.py
@@ -0,0 +1,23 @@
+'''
+Flask-uWSGI-WebSocket
+---------------------
+High-performance WebSockets for your Flask apps powered by `uWSGI <http://uwsgi-docs.readthedocs.org/en/latest/>`_.
+'''
+
+__docformat__ = 'restructuredtext'
+__version__ = '0.2.7'
+__license__ = 'MIT'
+__author__  = 'Zach Kelling'
+
+import os
+import sys
+import uuid
+
+from ._uwsgi import uwsgi
+from .websocket import *
+from .async import *
+
+try:
+    from ._gevent import *
+except ImportError:
+    pass
