66cd494b9b0405518986eeae8e4773c02cce72b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:24 +0000

    Add package bitcoin/1.1.18/bitcoin-1.1.18.tar.gz

---
+++ packages/bitcoin/bitcoin-1.1.18.tar.gz/bitcoin-1.1.18/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='bitcoin',
+      version='1.1.18',
+      description='Python Bitcoin Tools',
+      author='Vitalik Buterin',
+      author_email='vbuterin@gmail.com',
+      url='http://github.com/vbuterin/pybitcointools',
+      install_requires='six==1.8.0',
+      packages=['bitcoin'],
+      scripts=['pybtctool'],
+      data_files=[("", ["LICENSE"])]
+      )
