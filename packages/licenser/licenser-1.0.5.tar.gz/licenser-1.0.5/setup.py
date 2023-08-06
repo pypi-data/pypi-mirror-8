7ca2214072c0ae63435b89bb9d71418520546d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:24 +0000

    Add package licenser/1.0.5/licenser-1.0.5.tar.gz

---
+++ packages/licenser/licenser-1.0.5.tar.gz/licenser-1.0.5/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from os.path import dirname
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+pwd = dirname(__file__)
+
+setup(
+    name="licenser",
+    version="1.0.5",
+    description="Quickly add an open-source license to your project.",
+    author="Ty-Lucas Kelley",
+    author_email="tylucaskelley@gmail.com",
+    license="MIT",
+    url="http://github.com/tylucaskelley/licenser",
+    download_url="https://github.com/tylucaskelley/licenser/tarball/v1.0.5",
+    long_description=open("README.md").read(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities"
+    ],
+    entry_points={
+        "console_scripts": [
+            "licenser=licenser:main"
+        ]
+    },
+    packages=[
+        "licenser"
+    ],
+    package_dir={
+        "licenser": "licenser"
+    },
+    package_data={
+        "licenser": [
+            "licenses/*"
+        ]
+    },
+    test_suite="tests"
+)
