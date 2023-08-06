5c1ce50c0468f8291396a5e3f68cd1159dbe9b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:08 +0000

    Add package django-chunks2/2.8.3/django-chunks2-2.8.3.tar.gz

---
+++ packages/django-chunks2/django-chunks2-2.8.3.tar.gz/django-chunks2-2.8.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-chunks2',
+    version='2.8.3',
+    description='Keyed blocks of content for use in your Django templates',
+    keywords="django chunks templates",
+    author='Clint Ecker',
+    author_email='me@clintecker.com',
+    url='https://github.com/gotlium/django-chunks2',
+    maintainer='gotlium',
+    maintainer_email='gotlium@gmail.com',
+    packages=find_packages(exclude=['demo']),
+    include_package_data=True,
+    package_data={'chunks': [
+        'templates/chunks/*',
+        'static/chunks/*',
+        'locale/*/LC_MESSAGES/*'
+    ]},
+    install_requires=[
+        'easy_thumbnails',
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Utilities'
+    ],
+)
