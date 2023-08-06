4a4958bbd8161383054f028bfbffb34541ba742f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:36 +0000

    Add package distill_framework/0.1.11/distill_framework-0.1.11.zip

---
+++ packages/distill_framework/distill_framework-0.1.11.zip/distill_framework-0.1.11/distill/application.py
@@ -0,0 +1,190 @@
+from inspect import isclass
+import sys
+from functools import partial
+from routes import Mapper
+from distill.exceptions import HTTPNotFound, HTTPErrorResponse
+from distill.request import Request
+from distill.response import Response
+from distill.renderers import RenderFactory
+
+
+class Distill(object):
+    def __init__(self, rmap=None, settings=None, controllers=None):
+        """ INIT
+
+        Args:
+            base_node: The root of the traversal tree
+            before: Method to be called before the view is called
+            after: Method to be called after the view is called
+            settings: Settings dictionary, will be available in all requests
+            document_root: Root directory for mako templates
+        """
+        if rmap is None:
+            self.map = Mapper()
+        else:
+            self.map = rmap
+
+        if settings is None:
+            settings = {}
+
+        self._session_factory = None
+
+        self.settings = settings
+        self._before = []
+        self._after = []
+        self._exc_listeners = {}
+        self._controllers = {}
+        if controllers is not None:
+            self._controllers.update(controllers)
+
+        RenderFactory.create(settings)
+
+    def __call__(self, env, start_response):
+        """ Excpected WSGI method
+
+        Creates new request using the provided env, then traverses
+        the root node to the required view node
+        """
+        req = Request(env, self)
+
+        if self._session_factory:
+            req.session = self._session_factory(req)
+
+        try:
+            resp = self._request(env, req)
+        except HTTPErrorResponse as ex:
+            if self._exc_listeners and ex.__class__ in self._exc_listeners:
+                resp = ex
+                res = self._exc_listeners[ex.__class__](req, resp)
+                if isinstance(res, Response):
+                    resp = res
+                else:
+                    resp.body = str(res)
+
+                self._do_after(req, resp)
+                resp.finalize(env.get('wsgi.file_wrapper'))
+                start_response(resp.status, resp.wsgi_headers)
+                return resp.iterable
+            else:
+                self._do_after(req, ex)
+                start_response(ex.status, ex.wsgi_headers, sys.exc_info())
+                # By spec execution shouldn't get here, but in case it does
+                return []  # pragma: no cover
+
+        self._do_after(req, resp)
+        resp.finalize(env.get('wsgi.file_wrapper'))
+
+        start_response(resp.status, resp.wsgi_headers)
+        return resp.iterable
+
+    def _request(self, env, req):
+        """ Processes the request
+        Notes:
+            This method acts as a wrapper to ease exception handling
+
+        Args:
+            env: The wsgi environ variable
+        """
+        resp = Response()
+
+        self._do_before(req, resp)
+
+        context = self.map.match(req.path, env)
+        if context is not None:
+            req.matchdict = context
+            if 'controller' in context and context['controller'] in self._controllers:
+                if hasattr(self._controllers[context['controller']], context['action']):
+                    cls = self._controllers[context['controller']]
+                    controller = cls()
+                    res = getattr(controller, context['action'])(req, resp)
+                else:
+                    raise HTTPNotFound()
+            elif callable(context['action']):
+                res = context['action'](req, resp)
+            else:
+                raise HTTPNotFound()
+
+            if isinstance(res, Response):
+                if isinstance(res, HTTPErrorResponse):
+                    raise res
+                resp = res
+            elif res is not None:
+                resp.body = str(res)
+
+        else:
+            raise HTTPNotFound()
+
+        return resp
+
+    def _do_before(self, req, resp):
+        if self._before:
+            for f in self._before:
+                f(req, resp)
+
+    def _do_after(self, req, resp):
+        if self._after:
+            for f in self._after:
+                f(req, resp)
+
+        if req.resp_callbacks:
+            for f in req.resp_callbacks:
+                f(req, resp)
+
+    def use(self, function, before=True):
+        """ Adds a middleware to the application
+        Notes:
+            This method allows you to add middleware
+            that will be called either before or after
+            a request has been handled by the application
+            The function should accept two positional arguments
+            function(request, response)
+
+        Args:
+            function: The function to be called
+            befre: Should the function be called before a request
+        """
+        if not before:
+            self._after.append(function)
+        else:
+            self._before.append(function)
+
+    def map_connect(self, *args, **kwargs):
+        """ Adds a route to the routing table
+
+        Notes:
+            For further documentation see routes documection
+            on the Mapper object:
+
+            http://routes.readthedocs.org/en/latest/index.html
+
+        Args:
+            args: Positional arguments to be passed to the mapper
+            kwargs: Keyword arguments to be passed to the mapper
+        """
+        self.map.connect(*args, **kwargs)
+
+    def add_controller(self, name, controller):
+        """ Adds a controller to the application
+
+        Notes:
+            The controller should be passed as a class, not an instance
+            The name provided is used to lookup the contoller based
+            on the results of a route match.  You should be sure this
+            name matches the one you provided to the controller argument
+            of map_connect
+
+        Args:
+            name: Name of the controller
+            controller: Controller class
+        """
+        self._controllers[name] = controller
+
+    def on_except(self, exc, method):
+        self._exc_listeners[exc] = method
+
+    def set_session_factory(self, session_factory):
+        self._session_factory = session_factory
+
+    @staticmethod
+    def add_renderer(name, serializer):
+        RenderFactory.add_renderer(name, serializer)
---
+++ packages/distill_framework/distill_framework-0.1.11.zip/distill_framework-0.1.11/distill/response.py
@@ -0,0 +1,101 @@
+from distill import PY3
+
+
+class Response(object):
+    def __init__(self, status="200 OK", headers=None):
+        """Initializes an empty response"""
+        self.status = status
+        if headers is None:
+            headers = {}
+        self.headers = headers
+        self.body = None
+        self.file = None
+        self.file_len = None
+        self.iterable = None
+
+    @property
+    def wsgi_headers(self):
+        """Returns the response headers in the form WSGI expects"""
+        if PY3:  # pragma: no cover
+            items = list(self.headers.items())
+        else:  # pragma: no cover
+            items = self.headers.items()
+        wsgilist = []
+        for l in items:
+            if type(l[1]) == list:
+                wsgilist += [(l[0], li) for li in l[1]]
+            else:
+                wsgilist.append(l)
+        return wsgilist
+
+    def set_cookie(self, name, value, path="/", max_age=3600, domain=None,
+                   secure=False, comment=None):
+        """ Allows you to set a cookie on the client
+
+         Notes:
+            This function performs no check to ensure the cookie
+            is not already set in the response.  Additionally, this
+            function can be used to delete cookies by setting max_age
+            to zero
+
+        Args:
+            name: The name of the cookie to set
+            value: The value to give the cookie
+
+        Kwargs:
+            path: The path attribute on the cookie Default: /
+            max_age: Time before the cookie expires Default: 3600
+            domain: The domain attribute of the cookie Default: None
+            secure: Is the cookie secure Default: False
+            comment: The cookie comment Default: None
+        """
+        cookie = [(name, value), ("Max-Age", str(max_age)), ("Path", path), ("Version", "1")]
+
+        if domain:
+            if domain[:1] != '.':  # RFC specifies domains must start with a dot
+                domain = '.{0}'.format(domain)
+            cookie.append(("Domain", domain))
+        if secure:
+            cookie.append(("Secure", None))
+        if comment:
+            cookie.append(("Comment", comment))
+
+
+        cookie = '; '.join([attr for attr in ["%s=%s" % (key, value) if value else key for (key, value) in cookie]])
+        self.headers.setdefault('Set-Cookie', [])
+        self.headers['Set-Cookie'].append(cookie)
+
+    def finalize(self, wsgi_file_wrapper):
+        """ Finalizes the response to prepare it for transmission
+
+        Notes:
+            This argument performs all necessary processing
+            on the response object to prepare it to be sent
+            to the WSGI server.  This includes setting
+            Content-Length, wrapping the response's file
+            descriptor as described in PEP333, and converting
+            the response body to an interable
+
+        Args:
+            wsgi_file_wrapper: Wrapping function provided by WSGI server
+
+        """
+
+        if self.body:
+            self.headers['Content-Length'] = str(len(self.body))
+            if PY3:  # pragma: no cover
+                if type(self.body) == str:
+                    self.iterable = [bytes(self.body, 'utf-8')]
+                else:
+                    self.iterable = [bytes(self.body)]
+            else:  # pragma: no cover
+                self.iterable = [self.body]
+        elif self.file:
+            if self.file_len:
+                self.headers['Content-Length'] = str(self.file_len)
+            if wsgi_file_wrapper:
+                self.iterable = wsgi_file_wrapper(self.file, 8 * 1024)
+            else:
+                self.iterable = iter(lambda: self.file.read(8 * 1024), b'')
+        else:
+            self.iterable = []
