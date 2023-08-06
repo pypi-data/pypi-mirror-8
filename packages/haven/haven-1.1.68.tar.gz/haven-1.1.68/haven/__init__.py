fe5605ed2555860ea842df98eadcc2ae57d4dccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:17 +0000

    Add package haven/1.1.68/haven-1.1.68.tar.gz

---
+++ packages/haven/haven-1.1.68.tar.gz/haven-1.1.68/haven/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '1.1.68'
+
+from .log import logger
+from .utils import safe_call, safe_func
+
+try:
+    from gevent_impl import GHaven, GBlueprint, GTimer
+except:
+    GHaven = GBlueprint = GTimer = None
+
+from thread_impl import THaven, TBlueprint, TTimer
---
+++ packages/haven/haven-1.1.68.tar.gz/haven-1.1.68/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="haven",
+    version='1.1.68',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=['events', 'netkit'],
+    url="https://github.com/dantezhu/haven",
+    license="MIT",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="flask's style binary server framework",
+)
