ca5402d760b16ab5e4f64496d3a984fef3fbc207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:47 +0000

    Add package moke/1.1.8/moke-1.1.8.tar.gz

---
+++ packages/moke/moke-1.1.8.tar.gz/moke-1.1.8/RELEASE.rst
@@ -0,0 +1,17 @@
+Release 
+-------
+
+  - update moke.core.__version__
+  - update setup.py version
+  - update CHANGELOG.rst
+  - update doc/source/conf.py
+  - regenerate docs/html
+  - push docs/html to gh-pages
+  - push moke to github
+  - python setup.py sdist upload
+    
+  
+TODO
+----
+
+  - add version support to the moke executable
---
+++ packages/moke/moke-1.1.8.tar.gz/moke-1.1.8/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+NAME = "moke"
+VERSION = "1.1.8"
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 2.7",
+    "License :: OSI Approved :: BSD License"
+    ]
+
+setup(
+    name=NAME.lower(),
+    version=VERSION,
+    description="moke is not like make",
+    keywords="make, ant, rake, paver, build, shell, argparse, bash",
+    author="Marcin Cieslik",
+    author_email="mcieslik@med.umich.edu",
+    url='http://mcieslik-mctp.github.io/moke',
+    license="BSD License",
+    long_description=open('README.rst', 'r').read(),
+    classifiers=CLASSIFIERS,
+    packages = ["moke"],
+    package_dir = {"": "src"},
+    package_data = {"moke": ["data/*"]},
+    scripts = ["bin/moke"],
+    # Options
+    include_package_data=True,
+    zip_safe=False,
+    )
