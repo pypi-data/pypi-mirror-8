9a5ca333858ba30428b035e23098cd90da963791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:14 +0000

    Add package zope.login/2.0.0/zope.login-2.0.0.tar.gz

---
+++ packages/zope.login/zope.login-2.0.0.tar.gz/zope.login-2.0.0/CHANGES.rst
@@ -0,0 +1,25 @@
+Changelog
+=========
+
+2.0.0 (2014-12-24)
+------------------
+
+- Add support for PyPy and PyPy3.
+
+- Add support for Python 3.4.
+
+- Add support for testing on Travis.
+
+- Add support for Python 3.3.
+
+- Replace deprecated ``zope.interface.implements`` usage with equivalent
+  ``zope.interface.implementer`` decorator.
+
+- Drop support for Python 2.4 and 2.5.
+
+
+1.0.0 (2009-12-31)
+------------------
+
+- Extracted BasicAuthAdapter and FTPAuth adapters from zope.publisher. They
+  should have never gone into that package in the first place.
---
+++ packages/zope.login/zope.login-2.0.0.tar.gz/zope.login-2.0.0/src/zope/login/tests/test_ftpauth.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright (c) 2003 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from unittest import TestCase
+
+from zope.interface import implementer
+from zope.publisher.ftp import FTPAuth
+from zope.publisher.interfaces.ftp import IFTPCredentials
+
+
+@implementer(IFTPCredentials)
+class FTPCredentials(object):
+    __doc__ = IFTPCredentials.__doc__
+
+
+    def __init__(self, credentials):
+        self.credentials = credentials
+
+    def _authUserPW(self):
+        return self.credentials
+
+    unauth = 0
+    def unauthorized(self, challenge):
+        self.unauth += 1
+
+
+class Test(TestCase):
+
+    def test(self):
+        request = FTPCredentials(('bob', '123'))
+        auth = FTPAuth(request)
+        self.assertEqual(auth.getLogin(), 'bob')
+        self.assertEqual(auth.getPassword(), '123')
+
+        unauth = request.unauth
+        auth.needLogin('xxx')
+        self.assertEqual(request.unauth, unauth+1)
+
+        request = FTPCredentials(None)
+        auth = FTPAuth(request)
+        self.assertEqual(auth.getLogin(), None)
+        self.assertEqual(auth.getPassword(), None)
