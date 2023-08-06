b90d053ceadcdfa1e7bde1175c8d8f5f98f6df0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:58 +0000

    Add package fcs_simulation/0.1.1d/fcs_simulation-0.1.1d.tar.gz

---
+++ packages/fcs_simulation/fcs_simulation-0.1.1d.tar.gz/fcs_simulation-0.1.1d/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='fcs_simulation',
+      version='0.1.1d',
+      author='Tyler Parker',
+      description='Simulate FCS and generate photon arrival times',
+      url='http://github.com/computemachines/fcs_simulation',
+      author_email='tparker@umass.edu',
+      license='MIT',
+      packages=['fcs_simulation'],
+      install_requires=['photon_tools'],
+      dependency_links=['https://github.com/bgamari/photon-tools'],#/tarball/master#egg=photon-tools-1.0'],
+      zip_safe=False)
