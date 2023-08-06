a91b3daea6849749e09ecdd29972364d9ec7458f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:12 +0000

    Add package m3-objectpack/2.0.22.2/m3-objectpack-2.0.22.2.tar.gz

---
+++ packages/m3-objectpack/m3-objectpack-2.0.22.2.tar.gz/m3-objectpack-2.0.22.2/src/objectpack/demo/controller.py
@@ -0,0 +1,37 @@
+#coding:utf-8
+"""
+Обсервер и контроллеры
+"""
+
+from objectpack import observer
+
+
+def logger(msg):
+    print "Observer: %s" % msg
+
+# Наблюдатель
+obs = observer.Observer()
+    #logger=logger, verbose_level=observer.Observer.LOG_MORE)
+
+
+action_controller = observer.ObservableController(obs, "/controller")
+
+
+@obs.subscribe
+class Listener(object):
+
+    listen = ['.*/.*/ObjectListWindowAction']
+
+    def after(self, request, context, response):
+        # подмена заголовка окна
+        response.data.title = u'Му-ха-ха! %s' % response.data.title
+
+
+@obs.subscribe
+class StarToHash(object):
+
+    listen = ['.*/BandedColumnPack/.*']
+
+    def prepare_obj(self, obj):
+        obj['field1'] = obj.get('field1', False) and (obj['id'] % 2)
+        return obj
