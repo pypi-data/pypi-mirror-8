4946853b667b365798fd988a81101d553cb4c1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:44 +0000

    Add package protodict/1.0.0/protodict-1.0.0.tar.gz

---
+++ packages/protodict/protodict-1.0.0.tar.gz/protodict-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+#import distutils.command.bdist_conda
+
+
+setup(
+    name='protodict',
+    description='a small Python library for 2 ways conversion between dicts '
+        'and protocol buffers.',
+    version='1.0.0',
+    author='Eugene Van den Bulke',
+    author_email='eugene.vandenbulke@gmail.com',
+    url='https://github.com/3kwa/protodict',
+    license='Public Domain',
+    keywords=['protobuf', 'dict'],
+    install_requires=['protobuf>=2.3.0'],
+    package_dir={'':'src'},
+    py_modules=['protodict'],
+    setup_requires=['protobuf>=2.3.0', 'nose>=1.0', 'coverage', 'nosexcover'],
+    test_suite = 'nose.collector',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
