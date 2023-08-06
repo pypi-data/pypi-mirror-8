4be884189c1a7d0ad5540954560af04d34099766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:05 +0000

    Add package listElement_in_list/1.0.0/listElement_in_list-1.0.0.zip

---
+++ packages/listElement_in_list/listElement_in_list-1.0.0.zip/listElement_in_list-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='listElement_in_list',
+        version='1.0.0',
+        py_modules=['listElement_in_list'],
+        author='rge',
+        author_email='gerut@gmx.ch',
+        url='http://www.hegus.info',
+        description='Ausgabe von Listen',
+    )
