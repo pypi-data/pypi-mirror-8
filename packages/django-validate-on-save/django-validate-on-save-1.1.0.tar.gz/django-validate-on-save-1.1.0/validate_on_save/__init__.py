790e606530937c9dd434c31e15876e346e468e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:13 +0000

    Add package django-validate-on-save/1.1.0/django-validate-on-save-1.1.0.tar.gz

---
+++ packages/django-validate-on-save/django-validate-on-save-1.1.0.tar.gz/django-validate-on-save-1.1.0/validate_on_save/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# (c) 2012 Bright Interactive Limited. All rights reserved.
+# http://www.bright-interactive.com | info@bright-interactive.com
+
+from distutils.version import StrictVersion
+import django
+from django.db.models.loading import get_app, get_models
+
+
+__version__ = '1.1.0'
+
+
+def django_allows_app_config():
+    return StrictVersion(django.get_version()) >= StrictVersion('1.7')
+
+
+if django_allows_app_config():
+    default_app_config = 'validate_on_save.apps.ValidateOnSaveConfig'
+
+
+def validate_models_on_save(app_name):
+    """
+    Make all models in app_name be validated during their save() method
+    (surprisingly, this does not happen by default, apparently for backwards
+    compatibility reasons:
+    http://stackoverflow.com/questions/4441539/why-doesnt-djangos-model-save-call-full-clean).
+    """
+    app = get_app(app_name)
+    for model in get_models(app):
+        validate_model_on_save(model)
+
+
+def validate_model_on_save(model):
+    """
+    Make a model be validated during its save() method.
+
+    model: a model class.
+    """
+    django.db.models.signals.pre_save.connect(_validate, sender=model)
+
+
+def _validate(sender, signal, instance, raw, using, **kwargs):
+    if not raw:
+        instance.full_clean()
---
+++ packages/django-validate-on-save/django-validate-on-save-1.1.0.tar.gz/django-validate-on-save-1.1.0/validate_on_save/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# (c) 2012 Bright Interactive Limited. All rights reserved.
+# http://www.bright-interactive.com | info@bright-interactive.com
+
+from django.core.exceptions import ValidationError
+from django.db import models
+import validate_on_save
+from validate_on_save import django_allows_app_config
+
+
+class PickyModel(models.Model):
+    """
+    A model that always raises a ValidationError in full_clean(). Just for
+    use by validate_on_save's tests, to make sure that full_clean() is being
+    called.
+    """
+
+    def full_clean(self, *args, **kwargs):
+        super(PickyModel, self).full_clean(*args, **kwargs)
+        raise ValidationError("This data isn't good enough!")
+
+
+if not django_allows_app_config():
+    validate_on_save.validate_models_on_save('validate_on_save')
