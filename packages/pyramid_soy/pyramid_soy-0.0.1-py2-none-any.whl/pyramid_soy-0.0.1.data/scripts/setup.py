f3825b30625442cc2a98986127555968fa7fc4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:17 +0000

    Add package pyramid_soy/0.0.1/pyramid_soy-0.0.1-py2-none-any.whl

---
+++ packages/pyramid_soy/pyramid_soy-0.0.1-py2-none-any.whl/pyramid_soy-0.0.1.data/scripts/setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import find_packages
+from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+
+requires = [
+    'soy'
+]
+
+setup(name='pyramid_soy',
+      version='0.0.1',
+      description='Simple soy wrapper with simple cache.',
+      long_description=README,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='winkidney',
+      author_email='winkidney@gmail.com',
+      url='http://github.com/winkidney',
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      scripts=['setup.py',],
+      install_requires=requires,
+      tests_require=requires,
+      )
