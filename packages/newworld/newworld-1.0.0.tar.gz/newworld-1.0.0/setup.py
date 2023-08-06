2321efc25d8f1fd9c217126621b96c43479d4d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:22 +0000

    Add package newworld/1.0.0/newworld-1.0.0.tar.gz

---
+++ packages/newworld/newworld-1.0.0.tar.gz/newworld-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'newworld',
+    version = '1.0.0',
+    py_modules = ['newworld'],
+    author = 'vb',
+    author_email = 'vitali.baranov@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of phrase'
+    )
