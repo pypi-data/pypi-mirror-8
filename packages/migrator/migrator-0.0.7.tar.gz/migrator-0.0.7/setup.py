9a33cbdf5eb660db6a0ea69f7916fee72ac66cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:37 +0000

    Add package migrator/0.0.7/migrator-0.0.7.tar.gz

---
+++ packages/migrator/migrator-0.0.7.tar.gz/migrator-0.0.7/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+if __name__ == "__main__":
+    setup(name="migrator",
+          version="0.0.7",
+          description="Database Migration Assistant",
+          author="John Evans",
+          author_email="lgastako@gmail.com",
+          license="MIT",
+          url="https://github.com/lgastako/migrate",
+          install_requires=[
+              "docopt>=0.6.2",
+              "sqlalchemy",
+          ],
+          test_requires=[
+              "pytest",
+              "pytest-pep8"
+          ],
+          packages=find_packages(),
+          provides=["migrator"],
+          entry_points={
+              "console_scripts": [
+                  "migrate = migrator:main",
+              ]
+          })
