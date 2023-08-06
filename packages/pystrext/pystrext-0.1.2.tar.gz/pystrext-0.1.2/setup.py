408cb37a61cb24b25e88754eda01db3a3888c9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:07 +0000

    Add package pystrext/0.1.2/pystrext-0.1.2.tar.gz

---
+++ packages/pystrext/pystrext-0.1.2.tar.gz/pystrext-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='pystrext',
+      version='0.1.2',
+      description='Python string extension',
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+      ],
+      keywords='string helpers',
+      url='https://pypi.python.org/pypi/pystrext',
+      author='Eric Lapouyade',
+      author_email='elapouya@gmail.com',
+      license='LGPL 2.1',
+      packages=['pystrext'],
+      zip_safe=False)
