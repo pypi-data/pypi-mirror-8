9b4d68b130e4751a4195ce0f5f69e11033e2061e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:16 +0000

    Add package list_fun/1.0.0/list_fun-1.0.0.zip

---
+++ packages/list_fun/list_fun-1.0.0.zip/list_fun-1.0.0/list_fun.py
@@ -0,0 +1,10 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:16:31) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+"""这是list_fun模块，提供了一个list_fun（）函数，这个函数的作用是打印列表，其中有可能包含（也可能不包含）嵌套列表"""
+>>> def list_fun(the_list):
+"""入参the_list，指要打印的列表名称"""
+	for round_list in the_list:
+		if isinstance(round_list,list):
+			list_fun(round_list)
+		else:
+			print(round_list)
---
+++ packages/list_fun/list_fun-1.0.0.zip/list_fun-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+	name         = 'list_fun',
+	version      = '1.0.0',
+	py_modules   =  ['list_fun'],
+	author       =  'mcl_python',
+	author_email =  '405218748@qq.com',
+	description  =  'list function',
+	)
