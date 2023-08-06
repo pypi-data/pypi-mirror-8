f96f52ebdaff89cbdeb31ff1b459acda6c1a9567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:19 +0000

    Add package referredby/0.1.4/referredby-0.1.4.tar.gz

---
+++ packages/referredby/referredby-0.1.4.tar.gz/referredby-0.1.4/referredby/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+#  referredby.py
+#  referredby
+#
+
+"""
+Methods for determining who referred you.
+"""
+
+from collections import namedtuple
+
+try:
+    # python 3
+    import urllib.parse as urlparse
+except ImportError:
+    # python 2
+    import urlparse
+
+SearchEngine = namedtuple('SearchEngine', 'name domain keywords')
+MailProvider = namedtuple('MailProvider', 'name')
+
+from . import engines
+
+
+def who(url):
+    "Try to parse the referrer url into a search engine and query terms."
+    parts = urlparse.urlparse(url)
+    domain = parts.netloc
+    query = urlparse.parse_qs(parts.query)
+
+    result = engines.detect_any(domain)
+    if isinstance(result, engines.EngineDef):
+        keywords = query.get(result.param, [''])[0].split(' ') or None
+        return SearchEngine(result.name, domain, keywords)
+
+    elif isinstance(result, engines.MailDef):
+        return MailProvider(result.name)
+
+    return result
---
+++ packages/referredby/referredby-0.1.4.tar.gz/referredby-0.1.4/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+#  setup.py
+#  referredby
+#
+
+"""
+Packaging for the referredby project.
+"""
+
+from setuptools import setup
+
+VERSION = '0.1.4'
+
+setup(
+    name='referredby',
+    description="Parsing referrer URLS for common search engines.",
+    url="http://github.com/larsyencken/referredby/",
+    version=VERSION,
+    author="Lars Yencken",
+    author_email="lars@yencken.org",
+    license="ISC",
+    long_description=open('README.rst').read(),
+    packages=[
+        'referredby',
+    ],
+    test_suite='test_referredby',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
