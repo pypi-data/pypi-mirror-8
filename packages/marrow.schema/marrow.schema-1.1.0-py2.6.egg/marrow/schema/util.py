3f729467255c86ba4ea9b6327cc8329a20940240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:51 +0000

    Add package marrow.schema/1.1.0/marrow.schema-1.1.0-py2.6.egg

---
+++ packages/marrow.schema/marrow.schema-1.1.0-py2.6.egg/marrow/schema/util.py
@@ -0,0 +1,67 @@
+# encoding: utf-8
+
+"""Convienent utilities."""
+
+# ## Imports
+
+from warnings import warn
+from .compat import odict
+from .declarative import Container, Attribute
+
+
+# ## Class Definitions
+
+class Attributes(Container):
+	"""Easily access the known declarative attributes of an object, preserving definition order."""
+	
+	only = Attribute(default=None)  # Filter results based to instances of these.
+	
+	def __get__(self, obj, cls=None):
+		# make this into a view on top of obj.__attributes__
+		if not obj:
+			obj = cls
+		
+		if not self.only:
+			return obj.__attributes__.copy()
+		
+		return odict((k, v) for k, v in obj.__attributes__.items() if isinstance(v, self.only))
+
+
+# ## Function Definitions
+
+def ensure_tuple(length, tuples):
+	"""Yield `length`-sized tuples from the given collection.
+	
+	Will truncate longer tuples to the desired length, and pad using the leading element if shorter.
+	"""
+	
+	for elem in tuples:
+		# Handle non-tuples and non-lists as a single repeated element.
+		if not isinstance(elem, (tuple, list)):
+			yield (elem, ) * length
+			continue
+		
+		l = len(elem)
+		
+		# If we have the correct length already, yield it.
+		if l == length:
+			yield elem
+		
+		# If we're too long, truncate.
+		elif l > length:
+			yield tuple(elem[:length])
+		
+		# If we're too short, pad the *leading* element out.
+		elif l < length:
+			yield (elem[0], ) * (length - l) + tuple(elem)
+
+
+# ## Deprecated Classes
+# Deprecated naming conventions; for legacy use only.
+
+class DeclarativeAttributes(Attributes):
+	"""DeclarativeAttributes is now called Attributes."""
+	
+	def __init__(self, *args, **kw):
+		warn("Use of DeclarativeAttributes is deprecated, use Attributes instead.", DeprecationWarning)
+		super(DeclarativeAttributes, self).__init__(*args, **kw)
