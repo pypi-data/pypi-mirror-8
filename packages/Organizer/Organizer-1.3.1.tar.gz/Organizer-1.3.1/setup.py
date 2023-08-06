922106616a7c2a0c729641755c74271dd485f9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:04 +0000

    Add package Organizer/1.3.1/Organizer-1.3.1.tar.gz

---
+++ packages/Organizer/Organizer-1.3.1.tar.gz/Organizer-1.3.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="Organizer",
+version='1.3.1',
+py_modules=['OrganizerTools','OrganizerG','OrganizerMain'],
+author='Noah Rossignol',
+author_email='noahrossignol@yahoo.com',
+description='Organizes a list of tasks.')
