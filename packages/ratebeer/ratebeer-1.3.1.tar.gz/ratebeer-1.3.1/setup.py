298fe3e3063e791608ade859471104cf37068e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:47 +0000

    Add package ratebeer/1.3.1/ratebeer-1.3.1.tar.gz

---
+++ packages/ratebeer/ratebeer-1.3.1.tar.gz/ratebeer-1.3.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name="ratebeer",
+    version="1.3.1",
+    description="""RateBeer.com data scraper. Makes getting information from RateBeer.com as easy as:<br />
+    <br />
+    >>> rb = RateBeer()<br />
+    >>> rb.search("summit extra pale ale")<br />
+    {'beers': [{'name': [u'Summit Extra Pale Ale'],<br />
+        'num_ratings': 678,<br />
+        'rating': 73  ,<br />
+        'url': u'/beer/summit-extra-pale-ale/7344/'}],<br />
+    'breweries': []}<br />
+    <br />
+    See the full README at https://github.com/alilja/ratebeer
+    """,
+    keywords="ratebeer rate beer ratings",
+    author="Andrew Lilja",
+    author_email="andrewlilja@gmail.com",
+    url="https://github.com/alilja/ratebeer",
+    license="Unlicense (a.k.a. Public Domain)",
+    packages=["ratebeer"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires=[
+        "beautifulsoup4",
+        "lxml",
+        "requests",
+    ],
+    test_suite="test.py",
+)
