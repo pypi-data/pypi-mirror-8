42c9ed21166fd62106eff6ab03cec4172a36e444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:05 +0000

    Add package django-hmin/0.2/django-hmin-0.2.tar.gz

---
+++ packages/django-hmin/django-hmin-0.2.tar.gz/django-hmin-0.2/hmin/middleware.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+from .base import minify
+
+
+""" Minification html middleware
+"""
+ENABLED = getattr(settings, 'HTML_MINIFY', not settings.DEBUG)
+REMOVE_COMMENTS = gettattr(settings, 'HMIN_REMOVE_COMMENTS', True)
+EXCLUDE = []
+
+
+if hasattr(settings, 'HMIN_EXCLUDE'):
+    for url_pattern in settings.HMIN_EXCLUDE:
+        regex = re.compile(url_pattern)
+        EXCLUDE.append(regex)
+
+
+class MarkMiddleware(object):
+    def process_request(self, request):
+        request.need_to_minify = True
+
+
+class MinMiddleware(object):
+    def process_response(self, request, response):
+        # prevent from minifying cached pages
+        if not hasattr(request, 'need_to_minify') or\
+           not request.need_to_minify:
+            return response
+
+        # prevent from minifying excluded pages
+        if EXCLUDE:
+            path = request.path.lstrip('/')
+            for regex in EXCLUDE:
+                if regex.match(path):
+                    return response
+
+        if 'Content-Type' in response and\
+           'text/html' in response['Content-Type'] and ENABLED:
+            response.content = minify(response.content, REMOVE_COMMENTS)
+        return response
