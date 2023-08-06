dc90e854b7cdd22f7c37b04a4aeda966dc6bf494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:38 +0000

    Add package flmakesetup/0.0.2/flmakesetup-0.0.2.tar.gz

---
+++ packages/flmakesetup/flmakesetup-0.0.2.tar.gz/flmakesetup-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open("README.rst") as f:
+    readme = f.read()
+
+setup(
+    name="flmakesetup",
+    version="0.0.2",
+    author="takahrio iwatani",
+    author_email="taka.05022002@gmail.com",
+    description="easy create setup.py",
+    long_description=readme,
+    url="https://github.com/float1251/flmakesetup",
+    py_modules=["flmakesetup"],
+    entry_points={
+        "console_scripts":[
+            "flmakesetup = flmakesetup:main"
+        ]
+    }
+)
