4872059f659774d37e1b1fb7c0cfc6ecccb00b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:39 +0000

    Add package flask-restplus/0.4.0/flask-restplus-0.4.0.tar.gz

---
+++ packages/flask-restplus/flask-restplus-0.4.0.tar.gz/flask-restplus-0.4.0/flask_restplus/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+__version__ = '0.4.0'
+__description__ = 'Helpers, syntaxic sugar and Swagger documentation for Flask-Restful'
+
+try:
+    from flask.ext.restful import marshal, marshal_with, abort  # noqa
+
+    from . import fields, reqparse
+    from .api import Api  # noqa
+    from .resource import Resource  # noqa
+    from .exceptions import RestException, SpecsError, ValidationError
+
+    __all__ = (
+        '__version__',
+        '__description__',
+        'Api',
+        'Resource',
+        'marshal',
+        'marshal_with',
+        'abort',
+        'fields',
+        'reqparse',
+        'RestException',
+        'SpecsError',
+        'ValidationError',
+    )
+
+except ImportError:
+    pass
