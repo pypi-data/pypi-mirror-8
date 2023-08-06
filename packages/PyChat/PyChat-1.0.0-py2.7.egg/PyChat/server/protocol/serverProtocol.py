85462531cbbc9e49ec2a606428f9b8f407b9131c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:08 +0000

    Add package PyChat/1.0.0/PyChat-1.0.0-py2.7.egg

---
+++ packages/PyChat/PyChat-1.0.0-py2.7.egg/PyChat/server/protocol/serverProtocol.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2
+
+##
+# PyChat
+# https://github.com/leosartaj/PyChat.git
+#
+# Copyright (c) 2014 Sartaj Singh
+# Licensed under the MIT license.
+##
+
+from twisted.protocols import basic
+from twisted.python import log
+
+class serverProtocol(basic.LineReceiver):
+    """
+    Implements the server interaction protocol
+    """
+    from os import linesep as delimiter # os supported delimiter
+
+    def connectionMade(self):
+        self.peer = self.transport.getPeer()
+        self.factory.updateClients(self)
+        log.msg('Connected to %s' %(self.peer))
+
+    def lineReceived(self, line):
+        """
+        Handle recived lines
+        """
+        if line[0:2] == '$$':
+            self.peername = line[2:]
+            log.msg('PeerName of %s is %s' %(self.peer, self.peername))
+            self.relay('has joined')
+        else:
+            log.msg('Received %s from %s' %(line, self.peername))
+            self.relay(line)
+
+    def relay(self, line):
+        """
+        relay the message to other clients
+        """
+        line = self.peername + ' ' + line
+        for client in self.factory.getClients():
+            if client != self:
+                client.sendLine(line)
+
+    def connectionLost(self, reason):
+        """
+        safely disconnect user
+        """
+        self.factory.removeClients(self)
+        self.relay('has been disconnected')
+        self._logConnectionLost(reason)
+
+    def _logConnectionLost(self, reason):
+        """
+        log when connection is lost
+        """
+        line = 'Disconnected from %s' %(self.peer)
+        log.msg(line)
+        log.msg(reason.getErrorMessage())
