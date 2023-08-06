0895950a3ce9300ebcc26e1dbb25650f83f6c98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:06 +0000

    Add package django-resized/0.2.8/django-resized-0.2.8.tar.gz

---
+++ packages/django-resized/django-resized-0.2.8.tar.gz/django-resized-0.2.8/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Copyright (c) 2012 Ilya Shalyapin
+#
+#  django-resized is free software under terms of the MIT License.
+#
+
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name     = 'django-resized',
+    version  = '0.2.8',
+    packages = find_packages(),
+    requires = ['python (>= 2.5)', 'django (>= 1.0)'],
+    description  = 'Deletes old files.',
+    long_description = open('README.markdown').read(),
+    author       = 'Ilya Shalyapin',
+    author_email = 'ishalyapin@gmail.com',
+    url          = 'https://github.com/un1t/django-resized',
+    download_url = 'https://github.com/un1t/django-resized/tarball/master',
+    license      = 'MIT License',
+    keywords     = 'django',
+    classifiers  = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ],
+)
