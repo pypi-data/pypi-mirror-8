d6c9f5952abc82074ba0399fc1a64273d3cfa979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:32 +0000

    Add package GroupyAPI/0.5.8/GroupyAPI-0.5.8.tar.gz

---
+++ packages/GroupyAPI/GroupyAPI-0.5.8.tar.gz/GroupyAPI-0.5.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='GroupyAPI',
+    packages=['groupy', 'groupy.object', 'groupy.api'],
+    version='0.5.8',
+    install_requires=[
+        "responses==0.3.0",
+        "requests==2.3.0",
+        "Pillow==2.5.3",
+    ],
+    description='The simple yet powerful wrapper for the GroupMe API',
+    author='Robert Grant',
+    author_email='rhgrant10@gmail.com',
+    url='https://github.com/rhgrant10/Groupy', # use the URL to the github repo
+    keywords=['api', 'GroupMe'], # arbitrary keywords
+    classifiers=['Programming Language :: Python :: 3'],
+    long_description=open('README.rst', 'r').read()
+)
