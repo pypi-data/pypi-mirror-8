8c647b32a080a97849534de60a663ce4d5d6703d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:56 +0000

    Add package cmsplugin_contact_plus/1.1.13/cmsplugin_contact_plus-1.1.13.tar.gz

---
+++ packages/cmsplugin_contact_plus/cmsplugin_contact_plus-1.1.13.tar.gz/cmsplugin_contact_plus-1.1.13/cmsplugin_contact_plus/cms_plugins.py
@@ -0,0 +1,47 @@
+from django.utils.translation import ugettext_lazy as _
+
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+
+from cmsplugin_contact_plus.admin import ExtraFieldInline
+from cmsplugin_contact_plus.models import ContactPlus
+from cmsplugin_contact_plus.forms import ContactFormPlus
+
+
+class CMSContactPlusPlugin(CMSPluginBase):
+    model = ContactPlus
+    inlines = [ExtraFieldInline, ]
+    name = _('Contact Form')
+    render_template = "cmsplugin_contact_plus/contact.html"
+    cache = False
+
+    def render(self, context, instance, placeholder):
+        request = context['request']
+
+        if instance and instance.template:
+            self.render_template = instance.template
+
+        if request.method == "POST":
+            form = ContactFormPlus(contactFormInstance=instance, request=request, data=request.POST)
+            if form.is_valid():
+                form.send(instance.recipient_email, request, instance)
+                context.update({
+                    'contact': instance,
+                })
+                return context
+            else:
+                context.update({
+                    'contact': instance,
+                    'form': form,
+                })
+
+        else:
+            form = ContactFormPlus(contactFormInstance=instance, request=request)
+            context.update({
+                    'contact': instance,
+                    'form': form,
+            })
+        return context
+
+
+plugin_pool.register_plugin(CMSContactPlusPlugin)
