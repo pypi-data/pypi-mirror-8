6fb6eb92f1946eefed5cae0edd9a0083469a9ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:58 +0000

    Add package dbf/0.96.002/dbf-0.96.002.tar.gz

---
+++ packages/dbf/dbf-0.96.002.tar.gz/dbf-0.96.002/setup.py
@@ -0,0 +1,46 @@
+from distutils.core import setup
+from glob import glob
+import os
+import sys
+
+#html_docs = glob('dbf/html/*')
+
+long_desc="""
+Currently supports dBase III, FoxPro, and Visual FoxPro tables. Text is returned as unicode, and codepage settings in tables are honored. Memos and Null fields are supported.  Documentation needs work, but author is very responsive to e-mails.
+
+Not supported: index files (but can create tempory non-file indexes), auto-incrementing fields, and Varchar fields.
+
+Installation:  `pip install dbf`
+
+There may be messages about byte-compiling failures -- you can safely ignore them (this is a multi-version release, and 2 and 3 don't like some of each other's code).
+"""
+
+if sys.version_info[:2] < (3, 4):
+    requirements = ['enum34']
+else:
+    requirements = []
+
+setup( name='dbf',
+       version= '0.96.002',
+       license='BSD License',
+       description='Pure python package for reading/writing dBase, FoxPro, and Visual FoxPro .dbf files (including memos)',
+       long_description=long_desc,
+       url='https://pypi.python.org/pypi/dbf',
+       packages=['dbf', ],
+       provides=['dbf'],
+       install_requires=requirements,
+       author='Ethan Furman',
+       author_email='ethan@stoneleaf.us',
+       classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Topic :: Database',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            ],
+    )
+
