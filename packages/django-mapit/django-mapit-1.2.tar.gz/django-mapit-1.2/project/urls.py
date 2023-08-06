909a00bd21f7402023ff6cb636473998b7306199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:33 +0000

    Add package django-mapit/1.2/django-mapit-1.2.tar.gz

---
+++ packages/django-mapit/django-mapit-1.2.tar.gz/django-mapit-1.2/project/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include
+from django.contrib import admin
+admin.autodiscover()
+
+handler500 = 'mapit.shortcuts.json_500'
+
+urlpatterns = patterns(
+    '',
+    (r'^', include('mapit.urls')),
+    (r'^admin/', include(admin.site.urls)),
+)
