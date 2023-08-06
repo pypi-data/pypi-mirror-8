5aa594472661a42cbd81b9bc0c39b3510606c0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:39 +0000

    Add package aldryn-blog/0.4.6/aldryn-blog-0.4.6.tar.gz

---
+++ packages/aldryn-blog/aldryn-blog-0.4.6.tar.gz/aldryn-blog-0.4.6/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from aldryn_blog import __version__
+
+REQUIREMENTS = [
+    'django-taggit<0.12',
+    'django-filer',
+    'django_select2',
+    'djangocms-text-ckeditor',
+    'django-appconf',
+    'django-classy-tags',
+    'south>=0.8',
+    'aldryn_common',
+    'django-hvad',
+    'unidecode',
+    'django-appdata',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+setup(
+    name='aldryn-blog',
+    version=__version__,
+    description='Adds blogging abilities to django CMS',
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/aldryn/aldryn-blog',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+    zip_safe=False
+)
