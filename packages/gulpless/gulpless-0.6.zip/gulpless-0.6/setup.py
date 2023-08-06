b05d592c13721ae3a6a364488a17b9f66d475167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:21 +0000

    Add package gulpless/0.6/gulpless-0.6.zip

---
+++ packages/gulpless/gulpless-0.6.zip/gulpless-0.6/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="gulpless",
+      version="0.6",
+      description="Simple python build system",
+      author="Radu Dan",
+      author_email="za_creature@yahoo.com",
+      url="http://git.full-throttle.ro/radu/gulpless",
+      packages=["gulpless"],
+      install_requires=["watchdog", "termcolor", "pathtools"],
+      entry_points={"console_scripts": ["gulpless=gulpless:main"]})
