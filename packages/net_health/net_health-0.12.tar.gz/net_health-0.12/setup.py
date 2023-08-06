de95af735513f66d7b9b9da7f04094a7114cc1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:13 +0000

    Add package net_health/0.12/net_health-0.12.tar.gz

---
+++ packages/net_health/net_health-0.12.tar.gz/net_health-0.12/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='net_health',
+      version='0.12',
+      description='check network connectivity',
+      url='',
+      author='Gilad Zohari',
+      author_email='gzohari@gmail.com',
+      scripts=['bin/check_connectivity'],
+      license='MIT',
+      install_requires=[
+          'python-neutronclient', 'python-novaclient'
+      ],
+      packages=['net_health'],
+      entry_points={
+          'ceilometer.poll.compute': [
+              'latency_pollster=ceilometer.compute.pollsters.latency_pollster:LatencyPollster',
+          ],
+      },
+      zip_safe=False)
