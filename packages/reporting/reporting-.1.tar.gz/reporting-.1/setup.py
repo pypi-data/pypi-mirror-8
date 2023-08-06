68c7c625c7e378513d15f17192a342b0c67be3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:35 +0000

    Add package reporting/.1/reporting-.1.tar.gz

---
+++ packages/reporting/reporting-.1.tar.gz/reporting-.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+""" setup.py for report
+"""
+
+from setuptools import setup
+
+setup(
+    name         = 'reporting',
+    version      = '.1',
+    description  = 'patterns in reporting',
+    author       = 'mattvonrocketstein',
+    author_email = '$author@gmail',
+    url          = 'http://github.com/mattvonrocketstein/reporting',
+    download_url = 'https://github.com/mattvonrocketstein/reporting/tarball/pypi',
+    py_modules   = ['report'],
+)
