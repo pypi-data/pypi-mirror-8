277c05a1dc88b5e7493e6e08a552b1ecd0b1312b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:36 +0000

    Add package django-versioned-static-url/0.1.1/django-versioned-static-url-0.1.1.tar.gz

---
+++ packages/django-versioned-static-url/django-versioned-static-url-0.1.1.tar.gz/django-versioned-static-url-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='django-versioned-static-url',
+    version='0.1.1',
+    author='Robin',
+    author_email='robin.winslow@canonical.com',
+    url='https://github.com/ubuntudesign/django-versioned-static-url',
+    packages=[
+        'django_versioned_static_url',
+        'django_versioned_static_url.templatetags'
+    ],
+    description=(
+        'A template tag for generating a URL for a static file, '
+        'with a version string which will be effectively unique '
+        'based on the contents of the file.'
+    ),
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "Django >= 1.3",
+    ],
+)
