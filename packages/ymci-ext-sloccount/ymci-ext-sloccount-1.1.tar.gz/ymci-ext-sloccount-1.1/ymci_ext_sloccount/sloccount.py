4b21322c3e059d71188a7ad485a23339a36befc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:46 +0000

    Add package ymci-ext-sloccount/1.1/ymci-ext-sloccount-1.1.tar.gz

---
+++ packages/ymci-ext-sloccount/ymci-ext-sloccount-1.1.tar.gz/ymci-ext-sloccount-1.1/ymci_ext_sloccount/sloccount.py
@@ -0,0 +1,31 @@
+from ymci.ext.hooks import BuildHook
+
+import os
+from collections import defaultdict
+
+from .db import Sloccount
+
+
+class SloccountHook(BuildHook):
+
+    @property
+    def active(self):
+        return True
+
+    def validate_build(self):
+        sloccount_file = os.path.join(self.build.dir, 'sloccount.sc')
+        if os.path.exists(sloccount_file):
+            values = defaultdict(int)
+            with open(sloccount_file, 'r') as sloc:
+                lines = sloc.read()
+            _, sloc = lines.split('\n\n\n')
+
+            for line in sloc.splitlines():
+                count, language = line.split('\t')[0:2]
+                values[language] += int(count)
+            for language in values.keys():
+                self.build.sloccounts.append(Sloccount(
+                    language=language, count=values[language]))
+            self.out('Source Lines of Code: %s' % ', '.join(
+                ['%s %s lines' % (count, language) for language, count in
+                 values.items()]))
