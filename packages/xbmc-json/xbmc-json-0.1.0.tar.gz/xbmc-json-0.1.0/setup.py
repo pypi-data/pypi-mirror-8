b7df56031acb7d436394fed33c290d1cbfd66061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:49 +0000

    Add package xbmc-json/0.1.0/xbmc-json-0.1.0.tar.gz

---
+++ packages/xbmc-json/xbmc-json-0.1.0.tar.gz/xbmc-json-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import version
+
+PACKAGE = 'xbmc-json'
+
+setup(name = PACKAGE, version = version.VERSION,
+    license = "WTFPL",
+    description = "Python module for controlling xbmc over HTTP Json API",
+    author = "Jean-Christophe Saad-Dupuy",
+    author_email = "saad.dupuy@gmail.com",
+    url = "https://github.com/jcsaaddupuy/python-xbmc",
+    py_modules = [ "xbmcjson/xbmcjson" ],
+    packages = ["xbmcjson"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        ]
+    )
