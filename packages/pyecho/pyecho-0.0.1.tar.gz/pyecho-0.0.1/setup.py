17eec2c75bc34d53beeaf5d6f5d96bf55ceabfe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:24 +0000

    Add package pyecho/0.0.1/pyecho-0.0.1.tar.gz

---
+++ packages/pyecho/pyecho-0.0.1.tar.gz/pyecho-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyecho',
+      version='0.0.1',
+      description='A micro library for retrying failing operations.',
+      url='http://github.com/itsnauman/echo',
+      author='Nauman Ahmad',
+      author_email='nauman-ahmad@outlook.com',
+      license='MIT',
+      packages=['pyecho'],
+      zip_safe=False)
