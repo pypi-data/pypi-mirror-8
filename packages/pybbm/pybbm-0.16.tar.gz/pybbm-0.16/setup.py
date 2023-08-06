9f7719f5c648f6e547518a15e84b6908f8a0d7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:53 +0000

    Add package pybbm/0.16/pybbm-0.16.tar.gz

---
+++ packages/pybbm/pybbm-0.16.tar.gz/pybbm-0.16/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    version='0.16',
+    description='PyBB Modified. Django forum application',
+    long_description=open('README.rst').read(),
+    author='Pavel Zhukov',
+    author_email='gelios@gmail.com',
+    name='pybbm',
+    url='http://www.pybbm.org/',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['pybb/templates', 'pybb/static']},
+    install_requires=[
+        'django-annoying',
+    ],
+    test_suite='runtests.runtests',
+    license="BSD",
+    keywords="django application forum board",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
