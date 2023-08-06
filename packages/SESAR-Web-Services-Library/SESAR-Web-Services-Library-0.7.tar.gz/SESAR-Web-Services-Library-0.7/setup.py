cb5bb0b1b634cc4af2c6375aa1344728025dcfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:47 +0000

    Add package SESAR-Web-Services-Library/0.7/SESAR-Web-Services-Library-0.7.tar.gz

---
+++ packages/SESAR-Web-Services-Library/SESAR-Web-Services-Library-0.7.tar.gz/SESAR-Web-Services-Library-0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='SESAR-Web-Services-Library',
+      version='0.7',
+      description='SESAR web services library for IGSN management.',
+      url='https://github.com/Adam-Brown/SESAR-Web-Services-Lib',
+      author='Adam David Brown',
+      author_email='adam.brown@evbane.com',
+      license='MIT',
+      packages=['sesarwslib', 'sesarwslib.sample'],
+      zip_safe=False)
