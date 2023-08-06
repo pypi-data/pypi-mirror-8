c190e397713cc0a58a02fbded90c61575caaf699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:56 +0000

    Add package mr.bob2/0.2.2/mr.bob2-0.2.2.tar.gz

---
+++ packages/mr.bob2/mr.bob2-0.2.2.tar.gz/mr.bob2-0.2.2/mrbob/docs/source/developer.rst
@@ -0,0 +1,33 @@
+.. highlight:: bash
+
+Developer guide
+===============
+
+Setup developer environment
+---------------------------
+
+::
+
+    $ git clone https://github.com/iElectric/mr.bob.git
+    $ cd mrbob
+    $ virtualenv .
+    $ source bin/activate
+    $ python setup.py develop
+    $ easy_install mr.bob[test,development]
+    $ mrbob --help
+
+
+Running tests
+-------------
+
+Easy as::
+
+    $ make test
+
+
+Making a Release
+----------------
+
+Using `zest.releaser`::
+
+    $ bin/fullrelease
---
+++ packages/mr.bob2/mr.bob2-0.2.2.tar.gz/mr.bob2-0.2.2/setup.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import codecs
+
+from setuptools import setup
+from setuptools import find_packages
+
+install_requires = [
+    'setuptools',
+    'six>=1.2.0',  # 1.1.0 release doesn't have six.moves.input
+]
+
+if (3,) < sys.version_info < (3, 3):
+    # Jinja 2.7 drops Python 3.2 compat.
+    install_requires.append('Jinja2>=2.5.0,<2.7dev')
+else:
+    install_requires.append('Jinja2>=2.5.0')
+
+try:
+    import importlib  # NOQA
+except ImportError:
+    install_requires.append('importlib')
+
+try:
+    from collections import OrderedDict  # NOQA
+except ImportError:
+    install_requires.append('ordereddict')
+
+try:
+    import argparse  # NOQA
+except ImportError:
+    install_requires.append('argparse')
+
+
+def read(*rnames):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *rnames), 'r', 'utf-8').read()
+
+
+setup(name='mr.bob2',
+      version='0.2.2',
+      description='Bob renders directory structure templates',
+      long_description=read('README.rst') + read('HISTORY.rst'),
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+      ],
+      author='Domen Kozar, Tom Lazar',
+      author_email='',
+      url='https://github.com/iElectric/mr.bob.git',
+      license='BSD',
+      packages=find_packages(),
+      install_requires=install_requires,
+      extras_require={
+          'test': [
+              'nose',
+              'coverage<3.6dev',
+              'flake8>2.0',
+              'mock',
+          ],
+          'development': [
+              'zest.releaser',
+              'Sphinx',
+          ],
+      },
+      entry_points="""
+      [console_scripts]
+      mrbob = mrbob.cli:main
+      """,
+      include_package_data=True,
+      zip_safe=False,
+      )
