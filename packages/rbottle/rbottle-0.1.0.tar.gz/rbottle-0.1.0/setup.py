0420e8249394a0c6d21f065d8facc51a37e5e5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:03 +0000

    Add package rbottle/0.1.0/rbottle-0.1.0.tar.gz

---
+++ packages/rbottle/rbottle-0.1.0.tar.gz/rbottle-0.1.0/setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+from setuptools import setup, find_packages
+
+
+# Functions & classes =========================================================
+setup(
+    name='rbottle',
+    version='0.1.0',
+    description="Decorators to make REST easier in Bottle.",
+    long_description=open("README.rst").read(),
+    url='https://github.com/Bystroushaak/rbottle',
+
+    author='Bystroushaak',
+    author_email='bystrousak@kitakitsune.org',
+
+    classifiers=[
+        "Framework :: Bottle",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license='MIT',
+
+    packages=find_packages(".", exclude=['ez_setup']),
+
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        "bottle",
+        "sphinxcontrib-napoleon>=0.2.5"  # napoleon docstrings
+    ],
+)
