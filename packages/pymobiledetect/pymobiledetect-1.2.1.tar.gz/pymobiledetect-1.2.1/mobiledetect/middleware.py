09cd4b9de14d22eed264c8bfa2f06d69f3dfd500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:33 +0000

    Add package pymobiledetect/1.2.1/pymobiledetect-1.2.1.tar.gz

---
+++ packages/pymobiledetect/pymobiledetect-1.2.1.tar.gz/pymobiledetect-1.2.1/mobiledetect/middleware.py
@@ -0,0 +1,16 @@
+"""
+WSGI and Django Middleware for mobiledetect
+"""
+
+
+class BaseMobileDetectMiddleware(object):
+    pass
+
+
+class WSGIMobileDetectMiddleware(BaseMobileDetectMiddleware):
+    pass
+
+
+class DjangoMobileDetectMiddleware(BaseMobileDetectMiddleware):
+    pass
+
