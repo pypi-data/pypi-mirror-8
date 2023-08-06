cb6b88c445a3f7d88145110147b84173d6b5af06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:12 +0000

    Add package goolabs/0.1.0/goolabs-0.1.0.tar.gz

---
+++ packages/goolabs/goolabs-0.1.0.tar.gz/goolabs-0.1.0/tests/test_commands.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    unittest for "goolabs" command line tool
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :author: tell-k <ffk2005@gmail.com>
+    :copyright: tell-k. All Rights Reserved.
+"""
+from __future__ import division, print_function, absolute_import, unicode_literals  # NOQA
