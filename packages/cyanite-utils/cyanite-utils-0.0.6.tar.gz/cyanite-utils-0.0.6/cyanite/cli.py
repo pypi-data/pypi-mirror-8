e81554cc1944105bc24d0ed62f7b806998d1c243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:57 +0000

    Add package cyanite-utils/0.0.6/cyanite-utils-0.0.6.tar.gz

---
+++ packages/cyanite-utils/cyanite-utils-0.0.6.tar.gz/cyanite-utils-0.0.6/cyanite/cli.py
@@ -0,0 +1,82 @@
+import argparse
+
+from .Config import Config
+from .CyaniteCassandra import CyaniteCassandra
+from .CyaniteMetrics import CyaniteMetrics
+from .CyanitePaths import CyanitePaths
+from .util import common_parser, catch_sigint
+
+def cyanite_list():
+    catch_sigint()
+    parser = common_parser('List metrics')
+
+    parser.add_argument(
+        'metric',
+        metavar='METRIC',
+        nargs='*',
+        type=str,
+        help='metric name to search for')
+
+    args = parser.parse_args()
+    config = Config(args.config_file)
+    if args.verbose:
+        config.setverbose()
+
+    paths = CyanitePaths(config)
+
+    if len(args.metric) == 0:
+        paths.printpaths('*')
+    else:
+        for metric in args.metric:
+            paths.printpaths(metric)
+
+def cyanite_delete():
+    catch_sigint()
+    parser = common_parser('Delete metrics')
+
+    parser.add_argument(
+        'metric',
+        metavar='METRIC',
+        nargs='*',
+        type=str,
+        help='metric name to delete')
+
+    args = parser.parse_args()
+    config = Config(args.config_file)
+    if args.verbose:
+        config.setverbose()
+
+    cyanite = CyaniteCassandra(config)
+    paths = CyanitePaths(config)
+
+    for metric in args.metric:
+        cyanite.delete(metric)
+        paths.delete(metric)
+
+def cyanite_prune():
+    catch_sigint()
+    parser = common_parser('Prune metrics')
+
+    parser.add_argument(
+        '-s', '--seconds',
+        default='101520',
+        help='seconds of inactivity to prune')
+
+    parser.add_argument(
+        'metric',
+        metavar='METRIC',
+        nargs='*',
+        type=str,
+        help='metric name to prune')
+
+    args = parser.parse_args()
+    config = Config(args.config_file)
+    if args.verbose:
+        config.setverbose()
+    if args.seconds:
+        config.settimefrom(args.seconds)
+
+    metrics = CyaniteMetrics(config)
+
+    for metric in args.metric:
+        metrics.prune(metric)
