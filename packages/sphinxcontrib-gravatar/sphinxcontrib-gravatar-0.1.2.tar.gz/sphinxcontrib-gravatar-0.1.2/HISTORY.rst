0269b6f71203148aad1641afe22595ea47edc3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:54 +0000

    Add package sphinxcontrib-gravatar/0.1.2/sphinxcontrib-gravatar-0.1.2.tar.gz

---
+++ packages/sphinxcontrib-gravatar/sphinxcontrib-gravatar-0.1.2.tar.gz/sphinxcontrib-gravatar-0.1.2/HISTORY.rst
@@ -0,0 +1,15 @@
+History
+=======
+
+0.1.2(Jan 1, 2015)
+---------------------
+* Exclude test code from installed packages
+
+0.1.1(Dec 31, 2014)
+---------------------
+* Drop Python-3.2 support. #1
+* Support wheel format.
+
+0.1.0(Oct 2, 2014)
+---------------------
+* First release
---
+++ packages/sphinxcontrib-gravatar/sphinxcontrib-gravatar-0.1.2.tar.gz/sphinxcontrib-gravatar-0.1.2/setup.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+long_description = '\n'.join([
+    open(os.path.join(".", "README.rst")).read(),
+    open(os.path.join(".", "AUTHORS.rst")).read(),
+    open(os.path.join(".", "HISTORY.rst")).read(),
+])
+
+requires = ['Sphinx>=1.2']
+
+tests_require = [
+    "pytest-cov",
+    "pytest",
+    "mock",
+]
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    'Topic :: Documentation',
+    'Topic :: Utilities',
+]
+
+setup(
+    name='sphinxcontrib-gravatar',
+    version='0.1.2',
+    url='https://github.com/tell-k/sphinxcontrib-gravatar',
+    download_url='http://pypi.python.org/pypi/sphinxcontrib-gravatar',
+    license='BSD',
+    author='tell-k',
+    author_email='ffk2005 at gmail.com',
+    description='Sphinx "gravatar" extension',
+    long_description=long_description,
+    zip_safe=False,
+    classifiers=classifiers,
+    platforms='any',
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=requires,
+    tests_require=tests_require,
+    cmdclass={'test': PyTest},
+    namespace_packages=['sphinxcontrib'],
+)
