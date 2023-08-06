ff58dab417548cbf38545d4e79bb146f300cfb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:56 +0000

    Add package arc_library/1.0.20/arc_library-1.0.20.tar.gz

---
+++ packages/arc_library/arc_library-1.0.20.tar.gz/arc_library-1.0.20/arclib/django/decorators.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+
+from arclib.django.object import ArcReturnJSONObject
+
+def api(func):
+    @csrf_exempt
+    @require_http_methods(["POST"])
+    def wrap(request, *args, **kwargs):
+        return func(request, *args, **kwargs)
+
+    wrap.__doc__ = func.__doc__
+    wrap.__name__ = func.__name__
+    return wrap
+
+def exception_json(func):
+    def wrap(request, *args, **kwargs):
+        try:
+            func(request, *args, **kwargs)
+        except:
+            return ArcReturnJSONObject(False, 'exception_json').json_response()
+    return wrap
