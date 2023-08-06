ca07ba9756107a7a51ee8eb0704f6b2c9e65ef3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:13 +0000

    Add package django-jsignature/0.7.6/django-jsignature-0.7.6.tar.gz

---
+++ packages/django-jsignature/django-jsignature-0.7.6.tar.gz/django-jsignature-0.7.6/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='django-jsignature',
+    version='0.7.6',
+    author='Florent Lebreton',
+    author_email='florent.lebreton@makina-corpus.com',
+    url='https://github.com/fle/django-jsignature',
+    download_url="https://github.com/fle/django-jsignature/tarball/0.7.6",
+    description="Use jSignature jQuery plugin in your django projects",
+    long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' +
+		     open(os.path.join(here, 'CHANGES')).read(),
+    license='LPGL, see LICENSE file.',
+    install_requires=['Django'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Topic :: Utilities',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2.7'],
+)
