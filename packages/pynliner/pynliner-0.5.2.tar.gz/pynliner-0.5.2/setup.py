21fc9da82ca749531046b160f29257c77e625f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:33 +0000

    Add package pynliner/0.5.2/pynliner-0.5.2.tar.gz

---
+++ packages/pynliner/pynliner-0.5.2.tar.gz/pynliner-0.5.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+install_requires = [
+    'BeautifulSoup >=3.2.1,<4.0',
+    'cssutils >=0.9.7',
+]
+
+tests_require = [
+    'mock'
+] + install_requires
+
+setup(name='pynliner',
+      version='0.5.2',
+      description='Python CSS-to-inline-styles conversion tool for HTML using'
+                  ' BeautifulSoup and cssutils',
+      author='Tanner Netterville',
+      author_email='tannern@gmail.com',
+      install_requires=install_requires,
+      tests_require=tests_require,
+      test_suite='tests',
+      packages=['pynliner'],
+      provides=['pynliner'])
