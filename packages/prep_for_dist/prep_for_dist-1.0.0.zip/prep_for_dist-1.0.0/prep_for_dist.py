dcae4df718f07c0d320cd9211c1f3e1bd7c225c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:57 +0000

    Add package prep_for_dist/1.0.0/prep_for_dist-1.0.0.zip

---
+++ packages/prep_for_dist/prep_for_dist-1.0.0.zip/prep_for_dist-1.0.0/prep_for_dist.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+import sys
+
+def print_sys_path():
+  print(sys.path)
---
+++ packages/prep_for_dist/prep_for_dist-1.0.0.zip/prep_for_dist-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+ name = 'prep_for_dist',
+ version = '1.0.0',
+ py_modules = ['prep_for_dist'],
+ author = 'clarus',
+ author_email = 'clarus09@gmail.com',
+ url = 'http://www.google.com',
+ description = 'A simple prep_for_dist')
