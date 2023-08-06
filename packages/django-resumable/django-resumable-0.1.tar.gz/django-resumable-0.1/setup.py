13b812331657595741cde1b3baf67415248491b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:52 +0000

    Add package django-resumable/0.1/django-resumable-0.1.tar.gz

---
+++ packages/django-resumable/django-resumable-0.1.tar.gz/django-resumable-0.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='django-resumable',
+    version='0.1',
+    author=u'jean-philippe serafin',
+    author_email='serafinjp@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/jeanphix/django-resumable',
+    license='MIT licence',
+    description='Django resumable uploads',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'Django>=1.4',
+        'python-magic',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    zip_safe=False,
+)
