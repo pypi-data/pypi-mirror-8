c1b5f02d07752877d96cca68fa33be99ca02711c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:11 +0000

    Add package eps/1.4.2/eps-1.4.2.tar.gz

---
+++ packages/eps/eps-1.4.2.tar.gz/eps-1.4.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='eps',
+      version='1.4.2',
+      description='Easy plugin system',
+      author='Oleg Nechaev',
+      author_email='lega911@gmail.com',
+      url='https://bitbucket.org/lega911/eps',
+      packages=['eps']
+    )
