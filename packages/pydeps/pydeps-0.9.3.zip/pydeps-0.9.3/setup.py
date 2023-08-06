c9812b38c18f9e71595d7461de808f45be7f9dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:23 +0000

    Add package pydeps/0.9.3/pydeps-0.9.3.zip

---
+++ packages/pydeps/pydeps-0.9.3.zip/pydeps-0.9.3/setup.py
@@ -0,0 +1,25 @@
+
+import setuptools
+from setuptools import setup
+#from distutils.core import setup
+
+setup(
+    name='pydeps',
+    version='0.9.3',
+    packages=['pydeps'],
+    install_requires=[
+        'enum34'
+    ],
+    entry_points={
+        'console_scripts': [
+            #'py2dep = pydeps.py2depgraph:py2depgraph',
+            #'dep2dot = pydeps.depgraph2dot:depgraph2dot',
+            'pydeps = pydeps.pydeps:pydeps',
+        ]
+    },
+    url='https://github.com/thebjorn/pydeps',
+    license='BSD',
+    author='bjorn',
+    author_email='bp@datakortet.no',
+    description='Display module dependencies'
+)
