7f33937c847b93865a364fdbfc4b05befd03bc2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:53 +0000

    Add package disref/0.2/disref-0.2.tar.gz

---
+++ packages/disref/disref-0.2.tar.gz/disref-0.2/setup.py
@@ -0,0 +1,29 @@
+__author__ = "andrew.kelleher@buzzfeed.com (Andrew Kelleher)"
+
+try:
+  from setuptools import setup, find_packages
+except ImportError:
+  import distribute_setup
+  distribute_setup.use_setuptools()
+  from setuptools import setup, find_packages
+
+setup(
+    name='disref',
+    version='0.2',
+    packages=find_packages(),
+    author='Andrew Kelleher', 
+    author_email='andrew.kelleher@buzzfeed.com', 
+    description='Provides easy, fault tolerant, distributed references with redis as a backend.',
+    test_suite='test',
+    install_requires=[
+        'sherlock==0.3.0',
+        'pytz==2014.10',
+    ],
+    url='http://www.github.com/buzzfeed/disref',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Development Status :: 2 - Pre-Alpha',
+    ],
+    keywords="distributed reference references aggregation pipeline big data online algorithm"
+)
