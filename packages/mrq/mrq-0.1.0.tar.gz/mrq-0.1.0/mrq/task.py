01bf5435b81efef136aa90dce70bc7b8c0df4fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:35 +0000

    Add package mrq/0.1.0/mrq-0.1.0.tar.gz

---
+++ packages/mrq/mrq-0.1.0.tar.gz/mrq-0.1.0/mrq/task.py
@@ -0,0 +1,15 @@
+
+class Task(object):
+
+    cancel_on_retry = False
+    cancel_on_exception = False
+    cancel_on_timeout = False
+
+    # Are we the first task that a Job called?
+    is_main_task = False
+
+    def __init__(self):
+        pass
+
+    def run(self, params):
+        raise NotImplementedError
