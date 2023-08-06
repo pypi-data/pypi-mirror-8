094f9804dd7e43f74fcd3c2304a9d0706897be51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package spirol/1.0.0/spirol-1.0.0-py2.py3-none-any.whl

---
+++ packages/spirol/spirol-1.0.0-py2.py3-none-any.whl/spirol/__init__.py
@@ -0,0 +1,20 @@
+#    -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from spirol.core import spirol_core as spirol
+from spirol.errors import InputFormatError, UnknownImplementation, InvalidDirection, NotSupportedError, \
+    NonSpirolInterface
+from spirol.utils import SpirolType, SpirolDirection, SpirolCorner, TOP_LEFT, TOP_RIGHT, BOTTOM_RIGHT, BOTTOM_LEFT
+
+
+__author__ = 'Francis Horsman'
+__version__ = '1.0.0'
+__url__ = 'https://bitbucket.org/sys-git/spirol'
+__email__ = 'francis.horsman@gmail.com'
+__short_description__ = 'A pure-python non-repeating outside-in spiral iterator-generator for 2D arrays'
+__long_description__ = __short_description__ + \
+                        '.\nSpirals can be constructed clockwise or counter-clockwise and start from any corner.'
+
+if __name__ == '__main__':      # pragma: no cover
+    print(__long_description__)
