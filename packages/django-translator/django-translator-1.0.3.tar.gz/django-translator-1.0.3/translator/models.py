ba8fe350bb699cf79a3354aec85fb2ec56ab3b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:15 +0000

    Add package django-translator/1.0.3/django-translator-1.0.3.tar.gz

---
+++ packages/django-translator/django-translator-1.0.3.tar.gz/django-translator-1.0.3/translator/models.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from django.core.cache import cache
+from django.db import models
+from taggit.managers import TaggableManager
+from django.conf import settings
+from translator.util import get_key
+
+
+class Translation(models.Model):
+    key = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+    tags = TaggableManager(blank=True)
+
+    def __unicode__(self):
+        return self.key
+
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+
+        all_translation_keys = Translation.objects.all().values_list('key', flat=True)
+        for l in settings.LANGUAGES:
+            cache.delete_many([get_key(l[0], k) for k in all_translation_keys])
+        return super(Translation,self).save(force_insert, force_update, using, update_fields)
