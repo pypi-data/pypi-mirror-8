9c95187a2057241ca27f51ed10dff777ba7f3c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:31 +0000

    Add package dawn/0.1/dawn-0.1.tar.gz

---
+++ packages/dawn/dawn-0.1.tar.gz/dawn-0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+	name='dawn',
+	version='0.1',
+	description='Open and modify ePubs',
+	author='Arthur Darcet',
+	author_email='hello+dawn@glose.com',
+	url='http://github.com/Glose/dawn',
+	packages=['dawn'],
+	install_requires=[
+		'lxml>=3.4.0',
+		'oset>=0.1.3',
+	],
+	test_suite='test',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'Programming Language :: Python :: 3.3',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+)
