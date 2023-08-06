c4c95f32f32391135b42d5d2d35d80b3d27b4470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:10 +0000

    Add package pythontop40/0.1.0.dev2/pythontop40-0.1.0.dev2.zip

---
+++ packages/pythontop40/pythontop40-0.1.0.dev2.zip/pythontop40-0.1.0.dev2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+import json
+from os import path
+
+PACKAGE_DIR = "pythontop40"
+LONG_DESCRIPTION_FILE = "README.rst"
+PROJECT_DIR = path.abspath(path.dirname(__file__))
+# Get the long description from the relevant file
+with open(path.join(PROJECT_DIR, LONG_DESCRIPTION_FILE), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(
+        '{}/package_info.json'.format(PACKAGE_DIR)
+) as fp:
+    _info = json.load(fp)
+
+setup(
+    name='pythontop40',
+    version=_info['version'],
+    packages=find_packages(exclude=['tests']),
+    url=_info['url'],
+    license=_info['license'],
+    author=_info['author'],
+    author_email=_info['author_email'],
+    description=_info['description'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    install_requires=[
+        'munch==2.0.2',
+        'nap==2.0.0',
+        'arrow==0.4.4',
+        'booby>=0.7.0'
+    ],
+    dependency_links=[
+      'https://github.com/DannyGoodall/booby.git#egg=booby'
+    ]
+)
