77e87087f47e701d6a28f37fd263e69a837a07be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:36 +0000

    Add package rattail/0.3.41/rattail-0.3.41.tar.gz

---
+++ packages/rattail/rattail-0.3.41.tar.gz/rattail-0.3.41/rattail/db/api/people.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8  -*-
+################################################################################
+#
+#  Rattail -- Retail Software Framework
+#  Copyright © 2010-2012 Lance Edgar
+#
+#  This file is part of Rattail.
+#
+#  Rattail is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU Affero General Public License as published by the Free
+#  Software Foundation, either version 3 of the License, or (at your option)
+#  any later version.
+#
+#  Rattail is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Rattail.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+"""
+People API
+"""
+
+from sqlalchemy.orm.exc import NoResultFound
+
+from rattail.db import model
+
+
+__all__ = ['get_employee_by_id']
+
+
+def get_employee_by_id(session, employee_id):
+    """
+    Fetch an employee record by ID.
+    """
+
+    try:
+        return session.query(model.Employee)\
+            .filter(model.Employee.id == employee_id)\
+            .one()
+    except NoResultFound:
+        return None
