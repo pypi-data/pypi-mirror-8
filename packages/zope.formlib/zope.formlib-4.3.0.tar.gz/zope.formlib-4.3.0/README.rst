43d94d11e820e6995086bdee31a67670cd3bbb1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:10 +0000

    Add package zope.formlib/4.3.0/zope.formlib-4.3.0.tar.gz

---
+++ packages/zope.formlib/zope.formlib-4.3.0.tar.gz/zope.formlib-4.3.0/README.rst
@@ -0,0 +1,9 @@
+.. contents::
+
+========
+Overview
+========
+
+Forms are web components that use widgets to display and input data.
+Typically a template displays the widgets by accessing an attribute or
+method on an underlying class.
---
+++ packages/zope.formlib/zope.formlib-4.3.0.tar.gz/zope.formlib-4.3.0/setup.py
@@ -0,0 +1,113 @@
+##############################################################################
+#
+# Copyright (c) 2006-2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.formlib package
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+def alltests():
+    import os
+    import sys
+    import unittest
+    # use the zope.testrunner machinery to find all the
+    # test suites we've put under ourselves
+    import zope.testrunner.find
+    import zope.testrunner.options
+    here = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+    args = sys.argv[:]
+    defaults = ["--test-path", here]
+    options = zope.testrunner.options.get_options(args, defaults)
+    suites = list(zope.testrunner.find.find_suites(options))
+    return unittest.TestSuite(suites)
+
+setup(name='zope.formlib',
+      version='4.3.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Form generation and validation library for Zope',
+      long_description=(read('README.rst')
+                        + '\n\n' +
+                        read('src', 'zope', 'formlib', 'form.txt')
+                        + '\n\n' +
+                        read('src', 'zope', 'formlib', 'widgets.txt')
+                        + '\n\n' +
+                        read('src', 'zope', 'formlib', 'errors.txt')
+                        + '\n\n' +
+                        read('CHANGES.rst')
+                        ),
+      license='ZPL 2.1',
+      keywords="zope3 form widget",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3',
+          ],
+      url='http://pypi.python.org/pypi/zope.formlib',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope',],
+      extras_require=dict(
+          test=['zope.configuration',
+                'zope.testing',
+               ]
+          ),
+      install_requires=[
+        'setuptools',
+        'pytz',
+        'zope.browser>=1.1',
+        'zope.browserpage>=3.11.0',
+        'zope.component',
+        'zope.event',
+        'zope.i18n',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.lifecycleevent',
+        'zope.publisher',
+        'zope.schema>=3.5.1',
+        'zope.security',
+        'zope.traversing',
+        'zope.datetime',
+        ],
+      tests_require = [
+          'zope.configuration',
+          'zope.testing',
+          'zope.testrunner',
+          ],
+      test_suite = '__main__.alltests',
+      include_package_data = True,
+      zip_safe = False,
+      )
