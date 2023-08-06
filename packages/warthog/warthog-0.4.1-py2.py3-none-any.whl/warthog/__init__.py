df23c20c71b488c8902e025dd682423efeeee5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:01 +0000

    Add package warthog/0.4.1/warthog-0.4.1-py2.py3-none-any.whl

---
+++ packages/warthog/warthog-0.4.1-py2.py3-none-any.whl/warthog/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Warthog - Simple client for A10 load balancers
+#
+# Copyright 2014 Smarter Travel
+#
+# Available under the MIT license. See LICENSE for details.
+#
+
+"""
+warthog
+~~~~~~~
+
+"""
+
+__version__ = '0.4.1'
