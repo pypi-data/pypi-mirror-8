57bcf5416af0697ad2f798ff118f7c07b1f53d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:38 +0000

    Add package iniconf/0.1.6/iniconf-0.1.6.tar.gz

---
+++ packages/iniconf/iniconf-0.1.6.tar.gz/iniconf-0.1.6/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='iniconf',
+    version='0.1.6',
+    description='INI Config Creating/Parsing Tool',
+    long_description=open('README.md').read(),
+    url='https://github.com/brunogfranca/iniconf',
+    author='Bruno Franca',
+    author_email='bgfranca@gmail.com',
+    license=open('LICENSE.txt').read(),
+    packages=find_packages()
+)
