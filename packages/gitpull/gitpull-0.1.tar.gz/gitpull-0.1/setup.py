88c6ca66f29357af08ff30e8c89ef23d8bf21793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package gitpull/0.1/gitpull-0.1.tar.gz

---
+++ packages/gitpull/gitpull-0.1.tar.gz/gitpull-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    author = 'Adam Matan',
+    author_email = 'adam@matan.name',
+    description = 'A python script for batch-pull git repos',
+    license = 'MIT',
+    name = 'gitpull',
+    py_modules = ['gitpull'],
+    scripts = ['gitpull'],
+    url = 'https://github.com/adamatan/gitpull',
+    version = '0.1',
+    install_requires = ['termcolor', 'progressbar2', 'prettytable']
+)
