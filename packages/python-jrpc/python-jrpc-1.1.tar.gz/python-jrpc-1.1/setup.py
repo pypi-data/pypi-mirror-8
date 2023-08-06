4a302501cebcba3415af97bb22be4a8578e4b1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:44 +0000

    Add package python-jrpc/1.1/python-jrpc-1.1.tar.gz

---
+++ packages/python-jrpc/python-jrpc-1.1.tar.gz/python-jrpc-1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+  name = "python-jrpc",
+  version = "1.1",
+  packages = ["jrpc"],
+  author='Alex Sherman',
+  author_email='asherman1024@gmail.com',
+  url='https://github.com/alex-sherman/python-jrpc')
