b6f1e58589b27ed3dbb4876027604305ea0ff18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:19 +0000

    Add package ftpysetup/0.3/ftpysetup-0.3.tar.gz

---
+++ packages/ftpysetup/ftpysetup-0.3.tar.gz/ftpysetup-0.3/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+#----------------------------------------------------------------------------
+"""Flying Titans setuptools extensions."""
+__author__ = ('Lance Finn Helsten',)
+__version__ = '0.3'
+__copyright__ = """Copyright (C) 2014 Lance Finn Helsten"""
+__license__ = """
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import sys
+import os
+import setuptools
+setuptools.setup(
+    name = "ftpysetup",
+    version = __version__,
+    author = 'Lance Finn Helsten',
+    author_email = 'lanhel@flyingtitans.com',
+    description = __doc__,
+    long_description = open('README.rst').read(),
+    url = 'https://github.com/lanhel/ftpysetup',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Framework :: Setuptools Plugin',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    entry_points = {
+        "distutils.commands":[
+            "test = ftpysetup.runtest.runner:TestRunner",
+            "test_unit = ftpysetup.runtest.unit:UnitTestRunner",
+            "test_accept = ftpysetup.runtest.accept:AcceptTestRunner",
+            "venv = ftpysetup.venv:VirtualEnv",
+            "upvers = ftpysetup.upvers:UpdateVersion"
+        ],
+    },
+    packages = [
+        'ftpysetup.runtest',
+        'ftpysetup.venv',
+        'ftpysetup.upvers'
+    ],
+#    scripts = [
+#    ],
+)
+
+
+
+
