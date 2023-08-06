d5bb6dab13868798c40e40d28bab8a4db85e48fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:36 +0000

    Add package netsight.cloudstorage/1.7/netsight.cloudstorage-1.7.zip

---
+++ packages/netsight.cloudstorage/netsight.cloudstorage-1.7.zip/netsight.cloudstorage-1.7/CHANGES.rst
@@ -0,0 +1,121 @@
+Changelog
+=========
+
+1.7 (2014-12-09)
+----------------
+
+- Handling of content with multiple fields where at least one is below file
+  size threshold [benc]
+
+
+1.6.9 (2014-12-09)
+------------------
+
+- Added more verbose logging throughout [benc]
+
+1.6.8 (2014-12-09)
+------------------
+
+- Added more verbose error logging to callback task
+  [benc]
+- Added more logging to callback view
+  [benc]
+- Updated requests required version
+  [benc]
+
+
+1.6.7 (2014-12-08)
+------------------
+
+- Added more logging to upload_callback to aid debugging
+  [benc]
+
+
+1.6.6 (2014-11-27)
+------------------
+
+- Removed bucket creation in transcoding - no longer needed as not creating pipeline
+  [benc]
+- Fixed email notifications configuration
+  [benc]
+
+
+1.6.5 (2014-11-27)
+------------------
+
+- Removed pipeline creation
+  [benc]
+- Made pipeline name optional in control panel
+  [benc]
+
+
+1.6.1 (2014-11-21)
+------------------
+
+- Added workaround for "connection reset by peer"
+  [benc]
+
+
+1.6 (2014-11-17)
+----------------
+
+- Added abaility to disable email notifications
+  [benc]
+
+
+1.5 (2014-11-06)
+----------------
+
+- Added transcoding for video files
+  [benc]
+- Added customisable pipeline name
+  [benc]
+- Added fleshed out README
+  [mattss]
+- Added travis config
+  [mattss]
+
+
+1.4 (2014-10-23)
+----------------
+
+- AWS transcoding support!
+  [benc]
+- Improved support for virtual hosts
+  [benc, mattss]
+
+
+1.3 (2014-10-22)
+----------------
+
+- Half-baked release
+  [names removed to protect the innocent]
+
+
+1.2 (2014-09-26)
+----------------
+
+- General help text updates
+  [mattss]
+- Clear cloud storage setting when re-queued
+  [mattss]
+
+
+1.1 (2014-09-25)
+----------------
+
+- Switch to chunked uploads
+  [benc]
+- Fix bug with download patch
+  [mattss]
+- Add correct filename and mimetype to url generator
+  [mattss]
+- Add manual upload trigger view
+  [benc]
+
+
+1.0 (2014-09-23)
+----------------
+
+- Initial release
+  [benc]
---
+++ packages/netsight.cloudstorage/netsight.cloudstorage-1.7.zip/netsight.cloudstorage-1.7/src/netsight/cloudstorage/subscribers.py
@@ -0,0 +1,99 @@
+# -- The contents of this file is copyright (c) 2011 Netsight Internet     -- #
+# -- Solutions Ltd. All rights reserved. Please see COPYRIGHT.txt and      -- #
+# -- LICENCE.txt for further information.                                  -- #
+"""
+Event handlers
+"""
+import logging
+
+from plone import api
+from zope.component import queryAdapter
+from ZPublisher.HTTPRequest import FileUpload
+
+from .interfaces import ICloudStorage
+from .utils import get_value_from_registry
+
+logger = logging.getLogger('netsight.cloudstorage')
+
+
+def content_saved(object, event):
+    """
+    When an object is saved check its fields to see if any can be uploaded to
+    S3
+    """
+    adapter = queryAdapter(object, ICloudStorage)
+    if adapter is None:
+        return
+
+    if not hasattr(object.REQUEST, 'form'):
+        return
+
+    # look for file upload items on the request
+    for key, value in object.REQUEST.form.items():
+        if isinstance(value, FileUpload):
+            value.seek(0)
+            if len(value.read()):
+                logger.info(
+                    'Found file fields on %s. Enqueing upload',
+                    event.context.absolute_url()
+                )
+                adapter.enqueue()
+                return
+
+
+def mark_uploaded(event):
+    """
+    Event handler for completion of uploads
+
+    :param event: The event object
+    :type event: :class:`UploadComplete`
+    """
+    context = event.context
+    fieldname = event.fieldname
+    adapter = ICloudStorage(context)
+    logger.info('Celery says %s on %s has been uploaded',
+                fieldname,
+                context.absolute_url())
+    adapter.mark_as_cloud_available(fieldname)
+
+
+def email_creator(event):
+    """
+    Event handler to email content creator when upload completes
+
+    :param event: The event object
+    :type event: :class:`UploadComplete`
+    """
+    # Skip this if email notifications are disabled
+    if not get_value_from_registry('email_notifications'):
+        return
+    context = event.context
+    adapter = ICloudStorage(context)
+    # Only send email once all fields have been uploaded
+    if not adapter.has_uploaded_all_fields():
+        return
+
+    portal = api.portal.get()
+    creator = api.user.get(context.Creator())
+    creator_email = creator.getProperty('email')
+    subject = u'%s: Files for "%s" have been uploaded' % (
+        portal.Title().decode('utf8', 'ignore'),
+        context.Title().decode('utf8', 'ignore'),
+    )
+    body = """This is an automated email.
+
+    File data for the following item has been successfully
+    uploaded to secure cloud storage:
+
+    %s (%s)
+    %s
+    """ % (
+        context.Title(),
+        context.Type(),
+        context.absolute_url()
+    )
+    api.portal.send_email(
+        recipient=creator_email,
+        subject=subject,
+        body=body,
+    )
