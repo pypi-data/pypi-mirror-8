7ad58f5c39d96efec7f4953ac388e5712e5f76bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:56 +0000

    Add package infi.traceback/0.3.10.post4.gb48abbf/infi.traceback-0.3.10.post4.gb48abbf.tar.gz

---
+++ packages/infi.traceback/infi.traceback-0.3.10.post4.gb48abbf.tar.gz/infi.traceback-0.3.10.post4.gb48abbf/setup.py
@@ -0,0 +1,59 @@
+
+SETUP_INFO = dict(
+    name = 'infi.traceback',
+    version = '0.3.10.post4.gb48abbf',
+    author = 'Guy Rozendorn',
+    author_email = 'guy@rzn.co.il',
+
+    url = 'https://github.com/Infinidat/infi.traceback',
+    license = 'PSF',
+    description = """better tracebacks""",
+    long_description = """Tracebacks in Python are missing some useful debugging information, such is locals() in each stack frame
+This module provides several mechanisms for better tracebacks:
+* traceback_context. A context that in patches standard's library traceback module to print better tracebacks
+* traceback_decorator. A decorator that calls the decorated method inside the traceback_context
+* Nose Plugin. The plugin, enabled with '--with-infi-traceback', prints a better traceback for errors and failures
+* pretty_traceback_and_exit_decorator. A decorator for console script entry points that prints exceptions and raises SystemExit""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['infi.exceptools',
+'infi.pyutils',
+'mock',
+'nose',
+'setuptools'],
+    namespace_packages = ['infi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = [],
+        gui_scripts = [],
+        ),
+)
+
+if SETUP_INFO['url'] is None:
+    _ = SETUP_INFO.pop('url')
+
+SETUP_INFO['entry_points']['nose.plugins'] = ['infi.traceback = infi.traceback:NosePlugin']
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
---
+++ packages/infi.traceback/infi.traceback-0.3.10.post4.gb48abbf.tar.gz/infi.traceback-0.3.10.post4.gb48abbf/src/infi/traceback/tests.py
@@ -0,0 +1,59 @@
+import unittest
+from infi.traceback import pretty_traceback_and_exit_decorator
+from infi.traceback import set_truncation_limit
+
+@pretty_traceback_and_exit_decorator
+def main():
+    local_variable = None
+    raise NotImplementedError()
+
+@pretty_traceback_and_exit_decorator
+def system_exit():
+    raise SystemExit(3)
+
+class RaisingRepr(object):
+    def __repr__(self):
+        raise Exception()
+
+class TestCase(unittest.TestCase):
+    def test_safe_repr(self):
+        from infi.traceback import safe_repr
+        obj = RaisingRepr()
+        string = safe_repr(obj)
+
+    def test_main(self):
+        with self.assertRaises(SystemExit):
+            main()
+
+    def test_system_exit(self):
+        try:
+            system_exit()
+        except SystemExit, error:
+           self.assertEquals(error.args, (3, ))
+           
+    def test_truncated_repr(self):
+        from StringIO import StringIO
+        import sys
+        @pretty_traceback_and_exit_decorator
+        def func_with_variable():
+            long_variable = "a very long string that we will truncate to 10 chars"
+            raise Excetion()
+        old_stderr = sys.stderr
+        sys.stderr = StringIO()
+        set_truncation_limit(10)
+        try:
+            with self.assertRaises(SystemExit):
+                func_with_variable()
+            sys.stderr.seek(0)
+            output = sys.stderr.read()
+        finally:
+            sys.stderr = old_stderr
+        self.assertIn("long_variable", output)
+        self.assertIn("func_with_variable", output)
+        self.assertIn("repr truncated", output)
+        self.assertNotIn("10 chars", output)
+
+
+if __name__ == "__main__":
+    main()
+    
