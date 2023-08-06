ea5c4d6f86803fd130c1f3e2c6d4b2209009017b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:01 +0000

    Add package infi.pyvmomi_wrapper/0.1.6/infi.pyvmomi_wrapper-0.1.6.tar.gz

---
+++ packages/infi.pyvmomi_wrapper/infi.pyvmomi_wrapper-0.1.6.tar.gz/infi.pyvmomi_wrapper-0.1.6/src/infi/pyvmomi_wrapper/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.6"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '86a61aa0b0fbe4c85a6c419301a77ccab53ff13e'
+__git_head_subject__ = 'HIPVM-665 reset property collector when merge fails'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
