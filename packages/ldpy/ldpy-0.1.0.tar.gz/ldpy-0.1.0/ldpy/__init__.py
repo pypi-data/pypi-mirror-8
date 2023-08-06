027db02b59f21735404e3c9ef02f47a67b522ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:08 +0000

    Add package ldpy/0.1.0/ldpy-0.1.0.tar.gz

---
+++ packages/ldpy/ldpy-0.1.0.tar.gz/ldpy-0.1.0/ldpy/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf8 -*-
+
+#  Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__   = "0.1.0"
+__date__      = "2014/11/01"
+__license__   = "Apache Software License 2.0"
+__url__       = "http://github.com/wikier/ldpy"
+__contact__   = "sergio@wikier.org"
+__agent__     = "ldpy %s (http://github.com/wikier/ldpy)" % __version__
+__docformat__ = "restructuredtext en"
+
+import logging
+_LOGGER = logging.getLogger("ldpy")
+_LOGGER.info("LDPy Version: %s" % __version__)
+
+from client import Client
