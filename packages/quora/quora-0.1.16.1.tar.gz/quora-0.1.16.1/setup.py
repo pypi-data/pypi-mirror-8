8044c21ad6700476965884ac915a4a710eba7b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:16 +0000

    Add package quora/0.1.16.1/quora-0.1.16.1.tar.gz

---
+++ packages/quora/quora-0.1.16.1.tar.gz/quora-0.1.16.1/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+try:
+   import pypandoc
+   des = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   des = open('README.md').read()
+
+setup(
+    name='quora',
+    version='0.1.16.1',
+    description='Fetches and parses data from Quora.',
+    long_description=des,
+    author='Christopher Su',
+    author_email='chris+gh@christopher.su',
+    url='https://github.com/csu/pyquora',
+    packages=['quora'],
+    install_requires=[
+        "beautifulsoup4 == 4.3.2",
+        "feedparser == 5.1.3",
+        "requests==2.5.0"
+    ]
+)
