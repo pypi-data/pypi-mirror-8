3e18585af0fbf86d1f552b3b9b0dfaeaea9ed207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:55 +0000

    Add package mochi/0.0.2.3/mochi-0.0.2.3.tar.gz

---
+++ packages/mochi/mochi-0.0.2.3.tar.gz/mochi-0.0.2.3/mochi/__init__.py
@@ -0,0 +1,10 @@
+from mochi import mochi
+
+__author__ = 'Yasushi Itoh'
+__version__ = mochi.VERSION
+__license__ = 'MIT License'
+
+
+import sys
+
+IS_PYTHON_34 = sys.version_info.major == 3 and sys.version_info.minor == 4
