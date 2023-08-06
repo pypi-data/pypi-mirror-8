34549b84e4d59ac0f21f6529e737572ffedfe00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:25 +0000

    Add package requests-ntlm/0.1.0/requests_ntlm-0.1.0.tar.gz

---
+++ packages/requests-ntlm/requests_ntlm-0.1.0.tar.gz/requests_ntlm-0.1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='requests_ntlm',
+    version='0.1.0',
+    packages=[ 'requests_ntlm' ],
+    install_requires=[ 'requests>=1.0.0', 'python-ntlm3' ],
+    provides=[ 'requests_ntlm' ],
+    author='Ben Toews',
+    author_email='mastahyeti@gmail.com',
+    description='This package allows for HTTP NTLM authentication using the requests library.',
+    license='ISC',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: ISC License (ISCL)',
+    ],
+)
