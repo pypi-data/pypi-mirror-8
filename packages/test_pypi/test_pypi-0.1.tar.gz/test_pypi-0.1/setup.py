dc23573a74a4b9ac51c92dffb392caf1e6743e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:06 +0000

    Add package test_pypi/0.1/test_pypi-0.1.tar.gz

---
+++ packages/test_pypi/test_pypi-0.1.tar.gz/test_pypi-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="test_pypi",
+    version="0.1",
+    description="Test project to learn how pypi works",
+    url="",
+    author="Emiliano G. Molina",
+    author_email="emiliano.g.molina@gmail.com",
+    license="MIT",
+    packages=["test_pypi", ],
+    zip_safe=False)
