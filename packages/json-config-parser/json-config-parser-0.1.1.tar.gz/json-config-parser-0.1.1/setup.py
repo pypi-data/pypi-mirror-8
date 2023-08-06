47740fc04dbbcc78a7e5aca38ede16bec33f3fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:27 +0000

    Add package json-config-parser/0.1.1/json-config-parser-0.1.1.tar.gz

---
+++ packages/json-config-parser/json-config-parser-0.1.1.tar.gz/json-config-parser-0.1.1/setup.py
@@ -0,0 +1,32 @@
+"""
+JSON Config Parser
+==================
+
+A straightforward and unambiguous config file parser.
+Parses ini style files with json keys.
+"""
+from setuptools import setup
+
+setup(
+    name='json-config-parser',
+    version='0.1.1',
+    author='Ben Mather',
+    author_email='bwhmather@bwhmather.com',
+    description='A straightforward and unambiguous config file parser.',
+    long_description=__doc__,
+    url='https://github.com/bwhmather/json-config-parser/',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        ],
+    platforms='any',
+    py_packages=['jsonconfigparser', 'jsonconfigparser.tests'],
+    test_suite='jsonconfigparser.tests.suite',
+    )
