7f22a3a0ce3080635e07f975028109c40a568fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:11 +0000

    Add package looping_list/1.3.0/looping_list-1.3.0.tar.gz

---
+++ packages/looping_list/looping_list-1.3.0.tar.gz/looping_list-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'looping_list',
+	version = '1.3.0',
+	py_modules = ['listloop'],
+	author = 'Bommel',
+	author_email = 'wiesnert100@gmail.com',
+	url = 'https://pypi.python.org',
+	description = 'A simple print of nested lists',
+	)
