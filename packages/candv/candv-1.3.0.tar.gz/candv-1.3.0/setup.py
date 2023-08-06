3fcb08a9339a14cc700606174229809892f2aae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:37 +0000

    Add package candv/1.3.0/candv-1.3.0.tar.gz

---
+++ packages/candv/candv-1.3.0.tar.gz/candv-1.3.0/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+
+__here__ = os.path.abspath(os.path.dirname(__file__))
+
+README = open(os.path.join(__here__, 'README.rst')).read()
+REQUIREMENTS = [
+    i.strip() for i in
+    open(os.path.join(__here__, 'requirements.txt')).readlines()
+]
+
+setup(
+    name='candv',
+    version='1.3.0',
+    description="Constants and Values: create grouped non-standard named "
+                "constants, add values, verbose names, help texts or anything "
+                "you like to them",
+    long_description=README,
+    keywords=[
+        'constants', 'values', 'structures', 'choices',
+    ],
+    license='LGPLv3',
+    url='https://github.com/oblalex/candv',
+    author='Alexander Oblovatniy',
+    author_email='oblovatniy@gmail.com',
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+    ],
+    platforms=[
+        'any',
+    ],
+)
