e02873be9c71eb1f3303a5a3014a5ca163f14a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:55 +0000

    Add package nagplug/1.0.1/nagplug-1.0.1.tar.gz

---
+++ packages/nagplug/nagplug-1.0.1.tar.gz/nagplug-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "nagplug",
+    version = "1.0.1",
+    packages = find_packages(),
+    author = "Nicolas Limage",
+    description = "Nagios guidelines-compliant plugin creation library (Shinken, Icinga, Centreon)",
+    license = "MIT",
+    keywords = "nagios plugin shinken icinga centreon",
+    url = "https://github.com/nlm/nagplug",
+    test_suite = 'test_nagplug',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Monitoring',
+    ],
+)
