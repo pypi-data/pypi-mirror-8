2dd542259ed506496ad58492ed4efd1f09699695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:15 +0000

    Add package haven/1.1.62/haven-1.1.62.tar.gz

---
+++ packages/haven/haven-1.1.62.tar.gz/haven-1.1.62/haven/constants.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+LOGGER_NAME = 'haven'
+
+# 系统返回码
+RET_INVALID_CMD = -10000
+RET_INTERNAL = -10001
+
+# 默认host和port
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 7777
+SERVER_BACKLOG = 256
