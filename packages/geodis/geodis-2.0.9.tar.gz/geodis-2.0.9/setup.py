4acc416d3f043e49058185426124d949048d4361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:14 +0000

    Add package geodis/2.0.9/geodis-2.0.9.tar.gz

---
+++ packages/geodis/geodis-2.0.9.tar.gz/geodis-2.0.9/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='geodis',
+    description = 'A fast Redis based Geo resolving library',
+    version='2.0.9',
+    author='DoAT Media LTD.',
+    author_email='opensource@everything.me',
+    url='https://github.com/EverythingMe/geodis',
+    scripts=['geodis/geodis'],
+    packages=find_packages(),
+    include_package_data = True,
+    install_requires=['redis>=2.7.1', 'geohasher==0.1dev', 'upoints==0.12.2']
+
+)
+
+
