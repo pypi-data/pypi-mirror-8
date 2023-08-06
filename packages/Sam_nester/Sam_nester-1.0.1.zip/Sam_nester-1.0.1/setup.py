a53706832ee3a358d96e7c9e6fba7757b569993c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:50 +0000

    Add package Sam_nester/1.0.1/Sam_nester-1.0.1.zip

---
+++ packages/Sam_nester/Sam_nester-1.0.1.zip/Sam_nester-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name  = 'Sam_nester',
+    version  ='1.0.1',
+    py_modules  =['Sam_nester'],
+    author  ='Sam_python',
+    author_email  ='luqichuang@gmail.com',
+    url  ='',
+    description  ='',
+    )
