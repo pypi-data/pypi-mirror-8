cbe90e2f7eaf2978f204af2de9506b308214801d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:11 +0000

    Add package scrum-tools/0.1.1/scrum_tools-0.1.1-py2.7.egg

---
+++ packages/scrum-tools/scrum_tools-0.1.1-py2.7.egg/scrumtools/__init__.py
@@ -0,0 +1,7 @@
+from scrumtools import base, data, error, github, trello
+
+VERSION = (0, 0, 1)
+
+get_version = lambda: '.'.join(map(str, VERSION))
+
+__all__ = ['base', 'data', 'error', 'evaltool', 'github', 'trello']
---
+++ packages/scrum-tools/scrum_tools-0.1.1-py2.7.egg/scrumtools/base.py
@@ -0,0 +1,47 @@
+"""
+Copyright 2010-2014 DIMA Research Group, TU Berlin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Created on Apr 13, 2014
+"""
+
+from __future__ import absolute_import
+
+from cement.core import controller
+
+
+class BaseController(controller.CementBaseController):
+    class Meta:
+        label = 'base'
+        description = "A set of batch management tools for GitHub and Trello."
+
+        config_section = 'core'
+        config_defaults = dict(
+            # input format
+            users_file=None,
+            users_file_skip_first=False,
+            users_file_delimiter=';',
+            users_file_escape_char=None,
+            # input schema
+            users_schema='ID;Username;Group;Github;Trello',
+            users_schema_key_id='ID',
+            users_schema_key_username='Username',
+            users_schema_key_group='Group',
+            users_schema_key_github='Github',
+            users_schema_key_trello='Trello',
+        )
+
+    @controller.expose(hide=True)
+    def default(self):
+        self.app.args.parse_args(['--help'])
