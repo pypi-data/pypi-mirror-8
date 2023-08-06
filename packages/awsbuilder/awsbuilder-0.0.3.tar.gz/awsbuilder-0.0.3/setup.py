ae5bf3154c226c9726b28f8652c321f7dad60dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:50 +0000

    Add package awsbuilder/0.0.3/awsbuilder-0.0.3.tar.gz

---
+++ packages/awsbuilder/awsbuilder-0.0.3.tar.gz/awsbuilder-0.0.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="awsbuilder",
+
+    # Version number (initial):
+    version="0.0.3",
+
+    # Application author details:
+    author="Stuart Munro",
+    author_email="stuart.munro@digital.justice.gov.uk",
+
+    # Packages
+    packages=["awsbuilder"],
+
+    # Executables
+    scripts=['awsbuilder/awsbuilder'],
+
+    # Details
+    url="https://github.com/ministryofjustice/awsbuilder",
+
+    #
+    # license="LICENSE.txt",
+    description="Command line tools top create AWS objects based on a config file",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "boto==2.34.0",
+        "PyYAML==3.11"
+    ],
+
+    license = "MIT",
+
+    platforms = "Posix; MacOS X",
+
+    classifiers = ["Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Topic :: Internet"],
+)
