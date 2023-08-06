24b45b43cebfa023dc051377382d8c64f619b706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:30 +0000

    Add package demographica/0.2.0/demographica-0.2.0.tar.gz

---
+++ packages/demographica/demographica-0.2.0.tar.gz/demographica-0.2.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from setuptools import setup
+
+DESC = """A Python Package to infer ages using US Census Data"""
+
+setup(
+    name="demographica",
+    version='0.2.0',
+    description=DESC,
+    long_description=DESC,
+    author="Cameron Davidson-Pilon",
+    author_email="cam.davidson.pilon@gmail.com",
+    license="MIT",
+    packages=["demographica"],
+    url="https://github.com/CamDavidsonPilon/demographica",
+    install_requires=["numpy", "pandas>=0.14"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+    ],
+    package_data={
+        "demographica": [
+            "../README.md",
+            "../LICENSE",
+            "../datasets/*",
+        ]
+    },
+)
