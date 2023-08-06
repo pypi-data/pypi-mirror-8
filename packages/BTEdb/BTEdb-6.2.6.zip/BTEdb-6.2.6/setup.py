220fc2eb8dc325385517a2d78e972d536ba9e34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:03 +0000

    Add package BTEdb/6.2.6/BTEdb-6.2.6.zip

---
+++ packages/BTEdb/BTEdb-6.2.6.zip/BTEdb-6.2.6/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='BTEdb',
+      version='6.2.6',
+	  description="Python schemaless JSON/YAML database interface",
+	  author="Niles Rogoff",
+	  author_email="nilesrogoff@gmail.com",
+	  url="http://github.com/nilesr/BTEdb",
+      py_modules=['BTEdb'],
+	  install_requires = ["dill"],
+      )
