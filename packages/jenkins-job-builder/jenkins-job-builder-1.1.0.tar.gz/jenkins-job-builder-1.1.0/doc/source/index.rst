c610c58766d814cc63332592d5ecf9585f269724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:18 +0000

    Add package jenkins-job-builder/1.1.0/jenkins-job-builder-1.1.0.tar.gz

---
+++ packages/jenkins-job-builder/jenkins-job-builder-1.1.0.tar.gz/jenkins-job-builder-1.1.0/doc/source/index.rst
@@ -0,0 +1,29 @@
+Jenkins Job Builder
+===================
+
+Jenkins Job Builder takes simple descriptions of Jenkins_ jobs in
+YAML_ format, and uses them to configure Jenkins.  You can keep your
+job descriptions in human readable text format in a version control
+system to make changes and auditing easier.  It also has a flexible
+template system, so creating many similarly configured jobs is easy.
+
+
+.. _Jenkins: http://jenkins-ci.org/
+.. _YAML: http://www.yaml.org/
+
+Contents:
+
+.. toctree::
+   :maxdepth: 3
+
+   installation
+   definition
+   extending
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ packages/jenkins-job-builder/jenkins-job-builder-1.1.0.tar.gz/jenkins-job-builder-1.1.0/tests/cmd/test_cmd.py
@@ -0,0 +1,40 @@
+import os
+from six.moves import configparser, StringIO
+import mock
+import testtools
+from jenkins_jobs import cmd
+
+
+class CmdTestsBase(testtools.TestCase):
+
+    fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+    parser = cmd.create_parser()
+
+    def setUp(self):
+        super(CmdTestsBase, self).setUp()
+
+        # Testing the cmd module can sometimes result in the CacheStorage class
+        # attempting to create the cache directory multiple times as the tests
+        # are run in parallel.  Stub out the CacheStorage to ensure that each
+        # test can safely create the cache directory without risk of
+        # interference.
+        self.cache_patch = mock.patch('jenkins_jobs.builder.CacheStorage',
+                                      autospec=True)
+        self.cache_patch.start()
+
+        self.config = configparser.ConfigParser()
+        self.config.readfp(StringIO(cmd.DEFAULT_CONF))
+
+    def tearDown(self):
+        self.cache_patch.stop()
+        super(CmdTestsBase, self).tearDown()
+
+
+class CmdTests(CmdTestsBase):
+
+    def test_with_empty_args(self):
+        """
+        User passes no args, should fail with SystemExit
+        """
+        with mock.patch('sys.stderr'):
+            self.assertRaises(SystemExit, cmd.main, [])
