948f31b18493f258aa43f6fd1a8ed0fdf1a46f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:36 +0000

    Add package feedme/1.5.2/feedme-1.5.2.tar.gz

---
+++ packages/feedme/feedme-1.5.2.tar.gz/feedme-1.5.2/README.md
@@ -0,0 +1,38 @@
+FeedMe
+===========  
+
+A food ordering management system for Django. 
+
+This project was started to help 'dotKom' with ordering food for their work nights. 
+
+### Install via pip
+`pip install feedme`
+
+### Manual install:
+
+0. Download any of the releases.
+1. Add "feedme" to INSTALLED_APPS  
+ - `INSTALLED_APPS = (…, 'feedme', …)  
+2. Add the following to settings:  
+ - `FEEDME_GROUP = 'feedme_users_group'`  
+ - `FEEDME_ADMIN_GROUP = 'feedme_admins_group'`  
+ - Add the groups in admin if neccessary.  
+3. Include feedme.urls into system urls.py
+ - `url(r'^feedme/', include('feedme.urls'))`  
+
+---
+## Standalone setup
+
+This app supports a standalone setup, rather than integrating it into a current project.
+
+All you need is to create a `base.html` with these blocks
+
+- title
+- styles
+- submenu (nav)
+- content
+
+And use whatever template you want.
+The submenu included in feedme uses a bootstrap submenu, and some bootstrap elements and classes.
+
+Remember to set and serve static files.
---
+++ packages/feedme/feedme-1.5.2.tar.gz/feedme-1.5.2/feedme/forms.py
@@ -0,0 +1,72 @@
+from django.forms import ModelForm
+from feedme.models import Order, OrderLine, ManageOrderLimit, ManageOrders, Restaurant, ManageBalance, Poll, Answer
+
+
+class OrderLineForm(ModelForm):
+    name = 'orderline'
+
+    class Meta:
+        model = OrderLine
+        exclude = ('order', 'creator', 'paid_for')
+
+    def __init__(self, *args, **kwargs):
+        super(OrderLineForm, self).__init__(*args, **kwargs)
+        self.fields['users'].empty_label = None
+
+
+class OrderForm(ModelForm):
+    name = u'order'
+
+    class Meta:
+        model = Order
+        fields = ('date', )
+
+
+class ManageOrderForm(ModelForm):
+    name = u'orders'
+
+    class Meta:
+        model = ManageOrders
+
+
+class ManageBalanceForm(ModelForm):
+    name = u'transactions'
+
+    class Meta:
+        model = ManageBalance
+
+
+class ManageOrderLimitForm(ModelForm):
+    name = u'order limit'
+
+    class Meta:
+        model = ManageOrderLimit
+
+
+class NewOrderForm(ModelForm):
+    name = u'new order'
+
+    class Meta:
+        model = Order
+
+
+class NewRestaurantForm(ModelForm):
+    name = u'new restaurant'
+
+    class Meta:
+        model = Restaurant
+
+
+class NewPollForm(ModelForm):
+    name = 'new poll'
+
+    class Meta:
+        model = Poll
+
+
+class PollAnswerForm(ModelForm):
+    name = 'answer'
+
+    class Meta:
+        model = Answer
+        exclude = ('user', 'poll')
