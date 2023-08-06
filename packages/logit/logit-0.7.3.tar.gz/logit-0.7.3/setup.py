6e61d26bd1f4a8a1dc84fa3e50faafce1f7381ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:14 +0000

    Add package logit/0.7.3/logit-0.7.3.tar.gz

---
+++ packages/logit/logit-0.7.3.tar.gz/logit-0.7.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='logit',
+    version='0.7.3',
+    description='A private log for humans',
+    author='Will Bradley',
+    author_email='williambbradley@gmail.com',
+    license='CC0',
+    url='https://github.com/wbbradley/logit',
+    packages=['logit'],
+    install_requires=['pycrypto', 'boto', 'PyYAML',
+                      'attrdict'],
+    entry_points={'console_scripts': [
+        'logit = logit.main:main',
+    ]},
+)
