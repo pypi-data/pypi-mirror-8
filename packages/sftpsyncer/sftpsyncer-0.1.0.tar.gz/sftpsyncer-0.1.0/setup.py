99e5ece78798a3d35179992419e6009e1b41d506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package sftpsyncer/0.1.0/sftpsyncer-0.1.0.tar.gz

---
+++ packages/sftpsyncer/sftpsyncer-0.1.0.tar.gz/sftpsyncer-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='sftpsyncer',
+    version='0.1.0',
+    author='Chris Horsley',
+    author_email='chris.horsley@csirtfoundry.com',
+    packages=['sftpsyncer'],
+    scripts=[],
+    url='http://bitbucket.org/dsms/sftpsyncher/',
+    license='LICENSE.txt',
+    description='Synchronise files between a local cache and an SFTP server',
+    long_description=open('README').read(),
+    install_requires=[
+        "paramiko"
+    ],
+)
