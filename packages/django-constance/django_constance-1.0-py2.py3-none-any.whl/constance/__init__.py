fbfe0184a715aab238a748226907f17f37530ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:30 +0000

    Add package django-constance/1.0/django_constance-1.0-py2.py3-none-any.whl

---
+++ packages/django-constance/django_constance-1.0-py2.py3-none-any.whl/constance/__init__.py
@@ -0,0 +1,12 @@
+from .base import Config
+from django.utils.functional import SimpleLazyObject
+
+__version__ = '1.0'
+
+
+try:
+    from django.apps import AppConfig  # noqa
+except ImportError:
+    config = SimpleLazyObject(Config)
+else:
+    default_app_config = 'constance.apps.ConstanceConfig'
