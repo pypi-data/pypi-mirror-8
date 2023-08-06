491ac05ce944837b0a47356dd41c8fb8cf528971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:43 +0000

    Add package django-datebook/0.8/django-datebook-0.8.tar.gz

---
+++ packages/django-datebook/django-datebook-0.8.tar.gz/django-datebook-0.8/datebook/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Model admin
+"""
+from django.contrib import admin
+from models import *
+
+admin.site.register(Datebook)
+admin.site.register(DayEntry)
+admin.site.register(DayModel)
