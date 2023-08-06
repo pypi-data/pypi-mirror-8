79bd9c2630853d01b644444e550132fd78be2f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package openlabs_payment_gateway_authorize_net/3.2.1.1/openlabs_payment_gateway_authorize_net-3.2.1.1.tar.gz

---
+++ packages/openlabs_payment_gateway_authorize_net/openlabs_payment_gateway_authorize_net-3.2.1.1.tar.gz/openlabs_payment_gateway_authorize_net-3.2.1.1/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+
+    Payment Gateway
+
+    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Ltd.
+    :license: BSD, see LICENSE for more details
+
+'''
+from trytond.pool import Pool
+from .transaction import PaymentGatewayAuthorize, \
+    AddPaymentProfileView, AddPaymentProfile, AuthorizeNetTransaction, \
+    Party, Address
+
+
+def register():
+    Pool.register(
+        PaymentGatewayAuthorize,
+        AddPaymentProfileView,
+        AuthorizeNetTransaction,
+        Party,
+        Address,
+        module='payment_gateway_authorize_net', type_='model'
+    )
+    Pool.register(
+        AddPaymentProfile,
+        module='payment_gateway_authorize_net', type_='wizard'
+    )
