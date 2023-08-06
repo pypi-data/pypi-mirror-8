a4087bbcae5d5536e79fb662dc98b396503f9931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:57 +0000

    Add package tarbell/1.0/tarbell-1.0.tar.gz

---
+++ packages/tarbell/tarbell-1.0.tar.gz/tarbell-1.0/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+
+APP_NAME = 'tarbell'
+VERSION = '1.0'
+
+settings = dict()
+
+# Publish Helper.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+settings.update(
+    name=APP_NAME,
+    version=VERSION,
+    author=u'Tarbell Project',
+    author_email='davideads@gmail.com',
+    url='http://github.com/tarbell-project/tarbell',
+    license='MIT',
+    description='A very simple content management system',
+    long_description="""Read the docs at http://tarbell.readthedocs.org
+
+Tarbell makes it simple to put your work on the web, whether you’re a team of one or a dozen. With Tarbell, you can collaboratively build beautiful websites and publish them with ease.
+
+Tarbell makes use of familiar, flexible tools to take the magic (and frustration) out of publishing to the web. Google spreadsheets handle content management, so changes to your stories are easy to make without touching a line of code. Step-by-step prompts help you set up and configure your project, so that publishing it is a breeze.""",
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Flask==0.10.1",
+        "Jinja2==2.7.3",
+        "Markdown==2.4.1",
+        "MarkupSafe==0.23",
+        "PyYAML==3.11",
+        "boto==2.34.0",
+        "clint==0.4.1",
+        "gnureadline==6.3.3",
+        "google-api-python-client==1.3.1",
+        "keyring==4.0",
+        "python-dateutil>=2.2",
+        "requests==2.3.0",
+        "sh==1.09",
+        "wsgiref==0.1.2",
+        "xlrd==0.9.3",
+    ],
+    entry_points={
+        'console_scripts': [
+            'tarbell = tarbell.cli:main',
+        ],
+    },
+    keywords=['Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Internet',
+          ],
+)
+
+
+setup(**settings)
---
+++ packages/tarbell/tarbell-1.0.tar.gz/tarbell-1.0/tarbell/utils.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+tarbell.utils
+~~~~~~~~~
+
+This module provides utilities for Tarbell.
+"""
+
+import os
+import sys
+from clint.textui import colored, puts as _puts
+
+
+def is_werkzeug_process():
+    """Is the current process the werkzeug reloader thread? Return True if so."""
+    return os.environ.get('WERKZEUG_RUN_MAIN') == 'true'
+
+
+def puts(*args, **kwargs):
+    """Wrap puts to avoid getting called twice by Werkzeug reloader"""
+    if not is_werkzeug_process():
+        return _puts(*args, **kwargs)
+
+
+def list_get(l, idx, default=None):
+    """Get from a list with an optional default value."""
+    try:
+        if l[idx]:
+            return l[idx]
+        else:
+            return default
+    except IndexError:
+        return default
+
+
+def split_sentences(s, pad=0):
+    """Split sentences for formatting."""
+    sentences = []
+    for index, sentence in enumerate(s.split('. ')):
+        padding = ''
+        if index > 0:
+            padding = ' ' * (pad + 1)
+        if sentence.endswith('.'):
+            sentence = sentence[:-1]
+        sentences.append('%s %s.' % (padding, sentence.strip()))
+    return "\n".join(sentences)
+
+
+def show_error(msg):
+    """Displays error message."""
+    sys.stdout.flush()
+    sys.stderr.write("\n{0}: {1}".format(colored.red("Error"), msg + '\n'))
