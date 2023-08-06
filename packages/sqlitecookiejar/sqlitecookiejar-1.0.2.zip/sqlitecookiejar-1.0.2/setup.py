34ba56d5dc1d23b329a23e9747f07e9b27b3cf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:39 +0000

    Add package sqlitecookiejar/1.0/sqlitecookiejar-1.0.2.zip

---
+++ packages/sqlitecookiejar/sqlitecookiejar-1.0.2.zip/sqlitecookiejar-1.0.2/setup.py
@@ -0,0 +1,24 @@
+__author__ = 'dgarnier@timsoft.com'
+
+from setuptools import setup
+
+setup(
+    name="sqlitecookiejar",
+    description='FileCookieJar using SQLite files for persistence',
+    py_modules=['sqlitecookiejar'],
+    version='1.0.2',
+    long_description=__doc__,
+    zip_safe=False,
+    author_email='dgarnier@timsoft.com',
+    url='https://github.com/timsoft-oss/sqlitecookiejar',
+    license='Apache 2.0',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Session'
+    )
+)
