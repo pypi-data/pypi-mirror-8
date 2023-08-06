38ca69839c86b6d162777dd3165019c629349d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:50 +0000

    Add package pytidyclub/0.0.1/pytidyclub-0.0.1.tar.gz

---
+++ packages/pytidyclub/pytidyclub-0.0.1.tar.gz/pytidyclub-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pytidyclub',
+      version='0.0.1',
+      description='A simple Python wrapper for the TidyClub API',
+      url='http://github.com/kyerussell/pytidyclub',
+      author='Kye Russell',
+      author_email='me@kye.id.au',
+      license='MIT',
+      packages=['pytidyclub'],
+      zip_safe=False)
