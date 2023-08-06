0e0736677a8aa836b7b7a3fff485ea8c565bffa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:51 +0000

    Add package zope.error/4.2.0/zope.error-4.2.0.tar.gz

---
+++ packages/zope.error/zope.error-4.2.0.tar.gz/zope.error-4.2.0/CHANGES.rst
@@ -0,0 +1,109 @@
+=======
+CHANGES
+=======
+
+4.2.0 (2014-12-27)
+------------------
+
+- Add support for PyPy and PyPy3.
+
+- Add support for Python 3.4.
+
+
+4.1.1 (2014-12-22)
+------------------
+
+- Enable testing on Travis.
+
+
+4.1.0 (2013-02-21)
+------------------
+
+- Add compatibility with Python 3.3
+
+
+4.0.0 (2012-12-10)
+------------------
+
+- Replace deprecated ``zope.interface.implements`` usage with equivalent
+  ``zope.interface.implementer`` decorator.
+
+- Drop support for Python 2.4 and 2.5.
+
+- Sort request items for presentation in the error reporting utility.
+
+- Don't HTML-escape HTML tracebacks twice.
+
+
+3.7.4 (2012-02-01)
+------------------
+
+- Add explicit tests for escaping introduced in 3.7.3.
+
+- Handing names of classes those string representation cannot
+  be determined as untrusted input thus escaping them in error reports.
+
+- Fix tests on Python 2.4 and 2.5.
+
+3.7.3 (2012-01-17)
+------------------
+
+- Escape untrusted input before constructing HTML for error reporting.
+
+3.7.2 (2010-10-30)
+------------------
+
+- Set ``copy_to_zlog`` by default to 1/True.
+  Having it turned off is a small problem, because fatal (startup) errors
+  will not get logged anywhere.
+
+
+3.7.1 (2010-09-25)
+------------------
+
+- Add test extra to declare test dependency on ``zope.testing``.
+
+
+3.7.0 (2009-09-29)
+------------------
+
+- Clean up dependencies. Droped all testing dependencies as we only need
+  zope.testing now.
+
+- Fix ImportError when zope.testing is not available for some reason.
+
+- Remove zcml slug and old zpkg-related files.
+
+- Remove word "version" from changelog entries.
+
+- Change package's mailing list address to zope-dev at zope.org as
+  zope3-dev at zope.org is now retired. Also changed `cheeseshop` to
+  `pypi` in the package's homepage url.
+
+- Add dependency on ZODB3 as we use Persistent.
+
+- Use a mock request for testing. Dropped the dependency on zope.publisher
+  which was really only a testing dependency.
+
+- Reduce the dependency on zope.container to one on zope.location by no
+  longer using the Contained mix-in class.
+
+3.6.0 (2009-01-31)
+------------------
+
+- Use zope.container instead of zope.app.container
+
+- Move error log bootstrapping logic (which was untested) to
+  ``zope.app.appsetup``, to which we added a test.
+
+3.5.1 (2007-09-27)
+------------------
+
+- Rebump to replace faulty egg
+
+3.5.0
+-----
+
+- Initial documented release
+
+- Moved core components from ``zope.app.error`` to this package.
---
+++ packages/zope.error/zope.error-4.2.0.tar.gz/zope.error-4.2.0/setup.py
@@ -0,0 +1,78 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.error package
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+setup(name='zope.error',
+    version='4.2.0',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    description = "An error reporting utility for Zope3",
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('CHANGES.rst')
+        ),
+    license='ZPL 2.1',
+    keywords = "zope3 error",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://pypi.python.org/pypi/zope.error',
+	packages=find_packages('src'),
+	package_dir = {'': 'src'},
+    namespace_packages=['zope',],
+    install_requires=['setuptools',
+                      'zope.exceptions',
+                      'zope.interface',
+                      'zope.location',
+                      'persistent', # error.py imports from persistent
+                      'six'
+                      ],
+    extras_require=dict(
+          test=[
+              'zope.testing >= 3.8',
+              ]),
+    test_suite = 'zope.error.tests',
+    include_package_data = True,
+    zip_safe = False,
+    )
