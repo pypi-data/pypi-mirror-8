61d3fff351c43524b99e5faefed60ff6c101e039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:11 +0000

    Add package sightcall_auth/0.0.2/sightcall_auth-0.0.2.tar.gz

---
+++ packages/sightcall_auth/sightcall_auth-0.0.2.tar.gz/sightcall_auth-0.0.2/sightcall_auth/__init__.py
@@ -0,0 +1,9 @@
+"""
+A lib to connect to SightCall authentification service.
+"""
+
+from sightcall_auth.request import Auth
+from sightcall_auth.parse_p12 import parse_p12
+
+
+__version__ = "0.0.2"
