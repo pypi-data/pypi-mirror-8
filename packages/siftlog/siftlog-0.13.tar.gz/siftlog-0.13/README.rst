572ad5768bea04dff9b589bda6862e7771365cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:36 +0000

    Add package siftlog/0.13/siftlog-0.13.tar.gz

---
+++ packages/siftlog/siftlog-0.13.tar.gz/siftlog-0.13/README.rst
@@ -0,0 +1,132 @@
+Sift Log - JSON logging adapter for Python
+==========================================
+
+Features
+--------
+
+-  Tag log statements with arbitrary values for easier grouping and
+   analysis
+-  Add keyword arguments that are converted to JSON values
+-  Variable substitution
+-  Specifies where log calls are made from
+-  Meant to be used with core Python logging (formatters, handlers, etc)
+-  ``TRACE`` log level built-in
+
+Examples
+--------
+
+A simple log message
+^^^^^^^^^^^^^^^^^^^^
+
+.. code:: python
+
+    log.info('Hello')
+
+``{"msg": "Hello", "time": "12-12-14 10:12:01 EST", "level": "INFO", "loc": "test:log_test:20"}``
+
+Logging with tags
+^^^^^^^^^^^^^^^^^
+
+.. code:: python
+
+    log.debug('Creating new user', 'MONGO', 'STORAGE')
+
+``{"msg": "Creating new user", "time": "12-12-14 10:12:09 EST", "tags": ["MONGO", "STORAGE"], "level": "DEBUG", "loc": "test:log_test:20"}``
+
+Adding JSON keys
+^^^^^^^^^^^^^^^^
+
+.. code:: python
+
+    log.debug('Some key', is_admin=True, username='papito')
+
+``{"msg": "Some key", "is_admin": true, "username": "papito", "time": "12-12-14 10:12:04 EST", "level": "DEBUG", "loc": "test:log_test:20"}``
+
+String substitution
+^^^^^^^^^^^^^^^^^^^
+
+.. code:: python
+
+    log.debug('User "$username" admin? $is_admin', is_admin=False, username='fez')
+
+``{"msg": "User \"fez\" admin? False",  "username": "fez", "is_admin": false, "time": "12-12-14 10:12:18 EST", "level": "DEBUG", "loc": "test:log_test:20"}``
+
+Setup
+-----
+
+Logging to console
+^^^^^^^^^^^^^^^^^^
+
+.. code:: python
+
+    import sys
+    import logging
+    from siftlog import SiftLog
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(handler)
+
+    log = SiftLog(logger)
+
+In this fashion, you can direct the JSON logs to `any logging
+handler <https://docs.python.org/2/library/logging.handlers.html>`__
+
+Constants (re-occuring values)
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+You can define constants that will appear in every single log message.
+This is useful, for example, if you'd like to log process PID and
+hostname with every log message (recommended). This is done upon log
+adapter initialization:
+
+.. code:: python
+
+    import os
+    from siftlog import SiftLog
+    log = SiftLog(logger, pid=os.getpid(), env='INTEGRATION')
+
+``{"msg": "And here I am", "time": "12-12-14 11:12:24 EST", "pid": 37463, "env": "INTEGRATION", "level": "INFO"}``
+
+Custom time format
+^^^^^^^^^^^^^^^^^^
+
+.. code:: python
+
+    log = SiftLog(logger)
+    log.TIME_FORMAT = '%d-%m-%y %H:%m:%S %Z'
+
+Define the format as accepted by
+`time.strftime() <https://docs.python.org/2/library/time.html#time.strftime>`__
+
+Custom location format
+^^^^^^^^^^^^^^^^^^^^^^
+
+.. code:: python
+
+    log = SiftLog(logger)
+    log.LOCATION_FORMAT = '$module:$method:$line_no'
+
+The format should be a string containing any of the following variables:
+
+-  **$file**
+-  **$line\_no**
+-  **$method**
+-  **$module**
+
+Custom core key names
+^^^^^^^^^^^^^^^^^^^^^
+
+Core keys, such as ``msg`` and ``level`` can be overridden, if they
+clash with common keys you might be using.
+
+The following can be redefined:
+
+-  **MESSAGE** (default ``msg``)
+-  **LEVEL** (default ``level``)
+-  **LOCATION** (default ``loc``)
+-  **TAGS** (default ``tags``)
+-  **TIME** (default ``time``)
+
+As in: ``python log = SiftLog(logger) log.MESSAGE = 'MESSAGE'``
---
+++ packages/siftlog/siftlog-0.13.tar.gz/siftlog-0.13/siftlog/__init__.py
@@ -0,0 +1,145 @@
+import os
+import logging
+import json
+import inspect
+import time
+import itertools
+from string import Template
+
+logging.TRACE = 5
+logging.addLevelName(logging.TRACE, 'TRACE')
+
+class SiftLog(logging.LoggerAdapter):
+
+    def __init__(self, logger, **kwargs):
+        super(SiftLog, self).__init__(logger, {})
+        self.MESSAGE     = 'msg'
+        self.LEVEL       = 'level'
+        self.LOCATION    = 'loc'
+        self.TAGS        = 'tags'
+        self.TIME        = 'time'
+        self.TIME_FORMAT = '%d-%m-%y %H:%m:%S %Z'
+        self.LOCATION_FORMAT = '$module:$method:$line_no'
+
+        self._constants   = kwargs
+
+    def _get_log_stmt(self, level, msg, *tags, **kwargs):
+        msg = msg or ''
+
+        kwargs[self.LEVEL] = logging.getLevelName(level)
+
+        # append the optional constants defined on initialization
+        kwargs.update(self._constants)
+
+        # add message to the payload, substitute with the passed data
+        kwargs[self.MESSAGE] = Template(msg).safe_substitute(kwargs)
+
+        # caller location
+        loc = self.get_caller_info()
+        if loc:
+            kwargs[self.LOCATION] = loc
+
+        kwargs[self.TIME] = self.get_timestamp()
+
+        if tags:
+            kwargs[self.TAGS] = tags
+
+        try:
+            payload = self.to_json(kwargs)
+        except (Exception) as ex:
+            msg = 'LOGGER EXCEPTION "{0}" in  {1}'.format(str(ex), loc)
+            return json.dumps({
+                'msg': msg,
+                'level': logging.getLevelName(logging.ERROR)
+            })
+
+        return payload
+
+    def to_json(self, data):
+        return json.dumps(data)
+
+    def get_timestamp(self):
+        return time.strftime(self.TIME_FORMAT)
+
+    def get_caller_info(self):
+        caller = self._get_caller_info()
+
+        if not caller:
+            return None
+
+        return Template(self.LOCATION_FORMAT).safe_substitute(caller)
+
+    def _get_caller_info(self):
+        # pull the frames from the current stack, reversed,
+        # since it's easier to find the first siftlog frame
+        frames = [
+            (idx, frm, inspect.getmodule(frm[0])) 
+            for idx, frm 
+            in enumerate(reversed(inspect.stack()))
+        ]
+
+        # travel the stack from behind, looking for the first siftlog frame
+        res = itertools.dropwhile(lambda (idx, f, m): m and m.__name__ != 'siftlog' , frames)
+        # the first siftlog frame from back of the stack
+        siftlog_frame = res.next()
+        # its index
+        siftlog_frame_idx = siftlog_frame[0]
+
+        if siftlog_frame_idx == 0: # there is no caller module (console)
+            return None
+
+        # the frame before this one is what's calling the logger
+        frm = frames[siftlog_frame_idx - 1][1]
+
+        # now get the caller info
+        mod = inspect.getmodule(frm[0])
+        
+        return {
+            'file': frm[1],
+            'line_no' : frm[2],
+            'method'  : frm[3],
+            'module'  : mod.__name__,
+        }
+
+    def trace(self, msg, *args, **kwargs):
+        if not self.logger.isEnabledFor(logging.TRACE):
+            return 
+
+        self.log(logging.TRACE, msg,  *args, **kwargs)
+
+    def debug(self, msg, *args, **kwargs):
+        if not self.logger.isEnabledFor(logging.DEBUG):
+            return 
+
+        self.log(logging.DEBUG, msg,  *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        if not self.logger.isEnabledFor(logging.INFO):
+            return 
+
+        self.log(logging.INFO, msg,  *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        if not self.logger.isEnabledFor(logging.WARNING):
+            return 
+
+        self.log(logging.WARNING, msg,  *args, **kwargs)
+
+    def warn(self, msg, *args, **kwargs):
+        self.warning(msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        if not self.logger.isEnabledFor(logging.ERROR):
+            return 
+
+        self.log(logging.ERROR, msg,  *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        if not self.logger.isEnabledFor(logging.CRITICAL):
+            return 
+
+        self.log(logging.CRITICAL, msg,  *args, **kwargs)
+
+    def log(self, level, msg, *args, **kwargs):
+        payload = self._get_log_stmt(level, msg, *args, **kwargs)
+        self.logger.log(level, payload)
