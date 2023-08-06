4c069c96b571f63dd810e912e168a32ea6e3abd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:45 +0000

    Add package python-zenity/0.1/python-zenity-0.1.tar.gz

---
+++ packages/python-zenity/python-zenity-0.1.tar.gz/python-zenity-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='python-zenity',
+    version='0.1',
+    description='Simple dialog with Python and Gtk',
+    url='http://github.com/poulp/python-zenity',
+    author='poulp',
+    author_email='mathieu.nerv@gmail.com',
+    license='MIT',
+    packages=['pythonzenity'],
+    zip_safe=False
+)
+
