f8b7fbcf475607e33c8364dfbdf3e3b53dfce3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:08 +0000

    Add package qjson/0.1.6/qjson-0.1.6.tar.gz

---
+++ packages/qjson/qjson-0.1.6.tar.gz/qjson-0.1.6/qjson/qjson.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+class Payload(object):
+    def __init__(self, str_or_dict):
+        
+        if type(str_or_dict) == type({}):
+            self.__dict__ = str_or_dict
+        else:
+            self.__dict__ = json.loads(str_or_dict)
+
+        #recursive convert dict to python object
+        for k in self.__dict__:
+            if type(self.__dict__[k]) == type({}):
+                self.__dict__[k] = Payload(self.__dict__[k])
+
+
+def loads(s):
+    return Payload(s)
+
+if __name__ == '__main__':
+    print "qjson - quick and dirty way to convert json string to python object"
