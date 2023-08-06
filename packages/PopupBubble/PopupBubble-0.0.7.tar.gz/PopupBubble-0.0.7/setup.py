b19eec7371ba6d78a0e758ee4041cb1fc3780d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:42 +0000

    Add package PopupBubble/0.0.7/PopupBubble-0.0.7.tar.gz

---
+++ packages/PopupBubble/PopupBubble-0.0.7.tar.gz/PopupBubble-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+import platform, os
+sysname = platform.system()
+if sysname == "Windows":
+    binpath = os.getenv("SystemRoot")
+else:
+    binpath = r"/usr/local/bin"
+
+setup(
+    name = "PopupBubble",
+    packages = ["PopupBubble"],
+    version = "0.0.7",
+    data_files = [(binpath, ["scripts/pop_bubble.py", "scripts/pop_tk.py"])],
+    description = "Cross-platform desktop notification using Qt",
+    author = "Kaiyin Zhong",
+    author_email = "kindlychung@gmail.com",
+    url = "https://github.com/kindlychung/PopupBubble",
+    keywords = ["notification", "cross-platform"]
+    )
+
