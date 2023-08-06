42784c627221892922cd63b4bb5df46a0f8f6538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:56 +0000

    Add package django-request-profiler/0.6/django_request_profiler-0.6-py2-none-any.whl

---
+++ packages/django-request-profiler/django_request_profiler-0.6-py2-none-any.whl/request_profiler/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# models definitions for request_profiler
+from django.conf import settings
+
+# cache key used to store enabled rulesets.
+RULESET_CACHE_KEY = getattr(settings, 'REQUEST_PROFILER_RULESET_CACHE_KEY', "request_profiler__rulesets")  # noqa
+# how long to cache them for - defaults to 10s
+RULESET_CACHE_TIMEOUT = getattr(settings, 'REQUEST_PROFILER_RULESET_CACHE_TIMEOUT', 10)  # noqa
+
+# This is a function that can be used to override all rules to exclude requests from profiling
+# e.g. you can use this to ignore staff, or search engine bots, etc.
+GLOBAL_EXCLUDE_FUNC = getattr(settings, 'REQUEST_PROFILER_GLOBAL_EXCLUDE_FUNC', lambda r: True)
