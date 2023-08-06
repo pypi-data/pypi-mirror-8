3a6c7d4b77c9abe3438a327edf4ab0d2927e9f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:30 +0000

    Add package docker-registry-driver-azureblob/1.1.2/docker-registry-driver-azureblob-1.1.2.tar.gz

---
+++ packages/docker-registry-driver-azureblob/docker-registry-driver-azureblob-1.1.2.tar.gz/docker-registry-driver-azureblob-1.1.2/README.md
@@ -0,0 +1,34 @@
+docker-registry-driver-azure
+============================
+
+Microsoft Azure Blob Storage Driver for [docker-registry][docker-registry].
+
+This project is built on [Docker Global Hack Day #2][hackday].
+
+Usage:
+
+    docker run -p 5000:5000 \
+    	-e AZURE_STORAGE_ACCOUNT_NAME="<my acct>" \
+    	-e AZURE_STORAGE_ACCOUNT_KEY="<my key>" \
+    	-e AZURE_STORAGE_CONTAINER=<container name e.g. registry> \
+    	ahmetalpbalkan/registry-azure
+
+License
+=======
+
+Copyright 2014 Ahmet Alp Balkan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+[docker-registry]: https://github.com/docker/docker-registry
+[hackday]: https://blog.docker.com/2014/10/announcing-docker-global-hack-day-2/
---
+++ packages/docker-registry-driver-azureblob/docker-registry-driver-azureblob-1.1.2.tar.gz/docker-registry-driver-azureblob-1.1.2/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2014 Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+try:
+    import setuptools
+except ImportError:
+    import distutils.core as setuptools
+
+
+__author__ = 'Ahmet Alp Balkan'
+__copyright__ = 'Copyright 2014'
+__credits__ = []
+
+__version__ = '1.1.2'
+__maintainer__ = 'Ahmet Alp Balkan'
+__email__ = 'ahmetalpbalkan@gmail.com'
+
+__title__ = 'docker-registry-driver-azureblob'
+__build__ = 0x000000
+
+__url__ = 'https://github.com/ahmetalpbalkan/docker-registry-driver-azure'
+__description__ = 'Docker registry Azure Blob Storage driver'
+d = 'https://github.com/ahmetalpbalkan/docker-registry-driver-azure/archive/master.zip'
+
+setuptools.setup(
+    name=__title__,
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    maintainer=__maintainer__,
+    maintainer_email=__email__,
+    url=__url__,
+    license=open('./LICENSE').read(),
+    description=__description__,
+    download_url=d,
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Operating System :: OS Independent',
+                 'Topic :: Utilities',
+                 'License :: OSI Approved :: Apache Software License'],
+    platforms=['Independent'],
+    namespace_packages=['docker_registry',
+                        'docker_registry.drivers'],
+    packages=['docker_registry',
+              'docker_registry.drivers'],
+    install_requires=open('./requirements.txt').read(),
+    zip_safe=True
+)
