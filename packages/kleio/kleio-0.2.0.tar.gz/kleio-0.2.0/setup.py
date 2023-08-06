e90efb6a83ee27862bdc6bd56a7dff7cb1f4ea03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:56 +0000

    Add package kleio/0.2.0/kleio-0.2.0.tar.gz

---
+++ packages/kleio/kleio-0.2.0.tar.gz/kleio-0.2.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import re
+import pypandoc
+
+try:
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = open('README.md').read()
+
+
+def find_version(filename):
+    _version_re = re.compile(r'__version__ = "(.*)"')
+    for line in open(filename):
+        version_match = _version_re.match(line)
+        if version_match:
+            return version_match.group(1)
+
+version = find_version('kleio/__init__.py')
+
+packages = [
+    'kleio'
+]
+
+setup(
+    name = 'kleio',
+    version = version,
+    py_modules = [ 'kleio' ],
+    author = 'Stephan Zednik',
+    author_email = 'zednis2@rpi.edu',
+    url = 'http://github.com/tetherless-world/kleio',
+    description = 'A simple python implementation of the W3C PROV data model',
+    long_description = description,
+    install_requires = [ 'rdflib', 'rdflib-jsonld', 'isodate' ],
+    license = 'MIT',
+    packages = packages,
+    keywords = 'provenance PROV PROV-O rdf',
+    classifiers = [ 'Development Status :: 4 - Beta',
+                  'License :: OSI Approved :: MIT License',
+                  'Programming Language :: Python :: 3' ]
+)
