4cf8381c69f70a91b922493d975df563472babda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:29 +0000

    Add package cmdman/0.0.2/cmdman-0.0.2.tar.gz

---
+++ packages/cmdman/cmdman-0.0.2.tar.gz/cmdman-0.0.2/setup.py
@@ -0,0 +1,36 @@
+import sys
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    # General information.
+    name='cmdman',
+    description='Simple command line program wrapper adding debugging arguments.',
+
+    url='https://github.com/fmichea/cmdman',
+
+    # Version information.
+    license='BSD',
+    version='0.0.2',
+
+    # Author information.
+    author='Franck Michea',
+    author_email='franck.michea@gmail.com',
+
+    # File information.
+    packages=find_packages(exclude=['tests', 'examples']),
+
+    # PyPI categories.
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
+    ],
+)
