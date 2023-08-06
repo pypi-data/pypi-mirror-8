7c58caee04a80c3c578d314dae9009bdb86e8c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:56 +0000

    Add package infi.traceback/0.3.10.post4.gb48abbf/infi.traceback-0.3.10.post4.gb48abbf-py2.7.egg

---
+++ packages/infi.traceback/infi.traceback-0.3.10.post4.gb48abbf-py2.7.egg/infi/traceback/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.10.post4.gb48abbf"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = 'develop'
+__git_remote_tracking_branch__ = 'origin/develop'
+__git_remote_url__ = 'git@infinigit.infinidat.com:/host/traceback.git'
+__git_head_hash__ = 'b48abbf85a4d5bba6384185f43fa4b7fb5ec4a66'
+__git_head_subject__ = 'HIP-1425'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
