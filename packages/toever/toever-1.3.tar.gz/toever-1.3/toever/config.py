705adb24966cf6f0da474426f29b30cc7ece9b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:52 +0000

    Add package toever/1.3/toever-1.3.tar.gz

---
+++ packages/toever/toever-1.3.tar.gz/toever-1.3/toever/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+
+application_name = 'toever'
+version = '1.3'
+
+token_sandbox = False
+token_filepass = os.environ['HOME'] + '/.' + application_name
+token_geturl = 'https://www.evernote.com/api/DeveloperToken.action'
+
+if token_sandbox:
+    token_geturl = 'https://sandbox.evernote.com/api/DeveloperToken.action'
