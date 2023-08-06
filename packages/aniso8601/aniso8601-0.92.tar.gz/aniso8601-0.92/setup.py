2299ccc0ce4c2221c836849d196e6e29fa52edfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:31 +0000

    Add package aniso8601/0.92/aniso8601-0.92.tar.gz

---
+++ packages/aniso8601/aniso8601-0.92.tar.gz/aniso8601-0.92/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils import setup
+
+readme = open('README.rst', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(
+    name='aniso8601',
+    version='0.92',
+    description='A library for parsing ISO 8601 strings.',
+    long_description=README_TEXT,
+    author='Brandon Nielsen',
+    author_email='nielsenb@jetfuse.net',
+    url='https://bitbucket.org/nielsenb/aniso8601',
+    packages=['aniso8601'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
