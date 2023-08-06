cd72d0e4b24b05e14eaab5b8b32682d84bdfae4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:11 +0000

    Add package kamaki/0.13.1/kamaki-0.13.1.tar.gz

---
+++ packages/kamaki/kamaki-0.13.1.tar.gz/kamaki-0.13.1/kamaki/version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.13.1"
+__version_vcs_info__ = {
+    'branch': 'master',
+    'revid': '7b1ab62',
+    'revno': 2477}
+__version_user_email__ = "saxtouri@admin.grnet.gr"
+__version_user_name__ = "Stavros Sachtouris"
