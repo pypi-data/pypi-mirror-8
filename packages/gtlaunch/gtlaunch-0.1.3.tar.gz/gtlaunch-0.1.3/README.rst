04d72dbb0a1fac66c47c7e3e1245d6a81a068db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:10 +0000

    Add package gtlaunch/0.1.3/gtlaunch-0.1.3.tar.gz

---
+++ packages/gtlaunch/gtlaunch-0.1.3.tar.gz/gtlaunch-0.1.3/README.rst
@@ -0,0 +1,26 @@
+gtlaunch
+========
+
+.. image:: https://pypip.in/version/gtlaunch/badge.svg
+    :target: https://pypi.python.org/pypi/gtlaunch/
+    :alt: Latest PyPI version
+
+.. image:: https://pypip.in/download/gtlaunch/badge.svg
+    :target: https://pypi.python.org/pypi/gtlaunch/
+    :alt: Number of PyPI downloads
+
+.. image:: https://pypip.in/py_versions/gtlaunch/badge.svg
+    :target: https://pypi.python.org/pypi/gtlaunch/
+    :alt: Supported Python versions
+
+.. image:: https://pypip.in/wheel/gtlaunch/badge.svg
+        :target: https://pypi.python.org/pypi/gtlaunch/
+    :alt: Wheel Status
+
+.. image:: https://travis-ci.org/zsiciarz/gtlaunch.svg?branch=master
+    :target: https://travis-ci.org/zsiciarz/gtlaunch
+
+.. image:: https://coveralls.io/repos/zsiciarz/gtlaunch/badge.png?branch=master
+    :target: https://coveralls.io/r/zsiciarz/gtlaunch?branch=master
+
+Gnome Terminal launcher
---
+++ packages/gtlaunch/gtlaunch-0.1.3.tar.gz/gtlaunch-0.1.3/gtlaunch/launcher.py
@@ -0,0 +1,57 @@
+import json
+import os
+import subprocess
+
+
+class LauncherError(Exception):
+    pass
+
+
+class Launcher(object):
+    def __init__(self, options, lazy=False):
+        self.options = options
+        self.config = {}
+        self.project = None
+        if not lazy:
+            self.process_options(options)
+
+    def process_options(self, options):
+        if options.verbose:
+            print("Reading config file '{}'...".format(options.config))
+        try:
+            with open(os.path.expanduser(options.config), 'r') as fp:
+                self.config = json.load(fp)
+        except IOError:
+            raise LauncherError("Config file '{}' not found.".format(options.config))
+        except ValueError:
+            raise LauncherError("Config file '{}' is invalid JSON.".format(options.config))
+        if options.project:
+            try:
+                self.project = self.config[options.project]
+            except KeyError:
+                raise LauncherError("Project '{}' not found.".format(options.project))
+
+    def build_args(self, project):
+        args = ['gnome-terminal', '--maximize']
+        if 'cwd' in project:
+            args.extend([
+                '--working-directory', os.path.expanduser(project['cwd']),
+            ])
+        for idx, tab in enumerate(project['tabs']):
+            tab_option = '--tab' if idx == 0 else '--tab-with-profile=Default'
+            prefix = project.get('prefix', 'true')
+            command = "zsh -is eval '{} && {}'".format(prefix, tab['command'])
+            args.append(tab_option)
+            tab_title = tab.get('title', tab['command'])
+            args.extend(['--title', tab_title])
+            args.extend(['--command', command])
+        return args
+
+    def run(self):
+        if self.options.list:
+            print("\n".join(sorted(self.config.keys())))
+            return
+        args = self.build_args(self.project)
+        if self.options.verbose:
+            print("Running \"{}\"".format(' \\\n\t'.join(args)))
+        return subprocess.Popen(args)
