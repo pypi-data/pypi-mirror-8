9c0260341394c1c7f7435a8a9aedca4af1985f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:41 +0000

    Add package django-roxyfileman/1.0.0/django-roxyfileman-1.0.0.tar.gz

---
+++ packages/django-roxyfileman/django-roxyfileman-1.0.0.tar.gz/django-roxyfileman-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-roxyfileman',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Integrate Django with Roxy Fileman',
+    author='Maxim Poletaev',
+    author_email='zenwalker2@gmail.com',
+    url='https://github.com/zenwalker/django-roxyfileman',
+    keywords=['filemanager'],
+    license='GPLv3'
+)
