c206ec104553085d6943daac1caa93a2c3f22452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package pyolite/1.2/pyolite-1.2.tar.gz

---
+++ packages/pyolite/pyolite-1.2.tar.gz/pyolite-1.2/pyolite/models/lists/keys.py
@@ -0,0 +1,44 @@
+import hashlib
+
+from unipath import Path
+
+
+class ListKeys(list):
+  def __init__(self, user, *args, **kwargs):
+    super(ListKeys, self).__init__(*args, **kwargs)
+    self.user = user
+
+  def append(self, key):
+    key_path = Path(key)
+
+    if key_path.isfile():
+      with open(str(key_path)) as f:
+        key = f.read()
+
+    directory = Path(self.user.path, 'keydir', hashlib.md5(key).hexdigest())
+    directory.mkdir()
+
+    key_file = Path(directory, "%s.pub" % self.user.name)
+    key_file.write_file(key)
+
+    self.user.git.commit(['keydir'],
+                         'Added new key for user %s' % self.user.name)
+
+    super(ListKeys, self).append(key)
+
+  def remove(self, key):
+    directory = Path(self.user.path, 'keydir', hashlib.md5(key).hexdigest())
+    key_file = Path(directory, "%s.pub" % self.user.name)
+
+    if not key_file.exists():
+      raise ValueError("Invalid key")
+
+    key_file.remove()
+    key_file.parent.rmdir()
+
+    self.user.git.commit([str(key_file)],
+                         'Removed key for user %s' % self.user.name)
+
+  def __add__(self, keys):
+    for key in keys:
+      self.append(key)
