e200aaaacd46794c6f6a28de3fa1e4fbc08dd82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:27 +0000

    Add package kegmeter-common/0.99.1/kegmeter-common-0.99.1.tar.gz

---
+++ packages/kegmeter-common/kegmeter-common-0.99.1.tar.gz/kegmeter-common-0.99.1/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="kegmeter-common",
+    description="Kegmeter libraries used by both the app and the webserver",
+    url="https://github.com/Dennovin/kegmeter",
+    version="0.99.1",
+    author="OmniTI Computer Consulting, Inc.",
+    author_email="hello@omniti.com",
+    license="MIT",
+    namespace_packages=["kegmeter"],
+    packages=find_packages(),
+    install_requires=[
+        "ago >= 0.0.6",
+        "oauth2client >= 1.3.1",
+        "python-memcached >= 1.53",
+        "requests >= 1.2.3",
+        "simplejson >= 3.6.5",
+        ],
+    )
