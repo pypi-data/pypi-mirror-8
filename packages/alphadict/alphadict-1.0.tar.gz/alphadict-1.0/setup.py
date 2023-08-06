319bec6ce45ffef5ebb9c4c66143c3988f1e3e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:34 +0000

    Add package alphadict/1.0/alphadict-1.0.tar.gz

---
+++ packages/alphadict/alphadict-1.0.tar.gz/alphadict-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='alphadict',
+      version='1.0',
+      description='Recursively sorts dictionaries by key.',
+      url='https://github.com/dancrew32/AlphaDict',
+      author='Dan Masquelier',
+      author_email='dmasquelier@gmail.com',
+      license='MIT',
+      packages=['alphadict'],
+      zip_safe=False)
