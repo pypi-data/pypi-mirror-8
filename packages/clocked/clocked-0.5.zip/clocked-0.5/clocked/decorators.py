d185948c7be2e58facd0db84b4552e984e2a3aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:15 +0000

    Add package clocked/0.5/clocked-0.5.zip

---
+++ packages/clocked/clocked-0.5.zip/clocked-0.5/clocked/decorators.py
@@ -0,0 +1,48 @@
+""" Decorator support for clocked. """
+
+
+import inspect
+import new
+from clocked.clockit import Clocked
+
+
+def _create_function_wrapper(obj):
+    def wrapper(*args, **kwargs):
+        with Clocked(obj.__name__):
+            return obj(*args, **kwargs)
+    return wrapper
+
+
+def _create_method_wrapper(obj):
+    if obj.im_self is not None:
+        def wrapper(*args, **kwargs):
+            with Clocked(obj.__name__):
+                return obj.__func__(*args, **kwargs)
+        wrapper = new.instancemethod(wrapper, obj.im_self)
+    else:
+        def wrapper(*args, **kwargs):
+            with Clocked(obj.__name__):
+                return obj.__func__(*args, **kwargs)
+    return wrapper
+
+
+def clocked(obj):
+    """
+    Clocked decorator. Put this on a class or and individual function for it's
+    timing information to be tracked.
+    """
+
+    _is_class = inspect.isclass(obj)
+    _is_func = inspect.isfunction(obj)
+    if not _is_class and not _is_func:
+        raise Exception('unsupported type {}'.format(type(obj)))
+
+    if _is_func:
+        return _create_function_wrapper(obj)
+    elif _is_class:
+        for name, method in inspect.getmembers(obj, inspect.ismethod):
+            if inspect.ismethod(method) or inspect.isfunction(method):
+                wrapper = _create_method_wrapper(method)
+                setattr(obj, name, wrapper)
+
+    return obj
---
+++ packages/clocked/clocked-0.5.zip/clocked-0.5/setup.py
@@ -0,0 +1,27 @@
+
+
+from distutils.core import setup
+import os
+
+
+def read(fname):
+    # noinspection PyBroadException
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except:
+        return ""
+
+
+setup(
+    name='clocked',
+    packages=['clocked'],
+    version='0.5',
+    description='python library for high level profiling',
+    author='Jesse Buesking',
+    author_email='jessebuesking+pypi@gmail.com',
+    url='https://github.com/JesseBuesking/clocked',
+    download_url='https://github.com/JesseBuesking/clocked/tarball/0.2',
+    keywords=['benchmark', 'benchmarking', 'profiler', 'profiling'],
+    long_description=read('readme.md'),
+    classifiers=[]
+)
