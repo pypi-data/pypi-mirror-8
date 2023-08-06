09a6ac62253748d7c45b06eb0c8d817dd498cc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:02 +0000

    Add package django-entity-emailer/0.7.0/django-entity-emailer-0.7.0.tar.gz

---
+++ packages/django-entity-emailer/django-entity-emailer-0.7.0.tar.gz/django-entity-emailer-0.7.0/entity_emailer/models.py
@@ -0,0 +1,152 @@
+from datetime import datetime
+
+from django.core.exceptions import ValidationError
+from django.core.urlresolvers import reverse
+from django.db import models
+from entity.models import Entity, EntityKind
+from entity_event.models import Source
+from jsonfield import JSONField
+
+
+class EmailManager(models.Manager):
+    """
+    Provides the ability to easily create emails with the recipients.
+    """
+    def create_email(self, recipients=None, **kwargs):
+        """
+        Note that we pop the scheduled time from the kwargs before creating the email
+        and then update the scheduled time after the recipients have been added. This
+        avoids the potential race condition of the email being created before it is
+        picked up by a task that sends it.
+        """
+        scheduled = kwargs.pop('scheduled', datetime.utcnow())
+        email = Email.objects.create(scheduled=None, **kwargs)
+        if recipients:
+            email.recipients.add(*recipients)
+
+        email.scheduled = scheduled
+        email.save()
+        return email
+
+
+class Email(models.Model):
+    """Save an Email object and it is sent automagically!
+
+    If an Email object is saved without a `scheduled` time, it will
+    immediately and automatically be sent.
+
+    If an Email object is saved with a `scheduled` time, it will be
+    sent at that time.
+
+    Emails can be sent to an individual, by setting subentities to
+    False, or to a group, by setting send_to to a superentity, like a
+    Team or Organization, and setting subentities to True. This applies
+    to every entity in the recipients list.
+
+    Sending an email happens automatically, and consists of rendering
+    the given template with the given context.
+
+    Emails will not be sent to any individual who has unsubscribed
+    from emails of that source.
+    """
+    source = models.ForeignKey(Source)
+    sub_entity_kind = models.ForeignKey(EntityKind, null=True, default=None)
+    recipients = models.ManyToManyField(Entity)
+    subject = models.CharField(max_length=256)
+    from_address = models.CharField(max_length=256, default='')
+    template = models.ForeignKey('EmailTemplate')
+    context = JSONField()
+    uid = models.CharField(max_length=100, unique=True, null=True, default=None)
+    # The `scheduled` field uses a default value of the datetime.utcnow function.
+    # This means it will be called any time a new Email is created, but it also
+    # allows for a different schedule to be set (to schedule the email for some
+    # time in the future), which would not be possible with an auto_add_now=True.
+    scheduled = models.DateTimeField(null=True, default=datetime.utcnow)
+    sent = models.DateTimeField(null=True, default=None)
+
+    objects = EmailManager()
+
+    def get_context(self):
+        """
+        Retrieves the context for this email, passing it through the context loader of
+        the email template if necessary. It also adds the email url address to the context.
+        """
+        context = self.source.get_context(self.context)
+        context['entity_emailer_url'] = reverse('entity_emailer.email', args=[self.id])
+        return context
+
+
+class EmailTemplate(models.Model):
+    """A template for email to be sent. Rendered by django with context.
+
+    Of the four fields: `text_template_path`, 'html_template_path',
+    `text_template`, and `html_template`, at least one must be
+    non-empty.
+
+    Both a text and html template may be provided, either
+    through a path to the template, or a raw template object.
+
+    However, for either text or html templates, both a path and raw
+    template should not be provided.
+
+    For more complex context loading capabilities, provide an executable
+    function for loading the email context. This function accepts the
+    context stored for the email and returns the context again with any
+    other fetched values.
+
+    The email sending task will take care of rendering the template,
+    and creating a text or text/html message based on the rendered
+    template.
+    """
+    template_name = models.CharField(max_length=64, unique=True)
+    text_template_path = models.CharField(max_length=256, default='')
+    html_template_path = models.CharField(max_length=256, default='')
+    text_template = models.TextField(default='')
+    html_template = models.TextField(default='')
+
+    def clean(self):
+        template_fields = [
+            self.text_template_path, self.html_template_path,
+            self.text_template, self.html_template
+        ]
+        if not any(template_fields):
+            raise ValidationError('At least one template source must be provided')
+        if self.text_template_path and self.text_template:
+            raise ValidationError('Cannot provide a template path and template')
+        if self.html_template_path and self.html_template:
+            raise ValidationError('Cannot provide a template path and template')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(EmailTemplate, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.template_name
+
+
+class IndividualEmailManager(models.Manager):
+    def get_queryset(self):
+        return super(IndividualEmailManager, self).get_queryset().filter(sub_entity_kind__isnull=True)
+
+
+class IndividualEmail(Email):
+    """A proxy model of Email to support a different admin Interface.
+    """
+    class Meta:
+        proxy = True
+
+    objects = IndividualEmailManager()
+
+
+class GroupEmailManager(models.Manager):
+    def get_queryset(self):
+        return super(GroupEmailManager, self).get_queryset().filter(sub_entity_kind__isnull=False)
+
+
+class GroupEmail(Email):
+    """A proxy model of Email to support a different admin Interface.
+    """
+    class Meta:
+        proxy = True
+
+    objects = GroupEmailManager()
---
+++ packages/django-entity-emailer/django-entity-emailer-0.7.0.tar.gz/django-entity-emailer-0.7.0/entity_emailer/tasks.py
@@ -0,0 +1,171 @@
+from datetime import datetime
+import logging
+
+from celery import Task
+from db_mutex import db_mutex
+from django.conf import settings
+from django.core import mail
+from django.template.loader import render_to_string
+from django.template import Context, Template
+
+from entity_emailer.models import Email, EmailTemplate
+from entity_emailer import get_medium
+
+LOG = logging.getLogger(__name__)
+
+
+class SendUnsentScheduledEmails(Task):
+    """Send all unsent emails, whose scheduled time has passed.
+
+    This task should be added to a celery beat.
+    """
+    def run(self, *args, **kwargs):
+        with db_mutex('send-unsent-scheduled-emails'):
+            self.run_worker(*args, **kwargs)
+
+    def run_worker(self, *args, **kwargs):
+        current_time = datetime.utcnow()
+        to_send = Email.objects.filter(scheduled__lte=current_time, sent__isnull=True)
+        default_from_email = get_from_email_address()
+        emails = []
+        for email in to_send:
+            to_email_addresses = get_subscribed_email_addresses(email)
+            text_message, html_message = render_templates(email)
+            message = create_email_message(
+                to_emails=to_email_addresses,
+                from_email=email.from_address or default_from_email,
+                subject=email.subject,
+                text=text_message,
+                html=html_message,
+            )
+            emails.append(message)
+        connection = mail.get_connection()
+        connection.send_messages(emails)
+        to_send.update(sent=current_time)
+
+
+def create_email_message(to_emails, from_email, subject, text, html):
+    """Create the appropriate plaintext or html email object.
+
+    Returns:
+
+       email - an instance of either `django.core.mail.EmailMessage` or
+       `django.core.mail.EmailMulitiAlternatives` based on whether or
+       not `html_message` is empty.
+    """
+    if not html:
+        email = mail.EmailMessage(
+            subject=subject,
+            body=text,
+            to=to_emails,
+            from_email=from_email,
+        )
+    else:
+        email = mail.EmailMultiAlternatives(
+            subject=subject,
+            body=text,
+            to=to_emails,
+            from_email=from_email,
+        )
+        email.attach_alternative(html, 'text/html')
+    return email
+
+
+def get_from_email_address():
+    """Get a 'from' address based on the django settings.
+    """
+    return getattr(settings, 'ENTITY_EMAILER_FROM_EMAIL', settings.DEFAULT_FROM_EMAIL)
+
+
+def get_subscribed_email_addresses(email):
+    """From an email object determine the appropriate email addresses.
+
+    Excludes the addresses of those who unsubscribed from the email's
+    type.
+
+    Returns:
+      A list of strings: email addresses.
+    """
+    email_medium = get_medium()
+    if email.sub_entity_kind is not None:
+        all_entities = [
+            se
+            for recipient in email.recipients.all()
+            for se in recipient.get_sub_entities() if se.entity_kind_id == email.sub_entity_kind_id
+        ]
+    else:
+        all_entities = list(email.recipients.all())
+
+    send_to = email_medium.filter_source_targets_by_unsubscription(email.source_id, all_entities)
+    emails = [e.entity_meta['email'] for e in send_to]
+    return emails
+
+
+def render_templates(email):
+    """Render the correct templates with the correct context.
+
+    Args:
+      An email object. Contains references to template and context.
+
+    Returns:
+      A tuple of (rendered_text, rendered_html). Either, but not both
+      may be an empty string.
+    """
+    context = email.get_context()
+
+    # Process text template:
+    if email.template.text_template_path:
+        rendered_text = render_to_string(
+            email.template.text_template_path, context
+        )
+    elif email.template.text_template:
+        context = Context(context)
+        rendered_text = Template(email.template.text_template).render(context)
+    else:
+        rendered_text = ''
+
+    # Process html template
+    if email.template.html_template_path:
+        rendered_html = render_to_string(
+            email.template.html_template_path, context
+        )
+    elif email.template.html_template:
+        context = Context(context)
+        rendered_html = Template(email.template.html_template).render(context)
+    else:
+        rendered_html = ''
+
+    return (rendered_text, rendered_html)
+
+
+class ConvertEventsToEmails(Task):
+    """
+    Converts events to emails based on the email subscriptions.
+    """
+    def run(self, *args, **kwargs):
+        with db_mutex('convert-events-to-emails'):
+            self.run_worker(*args, **kwargs)
+
+    def run_worker(self, *args, **kwargs):
+        convert_events_to_emails()
+
+
+def convert_events_to_emails():
+    """
+    Converts unseen events to emails and marks them as seen.
+    """
+    email_medium = get_medium()
+
+    for event, targets in email_medium.events_targets(seen=False, mark_seen=True):
+        try:
+            template_name = event.context.get('entity_emailer_template', '')
+            template = EmailTemplate.objects.get(template_name=template_name)
+        except EmailTemplate.DoesNotExist:
+            err = 'Event does not have a template or the template does not exist. Context: {context}'
+            LOG.error(err.format(note=event, context=event.context))
+            # If we can't find a template, skip creating this email.
+            continue
+
+        Email.objects.create_email(
+            source=event.source, recipients=targets, template=template, context=event.context,
+            subject=event.context.get('entity_emailer_subject', 'Email'))
