e1977b158437490cdd999b99ad7a5f2f5be31d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:16 +0000

    Add package boadata/0.2.1/boadata-0.2.1.tar.gz

---
+++ packages/boadata/boadata-0.2.1.tar.gz/boadata-0.2.1/boadata/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+import re
+sys.path += [ "../.."]
+from PyQt4 import QtCore, QtGui
+from boadata.trees.file import DirectoryTree
+from boadata.trees.sql import DatabaseTree
+from boadata.gui.qt import MainWindow, DataTreeModel
+
+def run_app():
+    app = QtGui.QApplication(sys.argv)
+    
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+        if re.search("sql.*://", path):
+            node = DatabaseTree(path)
+        else:
+            node = DirectoryTree(path)
+        model = DataTreeModel(node) 
+    else:
+        model = None
+
+    mw = MainWindow()
+    if model:
+        mw.show_tree(model)
+    mw.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    run_app()
