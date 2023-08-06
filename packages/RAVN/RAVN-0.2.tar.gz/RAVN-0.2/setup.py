6c93badcc278c389879810be3e1abed076432203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:25 +0000

    Add package RAVN/0.2/RAVN-0.2.tar.gz

---
+++ packages/RAVN/RAVN-0.2.tar.gz/RAVN-0.2/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup, Command
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'tests/runtests.py'])
+        raise SystemExit(errno)
+
+setup(
+  name = 'RAVN',
+  packages = ['RAVN'], # this must be the same as the name above
+  version = '0.2',
+  description = 'Next Gen Smart Drone Platform',
+  author = 'RaptorBird Robotics Inc.',
+  author_email = 'contact@raptorbird.com',
+  url = 'https://github.com/raptorbird/RAVN', # use the URL to the github repo
+  download_url = 'https://github.com/raptorbird/RAVN/tarball/0.1', # I'll explain this in a second
+  keywords = ['ravn', 'drone', 'development', 'smart'], # arbitrary keywords
+  classifiers = [],
+  install_requires = ['ws4py', 'droneapi']
+)
