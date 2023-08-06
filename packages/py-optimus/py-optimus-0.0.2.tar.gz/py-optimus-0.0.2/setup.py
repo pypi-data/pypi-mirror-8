ac01c95c3dc4d11a632b0d0867e813cadd2a7a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:13 +0000

    Add package py-optimus/0.0.2/py-optimus-0.0.2.tar.gz

---
+++ packages/py-optimus/py-optimus-0.0.2.tar.gz/py-optimus-0.0.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='py-optimus',
+    author='John Deng',
+    author_email='denghuanzhong@gmail.com',
+    version='0.0.2',
+    description='Optimus is a Python web framework project constructor.',
+    url='http://github.com/johndeng/optimus',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'Click',
+        'jinja2'
+    ],
+    entry_points='''
+        [console_scripts]
+        optimus=optimus.optimus:main
+    '''
+)
