787fa349f160c25b7720ffc7f3a57219687df2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:33 +0000

    Add package lpaste/1.7/lpaste-1.7.zip

---
+++ packages/lpaste/lpaste-1.7.zip/lpaste-1.7/lpaste/darwin/clipboard.py
@@ -0,0 +1,29 @@
+import os
+from lpaste.source import CodeSource
+
+# from pyperclip (http://coffeeghost.net/2010/10/09/pyperclip-a-cross-platform-clipboard-module-for-python)
+
+def macSetClipboard(text):
+	outf = os.popen('pbcopy', 'w')
+	outf.write(text)
+	outf.close()
+
+def macGetClipboard():
+	outf = os.popen('pbpaste', 'r')
+	content = outf.read()
+	outf.close()
+	return content
+
+def get_source():
+	code = macGetClipboard()
+	src = CodeSource(code=code)
+	try:
+		# see if the code can compile as Python
+		compile(code, 'pasted_code.py', 'exec')
+		src.format = 'python'
+	except:
+		pass # use default format
+	return src
+
+def set_text(text):
+	macSetClipboard(text.encode('utf-8'))
