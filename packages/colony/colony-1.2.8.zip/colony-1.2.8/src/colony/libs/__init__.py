4678c9146d9d77a4339f1ea770fda54fa081e4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:21 +0000

    Add package colony/1.2.8/colony-1.2.8.zip

---
+++ packages/colony/colony-1.2.8.zip/colony-1.2.8/src/colony/libs/__init__.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Colony Framework
+# Copyright (c) 2008-2014 Hive Solutions Lda.
+#
+# This file is part of Hive Colony Framework.
+#
+# Hive Colony Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Colony Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import aes_util
+import bank_util
+import barcode_util
+import cache_util
+import call_util
+import control_util
+import country_util
+import crypt_util
+import encode_util
+import file_util
+import gtin_util
+import host_util
+import import_util
+import lazy_util
+import list_util
+import logging_util
+import map_util
+import math_util
+import number_util
+import object_util
+import observer_util
+import os_util
+import path_util
+import protection_util
+import quote_util
+import round_util
+import scheduling_util
+import size_util
+import stack_util
+import string_buffer_util
+import string_util
+import structures_util
+import test_util
+import time_util
+import update_thread_util
+import version_util
+import visitor_util
+
+from aes_util import *
+from bank_util import *
+from barcode_util import *
+from cache_util import *
+from call_util import *
+from control_util import *
+from country_util import *
+from crypt_util import *
+from encode_util import *
+from file_util import *
+from gtin_util import *
+from host_util import *
+from import_util import *
+from lazy_util import *
+from list_util import *
+from logging_util import *
+from map_util import *
+from math_util import *
+from number_util import *
+from object_util import *
+from observer_util import *
+from os_util import *
+from path_util import *
+from protection_util import *
+from quote_util import *
+from round_util import *
+from scheduling_util import *
+from size_util import *
+from stack_util import *
+from string_buffer_util import *
+from string_util import *
+from structures_util import *
+from test_util import *
+from time_util import *
+from update_thread_util import *
+from version_util import *
+from visitor_util import *
+
+from import_util import __import__
