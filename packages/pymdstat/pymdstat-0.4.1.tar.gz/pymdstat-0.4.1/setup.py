4942a9e66a6adb6d009978e35b9685f382cd3aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:05 +0000

    Add package pymdstat/0.4.1/pymdstat-0.4.1.tar.gz

---
+++ packages/pymdstat/pymdstat-0.4.1.tar.gz/pymdstat-0.4.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+data_files = [('share/doc/pymdstat', ['AUTHORS', 'LICENSE', 'NEWS', 'README.rst'])]
+
+setup(
+    name='pymdstat',
+    version='0.4.1',
+    description="Python library to parse Linux /proc/mdstat",
+    long_description=open('README.rst').read(),
+    author='Nicolas Hennion',
+    author_email='nicolas@nicolargo.com',
+    url='https://github.com/nicolargo/pymdstat',
+    # download_url='https://s3.amazonaws.com/pymdstat/pymdstat-0.4.1.tar.gz',
+    license="MIT",
+    keywords="raid linux",
+    packages=['pymdstat'],
+    include_package_data=True,
+    data_files=data_files,
+    # test_suite="pymdstat.test",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
