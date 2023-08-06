ecab1aa8186f4ae349eeb39edbcaaa511222db5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:58 +0000

    Add package djangocms_seotools/1.5/djangocms_seotools-1.5.zip

---
+++ packages/djangocms_seotools/djangocms_seotools-1.5.zip/djangocms_seotools-1.5/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Nikolay Dolganov"
+__email__ = "SirNikolasd@yandex.ru"
+
+from setuptools import setup
+
+setup(
+    name='djangocms_seotools',
+    version='1.5',
+    description='SEO and Custom tools for the Django CMS',
+    author='Nikolay <Werevolff> Dolganov',
+    author_email='sirnikolasd@yandex.ru',
+    url='https://github.com/werevolff/django_cms_seotools',
+    packages=['djangocms_seotools', 'djangocms_seotools.tests'],
+)
