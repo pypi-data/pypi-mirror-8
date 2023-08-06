e8e281a56695842f142073b34f22136332b9117a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:39 +0000

    Add package malleefowl/0.1.7/malleefowl-0.1.7.tar.gz

---
+++ packages/malleefowl/malleefowl-0.1.7.tar.gz/malleefowl-0.1.7/malleefowl/config.py
@@ -0,0 +1,55 @@
+import os
+from pywps import config as wpsconfig
+
+from malleefowl import wpslogging as logging
+logger = logging.getLogger(__name__)
+
+def mkdir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def getConfigValue(*args):
+    value = ''
+    try:
+        value = wpsconfig.getConfigValue(*args)
+    except Exception:
+        logger.exception("Could not get config value for %s", args)
+    return value
+
+def thredds_url():
+    return wpsconfig.getConfigValue("malleefowl", "thredds_url")
+    
+def timeout():
+    # default 1 day, in secs, 0 means for ever
+    timeout = 86400
+    try:
+        timeout = int(wpsconfig.getConfigValue("malleefowl", "timeout"))
+    except Exception:
+        logger.warn("timeout not configured ... using default %s", url)
+    return timeout
+
+def cache_path():
+    mypath = getConfigValue("cache", "cache_path")
+    if len(mypath) == 0:
+        mypath = os.path.join(os.sep, "tmp", "cache")
+    mkdir(mypath)
+    return mypath
+
+def cache_url():
+    return getConfigValue("cache", "cache_url")
+
+def archive_root():
+    archive_root = None
+    try:
+        archive_root = wpsconfig.getConfigValue("malleefowl", "archive_root")
+        archive_root = [path.strip() for path in archive_root.split(',')]
+    except Exception:
+        archive_root = []
+        logger.warn("archive root not configured")
+    return archive_root
+
+def mako_cache():
+    mako_cache = getConfigValue("malleefowl", "mako_cache")
+    mkdir(mako_cache)
+    return mako_cache
+
---
+++ packages/malleefowl/malleefowl-0.1.7.tar.gz/malleefowl-0.1.7/unit_tests/test_esgf_search.py
@@ -0,0 +1,246 @@
+import nose.tools
+from unittest import TestCase
+from nose import SkipTest
+from nose.plugins.attrib import attr
+
+from __init__ import TESTDATA, SERVICE
+
+class EsgSearchTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        from malleefowl.esgf.search import ESGSearch
+        cls.esgsearch = ESGSearch('http://localhost:8081/esg-search', distrib=False,
+                                  latest=True, replica=False)
+
+    @attr('online')
+    def test_dataset(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CORDEX') )
+        constraints.append( ('time_frequency', 'mon' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('experiment', 'historical') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='Dataset',
+            limit=1,
+            offset=0,
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) == 1, result)
+
+    @attr('online')
+    def test_file(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CORDEX') )
+        constraints.append( ('time_frequency', 'mon' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('experiment', 'historical') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File',
+            limit=1,
+            offset=0,
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) > 1, result)
+
+    @attr('online')
+    def test_file_cmip5_many(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CMIP5') )
+        constraints.append( ('time_frequency', 'mon' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('experiment', 'historical') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File',
+            limit=100,
+            offset=0,
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) > 1, result)
+
+    @attr('online')
+    def test_file_more_than_one(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CORDEX') )
+        constraints.append( ('time_frequency', 'mon' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('experiment', 'historical') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File',
+            limit=3,
+            offset=0,
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) > 1, result)
+
+    @attr('online')
+    def test_aggregation(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CORDEX') )
+        constraints.append( ('time_frequency', 'mon' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('experiment', 'historical') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='Aggregation',
+            limit=1,
+            offset=0,
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) == 0, result)
+
+    @attr('online')
+    def test_tds_file_cordex(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CORDEX') )
+        constraints.append( ('time_frequency', 'mon' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('experiment', 'historical') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File_Thredds',
+            limit=10,
+            offset=0,
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) > 1, result)
+
+    @attr('online')
+    def test_tds_file_cordex_date(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CORDEX') )
+        constraints.append( ('time_frequency', 'mon' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('experiment', 'historical') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File_Thredds',
+            limit=10,
+            offset=0,
+            temporal=True,
+            start='2001-01-01T12:00:00Z',
+            end='2005-12-31T12:00:00Z',
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) > 1, result)
+        nose.tools.ok_(summary['number_of_selected_files'] > 1, result)
+
+    @attr('online')
+    def test_file_cordex_many(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CORDEX') )
+        constraints.append( ('time_frequency', 'day' ) )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File',
+            limit=100,
+            offset=0,
+            temporal=False,
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) > 1, result)
+        nose.tools.ok_(summary['number_of_selected_files'] > 1, result)
+        #nose.tools.ok_(False, len(result))
+
+    @attr('online')
+    def test_tds_file_cordex_many(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CORDEX') )
+        constraints.append( ('time_frequency', 'day' ) )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File_Thredds',
+            limit=100,
+            offset=0,
+            temporal=False,
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) > 1, result)
+        nose.tools.ok_(summary['number_of_selected_files'] > 1, result)
+        #nose.tools.ok_(False, len(result))
+
+    @attr('online')
+    def test_tds_file_cordex_fly(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CORDEX') )
+        constraints.append( ('time_frequency', 'day' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('variable', 'tasmax') )
+        constraints.append( ('variable', 'tasmin') )
+        constraints.append( ('variable', 'pr') )
+        constraints.append( ('variable', 'prsn') )
+        constraints.append( ('experiment', 'historical') )
+        constraints.append( ('experiment', 'rcp26') )
+        constraints.append( ('experiment', 'rcp45') )
+        constraints.append( ('experiment', 'rcp85') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File',
+            limit=100,
+            offset=0,
+            temporal=True,
+            start='1960-01-01T12:00:00Z',
+            end='1970-12-31T12:00:00Z',
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) > 1, result)
+        nose.tools.ok_(summary['number_of_selected_files'] > 1, result)
+
+    @attr('online')
+    def test_tds_file_cmip5(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CMIP5') )
+        constraints.append( ('time_frequency', 'mon' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('experiment', 'historical') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File_Thredds',
+            limit=10,
+            offset=0,
+            constraints = constraints)
+
+        nose.tools.ok_(len(result) > 1, result)
+        nose.tools.ok_(summary['number_of_selected_files'] > 1, result)
+
+    @attr('online')
+    def test_tds_file_cmip5_date(self):
+        #raise SkipTest
+        constraints = []
+        constraints.append( ('project', 'CMIP5') )
+        constraints.append( ('time_frequency', 'mon' ) )
+        constraints.append( ('variable', 'tas') )
+        constraints.append( ('experiment', 'historical') )
+
+        (result, summary, facet_counts) = self.esgsearch.search(
+            search_type='File_Thredds',
+            limit=20,
+            offset=0,
+            constraints = constraints,
+            temporal=False,
+            start='1960-01-01T12:00:00Z',
+            end='1995-12-31T12:00:00Z')
+
+        nose.tools.ok_(len(result) > 1, result)
+        nose.tools.ok_(summary['number_of_selected_files'] > 1, result)
+
+    
+
+
+
+
