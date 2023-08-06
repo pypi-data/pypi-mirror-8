27a1e2b300e0d1f44dfa271afd0e8a201f676c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:08 +0000

    Add package MyProject/1.0/MyProject-1.0.zip

---
+++ packages/MyProject/MyProject-1.0.zip/MyProject-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'MyProject',
+    packages = ['pkgA', 'pkgB'],
+    scripts = ['runner'],
+    version = '1.0',
+    description = 'My first project',
+    author = 'dokelung',
+    author_email = 'dokelung@gmail.com',
+    keywords = ['Good Project'],
+    classifiers = [],
+)
