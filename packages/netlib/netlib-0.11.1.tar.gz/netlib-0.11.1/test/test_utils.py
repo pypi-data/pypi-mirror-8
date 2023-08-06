aed6cba0d9bac774784467378b089b4437959514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:39 +0000

    Add package netlib/0.11.1/netlib-0.11.1.tar.gz

---
+++ packages/netlib/netlib-0.11.1.tar.gz/netlib-0.11.1/test/test_utils.py
@@ -0,0 +1,12 @@
+from netlib import utils
+import socket
+
+def test_hexdump():
+    assert utils.hexdump("one\0"*10)
+
+
+def test_cleanBin():
+    assert utils.cleanBin("one") == "one"
+    assert utils.cleanBin("\00ne") == ".ne"
+    assert utils.cleanBin("\nne") == "\nne"
+    assert utils.cleanBin("\nne", True) == ".ne"
