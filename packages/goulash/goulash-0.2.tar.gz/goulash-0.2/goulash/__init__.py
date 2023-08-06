6d089008feb422f4c13a974b885dfeb3efad3ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:58 +0000

    Add package goulash/0.2/goulash-0.2.tar.gz

---
+++ packages/goulash/goulash-0.2.tar.gz/goulash-0.2/goulash/__init__.py
@@ -0,0 +1,4 @@
+""" goulash
+"""
+from .namespaces import Namespace
+from .metaclasses import supports_class_algebra
---
+++ packages/goulash/goulash-0.2.tar.gz/goulash-0.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+""" setup.py for goulash
+"""
+
+from setuptools import setup
+
+setup(
+    name         = 'goulash',
+    version      = '0.2',
+    description  = 'toolbox, random shared stuff from my other projects',
+    author       = 'mattvonrocketstein',
+    author_email = '$author@gmail',
+    url          = 'http://github.com/mattvonrocketstein/goulash',
+    download_url = 'https://github.com/mattvonrocketstein/goulash/tarball/pypi',
+    packages     = ['goulash'],
+    keywords     = ['goulash']
+    )
