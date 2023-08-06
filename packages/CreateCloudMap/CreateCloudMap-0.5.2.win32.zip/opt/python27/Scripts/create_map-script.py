a1ee832787ac3fe02f91ff16ddd8d2cb3029b86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:23 +0000

    Add package CreateCloudMap/0.5.2/CreateCloudMap-0.5.2.win32.zip

---
+++ packages/CreateCloudMap/CreateCloudMap-0.5.2.win32.zip/opt/python27/Scripts/create_map-script.py
@@ -0,0 +1,10 @@
+#!c:\opt\python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'CreateCloudMap==0.5.2','console_scripts','create_map'
+__requires__ = 'CreateCloudMap==0.5.2'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('CreateCloudMap==0.5.2', 'console_scripts', 'create_map')()
+    )
