855b8636fccad187d53e1f441af0b5e66e72f713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:24 +0000

    Add package compmake/3.4.5/compmake-3.4.5.tar.gz

---
+++ packages/compmake/compmake-3.4.5.tar.gz/compmake-3.4.5/src/compmake/jobs/queries.py
@@ -0,0 +1,135 @@
+""" Contains queries of the job DB. """
+import warnings
+
+from ..jobs import all_jobs, get_job
+from contracts import contract
+from contextlib import contextmanager
+from contracts.utils import raise_wrapped, check_isinstance
+from compmake.exceptions import CompmakeBug
+from compmake.jobs.storage import get_job_cache
+from compmake.structures import Cache
+
+
+__all__ = [
+    'parents',
+    'direct_parents',
+    'direct_children',
+    'children',
+    'top_targets', 
+    'tree',
+    'jobs_defined',
+    'definition_closure',
+]
+
+@contextmanager
+def trace_bugs(msg):
+    try:
+        yield
+    except CompmakeBug as e:
+        raise_wrapped(CompmakeBug, e, msg) 
+
+@contract(returns='set(str)')
+def jobs_defined(job_id, db):
+    """ 
+        Gets the jobs defined by the given job.
+        The job must be DONE.
+    """
+    assert isinstance(job_id, str)
+    with trace_bugs('jobs_defined(%r)' % job_id):
+        cache = get_job_cache(job_id, db=db)
+        if cache.state != Cache.DONE:
+            msg = ('Cannot get jobs_defined for job not done '
+                   +'(status: %s)' % Cache.state2desc[cache.state])
+            raise CompmakeBug(msg)
+        return set(cache.jobs_defined)
+        
+        
+@contract(jobs='Iterable', returns='set(str)')
+def definition_closure(jobs, db):
+    """ The result does not contain jobs (unless one job defines another) """
+    #print('definition_closure(%s)' % jobs)
+    check_isinstance(jobs, (list, set))
+    jobs = set(jobs)
+    from compmake.jobs.uptodate import CacheQueryDB
+    cq = CacheQueryDB(db)
+    stack = set(jobs)
+    result = set()
+    while stack:
+        #print('stack: %s' % stack)
+        a = stack.pop()
+        if cq.get_job_cache(a).state == Cache.DONE:
+            a_d = cq.jobs_defined(a)
+            #print('%s ->%s' % (a, a_d))
+            for x in a_d:
+                result.add(x)
+                stack.add(x)
+                
+    #print('  result = %s' % result)
+    return result
+
+
+def direct_parents(job_id, db):
+    """ Returns the direct parents of the specified job.
+        (Jobs that depend directly on this one) """        
+    assert isinstance(job_id, str)
+    with trace_bugs('direct_parents(%r)' % job_id):
+        computation = get_job(job_id, db=db)
+        return set(computation.parents)
+
+
+def direct_children(job_id, db):
+    """ Returns the direct children (dependencies) of the specified job """
+    assert isinstance(job_id, str)
+    with trace_bugs('direct_children(%r)' % job_id):
+        computation = get_job(job_id, db=db)
+        return set(computation.children)
+
+
+def children(job_id, db):
+    """ Returns children, children of children, etc. """
+    assert isinstance(job_id, str)
+    with trace_bugs('children(%r)' % job_id):
+        t = set()
+        for c in direct_children(job_id, db=db):
+            t.add(c)
+            t.update(children(c, db=db))
+        return t
+
+
+def top_targets(db):
+    """ Returns a list of all jobs which are not needed by anybody """
+    return [x for x in all_jobs(db=db) if not direct_parents(x, db=db)]
+
+
+# def bottom_targets(db):
+# """ Returns a list of all jobs with no dependencies. """
+# return [x for x in all_jobs(db=db) if not direct_children(x, db=db)]
+
+
+@contract(jobs='list|set')
+def tree(jobs, db):
+    """
+        Returns the tree of all dependencies of the jobs.
+        Note this is very inefficient because recursive.
+    """
+    warnings.warn('Do not use -- very inefficient')
+    t = set(jobs)
+    for job_id in jobs:
+        children_id = direct_children(job_id, db=db)
+        t = t.union(tree(children_id, db=db))
+    return t
+
+
+@contract(job_id='str')
+def parents(job_id, db):
+    """ Returns the set of all the parents, grandparents, etc.
+        (does not include job_id) """
+    assert isinstance(job_id, str), job_id
+    
+    with trace_bugs('parents(%r)' % job_id):
+        t = set()
+        parents_jobs = direct_parents(job_id, db=db)
+        for p in parents_jobs:
+            t.add(p)
+            t.update(parents(p, db=db))
+        return t
---
+++ packages/compmake/compmake-3.4.5.tar.gz/compmake-3.4.5/src/compmake/unittests/compmake_test.py
@@ -0,0 +1,139 @@
+from abc import ABCMeta
+from shutil import rmtree
+from tempfile import mkdtemp
+import os
+import unittest
+
+from compmake import set_compmake_config
+from compmake.context import Context
+from compmake.jobs import get_job, parse_job_list
+from compmake.scripts.master import compmake_main
+from compmake.storage import StorageFilesystem
+from compmake.exceptions import CommandFailed, MakeFailed
+from compmake.structures import Job
+from contracts import contract
+
+
+class CompmakeTest(unittest.TestCase):
+    __metaclass__ = ABCMeta
+
+    def setUp(self):
+        self.root0 = mkdtemp()
+        self.root = os.path.join(self.root0, 'compmake')
+        self.db = StorageFilesystem(self.root, compress=True)
+        self.cc = Context(db=self.db)
+        # don't use '\r'
+        set_compmake_config('interactive', False)
+        set_compmake_config('console_status', False)
+        from compmake.constants import CompmakeConstants
+        CompmakeConstants.debug_check_invariants = True
+        self.mySetUp()
+
+    def tearDown(self):
+        if False:
+            print('not deleting %s' % self.root0)
+        else:
+            rmtree(self.root0)
+
+    # optional init
+    # noinspection PyPep8Naming
+    def mySetUp(self):
+        pass
+
+    # useful
+    def comp(self, *args, **kwargs):
+        return self.cc.comp(*args, **kwargs)
+
+    @contract(job_id=str, returns=Job)
+    def get_job(self, job_id):
+        db = self.cc.get_compmake_db()
+        return get_job(job_id=job_id, db=db)
+
+    def get_jobs(self, expression):
+        """ Returns the list of jobs corresponding to the given expression. """
+        return list(parse_job_list(expression, context=self.cc))
+
+    def assert_cmd_success(self, cmds):
+        """ Executes the (list of) commands and checks it was succesful. """
+        try:
+            print('@ %s' % cmds)
+            
+            self.cc.batch_command(cmds)
+
+        except MakeFailed as e:
+            print('Detected MakeFailed')
+            print('Failed jobs: %s' % e.failed)
+            for job_id in e.failed:
+                self.cc.interpret_commands_wrap('details %s' % job_id)
+
+        except CommandFailed:
+            # msg = 'Command %r failed. (res=%s)' % (cmds, res)
+            raise
+
+        self.cc.interpret_commands_wrap('check_consistency raise_if_error=1')
+
+    def assert_cmd_fail(self, cmds):
+        """ Executes the (list of) commands and checks it was succesful. """
+        try:
+            self.cc.batch_command(cmds)
+        except CommandFailed:
+            pass
+        else:
+            msg = 'Command %r did not fail.' % cmds
+            raise Exception(msg)
+
+    @contract(cmd_string=str)
+    def assert_cmd_success_script(self, cmd_string):
+        """ This runs the "compmake_main" script which recreates the DB and
+        context from disk. """
+        ret = compmake_main([self.root, '--nosysexit', '-c', cmd_string])
+        self.assertEqual(ret, 0)
+
+    # useful tests
+    def assert_defined_by(self, job_id, expected):
+        self.assertEqual(self.get_job(job_id).defined_by, expected)
+
+    def assertEqualSet(self, a, b):
+        self.assertEqual(set(a), set(b))
+
+    @contract(expr=str)
+    def assertJobsEqual(self, expr, jobs, ignore_dyn_reports=True):
+
+        js = 'not-valid-yet'
+        try:
+            js = self.get_jobs(expr)
+            if ignore_dyn_reports:
+                js = [x for x in js if not 'dynreports' in x]
+            self.assertEqualSet(js, jobs)
+        except:
+            print('expr %r -> %s' % (expr, js))
+            print('differs from %s' % jobs)
+            raise
+
+    def assertMakeFailed(self, func, nfailed, nblocked):
+        try:
+            func()
+        except MakeFailed as e:
+            if len(e.failed) != nfailed:
+                msg = 'Expected %d failed, got %d: %s' % (
+                    nfailed, len(e.failed), e.failed)
+                raise Exception(msg)
+            if len(e.blocked) != nblocked:
+                msg = 'Expected %d blocked, got %d: %s' % (
+                    nblocked, len(e.blocked), e.blocked)
+                raise Exception(msg)
+        except Exception as e:
+            raise Exception('unexpected: %s' % e)
+     
+    def assert_job_uptodate(self, job_id, status):
+        res = self.up_to_date(job_id)
+        self.assertEqual(res, status, 'Want %r uptodate? %s' % (job_id, status))
+    
+    @contract(returns=bool)
+    def up_to_date(self, job_id):
+        from compmake.jobs.uptodate import CacheQueryDB
+        cq = CacheQueryDB(db=self.db)
+        up, reason, timestamp = cq.up_to_date(job_id)
+        print('up_to_date(%r): %s, %r, %s' % 
+              (job_id, up, reason, timestamp))
+        return up
