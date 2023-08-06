1ad07c0d9c907998767328bfa515b2fd4a6e56eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:21 +0000

    Add package preTeX/0.1.0/preTeX-0.1.0.zip

---
+++ packages/preTeX/preTeX-0.1.0.zip/preTeX-0.1.0/setup.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+from setuptools import setup
+setup(name='preTeX',
+      version="0.1.0",
+      packages=['pretex'],
+      author='Sebastian Werhausen',
+      author_email="swerhausen@gmail.com",
+      url='https://github.com/s9w/preTeX',
+      description="LaTeX preprocessor to simplify math typesetting",)
