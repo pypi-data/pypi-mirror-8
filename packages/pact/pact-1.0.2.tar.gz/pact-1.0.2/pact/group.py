72c01f1797174bde9155ff735bb18311a7c0b2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:01 +0000

    Add package pact/1.0.2/pact-1.0.2.tar.gz

---
+++ packages/pact/pact-1.0.2.tar.gz/pact-1.0.2/pact/group.py
@@ -0,0 +1,20 @@
+from .base import PactBase
+from .utils import GroupWaitPredicate
+
+
+class PactGroup(PactBase):
+
+    def __init__(self, pacts):
+        super(PactGroup, self).__init__()
+        self._pacts = list(pacts)
+
+    def __iadd__(self, other):
+        self._pacts.append(other)
+        return self
+
+    def _is_finished(self):
+        return all(p.finished() for p in self._pacts)
+
+    def _build_wait_predicate(self):
+        return GroupWaitPredicate(self._pacts)
+
