075e551fc5dd6cadd8db2dd5aee1054d13b54e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:06 +0000

    Add package j2hc/0.1.1/j2hc-0.1.1.tar.gz

---
+++ packages/j2hc/j2hc-0.1.1.tar.gz/j2hc-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'j2hc',
+    packages = ['j2hc'],
+    version = '0.1.1',
+    description = 'Jinja2 extension that removes whitespace between HTML tags',
+    author = 'Dmitry Dolgov',
+    author_email = '9erthalion6@gmail.com',
+    url = 'https://github.com/erthalion/jinja2-htmlcompress',
+    download_url = 'https://github.com/erthalion/jinja2-htmlcompress/tarball/0.1.1',
+    keywords = ['jinja2', 'html', 'compress'],
+    classifiers = [],
+)
