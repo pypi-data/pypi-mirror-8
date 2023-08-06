8164dc081b2c6ab9d2b7a5823fc8dc8bca2429fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:25 +0000

    Add package lip/1.0.0/lip-1.0.0.zip

---
+++ packages/lip/lip-1.0.0.zip/lip-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+          name = 'lip',
+          version = '1.0.0',
+          py_modules = ['lip'],
+          author = 'li',
+          author_email = 'hfpython@headfirslabs.com',
+          url = 'http://www.headfirstlabs.com',
+          description = 'A simple functon for list',
+     )
