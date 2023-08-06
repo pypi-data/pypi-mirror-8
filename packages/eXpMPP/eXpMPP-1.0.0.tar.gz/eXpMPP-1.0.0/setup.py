771ec9e8060ac6cb3057825818873566d081a74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package eXpMPP/1.0.0/eXpMPP-1.0.0.tar.gz

---
+++ packages/eXpMPP/eXpMPP-1.0.0.tar.gz/eXpMPP-1.0.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='eXpMPP',
+    version='1.0.0',
+    author='Louis Thibault',
+    author_email='louist87@gmail.com',
+    packages=['expmpp'],
+    include_package_data=True,
+    install_requires=['xmpppy'],
+    url='https://github.com/louist87/expmpp',
+    license='GPLv3.0',
+    description=('XMPP notifications for psychology experiments'),
+    keywords=['experiment', 'psychology', 'XMPP', 'google talk'],
+    long_description=open('README.md').read()
+)
