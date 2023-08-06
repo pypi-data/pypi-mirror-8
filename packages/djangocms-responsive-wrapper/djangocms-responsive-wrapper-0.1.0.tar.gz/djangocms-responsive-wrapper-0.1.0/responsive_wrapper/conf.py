450f9a9a17003018d0a7a38c5bff7c691eaa390b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:29 +0000

    Add package djangocms-responsive-wrapper/0.1.0/djangocms-responsive-wrapper-0.1.0.tar.gz

---
+++ packages/djangocms-responsive-wrapper/djangocms-responsive-wrapper-0.1.0.tar.gz/djangocms-responsive-wrapper-0.1.0/responsive_wrapper/conf.py
@@ -0,0 +1,20 @@
+from appconf import AppConf
+
+from django.conf import settings  # noqa
+from django.utils.translation import ugettext_lazy as _
+
+
+class ResponsiveWrapperConf(AppConf):
+    CACHE = getattr(settings, 'CMS_PLUGIN_CACHE', True)
+    CHILD_CLASSES = None
+    FIELDSETS = None
+    MODULE = _('Generic')
+    NAME = _('Responsive Wrapper')
+    PAGE_ONLY = False
+    PARENT_CLASSES = None
+    REQUIRE_PARENT = False
+    TEMPLATE = 'responsive_wrapper/default.html'
+    TEXT_ENABLED = False
+
+    class Meta:
+        prefix = 'responsive_wrapper'
