cd06a2dca927d6ec77669d5504f7a260f235097f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:19 +0000

    Add package imagen/1.0/imagen-1.0.tar.gz

---
+++ packages/imagen/imagen-1.0.tar.gz/imagen-1.0/README.txt
@@ -0,0 +1,19 @@
+ImaGen is a generic Python library for 0D, 1D, and 2D pattern
+distributions.
+
+Please see http://ioam.github.com/imagen/ for documentation and
+examples.
+
+
+Installation
+============
+
+ImaGen requires NumPy (http://numpy.scipy.org/) and Param
+(http://ioam.github.com/param/).
+
+Official releases of ImaGen are available at
+http://pypi.python.org/pypi/imagen, and can be installed along with
+dependencies via ``pip install imagen`` or ``easy_install imagen``.
+
+Alternatively, after seperately installing the dependencies, ImaGen
+can be installed via ``python setup.py install``.
---
+++ packages/imagen/imagen-1.0.tar.gz/imagen-1.0/setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup
+
+
+setup_args = {}
+
+#############################################################################################
+##### CEBALERT: copied from topographica; should be simplified
+
+required = {'param':">=1.0,<1.2",
+            'numpy':">=1.0,<1.9",
+            'PIL'  :">=1.1.6"} 
+
+# could add tkinter, paramtk
+# optional = {}
+            
+packages_to_install = [required]
+packages_to_state = [required]
+
+setup_args = {}
+
+if 'setuptools' in sys.modules:
+    # support easy_install without depending on setuptools
+    install_requires = []
+    for package_list in packages_to_install:
+        install_requires+=["%s%s"%(package,version) for package,version in package_list.items()]
+    setup_args['install_requires']=install_requires
+    setup_args['dependency_links']=["http://pypi.python.org/simple/"]
+    setup_args['zip_safe']=False # CEBALERT: probably ok for imagen; haven't checked
+    setup_args['provides']=['imagen','numbergen']
+
+for package_list in packages_to_state:
+    requires = []
+    requires+=["%s (%s)"%(package,version) for package,version in package_list.items()]
+    setup_args['requires']=requires
+
+#############################################################################################
+
+
+setup_args.update(dict(
+    name='imagen',
+    version='1.0',
+    description='Generic Python library for 0D, 1D, and 2D pattern distributions.',
+    long_description=open('README.txt').read(),
+    author= "IOAM",
+    author_email= "developers@topographica.org",
+    maintainer= "IOAM",
+    maintainer_email= "developers@topographica.org",
+    platforms=['Windows', 'Mac OS X', 'Linux'],
+    license='BSD',
+    url='http://ioam.github.com/imagen/',
+    packages = ["imagen","numbergen"],
+    classifiers = [
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries"]
+))
+
+
+if __name__=="__main__":
+    setup(**setup_args)
