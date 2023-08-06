4024d59214aa2f04a7f1b5c4b989385ce49f40ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:06 +0000

    Add package PyCross/0.3.3/PyCross-0.3.3.tar.gz

---
+++ packages/PyCross/PyCross-0.3.3.tar.gz/PyCross-0.3.3/setup.py
@@ -0,0 +1,33 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def readFile(fName):
+    with open(fName) as f:
+        lines = f.read()
+    return lines
+
+setup(
+    name = 'PyCross',
+    version = '0.3.3',
+    author = 'Sartaj Singh',
+    author_email = 'singhsartaj94@gmail.com',
+    description = ('Singleplayer/Multiplayer Tic-Tac-Toe game'),
+    long_description = readFile('README.md'),
+    license = 'MIT',
+    keywords = 'game tic-tac-toe tictactoe play',
+    url = 'http://github.com/leosartaj/PyCross',
+    packages=['PyCross', 'PyCross/gui', 'PyCross/gui', 'PyCross/gui/svg'],
+    package_data={'PyCross/gui/svg': ['*.svg'], 'PyCross/gui': ['*.glade']},
+    scripts=['bin/pycross'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: X11 Applications :: GTK',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Games/Entertainment :: Board Games',
+    ],
+)
