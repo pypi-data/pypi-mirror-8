fa7d38cfa68cc875ef9e935def6d0169367ea3ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:20 +0000

    Add package pycompilation/0.4.0/pycompilation-0.4.0.tar.gz

---
+++ packages/pycompilation/pycompilation-0.4.0.tar.gz/pycompilation-0.4.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+name_ = 'pycompilation'
+version_ = '0.4.0'
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: POSIX",
+    "Programming Language :: Python",
+    "Programming Language :: C",
+    "Programming Language :: C++",
+    "Programming Language :: Cython",
+    "Programming Language :: Fortran",
+    "Topic :: Software Development :: Code Generators",
+    "Topic :: Software Development :: Compilers",
+    "Topic :: Software Development :: Libraries :: Python Modules"
+]
+
+setup(
+    name=name_,
+    version=version_,
+    author='Björn Dahlgren',
+    author_email='bjodah@DELETEMEgmail.com',
+    description='Package for compilation (meta programming).',
+    license="BSD",
+    url='https://github.com/bjodah/'+name_,
+    download_url='https://github.com/bjodah/'+name_ +
+    '/archive/v'+version_+'.tar.gz',
+    packages=[name_],
+    classifiers=classifiers
+)
