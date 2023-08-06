5c6c97aab6432bfe70fce89fe768ff6d6e7ec8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:56 +0000

    Add package infi.rpc/0.2/infi.rpc-0.2.tar.gz

---
+++ packages/infi.rpc/infi.rpc-0.2.tar.gz/infi.rpc-0.2/src/infi/rpc/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '7aea7e23dfc8da4036719dede310f5ee1aa2b112'
+__git_head_subject__ = 'Finished release v0.2.'
+__git_head_message__ = '* release/v0.2:\n  buildout.cfg: adding Logbook>=0.8 to requirements\n  buildout.cfg: remove Logbook>=0.7.0.3 from requirements\n  add homepage\n  buildout.cfg: changed isolated python version to v2.7.6.21'
+__git_dirty_diff__ = ''
