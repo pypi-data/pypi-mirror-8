630707e45bef33ab83452a70816a5b49cab99095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package hospital/0.8.1/hospital-0.8.1.zip

---
+++ packages/hospital/hospital-0.8.1.zip/hospital-0.8.1/setup.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""Python packaging."""
+import os
+import sys
+
+from setuptools import setup
+
+
+#: Absolute path to directory containing setup.py file.
+here = os.path.abspath(os.path.dirname(__file__))
+#: Boolean, ``True`` if environment is running Python version 2.
+IS_PYTHON2 = sys.version_info[0] == 2
+
+
+NAME = 'hospital'
+DESCRIPTION = 'Framework around health checks, smoke tests and diagnoses: ' \
+              'monitor your applications and services at runtime.'
+README = open(os.path.join(here, 'README.rst')).read()
+VERSION = open(os.path.join(here, 'VERSION')).read().strip()
+AUTHOR = u'Benoît Bryon'
+EMAIL = u'benoit@marmelune.net'
+URL = 'https://{name}.readthedocs.org/'.format(name=NAME)
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+]
+KEYWORDS = [
+    'diagnosis',
+    'healthcheck',
+    'monitoring',
+    'probe',
+    'smoketest',
+    'supervision',
+    'testing',
+]
+PACKAGES = [NAME.replace('-', '_')]
+REQUIREMENTS = [
+    'setuptools>=0.7',
+    'requests',
+    'six',
+]
+if IS_PYTHON2:
+    REQUIREMENTS.extend(['mock'])
+ENTRY_POINTS = {
+    'console_scripts': [
+        'hospital-cli = hospital.cli:main',
+        'hospital-serve = hospital.wsgi:main',
+    ],
+    'pytest11': [
+        'hospital = hospital.pytest_plugin',
+    ],
+}
+
+
+if __name__ == '__main__':  # Don't run setup() when we import this module.
+    setup(name=NAME,
+          version=VERSION,
+          description=DESCRIPTION,
+          long_description=README,
+          classifiers=CLASSIFIERS,
+          keywords=' '.join(KEYWORDS),
+          author=AUTHOR,
+          author_email=EMAIL,
+          url=URL,
+          license='BSD',
+          packages=PACKAGES,
+          include_package_data=True,
+          zip_safe=False,
+          install_requires=REQUIREMENTS,
+          entry_points=ENTRY_POINTS)
