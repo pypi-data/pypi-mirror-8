5c7a71724a9ac93601d8f35bec3843c707b4493a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:07 +0000

    Add package srm_client/0.1.1/srm_client-0.1.1-py2.7.egg

---
+++ packages/srm_client/srm_client-0.1.1-py2.7.egg/srm_client/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '971217ac3fac23077ba31dee311240fba35d885f'
+__git_head_subject__ = 'Finished release v0.1.1.'
+__git_head_message__ = '* release/v0.1.1:\n  more fixes\n  fixing relative import\n  some fixes\n  purge'
+__git_dirty_diff__ = ''
