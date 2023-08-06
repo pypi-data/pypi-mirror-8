2a7bbfcb3053d86f7d7e0f4d8f41ccb1f10998cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:27 +0000

    Add package aplus/0.11.0/aplus-0.11.0.tar.gz

---
+++ packages/aplus/aplus-0.11.0.tar.gz/aplus-0.11.0/setup.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+    name="aplus",
+    version="0.11.0",
+    packages=['aplus'],
+    author=u"Michael M. Tiller",
+    author_email="michael.tiller@gmail.com"
+)
