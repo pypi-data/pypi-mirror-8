9ce824e43da712670053ca4e609c4a6dee67b58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:01 +0000

    Add package scription/0.72.03/scription-0.72.03.zip

---
+++ packages/scription/scription-0.72.03.zip/scription-0.72.03/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+
+long_desc="""\
+Scription -- simple script parameter parser
+===========================================
+
+
+"""
+
+setup( name='scription',
+       version= '0.72.03',
+       license='BSD License',
+       description='simple script parameter parser',
+       long_description=long_desc,
+       py_modules=['scription'],
+       provides=['scription'],
+       author='Ethan Furman',
+       author_email='ethan@stoneleaf.us',
+       classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: End Users/Desktop',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Topic :: Database',
+            'Programming Language :: Python :: 2.4',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            ],
+    )
+
