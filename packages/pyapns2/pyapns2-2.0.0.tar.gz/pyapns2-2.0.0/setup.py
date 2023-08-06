b32e8f0d038aa87cee6b2df0f9201d4b4a95a2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:36 +0000

    Add package pyapns2/2.0.0/pyapns2-2.0.0.tar.gz

---
+++ packages/pyapns2/pyapns2-2.0.0.tar.gz/pyapns2-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    author = 'Rohitash Kumar',
+    author_email = 'r0h1t4sh@gmail.com',
+    description = 'copy of http://github.com/simonwhitaker/PyAPNs',
+    download_url = 'https://github.com/r0h1t4sh/PyAPNs',
+    license = 'unlicense.org',
+    name = 'pyapns2',
+    py_modules = ['apns'],
+    scripts = ['apns-send'],
+    version = '2.0.0',
+)
