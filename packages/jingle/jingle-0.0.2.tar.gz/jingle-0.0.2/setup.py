70b36af130a46f437ab248ee100d479e3d3f0fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:49 +0000

    Add package jingle/0.0.2/jingle-0.0.2.tar.gz

---
+++ packages/jingle/jingle-0.0.2.tar.gz/jingle-0.0.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from glob import glob
+
+# ------------------------------------------------------------------------------
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+from jingle import __version__, __author__
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    sys.stderr.write("Setuptools are required to build this package.\n")
+    sys.stderr.write("Install it using your package manager (usually python-setuptools) or via pip (pip install setuptools).")
+    exit(1)
+
+# ------------------------------------------------------------------------------
+
+setup(
+    name="jingle",
+    version=__version__,
+    description="Config files renderer based on Jinja2",
+    author=__author__,
+    author_email="hi@kkvlk.me",
+    url="https://github.com/nimboost/jingle",
+    license="MIT",
+    install_requires=['setuptools', 'jinja2'],
+    py_modules=['jingle'],
+    scripts=['bin/jingle'],
+    keywords = "config file template jinja",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
