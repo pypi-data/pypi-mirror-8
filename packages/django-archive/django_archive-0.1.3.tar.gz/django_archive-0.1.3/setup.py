cf540bfc8c3620c8ef4704480864e3814035d21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:15 +0000

    Add package django-archive/0.1.3/django_archive-0.1.3.tar.gz

---
+++ packages/django-archive/django_archive-0.1.3.tar.gz/django_archive-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='django_archive',
+    version='0.1.3',
+    description='Management command for creating compressed archives of DB tables and uploaded media',
+    author='Nathan Osman',
+    author_email='nathan@quickmediasolutions.com',
+    url='https://github.com/nathan-osman/django-archive',
+    license='MIT',
+    packages=[
+        'django_archive',
+        'django_archive.management',
+        'django_archive.management.commands',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ],
+)
