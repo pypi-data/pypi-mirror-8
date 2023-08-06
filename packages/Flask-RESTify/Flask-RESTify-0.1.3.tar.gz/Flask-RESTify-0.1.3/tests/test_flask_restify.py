bebcbd5224c38475df7b4f51e9919cf472ab97bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:07 +0000

    Add package Flask-RESTify/0.1.3/Flask-RESTify-0.1.3.tar.gz

---
+++ packages/Flask-RESTify/Flask-RESTify-0.1.3.tar.gz/Flask-RESTify-0.1.3/tests/test_flask_restify.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_Flask-RESTify
+----------------------------------
+
+Tests for `Flask-RESTify` module.
+"""
+
+import unittest
+
+
+class FlaskRESTifyTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
