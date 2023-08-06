112715f730c8d3303734b988a81a129b596286a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:24 +0000

    Add package vulcanize/0.1.3/vulcanize-0.1.3.tar.gz

---
+++ packages/vulcanize/vulcanize-0.1.3.tar.gz/vulcanize-0.1.3/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2.7
+
+from setuptools import setup
+
+setup(
+    name = 'vulcanize',
+    packages = ['vulcanize'],
+    version = '0.1.3',
+    description = 'Vulcanizes HTML files that use Polymer',
+    author = 'Brett Slatkin',
+    author_email = 'brett@haxor.com',
+    url = 'https://github.com/bslatkin/pyvulcanize',
+    keywords = ['polymer'],
+    install_requires=['lxml'],
+    entry_points={
+        'console_scripts': [
+            'vulcanize=vulcanize.__main__:main',
+        ]
+    },
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+    long_description = """\
+Python implementation of the vulcanize tool that is part of the Polymer project.
+
+See the original at: https://github.com/Polymer/vulcanize
+
+See the Polymer project at: https://www.polymer-project.org/
+"""
+)
