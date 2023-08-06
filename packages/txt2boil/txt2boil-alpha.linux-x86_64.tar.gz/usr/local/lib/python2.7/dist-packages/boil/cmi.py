b630bde630fd5de3110fba0c7ad0864e0e22efaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:48 +0000

    Add package txt2boil/alpha/txt2boil-alpha.linux-x86_64.tar.gz

---
+++ packages/txt2boil/txt2boil-alpha.linux-x86_64.tar.gz/usr/local/lib/python2.7/dist-packages/boil/cmi.py
@@ -0,0 +1,80 @@
+"""Cooprative Multiple Inheritance (CMI) made easy.
+
+This module provides a function descriptor that transforms an ordinary
+overloaded function into a function that is compatible with CMI.  This
+allows us to mimic the Chain of Resposibility design pattern entirely
+using CMI.
+
+There are two API's.  One is the generic descripter that takes a
+binary operation and uses it to merge two answers together.  The other
+is two functions: one which locates the first non-None object and
+returns it, and the other which finds the minimum element of all the
+posibilities.
+
+Note that all the cls arguments to the descriptors must be passed
+wrapped in a lambda like so:
+
+class Foo:
+    @nonNoneCMI(lambda: Foo)
+    def bar(self):
+        pass
+
+Otherwise the name Foo won't be defined yet.
+
+"""
+
+from functools import wraps
+
+class AbstractCMI:
+
+    """A descriptor that enables easy CMI according to a function.
+
+    The final result function will be achieved by using merge as a
+    binary operation on the results of the current function and the
+    super-class' function of the same name.
+
+    """
+
+    def __init__(self, cls, merge):
+        """Initialize the abstract descriptor.
+
+        Note that cls has to be wrapped in a lambda because otherwise
+        there will be a name resolution error (since that class hasn't
+        been defined yet).
+        
+        cls   - the current class wrapped in a lambda
+        merge - the binary operator that will determine the final
+                result
+
+        """
+
+        self.cls = cls
+        self.merge = merge
+
+    def _getSuperFunc(self, s, func):
+        """Return the the super function."""
+
+        return getattr(super(self.cls(), s), func.__name__)
+
+    def __call__(self, func):
+        """Apply this abstract descriptor to func."""
+
+        @wraps(func)
+        def wrapper(s, *args, **kwargs):
+            a = func(s, *args, **kwargs)
+            b = self._getSuperFunc(s, func)(*args, **kwargs)
+            return self.merge(a, b)
+        return wrapper
+
+
+def minCMI(cls):
+    """Return an AbstractCMI that locates the minimum element."""
+
+    return AbstractCMI(cls, min)
+
+def nonNoneCMI(cls):
+    """Return an AbstractCMI that locates the first non-None element."""
+
+    return AbstractCMI(cls, lambda x, y: x if x is not None else y)
+
+__all__ = ['AbstractCMI', 'minCMI', 'nonNoneCMI']
