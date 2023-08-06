3766d09544a1040bc854e85043724852568cfc6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:24 +0000

    Add package infi.gevent-utils/0.2/infi.gevent_utils-0.2.tar.gz

---
+++ packages/infi.gevent-utils/infi.gevent_utils-0.2.tar.gz/infi.gevent_utils-0.2/src/infi/gevent_utils/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8641ccfc90f2df535d0f350fba8d91993701de72'
+__git_head_subject__ = 'SafePool'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
