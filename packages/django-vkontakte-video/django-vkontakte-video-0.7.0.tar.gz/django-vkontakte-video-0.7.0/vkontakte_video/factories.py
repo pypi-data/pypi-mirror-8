6c2379834313e112f2ee31e9b6fc7bca3c21b5dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:33 +0000

    Add package django-vkontakte-video/0.7.0/django-vkontakte-video-0.7.0.tar.gz

---
+++ packages/django-vkontakte-video/django-vkontakte-video-0.7.0.tar.gz/django-vkontakte-video-0.7.0/vkontakte_video/factories.py
@@ -0,0 +1,32 @@
+import random
+
+from django.utils import timezone
+import factory
+#from vkontakte_groups.factories import GroupFactory
+#from vkontakte_users.factories import UserFactory
+from . models import Album, Video
+
+
+class AlbumFactory(factory.DjangoModelFactory):
+
+    remote_id = factory.LazyAttributeSequence(lambda o, n: n)
+
+    videos_count = factory.LazyAttribute(lambda o: random.randint(0, 1000))
+
+    class Meta:
+        model = Album
+
+
+class VideoFactory(factory.DjangoModelFactory):
+
+    remote_id = factory.LazyAttributeSequence(lambda o, n: n)
+    album = factory.SubFactory(AlbumFactory)
+
+    duration = factory.LazyAttribute(lambda o: random.randint(0, 1000))
+    likes_count = factory.LazyAttribute(lambda o: random.randint(0, 1000))
+    comments_count = factory.LazyAttribute(lambda o: random.randint(0, 1000))
+
+    date = timezone.now()
+
+    class Meta:
+        model = Video
