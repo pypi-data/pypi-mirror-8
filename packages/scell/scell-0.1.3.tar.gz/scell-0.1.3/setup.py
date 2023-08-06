62afad78da0ad22be9c344dcf73ef030801210a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:28 +0000

    Add package scell/0.1.3/scell-0.1.3.tar.gz

---
+++ packages/scell/scell-0.1.3.tar.gz/scell-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='scell',
+    version='0.1.3',
+    description='simple wrapper atop select',
+    license='MIT',
+    author='Eugene Eeo',
+    author_email='packwolf58@gmail.com',
+    url='https://github.com/eugene-eeo/scell',
+    packages=['scell'],
+
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
