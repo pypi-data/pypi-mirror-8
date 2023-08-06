cdc933642301d52fdefdff9c20839fad83d7ea75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:07 +0000

    Add package oore/0.1.0/oore-0.1.0.tar.gz

---
+++ packages/oore/oore-0.1.0.tar.gz/oore-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+"""
+    setup
+    ~~~~~
+
+    :copyright: 2014 by Daniel Neuhäuser
+    :license: BSD, see LICENSE.rst for details
+"""
+from setuptools import setup
+
+
+setup(
+    name='oore',
+    version='0.1.0',
+    author='Daniel Neuhäuser',
+    author_email='ich@danielneuhaeuser.de',
+    url='https://github.com/DasIch/oore',
+
+    py_modules=['oore'],
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
+)
