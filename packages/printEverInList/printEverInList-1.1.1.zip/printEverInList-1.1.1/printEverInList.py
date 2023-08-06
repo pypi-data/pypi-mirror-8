646807d073ae3ad572506fc12777eb07ed977ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:57 +0000

    Add package printEverInList/1.1.1/printEverInList-1.1.1.zip

---
+++ packages/printEverInList/printEverInList-1.1.1.zip/printEverInList-1.1.1/printEverInList.py
@@ -0,0 +1,11 @@
+"""这是我的第一个python共享模块，它只是一个简单的递归打印数组里的各项内容"""
+"""参数Lists为列表，level为打印每个列表内容缩进制表符数"""
+def print_ever_in_list(Lists,level=0):
+    for List in Lists:
+        if isinstance(List,list):
+            print_ever_in_list(List,level+1)
+        else:
+            for num in range(level):
+                print("\t",end='')
+            print(List)
+"""就是一个递归而已"""
