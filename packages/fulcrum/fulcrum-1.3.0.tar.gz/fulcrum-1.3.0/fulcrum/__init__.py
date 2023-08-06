2f305d5ae6358bd15aa91bc5fd561440e1c95e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:09 +0000

    Add package fulcrum/1.3.0/fulcrum-1.3.0.tar.gz

---
+++ packages/fulcrum/fulcrum-1.3.0.tar.gz/fulcrum-1.3.0/fulcrum/__init__.py
@@ -0,0 +1,24 @@
+from fulcrum.api import APIConfig
+from fulcrum.api.endpoints import (Forms, Records, Webhooks, Photos,
+                                   Memberships, Roles, ChoiceLists,
+                                   ClassificationSets, Projects, Videos,
+                                   Changesets, ChildRecords)
+
+__version__ = '1.3.0'
+
+
+class Fulcrum(object):
+    def __init__(self, key, uri='https://api.fulcrumapp.com'):
+        api_config = APIConfig(key=key, uri=uri)
+        self.forms = Forms(api_config=api_config)
+        self.records = Records(api_config=api_config)
+        self.webhooks = Webhooks(api_config=api_config)
+        self.photos = Photos(api_config=api_config)
+        self.memberships = Memberships(api_config=api_config)
+        self.roles = Roles(api_config=api_config)
+        self.choice_lists = ChoiceLists(api_config=api_config)
+        self.classification_sets = ClassificationSets(api_config=api_config)
+        self.projects = Projects(api_config=api_config)
+        self.videos = Videos(api_config=api_config)
+        self.changesets = Changesets(api_config=api_config)
+        self.child_records = ChildRecords(api_config=api_config)
