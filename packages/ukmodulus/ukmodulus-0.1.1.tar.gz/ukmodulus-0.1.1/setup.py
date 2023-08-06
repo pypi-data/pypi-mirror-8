2f8e0caa47bed126756100dd34d2b7dad6d6c37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:27 +0000

    Add package ukmodulus/0.1.1/ukmodulus-0.1.1.tar.gz

---
+++ packages/ukmodulus/ukmodulus-0.1.1.tar.gz/ukmodulus-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='ukmodulus',
+    packages=['ukmodulus'],
+    version='0.1.1',
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.rst'],
+    },
+    description='Implementation of VocaLinks UK Modulus checking for UK account numbers and sort codes',
+    author='Andrew Barrett',
+    author_email='bazerk+pypy@gmail.com',
+    url='https://github.com/bazerk/uk-modulus-checking',
+    download_url='https://github.com/bazerk/uk-modulus-checking/tarball/0.1',
+    keywords=['uk', 'banking', 'validation'],
+    classifiers=[],
+)
