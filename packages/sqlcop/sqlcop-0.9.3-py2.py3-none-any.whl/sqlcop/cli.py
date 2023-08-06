a1fe36707962fc81662e52b2e01113a730024ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:57 +0000

    Add package sqlcop/0.9.3/sqlcop-0.9.3-py2.py3-none-any.whl

---
+++ packages/sqlcop/sqlcop-0.9.3-py2.py3-none-any.whl/sqlcop/cli.py
@@ -0,0 +1,66 @@
+import sys
+import sqlparse
+import optparse
+from .checks import CrossJoinCheck
+
+
+def parse_file(filename):
+    return open(filename, 'r').readlines()
+
+
+CHECKS = (
+    (CrossJoinCheck, 'query contains cross join'),
+)
+
+
+def check_query(options, el):
+    """
+    Run each of the defined checks on a query.
+    """
+    stmt = sqlparse.parse(el)
+    checks = (
+        (check_class(**options), message)
+        for check_class, message in CHECKS
+    )
+    for check in checks:
+        if check[0](stmt[0]):
+            return False, check[1]
+    return True, ''
+
+
+def main():
+    parser = optparse.OptionParser('sqlcop')
+    parser.add_option(
+        '--db-urls',
+        help=(
+            'Comma-separated db urls. '
+            'Used to fetch schema for the database so sqlcop '
+            'can make more accurate judgement based on the schema'
+        )
+    )
+    opt, args = parser.parse_args()
+    if len(args) < 1:
+        parser.error('SQL file required')
+
+    queries = parse_file(args[0])
+
+    db_urls = opt.db_urls.split(',')
+    if not isinstance(db_urls, list):
+        db_urls = list(db_urls)
+
+    failed = False
+    options = {'db_urls': db_urls}
+    for query in queries:
+        passed, message = check_query(options, query)
+        if not passed:
+            failed = True
+            print_message(message, query)
+    sys.exit(255 if failed else 0)
+
+
+def print_message(message, query):
+    print "FAILED - %s" % (message)
+    print "-" * 70
+    print
+    print "Query:"
+    print "%s" % query
