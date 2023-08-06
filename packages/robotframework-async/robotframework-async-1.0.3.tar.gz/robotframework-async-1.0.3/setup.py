e6ae374c7d88e52abad3607e68ad7cc9fabefa73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:21 +0000

    Add package robotframework-async/1.0.3/robotframework-async-1.0.3.tar.gz

---
+++ packages/robotframework-async/robotframework-async-1.0.3.tar.gz/robotframework-async-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name = "robotframework-async",
+    version = "1.0.3",
+    description = "Generic Robot Framework library for asynchronous keyword execution",
+    author = "Fredrik Reveny",
+    author_email = "chetic@gmail.com",
+    license = "MIT",
+    url = "https://github.com/Chetic/robotframework-async",
+    download_url = "https://github.com/Chetic/robotframework-async",
+    keywords = ["async", "robotframework"],
+    install_requires = ["robotframework >= 2.8.6"],
+    packages = ["AsyncLibrary"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+)
