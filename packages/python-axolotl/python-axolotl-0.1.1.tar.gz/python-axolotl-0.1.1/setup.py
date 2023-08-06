b3fc2c254536ff1beced4727227cdfe549ca10da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:49 +0000

    Add package python-axolotl/0.1.1/python-axolotl-0.1.1.tar.gz

---
+++ packages/python-axolotl/python-axolotl-0.1.1.tar.gz/python-axolotl-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+deps = ["protobuf", "pycrypto", "python-axolotl-curve25519"]
+
+setup(
+    name='python-axolotl',
+    version="0.1.1",
+    packages= find_packages(),
+    install_requires = deps,
+    license='GPLv3 License',
+    author='Tarek Galal',
+    author_email='tare2.galal@gmail.com',
+    description="Python port of libaxolotl-android, originally written by Moxie Marlinspik",
+    platforms='any'
+)
