fa38268f87e4c8fc6f9bfa8d265f8d8418fc07ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package binstar-build/0.9.5/binstar-build-0.9.5.tar.gz

---
+++ packages/binstar-build/binstar-build-0.9.5.tar.gz/binstar-build-0.9.5/binstar_build_client/scripts/build.py
@@ -0,0 +1,38 @@
+'''
+Binstar Build command
+
+Initialize the build directory:
+
+    binstar-build init
+
+This will create a default .binstar.yml file in the current directory
+
+Submit a build:
+
+    binstar-build submit
+
+Tail the output of a build until it is complete:
+
+    binstar-build tail user/package 1.0
+
+'''
+
+from __future__ import (print_function, unicode_literals, division,
+    absolute_import)
+
+import logging
+
+from binstar_build_client import __version__ as version
+from binstar_build_client import commands
+from binstar_client.scripts.cli import binstar_main
+
+
+logger = logging.getLogger('binstar')
+
+def main(args=None, exit=True):
+    return binstar_main(commands, args, exit,
+                        description=__doc__, version=version)
+
+
+if __name__ == '__main__':
+    main()
