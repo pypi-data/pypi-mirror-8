6706fafa8b6d950ef0235b9cb83299d380ad6207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:22 +0000

    Add package createcoverage/1.3.1/createcoverage-1.3.1.zip

---
+++ packages/createcoverage/createcoverage-1.3.1.zip/createcoverage-1.3.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+version = '1.3.1'
+
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CREDITS.rst').read(),
+    open('CHANGES.rst').read(),
+    ])
+
+install_requires = [
+    'setuptools',
+    'coverage >= 3.4',
+    ],
+
+tests_require = [
+    ]
+
+setup(name='createcoverage',
+      version=version,
+      description="Single command to create coverage reports (assumes a bin/test)",
+      long_description=long_description,
+      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[],
+      keywords=[],
+      author='Reinout van Rees',
+      author_email='reinout@vanrees.org',
+      url='https://pypi.python.org/pypi/createcoverage',
+      license='GPL',
+      packages=['createcoverage'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require = {'test': tests_require},
+      entry_points={
+          'console_scripts': [
+              'createcoverage = createcoverage.script:main',
+              # Hack to make sure bin/coverage exists when we don't otherwise
+              # explicitly tell buildout to create the script for this.
+              'coverage = coverage:main',
+          ]},
+      )
