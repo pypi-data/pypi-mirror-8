48edb559fdd58c0d9df1efbeaa8682c1a24731b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:58 +0000

    Add package py-web-search/0.1.1.3/py-web-search-0.1.1.3.tar.gz

---
+++ packages/py-web-search/py-web-search-0.1.1.3.tar.gz/py-web-search-0.1.1.3/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+ 
+setup(
+    name = 'py-web-search',
+    packages = ['pws'],
+    version = '0.1.1.3',
+    description = 'A Python module to fetch and parse results from different search engines.',
+    author='Rohith PR',
+    author_email='praroh2@gmail.com',
+    url='https://github.com/rohithpr/py-web-search',
+    download_url = 'https://github.com/rohithpr/py-web-search/tarball/0.1.1.3',
+    keywords = ['web search', 'web scraping'],
+    classifiers=['Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research'],
+)
