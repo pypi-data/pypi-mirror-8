3ef403e62f8340efcf5b8a33c335b94f194636ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:15 +0000

    Add package helga-pull-requests/0.0.4/helga-pull-requests-0.0.4.tar.gz

---
+++ packages/helga-pull-requests/helga-pull-requests-0.0.4.tar.gz/helga-pull-requests-0.0.4/README.rst
@@ -0,0 +1,20 @@
+helga-pull-requests
+===================
+
+A helga plugin to quickly link to github pull requests. This will match either the pattern
+<repo_name>-pr<number> (when PULL_REQUESTS_DEFAULT_ACCOUNT is set) or <account>/<repo_name>-pr<number>.
+
+Settings
+--------
+
+**PULL_REQUESTS_DEFAULT_ACCOUNT** A default account to use for empty matches of <repo_name>-pr<number>
+**PULL_REQUESTS_GITHUB_HOST** A url to use for github. Defaults to `https://github.com`.
+
+License
+-------
+
+Copyright (c) 2014 Shaun Duncan
+
+Licensed under an `MIT`_ license.
+
+.. _`MIT`: https://github.com/shaunduncan/helga-pull-requests/blob/master/LICENSE
---
+++ packages/helga-pull-requests/helga-pull-requests-0.0.4.tar.gz/helga-pull-requests-0.0.4/helga_pull_requests.py
@@ -0,0 +1,20 @@
+from helga import settings
+from helga.plugins import match
+
+
+@match(r'(([\w\-\.]+?)/)?([\w\-\.]+?)-pr([\d]+)')
+def pull_requests(client, channel, nick, message, matches):
+    prs = []
+    for _, org, repo, pr in matches:
+        org = org or getattr(settings, 'PULL_REQUESTS_DEFAULT_ACCOUNT', '')
+        host = getattr(settings, 'PULL_REQUESTS_GITHUB_HOST', 'https://github.com')
+
+        if not org:
+            continue
+
+        prs.append(u'{host}/{org}/{repo}/pull/{pr}'.format(host=host, org=org, repo=repo, pr=pr))
+
+    if not prs:
+        return None
+
+    return u'{0} might be talking about pull request: {1}'.format(nick, ', '.join(prs))
