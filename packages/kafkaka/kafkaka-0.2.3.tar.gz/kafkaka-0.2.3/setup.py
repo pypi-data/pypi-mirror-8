7fd92940c34e455bcc06675053dcbcccdf1f7e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:53 +0000

    Add package kafkaka/0.2.3/kafkaka-0.2.3.tar.gz

---
+++ packages/kafkaka/kafkaka-0.2.3.tar.gz/kafkaka-0.2.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='kafkaka',
+      version='0.2.3',
+      description='Kafka Client with smarter protocol described and adapter for Gevent',
+      author='Du Wei',
+      author_email='pandorid@gmail.com',
+      url='https://github.com/wesdu/kafkaka',
+      license="Apache License 2.0",
+      classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: Chinese (Traditional)',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='Kafka,bstruct,gevent,client',
+      packages=['kafkaka'],
+      package_dir={'kafkaka':'kafkaka'},
+      package_data={'kafkaka':['*.*']}, requires=['gevent']
+)
