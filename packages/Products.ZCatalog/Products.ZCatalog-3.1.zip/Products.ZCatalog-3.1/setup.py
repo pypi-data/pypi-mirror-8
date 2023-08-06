3d657df52ebc85e66da3e0f3cfb6e041a2f2a686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:00 +0000

    Add package Products.ZCatalog/3.1/Products.ZCatalog-3.1.zip

---
+++ packages/Products.ZCatalog/Products.ZCatalog-3.1.zip/Products.ZCatalog-3.1/setup.py
@@ -0,0 +1,64 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Products.ZCatalog',
+    version='3.1',
+    url='http://pypi.python.org/pypi/Products.ZCatalog',
+    license='ZPL 2.1',
+    description="Zope 2's indexing and search solution.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=open('README.txt').read() + '\n' +
+                     open('CHANGES.txt').read(),
+    packages=find_packages('src'),
+    namespace_packages=['Products'],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2 :: Only",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    install_requires=[
+        'setuptools',
+        'AccessControl',
+        'Acquisition',
+        'DateTime',
+        'DocumentTemplate',
+        'ExtensionClass',
+        'Missing',
+        'Persistence',
+        'Products.ZCTextIndex',
+        'Record',
+        'RestrictedPython',
+        'zExceptions',
+        'ZODB3',
+        'Zope2',
+        'zope.dottedname',
+        'zope.interface',
+        'zope.schema',
+        'zope.testing',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
