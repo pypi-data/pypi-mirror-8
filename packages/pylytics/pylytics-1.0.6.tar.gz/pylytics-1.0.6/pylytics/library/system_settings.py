c142d23c005ba133144c8ad3b3fbd624aff82d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:28 +0000

    Add package pylytics/1.0.6/pylytics-1.0.6.tar.gz

---
+++ packages/pylytics/pylytics-1.0.6.tar.gz/pylytics-1.0.6/pylytics/library/system_settings.py
@@ -0,0 +1,4 @@
+# For now the batch size is hardcoded (i.e. the number of rows inserted per
+# insert statement). Eventually this will be dynamically sized based on the 
+# max packet size.
+BATCH_SIZE = 1000
