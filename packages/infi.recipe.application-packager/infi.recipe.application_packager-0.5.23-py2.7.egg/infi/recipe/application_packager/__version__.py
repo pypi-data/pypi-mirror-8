ab7f92fadd9b24fc76e8610920832bde0881b428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:20 +0000

    Add package infi.recipe.application-packager/0.5.23/infi.recipe.application_packager-0.5.23-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.5.23-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.5.23"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.5.23)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'adfc1e543aafea94c1c8622dcba613b91d14af09'
+__git_head_subject__ = 'Finished release v0.5.23.'
+__git_head_message__ = '* release/v0.5.23:\n  HOSTDEV-1922 fixing NameError in previous commit\n  HOSTDEV-1922 refactoring the test, hopefully it will pass on Ubuntu now\n  HOSTDEV-1922 logging stdout\n  HOSTDEV-1922 fixing NameError in previous commit\n  HOSTDEV-1922 adding debug messages\n  Revert "HOSTDEV-1922 fixing the test on Ubuntu"\n  HOSTDEV-1922 fixing the test on Ubuntu\n  HOSTDEV-1922 fixing the test on Windows\n  TRIVIAL making the tests pass\n  HOSTDEV-1922 adding a test and a fix for the case where close-on-upgrade-or-removal=false\n  fix tests\n  fix tests\n  add missing custom_installdir\n  HOSTDEV-1885 /archives on python.infinidat.com is obsolete\n  try to solve template loading failure\n  HOSTDEV-1839 move the msi/wix modules from lxml to jinja2 and remove the lxml dependency (WIP)\n  TRIVIAL fix typo\n  HIPVM-602 ditch lxml, use jinja2 and xml instead'
+__git_dirty_diff__ = ''
