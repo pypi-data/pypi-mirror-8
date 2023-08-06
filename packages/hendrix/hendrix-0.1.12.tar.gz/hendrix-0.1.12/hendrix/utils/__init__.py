72b3574cfa5d8cff6f331f746ae14bf2d8d9ebd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:21 +0000

    Add package hendrix/0.1.12/hendrix-0.1.12.tar.gz

---
+++ packages/hendrix/hendrix-0.1.12.tar.gz/hendrix-0.1.12/hendrix/utils/__init__.py
@@ -0,0 +1,40 @@
+import chalk
+import os
+import sys
+from path import path
+
+
+HENDRIX_DIR = path(__file__).abspath().dirname()
+
+SHARE_PATH = os.path.join(
+    os.path.dirname(sys.executable),
+    'share/hendrix'
+)
+
+
+def get_pid(options):
+    """returns The default location of the pid file for process management"""
+    namespace = options['settings'] if options['settings'] else options['wsgi']
+    return '%s/%s_%s.pid' % (
+        HENDRIX_DIR, options['http_port'], namespace.replace('.', '_')
+    )
+
+
+def responseInColor(request, status, headers, prefix='Response', opts=None):
+    "Prints the response info in color"
+    code, message = status.split(None, 1)
+    message = '%s [%s] => Request %s %s %s on pid %d' % (
+        prefix,
+        code,
+        str(request.host),
+        request.method,
+        request.path,
+        os.getpid()
+    )
+    signal = int(code)/100
+    if signal == 2:
+        chalk.green(message, opts=opts)
+    elif signal == 3:
+        chalk.blue(message, opts=opts)
+    else:
+        chalk.red(message, opts=opts)
