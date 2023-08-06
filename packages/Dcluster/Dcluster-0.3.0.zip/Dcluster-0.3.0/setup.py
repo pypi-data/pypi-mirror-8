4e907e33dbf19b03dad7128329abe12fda4bfbbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:09 +0000

    Add package Dcluster/0.3.0/Dcluster-0.3.0.zip

---
+++ packages/Dcluster/Dcluster-0.3.0.zip/Dcluster-0.3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'Dcluster',
+    packages = ['Dcluster'], # this must be the same as the name above
+    version = '0.3.0',
+    description = 'A Python package for Clustering by fast search and find of density peaks',
+    author = 'Guipeng Li',
+    author_email = 'guipenglee@gmail.com',
+    url = 'https://github.com/GuipengLi/Dcluster',   # use the URL to the github repo
+    keywords = ['Cluster', 'fast', 'density'], # arbitrary keywords
+    classifiers = [],
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    install_requires=["numpy >= 1.2.0", "matplotlib >= 1.3.0"],
+)
