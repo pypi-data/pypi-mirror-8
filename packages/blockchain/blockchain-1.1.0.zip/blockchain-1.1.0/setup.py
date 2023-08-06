be1099bc13472390fecf5e3dde86cefb7789031e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:00 +0000

    Add package blockchain/1.1.0/blockchain-1.1.0.zip

---
+++ packages/blockchain/blockchain-1.1.0.zip/blockchain-1.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='blockchain',
+      version='1.1.0',
+      description='Blockchain API library (v1)',
+      author='Blockchain.info',
+      author_email='support@blockchain.zendesk.com',
+      url='https://github.com/blockchain/api-v1-client-python',
+      license='MIT',
+      packages=['blockchain'],
+      keywords='blockchain.info api blockchain',
+      )
