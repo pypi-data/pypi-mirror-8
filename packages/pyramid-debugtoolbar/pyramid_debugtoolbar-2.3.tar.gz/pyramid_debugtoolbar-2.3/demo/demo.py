887cb67a4b904bdea260424fee3512643bd5b592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:08 +0000

    Add package pyramid-debugtoolbar/2.3/pyramid_debugtoolbar-2.3.tar.gz

---
+++ packages/pyramid-debugtoolbar/pyramid_debugtoolbar-2.3.tar.gz/pyramid_debugtoolbar-2.3/demo/demo.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import logging
+import shutil
+
+from pyramid.config import Configurator
+from pyramid.httpexceptions import HTTPFound
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.session import UnencryptedCookieSessionFactoryConfig
+from pyramid.view import view_config
+from wsgiref.simple_server import make_server
+
+try:
+    import sqlalchemy
+except ImportError: # pragma: no cover
+    sqlalchemy = None
+
+# True if we are running on Python 3.
+PY3 = sys.version_info[0] == 3
+
+if PY3: # pragma: no cover
+    binary_type = bytes
+else:
+    binary_type = str
+
+def text_(s, encoding='latin-1', errors='strict'):
+    """ If ``s`` is an instance of ``binary_type``, return
+    ``s.decode(encoding, errors)``, otherwise return ``s``"""
+    if isinstance(s, binary_type):
+        return s.decode(encoding, errors)
+    return s # pragma: no cover
+
+logging.basicConfig(level=logging.NOTSET)
+log = logging.getLogger(__file__)
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+@view_config(route_name='test_exc')
+def exc(request):
+    raise NotImplementedError
+
+@view_config(route_name='test_notfound')
+def notfound(request):
+    raise HTTPNotFound()
+
+@view_config(route_name='test_ajax', renderer='__main__:templates/ajax.mako')
+def test_ajax(request):
+    return {}
+
+@view_config(route_name='call_ajax', renderer='json')
+def call_ajax(request):
+    return {'ajax':'success'}
+
+@view_config(context=HTTPNotFound, renderer='__main__:templates/notfound.mako')
+def notfound_view(request):
+    request.response.status_code = 404
+    return {}
+
+@view_config(renderer='__main__:templates/index.mako') # found via traversal
+def test_page(request):
+    title = 'Pyramid Debugtoolbar'
+    log.info(title)
+    return {
+        'title': title,
+        'show_jinja2_link': True,
+        'show_sqla_link': bool(sqlalchemy)}
+
+@view_config(route_name='test_redirect')
+def test_redirect(request):
+    return HTTPFound(location='/')
+
+@view_config(route_name='test_highorder',
+             renderer='__main__:templates/highorder.mako')
+def test_highorder(request):
+    return {}
+
+@view_config(route_name='test_predicates',
+             renderer='__main__:templates/index.mako')
+def test_predicates(request):
+    return {'title': 'Test route predicates'}
+
+@view_config(route_name='test_chameleon_exc',
+             renderer='__main__:templates/error.pt')
+@view_config(route_name='test_mako_exc',
+             renderer='__main__:templates/error.mako')
+@view_config(route_name='test_jinja2_exc',
+             renderer='__main__:templates/error.jinja2')
+def test_template_exc(request):
+    return {'title': 'Test template exceptions'}
+
+class DummyRootFactory(object):
+    def __init__(self, request):
+        self.request = request
+    def __getitem__(self, name):
+        return self
+
+if __name__ == '__main__':
+    # configuration settings
+    try:
+        # ease testing py2 and py3 in same directory
+        shutil.rmtree(os.path.join(here, 'mako_modules'))
+    except:
+        pass
+    settings = {}
+    settings['reload_templates'] = True
+    settings['mako.directories'] = '__main__:templates'
+    settings['mako.module_directory'] = '__main__:mako_modules'
+    settings['debugtoolbar.reload_templates'] = True
+    settings['debugtoolbar.hosts'] = ['127.0.0.1']
+    settings['debugtoolbar.intercept_redirects'] = True
+    # session factory
+    session_factory = UnencryptedCookieSessionFactoryConfig('itsaseekreet')
+    # configuration setup
+    config = Configurator(settings=settings, session_factory=session_factory,
+                          root_factory=DummyRootFactory)
+    # static view
+    config.add_static_view('static', os.path.join(here, 'static'))
+    # routes setup
+    config.add_route('test_redirect', '/redirect')
+    config.add_route('test_predicates', '/predicates', request_method='GET')
+    config.add_route('test_exc', '/exc')
+    config.add_route('test_notfound', '/notfound')
+    config.add_route('test_chameleon_exc', '/chameleon_exc')
+    config.add_route('test_mako_exc', '/mako_exc')
+    config.add_route('test_jinja2_exc', '/jinja2_exc')
+    config.add_route('test_highorder', text_(b'/La Pe\xc3\xb1a', 'utf-8'))
+    config.add_route('test_ajax', '/ajax')
+    config.add_route('call_ajax', '/call_ajax')
+    config.scan('__main__')
+    config.include('pyramid_chameleon')
+    config.include('pyramid_jinja2')
+    config.include('pyramid_mako')
+
+    if sqlalchemy:
+        config.include('sqla')
+    config.include('pyramid_debugtoolbar')
+    app = config.make_wsgi_app()
+    httpd = make_server('', 8080, app)
+    httpd.serve_forever()
---
+++ packages/pyramid-debugtoolbar/pyramid_debugtoolbar-2.3.tar.gz/pyramid_debugtoolbar-2.3/setup.py
@@ -0,0 +1,85 @@
+##############################################################################
+#
+# Copyright (c) 2008-2013 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires = [
+    'pyramid>=1.2dev',
+    'pyramid_mako>=0.3.1', # lazy configuration loading works
+    'repoze.lru',
+    ]
+
+# Pygments 2.0 dropped 3.2 support
+if (sys.version_info[0], sys.version_info[1]) == (3, 2):
+    install_requires.append('Pygments<=1.99')
+else:
+    install_requires.append('Pygments')
+
+testing_extras = [
+    'nose',
+    'coverage',
+    ]
+
+docs_extras = [
+    'Sphinx',
+    ]
+
+setup(name='pyramid_debugtoolbar',
+      version='2.3',
+      description=('A package which provides an interactive HTML debugger '
+                   'for Pyramid application development'),
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "License :: Repoze Public License",
+        ],
+      keywords='wsgi pylons pyramid transaction',
+      author=("Chris McDonough, Michael Merickel, Casey Duncan, "
+              "Blaise Laflamme"),
+      author_email="pylons-devel@googlegroups.com",
+      url="http://docs.pylonsproject.org/projects/pyramid-debugtoolbar/en/latest/",
+      license="BSD",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require = {
+          'testing':testing_extras,
+          'docs':docs_extras,
+          },
+      package_data={'pyramid_debugtoolbar': ['static/css/*', 'static/font/*',
+          'static/img/*', 'static/js/*', 'templates/*', 'panels/templates/*']
+          },
+      test_suite="pyramid_debugtoolbar",
+      entry_points='',
+      )
