2e3b35505deb525a29bc1f7e8156035f5c5988c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:08 +0000

    Add package opengeode/1.0RC/opengeode-1.0RC.tar.gz

---
+++ packages/opengeode/opengeode-1.0RC.tar.gz/opengeode-1.0RC/opengeode/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    This module is a free SDL editor.
+    It allows to graphically design state machines, using a formal, well-
+    defined language, and generate Ada code from the models.
+"""
+
+
+from opengeode import opengeode, __version__
