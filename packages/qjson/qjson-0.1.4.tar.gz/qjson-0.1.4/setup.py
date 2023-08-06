fb69dde9ba8875f16b6c94f4965d38984ab5d310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:08 +0000

    Add package qjson/0.1.4/qjson-0.1.4.tar.gz

---
+++ packages/qjson/qjson-0.1.4.tar.gz/qjson-0.1.4/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import qjson
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+history = 'first upload to pypi'
+
+
+setup(
+    name='qjson',
+
+    description='quick and dirty way to convert json string to python object',
+    long_description=readme + '\n\n' + history,
+
+    url='https://github.com/jatsz/qjson',
+    download_url='https://github.com/jatsz/qjson/tarball/master',
+
+    version=qjson.__version__,
+    license=qjson.__license__,
+
+    author=qjson.__author__,
+    author_email='imjatsz@gmail.com',
+
+    packages=['qjson'],
+
+    classifiers=[],
+
+    keywords = "json",
+
+)
