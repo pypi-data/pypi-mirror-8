de09b35b0ad889a8fe7b1cf496bc4a2730c789bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:57 +0000

    Add package artemis/0.1.3/artemis-0.1.3.zip

---
+++ packages/artemis/artemis-0.1.3.zip/artemis-0.1.3/artemis/__init__.py
@@ -0,0 +1,6 @@
+
+from schematics import types
+from tinydb import where
+
+from web import ArtemisApplication
+from models import ArtemisModel, restful
---
+++ packages/artemis/artemis-0.1.3.zip/artemis-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='artemis',
+    version='0.1.3',
+    packages=['artemis'],
+    url='',
+    license='',
+    install_requires=["six", "tornado>=4.0", "schematics>=1.0", "tinydb>=2.1.0"],
+    author='oakfang',
+    description='Async Restful TinyDB Models'
+)
