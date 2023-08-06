318f87b8584739ec08db8ff3069596032c7cce94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:58 +0000

    Add package slacky/0.1.1/slacky-0.1.1.tar.gz

---
+++ packages/slacky/slacky-0.1.1.tar.gz/slacky-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='slacky',
+    version="0.1.1",
+    description="Package for Slack's API",
+    author='Mao Nabeta',
+    author_email='mao.nabeta@gmail.com',
+    url='https://github.com/nabetama/slacky',
+    packages=['slacky'],
+    install_requires=['requests', 'six'],
+    provides=['slack'],
+    keywords='slack',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Communications :: Chat',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
+    )
