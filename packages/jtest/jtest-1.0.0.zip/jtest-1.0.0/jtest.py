cc833731741993bac43e09eeb746a22b4944ed5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:53 +0000

    Add package jtest/1.0.0/jtest-1.0.0.zip

---
+++ packages/jtest/jtest-1.0.0.zip/jtest-1.0.0/jtest.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+def jtest():
+    return "jtest"
+
+if __name__ == "__main__":
+    print jtest()
---
+++ packages/jtest/jtest-1.0.0.zip/jtest-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup (
+       name = "jtest",
+       version = "1.0.0",
+       description = "This is a test",
+       py_modules = ['jtest'],
+       author = 'simapple',
+       author_email = 'simapple@live.com',
+      )
