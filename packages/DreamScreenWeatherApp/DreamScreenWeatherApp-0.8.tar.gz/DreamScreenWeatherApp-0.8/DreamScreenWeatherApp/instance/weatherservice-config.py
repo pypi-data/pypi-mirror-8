8134654b4bb1a2880cb6554952b76dd8a4aa849b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:15 +0000

    Add package DreamScreenWeatherApp/0.8/DreamScreenWeatherApp-0.8.tar.gz

---
+++ packages/DreamScreenWeatherApp/DreamScreenWeatherApp-0.8.tar.gz/DreamScreenWeatherApp-0.8/DreamScreenWeatherApp/instance/weatherservice-config.py
@@ -0,0 +1,21 @@
+DEBUG=True
+
+# MANDATORY DATA URL
+# weather info - keys must be in upper case!!!!
+QUERY_URL='http://www.wunderground.com/weather-forecast/zmw:10308.1.99999'
+#QUERY_URL='http://www.wunderground.com/personal-weather-station/dashboard?ID=KNYSTATE9'
+
+API_URL='http://api.wunderground.com/api/'
+API_KEY='663ad86d0906f344'
+API_FORECAST='/forecast10day/q/' 
+API_CONDITIONS='/conditions/q/' 
+PWS='KNYSTATE9'
+FORMAT='json'
+
+# these are optional simply to adjust the display of city, state and zip
+ZIPCODE=10308
+CITY='Staten Island'
+STATE='NY'
+COUNTRY='USA'
+
+
