5afb0f810ee64149ffe459d2c9acf83cdb88b777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:48 +0000

    Add package compmake/3.4.2/compmake-3.4.2.tar.gz

---
+++ packages/compmake/compmake-3.4.2.tar.gz/compmake-3.4.2/src/compmake/jobs/result_dict.py
@@ -0,0 +1,42 @@
+from contracts import check_isinstance
+
+__all__ = [
+    'result_dict_check',
+    'result_dict_raise_if_error',
+]
+
+
+def result_dict_check(res):
+    check_isinstance(res, dict)
+    if 'new_jobs' in res:
+        msg = 'Invalid result dict: %r' % res
+        assert 'new_jobs' in res, msg
+        assert 'deleted_jobs' in res, msg
+        assert 'user_object_deps' in res, msg
+        
+        
+    elif 'fail' in res:
+        pass
+    elif 'bug' in res:
+        pass
+    elif 'abort' in res:
+        pass
+    else:
+        msg = 'Malformed result dict: %s' % res
+        raise ValueError(msg)
+
+
+def result_dict_raise_if_error(res):
+    from compmake.exceptions import JobFailed
+    from compmake.exceptions import HostFailed, CompmakeBug
+
+    result_dict_check(res)
+
+    if 'fail' in res:
+        raise JobFailed.from_dict(res)
+
+    if 'abort' in res:
+        raise HostFailed.from_dict(res)
+
+    if 'bug' in res:
+        raise CompmakeBug.from_dict(res)
