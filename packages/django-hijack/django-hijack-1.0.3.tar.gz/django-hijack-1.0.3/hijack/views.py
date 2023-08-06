0081601e208d0acabc7d302a7c786c7fb7e67e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:40 +0000

    Add package django-hijack/1.0.3/django-hijack-1.0.3.tar.gz

---
+++ packages/django-hijack/django-hijack-1.0.3.tar.gz/django-hijack-1.0.3/hijack/views.py
@@ -0,0 +1,42 @@
+from django.contrib.admin.views.decorators import staff_member_required
+
+
+from django.contrib.auth.decorators import login_required
+
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
+
+
+from hijack.helpers import login_user
+from hijack.helpers import release_hijack as release_hijack_fx
+from compat import get_user_model
+
+@staff_member_required
+def login_with_id(request, userId):
+    if isinstance(userId, int):
+        return HttpResponseBadRequest('userId must be an integer value.')
+    user = get_object_or_404(get_user_model(), pk=userId)
+    return login_user(request, user)
+
+
+@staff_member_required
+def login_with_email(request, email):
+    user = get_object_or_404(get_user_model(), email=email)
+    return login_user(request, user)
+
+
+@staff_member_required
+def login_with_username(request, username):
+    user = get_object_or_404(get_user_model(), username=username)
+    return login_user(request, user)
+
+
+@login_required
+def release_hijack(request):
+    return release_hijack_fx(request)
+
+
+def disable_hijack_warning(request):
+    request.session['is_hijacked_user']=False
+    return HttpResponseRedirect(request.GET.get('next','/'))
