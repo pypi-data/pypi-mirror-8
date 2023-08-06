62ba2a0d0dd54355974b2f86fc66401d8639c3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:14 +0000

    Add package iron_celery/0.4.3/iron_celery-0.4.3.tar.gz

---
+++ packages/iron_celery/iron_celery-0.4.3.tar.gz/iron_celery-0.4.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = "iron_celery",
+    packages = ["iron_celery"],
+    install_requires = ["iron_mq", "iron_cache", "celery"],
+    version = "0.4.3",
+    description = "IronMQ transport and IronCache backend for Celery",
+    author = "Iron.io",
+    author_email = "info@iron.io",
+    url = "https://github.com/iron-io/iron_celery"
+)
