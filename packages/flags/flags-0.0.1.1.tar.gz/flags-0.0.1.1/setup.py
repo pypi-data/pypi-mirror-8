8bcfb599fd352730b688b62e744b3e9e0e200a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:58 +0000

    Add package flags/0.0.1.1/flags-0.0.1.1.tar.gz

---
+++ packages/flags/flags-0.0.1.1.tar.gz/flags-0.0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'flags',
+    version = '0.0.1.1',
+    description = 'Packing and unpacking integer representations of binary flags',
+    author='Brian Peterson',
+    author_email='bepetersn@gmail.com',
+    url='https://github.com/bepetersn/flags',
+    license='',
+    py_modules=['flags'],
+    classifiers=[    
+],
+)
