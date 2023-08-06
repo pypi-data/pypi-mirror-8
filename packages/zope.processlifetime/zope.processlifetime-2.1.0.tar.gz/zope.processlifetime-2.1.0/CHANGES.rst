a51d42d76cb84108d03da031992faf11a9f6d32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:59 +0000

    Add package zope.processlifetime/2.1.0/zope.processlifetime-2.1.0.tar.gz

---
+++ packages/zope.processlifetime/zope.processlifetime-2.1.0.tar.gz/zope.processlifetime-2.1.0/CHANGES.rst
@@ -0,0 +1,30 @@
+Changes
+=======
+
+2.1.0 (2014-12-27)
+------------------
+
+- Add support for PyPy and PyPy3.
+
+- Add support for Python 3.4.
+
+- Add support for testing on Travis.
+
+
+2.0.0 (2013-02-22)
+------------------
+
+- Replace deprecated ``zope.interface.implements`` usage with equivalent
+  ``zope.interface.implementer`` decorator.
+
+- Add support for Python 3.2 and 3.3
+
+- Drop support for Python 2.4 and 2.5.
+
+
+
+1.0 (2009-05-13)
+----------------
+
+- Split out event interfaces / implementations from ``zope.app.appsetup``
+  version 3.10.2.
---
+++ packages/zope.processlifetime/zope.processlifetime-2.1.0.tar.gz/zope.processlifetime-2.1.0/setup.py
@@ -0,0 +1,64 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.processlifetime package
+"""
+import os
+from setuptools import find_packages, setup
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+setup(
+    name='zope.processlifetime',
+    version='2.1.0',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    description="Zope process lifetime events",
+    long_description=(read('README.rst') + '\n\n' +
+                      read('CHANGES.rst')),
+    license='ZPL 2.1',
+    keywords="zope process lifetime events",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3',
+        'Framework :: Zope2',
+        ],
+    url='http://pypi.python.org/pypi/zope.processlifetime',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    test_suite='zope.processlifetime.tests',
+    namespace_packages=['zope'],
+    install_requires=['setuptools',
+                      'zope.interface',
+                     ],
+    include_package_data=True,
+    zip_safe=False,
+    )
