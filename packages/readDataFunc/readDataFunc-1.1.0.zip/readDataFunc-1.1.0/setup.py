e97445afa18ef17753b3081d435e3f4404a1d3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:35 +0000

    Add package readDataFunc/1.1.0/readDataFunc-1.1.0.zip

---
+++ packages/readDataFunc/readDataFunc-1.1.0.zip/readDataFunc-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='readDataFunc',
+        version='1.1.0',
+        py_modules=['readDataFunc'],
+        author='ArTurn',
+        author_email='a0918396632@gmail.com',
+        url='',
+        description='This is a setup file that my practice',
+     )
