33d4104dfcd22b1fc0fc7baf54b64ce192944202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:01 +0000

    Add package gnsstime/0.0.1/gnsstime-0.0.1.tar.gz

---
+++ packages/gnsstime/gnsstime-0.0.1.tar.gz/gnsstime-0.0.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+version = '0.0.1'
+name = 'gnsstime'
+short_description = 'Extended datetime for the gnss analysis.'
+long_description = """\
+`gnsstime` is a extended datetime for the gnss analysis.
+
+Requirements
+------------
+* Python 3.3 or later (not support 2.x)
+
+Features
+--------
+* nothing
+
+History
+-------
+0.0.1 (2014-12-05)
+~~~~~~~~~~~~~~~~~~
+* first release
+
+"""
+
+classifiers = [
+   "Development Status :: 3 - Alpha",
+   "License :: OSI Approved :: Python Software Foundation License",
+   "Programming Language :: Python",
+   "Topic :: Scientific/Engineering",
+]
+
+setup(
+    name=name,
+    version=version,
+    description=short_description,
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords=['gps', 'gnss', 'datetime'],
+    author='Satoshi Kawamoto',
+    author_email='satoshi.pes@gmail.com',
+    url='None',
+    license='PSL',
+    scripts = ['gnsstime.py']
+)
+
+
