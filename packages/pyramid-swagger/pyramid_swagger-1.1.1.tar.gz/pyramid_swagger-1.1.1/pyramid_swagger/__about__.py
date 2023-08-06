b8137ea6f3637f76615c3519ed7a2cbaa703879d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package pyramid-swagger/1.1.1/pyramid_swagger-1.1.1.tar.gz

---
+++ packages/pyramid-swagger/pyramid_swagger-1.1.1.tar.gz/pyramid_swagger-1.1.1/pyramid_swagger/__about__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "pyramid_swagger"
+__summary__ = "Swagger tools for use in pyramid webapps"
+__uri__ = "https://github.com/striglia/pyramid_swagger"
+
+__version__ = "1.1.1"
+
+__author__ = "Scott Triglia"
+__email__ = "striglia@yelp.com"
+
+__license__ = "Copyright 2013-2014 Yelp"
+__copyright__ = "Copyright 2013-2014 Yelp"
