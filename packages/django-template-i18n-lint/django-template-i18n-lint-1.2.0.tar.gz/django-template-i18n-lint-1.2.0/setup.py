4382dbaf222f432071b47e8058e5dff80353983c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:40 +0000

    Add package django-template-i18n-lint/1.2.0/django-template-i18n-lint-1.2.0.tar.gz

---
+++ packages/django-template-i18n-lint/django-template-i18n-lint-1.2.0.tar.gz/django-template-i18n-lint-1.2.0/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="django-template-i18n-lint",
+    version="1.2.0",
+    author="Rory McCann",
+    author_email="rory@technomancy.org",
+    py_modules=['django_template_i18n_lint'],
+    license='GPLv3+',
+    url='http://www.technomancy.org/python/django-template-i18n-lint/',
+    description='Lint tool to find non-trans/blocktrans text in django templates',
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    entry_points={
+        'console_scripts': [
+            'django-template-i18n-lint = django_template_i18n_lint:main',
+        ]
+    },
+)
