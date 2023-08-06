efd12a2e24a52b4b3c860c9ea387fc49353ab8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:46 +0000

    Add package threebot/0.0.12/threebot-0.0.12.tar.gz

---
+++ packages/threebot/threebot-0.0.12.tar.gz/threebot-0.0.12/setup.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+import threebot as app
+
+setup(
+    name="threebot",
+    version=app.__version__,
+    author_email="admin@arteria.ch",
+    maintainer_email="renner@arteria.ch",
+    packages=find_packages(),
+    include_package_data=True,
+
+    install_requires=[
+        "jsonfield",
+        "django-sekizai",
+        "django-background-task"
+        "pyzmq",
+        "threebot_crypto",
+    ],
+
+)
