593d7c2411754c6c2ff1a17eb8e4b0fca14d5487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:12 +0000

    Add package htdataredirector/0.1.5/htdataredirector-0.1.5.tar.gz

---
+++ packages/htdataredirector/htdataredirector-0.1.5.tar.gz/htdataredirector-0.1.5/setup.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+sys.path.insert(0, os.path.abspath('lib'))
+
+exec(open(os.path.dirname(os.path.abspath(__file__)) + '/lib/htdataredirector/_version.py').read())
+
+setup(
+    name='htdataredirector',
+    version=__version__,
+    packages=find_packages('lib'),
+    package_dir={
+        '': 'lib'
+    },
+    entry_points={
+        'console_scripts': [
+            'htdataclient = htdataredirector.client:runner',
+        ],
+    },
+    install_requires=[
+        'pyserial >= 2.6.9',
+        'httplib2 >= 0.9'
+    ],
+    package_data={
+    '': ['*.txt', '*.rst', '*.md']
+    },
+    description='',
+    author='Johnny Robeson',
+    author_email='johnny@localmomentum.net',
+    url='https://github.com/lazerball/htdataredirector',
+    license='AGPLv3+',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    use_2to3=False,
+)
