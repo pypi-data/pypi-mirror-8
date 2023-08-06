bc1a5c6b77b95be64728621b538e7d221c701efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:41 +0000

    Add package liver/0.0.7/liver-0.0.7.tar.gz

---
+++ packages/liver/liver-0.0.7.tar.gz/liver-0.0.7/liver/migrations/0010_auto__add_field_record_recorder.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Record.recorder'
+        db.add_column(u'liver_record', 'recorder',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['liver.Recorder'], null=True, on_delete=models.SET_NULL, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Record.recorder'
+        db.delete_column(u'liver_record', 'recorder_id')
+
+
+    models = {
+        u'liver.record': {
+            'Meta': {'object_name': 'Record'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'insertion_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'metadata_json': ('django.db.models.fields.TextField', [], {'max_length': '5000', 'blank': 'True'}),
+            'modification_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '5000', 'blank': 'True'}),
+            'profiles_json': ('django.db.models.fields.TextField', [], {'max_length': '5000', 'blank': 'True'}),
+            'record_job': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.RecordJob']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'recorder': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.Recorder']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'to_delete': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'liver.recorder': {
+            'Meta': {'object_name': 'Recorder'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '5000'}),
+            'token': ('django.db.models.fields.CharField', [], {'default': "'e370b3f2-5781-11e4-83cd-8331ab616f06'", 'max_length': '5000'})
+        },
+        u'liver.recordjob': {
+            'Meta': {'object_name': 'RecordJob'},
+            'completion_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'execution_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'insertion_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'modification_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
+            'record_source': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.RecordSource']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'recorder': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.Recorder']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'result': ('django.db.models.fields.CharField', [], {'default': "'None'", 'max_length': '5000', 'null': 'True', 'blank': 'True'}),
+            'scheduled_duration': ('django.db.models.fields.IntegerField', [], {}),
+            'scheduled_start_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'sources_group': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.SourcesGroup']"}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '5000'})
+        },
+        u'liver.recordjobmetadata': {
+            'Meta': {'object_name': 'RecordJobMetadata'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '5000', 'blank': 'True'}),
+            'record_job': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.RecordJob']"}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '5000', 'blank': 'True'})
+        },
+        u'liver.recordmetadata': {
+            'Meta': {'object_name': 'RecordMetadata'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '5000', 'blank': 'True'}),
+            'record_source': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.RecordSource']"}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '5000', 'blank': 'True'})
+        },
+        u'liver.recordrule': {
+            'Meta': {'object_name': 'RecordRule'},
+            'availability_window': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'metadata_key_filter': ('django.db.models.fields.CharField', [], {'max_length': '5000', 'blank': 'True'}),
+            'metadata_value_filter': ('django.db.models.fields.CharField', [], {'max_length': '5000', 'blank': 'True'}),
+            'offset_end': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'offset_start': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'record_source': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.RecordSource']"})
+        },
+        u'liver.recordsource': {
+            'Meta': {'object_name': 'RecordSource'},
+            'enabled': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'enabled_since': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'enabled_until': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'null': 'True', 'blank': 'True'}),
+            'external_id': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '5000'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'insertion_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'modification_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
+            'sources_group': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.SourcesGroup']", 'null': 'True', 'blank': 'True'})
+        },
+        u'liver.source': {
+            'Meta': {'object_name': 'Source'},
+            'external_id': ('django.db.models.fields.CharField', [], {'max_length': '5000'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'insertion_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'modification_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '5000'}),
+            'sources_group': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['liver.SourcesGroup']", 'null': 'True', 'blank': 'True'}),
+            'uri': ('django.db.models.fields.CharField', [], {'max_length': '5000'})
+        },
+        u'liver.sourcesgroup': {
+            'Meta': {'object_name': 'SourcesGroup'},
+            'default_availability_window': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'default_offset_end': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'default_offset_start': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'external_id': ('django.db.models.fields.CharField', [], {'max_length': '5000'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'insertion_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'modification_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '5000'})
+        }
+    }
+
+    complete_apps = ['liver']
