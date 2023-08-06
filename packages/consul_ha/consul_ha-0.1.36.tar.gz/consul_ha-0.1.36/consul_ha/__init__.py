10192cf0bc3f9f54385b62ca7c368246422628af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package consul_ha/0.1.36/consul_ha-0.1.36.tar.gz

---
+++ packages/consul_ha/consul_ha-0.1.36.tar.gz/consul_ha-0.1.36/consul_ha/__init__.py
@@ -0,0 +1,1 @@
+import os\n__version__ = '0.1.36'\nfrom consul_ha import ConsulHa
---
+++ packages/consul_ha/consul_ha-0.1.36.tar.gz/consul_ha-0.1.36/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+from os import environ
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='consul_ha',
+    version='0.1.%s' % environ.get("CIRCLE_BUILD_NUM"),
+    url='https://github.com/mongohq/consul_ha',
+    author='Chris Winslett',
+    author_email='chris@compose.io',
+    description='Server for managing application state and high availability using Consul',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        ],
+
+    keywords='consul high availablity',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    install_requires=[],
+
+    # List additional groups of dependencies here (e.g. development dependencies).
+    # You can install these using the following syntax, for example:
+    # $ pip install -e .[dev,test]
+    extras_require = {
+            'dev': ['check-manifest'],
+            'test': [],
+            },
+)
