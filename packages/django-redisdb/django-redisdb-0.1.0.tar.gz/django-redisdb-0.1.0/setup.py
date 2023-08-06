385ba3da0d322394725327d777fc2cf994daddeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:08 +0000

    Add package django-redisdb/0.1.0/django-redisdb-0.1.0.tar.gz

---
+++ packages/django-redisdb/django-redisdb-0.1.0.tar.gz/django-redisdb-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='django-redisdb',
+    version='0.1.0',
+    description='Django redis backend',
+    long_description=long_description,
+    url="http://kidosoft.pl",
+    author="Jakub STOLARSKI (Dryobates)",
+    author_email="jakub.stolarski@kidosoft.pl",
+    license="beerware",
+    keywords="django redis",
+    packages=find_packages('src', exclude=['example*']),
+    package_dir={'': 'src'},
+    install_requires=[
+        'redis',
+        'hash_ring',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
