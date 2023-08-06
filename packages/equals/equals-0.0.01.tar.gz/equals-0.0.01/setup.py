25af90251c866dda407c12fdf6924f127dc09502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package equals/0.0.1/equals-0.0.01.tar.gz

---
+++ packages/equals/equals-0.0.01.tar.gz/equals-0.0.01/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    description = 'Python Fuzzy Matchers',
+    name =  'equals',
+    version = '0.0.01',
+    author='Todd Siflet',
+    author_email='todd.siflet@gmail.com',
+    py_modules = [],
+)
