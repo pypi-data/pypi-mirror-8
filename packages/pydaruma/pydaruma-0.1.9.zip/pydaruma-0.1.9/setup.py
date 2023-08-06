f3433e1882ed2d5da356928e3f410791416d47ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:12 +0000

    Add package pydaruma/0.1.9/pydaruma-0.1.9.zip

---
+++ packages/pydaruma/pydaruma-0.1.9.zip/pydaruma-0.1.9/setup.py
@@ -0,0 +1,33 @@
+__author__ = 'Edinei'
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="pydaruma",
+
+    # Version number (initial):
+    version="0.1.9",
+
+    # Application author details:
+    author="Edinei Colli",
+    author_email="colli.edinei@gmail.com",
+
+    # Packages
+    packages=["pydaruma"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="http://github.com/edineicolli/pydaruma",
+
+    #
+    license="GNU GPL",
+    description="Wrapper for Daruma Framework library.",
+
+    long_description="Integração com impressoras Daruma utilizando a biblioteca DarumaFramework",
+
+    # Dependent packages (distributions)
+    install_requires=[
+    ],
+)
