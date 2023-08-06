296c144a16907c2598c97e4ef71d324e3fb68ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:32 +0000

    Add package aws-voyeur/0.1.0/aws-voyeur-0.1.0.tar.gz

---
+++ packages/aws-voyeur/aws-voyeur-0.1.0.tar.gz/aws-voyeur-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='aws-voyeur',
+    version='0.1.0',
+    url='https://github.com/texastribune/aws-voyeur',
+    license='BSD',
+    author='Chris Chang',
+    author_email='cchang@texastribune.org',
+    description='List AWS inventory',
+    long_description=open('README.md').read(),
+    py_modules=['voyeur'],
+    entry_points={
+        'console_scripts': [
+            'voyeur = voyeur:main',
+        ],
+    },
+    install_requires=[
+        'boto',
+        'tabulate',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
