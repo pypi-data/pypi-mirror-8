9271fb72216d6d31d6ec5ccf57ff833541aa1a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:25 +0000

    Add package royweb/0.6.6/royweb-0.6.6.tar.gz

---
+++ packages/royweb/royweb-0.6.6.tar.gz/royweb-0.6.6/royweb/__init__.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+# Filename: __init__.py
+"""
+The Restless Oyster online monitoring utility.
+
+"""
+__author__ = "Tamas Gal"
+__copyright__ = ("Copyright 2014, Tamas Gal and the KM3NeT collaboration "
+                 "(http://km3net.org)")
+__credits__ = []
+__license__ = "MIT"
+__version__ = "0.6.6"
+__maintainer__ = "Tamas Gal"
+__email__ = "tgal@km3net.de"
+__status__ = "Development"
+
