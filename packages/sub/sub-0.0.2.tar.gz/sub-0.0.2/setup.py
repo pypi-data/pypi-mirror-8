f551590ced6056e4e8d4b8570dca8351a53206e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:23 +0000

    Add package sub/0.0.2/sub-0.0.2.tar.gz

---
+++ packages/sub/sub-0.0.2.tar.gz/sub-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'sub',
+    version = '0.0.2',
+    author = 'Sartaj Singh',
+    author_email = 'singhsartaj94@gmail.com',
+    description = ('Simple Tool to download Subtitles.'),
+    license = 'MIT',
+    keywords = 'subtitles download movies tv shows',
+    url = 'http://github.com/leosartaj/sub',
+    packages=['sub'],
+    scripts=['bin/sub'],
+    long_description=read('README.md'),
+    package_data={'sub': ['README.md', 'LICENSE-MIT.txt', 'CONTRIBUTING.md']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
