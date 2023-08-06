a38d76324c569554fcbf54adb5abbdd5ab1a050b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:28 +0000

    Add package redis_kernel/0.3/redis_kernel-0.3.tar.gz

---
+++ packages/redis_kernel/redis_kernel-0.3.tar.gz/redis_kernel-0.3/redis_kernel/constants.py
@@ -0,0 +1,8 @@
+NAME = 'redis_kernel'
+VERSION = '0.3'
+LANGUAGE = 'redis'
+BANNER = 'redis kernel 0.3'
+DEFAULT_HOST = 'localhost'
+DEFAULT_PORT = '6379'
+DISPLAY_NAME = 'Redis'
+DESCRIPTION = 'A redis kernel for IPython'
