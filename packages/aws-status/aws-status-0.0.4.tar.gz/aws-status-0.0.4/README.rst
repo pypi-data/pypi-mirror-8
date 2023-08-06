40ff1451561a86d17304bfa1eae062caa7afed31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:09 +0000

    Add package aws-status/0.0.4/aws-status-0.0.4.tar.gz

---
+++ packages/aws-status/aws-status-0.0.4.tar.gz/aws-status-0.0.4/README.rst
@@ -0,0 +1,11 @@
+aws-status
+==========
+
+Helps you check AWS status as seen from the official status page http://status.aws.amazon.com/
+
+Thanks
+------
+
+- a great article about packaging python apps: https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+- one example of parsing AWS status feeds (in Python): https://github.com/rhartkopf/check_aws_status_feed/blob/master/check_aws_status_feed.py
+- an other example (in Ruby): https://gist.github.com/ktheory/1604786
---
+++ packages/aws-status/aws-status-0.0.4.tar.gz/aws-status-0.0.4/setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup, find_packages
+
+version = __import__('aws_status').__version__
+
+def read(path):
+    """Return the content of a file."""
+    with open(path, 'r') as f:
+        return f.read()
+
+setup(
+    name='aws-status',
+    version=version,
+    description='Wraps AWS status informations obtained via the status page.',
+    long_description=(read('README.rst')),
+    url='http://github.com/jbbarth/aws-status',
+    license='MIT',
+    author='Jean-Baptiste Barth',
+    author_email='jeanbaptiste.barth@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    scripts=[
+        'bin/aws-status-list',
+    ],
+    install_requires=[
+        'feedparser>=5.1.3',
+    ],
+    include_package_data=True,
+    #see http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Monitoring',
+    ],
+)
