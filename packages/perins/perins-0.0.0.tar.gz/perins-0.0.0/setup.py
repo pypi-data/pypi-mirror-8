c4fac3105e7f69911acf0c0648a357b59047e274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:25 +0000

    Add package perins/0.0.0/perins-0.0.0.tar.gz

---
+++ packages/perins/perins-0.0.0.tar.gz/perins-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='perins',
+    packages=['perins'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ],
+)
