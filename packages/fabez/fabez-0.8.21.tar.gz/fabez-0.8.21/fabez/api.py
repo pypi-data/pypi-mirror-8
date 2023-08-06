bd2a31c800f0b7a400c2fd041ce1fe05dbb34bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:16 +0000

    Add package fabez/0.8.21/fabez-0.8.21.tar.gz

---
+++ packages/fabez/fabez-0.8.21.tar.gz/fabez-0.8.21/fabez/api.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from env import *
+from cmd import *
+from server import *
+from io import *
+from perm import *
+from utils import *
+from pythonic import *
+from etc import *
+
+
+
+
+
+
+
