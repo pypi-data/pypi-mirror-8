77de82107f325829864dad212b715910b6859172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:26 +0000

    Add package saml/0.5.0/saml-0.5.0.tar.gz

---
+++ packages/saml/saml-0.5.0.tar.gz/saml-0.5.0/saml/__init__.py
@@ -0,0 +1,25 @@
+"""Security Assertion Markup Language (SAML) v2.0
+
+A python interface to produce and consume Security Assertion Markup
+Language (SAML) v2.0 messages.
+
+@par References
+    - https://www.oasis-open.org/standards#samlv2.0
+"""
+# Version of the library.
+from ._version import __version__, __version_info__  # noqa
+VERSION = __version__
+
+# Version of the SAML standard supported.
+from .schema import VERSION as SAML_VERSION
+
+from .signature import sign, verify
+from . import client
+
+__all__ = [
+    'VERSION',
+    'SAML_VERSION',
+    'sign',
+    'verify',
+    'client'
+]
