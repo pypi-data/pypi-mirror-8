5eaa5cf330bb92954e90621c8458f65f10357fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:16 +0000

    Add package nagios-cloudwatch-plugin/0.2.6/nagios-cloudwatch-plugin-0.2.6.tar.gz

---
+++ packages/nagios-cloudwatch-plugin/nagios-cloudwatch-plugin-0.2.6.tar.gz/nagios-cloudwatch-plugin-0.2.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nagios-cloudwatch-plugin',
+        version='0.2.6',
+        author='William Hutson',
+        author_email='wilrnh@gmail.com',
+        license="MIT",
+        keywords="nagios cloudwatch plugin",
+        url='https://github.com/FastSociety/nagios-cloudwatch-plugin',
+        description='Nagios plugin to check AWS CloudWatch metrics',
+        long_description=open('README.txt').read(),
+        install_requires=["argparse","nagiosplugin","boto"],
+        
+        scripts=["check_cloudwatch.py"]
+)
