d93973e4506d1f0c3c73b5440ca6c44fea66be35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:39 +0000

    Add package bigfile/0.1pre/bigfile-0.1pre.tar.gz

---
+++ packages/bigfile/bigfile-0.1pre.tar.gz/bigfile-0.1pre/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+from distutils.extension import Extension
+import numpy
+
+extensions = [
+        Extension("bigfile.pyxbigfile", ["python/pyxbigfile.pyx"],
+            include_dirs = ["src/", numpy.get_include()])]
+
+setup(
+    name="bigfile", version="0.1pre",
+    author="Yu Feng",
+    author_email="rainwoodman@gmail.com",
+    url="http://github.com/rainwoodman/bigfile",
+    description="python binding of BigFile, a peta scale IO format",
+    zip_safe = False,
+    package_dir = {'bigfile': 'python'},
+    install_requires=['cython', 'numpy'],
+    packages= ['bigfile'],
+    requires=['numpy'],
+    ext_modules = cythonize(extensions)
+)
