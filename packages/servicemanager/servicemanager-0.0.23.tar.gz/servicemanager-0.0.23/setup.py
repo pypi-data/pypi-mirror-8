edd69903ddaf19ef6344c8751949e612b89f85d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:05 +0000

    Add package servicemanager/0.0.23/servicemanager-0.0.23.tar.gz

---
+++ packages/servicemanager/servicemanager-0.0.23.tar.gz/servicemanager-0.0.23/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+#with open('requirements.txt') as f:
+#    required = f.read().splitlines()
+
+setup(name='servicemanager',
+      version='0.0.23',
+      description='A python tool to manage developing and testing with lots of microservices',
+      url='https://github.com/hmrc/service-manager',
+      author='vaughansharman',
+      license='Apache Licence 2.0',
+      packages=['servicemanager', 'servicemanager.actions', 'servicemanager.server', 'servicemanager.service', 'servicemanager.thirdparty'],
+      install_requires=['requests==2.2.1','pymongo==2.6.3','bottle==0.12.4','pytest==2.5.2','argcomplete==0.8.1'],
+      scripts=['bin/sm', 'bin/smserver'],
+      zip_safe=False)
