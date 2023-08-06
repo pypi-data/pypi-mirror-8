9543c0bcf48015c52ed1e7dc4315356e6bc3fd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:48 +0000

    Add package sailthru-client/2.1.7/sailthru-client-2.1.7.tar.gz

---
+++ packages/sailthru-client/sailthru-client-2.1.7.tar.gz/sailthru-client-2.1.7/sailthru/__init__.py
@@ -0,0 +1,9 @@
+from sailthru_client import SailthruClient
+from sailthru_error import SailthruClientError
+from sailthru_response import SailthruResponse, SailthruResponseError
+
+__author__ = 'Prajwal Tuladhar'
+__doc__ = 'Python binding for Sailthru API based on Request'
+__copyright__ = 'Copyright 2012, Sailthru Inc.'
+__license__ = 'MIT'
+__version__ = '2.1.3'
---
+++ packages/sailthru-client/sailthru-client-2.1.7.tar.gz/sailthru-client-2.1.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='sailthru-client', 
+    version='2.1.7',
+    packages=find_packages(),
+    description='Python client for Sailthru API',
+    long_description=open('README.md').read(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
+    install_requires=[
+        'requests >= 0.12.0',
+        'simplejson >= 2.0'
+    ],
+    keywords='sailthru api',
+    author='Prajwal Tuladhar',
+    author_email='praj@sailthru.com',
+    url='https://github.com/sailthru/sailthru-python-client',
+    license='MIT License',
+    include_package_data=True,
+    zip_safe=True
+)
