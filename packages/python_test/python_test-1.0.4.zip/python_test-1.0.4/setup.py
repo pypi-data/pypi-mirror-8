1f6701e42c7642b8e7570d5a7c916d9034f3fbb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:54 +0000

    Add package python_test/1.0.4/python_test-1.0.4.zip

---
+++ packages/python_test/python_test-1.0.4.zip/python_test-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'python_test',
+    version = '1.0.4',
+    py_modules = ['python_test'],
+    author = 'ken',
+    author_email = 'shineken001@163.com',
+    url = 'http://www.shineken.com',
+    description = 'A simple printer of nested list',
+	)
