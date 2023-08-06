359b706007df8be43e62a078cbdfebb1e3ed1a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:11 +0000

    Add package numberify/0.0.1/numberify-0.0.1.tar.gz

---
+++ packages/numberify/numberify-0.0.1.tar.gz/numberify-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import numberify
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name=numberify.__title__,
+    version=numberify.__version__,
+    author=numberify.__author__,
+    author_email='indiasuny000@gmail.com',
+    url='https://github.com/darkowlzz/numberify',
+    description='Prepend each line of a file with line number',
+    long_description=readme,
+    license='http://www.gnu.org/licenses/',
+    packages=find_packages(),
+    setup_requires=['nose'],
+    scripts=['numberify/numberify.py'],
+    test_suite='test'
+)
