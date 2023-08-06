045bbc27711c0d88831281a9965da09bcb5b786c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:49 +0000

    Add package gandalf-client/0.5.2/gandalf-client-0.5.2.tar.gz

---
+++ packages/gandalf-client/gandalf-client-0.5.2.tar.gz/gandalf-client-0.5.2/gandalf/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of gandalf-client.
+# https://github.com/rfloriano/gandalf-client
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2014 Rafael Floriano da Silva rflorianobr@gmail.com
+
+
+class GandalfException(Exception):
+    def __init__(self, response, obj):
+        self.response = response
+        self.obj = obj
+        self.status_code = obj.get_code(response)
+        self.content = obj.get_content(response)
+        super(GandalfException, self).__init__(
+            '{0} (Gandalf server response HTTP {1})'
+            .format(self.content.strip("\n "), self.status_code)
+        )
---
+++ packages/gandalf-client/gandalf-client-0.5.2.tar.gz/gandalf-client-0.5.2/gandalf/tornado_cli.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import tornado.gen as gen
+import tornado.httpclient as httpclient
+
+import gandalf
+import gandalf.client as client
+
+
+class AsyncTornadoGandalfClient(client.GandalfClient):
+    @gen.coroutine
+    def _request(self, *args, **kwargs):
+        url = kwargs.pop('url')
+        data = kwargs.pop('data', None)
+        if data:
+            kwargs['body'] = data
+
+        try:
+            response = yield self.client(url, *args, **kwargs)
+        except httpclient.HTTPError as e:
+            raise gandalf.GandalfException(e.response, obj=self)
+        raise gen.Return(response)
+
+    def get_code(self, response):
+        return response.code
+
+    def get_raw(self, response):
+        return response.body
