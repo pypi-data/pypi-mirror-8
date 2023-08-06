31d19f27f74bc22dfdb83c42e99b2dc74222f209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:15 +0000

    Add package CFEprintList/1.1.0/CFEprintList-1.1.0.tar.gz

---
+++ packages/CFEprintList/CFEprintList-1.1.0.tar.gz/CFEprintList-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 		'CFEprintList',
+	version =	'1.1.0',
+	py_modules =['CFEprintList'],
+	author =	'Carlos Escapa',
+	author_email='noneofyourbusiness@none.com',
+	url =		'cnn.com',
+	description='This is a simple exercise'
+)
