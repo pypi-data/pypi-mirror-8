8f87a2982e24a16255898f020b2d19853d21abde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:20 +0000

    Add package coinmarketcap/0.7/coinmarketcap-0.7.tar.gz

---
+++ packages/coinmarketcap/coinmarketcap-0.7.tar.gz/coinmarketcap-0.7/coinmarketcap/coinmarketcap_api.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from coinmarketcap_utils import coin_info
+from coinmarketcap_utils import update_info
+from coinmarketcap_utils import top_currencies
+from coinmarketcap_utils import market_cap_info
+from coinmarketcap_utils import coinmarketcap_info
+
+__title__   = 'coinmarketcap'
+__version__ = '0.7'
+__author__  = '@c0ding'
+__repo__    = 'https://github.com/c0ding/coinmarketcap-api'
+__license__ = 'Apache v2.0 License'
+
+def about():
+	"""Returns some information about the coinmarketcap module."""
+
+	return '{} v.{} is maintained by {} and available at {}.'.format(__title__, __version__, __author__, __repo__)
+
+
+def rank(PARAMETER):
+	return coin_info(PARAMETER)[0]
+
+	
+def name(PARAMETER):
+	return coin_info(PARAMETER)[1]
+
+	
+def short(PARAMETER):
+	return coin_info(PARAMETER)[2]
+	
+
+def market_cap(PARAMETER):
+	return coin_info(PARAMETER)[3]
+
+	
+def price(PARAMETER):
+	return coin_info(PARAMETER)[4]
+
+
+def coin_supply(PARAMETER):
+	return coin_info(PARAMETER)[5]
+
+
+def market_volume(PARAMETER):
+	return coin_info(PARAMETER)[6]
+
+
+def cap_change_1h(PARAMETER):
+	return coin_info(PARAMETER)[7]
+
+
+def cap_change_24h(PARAMETER):
+	return coin_info(PARAMETER)[8]
+
+
+def cap_change_7d(PARAMETER):
+	return coin_info(PARAMETER)[9]
+
+
+def top(PARAMETER):
+	return top_currencies(PARAMETER)
+
+def mineable(PARAMETER):
+	try:
+		if coin_info(PARAMETER)[10] == '*':
+			return 'false'
+		if coin_info(PARAMETER)[10] == '**':
+			return 'true'
+	except IndexError:
+		return 'true'
+
+		
+def premined(PARAMETER):
+	try:
+		if coin_info(PARAMETER)[10] == '**':
+			return 'true'
+		if coin_info(PARAMETER)[10] == '*':
+			return 'false'
+	except IndexError:
+		return 'false'
+
+
+def last_updated():
+	return update_info()[0]
+
+	
+def total_market_cap():
+	return market_cap_info()[1] + market_cap_info()[2]
+
+
+def stats():
+	return str(coinmarketcap_info()['active_currencies']) + ' Currencies / '\
+	+ str(coinmarketcap_info()['active_markets']) + ' Markets / ' \
+	+ str(coinmarketcap_info()['active_assets']) + ' Assets'
+
+
+def coin_summary(PARAMETER):
+	keys = ['name', 'rank', 'short', 'market_cap', 'market_volume', 'cap_change_1h',\
+	'cap_change_24h', 'cap_change_7d', 'price', 'coin_supply', 'mineable', 'premined']
+	values = [name(PARAMETER), rank(PARAMETER), short(PARAMETER), market_cap(PARAMETER),\
+	market_volume(PARAMETER), cap_change_1h(PARAMETER), cap_change_24h(PARAMETER),\
+	cap_change_7d(PARAMETER), price(PARAMETER), coin_supply(PARAMETER), mineable(PARAMETER),\
+	premined(PARAMETER)]
+	coin_summary_info = {}
+	for i in range(len(keys)):
+	    coin_summary_info[keys[i]] = values[i]
+	return coin_summary_info
---
+++ packages/coinmarketcap/coinmarketcap-0.7.tar.gz/coinmarketcap-0.7/coinmarketcap/coinmarketcap_utils.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import json
+import urllib
+import lxml.html
+
+__title__   = 'coinmarketcap'
+__version__ = '0.7'
+__author__  = '@c0ding'
+__repo__    = 'https://github.com/c0ding/coinmarketcap-api'
+__license__ = 'Apache v2.0 License'
+
+ENTRY_POINT_URL = 'http://coinmarketcap.com/all.html'
+
+def coin_info(PARAMETER):
+	raw_data = lxml.html.parse(ENTRY_POINT_URL)
+	tree = raw_data.xpath('//tr[@id="id-' + PARAMETER + '"]/td//text()')
+	clean_space = map(lambda x: x.strip(), tree)
+	coin_details = [x for x in clean_space if x]
+	mineable_char = '*'
+	premine_char = '**'
+	if mineable_char in coin_details:
+		coin_details.remove(mineable_char)
+		mineable_index = len(coin_details)
+		coin_details.insert(mineable_index, mineable_char)
+	if premine_char in coin_details:
+		coin_details.remove(premine_char)
+		premine_index = len(coin_details)
+		coin_details.insert(premine_index, premine_char)
+	return coin_details
+
+
+def top_currencies(PARAMETER):
+	raw_data = lxml.html.parse(ENTRY_POINT_URL)
+	row = raw_data.xpath('//tr/td[@class="no-wrap currency-name"]//text()')
+	regex = re.compile("(?:[^\n]*(\n+))+")
+	currencies_list = filter(lambda i: not regex.search(i), row)
+	return currencies_list[:PARAMETER]
+
+
+def update_info():
+	raw_data = lxml.html.parse(ENTRY_POINT_URL)
+	update_details = raw_data.xpath("//p[@class='small']//text()")
+	return update_details
+
+
+def market_cap_info():
+	raw_data = lxml.html.parse(ENTRY_POINT_URL)
+	market_cap_details = raw_data.xpath("//div[@id='total_market_cap']//text()")
+	return market_cap_details
+
+
+def coinmarketcap_info():
+	raw_data = urllib.urlopen('http://coinmarketcap.com/static/generated_pages/global/stats.json')
+	coinmarketcap_details = json.load(raw_data)
+	return coinmarketcap_details
