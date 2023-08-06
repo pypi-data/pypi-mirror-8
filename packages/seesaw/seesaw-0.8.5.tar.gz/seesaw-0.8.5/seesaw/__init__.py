96d82ef6ceda3614a499ec709b0c1275ffaf7210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:49 +0000

    Add package seesaw/0.8.5/seesaw-0.8.5.tar.gz

---
+++ packages/seesaw/seesaw-0.8.5.tar.gz/seesaw-0.8.5/seesaw/__init__.py
@@ -0,0 +1,7 @@
+'''ArchiveTeam seesaw kit'''
+
+__title__ = 'seesaw'
+__version__ = '0.8.5'
+__build__ = 0x00805
+runner_type = ""
+warrior_build = ""
---
+++ packages/seesaw/seesaw-0.8.5.tar.gz/seesaw-0.8.5/seesaw/util_test.py
@@ -0,0 +1,37 @@
+import unittest
+
+import seesaw
+import seesaw.six
+from seesaw.util import find_executable, unique_id_str
+
+
+class UtilTest(unittest.TestCase):
+    def test_find_executable(self):
+        if seesaw.six.PY3:
+            exes = ['./run-pipeline3', '../run-pipeline3']
+        else:
+            exes = ['./run-pipeline', '../run-pipeline']
+
+        self.assertTrue(find_executable(
+            'pipeline runner',
+            seesaw.__version__,
+            exes,
+            version_arg='--version')
+        )
+
+    def test_find_executable_bad_version(self):
+        if seesaw.six.PY3:
+            exes = ['./run-pipeline3', '../run-pipeline3']
+        else:
+            exes = ['./run-pipeline', '../run-pipeline']
+
+        self.assertFalse(find_executable(
+            'pipeline runner',
+            '123-notrealversion',
+            exes,
+            version_arg='--version')
+        )
+
+    def test_unique_id_str(self):
+        # check for no crash
+        self.assertTrue(unique_id_str())
