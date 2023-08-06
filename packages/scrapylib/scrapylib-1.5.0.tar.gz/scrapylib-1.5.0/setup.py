b794b69813a4ec81e3600276c43434928c4b2624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package scrapylib/1.5.0/scrapylib-1.5.0.tar.gz

---
+++ packages/scrapylib/scrapylib-1.5.0.tar.gz/scrapylib-1.5.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='scrapylib',
+    version='1.5.0',
+    license='BSD',
+    description='Scrapy helper functions and processors',
+    author='Scrapinghub',
+    author_email='info@scrapinghub.com',
+    url='http://github.com/scrapinghub/scrapylib',
+    packages=['scrapylib', 'scrapylib.constraints', 'scrapylib.processors'],
+    platforms=['Any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+    install_requires=['Scrapy>=0.22.0']
+)
