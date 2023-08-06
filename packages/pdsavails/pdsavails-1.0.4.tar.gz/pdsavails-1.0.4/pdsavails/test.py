a6f4c7062d4c5b8807edcc5521b5f7b79134b74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:07 +0000

    Add package pdsavails/1.0.4/pdsavails-1.0.4.tar.gz

---
+++ packages/pdsavails/pdsavails-1.0.4.tar.gz/pdsavails-1.0.4/pdsavails/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+import unittest
+import random
+from __init__ import PDSAvails
+
+TEST_API_KEY='58178a3b56814f9d'
+TEST_API_PASS='73f253a65db94cd3'
+
+
+class TestPDSAvails(unittest.TestCase):
+
+
+    def setUp(self):
+        self.avails = PDSAvails(TEST_API_KEY, TEST_API_PASS)
+
+
+    def test001_validate_user(self):
+        """
+        Test to make sure a valid user account
+        and that the status code returned is 200.
+        """
+        res = self.avails.validate_credentials()
+        self.assertEquals(res['StatusCode'], 200)
+
+    def test002_get_platform_data(self):
+        """
+        Private method.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
---
+++ packages/pdsavails/pdsavails-1.0.4.tar.gz/pdsavails-1.0.4/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='pdsavails',
+      version='1.0.4',
+      description='Integration library for PDSAvails',
+      author='David Litwin',
+      author_email='david@premieredigital.net',
+      url='https://github.com/premieredigital/PDSAvails/',
+      license="MIT License",
+      install_requires=['requests>=1.0'],
+      tests_require=['mock', 'nose'],
+      packages=['pdsavails'],
+      platforms='any',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+     )
+
