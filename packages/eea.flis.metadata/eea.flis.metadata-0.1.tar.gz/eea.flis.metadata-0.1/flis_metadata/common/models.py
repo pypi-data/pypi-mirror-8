3934761ffd5bf4bf6ebbe91454d64df74cecf8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:13 +0000

    Add package eea.flis.metadata/0.1/eea.flis.metadata-0.1.tar.gz

---
+++ packages/eea.flis.metadata/eea.flis.metadata-0.1.tar.gz/eea.flis.metadata-0.1/flis_metadata/common/models.py
@@ -0,0 +1,62 @@
+import inspect
+import sys
+
+from django.db.models import BooleanField
+from django.db.models import CharField
+from django.db.models import Model
+
+
+class ReplicatedModel(Model):
+
+    class Meta:
+        abstract = True
+
+    is_deleted = BooleanField(default=False)
+
+
+class GeographicalScope(ReplicatedModel):
+
+    title = CharField(max_length=128)
+    require_country = BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-pk',)
+
+    def __unicode__(self):
+        return self.title
+
+
+class EnvironmentalTheme(ReplicatedModel):
+
+    title = CharField(max_length=128)
+
+    class Meta:
+        ordering = ('-pk',)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Country(ReplicatedModel):
+
+    iso = CharField(max_length=2, primary_key=True)
+    name = CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.name
+
+
+def _is_replicated_model(cls):
+    """Check wether a class is a model to be replicated.
+
+    This is implementend by extracting all models that inherit from
+    ReplicatedModel
+    """
+    return (inspect.isclass(cls) and
+            issubclass(cls, ReplicatedModel) and
+            cls != ReplicatedModel)
+
+
+def get_replicated_models():
+    """Return all replicated models in this module."""
+    return inspect.getmembers(sys.modules[__name__], _is_replicated_model)
