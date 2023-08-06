197fd2c2138077bab4d746c8f16b21aff091dc20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:10 +0000

    Add package hwrt/0.1.77/hwrt-0.1.77.tar.gz

---
+++ packages/hwrt/hwrt-0.1.77.tar.gz/hwrt-0.1.77/bin/evaluate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""Evaluate data."""
+
+import logging
+import sys
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.DEBUG,
+                    stream=sys.stdout)
+import yaml
+# mine
+import hwrt.utils as utils
+
+
+def main(model_folder):
+    recording = yaml.load(sys.stdin.readline())
+    logging.info(recording)
+    logging.info("Start evaluation...")
+    print(utils.evaluate_model(recording, model_folder))
+
+
+def get_parser():
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-m", "--model",
+                        dest="model",
+                        help="where is the model folder (with a info.yml)?",
+                        metavar="FOLDER",
+                        type=lambda x: utils.is_valid_folder(parser, x),
+                        default=utils.default_model())
+    return parser
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+    main(args.model)
