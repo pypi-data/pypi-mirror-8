a6fc87ee2052ce3f7b036f5b745a8cbfc46170bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:27 +0000

    Add package twisted-json-socket/0.1.1/twisted-json-socket-0.1.1.tar.gz

---
+++ packages/twisted-json-socket/twisted-json-socket-0.1.1.tar.gz/twisted-json-socket-0.1.1/setup.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name="twisted-json-socket",
+    version="0.1.1",
+    author="Bo Kanstrup, Mads Sülau Jørgensen",
+    author_email="bkh@konstellation.dk, msj@konstellation.dk",
+    description=("Protocol for twisted json socket"),
+    license="BSD",
+    keywords="twsited json socket protocol",
+    url="https://bitbucket.org/madssj/twisted-json-socket",
+    packages=find_packages('src'),
+    long_description=read('README'),
+    package_dir={'': 'src'},
+    install_requires=['twisted'],
+)
