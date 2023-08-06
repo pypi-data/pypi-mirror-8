8fb6655af6936481464500dea3bad54b4c86c971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:16 +0000

    Add package mwtemplates/0.3.2/mwtemplates-0.3.2.tar.gz

---
+++ packages/mwtemplates/mwtemplates-0.3.2.tar.gz/mwtemplates-0.3.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(name='mwtemplates',
+      version='0.3.2',
+      description='MediaWiki template parser and editor',
+      long_description=README,
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      keywords='mediawiki wikipedia',
+      author='Dan Michael Heggø',
+      author_email='danmichaelo@gmail.com',
+      url='https://github.com/danmichaelo/mwtemplates',
+      license='MIT',
+      packages=['mwtemplates'],
+      install_requires=['lxml']
+      )
