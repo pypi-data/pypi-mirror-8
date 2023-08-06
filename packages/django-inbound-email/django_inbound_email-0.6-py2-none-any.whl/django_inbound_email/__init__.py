547454a9b38954839ee356935ee933d00915685b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:00 +0000

    Add package django-inbound-email/0.6/django_inbound_email-0.6-py2-none-any.whl

---
+++ packages/django-inbound-email/django_inbound_email-0.6-py2-none-any.whl/django_inbound_email/__init__.py
@@ -0,0 +1,8 @@
+"""An inbound email handler for Django."""
+
+__title__ = 'django-inbound-email'
+__version__ = '0.6'
+__author__ = 'YunoJuno Ltd'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 YunoJuno'
+__description__ = 'A Django app for receiving inbound emails.'
