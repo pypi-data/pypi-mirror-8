b927f5bdd9ba961dc8bf662c3dcf40feee9399a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:54 +0000

    Add package toxicbuild/0.2.1.1/toxicbuild-0.2.1.1.tar.gz

---
+++ packages/toxicbuild/toxicbuild-0.2.1.1.tar.gz/toxicbuild-0.2.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+VERSION = '0.2.1.1'
+DESCRIPTION = """
+Hackish pieces of software to an easy buildbot config
+"""
+LONG_DESCRIPTION = DESCRIPTION
+
+setup(name='toxicbuild',
+      version=VERSION,
+      author='Juca Crispim',
+      author_email='jucacrispim@gmail.com',
+      url='http://toxicbuild.readthedocs.org/en/latest/',
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      packages=find_packages(exclude=['tests', 'tests.*']),
+      include_package_data=True,
+      install_requires=['buildbot>=0.8.9', 'buildbot-slave>=0.8.9'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: No Input/Output (Daemon)',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Testing',
+      ],
+      scripts=['script/toxicbuild'],
+      test_suite='tests',
+      provides=['toxicbuild'],)
