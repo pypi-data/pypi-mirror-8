e7767558e2c22aae7374db7ed4dfd81b09371150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:58 +0000

    Add package PyTurbSim/0.3.5/PyTurbSim-0.3.5.tar.gz

---
+++ packages/PyTurbSim/PyTurbSim-0.3.5.tar.gz/PyTurbSim-0.3.5/pyts/_version.py
@@ -0,0 +1,9 @@
+"""
+This module contains PyTurbSim version information.
+"""
+__prog_name__ = 'PyTurbSim'
+__version__ = '0.3.5'
+__version_date__ = 'Nov-26-2014'
+pkg_name = 'pyts'
+
+VerString = __prog_name__ + ' v' + __version__
