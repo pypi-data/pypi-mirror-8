d8818e7aa3c8a69075fbddaf9730c3099a070840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:22 +0000

    Add package pyexperian/0.5/pyexperian-0.5.tar.gz

---
+++ packages/pyexperian/pyexperian-0.5.tar.gz/pyexperian-0.5/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Python wrapper for Experian\'s Net Connect API',
+    'author': 'Carlos Eduardo Rivera',
+    'url': 'https://github.com/AbleEng/pyexperian',
+    'download_url': 'https://github.com/AbleEng/pyexperian',
+    'author_email': 'carlos@hiable.com',
+    'version': '0.5',
+    'install_requires': ['nose', 'requests', 'xmltodict'],
+    'packages': ['pyexperian', 'pyexperian.test', 'pyexperian.lib'],
+    'scripts': [],
+    'name': 'pyexperian'
+}
+
+setup(**config)
