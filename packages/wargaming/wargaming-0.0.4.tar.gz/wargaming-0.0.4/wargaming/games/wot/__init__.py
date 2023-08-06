6eae8c0eb91446274944eadca4edc4fb2c5cc51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:23 +0000

    Add package wargaming/0.0.4/wargaming-0.0.4.tar.gz

---
+++ packages/wargaming/wargaming-0.0.4.tar.gz/wargaming-0.0.4/wargaming/games/wot/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import six
+
+from wargaming.api import BaseAPI, MetaAPI
+from wargaming.games.wot.accounts import Accounts
+from wargaming.games.wot.auth import Auth
+from wargaming.games.wot.clans import Clans
+from wargaming.games.wot.globalwar import GlobalWar
+
+
+__all__ = ('API', )
+
+
+@six.add_metaclass(MetaAPI)
+class API(BaseAPI):
+
+    def __init__(self, application_id, base_url='https://api.worldoftanks.com/wot/'):
+        super(API, self).__init__(application_id, base_url)
+
+    accounts = Accounts()
+
+    auth = Auth()
+
+    clans = Clans()
+
+    globalwar = GlobalWar()
