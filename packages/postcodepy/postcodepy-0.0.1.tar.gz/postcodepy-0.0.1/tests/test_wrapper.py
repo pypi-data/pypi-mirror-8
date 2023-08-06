1d6c93f681210afa1dfb4bfecdd2c3fb6788f3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:09 +0000

    Add package postcodepy/0.0.1/postcodepy-0.0.1.tar.gz

---
+++ packages/postcodepy/postcodepy-0.0.1.tar.gz/postcodepy-0.0.1/tests/test_wrapper.py
@@ -0,0 +1,34 @@
+import unittest
+from postcodepy.postcodepy import API, PostcodeError
+
+access_key="WY7vmPZN9e0p1W4qdaYKbHyDfL7J94YgbIp2z944oZE"
+access_secret="c57Og9t8Lb14A8mHru9BEHwzdoyF7cBqb3xV5rQB4aC"
+
+class TestUM(unittest.TestCase):
+
+    def test_Postcode(self):
+      """ test retrieval of data
+      """
+      api = API( environment='live', access_key=access_key, access_secret=access_secret)
+      pc = ('1071XX', 1)
+      retValue = api.get_postcodedata( pc[0], pc[1] )
+      self.assertEqual( retValue['city'], "Amsterdam" ) and \
+            self.assertEqual( retValue['street'], "Museumstraat" ) 
+
+    def test_PostcodeFail(self):
+      """ no data for this postcode, a request that should fail 
+      """
+      api = API( environment='live', access_key=access_key, access_secret=access_secret)
+      pc = ('1077XX', 1)
+      self.assertRaises( PostcodeError, lambda: api.get_postcodedata( pc[0], pc[1] ))
+
+    def test_SecretFail(self):
+      """ no secret : a request that should fail 
+      """
+      self.assertRaises( PostcodeError,  lambda: API( environment='live', access_key=access_key))
+
+    def test_KeyFail(self):
+      """ no key : a request that should fail 
+      """
+      self.assertRaises( PostcodeError,  lambda: API( environment='live', access_secret=access_secret))
+
