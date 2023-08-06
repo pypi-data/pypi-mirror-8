301c736b13854f58648b398354c63cd9fd14ab99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:39 +0000

    Add package timezones/1.8.1/timezones-1.8.1.tar.gz

---
+++ packages/timezones/timezones-1.8.1.tar.gz/timezones-1.8.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# Copyright (c) 2007 Qtrac Ltd. All rights reserved.
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+
+from setuptools import setup
+
+setup(name='timezones',
+      version = '1.8.1',
+      author="amix",
+      author_email="amix@amix.dk",
+      url="http://www.amix.dk/",
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      packages=['timezones'],
+      install_requires=[
+        'pygeoip==0.2.5',
+        'pytz'
+      ],
+      platforms=["Any"],
+      license="MIT",
+      keywords='timezones timezone pytz',
+      description="A Python library that provides better selection of common timezones, can output HTML and auto select the best timezone based on user's IP.",
+      long_description="""\
+timezones
+---------------
+
+A Python library that provides better selection of common timezones, can output HTML and auto select the best timezone based on user's IP.
+
+Visit http://doist.github.com/python-timezones/ for more information.
+
+Copyright: 2012 by Doist
+License: MIT.""")
---
+++ packages/timezones/timezones-1.8.1.tar.gz/timezones-1.8.1/timezones/test_timezones.py
@@ -0,0 +1,58 @@
+import zones
+import tz_utils
+
+
+def test_sort():
+    # +04:00 is bigger than +04:30
+    assert zones._sort_by_tzoffset('+0400', '+0430') == -1
+
+    # +04:00 equal to +04:00
+    assert zones._sort_by_tzoffset('+0400', '+0400') == 0
+
+    # +05:00 is bigger to +04:30
+    assert zones._sort_by_tzoffset('+0500', '+0430') == 1
+
+    # +01:00 is smaller than +04:00
+    assert zones._sort_by_tzoffset('+0100', '+0400') == -1
+
+    # -10:00 is smaller than +04:30
+    assert zones._sort_by_tzoffset('-1000', '+0430') == -1
+
+    # -05:00 is bigger than -10:00
+    assert zones._sort_by_tzoffset('-0500', '-1030') == 1
+
+    # 00:00 is bigger than -10:00
+    assert zones._sort_by_tzoffset('+0000', '-1030') == 1
+
+
+def test_get_timezone():
+    assert tz_utils.get_timezone('Europe/Moscow') != None
+    assert tz_utils.get_timezone('Europe/Moscow1') == None
+    assert tz_utils.get_timezone('GMT +1:00') != None
+
+    assert tz_utils.is_valid_timezone('GMT +1:00') == True
+    assert tz_utils.is_valid_timezone('Europe/Moscow') == True
+    assert tz_utils.is_valid_timezone('Europe/Moscow1') == False
+
+
+def test_guess_timezone():
+    tz_utils.GEOIP_DATA_LOCATION = '/usr/local/geo_ip/GeoLiteCity.dat'
+    assert tz_utils.guess_timezone_by_ip('201.246.115.62', only_name=True) == 'Chile/Continental'
+    assert tz_utils.guess_timezone_by_ip('000.000.000.000', only_name=True) == None
+    assert tz_utils.guess_timezone_by_ip('127.0.0.1', only_name=True) == None
+
+
+def test_guess_timezone_by_javascript():
+    asia_yekaterinburg = tz_utils.get_timezone('Asia/Yekaterinburg')
+    for i in xrange(2):  # to ensure that cache breaks nothing
+        assert tz_utils.guess_timezone_by_javascript('Tue Feb 01 2005 00:00:00 GMT+0500 (YEKT)') == asia_yekaterinburg
+        assert tz_utils.guess_timezone_by_javascript('Mon Aug 01 2005 00:00:00 GMT+0600 (YEKST)') == asia_yekaterinburg
+        assert tz_utils.guess_timezone_by_javascript('Mon Aug 01 2005 00:00:00 GMT+0600 (OOOPS)') == None
+
+    assert tz_utils.guess_timezone_by_javascript('Tue Feb 01 2005 00:00:00 GMT+0500 (YEKT)', True) == 'Asia/Yekaterinburg'
+    assert tz_utils.guess_timezone_by_javascript('Mon Aug 01 2005 00:00:00 GMT+0600 (YEKST)', True) == 'Asia/Yekaterinburg'
+    assert tz_utils.guess_timezone_by_javascript('Mon Aug 01 2005 00:00:00 GMT+0600 (OOOPS)', True) == None
+
+
+def test_get_timezonez():
+    assert len(list(zones.get_timezones(only_us=True))) == 8
