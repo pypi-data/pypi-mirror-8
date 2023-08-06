c028dcc8a68928f67127ffa0069e3206d0f59210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:50 +0000

    Add package alexer/0.2/alexer-0.2.tar.gz

---
+++ packages/alexer/alexer-0.2.tar.gz/alexer-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'alexer',
+    packages = ['alexer'],
+    version = '0.2',
+    description='dead simple lexer',
+    author = 'Kyumin Kim',
+    author_email = 'math4tots@gmail.com',
+    url = 'https://github.com/math4tots/alexer',
+    download_url = 'https://github.com/math4tots/alexer/0.2',
+    keywords = ['lexer'],
+    classifiers = [])
+
