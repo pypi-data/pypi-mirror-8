c810ca045252595215586e29f7327822349ef8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:01 +0000

    Add package xmastree/0.0.4/xmastree-0.0.4.tar.gz

---
+++ packages/xmastree/xmastree-0.0.4.tar.gz/xmastree-0.0.4/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from xmastree import __version__
+
+readme = open('README.md').read()
+
+requirements = [
+    'termcolor'
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='xmastree',
+    version=__version__,
+    description='merry xmas',
+    long_description=readme,
+    author='axiaoxin',
+    author_email='254606826@qq.com',
+    url='https://github.com/axiaoxin/xmastree',
+    packages=[
+        'xmastree',
+    ],
+    package_dir={'xmastree':
+                 'xmastree'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='xmastree',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+    entry_points={
+        'console_scripts': ['xmastree = xmastree.xmastree:main']
+    },
+    tests_require=test_requirements
+)
