1663ae01f1a3f443c5c2a22294fa9dc4601243ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:51 +0000

    Add package SakaiPy/1.1.0/SakaiPy-1.1.0.tar.gz

---
+++ packages/SakaiPy/SakaiPy-1.1.0.tar.gz/SakaiPy-1.1.0/SakaiPy/SakaiTools/UserInfo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import SakaiPy.RequestGenerator
+
+
+
+class UserInfo(object):
+
+	def __init__(self, rq):
+        self.requester = rq
+
+    def getUserInfoObjectByNetID(self,netid):
+    	print "jhere"
+
+
+	def getOwnUserInfo(self):
+		
