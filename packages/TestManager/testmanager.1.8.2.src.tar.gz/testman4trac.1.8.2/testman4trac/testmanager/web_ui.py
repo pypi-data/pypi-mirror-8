30f95410b9c33c65809ad1978b5249262205e74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:19 +0000

    Add package TestManager/1.8.2/testmanager.1.8.2.src.tar.gz

---
+++ packages/TestManager/testmanager.1.8.2.src.tar.gz/testman4trac.1.8.2/testman4trac/testmanager/web_ui.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010-2015 Roberto Longobardi
+# 
+# This file is part of the Test Manager plugin for Trac.
+# 
+# The Test Manager plugin for Trac is free software: you can 
+# redistribute it and/or modify it under the terms of the GNU 
+# General Public License as published by the Free Software Foundation, 
+# either version 3 of the License, or (at your option) any later 
+# version.
+# 
+# The Test Manager plugin for Trac is distributed in the hope that it 
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with the Test Manager plugin for Trac. See the file LICENSE.txt. 
+# If not, see <http://www.gnu.org/licenses/>.
+#
+
+from genshi.builder import tag
+from trac.core import Component, implements
+from trac.web.chrome import ITemplateProvider, INavigationContributor
+
+
+try:
+    from testmanager.api import _, tag_, N_
+except ImportError:
+	from trac.util.translation import _, N_
+	tag_ = _
+
+class TestManagerTemplateProvider(Component):
+    """Provides templates and static resources for the TestManager plugin."""
+
+    implements(ITemplateProvider)
+
+    # ITemplateProvider methods
+    def get_templates_dirs(self):
+        """
+        Return the absolute path of the directory containing the provided
+        Genshi templates.
+        """
+        from pkg_resources import resource_filename
+        return [resource_filename(__name__, 'templates')]
+
+    def get_htdocs_dirs(self):
+        """Return the absolute path of a directory containing additional
+        static resources (such as images, style sheets, etc).
+        """
+        from pkg_resources import resource_filename
+        return [('testmanager', resource_filename(__name__, 'htdocs'))]
+
+
+class TestManager(Component):
+    """Implements the /testmanager handler and the Test Manager tab."""
+
+    implements(INavigationContributor)
+
+    # INavigationContributor methods
+    def get_active_navigation_item(self, req):
+        if 'TEST_VIEW' in req.perm:
+            return 'testmanager'
+
+    def get_navigation_items(self, req):
+        if 'TEST_VIEW' in req.perm:
+            yield ('mainnav', 'testmanager',
+                tag.a(_("Test Manager"), href=req.href.wiki()+'/TC', accesskey='M'))
+
+
