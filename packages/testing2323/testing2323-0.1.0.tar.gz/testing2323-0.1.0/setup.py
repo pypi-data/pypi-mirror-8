dd7ff2a36373137eec30ab0c1d2e5176a683881a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:32 +0000

    Add package testing2323/0.1.0/testing2323-0.1.0.tar.gz

---
+++ packages/testing2323/testing2323-0.1.0.tar.gz/testing2323-0.1.0/setup.py
@@ -0,0 +1,16 @@
+"""This file defines how taspconf is installed in a python environment.
+Please review to the README.md's Installation section on "Using
+setup.py".
+"""
+import setuptools
+
+setuptools.setup(
+    name='testing2323',
+    version='0.1.0',
+    description='a test',
+    py_modules=['testing'],
+    install_requires=[''],
+    author='Danielle Jenkins',
+    author_email='djenkins@bitkitten.net',
+    url='',
+)
