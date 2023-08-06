cb840fd98692d932ccf45f1a49a3987b90d5a292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:17 +0000

    Add package Tinkerer/1.5/Tinkerer-1.5.zip

---
+++ packages/Tinkerer/Tinkerer-1.5.zip/Tinkerer-1.5/tinkerer/__init__.py
@@ -0,0 +1,14 @@
+'''
+    Tinkerer Blog
+    ~~~~~~~~~~~~~
+
+    Blogging with Sphinx. Like a boss.
+
+    :copyright: Copyright 2011-2014 by Vlad Riscutia and contributors (see
+    CONTRIBUTORS file)
+    :license: FreeBSD, see LICENSE file
+'''
+__version__ = "1.5"
+
+master_doc = "master"
+source_suffix = ".rst"
