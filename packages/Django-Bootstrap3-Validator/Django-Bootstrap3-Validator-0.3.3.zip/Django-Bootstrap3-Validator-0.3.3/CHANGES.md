3794997662d4c026954dc9009839f3f745ed0160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:07 +0000

    Add package Django-Bootstrap3-Validator/0.3/Django-Bootstrap3-Validator-0.3.3.zip

---
+++ packages/Django-Bootstrap3-Validator/Django-Bootstrap3-Validator-0.3.3.zip/Django-Bootstrap3-Validator-0.3.3/CHANGES.md
@@ -0,0 +1,34 @@
+## 0.1
+supported validators:
+
+* between
+* callback
+* choices
+* date
+* different
+* emailAddress
+* greaterThan
+* id
+* integer
+* lessThan
+* notEmpty
+* numeric
+* phone
+* remote
+* stringLength
+* uri
+* zipCode
+
+## 0.2 
+
+* adjust some validator's implements
+* fix stringLength error
+* validator tag add requirejs argument, default is False.
+
+## 0.3
+
+* update bootstrapValidator to 0.5.3
+* add language options
+* add validator_fields 
+* add message to validators
+* add validator_requirejs_config tag
---
+++ packages/Django-Bootstrap3-Validator/Django-Bootstrap3-Validator-0.3.3.zip/Django-Bootstrap3-Validator-0.3.3/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+import os
+from setuptools import find_packages
+
+base_dir = os.path.dirname(__file__)
+setup(
+    name='Django-Bootstrap3-Validator',
+    version='0.3.3',
+    author='Xu Yuan',
+    author_email='ankh2008@gmail.com',
+    packages=find_packages(),
+    url="https://github.com/yuanxu/django-bootstrap3-validator",
+    license='LICENSE.txt',
+    description='BootstrapValidator support for Django projects',
+    include_package_data=True,
+    long_description=open(os.path.join(base_dir, 'README.md')).read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
+)
