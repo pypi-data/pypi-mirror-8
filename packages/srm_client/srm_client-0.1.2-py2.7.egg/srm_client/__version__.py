d6e5d13e0596fb120437dfc5409df956ea6734e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:27 +0000

    Add package srm_client/0.1.2/srm_client-0.1.2-py2.7.egg

---
+++ packages/srm_client/srm_client-0.1.2-py2.7.egg/srm_client/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f27d177af7a51f9d52c3492c886ce29171c1a7f4'
+__git_head_subject__ = 'Finished release v0.1.2.'
+__git_head_message__ = '* release/v0.1.2:\n  protectvms: fixing multiple vms\n  get_unprotected_datastores: handling the case there are none'
+__git_dirty_diff__ = ''
