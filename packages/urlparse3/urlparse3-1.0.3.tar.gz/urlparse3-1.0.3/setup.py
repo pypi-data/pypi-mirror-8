90783029ab3de2cd4b1074840c6526a613e2d490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:46 +0000

    Add package urlparse3/1.0.3/urlparse3-1.0.3.tar.gz

---
+++ packages/urlparse3/urlparse3-1.0.3.tar.gz/urlparse3-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "urlparse3",
+    version = "1.0.3",
+    packages = find_packages(),
+    author = "Alexander Kozin", 
+    author_email = "alexander@kozin.me",
+    description = "urlparse3 is simple and powerful url parsing tool",
+    long_description=open("README.rst").read(),
+    license = "BSD",
+    keywords = "urlparse parsing parse url",
+    
+)
