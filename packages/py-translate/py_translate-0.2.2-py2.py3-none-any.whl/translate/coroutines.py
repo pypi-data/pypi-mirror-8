4cc6c4313799cbbc5528acf8192b6fe1fc536792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:21 +0000

    Add package py-translate/0.2.2/py_translate-0.2.2-py2.py3-none-any.whl

---
+++ packages/py-translate/py_translate-0.2.2-py2.py3-none-any.whl/translate/coroutines.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+"""
+coroutines
+~~~~~~~~~~
+
+All functions definied within this file are essentially coroutines or
+helper functions for working with coroutines.
+
+Therefore members follow the coroutine pattern either as consumers, producers or
+consumer/producers.
+
+"""
+try:
+    from urllib.parse import quote
+except ImportError:
+    from urllib2 import quote
+
+from sys import stdin, stdout
+from functools import wraps, partial, reduce
+from multiprocessing.dummy import Pool as ThreadPool
+
+__all__ = 'coroutine', 'chunk', 'spool', 'source', 'set_task', 'write_stream'
+
+# TODO: Get rid of this global variable
+MAX_WORK = 10
+
+def coroutine(func):
+    """
+    Initializes coroutine essentially priming it to the yield statement.
+    Used as a decorator over functions that generate coroutines.
+
+    .. code-block:: python
+
+        # Basic coroutine producer/consumer pattern
+        from translate import coroutine
+
+        @coroutine
+        def coroutine_foo(bar):
+            try:
+                while True:
+                    baz = (yield)
+                    bar.send(baz)
+
+            except GeneratorExit:
+                bar.close()
+
+    :param func: Unprimed Generator
+    :type func: Function
+
+    :return: Initialized Coroutine
+    :rtype: Function
+    """
+
+    @wraps(func)
+    def initialization(*args, **kwargs):
+
+        start = func(*args, **kwargs)
+        next(start)
+
+        return start
+
+    return initialization
+
+
+def write_stream(script):
+    """
+    :param script: Translated Text
+    :type script: Iterable
+
+    :return None:
+    """
+
+    list(map(stdout.write,
+        (lines['trans'] for trans in script for lines in trans['sentences'])
+    ))
+
+    stdout.write('\n')
+
+
+@coroutine
+def set_task(translation_function, source, dest):
+    """
+    Task Setter Coroutine
+
+    :param translation_function: Translator
+    :type translation_function: Function
+
+    :param source: Source Language Code
+    :type source: String
+
+    :param dest: Destination Language Code
+    :type dest: String
+    """
+    tasks       = []
+    workers     = ThreadPool(MAX_WORK)
+    interpreter = partial(translation_function, source, dest)
+
+    try:
+        while True:
+            tasks = yield
+            write_stream(workers.map(interpreter, tasks))
+
+    except GeneratorExit:
+        write_stream(workers.map(interpreter, tasks))
+        workers.close(); workers.join()
+
+@coroutine
+def chunk(task):
+    """
+    Chunk text into a queue and send by copy to a task setter coroutine.
+    Once a queue has been built it is sent by copy and then destroyed.
+
+    :param task: Task setter
+    :type task: Coroutine
+    """
+    task_queue = []
+
+    try:
+        while True:
+
+            while len(task_queue) < MAX_WORK:
+                line = yield
+                task_queue.append(line)
+
+            task.send(task_queue)
+            task_queue = []
+
+    except GeneratorExit:
+        task.send(task_queue)
+        task.close()
+
+
+@coroutine
+def spool(iterable, maxlen=1500):
+    """
+    Consumes text streams and spools them together for more io efficient processes.
+
+    :param iterable: Sends text stream for further processing
+    :type iterable: Coroutine
+
+    :param maxlen: Maximum query string size
+    :type maxlen: Integer
+    """
+    words = 0
+    spool = ''
+
+    try:
+        while True:
+
+            while words < maxlen:
+                stream = yield
+                spool += stream
+                words += len(quote(stream).encode('utf-8'))
+
+            iterable.send(spool)
+            words = 0
+            spool = str()
+
+    except GeneratorExit:
+        iterable.send(spool)
+        iterable.close()
+
+
+# TODO: Implement FileIO
+def source(target):
+    """
+    Coroutine starting point. Produces text stream and forwards to consumers
+
+    :param target: Target coroutine consumer
+    :type target: Coroutine
+    """
+
+    if stdin.isatty():
+        # target.send(args.file)
+        return
+
+    for line in stdin:
+        target.send(line)
+
+    target.close()
---
+++ packages/py-translate/py_translate-0.2.2-py2.py3-none-any.whl/translate/translator.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+translator
+~~~~~~~~~~
+
+Defines the interaction with the translation service.
+Since the program interfaces with the google web service, this
+module deals with the client side logic of pushing the translation request
+to the the server.
+"""
+
+import json
+import functools
+
+try:
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+
+except ImportError:
+    from urllib2 import urlopen, Request
+    from urllib import urlencode
+
+from .__version__ import __version__ as version
+from .__version__ import __build__ as build
+
+
+__all__ = 'push_url', 'translator'
+
+def push_url(site):
+    '''
+    Decorates a function returning the url of translation API.
+    Creates and maintains HTTP connection state
+
+    Returns a dict response object from the server containing the translated
+    text and metadata of the request body
+
+    :param site: translator
+    :type site: Function
+
+    :return: HTTP Response
+    :rtype: Function
+    '''
+
+    @functools.wraps(site)
+    def connection(*args, **kwargs):
+
+        req_stream = str()
+        req        = None
+
+        agent   = 'py-translate v{version} {build}'.format(**globals())
+        headers = {'User-Agent': agent}
+        url     = site(*args, **kwargs)
+        request = Request(url, headers=headers)
+
+        try:
+            req        = urlopen(request)
+            req_stream = req.read()
+            req_stream = req_stream.decode('utf-8')
+
+        finally:
+            req.close()
+
+        return json.loads(req_stream)
+
+    return connection
+
+
+@push_url
+def translator(source, target, phrase):
+    """
+    Returns the url encoded string that will be pushed to the translation
+    server for parsing.
+
+    List of acceptable language codes for source and target languages
+    can be found as a JSON file in the etc directory.
+
+    Some source languages are limited in scope of the possible target languages
+    that are available.
+
+    .. code-block:: python
+
+        >>> from translate import translator
+        >>> translator('en', 'zh-TW', 'Hello World!')
+            '你好世界！'
+
+    :param source: Language code for translation source
+    :type source: String
+
+    :param target: Language code that source will be translate into
+    :type target: String
+
+    :param phrase: Text body string that will be url encoded and translated
+    :type phrase: String
+
+    :return: url
+    :rtype: String
+    """
+    base   = 'http://translate.google.com/translate_a/t'
+    params = urlencode(
+        {
+        'client': 'webapp',
+            'ie': 'utf-8',
+            'oe': 'utf-8',
+            'sl': source,
+            'tl': target,
+             'q': phrase,
+        }
+    )
+    url    = '?'.join([base, params])
+
+    return url
