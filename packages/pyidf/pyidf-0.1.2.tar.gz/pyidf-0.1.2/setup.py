0e65136f11d77c0ac94890753295e348a1eacf00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:01 +0000

    Add package pyidf/0.1.2/pyidf-0.1.2.tar.gz

---
+++ packages/pyidf/pyidf-0.1.2.tar.gz/pyidf-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from pyidf import __version__
+
+setup(name='pyidf',
+      version=__version__,
+      description='Python library to read, modify and create EnergyPlus idf files',
+      url='https://github.com/rbuffat/pyepw',
+      download_url='https://github.com/rbuffat/pyidf/tarball/0.1.2',
+      author='Rene Buffat',
+      author_email='buffat@gmail.com',
+      license='Apache License 2.0',
+      classifiers=['Development Status :: 2 - Pre-Alpha'],
+      packages=['pyidf'],
+      keywords=['EnergyPlus', 'IDF'])
