65b66b41bf000f0a4dbe78e8c4bae5a852afed43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:03 +0000

    Add package gtf_to_genes/1.40/gtf_to_genes-1.40.tar.gz

---
+++ packages/gtf_to_genes/gtf_to_genes-1.40.tar.gz/gtf_to_genes-1.40/gtf_to_genes/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+################################################################################
+#
+#   __init__.py
+#
+#
+#   Copyright (c) 11/3/2010 Leo Goodstadt
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to deal
+#   in the Software without restriction, including without limitation the rights
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#   THE SOFTWARE.
+#################################################################################
+from gene import t_parse_gtf, t_gene, t_transcript
+from index_gtf_files import (
+            index_gtf_files,
+            get_indexed_genes_for_identifier,
+            get_indexed_genes_matching_gtf_file_name,
+            get_indexed_genes_matching_identifier)
+from gene_utilities import index_genes_by_gene_id, index_transcripts
+import gtf_to_genes_version
+__version__=gtf_to_genes_version.__version
+
+
+
+
+
+
