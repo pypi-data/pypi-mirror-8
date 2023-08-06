9b99fc91d8d054e3d20309c4f2856f7f1dcc3c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:52 +0000

    Add package moves/0.1/moves-0.1.tar.gz

---
+++ packages/moves/moves-0.1.tar.gz/moves-0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='moves',
+      version='0.1',
+      description='Moves Client API',
+      author='Derek Arnold',
+      author_email='derek@derekarnold.net',
+      url='https://github.com/lysol/moves',
+      download_url='http://github.com/lysol/moves/archive/v0.1.tar.gz',
+      packages=['moves'],
+      # install_requires=open('requirements.txt').read(),
+      # long_description=open('README.md').read(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      license='MIT'
+      )
