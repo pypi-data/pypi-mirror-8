2e55096d0f41c56d037e89b6e8e5f0a80eda3c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package PyReact/0.5.0/PyReact-0.5.0.tar.gz

---
+++ packages/PyReact/PyReact-0.5.0.tar.gz/PyReact-0.5.0/react/test/files/test.js
@@ -0,0 +1,7 @@
+var testComponent =
+  React.createElement("div", null, 
+    React.createElement("ul", null, 
+      React.createElement("li", null, "Hello"), 
+      React.createElement("li", null, "World")
+    )
+  );
