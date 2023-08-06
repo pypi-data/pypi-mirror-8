9159d3d8a6fc6096031ee6284cf7b8e964572f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:00 +0000

    Add package sacad/1.1.0/sacad-1.1.0.tar.gz

---
+++ packages/sacad/sacad-1.1.0.tar.gz/sacad-1.1.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+VERSION = "1.1.0"
+
+
+with open("requirements.txt", "rt") as f:
+  requirements= f.read().splitlines()
+
+setup(name="sacad",
+      version=VERSION,
+      author="desbma",
+      packages=find_packages(),
+      entry_points={"console_scripts": ["sacad = sacad:cl_main"]},
+      package_data={"": ["LICENSE", "README.md", "requirements.txt"]},
+      test_suite="tests",
+      install_requires=requirements,
+      description="Search and download music album covers",
+      url="https://github.com/desbma/sacad",
+      download_url="https://github.com/desbma/sacad/archive/%s.tar.gz" % (VERSION),
+      keywords=["dowload", "album", "cover", "art", "albumart", "music"],
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "Environment :: Console",
+                   "Intended Audience :: End Users/Desktop",
+                   "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Programming Language :: Python :: 3.3",
+                   "Programming Language :: Python :: 3.4",
+                   "Topic :: Internet :: WWW/HTTP",
+                   "Topic :: Multimedia :: Graphics",
+                   "Topic :: Utilities"])
