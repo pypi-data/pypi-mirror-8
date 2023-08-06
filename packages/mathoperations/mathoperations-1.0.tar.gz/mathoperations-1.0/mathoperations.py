653753bad522896a0bcc14b0e2be9ac321eb1476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:58 +0000

    Add package mathoperations/1.0/mathoperations-1.0.tar.gz

---
+++ packages/mathoperations/mathoperations-1.0.tar.gz/mathoperations-1.0/mathoperations.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+def square(a):
+    return a * a
+
+def cube(a):
+    return a * a * a
+
+def root(a):
+    return pow(a,0.5)
+
---
+++ packages/mathoperations/mathoperations-1.0.tar.gz/mathoperations-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='mathoperations',
+      version='1.0',
+      py_modules = ['mathoperations'],
+      description='Simple Math Operations library',
+      author='Vijayasarathy',
+      author_email='shanv82@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',      
+     )
