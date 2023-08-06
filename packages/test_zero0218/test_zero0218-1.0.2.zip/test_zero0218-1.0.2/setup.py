7695c412d6f839283b3f210d35175f6dd97ec531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:13 +0000

    Add package test_zero0218/1.0.2/test_zero0218-1.0.2.zip

---
+++ packages/test_zero0218/test_zero0218-1.0.2.zip/test_zero0218-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = "test_zero0218",
+	version = "1.0.2",
+	py_modules = ["test_zero0218"],
+	author = "zhanglei",
+	author_email = "372414985@qq.com",
+	url = "http://www.zhanglei.ac.cn",
+	description = "This is my test def",
+)
