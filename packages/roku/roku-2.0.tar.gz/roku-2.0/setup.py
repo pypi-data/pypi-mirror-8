719a95a8e27919facc5bc012f710a6794d7183a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:23 +0000

    Add package roku/2.0/roku-2.0.tar.gz

---
+++ packages/roku/roku-2.0.tar.gz/roku-2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='roku',
+    version='2.0',
+    description='Client for the Roku media player',
+    long_description=readme,
+    author='Jeremy Carbaugh',
+    author_email='jcarbaugh@gmail.com',
+    url='https://github.com/jcarbaugh/python-roku',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.1.0',
+        'six'
+    ],
+    license='BSD License',
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
