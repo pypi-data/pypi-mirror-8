e14f9e832b9a9bcb5cff8cac589f87597d4c0452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:36 +0000

    Add package quality-report/1.14.0/quality_report-1.14.0-py2.7.egg

---
+++ packages/quality-report/quality_report-1.14.0-py2.7.egg/qualitylib/domain/__init__.py
@@ -0,0 +1,37 @@
+'''
+Copyright 2012-2014 Ministerie van Sociale Zaken en Werkgelegenheid
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+# Package for domain objects.
+
+from qualitylib.domain.base import DomainObject
+
+from qualitylib.domain.measurement.metric import Metric, HigherIsBetterMetric, \
+    HigherPercentageIsBetterMetric, LowerPercentageIsBetterMetric, \
+    LowerIsBetterMetric
+from qualitylib.domain.measurement.metric_source import MetricSource, \
+    MissingMetricSource
+from qualitylib.domain.measurement.metric_mixin import MetaMetricMixin
+from qualitylib.domain.measurement.target import TechnicalDebtTarget, \
+    DynamicTechnicalDebtTarget
+
+from qualitylib.domain.software_development.product import Product
+from qualitylib.domain.software_development.team import Team
+from qualitylib.domain.software_development.person import Person
+from qualitylib.domain.software_development.project import Project
+from qualitylib.domain.software_development.street import Street
+from qualitylib.domain.software_development.document import Document
+from qualitylib.domain.software_development.quality_attribute import \
+    QualityAttribute
