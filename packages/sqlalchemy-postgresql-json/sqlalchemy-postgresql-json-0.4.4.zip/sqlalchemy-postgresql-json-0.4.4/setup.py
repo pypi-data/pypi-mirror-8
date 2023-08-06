94d488d6dab73596bb3133af59347d89f7c46c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:04 +0000

    Add package sqlalchemy-postgresql-json/0.4.4/sqlalchemy-postgresql-json-0.4.4.zip

---
+++ packages/sqlalchemy-postgresql-json/sqlalchemy-postgresql-json-0.4.4.zip/sqlalchemy-postgresql-json-0.4.4/setup.py
@@ -0,0 +1,19 @@
+""" sqlalchemy_postgresql_json
+	Adds JSON support to SQLAlchemy, with magic helpers to keep things mutable
+	And now adds LTREE support as well
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlalchemy-postgresql-json',
+    author="Franklyn Tackitt",
+    author_email="franklyn@tackitt.net",
+    url="https://github.com/DisruptiveLabs/sqlalchemy_postgresql_json",
+    download_url="https://github.com/DisruptiveLabs/sqlalchemy_postgresql_json/tarball/0.4.4",
+    version="0.4.4",
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['psycopg2', 'sqlalchemy'])
---
+++ packages/sqlalchemy-postgresql-json/sqlalchemy-postgresql-json-0.4.4.zip/sqlalchemy-postgresql-json-0.4.4/sqlalchemy_postgresql_json/__init__.py
@@ -0,0 +1,16 @@
+from pgjson import *
+from ltree import *
+from mutable import *
+
+__all__ = ['monkey_patch_array',
+           'monkey_patch_json',
+           'monkey_patch_all',
+           'JSONMutableDict',
+           'JSONMutableList',
+           'JSON',
+           'register_json',
+           'LTXTQUERY',
+           'LTREE',
+           'LQUERY',
+           'MutableList',
+           'MutableDict']
