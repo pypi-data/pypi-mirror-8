16e696ec78b0b71dcd07fb54a6aae143abd6a2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:18 +0000

    Add package jmbo-foundry/1.2.6.1/jmbo-foundry-1.2.6.1.tar.gz

---
+++ packages/jmbo-foundry/jmbo-foundry-1.2.6.1.tar.gz/jmbo-foundry-1.2.6.1/setup.py
@@ -0,0 +1,71 @@
+import multiprocessing
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-foundry',
+    version='1.2.6.1',
+    description='Jmbo Foundry ties together the various Jmbo products enabling you to rapidly build multilingual web and mobi sites with the minimum amount of code and customization.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-foundry',
+    packages = find_packages(),
+    dependency_links = [
+    ],
+    install_requires = [
+        # todo: attempt to get rid of these five
+        'django-section',
+        'django-gizmo',
+        'django-generate',
+        'django-registration',
+        'django-snippetscream',
+
+        'django-googlesearch',
+        'django-export',
+        'django-simple-autocomplete',
+        'django-pagination',
+        'django-object-tools>=0.0.5',
+        'django-debug-toolbar',
+        'django_compressor',
+
+        'jmbo>=1.1',
+        'jmbo-gallery>=0.2.1',
+        'jmbo-music',
+        'jmbo-calendar',
+        'jmbo-chart',
+        'jmbo-post>=0.1.2',
+        'jmbo-show>=0.2',
+        'jmbo-banner>=0.2.2',
+        'jmbo-competition',
+        'jmbo-contact>=0.1.2',
+        'jmbo-poll',
+        'jmbo_analytics',
+        'jmbo-friends',
+	    'jmbo-downloads',
+        'jmbo_twitter',
+        'jmbo_sitemap>=0.1',
+
+        'jellyfish',
+        'BeautifulSoup',
+        'PyJWT==0.1.6',
+        'requests',
+        'gunicorn',
+        'python-memcached',
+    ],
+    include_package_data=True,
+    tests_require=[
+        'django-setuptest>=0.1.4',
+    ],
+    test_suite="setuptest.setuptest.SetupTestSuite",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
