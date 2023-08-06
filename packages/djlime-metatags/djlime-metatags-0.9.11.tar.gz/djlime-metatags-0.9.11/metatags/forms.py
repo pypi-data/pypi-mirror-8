749a9d19f1de14354e7124577294420da56233f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:28 +0000

    Add package djlime-metatags/0.9.11/djlime-metatags-0.9.11.tar.gz

---
+++ packages/djlime-metatags/djlime-metatags-0.9.11.tar.gz/djlime-metatags-0.9.11/metatags/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+
+from .models import MetaTag
+
+
+class InlineMetaTagForm(forms.ModelForm):
+
+    class Meta:
+        fields = ('title', 'keywords', 'description')
+
+    class Media:
+        css = {
+            'all': ('css/meta_tags.css',)
+        }
+
+
+class MetaTagForm(InlineMetaTagForm):
+    url = forms.RegexField(label=_('URL-path'), max_length=100, regex=r'^[-\w/\.~]+$',
+                           help_text=_("Example: '/about/contact/'. Make sure to have leading "
+                                       "and trailing slashes."),
+                           error_message=_("This value must contain only letters, numbers,"
+                                           "dots, underscores, dashes, slashes or tildes."))
+
+    class Meta:
+        fields = ('url', 'title', 'keywords', 'description')
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+
+        if not url.startswith('/'):
+            raise forms.ValidationError(_('URL is missing a leading slash.'))
+
+        if (settings.APPEND_SLASH and 'django.middleware.common.CommonMiddleware' in settings.MIDDLEWARE_CLASSES and
+                not url.endswith('/')):
+            raise forms.ValidationError(_('URL is missing a trailing slash.'))
+
+        if MetaTag.objects.filter(url=url).exists():
+            if self.instance is None or url != self.instance.url:
+                raise forms.ValidationError(_('Meta-tags for a given URL-path have already been identified.'))
+
+        return url
---
+++ packages/djlime-metatags/djlime-metatags-0.9.11.tar.gz/djlime-metatags-0.9.11/metatags/templatetags/meta_tags.py
@@ -0,0 +1,50 @@
+from django.template import Library
+from django.utils.encoding import force_text
+
+from metatags.models import MetaTag
+from metatags.utils import truncate_language_code
+
+
+register = Library()
+
+
+def _get_page_title(page_object, page_title_field):
+    return getattr(page_object, page_title_field, force_text(page_object))
+
+
+@register.inclusion_tag('metatags/_meta_tags.html', takes_context=True)
+def include_meta_tags(context, page_object=None, page_title_field='title',
+                      default_title='', default_keywords='', default_description=''):
+    if page_object is not None:
+        # Get the meta tags for the object
+        try:
+            meta_tags = MetaTag.objects \
+                .get(object_id=page_object.id,
+                     content_type__app_label=page_object._meta.app_label,
+                     content_type__model=page_object._meta.module_name)
+            # Get not blank title
+            meta_tags.title = meta_tags.title or \
+                _get_page_title(page_object, page_title_field)
+            meta_tags.keywords = meta_tags.keywords or default_keywords
+            meta_tags.description = meta_tags.description or default_description
+        except MetaTag.DoesNotExist:
+            meta_tags = {
+                'title': _get_page_title(page_object, page_title_field),
+                'keywords': default_keywords,
+                'description': default_description
+            }
+    else:
+        # Get the meta tags for the URL-path
+        try:
+            url_path = truncate_language_code(context['request'].path_info)
+            meta_tags = MetaTag.objects.get(url=url_path)
+            meta_tags.title = meta_tags.title or default_title
+            meta_tags.keywords = meta_tags.keywords or default_keywords
+            meta_tags.description = meta_tags.description or default_description
+        except (KeyError, MetaTag.DoesNotExist):
+            meta_tags = {
+                'title': default_title,
+                'keywords': default_keywords,
+                'description': default_description
+            }
+    return {'meta_tags': meta_tags}
