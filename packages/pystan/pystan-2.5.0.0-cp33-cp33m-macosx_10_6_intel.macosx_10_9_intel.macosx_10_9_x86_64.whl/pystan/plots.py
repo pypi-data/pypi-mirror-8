9536c50b62ac11749a5c4a25b40217d4c42deffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:10 +0000

    Add package pystan/2.5.0.0/pystan-2.5.0.0-cp33-cp33m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.whl

---
+++ packages/pystan/pystan-2.5.0.0-cp33-cp33m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.whl/pystan/plots.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger('pystan')
+
+
+def traceplot(fit, pars):
+    """Use pymc's traceplot to display parameters"""
+    # FIXME: eventually put this in the StanFit object
+    # FIXME: write a to_pymc(_trace) function
+    try:
+        from pystan.external.pymc import plots
+    except ImportError:
+        logger.critical("matplotlib required for plotting.")
+        raise
+    return plots.traceplot(fit.extract(), pars)
