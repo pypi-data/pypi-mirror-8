98ea08debcfd35b26337d62ccd7c93d43627b212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:35 +0000

    Add package nagios-taskserver-plugin/0.2.6/nagios-taskserver-plugin-0.2.6.tar.gz

---
+++ packages/nagios-taskserver-plugin/nagios-taskserver-plugin-0.2.6.tar.gz/nagios-taskserver-plugin-0.2.6/nagios_taskserver_plugin/main.py
@@ -0,0 +1,31 @@
+import subprocess
+import time
+
+
+class FailedToSynchronize(Exception):
+    pass
+
+
+def main(*args):
+    config_path = args[0]
+
+    started = time.time()
+    cmd = [
+        'task',
+        'rc:%s' % config_path,
+        'sync',
+    ]
+    proc = subprocess.Popen(
+        cmd,
+        shell=False,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    _, stderr = proc.communicate()
+    if proc.returncode != 0:
+        raise FailedToSynchronize(stderr)
+    finished = time.time()
+
+    total_duration = finished - started
+
+    return "Sync Successful", "time=%ss;0.5;5.0;0;99" % total_duration
