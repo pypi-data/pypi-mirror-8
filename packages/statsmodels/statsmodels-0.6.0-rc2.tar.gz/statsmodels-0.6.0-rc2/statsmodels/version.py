14293cf1ef45cd0e27514f0ebc7f579484fdc7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:05 +0000

    Add package statsmodels/0.6.0-rc2/statsmodels-0.6.0-rc2.tar.gz

---
+++ packages/statsmodels/statsmodels-0.6.0-rc2.tar.gz/statsmodels-0.6.0-rc2/statsmodels/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.6.0-rc2'
+version = '0.6.0-rc2'
+full_version = '0.6.0-rc2'
+git_revision = 'e46317f670f1505cf4fff3246aeaa496a2652fe7'
+release = True
+
+if not release:
+    version = full_version
