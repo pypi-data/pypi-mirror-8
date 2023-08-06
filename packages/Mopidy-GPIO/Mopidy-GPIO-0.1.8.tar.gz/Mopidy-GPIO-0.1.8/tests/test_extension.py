b9b1ae291165d8a2d579c2c74a5c68101e64a14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:02 +0000

    Add package Mopidy-GPIO/0.1.8/Mopidy-GPIO-0.1.8.tar.gz

---
+++ packages/Mopidy-GPIO/Mopidy-GPIO-0.1.8.tar.gz/Mopidy-GPIO-0.1.8/tests/test_extension.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+import unittest
+
+# from mopidy_ttsgpio import frontend as frontend_lib
+from mopidy_gpio import Extension
+
+
+class ExtensionTest(unittest.TestCase):
+
+    def test_get_default_config(self):
+        ext = Extension()
+
+        config = ext.get_default_config()
+
+        self.assertIn('[gpio]', config)
+        self.assertIn('enabled = true', config)
+
+    def test_get_config_schema(self):
+        ext = Extension()
+        schema = ext.get_config_schema()
+        self.assertIn('pin_button_main', schema)
+
+        # TODO Test the content of your config schema
+        # self.assertIn('username', schema)
+        # self.assertIn('password', schema)
+
+    # TODO Write more tests
