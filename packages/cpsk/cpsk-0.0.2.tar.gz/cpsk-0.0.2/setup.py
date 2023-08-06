6b0739cbdd957b29e024129d084544c26fcc0f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:01 +0000

    Add package cpsk/0.0.2/cpsk-0.0.2.tar.gz

---
+++ packages/cpsk/cpsk-0.0.2.tar.gz/cpsk-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='cpsk',
+    
+    description='an unofficial api for grabbing data from cp.sk',
+    author='Adrian Matejov',
+    author_email='a.matejov@centrum.sk',
+    version='0.0.2',
+   
+    url='https://github.com/Adman/python-cpsk-api',
+    
+    include_package_data=True,
+    packages=['cpsk'],
+
+    license=open('./LICENSE').read()
+)
