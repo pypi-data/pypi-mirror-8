c0d40046d075e20408476f4c8ea9dfba32ba4580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:03 +0000

    Add package libfilecreator/1.0/libfilecreator-1.0.tar.gz

---
+++ packages/libfilecreator/libfilecreator-1.0.tar.gz/libfilecreator-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='libfilecreator',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/darthlukan/libfilecreator',
+    license='GPLv3',
+    author='Brian Tomlinson',
+    author_email='brian@smartm.no',
+    description='Creates a file populated with data based on the desired file type and passed in file content values.'
+)
