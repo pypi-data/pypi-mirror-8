9ca6fb2bccb037f300672e34beed987431e8bdf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:55 +0000

    Add package django-omnibus/0.2.0/django-omnibus-0.2.0.tar.gz

---
+++ packages/django-omnibus/django-omnibus-0.2.0.tar.gz/django-omnibus-0.2.0/README.md
@@ -0,0 +1,39 @@
+# django-omnibus
+
+Django/JavaScript WebSocket Connections.
+
+[![Coverage Status](https://coveralls.io/repos/moccu/django-omnibus/badge.png)](https://coveralls.io/r/moccu/django-omnibus)
+[![Travis Status](https://travis-ci.org/moccu/django-omnibus.png?branch=master)](https://travis-ci.org/moccu/django-omnibus)
+
+## What is django-omnibus
+
+*django-omnibus* is a Django library which helps to create websocket-based
+connections between a browser and a server to deliver messages.
+
+Some use cases could be:
+
+* Chat systems
+* Realtime stream updates
+* Inter-browser communication
+* file transfers
+* and so on..
+
+*django-omnibus* is quite extensible. The connection handler, the
+Tornado web application and the authenticator can be replaced by just changing
+a setting.
+
+For browser compatibility *django-omnibus* also supports
+[SockJS](https://github.com/sockjs/sockjs-client) (which provides fallbacks for
+older browsers).
+
+On the client side, django-omnibus provides a library which handles
+the connection, authentication and channel subscription (multiple channels can
+be subscribed using one connection).
+
+## Installation & Documentation
+
+All documentation is in the "docs/source" directory and online at
+[Read the Docs](https://django-omnibus.readthedocs.org/).
+
+## License
+*django-omnibus* is licenced under the [BSD License](LICENSE.md).
---
+++ packages/django-omnibus/django-omnibus-0.2.0.tar.gz/django-omnibus-0.2.0/setup.py
@@ -0,0 +1,123 @@
+import codecs
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+
+test_requires = [
+    'pytest>=2.5.2',
+    'pytest-cov>=1.6',
+    'pytest-flakes>=0.2',
+    'pytest-pep8>=1.0.5',
+    'pytest-django>=2.6',
+    'mock==1.0.1',
+    'pep8==1.4.6'
+]
+
+
+install_requires = [
+    'Django>=1.4',
+    'pyzmq==14.1.1',
+    'tornado==3.1.1',
+    'sockjs-tornado>=1.0.0',
+]
+
+
+dev_requires = [
+    'tox',
+]
+
+
+docs_requires = [
+    'sphinx',
+    'sphinx_rtd_theme'
+]
+
+
+class PyTest(TestCommand):
+    user_options = [('cov=', None, 'Run coverage'),
+                    ('cov-xml=', None, 'Generate junit xml report'),
+                    ('cov-html=', None, 'Generate junit html report'),
+                    ('junitxml=', None, 'Generate xml of test results'),
+                    ('clearcache', None, 'Clear cache first')]
+    boolean_options = ['clearcache']
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.cov = None
+        self.cov_xml = False
+        self.cov_html = False
+        self.junitxml = None
+        self.clearcache = False
+
+    def run_tests(self):
+        import pytest
+
+        params = {'args': self.test_args}
+
+        if self.cov is not None:
+            params['plugins'] = ['cov']
+            params['args'].extend(['--cov', self.cov, '--cov-report', 'term-missing'])
+            if self.cov_xml:
+                params['args'].extend(['--cov-report', 'xml'])
+            if self.cov_html:
+                params['args'].extend(['--cov-report', 'html'])
+        if self.junitxml is not None:
+            params['args'].extend(['--junitxml', self.junitxml])
+        if self.clearcache:
+            params['args'].extend(['--clearcache'])
+
+        self.test_suite = True
+
+        errno = pytest.main(**params)
+        sys.exit(errno)
+
+
+setup(
+    name='django-omnibus',
+    version='0.2.0',
+    description='Django/JavaScript WebSocket Connections',
+    long_description=read('README.md'),
+    author='Moccu GmbH & Co. KG',
+    author_email='info@moccu.com',
+    url='https://github.com/moccu/django-omnibus/',
+    packages=find_packages(exclude=[
+        'testing',
+        'testing.pytests',
+        'examples',
+    ]),
+    include_package_data=True,
+    extras_require={
+        'docs': docs_requires,
+        'tests': test_requires,
+        'dev': dev_requires,
+    },
+    test_suite='.',
+    install_requires=install_requires,
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+)
