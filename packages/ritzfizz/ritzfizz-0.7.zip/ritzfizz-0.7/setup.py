31538af6a45a01ce5654efc683f1e83ee1a4d5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:27 +0000

    Add package ritzfizz/0.7/ritzfizz-0.7.zip

---
+++ packages/ritzfizz/ritzfizz-0.7.zip/ritzfizz-0.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+  name = 'ritzfizz',
+  packages = ['ritzfizz'],
+  version = '0.7',
+  description = 'Ritzfizz Sphinx Theme',
+  author = 'Adam Temple',
+  author_email = 'adam@bixly.com',
+  url = 'https://github.com/adamhub/RitzFizz',
+  download_url = 'https://github.com/adamhub/RitzFizz/tarball/0.7',
+  keywords = ['sphinx', 'theme', 'readthedocs', 'ritzfizz'],
+  classifiers = [],
+  include_package_data=True,
+)
