8853afb3bd7903254969464aa66e0e97ab991a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:42 +0000

    Add package migratore/0.3.2/migratore-0.3.2.zip

---
+++ packages/migratore/migratore-0.3.2.zip/migratore-0.3.2/src/migratore/info.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+NAME = "migratore"
+VERSION = "0.3.2"
+AUTHOR = "Hive Solutions Lda."
+EMAIL = "development@hive.pt"
+DESCRIPTION = "Migratore Infra-structure"
+LICENSE = "GNU General Public License (GPL), Version 3"
+KEYWORDS = "migratore migration sql"
+URL = "http://migratore.hive.pt"
+COPYRIGHT = "2008-2014 Hive Solutions Lda."
