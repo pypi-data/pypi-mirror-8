bb6e44ddad8708a78696c413df6ddbef9573bdb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:25 +0000

    Add package pyhs2/0.6.0/pyhs2-0.6.0.tar.gz

---
+++ packages/pyhs2/pyhs2-0.6.0.tar.gz/pyhs2-0.6.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='pyhs2',
+    version='0.6.0',
+    author='Brad Ruderman',
+    author_email='bradruderman@gmail.com',
+    packages=['pyhs2', 'pyhs2/cloudera', 'pyhs2/TCLIService'],
+    url='https://github.com/BradRuderman/pyhs2',
+    license='LICENSE.txt',
+    description='Python Hive Server 2 Client Driver',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "sasl",
+        "thrift",
+    ],
+    test_suite='pyhs2.test',
+    tests_require=["mock"]
+
+)
