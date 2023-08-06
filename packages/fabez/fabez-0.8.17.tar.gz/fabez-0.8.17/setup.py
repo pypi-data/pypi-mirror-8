52afc10159f2960bb78c5e44dad155f3ecbb441e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:38 +0000

    Add package fabez/0.8.17/fabez-0.8.17.tar.gz

---
+++ packages/fabez/fabez-0.8.17.tar.gz/fabez-0.8.17/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from setuptools import find_packages
+
+
+import fabez
+
+setup(
+    name='fabez',
+    version=fabez.version,
+    packages=['fabez'],
+    url='https://github.com/kbonez/fabez',
+    download_url='https://github.com/kbonez/fabez/tarball/master',
+    license='http://opensource.org/licenses/MIT',
+    install_requires=[
+        'fabric',
+        'cliez'
+    ],
+    author='Breeze.Kay',
+    author_email='wangwenpei@kbonez.com',
+    description='fabric helper tools',
+    keywords='fabric,fabez,ez',
+    package_data={
+        'fabez': ['tpl/*.*']
+    },
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'fabez = fabez.main:main'
+        ]
+    },
+
+)
+
