f440dabe0aa550e4c7e92719b0de48e79682ba63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:15 +0000

    Add package Rhino/0.0.2/Rhino-0.0.2.tar.gz

---
+++ packages/Rhino/Rhino-0.0.2.tar.gz/Rhino-0.0.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+version = '0.0.2'
+
+with open('README.rst') as f:
+    README = f.read()
+
+setup(
+    name='Rhino',
+    version=version,
+    author='Stanis Trendelenburg',
+    author_email='stanis.trendelenburg@gmail.com',
+    packages=find_packages(exclude=['test*', 'example*']),
+    url='https://github.com/trendels/rhino',
+    license='MIT',
+    description='A microframework for building RESTful web services',
+    long_description=README,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
