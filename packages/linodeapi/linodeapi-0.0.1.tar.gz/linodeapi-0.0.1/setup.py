11a8d6230cdadb735df8a688ae021b3622c5c37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:38 +0000

    Add package linodeapi/0.0.1/linodeapi-0.0.1.tar.gz

---
+++ packages/linodeapi/linodeapi-0.0.1.tar.gz/linodeapi-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name = 'linodeapi',
+    packages = ['linodeapi'],
+    version = '0.0.1',
+    description = 'API for linode',
+    author='Revel',
+    author_email='devops@revelup.com',
+    url='http://revelup.com/',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
+)
