10d1978565f59c68d92e9d8db3b2018591160fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:21 +0000

    Add package flask-restplus/0.3.0/flask_restplus-0.3.0-py2.py3-none-any.whl

---
+++ packages/flask-restplus/flask_restplus-0.3.0-py2.py3-none-any.whl/flask_restplus/namespace.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+class ApiNamespace(object):
+    def __init__(self, api, name, description=None, endpoint=None, path=None, **kwargs):
+        self.api = api
+        self.name = name
+        self.path = path or ('/' + name)
+        self.description = description
+        self.resources = []
+        self.models = []
+        self.endpoint = str(endpoint or 'ns-{0}'.format(self.name.lower().replace(' ', '-')))
+
+    @property
+    def full_endpoint(self):
+        return '{0}.{1}'.format(self.api.endpoint, self.endpoint)
+
+    def add_resource(self, resource, *urls, **kwargs):
+        self.resources.append((resource, urls, kwargs))
+        self.api.add_resource(resource, *urls, namespace=self, **kwargs)
+
+    def add_model(self, model):
+        self.models.append(model)
+
+    def route(self, *urls, **kwargs):
+        def wrapper(cls):
+            doc = kwargs.pop('doc', None)
+            if doc:
+                self.api._handle_api_doc(cls, doc)
+            self.add_resource(cls, *[self.path + url for url in urls], **kwargs)
+            return cls
+        return wrapper
