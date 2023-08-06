2577dc64adf1a76697d9f265e9352527cfa26eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:57 +0000

    Add package Flask-Cors/1.10.2/Flask-Cors-1.10.2.tar.gz

---
+++ packages/Flask-Cors/Flask-Cors-1.10.2.tar.gz/Flask-Cors-1.10.2/setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+    test
+    ~~~~
+    Flask-CORS is a simple extension to Flask allowing you to support cross
+    origin resource sharing (CORS) using a simple decorator.
+
+    :copyright: (c) 2014 by Cory Dolphin.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from setuptools import setup
+
+from _version import __version__
+
+setup(
+    name='Flask-Cors',
+    version=__version__,
+    url='https://github.com/wcdolphin/flask-cors',
+    license='MIT',
+    author='Cory Dolphin',
+    author_email='wcdolphin@gmail.com',
+    description="A Flask extension adding a decorator for CORS support",
+    long_description=open('README.rst').read(),
+    py_modules=['flask_cors', '_version'],
+    # if you would be using a package instead use packages instead
+    # of py_modules:
+    # packages=['flask_sqlite3'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask >= 0.9',
+        'Six'
+    ],
+    tests_require=[
+        'nose'
+    ],
+    test_suite='nose.collector',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
