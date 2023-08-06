f6c42ae11fb30b33551e7276c43241a6dd7e50f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:08 +0000

    Add package django-FIUT/0.1.7/django-FIUT-0.1.7.tar.gz

---
+++ packages/django-FIUT/django-FIUT-0.1.7.tar.gz/django-FIUT-0.1.7/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+
+setup(
+    name='django-FIUT',
+    version='0.1.7',
+    author='Rafal Mironczyk',
+    author_email='voyageur.pl@gmail.com',
+    license='MIT',
+    description='Fine, Incredibly Useful Tools',
+    packages=['fiut'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+    url='https://github.com/v0y/django-FIUT',
+)
