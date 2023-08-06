91701f3b057343d3c0a26e2ba9205d097f89ac28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:57 +0000

    Add package comment-builder/0.1/comment-builder-0.1.tar.gz

---
+++ packages/comment-builder/comment-builder-0.1.tar.gz/comment-builder-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    author = 'Adam Matan',
+    author_email = 'adam@matan.name',
+    description = 'A python script for displaying aligned comments',
+    license = 'MIT',
+    name = 'comment-builder',
+    py_modules = ['comment'],
+    scripts = ['comment'],
+    url = 'https://github.com/adamatan/comment-builder',
+    version = '0.1',
+)
