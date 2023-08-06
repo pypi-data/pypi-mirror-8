8ec33e02f2d160fd08900f59a39df6e04ed195cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:02 +0000

    Add package Hatak_Toster/0.2.1/Hatak_Toster-0.2.1.tar.gz

---
+++ packages/Hatak_Toster/Hatak_Toster-0.2.1.tar.gz/Hatak_Toster-0.2.1/src/haplugin/toster/fixtures.py
@@ -0,0 +1,23 @@
+fixtures = {}
+
+
+class Fixtures(object):
+
+    def __init__(self, db, application):
+        self.db = db
+        self.fixtures = fixtures
+        self.application = application
+
+    def _create(self, cls, **kwargs):
+        obj = cls.get_or_create(self.db, **kwargs)
+        data = self.fixtures.get(cls.__name__, {})
+        data[kwargs['name']] = obj
+        self.fixtures[cls.__name__] = data
+        return obj
+
+    def _create_nameless(self, cls, **kwargs):
+        obj = cls.get_or_create(self.db, **kwargs)
+        data = self.fixtures.get(cls.__name__, [])
+        data.append(obj)
+        self.fixtures[cls.__name__] = data
+        return obj
