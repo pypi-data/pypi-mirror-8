b17473f0ff080041f9da0f30f5ac7df94b7f5655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:58 +0000

    Add package state-machine-crawler/2.0.0/state-machine-crawler-2.0.0.tar.gz

---
+++ packages/state-machine-crawler/state-machine-crawler-2.0.0.tar.gz/state-machine-crawler-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+SOURCE_VERSION = "2.0.0"
+
+
+setup(
+    name="state-machine-crawler",
+    description="A library for following automata based programming model.",
+    version=SOURCE_VERSION,
+    packages=find_packages(),
+    author="Anton Berezin",
+    author_email="gurunars@gmail.com"
+)
