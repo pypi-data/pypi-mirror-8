92761677108f72f24041628709d20195cb623e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:25 +0000

    Add package kinopoiskpy/0.5.5/kinopoiskpy-0.5.5.tar.gz

---
+++ packages/kinopoiskpy/kinopoiskpy-0.5.5.tar.gz/kinopoiskpy-0.5.5/kinopoisk/movie/__init__.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+from kinopoisk.utils import KinopoiskObject, Manager, get_request
+
+
+class Movie(KinopoiskObject):
+
+    def set_defaults(self):
+        self.title = ''
+        self.title_original = ''
+        self.plot = ''
+
+        self.year = None
+        self.countries = []
+        self.tagline = ''
+
+        self.directors = []
+        self.scenarios = []
+        self.producers = []
+        self.operators = []
+        self.composers = []
+        self.genres = []
+
+        self.budget = None
+        self.profit_usa = None
+        self.profit_russia = None
+        self.audience = []
+
+        self.rating = None
+        self.runtime = None
+        self.release = None
+
+        self.posters = []
+        self.trailers = []
+        self.youtube_ids = []
+
+        self.series = None
+        self.seasons = []
+
+    def __init__(self, *args, **kwargs):
+        super(Movie, self).__init__(*args, **kwargs)
+        self.set_defaults()
+
+        from sources import MovieLink, MoviePremierLink, MovieMainPage, MoviePostersPage, MovieTrailersPage, MovieSeries  # import here for successful installing via pip
+        self.register_source('link', MovieLink)
+        self.register_source('premier_link', MoviePremierLink)
+        self.register_source('main_page', MovieMainPage)
+        self.register_source('posters', MoviePostersPage)
+        self.register_source('trailers', MovieTrailersPage)
+        self.register_source('series', MovieSeries)
+
+    def __repr__(self):
+        return ('<%s (%s), %s>' % (self.title, self.title_original, self.year or '-')).encode('utf-8')
+
+    def add_trailer(self, trailer_params):
+        trailer = Trailer(trailer_params)
+        if trailer.is_valid and trailer.id not in [tr.id for tr in self.trailers]:
+            self.trailers.append(trailer)
+
+    def add_series_season(self, year, episodes):
+        self.seasons.append(SeriesSeason(year, [SeriesEpisode(title, date) for title, date in episodes]))
+
+
+class Trailer(object):
+
+    def set_defaults(self):
+        self.id = None
+        self.width = None
+        self.heigth = None
+        self.file = None
+        self.dom = 'tr'
+        self.advsys = 'rutube'
+        self.sbt = ''
+        self.genres = None
+        self.preview_file = None
+        self.preview_width = None
+        self.preview_heigth = None
+
+    def __init__(self, params=None):
+        self.set_defaults()
+
+        if params:
+            self.id = params['trailerId'].replace('top', '')
+            self.width = params['trailerW']
+            self.heigth = params['trailerH']
+            self.file = params['trailerFile']
+            self.dom = params['trailerDom']
+            self.advsys = params['trailerAdvsys']
+            self.sbt = params['trailerSbt']
+            self.genres = params['genres']
+            self.preview_file = params['previewFile']
+            self.preview_width = params['previewW']
+            self.preview_heigth = params['previewH']
+
+    @property
+    def is_valid(self):
+        '''
+        Check if filename is correct
+        '''
+        # not youtube video '521689/' (http://www.kinopoisk.ru/film/521689/video/)
+        return self.file[-1] != '/'
+
+
+class SeriesEpisode(object):
+
+    def set_defaults(self):
+        self.title = ''
+        self.release_date = None
+
+    def __init__(self, title=None, release_date=None):
+        self.set_defaults()
+
+        self.title = title
+        self.release_date = release_date
+
+    def __repr__(self):
+        return '<%s "%s", %s>' % (
+            self.__class__.__name__,
+            self.title.encode('utf-8') if self.title else '???',
+            self.release_date or '-'
+        )
+
+
+class SeriesSeason(object):
+
+    def set_defaults(self):
+        self.year = None
+        self.episodes = []
+
+    def __init__(self, year, episodes=None):
+        self.set_defaults()
+
+        self.year = year
+        if episodes:
+            self.episodes = episodes
+
+    def __repr__(self):
+        return '<%s of %d: %d>' % (self.__class__, self.year, len(self.episodes))
+
+
+class MovieManager(Manager):
+    '''
+    Movie manager
+    '''
+    kinopoisk_object = Movie
+
+    def get_url_with_params(self, query):
+        # http://www.kinopoisk.ru/index.php?level=7&from=forma&result=adv&m_act[from]=forma&m_act[what]=content&m_act[find]=pulp+fiction
+        return ('http://www.kinopoisk.ru/index.php', {
+            'level': 7,
+            'from': 'forma',
+            'result': 'adv',
+            'm_act[from]': 'forma',
+            'm_act[what]': 'content',
+            'm_act[find]': query,
+        })
+        # возвращает не по релевантности, а непонятно как
+        # http://www.kinopoisk.ru/index.php?level=7&ser=a:3:{s:4:"find";s:3:"day";s:4:"what";s:7:"content";s:5:"count";a:1:{s:7:"content";s:3:"113";}}&show=all
+        return ('http://www.kinopoisk.ru/index.php', {
+            'level': 7,
+            'ser': 'a:3:{s:4:"find";s:%d:"%s";s:4:"what";s:7:"content";s:5:"count";a:1:{s:7:"content";s:3:"113";}}' % (len(query), query),
+            'show': 'all',
+        })
+
+
+class MoviePremiersManager(Manager):
+
+    kinopoisk_object = Movie
+
+    def get_url_with_params(self):
+        return ('http://www.kinopoisk.ru/level/8/view/prem/', {})
+
+    def all(self):
+        from BeautifulSoup import BeautifulSoup
+
+        url, params = self.get_url_with_params()
+        response = get_request(url, params=params)
+        content = response.content.decode('windows-1251', 'ignore')
+
+        content_soup = BeautifulSoup(content)
+        instances = []
+        for premier in content_soup.findAll('div', {'class': 'premier_item'}):
+            instance = self.kinopoisk_object()
+            instance.parse('premier_link', premier)
+            instances += [instance]
+
+        return instances
+
+
+Movie.objects = MovieManager()
+Movie.premiers = MoviePremiersManager()
---
+++ packages/kinopoiskpy/kinopoiskpy-0.5.5.tar.gz/kinopoiskpy-0.5.5/kinopoisk/person/sources.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+import re
+
+from kinopoisk.utils import KinopoiskPage, KinopoiskImagesPage, get_request
+
+
+class PersonLink(KinopoiskPage):
+    '''
+    Parser person info from links
+    '''
+    def parse(self, instance, content):
+        link = re.compile(r'<p class="name"><a href="/name/(\d+)/[^"]*">(.+?)</a>').findall(content)
+        if link:
+            instance.id = self.prepare_int(link[0][0])
+            instance.name = self.prepare_str(link[0][1])
+
+        year = re.compile(r'<span class="year">(\d{4})</span>').findall(content)
+        if year:
+            instance.year_birth = self.prepare_int(year[0])
+
+        otitle = re.compile(r'<span class="gray">(.*?)</span>').findall(content)
+        if otitle:
+            instance.name_original = self.prepare_str(otitle[0])
+
+        instance.set_source('link')
+
+
+class PersonMainPage(KinopoiskPage):
+    '''
+    Parser of main person page
+    '''
+    url = '/name/%d/'
+
+    def parse(self, instance, content):
+
+        id = re.compile(r"<link rel=\"canonical\" href=\"http://www.kinopoisk.ru/name/(\d+)/\" />").findall(content)
+        if id:
+            instance.id = self.prepare_int(id[0])
+
+        name = re.compile(r'<h1 class="moviename-big" itemprop="name">(.+?)</h1>').findall(content)
+        if name:
+            instance.name = self.prepare_str(name[0])
+
+        name_original = re.compile(r'<span itemprop="alternativeHeadline">([\w\s]+)\s+</span>').findall(content)
+        if name_original:
+            instance.name_original = self.prepare_str(name_original[0])
+
+        content_info = re.compile(r'<tr\s*>\s*<td class="type">(.+?)</td>\s*<td[^>]*>(.+?)</td>\s*</tr>').findall(content)
+        for name, value in content_info:
+            if name.encode('utf-8') == 'дата рождения':
+                year_birth = re.compile(r'<a href="/lists/m_act%5Bbirthday%5D%5Byear%5D/\d{4}/">(\d{4})</a>').findall(value)
+                if year_birth:
+                    instance.year_birth = self.prepare_int(year_birth[0])
+
+        if instance.id:
+            response = get_request(instance.get_url('info'))
+            if response.content:
+                instance.information = response.content.decode('windows-1251', 'ignore').replace(' class="trivia"', '')
+
+        instance.set_source('main_page')
+
+
+class PersonPhotosPage(KinopoiskImagesPage):
+    '''
+    Parser of person photos page
+    '''
+    url = '/name/%d/photos/'
+    field_name = 'photos'
