a32a3d0db804b33977a403aeb4a2cd0df69dd885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:18 +0000

    Add package py-sphviewer/0.160/py-sphviewer-0.160.tar.gz

---
+++ packages/py-sphviewer/py-sphviewer-0.160.tar.gz/py-sphviewer-0.160/setup.py
@@ -0,0 +1,36 @@
+#from distutils.core import setup, Extension
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
+module_scene = Extension('sphviewer/extensions/scene', sources = ['sphviewer/extensions/scenemodule.c'],
+                         extra_compile_args=['-fopenmp'],
+                         extra_link_args=['-lgomp'])
+
+module_render = Extension('sphviewer/extensions/render', sources = ['sphviewer/extensions/rendermodule.c'],
+                          extra_compile_args=['-fopenmp'],
+                          extra_link_args=['-lgomp'])
+
+setup(name='py-sphviewer',
+      version='0.161',
+      description='Py-SPHViewer is a framework for rendering particles using the smoothed particle hydrodynamics scheme.',
+      author='Alejandro Benitez Llambay',
+      author_email='alejandrobll@oac.uncor.edu',
+      url='https://code.google.com/p/py-sphviewer/',
+      packages=['sphviewer','sphviewer.extensions'],
+      requires = ['pykdtree', 'numpy'],
+      install_requires = ['pykdtree', 'numpy'],
+      package_data={'sphviewer': ['*.c','*.txt']},
+      ext_modules = [module_scene, module_render],
+      license='GNU GPL v3',
+      classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: 3D Rendering',
+        'Topic :: Multimedia :: Graphics :: Viewers',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Utilities'
+            ],
+      keywords="smoothed particle hydrodynamics render particles nbody galaxy formation dark matter sph cosmology movies",      
+     )
