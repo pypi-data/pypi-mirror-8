8df347f96df9a4de97cda1f3075767a08f6700ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:31 +0000

    Add package csample/0.2.1/csample-0.2.1.tar.gz

---
+++ packages/csample/csample-0.2.1.tar.gz/csample-0.2.1/README.rst
@@ -0,0 +1,106 @@
+csample: Hash-based sampling library for Python
+===============================================
+
+|travismaster|
+
+.. |travismaster| image:: https://secure.travis-ci.org/box-and-whisker/csample.png?branch=master
+   :target: http://travis-ci.org/box-and-whisker/csample
+
+Hash-based sampling is a filtering method that tries to approximate random
+sampling by using a hash function as a selection criterion.
+
+Following list describes some features of the method:
+
+*   Since there are no randomness involved at all, the same data set with the
+    same sampling rate (and also with the same salt value) always yields
+    exactly the same result.
+*   The size of population doesn't need to be specified beforehand. It means
+    that the sampling process can be applied to data stream with unknown size
+    such as system logs.
+
+
+Applications
+============
+
+Here are some real and hypothetical applications:
+
+*   `[RFC5475] Sampling and Filtering Techniques for IP Packet Selection <https://tools.ietf.org/html/rfc5475>`_
+    is a well-known application.
+*   Online streaming algorithm to select 10% of users for A/B testing.
+    "Consistent" nature of the algorithm guarantees that any user ID selected
+    once will always be selected again. There's no need to maintain a list of
+    selected user IDs.
+
+
+Usage
+=====
+
+Two sampling functions are provided for a convenience.
+
+``sample_line()`` accepts iterable type containing strs::
+
+    data = [
+        'alan',
+        'brad',
+        'cate',
+        'david',
+    ]
+    samples = csample.sample_line(data, 0.5)
+
+``sample_tuple()`` expects tuples instead of strs as a content of
+iterable. The third argument 0 indicates a column index::
+
+    data = [
+        ('alan', 10, 5),
+        ('brad', 53, 7),
+        ('cate', 12, 6),
+        ('david', 26, 5),
+    ]
+    samples = csample.sample_tuple(data, 0.5, 0)
+
+In both cases, the function returns immediately with sampled iterable.
+
+Read the `full documentation. <https://csample.readthedocs.org/en/latest/>`_
+
+
+Command-line interface
+======================
+
+``csample`` also provides command-line interface.
+
+Following command prints 50% sample from 100 integers::
+
+    > seq 100 | csample -r 0.5
+
+To see more options use ``--help`` command-line argument::
+
+    > csample --help
+
+
+Hash functions
+==============
+
+To obtain fairly random/unbiased sample, it is critical to use suitable hash
+function.
+
+There could be many criteria such as `avalanche effect <http://en.wikipedia.org/wiki/Avalanche_effect>`_.
+For those who are interested, see link below:
+
+*   `Empirical Evaluation of Hash Functions for Multipoint Measurements <http://www.sigcomm.org/sites/default/files/ccr/papers/2008/July/1384609-1384614.pdf>`_
+
+``csample`` currently supports `xxhash`_ and `spooky`_.
+
+.. _xxhash: https://code.google.com/p/xxhash/
+.. _spooky: http://burtleburtle.net/bob/hash/spooky.html
+
+
+Installation
+============
+
+Installing csample is easy::
+
+    pip install csample
+
+or download the source and run::
+
+    python setup.py install
---
+++ packages/csample/csample-0.2.1.tar.gz/csample-0.2.1/csample.py
@@ -0,0 +1,98 @@
+"""
+csample: Hash-based sampling library for Python
+"""
+from __future__ import division
+import argparse
+import six
+import sys
+import xxhash
+import spooky
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Perform hash-based filtering')
+    parser.add_argument('-r', '--rate', type=float, required=True, help='sampling rate from 0.0 to 1.0')
+    parser.add_argument('-s', '--salt', type=str, default='DEFAULT_SALT', help='salt for hash function')
+    parser.add_argument('-c', '--col', type=int, default=-1, help='column index (starts from 0)')
+    parser.add_argument('--hash', type=str, default='xxhash32', help='hash function: xxhash32 (default), spooky32')
+    parser.add_argument('--sep', type=str, default=',', help='column separator')
+
+    args = parser.parse_args()
+    rate = args.rate
+    salt = args.salt
+    col = args.col
+    hashfunc = args.hash
+    sep = six.u(args.sep)
+    if col == -1:
+        stream = sample_line(sys.stdin, rate, funcname=hashfunc, salt=salt)
+    else:
+        tuples = (line.split(sep) for line in sys.stdin)
+        stream = sample_tuple(tuples, rate, col, funcname=hashfunc, salt=salt)
+
+    for line in stream:
+        sys.stdout.write(line)
+
+
+def sample_tuple(s, rate, col, funcname='xxhash32', salt='DEFAULT_SALT'):
+    """Sample tuples in given stream `s`.
+
+    Performs hash-based sampling with given sampling `rate` by applying a hash
+    function `funcname`. Sampling with the same `salt` (or seed) always yields
+    result.
+
+    Following example shows how to sample approximately 50% of log data based
+    on user ID column. Note that the returned value is a generator:
+
+    >>> logs = (
+    ...     # user id, event type, timestamp
+    ...     ('alan', 'event a', 0),
+    ...     ('alan', 'event b', 1),
+    ...     ('brad', 'event a', 2),
+    ...     ('cate', 'event a', 3),
+    ...     ('cate', 'event a', 4),
+    ...     ('brad', 'event b', 5),
+    ...     ('brad', 'event c', 6),
+    ... )
+    >>> list(sample_tuple(logs, 0.5, 0))
+    [('brad', 'event a', 2), ('brad', 'event b', 5), ('brad', 'event c', 6)]
+
+    :param s: stream of tuples
+    :param rate: sampling rate
+    :param col: index of column to be hashed
+    :param funcname: name of hash function: xxhash32 (default), spooky
+    :param salt: salt or seed for hash function
+    :return: sampled stream of tuples
+    """
+    func = _hash_with_salt(funcname, salt)
+    return (l for l in s if func(l[col]) < rate)
+
+
+def sample_line(s, rate, funcname='xxhash32', salt='DEFAULT_SALT'):
+    """Sample strings in given stream `s`.
+
+    The function expects strings instead of tuples, except for that the
+    function does the exactly same thing with `sample_tuple()`.
+
+    :param s: stream of strings
+    :param rate: sampling rate
+    :param funcname: name of hash function: xxhash32 (default), spooky
+    :param salt: salt of seed for hash function
+    :return: sample stream of strings
+    """
+    func = _hash_with_salt(funcname, salt)
+    return (l for l in s if func(l) < rate)
+
+
+def _hash_with_salt(funcname, salt):
+    seed = xxhash.xxh32(salt).intdigest()
+
+    if funcname == 'xxhash32':
+        return lambda x: xxhash.xxh32(x, seed=seed).intdigest() / 0xFFFFFFFF
+    elif funcname == 'spooky32':
+        return lambda x: spooky.hash32(x, seed=seed) / 0xFFFFFFFF
+    else:
+        raise ValueError('Unknown function name: %s' % funcname)
+
+
+if __name__ == '__main__':
+    main()
