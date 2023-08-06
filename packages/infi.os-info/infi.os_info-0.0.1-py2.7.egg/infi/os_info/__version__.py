4ed3eb267c97b36848b964c1d0c0b9d6bdb567b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:46 +0000

    Add package infi.os-info/0.0.1/infi.os_info-0.0.1-py2.7.egg

---
+++ packages/infi.os-info/infi.os_info-0.0.1-py2.7.egg/infi/os_info/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.0.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ca0955f42bdf6c01418f7438be7cabc919405831'
+__git_head_subject__ = 'Finished release v0.0.1.'
+__git_head_message__ = '* release/v0.0.1:\n  console-scripts\n  first implementation and tests\n  added all project files'
+__git_dirty_diff__ = ''
