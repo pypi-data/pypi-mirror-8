4aef09b3f19fe4a54a072390411a71c545848800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:42 +0000

    Add package python-statsd/1.7.1/python-statsd-1.7.1.tar.gz

---
+++ packages/python-statsd/python-statsd-1.7.1.tar.gz/python-statsd-1.7.1/statsd/timer.py
@@ -0,0 +1,159 @@
+import contextlib
+import time
+import statsd
+
+
+class Timer(statsd.Client):
+    '''
+    Statsd Timer Object
+
+    Additional documentation is available at the parent class
+    :class:`~statsd.client.Client`
+
+    :keyword name: The name for this timer
+    :type name: str
+    :keyword connection: The connection to use, will be automatically created if
+        not given
+    :type connection: :class:`~statsd.connection.Connection`
+    :keyword min_send_threshold: Timings smaller than this will not be sent so -1
+        can be used for all.
+    :type min_send_threshold: int
+
+    >>> timer = Timer('application_name').start()
+    >>>  # do something
+    >>> timer.stop('executed_action')
+    True
+    '''
+
+    def __init__(self, name, connection=None, min_send_threshold=-1):
+        super(Timer, self).__init__(name, connection=connection)
+        self._start = None
+        self._last = None
+        self._stop = None
+        self.min_send_threshold = min_send_threshold
+
+    def start(self):
+        '''Start the timer and store the start time, this can only be executed
+        once per instance
+
+        It returns the timer instance so it can be chained when instantiating
+        the timer instance like this:
+        ``timer = Timer('application_name').start()``'''
+        assert self._start is None, (
+            'Unable to start, the timer is already running')
+        self._last = self._start = time.time()
+        return self
+
+    def send(self, subname, delta):
+        '''Send the data to statsd via self.connection
+
+        :keyword subname: The subname to report the data to (appended to the
+            client name)
+        :type subname: str
+        :keyword delta: The time delta (time.time() - time.time()) to report
+        :type delta: float
+        '''
+        ms = delta * 1000
+        if ms > self.min_send_threshold:
+            name = self._get_name(self.name, subname)
+            self.logger.info('%s: %0.08fms', name, ms)
+            return statsd.Client._send(self, {name: '%0.08f|ms' % ms})
+        else:
+            return True
+
+    def intermediate(self, subname):
+        '''Send the time that has passed since our last measurement
+
+        :keyword subname: The subname to report the data to (appended to the
+            client name)
+        :type subname: str
+        '''
+        t = time.time()
+        response = self.send(subname, t - self._last)
+        self._last = t
+        return response
+
+    def stop(self, subname='total'):
+        '''Stop the timer and send the total since `start()` was run
+
+        :keyword subname: The subname to report the data to (appended to the
+            client name)
+        :type subname: str
+        '''
+        assert self._stop is None, (
+            'Unable to stop, the timer is already stopped')
+        self._stop = time.time()
+        return self.send(subname, self._stop - self._start)
+
+    def _decorate(self, name, function, class_=None):
+        class_ = class_ or Timer
+
+        def _decorator(*args, **kwargs):
+            timer = self.get_client(name, class_)
+            timer.start()
+            try:
+                return function(*args, **kwargs)
+            finally:
+                # Stop the timer, send the message and cleanup
+                timer.stop('')
+                del timer
+
+        return _decorator
+
+    def decorate(self, function_or_name):
+        '''Decorate a function to time the execution
+
+        The method can be called with or without a name. If no name is given
+        the function defaults to the name of the function.
+
+        :keyword function_or_name: The name to post to or the function to wrap
+
+        >>> from statsd import Timer
+        >>> timer = Timer('application_name')
+        >>>
+        >>> @timer.decorate
+        ... def some_function():
+        ...     # resulting timer name: application_name.some_function
+        ...     pass
+        >>>
+        >>> @timer.decorate('my_timer')
+        ... def some_other_function():
+        ...     # resulting timer name: application_name.my_timer
+        ...     pass
+
+        '''
+        if callable(function_or_name):
+            return self._decorate(function_or_name.__name__, function_or_name)
+        else:
+            return lambda f: self._decorate(function_or_name, f)
+
+    @contextlib.contextmanager
+    def time(self, subname=None, class_=None):
+        '''Returns a context manager to time execution of a block of code.
+
+        :keyword subname: The subname to report data to
+        :type subname: str
+        :keyword class_: The :class:`~statsd.client.Client` subclass to use
+            (e.g. :class:`~statsd.timer.Timer` or
+            :class:`~statsd.counter.Counter`)
+        :type class_: :class:`~statsd.client.Client`
+
+        >>> from statsd import Timer
+        >>> timer = Timer('application_name')
+        >>>
+        >>> with timer.time():
+        ...     # resulting timer name: application_name
+        ...     pass
+        >>>
+        >>>
+        >>> with timer.time('context_timer'):
+        ...     # resulting timer name: application_name.context_timer
+        ...     pass
+
+        '''
+        if class_ is None:
+            class_ = Timer
+        timer = self.get_client(subname, class_)
+        timer.start()
+        yield
+        timer.stop('')
---
+++ packages/python-statsd/python-statsd-1.7.1.tar.gz/python-statsd-1.7.1/tests/test_timer.py
@@ -0,0 +1,178 @@
+from __future__ import with_statement
+from unittest import TestCase
+import mock
+import statsd
+
+
+class TestTimerBase(TestCase):
+    def tearDown(self):
+        self._time_patch.stop()
+
+    def get_time(self, mock_client, key):
+        return float(self.get_arg(mock_client, key).split('|')[0])
+
+    def get_arg(self, mock_client, key):
+        return mock_client._send.call_args[0][1][key]
+
+
+class TestTimerDecorator(TestTimerBase):
+    def setUp(self):
+        self.timer = statsd.Timer('timer')
+
+        # get time.time() to always return the same value so that this test
+        # isn't system load dependant.
+        self._time_patch = mock.patch('time.time')
+        time_time = self._time_patch.start()
+
+        def generator():
+            i = 0.0
+            while True:
+                i += 0.1234
+                yield i
+        time_time.side_effect = generator()
+
+    @mock.patch('statsd.Client')
+    def test_decorator_a(self, mock_client):
+        @self.timer.decorate
+        def a():
+            pass
+
+        a()
+
+        assert self.get_time(mock_client, 'timer.a') == 123.4, \
+            'This test must execute within 2ms'
+
+    @mock.patch('statsd.Client')
+    def test_decorator_named_spam(self, mock_client):
+        @self.timer.decorate('spam')
+        def a():
+            pass
+        a()
+
+        assert self.get_time(mock_client, 'timer.spam') == 123.4, \
+            'This test must execute within 2ms'
+
+    @mock.patch('statsd.Client')
+    def test_nested_naming_decorator(self, mock_client):
+        timer = self.timer.get_client('eggs0')
+
+        @timer.decorate('d0')
+        def a():
+            pass
+        a()
+
+        assert self.get_time(mock_client, 'timer.eggs0.d0') == 123.4, \
+            'This test must execute within 2ms'
+
+
+class TestTimerContextManager(TestTimerBase):
+
+    def setUp(self):
+        self.timer = statsd.Timer('cm')
+
+        # get time.time() to always return the same value so that this test
+        # isn't system load dependant.
+        self._time_patch = mock.patch('time.time')
+        time_time = self._time_patch.start()
+
+        def generator():
+            i = 0.0
+            while True:
+                i += 0.1234
+                yield i
+        time_time.side_effect = generator()
+
+    @mock.patch('statsd.Client')
+    def test_context_manager_default(self, mock_client):
+        timer = self.timer.get_client('default')
+        with timer.time():
+            pass
+
+        assert self.get_time(mock_client, 'cm.default') == 123.4, \
+            'This test must execute within 2ms'
+
+    @mock.patch('statsd.Client')
+    def test_context_manager_named(self, mock_client):
+        timer = self.timer.get_client('named')
+        with timer.time('name'):
+            pass
+
+        assert self.get_time(mock_client, 'cm.named.name') == 123.4, \
+            'This test must execute within 2ms'
+
+    @mock.patch('statsd.Client')
+    def test_context_manager_class(self, mock_client):
+        timer = self.timer.get_client('named')
+        with timer.time(class_=statsd.Timer):
+            pass
+
+        assert self.get_time(mock_client, 'cm.named') == 123.4, \
+            'This test must execute within 2ms'
+
+
+class TestTimerAdvancedUsage(TestTimerDecorator):
+
+    @mock.patch('statsd.Client')
+    def test_timer_total(self, mock_client):
+        timer4 = statsd.Timer('timer4')
+        timer4.start()
+        timer4.stop()
+        assert self.get_time(mock_client, 'timer4.total') == 123.4, \
+            'This test must execute within 2ms'
+
+        timer5 = statsd.Timer('timer5')
+        timer5.start()
+        timer5.stop('test')
+        assert self.get_time(mock_client, 'timer5.test') == 123.4, \
+            'This test must execute within 2ms'
+
+    @mock.patch('statsd.Client')
+    def test_timer_intermediate(self, mock_client):
+        timer6 = statsd.Timer('timer6')
+        timer6.start()
+        timer6.intermediate('extras')
+        assert self.get_time(mock_client, 'timer6.extras') == 123.4, \
+            'This test must execute within 2ms'
+        timer6.stop()
+        assert self.get_time(mock_client, 'timer6.total') == 370.2, \
+            'This test must execute within 2ms'
+
+        timer7 = statsd.Timer('timer7')
+        timer7.start()
+        timer7.intermediate('extras')
+        assert self.get_time(mock_client, 'timer7.extras') == 123.4, \
+            'This test must execute within 2ms'
+        timer7.stop('test')
+        assert self.get_time(mock_client, 'timer7.test') == 370.2, \
+            'This test must execute within 2ms'
+
+
+class TestTimerZero(TestTimerBase):
+    def setUp(self):
+        # get time.time() to always return the same value so that this test
+        # isn't system load dependant.
+        self._time_patch = mock.patch('time.time')
+        time_time = self._time_patch.start()
+
+        def generator():
+            while True:
+                yield 0
+        time_time.side_effect = generator()
+
+    def tearDown(self):
+        self._time_patch.stop()
+
+    @mock.patch('statsd.Client')
+    def test_timer_zero(self, mock_client):
+        timer8 = statsd.Timer('timer8', min_send_threshold=0)
+        timer8.start()
+        timer8.stop()
+        assert mock_client._send.call_args is None, \
+            '0 timings shouldnt be sent'
+
+        timer9 = statsd.Timer('timer9', min_send_threshold=0)
+        timer9.start()
+        timer9.stop('test')
+        assert mock_client._send.call_args is None, \
+            '0 timings shouldnt be sent'
+
