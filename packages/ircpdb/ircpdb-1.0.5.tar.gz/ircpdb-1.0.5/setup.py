965dd7b8509a66d3f9e2cf89c4a353006be56404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:06 +0000

    Add package ircpdb/1.0.5/ircpdb-1.0.5.tar.gz

---
+++ packages/ircpdb/ircpdb-1.0.5.tar.gz/ircpdb-1.0.5/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+import os
+from setuptools import setup, find_packages
+
+
+requirements_path = os.path.join(
+    os.path.dirname(__file__),
+    'requirements.txt',
+)
+try:
+    from pip.req import parse_requirements
+    requirements = [
+        str(req.req) for req in parse_requirements(requirements_path)
+    ]
+except ImportError:
+    requirements = []
+    with open(requirements_path, 'r') as in_:
+        requirements = [
+            req for req in in_.readlines()
+            if not req.startswith('-')
+            and not req.startswith('#')
+        ]
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+setup(
+    name="ircpdb",
+    version="1.0.5",
+    description="Remotely debug your Python application via IRC.",
+    long_description=README + "\n\n" + CHANGES,
+    author="Adam Coddington",
+    author_email="me@adamcoddington.net",
+    url="http://github.com/coddingtonbear/ircpdb",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Topic :: Software Development :: Debuggers",
+    ]
+)
