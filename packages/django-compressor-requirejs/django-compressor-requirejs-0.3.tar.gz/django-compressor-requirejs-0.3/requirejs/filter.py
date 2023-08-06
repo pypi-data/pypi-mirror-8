15f748416051d168f0a3137b221c5486e3f35c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:08 +0000

    Add package django-compressor-requirejs/0.3/django-compressor-requirejs-0.3.tar.gz

---
+++ packages/django-compressor-requirejs/django-compressor-requirejs-0.3.tar.gz/django-compressor-requirejs-0.3/requirejs/filter.py
@@ -0,0 +1,147 @@
+import re
+import json
+
+from django.utils.six import text_type
+from django.utils.safestring import mark_safe
+from django.contrib.staticfiles import finders
+from django.core.files.base import ContentFile
+from django.template.loaders.app_directories import app_template_dirs
+
+# noinspection PyPackageRequirements
+from compressor.conf import settings
+# noinspection PyPackageRequirements
+from compressor.filters.base import FilterBase
+# noinspection PyPackageRequirements
+from compressor.js import JsCompressor
+
+from .finder import ModuleFinder
+from .utils import get_installed_app_labels
+
+define_replace_pattern = re.compile(r'define\s*\(([^\)]*?)\)')
+
+REQUIREJS_PATHS = settings.REQUIREJS_PATHS if hasattr(settings, 'REQUIREJS_PATHS') else {}
+REQUIREJS_BUNDLES = settings.REQUIREJS_BUNDLES if hasattr(settings, 'REQUIREJS_BUNDLES') else {}
+REQUIREJS_APP_ALIAS = settings.REQUIREJS_APP_ALIAS if hasattr(settings, 'REQUIREJS_APP_ALIAS') else None
+
+
+class RequireJSCompiler(FilterBase):
+    def __init__(self, content, attrs=None, filter_type=None, charset=None, filename=None):
+        self.charset = charset
+        self.attrs = attrs
+        self.finder = self.get_module_finder()
+        super(RequireJSCompiler, self).__init__(content, filter_type, filename)
+
+    # noinspection PyMethodMayBeStatic
+    def get_module_finder(self):
+        template_directories = settings.TEMPLATE_DIRS + app_template_dirs
+        return ModuleFinder(template_directories, finders, REQUIREJS_APP_ALIAS)
+
+    def input(self, **kwargs):
+        if self.filename:
+            with open(self.filename, 'r') as f:
+                require_content = f.read()
+        else:
+            require_content = self.content
+
+        config = self.get_default_config()
+        if settings.COMPRESS_ENABLED:
+            config.update(self.get_bundle_config())
+
+        return text_type("var require = {config};{content}").format(config=json.dumps(config), content=require_content)
+
+    def output(self, **kwargs):
+        raise NotImplementedError
+
+    #
+    # Bundle creation
+    #
+
+    @staticmethod
+    def get_bundle_content(module, original_content):
+        """
+        Rewrite the module to include it's module path so it can be included in a bundle
+        """
+        return define_replace_pattern.sub(
+            r'define("{module}", \1)'.format(module=module),
+            text_type(original_content),
+            re.MULTILINE
+        )
+
+    def get_bundle_module(self, module):
+        """
+        Rewrite a module into a bundle, which means we have to add the name of the module into the define() call
+        """
+        path = self.finder.find_module(module)
+        if not path:
+            raise ValueError("Could not find module {} on disk".format(module))
+        with open(path, 'r') as f:
+            return self.get_bundle_content(module, f.read())
+
+    def write_bundle(self, basename, modules):
+        """
+        Let compressor write the bundled modules with a basename.
+        """
+        bundles = [self.get_bundle_module(module) for module in modules]
+        return self.write_output('\n'.join(bundles), '{name}.js'.format(name=basename))
+
+    @staticmethod
+    def write_output(content, basename):
+        """
+        Leverage django-compressor's JsCompressor to write the Javascript, making use of configured filters
+        and other settings.
+        """
+        # Compress it
+        compressor = JsCompressor()
+        filtered = compressor.filter(content, method='input', kind='js')
+        output = compressor.filter_output(filtered)
+        path = compressor.get_filepath(output, basename=basename)
+        # Write it
+        compressor.storage.save(path, ContentFile(output.encode(compressor.charset)))
+        return mark_safe(compressor.storage.url(path))
+
+    #
+    # RequireJS config generation
+    #
+
+    def get_bundle_config(self):
+        """
+        Generate the ``bundles`` configuration option for RequireJS.
+        """
+        modules = self.finder.get_modules()
+        bundles = {}
+        if REQUIREJS_BUNDLES:
+            # Let the configured bundles get generated, leaving the remaining modules for the ``main`` bundle.
+            for name, bundle_modules in REQUIREJS_BUNDLES.items():
+                bundle_path = self.write_bundle(name, bundle_modules)
+                bundles[bundle_path] = list(bundle_modules)
+                for m in bundle_modules:
+                    modules.discard(m)
+
+        # If we still have modules, write them
+        if modules:
+            bundles[self.write_bundle('main', modules)] = list(modules)
+
+        return {
+            'bundles': bundles,
+        } if bundles else {}
+
+    @staticmethod
+    def get_default_config():
+        """
+        Generate a default config for RequireJS setting the ``baseUrl`` to our ``STATIC_URL`` so we can define
+        our modules in static, and add, if configured, aliases for all installed apps
+        """
+
+        paths = {}
+        if REQUIREJS_APP_ALIAS:
+            paths.update({
+                app: '{app}/{alias}'.format(app=app, alias=REQUIREJS_APP_ALIAS)
+                for app in get_installed_app_labels()
+            })
+
+        paths.update(REQUIREJS_PATHS)
+
+        return {
+            'baseUrl': settings.STATIC_URL,
+            'paths': paths,
+        }
---
+++ packages/django-compressor-requirejs/django-compressor-requirejs-0.3.tar.gz/django-compressor-requirejs-0.3/requirejs/finder.py
@@ -0,0 +1,132 @@
+import os
+import re
+from itertools import chain
+
+from .utils import is_app_installed
+
+
+require_pattern = re.compile(r'(?:;|\s|>|^)require\s*\(\s*?(\[[^\]]*\])')
+define_pattern = re.compile(r'(?:;|\s|>|^)define\s*\(\s*?(\[[^\]]*\])')
+
+
+class ModuleFinder(object):
+    """
+    Find RequireJS modules in a Django project
+    """
+
+    def __init__(self, template_directories, static_finder, app_alias=None):
+        self.app_alias = app_alias
+        self.static_finder = static_finder
+        self.template_directories = template_directories
+
+    #
+    # File discovery
+    #
+
+    def get_modules(self):
+        """
+        Main function to query for modules in Django project
+        """
+        # TODO: work out dependency graph instead of list? Is this really needed for bundles?
+        return self.resolve_dependencies(self.get_template_dependencies())
+
+    def get_template_files(self):
+        """
+        Quick and simple template discovery for TEMPLATE_DIRS and app-based template dirs
+        """
+        template_files = []
+        for template_dir in self.template_directories:
+            for directory, dirnames, filenames in os.walk(template_dir):
+                for filename in filenames:
+                    template_files.append(os.path.join(directory, filename))
+        return template_files
+
+    def find_module(self, name):
+        """
+        Locate a static file for a RequireJS module name.
+        """
+        module_js = '{}.js'.format(name)
+
+        path = self.static_finder.find(module_js)
+
+        # Check for app alias if we cannot find it
+        module_parts = module_js.split('/')
+        if path is None and self.app_alias and is_app_installed(module_parts[0]):
+            module_parts.insert(1, self.app_alias)
+            path = self.static_finder.find('/'.join(module_parts))
+
+        return path
+
+    #
+    # Helpers
+    #
+
+    @staticmethod
+    def get_dependencies_from_match(match):
+        """
+        Resolve dependencies from the regex match found in a require() or define() call
+        """
+        # Remove list brackets and strip all whitespace
+        items = [m.strip() for m in match.strip()[1:-1].split(",")]
+
+        # Remove commented out items and filter out non-string values. We will not raise
+        # an error since it might be a dynamic dependency.
+        items = [
+            i for i in items if
+            not i.startswith("//")
+            and len(i) > 1
+            and i[0] == i[-1]
+            and i[0] in ["'", '"']
+        ]
+
+        # Strip the quotes
+        items = [i[1:-1] for i in items]
+
+        return items
+
+    #
+    # Dependency discovery
+    #
+
+    def get_dependencies(self, content, find_require=True, find_define=True):
+        patterns = []
+        if find_require:
+            patterns.append(require_pattern)
+        if find_define:
+            patterns.append(define_pattern)
+
+        for match in chain(*[pattern.findall(content) for pattern in patterns]):
+            for dep in self.get_dependencies_from_match(match):
+                yield dep
+
+    def get_module_dependencies(self, path):
+        """
+        Load a module and check for require() or define() calls
+        """
+        with open(path, 'r') as f:
+            for dep in self.get_dependencies(f.read()):
+                yield dep
+
+    def get_template_dependencies(self):
+        """
+        Walk through templates defined in the project and find require() calls
+        """
+        for template in self.get_template_files():
+            with open(template, 'r') as f:
+                for dep in self.get_dependencies(f.read(), find_define=False):
+                    yield dep
+
+    def resolve_dependencies(self, modules, known=None):
+        """
+        Recursively walk through modules and find their dependencies.
+        """
+        if known is None:
+            known = set()
+
+        for module in [m for m in modules if m not in known]:
+            path = self.find_module(module)
+            if path:  # only continue if we find the module on disk
+                known.add(module)
+                # Fetch all module's dependencies
+                known.update(self.resolve_dependencies(self.get_module_dependencies(path), known=known))
+        return known
