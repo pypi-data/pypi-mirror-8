674da16e45976c6220f88940f8c468c3084bda88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:15 +0000

    Add package datPython/1.0.0.2/datPython-1.0.0.2.tar.gz

---
+++ packages/datPython/datPython-1.0.0.2.tar.gz/datPython-1.0.0.2/setup.py
@@ -0,0 +1,25 @@
+import os
+
+from setuptools import setup
+
+setup(
+    name='datPython',
+    version = '1.0.0.2',
+    description='The Python API for dat',
+    url='https://github.com/pkafei/Dat-Python',
+    download_url = 'https://github.com/pkafei/Dat-Python/tarball/0.1',
+    author='Portia Burton',
+    author_email='plburton@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
