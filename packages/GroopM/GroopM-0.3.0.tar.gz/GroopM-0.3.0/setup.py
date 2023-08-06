8080b8949345335e69cbe1823bf62d978be797b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:12 +0000

    Add package GroopM/0.3.0/GroopM-0.3.0.tar.gz

---
+++ packages/GroopM/GroopM-0.3.0.tar.gz/GroopM-0.3.0/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='GroopM',
+    version='0.3.0',
+    author='Michael Imelfort',
+    author_email='mike@mikeimelfort.com',
+    packages=['groopm'],
+    scripts=['bin/groopm'],
+	url='http://pypi.python.org/pypi/GroopM/',
+    license='LICENSE.txt',
+    description='Metagenomic binning suite',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy >= 1.6.1",
+        "scipy >= 0.10.1",
+        "matplotlib >= 1.1.0",
+        "tables >= 2.3",
+        "PIL >= 1.1.7",
+        "BamM >= 1.3.0"
+    ],
+)
