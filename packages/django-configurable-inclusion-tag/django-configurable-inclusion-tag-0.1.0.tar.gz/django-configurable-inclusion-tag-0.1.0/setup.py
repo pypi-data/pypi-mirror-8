7591b851916fa1b81510a663c1913b95c1ffa90a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:03 +0000

    Add package django-configurable-inclusion-tag/0.1.0/django-configurable-inclusion-tag-0.1.0.tar.gz

---
+++ packages/django-configurable-inclusion-tag/django-configurable-inclusion-tag-0.1.0.tar.gz/django-configurable-inclusion-tag-0.1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+readme = open('README.md').read()
+
+setup(
+    name='django-configurable-inclusion-tag',
+    version='0.1.0',
+    author_email='gattster@gmail.com',
+    author='Philip Gatt',
+    description="A smart template inclusion tag",
+    long_description=readme,
+    url='http://github.com/defcube/django_configurable_inclusion_tag/',
+    py_modules=["django_configurable_inclusion_tag"],
+    data_files=[('', ['README.md'])],
+    )
