15b35164a246c1d850adb11c4cc6d9c9b308a647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:53 +0000

    Add package pagseguro/0.1.7.dev0/pagseguro-0.1.7.dev0.tar.gz

---
+++ packages/pagseguro/pagseguro-0.1.7.dev0.tar.gz/pagseguro-0.1.7.dev0/README.md
@@ -0,0 +1,241 @@
+python-pagseguro
+================
+[![Build
+Status](https://travis-ci.org/rochacbruno/python-pagseguro.png)](https://travis-ci.org/rochacbruno/python-pagseguro)
+[![Build
+Status](https://drone.io/github.com/rochacbruno/python-pagseguro/status.png)](https://drone.io/github.com/rochacbruno/python-pagseguro/latest)
+[![Coverage
+Status](https://coveralls.io/repos/rochacbruno/python-pagseguro/badge.png)](https://coveralls.io/r/rochacbruno/python-pagseguro)
+
+Integração com a API v2 de pagamentos e notificações do Pagseguro utilizando requests.
+
+Instalando
+==========================
+```bash
+pip install pagseguro
+```
+
+ou
+
+
+```bash
+pip install -e git+https://github.com/rochacbruno/python-pagseguro#egg=pagseguro
+```
+
+ou
+
+```
+git clone https://github.com/rochacbruno/python-pagseguro
+cd python-pagseguro
+pip install -r requirements.txt
+python setup.py install
+```
+
+Rodando os testes
+=================
+
+```
+make test
+```
+
+Como usar
+=========
+
+### Carrinho de compras / ordem de venda
+
+Uma instancia de **PagSeguro** funciona como uma ordem de venda, ou um carrinho de compras. É preciso criar a instancia passando como parametro email e token.
+
+> Opcionalmente é possivel passar o parametro **data** contendo valores a serem passados diretamente para a API.
+
+```python
+from pagseguro import PagSeguro
+
+pg = PagSeguro(email="seuemail@dominio.com", token="ABCDEFGHIJKLMNO")
+```
+
+### Configurando os dados do comprador
+
+```python
+pg.sender = {
+    "name": "Bruno Rocha",
+    "area_code": 11,
+    "phone": 981001213,
+    "email": "rochacbruno@gmail.com",
+}
+```
+
+### Configurando endereço de entrega
+```python
+pg.shipping = {
+    "type": pg.SEDEX,
+    "street": "Av Brig Faria Lima",
+    "number": 1234,
+    "complement": "5 andar",
+    "district": "Jardim Paulistano",
+    "postal_code": "06650030",
+    "city": "Sao Paulo",
+    "state": "SP",
+    "country": "BRA"
+}
+```
+
+Caso o **country** não seja informado o valor default será "BRA"
+
+O **type** pode ser pg.SEDEX, pg.PAC, pg.NONE
+> Opcionalmente pode ser numerico seguindo a tabela pagseguro:
+
+| Número | Descrição | Type |
+| ------ | --------- | ---- |
+| 1 | PAC | pg.PAC |
+| 2 | SEDEX | pg.SEDEX |
+| 3 | Nao especificado | pg.None |
+
+Valores opcionais para **shipping**
+- "cost": "123456.26"
+    Decimal, com duas casas decimais separadas por ponto (p.e, 1234.56), maior que 0.00 e menor ou igual a 9999999.00.
+
+
+### Configurando a referencia
+
+Referencia é geralmente o código que identifica a compra em seu sistema
+
+Por padrão a referencia será prefizada com "REF", mas isso pode ser alterado setando um prefixo diferente
+
+```python
+pg.reference_prefix = "CODE"
+pg.reference_prefix = None  # para desabilitar o prefixo
+```
+
+```python
+pg.reference = "00123456789"
+print pg.reference
+"REF00123456789"
+```
+
+### Configurando valor extra
+
+Especifica um valor extra que deve ser adicionado ou subtraído ao valor total do pagamento. Esse valor pode representar uma taxa extra a ser cobrada no pagamento ou um desconto a ser concedido, caso o valor seja negativo.
+
+Formato: Float (positivo ou negativo).
+
+```python
+pg.extra_amount = 12.70
+```
+
+### Inserindo produtos no carrinho
+
+O carrinho de compras é uma lista contendo dicionários que representam cada produto nos seguinte formato.
+
+#### adicionando vários produtos
+
+```python
+pg.items = [
+    {"id": "0001", "description": "Produto 1", "amount": 354.20, "quantity": 2, "weight": 200},
+    {"id": "0002", "description": "Produto 2", "amount": 50, "quantity": 1, "weight": 1000}
+]
+```
+
+O **weight** do produto é representado em gramas
+
+#### Adicionando um produto por vez
+
+Da forma tradicional
+
+```python
+pg.items.append(
+    {"id": "0003", "description": "Produto 3", "amount": 354.20, "quantity": 2, "weight": 200},
+)
+```
+
+Ou atraves do helper
+
+```python
+pg.add_item(id="0003", description="produto 4", amount=320, quantity=1, weight=2500)
+```
+
+### Configurando a URL de redirect
+
+Para onde o comprador será redirecionado após completar o pagamento
+
+```python
+pg.redirect_url = "http://meusite.com/obrigado"
+```
+
+### Configurando a URL de notificaçao (opcional)
+
+```python
+pg.notification_url = "http://meusite.com/notification"
+```
+
+### Efetuando o processo de checkout
+
+Depois que o carrinho esta todo configurado e com seus itens inseridos, ex: quando o seu cliente clicar no botão "efetuar pagamento", o seguinte método deverá ser executado.
+
+```python
+response = pg.checkout()
+```
+
+O método checkout faz a requisição ao pagseguro e retorna um objeto PagSeguroResponse com os atributos code, date, payment_url, errors.
+
+É aconselhavel armazenar o código da transação em seu banco de dados juntamente com as informações do carrinho para seu controle interno.
+
+Utilize a **payment_url** para enviar o comprador para a página de pagamento do pagseguro.
+
+```python
+return redirect(response.payment_url)
+```
+
+Após o pagamento o comprador será redirecionado de volta para os eu site através da configuração de url de retorno global ou utilizará a url especificada no parametro **redirect_url**
+
+# Notificações
+
+O PagSeguro envia as notificações para a URL que você configurou usando o protocolo HTTP, pelo método POST.
+
+Suponde que você receberá a notificação em: http://seusite.com/notification
+
+> Pseudo codigo
+
+```python
+from pagseguro import PagSeguro
+
+def notification_view(request):
+    notification_code = request.POST['notificationCode']
+    pg = PagSeguro(email="seuemail@dominio.com", token="ABCDEFGHIJKLMNO")
+    notification_data = pg.check_notification(notification_code)
+    ...
+```
+
+No exemplo acima pegamos o **notificationCode** que foi enviado através do pagseguro e fizemos uma consulta para pegar os dados da notificação, o retorno será em um dicionário Python com o seguinte formato:
+
+```python
+{
+    "date": datetime(2013, 01, 01, 18, 23, 0000),
+    "code": "XDFD454545",
+    "reference": "REF00123456789",
+    "type": 1,
+    "status": 3,
+    "cancellationSource": "INTERNAL",
+    ...
+}
+```
+
+A lista completa de valores pode ser conferida em  https://pagseguro.uol.com.br/v2/guia-de-integracao/api-de-notificacoes.html
+
+
+# Implementações
+
+> Implementações a serem feitas, esperando o seu Pull Request!!!
+
+## Quokka CMS
+[Quokka Cart PagSeguro Processor](https://github.com/pythonhub/quokka-cart/blob/master/processors/pagseguro_processor.py)
+
+## Exemplo em Django
+
+## Exemplo em Flask
+
+[FlaskSeguro](https://github.com/rochacbruno/python-pagseguro/tree/master/examples/flask)
+by @shyba
+
+## Exemplo em web2py
+
+
---
+++ packages/pagseguro/pagseguro-0.1.7.dev0.tar.gz/pagseguro-0.1.7.dev0/pagseguro/__init__.py
@@ -0,0 +1,271 @@
+# coding: utf-8
+import logging
+import requests
+import xmltodict
+
+from .configs import Config
+from .utils import parse_date, is_valid_email, is_valid_cpf
+
+logger = logging.getLogger()
+
+
+class PagSeguroNotificationResponse(object):
+    def __init__(self, xml, config=None):
+        self.xml = xml
+        self.config = config or {}
+        self.parse_xml(xml)
+
+    def __getitem__(self, key):
+        getattr(self, key, None)
+
+    def parse_xml(self, xml):
+        try:
+            parsed = xmltodict.parse(xml, encoding="iso-8859-1")
+        except Exception as e:
+            logger.debug(
+                "Cannot parse the returned xml '{0}' -> '{1}'".format(xml, e)
+            )
+            parsed = {}
+
+        transaction = parsed.get('transaction', {})
+        for k, v in transaction.iteritems():
+            setattr(self, k, v)
+
+
+class PagSeguroCheckoutResponse(object):
+    def __init__(self, xml, config=None):
+        self.xml = xml
+        self.config = config or {}
+        self.code = None
+        self.date = None
+        self.errors = None
+        self.payment_url = None
+        logger.debug(self.__dict__)
+        self.parse_xml(xml)
+
+    def parse_xml(self, xml):
+        """ parse returned data """
+        try:
+            parsed = xmltodict.parse(xml, encoding="iso-8859-1")
+        except Exception as e:
+            logger.debug(
+                "Cannot parse the returned xml '{0}' -> '{1}'".format(xml, e)
+            )
+            parsed = {}
+
+        if 'errors' in parsed:
+            self.errors = parsed['errors']['error']
+            return
+
+        checkout = parsed.get('checkout', {})
+        self.code = checkout.get('code')
+        self.date = parse_date(checkout.get('date'))
+
+        self.payment_url = self.config.PAYMENT_URL % self.code
+
+
+class PagSeguroTransactionSearchResult(object):
+
+    current_page = None
+    total_pages = None
+    results_in_page = None
+    transactions = []
+
+    def __init__(self, xml, config=None):
+        self.xml = xml
+        self.config = config or {}
+        self.parse_xml(xml)
+
+    def __getitem__(self, key):
+        getattr(self, key, None)
+
+    def parse_xml(self, xml):
+        try:
+            parsed = xmltodict.parse(xml, encoding="iso-8859-1")
+        except Exception as e:
+            logger.debug(
+                "Cannot parse the returned xml '{0}' -> '{1}'".format(xml, e)
+            )
+            parsed = {}
+
+        search_result = parsed.get('transactionSearchResult', {})
+        self.transactions = search_result.get('transactions', {})
+        self.transactions = self.transactions.get('transaction', [])
+        if not isinstance(self.transactions, list):
+            self.transactions = [self.transactions]
+        self.current_page = search_result.get('currentPage', None)
+        if self.current_page is not None:
+            self.current_page = int(self.current_page)
+        self.results_in_page = search_result.get('resultsInThisPage', None)
+        if self.results_in_page is not None:
+            self.results_in_page = int(self.results_in_page)
+        self.total_pages = search_result.get('totalPages', None)
+        if self.total_pages is not None:
+            self.total_pages = int(self.total_pages)
+
+
+class PagSeguro(object):
+    """ Pag Seguro V2 wrapper """
+
+    PAC = 1
+    SEDEX = 2
+    NONE = 3
+
+    def __init__(self, email, token, data=None):
+        self.config = Config()
+        self.data = {}
+        self.data['email'] = email
+        self.data['token'] = token
+
+        if data and isinstance(data, dict):
+            self.data.update(data)
+
+        self.items = []
+        self.sender = {}
+        self.shipping = {}
+        self._reference = ""
+        self.extra_amount = None
+        self.redirect_url = None
+        self.notification_url = None
+        self.abandon_url = None
+
+    def build_checkout_params(self, **kwargs):
+        """ build a dict with params """
+        params = kwargs or {}
+        if self.sender:
+            params['senderName'] = self.sender.get('name')
+            params['senderAreaCode'] = self.sender.get('area_code')
+            params['senderPhone'] = self.sender.get('phone')
+            params['senderEmail'] = is_valid_email(self.sender.get('email'))
+            params['senderCPF'] = is_valid_cpf(self.sender.get('cpf'))
+            params['senderBornDate'] = self.sender.get('born_date')
+
+        if self.shipping:
+            params['shippingType'] = self.shipping.get('type')
+            params['shippingAddressStreet'] = self.shipping.get('street')
+            params['shippingAddressNumber'] = self.shipping.get('number')
+            params['shippingAddressComplement'] = self.shipping.get(
+                'complement'
+            )
+            params['shippingAddressDistrict'] = self.shipping.get('district')
+            params['shippingAddressPostalCode'] = self.shipping.get(
+                'postal_code'
+            )
+            params['shippingAddressCity'] = self.shipping.get('city')
+            params['shippingAddressState'] = self.shipping.get('state')
+            params['shippingAddressCountry'] = self.shipping.get('country',
+                                                                 'BRA')
+
+        if self.shipping and self.shipping.get('cost'):
+            params['shippingCost'] = self.shipping.get('cost')
+
+        if self.extra_amount:
+            params['extraAmount'] = self.extra_amount
+
+        params['reference'] = self.reference
+
+        if self.redirect_url:
+            params['redirectURL'] = self.redirect_url
+
+        if self.notification_url:
+            params['notificationURL'] = self.notification_url
+
+        if self.abandon_url:
+            params['abandonURL'] = self.abandon_url
+
+        for i, item in enumerate(self.items, 1):
+            params['itemId%s' % i] = item.get('id')
+            params['itemDescription%s' % i] = item.get('description')
+            params['itemAmount%s' % i] = item.get('amount')
+            params['itemQuantity%s' % i] = item.get('quantity')
+            params['itemWeight%s' % i] = item.get('weight')
+            params['itemShippingCost%s' % i] = item.get('shipping_cost')
+
+        self.data.update(params)
+        self.clean_none_params()
+
+    def clean_none_params(self):
+        copy = dict(self.data)
+        for k, v in copy.iteritems():
+            if not v:
+                del self.data[k]
+
+    @property
+    def reference_prefix(self):
+        return self.config.REFERENCE_PREFIX or "%s"
+
+    @reference_prefix.setter
+    def reference_prefix(self, value):
+        self.config.REFERENCE_PREFIX = (value or "") + "%s"
+
+    @property
+    def reference(self):
+        return self.reference_prefix % self._reference
+
+    @reference.setter
+    def reference(self, value):
+        if not isinstance(value, (str, unicode)):
+            value = str(value)
+        if value.startswith(self.reference_prefix):
+            value = value[len(self.reference_prefix):]
+        self._reference = value
+
+    def get(self, url):
+        """ do a get transaction """
+        return requests.get(url, params=self.data, headers=self.config.HEADERS)
+
+    def post(self, url):
+        """ do a post request """
+        return requests.post(url, data=self.data, headers=self.config.HEADERS)
+
+    def checkout(self, **kwargs):
+        """ create a pagseguro checkout """
+        self.data['currency'] = self.config.CURRENCY
+        self.build_checkout_params(**kwargs)
+        response = self.post(url=self.config.CHECKOUT_URL)
+        return PagSeguroCheckoutResponse(response.content, config=self.config)
+
+    def check_notification(self, code):
+        """ check a notification by its code """
+        response = self.get(url=self.config.NOTIFICATION_URL % code)
+        return PagSeguroNotificationResponse(response.content, self.config)
+
+    def check_transaction(self, code):
+        """ check a transaction by its code """
+        response = self.get(url=self.config.TRANSACTION_URL % code)
+        return PagSeguroNotificationResponse(response.content, self.config)
+
+    def query_transactions(self, initial_date, final_date, page=None,
+                           max_results=None):
+        """ query transaction by date range """
+        last_page = False
+        results = []
+        while last_page is False:
+            search_result = self._consume_query_transactions(
+                initial_date, final_date, page, max_results
+            )
+            results.extend(search_result.transactions)
+            if search_result.current_page is None or \
+               search_result.total_pages is None or \
+               search_result.current_page == search_result.total_pages:
+                last_page = True
+            else:
+                page = search_result.current_page + 1
+
+        return results
+
+    def _consume_query_transactions(self, initial_date, final_date, page=None,
+                                    max_results=None):
+        querystring = {
+            'initialDate': initial_date.strftime('%Y-%m-%dT%H:%M'),
+            'finalDate': final_date.strftime('%Y-%m-%dT%H:%M'),
+            'page': page,
+            'maxPageResults': max_results,
+        }
+        self.data.update(querystring)
+        self.clean_none_params()
+        response = self.get(url=self.config.QUERY_TRANSACTION_URL)
+        return PagSeguroTransactionSearchResult(response.content, self.config)
+
+    def add_item(self, **kwargs):
+        self.items.append(kwargs)
