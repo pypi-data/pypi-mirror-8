fc16eb6b39b6114fdfa90f558a691dc48dcb2b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:30 +0000

    Add package bqclient/0.5.0/bqclient-0.5.0.tar.gz

---
+++ packages/bqclient/bqclient-0.5.0.tar.gz/bqclient-0.5.0/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+import sys
+from setuptools import find_packages
+if sys.version_info > (3,):
+    from setuptools import setup
+else:
+    from distutils.core import setup
+
+NAME = "bqclient"
+DESCRIPTION = "BotQueue's client bumblebee"
+URL = "http://github.com/Hoektronics/bumblebee/"
+
+PACKAGES = ["bumblebee", "bumblebee.drivers"]
+PKG_DATA = {"bumblebee":["config-dist.json","imagesnap"]}
+DEPENDS = ["git://github.com/makerbot/pyserial#egg=pyserial"]
+REQUIRES = ["Pygments", "pyserial", "requests-oauth", "beautifulsoup4"]
+EXCLUDES = ["pydoc"]
+
+setup(name=NAME,
+      author="Zach 'Hoeken' Smith",
+      author_email="hoeken@gmail.com",
+      maintainer="Justin Nesselrotte",
+      maintainer_email="jnesselr@harding.edu",
+      version='0.5.0',
+      url=URL,
+      packages=PACKAGES,
+      entry_points={
+         "console_scripts":["bumblebee = bumblebee.app:main"]
+      },
+      dependency_links=DEPENDS,
+      install_requires=REQUIRES,
+      package_data=PKG_DATA
+     )
+
