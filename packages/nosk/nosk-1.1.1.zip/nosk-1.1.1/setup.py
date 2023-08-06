8f631ef8d1fad49b1832d4e8be6f875d122eefce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:12 +0000

    Add package nosk/1.1.1/nosk-1.1.1.zip

---
+++ packages/nosk/nosk-1.1.1.zip/nosk-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='nosk',
+    version='1.1.1',
+    py_modules=['nosk'],
+    author='NoShangke',
+    author_email='imakeit@live.cn',
+    url='weibo.com/u/2823860545',
+    description='An app for print list',
+    )
