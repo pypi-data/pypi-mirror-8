1c80ec4fc68e57fce955a7f31daeed9f232a7c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:27 +0000

    Add package plaid-python/0.2.0/plaid-python-0.2.0.tar.gz

---
+++ packages/plaid-python/plaid-python-0.2.0.tar.gz/plaid-python-0.2.0/setup.py
@@ -0,0 +1,32 @@
+from ez_setup import use_setuptools
+
+use_setuptools()
+
+from setuptools import setup, find_packages
+import plaid
+
+
+url = 'https://github.com/plaid/plaid-python'
+
+setup(
+    name='plaid-python',
+    version=plaid.__version__,
+    description='Simple Python API client for Plaid',
+    long_description='',
+    keywords='api, client, plaid',
+    author='Chris Forrette',
+    author_email='chris@chrisforrette.com',
+    url=url,
+    download_url='{}/tarball/v{}'.format(url, plaid.__version__),
+    license='MIT',
+    packages=find_packages(exclude='tests'),
+    package_data={'README': ['README.md']},
+    install_requires=['requests==2.2.1'],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+    ]
+)
