9636902042bf789974fb5010cf011ff74e1b5374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:56 +0000

    Add package zope.tales/4.1.0/zope.tales-4.1.0.tar.gz

---
+++ packages/zope.tales/zope.tales-4.1.0.tar.gz/zope.tales-4.1.0/CHANGES.rst
@@ -0,0 +1,85 @@
+Changes
+=======
+
+4.1.0 (2014-12-29)
+------------------
+
+.. note::
+
+   Support for PyPy3 is pending release of a fix for:
+   https://bitbucket.org/pypy/pypy/issue/1946
+
+- Add support for Python 3.4.
+
+- Add support for testing on Travis.
+
+
+4.0.2 (2013-11-12)
+------------------
+
+- Add missing ``six`` dependency
+
+
+4.0.1 (2013-02-22)
+------------------
+
+- Fix a previously untested Python 3.3 compatibility problem.
+
+
+4.0.0 (2013-02-14)
+------------------
+
+- Remove hard dependency on ``zope.tal``, which was already conditionalized
+  but required via ``setup.py``.
+
+- Add support for Python 3.3 and PyPy.
+
+- Replace deprecated ``zope.interface.implements`` usage with equivalent
+  ``zope.interface.implementer`` decorator.
+
+- Drop support for Python 2.4 and 2.5.
+
+- Fix documentation link in README.txt
+
+
+3.5.2 (2012-05-23)
+------------------
+
+- Subexpressions of a 'string:' expression can be only path expressions.
+  https://bugs.launchpad.net/zope.tales/+bug/1002242
+
+
+3.5.1 (2010-04-30)
+------------------
+
+- Remove use of ``zope.testing.doctestunit`` in favor of stdlib's 'doctest.
+
+
+3.5.0 (2010-01-01)
+------------------
+
+- Port the lazy expression from ``Products.PageTemplates``.
+
+
+3.4.0 (2007-10-03)
+------------------
+
+- Update package setup.
+
+- Initial release outside the Zope 3 trunk.
+
+
+3.2.0 (2006-01-05)
+------------------
+
+- Corresponds to the verison of the zope.tales package shipped as part of
+  the Zope 3.2.0 release.
+
+- Documentation / test fixes.
+
+
+3.0.0 (2004-11-07)
+------------------
+
+- Corresponds to the verison of the zope.tales package shipped as part of
+  the Zope X3.0.0 release.
---
+++ packages/zope.tales/zope.tales-4.1.0.tar.gz/zope.tales-4.1.0/setup.py
@@ -0,0 +1,91 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.tales package
+"""
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+def alltests():
+    import os
+    import sys
+    import unittest
+    # use the zope.testrunner machinery to find all the
+    # test suites we've put under ourselves
+    import zope.testrunner.find
+    import zope.testrunner.options
+    here = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+    args = sys.argv[:]
+    defaults = ["--test-path", here]
+    options = zope.testrunner.options.get_options(args, defaults)
+    suites = list(zope.testrunner.find.find_suites(options))
+    return unittest.TestSuite(suites)
+
+setup(name='zope.tales',
+      version='4.1.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Zope Template Application Language Expression Syntax '
+                  '(TALES)',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')
+      ),
+      keywords="zope template xml tales",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.tales',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope'],
+      extras_require=dict(
+          test=['zope.testing'],
+          tal=['zope.tal']),
+      install_requires=[
+          'setuptools',
+          'zope.interface',
+          'six'],
+      tests_require=['zope.testing', 'zope.testrunner'],
+      test_suite='__main__.alltests',
+      include_package_data=True,
+      zip_safe=False,
+      )
