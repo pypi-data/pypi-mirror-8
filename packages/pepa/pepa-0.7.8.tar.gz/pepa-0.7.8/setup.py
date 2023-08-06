2ede6c57067de864bd780b60b09d4efe909298b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:32 +0000

    Add package pepa/0.7.8/pepa-0.7.8.tar.gz

---
+++ packages/pepa/pepa-0.7.8.tar.gz/pepa-0.7.8/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Setup script for pepa
+'''
+
+from setuptools import setup, find_packages
+import sys, os
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: POSIX :: Linux',
+]
+REQUIRES = [
+    'pyyaml',
+    'jinja2',
+    'argparse',
+    'logging',
+    'colorlog',
+    'cerberus',
+    'requests',
+]
+
+setup(
+    name             = 'pepa',
+    version          = '0.7.8',
+
+    description      = 'Configuration templating for SaltStack using Hierarchical substitution and Jinja',
+    long_description = open("README.rst").read(),
+
+    author           = 'Michael Persson',
+    author_email     = 'michael.ake.persson@gmail.com',
+    url              = 'https://github.com/mickep76/pepa.git',
+    license          = 'Apache License, Version 2.0',
+
+    packages         = find_packages(exclude=['examples', 'tests']),
+    classifiers      = CLASSIFIERS,
+    scripts          = ['scripts/pepa', 'scripts/pepa-test'],
+    install_requires = REQUIRES,
+)
