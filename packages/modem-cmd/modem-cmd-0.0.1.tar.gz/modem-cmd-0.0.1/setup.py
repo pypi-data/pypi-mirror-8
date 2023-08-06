33f9a1f0f2fe3002ddcb2b9db47e511f41217da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:31 +0000

    Add package modem-cmd/0.0.1/modem-cmd-0.0.1.tar.gz

---
+++ packages/modem-cmd/modem-cmd-0.0.1.tar.gz/modem-cmd-0.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+""" modem-cmd installation script """
+
+VERSION = '0.0.1'
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('requirements.txt') as f:
+    requires = f.readlines()
+
+setup(name='modem-cmd',
+      version='{0}'.format(VERSION),
+      description='Send arbitrary AT commands to your modem',
+      license='MIT',
+      author='YuLun Shih',
+      author_email='shih@yulun.me',
+      url='https://github.com/imZack/modem-cmd',
+      download_url=
+      'https://github.com/imZack/modem-cmd/archive/{0}.tar.gz'.format(VERSION),
+      keywords=['modem', 'at commands', 'serial'],
+      py_modules=['modemcmd'],
+      scripts=['modemcmd.py'],
+      install_requires=requires,
+      classifiers=[
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Telecommunications Industry',
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Communications :: Telephony',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: System :: Hardware',
+          'Topic :: Terminals :: Serial',
+          'Topic :: Utilities',
+          'Operating System :: OS Independent',
+      ]
+      )
