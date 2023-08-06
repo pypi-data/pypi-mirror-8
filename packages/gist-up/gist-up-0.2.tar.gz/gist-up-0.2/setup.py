0791f0318ea9588afcb32d1464b86d17a52e8b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:54 +0000

    Add package gist-up/0.2/gist-up-0.2.tar.gz

---
+++ packages/gist-up/gist-up-0.2.tar.gz/gist-up-0.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+desc = ('A command line utility for publishing a directory and '
+        'updating gists on your github account.')
+
+setup(name='gist-up',
+      # packages=['gist-up'],
+      version='0.2',
+      description=desc,
+      author='James R. Qualls',
+      author_email='qualls.james@gmail.com',
+      url='https://github.com/sourcec0de/gistup',
+      download_url='https://github.com/sourcec0de/gistup/tarball/0.1',
+      keywords=['gist', 'blogging', 'command line', 'github', 'publish'],
+      scripts=['bin/gistup'],
+      install_requires=[
+          'PyGithub==1.25.2',
+          'PyYAML==3.11',
+          'argparse==1.2.2',
+          'termcolor==1.1.0'
+      ],
+      classifiers=[])
