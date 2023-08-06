659d8fd9db35a1876116c1058cd4a20609c22b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:56 +0000

    Add package gunicorn-thrift/0.1.1/gunicorn_thrift-0.1.1-py2-none-any.whl

---
+++ packages/gunicorn-thrift/gunicorn_thrift-0.1.1-py2-none-any.whl/gunicorn_thrift/utils.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import os
+import importlib
+from gunicorn.errors import AppImportError
+
+
+def load_obj(import_path):
+    parts = import_path.split(":", 1)
+    if len(parts) == 1:
+        raise ValueError("Wrong import path, module:obj please")
+
+    module, obj = parts[0], parts[1]
+
+    try:
+        mod = importlib.import_module(module)
+    except ImportError:
+        if module.endswith(".py") and os.path.exists(module):
+            raise ImportError(
+                "Failed to find application, did "
+                "you mean '%s:%s'?" % (module.rsplit(".", 1)[0], obj)
+                )
+        else:
+            raise
+
+    try:
+        app = getattr(mod, obj)
+    except AttributeError:
+        raise AppImportError("Failed to find application object: %r" % obj)
+
+    return app
