8791d4659afb22254208c5202f0a0429d234e4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:14 +0000

    Add package tango-articles/0.7.1/tango-articles-0.7.1.tar.gz

---
+++ packages/tango-articles/tango-articles-0.7.1.tar.gz/tango-articles-0.7.1/CHANGES.md
@@ -0,0 +1,40 @@
+# Tango Articles Change Log
+
+## 0.7.1
+* Updated h-entry microformat tags
+
+## 0.7.0
+* Updates for Django 1.7
+* Moved admin media definitions out. Now supported by tango-admin.
+
+## 0.6.0
+* Stubbed base URLs
+
+## 0.5.4
+* Comment recovery
+
+## 0.5.3
+* Articles list images resolve correctly
+
+## 0.5.2
+* Articles list now defaults to destination_slug 'articles', so the url /articles/ or /news/ returns all articles with the destination 'articles' (assuming no more specific destination is wanted)
+
+## 0.5.1
+* Corrected bad template tag in article detail
+
+## 0.5
+* No more calls to markup
+* Improved how-to documentation
+* Removed redundant dependencies
+
+## 0.4.1
+* Removed requirements satisfied by tango-shared
+* Improved hnew microformatting in article details
+
+### 0.4
+* Drag and drop ordering of article images
+* Bulk photo uploading
+* Improved article detail templates
+* Clearer blog and article entry forms
+* Tentative Python 3 support
+* Django 1.6 support
---
+++ packages/tango-articles/tango-articles-0.7.1.tar.gz/tango-articles-0.7.1/articles/admin.py
@@ -0,0 +1,96 @@
+from django.contrib import admin
+
+from .forms import BaseArticleForm
+from .models import ArticleImage, Sidebar, Destination, Category, Brief, Article
+from .models import supports_video, supports_polls, supports_galleries
+
+from tango_admin.admin import TextCounterWidget
+
+if supports_video:
+    from video.admin import VideoInline
+
+
+class ArticleImagesInline(admin.TabularInline):
+    model = ArticleImage
+    extra = 3
+
+
+class SidebarInline(admin.TabularInline):
+    model = Sidebar
+    extra = 2
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    form = BaseArticleForm
+
+    ordering = ['-created']
+    list_display = ('title', 'author', 'destination', 'created',)
+    list_filter = ('created', 'last_modified', 'enable_comments', 'publication', 'destination')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['body', 'title']
+    filter_horizontal = ['sections', 'articles']
+    inlines = [
+        ArticleImagesInline,
+        SidebarInline,
+    ]
+    related = ('Related', {
+        'fields': ['articles'],
+        'description': 'Other content directly related to this article'
+    })
+    if supports_video:
+        inlines.append(VideoInline)
+    if supports_galleries:
+        related[1]['fields'].insert(0, 'galleries')
+        filter_horizontal.append('galleries')
+    if supports_polls:
+        related[1]['fields'].insert(0, 'polls')
+
+    fieldsets = (
+        ('Author info', {'fields': (('author', 'guest_author'))}),
+        ('Header', {'fields': ('overline', 'title', 'subhead')}),
+        ('Content', {'fields': ('summary', 'body', 'pull_quote', 'endnote')}),
+        ('Admin fields', {
+            'description': 'You should rarely, if ever, need to touch these fields.',
+            'fields': ('slug', 'enable_comments', 'sites', 'override_url'),
+            'classes': ['collapse']
+        }),
+        ('Routing', {'fields': ('destination', 'sections')}),
+        related,
+        ('Meta',     {
+            'fields': (('publication'), 'featured'),
+            'description': 'Additional information about this story'
+        }),
+        ('Bulk photo upload', {
+            'fields': ('upload',),
+            'description': "Upload multiple photos at once, if you don't want to handle them individually."
+        })
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        for img in request.FILES.getlist('upload'):
+            ArticleImage(
+                image = img,
+                article = obj
+            ).save()
+        obj.bulk_upload = None
+        obj.save()
+
+
+class BriefAdmin(admin.ModelAdmin):
+    list_display = ('pub_date', 'text')
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'text':
+            kwargs['widget'] = TextCounterWidget()
+        return super(BriefAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
+
+class DestinationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Destination, DestinationAdmin)
+admin.site.register(Category)
+admin.site.register(Brief, BriefAdmin)
+admin.site.register(Article, ArticleAdmin)
