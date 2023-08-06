3a7f7bc31f9dc26cad0e1ba2a2e38b991146d1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:01 +0000

    Add package fsnav/0.9.1/fsnav-0.9.1.tar.gz

---
+++ packages/fsnav/fsnav-0.9.1.tar.gz/fsnav-0.9.1/fsnav/cmdl/count.py
@@ -0,0 +1,34 @@
+"""
+FS Nav utility: count
+"""
+
+
+import sys
+
+import click
+
+import fsnav
+from fsnav.cmdl import options
+
+
+@click.command()
+@options.version
+@options.license
+@click.argument(
+    'paths', metavar='path', nargs=-1, required=True
+)
+@click.pass_context
+def main(ctx, paths):
+
+    """
+    Quickly count items on the file system.
+
+    Only paths that exist will be counted
+    """
+
+    try:
+        click.echo(fsnav.count(paths))
+        sys.exit(0)
+    except Exception as e:
+        click.echo(e.message, err=True)
+        sys.exit(1)
