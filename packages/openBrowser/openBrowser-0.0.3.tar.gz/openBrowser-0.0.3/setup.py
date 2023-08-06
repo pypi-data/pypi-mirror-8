5bf7afdaa9a96b447b352c8147148c98d3d04cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:47 +0000

    Add package openBrowser/0.0.3/openBrowser-0.0.3.tar.gz

---
+++ packages/openBrowser/openBrowser-0.0.3.tar.gz/openBrowser-0.0.3/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name = 'openBrowser',
+    packages = ['openBrowser'],
+    version = '0.0.3',
+    description = 'A CLI helper to quick open file/url with browser in command line',
+    license='LICENSE',
+    author = 'lufeng',
+    author_email = 'lufengd3@gmail.com',
+    url = 'https://github.com/keith3/myPip/tree/master/openBrowser',
+    # download_url = 'https://github.com/keith3/myPip/openBrowser', 
+    keywords = ['browser', 'cli'],
+    classifiers = [],
+    entry_points = {
+        'console_scripts': [
+            'xopen = openBrowser.main:main',    
+        ]
+    },
+)
