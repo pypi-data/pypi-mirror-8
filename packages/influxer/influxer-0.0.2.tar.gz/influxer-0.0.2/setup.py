5b5fcc04422a869e752ba438cb1aea7f2051a7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:33 +0000

    Add package influxer/0.0.2/influxer-0.0.2.tar.gz

---
+++ packages/influxer/influxer-0.0.2.tar.gz/influxer-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='influxer',
+    version='0.0.2',
+    description='A small pixel tracker built in python and designed to send data to InfluxDB',
+    author='Vince Forgione',
+    author_email='vforgione@theonion.com',
+    packages=['influxer'],
+    install_requires=[
+        'influxdb==0.1.13'
+    ],
+    url='https://github.com/theonion/influxer'
+)
