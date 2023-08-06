fb7285d8ea1107e69d61192c160690005b423b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:37 +0000

    Add package json-formatter/0.1.0-alpha-20141128-1014-94cc025/json-formatter-0.1.0-alpha-20141128-1014-94cc025.tar.gz

---
+++ packages/json-formatter/json-formatter-0.1.0-alpha-20141128-1014-94cc025.tar.gz/json-formatter-0.1.0-alpha-20141128-1014-94cc025/setup.py
@@ -0,0 +1,32 @@
+import sys
+
+if sys.version_info < (2, 6):
+    print(sys.stderr, "{}: need Python 2.6 or later.".format(sys.argv[0]))
+    print(sys.stderr, "Your python is {}".format(sys.version))
+    sys.exit(1)
+
+from setuptools import setup
+
+setup(
+    name="json-formatter",
+    version="0.1.0",
+    url="http://github.com/nikicat/python-json-logger",
+    license="BSD",
+    description="A python library adding a json log formatter",
+    author="Zakaria Zajac",
+    author_email="zak@madzak.com",
+    package_dir={'': 'src'},
+    packages=[''],
+    test_suite="tests.tests",
+    install_requires=['setuptools'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Logging',
+    ]
+)
