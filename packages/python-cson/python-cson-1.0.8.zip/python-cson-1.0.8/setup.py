9e7b4e986054ec7a9e54b4d0fa886a879ac88561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:12 +0000

    Add package python-cson/1.0.8/python-cson-1.0.8.zip

---
+++ packages/python-cson/python-cson-1.0.8.zip/python-cson-1.0.8/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+def read_readme(fname):
+   try:
+      import pypandoc
+      return pypandoc.convert('README.md','rst')
+   except (IOError, ImportError):
+      return ''
+
+setup(
+    name = "python-cson",
+    version = "1.0.8",
+    url = 'https://github.com/lifthrasiir/cson',
+    download_url = 'https://github.com/gt3389b/python-cson/',
+    license = 'MIT',
+    description = "Python library for CSON (schema-compressed JSON)",
+    author = 'Russell Leake',
+    author_email = 'gt3389b@gmail.com',
+    py_modules = ["cson"],
+    long_description = read_readme('README.md'),
+    include_package_data = True,
+    zip_safe = False,
+    keywords = 'json cson cursive'
+)
