dc5e36302a4789d0b2859df914f0667b7184aa4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:34 +0000

    Add package syringe/0.3.0/syringe-0.3.0.tar.gz

---
+++ packages/syringe/syringe-0.3.0.tar.gz/syringe-0.3.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+
+try:
+    from unittest import mock  # noqa
+except:
+    kwargs = {
+        'tests_require': 'mock',
+        'extras_require': {
+            'mock': 'mock'
+        }
+    }
+else:
+    kwargs = {}
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+
+setup(
+    name='syringe',
+    version='0.3.0',
+    author='Remco Haszing',
+    author_email='remcohaszing@gmail.com',
+    url='https://github.com/remcohaszing/python-syringe',
+    license='MIT',
+    description='A simple dependency injection library',
+    long_description=readme,
+    py_modules=['syringe'],
+    test_suite='tests',
+    zip_safe=True,
+    **kwargs)
