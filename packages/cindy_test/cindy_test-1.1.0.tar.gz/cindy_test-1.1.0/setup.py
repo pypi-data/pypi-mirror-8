dfdf9c79dbd2ebd3a6077310571d8eeec02b47b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:25 +0000

    Add package cindy_test/1.1.0/cindy_test-1.1.0.tar.gz

---
+++ packages/cindy_test/cindy_test-1.1.0.tar.gz/cindy_test-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = 'cindy_test',
+    version = '1.1.0',
+    py_modules = ['cindy_test'],
+    author = 'cindy',
+    author_email = 'zhengping.pin@gmail.com',
+    description = 'A simple printer of nested list',
+    )
