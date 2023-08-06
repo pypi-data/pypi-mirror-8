2c08f35100dc41a73b329af5b4a3615e1f525c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:06 +0000

    Add package topographica/0.9.8-1/topographica-0.9.8-1.tar.gz

---
+++ packages/topographica/topographica-0.9.8-1.tar.gz/topographica-0.9.8-1/topo/tests/testoutputfnsoptimized.py
@@ -0,0 +1,23 @@
+"""
+
+$Id$
+"""
+
+# CEBHACKALERT: we should delete this file, right?
+
+import unittest
+
+from numpy.oldnumeric import array, Float32
+
+# Currently empty; should be expanded
+cases = []
+
+suite = unittest.TestSuite()
+suite.addTests(unittest.makeSuite(case) for case in cases)
+              
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=1).run(suite)
+    
+
+
+        
