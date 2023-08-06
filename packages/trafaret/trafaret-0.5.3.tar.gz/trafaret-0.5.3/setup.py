992afb59d3382827c17576b6dc2659047d89490b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:22 +0000

    Add package trafaret/0.5.3/trafaret-0.5.3.tar.gz

---
+++ packages/trafaret/trafaret-0.5.3.tar.gz/trafaret-0.5.3/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os.path
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setupconf = dict(
+    name='trafaret',
+    version='0.5.3',
+    license='BSD',
+    url='https://github.com/Deepwalker/trafaret/',
+    author='Barbuza, Deepwalker, nimnull',
+    author_email='krivushinme@gmail.com',
+    description=('Validation and parsing library'),
+    long_description=read('README.rst'),
+    keywords='validatation form forms data schema',
+
+    packages=['trafaret', 'trafaret.contrib'],
+    extras_require=dict(
+        objectid=['pymongo>=2.4.1'],
+        rfc3339=['python-dateutil>=1.5']
+    ),
+    entry_points=dict(
+        trafaret=[
+            '.MongoId = trafaret.contrib.object_id:MongoId [objectid]',
+            '.DateTime = trafaret.contrib.rfc_3339:DateTime [rfc3339]'
+        ]
+    ),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
+
+if __name__ == '__main__':
+    setup(**setupconf)
---
+++ packages/trafaret/trafaret-0.5.3.tar.gz/trafaret-0.5.3/trafaret/extras.py
@@ -0,0 +1,45 @@
+from . import Key, DataError, Any, catch_error, Dict
+
+
+class KeysSubset(Key):
+    """
+    From checkers and converters dict must be returned. Some for errors.
+
+    >>> from . import extract_error, Mapping, String
+    >>> cmp_pwds = lambda x: {'pwd': x['pwd'] if x.get('pwd') == x.get('pwd1') else DataError('Not equal')}
+    >>> d = Dict({KeysSubset('pwd', 'pwd1'): cmp_pwds, 'key1': String})
+    >>> sorted(d.check({'pwd': 'a', 'pwd1': 'a', 'key1': 'b'}).keys())
+    ['key1', 'pwd']
+    >>> extract_error(d.check, {'pwd': 'a', 'pwd1': 'c', 'key1': 'b'})
+    {'pwd': 'Not equal'}
+    >>> extract_error(d.check, {'pwd': 'a', 'pwd1': None, 'key1': 'b'})
+    {'pwd': 'Not equal'}
+    >>> get_values = (lambda d, keys: [d[k] for k in keys if k in d])
+    >>> join = (lambda d: {'name': ' '.join(get_values(d, ['name', 'last']))})
+    >>> Dict({KeysSubset('name', 'last'): join}).check({'name': 'Adam', 'last': 'Smith'})
+    {'name': 'Adam Smith'}
+    >>> Dict({KeysSubset(): Dict({'a': Any})}).check({'a': 3})
+    {'a': 3}
+    """
+
+    def __init__(self, *keys):
+        self.keys = keys
+        self.name = '[%s]' % ', '.join(self.keys)
+        self.trafaret = Any()
+
+    def pop(self, data):
+        subdict = dict((k, data.pop(k)) for k in self.keys_names() if k in data)
+        res = catch_error(self.trafaret, subdict)
+        if isinstance(res, DataError):
+            for k, e in res.error.items():
+                yield k, e if isinstance(e, DataError) else DataError(e)
+        else:
+            for k, v in res.items():
+                yield k, v
+
+    def keys_names(self):
+        if isinstance(self.trafaret, Dict):
+            for key in self.trafaret.keys_names():
+                yield key
+        for key in self.keys:
+            yield key
