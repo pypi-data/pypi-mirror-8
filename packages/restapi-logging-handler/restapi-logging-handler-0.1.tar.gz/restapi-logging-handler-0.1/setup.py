ae0e3eebb7d0d7cefafa9869c055d8d500315fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:51 +0000

    Add package restapi-logging-handler/0.1/restapi-logging-handler-0.1.tar.gz

---
+++ packages/restapi-logging-handler/restapi-logging-handler-0.1.tar.gz/restapi-logging-handler-0.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='restapi-logging-handler',
+    version='0.1',
+    description='A handler for the python logging module that allows \
+        sending logs through a REST-ful API.',
+    long_description=(read('README.md')),
+    packages=['restapi_logging_handler'],
+    install_requires=['requests'],
+    author='RJ Gilligan',
+    author_email='rj.gilligan@nrgnergy.com',
+    url='https://github.com/narwhaljames/restapi-logging-handler.git',
+    download_url='https://github.com/narwhaljames/restapi-logging-handler/tarball/0.1',
+    keywords=['rest', 'api', 'logging', 'handler', 'loggly'],
+    classifiers=[],
+    license='MIT',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
