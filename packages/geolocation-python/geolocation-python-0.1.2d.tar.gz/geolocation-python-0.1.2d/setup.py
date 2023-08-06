caee1aa800ed7617e1d7d4e1dc9ca7b2befdacc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:37 +0000

    Add package geolocation-python/0.1.2d/geolocation-python-0.1.2d.tar.gz

---
+++ packages/geolocation-python/geolocation-python-0.1.2d.tar.gz/geolocation-python-0.1.2d/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('LICENSE.txt', 'r') as f:
+    license_ = f.read()
+
+setup(
+    name='geolocation-python',
+    version='0.1.2d',
+    packages=['geolocation'],
+    url='',
+    download_url='https://github.com/slawek87/geolocation-python/',
+    license=license_,
+    author=u'Sławomir Kabik',
+    author_email='slawek@redsoftware.pl',
+    description='Geolocation is a simple and clever application which uses google maps api. '
+                'This application allows you to easily and quickly get information about given localisation. '
+                'Application returns such information as: country, city, route/street, street number, lat and lng.',
+    long_description=readme,
+    keywords=['Google Maps Api', 'Google lat', 'Google lng', 'Google Maps'],
+    install_requires=['setuptools', 'requests'],
+)
