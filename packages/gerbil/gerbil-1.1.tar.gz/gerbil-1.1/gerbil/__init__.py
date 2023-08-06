b4cdd57b05bc4eb8c6e55a2575b9e96d4298956f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:16 +0000

    Add package gerbil/1.1/gerbil-1.1.tar.gz

---
+++ packages/gerbil/gerbil-1.1.tar.gz/gerbil-1.1/gerbil/__init__.py
@@ -0,0 +1,65 @@
+from optparse import OptionParser
+from .gerbil import create_footer, merge_files, create_font_args
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+
+def main():
+    parser = OptionParser()
+    parser.add_option('-t', '--text',
+                      help="The text to appear on footer the page.")
+    parser.add_option('-i', '--input',
+                      help="The input file for the text to be added to.")
+    parser.add_option('-o', '--output',
+                      help="The ouput file to be saved.")
+    parser.add_option('-f', '--font',
+                      default="Bliss-Regular.ttf",
+                      help="Path to the TrueType font file to be used (*.ttf)")
+    parser.add_option('-c', '--font-color',
+                      default="#545454",
+                      help="Hex color, defaults to Grey: #545454")
+    parser.add_option('-s', '--font-size',
+                      type="float",
+                      default=8,
+                      help="The font size px to be used (default = 8)")
+    parser.add_option('-a', '--author',
+                      help="The author to appear in metadata.")
+    parser.add_option('-u', '--subject',
+                      help="The subject to appear in metadata.")
+    parser.add_option('--top',
+                      type="float",
+                      help="The padding from the left hand side of the page (cm)")
+    parser.add_option('--side',
+                      type="float",
+                      help="The padding from the top of the page (cm)")
+    parser.add_option('-x', '--page-width',
+                      type="float",
+                      help="The width of the page (cm)")
+    parser.add_option('-y', '--page-height',
+                      type="float",
+                      help="The height of the page (cm)")
+    parser.add_option('--paper-size',
+                      default="A4",
+                      help="Default = A4. The named size of the paper Supported: A0 - A6, B0 - B6, LETTER, LEGAL.  Paramter ignored if -x and -y are supplied ")
+    parser.add_option('--landscape',
+                      action="store_true", dest="landscape", default=False,
+                      help="Default = portrait unless this flag is supplied. Defines the page orientation,  (ignored if -x and -y are given)")
+    parser.add_option('--skip-pages',
+                      type="int",
+                      default=0,
+                      help="number of pages to skip before stamping starts. Default = 0"
+                      )
+    (options, args) = parser.parse_args()
+
+    err = create_font_args(options)
+    if not err:
+        pdfmetrics.registerFont(TTFont(options.font_name, options.font_path))
+        f, err = create_footer(options)
+        if err is None:
+            merge_files(options, f)
+        else:
+            print err
+    else:
+        print "Can't find {} Exiting".format(options.font)
+        exit(1)
---
+++ packages/gerbil/gerbil-1.1.tar.gz/gerbil-1.1/gerbil/gerbil.py
@@ -0,0 +1,124 @@
+import os
+from os.path import splitext, isfile, basename
+from optparse import OptionParser
+import StringIO
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+from reportlab.pdfgen import canvas
+from reportlab.lib import pagesizes
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib import colors
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFError
+from reportlab.lib.units import cm
+from clint.textui import progress
+
+
+def get_paper_size(size):
+    return {
+        'A0': pagesizes.A0,
+        'A1': pagesizes.A1,
+        'A2': pagesizes.A2,
+        'A3': pagesizes.A3,
+        'A4': pagesizes.A4,
+        'A5': pagesizes.A5,
+        'A6': pagesizes.A6,
+        'LETTER': pagesizes.LETTER,
+        'LEGAL': pagesizes.LEGAL,
+        'B0': pagesizes.B0,
+        'B1': pagesizes.B1,
+        'B2': pagesizes.B2,
+        'B3': pagesizes.B3,
+        'B4': pagesizes.B4,
+        'B5': pagesizes.B5,
+        'B6': pagesizes.B6,
+    }.get(size, pagesizes.A4)
+
+
+def get_page_size(options):
+    if options.page_width and options.page_height:
+        width, height = options.page_width * cm, options.page_height * cm
+    else:
+        width, height = get_paper_size(options.paper_size)
+        if options.landscape:
+            width, height = height, width
+    return width, height
+
+
+def test_if_file_exists(filename):
+    if filename is None:
+        return False
+    return isfile(filename)
+
+
+def create_font_args(options):
+    if test_if_file_exists(options.font):
+        font_name, font_path = (basename(splitext(options.font)[0]),
+                                options.font)
+        options.font_name = font_name
+        options.font_path = font_path
+        return False
+    else:
+        return True
+
+
+def create_footer(options):
+    if not options.text:
+        return None, "No text to write, exiting"
+
+    pdf = StringIO.StringIO()
+    width, height = get_page_size(options)
+
+    can = canvas.Canvas(pdf, pagesize=(width, height))
+    color = colors.HexColor(options.font_color)
+    can.setFillColor(color)
+
+    size = 8
+    if options.font_size:
+        size = options.font_size
+    can.setFont(options.font_name, size)
+    if options.top and options.side:
+        can.drawString(
+            options.side * cm, height - options.top * cm, options.text)
+    else:
+        can.drawCentredString(width / 2.0, height / 2.0, options.text)
+    can.save()
+    return pdf, None
+
+
+def merge_files(options, footer):
+    footer.seek(0)
+    new_pdf = PdfFileReader(footer)
+
+    if not test_if_file_exists(options.input):
+        print "Could not read the input file - did you specify one?"
+        exit(1)
+
+    if not options.output:
+        print "No output path specified"
+        exit(1)
+
+    try:
+        book = PdfFileReader(open(options.input, "rb"))
+    except Exception, e:
+        print "Unable to load input PDF - {}".format(e)
+        exit(1)
+
+    output = PdfFileWriter()
+
+    if options.author:
+        output.addMetadata({"/Author": options.author})
+
+    if options.subject:
+        output.addMetadata({"/Subject": options.subject})
+
+    for index, page in progress.dots(enumerate(book.pages)):
+        page = book.getPage(index)
+        if index >= options.skip_pages:
+            page.mergePage(new_pdf.getPage(0))
+        output.addPage(page)
+
+    outputStream = open(options.output, "wb")
+    output.write(outputStream)
+    outputStream.close()
+    print "Written {}".format(options.output)
