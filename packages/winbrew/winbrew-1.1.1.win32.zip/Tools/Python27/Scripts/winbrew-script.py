456f0ce34338bc16bb052ec1d8c5856931022ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:58 +0000

    Add package winbrew/1.1.1/winbrew-1.1.1.win32.zip

---
+++ packages/winbrew/winbrew-1.1.1.win32.zip/Tools/Python27/Scripts/winbrew-script.py
@@ -0,0 +1,10 @@
+#!D:\Tools\Python27\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'winbrew==1.1.1','console_scripts','winbrew'
+__requires__ = 'winbrew==1.1.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('winbrew==1.1.1', 'console_scripts', 'winbrew')()
+    )
