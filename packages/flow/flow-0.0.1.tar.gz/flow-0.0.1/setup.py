d71b44a3d8fd885d5af8e42cc174b64eee05676e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:39 +0000

    Add package flow/0.0.1/flow-0.0.1.tar.gz

---
+++ packages/flow/flow-0.0.1.tar.gz/flow-0.0.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# python imports
+from distutils.core import setup
+
+
+setup(
+    name         = "flow",
+    version      = "0.0.1",
+    packages     = ["flow"],
+    description  = "Lightweight python framework for easy but powerful flow "
+                   "control.",
+    author       = "Marcel Rieger",
+    author_email = "marcelrieger@icloud.com",
+    url          = "https://github.com/riga/flow",
+    keywords     = [
+        "job", "jobs", "steering", "workload", "workflow", "control", "flow"
+    ],
+    classifiers  = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
+)
