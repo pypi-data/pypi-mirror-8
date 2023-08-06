c3096d01602735ce7c214901559ea51a1648cf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:48 +0000

    Add package django-simple-elasticsearch/0.9.2/django-simple-elasticsearch-0.9.2.tar.gz

---
+++ packages/django-simple-elasticsearch/django-simple-elasticsearch-0.9.2.tar.gz/django-simple-elasticsearch-0.9.2/tests/test_django-simple-elasticsearch.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_django-simple-elasticsearch
+----------------------------------
+
+Tests for `django-simple-elasticsearch` module.
+"""
+
+import unittest
+
+from django-simple-elasticsearch import django-simple-elasticsearch
+
+
+class TestDjango-simple-elasticsearch(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
