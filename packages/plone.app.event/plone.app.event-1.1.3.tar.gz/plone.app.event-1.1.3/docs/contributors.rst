441f107506fca06f41135699da73563720ae5120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:25 +0000

    Add package plone.app.event/1.1.3/plone.app.event-1.1.3.tar.gz

---
+++ packages/plone.app.event/plone.app.event-1.1.3.tar.gz/plone.app.event-1.1.3/docs/contributors.rst
@@ -0,0 +1,58 @@
+Contributing
+============
+
+Contributions
+-------------
+
+Please report any bugs, issues or feature requests here:
+https://github.com/plone/plone.app.event/issues.
+
+And better yet, help out with documentation and pull-requests!
+
+.. note::
+
+    To accept your pull requests, we need you need to have Plone contributors
+    agreement signed and sent-in. For more information, see:
+    ` Contributor's Agreement for Plone Explained <http://plone.org/foundation/contributors-agreement/contributors-agreement-explained>`_.
+
+
+Contributors
+------------
+
+Contributors, please add you name here! By doing this, you also state, that you
+have signed the `Contributor's Agreement for Plone Explained
+<http://plone.org/foundation/contributors-agreement/contributors-agreement-explained>`_.
+Download it from `plone.org
+<http://plone.org/foundation/contributors-agreement/agreement.pdf/view>`_.
+Thanks!
+
+- Johannes Raggam, thet (Main author and PLIP implementation)
+- Andreas Jung, zopyx
+- David Glick, davisagli
+- Érico Andrei, ericof
+- Franklin Kingma, kingel
+- Gauthier Bastien, gbastien
+- Georg Bernhard, gogo
+- Giacomo Spettoli, giacomos
+- Guido Stevens, gyst
+- JeanMichel FRANCOIS, toutpt
+- Jens Klein, jensens
+- Lennart Regebro, regebro
+- Nathan Van Gheem, vangheem
+- Philip Bauer, pbauer
+- Robert Niederreiter, rnixx
+- Rok Garbas, garbas
+- Róman Joost, romanofski
+- Sean Upton, seanupton
+- Simone Orsi, simahawk
+- Thomas Desvenain, tdesvenain
+- Timo Stollenwerk, timo
+- Tom Gross, tomgross
+- Vincent Fretin, vincentfretin
+- Vitaliy Podoba, piv, vipod
+- Wolfgang Thomas, pysailor
+
+Find out who contributed::
+
+    $ git shortlog -s -e
+
---
+++ packages/plone.app.event/plone.app.event-1.1.3.tar.gz/plone.app.event-1.1.3/setup.py
@@ -0,0 +1,126 @@
+from setuptools import setup
+from setuptools import find_packages
+
+import os
+
+
+version = '1.1.3'
+
+
+setup(
+    name='plone.app.event',
+    version=version,
+    description="The Plone calendar framework",
+    long_description="%s%s%s%s" % (
+        open("README.rst").read() + "\n",
+        open(os.path.join('docs', 'installation.rst')).read() + "\n",
+        open(os.path.join('docs', 'contributors.rst')).read() + '\n',
+        open("CHANGES.rst").read(),
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)"
+    ],
+    keywords='plone event',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/plone/plone.app.event',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        'Acquisition',
+        'DateTime',
+        'Products.CMFCore',
+        'Products.CMFPlone',
+        'Products.DateRecurringIndex',
+        'Products.GenericSetup',
+        'Products.ZCatalog',
+        'Products.statusmessages',
+        'Zope2',
+        'collective.elephantvocabulary',
+        'icalendar>3.0',
+        'plone.app.contentlisting',
+        'plone.app.form',  # TODO: remove when z3cform portlets are in place
+        'plone.app.layout',
+        'plone.app.portlets >=2.5.1, <3.0',
+        'plone.app.querystring',
+        'plone.app.registry',
+        'plone.app.vocabularies',
+        'plone.browserlayer',
+        'plone.event>=1.0',
+        'plone.folder',
+        'plone.formwidget.datetime>=1.1',
+        'plone.formwidget.recurrence>=1.2.1',
+        'plone.memoize',
+        'plone.namedfile',
+        'plone.portlets',
+        'plone.registry',
+        'plone.uuid',
+        'plone.z3cform',
+        'pytz',
+        'transaction',
+        'z3c.form',
+        'zope.annotation',
+        'zope.component',
+        'zope.container',
+        'zope.contentprovider',
+        'zope.event',
+        'zope.formlib',
+        'zope.globalrequest',
+        'zope.i18n',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.lifecycleevent',
+        'zope.publisher',
+        'zope.schema',
+    ],
+    extras_require={
+        'archetypes': [
+            'AccessControl',
+            'Products.ATContentTypes',
+            'Products.Archetypes',
+            'Products.contentmigration',
+            'plone.app.imaging',
+            'plone.formwidget.datetime [archetypes]',
+            'plone.formwidget.recurrence [archetypes]',
+        ],
+        'dexterity': [
+            'plone.app.dexterity',
+            'plone.app.textfield',
+            'plone.app.z3cform',
+            'plone.autoform>=1.4',
+            'plone.behavior',
+            'plone.dexterity',
+            'plone.formwidget.datetime [z3cform]',
+            'plone.formwidget.recurrence [z3cform]',
+            'plone.indexer',
+            'plone.supermodel',
+        ],
+        'test': [
+            'mock',
+            'plone.app.collection',
+            'plone.app.contenttypes >= 1.1b1',
+            'plone.app.event [archetypes, dexterity]',
+            'plone.app.robotframework',
+            'plone.app.testing [robot]',
+            'plone.testing',
+            'robotsuite',
+            'transaction',
+            'unittest2',
+            'zExceptions',
+            'zope.event',
+        ]
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """
+)
