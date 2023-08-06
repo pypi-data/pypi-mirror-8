168e0c024373dc21acb6c8d8a7649212218c5bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:27 +0000

    Add package XlsxWriter/0.6.3/XlsxWriter-0.6.3.tar.gz

---
+++ packages/XlsxWriter/XlsxWriter-0.6.3.tar.gz/XlsxWriter-0.6.3/setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='XlsxWriter',
+    version='0.6.3',
+    author='John McNamara',
+    author_email='jmcnamara@cpan.org',
+    url='https://github.com/jmcnamara/XlsxWriter',
+    packages=['xlsxwriter'],
+    scripts=['examples/vba_extract.py'],
+    license='BSD',
+    description='A Python module for creating Excel XLSX files.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
