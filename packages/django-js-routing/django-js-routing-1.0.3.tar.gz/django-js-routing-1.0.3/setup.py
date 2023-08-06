20c1e546abe92f12afa436d909a944d8bf15d433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:08 +0000

    Add package django-js-routing/1.0.3/django-js-routing-1.0.3.tar.gz

---
+++ packages/django-js-routing/django-js-routing-1.0.3.tar.gz/django-js-routing-1.0.3/setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import setup
+
+
+try:
+    readme_content = open(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'README.md'
+        )
+    ).read()
+except IOError:
+    readme_content = ''
+
+setup(
+    name='django-js-routing',
+    version='1.0.3',
+    url='https://github.com/magdkudama/django-js-routing',
+    author='Magd Kudama',
+    author_email='magdkudama@gmail.com',
+    license='MIT',
+    long_description=readme_content,
+    keywords=['django', 'routing', 'js'],
+    packages=['django_js_routing'],
+    include_package_data=True,
+    description='Expose your Django routes to JavaScript',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+    ],
+)
