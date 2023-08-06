82c799c53ce01ed1d78451ea98bbcbd4ad5672d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:20 +0000

    Add package eta/0.9.8f/eta-0.9.8f.tar.gz

---
+++ packages/eta/eta-0.9.8f.tar.gz/eta-0.9.8f/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='eta',
+      version='0.9.8f',
+      description='ETA Progress bar for command-line utilities',
+      author='Marcus Breese',
+      author_email='marcus@breese.com',
+      url='http://github.com/mbreese/eta/',
+      packages=['eta'],
+     )
+
