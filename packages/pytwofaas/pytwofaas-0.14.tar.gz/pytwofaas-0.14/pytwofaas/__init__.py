d620f568a794c23ec341eb6ca1390df9c073df91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:32 +0000

    Add package pytwofaas/0.14/pytwofaas-0.14.tar.gz

---
+++ packages/pytwofaas/pytwofaas-0.14.tar.gz/pytwofaas-0.14/pytwofaas/__init__.py
@@ -0,0 +1,36 @@
+import requests
+
+class PyTwoFaas():
+
+  url = "http://127.0.0.1:5000"
+
+  def __init__(self, companyToken):
+    self.cToken = companyToken
+    print self.cToken
+
+  def sendAuth(self, clientId, factor, authType):
+    if authType is not "email":
+      payload = { 'userID' : clientId, 'compTK': self.cToken, 'userNum': factor }
+    else:
+      payload = { 'userID' : clientId, 'compTK': self.cToken, 'userEmail': factor }
+    r = requests.post(self.url + "/init/" + authType, data=payload)
+    return r.json()
+
+  def sendAuthSMS(self, clientId, phoneNum):
+    self.sendAuth(clientId, phoneNum, "sms")
+
+  def sendAuthCall(self, clientId, phoneNum):
+    self.sendAuth(clientId, phoneNum, "call")
+
+  def sendAuthEmail(self, clientId, email):
+    self.sendAuth(clientId, phoneNum, "email")
+
+  def sendUserInput(self, clientId, code):
+    payload = {'userID' :clientId,'compTK':self.cToken,'twoAuth':code}
+    r = requests.post(self.url + "/validate", data=payload)
+    return r.json()
+
+  def isAuthenticatedUser(self, clientId):
+    payload = { 'userID': clientId, 'compTK':self.cToken}
+    r = requests.post(self.url + "/valid", data=payload)
+    return r.json()
