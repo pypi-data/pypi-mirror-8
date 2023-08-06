4b4de501c3b6de67a3b0113ca721be05924f05a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:47 +0000

    Add package pynuodb/2.2/pynuodb-2.2.tar.gz

---
+++ packages/pynuodb/pynuodb-2.2.tar.gz/pynuodb-2.2/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='pynuodb',
+    version='2.2',
+    author='NuoDB',
+    author_email='info@nuodb.com',
+    description='NuoDB Python driver',
+    keywords='nuodb scalable cloud database',
+    packages=['pynuodb'],
+    package_dir={'pynuodb': 'pynuodb'},
+    url='https://github.com/nuodb/nuodb-python',
+    license='BSD licence, see LICENCE.txt',
+    long_description=open('README.md').read(),
+)
+
