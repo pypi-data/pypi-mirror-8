af5bb23794ea7f6dc2d8db0057160aa79071ab6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:07 +0000

    Add package waftools/0.4.0/waftools-0.4.0.zip

---
+++ packages/waftools/waftools-0.4.0.zip/waftools-0.4.0/waftools/gnucc.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Michel Mooij, michel.mooij7@gmail.com
+
+
+'''Selects GNU as default C/C++ compiler, adds common release/debug 
+settings
+'''
+
+
+def options(opt):
+	'''Add default (C/C++) command line options for GNU compilers
+	
+	:param opt: options context 
+	:type opt: waflib.Options.OptionsContext
+	'''
+	opt.add_option('--check_c_compiler', dest='check_c_compiler', default='gcc', action='store', help='Selects C compiler type.')
+	opt.add_option('--check_cxx_compiler', dest='check_cxx_compiler', default='gxx', action='store', help='Selects C++ compiler type.')
+	opt.add_option('--debug', dest='debug', default=False, action='store_true', help='build with debug information.')
+
+
+def configure(conf):
+	'''Configures general environment settings for GNU compilers; e.g. set
+	default C/C++ compiler flags and defines based on the value of the 
+	command line --debug option.
+	
+	:param conf: configuration context 
+	:type conf: waflib.Configure.ConfigurationContext
+	'''
+	flags = ['-Wall', '-pthread']
+
+	if conf.options.debug:
+		flags.extend(['-g', '-ggdb'])
+		defines = []
+	else:
+		flags.extend(['-O3'])
+		defines = ['NDEBUG']
+
+	for cc in ('CFLAGS', 'CXXFLAGS'):
+		for flag in flags:
+			conf.env.append_unique(cc, flag)
+	for define in defines:
+		conf.env.append_unique('DEFINES', define)
+
