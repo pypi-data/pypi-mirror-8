5606b6c34d7008348dfa1bc802776cc0cbffe9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:11 +0000

    Add package mosm_primes/1.0.3/mosm_primes-1.0.3.tar.gz

---
+++ packages/mosm_primes/mosm_primes-1.0.3.tar.gz/mosm_primes-1.0.3/primes.py
@@ -0,0 +1,45 @@
+"""
+Simple Check of prime numbers, with the empiric indian math method.
+"""
+
+
+import math
+
+def _test(number):
+	if type(number) == int:
+		if number >= 1:
+			return True
+	raise ValueError("The number is not integer or integer positive.")
+
+
+"""
+This function requires a integer number and 
+return True if is prime or False if not
+"""
+def isPrime(number):
+	if(_test(number)):
+		global firstDivisor
+		limit = range(2,int(math.sqrt(number))+1)
+		for eachDivisor in limit:
+			if number % eachDivisor == 0:
+				firstDivisor = eachDivisor
+				return False
+		return True
+
+"""
+This function requires a integer number and return
+a list of prime components of this number
+"""
+def primeComponents(number):
+	if(_test(number)):
+		pComponents = set([1])
+		while(True):
+			if(isPrime(number) == False):
+				pComponents.add(firstDivisor)
+				number = number // firstDivisor
+			else:
+				pComponents.add(number)
+				return sorted(list(pComponents))
+
+
+
---
+++ packages/mosm_primes/mosm_primes-1.0.3.tar.gz/mosm_primes-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'mosm_primes',
+	version		= '1.0.3',
+	py_modules	= ['primes'],
+	author		= 'Marcelo Odir',
+	author_email	= 'marceloodir@gmail.com',
+	url		= '',
+	description	= 'List of components prime of a integer number',
+)
