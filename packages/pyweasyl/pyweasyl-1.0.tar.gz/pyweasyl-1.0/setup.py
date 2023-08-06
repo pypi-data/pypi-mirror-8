977f87e111de0d990b1647b4ecc730d59aa1bee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:08 +0000

    Add package pyweasyl/1.0/pyweasyl-1.0.tar.gz

---
+++ packages/pyweasyl/pyweasyl-1.0.tar.gz/pyweasyl-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyweasyl',
+      description='Python bindings for the Weasyl API',
+      version='1.0',
+      author='Rechner Fox',
+      author_email='me@profoundfox.com',
+      url='https://github.com/rechner/pyweasyl',
+      py_modules=['weasyl'])
+
