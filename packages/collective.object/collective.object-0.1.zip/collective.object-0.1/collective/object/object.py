3c3c27448ce5befd1beb2a4ee612103827c8a3df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:14 +0000

    Add package collective.object/0.1/collective.object-0.1.zip

---
+++ packages/collective.object/collective.object-0.1.zip/collective.object-0.1/collective/object/object.py
@@ -0,0 +1,65 @@
+from five import grok
+
+from z3c.form import group, field
+from zope import schema
+from zope.interface import invariant, Invalid
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from plone.dexterity.content import Container
+
+from plone.directives import dexterity, form
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.dexterity.browser.view import DefaultView
+
+from collective.object import MessageFactory as _
+
+
+# Interface class; used to define content-type schema.
+
+class IObject(form.Schema, IImageScaleTraversable):
+    """
+    Information about a collectible object used by museums
+    """
+
+    # If you want a schema-defined interface, delete the model.load
+    # line below and delete the matching file in the models sub-directory.
+    # If you want a model-based interface, edit
+    # models/object.xml to define the content type.
+
+    form.model("models/object.xml")
+
+
+# Custom content-type class; objects created for this content type will
+# be instances of this class. Use this class to add content-type specific
+# methods and properties. Put methods that are mainly useful for rendering
+# in separate view classes.
+
+class Object(Container):
+    grok.implements(IObject)
+
+    # Add your class methods and properties here
+    pass
+
+
+# View class
+# The view will automatically use a similarly named template in
+# object_templates.
+# Template filenames should be all lower case.
+# The view will render when you request a content object with this
+# interface with "/@@sampleview" appended.
+# You may make this the default view for content objects
+# of this type by uncommenting the grok.name line below or by
+# changing the view class name and template filename to View / view.pt.
+
+class ObjectView(DefaultView):
+    """ sample view class """
+
+    pass
+
+    # grok.name('view')
+
+    # Add view methods here
