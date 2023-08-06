2dd27f65f17c709a0434743bb93cc455259a6275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:33 +0000

    Add package mcapi/0.3/mcapi-0.3.tar.gz

---
+++ packages/mcapi/mcapi-0.3.tar.gz/mcapi-0.3/mcapi/profile.py
@@ -0,0 +1,51 @@
+## Based on Java from https://github.com/Mojang/AccountsClient/
+
+import requests
+import json
+
+from .exceptions import RateLimited
+
+AGENT = "minecraft"
+PROFILE_URL = "https://api.mojang.com/profiles/minecraft"
+UUID_PROFILE_URL = 'https://sessionserver.mojang.com/session/minecraft/profile/{uuid}'
+
+class ProfileCriteria(dict):
+    def __init__(self, name, agent):
+        self['name'] = name
+        self['agent'] = agent
+
+def get_profile(uuid, timeout=10):
+    url = UUID_PROFILE_URL.format(uuid=uuid)
+    try:
+        r = requests.get(url, timeout=timeout)
+        profile = r.json()
+    except:
+        profile = None
+
+    return profile
+
+def get_uuid(*name, **kwargs):
+    timeout = 10
+    if "timeout" in kwargs:
+        timeout = kwargs["timeout"]
+    if len(name) == 0:
+        return None
+    p = []
+
+    page = 1
+    while True:
+        if len(name) == 0:
+            break
+        crit = name[:100]
+        name = name [100:]
+        data = json.dumps(crit)
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        r = requests.post(PROFILE_URL, data=data, headers=headers, timeout=timeout)
+        if r.status_code == 429:
+            raise RateLimited(r.text)
+        profiles = r.json()
+        p.extend(profiles)
+
+        page += 1
+                
+    return p
---
+++ packages/mcapi/mcapi-0.3.tar.gz/mcapi-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='mcapi',
+    version='0.3',
+    description='Simple mc api library',
+    install_requires=['requests'],
+    author='Deaygo',
+    author_email='deaygo@thezomg.com',
+    packages=['mcapi']
+)
