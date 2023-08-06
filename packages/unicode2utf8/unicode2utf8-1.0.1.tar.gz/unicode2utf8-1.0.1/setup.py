5662fa79abfcb2e42138ef72e39b060580908c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:01 +0000

    Add package unicode2utf8/1.0.1/unicode2utf8-1.0.1.tar.gz

---
+++ packages/unicode2utf8/unicode2utf8-1.0.1.tar.gz/unicode2utf8-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+ 
+setup(
+    name = 'unicode2utf8',
+    packages = ['unicode2utf8'],
+    version = '1.0.1',
+    description = 'Changes encoding from unicode to utf-8',
+    author='Miguel E. Beltre',
+    author_email='miguelbeltre@packlink.com',
+    url='https://github.com/miguelbeltre/unicode2utf8',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers'
+    ]
+)
