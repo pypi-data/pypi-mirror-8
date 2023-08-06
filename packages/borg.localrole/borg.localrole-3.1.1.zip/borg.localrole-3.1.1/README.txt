c263532955d5f8d1d38d7c0708fccf2b5406c3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:16 +0000

    Add package borg.localrole/3.1.1/borg.localrole-3.1.1.zip

---
+++ packages/borg.localrole/borg.localrole-3.1.1.zip/borg.localrole-3.1.1/README.txt
@@ -0,0 +1,6 @@
+Overview
+========
+
+A PAS plugin which can manage local roles via an adapter lookup on the current
+context.
+
---
+++ packages/borg.localrole/borg.localrole-3.1.1.zip/borg.localrole-3.1.1/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages
+
+name = 'borg.localrole'
+version = '3.1.1'
+
+readme = open('README.txt').read()
+history = open('CHANGES.txt').read()
+
+setup(name = name,
+      version = version,
+      description = 'A PAS plugin which can manage local roles via an '
+                    'adapter lookup on the current context',
+      long_description = readme + '\n' + history,
+      keywords = 'Plone PAS local roles',
+      author = 'Borg Collective',
+      author_email = 'borg@plone.org',
+      url = 'http://pypi.python.org/pypi/borg.localrole',
+      license = 'LGPL',
+      packages = find_packages(exclude=['ez_setup']),
+      namespace_packages = ['borg'],
+      include_package_data = True,
+      platforms = 'Any',
+      zip_safe = False,
+      extras_require=dict(
+        test=[
+            'Products.ATContentTypes',
+            'plone.app.testing',
+        ]
+      ),
+      install_requires=[
+        'setuptools',
+        'zope.annotation',
+        'zope.component',
+        'zope.deferredimport',
+        'zope.interface',
+        'Products.CMFCore',
+        'Products.GenericSetup',
+        'Products.PlonePAS',
+        'Products.PluggableAuthService',
+        'plone.memoize',
+        'Acquisition',
+        'Zope2',
+      ],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 5.0',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+)
