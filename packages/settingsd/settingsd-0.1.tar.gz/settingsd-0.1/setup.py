d416377efe967c9de64e61f2cbfee79c8fc07a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:09 +0000

    Add package settingsd/0.1/settingsd-0.1.tar.gz

---
+++ packages/settingsd/settingsd-0.1.tar.gz/settingsd-0.1/setup.py
@@ -0,0 +1,27 @@
+# encoding: utf8
+
+
+from setuptools import setup
+
+
+setup(
+    zip_safe=True,
+    name='settingsd',
+    version='0.1',
+    description='settings.d',
+    long_description='settings.d',
+    url='https://github.com/xtfxme/settingsd',
+    author='C Anthony Risinger',
+    author_email='c@anthonyrisinger.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        ],
+    )
