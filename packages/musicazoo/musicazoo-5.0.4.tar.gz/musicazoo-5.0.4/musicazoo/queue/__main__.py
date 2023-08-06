b0a93f73249cda21475c25f94d1fec95c1d9d0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:51 +0000

    Add package musicazoo/5.0.4/musicazoo-5.0.4.tar.gz

---
+++ packages/musicazoo/musicazoo-5.0.4.tar.gz/musicazoo-5.0.4/musicazoo/queue/__main__.py
@@ -0,0 +1,33 @@
+import musicazoo.queue
+import musicazoo.lib.service as service
+from musicazoo.queue.module import Module
+import os
+import signal
+
+cwd=os.path.dirname(__file__)
+
+class Youtube(Module):
+    TYPE_STRING='youtube'
+    process = ['python',os.path.join(cwd,'modules/youtube.py')]
+
+class Problem(Module):
+    TYPE_STRING='problem'
+    process = ['python',os.path.join(cwd,'modules/problem.py')]
+
+class Text(Module):
+    TYPE_STRING='text'
+    process = ['python',os.path.join(cwd,'modules/text.py')]
+
+modules = [Youtube, Problem, Text]
+
+q=musicazoo.queue.Queue(modules)
+
+def shutdown_handler(signum,frame):
+    print
+    print "Received signal, attempting graceful shutdown..."
+    service.ioloop.add_callback_from_signal(q.shutdown)
+
+signal.signal(signal.SIGTERM, shutdown_handler)
+signal.signal(signal.SIGINT, shutdown_handler)
+
+service.ioloop.start()
