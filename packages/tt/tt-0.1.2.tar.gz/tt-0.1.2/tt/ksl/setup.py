993d33d488d80b6d8d9666a75a904bd13770affd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:37 +0000

    Add package tt/0.1.2/tt-0.1.2.tar.gz

---
+++ packages/tt/tt-0.1.2.tar.gz/tt-0.1.2/tt/ksl/setup.py
@@ -0,0 +1,30 @@
+#This script will build the main subpackages  
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration, get_info
+    config = Configuration('ksl', parent_package, top_path)
+    tt_dir = '../tt-fort/'
+    exp_dir = '../tt-fort/expm'
+    src = ['ttals.f90','tt_ksl.f90']
+    exp_src = ['explib.f90', 'normest.f90','expokit.f','dlacn1.f','dlapst.f','dlarpc.f','zlacn1.f']
+    exp_src = [exp_dir + '/' + x for x in exp_src]
+    config.add_library('expokit',sources=exp_src)
+    src = [tt_dir + x for x in src]
+    src.append('tt_ksl.pyf')
+    config.add_extension('dyn_tt',sources=src,depends=['print_lib','expokit','mytt'],
+			libraries=['print_lib','expokit','mytt'],)
+    return config
+    
+#from distutils.core import setup
+#from numpy.distutils.core import setup, Extension
+#src = ['tt_f90.f90','tt_f90.pyf']
+#inc_dir = ['tt-fort']
+#lib = ['tt-fort/mytt.a']
+#ext = Extension('tt_f90', src, include_dirs=inc_dir)
+#setup(ext_modules = [ext])
+
+
+if __name__ == '__main__':
+    print('This is the wrong setup.py to run')
+
+
+#, include_dirs=None, define_macros=None, undef_macros=None, library_dirs=None, libraries=None, runtime_library_dirs=None, extra_objects=None, extra_compile_args=None, extra_link_args=None, export_symbols=None, swig_opts=None, depends=None, language=None, f2py_options=None, module_dirs
