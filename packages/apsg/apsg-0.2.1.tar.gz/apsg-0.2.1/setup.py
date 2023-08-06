f8d0b7671b07f5f27d1ab7bb119f840aba24c8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:31 +0000

    Add package apsg/0.2.1/apsg-0.2.1.tar.gz

---
+++ packages/apsg/apsg-0.2.1.tar.gz/apsg-0.2.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    'numpy >= 1.8',
+    'matplotlib >= 1.2'
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='apsg',
+    version='0.2.1',
+    description='APSG - structural geology module for Python',
+    long_description=readme + '\n\n' + history,
+    author='Ondrej Lexa',
+    author_email='lexa.ondrej@gmail.com',
+    url='http://ondrolexa.github.io/apsg',
+    packages=[
+        'apsg',
+    ],
+    package_dir={'apsg':
+                 'apsg'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='apsg',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
