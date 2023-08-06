ee04695f255469ed7b0586d88c0d4524fccb3298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:55 +0000

    Add package trytond_sentry/3.4.0.1/trytond_sentry-3.4.0.1.tar.gz

---
+++ packages/trytond_sentry/trytond_sentry-3.4.0.1.tar.gz/trytond_sentry-3.4.0.1/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+    setup
+
+    :copyright: (c) 2012-2014 by Openlabs Technologies & Consulting (P) Limited
+    :license: GPLv3, see LICENSE for more details.
+"""
+from setuptools import setup
+
+setup(
+    name='trytond_sentry',
+    version='3.4.0.1',
+    description='Sentry Client for Tryton',
+    long_description=open('README.rst').read(),
+    author="Openlabs Technologies & Consulting (P) Limited",
+    author_email="info@openlabs.co.in",
+    url="https://github.com/openlabs/trytond-sentry",
+    package_dir={'trytond_sentry': '.'},
+    packages=[
+        'trytond_sentry',
+    ],
+    scripts=[
+        'bin/trytond_sentry',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Office/Business',
+    ],
+    license='GPL-3',
+    install_requires=[
+        "trytond>=3.4,<3.5",
+        "raven",
+    ],
+    zip_safe=False,
+)
