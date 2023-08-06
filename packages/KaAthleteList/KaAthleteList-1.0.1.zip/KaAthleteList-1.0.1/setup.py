4ed7c26f01700d0f2c045288f11b8be73d93df5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:33 +0000

    Add package KaAthleteList/1.0.1/KaAthleteList-1.0.1.zip

---
+++ packages/KaAthleteList/KaAthleteList-1.0.1.zip/KaAthleteList-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'KaAthleteList',
+	version = '1.0.1',
+	py_modules = ['athletelist'],
+	author = 'maolv_kaka',
+	author_email = 'haitaogao08@126.com',
+	url = 'http://www.douban.com/people/maolv_kaka/',
+	description = 'A simple class'
+	)
