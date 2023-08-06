281274bb109cd6416f0c3638e0132875bd832763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:20 +0000

    Add package flatxml/0.12/flatxml-0.12.tar.gz

---
+++ packages/flatxml/flatxml-0.12.tar.gz/flatxml-0.12/setup.py
@@ -0,0 +1,28 @@
+import os
+import re
+
+v = open(os.path.join(os.path.dirname(__file__), 'flatxml', '__init__.py'))
+VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(v.read()).group(1)
+v.close()
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Transform XML blob, file, or OrderedDict to a flat CSV or dictionary.',
+    'author': 'Carlos Eduardo Rivera',
+    'url': 'https://github.com/AbleEng/flatxml',
+    'download_url': 'https://github.com/AbleEng/flatxml',
+    'author_email': 'carlos@hiable.com',
+    'version': VERSION,
+    'install_requires': ['xmltodict'],
+    'tests_require': ['nose'],
+    'packages': ['flatxml', 'flatxml.test'],
+    'scripts': [],
+    'name': 'flatxml',
+    'keywords': 'flat xml csv'
+}
+
+setup(**config)
