88ed945ddcf581c8bfc19386bbb290874530e06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:03 +0000

    Add package TechParser/1.7.1/TechParser-1.7.1.tar.gz

---
+++ packages/TechParser/TechParser-1.7.1.tar.gz/TechParser-1.7.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+v = "1.7.1"
+
+setup(name="TechParser",
+	version=v,
+	author="Ivan Konovalov",
+	author_email="rvan.mega@gmail.com",
+	description="Lets You parse articles from various related to IT sites.",
+	url="https://github.com/SPython/tech-parser",
+	download_url="https://github.com/SPython/tech-parser/tarball/"+v,
+	keywords=["parser", "article", "web"],
+	requires=["grab", "mako", "bottle", "Daemo"],
+	classifiers=[],
+	packages=["TechParser"],
+	package_data={"TechParser": ["templates/*",
+		"static/jquery.touchSwipe.min.js", "static/fonts/*",
+		"static/style.css", "static/jquery-2.1.1.min.js", "static/icons/*"]})
