f666cfcfa2d151ce3aa707ae301b555bfb6060c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package caimaoy_util/0.0.1.6.alpha/caimaoy_util-0.0.1.6.alpha.zip

---
+++ packages/caimaoy_util/caimaoy_util-0.0.1.6.alpha.zip/caimaoy_util-0.0.1.6.alpha/caimaoy_util.py
@@ -0,0 +1,142 @@
+# -*- coding: UTF-8 -*-
+
+'''
+caimaoy's util
+
+
+Last modified time: 2014-10-14 19:36:09
+Edit time: 2014-10-14 19:36:33
+File name: process_exist.py
+Edit by caimaoy
+'''
+
+
+def is_process_exist(p):
+    '''判断windows系统下是否存在进程p
+    '''
+
+    try:
+        import win32com.client
+    except Exception as e:
+        print e
+    ret = 0
+    try:
+        WMI = win32com.client.GetObject('winmgmts:')
+        processCodeCov = WMI.ExecQuery('select * from Win32_Process where Name="%s"' % p)
+    except Exception as e:
+        print p + "error : ", e;
+    if len(processCodeCov) > 0:
+        ret = True
+    else:
+        ret = False
+    return ret
+
+
+# tail.py
+# Usage: python tail.py FILENAME LINES
+# similar to linux command: tail -n LINES FILENAME
+def last_lines(filename, lines = 1):
+    #print the last several line(s) of a text file
+    """
+    Argument filename is the name of the file to print.
+    Argument lines is the number of lines to print from last.
+    """
+    lines = int(lines)
+    block_size = 1024
+    block = ''
+    nl_count = 0
+    start = 0
+    fsock = file(filename, 'rU')
+    try:
+        #seek to end
+        fsock.seek(0, 2)
+        #get seek position
+        curpos = fsock.tell()
+        while(curpos > 0): #while not BOF
+            #seek ahead block_size+the length of last read block
+            curpos -= (block_size + len(block));
+            if curpos < 0: curpos = 0
+            fsock.seek(curpos)
+            #read to end
+            block = fsock.read()
+            nl_count = block.count('\n')
+            #if read enough(more)
+            if nl_count >= lines: break
+        #get the exact start position
+        for n in range(nl_count-lines+1):
+            start = block.find('\n', start)+1
+    finally:
+        fsock.close()
+    #print it out
+    # print block[start:]
+    return block[start:]
+
+    '''
+    import sys
+    last_lines(sys.argv[1], sys.argv[2]) #print the last several lines of THIS file
+    '''
+
+def call_md5(s):
+    """计算字符串的md5
+
+    :s: 输入字符串
+    :returns: 字符串s的md5值
+
+    """
+    from hashlib import md5
+    m = md5()
+    m.update(s)
+    return m.hexdigest()
+
+
+def file2md5(filename):
+    """计算文件md5值
+
+    :filename: path of a file
+    :returns: the md5 of a file
+
+    """
+    from hashlib import md5
+    m = md5()
+    ret = True
+    try:
+        with open(filename, 'rb') as f:
+            m.update(f.read())
+    except Exception as e:
+        print e
+    return m.hexdigest()
+
+
+def obj2dict(obj):
+    """
+    summary:
+        将object转换成dict类型
+    """
+    memberlist = [m for m in dir(obj)]
+    _dict = {}
+    for m in memberlist:
+        if m[0] != "_" and not callable(m):
+            _dict[m] = getattr(obj,m)
+    return _dict
+
+
+def dict2class(dic, cls):
+    """
+    summary:
+        将dict转换成obj类型
+    """
+    c = cls()
+    for k in dic:
+        c.__setattr__(k, dic[k])
+    return c
+
+if __name__ == '__main__':
+    '''
+    f = r'E:\test-pc-av\baidu_security\pc_av\Src\daily_build\result\Result\SystemScanPerf.txt'
+    for i in last_lines(f, 5).split('\n'):
+        print i
+    print call_md5('0')
+    print file2md5(r'E:\test-pc-av\baidu_security\pc_av\Src\daily_build\result\Result\SystemScanPerf.txt')
+    print file2md5(r'E:\lest-pc-av\baidu_security\pc_av\Src\daily_build\result\Result\SystemScanPerf.txt')
+    '''
+    pass
---
+++ packages/caimaoy_util/caimaoy_util-0.0.1.6.alpha.zip/caimaoy_util-0.0.1.6.alpha/setup.py
@@ -0,0 +1,105 @@
+# -*- coding: UTF-8 -*-
+
+'''
+Last modified time: 2014-10-14 20:22:13
+Edit time: 2014-10-14 20:30:10
+File name: setup.py
+Edit by caimaoy
+'''
+
+import codecs
+import os
+import sys
+ 
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+"""
+打包的用的setup必须引入，
+"""
+
+def read(fname):
+    """
+    定义一个read方法，用来读取目录下的长描述
+    我们一般是将README文件中的内容读取出来作为长描述，这个会在PyPI中你这个包的页面上展现出来，
+    你也可以不用这个方法，自己手动写内容即可，
+    PyPI上支持.rst格式的文件。暂不支持.md格式的文件，<BR>.rst文件PyPI会自动把它转为HTML形式显示在你包的信息页面上。
+    """
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+
+NAME = "caimaoy_util"
+"""
+名字，一般放你包的名字即可
+"""
+
+PACKAGES = []
+"""
+包含的包，可以多个，这是一个列表
+"""
+
+DESCRIPTION = "caimaoy's util"
+"""
+关于这个包的描述
+"""
+
+LONG_DESCRIPTION = read("README.rst")
+"""
+参见read方法说明
+"""
+
+KEYWORDS = "caimaoy python util"
+"""
+关于当前包的一些关键字，方便PyPI进行分类。
+"""
+
+AUTHOR = "caimaoy"
+
+AUTHOR_EMAIL = "caimaoy@gmail.com"
+
+URL = "https://github.com/caimaoy"
+"""
+你这个包的项目地址，如果有，给一个吧，没有你直接填写在PyPI你这个包的地址也是可以的
+"""
+
+VERSION = "0.0.1.6 alpha"
+"""
+当前包的版本，这个按你自己需要的版本控制方式来
+"""
+
+LICENSE = "MIT"
+"""
+授权方式，我喜欢的是MIT的方式，你可以换成其他方式
+"""
+
+SCRIPT = []
+
+PY_MODULES = ['caimaoy_util']
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    scripts = SCRIPT,
+    py_modules = PY_MODULES,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft',
+    ],
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    packages = PACKAGES,
+    include_package_data=True,
+    zip_safe=True,
+)
+
+## 把上面的变量填入了一个setup()中即可。
