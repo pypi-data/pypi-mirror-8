84240d7db053cd25646a8904c8ad257e564dccce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:57 +0000

    Add package pyramid-celery/1.4/pyramid_celery-1.4.tar.gz

---
+++ packages/pyramid-celery/pyramid_celery-1.4.tar.gz/pyramid_celery-1.4/pyramid_celery/__init__.py
@@ -0,0 +1,71 @@
+# used by celerybeat
+from datetime import timedelta
+from celery.schedules import crontab
+
+from celery.app import defaults
+try:
+    from celery import registry
+except ImportError:
+    # Celery >= 3.1
+    from celery import current_app as registry
+
+from celery import current_app as celery
+
+def str_to_bool(term, table={"false": False, "no": False, "0": False,
+                             "true":  True, "yes": True,  "1": True}):
+    try:
+        return table[term.lower()]
+    except KeyError:
+        raise TypeError("Can't coerce %r to type bool" % (term, ))
+
+def clean_quoted_config(config, key):
+    # ini doesn't allow quoting, but lets support it to fit with celery
+    config[key] = config[key].replace('"', '')
+
+TYPES_TO_OBJ = {
+    'any': (object, None),
+    'bool': (bool, str_to_bool),
+    'dict': (dict, eval),
+    'float': (float, float),
+    'int': (int, int),
+    'list': (list, eval),
+    'tuple': (tuple, eval),
+    'string': (str, str),
+}
+
+
+OPTIONS = dict(
+    (key, TYPES_TO_OBJ[opt.type])
+    for key, opt in defaults.flatten(defaults.NAMESPACES)
+)
+
+
+def convert_celery_options(config):
+    """
+    Converts celery options to apropriate types
+    """
+
+    for key, value in config.iteritems():
+        opt_type = OPTIONS.get(key)
+        if opt_type:
+            if opt_type[0] == str:
+                clean_quoted_config(config, key)
+            elif opt_type[0] is object:
+                try:
+                    config[key] = eval(value)
+                except:
+                    pass  # any can be anything; even a string
+            elif not isinstance(value, opt_type[0]):
+                config[key] = opt_type[1](value)
+
+
+def includeme(config):
+
+    convert_celery_options(config.registry.settings)
+    celery.add_defaults(config.registry.settings)
+    # delete cached property in order to get them reloaded from the new conf
+    del(celery.backend)
+    for name, task in registry.tasks.items():
+        # ensure that every already registed tasks doens use an unconfigured
+        # backend.
+        task.backend = celery.backend
---
+++ packages/pyramid-celery/pyramid_celery-1.4.tar.gz/pyramid_celery-1.4/setup.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = ['pyramid', 'celery']
+if sys.version_info < (2, 7):
+    requires.append('argparse')
+
+
+setup(name='pyramid_celery',
+      version='1.4',
+      description='Celery integration with pyramid',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      author='John Anderson',
+      author_email='sontek@gmail.com',
+      url='https://github.com/sontek/pyramid_celery',
+      keywords='paste pyramid celery message queue amqp job task distributed',
+      license='BSD',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires + ['pytest', 'mock'],
+      test_suite="pyramid_celery",
+      entry_points="""\
+        [console_scripts]
+        pcelery = pyramid_celery.commands.celery:main
+        # keep for compatibility
+        pceleryd = pyramid_celery.commands.celeryd:main
+        pceleryctl = pyramid_celery.commands.celeryctl:main
+        pcelerybeat = pyramid_celery.commands.celerybeat:main
+        pceleryev = pyramid_celery.commands.celeryev:main
+        """
+      )
