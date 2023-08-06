dbec1080e5fc066a698afe086a985f445cc6b0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:18 +0000

    Add package zdb/0.1.3/zdb-0.1.3.tar.gz

---
+++ packages/zdb/zdb-0.1.3.tar.gz/zdb-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+required = []
+
+setup(
+    name="zdb",
+    version="0.1.3",
+    author="Greg Lamp",
+    author_email="greg@yhathq.com",
+    url="https://github.com/yhat/zdb/",
+    license="BSD",
+    packages=find_packages(),
+    package_dir={"zdb": "zdb"},
+    package_data={"zdb": []},
+    description="a db package that doesn't suck",
+    long_description=open("README.rst").read(),
+    install_requires=required,
+)
+
