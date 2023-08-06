3d35fcf77a19c2d01ebaf0d7b4672ec17a65d5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:08 +0000

    Add package pyexcel/0.1.1/pyexcel-0.1.1.tar.gz

---
+++ packages/pyexcel/pyexcel-0.1.1.tar.gz/pyexcel-0.1.1/pyexcel/__init__.py
@@ -0,0 +1,57 @@
+"""
+    pyexcel
+    ~~~~~~~~~~~~~~~~~~~
+
+    **pyexcel** is a wrapper library to read, manipulate and
+    write data in different excel formats: csv, ods, xls, xlsx
+    and xlsm. It does not support styling, charts.
+
+    :copyright: (c) 2014 by C. W.
+    :license: GPL v3
+"""
+from .book import (
+    BookReader,
+    Book,
+    load_book,
+    load_book_from_memory)
+from .writers import Writer, BookWriter
+from .sheets import (
+    Sheet,
+    load,
+    transpose,
+    Reader,
+    SeriesReader,
+    ColumnSeriesReader,
+    load_from_dict,
+    load_from_records,
+    load_from_memory,
+    load_from_sql)
+from .utils import to_dict, to_array, to_records, dict_to_array, from_records
+from .formatters import (
+    ColumnFormatter,
+    RowFormatter,
+    SheetFormatter,
+    NamedColumnFormatter,
+    NamedRowFormatter)
+from .filters import (
+    ColumnIndexFilter,
+    ColumnFilter,
+    RowFilter,
+    EvenColumnFilter,
+    OddColumnFilter,
+    EvenRowFilter,
+    OddRowFilter,
+    RowIndexFilter,
+    SingleColumnFilter,
+    RowValueFilter,
+    NamedRowValueFilter,
+    ColumnValueFilter,
+    NamedColumnValueFilter,
+    SingleRowFilter)
+from .cookbook import (
+    merge_csv_to_a_book,
+    merge_all_to_a_book,
+    split_a_book,
+    extract_a_sheet_from_a_book)
+
+__VERSION__ = '0.1.1'
