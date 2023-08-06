da4385f25ab1a661e6b7bcc4b32d79b1fb0e2826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:25 +0000

    Add package django-debug-toolbar-multilang/1.0/django-debug-toolbar-multilang-1.0.tar.gz

---
+++ packages/django-debug-toolbar-multilang/django-debug-toolbar-multilang-1.0.tar.gz/django-debug-toolbar-multilang-1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import sys
+
+
+def get_long_description():
+    with open('README.rst', "rt") as file:
+        return file.read()
+
+requirements = []
+if sys.version_info < (3, 3):
+    requirements.append("mock")
+
+requirements.append("Django>=1.7.1")
+requirements.append("django-debug-toolbar")
+
+setup(
+    name='django-debug-toolbar-multilang',
+    version='1.0',
+    description='Language panel for django-debug-toolbar',
+    long_description=get_long_description(),
+    url='http://github.com/matt3o12/django-debug-toolbar-multilang',
+    author='Matteo Kloiber',
+    author_email='info@matt3o12.de',
+    license='MIT',
+    packages=["debug_toolbar_multilang"],
+    install_requires=requirements,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Natural Language :: German",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+
+    ]
+)
