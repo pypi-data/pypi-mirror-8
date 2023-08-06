da557df8c0e5dbb85cad8e6a51e417bc540a6109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:46 +0000

    Add package ratebeer/1.2.2a/ratebeer-1.2.2a.tar.gz

---
+++ packages/ratebeer/ratebeer-1.2.2a.tar.gz/ratebeer-1.2.2a/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(name="ratebeer",
+      version="1.2.2a",
+      description="RateBeer.com data scraper.",
+      keywords="ratebeer rate beer ratings",
+      author="Andrew Lilja",
+      author_email="andrewlilja@gmail.com",
+      url="https://github.com/alilja/ratebeer",
+      license="Unlicense (a.k.a. Public Domain)",
+      packages=["ratebeer"],
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2',
+            'Topic :: Internet',
+            'Topic :: Internet :: WWW/HTTP',
+      ],
+      install_requires=[
+            "beautifulsoup4",
+            "lxml",
+            "requests",
+      ],
+      test_suite="test.py",)
