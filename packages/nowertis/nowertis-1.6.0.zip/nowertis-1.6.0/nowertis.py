e674db193c9aea24a8c22516546b01f83982f66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:02 +0000

    Add package nowertis/1.6.0/nowertis-1.6.0.zip

---
+++ packages/nowertis/nowertis-1.6.0.zip/nowertis-1.6.0/nowertis.py
@@ -0,0 +1,15 @@
+"""this is my module with my functions"""
+import sys
+
+def print_lol(the_list,indent=False, level=0, fh=sys.stdout):
+    """First example function"""
+    for each_item in the_list:
+      if isinstance(each_item, list):
+        print_lol(each_item,indent, level+1, fh)
+      else:
+            if indent:
+                for tab_stop in range(level):
+                    print('\t', end='', file=fh)
+            print(each_item, file=fh)            
+def suma(x,y):
+  return x+y
