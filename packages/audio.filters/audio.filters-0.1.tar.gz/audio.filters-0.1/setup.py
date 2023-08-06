17bfa9efc7101213435d1b8a9728c165c0ef6470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:58 +0000

    Add package audio.filters/0.1/audio.filters-0.1.tar.gz

---
+++ packages/audio.filters/audio.filters-0.1.tar.gz/audio.filters-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Third-Party Libraries
+import setuptools
+#from Cython.Build import cythonize
+
+# Source:
+# https://github.com/cython/cython/wiki/PackageHierarchy
+
+if __name__ == "__main__":
+    setuptools.setup(
+      name = "audio.filters",
+      version = "0.1",
+      packages = setuptools.find_packages(),
+      #ext_modules = cythonize("audio/filters.pyx")
+    )
+
