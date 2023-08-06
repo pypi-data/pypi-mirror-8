8904be54a1dee5b84793364479a9f0a59d33f0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:21 +0000

    Add package pyconfigurator/0.4/pyconfigurator-0.4.zip

---
+++ packages/pyconfigurator/pyconfigurator-0.4.zip/pyconfigurator-0.4/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyconfigurator',
+    packages = ['configurator'], # this must be the same as the name above
+    version = '0.4',
+    description = 'A library for easy configuration',
+    author = 'Charles Smith, Jeff Magnusson',
+    author_email = 'charles.s.smith@gmail.com, magnussj@gmail.com',
+    url = 'https://github.com/charsmith/configurator', # use the URL to the github repo
+    keywords = ['configuration', 'ini'], # arbitrary keywords
+    license = 'Apache Software License',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
