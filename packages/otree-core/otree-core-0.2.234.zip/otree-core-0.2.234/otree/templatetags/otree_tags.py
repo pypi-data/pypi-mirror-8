8021c194b138bf40ffa2725835512e2fc48d8cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:45 +0000

    Add package otree-core/0.2.234/otree-core-0.2.234.zip

---
+++ packages/otree-core/otree-core-0.2.234.zip/otree-core-0.2.234/otree/templatetags/otree_tags.py
@@ -0,0 +1,30 @@
+"""
+Template tags to for the otree template users.
+"""
+
+from django import template
+from django.template.loader import render_to_string
+from otree.common import Currency
+
+register = template.Library()
+
+
+class NextButtonNode(template.Node):
+    def render(self, context):
+        context.update({})
+        try:
+            return render_to_string(
+                'otree/NextButton.html',
+                context)
+        finally:
+            context.pop()
+
+    @classmethod
+    def parse(cls, parser, tokens):
+        return cls()
+
+
+register.tag('next_button', NextButtonNode.parse)
+
+
+register.filter('c',Currency)
