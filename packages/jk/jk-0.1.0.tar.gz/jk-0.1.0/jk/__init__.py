95e88923260c9eca5ebbb8c65264d403cebe6f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:17 +0000

    Add package jk/0.1.0/jk-0.1.0.tar.gz

---
+++ packages/jk/jk-0.1.0.tar.gz/jk-0.1.0/jk/__init__.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""JK library
+
+Library containing useful functions for manipulating iterables.
+
+Inspiration was drawn from Python itertools' recipes and Ruby's Enumerable API.
+"""
+
+__author__ = 'Elias Dorneles'
+__email__ = 'eliasdorneles@gmail.com'
+__version__ = '0.1.0'
+
+
+from itertools import islice, tee
+
+try:
+    from itertools import izip as zip
+except ImportError:
+    pass
+
+try:
+    string_type = basestring
+except NameError:
+    string_type = str
+
+import re
+
+
+def each_cons(sequence, size):
+    """Iterates lazily through a sequence looking at a sliding window
+    with given size, for each time.
+
+    each_cons([1, 2, 3, 4], 2) --> [(0, 1), (1, 2), (2, 3), (3, 4)]
+    """
+    return zip(*(islice(it, start, None)
+                 for start, it in enumerate(tee(sequence, size))))
+
+
+def slice_before(predicate, iterable):
+    """Returns groups of elements from iterable,
+    slicing just before predicate(elem) is True
+    """
+    if isinstance(predicate, string_type):
+        predicate = re.compile(predicate)
+
+    if hasattr(predicate, 'match'):
+        predicate = predicate.match
+
+    record = []
+
+    for i, item in enumerate(iterable):
+        if i == 0:
+            record = [item]
+            continue
+
+        if predicate(item):
+            yield tuple(record)
+            record = [item]
+        else:
+            record.append(item)
+
+    if record:
+        yield tuple(record)
+
+
+def slice_after(predicate, iterable):
+    """Returns groups of elements from iterable,
+    slicing just after predicate(elem) is True
+    """
+    record = []
+    for i, item in enumerate(iterable):
+        record.append(item)
+
+        if predicate(item):
+            yield tuple(record)
+            record = []
+
+    if record:
+        yield tuple(record)
+
+
+def nth(iterable, n, default=None):
+    """Returns the nth item or a default value
+
+    Note that nth is an ordinal number, so this is not zero-indexed:
+    use n=1 for first element, n=4 for fourth, and so on."""
+
+    return next(islice(iterable, n - 1, None), default)
+
+
+def first(iterable, default=None):
+    "Returns the first item or a default value"
+    return nth(iterable, 1, default)
+
+
+def second(iterable, default=None):
+    "Returns the second item or a default value"
+    return nth(iterable, 2, default)
+
---
+++ packages/jk/jk-0.1.0.tar.gz/jk-0.1.0/tests/test_jk.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_jk
+----------------------------------
+
+Tests for `jk` module.
+"""
+
+import unittest
+
+import jk
+import re
+
+
+def simple_gen():
+    return (x for x in range(5))
+
+
+class TestJk(unittest.TestCase):
+
+    def test_each_cons_work_with_generators(self):
+        self.assertEquals([(0, 1), (1, 2), (2, 3), (3, 4)],
+                          list(jk.each_cons(simple_gen(), 2)))
+
+    def test_each_cons_yelds_nothing_when_iterable_ends_earlier(self):
+        self.assertEquals([], list(jk.each_cons(range(4), 5)))
+
+    def test_first_from_any_iterable(self):
+        self.assertEquals('first', jk.first(['first', 'second']))
+        self.assertEquals(1, jk.first(range(1, 5)))
+        self.assertEquals(0, jk.first(simple_gen()))
+        self.assertEquals('default', jk.first([], 'default'))
+
+    def test_second_from_any_iterable(self):
+        self.assertEquals('second', jk.second(['first', 'second']))
+        self.assertEquals(2, jk.second(range(1, 5)))
+        self.assertEquals(1, jk.second(simple_gen()))
+        self.assertEquals('default', jk.second([], 'default'))
+
+    def test_slice_before_simple(self):
+        # setup:
+        numbers = (x for x in [2, 3, 111, 4, 5, 6, 7, 111])
+
+        # when:
+        result = jk.slice_before(lambda x: x == 111, numbers)
+
+        # then:
+        self.assertEquals(
+            [(2, 3), (111, 4, 5, 6, 7), (111,)],
+            list(result)
+        )
+
+    def test_slice_before_when_first_element_matches(self):
+        # setup:
+        numbers = [111, 2, 3, 111, 4, 5, 6, 7, 111]
+
+        # when:
+        result = jk.slice_before(lambda x: x == 111, numbers)
+
+        # then:
+        self.assertEquals(
+            [(111, 2, 3), (111, 4, 5, 6, 7), (111,)],
+            list(result)
+        )
+
+    def test_slice_before_with_string_pattern_predicate(self):
+        # setup:
+        text_lines = """one
+        two
+        three
+        START: four
+        five
+        START: six
+        START: seven""".split('\n')
+
+        # when:
+        result = jk.slice_before('^START:', (l.strip() for l in text_lines))
+
+        # then:
+        self.assertEquals(
+            [
+                ('one', 'two', 'three'),
+                ('START: four', 'five'),
+                ('START: six',),
+                ('START: seven',),
+            ],
+            list(result)
+        )
+
+    def test_slice_after_simple(self):
+        # setup:
+        numbers = (x for x in [111, 2, 3, 111, 4, 5, 6, 7, 111])
+
+        # when:
+        result = jk.slice_after(lambda x: x == 111, numbers)
+
+        # then:
+        self.assertEquals(
+            [(111,), (2, 3, 111), (4, 5, 6, 7, 111)],
+            list(result)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
