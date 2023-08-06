a463ea236424eb530e83b9ba5cb8d579d6cf22b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:59 +0000

    Add package Quandl/2.3/Quandl-2.3.tar.gz

---
+++ packages/Quandl/Quandl-2.3.tar.gz/Quandl-2.3/Quandl/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+__authors__ = (
+    'Mark Hartney',
+    'Chris Stevens',
+)
+__maintainer__ = 'Chris Stevens'
+__email__ = 'connect@quandl.com'
+__url__ = 'http://www.quandl.com/'
+__license__ = 'MIT'
+__version__ = '2.3'
+
+#############################################
+#
+# Don't import anything in __init__.py!
+# Otherwise setup.py can't import the private constants above (__authors__, etc)
+# because it won't have installed dependencies yet!
+#
+# from .Quandl import (
+#     get,
+#     push,
+#     search,
+#     WrongFormat,
+#     MultisetLimit,
+#     ParsingError,
+#     CallLimitExceeded,
+#     DatasetNotFound,
+#     ErrorDownloading,
+#     MissingToken,
+#     DateNotRecognized,
+#     CodeFormatError
+#     )
+#
+###############################################
