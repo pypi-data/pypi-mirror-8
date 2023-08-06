5584d159845b463f6e1174a5dbbb9604f9447e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:19 +0000

    Add package kppy/1.4.2/kppy-1.4.2.tar.gz

---
+++ packages/kppy/kppy-1.4.2.tar.gz/kppy-1.4.2/kppy/exceptions.py
@@ -0,0 +1,21 @@
+"""Defines KPError exception class"""
+
+class KPError(Exception):
+    """KPError is a exception class to handle exception raised by KPDB.
+    
+    Usage:
+    
+    Handle KPError like every else expection. You can print the error message
+    via an expection instance.
+    
+    Example:
+    
+    try:
+        ...
+    except KPError as e:
+        print(e)
+        
+    """
+
+    pass
+
---
+++ packages/kppy/kppy-1.4.2.tar.gz/kppy-1.4.2/kppy/test.py
@@ -0,0 +1,4 @@
+from database import KPDBv1
+
+db = KPDBv1("./test_parsing.kdb", "test", None)
+db.load()
