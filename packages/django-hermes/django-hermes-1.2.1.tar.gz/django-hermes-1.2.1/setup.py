1bf8f542a4ac07b40cb94d8800a7aa3a7ca26466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:25 +0000

    Add package django-hermes/1.2.1/django-hermes-1.2.1.tar.gz

---
+++ packages/django-hermes/django-hermes-1.2.1.tar.gz/django-hermes-1.2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-hermes',
+    version='1.2.1',
+    author='Chris Pickett',
+    author_email='chris.pickett@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/bunchesofdonald/django-hermes',
+    license='MIT',
+    long_description=open('README.rst').read(),
+    description='A light-weight blogging app for Django.',
+)
