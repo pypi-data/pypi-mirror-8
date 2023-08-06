f87d1ed07c79d71fb12422e4ef6ef52dfcf2781d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:18 +0000

    Add package django-simple-polymorph/0.2/django-simple-polymorph-0.2.tar.gz

---
+++ packages/django-simple-polymorph/django-simple-polymorph-0.2.tar.gz/django-simple-polymorph-0.2/simple_polymorph/__init__.py
@@ -0,0 +1,18 @@
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class PolyMorph(models.Model):
+
+    class Meta(object):
+        abstract = True
+
+    real_type = models.ForeignKey(ContentType)
+
+    def save(self, **kwargs):
+        if not self.real_type:
+            self.real_type = ContentType.objects.get_for_model(self)
+        super(PolyMorph, self).save(**kwargs)
+
+    def polymorph(self):
+        return self.real_type.get_object_for_this_type(pk=self.pk)
