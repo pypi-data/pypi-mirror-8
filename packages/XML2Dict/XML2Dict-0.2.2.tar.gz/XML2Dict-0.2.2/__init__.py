a335a3b75742525ff3295ef590a48044ebe9a231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:24 +0000

    Add package XML2Dict/0.2.2/XML2Dict-0.2.2.tar.gz

---
+++ packages/XML2Dict/XML2Dict-0.2.2.tar.gz/XML2Dict-0.2.2/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+XML2Dict: Convert xml and python dict
+
+@since: Created on 2009-5-18
+@author: Mc.Spring
+@contact: Heresy.Mc@gmail.com
+@copyright: Copyright (C) 2009 MC.Spring Team. All rights reserved.
+@license: http://www.apache.org/licenses/LICENSE-2.0 Apache License
+'''
+
+__version__ = '0.2.2'
+__all__ = [
+    'parsexml', 'parsedict',
+    'XML2Dict', 'Dict2XML',
+]
+
+__author__ = 'Mc.Spring <Heresy.Mc@gmail.com>'
+
+
+from encoder import XML2Dict
+from decoder import Dict2XML
+
+
+def parsexml(s, cls=None, coding=None):
+    if cls is None:
+        cls = XML2Dict
+
+    return cls(coding).parse(s) if coding else cls().parse(s)
+
+
+def parsedict(d, cls=None):
+    if cls is None:
+        cls = Dict2XML
+
+    return cls().parse(d)
