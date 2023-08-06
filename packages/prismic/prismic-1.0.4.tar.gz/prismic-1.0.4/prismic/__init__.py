01e4ed837e2535fdea5fae8088b7862174101563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:33 +0000

    Add package prismic/1.0.4/prismic-1.0.4.tar.gz

---
+++ packages/prismic/prismic-1.0.4.tar.gz/prismic-1.0.4/prismic/__init__.py
@@ -0,0 +1,22 @@
+"""
+Prismic.io python library.
+"""
+
+__title__ = 'prismic'
+__version__ = '1.0.2'
+__author__ = 'Prismic Team'
+__license__ = 'Apache 2'
+
+from .api import get, Api, SearchForm, Document
+from .fragments import Fragment
+
+# Set a default logger to prevent "No handler found" warnings
+import logging
+try:  # Python >=2.7
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
