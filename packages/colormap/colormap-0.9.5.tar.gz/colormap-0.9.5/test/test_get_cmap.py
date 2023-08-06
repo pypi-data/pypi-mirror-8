7970f0f3fb669f2602f27be193de5a63c91eecbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:40 +0000

    Add package colormap/0.9.5/colormap-0.9.5.tar.gz

---
+++ packages/colormap/colormap-0.9.5.tar.gz/colormap-0.9.5/test/test_get_cmap.py
@@ -0,0 +1,16 @@
+from colormap import cmap_builder as get_cmap
+
+
+def test_get_cmap():
+    get_cmap("heat")
+    get_cmap("heat_r")
+    get_cmap("spectral")
+    get_cmap("red_black_blue")
+    try:
+        get_cmap('dummy')
+        assert False
+    except:
+        assert True
+
+    get_cmap('red', 'black', 'yellow')
+    get_cmap('red', 'black')
