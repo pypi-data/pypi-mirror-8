dcdd2e3e4feb9d9a8c968ace239986988d57eb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:12 +0000

    Add package kotti_contactform/0.4.1/kotti_contactform-0.4.1.tar.gz

---
+++ packages/kotti_contactform/kotti_contactform-0.4.1.tar.gz/kotti_contactform-0.4.1/kotti_contactform/alembic/versions/39ded03760da_add_column_show_atta.py
@@ -0,0 +1,28 @@
+"""add column show_attachment
+
+Revision ID: 39ded03760da
+Revises: None
+Create Date: 2012-06-27 14:26:30.826076
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '39ded03760da'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import table, column
+
+
+contact_form = table('contact_forms', column('show_attachment', sa.Boolean))
+
+
+def upgrade():
+    op.add_column('contact_forms', sa.Column('show_attachment', sa.Boolean))
+    op.execute(contact_form.update().values({'show_attachment': True}))
+    op.alter_column('contact_forms', 'show_attachment', nullable=False)
+
+
+def downgrade():
+    op.drop_column('contact_forms', 'show_attachment')
---
+++ packages/kotti_contactform/kotti_contactform-0.4.1.tar.gz/kotti_contactform-0.4.1/setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+install_requires = [
+    'alembic',
+    'Babel',
+    'colander >= 0.9.8',
+    'httplib2',
+    'Kotti >= 0.9.2',
+    'kotti_settings',
+    'Pillow',
+]
+
+
+setup(name='kotti_contactform',
+      version='0.4.1',
+      description="Simple contact form for Kotti sites",
+      long_description=README + '\n\n' + CHANGES,
+      keywords='kotti contact form',
+      maintainer='Kotti developers',
+      maintainer_email='kotti@googlegroups.com',
+      url='https://github.com/Kotti/kotti_contactform',
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python",
+          "Framework :: Pylons",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "License :: OSI Approved :: BSD License",
+      ],
+      author='Kotti developers',
+      author_email='kotti@googlegroups.com',
+      license='BSD-2-Clause',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={},
+      message_extractors={"kotti_contactform": [
+          ("**.py", "lingua_python", None),
+          ("**.pt", "lingua_xml", None),
+      ]},
+      )
