d57e6ee48a5beac90f6110e41c3dc13b17a64a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:15 +0000

    Add package lynx/0.2.3/lynx-0.2.3.tar.gz

---
+++ packages/lynx/lynx-0.2.3.tar.gz/lynx-0.2.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+long_description = f.read().strip()
+f.close()
+
+
+
+setup(name='lynx',
+      version='0.2.3',
+      description = "Python configuration library",
+      license="The MIT License (MIT)",
+      url = "https://github.com/omershelef/lynx/",
+      long_description = long_description,
+      author = "Omer Shelef",
+      author_email = "shlaflaf@gmail.com",
+      packages=find_packages(
+        where='.'
+      ),
+      tests_require=['pytest'],
+      test_suite="tests.test_lynx.run_tests"
+      )
