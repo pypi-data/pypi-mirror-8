200dc44da4a0131553581e124f1d2ecb065bcb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:28 +0000

    Add package pynhost/0.1.6/pynhost-0.1.6.tar.gz

---
+++ packages/pynhost/pynhost-0.1.6.tar.gz/pynhost-0.1.6/pynhost/grammarbase.py
@@ -0,0 +1,16 @@
+import inspect
+from pynhost import ruleparser
+
+class GrammarBase:
+    def __init__(self):
+        self.rules = []
+        self.dictionary = {}
+        self.mapping = {}
+
+    def _is_loaded(self):
+        return True
+
+def set_rules(grammar):
+    for rule_text, actions in grammar.mapping.items():
+        rule = ruleparser.Rule(rule_text, grammar.dictionary, actions)
+        grammar.rules.append(rule)
