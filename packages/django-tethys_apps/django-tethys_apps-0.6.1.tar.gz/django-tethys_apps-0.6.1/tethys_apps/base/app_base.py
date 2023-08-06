1416a9c8872fe7b28df2d3509650e7220c5a9c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:56 +0000

    Add package django-tethys_apps/0.6.1/django-tethys_apps-0.6.1.tar.gz

---
+++ packages/django-tethys_apps/django-tethys_apps-0.6.1.tar.gz/django-tethys_apps-0.6.1/tethys_apps/base/app_base.py
@@ -0,0 +1,44 @@
+"""
+********************************************************************************
+* Name: app_base.py
+* Author: Nathan Swain and Scott Christensen
+* Created On: August 19, 2013
+* Copyright: (c) Brigham Young University 2013
+* License: BSD 2-Clause
+********************************************************************************
+"""
+
+
+class TethysAppBase(object):
+    """
+    Base class used for building apps
+    """
+    name = ''
+    index = ''
+    icon = ''
+    root_url = ''
+    color = ''
+
+    def __repr__(self):
+        """
+        String representation
+        """
+        return '<TethysApp: {0}>'.format(self.name)
+
+    def url_map(self):
+        """
+        Must return a list of UrlMap objects
+        """
+        raise NotImplementedError()
+    
+    def persistent_stores(self):
+        """
+        May return a list of PersistentStore objects
+        """
+        return None
+
+    def dataset_services(self):
+        """
+        May return a list of DatasetService objects
+        """
+        return None
