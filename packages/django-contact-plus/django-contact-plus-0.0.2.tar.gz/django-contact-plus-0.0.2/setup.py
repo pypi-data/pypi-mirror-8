f56ec6feeabed0ba15f65f24a5c1a806135f7abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:17 +0000

    Add package django-contact-plus/0.0.2/django-contact-plus-0.0.2.tar.gz

---
+++ packages/django-contact-plus/django-contact-plus-0.0.2.tar.gz/django-contact-plus-0.0.2/setup.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="django-contact-plus",
+    version="0.0.2",
+    author_email="admin@arteria.ch",
+    packages=find_packages(),
+    include_package_data=True,
+    description='Core of cmsplugin_contact_plus extracted as a standalone Django app',
+)
