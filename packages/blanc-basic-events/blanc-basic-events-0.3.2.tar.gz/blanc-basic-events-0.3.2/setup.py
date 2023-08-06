4d2cbfac35cbc21b6233f9d0c0a66afe4f87f9f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:12 +0000

    Add package blanc-basic-events/0.3.2/blanc-basic-events-0.3.2.tar.gz

---
+++ packages/blanc-basic-events/blanc-basic-events-0.3.2.tar.gz/blanc-basic-events-0.3.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='blanc-basic-events',
+    version='0.3.2',
+    description='Blanc Basic Events for Django',
+    long_description=open('README.rst').read(),
+    url='https://github.com/blancltd/blanc-basic-events',
+    maintainer='Alex Tomkins',
+    maintainer_email='alex@blanc.ltd.uk',
+    platforms=['any'],
+    install_requires=[
+        'blanc-basic-assets>=0.3',
+        'icalendar>=3.6',
+    ],
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    license='BSD',
+)
