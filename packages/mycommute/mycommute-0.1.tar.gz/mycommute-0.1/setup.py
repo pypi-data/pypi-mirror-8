5c302e41a6f740304e0b576428548227f75b030b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:21 +0000

    Add package mycommute/0.1/mycommute-0.1.tar.gz

---
+++ packages/mycommute/mycommute-0.1.tar.gz/mycommute-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='mycommute',
+      version='0.1',
+      description='Analyse my London commuting',
+      url='http://github.com/ppuggioni/mycommute',
+      author='Paolo Puggioni',
+      author_email='p.paolo321@gmail.com',
+      license='Apache2.0',
+      packages=['mycommute'],
+      install_requires=[
+          'numpy',
+          'pandas',
+          'matplotlib',
+          
+      ],
+      zip_safe=False)
