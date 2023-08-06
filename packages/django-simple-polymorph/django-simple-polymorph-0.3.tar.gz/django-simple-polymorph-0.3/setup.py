e1076b7ab4031b586c4fd6fba7d02a22e70c7505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:19 +0000

    Add package django-simple-polymorph/0.3/django-simple-polymorph-0.3.tar.gz

---
+++ packages/django-simple-polymorph/django-simple-polymorph-0.3.tar.gz/django-simple-polymorph-0.3/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='django-simple-polymorph',
+    version='0.3',
+    description="A simple utility to polymorph inherited models",
+    author="Danemco, LLC",
+    author_email='dev@velocitywebworks.com',
+    url='https://bitbucket.org/freakypie/django-simple-polymorph',
+    packages=find_packages(),
+    install_requires=[],
+)
