49f9e3c2939ae9fb1c6981eb5918b30bde4ec681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:39 +0000

    Add package yahmm/1.1.2/yahmm-1.1.2.zip

---
+++ packages/yahmm/yahmm-1.1.2.zip/yahmm-1.1.2/setup.py
@@ -0,0 +1,48 @@
+from distutils.core import setup
+from distutils.extension import Extension
+import numpy as np
+
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    use_cython = False
+else:
+    use_cython = True
+
+cmdclass = { }
+
+if use_cython:
+    ext_modules = [
+        Extension("yahmm.yahmm", [ "yahmm/yahmm.pyx" ], include_dirs=[np.get_include()]),
+    ]
+    cmdclass.update({ 'build_ext': build_ext })
+else:
+    ext_modules = [
+        Extension("yahmm.yahmm", [ "yahmm/yahmm.c" ], include_dirs=[np.get_include()]),
+    ]
+
+script_names = [ 'example.py', 'rainy_sunny_hmm.py', 'tied_state_hmm.py', 
+    'infinite_hmm.py']
+
+scripts = [ "examples/{}".format( name ) for name in script_names ]
+
+setup(
+    name='yahmm',
+    version='1.1.2',
+    author='Adam Novak, Jacob Schreiber',
+    author_email='anovak1@ucsc.edu, jmschreiber91@gmail.com',
+    packages=['yahmm'],
+    scripts=scripts,
+    url='http://pypi.python.org/pypi/yahmm/',
+    license='LICENSE.txt',
+    description='YAHMM is a HMM package for Python, implemented in Cython for speed.',
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
+    install_requires=[
+        "cython >= 0.20.1",
+        "numpy >= 1.8.0",
+        "scipy >= 0.13.3",
+        "networkx >= 1.8.1",
+        "matplotlib >= 1.3.1"
+    ],
+)
