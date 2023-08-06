4b05ffb4c5ddb0b91d110895f0011472d836bb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:19 +0000

    Add package Yuntongxun/2.6/Yuntongxun-2.6.tar.gz

---
+++ packages/Yuntongxun/Yuntongxun-2.6.tar.gz/Yuntongxun-2.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'Yuntongxun',
+  	packages = ['yuntongxun'], # this must be the same as the name above
+  	version = '2.6',
+  	description = '',
+  	author = '',
+  	author_email = '',
+  	url = 'https://github.com/nowboy/yuntongxun', # use the URL to the github repo
+  	download_url = 'https://github.com/nowboy/yuntongxun/archive/master.zip', # I'll explain this in a second
+  	keywords = [], # arbitrary keywords
+  	classifiers = [],
+)
