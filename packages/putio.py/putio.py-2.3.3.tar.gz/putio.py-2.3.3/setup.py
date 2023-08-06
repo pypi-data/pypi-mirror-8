d6f711a078610fe7caa31cf7fb5a38d2a91f41a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:53 +0000

    Add package putio.py/2.3.3/putio.py-2.3.3.tar.gz

---
+++ packages/putio.py/putio.py-2.3.3.tar.gz/putio.py-2.3.3/setup.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from setuptools import setup
+
+setup(
+    name='putio.py',
+    version='2.3.3',
+    author=u'Cenk Altı',
+    author_email='cenkalti@gmail.com',
+    url='http://github.com/cenkalti/putio.py',
+    py_modules=['putio'],
+    include_package_data=True,
+    zip_safe=True,
+    platforms='any',
+    install_requires=['requests', 'iso8601'],
+)
