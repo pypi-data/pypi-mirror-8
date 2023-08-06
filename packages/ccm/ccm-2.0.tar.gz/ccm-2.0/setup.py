a57d03b9f50e944f5cb64fa93b4cfbf5b9b0bb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:37 +0000

    Add package ccm/2.0/ccm-2.0.tar.gz

---
+++ packages/ccm/ccm-2.0.tar.gz/ccm-2.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from os.path import abspath, join, dirname
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='ccm',
+    version='2.0',
+    description='Cassandra Cluster Manager',
+    long_description=open(abspath(join(dirname(__file__), 'README.md'))).read(),
+    author='Sylvain Lebresne',
+    author_email='sylvain@datastax.com',
+    url='https://github.com/pcmanus/ccm',
+    packages=['ccmlib', 'ccmlib.cmds'],
+    scripts=['ccm'],
+    install_requires=['pyYaml', 'six >=1.4.1'],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ],
+
+)
