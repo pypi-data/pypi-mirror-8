94e7407655d7f77cf719ed5b380a90c4eff0aedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:34 +0000

    Add package rollbar/0.9.1/rollbar-0.9.1.tar.gz

---
+++ packages/rollbar/rollbar-0.9.1.tar.gz/rollbar-0.9.1/setup.py
@@ -0,0 +1,51 @@
+import os.path
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+README_PATH = os.path.join(HERE, 'README.md')
+try:
+    README = open(README_PATH).read()
+except IOError:
+    README = ''
+
+setup(
+    name='rollbar',
+    packages=find_packages(),
+    version='0.9.1',
+    entry_points= {
+        'paste.filter_app_factory': [
+            'pyramid=rollbar.contrib.pyramid:create_rollbar_middleware'
+        ],
+        'console_scripts': ['rollbar=rollbar.cli:main']
+    },
+    description='Logs exceptions and other data to Rollbar. Provides a generic interface, as well as a Django middleware and a Pyramid tween.',
+    long_description=README,
+    author='Rollbar, Inc.',
+    author_email='support@rollbar.com',
+    test_suite='rollbar.test',
+    url='http://github.com/rollbar/pyrollbar',
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Pyramid",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Bug Tracking",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        ],
+    install_requires=[
+        'requests',
+        ],
+    tests_require=[
+        'mock',
+        'webob',
+        ],
+    )
+
