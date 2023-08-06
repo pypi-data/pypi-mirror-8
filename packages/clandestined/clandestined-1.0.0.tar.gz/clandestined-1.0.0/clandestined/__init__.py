5d85c2249f07ee65f8943ecb4f3ecb06ccf2bfdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:57 +0000

    Add package clandestined/1.0.0/clandestined-1.0.0.tar.gz

---
+++ packages/clandestined/clandestined-1.0.0.tar.gz/clandestined-1.0.0/clandestined/__init__.py
@@ -0,0 +1,4 @@
+from .clandestined import (
+    Cluster,
+    RendezvousHash,
+)
---
+++ packages/clandestined/clandestined-1.0.0.tar.gz/clandestined-1.0.0/setup.py
@@ -0,0 +1,31 @@
+
+import sys
+
+from distutils.core import setup
+from distutils.core import Extension
+
+extra_compile_args = []
+
+if sys.version.startswith('3.4'):
+    extra_compile_args.append("-Wno-error=declaration-after-statement")
+
+requirements = []
+test_requirements = []
+
+setup(
+    name='clandestined',
+    version='1.0.0',
+    license='MIT',
+    author="Ernest W. Durbin III",
+    author_email='ewdurbin@gmail.com',
+    description='rendezvous hashing implementation based on murmur3 hash',
+    url='https://github.com/ewdurbin/clandestined-python',
+    packages=['clandestined'],
+    ext_modules=[Extension('clandestined._murmur3', ['ext/_murmur3.c'],
+                           extra_compile_args=extra_compile_args)],
+    scripts=[],
+    test_suite='tests',
+    install_requires=requirements,
+    tests_require=test_requirements,
+)
+
