1e3b8468da96f7cde43b11046e0476568b7b294a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package socialshares/0.3.2/socialshares-0.3.2.tar.gz

---
+++ packages/socialshares/socialshares-0.3.2.tar.gz/socialshares-0.3.2/socialshares/platforms/linkedin.py
@@ -0,0 +1,12 @@
+import grequests
+
+def fetch(url):
+    return grequests.get('http://www.linkedin.com/countserv/count/share', 
+        params={'url': url, 'format': 'json'})
+
+def parse(response):
+    if response.status_code != 200:
+        raise IOError()
+        
+    result = response.json()
+    return result['count']
