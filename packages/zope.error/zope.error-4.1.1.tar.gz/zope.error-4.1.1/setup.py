eac46836c64841028d66c2cd3966acd5cb33a9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:17 +0000

    Add package zope.error/4.1.1/zope.error-4.1.1.tar.gz

---
+++ packages/zope.error/zope.error-4.1.1.tar.gz/zope.error-4.1.1/setup.py
@@ -0,0 +1,73 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.error package
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.error',
+    version='4.1.1',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    description = "An error reporting utility for Zope3",
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('CHANGES.rst')
+        ),
+    license='ZPL 2.1',
+    keywords = "zope3 error",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://pypi.python.org/pypi/zope.error',
+	packages=find_packages('src'),
+	package_dir = {'': 'src'},
+    namespace_packages=['zope',],
+    install_requires=['setuptools',
+                      'zope.exceptions',
+                      'zope.interface',
+                      'zope.location',
+                      'persistent', # error.py imports from persistent
+                      'six'
+                      ],
+    extras_require=dict(
+          test=[
+              'zope.testing >= 3.8',
+              ]),
+    test_suite = 'zope.error.tests',
+    include_package_data = True,
+    zip_safe = False,
+    )
