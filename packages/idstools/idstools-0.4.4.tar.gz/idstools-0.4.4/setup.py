358df7acb58e577a7b5ed8745ca3cf8350c8fa39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:45 +0000

    Add package idstools/0.4.4/idstools-0.4.4.tar.gz

---
+++ packages/idstools/idstools-0.4.4.tar.gz/idstools-0.4.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+import idstools
+
+setup(
+    name="idstools",
+    version=idstools.version,
+    description="IDS Utility Library",
+    author="Jason Ish",
+    author_email="ish@unx.ca",
+    packages=[
+        "idstools",
+        "idstools.scripts",
+        "idstools.compat",
+        "idstools.compat.argparse",
+    ],
+    url="https://github.com/jasonish/py-idstools",
+    license="BSD",
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+    ],
+    scripts = [
+        "bin/idstools-gensidmsgmap",
+        "bin/idstools-u2fast",
+        "bin/idstools-u2json",
+    ],
+)
