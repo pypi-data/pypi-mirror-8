9e5da0e1460a7a67e4a6d7fd30c8183c1703063a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:45 +0000

    Add package django-better-filter-widget/0.3/django-better-filter-widget-0.3.tar.gz

---
+++ packages/django-better-filter-widget/django-better-filter-widget-0.3.tar.gz/django-better-filter-widget-0.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-better-filter-widget',
+    version='0.3',
+    author='Exotic Objects LLC',
+    author_email='git@extc.co',
+    license='MIT',
+    url='https://github.com/ExoticObjects/django-better-filter-widget',
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    description='A better filter widget for foreign key relationships',
+    packages=find_packages(),
+)
