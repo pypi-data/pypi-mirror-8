e389b356143ea4126b2eb4bfca04b29f63a06912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:34 +0000

    Add package ultimatum/4.0.5/ultimatum-4.0.5.tar.gz

---
+++ packages/ultimatum/ultimatum-4.0.5.tar.gz/ultimatum-4.0.5/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Scripts and system tool wrappers for FreeBSD (and other *BSDs)
+
+This module is split from module to platform dependent tool
+"""
+
+import sys
+import os
+import glob
+
+from setuptools import setup, find_packages
+
+VERSION='4.0.5'
+README = open(os.path.join(os.path.dirname(__file__),'README.md'),'r').read()
+
+setup(
+    name = 'ultimatum',
+    keywords = 'System Management Utility FreeBSD Scripts',
+    description = 'Sysadmin utility modules and scripts for FreeBSD',
+    author = 'Ilkka Tuohela',
+    author_email = 'hile@iki.fi',
+    long_description = README,
+    version = VERSION,
+    url = 'http://tuohela.net/packages/ultimatum',
+    license = 'PSF',
+    zip_safe = False,
+    packages = find_packages(),
+    scripts = glob.glob('bin/*'),
+    install_requires = (
+        'seine>=2.4.1',
+        'systematic>=4.2.0',
+    ),
+)
+
