3a9ebc4d0ed20d3f12e4527b9e12b798c22745ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:11 +0000

    Add package pytaskpool/1.0/pytaskpool-1.0.tar.gz

---
+++ packages/pytaskpool/pytaskpool-1.0.tar.gz/pytaskpool-1.0/examples/pytask_ex1.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+from time import sleep
+from random import randint
+
+import pytaskpool as tp
+
+
+def test_func(x):
+    """
+    This  function return the square root of x in an array after randomly waiting between 0 to 2.56s
+    """
+    rand_time = randint(0, 256) / 100.
+    sleep(rand_time)
+    print "function :", x, ": sleeping", rand_time, "before exit"
+    return [x ** x]
+
+
+# how many time we execute the function
+count = 32
+
+# how many process we'll use
+process = 4
+
+count_depth = range(count)
+excepted = [[r ** r] for r in count_depth]
+
+# creation of a pool of 8 process
+tp = tp.TaskPool([], 8)
+
+print "Launching", count, "functions that will wait between 0 to 2.56s using", process, "processes"
+
+# launch
+for y in count_depth:
+    tp.launch(test_func, y)
+
+print "All launched, waiting all to finish"
+
+unres = [r for r in tp.get_unsorted_results()]
+print "All tasks done"
+
+res = [r for r in tp.get_sorted_results()]
+
+if res == excepted:
+    print "results match the excepted"
---
+++ packages/pytaskpool/pytaskpool-1.0.tar.gz/pytaskpool-1.0/setup.py
@@ -0,0 +1,85 @@
+# coding=utf-8
+
+"""
+Setup file for pytaskpool
+"""
+
+from setuptools import setup, find_packages, Command
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys, subprocess
+
+        errno = subprocess.call([sys.executable, 'runtests.py', '-v'])
+        raise SystemExit(errno)
+
+
+setup(
+    name='pytaskpool',
+    version='1.0',
+
+    description='A simple multiprocessing function pool',
+    long_description=long_description,
+
+    author='AMIAUD Christopher',
+    author_email='christopher.amiaud@gmail.com',
+
+    url='https://github.com/govlog/pytaskpool',
+    download_url='https://github.com/govlog/pytaskpool/tarball/1.0',
+
+    license='GPL',
+
+    classifiers=[
+
+        'Development Status :: 5 - Production/Stable',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Topic :: System :: Hardware :: Symmetric Multi-processing',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Programming Language :: Python :: 2.3',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+
+    ],
+
+    keywords='simple multiprocessing function pool',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    install_requires=['multiprocessing'],
+
+    cmdclass={'test': PyTest}
+
+
+)
