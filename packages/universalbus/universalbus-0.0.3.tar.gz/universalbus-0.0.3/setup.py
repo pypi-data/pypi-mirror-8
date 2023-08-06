ada38368db58945beb7385834c630258caeeb004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package universalbus/0.0.3/universalbus-0.0.3.tar.gz

---
+++ packages/universalbus/universalbus-0.0.3.tar.gz/universalbus-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+VERSION = "0.0.3"
+
+setup(
+    name='universalbus',
+    description="Universal Bus over rabbitmq.",
+    version=VERSION,
+    url='https://github.com/KokocGroup/universal-bus',
+    download_url='https://github.com/KokocGroup/universal-bus/tarball/v{}'.format(VERSION),
+    packages=['universalbus'],
+    install_requires=[
+        'pika',
+    ],
+)
---
+++ packages/universalbus/universalbus-0.0.3.tar.gz/universalbus-0.0.3/universalbus/__init__.py
@@ -0,0 +1,39 @@
+import pika
+import json
+
+
+class EventSender(object):
+    def __init__(self, login, password, host, virtual_host, exchange):
+        self.exchange = exchange
+        conn_params = pika.ConnectionParameters(
+            host=host,
+            virtual_host=virtual_host,
+            credentials=pika.PlainCredentials(login, password)
+        )
+        self.connection = pika.BlockingConnection(conn_params)
+
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.exchange, type='topic', auto_delete=False)
+
+    def __del__(self):
+        self.connection.close()
+
+    def push(self, routing_key, message):
+        dumped_message = json.dumps(message)
+        self.channel.basic_publish(exchange=self.exchange, routing_key=routing_key, body=dumped_message)
+
+
+class EventListener(EventSender):
+    def __init__(self, login, password, host, virtual_host, exchange, queue, bindings):
+        super(EventListener, self).__init__(login, password, host, virtual_host, exchange)
+        self.queue = queue
+        self.channel.queue_declare(queue, exclusive=False)
+
+        for binding_key in bindings:
+            self.channel.queue_bind(exchange=exchange,
+                                    queue=queue,
+                                    routing_key=binding_key)
+
+    def register_callback(self, callback, no_ack=True):
+        self.channel.basic_consume(callback, queue=self.queue, no_ack=no_ack)
+        self.channel.start_consuming()
