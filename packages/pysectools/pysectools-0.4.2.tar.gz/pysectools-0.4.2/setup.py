70fe4e5f69e648e007282e9c2613fcd504ede1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:17 +0000

    Add package pysectools/0.4.2/pysectools-0.4.2.tar.gz

---
+++ packages/pysectools/pysectools-0.4.2.tar.gz/pysectools-0.4.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="pysectools",
+    version="0.4.2",
+    description="""A package of security-related Python functions. Dropping
+    privileges, entering sandboxes, generating random numbers, asking for
+    passwords...""",
+#    long_description="""""",
+    license="WTFPL",
+    author="Greg V",
+    author_email="greg@unrelenting.technology",
+    url="https://github.com/myfreeweb/pysectools",
+    packages=["pysectools"],
+    keywords=["security", "pinentry", "getpass", "capsicum", "random", "rng",
+              "arc4random"],
+    classifiers=[
+        "Operating System :: POSIX",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: Public Domain",
+        "Topic :: Security"
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    package_data={
+        "": ["README.md", "COPYING"]
+    }
+)
