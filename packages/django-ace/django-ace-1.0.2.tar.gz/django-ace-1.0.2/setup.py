8dc4c4d5d21c8af27fbe3643b59802798073d5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:20 +0000

    Add package django-ace/1.0.2/django-ace-1.0.2.tar.gz

---
+++ packages/django-ace/django-ace-1.0.2.tar.gz/django-ace-1.0.2/setup.py
@@ -0,0 +1,34 @@
+from os.path import join
+import re
+from setuptools import setup, find_packages
+
+
+# dynamically pull the version from django_ace/__init__.py
+version = re.search('^__version__ = "(.+?)"$',
+                    open(join('django_ace', '__init__.py')).read(), re.MULTILINE).group(1)
+
+setup(
+    name='django-ace',
+    version=version,
+    description='django-ace provides integration for ajax.org ACE with Django',
+    long_description=open('README.rst').read(),
+
+    author='Bradley Ayers',
+    author_email='bradley.ayers@gmail.com',
+    license="Simplified BSD",
+    url='https://github.com/bradleyayers/django-ace',
+
+    packages=find_packages(exclude=["example", "example.*"]),
+    include_package_data=True,
+    install_requires=['Django'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        ],
+    )
