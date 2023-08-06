2493ba9997b21c9de2728030d1e6f41e82b50056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:43 +0000

    Add package fabrickit/0.1.4/fabrickit-0.1.4.tar.gz

---
+++ packages/fabrickit/fabrickit-0.1.4.tar.gz/fabrickit-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'fabrickit',
+  packages = ['fabrickit'], 
+  version = '0.1.4',
+  description = 'Fabric API wrapper',
+  author = 'Hyukjin Kwon',
+  author_email = 'gurwls223@gmail.com',
+  url = 'https://github.com/HyukjinKwon/fabrickit',
+  download_url = 'https://github.com/HyukjinKwon/fabrickit/tarball/0.1',
+  keywords = ['fabric', 'provision'],
+  classifiers = [],
+  install_requires = ['fabric']
+)
