e45652f7c0063d3e086035d8f4ac8349672762b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:17 +0000

    Add package autosign/0.1.4/autosign-0.1.4.tar.gz

---
+++ packages/autosign/autosign-0.1.4.tar.gz/autosign-0.1.4/autosign/info/desc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+##
+# autosign
+# https://github.com/leosartaj/autosign.git
+# 
+# copyright (c) 2014 sartaj singh
+# licensed under the mit license.
+##
+
+"""
+Define description variable
+"""
+
+from version import __version__
+
+__desc__ = 'autosign  ' + __version__
