a6e827d054bc3ecc280aa58fc79915e9bf63a856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:51 +0000

    Add package shareboard/0.1.7/shareboard-0.1.7.tar.gz

---
+++ packages/shareboard/shareboard-0.1.7.tar.gz/shareboard-0.1.7/src/shareboard/parser.py
@@ -0,0 +1,18 @@
+# vim: set fileencoding=utf-8 :
+import subprocess
+
+
+class ShellParser(object):
+    def __init__(self, command):
+        self.command = command
+
+    def parse(self, text, cwd=None, encoding='utf-8'):
+        if cwd == "":
+            cwd = None
+        p = subprocess.Popen(self.command, shell=True, cwd=cwd,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE)
+        # the text should be encoded to use in Shell
+        stdout, stderr = p.communicate(text.encode(encoding))
+        # the text should be decoded to use in Python
+        return unicode(stdout, encoding)
---
+++ packages/shareboard/shareboard-0.1.7.tar.gz/shareboard-0.1.7/src/shareboard/server.py
@@ -0,0 +1,74 @@
+# vim: set fileencoding=utf-8 :
+import cgi
+import urlparse
+import os.path
+from BaseHTTPServer import HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler
+
+
+REQUEST_IGNORE_PATHS = (
+        '/favicon.ico',
+    )
+
+
+class HTTPPreviewRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path in REQUEST_IGNORE_PATHS:
+            return
+        # respond data
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(getattr(self.server, 'text', ''))
+
+    def do_POST(self):
+        try:
+            request = cgi.FieldStorage(
+                    self.rfile, self.headers, environ={
+                    'REQUEST_METHOD': 'POST',
+                    'CONTENT_TYPE': self.headers['Content-Type'],
+                })
+            if 'text' not in request:
+                msg = 'The request should contain "text" field to set new text'
+                self.send_response(400, msg)
+                self.end_headers()
+                self.wfile.write(msg)
+                return
+            # decode text to unicode
+            text = unicode(request['text'].value, self.server.encoding)
+            filename = request.getvalue('filename', 'index.html')
+            # respond OK
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write('OK')
+            # modify the text with callback if it's specified
+            if self.server.callback:
+                cwd = os.path.dirname(filename)
+                text = self.server.callback(text, cwd)
+            # store the value
+            self.server.text = text
+            # emit request recieved if it's required
+            if hasattr(self.server, 'emitter'):
+                self.server.emitter.emit_request_recieved(text, filename)
+        except Exception, e:
+            print "Error:", e
+            self.send_response(500, e)
+            self.end_headers()
+            self.wfile.write("Unexpected error: %s" % e)
+
+    def log_message(self, format, *args):
+        if not self.server.silent:
+            # `super(cls, self).log_message(format, *args)` did not work
+            BaseHTTPRequestHandler.log_message(self, format, *args)
+        return None
+
+
+class RequestServer(object):
+    def __init__(self, host, port, encoding='utf-8', callback=None, silent=False):
+        super(RequestServer, self).__init__()
+        self.httpd = HTTPServer((host, port), HTTPPreviewRequestHandler)
+        self.httpd.encoding = encoding
+        self.httpd.callback = callback
+        self.httpd.silent = silent
+
+    def start(self):
+        self.httpd.serve_forever()
