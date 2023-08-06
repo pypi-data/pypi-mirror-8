69766adf49cfe0eeaea1f032cf78bb6962c64d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:43 +0000

    Add package GeobricksMetadataManager/0.1.0/GeobricksMetadataManager-0.1.0.tar.gz

---
+++ packages/GeobricksMetadataManager/GeobricksMetadataManager-0.1.0.tar.gz/GeobricksMetadataManager-0.1.0/geobricks_metadata_manager/config/config.py
@@ -0,0 +1,35 @@
+import logging
+
+config = {
+
+    "settings": {
+
+        # To be used by Flask: DEVELOPMENT ONLY
+        "debug": True,
+
+        # Flask host: DEVELOPMENT ONLY
+        "host": "localhost",
+
+        # Flask port: DEVELOPMENT ONLY
+        "port": 5904,
+
+        # Logging configurations
+        "logging": {
+            "level": logging.INFO,
+            "format": "%(asctime)s | %(levelname)-8s | %(name)-20s | Line: %(lineno)-5d | %(message)s",
+            "datefmt": "%d-%m-%Y | %H:%M:%s"
+        },
+
+        # metadata config
+        "metadata": {
+            "url_create_metadata": "//exldvsdmxreg1.ext.fao.org:7788/v2/msd/resources/metadata",
+            "url_get_metadata_uid": "//exldvsdmxreg1.ext.fao.org:7788/v2/msd/resources/metadata/uid/<uid>",
+            # get metadata
+            "url_get_metadata": "//exldvsdmxreg1.ext.fao.org:7788/v2/msd/resources/metadata/uid/<uid>",
+            "url_get_full_metadata": "//exldvsdmxreg1.ext.fao.org:7788/v2/msd/resources/metadata/uid/<uid>?full=true&dsd=true",
+            # coding system
+            "url_create_coding_system": "//exldvsdmxreg1.ext.fao.org:7788/v2/msd/resources",
+            "url_data_coding_system": "://exldvsdmxreg1.ext.fao.org:7788/v2/msd/resources/data/uid/<uid>",
+        }
+    }
+}
---
+++ packages/GeobricksMetadataManager/GeobricksMetadataManager-0.1.0.tar.gz/GeobricksMetadataManager-0.1.0/test/config/metadata_manager_config_test.py
@@ -0,0 +1,9 @@
+config = {
+    "settings": {
+        "metadata": {
+             "url_create_metadata": "http://exldvsdmxreg1.ext.fao.org:7788/v2/msd/resources/metadata",
+             "url_get_metadata_uid": "http://exldvsdmxreg1.ext.fao.org:7788/v2/msd/resources/metadata/uid/<uid>",
+             "url_get_metadata": "http://exldvsdmxreg1.ext.fao.org:7788/v2/msd/resources/metadata/uid/<uid>"
+        }
+    }
+}
