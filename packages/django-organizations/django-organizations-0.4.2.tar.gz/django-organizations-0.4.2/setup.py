ccbf193320f242cd18615a8e3e7c351022ec11e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:40 +0000

    Add package django-organizations/0.4.2/django-organizations-0.4.2.tar.gz

---
+++ packages/django-organizations/django-organizations-0.4.2.tar.gz/django-organizations-0.4.2/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import organizations
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
+setup(
+    author="Ben Lopatin",
+    author_email="ben@wellfire.co",
+    name='django-organizations',
+    version=organizations.__version__,
+    description='Group accounts for Django',
+    long_description=readme + '\n\n' + history,
+    url='https://github.com/bennylope/django-organizations/',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=[
+        'organizations',
+        'organizations.backends',
+        'organizations.south_migrations',
+        'organizations.templatetags',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    install_requires=[
+        'Django>=1.4.2,<1.8',
+    ],
+    test_suite='tests',
+    include_package_data=True,
+    zip_safe=False,
+)
