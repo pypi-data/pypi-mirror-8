dab9db6fb914cf9ca0e9fb458d979f2e00090d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:40 +0000

    Add package Flask-RESTful/0.2.5/Flask-RESTful-0.2.5.tar.gz

---
+++ packages/Flask-RESTful/Flask-RESTful-0.2.5.tar.gz/Flask-RESTful-0.2.5/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-RESTful',
+    version='0.2.5',
+    url='https://www.github.com/twilio/flask-restful/',
+    author='Kyle Conroy',
+    author_email='help@twilio.com',
+    description='Simple framework for creating REST APIs',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    test_suite = 'nose.collector',
+    setup_requires=[
+        'nose>=1.1.2',
+        'mock>=0.8',
+        'blinker>=1.2',
+    ],
+    install_requires=[
+        'Flask>=0.8',
+        'six>=1.3.0',
+    ],
+    # Install these with "pip install -e '.[paging]'" or '.[docs]'
+    extras_require={
+        'paging': 'pycrypto>=2.6',
+        'docs': 'sphinx',
+    }
+)
