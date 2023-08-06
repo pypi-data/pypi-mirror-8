278b85c9a16388b62eaa18fe5d1ec3f1b68c69e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:56 +0000

    Add package makesurface/0.0.2dev/makesurface-0.0.2dev.tar.gz

---
+++ packages/makesurface/makesurface-0.0.2dev.tar.gz/makesurface-0.0.2dev/makesurface/scripts/tools.py
@@ -0,0 +1,33 @@
+class rasterIndexer:
+    def __init__(self, shape, bounds):
+        self.shape = shape
+        self.bounds = bounds
+        self.xRange = bounds.right - bounds.left
+        self.yRange = bounds.top - bounds.bottom
+        self.cellSizeX = (self.xRange) / shape[1]
+        self.cellSizeY = (self.yRange) / shape[0]
+
+    def getIndices(self, x, y):
+        return [int(((1 - y - self.bounds.bottom) / self.yRange) * self.shape[0]),
+                int(((x - self.bounds.left) / self.xRange) * self.shape[1])]
+
+def resampleAffine(otrans, factor):
+    from rasterio import Affine
+    return Affine(otrans.a / float(factor),otrans.b,otrans.c,
+             otrans.d,otrans.e / float(factor), otrans.f)
+
+def handleGrib2(gribArr, otrans):
+    from rasterio import Affine
+    import numpy as np
+    from scipy.ndimage import zoom
+    gribArr = zoom(gribArr, 2, order=1)
+    outAff = Affine(otrans.a / 2.0, otrans.b,otrans.c - 180.0 + (otrans.a / 2.0),
+             otrans.d,otrans.e / 2.0, otrans.f)
+    oshape = gribArr.shape
+    fixGrib = np.hstack((gribArr[:, oshape[1] / 2 + 1:oshape[1]],gribArr[:, 0:oshape[1] / 2 + 1]))
+    return fixGrib, outAff
+
+if __name__ == '__main__':
+    rasterIndexer()
+    handleGrib2()
+    resampleAffine
