e57d657250cb6bad1ca5aa7e9d133b1345d5b4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:32 +0000

    Add package gocept.jslint/1.1.1/gocept.jslint-1.1.1.zip

---
+++ packages/gocept.jslint/gocept.jslint-1.1.1.zip/gocept.jslint-1.1.1/setup.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+import sys
+
+install_requires = [
+    'setuptools',
+]
+
+if sys.version_info < (2, 7):
+    install_requires.append('unittest2')
+
+
+setup(
+    name='gocept.jslint',
+    version='1.1.1',
+    author='Wolfgang Schnerring',
+    author_email='ws@gocept.com',
+    url='https://bitbucket.org/gocept/gocept.jslint',
+    description="""\
+Python-unittest integration for jslint.
+""",
+    long_description=(
+        open('README.txt').read()
+        + '\n\n'
+        + open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='ZPL',
+    namespace_packages=['gocept'],
+    install_requires=install_requires,
+    extras_require=dict(test=[
+        'mock',
+    ]),
+    entry_points=dict(console_scripts=[
+        'gocept-jshint = gocept.jslint.util:run_jslint',
+    ]),
+)
