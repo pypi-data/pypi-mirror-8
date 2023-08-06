0fc9b5be3c5056cb96deacb22c9e9294337475c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:25 +0000

    Add package ZEO/4.1.0/ZEO-4.1.0.tar.gz

---
+++ packages/ZEO/ZEO-4.1.0.tar.gz/ZEO-4.1.0/CHANGES.rst
@@ -0,0 +1,51 @@
+Changelog
+=========
+
+4.1.0 (2015-01-06)
+------------------
+
+- Add support for Python 3.4.
+
+- Added a new ``ruok`` client protocol for getting server status on
+  the ZEO port without creating a full-blown client connection and
+  without logging in the server log.
+
+- Log errors on server side even if using multi threaded delay.
+
+
+4.0.0 (2013-08-18)
+------------------
+
+- Avoid reading excess random bytes when setting up an auth_digest session.
+
+- Optimize socket address enumeration in ZEO client (avoid non-TCP types).
+
+- Improve Travis CI testing support.
+
+- Assign names to all threads for better runtime debugging.
+
+- Fix "assignment to keyword" error under Py3k in 'ZEO.scripts.zeoqueue'.
+
+4.0.0b1 (2013-05-20)
+--------------------
+
+- Depend on ZODB >= 4.0.0b2
+
+- Add support for Python 3.2 / 3.3.
+
+4.0.0a1 (2012-11-19)
+--------------------
+
+First (in a long time) separate ZEO release.
+
+Since ZODB 3.10.5:
+
+- Storage servers now emit Serving and Closed events so subscribers
+  can discover addresses when dynamic port assignment (bind to port 0)
+  is used. This could, for example, be used to update address
+  information in a ZooKeeper database.
+
+- Client storages have a method, new_addr, that can be used to change
+  the server address(es). This can be used, for example, to update a
+  dynamically determined server address from information in a
+  ZooKeeper database.
---
+++ packages/ZEO/ZEO-4.1.0.tar.gz/ZEO-4.1.0/setup.py
@@ -0,0 +1,137 @@
+##############################################################################
+#
+# Copyright (c) 2002, 2003 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+"""
+version = '4.1.0'
+from setuptools import setup, find_packages
+import os
+import sys
+
+if sys.version_info < (2, 6):
+    print("This version of ZEO requires Python 2.6 or higher")
+    sys.exit(0)
+
+classifiers = """\
+Intended Audience :: Developers
+License :: OSI Approved :: Zope Public License
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: Implementation :: CPython
+Topic :: Database
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Microsoft :: Windows
+Operating System :: Unix
+Framework :: ZODB
+"""
+
+def _modname(path, base, name=''):
+    if path == base:
+        return name
+    dirname, basename = os.path.split(path)
+    return _modname(dirname, base, basename + '.' + name)
+
+def _flatten(suite, predicate=lambda *x: True):
+    from unittest import TestCase
+    for suite_or_case in suite:
+        if predicate(suite_or_case):
+            if isinstance(suite_or_case, TestCase):
+                yield suite_or_case
+            else:
+                for x in _flatten(suite_or_case):
+                    yield x
+
+def _no_layer(suite_or_case):
+    return getattr(suite_or_case, 'layer', None) is None
+
+def _unittests_only(suite, mod_suite):
+    for case in _flatten(mod_suite, _no_layer):
+        suite.addTest(case)
+
+def alltests():
+    import logging
+    import pkg_resources
+    import unittest
+    import ZEO.ClientStorage
+
+    class NullHandler(logging.Handler):
+        level = 50
+
+        def emit(self, record):
+            pass
+
+    logging.getLogger().addHandler(NullHandler())
+
+    suite = unittest.TestSuite()
+    base = pkg_resources.working_set.find(
+        pkg_resources.Requirement.parse('ZEO')).location
+    for dirpath, dirnames, filenames in os.walk(base):
+        if os.path.basename(dirpath) == 'tests':
+            for filename in filenames:
+                if filename != 'testZEO.py': continue
+                if filename.endswith('.py') and filename.startswith('test'):
+                    mod = __import__(
+                        _modname(dirpath, base, os.path.splitext(filename)[0]),
+                        {}, {}, ['*'])
+                    _unittests_only(suite, mod.test_suite())
+    return suite
+
+tests_require = ['zope.testing', 'manuel', 'random2']
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    )
+setup(name="ZEO",
+      version=version,
+      description = long_description.split('\n', 2)[1],
+      long_description = long_description,
+      url = 'https://pypi.python.org/pypi/ZEO',
+      maintainer="Zope Foundation and Contributors",
+      maintainer_email="zodb-dev@zope.org",
+      packages = find_packages('src'),
+      package_dir = {'': 'src'},
+      license = "ZPL 2.1",
+      platforms = ["any"],
+      classifiers = filter(None, classifiers.split("\n")),
+      test_suite="__main__.alltests", # to support "setup.py test"
+      tests_require = tests_require,
+      extras_require = dict(test=tests_require),
+      install_requires = [
+          'ZODB >= 4.0.0b2',
+          'six',
+          'transaction',
+          'persistent',
+          'zc.lockfile',
+          'ZConfig',
+          'zdaemon',
+          'zope.interface',
+          ],
+      zip_safe = False,
+      entry_points = """
+      [console_scripts]
+      zeopack = ZEO.scripts.zeopack:main
+      runzeo = ZEO.runzeo:main
+      zeopasswd = ZEO.zeopasswd:main
+      zeoctl = ZEO.zeoctl:main
+      zeo-nagios = ZEO.nagios:main
+      """,
+      include_package_data = True,
+      )
