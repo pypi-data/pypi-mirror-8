a73488357c6b5d83232669b5188db2a186f8faa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:21 +0000

    Add package edopkg/0.1.2/edopkg-0.1.2.zip

---
+++ packages/edopkg/edopkg-0.1.2.zip/edopkg-0.1.2/edopkg/config.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+DEFAULT_PATH = r'~/.edopkgrc'
+OC_API = r'https://oc-api.everydo.cn'
+CLIENT_ID = 'test'
+CLIENT_SECRET = '022127e182a934dea7d69s10697s8ac2'
+
---
+++ packages/edopkg/edopkg-0.1.2.zip/edopkg-0.1.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   ZopeEdit, client for ExternalEditor
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+setup(
+        name='edopkg',
+        version='0.1.2',
+        zip_safe=False,
+        author="xiehaiyang",
+        author_email="service@everydo.com",
+        url="http://everydo.com/",
+        description=u"Easydo Package Syncer",
+        long_description=README,
+        packages = find_packages(),
+        entry_points = {
+            'console_scripts': ['edopkg = edopkg.main:main']
+        },
+        install_requires=[
+            'edo_client',
+            'PyYAML'
+        ],
+)
