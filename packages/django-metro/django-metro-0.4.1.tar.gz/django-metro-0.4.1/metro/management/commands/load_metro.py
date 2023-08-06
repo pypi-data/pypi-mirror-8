a21d243de5c68a28f3d07677c7e1817e78e45c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:53 +0000

    Add package django-metro/0.4.1/django-metro-0.4.1.tar.gz

---
+++ packages/django-metro/django-metro-0.4.1.tar.gz/django-metro-0.4.1/metro/management/commands/load_metro.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+
+from metro.parser import provider
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        provider.download_all()
+        print 'Done'
---
+++ packages/django-metro/django-metro-0.4.1.tar.gz/django-metro-0.4.1/metro/parser/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from importlib import import_module
+from django.conf import settings
+
+from metro.models import Metro, MetroLine
+
+provider = None
+
+# import data provider
+try:
+    module = import_module(
+        'metro.parser.providers.%s' % (
+            getattr(settings, 'METRO_PROVIDER', 'moscow').lower()
+        )
+    )
+    provider = module.DataProvider(
+        station_model=Metro, line_model=MetroLine
+    )
+except ImportError:
+    raise NotImplementedError(
+        u'Provider for this city does not implemented'
+    )
