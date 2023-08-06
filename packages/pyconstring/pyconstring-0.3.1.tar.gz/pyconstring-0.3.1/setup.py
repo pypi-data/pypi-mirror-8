97489ab49b780c653454813d1540aa2aea8ccd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:37 +0000

    Add package pyconstring/0.3.1/pyconstring-0.3.1.tar.gz

---
+++ packages/pyconstring/pyconstring-0.3.1.tar.gz/pyconstring-0.3.1/setup.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+
+from setuptools import setup
+import pyconstring
+
+def read_file(path):
+    with open(path) as f:
+        return f.read()
+
+setup(
+    name='pyconstring',
+    version=pyconstring.__version__,
+    description='Tool to handle connection strings',
+    long_description=read_file('README.rst'),
+    url='https://github.com/ikaros45/pyconstring',
+    author='Bor González-Usach',
+    author_email='bgusach@gmail.com',
+    license='MIT',
+    packages=[b'pyconstring', b'tests'],
+    keywords='connection string',
+    zip_safe=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Topic :: Database',
+        'Topic :: Utilities',
+    ],
+)
