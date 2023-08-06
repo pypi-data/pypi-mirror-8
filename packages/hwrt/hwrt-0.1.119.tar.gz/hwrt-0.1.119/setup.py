41a693ed81c28ed6a955d1abbfa1c8113f7fa21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:48 +0000

    Add package hwrt/0.1.119/hwrt-0.1.119.tar.gz

---
+++ packages/hwrt/hwrt-0.1.119.tar.gz/hwrt-0.1.119/setup.py
@@ -0,0 +1,47 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'hwrt',
+    'version': '0.1.119',
+    'author': 'Martin Thoma',
+    'author_email': 'info@martin-thoma.de',
+    'packages': ['hwrt'],
+    'scripts': ['bin/backup.py', 'bin/view.py', 'bin/download.py',
+                'bin/test.py', 'bin/train.py', 'bin/analyze_data.py',
+                'bin/hwrt', 'bin/record.py'],
+    'package_data': {'hwrt': ['templates/*', 'misc/*']},
+    'url': 'https://github.com/MartinThoma/hwrt',
+    'license': 'MIT',
+    'description': 'Handwriting Recognition Tools',
+    'long_description': """A tookit for handwriting recognition. It was
+    developed as part of the bachelors thesis of Martin Thoma.""",
+    'install_requires': [
+        "argparse",
+        "theano",
+        "nose",
+        "natsort",
+        "PyYAML",
+        "matplotlib",
+        "shapely"
+    ],
+    'keywords': ['HWRT', 'recognition', 'handwriting', 'on-line'],
+    'download_url': 'https://github.com/MartinThoma/hwrt',
+    'classifiers': ['Development Status :: 3 - Alpha',
+                    'Environment :: Console',
+                    'Intended Audience :: Developers',
+                    'Intended Audience :: Science/Research',
+                    'License :: OSI Approved :: MIT License',
+                    'Natural Language :: English',
+                    'Programming Language :: Python :: 2.7',
+                    'Programming Language :: Python :: 3',
+                    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                    'Topic :: Software Development',
+                    'Topic :: Utilities'],
+    'zip_safe': False,
+    'test_suite': 'nose.collector'
+}
+
+setup(**config)
