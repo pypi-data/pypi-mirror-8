423d53e7309b9264b57f4e8da9e6eac16b5fdd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package halogen/1.0.3/halogen-1.0.3.tar.gz

---
+++ packages/halogen/halogen-1.0.3.tar.gz/halogen-1.0.3/CHANGES.rst
@@ -0,0 +1,13 @@
+Changelog
+=========
+
+1.0.3
+-----
+
+* Allow Embedded fields to be marked as not required (mattupstate)
+* Field order is preserved in serialized documents (mattupstate)
+
+1.0.0
+-----
+
+* Initial public release
---
+++ packages/halogen/halogen-1.0.3.tar.gz/halogen-1.0.3/halogen/__init__.py
@@ -0,0 +1,16 @@
+from halogen.schema import Schema, Attr, Link, Curie, Embedded, Accessor
+from halogen import types
+from halogen import validators
+from halogen import exceptions
+
+__all__ = [
+    "Accessor",
+    "Attr",
+    "Curie",
+    "Embedded",
+    "exceptions",
+    "Link",
+    "Schema",
+    "types",
+    "validators",
+]
