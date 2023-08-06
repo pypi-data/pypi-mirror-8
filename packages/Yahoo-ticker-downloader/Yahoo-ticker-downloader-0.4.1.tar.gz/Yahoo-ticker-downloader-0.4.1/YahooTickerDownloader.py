671c882ff5ee591a80777912f935b5100583d1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:20 +0000

    Add package Yahoo-ticker-downloader/0.4.1/Yahoo-ticker-downloader-0.4.1.tar.gz

---
+++ packages/Yahoo-ticker-downloader/Yahoo-ticker-downloader-0.4.1.tar.gz/Yahoo-ticker-downloader-0.4.1/YahooTickerDownloader.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+import sys
+import pickle
+import tablib
+from time import sleep
+
+from ytd.downloader.StockDownloader import StockDownloader
+from ytd.downloader.ETFDownloader import ETFDownloader
+from ytd.downloader.FutureDownloader import FutureDownloader
+from ytd.downloader.IndexDownloader import IndexDownloader
+from ytd.downloader.MutualFundDownloader import MutualFundDownloader
+from ytd.downloader.CurrencyDownloader import CurrencyDownloader
+from ytd.downloader.WarrantDownloader import WarrantDownloader
+from ytd.downloader.BondDownloader import BondDownloader
+from ytd.compat import unicode
+
+import bs4
+
+# Do not remove this line. It contains magic.
+# Required for correct pickling/unpickling.
+sys.setrecursionlimit(10000)
+
+options = {
+    "stocks": StockDownloader(),
+    "etf": ETFDownloader(),
+    "future": FutureDownloader(),
+    "index": IndexDownloader(),
+    "mutualfund": MutualFundDownloader(),
+    "currency": CurrencyDownloader(),
+    "warrant": WarrantDownloader(),
+    "bond": BondDownloader(),
+}
+
+
+def loadDownloader():
+    with open("downloader.pickle", "rb") as f:
+        return pickle.load(f)
+
+
+def saveDownloader(downloader):
+    with open("downloader.pickle", "wb") as f:
+        pickle.dump(downloader, file=f, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def downloadEverything(downloader):
+
+    loop = 0
+    while not downloader.isDone():
+
+        symbols = downloader.nextRequest()
+        print("Got " + str(len(symbols)) + " downloaded " + downloader.type + " symbols:")
+        if(len(symbols)>2):
+            print (" " + unicode(symbols[0]))
+            print (" " + unicode(symbols[1]))
+            print ("  ect...")
+        downloader.printProgress()
+
+        # Save download state occasionally.
+        # We do this in case this long running is suddenly interrupted.
+        loop = loop + 1
+        if loop % 200 == 0:
+            print ("Saving downloader to disk...")
+            saveDownloader(downloader)
+            print ("Downloader successfully saved.")
+            print ("")
+
+        if not downloader.isDone():
+            sleep(5)  # So we don't overload the server.
+
+def main():
+    downloader = None
+
+    print("Checking if we can resume a old download session")
+    try:
+        downloader = loadDownloader()
+        print("Downloader found on disk, resuming")
+    except:
+        print("No old downloader found on disk")
+        if(len(sys.argv) <= 1):
+            print("First argument must be the symbol type to download:")
+            for key in list(options.keys()):
+                print( " " + key )
+            print("Example: YahooTickerDownloader.py stocks")
+            exit(1)
+        else:
+            downloader = options[sys.argv[1]]
+
+    try:
+        if not downloader.isDone():
+            print("Downloading " + downloader.type)
+            print("")
+            downloadEverything(downloader)
+            print ("Saving downloader to disk...")
+            saveDownloader(downloader)
+            print ("Downloader successfully saved.")
+            print ("")
+        else:
+            print("The downloader has already finished downloading everything")
+            print("")
+
+    except Exception as ex:
+        print("A exception occurred while downloading. Suspending downloader to disk")
+        saveDownloader(downloader)
+        print("Successfully saved download state")
+        print("Remove downloader.pickle if this error persists")
+        print("Issues can be reported on https://github.com/Benny-/Yahoo-ticker-symbol-downloader/issues")
+        print("")
+        raise
+    except KeyboardInterrupt as ex:
+        print("Suspending downloader to disk")
+        saveDownloader(downloader)
+
+    if downloader.isDone():
+        print("Exporting "+downloader.type+" symbols")
+
+        data = tablib.Dataset()
+        data.headers = downloader.getRowHeader()
+
+        # This piece is a workaround for old saved downloader.pickle's
+        # who still have bs4.element.NavigableString classes pickled.
+        # It can be removed in next release.
+        for symbol in downloader.getCollectedSymbols():
+            row = symbol.getRow()
+            for i, cell in enumerate(row):
+                if cell is None:
+                    row[i] = ""
+                if isinstance(cell, bs4.element.NavigableString):
+                    row[i] = unicode(cell)
+
+            data.append(row)
+
+        with open(downloader.type + '.csv', 'w') as f:
+            f.write(data.csv)
+
+        with open(downloader.type + '.json', 'w') as f:
+            f.write(data.json)
+
+        with open(downloader.type + '.yaml', 'w') as f:
+            f.write(data.yaml)
+
+        with open(downloader.type + '.xls', 'wb') as f:
+            f.write(data.xls)
+
+if __name__ == "__main__":
+    main()
---
+++ packages/Yahoo-ticker-downloader/Yahoo-ticker-downloader-0.4.1.tar.gz/Yahoo-ticker-downloader-0.4.1/ytd/SymbolDownloader.py
@@ -0,0 +1,119 @@
+import requests
+import string
+
+from bs4 import BeautifulSoup
+
+class SymbolDownloader:
+    """Abstract class"""
+
+    def __init__(self, type):
+        # All downloaded symbols are stored in a dict before exporting
+        # This is to unsure no duplicate data
+        self.symbols = {}
+        self.rsession = requests.Session()
+        self.type = type
+
+        self.queries = string.ascii_lowercase;
+        self.current_q = self.queries[0]
+        self.current_q_item_offset = 0
+        self.current_q_total_items = 'Unknown'  # This field is normally a int
+        self.current_page_retries = 0
+        self.done = False
+
+    def _fetchHtml(self):
+        query_string = {
+                's': self.current_q,
+                't': self.type[0],
+                'm': 'ALL',
+                'r': '',
+                'b': str(self.current_q_item_offset)
+            }
+        user_agent = {'User-agent': 'yahoo-ticker-symbol-downloader'}
+        req = requests.Request('GET', 'https://finance.yahoo.com/lookup/',
+                headers=user_agent, params=query_string)
+        req = req.prepare()
+        print("req " + req.url) # Used for debugging
+        resp = self.rsession.send(req)
+        resp.raise_for_status()
+
+        return resp.text
+
+    def decodeSymbolsContainer(self, symbolsContainer):
+        raise Exception("Function to extract symbols must be overwritten in subclass. Generic symbol downloader does not know how.")
+
+    def _getQueryIndex(self):
+        return self.queries.index(self.current_q)
+
+    def getTotalQueries(self):
+        return len(self.queries)
+
+    def _getTotalItemsFromSoup(self, soup):
+        total_items = None
+        try:
+            div = soup.find(id="pagination")
+            yikkes = str(div).split("of")[1].split("|")[0]
+            yikkes = "".join([char for char in yikkes if char in string.digits])
+            total_items = int(yikkes)
+        except Exception as ex:
+            total_items = 'Unknown'
+        return total_items
+
+    def _nextQuery(self):
+        self.current_page_retries = 0
+        self.current_q_item_offset = 0
+        self.current_q_total_items = 'Unknown'
+
+        if self._getQueryIndex() + 1 >= len(self.queries):
+            self.current_q = self.queries[0]
+            self.done = True
+        else:
+            self.current_q = self.queries[self._getQueryIndex() + 1]
+
+    def nextRequest(self):
+        html = self._fetchHtml()
+        soup = BeautifulSoup(html)
+        symbols = None
+
+        try:
+            # A exception is thrown here for the following reasons:
+            # 1. Yahoo does not include a table (or any results!) if you
+            #    request items at offset 2020 or more
+            # 2. Yahoo randomly screws a http request up and table is missing.
+            #    A succesive http request might not result in a exception here.
+            symbolsContainer = soup.find("table", {"class": "yui-dt"}).tbody
+            symbols = self.decodeSymbolsContainer(symbolsContainer)
+        except:
+            symbols = []
+
+        for symbol in symbols:
+            self.symbols[symbol.ticker] = symbol
+
+        self.current_q_item_offset = self.current_q_item_offset + len(symbols)
+        self.current_q_total_items = self._getTotalItemsFromSoup(soup)
+        if len(symbols) == 0:
+            self.current_page_retries += 1
+            if self.current_page_retries > 20:
+                self._nextQuery()
+        else:
+            self.current_page_retries = 0
+        return symbols
+
+    def isDone(self):
+        return self.done
+
+    def getCollectedSymbols(self):
+        return self.symbols.values()
+
+    def getRowHeader(self):
+        return ["Ticker", "Name", "Exchange"]
+
+    def printProgress(self):
+        if self.isDone():
+            print("Progress: Done!")
+        else:
+            print("Progress:" +
+                " Query " + str(self._getQueryIndex()) + "/" + str(self.getTotalQueries()) + "."
+                " Items handled in current query: " + str(self.current_q_item_offset) + "/" + str(self.current_q_total_items) + "."
+                " Total collected unique " + self.type + " entries: " + str(len(self.symbols))
+                )
+        print ("")
