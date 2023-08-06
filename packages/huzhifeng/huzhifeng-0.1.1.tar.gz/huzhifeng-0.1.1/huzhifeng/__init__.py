89436b67e2151a2be749cc72baec4ba9104e04be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:19 +0000

    Add package huzhifeng/0.1.1/huzhifeng-0.1.1.tar.gz

---
+++ packages/huzhifeng/huzhifeng-0.1.1.tar.gz/huzhifeng-0.1.1/huzhifeng/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python                                         
+# Copyright 2014 huzhifeng <hu_zhifeng@qq.com>  
+from lib import *
---
+++ packages/huzhifeng/huzhifeng-0.1.1.tar.gz/huzhifeng-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'huzhifeng',
+    packages = ['huzhifeng'], 
+    version = '0.1.1',
+    description = 'My private Python API Wrapper',
+    author = 'Zhifeng Hu',
+    author_email = 'hu_zhifeng@qq.com',
+    url = 'https://github.com/huzhifeng/pyhuzhifeng',
+    download_url = 'https://github.com/huzhifeng/pyhuzhifeng/tarball/0.1.0',
+    keywords = ['huzhifeng', 'api', 'lib'], 
+    install_requires = ['requests', 'wget'], 
+    classifiers = [],  
+)
