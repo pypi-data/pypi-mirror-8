0ceba0a918c32652722cf3197ff64ca1d8959855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:45 +0000

    Add package collective.prettydate/1.2.2/collective.prettydate-1.2.2.zip

---
+++ packages/collective.prettydate/collective.prettydate-1.2.2.zip/collective.prettydate-1.2.2/setup.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+version = '1.2.2'
+description = "Represents a date on a relative format so, instead of \
+'01/02/2012', it would be displayed as '4 hours ago', 'yesterday' or 'last \
+week', which is easier to read and understand for most people."
+long_description = open("README.txt").read() + "\n" + \
+    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+    open(os.path.join("docs", "HISTORY.txt")).read()
+
+setup(name='collective.prettydate',
+      version=version,
+      description=description,
+      long_description=long_description,
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Office/Business :: News/Diary",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='plone prettydate datetime',
+      author='Franco Pellegrini',
+      author_email='frapell@gmail.com',
+      url='https://github.com/collective/collective.prettydate',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.i18nmessageid',
+          'zope.interface',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+              'unittest2',
+              'zope.component',
+          ],
+      },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
