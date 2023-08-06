a7577fdbe49a00d7c2b1307eb6a6ff8b786fd72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:26 +0000

    Add package ukmodulus/0.1/ukmodulus-0.1.tar.gz

---
+++ packages/ukmodulus/ukmodulus-0.1.tar.gz/ukmodulus-0.1/ukmodulus/__init__.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+"""
+Runs the checks detailed in VocaLinks Document vocalink_-_validating_account_numbers_v3.10.pdf
+Available at http://www.vocalink.com/products/payments/customer-support-services/modulus-checking.aspx
+
+TODO Currently doesn't deal with account numbers which are not 8 digits long.
+"""
+
+__title__ = 'ukmodulus'
+__version__ = '0.0.1'
+__author__ = 'Andrew Barrett'
+__license__ = 'MIT'
+
+
+import os
+import re
+from collections import namedtuple
+
+
+ModulusWeight = namedtuple('ModulusWeight',
+                           ['range_start',
+                            'range_end',
+                            'algorithm',
+                            'weightings',
+                            'exception_code'])
+
+POSITION_U = 0
+POSITION_V = 1
+POSITION_W = 2
+POSITION_X = 3
+POSITION_Y = 4
+POSITION_Z = 5
+POSITION_A = 6
+POSITION_B = 7
+POSITION_C = 8
+POSITION_D = 9
+POSITION_E = 10
+POSITION_F = 11
+POSITION_G = 12
+POSITION_H = 13
+
+DBLAL = 'DBLAL'
+MOD10 = 'MOD10'
+MOD11 = 'MOD11'
+
+_sc_subs = {}
+_weightings = []
+_mod_values = {
+    DBLAL: 10,
+    MOD10: 10,
+    MOD11: 11,
+}
+
+
+def _clean_input(number, required_length=None):
+    number = re.sub('[-\s]', '', number)
+    if re.match('^\d+$', number) is None:
+        raise ValueError
+    if required_length is not None and len(number) != required_length:
+        raise ValueError
+    return number
+
+
+def _get_weightings(sort_code):
+    sort_code = int(sort_code)
+    weightings = []
+    for w in _weightings:
+        if w.range_start > sort_code:
+            break
+        if w.range_start <= sort_code <= w.range_end:
+            weightings.append(w)
+    return weightings
+
+
+def _combine_sort_and_account(sort_code, account_number, weighting):
+    if weighting.exception_code == '5' and sort_code in _sc_subs:
+        sort_code = _sc_subs[sort_code]
+    elif weighting.exception_code == '8':
+        sort_code = '090126'
+    elif weighting.exception_code == '9':
+        sort_code = '309634'
+    combined = sort_code + account_number
+    return combined
+
+
+def _skip_check(weighting, combined):
+    if weighting.exception_code == '3':
+        if combined[POSITION_C] == '6' or combined[POSITION_C] == '9':
+            return True
+    elif weighting.exception_code == '6':
+        if combined[POSITION_A] in '45678' and combined[POSITION_G] == combined[POSITION_H]:
+            return True
+    return False
+
+
+def _get_weighting_to_use(weighting, combined):
+    weightings = weighting.weightings
+    if weighting.exception_code == '2':
+        if combined[POSITION_A] != '0' and combined[POSITION_G] != '9':
+            weightings = [0, 0, 1, 2, 5, 3, 6, 4, 8, 7, 10, 9, 3, 1]
+        elif combined[POSITION_A] != '0' and combined[POSITION_G] == '9':
+            weightings = [0, 0, 0, 0, 0, 0, 0, 0, 8, 7, 10, 9, 3, 1]
+    elif weighting.exception_code == '7':
+        if combined[POSITION_G] == '9':
+            weightings = list(weightings)
+            for ix in range(POSITION_B + 1):
+                weightings[ix] = 0
+    elif weighting.exception_code == '10':
+        ab = combined[POSITION_A] + combined[POSITION_B]
+        if ab in ('09', '99') and combined[POSITION_G] == '9':
+            weightings = list(weightings)
+            for ix in range(POSITION_B + 1):
+                weightings[ix] = 0
+    return weightings
+
+
+def _exception_5_remainder_check(algorithm, combined, remainder):
+    if algorithm == DBLAL:
+        if remainder == 0 and combined[POSITION_H] == '0':
+            return True
+        else:
+            check = 10 - remainder
+            return check == int(combined[POSITION_H])
+    else:
+        if remainder == 1:
+            return False
+        if remainder == 0 and combined[POSITION_G] == '0':
+            return True
+        check = 11 - remainder
+        return check == int(combined[POSITION_G])
+
+
+def _run_check(sort_code, account_number, weighting):
+    combined = _combine_sort_and_account(sort_code, account_number, weighting)
+    if _skip_check(weighting, combined):
+        return True
+    weightings = _get_weighting_to_use(weighting, combined)
+    total = 0
+    for ix, ch in enumerate(combined):
+        value = int(ch) * weightings[ix]
+        if weighting.algorithm == DBLAL:
+            total += sum(int(x) for x in str(value))
+        else:
+            total += value
+    if weighting.exception_code == '1':
+        total += 27
+    mod_value = _mod_values.get(weighting.algorithm, None)
+    remainder = total % mod_value
+    if weighting.exception_code == '5':
+        return _exception_5_remainder_check(weighting.algorithm, combined, remainder)
+    if weighting.exception_code == '4':
+        checkval = int(combined[POSITION_G] + combined[POSITION_H])
+    else:
+        checkval = 0
+    return remainder == checkval
+
+
+def validate_number(sort_code, account_number):
+    """
+    TODO Deal with non-standard account numbers as listed by section 4.2 in the VocaLink spec
+    """
+    sort_code = _clean_input(sort_code, required_length=6)
+    account_number = _clean_input(account_number, required_length=8)
+    weightings = _get_weightings(sort_code)
+    check_count = len(weightings)
+    if not weightings:
+        return True
+    first_check = weightings[0]
+    if _run_check(sort_code, account_number, first_check):
+        if check_count == 1 or first_check.exception_code in ('2', '9', '10', '11', '12', '13', '14'):
+            return True
+        return _run_check(sort_code, account_number, weightings[1])
+    else:
+        if first_check.exception_code == '14':
+            if account_number[7] not in '019':
+                return False
+            account_number = ('0' + account_number)[:8]
+            return _run_check(sort_code, account_number, weightings[0])
+        if check_count == 1 or first_check.exception_code not in ('2', '9', '10', '11', '12', '13', '14'):
+            return False
+        return _run_check(sort_code, account_number, weightings[1])
+
+
+def _load_weightings():
+    with open(os.path.join(os.path.dirname(__file__), 'valacdos.txt'), 'r') as f:
+        for line in f.readlines():
+            if not line:
+                break
+            split = line.split()
+            column_count = len(split)
+            if column_count not in [17, 18]:
+                raise ValueError
+            weighting = ModulusWeight(int(split[0]),
+                                      int(split[1]),
+                                      split[2],
+                                      [int(x) for x in split[3:17]],
+                                      split[17] if column_count == 18 else None)
+            _weightings.append(weighting)
+
+
+def _load_scsubs():
+    with open(os.path.join(os.path.dirname(__file__), 'scsubtab.txt'), 'r') as f:
+        for line in f.readlines():
+            if not line:
+                break
+            split = line.split()
+            if len(split) != 2:
+                raise ValueError
+            _sc_subs[split[0]] = split[1]
+
+
+_load_weightings()
+_load_scsubs()
---
+++ packages/ukmodulus/ukmodulus-0.1.tar.gz/ukmodulus-0.1/ukmodulus/tests.py
@@ -0,0 +1,222 @@
+import unittest
+# noinspection PyProtectedMember
+from ukmodulus import validate_number, _clean_input, _get_weightings, ModulusWeight, _run_check, _sc_subs
+
+
+class SimpleValidationTests(unittest.TestCase):
+    def test_clean_input(self):
+        self.assertRaises(ValueError, _clean_input, 'invalid')
+        self.assertRaises(ValueError, _clean_input, '12a3456')
+        self.assertRaises(ValueError, _clean_input, '1234567', 6)
+        self.assertEqual(_clean_input('12-34-56'), '123456')
+        self.assertEqual(_clean_input('12 34 56'), '123456')
+
+
+class RunAlgorithmChecks(unittest.TestCase):
+    def test_dbla(self):
+        cases = [
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], None), '499273', '12345678',
+             True),
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], None), '499273', '12345679',
+             False),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_mod10(self):
+        cases = [
+            (ModulusWeight('', '', 'MOD10', [0, 0, 0, 0, 0, 0, 7, 1, 3, 7, 1, 3, 7, 1], None), '080211', '11111111',
+             True),
+            (ModulusWeight('', '', 'MOD10', [0, 0, 0, 0, 0, 0, 7, 1, 3, 7, 1, 3, 7, 1], None), '080211', '11111112',
+             False),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_mod11(self):
+        cases = [
+            (ModulusWeight('', '', 'MOD11', [0, 0, 0, 0, 0, 0, 7, 5, 8, 3, 4, 6, 2, 1], None), '000000', '58177632',
+             True),
+            (ModulusWeight('', '', 'MOD11', [0, 0, 0, 0, 0, 0, 7, 5, 8, 3, 4, 6, 2, 1], None), '000000', '58177633',
+             False),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_exception_1(self):
+        cases = [
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], '1'), '499273', '12345678',
+             False),
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], '1'), '499273', '12345671',
+             True),
+        ]
+
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_exception_3(self):
+        cases = [
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], '3'), '499273', '12645678',
+             True),
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], '3'), '499273', '12945671',
+             True),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_exception_4(self):
+        cases = [
+            (ModulusWeight('', '', 'MOD11', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], '4'), '000000', '00000009',
+             True),
+            (ModulusWeight('', '', 'MOD11', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], '4'), '000000', '00000011',
+             False),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+
+class ValcadosDataLoadingTests(unittest.TestCase):
+    """
+    These test the loading of the valacdos.txt data
+    """
+
+    def test_valid_weightings(self):
+        weightings = _get_weightings('010005')
+        self.assertEqual(len(weightings), 1)
+        self.assertEqual(weightings[0].algorithm, 'MOD11')
+        self.assertEqual(weightings[0].exception_code, None)
+        self.assertEqual(weightings[0].weightings, [0, 0, 0, 0, 0, 0, 8, 7, 6, 5, 4, 3, 2, 1])
+        weightings = _get_weightings('070116')
+        self.assertEqual(len(weightings), 2)
+        self.assertEqual(weightings[0].algorithm, 'MOD11')
+        self.assertEqual(weightings[0].exception_code, '12')
+        self.assertEqual(weightings[1].algorithm, 'MOD10')
+        weightings = _get_weightings('989999')
+        self.assertEqual(len(weightings), 2)
+        self.assertEqual(weightings[0].algorithm, 'MOD11')
+        self.assertEqual(weightings[1].algorithm, 'DBLAL')
+
+    def test_invalid_weightings(self):
+        weightings = _get_weightings('010003')
+        self.assertEqual(len(weightings), 0)
+        weightings = _get_weightings('049999')
+        self.assertEqual(len(weightings), 0)
+        weightings = _get_weightings('999999')
+        self.assertEqual(len(weightings), 0)
+
+    def test_scsubs(self):
+        self.assertEqual(_sc_subs.get('938173', None), '938017')
+        self.assertEqual(_sc_subs.get('938654', None), '938621')
+        self.assertEqual(_sc_subs.get('838654', None), None)
+
+
+class ValacdosTests(unittest.TestCase):
+    """
+    These are the test cases listed in the Vocalink documentation (v3.10)
+    """
+
+    def test_case_no1(self):
+        self.assertEqual(validate_number('089999', '66374958'), True)
+
+    def test_case_no2(self):
+        self.assertEqual(validate_number('107999', '88837491'), True)
+
+    def test_case_no3(self):
+        self.assertEqual(validate_number('202959', '63748472'), True)
+
+    def test_case_no4(self):
+        self.assertEqual(validate_number('871427', '46238510'), True)
+
+    def test_case_no5(self):
+        self.assertEqual(validate_number('872427', '46238510'), True)
+
+    def test_case_no6(self):
+        self.assertEqual(validate_number('871427', '09123496'), True)
+
+    def test_case_no7(self):
+        self.assertEqual(validate_number('871427', '99123496'), True)
+
+    def test_case_no8(self):
+        self.assertEqual(validate_number('820000', '73688637'), True)
+
+    def test_case_no9(self):
+        self.assertEqual(validate_number('827999', '73988638'), True)
+
+    def test_case_no10(self):
+        self.assertEqual(validate_number('827101', '28748352'), True)
+
+    def test_case_no11(self):
+        self.assertEqual(validate_number('134020', '63849203'), True)
+
+    def test_case_no12(self):
+        self.assertEqual(validate_number('118765', '64371389'), True)
+
+    def test_case_no13(self):
+        self.assertEqual(validate_number('200915', '41011166'), True)
+
+    def test_case_no14(self):
+        self.assertEqual(validate_number('938611', '07806039'), True)
+
+    def test_case_no15(self):
+        self.assertEqual(validate_number('938600', '42368003'), True)
+
+    def test_case_no16(self):
+        self.assertEqual(validate_number('938063', '55065200'), True)
+
+    def test_case_no17(self):
+        self.assertEqual(validate_number('772798', '99345694'), True)
+
+    def test_case_no18(self):
+        self.assertEqual(validate_number('086090', '06774744'), True)
+
+    def test_case_no19(self):
+        self.assertEqual(validate_number('309070', '02355688'), True)
+
+    def test_case_no20(self):
+        self.assertEqual(validate_number('309070', '12345668'), True)
+
+    def test_case_no21(self):
+        self.assertEqual(validate_number('309070', '12345677'), True)
+
+    def test_case_no22(self):
+        self.assertEqual(validate_number('309070', '99345694'), True)
+
+    def test_case_no23(self):
+        self.assertEqual(validate_number('938063', '15764273'), False)
+
+    def test_case_no24(self):
+        self.assertEqual(validate_number('938063', '15764264'), False)
+
+    def test_case_no25(self):
+        self.assertEqual(validate_number('938063', '15763217'), False)
+
+    def test_case_no26(self):
+        self.assertEqual(validate_number('118765', '64371388'), False)
+
+    def test_case_no27(self):
+        self.assertEqual(validate_number('203099', '66831036'), False)
+
+    def test_case_no28(self):
+        self.assertEqual(validate_number('203099', '58716970'), False)
+
+    def test_case_no29(self):
+        self.assertEqual(validate_number('089999', '66374959'), False)
+
+    def test_case_no30(self):
+        self.assertEqual(validate_number('107999', '88837493'), False)
+
+    def test_case_no31(self):
+        self.assertEqual(validate_number('074456', '12345112'), True)
+
+    def test_case_no32(self):
+        self.assertEqual(validate_number('070116', '34012583'), True)
+
+    def test_case_no33(self):
+        self.assertEqual(validate_number('074456', '11104102'), True)
+
+    def test_case_no34(self):
+        self.assertEqual(validate_number('180002', '00000190'), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
