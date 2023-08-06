4cafdf7dba96e1ea281d26c64824983e18264d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:02 +0000

    Add package massweb/0.2.0/massweb-0.2.0.tar.gz

---
+++ packages/massweb/massweb-0.2.0.tar.gz/massweb-0.2.0/massweb/targets/fuzzy_target.py
@@ -0,0 +1,31 @@
+from massweb.payloads.payload import Payload
+from massweb.targets.target import Target
+
+class FuzzyTarget(Target):
+
+    def __eq__(self, other):
+        return self.url == other.url and self.payload == other.payload and self.ttype == other.ttype
+
+    def __hash__(self):
+        return hash((self.url, self.payload, self.ttype))
+
+    def __init__(self, url, fuzzy_param, ttype = "get", data = None, payload = None):
+
+        if type(payload) != Payload:
+            raise Exception("payload must be of type Payload")
+
+        super(FuzzyTarget, self).__init__(url, data = data, ttype = ttype)
+        self.payload = payload
+        self.fuzzy_param = fuzzy_param
+
+if __name__ == "__main__":
+
+    ft1 = FuzzyTarget(u"ddd", "dd", payload = Payload("ddd", check_type_list = ["ddd"]))
+    ft2 = FuzzyTarget(u"ddd", "dd", payload = Payload("ddd", check_type_list = ["ddd"]))
+
+    if ft1 == ft2:
+        print "equal"
+
+    print ft1.payload
+    print ft1.url
+    print ft1
