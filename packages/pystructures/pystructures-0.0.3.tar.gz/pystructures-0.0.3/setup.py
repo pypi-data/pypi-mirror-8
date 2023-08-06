984a1a4d7f049c63f4a4335140aa9762d358a451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:10 +0000

    Add package pystructures/0.0.3/pystructures-0.0.3.tar.gz

---
+++ packages/pystructures/pystructures-0.0.3.tar.gz/pystructures-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pystructures',
+      version='0.0.3',
+      description='Data Structures made simple',
+      url='http://github.com/apranav19/pystructures',
+      author='Pranav Angara',
+      author_email='apranav19@gmail.com',
+      license='MIT',
+      packages=['pystructures'],
+      install_requires=['future'],
+      zip_safe=False)
