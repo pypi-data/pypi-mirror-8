8b70d1ef38a26f31be0fb45f47eb453998916c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:04 +0000

    Add package easyimap/0.6.0/easyimap-0.6.0.zip

---
+++ packages/easyimap/easyimap-0.6.0.zip/easyimap-0.6.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+from sys import version
+if version < '2.6.0':
+    raise Exception("This module doesn't support any version less than 2.6")
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    "Programming Language :: Python",
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+requires = ['chardet>=2.3.0']
+
+setup(
+    author='Keita Oouchi',
+    author_email='keita.oouchi@gmail.com',
+    url='https://github.com/keitaoouchi/easyimap',
+    name='easyimap',
+    version='0.6.0',
+    package_dir={"easyimap": "easyimap"},
+    packages=['easyimap'],
+    license='BSD License',
+    classifiers=classifiers,
+    description='Simple imap wrapper.',
+    long_description=long_description,
+    install_requires=requires,
+)
