fbf28f54461b46a29ad7ddf052b4c0b884131c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:02 +0000

    Add package lawyerup/0.1.4/lawyerup-0.1.4-py2.py3-none-any.whl

---
+++ packages/lawyerup/lawyerup-0.1.4-py2.py3-none-any.whl/lawyerup/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2013, RedJack, LLC.
+# All rights reserved.
+#
+# Please see the COPYING file in this distribution for license details.
+__author__ = 'Andy Freeland'
+__email__ = 'andy.freeland@redjack.com'
+__version__ = '0.1.4'
