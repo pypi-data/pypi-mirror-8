654da93fb3c7ad61fafefb1d943b04167ba92640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:55 +0000

    Add package djangohelpers/0.18.1/djangohelpers-0.18.1.tar.gz

---
+++ packages/djangohelpers/djangohelpers-0.18.1.tar.gz/djangohelpers-0.18.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.18.1'
+
+long_description = open('README.txt').read()
+
+setup(name='djangohelpers',
+      version=version,
+      description="a collection of useful middleware, template tags, etc",
+      long_description=long_description,
+      classifiers=[], 
+      keywords='',
+      author='Ethan Jucovy',
+      author_email='ejucovy@gmail.com',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+      ],
+      entry_points="""
+      [console_scripts]
+      maketemplatetag = djangohelpers.console_scripts:main
+      """,
+      )
