5a09599697ef0611c65d618f4cfcd29961c6a42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:13 +0000

    Add package limeclient/0.0.3/limeclient-0.0.3.zip

---
+++ packages/limeclient/limeclient-0.0.3.zip/limeclient-0.0.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='limeclient',
+    version='0.0.3',
+    description='Python client for the LIME Pro API',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 3'
+    ],
+    author='Lundalogik AB',
+    author_email='info@lundalogik.se',
+    license='MPL 2.0',
+
+    packages=['limeclient'],
+    include_package_data=True,
+
+    install_requires=['requests>=2.4.3']
+)
