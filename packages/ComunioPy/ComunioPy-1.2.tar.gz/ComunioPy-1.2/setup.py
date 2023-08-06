38f8439062ada82866c6833929e32178b29f1354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:30 +0000

    Add package ComunioPy/1.2/ComunioPy-1.2.tar.gz

---
+++ packages/ComunioPy/ComunioPy-1.2.tar.gz/ComunioPy-1.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = 'ComunioPy',
+    packages = ['ComunioPy'],
+    version = '1.2',
+    description = 'API for comunio',
+    license = 'MIT',
+    author = 'Javier Corbín',
+    author_email = 'javi.corbin@gmail.com',
+    url = 'https://github.com/jotacor/ComunioPy',
+    download_url = 'https://github.com/jotacor/ComunioPy/archive/v1.2.tar.gz', 
+    keywords = ['comunio', 'API'],
+    classifiers = [],
+    long_description = 'Scraping of the webpage Comunio'
+)
