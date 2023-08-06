a426c2772b27bd6cc925dc5fe0a35db9706168d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:00 +0000

    Add package senpy/0.2.6/senpy-0.2.6.tar.gz

---
+++ packages/senpy/senpy-0.2.6.tar.gz/senpy-0.2.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import senpy
+
+setup(
+  name = 'senpy',
+  packages = ['senpy'], # this must be the same as the name above
+  version = senpy.VERSION,
+  description = '''
+    A sentiment analysis server implementation. Designed to be \
+extendable, so new algorithms and sources can be used.
+    ''',
+  author = 'J. Fernando Sanchez',
+  author_email = 'balkian@gmail.com',
+  url = 'https://github.com/balkian/senpy', # use the URL to the github repo
+  download_url = 'https://github.com/balkian/senpy/archive/{}.tar.gz'.format(senpy.VERSION),
+  keywords = ['eurosentiment', 'sentiment', 'emotions', 'nif'], # arbitrary keywords
+  classifiers = [],
+)
