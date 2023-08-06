3abe54527390be2a0aa3cead72787f40bc16ab54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:00 +0000

    Add package fptest/0.0.1/fptest-0.0.1.tar.gz

---
+++ packages/fptest/fptest-0.0.1.tar.gz/fptest-0.0.1/setup.py
@@ -0,0 +1,47 @@
+from os import path
+from codecs import open
+
+from setuptools import setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="fptest",
+    version='0.0.1',
+    description='A TIBCO Fulfilment Provisioning test helper',
+    # long_description=long_description,
+    packages=['fptest'],
+    url='https://github.com/oxo42/FpTest',
+    author='John Oxley',
+    author_email='john.oxley@gmail.com',
+    license='Apache',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        # 3 - Alpha
+        # 4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    requires=['lxml', 'requests']
+)
