2665ac12a2f69c939c8986921638e017727d5654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:09 +0000

    Add package empowering/0.9.0/empowering-0.9.0.tar.gz

---
+++ packages/empowering/empowering-0.9.0.tar.gz/empowering-0.9.0/empowering/results.py
@@ -0,0 +1,27 @@
+from empowering.service import EmpoweringResource
+
+
+class OT101Results(EmpoweringResource):
+    path = 'OT101Results'
+
+
+class OT103Results(EmpoweringResource):
+    path = 'OT103Results'
+
+
+class OT106Results(EmpoweringResource):
+    path = 'OT106Results'
+
+
+class OT201Results(EmpoweringResource):
+    path = 'OT201Results'
+
+
+class OT204Results(EmpoweringResource):
+    path = 'OT204Results'
+
+class OT503Results(EmpoweringResource):
+    path = 'OT503Results'
+
+class BT111Results(EmpoweringResource):
+    path = 'BT111Results'
