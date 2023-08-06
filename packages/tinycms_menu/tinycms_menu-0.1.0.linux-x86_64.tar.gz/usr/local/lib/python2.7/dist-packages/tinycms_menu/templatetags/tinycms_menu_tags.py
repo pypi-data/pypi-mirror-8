dfdf7a25b29a4bedff5c7894234bad9b966ddf86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:09 +0000

    Add package tinycms_menu/0.1.0/tinycms_menu-0.1.0.linux-x86_64.tar.gz

---
+++ packages/tinycms_menu/tinycms_menu-0.1.0.linux-x86_64.tar.gz/usr/local/lib/python2.7/dist-packages/tinycms_menu/templatetags/tinycms_menu_tags.py
@@ -0,0 +1,54 @@
+from django import template
+from django.utils import translation
+from tinycms.models import *
+from tinycms_menu.models import *
+
+register = template.Library()
+
+def internal_create_children_menu(item,sublevel,lang,tag):
+    result =""
+    menus = MenuItem.objects.filter(page=item,language=lang)
+    if(menus.count()!=0):
+        result = result + "<li><a href='"+item.get_absolute_url()+"'>"+menus[0].title+"</a>"
+        if(sublevel>0):
+            if(item.get_children().count()>0):
+                result = result +"<"+tag+">"+ create_children_menu(item,sublevel-1,lang,tag) +"</"+tag+">"
+        result = result+"</li>"
+    return result
+
+
+def create_children_menu(parent,sublevel,lang,tag):
+    result =""
+    for item in parent.get_children():
+        result = result + internal_create_children_menu(item,sublevel,lang,tag)
+    return result
+
+
+@register.simple_tag(takes_context=True)
+def show_absolute_menu(context, sublevel = 0, tag="ul"):
+    """Show absolute menu.
+
+    """
+    page_roots = Page.objects.root_nodes()
+    result =""
+
+    lang = translation.get_language()
+
+    for item in page_roots:
+        result = result + internal_create_children_menu(item,sublevel,lang,tag)
+    return result
+
+
+@register.simple_tag(takes_context=True)
+def show_submenu(context, sublevel = 0, tag="ul"):
+    """Show sub menu.
+
+    """
+    current = context["page"]
+
+    lang = translation.get_language()
+
+    result = internal_create_children_menu(current,sublevel,lang,tag)
+    return result
+
+
