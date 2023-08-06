694df05991a811e74caa23b89267343d1b164d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:01 +0000

    Add package nesterhoverqjl/1.0.0/nesterhoverqjl-1.0.0.zip

---
+++ packages/nesterhoverqjl/nesterhoverqjl-1.0.0.zip/nesterhoverqjl-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nesterhoverqjl',
+    version='1.0.0',
+    py_modles=['nesterhoverqjl'],
+    author='leo',
+    author_email='xx@qq.com',
+    url='http://www.xx.com',
+    description='显示循环',
+    )
