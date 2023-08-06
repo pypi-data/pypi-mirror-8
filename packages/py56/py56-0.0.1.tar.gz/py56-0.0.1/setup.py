c91e6d13b8c68b8ab6da784fd95b6e09145c61de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:09 +0000

    Add package py56/0.0.1/py56-0.0.1.tar.gz

---
+++ packages/py56/py56-0.0.1.tar.gz/py56-0.0.1/setup.py
@@ -0,0 +1,12 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'py56',
+    version      = '0.0.1',
+    author       = 'Allan Sun',
+    author_email = 'alnsun.cn@gmail.com',
+    url          = 'https://pythoner.cn',
+    description  = 'Python module for 56 (56Video)',
+    packages     = find_packages()
+)
