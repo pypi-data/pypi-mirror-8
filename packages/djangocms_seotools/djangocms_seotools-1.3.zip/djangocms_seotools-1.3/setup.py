49f1a09b41ea87dccdc673c9b352ec0b904eb5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:52 +0000

    Add package djangocms_seotools/1.3/djangocms_seotools-1.3.zip

---
+++ packages/djangocms_seotools/djangocms_seotools-1.3.zip/djangocms_seotools-1.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Nikolay Dolganov"
+__email__ = "SirNikolasd@yandex.ru"
+
+from setuptools import setup
+from pip.req import parse_requirements
+import os
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+install_reqs = parse_requirements(os.path.join(CURRENT_DIR, 'requirements.txt'))
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='djangocms_seotools',
+    version='1.3',
+    description='SEO and Custom tools for the Django CMS',
+    author='Nikolay <Werevolff> Dolganov',
+    author_email='sirnikolasd@yandex.ru',
+    url='https://github.com/werevolff/django_cms_seotools',
+    packages=['djangocms_seotools', 'djangocms_seotools.tests'],
+    data_files=[('Requirements', ['requirements.txt'])],
+    install_requires=reqs
+)
