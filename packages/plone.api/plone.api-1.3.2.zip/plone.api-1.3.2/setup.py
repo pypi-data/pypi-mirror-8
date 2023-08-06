9c483f3c38fc3b231b72f3d391862558ddd3a96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:22 +0000

    Add package plone.api/1.3.2/plone.api-1.3.2.zip

---
+++ packages/plone.api/plone.api-1.3.2.zip/plone.api-1.3.2/setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = \
+    read('README.rst') + \
+    read('docs', 'CHANGES.rst') + \
+    read('docs', 'LICENSE.txt')
+
+version = '1.3.2'
+
+setup(
+    name='plone.api',
+    version=version,
+    description='A Plone API.',
+    long_description=long_description,
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    license='GPL version 2',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Plone',
+        'decorator',
+    ],
+    extras_require={
+        'develop': [
+            'coverage',
+            'flake8',
+            'jarn.mkrelease',
+            'manuel',
+            'Sphinx',
+            'zest.releaser',
+        ],
+        'test': [
+            'manuel',
+            'mock',
+            'plone.app.dexterity',
+            'plone.app.testing',
+            'unittest2',
+        ],
+    },
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 4.3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    platforms='Any',
+)
