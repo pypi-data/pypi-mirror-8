5886d0d41c6ec8bdcfcc4e262fa47873823482b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package pymemcache/1.2.6/pymemcache-1.2.6.tar.gz

---
+++ packages/pymemcache/pymemcache-1.2.6.tar.gz/pymemcache-1.2.6/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from pymemcache import __version__
+
+setup(
+    name = 'pymemcache',
+    version = __version__,
+    author = 'Charles Gordon',
+    author_email = 'charles@pinterest.com',
+    packages = find_packages(),
+    setup_requires = ['nose>=1.0'],
+    install_requires = ['six'],
+    description = 'A comprehensive, fast, pure Python memcached client',
+    long_description = open('README.md').read(),
+    license = 'Apache License 2.0',
+    url = 'https://github.com/Pinterest/pymemcache',
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Database',
+    ],
+)
+
