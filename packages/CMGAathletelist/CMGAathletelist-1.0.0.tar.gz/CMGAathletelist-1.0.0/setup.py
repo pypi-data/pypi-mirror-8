80303e107206c463cc07bc4a2486abdabb0d324b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:36 +0000

    Add package CMGAathletelist/1.0.0/CMGAathletelist-1.0.0.tar.gz

---
+++ packages/CMGAathletelist/CMGAathletelist-1.0.0.tar.gz/CMGAathletelist-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			= 'CMGAathletelist',
+		version 		= '1.0.0',
+		py_modules 		= ['AthleteList'],
+		author 			= '@CaioMGA',
+		author_email 	        = 'caiomga@gmail.com',
+		url 			= 'http://caiomga.tumblr.com/',
+		description 	        = 'HeadFirst AthleteList class and functions',
+	)
