7e0e3aa2fda09aa5510258d440e4ee2d26cde503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:04 +0000

    Add package autosign/1.0.0/autosign-1.0.0-py2.7.egg

---
+++ packages/autosign/autosign-1.0.0-py2.7.egg/autosign/parse/parent_parser.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+
+##
+# autosign
+# https://github.com/leosartaj/autosign.git
+# 
+# copyright (c) 2014 sartaj singh
+# licensed under the mit license.
+##
+
+"""
+Contains helper functions for parsing arguments
+"""
+
+import argparse # parsing the options
+
+try:
+    from autosign import __desc__ # try to get version number
+except ImportError:
+    __desc__ = 'UNKNOWN'
+
+def gen_parent_parser():
+    """
+    Parses the arguments
+    """
+    parser = argparse.ArgumentParser(add_help=False)
+
+    help = "Current version of autosign"
+    parser.add_argument('--version', '-v',  action='version', help=help, version=__desc__)
+
+    help = "For Verbose Output."
+    parser.add_argument('--verbose',  action='store_true', help=help, dest='verbose')
+
+    help = "For recursive operation. Defaults to False"
+    parser.add_argument('--recursive', '-r',  action='store_true', help=help, dest='recursive')
+
+    return parser
