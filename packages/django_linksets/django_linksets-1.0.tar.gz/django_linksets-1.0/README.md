314edb83b3005217e36e7c68486d08eb8c31d005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:46 +0000

    Add package django_linksets/1.0/django_linksets-1.0.tar.gz

---
+++ packages/django_linksets/django_linksets-1.0.tar.gz/django_linksets-1.0/README.md
@@ -0,0 +1,18 @@
+## Installation
+
+pip install git+ssh://git@github.com:gethee2anunnery/django-linksets.git#egg=django_linksets
+python manage.py syncdb --migrate
+
+
+## Configuration
+
+
+At minimum you will need to implementent:
+
+* admin.py
+* models.py
+* utils.py
+* templatetags/links.py
+
+
+
---
+++ packages/django_linksets/django_linksets-1.0.tar.gz/django_linksets-1.0/linksets/utils.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.template.defaultfilters import slugify
+
+
+def unique_slugify(instance, value, slug_field_name='slug', queryset=None,
+                   slug_separator='-'):
+    """
+    Calculates and stores a unique slug of ``value`` for an instance.
+
+    ``slug_field_name`` should be a string matching the name of the field to
+    store the slug in (and the field to check against for uniqueness).
+
+    ``queryset`` usually doesn't need to be explicitly provided - it'll default
+    to using the ``.all()`` queryset from the model's default manager.
+    """
+    slug_field = instance._meta.get_field(slug_field_name)
+
+    slug = getattr(instance, slug_field.attname)
+    slug_len = slug_field.max_length
+
+    # Sort out the initial slug, limiting its length if necessary.
+    slug = slugify(value)
+    if slug_len:
+        slug = slug[:slug_len]
+    slug = _slug_strip(slug, slug_separator)
+    original_slug = slug
+
+    # Create the queryset if one wasn't explicitly provided and exclude the
+    # current instance from the queryset.
+    if queryset is None:
+        queryset = instance.__class__._default_manager.all()
+    if instance.pk:
+        queryset = queryset.exclude(pk=instance.pk)
+
+    # Find a unique slug. If one matches, at '-2' to the end and try again
+    # (then '-3', etc).
+    next = 2
+    while not slug or queryset.filter(**{slug_field_name: slug}):
+        slug = original_slug
+        end = '%s%s' % (slug_separator, next)
+        if slug_len and len(slug) + len(end) > slug_len:
+            slug = slug[:slug_len-len(end)]
+            slug = _slug_strip(slug, slug_separator)
+        slug = '%s%s' % (slug, end)
+        next += 1
+
+    setattr(instance, slug_field.attname, slug)
