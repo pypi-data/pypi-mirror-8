12d7480016b31cfb601853d4ec520f6f45afae03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:03 +0000

    Add package unittest-xml-reporting/1.10.0/unittest-xml-reporting-1.10.0.tar.gz

---
+++ packages/unittest-xml-reporting/unittest-xml-reporting-1.10.0.tar.gz/unittest-xml-reporting-1.10.0/src/xmlrunner/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module provides the XMLTestRunner class, which is heavily based on the
+default TextTestRunner.
+"""
+
+# Allow version to be detected at runtime.
+from .version import __version__
+
+from .runner import XMLTestRunner
