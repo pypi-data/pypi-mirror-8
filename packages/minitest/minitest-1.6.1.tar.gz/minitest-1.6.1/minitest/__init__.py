32a732f26c82ad7b1e0d662608471a884abc00ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:04 +0000

    Add package minitest/1.6.1/minitest-1.6.1.tar.gz

---
+++ packages/minitest/minitest-1.6.1.tar.gz/minitest-1.6.1/minitest/__init__.py
@@ -0,0 +1,7 @@
+from with_test import test, test_case, get_test_self, inject, inject_customized_must_method
+
+__author__ = 'Colin Ji'
+__versioninfo__ = (1, 6, 1)
+__version__ = '.'.join(map(str, __versioninfo__))
+
+__all__ = ['test', 'test_case', 'get_test_self', 'inject', 'inject_customized_must_method']
