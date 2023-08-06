e14c3e0585dbe494aecc72ea820691f5e90c9d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:12 +0000

    Add package slumber/0.6.1/slumber-0.6.1.tar.gz

---
+++ packages/slumber/slumber-0.6.1.tar.gz/slumber-0.6.1/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+install_requires = ["requests"]
+tests_require = ["mock"]
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name = "slumber",
+    version = "0.6.1",
+    description = "A library that makes consuming a REST API easier and more convenient",
+    long_description="\n\n".join([
+        open(os.path.join(base_dir, "README.rst"), "r").read(),
+        open(os.path.join(base_dir, "CHANGELOG.rst"), "r").read()
+    ]),
+    url = "http://github.com/samgiles/slumber",
+    maintainer = "Samuel Giles",
+    maintainer_email = "sam.e.giles@gmail.com",
+    packages = ["slumber"],
+    zip_safe = False,
+    install_requires = install_requires,
+    tests_require = tests_require,
+    test_suite = "tests.get_tests",
+)
