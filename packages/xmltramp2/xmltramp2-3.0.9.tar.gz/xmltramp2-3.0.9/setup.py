227a29974a299cea3d114afe31c197edcb7f6877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:55 +0000

    Add package xmltramp2/3.0.9/xmltramp2-3.0.9.tar.gz

---
+++ packages/xmltramp2/xmltramp2-3.0.9.tar.gz/xmltramp2-3.0.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='xmltramp2',
+    version='3.0.9',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    description='A modern refactoring of the venerable xmltramp application',
+    long_description=open('README.md').read(),
+    url='https://github.com/tBaxter/xmltramp2',
+    license='GPL',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+          'six',
+    ],
+    classifiers=(
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Text Processing :: Markup :: XML",
+    ),
+)
