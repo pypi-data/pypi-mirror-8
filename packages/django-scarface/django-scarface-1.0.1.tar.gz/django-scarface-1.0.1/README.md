0d40f3105ca3bfd57160cf9515b3119747bb4d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:23 +0000

    Add package django-scarface/1.0.1/django-scarface-1.0.1.tar.gz

---
+++ packages/django-scarface/django-scarface-1.0.1.tar.gz/django-scarface-1.0.1/README.md
@@ -0,0 +1,25 @@
+<img src="docs/logo.png" width="322" alt="django-scarface" />
+-----------
+
+Send push notifications to mobile devices using Amazon SNS
+
+[![Documentation Status](https://readthedocs.org/projects/django-scarface/badge/?version=latest)](https://readthedocs.org/projects/django-scarface/?badge=latest)
+
+Documentation
+-----------
+http://django-scarface.readthedocs.org/
+
+Project Home
+------------
+https://github.com/dreipol/django-scarface
+
+PyPi
+------------
+https://pypi.python.org/pypi/django-scarface
+
+
+Trivia
+------------
+django-scarface is named after the track ‘Push it to the Limit’ featured in the movie Scarface (1983).
+
+![Scarface the movie (1983)](docs/scarface-movie.png "Scarface the movie (1983)")
---
+++ packages/django-scarface/django-scarface-1.0.1.tar.gz/django-scarface-1.0.1/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+reqs =['South>=0.8.4', 'boto>=2.34.0',]
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-scarface',
+    version='1.0.1',
+    packages=['scarface'],
+    include_package_data=True,
+    license='MIT License',
+    description='Send push notifications to mobile devices using Amazon SNS.',
+    long_description=README,
+    url='http://www.dreipol.ch/',
+    author='dreipol GmbH',
+    author_email='dev@dreipol.ch',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', 
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=reqs,
+)
