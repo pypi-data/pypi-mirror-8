c938b81278d77f5eb9159524e7bc4ca264bb3f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package negotiator-guest/0.7/negotiator-guest-0.7.tar.gz

---
+++ packages/negotiator-guest/negotiator-guest-0.7.tar.gz/negotiator-guest-0.7/negotiator_guest/__init__.py
@@ -0,0 +1,150 @@
+# Scriptable KVM/QEMU guest agent in Python.
+#
+# Author: Peter Odding <peter@peterodding.com>
+# Last Change: October 24, 2014
+# URL: https://negotiator.readthedocs.org
+
+"""
+``negotiator_guest`` - The guest agent daemon
+=============================================
+
+This module implements the guest agent, the Python daemon process that's always
+running inside KVM/QEMU guests.
+"""
+
+# Standard library modules.
+import fcntl
+import itertools
+import logging
+import multiprocessing
+import os
+import signal
+import sys
+import time
+
+# External dependencies.
+from humanfriendly import Timer
+
+# Modules included in our project.
+from negotiator_common import NegotiatorInterface
+from negotiator_common.utils import compact
+
+# Semi-standard module versioning.
+__version__ = '0.7'
+
+# Initialize a logger for this module.
+logger = logging.getLogger(__name__)
+
+
+class GuestAgent(NegotiatorInterface):
+
+    """Implementation of the daemon running inside KVM/QEMU guests."""
+
+    def __init__(self, character_device):
+        """
+        Initialize a negotiator guest agent.
+
+        :param character_device: The absolute pathname of the character device
+                                 that we should use to connect to the host (a
+                                 string).
+        """
+        # Initialize the super class, passing it a file like object connected
+        # to the character device in read/write mode.
+        super(GuestAgent, self).__init__(handle=open(character_device, 'r+'),
+                                         label="character device %s" % character_device)
+
+    def raw_readline(self):
+        """
+        Read a newline terminated string from the remote side.
+
+        This method overrides the
+        :py:func:`~negotiator_common.NegotiatorInterface.raw_readline()` method
+        of the :py:func:`~negotiator_common.NegotiatorInterface` class to
+        implement blocking reads based on :py:data:`os.O_ASYNC` and
+        :py:data:`signal.SIGIO` (see also :py:class:`WaitForRead`).
+
+        :returns: The data read from the remote side (a string).
+        """
+        logger.debug("Preparing to read line from %s ..", self.conn_label)
+        data = self.conn_handle.readline()
+        if not data:
+            # If the read returns an empty string the channel is not connected.
+            # At this point we'll bother to prepare a convoluted way to block
+            # until the channel does become connected.
+            logger.debug("Emulating blocking read of %s ..", self.conn_label)
+            # Set the O_ASYNC flag on the file descriptor connected to the
+            # character device (this is required to use SIGIO signals).
+            flags = fcntl.fcntl(self.conn_handle, fcntl.F_GETFL)
+            fcntl.fcntl(self.conn_handle, fcntl.F_SETFL, flags | os.O_ASYNC)
+            # Spawn a subprocess to reliably handle SIGIO signals. Due to the
+            # nature of (SIGIO) signals more than one signal may be delivered
+            # and this is a big problem when you want to do more than just call
+            # sys.exit(). The alternative to this would be signal.alarm() but
+            # that function has an inherent race condition. To fix that race
+            # condition there is sigsuspend() but this function is not
+            # available in the Python standard library.
+            waiter = WaitForRead()
+            waiter.daemon = True
+            waiter.start()
+            # Connect the file descriptor to the subprocess.
+            fcntl.fcntl(self.conn_handle, fcntl.F_SETOWN, waiter.pid)
+            # The channel may have become connected after we last got an empty
+            # read but before we spawned our subprocess, so check one more
+            # time to make sure.
+            data = self.conn_handle.readline()
+            if data:
+                # If there is data available now we prepared for nothing, so
+                # all we need to do is clean up.
+                waiter.terminate()
+            else:
+                # If there is still no data available we'll wait for the
+                # subprocess to indicate that data should be available.
+                waiter.join()
+                data = self.conn_handle.readline()
+        logger.debug("Read %i bytes from %s: %r", len(data), self.conn_label, data)
+        return data
+
+
+class WaitForRead(multiprocessing.Process):
+
+    """Used by :py:func:`GuestAgent.raw_readline()` to implement blocking reads."""
+
+    def run(self):
+        """Endless loop that waits for one or more ``SIGIO`` signals to arrive."""
+        logger.debug("Installing SIGIO signal handler ..")
+        signal.signal(signal.SIGIO, self.signal_handler)
+        timer = Timer()
+        for seconds in itertools.count():
+            logger.debug("Waiting for SIGIO signal (%s) ..", timer)
+            time.sleep(seconds)
+
+    def signal_handler(self, signal_number, frame):
+        """Signal handler for ``SIGIO`` signals that immediately exits the process."""
+        sys.exit(0)
+
+
+def find_character_device(port_name):
+    """
+    Find the character device for the given port name.
+
+    :param port_name: The name of the virtio port (a string).
+    :returns: The absolute pathname of a character device (a string).
+    :raises: :py:exc:`Exception` when the character device cannot be found.
+    """
+    root = '/sys/class/virtio-ports'
+    logger.debug("Automatically selecting appropriate character device based on %s ..", root)
+    for entry in os.listdir(root):
+        name_file = os.path.join(root, entry, 'name')
+        if os.path.isfile(name_file):
+            with open(name_file) as handle:
+                contents = handle.read().strip()
+            if contents == port_name:
+                character_device = '/dev/%s' % entry
+                logger.debug("Selected character device: %s", character_device)
+                return character_device
+    raise Exception(compact("""
+        Failed to select the appropriate character device for the port name
+        {name}! This is probably caused by a configuration issue on either the
+        QEMU host or inside the QEMU guest. Please refer to the following web
+        page for help: http://negotiator.readthedocs.org/en/latest/#character-device-detection-fails
+    """, name=repr(port_name)))
---
+++ packages/negotiator-guest/negotiator-guest-0.7.tar.gz/negotiator-guest-0.7/negotiator_guest/cli.py
@@ -0,0 +1,139 @@
+# Scriptable KVM/QEMU guest agent in Python.
+#
+# Author: Peter Odding <peter@peterodding.com>
+# Last Change: October 24, 2014
+# URL: https://negotiator.readthedocs.org
+
+"""
+Usage: negotiator-guest [OPTIONS]
+
+Start the negotiator-guest daemon.
+
+Supported options:
+
+  -l, --list-commands
+
+    List the commands that the host exposes to its guests.
+
+  -e, --execute=COMMAND
+
+    Execute the given command on the KVM/QEMU host. The standard output stream
+    of the command on the host is intercepted and copied to the standard output
+    stream on the guest. If the command exits with a nonzero status code the
+    negotiator-guest program will also exit with a nonzero status code.
+
+  -d, --daemon
+
+    Start the guest daemon. When using this command line option the
+    `negotiator-guest' program never returns (unless an unexpected error
+    condition occurs).
+
+  -t, --timeout=SECONDS
+
+    Set the number of seconds before a remote call without a response times
+    out. A value of zero disables the timeout (in this case the command can
+    hang indefinitely). The default is 10 seconds.
+
+  -c, --character-device=PATH
+
+    By default the appropriate character device is automatically selected based
+    on /sys/class/virtio-ports/*/name. If the automatic selection doesn't work,
+    you can set the absolute pathname of the character device that's used to
+    communicate with the negotiator-host daemon running on the KVM/QEMU host.
+
+  -v, --verbose
+
+    Make more noise (enables debugging).
+
+  -q, --quiet
+
+    Only show warnings and errors.
+
+  -h, --help
+
+    Show this message and exit.
+"""
+
+# Standard library modules.
+import getopt
+import logging
+import shlex
+import sys
+
+# External dependencies.
+import coloredlogs
+from humanfriendly import Timer
+
+# Modules included in our project.
+from negotiator_common.config import GUEST_TO_HOST_CHANNEL_NAME, HOST_TO_GUEST_CHANNEL_NAME, DEFAULT_TIMEOUT
+from negotiator_common.utils import TimeOut
+from negotiator_guest import GuestAgent, find_character_device
+
+# Initialize a logger for this module.
+logger = logging.getLogger(__name__)
+
+
+def main():
+    """Command line interface for the ``negotiator-guest`` program."""
+    # Initialize logging to the terminal.
+    coloredlogs.install(level=logging.INFO)
+    # Parse the command line arguments.
+    list_commands = False
+    execute_command = None
+    start_daemon = False
+    timeout = DEFAULT_TIMEOUT
+    character_device = None
+    try:
+        options, arguments = getopt.getopt(sys.argv[1:], 'le:dt:c:vqh', [
+            'list-commands', 'execute=', 'daemon', 'timeout=',
+            'character-device=', 'verbose', 'quiet', 'help'
+        ])
+        for option, value in options:
+            if option in ('-l', '--list-commands'):
+                list_commands = True
+            elif option in ('-e', '--execute'):
+                execute_command = value
+            elif option in ('-d', '--daemon'):
+                start_daemon = True
+            elif option in ('-t', '--timeout'):
+                timeout = int(value)
+            elif option in ('-c', '--character-device'):
+                character_device = value
+            elif option in ('-v', '--verbose'):
+                coloredlogs.increase_verbosity()
+            elif option in ('-q', '--quiet'):
+                coloredlogs.decrease_verbosity()
+            elif option in ('-h', '--help'):
+                usage()
+                sys.exit(0)
+        if not (list_commands or execute_command or start_daemon):
+            usage()
+            sys.exit(0)
+    except Exception:
+        logger.exception("Failed to parse command line arguments!")
+        sys.exit(1)
+    # Start the guest daemon.
+    try:
+        if not character_device:
+            channel_name = HOST_TO_GUEST_CHANNEL_NAME if start_daemon else GUEST_TO_HOST_CHANNEL_NAME
+            character_device = find_character_device(channel_name)
+        ga = GuestAgent(character_device)
+        if start_daemon:
+            ga.enter_main_loop()
+        elif list_commands:
+            with TimeOut(timeout):
+                print('\n'.join(ga.call_remote_method('list_commands')))
+        elif execute_command:
+            with TimeOut(timeout):
+                timer = Timer()
+                output = ga.call_remote_method('execute', *shlex.split(execute_command), capture=True)
+                logger.debug("Took %s to execute remote command.", timer)
+                print(output.rstrip())
+    except Exception:
+        logger.exception("Caught a fatal exception! Terminating ..")
+        sys.exit(1)
+
+
+def usage():
+    """Print a user friendly usage message to the terminal."""
+    print(__doc__.strip())
