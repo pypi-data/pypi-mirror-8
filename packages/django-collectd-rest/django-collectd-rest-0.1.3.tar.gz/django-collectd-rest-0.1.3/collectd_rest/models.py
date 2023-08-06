3edbbccbf704b5448e72838cf833c694f0e4614a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:38 +0000

    Add package django-collectd-rest/0.1.3/django-collectd-rest-0.1.3.tar.gz

---
+++ packages/django-collectd-rest/django-collectd-rest-0.1.3.tar.gz/django-collectd-rest-0.1.3/collectd_rest/models.py
@@ -0,0 +1,24 @@
+from collectd_rest.rrd import render, RRDError
+from django.db import models
+from django.core.exceptions import ValidationError
+
+class GraphGroup(models.Model):
+	name = models.CharField(max_length=256, blank = False, null = True, unique = True)
+	title = models.CharField(max_length=256)
+
+class Graph(models.Model):
+	name = models.CharField(max_length=256, blank = False, null = True, unique = True)
+	title = models.CharField(max_length=256)
+	priority = models.IntegerField(default=0)
+	group = models.ForeignKey('GraphGroup', related_name='graphs')
+	command = models.TextField(blank = False, null = True)
+
+	class Meta:
+		ordering = ['-priority']
+
+	def clean(self):
+		try:
+			render(self.command, 'PNG')
+		except RRDError as e:
+			raise ValidationError({'command':e.message})
+
