ecdd442fa5b734b3677e1eb36267b017d0957132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:53 +0000

    Add package ddbscan/0.3.0/ddbscan-0.3.0.tar.gz

---
+++ packages/ddbscan/ddbscan-0.3.0.tar.gz/ddbscan-0.3.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import multiprocessing
+
+version = '0.3.0'
+
+install_requires = ["scipy >= 0.13.3",
+                    "numpy >=1.8.1"]
+
+tests_require = ["nose"]
+
+classifiers=[
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+]
+
+setup(
+    name='ddbscan',
+    version=version,
+    author='Allan Inocencio de Souza Costa',
+    author_email='allan@cloudwalk.io',
+    url='https://github.com/cloudwalkio/ddbscan',
+    packages=['ddbscan'],
+    license='MIT',
+    description='Discrete DBSCAN algorithm optimized for discrete and bounded data.',
+    long_description=open('README.rst').read(),
+    test_suite = 'nose.collector',
+    tests_require=tests_require,
+    install_requires=install_requires,
+    classifiers=classifiers,
+)
