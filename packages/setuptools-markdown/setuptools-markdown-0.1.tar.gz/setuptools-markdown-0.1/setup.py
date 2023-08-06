116790fd302ff7001078b7acd8c8b69c46e02a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:17 +0000

    Add package setuptools-markdown/0.1/setuptools-markdown-0.1.tar.gz

---
+++ packages/setuptools-markdown/setuptools-markdown-0.1.tar.gz/setuptools-markdown-0.1/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+version = '0.1'
+long_description_filename = os.path.join(
+    os.path.dirname(__file__), 'README.rst')
+long_description = open(long_description_filename).read()
+
+setup(
+    name="setuptools-markdown",
+    version=version,
+    author="Marc Abramowitz",
+    author_email="marc@marc-abramowitz.com",
+    url="https://github.com/msabramo/setuptools-markdown",
+    keywords='distutils setuptools markdown',
+    description="Use Markdown for your project description",
+    long_description=long_description,
+    license='MIT',
+    install_requires=['pypandoc'],
+    py_modules=['setuptools_markdown'],
+    zip_safe=False,
+    entry_points="""
+        [distutils.setup_keywords]
+        long_description_markdown_filename=setuptools_markdown:long_description_markdown_filename
+        """
+)
