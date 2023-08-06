17f4960354f7c12243895b96968488b8be26fc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package django-microsip/0.0.13/django-microsip-0.0.13.zip

---
+++ packages/django-microsip/django-microsip-0.0.13.zip/django-microsip-0.0.13/microsip/comun/clientes/models_changes_registry.py
@@ -0,0 +1,6 @@
+# import django_dynamic_models
+# from django.db import models
+
+# django_dynamic_models.change.register('LineaGrupo', **{
+#     'cuenta_ventas' : models.CharField(max_length=30, blank = True, null = True, db_column='CUENTA_VENTAS'),        
+# })
