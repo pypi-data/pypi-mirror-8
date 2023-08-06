6034a1e04fa6f5848060ef566f52ac912d28bcb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:46 +0000

    Add package openlabs_nereid_webshop/3.2.1.8/openlabs_nereid_webshop-3.2.1.8.tar.gz

---
+++ packages/openlabs_nereid_webshop/openlabs_nereid_webshop-3.2.1.8.tar.gz/openlabs_nereid_webshop-3.2.1.8/static/js/app.js
@@ -0,0 +1,18 @@
+// Script to hide Special offers ticker when you click on close icon
+$('.new-offers .fa-times-circle').click(function() {
+  event.preventDefault();
+   $('.new-offers').hide();
+});
+
+// Script which keeps the menu bar on the top when the site scrolls down.
+$(function(){ 
+  var stickyTop = $('.sticky').offset().top; // returns number
+  $(window).scroll(function(){ // scroll event
+    var windowTop = $(window).scrollTop(); // returns number
+    if (stickyTop < windowTop) {
+      $('.sticky').css({ position: 'fixed', top: 0 });
+    } else {
+      $('.sticky').css('position','static');
+    }
+  });
+});
