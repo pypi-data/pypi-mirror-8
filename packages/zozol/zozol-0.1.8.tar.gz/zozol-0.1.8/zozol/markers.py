d939bfd2723c863cf4cd57594050cfba1b8c9219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:37 +0000

    Add package zozol/0.1.8/zozol-0.1.8.tar.gz

---
+++ packages/zozol/zozol-0.1.8.tar.gz/zozol-0.1.8/zozol/markers.py
@@ -0,0 +1,30 @@
+class Tagged(object):
+    def __init__(self, tag=None, data=None, off=0, tlen=0, typ=None):
+        self.data = data
+        self.off = off
+        self.tlen = tlen
+        self.tag = tag
+        self.typ = typ
+
+    def encode(self):
+        return self.data[self.off:self.off + self.tlen]
+
+
+class Implicit(Tagged):
+    pass
+
+class Explicit(Tagged):
+    pass
+
+
+class Optional(object):
+    def __init__(self, typ):
+        self.typ = typ
+
+
+class Default(object):
+    def __init__(self, value, typ):
+        self.value = value
+        self.typ = typ
+
+
