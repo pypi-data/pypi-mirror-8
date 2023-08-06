f7c318b376029a6982959554fe45d6fb87166858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:02 +0000

    Add package easyfab/0.4.7/easyfab-0.4.7.tar.gz

---
+++ packages/easyfab/easyfab-0.4.7.tar.gz/easyfab-0.4.7/setup.py
@@ -0,0 +1,60 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('README.rst') as f:
+    readme = f.read()
+
+requires = [
+        'jinja2',
+        'fabric',
+        'unipath',
+]
+
+setup(
+    name='easyfab',
+    version='0.4.7',
+    description='Simple deployments with fabric',
+    long_description=readme,
+    url='https://github.com/phonkee/easyfab/',
+    author='phonkee',
+    author_email='phonkee@phonkee.eu',
+    license=license,
+    packages=[
+        'easyfab',
+    ],
+    scripts=[
+        'bin/easyfab'
+    ],
+    install_requires=requires,
+    include_package_data = True,
+    package_data={
+        '': [
+            'LICENSE',
+            'README.rst',
+        ],
+        'easyfab': [
+            '*.conf',
+            'templates/*.*',
+            'templates/deployment/*.*',
+            'templates/deployment/conf/*.*',
+        ]
+    },
+    data_files=[
+        ('', ['LICENSE', 'README.rst'])
+    ],
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
