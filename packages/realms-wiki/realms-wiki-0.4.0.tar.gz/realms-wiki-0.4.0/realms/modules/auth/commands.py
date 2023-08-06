18026e281290934db1e6e0feb34e59bc92af2f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:43 +0000

    Add package realms-wiki/0.4.0/realms-wiki-0.4.0.tar.gz

---
+++ packages/realms-wiki/realms-wiki-0.4.0.tar.gz/realms-wiki-0.4.0/realms/modules/auth/commands.py
@@ -0,0 +1,36 @@
+import click
+from realms.lib.util import random_string
+from realms.modules.auth.models import User
+from realms.lib.util import green, red, yellow
+
+
+@click.group(short_help="Auth Module")
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('username')
+@click.argument('email')
+@click.option('--password', help='Leave blank for random password')
+def create_user(username, email, password):
+    """ Create a new user
+    """
+    show_pass = not password
+
+    if not password:
+        password = random_string(12)
+
+    if User.get_by_username(username):
+        red("Username %s already exists" % username)
+        return
+
+    if User.get_by_email(email):
+        red("Email %s already exists" % email)
+        return
+
+    User.create(username, email, password)
+    green("User %s created" % username)
+
+    if show_pass:
+        yellow("Password: %s" % password)
