82724923617153a71f2d1edc7eebc2d8b6bfb397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:25 +0000

    Add package haas/0.5.1/haas-0.5.1-py27-none-any.whl

---
+++ packages/haas/haas-0.5.1-py27-none-any.whl/haas/_version.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-2014 Simon Jagoe
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the 3-clause BSD license.  See the LICENSE.txt file for details.
+from __future__ import absolute_import, unicode_literals
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '0.5.1'
+full_version = '0.5.1'
+git_revision = '33535f85e5f5e0884d4f681132f10e0138131da8'
+is_released = True
+
+if not is_released:
+    version = full_version
