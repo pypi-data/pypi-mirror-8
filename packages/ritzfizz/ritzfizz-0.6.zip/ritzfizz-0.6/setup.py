caff74668dfffc2bdfcd32ee237f4ca544ff7cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:20 +0000

    Add package ritzfizz/0.6/ritzfizz-0.6.zip

---
+++ packages/ritzfizz/ritzfizz-0.6.zip/ritzfizz-0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+  name = 'ritzfizz',
+  packages = ['ritzfizz'], # this must be the same as the name above
+  version = '0.6',
+  description = 'ritzfizz Sphinx Theme',
+  author = 'Nick Wortley',
+  author_email = 'nick@bixly.com',
+  url = 'https://github.com/NickWortley/nebrios_docs', # use the URL to the github repo
+  download_url = 'https://github.com/NickWortley/nebrios_docs/tarball/0.6', # I'll explain this in a second
+  keywords = ['sphinx', 'theme', 'readthedocs', 'ritzfizz'], # arbitrary keywords
+  classifiers = [],
+  include_package_data=True,
+)
+
