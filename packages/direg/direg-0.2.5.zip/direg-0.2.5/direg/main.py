ddad1465cbe5d8a8ad28992a32b92c25fb0f64d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:25 +0000

    Add package direg/0.2.5/direg-0.2.5.zip

---
+++ packages/direg/direg-0.2.5.zip/direg-0.2.5/direg/main.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""For the moment, this is pretty much all of direg. __init__.py imports all of this module.
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+
+import os
+import imp
+import sys
+import inspect
+
+registries = []
+
+
+def load_module(name, path):
+    """Load and return module object at "path"
+    """
+    try:
+        module = imp.load_source(name, path)
+    except IOError:
+        logger.info('{0}: no such file'.format(path))
+        module = imp.new_module(name)
+        module.data = {}
+    return module
+
+
+def get_registry_data(registries):
+    """Merge and return the namespaces of modules listed in iterable "registries"
+    """
+    data = {}
+    for registry in registries:
+        module = load_module('_module', registry)
+        data.update(module.data)
+    return data
+
+
+def get_class_from_frame(fr):
+    args, _, _, value_dict = inspect.getargvalues(fr)
+    if len(args) and args[0] == 'self':
+        instance = value_dict.get('self', None)
+        if instance:
+            return getattr(instance, '__class__', None).__name__
+    return None
+
+
+def compute_key(s):
+    frame = sys._getframe().f_back
+    frame = frame.f_back
+    fun_name = frame.f_code.co_name
+    if fun_name == '<module>':
+        fun_name = ''
+    components = []
+    mod_name = inspect.getmodule(frame.f_code).__name__
+    components.append(mod_name)
+    zclass = get_class_from_frame(frame)
+    if zclass is not None:
+        components.append(zclass)
+    if fun_name:
+        components.append(fun_name)
+    components.append(s)
+    key = '.'.join(components)
+    return key
+
+
+def get_value(s):
+    """Look up key in a given context. For example, in a module called "foo", if we have:
+
+            class Bar:
+                def meth(self):
+                    return get_value('my_var')
+
+            b = Bar()
+            x = b.meth()
+
+        Then the registry/registries will be consulted for a key 'foo.Bar.meth.my_var' and it's corresponding value
+        will be returned.
+    """
+    key = compute_key(s)
+    value = get_registry_data(registries)[key]
+    return value
---
+++ packages/direg/direg-0.2.5.zip/direg-0.2.5/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+import direg
+
+# README.rst dynamically generated:
+with open('README.rst', 'w') as f:
+    f.write(direg.__doc__)
+
+
+def read(file):
+    with open(file, 'r') as f:
+        return f.read().strip()
+
+setup(
+    name='direg',
+    version=read('VERSION'),
+    description=('A drop-in registry. Easily maintain a central configuration registry for your complex '
+                 'python project.'),
+    long_description=read('README.rst'),
+    author='Mike Burr',
+    author_email='mburr@unintuitive.org',
+    url='https://github.com/stnbu/direg',
+    download_url='https://github.com/stnbu/direg/archive/master.zip',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+    packages=['direg'],
+    keywords=['configuration', 'registry', 'refactor'],
+    test_suite='nose.collector',
+    test_requires=['nose'],
+)
