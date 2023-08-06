8e41530c0d8768af29148a0e575249fe32d049ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:53 +0000

    Add package django-waffle/0.10.1/django-waffle-0.10.1.tar.gz

---
+++ packages/django-waffle/django-waffle-0.10.1.tar.gz/django-waffle-0.10.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-waffle',
+    version='0.10.1',
+    description='A feature flipper for Django.',
+    long_description=open('README.rst').read(),
+    author='James Socol',
+    author_email='james.socol@gmail.com',
+    url='http://github.com/jsocol/django-waffle',
+    license='BSD',
+    packages=find_packages(exclude=['test_app', 'test_settings']),
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
