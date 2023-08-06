719193e963d229aca4d4b7d777e3a5a3eb5b0491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:03 +0000

    Add package tstamp/1.0.1/tstamp-1.0.1.tar.gz

---
+++ packages/tstamp/tstamp-1.0.1.tar.gz/tstamp-1.0.1/README.txt
@@ -0,0 +1,4 @@
+timestamp
+=========
+
+a python library which simplifies the task of getting various timestamps
---
+++ packages/tstamp/tstamp-1.0.1.tar.gz/tstamp-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import setup
+from setuptools.dist import Distribution
+
+setup(
+    name="tstamp",
+    version="1.0.1",
+    packages=['tstamp'],
+    description="A library which provides a higher level interface for creating and formatting timestamps.",
+    url="https://github.com/iLoveTux/timestamp",
+    author="iLoveTux",
+    author_email="ilovetux@ymail.com",
+    license="MIT",
+    keywords="time timestamp epoch"
+)
+
