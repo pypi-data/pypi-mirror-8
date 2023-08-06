b84016fd0040265bd3c04c128c1614320d7d37b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:00 +0000

    Add package mprpc/0.1.4/mprpc-0.1.4.tar.gz

---
+++ packages/mprpc/mprpc-0.1.4.tar.gz/mprpc-0.1.4/mprpc/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from client import RPCClient, RPCPoolClient
+from server import RPCServer
---
+++ packages/mprpc/mprpc-0.1.4.tar.gz/mprpc-0.1.4/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, Extension, find_packages
+
+setup(
+    name='mprpc',
+    version='0.1.4',
+    description='A fast MessagePack RPC library',
+    long_description=open('README.rst').read(),
+    author='Studio Ousia',
+    author_email='admin@ousia.jp',
+    url='http://github.com/studio-ousia/mprpc',
+    packages=find_packages(),
+    ext_modules=[
+        Extension('mprpc.client', ['mprpc/client.c']),
+        Extension('mprpc.server', ['mprpc/server.c'])
+    ],
+    license=open('LICENSE').read(),
+    include_package_data=True,
+    keywords=['rpc', 'msgpack', 'messagepack', 'msgpackrpc', 'messagepackrpc',
+              'messagepack rpc', 'gevent'],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+    install_requires=[
+        'gsocketpool',
+        'gevent',
+        'msgpack-python',
+    ],
+    tests_require=[
+        'nose',
+        'mock',
+    ],
+    test_suite = 'nose.collector'
+)
