cb3ee0814e5e211d6180b9b161c007b2912163aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:20 +0000

    Add package gomatic/0.2.0/gomatic-0.2.0.tar.gz

---
+++ packages/gomatic/gomatic-0.2.0.tar.gz/gomatic-0.2.0/gomatic/__init__.py
@@ -0,0 +1,2 @@
+from .goServerConfigurator import Agent, BuildArtifact, ExecTask, FetchArtifactDir, FetchArtifactFile, FetchArtifactTask, \
+    GitMaterial, GoServerConfigurator, HostRestClient, Job, Pipeline, PipelineGroup, PipelineMaterial, RakeTask, Tab, TestArtifact
