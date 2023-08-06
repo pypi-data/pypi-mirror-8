8c559f187171dc64fd9a95d13bffd2eca5a5a363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:42 +0000

    Add package switchboard/1.2.0/switchboard-1.2.0.tar.gz

---
+++ packages/switchboard/switchboard-1.2.0.tar.gz/switchboard-1.2.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+version = '1.2.0'
+
+setup(name='switchboard',
+      version=version,
+      description="Feature flipper for Pyramid, Pylons, or TurboGears apps.",
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='switches feature flipper pyramid pylons turbogears',
+      author='Kyle Adams',
+      author_email='kadams54@users.sourceforge.net',
+      url='http://sf.net/projects/switchboardpy',
+      download_url='https://sf.net/projects/switchboardpy/files/latest',
+      license='Apache License',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      install_requires=[
+          'FormEncode >= 1.2',
+          'pymongo >= 2.3',
+          'blinker >= 1.2',
+          'WebOb >= 0.9',
+          'Mako >= 0.9',
+          'decorator',
+      ],
+      zip_safe=False,
+      tests_require=[
+          'nose >= 0.11',
+          'mock >= 1.0',
+          'bobo >= 1.0',
+      ],
+      test_suite='nose.collector',
+      )
