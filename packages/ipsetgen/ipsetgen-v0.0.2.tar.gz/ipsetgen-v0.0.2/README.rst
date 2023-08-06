aa550a16d8e918f327850e394837b0e355e131ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:28 +0000

    Add package ipsetgen/v0.0.2/ipsetgen-v0.0.2.tar.gz

---
+++ packages/ipsetgen/ipsetgen-v0.0.2.tar.gz/ipsetgen-v0.0.2/README.rst
@@ -0,0 +1,6 @@
+ipset-generator
+===============
+
+role-based ipset_ generator.
+
+.. _ipset: http://ipset.netfilter.org/ipset.man.html
---
+++ packages/ipsetgen/ipsetgen-v0.0.2.tar.gz/ipsetgen-v0.0.2/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='ipsetgen',
+    version='v0.0.2',
+    packages=['ipsetgen'],
+    include_package_data=True,
+    license='BSD 3-Clause License',
+    description='role-based ipset generator',
+    long_description=README,
+    url='https://github.com/fly/ipsetgen',
+    author='Jon Chen',
+    author_email='bsd@voltaire.sh',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+    install_requires=[
+        'pyyaml'
+    ]
+)
