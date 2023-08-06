2cc80a7b275beb7f13fd6ffd745ad58f4c0b1473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:02 +0000

    Add package Chapeau/0.0.1/Chapeau-0.0.1.tar.gz

---
+++ packages/Chapeau/Chapeau-0.0.1.tar.gz/Chapeau-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name = 'Chapeau',
+	version = '0.0.1',
+	url = 'https://github.com/lmontopo/Chapeau',
+	author = 'Leta Montopoli and Margo Smith',
+	author_email = 'lmontopo@gmail.com',
+	modules = ['chapeau.py'] )
