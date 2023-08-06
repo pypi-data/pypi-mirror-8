f9e6c0c685b45ae9dc29904410705fb54302c542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:15 +0000

    Add package django-kidotest/1.1/django-kidotest-1.1.tar.gz

---
+++ packages/django-kidotest/django-kidotest-1.1.tar.gz/django-kidotest-1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name="django-kidotest",
+    version="1.1",
+    description='Code snippets which help writing automated tests for Django.',
+    long_description=long_description,
+    url="http://kidosoft.pl",
+    author="Jakub STOLARSKI (Dryobates)",
+    author_email="jakub.stolarski@kidosoft.pl",
+    license="beerware",
+    keywords="django testing",
+    packages=find_packages(exclude=['*tests*', '*example*']),
+    install_requires=[
+        'mock',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
