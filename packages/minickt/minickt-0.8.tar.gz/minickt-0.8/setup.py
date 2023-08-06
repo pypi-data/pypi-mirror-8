bcc50d32b048641a0cd0c80f9ec626426f311746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:13 +0000

    Add package minickt/0.8/minickt-0.8.tar.gz

---
+++ packages/minickt/minickt-0.8.tar.gz/minickt-0.8/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+	name = 'minickt',
+	packages = ['minickt', 'cktParser', 'cktWriter'],
+	scripts = ['minic'],
+	install_requires=['matplotlib','networkx'],
+	version = '0.8',
+	description = 'Analyzer and simulator of logic circuit',
+	author = 'dokelung',
+	author_email = 'dokelung@gmail.com',
+	url = 'https://github.com/dokelung/minickt',
+	download_url = 'https://github.com/dokelung/minickt/tarball/v0.8',
+	keywords = ['circuit','eda','verification','logic','gate'],
+	classifiers = [],
+)
