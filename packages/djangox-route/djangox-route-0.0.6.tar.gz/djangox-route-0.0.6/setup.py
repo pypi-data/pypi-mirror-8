331bcd829f9e4a2f96225771b02dac1bd5b81ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:30 +0000

    Add package djangox-route/0.0.6/djangox-route-0.0.6.tar.gz

---
+++ packages/djangox-route/djangox-route-0.0.6.tar.gz/djangox-route-0.0.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(name='djangox-route',
+      description='Discover django views and register to urlconf by convention.',
+      author='Youngrok Pak',
+      author_email='pak.youngrok@gmail.com',
+      keywords= 'rest route autodiscover django djangox',
+      url='https://github.com/youngrok/djangox-route',
+      version='0.0.6',
+      namespace_packages = ['djangox'],
+      packages=['djangox',
+                'djangox.route', 
+                ],
+
+      classifiers = [
+                     'Development Status :: 3 - Alpha',
+                     'Topic :: Software Development :: Libraries',
+                     'License :: OSI Approved :: BSD License']
+      )
