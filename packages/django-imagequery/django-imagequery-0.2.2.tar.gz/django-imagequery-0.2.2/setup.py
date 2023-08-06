1e0da6dd14d8873b973b551328831958b6f43607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:57 +0000

    Add package django-imagequery/0.2.2/django-imagequery-0.2.2.tar.gz

---
+++ packages/django-imagequery/django-imagequery-0.2.2.tar.gz/django-imagequery-0.2.2/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-imagequery",
+    version = "0.2.2",
+    author = 'David Danier',
+    author_email = "david.danier@team23.de",
+    url = "https://github.com/ddanier/django_imagequery",
+    
+    packages = find_packages(exclude=[
+        'example',
+        'example.*'
+    ]),
+    package_data = {
+        'imagequery.tests': [
+            'samplefonts/*.ttf',
+            'sampleimages/*.png',
+            'sampleimages/*.jpg',
+            'sampleimages/results/*.png',
+            'sampleimages/results/*.jpg',
+        ],
+    },
+    license = "BSD License",
+    keywords = "django imagequery PIL",
+    description = "Image manipulation written like well known QuerySet operations",
+    install_requires=[
+        'setuptools',
+        'Django',
+    ],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Utilities',
+    ],
+    zip_safe=False,
+)
+
