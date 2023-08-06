fb77f7bb105ef742b2e30ebd129c794ec1003683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:36 +0000

    Add package Mongothon/0.7.19/Mongothon-0.7.19.tar.gz

---
+++ packages/Mongothon/Mongothon-0.7.19.tar.gz/Mongothon-0.7.19/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="Mongothon",
+    version="0.7.19",
+    author="Tom Leach",
+    author_email="tom@gc.io",
+    description="A MongoDB object-document mapping layer for Python",
+    license="BSD",
+    keywords="mongo mongodb database pymongo odm validation",
+    url="http://github.com/gamechanger/mongothon",
+    packages=["mongothon"],
+    long_description="Mongothon is a MongoDB object-document mapping " +
+                     "API for Python, loosely based on the awesome " +
+                     "mongoose.js library.",
+    install_requires=['pymongo>=2.5.0', 'inflection==0.2.0', 'schemer>=0.2.0, <0.3.0'],
+    tests_require=['mock', 'nose']
+    )
