648ca4d2f9e0037376abcc4e2a3ec02e1633b6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:33 +0000

    Add package django-security/0.1.24b/django-security-0.1.24b.tar.gz

---
+++ packages/django-security/django-security-0.1.24b.tar.gz/django-security-0.1.24b/setup.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2011, SD Elements. See LICENSE.txt for details.
+
+import os
+from distutils.core import setup
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+readme = f.read()
+f.close()
+
+setup(name="django-security",
+      description='A collection of tools to help secure a Django project.',
+      long_description=readme,
+      maintainer="SD Elements",
+      maintainer_email="django-security@sdelements.com",
+      version="0.1.24b",
+      packages=["security", "security.migrations", "security.auth_throttling"],
+      url='https://github.com/sdelements/django-security',
+      classifiers=[
+          'Framework :: Django',
+          'Environment :: Web Environment',
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: BSD License',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+	  'Topic :: Security',
+      ],
+      install_requires=['django>=1.4',],
+      )
+
