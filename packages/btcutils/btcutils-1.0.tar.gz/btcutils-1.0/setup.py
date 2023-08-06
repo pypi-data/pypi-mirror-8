5a62940497af39bf31abf9c8a71721842c72619b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:13 +0000

    Add package btcutils/1.0/btcutils-1.0.tar.gz

---
+++ packages/btcutils/btcutils-1.0.tar.gz/btcutils-1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup, Extension
+import os
+
+os.environ["CC"] = "g++"
+os.environ["CXX"] = "g++"
+
+module1 = Extension('btcutils',
+                    sources = [
+                        'uint256.cpp',
+                        'utilstrencodings.cpp',
+                        'sha2.cpp',
+                        'main.cpp',
+                        ],
+                     language = "c++")
+setup (name = 'btcutils',
+       version = '1.0',
+       description = 'Python native extension providing Bitcoin related functions',
+       ext_modules = [module1])
