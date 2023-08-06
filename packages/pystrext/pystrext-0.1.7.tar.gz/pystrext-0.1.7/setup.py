e898361d8827773da885ab717056234457b217b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:00 +0000

    Add package pystrext/0.1.7/pystrext-0.1.7.tar.gz

---
+++ packages/pystrext/pystrext-0.1.7.tar.gz/pystrext-0.1.7/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+import os
+
+def read(*names):
+    values = dict()
+    for name in names:
+        filename = name + '.rst'
+        if os.path.isfile(filename):
+            fd = open(filename)
+            value = fd.read()
+            fd.close()
+        else:
+            value = ''
+        values[name] = value
+    return values
+
+
+long_description = """
+%(README)s
+
+News
+====
+%(CHANGES)s
+""" % read('README', 'CHANGES')
+
+setup(name='pystrext',
+      version='0.1.7',
+      description='Python string extension',
+      long_description=long_description,
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+      ],
+      keywords='string helpers',
+      url='https://pypi.python.org/pypi/pystrext',
+      author='Eric Lapouyade',
+      author_email='elapouya@gmail.com',
+      license='LGPL 2.1',
+      packages=['pystrext'],
+      install_requires = ['Sphinx', 'sphinxcontrib-napoleon'],
+      zip_safe=False)
