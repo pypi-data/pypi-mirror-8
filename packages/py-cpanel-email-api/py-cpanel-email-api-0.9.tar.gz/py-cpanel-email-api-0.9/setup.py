1b9a43d1b8e209a3106514a4a53ec12e61640769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:43 +0000

    Add package py-cpanel-email-api/0.9/py-cpanel-email-api-0.9.tar.gz

---
+++ packages/py-cpanel-email-api/py-cpanel-email-api-0.9.tar.gz/py-cpanel-email-api-0.9/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='py-cpanel-email-api',
+      description='Python client library for interacting with Cpanel\'s API2 Email Module',
+      author='Benton Snyder',
+      author_email='benton@bensnyde.me',
+      url='https://github.com/bensnyde/py-cpanel-email-api/',
+      version='0.9',
+      py_modules=['cpanel_email_api'],
+      )
