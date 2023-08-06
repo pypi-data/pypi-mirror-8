d30d1a890eeb116917ed4b396f106cd1c830f238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:55 +0000

    Add package pybtex-docutils/0.2.1/pybtex-docutils-0.2.1.tar.gz

---
+++ packages/pybtex-docutils/pybtex-docutils-0.2.1.tar.gz/pybtex-docutils-0.2.1/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import codecs
+
+
+def readfile(filename):
+    with codecs.open(filename, encoding="utf-8") as stream:
+        return stream.read().split("\n")
+
+doclines = readfile("README.rst")
+requires = readfile("requirements.txt")
+version = readfile("VERSION")[0].strip()
+
+setup(
+    name='pybtex-docutils',
+    version=version,
+    url='https://github.com/mcmtroffaes/pybtex-docutils',
+    download_url='http://pypi.python.org/pypi/pybtex-docutils',
+    license='MIT',
+    author='Matthias C. M. Troffaes',
+    author_email='matthias.troffaes@gmail.com',
+    description=doclines[0],
+    long_description="\n".join(doclines[2:]),
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Text Editors :: Text Processing',
+        'Topic :: Text Processing :: Markup :: XML'
+    ],
+    platforms='any',
+    py_modules=['pybtex_docutils'],
+    install_requires=requires,
+    use_2to3=True,
+    entry_points={
+        'pybtex.backends': [
+            'docutils = pybtex_docutils:Backend',
+        ]
+    },
+)
---
+++ packages/pybtex-docutils/pybtex-docutils-0.2.1.tar.gz/pybtex-docutils-0.2.1/test/test_install_example.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import nose.tools
+
+expected_result = (
+    u'<paragraph>D.\xa0Lindley. <emphasis>Making Decisions</emphasis>. '
+    u'Wiley, 2nd edition, 1985.</paragraph>'
+    )
+
+def test_install_example():
+    result = []
+
+    ### example begin ###
+    import six
+    import pybtex.database.input.bibtex
+    import pybtex.plugin
+
+    style = pybtex.plugin.find_plugin('pybtex.style.formatting', 'plain')()
+    backend = pybtex.plugin.find_plugin('pybtex.backends', 'docutils')()
+    parser = pybtex.database.input.bibtex.Parser()
+    data = parser.parse_stream(six.StringIO(u"""
+    @Book{1985:lindley,
+      author =    {D. Lindley},
+      title =     {Making Decisions},
+      publisher = {Wiley},
+      year =      {1985},
+      edition =   {2nd},
+    }
+    """))
+    for entry in style.format_entries(six.itervalues(data.entries)):
+        print(backend.paragraph(entry))
+    ### example end ###
+        result.append(backend.paragraph(entry))
+
+    nose.tools.assert_equal(len(result), 1)
+    nose.tools.assert_equal(six.text_type(result[0]), expected_result)
