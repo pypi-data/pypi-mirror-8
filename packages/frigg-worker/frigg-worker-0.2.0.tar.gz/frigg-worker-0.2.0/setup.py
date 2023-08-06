6107f19c8ae0f8ccd6e0528c4daca458473b496a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:49 +0000

    Add package frigg-worker/0.2.0/frigg-worker-0.2.0.tar.gz

---
+++ packages/frigg-worker/frigg-worker-0.2.0.tar.gz/frigg-worker-0.2.0/setup.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='frigg-worker',
+    version='0.2.0',
+    description='',
+    author='The frigg team',
+    author_email='hi@frigg.io',
+    license='MIT',
+    url='https://github.com/frigg/frigg-worker',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=[
+        'fabric',
+        'redis',
+        'pyyaml',
+        'requests',
+        'raven'
+    ],
+    entry_points={
+        'console_scripts': ['frigg-worker = frigg.worker.cli:main']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+    ]
+)
