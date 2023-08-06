b021fb1f5b73321c40213fea6d92cf4c13765932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:46 +0000

    Add package base36/0.1.1/base36-0.1.1.tar.gz

---
+++ packages/base36/base36-0.1.1.tar.gz/base36-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+with open('README.rst') as readme:
+    next(readme)
+    long_description = ''.join(readme).strip()
+
+
+setup(
+    name='base36',
+    version='0.1.1',
+    author='Jiangge Zhang',
+    author_email='tonyseek@gmail.com',
+    description='Yet another implementation for the positional numeral system '
+                'using 36 as the radix.',
+    long_description=long_description,
+    url='https://github.com/tonyseek/python-base36',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    py_modules=['base36'],
+    platforms=['Any'],
+)
