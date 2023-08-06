21b2636281a24a6e902f183c46406a0c5306d0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:00 +0000

    Add package VideoAnalysis/0.1.7/VideoAnalysis-0.1.7.tar.gz

---
+++ packages/VideoAnalysis/VideoAnalysis-0.1.7.tar.gz/VideoAnalysis-0.1.7/README.txt
@@ -0,0 +1,38 @@
+Widgets for analyzing videos of worms. 
+
+To install, use:
+
+    setup.py install
+
+To see other install options:
+
+    setup.py --help
+
+In addition to the listed requirements, you will need to install
+wormtracker, roitools, and OpenCV, which cannot be installed from
+PyPI. 
+
+To install OpenCV, see
+http://docs.opencv.org/doc/tutorials/introduction/table_of_content_introduction/table_of_content_introduction.html. 
+
+wormtracker and roitools are by Stephen Helms and can be obtained from
+him (shelms@amolf.nl). wormtracker in turn requires scipy, h5py, and
+scikit-image, which are available from PyPI. For convenience, these
+are also listed as requirements of VideoAnalysis.
+
+Other tricks to look out for: First, you will need an up-to-date
+version of the ipython notebook, such as 2.4. Many ipython
+installations have a very early version of the notebook, such as
+0.x. This won't work. You can fix this with 
+
+    pip install --upgrade --user ipython
+
+You may find, though, when you actually run 'ipython notebook', that
+it fails with the exception 'ImportError: IPython.html requires pyzmq
+>= 2.1.11'. To fix this, use
+
+    pip install pyzmq
+
+You may get a similar error for tornado, which you can also install
+with pip. (Why these are not listed in PyPI as requirements for
+ipython[notebook] I don't understand...)
---
+++ packages/VideoAnalysis/VideoAnalysis-0.1.7.tar.gz/VideoAnalysis-0.1.7/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+#
+# This package also requires Stephen Helms wormtracker package, but
+# that is not available on PyPI, so has to be installed by hand. It
+# also requires OpenCV, which in my experience doesn't install well
+# (or at all) from pyopencv, so you're on your own for that as well.
+#
+requirements = [
+    'ipython',
+    'PIL', 'hashlib', 'numpy', 'IPython',
+    'IPython.html', 'IPython.utils.traitlets',
+    'PIL', 'hashlib', 'numpy', 'scipy', 'h5py', 'skimage',
+    'matplotlib', 'ROIWidgets', 'Avery',
+]
+install_requirements = [
+    'ipython >= 2.2',
+    'ipython[notebook] >= 2.2',
+    'Pillow', 'hashlib', 'numpy', 'scipy', 'h5py', 'scikit-image',
+    'matplotlib', 'ROIWidgets', 'Avery',
+]
+
+setup(
+    name='VideoAnalysis',
+    version='0.1.7',
+    description='Widgets for analysis of worm videos',
+    author='Leon Avery',
+    author_email='lavery3@vcu.edu',
+    url='https://pypi.python.org/pypi/VideoAnalysis',
+    packages=find_packages(),
+    install_requires=install_requirements,
+    requires=requirements,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: Free for non-commercial use',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Widget Sets',
+    ],
+)
