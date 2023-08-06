68989f82141776585ed8271ccaba8d063e4c6027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:05 +0000

    Add package appier/0.8.5/appier-0.8.5.tar.gz

---
+++ packages/appier/appier-0.8.5.tar.gz/appier-0.8.5/src/appier/__init__.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Appier Framework
+# Copyright (C) 2008-2014 Hive Solutions Lda.
+#
+# This file is part of Hive Appier Framework.
+#
+# Hive Appier Framework is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Appier Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Appier Framework. If not, see <http://www.apache.org/licenses/>.
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+from . import api
+from . import async
+from . import base
+from . import config
+from . import controller
+from . import defines
+from . import exceptions
+from . import export
+from . import http
+from . import legacy
+from . import log
+from . import meta
+from . import model
+from . import mongo
+from . import observer
+from . import part
+from . import request
+from . import scheduler
+from . import serialize
+from . import session
+from . import settings
+from . import smtp
+from . import structures
+from . import typesf
+from . import util
+from . import validation
+
+from .api import *
+from .async import *
+from .base import *
+from .config import *
+from .controller import *
+from .defines import *
+from .exceptions import *
+from .export import *
+from .http import *
+from .legacy import *
+from .log import *
+from .meta import *
+from .model import *
+from .mongo import *
+from .observer import *
+from .part import *
+from .request import *
+from .scheduler import *
+from .serialize import *
+from .session import *
+from .settings import *
+from .smtp import *
+from .structures import *
+from .typesf import *
+from .util import *
+from .validation import *
+
+HTTPError = exceptions.HTTPError
