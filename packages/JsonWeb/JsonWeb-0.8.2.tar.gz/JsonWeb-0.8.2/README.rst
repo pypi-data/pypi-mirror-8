7a66e8b499ca497114b075f263f80a68f217dd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:50 +0000

    Add package JsonWeb/0.8.2/JsonWeb-0.8.2.tar.gz

---
+++ packages/JsonWeb/JsonWeb-0.8.2.tar.gz/JsonWeb-0.8.2/README.rst
@@ -0,0 +1,33 @@
+JsonWeb 
+========
+
+.. image:: https://travis-ci.org/boris317/JsonWeb.png?branch=master
+   :target: https://travis-ci.org/boris317/JsonWeb
+
+Add JSON (de)serialization to your python objects ::
+
+    >>> from jsonweb import decode, encode
+    
+    >>> @encode.to_object()
+    ... @decode.from_object()
+    ... class User(object):
+    ...     def __init__(self, nick, email):
+    ...         self.nick = nick
+    ...         self.email = email
+    
+    >>> json_str = encode.dumper(User("cool_user123", "cool_user123@example.com"))
+    >>> print json_str
+    {"nick": "cool_user123", "__type__": "User", "email": "cool_user123@example.com"}
+    
+    >>> user = decode.loader(json_str)
+    >>> print user.nick
+    cool_user123
+    >>> print user
+    <User object at 0x10145e390>
+    
+.. note ::
+
+    JsonWeb is still very much under development. Things will change.
+
+See  `documentation <http://www.jsonweb.info/>`_
+
---
+++ packages/JsonWeb/JsonWeb-0.8.2.tar.gz/JsonWeb-0.8.2/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.8.2'
+
+if os.path.exists("README.rst"):
+    long_description = open("README.rst").read()
+else:
+    long_description = "See http://www.jsonweb.net/en/latest"
+
+setup(name='JsonWeb',
+      version=version,
+      description="Quickly add json serialization and deserialization to your python classes.",
+      long_description=long_description,
+      keywords='',
+      author='Shawn Adams',
+      author_email='',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+      )
