0fd8e8ec2fe5286877d4ef159443496d2154277f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:19 +0000

    Add package django-muckraking/0.3.2/django-muckraking-0.3.2.tar.gz

---
+++ packages/django-muckraking/django-muckraking-0.3.2.tar.gz/django-muckraking-0.3.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-muckraking',
+    version='0.3.2',
+    author='Ryan Bagwell',
+    author_email='ryan@ryanbagwell.com',
+    url='https://github.com/ryanbagwell/django-muckraking',
+    packages=find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description="A collection of random Django utilities",
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "Django >= 1.4",
+        "django-classy-tags==0.4",
+        "gitpython==0.3.2.RC1"
+    ]
+)
