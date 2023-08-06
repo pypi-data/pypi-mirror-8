f060ef9a5bfe3021988ce2976ad4132ffa81b216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:27 +0000

    Add package txt2boil/0.3/txt2boil-0.3.zip

---
+++ packages/txt2boil/txt2boil-0.3.zip/txt2boil-0.3/txt2boil/core/hookedre.py
@@ -0,0 +1,36 @@
+"""A regex match object with a hook for generating output.
+
+"""
+
+import re
+
+
+class HookedRegex(object):
+
+    """A regex object with a hook that generates output from what it has
+    matched.
+
+    """
+
+    def __new__(cls, match, template, text):
+        if re.match(match, text, re.M) is None:
+            return None
+        return super(HookedRegex, cls).__new__(cls, match, template, text)
+
+    def __init__(self, match, template, text):
+        """Initialize the object as a clone of another regex, but add a hook
+        to it this time.
+
+        """
+
+        self.regex = re.match(match, text, re.M)
+        self.template = template
+
+    def __bool__(self):
+        return self.regex is not None
+
+    def group(self, n=0):
+        return self.regex.group(n)
+
+    def output(self, match):
+        match.expand(self.template.format(self.regex.group(2)))
---
+++ packages/txt2boil/txt2boil-0.3.zip/txt2boil-0.3/txt2boil/test/testracket.py
@@ -0,0 +1,83 @@
+from testgen import TestGen
+from langs import Racket
+
+
+class RacketConstantTester(TestGen, Racket):
+
+    """Test the boilerplate generation in Racket code.
+
+    """
+
+    basicText = r"""
+;; Constant Gen: r(\d+)/(\d+) (make-my-rational \1 \2)
+
+r5/7
+"""
+
+    basicAnswer = r"""
+;; Constant Gen: r(\d+)/(\d+) (make-my-rational \1 \2)
+(define r5/7 (make-my-rational 5 7))
+
+r5/7
+"""
+
+    blahComment = r"""
+;; blah
+"""
+
+    def test_solecomment(self):
+        """Test the constant generation.
+
+        """
+
+        self.checkGenerates(self.basicText, self.basicAnswer)
+
+    def test_onecommentwithothers(self):
+        """Test with the existence of other comments.
+
+        """
+
+        self.checkGenerates(self.blahComment + self.basicText,
+                            self.blahComment + self.basicAnswer)
+
+    def test_extractmany(self):
+        """Test the extraction of many comments.
+
+        """
+
+        c = self.comments(r"""
+;; foo
+
+;; bar
+""")
+        self.assertEqual(c, ['foo\n', 'bar\n'])
+
+    def test_commentextraction(self):
+        """Test the extraction of comments.
+
+        """
+
+        c = self.comments(self.basicAnswer)
+        self.assertEqual(
+            c, [r"Constant Gen: r(\d+)/(\d+) (make-my-rational \1 \2)" +
+                "\n"])
+
+    lineGenTest = r"""
+;; Line Gen:
+;; r(\d+)/(\d+)
+;; (define \g<0> (make-my-rational \1 \2))
+
+r5/7
+"""
+
+    lineGenAnswer = r"""
+;; Line Gen:
+;; r(\d+)/(\d+)
+;; (define \g<0> (make-my-rational \1 \2))
+(define r5/7 (make-my-rational 5 7))
+
+r5/7
+"""
+
+    def testLineGen(self):
+        self.checkGenerates(self.lineGenTest, self.lineGenAnswer)
