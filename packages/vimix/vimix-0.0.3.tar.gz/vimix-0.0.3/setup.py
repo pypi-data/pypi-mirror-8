95ca7f5a4d0526d615c46f596c895c53d723fa66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:08 +0000

    Add package vimix/0.0.3/vimix-0.0.3.tar.gz

---
+++ packages/vimix/vimix-0.0.3.tar.gz/vimix-0.0.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='vimix',
+    version='0.0.3',
+    packages=['vimix'],
+    scripts=['scripts/vimix'],
+    package_data={
+        '': ['templates/*.txt'],
+    },
+    author='boku',
+    author_email='punipuniomochi@gmail.com',
+    description='Make minimum environment for Vim script',
+    license='WTFPL',
+    url='https://github.com/mtwtkman/vimix',
+    download_url='https://github.com/mtwtkman/vimix/tarball/master',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development"
+    ],
+)
