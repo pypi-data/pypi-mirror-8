f3c8ea62b14daa4fd010f29d1c41d2c4d9a8aed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:21 +0000

    Add package cx_test/1.0.1/cx_test-1.0.1.zip

---
+++ packages/cx_test/cx_test-1.0.1.zip/cx_test-1.0.1/cx_test.py
@@ -0,0 +1,11 @@
+﻿"""这是一个模块"""
+def printList(theList):
+	"""遍历列表"""
+	for item in theList:
+		if isinstance(item,list):
+			print_lol(item)
+		else:
+			print(item)
+
+def helloWorld():
+	print("helloWorld")
---
+++ packages/cx_test/cx_test-1.0.1.zip/cx_test-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'cx_test',
+	version = '1.0.1',
+	py_modules = ['cx_test'],
+	author = 'cx',
+	author_email = 'onechenxing@gmail.com',
+	url = 'null',
+	description = 'hello world'
+	)
