87ae748e77a5d30f22658f23d9a560732b60c3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:57 +0000

    Add package plone.mls.listing/0.9.16/plone.mls.listing-0.9.16.tar.gz

---
+++ packages/plone.mls.listing/plone.mls.listing-0.9.16.tar.gz/plone.mls.listing-0.9.16/src/plone/mls/listing/setuphandlers.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Setup handlers for plone.mls.listing."""
+
+# zope imports
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
+from zope.component import getUtility
+
+LISTING_TYPE = 'plone.mls.listing.listing'
+
+
+def setup_article(context):
+    """Set up raptus.article."""
+    if not context.readDataFile('plone.mls.listing_various.txt'):
+        return
+
+    site = getUtility(IPloneSiteRoot)
+    quickinstaller = getToolByName(site, 'portal_quickinstaller')
+    portal_types = getToolByName(site, 'portal_types')
+    if quickinstaller.isProductInstalled('raptus.article.core'):
+        article = portal_types.get('Article', None)
+        if article is None:
+            return
+        if LISTING_TYPE not in article.allowed_content_types:
+            article.allowed_content_types += (LISTING_TYPE, )
