7dabaeeeb130486f060d1055be06c2caef20d87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package learners/1.0.1/learners-1.0.1.tar.gz

---
+++ packages/learners/learners-1.0.1.tar.gz/learners-1.0.1/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "learners",
+    version = "1.0.1",
+    author = "Fabien Benureau",
+    author_email = "fabien.benureau@inria.fr",
+    url='flowers.inria.fr',
+    download_url='https://github.com/humm/learners/tarball/1.0',
+    maintainer='Fabien Benureau',
+    description = "A collection of simple incremental forward and inverse model learning algorithms",
+    license = "Open Science License (see fabien.benureau.com/openscience.html)",
+    keywords = "learning algorithm",
+    packages=['learners', 'learners.algorithms'],
+    install_requires=['forest','numpy'],
+    dependency_links=[
+        "https://github.com/flowersteam/forest/tarball/master#egg=forest-1.0",
+    ],
+)
