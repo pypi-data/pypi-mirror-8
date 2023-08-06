3188f17d0e9bb474e93631bd6f8a1b66f212d033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:51 +0000

    Add package django-mail-queue/2.2.0/django_mail_queue-2.2.0-py2.py3-none-any.whl

---
+++ packages/django-mail-queue/django_mail_queue-2.2.0-py2.py3-none-any.whl/django_mail_queue-2.2.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Mail Queueing for Django
+
+
---
+++ packages/django-mail-queue/django_mail_queue-2.2.0-py2.py3-none-any.whl/mailqueue/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from .models import MailerMessage, Attachment
+
+
+class AttachmentInline(admin.TabularInline):
+    model = Attachment
+    extra = 0
+
+
+class MailerAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'to_address', 'app', 'sent', 'last_attempt')
+    search_fields = ['to_address', 'subject', 'app', 'bcc_address']
+    actions = ['send_failed']
+    inlines = [AttachmentInline]
+
+    def send_failed(self, request, queryset):
+        emails = queryset.filter(sent=False)
+        for email in emails:
+            email.send_mail()
+        self.message_user(request, _("Emails queued."))
+    send_failed.short_description = _("Send failed")
+
+admin.site.register(MailerMessage, MailerAdmin)
