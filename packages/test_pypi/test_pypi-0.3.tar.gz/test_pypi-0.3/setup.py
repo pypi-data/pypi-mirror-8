496ffbf7b06c02c321d751e4b73a079be17d882e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:08 +0000

    Add package test_pypi/0.3/test_pypi-0.3.tar.gz

---
+++ packages/test_pypi/test_pypi-0.3.tar.gz/test_pypi-0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+setuptools.setup(
+    name="test_pypi",
+    version="0.3",
+    description="Test project to learn how pypi works",
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    url="comming.soon",
+    author="Emiliano G. Molina",
+    author_email="emiliano.g.molina@gmail.com",
+    license="MIT",
+    packages=["test_pypi", ],
+    install_requires=[
+        "tornado",
+    ],
+    zip_safe=False)
