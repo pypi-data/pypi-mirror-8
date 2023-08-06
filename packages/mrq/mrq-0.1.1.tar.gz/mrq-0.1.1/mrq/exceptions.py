037a1f33d1456b1149656694c48f097763e50c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:36 +0000

    Add package mrq/0.1.1/mrq-0.1.1.tar.gz

---
+++ packages/mrq/mrq-0.1.1.tar.gz/mrq-0.1.1/mrq/exceptions.py
@@ -0,0 +1,31 @@
+from gevent import GreenletExit
+
+
+# Inherits from BaseException to avoid being caught when not intended.
+class JobTimeoutException(BaseException):
+    pass
+
+
+class CancelInterrupt(BaseException):
+    pass
+
+
+# Inherits from BaseException to avoid being caught when not intended.
+class RetryInterrupt(BaseException):
+    countdown = None
+    queue = None
+
+    def __str__(self):
+        return "<RetryInterrupt %s seconds, %s queue>" % (self.countdown, self.queue)
+
+
+class StopRequested(GreenletExit):
+
+    """ Thrown in the mail greenlet to stop dequeuing jobs. """
+    pass
+
+
+class JobInterrupt(GreenletExit):
+
+    """ Interrupts that stop a job in its execution, e.g. when responding to a SIGTERM. """
+    pass
