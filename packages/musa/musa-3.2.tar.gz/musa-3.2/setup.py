1968f610e5f3ba00bc614afd0bb4d924e41daa7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:34 +0000

    Add package musa/3.2/musa-3.2.tar.gz

---
+++ packages/musa/musa-3.2.tar.gz/musa-3.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os,glob
+from setuptools import setup,find_packages
+
+VERSION='3.2'
+
+setup(
+    name = 'musa',
+    version = VERSION,
+    license = 'PSF',
+    author = 'Ilkka Tuohela',
+    author_email = 'hile@iki.fi',
+    description = 'Module for music tagging and library management',
+    long_description = open(os.path.join(os.path.dirname(__file__),'README.md'),'r').read(),
+    keywords = 'music library tag management',
+    url = 'http://tuohela.net/packages/musa',
+    zip_safe = False,
+    packages = ( 'musa', ),
+    package_data = { '': [ '*.md', '*.txt' ] },
+    scripts = glob.glob('bin/*'),
+    install_requires = ( 
+        'configobj', 
+        'soundforest>=3.2', 
+    ),
+)
+
