4cf4b1e6e1c716b31abf7358c9b7eeaeb81b35e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:27 +0000

    Add package ktcal2/0.1.3/ktcal2-0.1.3.tar.gz

---
+++ packages/ktcal2/ktcal2-0.1.3.tar.gz/ktcal2-0.1.3/setup.py
@@ -0,0 +1,28 @@
+# from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
+
+setup(
+    name='ktcal2',
+    version='0.1.3',
+    packages=find_packages(),
+    url='https://github.com/cr0hn/ktcal2',
+    install_requires=["asyncssh-unofficial"],
+    license='BSD',
+    author='cr0hn',
+    author_email='cr0hn<-at->cr0hn.com',
+    description='SSH brute forcer tool and library, using AsyncIO of Python 3.4',
+    entry_points={'console_scripts': [
+        'kt-cal2 = ktcal2.kt_cal2:main',
+    ]},
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+    ]
+)
