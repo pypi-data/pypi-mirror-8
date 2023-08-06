dc04c6da18454491bffc744730825de895020bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:48 +0000

    Add package django-facebook-pages-statistic/0.0.8/django-facebook-pages-statistic-0.0.8.tar.gz

---
+++ packages/django-facebook-pages-statistic/django-facebook-pages-statistic-0.0.8.tar.gz/django-facebook-pages-statistic-0.0.8/facebook_pages_statistic/signals.py
@@ -0,0 +1,14 @@
+from django.dispatch import receiver
+from facebook_api.signals import facebook_api_post_fetch
+from facebook_pages.models import Page
+from . models import PageStatistic
+
+
+@receiver(facebook_api_post_fetch, sender=Page)
+def page_statistic_create(sender, instance, **kwargs):
+    if instance.likes_count is None and instance.talking_about_count is None:
+        return
+
+    PageStatistic.objects.create(page=instance,
+         likes_count=instance.likes_count,
+         talking_about_count=instance.talking_about_count)
