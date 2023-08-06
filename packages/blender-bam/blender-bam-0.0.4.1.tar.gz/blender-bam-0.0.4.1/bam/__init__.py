577d90e67bb318eecc6d6b88b9b74340dad5ed42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:43 +0000

    Add package blender-bam/0.0.4.1/blender-bam-0.0.4.1.tar.gz

---
+++ packages/blender-bam/blender-bam-0.0.4.1.tar.gz/blender-bam-0.0.4.1/bam/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import sys
+__version__ = "0.0.4.1"
+
+def main(argv=sys.argv):
+    from .cli import main
+    sys.exit(main(argv[1:]))
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
