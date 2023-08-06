945eadd1353f60db76ce964b75248fb49d2b8bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:46 +0000

    Add package openBrowser/0.0.2/openBrowser-0.0.2.tar.gz

---
+++ packages/openBrowser/openBrowser-0.0.2.tar.gz/openBrowser-0.0.2/openBrowser/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+'''
+CLI helper
+Open url or files with browser in command line
+Homepage: https://github.com/keith3/myPip/openBrowser
+Author: http://lufeng.me
+'''
+
+import sys
+import webbrowser
+from time import sleep
+
+def main():
+    if '-v' in sys.argv:
+        print 'openBrowser 0.0.1'
+    elif '-h' in sys.argv:
+        print "-v show version"
+        print "-h helper"
+    else:
+        target = sys.argv[1]
+        webbrowser.open(target)
+
+        sleep(3)
+        exit()
+
+if __name__ == '__main__':
+    main()
