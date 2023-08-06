1e139b8990f551b3d2f5c15f7fc6b9383bf8bb6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:13 +0000

    Add package betfairng/0.1.5/betfairng-0.1.5.tar.gz

---
+++ packages/betfairng/betfairng-0.1.5.tar.gz/betfairng-0.1.5/betfairng/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'betfairng'
+__author__ = 'Russell Gray'
+__copyright__ = 'Copyright 2014 Russell Gray'
+
+from .betfairng import authenticate, BettingApi, AccountsApi
+from .legacyv6 import BFGlobalService, BFExchangeService
