0d77f07c32c2a2ec1db08bf2139e587d7c240b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:25 +0000

    Add package haas/0.5.0/haas-0.5.0.tar.gz

---
+++ packages/haas/haas-0.5.0.tar.gz/haas-0.5.0/haas/_version.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013-2014 Simon Jagoe
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the 3-clause BSD license.  See the LICENSE.txt file for details.
+from __future__ import absolute_import, unicode_literals
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+version = '0.5.0'
+full_version = '0.5.0'
+git_revision = '4f31df9d8a083c53b1695780a96fe6f1ba458b83'
+is_released = True
+
+if not is_released:
+    version = full_version
