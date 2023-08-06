fcc1404f1826308f44222a4b39146b20f3042fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:47 +0000

    Add package memeapi/0.2.1/memeapi-0.2.1.tar.gz

---
+++ packages/memeapi/memeapi-0.2.1.tar.gz/memeapi-0.2.1/memeapi/__init__.py
@@ -0,0 +1,17 @@
+"""
+memeapi
+-------
+
+memeapi is a library for python that wraps the memegenerator.net API.
+"""
+
+__author__ = 'Cristian Cabrera <surrealcristian@gmail.com>'
+__version__ = '0.2.1'
+
+from .api import (
+    generators_search, generators_select_by_popular, generators_select_by_new,
+    generators_select_by_trending, generators_select_related_by_display_name,
+    generators_select_by_url_name_or_generator_id, instances_select_by_popular,
+    instances_create, instances_select_by_new, instances_select,
+    content_flag_create
+)
