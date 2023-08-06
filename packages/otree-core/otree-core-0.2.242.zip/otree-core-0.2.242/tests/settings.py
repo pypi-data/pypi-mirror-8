b6a4b00f040c65e9b43e742812e5a314742c54fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:01 +0000

    Add package otree-core/0.2.242/otree-core-0.2.242.zip

---
+++ packages/otree-core/otree-core-0.2.242.zip/otree-core-0.2.242/tests/settings.py
@@ -0,0 +1,57 @@
+import os
+import otree.settings
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DEBUG = True
+
+ADMIN_PASSWORD = 'otree'
+SECRET_KEY = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+CREATE_DEFAULT_SUPERUSER = True
+ADMIN_USERNAME = 'admin'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+# e.g. EUR, CAD, GBP, CHF, CNY, JPY
+PAYMENT_CURRENCY_CODE = 'EUR'
+USE_POINTS = False
+
+
+# e.g. en-gb, de-de, it-it, fr-fr. see: https://docs.djangoproject.com/en/1.7/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+
+INSTALLED_APPS = [
+    'otree',
+    'tests',
+    'tests.demo',
+]
+
+if os.environ.has_key('SENTRY_DSN'):
+    INSTALLED_APPS += [
+        'raven.contrib.django.raven_compat',
+    ]
+
+
+INSTALLED_OTREE_APPS = [
+    'tests.simple_game',
+    'tests.simple_game_copy',
+]
+
+MIDDLEWARE_CLASSES = ()
+
+ROOT_URLCONF = 'otree.default_urls'
+
+SESSIONS_MODULE = 'tests.sessions'
+
+ACCESS_CODE_FOR_OPEN_SESSION = 'idd1610'
+
+
+otree.settings.augment_settings(globals())
