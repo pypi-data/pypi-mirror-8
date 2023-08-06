81f8099185cab6bd753b11c08e1fc4f69590450b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:12 +0000

    Add package django-mapit/1.2.1/django_mapit-1.2.1-py2-none-any.whl

---
+++ packages/django-mapit/django_mapit-1.2.1-py2-none-any.whl/mapit/middleware/__init__.py
@@ -0,0 +1,18 @@
+import re
+
+from .view_error import ViewException, ViewExceptionMiddleware  # noqa
+
+
+class JSONPMiddleware(object):
+    def process_response(self, request, response):
+        # If the response is a redirect, the callback will be dealt
+        # on the next request:
+        if response.status_code == 302:
+            return response
+        else:
+            cb = request.GET.get('callback')
+            if cb and re.match('[a-zA-Z0-9_$.]+$', cb):
+                cb = cb.encode('utf-8')
+                response.content = b'typeof ' + cb + b" === 'function' && " + cb + b'(' + response.content + b')'
+                response.status_code = 200  # Must return OK for JSONP to be processed
+            return response
